6e321c9165d8cbc6d3b7ff9587b0c659e54a3f6e
Will Schroeder <will.schroeder@kitware.com>
1994-01-11 09:43:53 -0500

    Initial revision

---
+++ src/Mapper.cc
@@ -0,0 +1,56 @@
+//
+// Methods for abstract class mapper
+//
+#include "Mapper.h"
+
+vlMapper::~vlMapper()
+{
+  if (this->Lut)
+    {
+    this->Lut->UnRegister((void *)this);
+    }
+}
+
+void vlMapper::SetStartRender(void (*f)())
+{
+  if ( f != this->StartRender )
+    {
+    this->StartRender = f;
+    this->Modified();
+    }
+}
+
+void vlMapper::SetEndRender(void (*f)())
+{
+  if ( f != this->EndRender )
+    {
+    this->EndRender = f;
+    this->Modified();
+    }
+}
+void vlMapper::SetLookupTable(vlLookupTable *lt) 
+{
+  if ( lt != this->Lut )
+    {
+    this->Lut = lt;
+    this->Lut->Register((void *)this);
+    this->Modified();
+    }
+}
+
+void vlMapper::SetScalarsVisible(int flag)
+{
+  if ( flag != this->ScalarsVisible )
+    {
+    this->ScalarsVisible = flag;
+    this->Modified();
+    }
+}
+
+int vlMapper::GetScalarsVisible()
+{
+  return this->ScalarsVisible;
+}
+
+
+
