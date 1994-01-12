746dfc8f900d596e09495568f14e78a90fe076a2
Ken Martin <ken.martin@kitware.com>
1994-01-12 07:55:00 -0500

    Initial revision

---
+++ include/Light.hh
@@ -0,0 +1,47 @@
+/*=========================================================================
+
+  Program:   OSCAR 
+  Module:    Light.hh
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Description:
+---------------------------------------------------------------------------
+This file is part of the vis library
+
+- Ken Martin
+
+=========================================================================*/
+#ifndef __vlLight_hh
+#define __vlLight_hh
+
+/* need for virtual function */
+class vlRenderer;
+
+class vlLight
+{
+ public:
+  float FocalPoint[3];
+  float Position[3];
+  float Intensity;
+  float Color[3];
+  int   OnStatus;
+
+ public:
+  vlLight();
+  virtual void Render(vlRenderer *ren,int light_index) = 0;
+
+  void SetPosition(float, float, float);
+  void SetPosition(float a[3]);
+  float *GetPosition();
+
+  void SetFocalPoint(float , float, float);
+  void SetFocalPoint(float a[3]);
+  float *GetFocalPoint();
+
+  int GetStatus();
+};
+
+#endif
+
