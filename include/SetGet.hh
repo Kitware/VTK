c985f0dac0526669be2767c9cc7d6357141bc305
Will Schroeder <will.schroeder@kitware.com>
1994-01-18 04:30:36 -0500

    ENH: Added clamping and setting objects.

--- include/SetGet.hh
+++ include/SetGet.hh
@@ -1,7 +1,20 @@
-/* for super speedy execution define __vlNoDebug */
+//
+// Macros for setting and getting instance variables.  Macros are available for 
+// built-in types; vector arrays of built-in types size 2,3,4; and for 
+// setting objects (i.e., Registering objects).  Macros enforce proper use of 
+// Debug, Modified time, and Registering objects.
+//
+
 #ifndef __vlSetGet_hh
 #define __vlSetGet_hh
+// 
+// For super speedy execution define __vlNoDebug
+//
+//#define __vlNoDebug
 
+//
+// Setting built-in type.  Creates member Set"name"() (e.g., SetVisibility());
+//
 #define vlSetMacro(name,type) \
 void Set##name (type _arg) \
   { \
@@ -13,16 +26,74 @@ void Set##name (type _arg) \
     } \
   } 
 
+//
+// Getting built-in type.  Creates member Get"name"() (e.g., GetVisibility());
+//
 #define vlGetMacro(name,type) \
 type Get##name () { \
   if (Debug) cerr << GetClassName() << " " << this << ", returning " << #name " of " << name << "\n"; \
   return name; \
   } 
 
+//
+// Set built-in type where value is constrained between min/max limits.
+// Create member Set"name"() (e.q., SetRadius()). #defines are convienience for 
+// clampping open-ended values.
+//
+#define LARGE_FLOAT 1.0e29
+#define LARGE_INTEGER 2147483646 /* 2**31 - 1 */
+#define vlSetClampMacro(name,type,min,max) \
+void Set##name (type _arg) \
+  { \
+  if (Debug) cerr << GetClassName() << " " << this << ", setting " << #name " to " << _arg << "\n"; \
+  if (name != _arg) \
+    { \
+    name = (_arg<min?min:(_arg>max?max:_arg)); \
+    Modified(); \
+    } \
+  } 
+
+//
+// Set pointer to object; uses vlObject reference counting methodology.
+// Creates method Set"name"() (e.g., SetPoints()).
+//
+#define vlSetObjectMacro(name,type) \
+void Set##name (type* _arg) \
+  { \
+  if (Debug) cerr << GetClassName() << " " << this << ", setting " << #name " to " << &_arg << "\n"; \
+  if (name != _arg) \
+    { \
+    if (name != 0) name->UnRegister((void *)this); \
+    name = _arg; \
+    name->Register((void *)this); \
+    Modified(); \
+    } \
+  } 
+
+//
+// Get pointer to object.  Creates member Get"name" (e.g., GetPoints()).
+//
+#define vlGetObjectMacro(name,type) \
+type *Get##name () \
+  { \
+  if (Debug) cerr << GetClassName() << " " << this << ", returning " << #name " address " << name << "\n"; \
+  return name; \
+  } 
+
+//
+// Create members "name"On() and "name"Off() (e.g., VisibilityOn() VisibilityOff()).
+// Set method must be defined to use this macro.
+//
 #define vlBooleanMacro(name,type) \
 void name##On () { Set##name((type)1);}; \
 void name##Off () { Set##name((type)0);}
 
+//
+// Following set macros for vectors define two members for each macro.  The first 
+// allows setting of individual components (e.g., SetColor(float,float,float)), 
+// the second allows setting from an array (e.g., SetColor(float* rgb[3])).
+// The macros vary in the size of the vector they deal with.
+//
 #define vlSetVector2Macro(name,type) \
 void Set##name (type _arg1, type _arg2) \
   { \
@@ -95,6 +166,10 @@ void Set##name (type _arg[4]) \
   name[3] = _arg[3]; \
   } 
 
+//
+// Get vector macro returns pointer to type (i.e., array of type). 
+// Example: float *GetColor()
+//
 #define vlGetVectorMacro(name,type) \
 type *Get##name () { \
   if (Debug) cerr << GetClassName() << " " << this << ", returning " << #name " pointer " << name << "\n"; \
