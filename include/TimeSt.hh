ab139214d41dfb196a5288bde7d6cd5dca2a0cda
Will Schroeder <will.schroeder@kitware.com>
1994-02-03 07:58:54 -0500

    *** empty log message ***

--- include/TimeSt.hh
+++ include/TimeSt.hh
@@ -1,3 +1,20 @@
+/*=========================================================================
+
+
+  Program:   Visualization Library
+  Module:    TimeSt.hh
+  Language:  C++
+
+Date:      $Date$
+  Version:   $Revision$
+
+This file is part of the Visualization
+Library. No part of this file or its
+contents may be copied, reproduced or
+altered in any way without the express
+written consent of the authors.
+Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
+=========================================================================*/
 //
 // Classes that need to keep track of modification / execution time use
 // this class.
@@ -8,9 +25,11 @@
 class vlTimeStamp 
 {
 public:
-  vlTimeStamp() {this->ModifiedTime = ++vlTime;};
+  vlTimeStamp() {this->ModifiedTime = 0;};
   void Modified() {this->ModifiedTime = ++vlTime;};
   unsigned long int GetMtime() {return ModifiedTime;};
+  // Using >= and <= in the operators below handles special cases when
+  // modified times are equal. Only occurs for recently instantiated objects.
   int operator>(vlTimeStamp& ts) 
     {return (this->ModifiedTime > ts.ModifiedTime);};
   int operator<(vlTimeStamp& ts) 
--- src/Object.cc
+++ src/Object.cc
@@ -1,9 +1,28 @@
+/*=========================================================================
+
+
+  Program:   Visualization Library
+  Module:    Object.cc
+  Language:  C++
+
+Date:      $Date$
+  Version:   $Revision$
+
+  This file is part of the Visualization
+Library. No part of this file or its
+contents may be copied, reproduced or
+altered in any way without the express
+written consent of the authors.
+Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
+=========================================================================*/
 #include "Object.hh"
 
 vlObject::vlObject()
 {
   this->RefCount = 0;
   this->Debug = 0;
+
+  this->Modified(); // Insures modified time > than any other time
 }
 
 vlObject::~vlObject() 
@@ -11,12 +30,11 @@ vlObject::~vlObject()
   if (this->RefCount > 0)
     {
     cerr << this->GetClassName()
-         <<": Trying to delete object with non-zero refCount\n";
+         <<": Trying to delete object with non-zero refCount\n\n";
     }
   if (this->Debug)
     {
-    cerr << this->GetClassName() << " (" << this << "):"
-         <<" Destructing!\n";
+    cerr << this->GetClassName() << " " << this <<", Destructing!\n\n";
     }
 }
 
@@ -25,8 +43,8 @@ void vlObject::Register(void *p)
   this->RefCount++;
   if ( this->Debug )
     {
-    cerr << this->GetClassName() << " (" << this << "):"
-         << " Registered by " << (void *)p << "\n";
+    cerr << this->GetClassName() << " " << this
+         << ", Registered by " << (void *)p << "\n\n";
     }
 }
 
@@ -34,8 +52,8 @@ void vlObject::UnRegister(void *p)
 {
   if ( this->Debug )
     {
-    cerr << this->GetClassName() << " (" << this << "):"
-         << " UnRegistered by " << (void *)p << "\n";
+    cerr << this->GetClassName() << " " << this
+         << ", UnRegistered by " << (void *)p << "\n\n";
     }
   if (--this->RefCount <= 0) delete this;
 }
