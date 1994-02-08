34cc314e0ee6b248c67596de1e4e30d1cc5ced9c
Ken Martin <ken.martin@kitware.com>
1994-02-08 07:29:40 -0500

    Added copyright

--- include/RenderM.hh
+++ include/RenderM.hh
@@ -1,16 +1,16 @@
 /*=========================================================================
 
-  Program:   OSCAR 
+  Program:   Visualization Library
   Module:    RenderM.hh
   Language:  C++
   Date:      $Date$
   Version:   $Revision$
 
-Description:
----------------------------------------------------------------------------
-This file is part of the vis library
+This file is part of the Visualization Library. No part of this file or its
+contents may be copied, reproduced or altered in any way without the express
+written consent of the authors.
 
-- Ken Martin
+Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
 
 =========================================================================*/
 #ifndef __vlRenderMaster_hh
@@ -24,6 +24,7 @@ class vlRenderMaster : public vlObject
  public:
   vlRenderMaster();
   char *GetClassName() {return "vlRenderMaster";};
+  // no ivars so no printself  void PrintSelf(ostream& os, vlIndent indent);
   vlRenderWindow *MakeRenderWindow(char *ren);
   vlRenderWindow *MakeRenderWindow(void);
 };
--- src/RenderM.cc
+++ src/RenderM.cc
@@ -1,16 +1,16 @@
 /*=========================================================================
 
-  Program:   VisLib
+  Program:   Visualization Library
   Module:    RenderM.cc
   Language:  C++
   Date:      $Date$
   Version:   $Revision$
 
-Description:
----------------------------------------------------------------------------
-This file is part of the vis library
+This file is part of the Visualization Library. No part of this file or its
+contents may be copied, reproduced or altered in any way without the express
+written consent of the authors.
 
-- Ken Martin
+Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
 
 =========================================================================*/
 #include <stdlib.h>
