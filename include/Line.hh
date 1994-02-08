d49f9f70e4112b6327ecfaa97738acd9e8c69fe6
Will Schroeder <will.schroeder@kitware.com>
1994-02-08 06:46:48 -0500

    Initial revision

---
+++ include/Line.hh
@@ -0,0 +1,43 @@
+/*=========================================================================
+
+  Program:   Visualization Library
+  Module:    Line.hh
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Description:
+---------------------------------------------------------------------------
+This file is part of the Visualization Library. No part of this file
+or its contents may be copied, reproduced or altered in any way
+without the express written consent of the authors.
+
+Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994 
+
+=========================================================================*/
+//
+// Create line centered at origin
+//
+#ifndef __vlLine_h
+#define __vlLine_h
+
+#include "Object.hh"
+#include "Points.hh"
+#include "CellArr.hh"
+#include "FNormals.hh"
+
+#define MAX_RESOLUTION MAX_VERTS
+
+class vlLine : public vlObject
+{
+public:
+  vlLine() {};
+  char *GetClassName() {return "vlLine";};
+
+  int GenerateNormals(vlPoints *, vlCellArray *, vlFloatNormals *);
+
+};
+
+#endif
+
+
