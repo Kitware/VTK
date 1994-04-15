db5b1df8300cf2013ceb4215e5c2cb6fcd014ed2
Will Schroeder <will.schroeder@kitware.com>
1994-04-15 06:01:33 -0400

    *** empty log message ***

--- include/vlMath.hh
+++ include/vlMath.hh
@@ -41,6 +41,12 @@ public:
   float Norm(float x[3])
     {return sqrt(x[0]*x[0] + x[1]*x[1] + x[2]*x[2]);};
 
+  void Normalize(float x[3])
+    {float den; int i;
+     if ( (den = this->Norm(x)) != 0.0 )
+        for (i=0; i < 3; i++) x[i] /= den;
+    }
+
   float Determinate3x3(float *c1, float *c2, float *c3)
     {return c1[0]*c2[1]*c3[2] + c2[0]*c3[1]*c1[2] + c3[0]*c1[1]*c2[2] -
      c1[0]*c3[1]*c2[2] - c2[0]*c1[1]*c3[2] - c3[0]*c2[1]*c1[2];};
