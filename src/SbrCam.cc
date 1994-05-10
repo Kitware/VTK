49855235b2bd43823f3e2d0dc29719eb5858c5ec
Ken Martin <ken.martin@kitware.com>
1994-05-10 04:28:49 -0400

    fixed a tricky camera problem

--- src/SbrCam.cc
+++ src/SbrCam.cc
@@ -31,6 +31,7 @@ void vlSbrCamera::Render(vlRenderer *ren)
 void vlSbrCamera::Render(vlSbrRenderer *ren)
 {
   float aspect[3];
+  float vaspect[2];
   float *viewport;
   float *background;
   int	width,height;
@@ -44,6 +45,8 @@ void vlSbrCamera::Render(vlSbrRenderer *ren)
   int *size;
   int *screen_size;
   vlSbrRenderWindow *rw;
+  float trans[2];
+  float old;
 
   fd = ren->GetFd();
 
@@ -56,13 +59,13 @@ void vlSbrCamera::Render(vlSbrRenderer *ren)
   cam.back = this->ClippingRange[1] - this->Distance;
   cam.camx = this->Position[0];
   cam.camy = this->Position[1];
-  cam.camz = this->Position[2];
+  cam.camz = - this->Position[2];
   cam.refx = this->FocalPoint[0];
   cam.refy = this->FocalPoint[1];
-  cam.refz = this->FocalPoint[2];
+  cam.refz = - this->FocalPoint[2];
   cam.upx = this->ViewUp[0];
   cam.upy = this->ViewUp[1];
-  cam.upz = this->ViewUp[2];
+  cam.upz = - this->ViewUp[2];
   cam.projection = CAM_PERSPECTIVE;
 
   // set this renderer's viewport, must turn off z-buffering when changing
@@ -82,18 +85,24 @@ void vlSbrCamera::Render(vlSbrRenderer *ren)
   // make sure the aspect is up to date
   if (size[0] > size[1])
     {
-    aspect[0] = size[0]/size[1];
+    aspect[0] = size[0]/(float)size[1];
     aspect[1] = 1.0;
     }
   else
     {
     aspect[0] = 1.0;
-    aspect[1] = size[1]/size[0];
+    aspect[1] = size[1]/(float)size[0];
     }
   ren->SetAspect(aspect);
 
-  vdc_extent(fd,0.0,-screen_size[1]/size[1]+1.0,0.0,
-	     screen_size[0]/size[0],1.0,1.0);
+  vdc_extent(fd,0.0,-screen_size[1]/(float)size[1]+1.0,0.0,
+	     screen_size[0]/(float)size[0],1.0,1.0);
+  vlDebugMacro(<< " screen_size " << screen_size[0] << " " 
+  << screen_size[1] << endl);
+
+  vlDebugMacro(<< " size " << size[0] << " " << size[1] << endl);
+  vlDebugMacro(<< " viewport " << viewport[0] << " " << viewport[1] 
+  << " " << viewport[2] << " " << viewport[3] << endl);
 
   // set viewport to clear entire window 
   view_port(fd, viewport[0], viewport[1],
@@ -117,8 +126,8 @@ void vlSbrCamera::Render(vlSbrRenderer *ren)
   hidden_surface(fd, FALSE, FALSE);
   vlDebugMacro(<< " SB_hidden_surface: False False\n");
 
-  vdc_extent(fd,0.0,-screen_size[1]/size[1]+1.0,0.0,
-	     screen_size[0]/size[0],1.0,1.0);
+  vdc_extent(fd,0.0,-screen_size[1]/(float)size[1]+1.0,0.0,
+	     screen_size[0]/(float)size[0],1.0,1.0);
   view_port(fd, viewport[0], viewport[1],
 	    viewport[2], viewport[3]);
   
@@ -128,11 +137,36 @@ void vlSbrCamera::Render(vlSbrRenderer *ren)
   // install the camera
   view_camera(fd, &cam); 
 
+  // calculate the viewport aspect
+  // we basically compensate for starbases attempt to compensate for
+  // non square windows.
+  vaspect[0] = viewport[2] - viewport[0];
+  vaspect[1] = viewport[3] - viewport[1];
+  if ((vaspect[0] >= vaspect[1])&&(aspect[0] <= aspect[1]))
+    {
+    old = vaspect[0];
+    vaspect[0] = aspect[0]*vaspect[1]/old;
+    vaspect[1] = aspect[1]*vaspect[1]/old;
+    }
+  else
+    {
+    if ((vaspect[0] <= vaspect[1])&&(aspect[0] >= aspect[1]))
+      {
+      old = vaspect[1];
+      vaspect[1] = aspect[1]*vaspect[0]/old;
+      vaspect[0] = aspect[0]*vaspect[0]/old;
+      }
+    }
+
+  // another try 
+  trans[0] = (viewport[2] + viewport[0])*(1.0 - 1.0/vaspect[0])/2.0;
+  trans[1] = (viewport[3] + viewport[1])*(1.0 - 1.0/vaspect[1])/2.0;
+
   // now handle the anisotropy 
-  xform[0][0] = 1.0/aspect[0];
-  xform[1][1] = 1.0/aspect[1];
-  xform[3][1] = (1.0 - xform[1][1])/2.0;
-  xform[3][0] = (1.0 - xform[0][0])/2.0;
+  xform[0][0] = 1.0/vaspect[0];
+  xform[1][1] = 1.0/vaspect[1];
+  xform[3][1] = trans[1];
+  xform[3][0] = trans[0];
   view_matrix3d(fd, xform, POST_CONCAT_VW);
 
   // if we have a stereo renderer, draw other eye next time 
