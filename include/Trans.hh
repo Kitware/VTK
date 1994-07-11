c66034cb0b8cb4b066b8602e6f8abccdaf9e70a1
Will Schroeder <will.schroeder@kitware.com>
1994-07-11 18:15:55 -0400

    ENH: Example comments.

--- include/Trans.hh
+++ include/Trans.hh
@@ -3,16 +3,29 @@
   Program:   Visualization Library
   Module:    Trans.hh
   Language:  C++
-  Date:      5/8/94
-  Version:   1.6
+  Date:      $Date$
+  Version:   $Revision$
 
-This file is part of the Visualization Library. No part of this file or its
-contents may be copied, reproduced or altered in any way without the express
-written consent of the authors.
+Description:
+---------------------------------------------------------------------------
+This file is part of the Visualization Library. No part of this file
+or its contents may be copied, reproduced or altered in any way
+without the express written consent of the authors.
 
-Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
+Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994 
 
 =========================================================================*/
+
+// .NAME vlTransform - a general matrix transformation class
+// .SECTION Description
+// vlTransform maintains a stack of 4x4 transformation matrices.  A
+// variety of methods are provided to manipulate the translation,
+// scale, and rotation components of the matrix.  Methods operate on
+// the transformation at the top of the stack.
+// .SECTION Caveats
+// By default the initial matrix is the identity matrix.
+// .EXAMPLE XFormSph.cc
+
 #ifndef __vlTransform_hh
 #define __vlTransform_hh
 
@@ -22,19 +35,14 @@ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
 #include "Normals.hh"
 #include "Vectors.hh"
 
-// .NAME vlTransform - a general matrix transformation class
-// .LIBRARY common
-// .HEADER Visualization Library
-// .INCLUDE common/Trans.hh
-// .FILE Trans.hh
-// .FILE Trans.cc
-
 class vlTransform : public vlObject
 {
  public:
   vlTransform ();
   vlTransform (const vlTransform& t);
   ~vlTransform ();
+  char *GetClassName () {return "vlTransform";};
+  void PrintSelf (ostream& os, vlIndent indent);
   void Identity ();
   void Pop ();
   void PostMultiply ();
@@ -55,12 +63,9 @@ class vlTransform : public vlObject
   void GetScale ( float & x, float & y, float & z);
   vlMatrix4x4 & GetMatrix ();
   void GetMatrix (vlMatrix4x4 & ctm);
-  char *GetClassName () {return "vlTransform";};
-  void PrintSelf (ostream& os, vlIndent indent);
   void Concatenate (vlMatrix4x4 & matrix);
   void Multiply4x4 ( vlMatrix4x4 & a, vlMatrix4x4 & b, vlMatrix4x4 & c);
-  void PointMultiply (float in[4],float out[4]) 
-     {this->Stack[0]->PointMultiply(in,out);};
+  void PointMultiply (float in[4],float out[4]);
   void MultiplyPoints(vlPoints *inPts, vlPoints *outPts);
   void MultiplyVectors(vlVectors *inVectors, vlVectors *outVectors);
   void MultiplyNormals(vlNormals *inNormals, vlNormals *outNormals);
@@ -76,5 +81,10 @@ class vlTransform : public vlObject
   float Orientation[3];
 
 };
+
+inline void vlTransform::PointMultiply (float in[4],float out[4]) 
+{
+  this->Stack[0]->PointMultiply(in,out);
+}
 
 #endif
