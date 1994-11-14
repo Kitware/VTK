404494840f0e403c57c94f447dce7df4d61dc262
Will Schroeder <will.schroeder@kitware.com>
1994-11-14 05:08:39 -0500

    Initial revision

---
+++ include/VArray.hh
@@ -0,0 +1,127 @@
+/*=========================================================================
+
+  Program:   Visualization Library
+  Module:    VArray.hh
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+This file is part of the Visualization Library. No part of this file
+or its contents may be copied, reproduced or altered in any way
+without the express written consent of the authors.
+
+Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994 
+
+=========================================================================*/
+// .NAME vlVoidArray - dynamic, self adjusting array of void* pointers
+// .SECTION Description
+// vlVoidArray is an array of pointers to void. It provides methods
+// for insertion and retrieval of these pointers values, and will 
+// automatically resize itself to hold new data.
+
+#ifndef __vlVoidArray_h
+#define __vlVoidArray_h
+
+#include "Object.hh"
+
+class vlVoidArray : public vlObject 
+{
+public:
+  vlVoidArray():Array(NULL),Size(0),MaxId(-1),Extend(1000) {};
+  int Allocate(const int sz, const int ext=1000);
+  void Initialize();
+  vlVoidArray(const int sz, const int ext=1000);
+  vlVoidArray(const vlVoidArray& fa);
+  ~vlVoidArray();
+  virtual char *GetClassName() {return "vlVoidArray";};
+  void PrintSelf(ostream& os, vlIndent indent);
+
+  // access/insertion methods
+  void *GetValue(const int id);
+  void** GetPtr(const int id);
+  vlVoidArray &InsertValue(const int id, void* p);
+  int InsertNextValue(void* v);
+
+  // special operators
+  vlVoidArray &operator=(const vlVoidArray& fa);
+  void operator+=(const vlVoidArray& fa);
+  void operator+=(void* p);
+  void* &operator[](const int i);
+
+  // miscellaneous methods
+  void Squeeze();
+  int GetSize();
+  int GetMaxId();
+  void** GetArray();
+  void Reset();
+
+private:
+  void* *Array;   // pointer to data
+  int Size;       // allocated size of data
+  int MaxId;     // maximum index inserted thus far
+  int Extend;     // grow array by this point
+  void** Resize(const int sz);  // function to resize data
+};
+
+// Description:
+// Get the data at a particular index.
+inline void* vlVoidArray::GetValue(const int id) {return this->Array[id];};
+
+// Description:
+// Get the address of a particular data index.
+inline void** vlVoidArray::GetPtr(const int id) {return this->Array + id;};
+
+// Description:
+// Insert data at a specified position in the array.
+inline vlVoidArray& vlVoidArray::InsertValue(const int id, void* p)
+{
+  if ( id >= this->Size ) this->Resize(id);
+  this->Array[id] = p;
+  if ( id > this->MaxId ) this->MaxId = id;
+  return *this;
+}
+
+// Description:
+// Insert data at the end of the array. Return its location in the array.
+inline int vlVoidArray::InsertNextValue(void* p)
+{
+  this->InsertValue (++this->MaxId,p);
+  return this->MaxId;
+}
+inline void vlVoidArray::operator+=(void* p) 
+{
+  this->InsertNextValue(p);
+}
+
+// Description:
+// Does insert or get (depending on location on lhs or rhs of statement). Does
+// not do automatic resizing - user's responsibility to range check.
+inline void* &vlVoidArray::operator[](const int i)
+{
+  if (i > this->MaxId) this->MaxId = i; 
+  return this->Array[i];
+}
+
+// Description:
+// Resize object to just fit data requirement. Reclaims extra memory.
+inline void vlVoidArray::Squeeze() {this->Resize (this->MaxId+1);};
+
+// Description:
+// Get the allocated size of the object in terms of number of data items.
+inline int vlVoidArray::GetSize() {return this->Size;};
+
+// Description:
+// Returning the maximum index of data inserted so far.
+inline int vlVoidArray::GetMaxId() {return this->MaxId;};
+
+// Description:
+// Get the pointer to the array. Useful for interfacing to C or 
+// FORTRAN routines.
+inline void** vlVoidArray::GetArray() {return this->Array;};
+
+// Description:
+// Reuse the memory allocated by this object. Objects appears like
+// no data has been previously inserted.
+inline void vlVoidArray::Reset() {this->MaxId = -1;};
+
+#endif
---
+++ src/VArray.cc
@@ -0,0 +1,147 @@
+/*=========================================================================
+
+  Program:   Visualization Library
+  Module:    VArray.cc
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+This file is part of the Visualization Library. No part of this file
+or its contents may be copied, reproduced or altered in any way
+without the express written consent of the authors.
+
+Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994 
+
+=========================================================================*/
+#include "VArray.hh"
+
+// Description:
+// Allocate memory for this array. Delete old storage if present.
+int vlVoidArray::Allocate(const int sz, const int ext)
+{
+  if ( this->Array ) delete [] this->Array;
+
+  this->Size = ( sz > 0 ? sz : 1);
+  if ( (this->Array = new (void *)[sz]) == NULL ) return 0;
+  this->Extend = ( ext > 0 ? ext : 1);
+  this->MaxId = -1;
+
+  return 1;
+}
+
+// Description:
+// Release storage and reset array to initial state.
+void vlVoidArray::Initialize()
+{
+  if ( this->Array != NULL )
+    {
+    delete [] this->Array;
+    this->Array = NULL;
+    }
+  this->Size = 0;
+  this->MaxId = -1;
+}
+
+// Description:
+// Construct with specified storage and extend value.
+vlVoidArray::vlVoidArray(const int sz, const int ext)
+{
+  this->Size = ( sz > 0 ? sz : 1);
+  this->Array = new (void *)[sz];
+  this->Extend = ( ext > 0 ? ext : 1);
+  this->MaxId = -1;
+}
+
+vlVoidArray::~vlVoidArray()
+{
+  delete [] this->Array;
+}
+
+// Description:
+// Construct array from another array. Copy each element of other array.
+vlVoidArray::vlVoidArray(const vlVoidArray& fa)
+{
+  int i;
+
+  this->MaxId = fa.MaxId;
+  this->Size = fa.Size;
+  this->Extend = fa.Extend;
+
+  this->Array = new (void *)[this->Size];
+  for (i=0; i<this->MaxId; i++)
+    this->Array[i] = fa.Array[i];
+
+}
+
+// Description:
+// Deep copy of another array.
+vlVoidArray& vlVoidArray::operator=(const vlVoidArray& fa)
+{
+  int i;
+
+  if ( this != &fa )
+    {
+    delete [] this->Array;
+
+    this->MaxId = fa.MaxId;
+    this->Size = fa.Size;
+    this->Extend = fa.Extend;
+
+    this->Array = new (void *)[this->Size];
+    for (i=0; i<=this->MaxId; i++)
+      this->Array[i] = fa.Array[i];
+    }
+  return *this;
+}
+
+// Description:
+// Append one array onto the end of this array.
+void vlVoidArray::operator+=(const vlVoidArray& fa)
+{
+  int i, sz;
+
+  if ( this->Size <= (sz = this->MaxId + fa.MaxId + 2) ) this->Resize(sz);
+
+  for (i=0; i<=fa.MaxId; i++)
+    {
+    this->Array[this->MaxId+1+i] = fa.Array[i];
+    }
+  this->MaxId += fa.MaxId + 1;
+}
+
+void vlVoidArray::PrintSelf(ostream& os, vlIndent indent)
+{
+  vlObject::PrintSelf(os,indent);
+
+  os << indent << "Array: " << this->Array << "\n";
+  os << indent << "Size: " << this->Size << "\n";
+  os << indent << "MaxId: " << this->MaxId << "\n";
+  os << indent << "Extend size: " << this->Extend << "\n";
+}
+
+// Protected function does "reallocate"
+//
+void** vlVoidArray::Resize(const int sz)
+{
+  void** newArray;
+  int newSize;
+
+  if (sz >= this->Size) newSize = this->Size + 
+    this->Extend*(((sz-this->Size)/this->Extend)+1);
+  else newSize = sz;
+
+  if ( (newArray = new (void *)[newSize]) == NULL )
+    { 
+    vlErrorMacro(<< "Cannot allocate memory\n");
+    return 0;
+    }
+
+  memcpy(newArray, this->Array,
+         (sz < this->Size ? sz : this->Size) * sizeof(void *));
+
+  this->Size = newSize;
+  delete [] this->Array;
+  this->Array = newArray;
+
+  return this->Array;
+}
