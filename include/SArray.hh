be7b22fd977dc318808ef6359e43fe6682c91883
Will Schroeder <will.schroeder@kitware.com>
1994-11-28 11:25:47 -0500

    *** empty log message ***

--- include/SArray.hh
+++ include/SArray.hh
@@ -6,7 +6,6 @@
   Date:      $Date$
   Version:   $Revision$
 
----------------------------------------------------------------------------
 This file is part of the Visualization Library. No part of this file
 or its contents may be copied, reproduced or altered in any way
 without the express written consent of the authors.
