41e6e8c807bf89cbaadae30779d6eb215f0d8c9e
Will Schroeder <will.schroeder@kitware.com>
1994-12-09 13:34:10 -0500

    ERR: Minor comment change.

--- src/Voxel.cc
+++ src/Voxel.cc
@@ -120,7 +120,7 @@ void vlVoxel::ShapeFunctions(float pcoords[3], float sf[8])
 static int edges[12][2] = { {0,1}, {1,3}, {3,2}, {2,0},
                             {4,5}, {5,7}, {7,6}, {6,4},
                             {0,4}, {1,5}, {2,6}, {3,7}};
-// define in terms vlRectangle understands
+// define in terms vlPixel understands
 static int faces[6][4] = { {0,2,4,6}, {1,3,5,7},
                            {0,1,4,5}, {2,3,6,7},
                            {0,1,2,3}, {4,5,6,7} };
