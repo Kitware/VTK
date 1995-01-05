ada753b6791a2d09e8d0ec56584662dc3bff8c80
Ken Martin <ken.martin@kitware.com>
1995-01-05 08:40:42 -0500

    added texture

--- src/Property.cc
+++ src/Property.cc
@@ -52,9 +52,6 @@ vlProperty::vlProperty()
   this->Representation = VL_SURFACE;
   this->EdgeVisibility = 0;
   this->Backface = 0;
-  this->Subdivide = 0;
-
-  this->Texture  = NULL;
 }
 
 
@@ -160,14 +157,5 @@ void vlProperty::PrintSelf(ostream& os, vlIndent indent)
   os << indent << "Specular Color: (" << this->SpecularColor[0] << ", " 
      << this->SpecularColor[1] << ", " << this->SpecularColor[2] << ")\n";
   os << indent << "Specular Power: " << this->SpecularPower << "\n";
-  os << indent << "Subdivide: " << (this->Subdivide ? "On\n" : "Off\n");
   os << indent << "Transparency: " << this->Transparency << "\n";
-  if ( this->Texture )
-    {
-    os << indent << "Texture: (" << (void *)this->Texture << ")\n";
-    }
-  else
-    {
-    os << indent << "Texture: (none)\n";
-    }
 }
