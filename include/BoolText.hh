1771b0dceadd1bd4da5f864b27c5e7266766f012
Will Schroeder <will.schroeder@kitware.com>
1995-01-15 16:29:50 -0500

    *** empty log message ***

--- include/BoolText.hh
+++ include/BoolText.hh
@@ -21,26 +21,111 @@ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
 // coordinates are normally generated using a filter like 
 // vlImplicitTextureCoords which generates the texture coordinates for 
 // any implicit function.
+//   vlBooleanTexture generates the map according to the s-t texture
+// coordinates plus the notion of being in, on, or outside of a
+// region. An in region is when the texture coordinate is between
+// (0,0.5-thickness/2).  An out region is where the texture coordinate
+// is (0.5+thickness/2). An on region is between
+// (0.5-thickness/2,0.5+thickness/2). The combination in, on, and out
+// for each of the s-t texture coordinates results in 8 possible
+// combinations. For each combination, a different value of intensity
+// and transparency can be assigned. To assign maximum intensity
+// and/or opacity use the value 255. A minimum value of 0 results in
+// a black region (for intensity) and a fully transparent region (for
+// transparency).
 // .SECTION See Also
 // vlImplicitTextureCoords
 
 #ifndef __vlBooleanTexture_h
 #define __vlBooleanTexture_h
 
-#include "Object.hh"
+#include "SPtsSrc.hh"
 
-class vlBooleanTexture : public vlObject //needs base class from texture object
+class vlBooleanTexture : public vlStructuredPointsSource
 {
 public:
   vlBooleanTexture();
+  ~vlBooleanTexture() {};
   char *GetClassName() {return "vlBooleanTexture";};
   void PrintSelf(ostream& os, vlIndent indent);
 
+  // Description:
+  // Set the X texture map dimension.
+  vlSetMacro(XSize,int);
+  vlGetMacro(XSize,int);
+
+  // Description:
+  // Set the Y texture map dimension.
+  vlSetMacro(YSize,int);
+  vlGetMacro(YSize,int);
+
+  // Description:
+  // Set the thickness of the "on" region.
   vlSetMacro(Thickness,int);
   vlGetMacro(Thickness,int);
 
+  // Description:
+  // Specify intensity/transparency for "in/in" region.
+  vlSetVector2Macro(InIn,unsigned char);
+  vlGetVectorMacro(InIn,unsigned char,2);
+
+  // Description:
+  // Specify intensity/transparency for "in/out" region.
+  vlSetVector2Macro(InOut,unsigned char);
+  vlGetVectorMacro(InOut,unsigned char,2);
+
+  // Description:
+  // Specify intensity/transparency for "out/in" region.
+  vlSetVector2Macro(OutIn,unsigned char);
+  vlGetVectorMacro(OutIn,unsigned char,2);
+
+  // Description:
+  // Specify intensity/transparency for "out/out" region.
+  vlSetVector2Macro(OutOut,unsigned char);
+  vlGetVectorMacro(OutOut,unsigned char,2);
+
+  // Description:
+  // Specify intensity/transparency for "on/on" region.
+  vlSetVector2Macro(OnOn,unsigned char);
+  vlGetVectorMacro(OnOn,unsigned char,2);
+
+  // Description:
+  // Specify intensity/transparency for "on/in" region.
+  vlSetVector2Macro(OnIn,unsigned char);
+  vlGetVectorMacro(OnIn,unsigned char,2);
+
+  // Description:
+  // Specify intensity/transparency for "on/out" region.
+  vlSetVector2Macro(OnOut,unsigned char);
+  vlGetVectorMacro(OnOut,unsigned char,2);
+
+  // Description:
+  // Specify intensity/transparency for "in/on" region.
+  vlSetVector2Macro(InOn,unsigned char);
+  vlGetVectorMacro(InOn,unsigned char,2);
+
+  // Description:
+  // Specify intensity/transparency for "out/on" region.
+  vlSetVector2Macro(OutOn,unsigned char);
+  vlGetVectorMacro(OutOn,unsigned char,2);
+
 protected:
+  void Execute();
+
+  int XSize;
+  int YSize;
+
   int Thickness;
+  unsigned char InIn[2];
+  unsigned char InOut[2];
+  unsigned char OutIn[2];
+  unsigned char OutOut[2];
+  unsigned char OnOn[2];
+  unsigned char OnIn[2];
+  unsigned char OnOut[2];
+  unsigned char InOn[2];
+  unsigned char OutOn[2];
+
 };
 
 #endif
--- src/BoolText.cc
+++ src/BoolText.cc
@@ -14,9 +14,80 @@ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
 
 =========================================================================*/
 #include "BoolText.hh"
+#include "AGraymap.hh"
 
 vlBooleanTexture::vlBooleanTexture()
 {
+  this->Thickness = 0;
+
+  this->XSize = this->YSize = 12;
+
+  this->InIn[0] = this->InIn[1] = 255;
+  this->InOut[0] = this->InOut[1] = 255;
+  this->OutIn[0] = this->OutIn[1] = 255;
+  this->OutOut[0] = this->OutOut[1] = 255;
+  this->OnOn[0] = this->OnOn[1] = 255;
+  this->OnIn[0] = this->OnIn[1] = 255;
+  this->OnOut[0] = this->OnOut[1] = 255;
+  this->InOn[0] = this->InOn[1] = 255;
+  this->OutOn[0] = this->OutOn[1] = 255;
+}
+
+void vlBooleanTexture::Execute()
+{
+  int numPts, i, j;
+  vlAGraymap *newScalars;
+  int midILower, midJLower, midIUpper, midJUpper;
+
+  this->Initialize();
+
+  if ( (numPts = this->XSize * this->YSize) < 1 )
+    {
+    vlErrorMacro(<<"Bad texture (xsize,ysize) specification!");
+    return;
+    }
+
+  this->SetDimensions(this->XSize,this->YSize,1);
+  newScalars = new vlAGraymap(numPts);
+//
+// Compute size of various regions
+//
+  midILower = (int) ((float)(this->XSize - 1) / 2.0 - this->Thickness / 2.0);
+  midJLower = (int) ((float)(this->YSize - 1) / 2.0 - this->Thickness / 2.0);
+  midIUpper = (int) ((float)(this->XSize - 1) / 2.0 + this->Thickness / 2.0);
+  midJUpper = (int) ((float)(this->YSize - 1) / 2.0 + this->Thickness / 2.0);
+//
+// Create texture map
+//
+  for (j = 0; j < this->YSize; j++) 
+    {
+    for (i = 0; i < this->XSize; i++) 
+      {
+      if (i < midILower && j < midJLower) 
+        newScalars->InsertNextColor(this->InIn);
+      else if (i > midIUpper && j < midJLower) 
+        newScalars->InsertNextColor(this->OutIn);
+      else if (i < midILower && j > midJUpper) 
+        newScalars->InsertNextColor(this->InOut);
+      else if (i > midIUpper && j > midJUpper) 
+        newScalars->InsertNextColor(this->OutOut);
+      else if ((i >= midILower && i <= midIUpper) && (j >= midJLower && j <= midJUpper)) 
+        newScalars->InsertNextColor(this->OnOn);
+      else if ((i >= midILower && i <= midIUpper) && j < midJLower) 
+        newScalars->InsertNextColor(this->OnIn);
+      else if ((i >= midILower && i <= midIUpper) && j > midJUpper) 
+        newScalars->InsertNextColor(this->OnOut);
+      else if (i < midILower && (j >= midJLower && j <= midJUpper)) 
+        newScalars->InsertNextColor(this->InOn);
+      else if (i > midIUpper && (j >= midJLower && j <= midJUpper)) 
+        newScalars->InsertNextColor(this->OutOn);
+      }
+    }
+
+//
+// Update ourselves
+//
+  this->PointData.SetScalars(newScalars);
 }
 
 void vlBooleanTexture::PrintSelf(ostream& os, vlIndent indent)
@@ -24,5 +95,14 @@ void vlBooleanTexture::PrintSelf(ostream& os, vlIndent indent)
   vlObject::PrintSelf(os,indent);
 
   os << indent << "Thickness: " << this->Thickness << "\n";
+  os << indent << "In/In: (" << this->InIn[0] << "," << this->InIn[1] << ")\n";
+  os << indent << "In/Out: (" << this->InOut[0] << "," << this->InOut[1] << ")\n";
+  os << indent << "Out/In: (" << this->OutIn[0] << "," << this->OutIn[1] << ")\n";
+  os << indent << "Out/Out: (" << this->OutOut[0] << "," << this->OutOut[1] << ")\n";
+  os << indent << "On/On: (" << this->OnOn[0] << "," << this->OnOn[1] << ")\n";
+  os << indent << "On/In: (" << this->OnIn[0] << "," << this->OnIn[1] << ")\n";
+  os << indent << "On/Out: (" << this->OnOut[0] << "," << this->OnOut[1] << ")\n";
+  os << indent << "In/On: (" << this->InOn[0] << "," << this->InOn[1] << ")\n";
+  os << indent << "Out/On: (" << this->OutOn[0] << "," << this->OutOn[1] << ")\n";
 }
 
