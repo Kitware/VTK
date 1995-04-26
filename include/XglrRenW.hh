508420d3717ada63446e1adfddbfe1b5d998b73d
Ken Martin <ken.martin@kitware.com>
1995-04-26 18:17:36 -0400

    added missing prototype

--- include/XglrRenW.hh
+++ include/XglrRenW.hh
@@ -53,6 +53,7 @@ public:
 
   // stereo rendering stuff
   virtual void StereoUpdate();
+  virtual void CopyResultFrame();
 
   // Description:
   // Set/Get the pixel data of an image, transmitted as RGBRGB... 
