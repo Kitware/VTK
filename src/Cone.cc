72b72df2d7d1fc187f9646015af8bce3085dcea7
Will Schroeder <will.schroeder@kitware.com>
1995-06-08 16:24:08 -0400

    ENH: Finale implementation.

--- src/Cone.cc
+++ src/Cone.cc
@@ -14,6 +14,7 @@ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
 
 =========================================================================*/
 #include "Cone.hh"
+#include "vlMath.hh"
 
 // Description
 // Construct cone with angle of 45 degrees.
@@ -26,13 +27,20 @@ vlCone::vlCone()
 // Evaluate cone equation.
 float vlCone::EvaluateFunction(float x[3])
 {
-  return 0;
+  static vlMath math;
+  float tanTheta = (float) tan((double)this->Angle*math.DegreesToRadians());
+  return x[0]*x[0] + x[1]*x[1] - x[2]*tanTheta;
 }
 
 // Description
 // Evaluate cone normal.
 void vlCone::EvaluateGradient(float x[3], float g[3])
 {
+  static vlMath math;
+  float tanTheta = (float) tan((double)this->Angle*math.DegreesToRadians());
+  g[0] = 2.0*x[0];
+  g[1] = 2.0*x[1];
+  g[2] = -tanTheta;
 }
 
 void vlCone::PrintSelf(ostream& os, vlIndent indent)
