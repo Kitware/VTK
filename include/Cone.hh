817bc1ecd54ab1a9f8beafdff126e71b2735fee9
Will Schroeder <will.schroeder@kitware.com>
1995-06-08 16:04:32 -0400

    ENH: Final definition...

--- include/Cone.hh
+++ include/Cone.hh
@@ -16,8 +16,13 @@ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
 // .NAME vlCone - implicit function for a cone
 // .SECTION Description
 // vlCone computes the implicit function and function gradient for a cone.
-// vlCone is a concrete implementation of vlImplicitFunction. The cone is
-// centered at the origin with axis of rotation coincident with z-axis.
+// vlCone is a concrete implementation of vlImplicitFunction. The cone vertex
+// is located at the origin with axis of rotation coincident with z-axis. (Use
+// a transform filter if necessary to reposition). The angle specifies the
+// angle between the axis of rotation and the side of the cone.
+// .SECTION Caveats
+// The cone is infinite in extent. To truncate the cone use the 
+// vlImplicitBoolean in combination with clipping planes.
 
 #ifndef __vlCone_h
 #define __vlCone_h
@@ -36,18 +41,12 @@ public:
   void EvaluateGradient(float x[3], float g[3]);
 
   // Description:
-  // Set/Get height of cone.
-  vlSetMacro(Height,float);
-  vlGetMacro(Height,float);
-
-  // Description:
-  // Set/Get the radius of the cone's base.
-  vlSetMacro(BaseRadius,float);
-  vlGetMacro(BaseRadius,float);
+  // Set/Get the cone angle (expressed in degrees).
+  vlSetClampMacro(Angle,float,0,89.0);
+  vlGetMacro(Angle,float);
 
 protected:
-  float Height;
-  float BaseRadius;
+  float Angle;
 
 };
 
