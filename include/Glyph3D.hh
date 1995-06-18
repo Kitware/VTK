362fd0f12c432e364b4a8cda1493e60056dd113f
Will Schroeder <will.schroeder@kitware.com>
1995-06-18 10:37:51 -0400

    *** empty log message ***

--- include/Glyph3D.hh
+++ include/Glyph3D.hh
@@ -20,6 +20,8 @@ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen 1993, 1994
 // along the input vectors or normals, and it may be scaled according
 // to scalar data or vector magnitude. The geometry is supplied via the 
 // Source instance variable; the points come from the Input.
+// .SECTION See Also
+// vlTensorEllipsoid
 
 #ifndef __vlGlyph3D_h
 #define __vlGlyph3D_h
