95835874e2c74c8ea763362ebbc519534829961d
Will Schroeder <will.schroeder@kitware.com>
1995-08-02 06:45:51 -0400

    ENH: Got it working for mace example.

--- src/vtkSweptSurface.cc
+++ src/vtkSweptSurface.cc
@@ -159,18 +159,18 @@ void vtkSweptSurface::Execute()
     else if ( this->Interpolation < 0 ) numSteps = 1;
     else numSteps = this->ComputeNumberOfSteps(transform1,transform2);
 
+    for (i=0; i<3; i++)
+      {
+      position1[i] = position2[i];
+      orient1[i] = orient2[i];
+      }
+    t.GetPosition(position2[0], position2[1], position2[2]);
+    t.GetOrientation(orient2[0], orient2[1], orient2[2]);
+
     for (stepNum=0; stepNum < numSteps; stepNum++)
       {
       // linearly interpolate position and orientation
       time = (float) stepNum / numSteps;
-
-      for (i=0; i<3; i++)
-        {
-        position1[i] = position2[i];
-        orient1[i] = orient2[i];
-        }
-      t.GetPosition(position2[0], position2[1], position2[2]);
-      t.GetOrientation(orient2[0], orient2[1], orient2[2]);
 
       for (i=0; i<3; i++)
         {
