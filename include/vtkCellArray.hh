8246322626981c63e523782dc7e03120a2a1d3a1
Will Schroeder <will.schroeder@kitware.com>
1995-08-31 04:39:41 -0400

    *** empty log message ***

--- include/vtkCellArray.hh
+++ include/vtkCellArray.hh
@@ -45,7 +45,8 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 // of the form: (n,id1,id2,...,idn, n,id1,id2,...,idn, ...)
 // where n is the number of points in the cell, and id is a zero-offset index 
 // into an associated point list.
-//    Advantages of this data structure are its compactness, simplicity, and 
+//
+// Advantages of this data structure are its compactness, simplicity, and 
 // easy interface to external data.  However, it is totally inadequate for 
 // random access.  This functionality (when necessary) is accomplished by 
 // using the vtkCellList and vtkLinkList objects to extend the definition of 
