26ca5997db4c47b07161eb2b9e9f4f23e77eae17
Will Schroeder <will.schroeder@kitware.com>
1996-04-26 12:23:22 -0400

    ENH: Fixed header

--- include/vtkExtractEdges.hh
+++ include/vtkExtractEdges.hh
@@ -1,10 +1,10 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    $$
+  Module:    vtkExtractEdges.hh
   Language:  C++
-  Date:      $$
-  Version:   $$
+  Date:      $Date$
+  Version:   $Revision$
 
 
 Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
