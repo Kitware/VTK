e9aead708f1d66e0249eba70b9b1c08e32300ddc
Will Schroeder <will.schroeder@kitware.com>
1996-06-08 09:16:30 -0400

    ENH: Added new test examples.

---
+++ examples/tcl/Cylinder.tcl
@@ -0,0 +1,41 @@
+# this is a tcl version of the Mace example
+# include get the vtk interactor ui
+source vtkInt.tcl
+
+# Create the render master
+#
+vtkRenderMaster rm;
+
+# Now create the RenderWindow, Renderer and both Actors
+#
+set renWin [rm MakeRenderWindow];
+set ren1   [$renWin MakeRenderer];
+set iren [$renWin MakeRenderWindowInteractor];
+
+vtkCylinderSource cone;
+#vtkDiskSource cone;
+#cone SetInnerRadius 1.0;
+#cone SetOuterRadius 1.0;
+vtkPolyMapper coneMapper;
+    coneMapper SetInput [cone GetOutput];
+vtkActor coneActor;
+    coneActor SetMapper coneMapper;
+
+# Add the actors to the renderer, set the background and size
+#
+$ren1 AddActors coneActor;
+$ren1 SetBackground 0.1 0.2 0.4;
+$renWin SetSize 450 450;
+
+# Get handles to some useful objects
+#
+$iren SetUserMethod {wm deiconify .vtkInteract};
+$iren Initialize;
+$renWin Render;
+set coneProp [coneActor GetProperty];
+
+# prevent the tk window from showing up then start the event loop
+wm withdraw .
+$iren Start;
+
+
---
+++ examples/tcl/MSquares.tcl
@@ -0,0 +1,55 @@
+vtkRenderMaster rm;
+
+# create a window to render into
+set renWin [rm MakeRenderWindow];
+
+# create a renderer
+set ren1 [$renWin MakeRenderer];
+
+# interactiver renderer catches mouse events (optional)
+set iren [$renWin MakeRenderWindowInteractor];
+
+# Quadric definition
+  vtkQuadric quadric;
+    quadric SetCoefficients .5 1 .2 0 .1 0 0 .2 0 0;
+
+  vtkSampleFunction sample;
+    sample SetSampleDimensions 30 30 30;
+    sample SetImplicitFunction quadric;
+    
+  vtkExtractVOI extract;
+    extract SetInput [sample GetOutput];
+    extract SetVOI 0 29 0 29 15 15;
+    extract SetSampleRate 1 2 3;
+    extract DebugOn;
+
+  vtkMarchingSquares contours;
+    contours SetInput [extract GetOutput];
+    contours GenerateValues 13 0.0 1.2;
+    contours DebugOn;
+
+  vtkPolyMapper contMapper;
+    contMapper SetInput [contours GetOutput];
+    contMapper SetScalarRange 0.0 1.2;
+
+  vtkActor contActor;
+    contActor SetMapper contMapper;
+
+# Create outline
+  vtkOutlineFilter outline;
+    outline SetInput [sample GetOutput];
+
+  vtkPolyMapper outlineMapper;
+    outlineMapper SetInput [outline GetOutput];
+
+  vtkActor outlineActor;
+    outlineActor SetMapper outlineMapper;
+    eval [outlineActor GetProperty] SetColor 0 0 0;
+
+  $ren1 SetBackground 1 1 1;
+  $ren1 AddActors contActor;
+  $ren1 AddActors outlineActor;
+
+$iren Initialize;;
+
+wm withdraw .;
