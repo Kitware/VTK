f4dbc499a0751f7b4c52a59c7df856e8c2e91010
Will Schroeder <will.schroeder@kitware.com>
1996-06-09 16:39:32 -0400

    ENH: Test dicer object.

---
+++ examples/tcl/dicer.tcl
@@ -0,0 +1,55 @@
+# get the interactor ui and colors
+source vtkInt.tcl
+source "colors.tcl"
+
+# First create the render master
+vtkRenderMaster rm;
+
+# Now create the RenderWindow, Renderer and both Actors
+#
+set renWin [rm MakeRenderWindow];
+set ren1   [$renWin MakeRenderer];
+set iren [$renWin MakeRenderWindowInteractor];
+
+# create pipeline
+#
+vtkSTLReader reader;
+    reader SetFilename ../../data/42400-IDGH.stl;
+    reader DebugOn;
+vtkDicer dicer;
+    dicer SetInput [reader GetOutput];
+    dicer SetNumberOfPointsPerPiece 1000;
+    dicer DebugOn;
+    dicer Update;
+vtkDataSetMapper isoMapper;
+    isoMapper SetInput [dicer GetOutput];
+    isoMapper SetScalarRange 0 [dicer GetNumberOfPieces];
+vtkActor isoActor;
+    isoActor SetMapper isoMapper;
+    eval [isoActor GetProperty] SetColor $raw_sienna;
+
+vtkOutlineFilter outline;
+    outline SetInput [reader GetOutput];
+vtkPolyMapper outlineMapper;
+    outlineMapper SetInput [outline GetOutput];
+vtkActor outlineActor;
+    outlineActor SetMapper outlineMapper;
+    [outlineActor GetProperty] SetColor 0 0 0;
+
+# Add the actors to the renderer, set the background and size
+#
+$ren1 AddActors outlineActor;
+$ren1 AddActors isoActor;
+$ren1 SetBackground 1 1 1;
+$renWin SetSize 750 750;
+eval $ren1 SetBackground $slate_grey;
+
+# render the image
+#
+$iren SetUserMethod {wm deiconify .vtkInteract};
+$renWin Render;
+
+# prevent the tk window from showing up then start the event loop
+wm withdraw .
+
+$iren Start;
