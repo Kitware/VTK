8c8f588c61eaf2803583639f54c6a308c7ccbc3a
Will Schroeder <will.schroeder@kitware.com>
1996-06-13 13:35:14 -0400

    ENH: Got Normal and Center working.

--- src/vtkPlaneSource.cc
+++ src/vtkPlaneSource.cc
@@ -1,41 +1,41 @@
 /*=========================================================================
 
-  Program:   Visualization Toolkit
-  Module:    PlaneSrc.cc
+  Program:   Visualization Library
+  Module:    vtkPlaneSource.cc
   Language:  C++
-  Date:      5/15/94
-  Version:   1.12
+  Date:      $Date$
+  Version:   $Revision$
 
 
 Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
 
 This software is copyrighted by Ken Martin, Will Schroeder and Bill Lorensen.
 The following terms apply to all files associated with the software unless
-explicitly disclaimed in individual files. This copyright specifically does
-not apply to the related textbook "The Visualization Toolkit" ISBN
+explicitly disclaimed in individual files. This copyright specifically does 
+not apply to the related textbook "The Visualization Toolkit" ISBN 
 013199837-4 published by Prentice Hall which is covered by its own copyright.
 
-The authors hereby grant permission to use, copy, and distribute this
-software and its documentation for any purpose, provided that existing
-copyright notices are retained in all copies and that this notice is included
-verbatim in any distributions. Additionally, the authors grant permission to
-modify this software and its documentation for any purpose, provided that
-such modifications are not distributed without the explicit consent of the
-authors and that existing copyright notices are retained in all copies. Some
-of the algorithms implemented by this software are patented, observe all
-applicable patent law.
+The authors hereby grant permission to use, copy, and distribute
+this software and its documentation for any purpose, provided
+that existing copyright notices are retained in all copies and that this
+notice is included verbatim in any distributions. Additionally, the 
+authors grant permission to modify this software and its documentation for 
+any purpose, provided that such modifications are not distributed without
+the explicit consent of the authors and that existing copyright notices are 
+retained in all copies.
 
-IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
-DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
-OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
-EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY
+FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ARISING OUT OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY
+DERIVATIVES THEREOF, EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
 
-THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
-BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
-"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
-MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
-
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE
+IS PROVIDED ON AN "AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE
+NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
+MODIFICATIONS.
 
 =========================================================================*/
 #include "vtkPlaneSource.hh"
@@ -43,6 +43,7 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 #include "vtkFloatNormals.hh"
 #include "vtkFloatTCoords.hh"
 #include "vtkMath.hh"
+#include "vtkTransform.hh"
 
 static vtkMath math;
 
@@ -67,6 +68,8 @@ vtkPlaneSource::vtkPlaneSource()
 
   this->Normal[2] = 1.0;
   this->Normal[0] = this->Normal[1] = 0.0;
+
+  this->Center[0] = this->Center[1] = this->Center[2] = 0.0;
 }
 
 // Description:
@@ -104,7 +107,7 @@ void vtkPlaneSource::Execute()
     v1[i] = this->Point1[i] - this->Origin[i];
     v2[i] = this->Point2[i] - this->Origin[i];
     }
-  if ( !this->UpdateNormal(v1,v2) ) return;
+  if ( !this->UpdatePlane(v1,v2) ) return;
 
   //
   // Set things up; allocate memory
@@ -170,20 +173,20 @@ void vtkPlaneSource::Execute()
 
 // Description:
 // Set the normal to the plane. Will modify the Origin, Point1, and Point2
-// instance variables are necessary (i.e., rotate the plane around its center).
+// instance variables as necessary (i.e., rotate the plane around its center).
 void vtkPlaneSource::SetNormal(float N[3])
 {
-  float n[3], v1[3], v2[3], center[3];
-  float rotVector[3];
+  float n[3], v1[3], v2[3], p[4];
+  float rotVector[3], theta;
   int i;
+  vtkTransform transform;
 
   // compute plane axes
   for ( i=0; i < 3; i++)
     {
     n[i] = N[i];
-    v1[i] = (this->Point1[i] - this->Origin[i]) / 2.0;
-    v2[i] = (this->Point2[i] - this->Origin[i]) / 2.0;
-    center[i] = this->Origin[i] + v1[i] + v2[i];
+    v1[i] = this->Point1[i] - this->Origin[i];
+    v2[i] = this->Point2[i] - this->Origin[i];
     }
 
   //make sure input is decent
@@ -192,25 +195,82 @@ void vtkPlaneSource::SetNormal(float N[3])
     vtkErrorMacro(<<"Specified zero normal");
     return;
     }
-  if ( !this->UpdateNormal(v1,v2) ) return;
+  if ( !this->UpdatePlane(v1,v2) ) return;
   
   //compute rotation vector
   math.Cross(this->Normal,n,rotVector);
   if ( math.Normalize(rotVector) == 0.0 ) return; //no rotation
+  theta = acos((double)math.Dot(this->Normal,n)) / math.DegreesToRadians();
 
-  // determine components of rotation around the three axes
-  
+  // create rotation matrix
+  transform.PostMultiply();
+
+  transform.Translate(-this->Center[0],-this->Center[1],-this->Center[2]);
+  transform.RotateWXYZ(theta,rotVector[0],rotVector[1],rotVector[2]);
+  transform.Translate(this->Center[0],this->Center[1],this->Center[2]);
+
+  // transform the three defining points
+  transform.SetPoint(this->Origin[0],this->Origin[1],this->Origin[2],1.0);
+  transform.GetPoint(p);
+  for (i=0; i < 3; i++) this->Origin[i] = p[i] / p[3];
+
+  transform.SetPoint(this->Point1[0],this->Point1[1],this->Point1[2],1.0);
+  transform.GetPoint(p);
+  for (i=0; i < 3; i++) this->Point1[i] = p[i] / p[3];
+
+  transform.SetPoint(this->Point2[0],this->Point2[1],this->Point2[2],1.0);
+  transform.GetPoint(p);
+  for (i=0; i < 3; i++) this->Point2[i] = p[i] / p[3];
+
+  this->Modified();
 }
 
 // Description:
 // Set the normal to the plane. Will modify the Origin, Point1, and Point2
-// instance variables are necessary (i.e., rotate the plane around its center).
+// instance variables as necessary (i.e., rotate the plane around its center).
 void vtkPlaneSource::SetNormal(float nx, float ny, float nz)
 {
   float n[3];
 
   n[0] = nx; n[1] = ny; n[2] = nz;
   this->SetNormal(n);
+}
+
+// Description:
+// Set the center of the plane. Will modify the Origin, Point1, and Point2
+// instance variables as necessary (i.e., translate the plane).
+void vtkPlaneSource::SetCenter(float center[3])
+{
+  if ( this->Center[0] == center[0] && this->Center[1] == center[1] &&
+  this->Center[2] == center[2] )
+    {
+    return; //no change
+    }
+  else
+    {
+    float d;
+
+    for ( int i=0; i < 3; i++ )
+      {
+      d = center[i] - this->Center[i];
+      this->Center[i] = center[i];
+      this->Origin[i] += d;
+      this->Point1[i] += d;
+      this->Point2[i] += d;
+      }
+    this->Modified();
+    }
+}
+
+// Description:
+// Set the center of the plane. Will modify the Origin, Point1, and Point2
+// instance variables as necessary (i.e., translate the plane).
+void vtkPlaneSource::SetCenter(float x, float y, float z)
+{
+  float center[3];
+
+  center[0] = x; center[1] = y; center[2] = z;
+  this->SetCenter(center);
 }
 
 // Description:
@@ -230,9 +290,16 @@ void vtkPlaneSource::Push(float distance)
   this->Modified();
 }
 
-// Protected method updates normals from two axes.
-int vtkPlaneSource::UpdateNormal(float v1[3], float v2[3])
+// Protected method updates normals and plane center from two axes.
+int vtkPlaneSource::UpdatePlane(float v1[3], float v2[3])
 {
+  // set plane center
+  for ( int i=0; i < 3; i++ )
+    {
+    this->Center[i] = this->Origin[i] + 0.5*(v1[i] + v2[i]);
+    }
+
+  // set plane normal
   math.Cross(v1,v2,this->Normal);
   if ( math.Normalize(this->Normal) == 0.0 )
     {
@@ -267,5 +334,9 @@ void vtkPlaneSource::PrintSelf(ostream& os, vtkIndent indent)
   os << indent << "Normal: (" << this->Normal[0] << ", "
                               << this->Normal[1] << ", "
                               << this->Normal[2] << ")\n";
+
+  os << indent << "Center: (" << this->Center[0] << ", "
+                              << this->Center[1] << ", "
+                              << this->Center[2] << ")\n";
 
 }
