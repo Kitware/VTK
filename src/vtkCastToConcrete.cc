90c7390d46db0bc9d60d55393d0261d8a587e12d
Will Schroeder <will.schroeder@kitware.com>
1996-07-17 16:37:35 -0400

    ENH: Worked around tcl wrapper limitations.

--- src/vtkCastToConcrete.cc
+++ src/vtkCastToConcrete.cc
@@ -1,6 +1,6 @@
 /*=========================================================================
 
-  Program:   Visualization Library
+  Program:   Visualization Toolkit
   Module:    vtkCastToConcrete.cc
   Language:  C++
   Date:      $Date$
@@ -11,34 +11,45 @@ Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
 
 This software is copyrighted by Ken Martin, Will Schroeder and Bill Lorensen.
 The following terms apply to all files associated with the software unless
-explicitly disclaimed in individual files. This copyright specifically does 
-not apply to the related textbook "The Visualization Toolkit" ISBN 
+explicitly disclaimed in individual files. This copyright specifically does
+not apply to the related textbook "The Visualization Toolkit" ISBN
 013199837-4 published by Prentice Hall which is covered by its own copyright.
 
-The authors hereby grant permission to use, copy, and distribute
-this software and its documentation for any purpose, provided
-that existing copyright notices are retained in all copies and that this
-notice is included verbatim in any distributions. Additionally, the 
-authors grant permission to modify this software and its documentation for 
-any purpose, provided that such modifications are not distributed without
-the explicit consent of the authors and that existing copyright notices are 
-retained in all copies.
+The authors hereby grant permission to use, copy, and distribute this
+software and its documentation for any purpose, provided that existing
+copyright notices are retained in all copies and that this notice is included
+verbatim in any distributions. Additionally, the authors grant permission to
+modify this software and its documentation for any purpose, provided that
+such modifications are not distributed without the explicit consent of the
+authors and that existing copyright notices are retained in all copies. Some
+of the algorithms implemented by this software are patented, observe all
+applicable patent law.
 
-IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY
-FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
-ARISING OUT OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY
-DERIVATIVES THEREOF, EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE
-IS PROVIDED ON AN "AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE
-NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
-MODIFICATIONS.
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
 
 =========================================================================*/
 #include "vtkCastToConcrete.hh"
+
+// Description:
+// Construct object.
+vtkCastToConcrete::vtkCastToConcrete()
+{
+  this->PolyData.SetSource(this);
+  this->StructuredPoints.SetSource(this);
+  this->StructuredGrid.SetSource(this);
+  this->UnstructuredGrid.SetSource(this);
+}
+
 
 // Description:
 // Special method just passes Update through pipeline.
@@ -70,12 +81,36 @@ void vtkCastToConcrete::Update()
   
 void vtkCastToConcrete::Execute()
 {
-  // if the input changes, need to re-execute GetOutput() method
-  if ( this->Output != NULL && this->Input != this->Output)
+  vtkDebugMacro(<<"Casting to concrete type...");
+
+  if ( ! strcmp(this->Input->GetDataType(),"vtkPolyData") )
     {
-    vtkErrorMacro(<<"Input change: invoke the appropriate GetOutput() method");
+    this->PolyData.CopyStructure(this->Input);
+    this->PolyData.GetPointData()->PassData(this->Input->GetPointData());
     }
-  this->Output = this->Input;
+
+  else if ( ! strcmp(this->Input->GetDataType(),"vtkStructuredPoints") )
+    {
+    this->StructuredPoints.CopyStructure(this->Input);
+    this->StructuredPoints.GetPointData()->PassData(this->Input->GetPointData());
+    }
+
+  else if ( ! strcmp(this->Input->GetDataType(),"vtkStructuredGrid") )
+    {
+    this->StructuredGrid.CopyStructure(this->Input);
+    this->StructuredGrid.GetPointData()->PassData(this->Input->GetPointData());
+    }
+
+  else if ( ! strcmp(this->Input->GetDataType(),"vtkUnstructuredGrid") )
+    {
+    this->UnstructuredGrid.CopyStructure(this->Input);
+    this->UnstructuredGrid.GetPointData()->PassData(this->Input->GetPointData());
+    }
+
+  else
+    {
+    this->Output = this->Input;
+    }
 }
 
 // Description:
@@ -91,28 +126,6 @@ vtkDataSet *vtkCastToConcrete::GetOutput()
 }
 
 // Description:
-// Get the output of this filter as type vtkPointSet. Performs run-time
-// checking on type. Returns NULL if wrong type.
-vtkPointSet *vtkCastToConcrete::GetPointSetOutput()
-{
-  if ( this->Input == NULL )
-    {
-    vtkErrorMacro(<<"Filter requires input to be set before output can be retrieved");
-    }
-  else
-    {
-    if ( strcmp(this->Input->GetClassName(),"vtkPolyData") &&
-    strcmp(this->Input->GetClassName(),"vtkStructuredGrid") &&
-    strcmp(this->Input->GetClassName(),"vtkUnstructuredGrid") )
-      {
-      vtkErrorMacro(<<"Cannot cast to type requested");
-      return NULL;
-      }
-    }
-  return (vtkPointSet *)this->Input;
-}
-
-// Description:
 // Get the output of this filter as type vtkPolyData. Performs run-time
 // checking on type. Returns NULL if wrong type.
 vtkPolyData *vtkCastToConcrete::GetPolyDataOutput()
@@ -123,13 +136,14 @@ vtkPolyData *vtkCastToConcrete::GetPolyDataOutput()
     }
   else
     {
-    if ( strcmp(this->Input->GetClassName(),"vtkPolyData") )
+    if ( strcmp(this->Input->GetDataType(),"vtkPolyData") )
       {
       vtkErrorMacro(<<"Cannot cast to type requested");
       return NULL;
       }
     }
-  return (vtkPolyData *)this->Input;
+
+  return &(this->PolyData);
 }
 
 // Description:
@@ -143,13 +157,14 @@ vtkStructuredPoints *vtkCastToConcrete::GetStructuredPointsOutput()
     }
   else
     {
-    if ( strcmp(this->Input->GetClassName(),"vtkStructuredPoints") )
+    if ( strcmp(this->Input->GetDataType(),"vtkStructuredPoints") )
       {
       vtkErrorMacro(<<"Cannot cast to type requested");
       return NULL;
       }
     }
-  return (vtkStructuredPoints *)this->Input;
+
+  return &(this->StructuredPoints);
 }
 
 // Description:
@@ -163,13 +178,14 @@ vtkStructuredGrid *vtkCastToConcrete::GetStructuredGridOutput()
     }
   else
     {
-    if ( strcmp(this->Input->GetClassName(),"vtkStructuredGrid") )
+    if ( strcmp(this->Input->GetDataType(),"vtkStructuredGrid") )
       {
       vtkErrorMacro(<<"Cannot cast to type requested");
       return NULL;
       }
     }
-  return (vtkStructuredGrid *)this->Input;
+
+  return &(this->StructuredGrid);
 }
 
 // Description:
@@ -183,12 +199,13 @@ vtkUnstructuredGrid *vtkCastToConcrete::GetUnstructuredGridOutput()
     }
   else
     {
-    if ( strcmp(this->Input->GetClassName(),"vtkUnstructuredGrid") )
+    if ( strcmp(this->Input->GetDataType(),"vtkUnstructuredGrid") )
       {
       vtkErrorMacro(<<"Cannot cast to type requested");
       return NULL;
       }
     }
-  return (vtkUnstructuredGrid *)this->Input;
+
+  return &(this->UnstructuredGrid);
 }
 
