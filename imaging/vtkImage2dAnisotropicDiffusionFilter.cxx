41ea789e3a6a3f3478ee81ea73909ae0e69ff5fc
Charles Law <charles.law@kitware.com>
1996-09-03 14:51:27 -0400

    Added an edge preserving diffusion filter.

---
+++ imaging/vtkImage2dAnisotropicDiffusionFilter.cxx
@@ -0,0 +1,255 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkImage2dAnisotropicDiffusionFilter.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
+
+This software is copyrighted by Ken Martin, Will Schroeder and Bill Lorensen.
+The following terms apply to all files associated with the software unless
+explicitly disclaimed in individual files. This copyright specifically does
+not apply to the related textbook "The Visualization Toolkit" ISBN
+013199837-4 published by Prentice Hall which is covered by its own copyright.
+
+The authors hereby grant permission to use, copy, and distribute this
+software and its documentation for any purpose, provided that existing
+copyright notices are retained in all copies and that this notice is included
+verbatim in any distributions. Additionally, the authors grant permission to
+modify this software and its documentation for any purpose, provided that
+such modifications are not distributed without the explicit consent of the
+authors and that existing copyright notices are retained in all copies. Some
+of the algorithms implemented by this software are patented, observe all
+applicable patent law.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+=========================================================================*/
+#include <math.h>
+#include "vtkImage2dAnisotropicDiffusionFilter.h"
+
+
+//----------------------------------------------------------------------------
+// Description:
+// Construct an instance of vtkImage2dAnisotropicDiffusionFilter fitler.
+vtkImage2dAnisotropicDiffusionFilter::vtkImage2dAnisotropicDiffusionFilter()
+{
+  this->SetAxes2d(VTK_IMAGE_X_AXIS, VTK_IMAGE_Y_AXIS);
+  
+  this->UseExecuteCenterOff();
+  this->HandleBoundariesOn();
+  this->SetNumberOfIterations(4);
+  this->DiffusionThreshold = 5.0;
+  this->DiffusionFactor = 0.3;
+}
+
+
+//----------------------------------------------------------------------------
+void 
+vtkImage2dAnisotropicDiffusionFilter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->vtkImageSpatialFilter::PrintSelf(os, indent);
+  os << indent << "NumberOfIterations: " << this->NumberOfIterations << "\n";
+  os << indent << "DiffusionThreshold: " << this->DiffusionThreshold << "\n";
+}
+
+
+
+//----------------------------------------------------------------------------
+// Description:
+// This method sets the number of inputs which also affects the
+// input neighborhood needed to compute one output pixel.
+void vtkImage2dAnisotropicDiffusionFilter::SetNumberOfIterations(int num)
+{
+  int temp;
+  
+  this->Modified();
+  vtkDebugMacro(<< "SetNumberOfIterations: " << num);
+  temp = num*2 + 1;
+  this->KernelSize[0] = temp;
+  this->KernelSize[1] = temp;
+  this->KernelMiddle[0] = num;
+  this->KernelMiddle[1] = num;
+
+  this->NumberOfIterations = num;
+}
+
+
+  
+  
+  
+  
+//----------------------------------------------------------------------------
+// Description:
+// This method contains a switch statement that calls the correct
+// templated function for the input region type.  The input and output regions
+// must have the same data type.
+void vtkImage2dAnisotropicDiffusionFilter::Execute2d(vtkImageRegion *inRegion, 
+						     vtkImageRegion *outRegion)
+{
+  int idx;
+  float ar0, ar1;
+  vtkImageRegion *in;
+  vtkImageRegion *out;
+  vtkImageRegion *temp;
+
+  inRegion->GetAspectRatio2d(ar0, ar1);
+  
+  // make the temporary regions to iterate over.
+  in = new vtkImageRegion;
+  out = new vtkImageRegion;
+  
+  // might as well make these floats
+  in->SetBounds(inRegion->GetBounds());
+  in->SetDataType(VTK_IMAGE_FLOAT);
+  in->CopyRegionData(inRegion);
+  out->SetBounds(inRegion->GetBounds());
+  out->SetDataType(VTK_IMAGE_FLOAT);
+  out->Allocate();
+  
+  // Loop performing the diffusion
+  // Note: region bounds could get smaller as the diffusion progresses
+  // (but never get smaller than output region).
+  for (idx = 0; idx < this->NumberOfIterations; ++idx)
+    {
+    this->Iterate(in, out, ar0, ar1);
+    temp = in;
+    in = out;
+    out = temp;
+    }
+  
+  // copy results into output.
+  outRegion->CopyRegionData(in);
+}
+
+
+
+
+
+//----------------------------------------------------------------------------
+// Description:
+// This method performs one pass of the diffusion filter.
+// The inRegion and outRegion are assumed to have data type float,
+// and have the same bounds.
+void vtkImage2dAnisotropicDiffusionFilter::Iterate(vtkImageRegion *inRegion, 
+						   vtkImageRegion *outRegion,
+						   float ar0, float ar1)
+{
+  int idx0, idx1;
+  int inInc0, inInc1;
+  int outInc0, outInc1;
+  int min0, max0, min1, max1;
+  float *inPtr0, *inPtr1;
+  float *outPtr0, *outPtr1;
+  float ar01, diff;
+
+  inRegion->GetBounds2d(min0, max0, min1, max1);
+  inRegion->GetIncrements2d(inInc0, inInc1);
+  outRegion->GetIncrements2d(outInc0, outInc1);
+  ar01 = sqrt(ar0 * ar0 + ar1 * ar1);
+  inPtr1 = (float *)(inRegion->GetVoidPointer2d());
+  outPtr1 = (float *)(outRegion->GetVoidPointer2d());
+  for (idx1 = min1; idx1 <= max1; ++idx1, inPtr1+=inInc1, outPtr1+=outInc1)
+    {
+    inPtr0 = inPtr1;
+    outPtr0 = outPtr1;    
+    for (idx0 = min0; idx0 <= max0; ++idx0, inPtr0+=inInc0, outPtr0+=outInc0)
+      {
+      // Copy center
+      *outPtr0 = *inPtr0;
+      // Start diffusing
+      // left
+      if (idx0 != min0)
+	{
+	diff = inPtr0[-inInc0] - *inPtr0;
+	if (fabs(diff) < ar0 * this->DiffusionThreshold)
+	  {
+	  *outPtr0 += diff * this->DiffusionFactor;
+	  }
+	}
+      // right
+      if (idx0 != max0)
+	{
+	diff = inPtr0[inInc0] - *inPtr0;
+	if (fabs(diff) < ar0 * this->DiffusionThreshold)
+	  {
+	  *outPtr0 += diff * this->DiffusionFactor;
+	  }
+	}
+      // up
+      if (idx1 != min1)
+	{
+	diff = inPtr0[-inInc1] - *inPtr0;
+	if (fabs(diff) < ar1 * this->DiffusionThreshold)
+	  {
+	  *outPtr0 += diff * this->DiffusionFactor;
+	  }
+	}
+      // down
+      if (idx1 != max1)
+	{
+	diff = inPtr0[inInc1] - *inPtr0;
+	if (fabs(diff) < ar1 * this->DiffusionThreshold)
+	  {
+	  *outPtr0 += diff * this->DiffusionFactor;
+	  }
+	}
+      // upper left
+      if (idx0 != min0 && idx1 != min1)
+	{
+	diff = inPtr0[-inInc0-inInc1] - *inPtr0;
+	if (fabs(diff) < ar01 * this->DiffusionThreshold)
+	  {
+	  *outPtr0 += diff * this->DiffusionFactor;
+	  }
+	}
+      // upper right
+      if (idx0 != max0 && idx1 != min1)
+	{
+	diff = inPtr0[inInc0-inInc1] - *inPtr0;
+	if (fabs(diff) < ar01 * this->DiffusionThreshold)
+	  {
+	  *outPtr0 += diff * this->DiffusionFactor;
+	  }
+	}
+      // lower left
+      if (idx0 != min0 && idx1 != max1)
+	{
+	diff = inPtr0[-inInc0+inInc1] - *inPtr0;
+	if (fabs(diff) < ar01 * this->DiffusionThreshold)
+	  {
+	  *outPtr0 += diff * this->DiffusionFactor;
+	  }
+	}
+      // lower right
+      if (idx0 != max0 && idx1 != max1)
+	{
+	diff = inPtr0[inInc0+inInc1] - *inPtr0;
+	if (fabs(diff) < ar01 * this->DiffusionThreshold)
+	  {
+	  *outPtr0 += diff * this->DiffusionFactor;
+	  }
+	}
+      }
+    }
+}
+
+
+
+
+
+
+
+
---
+++ imaging/vtkImage2dAnisotropicDiffusionFilter.h
@@ -0,0 +1,89 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkImage2dAnisotropicDiffusionFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
+
+This software is copyrighted by Ken Martin, Will Schroeder and Bill Lorensen.
+The following terms apply to all files associated with the software unless
+explicitly disclaimed in individual files. This copyright specifically does
+not apply to the related textbook "The Visualization Toolkit" ISBN
+013199837-4 published by Prentice Hall which is covered by its own copyright.
+
+The authors hereby grant permission to use, copy, and distribute this
+software and its documentation for any purpose, provided that existing
+copyright notices are retained in all copies and that this notice is included
+verbatim in any distributions. Additionally, the authors grant permission to
+modify this software and its documentation for any purpose, provided that
+such modifications are not distributed without the explicit consent of the
+authors and that existing copyright notices are retained in all copies. Some
+of the algorithms implemented by this software are patented, observe all
+applicable patent law.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+=========================================================================*/
+// .NAME vtkImage2dAnisotropicDiffusionFilter - edge preserving smoothing.
+// .SECTION Description
+// vtkImage2dAnisotropicDiffusionFilter  diffuses if pixel
+// difference is below a threshold.  It diffuses with all 8 neighbors.
+// Input and output can be any type.
+
+
+#ifndef __vtkImage2dAnisotropicDiffusionFilter_h
+#define __vtkImage2dAnisotropicDiffusionFilter_h
+
+
+#include "vtkImageSpatialFilter.h"
+
+class vtkImage2dAnisotropicDiffusionFilter : public vtkImageSpatialFilter
+{
+public:
+  vtkImage2dAnisotropicDiffusionFilter();
+  char *GetClassName() {return "vtkImage2dAnisotropicDiffusionFilter";};
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  void SetNumberOfIterations(int num);
+  // Description:
+  // Get the number of iterations.
+  vtkGetMacro(NumberOfIterations,int);
+
+  // Description:
+  // Set/Get the difference threshold that stops diffusion.
+  vtkSetMacro(DiffusionThreshold,float);
+  vtkGetMacro(DiffusionThreshold,float);
+  
+  
+  // Description:
+  // Set/Get the difference factor
+  vtkSetMacro(DiffusionFactor,float);
+  vtkGetMacro(DiffusionFactor,float);
+  
+  
+protected:
+  int NumberOfIterations;
+  float DiffusionThreshold;
+  float DiffusionFactor;  
+  
+  void Execute2d(vtkImageRegion *inRegion, vtkImageRegion *outRegion);
+  void Iterate(vtkImageRegion *in, vtkImageRegion *out, float ar0, float ar1);
+};
+
+#endif
+
+
+
