c06424be715446ed06f34a46769804374645a163
Charles Law <charles.law@kitware.com>
1996-09-09 15:46:02 -0400

    3d Mag new filter...

---
+++ imaging/vtkImage3dMagnifyFilter.h
@@ -0,0 +1,81 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkImage3dMagnifyFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
+
+This software is copyrighted by Ken Martin, Will Schroeder and Bill Lorensen.
+The following terms apply to all files associated with the software unless
+explicitly disclaimed in individual files. This copyright specifically does
+not apply to the related textbook "The Visualization Toolkit" ISBN
+013199837-4 published by Prentice Hall which is covered by its own copyright.
+
+The authors hereby grant permission to use, copy, and distribute this
+software and its documentation for any purpose, provided that existing
+copyright notices are retained in all copies and that this notice is included
+verbatim in any distributions. Additionally, the authors grant permission to
+modify this software and its documentation for any purpose, provided that
+such modifications are not distributed without the explicit consent of the
+authors and that existing copyright notices are retained in all copies. Some
+of the algorithms implemented by this software are patented, observe all
+applicable patent law.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+=========================================================================*/
+// .NAME vtkImage3dMagnifyFilter - Magnifies an image by integer factors.
+// .SECTION Description
+// vtkImage3dMagnifyFilter maps each pixel of the input onto a nxmxp region
+// of the output.  Location (0,0) remains in the same place.  The filter
+// can use trilinear interpolation or simple pixel replication. 
+
+
+#ifndef __vtkImage3dMagnifyFilter_h
+#define __vtkImage3dMagnifyFilter_h
+
+
+#include "vtkImage3dDecomposedFilter.h"
+#include "vtkImage1dMagnifyFilter.h"
+
+class vtkImage3dMagnifyFilter : public vtkImage3dDecomposedFilter
+{
+public:
+  vtkImage3dMagnifyFilter();
+  char *GetClassName() {return "vtkImage3dMagnifyFilter";};
+
+  // Description:
+  // Set/Get Magnification factors
+  void SetMagnificationFactors(int f0, int f1, int f2);
+  void SetMagnificationFactors(int *factors)
+  {this->SetMagnificationFactors(factors[0], factors[1], factors[2]);};
+  vtkGetVector3Macro(MagnificationFactors,int);
+  
+  // Description:
+  // Turn interpolation on and off (pixel replication)
+  void SetInterpolate(int interpolate);
+  int GetInterpolate();
+  vtkBooleanMacro(Interpolate,int);
+  
+
+protected:
+  int MagnificationFactors[3];
+};
+
+#endif
+
+
+
