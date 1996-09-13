ca75bf982f575213b1c002d96832c8d0b5327bb1
Charles Law <charles.law@kitware.com>
1996-09-13 09:21:10 -0400

    Initilialization

--- graphics/vtkStructuredPointsToImage.cxx
+++ graphics/vtkStructuredPointsToImage.cxx
@@ -42,6 +42,7 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 //----------------------------------------------------------------------------
 vtkStructuredPointsToImage::vtkStructuredPointsToImage()
 {
+  this->Input = NULL;
 }
 
 //----------------------------------------------------------------------------
