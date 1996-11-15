856bf7917bf905453b0378f3bd47e856d4174c03
LYMB Demo <lymbdemo@crd.ge.com>
1996-11-15 15:12:13 -0500

    Changed SetWindowName from a macro to an actual method.

--- graphics/vtkRenderWindow.cxx
+++ graphics/vtkRenderWindow.cxx
@@ -103,6 +103,27 @@ vtkRenderWindow::~vtkRenderWindow()
     delete [] this->WindowName;
 }
 
+void vtkRenderWindow::SetWindowName( char * _arg )
+{
+  if (Debug)   
+    cerr << "Debug: In " __FILE__ << ", line " << __LINE__ << "\n" 
+         << this->GetClassName() << " (" << this << "): setting " 
+         << WindowName  << " to " << _arg << "\n\n";
+
+  if ( WindowName && _arg && (!strcmp(WindowName,_arg))) return;
+  if (WindowName) delete [] WindowName;
+  if( _arg )
+    {
+    WindowName = new char[strlen(_arg)+1];
+    strcpy(WindowName,_arg);
+    }
+  else
+    {
+    WindowName = NULL;
+    }
+  this->Modified();
+}
+
 void vtkRenderWindow::SetDesiredUpdateRate(float rate)
 {
   vtkRenderer *aren;
