6bab36c1777752aa6ae6571547730b7799bd3ec4
LYMB Demo <lymbdemo@crd.ge.com>
1997-02-24 17:44:27 -0500

    Fixed sytax error in comment.  Was using c++ // notation. Changed to  /* ... */.

--- tcl/kit_init.c
+++ tcl/kit_init.c
@@ -5,7 +5,7 @@ char *names[1000];
 char *kitName;
 int anindex = 0;
 
-// warning this code is also in getclasses.cxx under pcmaker
+/* warning this code is also in getclasses.cxx under pcmaker */
 void stuffit()
 {
   int i;
