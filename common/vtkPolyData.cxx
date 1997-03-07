0c2531485361c892594fd4882d79f4a30fab03a0
Will Schroeder <will.schroeder@kitware.com>
1997-03-07 07:10:52 -0500

    ERR: Changed NULLCell to EmptyCell

--- common/vtkPolyData.cxx
+++ common/vtkPolyData.cxx
@@ -47,7 +47,7 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 #include "vtkTriangleStrip.h"
 #include "vtkQuad.h"
 #include "vtkPolygon.h"
-#include "vtkNULLCell.h"
+#include "vtkEmptyCell.h"
 
 // Initialize static member.  This member is used to simplify traversal
 // of verts, lines, polygons, and triangle strips lists.  It basically 
@@ -137,7 +137,7 @@ vtkCell *vtkPolyData::GetCell(int cellId)
   static vtkTriangleStrip strip;
   static vtkPolygon poly;
   static vtkQuad quad;
-  static vtkNULLCell NULLCell;
+  static vtkEmptyCell EmptyCell;
   int i, loc, numPts, *pts;
   vtkCell *cell = NULL;
   unsigned char type;
@@ -194,7 +194,7 @@ vtkCell *vtkPolyData::GetCell(int cellId)
       break;
 
     default:
-      return &NULLCell;
+      return &EmptyCell;
     }
 
   int last = numPts - 1;
