1695475d9f3366c1b8acaa880f1185b271f12513
Bill Lorensen <lorensen@crd.ge.com>
1997-04-07 13:45:39 -0400

    ERR: in constructor... string was assigned to FilePrefix. Changed to strdup of same string.

--- graphics/vtkVolumeReader.cxx
+++ graphics/vtkVolumeReader.cxx
@@ -46,7 +46,7 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 vtkVolumeReader::vtkVolumeReader()
 {
   this->FilePrefix = NULL;
-  this->FilePattern = "%s.%d";
+  this->FilePattern = strdup ("%s.%d");
   this->ImageRange[0] = this->ImageRange[1] = 1;
   this->DataOrigin[0] = this->DataOrigin[1] = this->DataOrigin[2] = 0.0;
   this->DataAspectRatio[0] = this->DataAspectRatio[1] = this->DataAspectRatio[2] = 1.0;
