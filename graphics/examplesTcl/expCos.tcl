2ff38979203833a52ea2d9e7258adc9e99e67f8b
Will Schroeder <will.schroeder@kitware.com>
1997-04-17 15:05:41 -0400

    ENH: Bessel function evaluation

---
+++ graphics/examplesTcl/expCos.tcl
@@ -0,0 +1,82 @@
+catch {load vtktcl}
+
+# get the interactor ui
+source vtkInt.tcl
+
+# create pipeline
+#
+# create plane to warp
+vtkPlaneSource plane
+    plane SetXResolution 200
+    plane SetYResolution 200
+vtkTransform transform
+   transform Scale 10 10 1
+vtkTransformPolyFilter transF
+   transF SetInput [plane GetOutput]
+   transF SetTransform transform
+   transF Update
+
+# compute Bessel function and derivatives. This portion could be 
+# encapsulated into source or filter object.
+#
+set output [transF GetOutput]
+set numPts [$output GetNumberOfPoints]
+vtkFloatPoints newPts
+vtkFloatScalars derivs
+vtkPolyData bessel
+    bessel CopyStructure $output
+    bessel SetPoints newPts
+    [bessel GetPointData] SetScalars derivs
+
+for {set i 0} {$i < $numPts} {incr i} {
+    set x [$output GetPoint $i]
+    set x0 [lindex $x 0]
+    set x1 [lindex $x 1]
+
+    set r [expr sqrt($x0*$x0 + $x1*$x1)]
+    set x2 [expr exp(-$r) * cos(10.0*$r)]
+    set deriv [expr -exp(-$r) * (cos(10.0*$r) + 10.0*sin(10.0*$r))]
+
+    newPts InsertPoint $i $x0 $x1 $x2
+    eval derivs InsertScalar $i $deriv
+}
+
+newPts Delete; #reference counting - it's ok
+derivs Delete;
+
+# warp plane
+vtkWarpScalar warp
+    warp SetInput bessel
+    warp XYPlaneOn
+    warp SetScaleFactor 0.5
+
+# mapper and actor
+vtkDataSetMapper mapper
+    mapper SetInput [warp GetOutput]
+    eval mapper SetScalarRange [$output GetScalarRange]
+vtkActor carpet
+    carpet SetMapper mapper
+
+# assign our actor to the renderer
+
+# Create graphics stuff
+#
+vtkRenderMaster rm
+
+# Now create the RenderWindow, Renderer and both Actors
+#
+set renWin [rm MakeRenderWindow]
+set ren1   [$renWin MakeRenderer]
+set iren [$renWin MakeRenderWindowInteractor]
+
+$ren1 AddActors carpet
+$renWin SetSize 500 500
+
+# render the image
+#
+$iren SetUserMethod {wm deiconify .vtkInteract}
+$renWin Render
+
+# prevent the tk window from showing up then start the event loop
+wm withdraw .
+
