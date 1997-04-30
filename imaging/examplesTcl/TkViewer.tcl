7ba15410df67180b5b72fcc8b598bffb3540f517
Ken Martin <ken.martin@kitware.com>
1997-04-30 15:11:46 -0400

    uses new TkImageViewerWidget

---
+++ imaging/examplesTcl/TkViewer.tcl
@@ -0,0 +1,40 @@
+catch {load vtktcl}
+
+source vtkImageInclude.tcl
+
+# This script uses a vtkTkRenderWidget to create a
+# Tk widget that is associated with a vtkRenderWindow.
+#source TkInteractor.tcl
+
+vtkImageFileReader reader
+reader ReleaseDataFlagOff
+reader SetAxes $VTK_IMAGE_COMPONENT_AXIS $VTK_IMAGE_X_AXIS $VTK_IMAGE_Y_AXIS 
+reader SetDataDimensions 3 512 256
+reader SetFileName "../../../data/earth.ppm"
+reader SetDataScalarType $VTK_UNSIGNED_CHAR
+
+vtkImageViewer viewer
+viewer SetAxes $VTK_IMAGE_X_AXIS $VTK_IMAGE_Y_AXIS $VTK_IMAGE_COMPONENT_AXIS
+viewer SetInput [reader GetOutput]
+viewer SetColorWindow 256
+viewer SetColorLevel 128
+viewer ColorFlagOn
+
+# Create the GUI: two renderer widgets and a quit button
+#
+wm withdraw .
+toplevel .top 
+
+frame .top.f1 
+
+vtkTkImageViewerWidget .top.f1.r1 -width 300 -height 300 -iv viewer
+#    BindTkRenderWidget .top.f1.r1
+
+button .top.btn  -text Quit -command exit
+
+pack .top.f1.r1 -side left -padx 3 -pady 3 -fill both -expand t
+pack .top.f1  -fill both -expand t
+pack .top.btn -fill x
+
+
+
