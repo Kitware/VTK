6e05d6c093607da5c6f8bd71e8b3d7fd95c2b4ff
Lisa Avila <lisa.avila@kitware.com>
1997-06-29 21:06:48 -0400

    Fixed circular dependency problems in the include files

--- graphics/vtkVolumeCollection.h
+++ graphics/vtkVolumeCollection.h
@@ -1,7 +1,46 @@
+/*=========================================================================
 
-// .NAME vtkNewVolumeCollection - a list of new volumes
+  Program:   Visualization Toolkit
+  Module:    vtkVolumeCollection.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-1996 Ken Martin, Will Schroeder, Bill Lorensen.
+
+This software is copyrighted by Ken Martin, Will Schroeder and Bill Lorensen.
+The following terms apply to all files associated with the software unless
+explicitly disclaimed in individual files. This copyright specifically does
+not apply to the related textbook "The Visualization Toolkit" ISBN
+013199837-4 published by Prentice Hall which is covered by its own copyright.
+
+The authors hereby grant permission to use, copy, and distribute this
+software and its documentation for any purpose, provided that existing
+copyright notices are retained in all copies and that this notice is included
+verbatim in any distributions. Additionally, the authors grant permission to
+modify this software and its documentation for any purpose, provided that
+such modifications are not distributed without the explicit consent of the
+authors and that existing copyright notices are retained in all copies. Some
+of the algorithms implemented by this software are patented, observe all
+applicable patent law.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+=========================================================================*/
+// .NAME vtkVolumeCollection - a list of new volumes
 // .SECTION Description
-// vtkNewVolumeCollection represents and provides methods to manipulate a 
+// vtkVolumeCollection represents and provides methods to manipulate a 
 // list of volumes (i.e., vtkVolume and subclasses). The list is unsorted 
 // and duplicate entries are not prevented.
 
@@ -12,7 +51,6 @@
 #define __vtkVolumeC_h
 
 #include "vtkCollection.h"
-#include "vtkVolume.h"
 
 class vtkVolume;
 
