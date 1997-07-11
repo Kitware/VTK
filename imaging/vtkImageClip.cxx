b791717284a2dc71f50cee03343b6e1022b625f9
Ken Martin <ken.martin@kitware.com>
1997-07-11 13:07:33 -0400

    bug fix

--- imaging/vtkImageClip.cxx
+++ imaging/vtkImageClip.cxx
@@ -253,7 +253,7 @@ static void vtkImageClipAxis(vtkImageRegion *region, T backGround)
   int flag;  // signals loop to stop after non background pixel.
   
   region->GetExtent(min0,max0, min1,max1, min2,max2, min3,max3, min4,max4);
-  region->GetIncrements(inc0, inc1, inc2, inc3);
+  region->GetIncrements(inc0, inc1, inc2, inc3, inc4);
   // loop to find min
   ptr0 = (T *)(region->GetScalarPointer(min0, min1, min2, min3, min4));
   flag = 1;
@@ -398,7 +398,8 @@ static void vtkImageClipCompute(vtkImageRegion *region, T *ptr)
   
   // We have a backGround value, now shrink the axes one by one.
   region->GetAxes(5, axes);
-  for (idx = 0; idx < 5; ++ idx)
+  // do not bother with component axis
+  for (idx = 0; idx < 4; ++ idx)
     {
     region->SetAxes(axes[idx]);
     vtkImageClipAxis(region, backGround);
@@ -460,6 +461,8 @@ void vtkImageClip::ComputeOutputWholeExtent()
   this->CTime.Modified();
   region->Delete();
 }
+
+
 
 
 
