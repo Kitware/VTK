dbb1889534cee1d3f5baa102f6f4d5956d064e69
Charles Law <charles.law@kitware.com>
1997-07-16 12:00:31 -0400

    aslklf aaaa

--- examplesTcl/StructuredPointsToImage.tcl
+++ examplesTcl/StructuredPointsToImage.tcl
@@ -9,25 +9,14 @@ vtkVolume16Reader v16
     v16 SetDataDimensions 256 256
     v16 SetFilePrefix "../../data/fullHead/headsq";
     v16 SetDataOrigin -127.5 -127.5 5.33
-    v16 SetImageRange 1 4
+    v16 SetImageRange 1 94
     v16 SetDataSpacing 1 1 5.33
     v16 SwapBytesOn
     v16 Update
 
 vtkStructuredPointsToImage sti
 sti SetInput [v16 GetOutput]
-sti Update
-
-puts [[sti GetOutput] Print]
-exit
-
-#vtkImageAnisotropicDiffusion2d diffusion;
-#diffusion SetInput [sti GetOutput];
-#diffusion SetDiffusionFactor 0.15;
-#diffusion SetDiffusionThreshold 20.0;
-#diffusion SetNumberOfIterations 10;
-#diffusion ReleaseDataFlagOff;
-
+#sti Update
 
 vtkImageXViewer viewer;
 #viewer DebugOn;
