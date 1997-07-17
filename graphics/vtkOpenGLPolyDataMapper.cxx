ea6a38e1ac529cc6a54fe7fc8eeb5d31d8d8dcff
Ken Martin <ken.martin@kitware.com>
1997-07-17 17:30:47 -0400

    wireframe tristrip fix

--- graphics/vtkOpenGLPolyDataMapper.cxx
+++ graphics/vtkOpenGLPolyDataMapper.cxx
@@ -347,7 +347,7 @@ void vtkOpenGLPolyDataMapper::Draw(vtkRenderer *aren, vtkActor *act)
 		vtkTriangle::ComputeNormal(p, 3, idx, polyNorm);
 		}
 	      }
-	    else if ( j == 0 )
+  	    else if ( j == 0 )
 	      {
 	      vtkTriangle::ComputeNormal(p, 3, pts, polyNorm);
 	      }
@@ -378,9 +378,13 @@ void vtkOpenGLPolyDataMapper::Draw(vtkRenderer *aren, vtkActor *act)
 	    }
 	  else 
 	    {
-	    if ( j && j < (npts-1) )
+  	    if ( j == 0 )
 	      {
-              idx[0] = pts[j-1]; idx[1] = pts[j]; idx[2] = pts[j+1]; 
+	      vtkTriangle::ComputeNormal(p, 3, pts, polyNorm);
+	      }
+	    else
+	      {
+              idx[0] = pts[j-2]; idx[1] = pts[j-1]; idx[2] = pts[j]; 
               vtkTriangle::ComputeNormal(p, 3, idx, polyNorm);
 	      }
 	    glNormal3fv(polyNorm);
@@ -410,9 +414,13 @@ void vtkOpenGLPolyDataMapper::Draw(vtkRenderer *aren, vtkActor *act)
 	    }
 	  else 
 	    {
-	    if (j < npts-1)
+	    if (j == 1)
 	      {
-              idx[0] = pts[j+1]; idx[1] = pts[j]; idx[2] = pts[j-1]; 
+	      vtkTriangle::ComputeNormal(p, 3, pts, polyNorm);
+	      }
+	    else
+	      {
+              idx[0] = pts[j-2]; idx[1] = pts[j]; idx[2] = pts[j-1]; 
               vtkTriangle::ComputeNormal(p, 3, idx, polyNorm);
 	      }
 	    glNormal3fv(polyNorm);
