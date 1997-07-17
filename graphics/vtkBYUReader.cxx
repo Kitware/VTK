62655ff045b3fe43ea66cd57a872bfc80f1dc19c
Bill Lorensen <lorensen@crd.ge.com>
1997-07-17 17:17:49 -0400

    ERR: not closing files.

--- graphics/vtkBYUReader.cxx
+++ graphics/vtkBYUReader.cxx
@@ -79,6 +79,7 @@ void vtkBYUReader::Execute()
   else
     {
     this->ReadGeometryFile(geomFp,numPts);
+    fclose(geomFp);
     }
 
   this->ReadDisplacementFile(numPts);
@@ -202,6 +203,7 @@ void vtkBYUReader::ReadDisplacementFile(int numPts)
     newVectors->SetVector(i,v);
     }
 
+  fclose(dispFp);
   vtkDebugMacro(<<"Read " << numPts << " displacements");
 
   output->GetPointData()->SetVectors(newVectors);
@@ -237,6 +239,7 @@ void vtkBYUReader::ReadScalarFile(int numPts)
     newScalars->SetScalar(i,s);
     }
 
+  fclose(scalarFp);
   vtkDebugMacro(<<"Read " << numPts << " scalars");
 
   output->GetPointData()->SetScalars(newScalars);
@@ -272,6 +275,7 @@ void vtkBYUReader::ReadTextureFile(int numPts)
     newTCoords->SetTCoord(i,t);
     }
 
+  fclose(textureFp);
   vtkDebugMacro(<<"Read " << numPts << " texture coordinates");
 
   output->GetPointData()->SetTCoords(newTCoords);
