8e6e335c5381cbd9d7b131fb6c6dd48f6e614cc4
Bill Lorensen <lorensen@crd.ge.com>
1997-07-17 06:55:51 -0400

    ERR: imporperly computing MOdelBounds

--- graphics/vtkImplicitModeller.cxx
+++ graphics/vtkImplicitModeller.cxx
@@ -280,7 +280,7 @@ void vtkImplicitModeller::Update()
 // Compute ModelBounds from input geometry.
 float vtkImplicitModeller::ComputeModelBounds()
 {
-  float *bounds, maxDist, h[3];
+  float *bounds, maxDist;
   int i;
   vtkStructuredPoints *output=this->GetOutput();
   float tempf[3];
@@ -298,7 +298,7 @@ float vtkImplicitModeller::ComputeModelBounds()
     }
 
   for (maxDist=0.0, i=0; i<3; i++)
-    if ( (h[i]=(bounds[2*i+1] - bounds[2*i])) > maxDist )
+    if ( (bounds[2*i+1] - bounds[2*i]) > maxDist )
       maxDist = bounds[2*i+1] - bounds[2*i];
 
   maxDist *= this->MaximumDistance;
@@ -308,8 +308,8 @@ float vtkImplicitModeller::ComputeModelBounds()
     {
     for (i=0; i<3; i++)
       {
-      this->ModelBounds[2*i] = bounds[2*i] - h[i]*this->AdjustDistance;
-      this->ModelBounds[2*i+1] = bounds[2*i+1] + h[i]*this->AdjustDistance;
+      this->ModelBounds[2*i] = bounds[2*i] - maxDist*this->AdjustDistance;
+      this->ModelBounds[2*i+1] = bounds[2*i+1] + maxDist*this->AdjustDistance;
       }
     }
 
