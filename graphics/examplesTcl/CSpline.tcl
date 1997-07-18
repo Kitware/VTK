1408e11acc62d612ac03dbee46a90a981bb702ad
Bill Lorensen <lorensen@crd.ge.com>
1997-07-18 11:25:45 -0400

    Examples for splines.

---
+++ graphics/examplesTcl/CSpline.tcl
@@ -0,0 +1,147 @@
+catch {load vtktcl}
+
+# get the interactor ui
+source ../../examplesTcl/vtkInt.tcl
+source ../../examplesTcl/colors.tcl
+
+# Now create the RenderWindow, Renderer and Interactor
+#
+vtkRenderer ren1
+vtkRenderWindow renWin
+    renWin AddRenderer ren1
+vtkRenderWindowInteractor iren
+    iren SetRenderWindow renWin
+
+vtkMath math
+set numberOfInputPoints 30
+
+vtkCardinalSpline aSplineX
+vtkCardinalSpline aSplineY
+vtkCardinalSpline aSplineZ
+
+# generate random points
+
+vtkFloatPoints inputPoints
+
+for {set i 0} {$i<$numberOfInputPoints} {incr i 1} {
+    set x  [math Random 0 1]
+    set y  [math Random 0 1]
+    set z  [math Random 0 1]
+    aSplineX AddPoint $i $x
+    aSplineY AddPoint $i $y
+    aSplineZ AddPoint $i $z
+    inputPoints InsertPoint $i $x $y $z
+}
+
+vtkPolyData inputData
+  inputData SetPoints inputPoints
+
+vtkSphereSource balls
+  balls SetRadius .01
+  balls SetPhiResolution 10
+  balls SetThetaResolution 10
+
+vtkGlyph3D glyphPoints
+glyphPoints SetInput inputData
+glyphPoints SetSource [balls GetOutput]
+
+vtkPolyDataMapper glyphMapper
+glyphMapper SetInput [glyphPoints GetOutput]
+
+vtkActor glyph
+  glyph SetMapper glyphMapper
+eval   [glyph GetProperty] SetDiffuseColor $tomato
+  [glyph GetProperty] SetSpecular .3
+  [glyph GetProperty] SetSpecularPower 30
+
+ren1 AddActor glyph
+
+
+vtkFloatPoints points
+# create a line
+vtkPolyData profileData
+
+set numberOfOutputPoints 400
+proc fit {} {
+  global numberOfInputPoints numberOfOutputPoints
+  points Reset
+  for {set i 0} {$i< $numberOfOutputPoints} {incr i 1} {
+      set t [expr ( $numberOfInputPoints - 1.0 ) / ( $numberOfOutputPoints - 1) * $i]
+      points InsertPoint $i [aSplineX Evaluate $t] [aSplineY Evaluate $t] [aSplineZ Evaluate $t]
+  }
+  profileData Modified
+      update
+}
+fit
+
+vtkCellArray lines
+  lines InsertNextCell $numberOfOutputPoints
+
+for {set i 0} {$i< $numberOfOutputPoints} {incr i 1} {
+  lines InsertCellPoint $i
+}
+
+
+    profileData SetPoints points
+    profileData SetLines lines
+
+
+vtkTubeFilter profileTubes
+  profileTubes SetNumberOfSides 8
+  profileTubes SetInput profileData
+  profileTubes SetRadius .005
+
+vtkPolyDataMapper profileMapper
+profileMapper SetInput [profileTubes GetOutput]
+
+vtkActor profile
+  profile SetMapper profileMapper
+eval  [profile GetProperty] SetDiffuseColor $banana
+  [profile GetProperty] SetSpecular .3
+  [profile GetProperty] SetSpecularPower 30
+
+ren1 AddActor profile
+
+[ren1 GetActiveCamera] Dolly 1.5
+renWin SetSize 500 500
+
+# render the image
+#
+iren Initialize
+iren SetUserMethod {wm deiconify .vtkInteract}
+
+proc varyLeft {} {
+    for {set left -1} { $left <= 1 } {set left [expr $left + .05]} {
+      aSplineX SetLeftValue $left
+      aSplineY SetLeftValue $left
+      aSplineZ SetLeftValue $left
+      fit
+      renWin Render
+    }
+}
+
+proc varyRight {} {
+    for {set right -1} { $right <= 1 } {set right [expr $right + .05]} {
+      aSplineX SetRightValue $right
+      aSplineY SetRightValue $right
+      aSplineZ SetRightValue $right
+      fit
+      renWin Render
+    }
+}
+
+proc constraint {value } {
+  aSplineX SetLeftConstraint $value
+  aSplineY SetLeftConstraint $value
+  aSplineZ SetLeftConstraint $value
+  aSplineX SetRightConstraint $value
+  aSplineY SetRightConstraint $value
+  aSplineZ SetRightConstraint $value
+}
+
+
+# prevent the tk window from showing up then start the event loop
+wm withdraw .
+
+renWin SetFileName CSpline.tcl.ppm
+#renWin SaveImageAsPPM
