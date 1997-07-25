5fdd27e2ad9a95829f7a38c5d4f824be1db1e4ea
Ken Martin <ken.martin@kitware.com>
1997-07-25 11:34:25 -0400

    removed debug

--- java/java_parse.tab.c
+++ java/java_parse.tab.c
@@ -324,7 +324,6 @@ output_function()
       (arg_types[10]%1000 != 109)&&
       ((arg_types[10]%1000)/100)) args_ok = 0;
   if (num_args && (arg_types[0] == 5000)&&(num_args != 2)) args_ok = 0;
-  fprintf(stderr,"made it to the big time %d\n",args_ok);
 
 
   /* eliminate unsigned char * and unsigned short * */
@@ -349,7 +348,6 @@ output_function()
 	(arg_types[i] != 303)) args_ok = 0;
     }
 
-  fprintf(stderr,"made it to the big time %d\n",args_ok);
   /* if we need a return type hint make sure we have one */
   if (!have_hint)
     {
@@ -386,7 +384,6 @@ output_function()
     args_ok = 0;
     }
 
-  fprintf(stderr,"made it to the big time %d\n",args_ok);
   if (in_public && args_ok)
     {
     /* make sure it's not a constructor */
