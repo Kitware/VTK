71fc20514775a800bb24239a9379706a04eefa16
Will Schroeder <will.schroeder@kitware.com>
1997-08-28 10:15:31 -0400

    Converted to new volume rendering architecture

--- graphics/examplesCxx/vol.cxx
+++ graphics/examplesCxx/vol.cxx
@@ -2,55 +2,72 @@
 
 main ()
 {
-  float range[2];
-  char c;
+  vtkSLCReader *reader = vtkSLCReader::New();
+      reader->SetFileName("../../../vtkdata/poship.slc");
 
-  vtkRenderer *ren1 = vtkRenderer::New();
-  vtkRenderWindow *renWin = vtkRenderWindow::New();
-    renWin->AddRenderer(ren1);
-    renWin->DoubleBufferOff();
-  vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
-    iren->SetRenderWindow(renWin);
+  // Create transfer functions for opacity and color
+  vtkPiecewiseFunction *opacityTransferFunction = vtkPiecewiseFunction::New();
+      opacityTransferFunction->AddPoint(20, 0.0);
+      opacityTransferFunction->AddPoint(255,0.2);
 
-  //
-  // Read data
-  //
-  vtkStructuredPointsReader *reader;
-    reader->SetFileName("../../../vtkdata/ironProt.vtk");
-    reader->Update();
-    reader->GetOutput()->GetPointData()->GetScalars()->GetRange(range);
+  vtkColorTransferFunction *colorTransferFunction = 
+             vtkColorTransferFunction::New();
+      colorTransferFunction->AddRedPoint(0.0, 0.0);
+      colorTransferFunction->AddRedPoint(64.0, 1.0);
+      colorTransferFunction->AddRedPoint(128.0, 0.0);
+      colorTransferFunction->AddRedPoint(255.0, 0.0);
+      colorTransferFunction->AddBluePoint(0.0, 0.0);
+      colorTransferFunction->AddBluePoint(64.0, 0.0);
+      colorTransferFunction->AddBluePoint(128.0, 1.0);
+      colorTransferFunction->AddBluePoint(192.0, 0.0);
+      colorTransferFunction->AddBluePoint(255.0, 0.0);
+      colorTransferFunction->AddGreenPoint(0.0, 0.0);
+      colorTransferFunction->AddGreenPoint(128.0, 0.0);
+      colorTransferFunction->AddGreenPoint(192.0, 1.0);
+      colorTransferFunction->AddGreenPoint(255.0, 0.2);
 
-  //
+  // Create properties, mappers, volume actors, and ray cast function
+  vtkVolumeProperty *volumeProperty = vtkVolumeProperty::New();
+      volumeProperty->SetColor(colorTransferFunction);
+      volumeProperty->SetOpacity(opacityTransferFunction);
+
+  vtkVolumeRayCastCompositeFunction  *compositeFunction = 
+            vtkVolumeRayCastCompositeFunction::New();
+
+  vtkVolumeRayCastMapper *volumeMapper = vtkVolumeRayCastMapper::New();
+      volumeMapper->SetScalarInput(reader->GetOutput());
+      volumeMapper->SetVolumeRayCastFunction(compositeFunction);
+
+  vtkVolume *volume = vtkVolume::New();
+      volume->SetVolumeMapper(volumeMapper);
+      volume->SetVolumeProperty(volumeProperty);
+
   // Create outline
-  //
   vtkOutlineFilter *outline = vtkOutlineFilter::New();
-    outline->SetInput(reader->GetOutput());
+      outline->SetInput(reader->GetOutput());
+
   vtkPolyDataMapper *outlineMapper = vtkPolyDataMapper::New();
-    outlineMapper->SetInput(outline->GetOutput());
-  vtkActor *outline1Actor = vtkActor::New();
-    outline1Actor->SetMapper(outlineMapper);
+      outlineMapper->SetInput(outline->GetOutput());
 
-  ren1->SetBackground(0.1,0.2,0.4);
-  ren1->AddActor(outline1Actor);
-  renWin->SetSize(150,150);
+  vtkActor *outlineActor = vtkActor::New();
+      outlineActor->SetMapper(outlineMapper);
+      outlineActor->GetProperty()->SetColor(1, 1, 1);
+
+  // Okay now the graphics stuff
+  vtkRenderer *ren1 = vtkRenderer::New();
+      ren1->SetBackground(0.1, 0.2, 0.4);
+  vtkRenderWindow *renWin = vtkRenderWindow::New();
+      renWin->AddRenderer(ren1);
+      renWin->SetSize(256, 256);
+  vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
+      iren->SetRenderWindow(renWin);
+
+  ren1->AddActor(outlineActor);
+  ren1->AddVolume(volume);
   renWin->Render();
-  ren1->GetActiveCamera()->Zoom(1.5);
-
-  vtkVolume *vol = vtkVolume::New();
-    vol->SetVolumeMapper(reader->GetOutput());
-    vol->GetLookupTable()->SetAlphaRange(0,0.3);
-    vol->SetScalarRange(range);
-
-  vtkVolumeRenderer *volRen = vtkVolumeRenderer::New();
-    volRen->AddVolume(vol);
-    volRen->SetStepSize(0.3);
-
-  ren1->SetVolumeRenderer(volRen);
 
   // interact with data
-  renWin->Render();
-
-  cin >> c;
+  iren->Start();
 
   // Clean up
   ren1->Delete();
@@ -58,7 +75,12 @@ main ()
   iren->Delete();
   outline->Delete();
   outlineMapper->Delete();
-  outline1Actor->Delete();
-  vol->Delete();
-  volRen->Delete();
+  outlineActor->Delete();
+  reader->Delete();
+  opacityTransferFunction->Delete();
+  colorTransferFunction->Delete();
+  volumeProperty->Delete();
+  compositeFunction->Delete();
+  volumeMapper->Delete();
+  volume->Delete();
 }
