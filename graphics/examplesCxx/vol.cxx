c0f13112029a01011fc20c62c9c4775fb48c46bc
Lisa Avila <lisa.avila@kitware.com>
1997-08-29 10:16:42 -0400

    the volume script used in the examples section of the book

--- graphics/examplesCxx/vol.cxx
+++ graphics/examplesCxx/vol.cxx
@@ -2,85 +2,76 @@
 
 main ()
 {
-  vtkSLCReader *reader = vtkSLCReader::New();
-      reader->SetFileName("../../../vtkdata/poship.slc");
-
-  // Create transfer functions for opacity and color
-  vtkPiecewiseFunction *opacityTransferFunction = vtkPiecewiseFunction::New();
-      opacityTransferFunction->AddPoint(20, 0.0);
-      opacityTransferFunction->AddPoint(255,0.2);
-
-  vtkColorTransferFunction *colorTransferFunction = 
-             vtkColorTransferFunction::New();
-      colorTransferFunction->AddRedPoint(0.0, 0.0);
-      colorTransferFunction->AddRedPoint(64.0, 1.0);
-      colorTransferFunction->AddRedPoint(128.0, 0.0);
-      colorTransferFunction->AddRedPoint(255.0, 0.0);
-      colorTransferFunction->AddBluePoint(0.0, 0.0);
-      colorTransferFunction->AddBluePoint(64.0, 0.0);
-      colorTransferFunction->AddBluePoint(128.0, 1.0);
-      colorTransferFunction->AddBluePoint(192.0, 0.0);
-      colorTransferFunction->AddBluePoint(255.0, 0.0);
-      colorTransferFunction->AddGreenPoint(0.0, 0.0);
-      colorTransferFunction->AddGreenPoint(128.0, 0.0);
-      colorTransferFunction->AddGreenPoint(192.0, 1.0);
-      colorTransferFunction->AddGreenPoint(255.0, 0.2);
-
-  // Create properties, mappers, volume actors, and ray cast function
-  vtkVolumeProperty *volumeProperty = vtkVolumeProperty::New();
-      volumeProperty->SetColor(colorTransferFunction);
-      volumeProperty->SetOpacity(opacityTransferFunction);
-
-  vtkVolumeRayCastCompositeFunction  *compositeFunction = 
-            vtkVolumeRayCastCompositeFunction::New();
-
-  vtkVolumeRayCastMapper *volumeMapper = vtkVolumeRayCastMapper::New();
-      volumeMapper->SetScalarInput(reader->GetOutput());
-      volumeMapper->SetVolumeRayCastFunction(compositeFunction);
-
-  vtkVolume *volume = vtkVolume::New();
-      volume->SetVolumeMapper(volumeMapper);
-      volume->SetVolumeProperty(volumeProperty);
-
-  // Create outline
-  vtkOutlineFilter *outline = vtkOutlineFilter::New();
-      outline->SetInput(reader->GetOutput());
-
-  vtkPolyDataMapper *outlineMapper = vtkPolyDataMapper::New();
-      outlineMapper->SetInput(outline->GetOutput());
-
-  vtkActor *outlineActor = vtkActor::New();
-      outlineActor->SetMapper(outlineMapper);
-      outlineActor->GetProperty()->SetColor(1, 1, 1);
-
-  // Okay now the graphics stuff
+  // Create the renderer, render window, and interactor
   vtkRenderer *ren1 = vtkRenderer::New();
-      ren1->SetBackground(0.1, 0.2, 0.4);
   vtkRenderWindow *renWin = vtkRenderWindow::New();
-      renWin->AddRenderer(ren1);
-      renWin->SetSize(256, 256);
+    renWin->AddRenderer(ren1);
   vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
-      iren->SetRenderWindow(renWin);
+    iren->SetRenderWindow(renWin);
 
-  ren1->AddActor(outlineActor);
+  // Read the data from a vtk file
+  vtkStructuredPointsReader *reader = vtkStructuredPointsReader::New();
+    reader->SetFileName("../../../vtkdata/ironProt.vtk");
+    reader->Update();
+
+  // Create a transfer function mapping scalar value to opacity
+  vtkPiecewiseFunction *oTFun = vtkPiecewiseFunction::New();
+    oTFun->AddSegment(80, 0.0, 255, 1.0);
+
+  // Create a transfer function mapping scalar value to color (grey)
+  vtkPiecewiseFunction *cTFun = vtkPiecewiseFunction::New();
+    cTFun->AddSegment(0, 1.0, 255, 1.0);
+
+  // Create a property for the volume and set the transfer functions.
+  // Turn shading on and use trilinear interpolation
+  vtkVolumeProperty *volumeProperty = vtkVolumeProperty::New();
+    volumeProperty->SetColor(cTFun);
+    volumeProperty->SetOpacity(oTFun);
+    volumeProperty->SetInterpolationTypeToLinear();
+    volumeProperty->ShadeOn();
+
+  // Create a ray function - this is a compositing ray function
+  vtkVolumeRayCastCompositeFunction *compositeFunction = 
+    vtkVolumeRayCastCompositeFunction::New();
+
+  // Create the volume mapper and set the ray function and scalar input
+  vtkVolumeRayCastMapper *volumeMapper = vtkVolumeRayCastMapper::New();
+    volumeMapper->SetScalarInput(reader->GetOutput());
+    volumeMapper->SetVolumeRayCastFunction(compositeFunction);
+
+  // Create the volume and set the mapper and property
+  vtkVolume *volume = vtkVolume::New();
+    volume->SetVolumeMapper(volumeMapper);
+    volume->SetVolumeProperty(volumeProperty);
+
+  // Add this volume to the renderer and get a closer look
   ren1->AddVolume(volume);
-  renWin->Render();
+  ren1->GetActiveCamera()->Azimuth(20.0);
+  ren1->GetActiveCamera()->Dolly(1.60);
 
-  // interact with data
+  renWin->SetSize(200,200);
+
+  renWin->Render();
+  renWin->SetFileName("/home/sobie/volume_piat1.ppm");
+  renWin->SaveImageAsPPM();
+
+  // Interact with the data at 3 frames per second
+  iren->SetDesiredUpdateRate(3.0);
+  iren->SetStillUpdateRate(0.001);
   iren->Start();
 
   // Clean up
   ren1->Delete();
   renWin->Delete();
   iren->Delete();
-  outline->Delete();
-  outlineMapper->Delete();
-  outlineActor->Delete();
   reader->Delete();
-  opacityTransferFunction->Delete();
-  colorTransferFunction->Delete();
+  oTFun->Delete();
+  cTFun->Delete();
   volumeProperty->Delete();
   compositeFunction->Delete();
   volumeMapper->Delete();
   volume->Delete();
 }
+
+
+
