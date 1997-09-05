b98d9bfcd137eb22689b5b9c39ae59016d987fcf
Matt Turek <turek@crd.ge.com>
1997-09-05 17:38:37 -0400

    Added ability to build tkWidget in working

--- tcl/tkAppInit.c
+++ tcl/tkAppInit.c
@@ -81,36 +81,49 @@ main(int argc, char **argv)
  */
 
 extern Vtkcommontcl_Init(Tcl_Interp *interp);
+
 #ifdef VTK_USE_GRAPHICS
 extern Vtkgraphicstcl_Init(Tcl_Interp *interp);
 #ifdef VTK_USE_TKWIDGET
 extern Vtktkrenderwidget_Init(Tcl_Interp *interp);
 #endif
 #endif
+
 #ifdef VTK_USE_IMAGING
 extern Vtkimagingtcl_Init(Tcl_Interp *interp);
 #ifdef VTK_USE_TKWIDGET
 extern Vtktkimageviewerwidget_Init(Tcl_Interp *interp);
 #endif
 #endif
+
 #ifdef VTK_USE_PATENTED
 extern Vtkpatentedtcl_Init(Tcl_Interp *interp);
 #endif
+
 #ifdef VTK_USE_CONTRIB
 extern Vtkcontribtcl_Init(Tcl_Interp *interp);
 #endif
+
 #ifdef VTK_USE_WORKING
 extern Vtkworkingtcl_Init(Tcl_Interp *interp);
+#ifdef VTK_USE_TKWIDGET
+extern Vtktkimagewindowwidget_Init(Tcl_Interp *interp);
 #endif
+#endif
+
+
 #ifdef VTK_USE_GEMSVOLUME
 extern Vtkgemsvolumetcl_Init(Tcl_Interp *interp);
 #endif
+
 #ifdef VTK_USE_GEAE
 extern Vtkgeaetcl_Init(Tcl_Interp *interp);
 #endif
+
 #ifdef VTK_USE_GEMSIO
 extern Vtkgemsiotcl_Init(Tcl_Interp *interp);
 #endif
+
 #ifdef VTK_USE_GEMSIP
 extern Vtkgemsiptcl_Init(Tcl_Interp *interp);
 #endif
@@ -175,6 +188,12 @@ int Tcl_AppInit(Tcl_Interp *interp)
     {
     return TCL_ERROR;
     }
+#ifdef VTK_USE_TKWIDGET
+  if (Vtktkimagewindowwidget_Init(interp) == TCL_ERROR)
+    {
+    return TCL_ERROR;
+    }
+#endif
 #endif
 
 #ifdef VTK_USE_CONTRIB
