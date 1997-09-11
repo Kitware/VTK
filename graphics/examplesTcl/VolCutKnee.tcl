c7e5ad1cf0d24a92f5ec36c79ebdf92bb8db4337
Lisa Avila <lisa.avila@kitware.com>
1997-09-11 15:59:44 -0400

    two example scripts from chapter 7

---
+++ graphics/examplesTcl/VolCutKnee.tcl
@@ -0,0 +1,99 @@
+catch {load vtktcl}
+source ../../examplesTcl/vtkInt.tcl
+
+vtkRenderer ren
+
+vtkRenderWindow renWin
+renWin AddRenderer ren
+
+vtkRenderWindowInteractor iren
+iren SetRenderWindow renWin
+
+iren SetUserMethod {wm deiconify .vtkInteract}
+
+
+vtkSLCReader reader
+reader SetFileName ../../../vtkdata/vw_knee.slc
+
+reader Update
+
+vtkPiecewiseFunction white_tfun
+white_tfun AddPoint    0    1.0
+white_tfun AddPoint  255    1.0
+
+
+vtkPiecewiseFunction tfun
+tfun AddPoint  70     0.0
+tfun AddPoint  80     1.0 
+
+ren SetBackground .1 .2 .4
+
+vtkVolumeProperty vol_prop
+vol_prop SetColor white_tfun
+vol_prop SetOpacity tfun
+vol_prop SetInterpolationTypeToLinear
+vol_prop ShadeOn
+
+vtkVolumeRayCastCompositeFunction comp_func
+
+vtkMultiThreadVolumeRayCastMapper  volmap
+volmap SetVolumeRayCastFunction comp_func
+volmap SetScalarInput [reader GetOutput]
+volmap SetSampleDistance 1.0
+
+vtkVolume vol
+vol SetVolumeProperty vol_prop
+vol SetVolumeMapper volmap
+
+ren AddVolume vol
+
+vtkContourFilter contour
+  contour SetInput [reader GetOutput]
+  contour SetValue 0 30.0
+
+vtkFloatPoints points
+  points InsertPoint 0 100.0 150.0 130.0
+  points InsertPoint 1 100.0 150.0 130.0
+  points InsertPoint 2 100.0 150.0 130.0
+
+vtkFloatNormals normals
+  normals InsertNormal 0 1.0 0.0 0.0
+  normals InsertNormal 1 0.0 1.0 0.0
+  normals InsertNormal 2 0.0 0.0 1.0
+  
+
+vtkPlanes planes
+  planes SetPoints points
+  planes SetNormals normals
+  
+
+vtkClipPolyData clipper
+  clipper SetInput [contour GetOutput]
+  clipper SetClipFunction planes
+  clipper GenerateClipScalarsOn
+
+vtkPolyDataMapper skin_mapper
+  skin_mapper SetInput [clipper GetOutput]
+  skin_mapper ScalarVisibilityOff
+
+vtkActor skin
+  skin SetMapper skin_mapper
+  [skin GetProperty] SetColor 0.8 0.4 0.2
+
+ren AddActor skin
+
+renWin SetSize 200 200
+
+
+[ren GetActiveCamera] SetPosition -47.5305 -319.315 92.0083
+[ren GetActiveCamera] SetFocalPoint 78.9121 89.8372 95.1229
+[ren GetActiveCamera] SetViewUp -0.00708891 0.00980254 -0.999927
+[ren GetActiveCamera] SetViewPlaneNormal -0.29525 -0.955392 -0.0072728
+[ren GetActiveCamera] SetClippingRange 42.8255 2141.28
+
+iren Initialize
+
+renWin Render
+
+wm withdraw .
+
