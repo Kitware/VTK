78b6ade8877753db4ce8cf50a5c1415f99664f60
Matt Turek <turek@crd.ge.com>
1997-10-16 14:46:30 -0400

    ENH: Now supports GetGenericDrawable. Turek

--- graphics/vtkXRenderWindow.h
+++ graphics/vtkXRenderWindow.h
@@ -73,6 +73,7 @@ public:
   virtual void *GetGenericWindowId()  {return (void *)this->WindowId;};
   virtual void *GetGenericParentId()  {return (void *)this->ParentId;};
   virtual void *GetGenericContext();
+  virtual void *GetGenericDrawable()  {return (void *)this->WindowId;};
 
   int     *GetSize();
   int     *GetScreenSize();
