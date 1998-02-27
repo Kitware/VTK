72cec5ba45291d6f9a7c714a1bce2c7860918901
Bill Lorensen <lorensen@crd.ge.com>
1998-02-27 11:36:02 -0500

    ENH: removed printouts from script

--- graphics/examplesTcl/clipArt.tcl
+++ graphics/examplesTcl/clipArt.tcl
@@ -15,9 +15,6 @@ vtkBMPReader image
   image ReleaseDataFlagOff
   image Update
 
-image SetStartMethod "puts -nonewline \"Read...\";flush stdout"
-image SetEndMethod "puts \"Complete\""
-
 proc PowerOfTwo {amt} {
   set pow 0
   incr amt -1
@@ -37,23 +34,17 @@ vtkImageConstantPad imagePowerOf2
   imagePowerOf2 SetInput [image GetOutput]
   imagePowerOf2 SetOutputWholeExtent 0 [expr $padX - 1] 0 [expr $padY - 1] 0 0
   imagePowerOf2 SetNumberOfThreads 1
-  imagePowerOf2 SetStartMethod "puts -nonewline \"PowerOf2...\";flush stdout"
-  imagePowerOf2 SetEndMethod "puts \"Complete\""
 
 vtkImageRGBToHSV toHSV
   toHSV SetInput [image GetOutput]
   toHSV ReleaseDataFlagOff
   toHSV SetNumberOfThreads 1
-  toHSV SetStartMethod "puts -nonewline \"toHSV...\";flush stdout"
-  toHSV SetEndMethod "puts \"Complete\""
 
 vtkImageExtractComponents extractImage
   extractImage SetInput [toHSV GetOutput]
   extractImage SetComponents 2
   extractImage ReleaseDataFlagOff
   extractImage SetNumberOfThreads 1
-  extractImage SetStartMethod "puts -nonewline \"extractV...\";flush stdout"
-  extractImage SetEndMethod "puts \"Complete\""
 
 vtkImageThreshold threshold
   threshold SetInput [extractImage GetOutput]
@@ -63,8 +54,6 @@ vtkImageThreshold threshold
   threshold SetNumberOfThreads 1
   threshold Update
 
-  threshold SetStartMethod "puts -nonewline \"Threshold...\";flush stdout"
-  threshold SetEndMethod "puts \"Complete\""
 
 set extent [[threshold GetOutput] GetWholeExtent]
 set seed1 "[lindex $extent 0] [lindex $extent 2]"
@@ -81,8 +70,6 @@ vtkImageSeedConnectivity connect
   eval connect AddSeed $seed2
   eval connect AddSeed $seed3
   eval connect AddSeed $seed4
-  connect SetStartMethod "puts -nonewline \"Connectivity...\";flush stdout"
-  connect SetEndMethod "puts \"Complete\""
   connect SetNumberOfThreads 1
 
 vtkImageGaussianSmooth smooth
@@ -90,15 +77,11 @@ vtkImageGaussianSmooth smooth
   smooth SetStandardDeviation 1 1
   smooth SetInput [connect GetOutput]
   smooth SetNumberOfThreads 1
-  smooth SetStartMethod "puts -nonewline \"Smooth...\";flush stdout"
-  smooth SetEndMethod "puts \"Complete\""
 
 vtkImageShrink3D shrink
   shrink SetInput [smooth GetOutput]
   shrink SetShrinkFactors 2 2 1
   shrink AveragingOn
-  shrink SetStartMethod "puts -nonewline \"Shrink...\";flush stdout"
-  shrink SetEndMethod "puts \"Complete\""
   shrink SetNumberOfThreads 1
 
 vtkImageToStructuredPoints toStructuredPoints
@@ -106,13 +89,9 @@ vtkImageToStructuredPoints toStructuredPoints
 
 vtkStructuredPointsGeometryFilter geometry
   geometry SetInput [toStructuredPoints GetOutput]
-  geometry SetStartMethod "puts -nonewline \"Geometry...\";flush stdout"
-  geometry SetEndMethod "puts \"Complete\""
 
 vtkTextureMapToPlane geometryTexture
   geometryTexture SetInput [geometry GetOutput]
-  geometryTexture SetStartMethod "puts -nonewline \"GeometryTexture...\";flush stdout"
-  geometryTexture SetEndMethod "puts \"Complete\""
 
 vtkCastToConcrete geometryPD
   geometryPD SetInput [geometryTexture GetOutput]
@@ -124,50 +103,36 @@ vtkClipPolyData clip
   clip InsideOutOff
   clip InsideOutOn
   [[clip GetOutput] GetPointData] CopyScalarsOff
-  clip SetStartMethod "puts -nonewline \"Clip...\";flush stdout"
-  clip SetEndMethod "puts \"Complete\""
   clip Update
 
 vtkTriangleFilter triangles
   triangles SetInput [clip GetOutput]
-  triangles SetStartMethod "puts -nonewline \"Triangles...\";flush stdout"
-  triangles SetEndMethod "puts \"Complete\""
 
 vtkDecimatePro decimate
   decimate SetInput [triangles GetOutput]
   decimate BoundaryVertexDeletionOn
   decimate SetDegree 25
   decimate PreserveTopologyOn
-  decimate SetStartMethod "puts -nonewline \"Decimate...\";flush stdout"
-  decimate SetEndMethod "puts \"Complete\""
 
 vtkLinearExtrusionFilter extrude
   extrude SetInput [decimate GetOutput]
   extrude SetExtrusionType 2
   extrude SetScaleFactor -20
-  extrude SetStartMethod "puts -nonewline \"Extrude...\";flush stdout"
-  extrude SetEndMethod "puts \"Complete\""
 
 vtkPolyDataNormals normals
   normals SetInput [extrude GetOutput]
   normals SetFeatureAngle 80
-  normals SetStartMethod "puts -nonewline \"Normals...\";flush stdout"
-  normals SetEndMethod "puts \"Complete\""
 
 vtkTransformTextureCoords scaleTexture
   scaleTexture SetInput [normals GetOutput]
   scaleTexture SetScale [expr ($orgX * 1.0) / $padX] [expr ($orgY * 1.0) / $padY] 1.0
   scaleTexture SetOrigin 0 0 0
-  scaleTexture SetStartMethod "puts -nonewline \"ScaleTexture...\";flush stdout"
-  scaleTexture SetEndMethod "puts \"Complete\""
 
 vtkCastToConcrete texturePD
   texturePD SetInput [scaleTexture GetOutput]
 
 vtkStripper strip
   strip SetInput [texturePD GetPolyDataOutput]
-  strip SetStartMethod "puts -nonewline \"Strip...\";flush stdout"
-  strip SetEndMethod "puts \"Complete\""
 
 vtkPolyDataMapper map
   map SetInput [strip GetOutput]
