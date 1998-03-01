5837803f4fa4c8cc3c25883fb802b02da75a21a7
Bill Lorensen <lorensen@crd.ge.com>
1998-03-01 14:53:11 -0500

    ENH: wrong filename for image svae

--- graphics/examplesTcl/expCos.tcl
+++ graphics/examplesTcl/expCos.tcl
@@ -82,7 +82,7 @@ renWin SetSize 500 500
 iren SetUserMethod {wm deiconify .vtkInteract}
 [ren1 GetActiveCamera] Zoom 1.5
 renWin Render
-#renWin SetFileName "valid/expCos.tcl.ppm"
+#renWin SetFileName "expCos.tcl.ppm"
 #renWin SaveImageAsPPM
 
 # prevent the tk window from showing up then start the event loop
