1c8eabf5c0440f2582156269db2f7ab08e734c04
Ken Martin <ken.martin@kitware.com>
1998-03-30 13:05:47 -0500

    ENH: updated to reflect the new field data cell data changes

--- imaging/examplesTcl/vtkImageInclude.tcl
+++ imaging/examplesTcl/vtkImageInclude.tcl
@@ -1,17 +1,22 @@
 catch {load vtktcl}
 # this file replicates definitions in classes for tcl.
 
-set VTK_FLOAT              1
-set VTK_INT                2
-set VTK_SHORT              3
-set VTK_UNSIGNED_SHORT     4
-set VTK_UNSIGNED_CHAR      5
+set VTK_VOID            0
+set VTK_BIT             1 
+set VTK_CHAR            2
+set VTK_UNSIGNED_CHAR   3
+set VTK_SHORT           4
+set VTK_UNSIGNED_SHORT  5
+set VTK_INT             6
+set VTK_UNSIGNED_INT    7
+set VTK_LONG            8
+set VTK_UNSIGNED_LONG   9
+set VTK_FLOAT          10
+set VTK_DOUBLE         11 
 
 set VTK_IMAGE_X_AXIS             0
 set VTK_IMAGE_Y_AXIS             1
 set VTK_IMAGE_Z_AXIS             2
-set VTK_IMAGE_TIME_AXIS          3
-set VTK_IMAGE_COMPONENT_AXIS     4
 
 set VTK_CLAW_NEAREST_NETWORK      0
 set VTK_CLAW_NEAREST_MINIMUM      1
