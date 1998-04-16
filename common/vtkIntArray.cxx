c0f3493f499f7a28ddaf93e602dbb59c4d631b74
LYMB Demo <lymbdemo@crd.ge.com>
1998-04-16 14:41:27 -0400

    Fixed indentation.  Turek.

--- common/vtkIntArray.cxx
+++ common/vtkIntArray.cxx
@@ -64,17 +64,16 @@ vtkIntArray::~vtkIntArray()
 // from deleting the array when it cleans up or reallocates memory.
 // The class uses the actual array provided; it does not copy the data 
 // from the suppled array.
-
 void vtkIntArray::SetArray(int* array, int size, int save)
 {
   if ((this->Array) && (!this->SaveUserArray))
     {
-      vtkDebugMacro (<< "Deleting the array...");
-      delete [] this->Array;
+    vtkDebugMacro (<< "Deleting the array...");
+    delete [] this->Array;
     }
   else 
     {
-      vtkDebugMacro (<<"Warning, array not deleted, but will point to new array.");
+    vtkDebugMacro (<<"Warning, array not deleted, but will point to new array.");
     }
 
   vtkDebugMacro(<<"Setting array to: " << array);
