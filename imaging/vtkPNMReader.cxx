bddf6899feb3d38040660a465eb2f8b531610e69
Ken Martin <ken.martin@kitware.com>
1998-08-05 09:25:12 -0400

    BUG: header size didn't account for possible trailing bytes in the file

--- imaging/vtkPNMReader.cxx
+++ imaging/vtkPNMReader.cxx
@@ -110,7 +110,7 @@ void vtkPNMReader::UpdateImageInformation()
   this->ComputeInternalFileName(this->DataExtent[4]);
   
   // get the magic number by reading in a file
-  fp = fopen(this->InternalFileName,"rb");
+  fp = fopen(this->InternalFileName,"r");
   if (!fp)
     {
     vtkErrorMacro("Unable to open file " << this->InternalFileName);
@@ -129,6 +129,12 @@ void vtkPNMReader::UpdateImageInformation()
   // now get the dimensions
   xsize = vtkPNMReaderGetInt(fp);
   ysize = vtkPNMReaderGetInt(fp);
+
+  // read max pixel value into comp for now
+  comp = vtkPNMReaderGetInt(fp);
+  
+  // Set the header size now that we have parsed it
+  this->SetHeaderSize(ftell(fp));
 
   fclose(fp);
 
@@ -175,4 +181,5 @@ void vtkPNMReader::UpdateImageInformation()
   
   vtkImageReader::UpdateImageInformation();
 }
+
 
