fae31b6730d65f917c309db16102c1145b79f722
Jim Miller <millerjv@crd.ge.com>
1998-08-07 11:53:54 -0400

    ENH: PC friendly and Quality dashboard friendly version

--- graphics/examplesTcl/rtExamples.tcl
+++ graphics/examplesTcl/rtExamples.tcl
@@ -4,133 +4,161 @@ catch {load vtktcl}
 #
 
 #
-# if REG_IMAGE_PATH is defined, then use if to qualify the error and test images
+# if VTK_RESULTING_IMAGE_PATH is defined, then use if to qualify the error 
+# and test images
 #
-if { [catch {set REG_IMAGE_PATH $env(REG_IMAGE_PATH)/}] != 0} { set REG_IMAGE_PATH "" }
+if { [catch {set VTK_RESULTING_IMAGE_PATH $env(VTK_RESULTING_IMAGE_PATH)/}] != 0} { set VTK_RESULTING_IMAGE_PATH "" }
+
+# on windows shut off global warnings because they hold up the tests
+if {$tcl_platform(os) == "Windows NT"} {
+    vtkObject rtTempObject;
+    rtTempObject GlobalWarningDisplayOff;
+}
 
 #
-# if ALTERNATIVE_IMAGE_FILE is defined, then use if to get another valid image
+# if VTK_PLATFORM is defined, then use if to get another valid image
 #
-if { [catch {set ALTERNATIVE_IMAGE_FILE ".$env(ALTERNATIVE_IMAGE_FILE)"}] != 0} { set ALTERNATIVE_IMAGE_FILE "" }
+if { [catch {set VTK_PLATFORM ".$env(VTK_PLATFORM)"}] != 0} { set VTK_PLATFORM "" }
+
+# determine where the "valid" images are kept
+if { [catch {set validPath $env(VTK_VALID_IMAGE_PATH) }] != 0} {
+    set validPath valid
+} else {
+    # are we in graphics or imaging or ?
+    set fullPathList [file split [file dirname [pwd]]]
+    set kitName [lindex $fullPathList [expr [llength $fullPathList] - 1 ]]
+    
+    # append the kitname onto the validPath
+    set validPath "$validPath/$kitName"
+}
+
+# set up the log file descriptor
+if { [catch {set logFileName $env(VTK_REGRESSION_LOG) }] != 0} {
+    set logFile stdout
+} else {
+    set logFile [open $logFileName "a+"]
+}
 
 # first find all the examples. they can be defined on command line or in
 # current directory
-
 if { $argv != ""} {
-  set files $argv
-  } else {
-  set files [lsort [glob {[A-z]*.tcl}]]
+    set files $argv
+} else {
+    set files [lsort [glob {[A-z]*.tcl}]]
 }
 
-# remove files that are not suitable for regression tests or simply don't work right now
+# remove files that are not suitable for regression tests or simply don't 
+# work right now
 set noTest {
-   rt.tcl rtAll.tcl rib.tcl TkInteractor.tcl TkRenderWidget.tcl RenderWidget.tcl 
-   rtExamples.tcl polyViewer.tcl KeyFrame.tcl cameraKey.tcl timing.tcl 
-   Decimate.tcl assembly2.tcl connPineRoot.tcl deciHawa.tcl 
-   deciPineRoot.tcl deleted.tcl mcTest.tcl viewMCubesFile.tcl
-   volTkInteractor.tcl spikeColor.tcl tkwin.tcl 3dsToRIB.tcl backdrop.tcl
-   ShotNoiseInclude.tcl TestFeatureAnd.tcl TestHistogram.tcl 
-   TestHistogramEqualization.tcl TestMIPFilter.tcl
-   TestSubsample3D.tcl
-   TestWriter.tcl Timing.tcl WindowLevelInterface.tcl
-   vtkImageInclude.tcl}
+    rt.tcl rtAll.tcl rib.tcl TkInteractor.tcl TkRenderWidget.tcl 
+    RenderWidget.tcl 
+    rtExamples.tcl polyViewer.tcl KeyFrame.tcl cameraKey.tcl timing.tcl 
+    Decimate.tcl assembly2.tcl connPineRoot.tcl deciHawa.tcl 
+    deciPineRoot.tcl deleted.tcl mcTest.tcl viewMCubesFile.tcl
+    volTkInteractor.tcl spikeColor.tcl tkwin.tcl 3dsToRIB.tcl backdrop.tcl
+    ShotNoiseInclude.tcl TestFeatureAnd.tcl TestHistogram.tcl 
+    TestHistogramEqualization.tcl TestMIPFilter.tcl
+    TestSubsample3D.tcl
+    TestWriter.tcl Timing.tcl WindowLevelInterface.tcl
+    vtkImageInclude.tcl}
 
 for {set i 0} {$i < [llength $noTest]} {incr i} {
-   if {[set pos [lsearch $files [lindex $noTest $i]]] != -1} {
+    if {[set pos [lsearch $files [lindex $noTest $i]]] != -1} {
       set files [lreplace $files $pos $pos ]
-   }
+    }
 }
+
+
 
 # now do the tests
 foreach afile $files {
-   #
-   # only tcl scripts with valid/ images are tested
-
-    set validImage valid/${afile}${ALTERNATIVE_IMAGE_FILE}.ppm
-   #
-   # first see if there is an alternate image for this architecture
-
+    #
+    # only tcl scripts with valid/ images are tested
+    set validImage $validPath/${afile}${VTK_PLATFORM}.ppm
+    
+    #
+    # first see if there is an alternate image for this architecture
     if {[catch {set channel [open ${validImage}]}] != 0 } {
-     set validImage valid/$afile.ppm
-     if {[catch {set channel [open ${validImage}]}] != 0 } {
-        puts "WARNING: There is no valid image for $afile"
-        continue
-     } else {
-        close $channel
-     }
+	set validImage $validPath/$afile.ppm
+	if {[catch {set channel [open ${validImage}]}] != 0 } {
+	    puts $logFile "WARNING: There is no valid image for $afile"
+	    continue
+	} else {
+	    close $channel
+	}
     } else {
-       close $channel
+	close $channel
     }
-   vtkMath rtExMath
-   rtExMath RandomSeed 6
-   
-   puts -nonewline "$afile took "
-   flush stdout
-   puts -nonewline "[expr [lindex [time {source $afile} 1] 0] / 1000000.0] seconds "
-   
-   vtkWindowToImageFilter w2if
-   
-   # look for a renderWindow ImageWindow or ImageViewer
-   # first check for some common names
-   if {[info commands renWin] == "renWin"} {
-      w2if SetInput renWin
-      set threshold 10
-   } else {
-      set threshold 0
-      if {[info commands viewer] == "viewer"} {
-	 w2if SetInput [viewer GetImageWindow]
-	 viewer Render
-      } else {
-	 if {[info commands imgWin] == "imgWin"} {
-	    w2if SetInput imgWin
-	    imgWin Render
-	 }
-      }
-   }
-
-   # run the event loop quickly to map any tkwidget windows
-   wm withdraw .
-   update
-
+    vtkMath rtExMath
+    rtExMath RandomSeed 6
+    
+    puts -nonewline $logFile "$afile took "
+    flush stdout
+    puts -nonewline $logFile "[expr [lindex [time {source $afile} 1] 0] / 1000000.0] seconds "
+    
+    vtkWindowToImageFilter w2if
+    
+    # look for a renderWindow ImageWindow or ImageViewer
+    # first check for some common names
+    if {[info commands renWin] == "renWin"} {
+	w2if SetInput renWin
+	set threshold 10
+    } else {
+	set threshold 0
+	if {[info commands viewer] == "viewer"} {
+	    w2if SetInput [viewer GetImageWindow]
+	    viewer Render
+	} else {
+	    if {[info commands imgWin] == "imgWin"} {
+		w2if SetInput imgWin
+		imgWin Render
+	    }
+	}
+    }
+  
+    # run the event loop quickly to map any tkwidget windows
+    wm withdraw .
+    update
+    
     if {[catch {set channel [open ${validImage}]}] != 0 } {
-      puts "\nWARNING: Creating a valid image for $afile"
-      vtkPNMWriter rtpnmw
-      rtpnmw SetInput [w2if GetOutput]
-      rtpnmw SetFileName ${validImage}
-      rtpnmw Write
-      vtkCommand DeleteAllObjects
-      catch {destroy .top}
-      continue
-   }
-   close $channel
+	puts $logFile "\nWARNING: Creating a valid image for $afile"
+	vtkPNMWriter rtpnmw
+	rtpnmw SetInput [w2if GetOutput]
+	rtpnmw SetFileName ${validImage}
+	rtpnmw Write
+	vtkCommand DeleteAllObjects
+	catch {destroy .top}
+	continue
+    }
+    close $channel
    
-   vtkPNMReader rtpnm
-       rtpnm SetFileName ${validImage}
+    vtkPNMReader rtpnm
+    rtpnm SetFileName ${validImage}
    
-   vtkImageDifference imgDiff
-   
-   if {$threshold == 0} {imgDiff AllowShiftOff; imgDiff SetThreshold 0}
-   imgDiff SetInput [w2if GetOutput]
-   imgDiff SetImage [rtpnm GetOutput]
-   imgDiff Update
+    vtkImageDifference imgDiff
+    
+    if {$threshold == 0} {imgDiff AllowShiftOff; imgDiff SetThreshold 1}
+    imgDiff SetInput [w2if GetOutput]
+    imgDiff SetImage [rtpnm GetOutput]
+    imgDiff Update
 
-   # a test has to be off by at least threshold pixels for us to care   
-   if {[imgDiff GetThresholdedError] <= $threshold} {
-       puts "and Passed"
-   } else {
-       puts "but failed with an error of [imgDiff GetThresholdedError]"
-         vtkPNMWriter rtpnmw
-         rtpnmw SetInput [imgDiff GetOutput]
-       rtpnmw SetFileName "${REG_IMAGE_PATH}$afile.error.ppm"
-         rtpnmw Write
-         vtkPNMWriter rtpnmw2
-         rtpnmw2 SetInput [w2if GetOutput]
-       rtpnmw2 SetFileName "${REG_IMAGE_PATH}$afile.test.ppm"
-         rtpnmw2 Write
-   }
-   
-   vtkCommand DeleteAllObjects
-   catch {destroy .top}
+    # a test has to be off by at least threshold pixels for us to care   
+    if {[imgDiff GetThresholdedError] <= $threshold} {
+	puts $logFile "and Passed"
+    } else {
+	puts $logFile "but failed with an error of [imgDiff GetThresholdedError]"
+	vtkPNMWriter rtpnmw
+	rtpnmw SetInput [imgDiff GetOutput]
+	rtpnmw SetFileName "${VTK_RESULTING_IMAGE_PATH}$afile.error.ppm"
+	rtpnmw Write
+	vtkPNMWriter rtpnmw2
+	rtpnmw2 SetInput [w2if GetOutput]
+	rtpnmw2 SetFileName "${VTK_RESULTING_IMAGE_PATH}$afile.test.ppm"
+	rtpnmw2 Write
+    }
+    
+    vtkCommand DeleteAllObjects
+    catch {destroy .top}
 }
 
 exit
