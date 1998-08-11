08ae26fbb35e99d8af32a8c3a4e3766607c48180
Ken Martin <ken.martin@kitware.com>
1998-08-11 12:58:56 -0400

    ENH: now includes a simple java regression test

---
+++ java/Regression.java
@@ -0,0 +1,41 @@
+import vtk.*;
+
+public class Regression {
+  static { System.loadLibrary("vtkJava"); }
+  public static void main (String []args) {
+    vtkRenderWindow renWin = new vtkRenderWindow();
+    vtkRenderer ren1 = new vtkRenderer();
+    renWin.AddRenderer(ren1);
+    vtkConeSource cone = new vtkConeSource();
+    cone.SetResolution(8);
+    vtkPolyDataMapper coneMapper = new vtkPolyDataMapper();
+    coneMapper.SetInput(cone.GetOutput());
+
+    vtkActor coneActor = new vtkActor();
+    coneActor.SetMapper(coneMapper);
+
+    ren1.AddActor(coneActor);
+    renWin.Render();
+
+    vtkWindowToImageFilter w2if = new vtkWindowToImageFilter();
+    w2if.SetInput(renWin);
+
+    vtkImageDifference imgDiff = new vtkImageDifference();
+    
+    vtkPNMReader rtpnm = new vtkPNMReader();
+    rtpnm.SetFileName("Regression.ppm");
+
+    imgDiff.SetInput(w2if.GetOutput());
+    imgDiff.SetImage(rtpnm.GetOutput());
+    imgDiff.Update();
+
+    if (imgDiff.GetThresholdedError() <= 10) 
+	{
+        System.out.println("Java regression test passed.<br>"); 
+	} 
+      else 
+        {
+        System.out.println("Java regression test error!<br>"); 
+	}	
+    } 
+}
