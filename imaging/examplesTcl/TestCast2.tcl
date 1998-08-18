a7c183b03011cb5632222eaf5d9a7565a889d041
Charles Law <charles.law@kitware.com>
1998-08-18 11:50:27 -0400

    Test ClampOverflow feature

---
+++ imaging/examplesTcl/TestCast2.tcl
@@ -0,0 +1,42 @@
+catch {load vtktcl}
+# Test the vtkImageCast filter.
+# Cast the shorts to unsinged chars.  This will cause overflow artifacts
+# because the data does not fit into 8 bits.
+
+
+source vtkImageInclude.tcl
+
+
+# Image pipeline
+
+vtkImageReader reader
+reader SetDataByteOrderToLittleEndian
+reader SetDataExtent 0 255 0 255 1 93
+reader SetFilePrefix "../../../vtkdata/fullHead/headsq"
+reader SetDataMask 0x7fff
+#reader ReleaseDataFlagOff
+#reader DebugOn
+
+vtkImageCast cast
+cast SetInput [reader GetOutput]
+cast SetOutputScalarType $VTK_UNSIGNED_CHAR
+cast ClampOverflowOn
+
+vtkImageViewer viewer
+viewer SetInput [cast GetOutput]
+viewer SetZSlice 22
+viewer SetColorWindow 200
+viewer SetColorLevel 60
+#viewer DebugOn
+viewer Render
+
+
+# make interface
+source WindowLevelInterface.tcl
+
+
+
+
+
+
+
---
+++ imaging/examplesTcl/TestShiftScale2.tcl
@@ -0,0 +1,44 @@
+catch {load vtktcl}
+# Shift and scale an image (in that order)
+# This filter is usefull for converting to a lower precision data type.
+# This script test the clamp overflow feature.
+
+
+source vtkImageInclude.tcl
+
+
+vtkImageReader reader
+#reader DebugOn
+[reader GetCache] ReleaseDataFlagOff
+reader SetDataByteOrderToLittleEndian
+reader SetDataExtent 0 255 0 255 1 93
+reader SetFilePrefix "../../../vtkdata/fullHead/headsq"
+reader SetDataMask 0x7fff
+#[reader GetOutput] SetMemoryLimit 30
+
+vtkImageShiftScale shiftScale
+shiftScale SetInput [reader GetOutput]
+shiftScale SetShift -1000.0
+shiftScale SetScale 4.0
+shiftScale SetOutputScalarTypeToUnsignedShort
+shiftScale ClampOverflowOn
+
+vtkImageShiftScale shiftScale2
+shiftScale2 SetInput [shiftScale GetOutput]
+shiftScale2 SetShift 0
+shiftScale2 SetScale 2.0
+
+vtkImageViewer viewer
+#viewer DebugOn
+viewer SetInput [shiftScale2 GetOutput]
+#viewer SetInput [reader GetOutput]
+viewer SetColorWindow 1024
+viewer SetColorLevel 512
+
+#make interface
+source WindowLevelInterface.tcl
+
+vtkPNMWriter w
+w SetFileName D:/vtknew/vtk/graphics/examplesTcl/mace2.ppm
+w SetInput [shiftScale GetOutput]
+#w Write
