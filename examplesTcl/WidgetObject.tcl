72af59db1bb3e8bbf96b1526c893f2286cd24a74
Charles Law <charles.law@kitware.com>
1998-09-02 10:16:07 -0400

    Changed interface for retrieving objects.

--- examplesTcl/WidgetObject.tcl
+++ examplesTcl/WidgetObject.tcl
@@ -3,37 +3,45 @@
 
 
 # generate a "unique" name for a widget variable
-proc GenerateWidgetVariable {widget varName} {
+proc GetWidgetVariable {widget varName} {
    regsub -all {\.} $widget "_" base
 
    return "$varName$base"
 }
 
 # returns an object which will be associated with a widget
-# no error checking
+# A convienience method that creates a name for you  
+# based on the widget name and varible value/
 proc NewWidgetObject {widget type varName} {
-   set var [GenerateWidgetVariable $widget $varName]
+   set var "[GetWidgetVariable $widget $varName]_Object"
    # create the vtk object
    $type $var
+
+   # It is better to keep interface consistent
+   # setting objects as variable values, and NewWidgetObject.
+   SetWidgetVariableValue $widget $varName $var
 
    return $var
 }
 
-# returns the name of an object previously created by NewWidgetObject
+# obsolete!!!!!!!
+# returns the same thing as GetWidgetVariableValue
 proc GetWidgetObject {widget varName} {
-   return [GenerateWidgetVariable $widget $varName]
+   puts "Warning: obsolete call: GetWidgetObject"
+   puts "Please use GetWidgetVariableValue"
+   return "[GetWidgetVariable $widget $varName]_Object"
 }
 
 # sets the value of a widget variable
 proc SetWidgetVariableValue {widget varName value} {
-   set var [GenerateWidgetVariable $widget $varName]
+   set var [GetWidgetVariable $widget $varName]
    global $var
    set $var $value
 }
 
 # This proc has alway eluded me.
 proc GetWidgetVariableValue {widget varName} {
-   set var [GenerateWidgetVariable $widget $varName]
+   set var [GetWidgetVariable $widget $varName]
    global $var
    set temp ""
    catch {eval "set temp [format {$%s} $var]"}
