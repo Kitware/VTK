aceaf5cedcdb82676d65f7522cc1e28bbc1ed826
Will Schroeder <will.schroeder@kitware.com>
1998-10-06 16:53:35 -0400

    ENH:Cleaned up comments

--- graphics/vtkCleanPolyData.h
+++ graphics/vtkCleanPolyData.h
@@ -61,9 +61,8 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 class VTK_EXPORT vtkCleanPolyData : public vtkPolyDataToPolyDataFilter
 {
 public:
-
-// Description:
-// Construct object with initial tolerance of 0.0.
+  // Description:
+  // Construct object with initial tolerance of 0.0.
   vtkCleanPolyData();
 
   ~vtkCleanPolyData();
@@ -76,10 +75,9 @@ public:
   vtkSetClampMacro(Tolerance,float,0.0,1.0);
   vtkGetMacro(Tolerance,float);
 
-
-// Description:
-// Specify a spatial locator for speeding the search process. By
-// default an instance of vtkLocator is used.
+  // Description:
+  // Specify a spatial locator for speeding the search process. By
+  // default an instance of vtkLocator is used.
   void SetLocator(vtkPointLocator *locator);
 
   void SetLocator(vtkPointLocator& locator) {this->SetLocator(&locator);};
