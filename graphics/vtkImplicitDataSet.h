bcd30404cba06027f0da6b13f7a1c1f5568c1275
Will Schroeder <will.schroeder@kitware.com>
1998-10-06 16:28:26 -0400

    ENH:Comment improvements

--- graphics/vtkImplicitDataSet.h
+++ graphics/vtkImplicitDataSet.h
@@ -69,10 +69,9 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 class VTK_EXPORT vtkImplicitDataSet : public vtkImplicitFunction
 {
 public:
-
-// Description
-// Construct an vtkImplicitDataSet with no initial dataset; the OutValue
-// set to a large negative number; and the OutGradient set to (0,0,1).
+  // Description
+  // Construct an vtkImplicitDataSet with no initial dataset; the OutValue
+  // set to a large negative number; and the OutGradient set to (0,0,1).
   vtkImplicitDataSet();
 
   ~vtkImplicitDataSet();
@@ -83,17 +82,14 @@ public:
   unsigned long int GetMTime();
 
   // ImplicitFunction interface
-
-// Description
-// Evaluate the implicit function. This returns the interpolated scalar value
-// at x[3].
+  // Description
+  // Evaluate the implicit function. This returns the interpolated scalar value
+  // at x[3].
   float EvaluateFunction(float x[3]);
 
-
-// Description
-// Evaluate implicit function gradient.
+  // Description
+  // Evaluate implicit function gradient.
   void EvaluateGradient(float x[3], float n[3]);
-
 
   // Description:
   // Specify the dataset used for the implicit function evaluation.
