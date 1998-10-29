67625192dc46244acb4a4e094c0fe13d78fa7a48
Jim Miller <millerjv@crd.ge.com>
1998-10-29 16:33:39 -0500

    USER: user reported bug on reading cell data

--- graphics/vtkStructuredGridReader.cxx
+++ graphics/vtkStructuredGridReader.cxx
@@ -231,7 +231,7 @@ void vtkStructuredGridReader::Execute()
 
       else if ( ! strncmp(line, "cell_data", 9) )
         {
-        if (!this->Reader->Read(&numCells))
+        if (!this->Reader->Read(&nCells))
           {
           vtkErrorMacro(<<"Cannot read cell data!");
           this->Reader->CloseVTKFile ();
