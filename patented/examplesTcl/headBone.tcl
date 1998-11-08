bcd2a798c4f21484144d225fcc6b13454d0d1fc5
Bill Lorensen <lorensen@crd.ge.com>
1998-11-08 08:29:44 -0500

    ENH: added VectorNorm filter.

--- patented/examplesTcl/headBone.tcl
+++ patented/examplesTcl/headBone.tcl
@@ -24,9 +24,16 @@ vtkVolume16Reader v16
 vtkMarchingCubes iso
     iso SetInput [v16 GetOutput]
     iso SetValue 0 1150
-vtkPolyDataMapper isoMapper
-    isoMapper SetInput [iso GetOutput]
-    isoMapper ScalarVisibilityOff
+    iso ComputeGradientsOn
+
+vtkVectorNorm gradient
+  gradient SetInput [iso GetOutput]
+
+vtkDataSetMapper isoMapper
+    isoMapper SetInput [gradient GetOutput]
+    isoMapper ScalarVisibilityOn
+    isoMapper SetScalarRange 0 1200
+
 vtkActor isoActor
     isoActor SetMapper isoMapper
 set isoProp [isoActor GetProperty]
