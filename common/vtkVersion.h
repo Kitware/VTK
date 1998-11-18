f7c43297706d2f7d5b3b89e1b8c0830523654aa8
Ken Martin <ken.martin@kitware.com>
1998-11-18 12:44:54 -0500

    Automated commit to force versioning.

--- common/vtkVersion.h
+++ common/vtkVersion.h
@@ -58,7 +58,7 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 #define VTK_MAJOR_VERSION 2
 #define VTK_MINOR_VERSION 2
 #define VTK_BUILD_VERSION 2
-#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.7 $, $Date: 1998-11-18 17:36:07 $ (GMT)"
+#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.8 $, $Date: 1998-11-18 17:44:54 $ (GMT)"
 
 
 class VTK_EXPORT vtkVersion : public vtkObject {
