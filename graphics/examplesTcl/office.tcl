f227bc3afe64a41d8f98697201065f95e5362d19
Bill Lorensen <lorensen@crd.ge.com>
1998-11-21 16:54:29 -0500

    ENH: now uses vtkDataSetReader.

--- graphics/examplesTcl/office.tcl
+++ graphics/examplesTcl/office.tcl
@@ -11,7 +11,7 @@ vtkRenderWindowInteractor iren
 
 # read data
 #
-vtkStructuredGridReader reader
+vtkDataSetReader reader
     reader SetFileName "../../../vtkdata/office.vtk"
     reader Update;#force a read to occur
 
@@ -21,8 +21,10 @@ set maxVelocity \
   [[[[reader GetOutput] GetPointData] GetVectors] GetMaxNorm]
 set maxTime [expr 35.0 * $length / $maxVelocity]
 
+vtkCastToConcrete castToStructuredGrid
+    castToStructuredGrid SetInput [reader GetOutput]
 vtkStructuredGridGeometryFilter table1
-    table1 SetInput [reader GetOutput]
+    table1 SetInput [castToStructuredGrid GetStructuredGridOutput]
     table1 SetExtent 11 15 7 9 8 8
 vtkPolyDataMapper mapTable1
     mapTable1 SetInput [table1 GetOutput]
@@ -32,7 +34,7 @@ vtkActor table1Actor
     [table1Actor GetProperty] SetColor .59 .427 .392
 
 vtkStructuredGridGeometryFilter table2
-    table2 SetInput [reader GetOutput]
+    table2 SetInput [castToStructuredGrid GetStructuredGridOutput]
     table2 SetExtent 11 15 10 12 8 8
 vtkPolyDataMapper mapTable2
     mapTable2 SetInput [table2 GetOutput]
@@ -42,7 +44,7 @@ vtkActor table2Actor
     [table2Actor GetProperty] SetColor .59 .427 .392
 
 vtkStructuredGridGeometryFilter FilingCabinet1
-    FilingCabinet1 SetInput [reader GetOutput]
+    FilingCabinet1 SetInput [castToStructuredGrid GetStructuredGridOutput]
     FilingCabinet1 SetExtent 15 15 7 9 0 8
 vtkPolyDataMapper mapFilingCabinet1
     mapFilingCabinet1 SetInput [FilingCabinet1 GetOutput]
@@ -52,7 +54,7 @@ vtkActor FilingCabinet1Actor
     [FilingCabinet1Actor GetProperty] SetColor .8 .8 .6
 
 vtkStructuredGridGeometryFilter FilingCabinet2
-    FilingCabinet2 SetInput [reader GetOutput]
+    FilingCabinet2 SetInput [castToStructuredGrid GetStructuredGridOutput]
     FilingCabinet2 SetExtent 15 15 10 12 0 8
 vtkPolyDataMapper mapFilingCabinet2
     mapFilingCabinet2 SetInput [FilingCabinet2 GetOutput]
@@ -62,7 +64,7 @@ vtkActor FilingCabinet2Actor
     [FilingCabinet2Actor GetProperty] SetColor .8 .8 .6
 
 vtkStructuredGridGeometryFilter bookshelf1Top
-    bookshelf1Top SetInput [reader GetOutput]
+    bookshelf1Top SetInput [castToStructuredGrid GetStructuredGridOutput]
     bookshelf1Top SetExtent 13 13 0 4 0 11
 vtkPolyDataMapper mapBookshelf1Top
     mapBookshelf1Top SetInput [bookshelf1Top GetOutput]
@@ -72,7 +74,7 @@ vtkActor bookshelf1TopActor
     [bookshelf1TopActor GetProperty] SetColor .8 .8 .6
 
 vtkStructuredGridGeometryFilter bookshelf1Bottom
-    bookshelf1Bottom SetInput [reader GetOutput]
+    bookshelf1Bottom SetInput [castToStructuredGrid GetStructuredGridOutput]
     bookshelf1Bottom SetExtent 20 20 0 4 0 11
 vtkPolyDataMapper mapBookshelf1Bottom
     mapBookshelf1Bottom SetInput [bookshelf1Bottom GetOutput]
@@ -82,7 +84,7 @@ vtkActor bookshelf1BottomActor
     [bookshelf1BottomActor GetProperty] SetColor .8 .8 .6
 
 vtkStructuredGridGeometryFilter bookshelf1Front
-    bookshelf1Front SetInput [reader GetOutput]
+    bookshelf1Front SetInput [castToStructuredGrid GetStructuredGridOutput]
     bookshelf1Front SetExtent 13 20 0 0 0 11
 vtkPolyDataMapper mapBookshelf1Front
     mapBookshelf1Front SetInput [bookshelf1Front GetOutput]
@@ -92,7 +94,7 @@ vtkActor bookshelf1FrontActor
     [bookshelf1FrontActor GetProperty] SetColor .8 .8 .6
 
 vtkStructuredGridGeometryFilter bookshelf1Back
-    bookshelf1Back SetInput [reader GetOutput]
+    bookshelf1Back SetInput [castToStructuredGrid GetStructuredGridOutput]
     bookshelf1Back SetExtent 13 20 4 4 0 11
 vtkPolyDataMapper mapBookshelf1Back
     mapBookshelf1Back SetInput [bookshelf1Back GetOutput]
@@ -102,7 +104,7 @@ vtkActor bookshelf1BackActor
     [bookshelf1BackActor GetProperty] SetColor .8 .8 .6
 
 vtkStructuredGridGeometryFilter bookshelf1LHS
-    bookshelf1LHS SetInput [reader GetOutput]
+    bookshelf1LHS SetInput [castToStructuredGrid GetStructuredGridOutput]
     bookshelf1LHS SetExtent 13 20 0 4 0 0
 vtkPolyDataMapper mapBookshelf1LHS
     mapBookshelf1LHS SetInput [bookshelf1LHS GetOutput]
@@ -112,7 +114,7 @@ vtkActor bookshelf1LHSActor
     [bookshelf1LHSActor GetProperty] SetColor .8 .8 .6
 
 vtkStructuredGridGeometryFilter bookshelf1RHS
-    bookshelf1RHS SetInput [reader GetOutput]
+    bookshelf1RHS SetInput [castToStructuredGrid GetStructuredGridOutput]
     bookshelf1RHS SetExtent 13 20 0 4 11 11
 vtkPolyDataMapper mapBookshelf1RHS
     mapBookshelf1RHS SetInput [bookshelf1RHS GetOutput]
@@ -122,7 +124,7 @@ vtkActor bookshelf1RHSActor
     [bookshelf1RHSActor GetProperty] SetColor .8 .8 .6
 
 vtkStructuredGridGeometryFilter bookshelf2Top
-    bookshelf2Top SetInput [reader GetOutput]
+    bookshelf2Top SetInput [castToStructuredGrid GetStructuredGridOutput]
     bookshelf2Top SetExtent 13 13 15 19 0 11
 vtkPolyDataMapper mapBookshelf2Top
     mapBookshelf2Top SetInput [bookshelf2Top GetOutput]
@@ -132,7 +134,7 @@ vtkActor bookshelf2TopActor
     [bookshelf2TopActor GetProperty] SetColor .8 .8 .6
 
 vtkStructuredGridGeometryFilter bookshelf2Bottom
-    bookshelf2Bottom SetInput [reader GetOutput]
+    bookshelf2Bottom SetInput [castToStructuredGrid GetStructuredGridOutput]
     bookshelf2Bottom SetExtent 20 20 15 19 0 11
 vtkPolyDataMapper mapBookshelf2Bottom
     mapBookshelf2Bottom SetInput [bookshelf2Bottom GetOutput]
@@ -142,7 +144,7 @@ vtkActor bookshelf2BottomActor
     [bookshelf2BottomActor GetProperty] SetColor .8 .8 .6
 
 vtkStructuredGridGeometryFilter bookshelf2Front
-    bookshelf2Front SetInput [reader GetOutput]
+    bookshelf2Front SetInput [castToStructuredGrid GetStructuredGridOutput]
     bookshelf2Front SetExtent 13 20 15 15 0 11
 vtkPolyDataMapper mapBookshelf2Front
     mapBookshelf2Front SetInput [bookshelf2Front GetOutput]
@@ -152,7 +154,7 @@ vtkActor bookshelf2FrontActor
     [bookshelf2FrontActor GetProperty] SetColor .8 .8 .6
 
 vtkStructuredGridGeometryFilter bookshelf2Back
-    bookshelf2Back SetInput [reader GetOutput]
+    bookshelf2Back SetInput [castToStructuredGrid GetStructuredGridOutput]
     bookshelf2Back SetExtent 13 20 19 19 0 11
 vtkPolyDataMapper mapBookshelf2Back
     mapBookshelf2Back SetInput [bookshelf2Back GetOutput]
@@ -162,7 +164,7 @@ vtkActor bookshelf2BackActor
     [bookshelf2BackActor GetProperty] SetColor .8 .8 .6
 
 vtkStructuredGridGeometryFilter bookshelf2LHS
-    bookshelf2LHS SetInput [reader GetOutput]
+    bookshelf2LHS SetInput [castToStructuredGrid GetStructuredGridOutput]
     bookshelf2LHS SetExtent 13 20 15 19 0 0
 vtkPolyDataMapper mapBookshelf2LHS
     mapBookshelf2LHS SetInput [bookshelf2LHS GetOutput]
@@ -172,7 +174,7 @@ vtkActor bookshelf2LHSActor
     [bookshelf2LHSActor GetProperty] SetColor .8 .8 .6
 
 vtkStructuredGridGeometryFilter bookshelf2RHS
-    bookshelf2RHS SetInput [reader GetOutput]
+    bookshelf2RHS SetInput [castToStructuredGrid GetStructuredGridOutput]
     bookshelf2RHS SetExtent 13 20 15 19 11 11
 vtkPolyDataMapper mapBookshelf2RHS
     mapBookshelf2RHS SetInput [bookshelf2RHS GetOutput]
@@ -182,7 +184,7 @@ vtkActor bookshelf2RHSActor
     [bookshelf2RHSActor GetProperty] SetColor .8 .8 .6
 
 vtkStructuredGridGeometryFilter window
-    window SetInput [reader GetOutput]
+    window SetInput [castToStructuredGrid GetStructuredGridOutput]
     window SetExtent 20 20 6 13 10 13
 vtkPolyDataMapper mapWindow
     mapWindow SetInput [window GetOutput]
@@ -192,7 +194,7 @@ vtkActor windowActor
     [windowActor GetProperty] SetColor .3 .3 .5
 
 vtkStructuredGridGeometryFilter outlet
-    outlet SetInput [reader GetOutput]
+    outlet SetInput [castToStructuredGrid GetStructuredGridOutput]
     outlet SetExtent 0 0 9 10 14 16
 vtkPolyDataMapper mapOutlet
     mapOutlet SetInput [outlet GetOutput]
@@ -202,7 +204,7 @@ vtkActor outletActor
     [outletActor GetProperty] SetColor 0 0 0
 
 vtkStructuredGridGeometryFilter inlet
-    inlet SetInput [reader GetOutput]
+    inlet SetInput [castToStructuredGrid GetStructuredGridOutput]
     inlet SetExtent 0 0 9 10 0 6
 vtkPolyDataMapper mapInlet
     mapInlet SetInput [inlet GetOutput]
@@ -212,7 +214,7 @@ vtkActor inletActor
     [inletActor GetProperty] SetColor 0 0 0
 
 vtkStructuredGridOutlineFilter outline
-    outline SetInput [reader GetOutput]
+    outline SetInput [castToStructuredGrid GetStructuredGridOutput]
 vtkPolyDataMapper mapOutline
     mapOutline SetInput [outline GetOutput]
 vtkActor outlineActor
@@ -225,7 +227,7 @@ vtkPointSource seeds
     eval seeds SetCenter 0.1 2.7 0.5
     seeds SetNumberOfPoints 25
 vtkStreamLine streamers
-    streamers SetInput [reader GetOutput]
+    streamers SetInput [castToStructuredGrid GetStructuredGridOutput]
     streamers SetSource [seeds GetOutput]
     streamers SetMaximumPropagationTime 500
     streamers SetStepLength 0.1
