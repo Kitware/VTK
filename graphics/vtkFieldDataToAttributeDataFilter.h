76f6f7bc213cb55a557929c65d01a814e4c9e0f4
Will Schroeder <will.schroeder@kitware.com>
1998-12-23 14:53:55 -0500

    ENH:Cleaned up comments

--- graphics/vtkFieldDataToAttributeDataFilter.h
+++ graphics/vtkFieldDataToAttributeDataFilter.h
@@ -40,30 +40,32 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 =========================================================================*/
 // .NAME vtkFieldDataToAttributeDataFilter - map field data to dataset attribute data
 // .SECTION Description
-// vtkFieldDataToAttributeDataFilter is an class that maps field data
-// into dataset attributes. The input to this filter is any type of dataset
-// and the output is the same dataset (geometry/topology) with new attribute 
-// data (attribute data is passed through if not replaced during filter execution).
+// vtkFieldDataToAttributeDataFilter is a class that maps field data into
+// dataset attributes. The input to this filter is any type of dataset and
+// the output is the same dataset (geometry/topology) with new attribute data
+// (attribute data is passed through if not replaced during filter
+// execution).
 //
-// To use this filter you must specify which field data from the input dataset to
-// use. There are three possibilities: the cell field data, the point field data, or
-// the field data associated with the data object superclass. Then you specify which
-// attribute data to create: either cell attribute data or point attribute data.
-// Finally, you must define how to construct the various attribute data types (e.g.,
-// scalars, vectors, normals, etc.) from the arrays and the components of the arrays
-// from the field data. This is done by associating components in the input field
-// with components making up the attribute data. For example, you would specify a
-// scalar with three components (RGB) by assigning 
-// components from the field for the R, then G, then B values of the scalars.
-// You may also have to specify component ranges (for each R-G-B) to make 
-// sure that the number of R, G, and B values is the same. Also, you may 
-// want to normalize the components which helps distribute the data 
-// uniformly.
+// To use this filter you must specify which field data from the input
+// dataset to use. There are three possibilities: the cell field data, the
+// point field data, or the field data associated with the data object
+// superclass. Then you specify which attribute data to create: either cell
+// attribute data or point attribute data.  Finally, you must define how to
+// construct the various attribute data types (e.g., scalars, vectors,
+// normals, etc.) from the arrays and the components of the arrays from the
+// field data. This is done by associating components in the input field with
+// components making up the attribute data. For example, you would specify a
+// scalar with three components (RGB) by assigning components from the field
+// for the R, then G, then B values of the scalars.  You may also have to
+// specify component ranges (for each R-G-B) to make sure that the number of
+// R, G, and B values is the same. Also, you may want to normalize the
+// components which helps distribute the data uniformly.
 //
-// This filter is often used in conjunction with vtkDataObjectToDataSetFilter. 
-// vtkDataObjectToDataSetFilter filter generates dataset topology and geometry 
-// and passes its input field data along to its output. Then this filter is used
-// to generate the attribute data to go along with the daraset.
+// This filter is often used in conjunction with
+// vtkDataObjectToDataSetFilter.  vtkDataObjectToDataSetFilter filter
+// generates dataset topology and geometry and passes its input field data
+// along to its output. Then this filter is used to generate the attribute
+// data to go along with the daraset.
 
 // .SECTION Caveats
 // Make sure that the data you extract is consistent. That is, if you have N
@@ -94,34 +96,40 @@ public:
   const char *GetClassName() {return "vtkFieldDataToAttributeDataFilter";};
 
   // Description:
-  // Construct object with input field set to the data object field, and the output
-  // attribute data set to generate point data.
-  static vtkFieldDataToAttributeDataFilter *New() {
-    return new vtkFieldDataToAttributeDataFilter;};
+  // Construct object with input field set to the data object field, and the
+  // output attribute data set to generate point data.
+  static vtkFieldDataToAttributeDataFilter *New() 
+    {return new vtkFieldDataToAttributeDataFilter;};
 
   // Description:
-  // Specify which field data to use to generate the output attribute data. There are
-  // three choices: the field data associated with the vtkDataObject superclass; the
-  // point field attribute data; and the cell field attribute data.
+  // Specify which field data to use to generate the output attribute
+  // data. There are three choices: the field data associated with the
+  // vtkDataObject superclass; the point field attribute data; and the cell
+  // field attribute data.
   vtkSetMacro(InputField,int);
   vtkGetMacro(InputField,int);
-  void SetInputFieldToDataObjectField() {this->SetInputField(VTK_DATA_OBJECT_FIELD);};
-  void SetInputFieldToPointDataField() {this->SetInputField(VTK_POINT_DATA_FIELD);};
-  void SetInputFieldToCellDataField() {this->SetInputField(VTK_CELL_DATA_FIELD);};
+  void SetInputFieldToDataObjectField() 
+    {this->SetInputField(VTK_DATA_OBJECT_FIELD);};
+  void SetInputFieldToPointDataField() 
+    {this->SetInputField(VTK_POINT_DATA_FIELD);};
+  void SetInputFieldToCellDataField() 
+    {this->SetInputField(VTK_CELL_DATA_FIELD);};
   
   // Description:
   // Specify which attribute data to output: point or cell data attributes.
   vtkSetMacro(OutputAttributeData,int);
   vtkGetMacro(OutputAttributeData,int);
-  void SetOutputAttributeDataToCellData() {this->SetOutputAttributeData(VTK_CELL_DATA);};
-  void SetOutputAttributeDataToPointData() {this->SetOutputAttributeData(VTK_POINT_DATA);};
+  void SetOutputAttributeDataToCellData() 
+    {this->SetOutputAttributeData(VTK_CELL_DATA);};
+  void SetOutputAttributeDataToPointData() 
+    {this->SetOutputAttributeData(VTK_POINT_DATA);};
 
   // Description:
-  // Define the component of the field to be used for the cell scalar components.
-  // Note that the parameter comp must lie between (0,4). To define
-  // the field component to use you specify an array name and the component
-  // in that array. The (min,max) values are the range of data in the component 
-  // you wish to extract.
+  // Define the component(s) of the field to be used for the scalar
+  // components.  Note that the parameter comp must lie between (0,4). To
+  // define the field component to use you specify an array name and the
+  // component in that array. The (min,max) values are the range of data in
+  // the component you wish to extract.
   void SetScalarComponent(int comp, char *arrayName, int arrayComp,
 			  int min, int max, int normalize);
   void SetScalarComponent(int comp, char *arrayName, int arrayComp)
@@ -133,11 +141,11 @@ public:
   int GetScalarComponentNormalizeFlag(int comp);
   
   // Description:
-  // Define the component of the field to be used for the cell vector components.
-  // Note that the parameter comp must lie between (0,3). To define
-  // the field component to use you specify an array name and the component
-  // in that array. The (min,max) values are the range of data in the component 
-  // you wish to extract.
+  // Define the component(s) of the field to be used for the vector
+  // components.  Note that the parameter comp must lie between (0,3). To
+  // define the field component to use you specify an array name and the
+  // component in that array. The (min,max) values are the range of data in
+  // the component you wish to extract.
   void SetVectorComponent(int comp, char *arrayName, int arrayComp,
 			  int min, int max, int normalize);
   void SetVectorComponent(int comp, char *arrayName, int arrayComp)
@@ -149,11 +157,11 @@ public:
   int GetVectorComponentNormalizeFlag(int comp);
   
   // Description:
-  // Define the component of the field to be used for the cell normal components.
-  // Note that the parameter comp must lie between (0,3). To define
-  // the field component to use you specify an array name and the component
-  // in that array. The (min,max) values are the range of data in the component 
-  // you wish to extract.
+  // Define the component(s) of the field to be used for the normal
+  // components.  Note that the parameter comp must lie between (0,3). To
+  // define the field component to use you specify an array name and the
+  // component in that array. The (min,max) values are the range of data in
+  // the component you wish to extract.
   void SetNormalComponent(int comp, char *arrayName, int arrayComp,
 			  int min, int max, int normalize);
   void SetNormalComponent(int comp, char *arrayName, int arrayComp)
@@ -165,11 +173,11 @@ public:
   int GetNormalComponentNormalizeFlag(int comp);
   
   // Description:
-  // Define the component of the field to be used for the cell tensor components.
-  // Note that the parameter comp must lie between (0,9). To define
-  // the field component to use you specify an array name and the component
-  // in that array. The (min,max) values are the range of data in the component 
-  // you wish to extract.
+  // Define the components of the field to be used for the tensor
+  // components.  Note that the parameter comp must lie between (0,9). To
+  // define the field component to use you specify an array name and the
+  // component in that array. The (min,max) values are the range of data in
+  // the component you wish to extract.
   void SetTensorComponent(int comp, char *arrayName, int arrayComp,
 			  int min, int max, int normalize);
   void SetTensorComponent(int comp, char *arrayName, int arrayComp)
@@ -181,11 +189,11 @@ public:
   int GetTensorComponentNormalizeFlag(int comp);
   
   // Description:
-  // Define the component of the field to be used for the cell texture coord components.
-  // Note that the parameter comp must lie between (0,9). To define
-  // the field component to use you specify an array name and the component
-  // in that array. The (min,max) values are the range of data in the component 
-  // you wish to extract.
+  // Define the components of the field to be used for the cell texture coord
+  // components.  Note that the parameter comp must lie between (0,9). To
+  // define the field component to use you specify an array name and the
+  // component in that array. The (min,max) values are the range of data in
+  // the component you wish to extract.
   void SetTCoordComponent(int comp, char *arrayName, int arrayComp,
 			  int min, int max, int normalize);
   void SetTCoordComponent(int comp, char *arrayName, int arrayComp)
@@ -206,17 +214,17 @@ public:
   // Helper functions, made public to support other classes
 
   // Description:
-  // Given an array of names of arrays in field data, return the common type for
-  // these arrays. For example, if a vector is constructed of the three type 
-  // (char,int,float), the return type is float.
+  // Given an array of names of arrays in field data, return the common type
+  // for these arrays. For example, if a vector is constructed of the three
+  // type (char,int,float), the return type is float.
   static int GetComponentsType(int numComp, vtkDataArray **arrays);
 
   // Description:
-  // Construct a portion of a data array (the comp portion) from another data array
-  // and its component. The variables min and max control the range of the data to
-  // use from the other data array; normalize is a flag that when set will normalize
-  // the data between (0,1).
-  static int ConstructArray(vtkDataArray *da, int comp, vtkDataArray *fieldArray,
+  // Construct a portion of a data array (the comp portion) from another data
+  // array and its component. The variables min and max control the range of
+  // the data to use from the other data array; normalize is a flag that when
+  // set will normalize the data between (0,1).
+  static int ConstructArray(vtkDataArray *da, int comp, vtkDataArray *frray,
 		     int fieldComp, int min, int max, int normalize);
 
   // Description:
@@ -267,20 +275,20 @@ protected:
   int DefaultNormalize;
 
   void ConstructScalars(int num, vtkFieldData *fd, vtkDataSetAttributes *attr, 
-			int componentRange[4][2], char *arrays[4], int arrayComponents[4],
-			int normalize[4], int numComp);
+			int componentRange[4][2], char *arrays[4], 
+			int arrayComponents[4],	int normalize[4], int numComp);
   void ConstructVectors(int num, vtkFieldData *fd, vtkDataSetAttributes *attr, 
-			int componentRange[3][2], char *arrays[3], int arrayComponents[3], 
-			int normalize[3]);
+			int componentRange[3][2], char *arrays[3], 
+			int arrayComponents[3], int normalize[3]);
   void ConstructNormals(int num, vtkFieldData *fd, vtkDataSetAttributes *attr, 
-			int componentRange[3][2], char *arrays[3], int arrayComponents[3], 
-			int normalize[3]);
+			int componentRange[3][2], char *arrays[3], 
+			int arrayComponents[3], int normalize[3]);
   void ConstructTCoords(int num, vtkFieldData *fd, vtkDataSetAttributes *attr, 
-			int componentRange[3][2], char *arrays[3], int arrayComponents[3],
-			int normalize[3], int numComp);
+			int componentRange[3][2], char *arrays[3], 
+			int arrayComponents[3],	int normalize[3], int numComp);
   void ConstructTensors(int num, vtkFieldData *fd, vtkDataSetAttributes *attr, 
-			int componentRange[9][2], char *arrays[9], int arrayComponents[9],
-			int normalize[9]);
+			int componentRange[9][2], char *arrays[9], 
+			int arrayComponents[9],	int normalize[9]);
   void ConstructFieldData(int num, vtkDataSetAttributes *attr);
   
 };
