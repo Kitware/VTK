8ece5068a432f7c9a521cca8b771f87266924963
Charles Law <charles.law@kitware.com>
1999-01-04 09:17:21 -0500

    I do not like this class, but it should have a regresion test.

---
+++ imaging/examplesTcl/TestStaticCache.tcl
@@ -0,0 +1,35 @@
+catch {load vtktcl}
+# I do not line the way this object works, but here is a regresion test.
+
+
+source vtkImageInclude.tcl
+
+# Image pipeline
+vtkPNMReader reader
+reader ReleaseDataFlagOff
+reader SetFileName "../../../vtkdata/earth.ppm"
+
+
+vtkImageStaticCache staticCache
+set data [[reader GetOutput] UpdateAndReturnData]
+puts [$data Print]
+staticCache SetCachedData $data
+
+
+vtkImageViewer viewer
+viewer SetInput staticCache
+viewer SetColorWindow 255
+viewer SetColorLevel 128
+viewer Render
+
+viewer SetPosition 50 50
+
+#make interface
+source WindowLevelInterface.tcl
+
+
+
+
+
+
+
