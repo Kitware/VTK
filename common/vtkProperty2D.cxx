47ebafb5ac36f1c55704fe059c66ad667b1457e4
Ken Martin <ken.martin@kitware.com>
1999-02-10 12:59:40 -0500

    ENH: made default color white

--- common/vtkProperty2D.cxx
+++ common/vtkProperty2D.cxx
@@ -47,8 +47,8 @@ vtkProperty2D::vtkProperty2D()
 {
   this->Opacity = 1.0;
   this->Color[0] = 1.0;
-  this->Color[1] = 0.0;
-  this->Color[2] = 0.0;  
+  this->Color[1] = 1.0;
+  this->Color[2] = 1.0;  
   this->CompositingOperator = VTK_SRC;
 }
 
