df67020ce45842ad0861c6d43269f84c0c115d1d
Will Schroeder <will.schroeder@kitware.com>
1999-03-02 11:48:24 -0500

    ERR:Support scalars in lines

--- imaging/vtkWin32PolyDataMapper2D.cxx
+++ imaging/vtkWin32PolyDataMapper2D.cxx
@@ -201,10 +201,6 @@ void vtkWin32PolyDataMapper2D::RenderOverlay(vtkViewport* viewport,
       cellScalars = 1;
       }
     }
-vtkDebugMacro(<< c);
-//    printf("c = %s\n", c);
-vtkDebugMacro(<< cellScalars);
-//    printf("cellScalars = %d\n", cellScalars);
 
   // set the colors for the foreground
   brush = CreateSolidBrush(RGB(red,green,blue));
@@ -266,10 +262,10 @@ vtkDebugMacro(<< cellScalars);
 	{
 	rgba = c->GetColor(pts[j]);
 	}
-      //      npen = (HPEN) CreatePen(PS_SOLID,0,RGB(rgba[0],rgba[1],rgba[2]));
-      //      pen =  (HPEN) SelectObject(hdc,npen);   
-      //      DeleteObject(pen);
-      //      pen = npen;
+      npen = (HPEN) CreatePen(PS_SOLID,0,RGB(rgba[0],rgba[1],rgba[2]));
+      pen =  (HPEN) SelectObject(hdc,npen);   
+      DeleteObject(pen);
+      pen = npen;
       }
     if (npts > currSize)
       {
