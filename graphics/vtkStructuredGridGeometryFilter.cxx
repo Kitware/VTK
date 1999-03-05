ac7994e9f8355f8a9a9f2e2598b16907536200ab
Bill Lorensen <lorensen@crd.ge.com>
1999-03-05 08:40:22 -0500

    ERR: Incorrect cell index computation.

--- graphics/vtkStructuredGridGeometryFilter.cxx
+++ graphics/vtkStructuredGridGeometryFilter.cxx
@@ -108,13 +108,33 @@ void vtkStructuredGridGeometryFilter::Execute()
   startIdx = extent[0] + extent[2]*dims[0] + extent[4]*dims[0]*dims[1];
 
 // The cell index is a bit more complicated at the boundaries
-  startCellIdx =  (extent[0] < dims[0] - 1) ? extent[0]
-                                            : extent[0]-1;
-  startCellIdx += (extent[2] < dims[1] - 1) ? extent[2]*(dims[0]-1)
-                                            : (extent[2]-1)*(dims[0]-1);
-  startCellIdx += (extent[4] < dims[2] - 1) ? extent[4]*(dims[0]-1)*(dims[1]-1)
-                                            : (extent[4]-1)*(dims[0]-1)*(dims[1]-1);
-
+  if (dims[0] == 1)
+    {
+    startCellIdx = extent[0];
+    }
+  else
+    {
+    startCellIdx =  (extent[0] < dims[0] - 1) ? extent[0]
+                                              : extent[0]-1;
+    }
+  if (dims[1] == 1)
+    {
+    startCellIdx += extent[2]*(dims[0]-1);
+    }
+  else
+    {
+    startCellIdx += (extent[2] < dims[1] - 1) ? extent[2]*(dims[0]-1)
+                                              : (extent[2]-1)*(dims[0]-1);
+    }
+  if (dims[2] == 1)
+    {
+    startCellIdx += extent[4]*(dims[0]-1)*(dims[1]-1);
+    }
+  else
+    {
+    startCellIdx += (extent[4] < dims[2] - 1) ? extent[4]*(dims[0]-1)*(dims[1]-1)
+                                              : (extent[4]-1)*(dims[0]-1)*(dims[1]-1);
+    }
   switch (dimension) 
     {
     default:
@@ -197,7 +217,7 @@ void vtkStructuredGridGeometryFilter::Execute()
         {
         if ( input->IsPointVisible(idx) || input->IsPointVisible(idx+offset[0]) )
           {
-          idx = startIdx + i*offset[0];
+          idx = startCellIdx + i*offset[0];
           ptIds[0] = i;
           ptIds[1] = i + 1;
           cellId = newLines->InsertNextCell(2,ptIds);
