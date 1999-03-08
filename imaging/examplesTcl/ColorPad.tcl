2dc01f10a9a369d80dfff9331793f99b78c9da56
Bill Lorensen <lorensen@crd.ge.com>
1999-03-08 07:08:28 -0500

    ENH: Reduced image size.

--- imaging/examplesTcl/ColorPad.tcl
+++ imaging/examplesTcl/ColorPad.tcl
@@ -12,14 +12,14 @@ reader SetFileName "../../../vtkdata/earth.ppm"
 
 vtkImageMirrorPad pad
 pad SetInput [reader GetOutput]
-pad SetOutputWholeExtent -230 350 -230 350 0 92
+pad SetOutputWholeExtent -220 340 -220 340 0 92
 
 vtkImageViewer viewer
 viewer SetInput [pad GetOutput]
 viewer SetZSlice 22
 viewer SetColorWindow 255
 viewer SetColorLevel 127
-[viewer GetActor2D] SetDisplayPosition 230 230
+[viewer GetActor2D] SetDisplayPosition 220 220
 
 #make interface
 source WindowLevelInterface.tcl
