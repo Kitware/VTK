cc7a0202ea610e16d0abfd8a285148b0958da13d
Bill Lorensen <lorensen@crd.ge.com>
1999-04-09 15:58:04 -0400

    ENH: changed default view.

--- contrib/examplesTcl/reconstructSurface.tcl
+++ contrib/examplesTcl/reconstructSurface.tcl
@@ -57,10 +57,14 @@ vtkRenderWindowInteractor iren
 ren1 AddActor surfaceActor
 ren1 SetBackground 1 1 1
 renWin SetSize 500 500
-renWin Render
-
-set cam1 [ren1 GetActiveCamera]
-$cam1 Zoom 1.5
+[ren1 GetActiveCamera] SetFocalPoint 0 0 0
+[ren1 GetActiveCamera] SetPosition 1 0 0
+[ren1 GetActiveCamera] ComputeViewPlaneNormal
+[ren1 GetActiveCamera] SetViewUp 0 0 1
+ren1 ResetCamera
+[ren1 GetActiveCamera] Azimuth 20
+[ren1 GetActiveCamera] Elevation 30
+[ren1 GetActiveCamera] Dolly 1.2
 
 # render the image
 #
@@ -68,7 +72,7 @@ iren SetUserMethod {wm deiconify .vtkInteract}
 
 renWin Render
 
-renWin SetFileName reconstructionSurface.tcl.ppm
+renWin SetFileName reconstructSurface.tcl.ppm
 #renWin SaveImageAsPPM
 
 # prevent the tk window from showing up then start the event loop
