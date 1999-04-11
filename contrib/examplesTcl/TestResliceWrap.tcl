fab3132ac89c630f5da479250417023d02e29a4b
David Gobbi <david.gobbi@gmail.com>
1999-04-11 19:15:05 -0400

    New regression test.  I'm including it because it helped me weed out some important bugs in the class.

---
+++ contrib/examplesTcl/TestResliceWrap.tcl
@@ -0,0 +1,42 @@
+catch {load vtktcl}
+# Simple viewer for images.
+
+source ../../imaging/examplesTcl/vtkImageInclude.tcl
+
+# Image pipeline
+
+vtkPNMReader reader
+  reader SetFileName "../../../vtkdata/masonry.ppm"
+  reader SetDataExtent 0 255 0 255 0 0
+  reader SetDataSpacing 1 1 1
+  reader SetDataOrigin 0 0 0
+  reader UpdateWholeExtent
+
+vtkTransform transform
+  transform RotateZ 45
+  transform Translate 0 0 0
+  transform Scale 1.414 1.414 1.414
+
+vtkImageReslice reslice
+  reslice SetInput [reader GetOutput]
+  reslice SetResliceTransform transform
+  reslice InterpolateOn
+  reslice SetInterpolationModeToCubic
+  reslice WrapOn
+
+vtkImageViewer viewer
+  viewer SetInput [reslice GetOutput]
+  viewer SetZSlice 1
+  viewer SetColorWindow 256
+  viewer SetColorLevel 127.5
+  viewer Render
+
+source ../../imaging/examplesTcl/WindowLevelInterface.tcl
+
+vtkWindowToImageFilter windowToimage
+  windowToimage SetInput [viewer GetImageWindow]
+vtkPNMWriter pnmWriter
+  pnmWriter SetInput [windowToimage GetOutput]
+  pnmWriter SetFileName "TestResliceWrap.tcl.ppm"
+#  pnmWriter Write
+
