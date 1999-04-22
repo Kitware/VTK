c74ea53d3cde6510d7ebb40ce0e8d47c832b2963
Lisa Avila <lisa.avila@kitware.com>
1999-04-22 11:28:36 -0400

    Oops - forgot to check these in with that big change. This is the superclass for both vtkMapper and vtkVolumeMapper

---
+++ graphics/vtkAbstractMapper.cxx
@@ -0,0 +1,124 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkAbstractMapper.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-1998 Ken Martin, Will Schroeder, Bill Lorensen.
+
+This software is copyrighted by Ken Martin, Will Schroeder and Bill Lorensen.
+The following terms apply to all files associated with the software unless
+explicitly disclaimed in individual files. This copyright specifically does
+not apply to the related textbook "The Visualization Toolkit" ISBN
+013199837-4 published by Prentice Hall which is covered by its own copyright.
+
+The authors hereby grant permission to use, copy, and distribute this
+software and its documentation for any purpose, provided that existing
+copyright notices are retained in all copies and that this notice is included
+verbatim in any distributions. Additionally, the authors grant permission to
+modify this software and its documentation for any purpose, provided that
+such modifications are not distributed without the explicit consent of the
+authors and that existing copyright notices are retained in all copies. Some
+of the algorithms implemented by this software are patented, observe all
+applicable patent law.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+=========================================================================*/
+#include "vtkAbstractMapper.h"
+#include "vtkDataSet.h"
+
+// Construct with initial range (0,1).
+vtkAbstractMapper::vtkAbstractMapper()
+{
+  this->Input = NULL;
+  this->Bounds[0] = this->Bounds[2] = this->Bounds[4] = -1.0;
+  this->Bounds[1] = this->Bounds[3] = this->Bounds[5] = 1.0;
+  this->Center[0] = this->Center[1] = this->Center[2] = 0.0;
+  this->TimeToDraw = 0.0;
+}
+
+vtkAbstractMapper::~vtkAbstractMapper()
+{
+}
+
+
+// Get the bounds for this Prop as (Xmin,Xmax,Ymin,Ymax,Zmin,Zmax).
+float *vtkAbstractMapper::GetBounds()
+{
+  static float bounds[] = {-1.0,1.0, -1.0,1.0, -1.0,1.0};
+
+  if ( ! this->Input ) 
+    {
+    return bounds;
+    }
+  else
+    {
+    this->Input->Update();
+    this->Input->GetBounds(this->Bounds);
+    return this->Bounds;
+    }
+}
+
+// Get the bounds for this Prop as (Xmin,Xmax,Ymin,Ymax,Zmin,Zmax).
+void vtkAbstractMapper::GetBounds(float bounds[6])
+{
+  this->GetBounds();
+  for (int i=0; i<6; i++)
+    {
+    bounds[i] = this->Bounds[i];
+    }
+}
+
+float *vtkAbstractMapper::GetCenter()
+{
+  this->GetBounds();
+  for (int i=0; i<3; i++) 
+    {
+    this->Center[i] = (this->Bounds[2*i+1] + this->Bounds[2*i]) / 2.0;
+    }
+  return this->Center;
+}
+
+float vtkAbstractMapper::GetLength()
+{
+  double diff, l=0.0;
+  int i;
+
+  this->GetBounds();
+  for (i=0; i<3; i++)
+    {
+    diff = this->Bounds[2*i+1] - this->Bounds[2*i];
+    l += diff * diff;
+    }
+ 
+  return (float)sqrt(l);
+}
+
+void vtkAbstractMapper::PrintSelf(ostream& os, vtkIndent indent)
+{
+  vtkProcessObject::PrintSelf(os,indent);
+
+  if ( this->Input )
+    {
+    os << indent << "Input: (" << this->Input << ")\n";
+    }
+  else
+    {
+    os << indent << "Input: (none)\n";
+    }
+}
+
