450b2a67cf1dde5833d8a5f26969e5c5a85951e4
Ken Martin <ken.martin@kitware.com>
1999-04-27 08:41:12 -0400

    ENH: added access to RGB functions

--- graphics/vtkColorTransferFunction.h
+++ graphics/vtkColorTransferFunction.h
@@ -129,6 +129,12 @@ public:
   void SetClamping(int val);
   int  GetClamping();
 
+  // Description:
+  // Get the underlying Reg Green and Blue Piecewise Functions
+  vtkPiecewiseFunction *GetRedFunction(){return this->Red;};
+  vtkPiecewiseFunction *GetGreenFunction(){return this->Green;};
+  vtkPiecewiseFunction *GetBlueFunction(){return this->Blue;};
+  
 protected:
   // Determines the function value outside of defined points
   // in each of the R,G,B transfer functions.
