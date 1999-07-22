4325c4ec81b27575c058e2404c0f8c2df4b8d5e5
Jim Miller <millerjv@crd.ge.com>
1999-07-22 17:00:20 -0400

    ERR: added  undef is required on the hp. vtkMutexLock  ends up including /usr/inclue/dce/cma_ux.h which has the gall to #define read as cma_read.

--- graphics/vtkDataReader.cxx
+++ graphics/vtkDataReader.cxx
@@ -57,6 +57,14 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 #include "vtkDoubleArray.h"
 #include "vtkFloatArray.h"
 
+
+// this undef is required on the hp. vtkMutexLock ends up including
+// /usr/inclue/dce/cma_ux.h which has the gall to #define read as cma_read
+
+#ifdef read
+#undef read
+#endif
+
 // Construct object.
 vtkDataReader::vtkDataReader()
 {
