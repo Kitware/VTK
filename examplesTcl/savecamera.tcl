9bd31912fedb069814becc30249c1ff231564e70
Bill Lorensen <lorensen@crd.ge.com>
1999-07-22 19:53:06 -0400

    ENH: how to save a camera.

---
+++ examplesTcl/savecamera.tcl
@@ -0,0 +1,14 @@
+#
+# save a vtk camera to a file in tcl format
+#
+proc saveCamera { fileName } {
+    set filePtr [open $fileName w]
+    set camera "\[ren1 GetActiveCamera\]"
+    set activeCamera [ren1 GetActiveCamera]
+    puts $filePtr "$camera SetPosition [$activeCamera GetPosition]"
+    puts $filePtr "$camera SetFocalPoint [$activeCamera GetFocalPoint]"
+    puts $filePtr "$camera SetViewAngle [$activeCamera GetViewAngle]"
+    puts $filePtr "$camera SetViewUp [$activeCamera GetViewUp]"
+    puts $filePtr "$camera SetViewPlaneNormal [$activeCamera GetViewPlaneNormal]"
+    close $filePtr
+}
