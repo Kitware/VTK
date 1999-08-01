123a557a460fec11cb2edc17f7c444eff334e16e
Charles Law <charles.law@kitware.com>
1999-08-01 17:30:52 -0400

    For viewer

---
+++ imaging/vtkImageMandelbrotSource.cxx
@@ -0,0 +1,265 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkImageMandelbrotSource.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+  Thanks:    Thanks to C. Charles Law who developed this class.
+
+Copyright (c) 1993-1995 Ken Martin, Will Schroeder,ill Lorensen.
+
+This software is copyrighted by Ken Martin, Will Schroeder and Bill Lorensen.
+The following terms apply to all files associated with the software unless
+explicitly disclaimed in individual files. This copyright specifically does
+not apply to the related textbook "The Visualization Toolkit" ISBN
+013199837-4 published by Prentice Hall which is covered by its own copyright.
+
+The authors hereby grant permission to use, copy, and distribute this
+software and its documentation for any purpose, provided that existing
+copyright notices are retained in all copies and that this notice is included
+verbatim in any distributions. Additionally, the authors grant permission to
+modify this software and its documentation for any purpose, provided that
+such modifications are not distributed without the explicit consent of the
+authors and that existing copyright notices are retained in all copies. Some
+of the algorithms implemented by this software are patented, observe all
+applicable patent law.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+=========================================================================*/
+#include "vtkImageData.h"
+#include "vtkImageMandelbrotSource.h"
+
+//----------------------------------------------------------------------------
+vtkImageMandelbrotSource::vtkImageMandelbrotSource()
+{
+  this->MaximumNumberOfIterations = 100;
+  this->WholeExtent[0] = 0;
+  this->WholeExtent[1] = 500;
+  this->WholeExtent[2] = 0;
+  this->WholeExtent[3] = 500;
+  this->WholeExtent[4] = 0;
+  this->WholeExtent[5] = 0;
+  this->Spacing = 0.005;
+  this->Origin[0] = -3.2;
+  this->Origin[1] = -2.5;
+  this->Origin[2] = 0.0;
+  this->JuliaSet = 0;
+}
+
+//----------------------------------------------------------------------------
+vtkImageMandelbrotSource::~vtkImageMandelbrotSource()
+{
+}
+
+//----------------------------------------------------------------------------
+void vtkImageMandelbrotSource::PrintSelf(ostream& os, vtkIndent indent)
+{
+  vtkImageSource::PrintSelf(os,indent);
+
+  os << indent << "Origin: (" << this->Origin[0] << ", "
+     << this->Origin[1] << ", " << this->Origin[2] << ")\n";
+  os << indent << "Spacing: " << this->Spacing << "\n";
+
+  os << indent << "WholeExtent: (" << this->WholeExtent[0] << ", "
+     << this->WholeExtent[1] << ", " << this->WholeExtent[2] << ", "
+     << this->WholeExtent[3] << ", " << this->WholeExtent[4] << ", " 
+     << this->WholeExtent[5] << ")\n";
+  os << "MaximumNumberOfIterations: " << this->MaximumNumberOfIterations << endl;
+}
+
+//----------------------------------------------------------------------------
+void vtkImageMandelbrotSource::SetWholeExtent(int extent[6])
+{
+  int idx, modified = 0;
+  
+  for (idx = 0; idx < 6; ++idx)
+    {
+    if (this->WholeExtent[idx] != extent[idx])
+      {
+      this->WholeExtent[idx] = extent[idx];
+      this->Modified();
+      }
+    }
+  if (modified)
+    {
+    this->Modified();
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkImageMandelbrotSource::SetWholeExtent(int minX, int maxX, 
+					    int minY, int maxY,
+					    int minZ, int maxZ)
+{
+  int extent[6];
+  
+  extent[0] = minX;  extent[1] = maxX;
+  extent[2] = minY;  extent[3] = maxY;
+  extent[4] = minZ;  extent[5] = maxZ;
+  this->SetWholeExtent(extent);
+}
+
+
+//----------------------------------------------------------------------------
+void vtkImageMandelbrotSource::GetWholeExtent(int extent[6])
+{
+  int idx;
+  
+  for (idx = 0; idx < 6; ++idx)
+    {
+    extent[idx] = this->WholeExtent[idx];
+    }
+}
+
+
+//----------------------------------------------------------------------------
+void vtkImageMandelbrotSource::UpdateInformation()
+{
+  int *ext = this->WholeExtent;
+  vtkImageData *output = this->GetOutput();
+
+  output->SetSpacing(this->Spacing, this->Spacing, this->Spacing);
+  output->SetWholeExtent(this->WholeExtent);
+  output->SetOrigin(this->Origin[0], this->Origin[1], this->Origin[2]);
+  output->SetNumberOfScalarComponents(1);
+  output->SetScalarType(VTK_UNSIGNED_SHORT);
+}
+
+//----------------------------------------------------------------------------
+void vtkImageMandelbrotSource::Zoom(double factor)
+{
+  if (factor == 1.0)
+    {
+    return;
+    }
+  this->Modified();
+  this->Spacing *= factor;
+}
+
+//----------------------------------------------------------------------------
+void vtkImageMandelbrotSource::Pan(double x, double y, double z)
+{
+  if (x == 0.0 && y == 0.0 && z == 0.0)
+    {
+    return;
+    }
+  this->Modified();
+  this->Origin[0] += this->Spacing * x;
+  this->Origin[1] += this->Spacing * y;
+  this->Origin[2] += this->Spacing * z;
+}
+
+//----------------------------------------------------------------------------
+void vtkImageMandelbrotSource::Execute(vtkImageData *data)
+{
+  int *ext;
+  unsigned short *ptr;
+  int min0, max0;
+  int idx0, idx1, idx2;
+  int inc0, inc1, inc2;
+  float *origin;
+  double x, y, z;
+  unsigned long count = 0;
+  unsigned long target;
+  
+  ext = data->GetUpdateExtent();
+  ptr = (unsigned short *)(data->GetScalarPointerForExtent(ext));
+  origin = data->GetOrigin();
+
+  vtkDebugMacro("Generating Extent: " << ext[0] << " -> " << ext[1] << ", "
+          << ext[2] << " -> " << ext[3]);
+
+  vtkDebugMacro("Generating bounds: " << origin[0] + ext[0]*this->Spacing << " -> "
+          << origin[0] + ext[1]*this->Spacing << ", "
+          << origin[1] + ext[2]*this->Spacing << " -> "
+          << origin[1] + ext[3]*this->Spacing);
+  // Get min and max of axis 0 because it is the innermost loop.
+  min0 = ext[0];
+  max0 = ext[1];
+  data->GetContinuousIncrements(ext, inc0, inc1, inc2);
+
+  target = (unsigned long)((ext[5]-ext[4]+1)*(ext[3]-ext[2]+1)/50.0);
+  target++;
+
+  for (idx2 = ext[4]; idx2 <= ext[5]; ++idx2)
+    {
+    z = (double)(origin[2]) + (double)(idx2)*(this->Spacing);
+    for (idx1 = ext[2]; !this->AbortExecute && idx1 <= ext[3]; ++idx1)
+      {
+      if (!(count%target))
+        {
+        this->UpdateProgress(count/(50.0*target));
+        }
+      count++;
+      y = (double)(origin[1]) + (double)(idx1)*(this->Spacing);
+      for (idx0 = min0; idx0 <= max0; ++idx0)
+	{
+        x = (double)(origin[0]) + (double)(idx0)*this->Spacing;
+
+        *ptr = this->EvaluateSet(x, y, z);
+
+	++ptr;
+	// inc0 is 0
+	}
+      ptr += inc1;
+      }
+    ptr += inc2;
+    }
+}
+
+
+
+//----------------------------------------------------------------------------
+unsigned short vtkImageMandelbrotSource::EvaluateSet(double x, double y, 
+                                                     double z)
+{
+  unsigned short count = 0;
+  double cReal, cImag, zReal, zImag;
+  double zReal2, zImag2;
+
+  if (this->JuliaSet)
+    {
+    zReal = x;
+    zImag = y;
+    cReal = z;
+    cImag = -1;
+    }
+  else
+    {
+    zReal = z;
+    zImag = 0.0;
+    cReal = x;
+    cImag = y;
+  }
+
+  zReal2 = zReal * zReal;
+  zImag2 = zImag * zImag;
+  while ((zReal2 + zImag2) < 4.0 && count < this->MaximumNumberOfIterations)
+    {
+    zImag = 2.0 * zReal * zImag + cImag;
+    zReal = zReal2 - zImag2 + cReal;
+    zReal2 = zReal * zReal;
+    zImag2 = zImag * zImag;
+    ++count;
+    }
+
+  return count;
+}
+
+
+
+
+
+
+
---
+++ imaging/vtkImageMandelbrotSource.h
@@ -0,0 +1,120 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkImageMandelbrotSource.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+  Thanks:    Thanks to C. Charles Law who developed this class.
+
+Copyright (c) 1993-1995 Ken Martin, Will Schroeder, Bill Lorensen.
+
+This software is copyrighted by Ken Martin, Will Schroeder and Bill Lorensen.
+The following terms apply to all files associated with the software unless
+explicitly disclaimed in individual files. This copyright specifically does
+not apply to the related textbook "The Visualization Toolkit" ISBN
+013199837-4 published by Prentice Hall which is covered by its own copyright.
+
+The authors hereby grant permission to use, copy, and distribute this
+software and its documentation for any purpose, provided that existing
+copyright notices are retained in all copies and that this notice is included
+verbatim in any distributions. Additionally, the authors grant permission to
+modify this software and its documentation for any purpose, provided that
+such modifications are not distributed without the explicit consent of the
+authors and that existing copyright notices are retained in all copies. Some
+of the algorithms implemented by this software are patented, observe all
+applicable patent law.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+=========================================================================*/
+// .NAME vtkImageMandelbrotSource - Mandelbrot image.
+// .SECTION Description
+// vtkImageMandelbrotSource creates an unsigned char image of the Mandelbrot
+// set.  The values in the image are the number of iterations it takes for
+// the magnitude of the value to get over 2.  The equation repeated is
+// z = z^2 + C (z and C are complex).  Initial value of z is zero, and the 
+// real value of C is mapped onto the x axis, and the imaginary value of C
+// is mapped onto the Y Axis.  I was thinking of extending this source
+// to generate Julia Sets (initial value of Z varies).  This would be 4
+// possible parameters to vary, but there are no more 4d images :(
+// The third dimension (z axis) is the imaginary value of the initial value.
+
+#ifndef __vtkImageMandelbrotSource_h
+#define __vtkImageMandelbrotSource_h
+
+#include "vtkImageSource.h"
+
+class VTK_EXPORT vtkImageMandelbrotSource : public vtkImageSource
+{
+public:
+  vtkImageMandelbrotSource();
+  ~vtkImageMandelbrotSource();
+  static vtkImageMandelbrotSource *New() {return new vtkImageMandelbrotSource;};
+  const char *GetClassName() {return "vtkImageMandelbrotSource";};
+  void PrintSelf(ostream& os, vtkIndent indent);   
+  
+  // Description:
+  // Mandelbrot mode vs Julia set mode.
+  vtkSetMacro(JuliaSet, int);
+  vtkGetMacro(JuliaSet, int);
+  vtkBooleanMacro(JuliaSet, int);
+
+  // Description:
+  // Set/Get the extent of the whole output image.
+  void SetWholeExtent(int extent[6]);
+  void SetWholeExtent(int minX, int maxX, int minY, int maxY, 
+			    int minZ, int maxZ);
+  void GetWholeExtent(int extent[6]);
+  int *GetWholeExtent() {return this->WholeExtent;}
+  
+  // Description:
+  // Position of pixel 0, 0 ,0
+  vtkSetVector3Macro(Origin, double);
+  vtkGetVector3Macro(Origin, double);
+   
+  // Description:
+  // Set/Get the spacing used to specify the scale of the image.
+  vtkSetMacro(Spacing, double);
+  vtkGetMacro(Spacing, double);
+
+  // Description:
+  // The maximum number of cycles run to see if the value goes over 2
+  vtkSetClampMacro(MaximumNumberOfIterations, unsigned short, 1, 5000);
+  vtkGetMacro(MaximumNumberOfIterations, unsigned short);
+
+  // Description:
+  // Convienence for Viewer.  Pan relative to spacing.
+  // Zoom constant factor.
+  void Zoom(double factor);
+  void Pan(double x, double y, double z);
+
+  void UpdateInformation();
+
+protected:
+  int WholeExtent[6];
+  double Origin[3];
+  double Spacing;
+  unsigned short MaximumNumberOfIterations;
+
+  // flag to change to julia set
+  int JuliaSet;
+  
+  void Execute(vtkImageData *outData);
+  unsigned short EvaluateSet(double x, double y, double z);
+};
+
+
+#endif
+
+
