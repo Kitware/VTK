9ec2aff3659df4ea234206481468a857fbfca053
Will Schroeder <will.schroeder@kitware.com>
1999-09-30 14:49:50 -0400

    ENH:All mappers subclass of vtkProcessObject

---
+++ common/vtkAbstractMapper.h
@@ -0,0 +1,89 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkAbstractMapper.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-1998 Ken Martin, Will Schroeder, Bill Lorensen.
+
+This software is copyrighted by Ken Martin, Will Schroeder and Bill Lorensen.
+The following terms apply to all files associated with the software unless
+explicitly disclaimed in individual files. This copyright specifically does
+not apply to the related textbook "The Visualization Toolkit" ISBN
+013199837-4 published by Prentice Hall which is covered by its own copyright.
+
+The authors hereby grant permission to use, copy, and distribute this
+software and its documentation for any purpose, provided that existing
+copyright notices are retained in all copies and that this notice is included
+verbatim in any distributions. Additionally, the authors grant permission to
+modify this software and its documentation for any purpose, provided that
+such modifications are not distributed without the explicit consent of the
+authors and that existing copyright notices are retained in all copies. Some
+of the algorithms implemented by this software are patented, observe all
+applicable patent law.
+
+IN NO EVENT SHALL THE AUTHORS OR DISTRIBUTORS BE LIABLE TO ANY PARTY FOR
+DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+OF THE USE OF THIS SOFTWARE, ITS DOCUMENTATION, OR ANY DERIVATIVES THEREOF,
+EVEN IF THE AUTHORS HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+THE AUTHORS AND DISTRIBUTORS SPECIFICALLY DISCLAIM ANY WARRANTIES, INCLUDING,
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE, AND NON-INFRINGEMENT.  THIS SOFTWARE IS PROVIDED ON AN
+"AS IS" BASIS, AND THE AUTHORS AND DISTRIBUTORS HAVE NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+=========================================================================*/
+// .NAME vtkAbstractMapper - abstract class specifies interface to map data
+// .SECTION Description
+// vtkAbstractMapper is an abstract class to specify interface between data and 
+// graphics primitives or software rendering techniques. Subclasses of 
+// vtkAbstractMapper can be used for rendering 2D data, geometry, or volumetric
+// data.
+//
+// .SECTION See Also
+// vtkAbstractMapper3D vtkMapper vtkVolumeMapper
+
+#ifndef __vtkAbstractMapper_h
+#define __vtkAbstractMapper_h
+
+#include "vtkProcessObject.h"
+class vtkWindow;
+
+class VTK_EXPORT vtkAbstractMapper : public vtkProcessObject
+{
+public:
+  const char *GetClassName() {return "vtkAbstractMapper";};
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Instantiate object.
+  static vtkAbstractMapper *New() {return new vtkAbstractMapper;};
+
+  // Description:
+  // Release any graphics resources that are being consumed by this mapper.
+  // The parameter window could be used to determine which graphic
+  // resources to release.
+  virtual void ReleaseGraphicsResources(vtkWindow *) {};
+
+  // Description:
+  // Get the time required to draw the geometry last time it was rendered
+  vtkGetMacro( TimeToDraw, float );
+
+protected:
+  vtkAbstractMapper();
+  ~vtkAbstractMapper() {};
+  vtkAbstractMapper(const vtkAbstractMapper&) {};
+  void operator=(const vtkAbstractMapper&) {};
+
+  float TimeToDraw;
+
+};
+
+#endif
+
+
