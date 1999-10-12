5058d46ac032763a1e0c6188e38ded45a5b771d2
David Gobbi <david.gobbi@gmail.com>
1999-10-11 22:31:34 -0400

    ERR: Found the 'real' bug that was causing the python example segfaults -      the ConfigureNotify event didn't Render unless the window changed      size.  However, when the window is first mapped a ConfigureNotify      is sent without a size change and a blank window results. ENH: Now Start() will call Initialize() if necessary

--- graphics/vtkXRenderWindowInteractor.cxx
+++ graphics/vtkXRenderWindowInteractor.cxx
@@ -160,6 +160,10 @@ void vtkXRenderWindowInteractor::SetTopLevelShell(Widget topLevel)
 // application is exited.
 void vtkXRenderWindowInteractor::Start()
 {
+  if (!this->Initialized)
+    {
+    this->Initialize();
+    }
   XtAppMainLoop(this->App);
 }
 
@@ -180,16 +184,12 @@ void vtkXRenderWindowInteractor::Initialize()
   static int any_initialized = 0;
   static XtAppContext app;
   vtkXRenderWindow *ren;
-  int needs_render;
   int depth;
   Colormap cmap;
   Visual  *vis;
   int *size;
   int *position;
   int argc = 0;
-
-  // set if the window hasn't rendered yet
-  needs_render = !this->Initialized;
 
   // make sure we have a RenderWindow and camera
   if ( ! this->RenderWindow)
@@ -286,11 +286,6 @@ void vtkXRenderWindowInteractor::Initialize()
   this->Enable();
   this->Size[0] = size[0];
   this->Size[1] = size[1];
-  if (needs_render) 
-    {
-    ren->Render();
-    }
-
 }
 
 void vtkXRenderWindowInteractor::Enable()
@@ -440,17 +435,10 @@ void vtkXRenderWindowInteractorCallback(Widget vtkNotUsed(w),
 	// just getting the last configure event
 	event = &result;
 	}
-      if ((((XConfigureEvent *)event)->width != me->Size[0]) ||
-	  (((XConfigureEvent *)event)->height != me->Size[1]))
-	{
-	me->UpdateSize(((XConfigureEvent *)event)->width,
-		       ((XConfigureEvent *)event)->height); 
-	
 	// only render if we are currently accepting events
-	if (me->GetEnabled())
-	  {
-	  me->GetRenderWindow()->Render();
-	  }
+      if (me->GetEnabled())
+	{
+	me->GetRenderWindow()->Render();
 	}
       }
       break;
