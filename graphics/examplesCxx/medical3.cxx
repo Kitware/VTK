f7e4f342e4b9c8074b5f14eeeb91e95cbae26f62
Will Schroeder <will.schroeder@kitware.com>
1999-10-19 15:20:02 -0400

    medical3.cxx

--- graphics/examplesCxx/medical3.cxx
+++ graphics/examplesCxx/medical3.cxx
@@ -8,7 +8,8 @@
 #include "vtkCamera.h"
 #include "vtkStripper.h"
 #include "vtkLookupTable.h"
-#include "vtkStructuredPointsGeometryFilter.h"
+#include "vtkExtractVOI.h"
+#include "vtkPlaneSource.h"
 #include "vtkContourFilter.h"
 
 #include "SaveImage.h"
@@ -73,6 +74,7 @@ void main( int argc, char *argv[] )
     bwLut->SetSaturationRange (0, 0);
     bwLut->SetHueRange (0, 0);
     bwLut->SetValueRange (0, 1);
+    bwLut->Build ();
 
   // create a hue lookup table
   vtkLookupTable *hueLut = vtkLookupTable::New();
@@ -80,6 +82,7 @@ void main( int argc, char *argv[] )
     hueLut->SetHueRange (0, 1);
     hueLut->SetSaturationRange (1, 1);
     hueLut->SetValueRange (1, 1);
+        hueLut->Build();
 
   // create a saturation lookup table
   vtkLookupTable *satLut = vtkLookupTable::New();
@@ -87,45 +90,73 @@ void main( int argc, char *argv[] )
     satLut->SetHueRange (.6, .6);
     satLut->SetSaturationRange (0, 1);
     satLut->SetValueRange (1, 1);
+        satLut->Build();
 
   // sagittal
-  vtkStructuredPointsGeometryFilter *saggitalSection = 
-      vtkStructuredPointsGeometryFilter::New();
-    saggitalSection->SetExtent (32,32, 0,63, 0, 93);
+  vtkExtractVOI *saggitalSection = vtkExtractVOI::New();
+    saggitalSection->SetVOI (32,32, 0,63, 0, 93);
     saggitalSection->SetInput (v16->GetOutput());
+  vtkTexture *saggitalTexture = vtkTexture::New();
+    saggitalTexture->SetInput(saggitalSection->GetOutput());
+    saggitalTexture->InterpolateOn();
+    saggitalTexture->SetLookupTable (bwLut);
+    saggitalTexture->MapColorScalarsThroughLookupTableOn();
+  vtkPlaneSource *saggitalPlane = vtkPlaneSource::New();
+    saggitalPlane->SetXResolution(1);
+    saggitalPlane->SetYResolution(1);
+    saggitalPlane->SetOrigin(3.2*32.0, 0.0, 0.0);
+    saggitalPlane->SetPoint1(3.2*32.0, 3.2*63.0, 0.0);
+    saggitalPlane->SetPoint2(3.2*32.0, 0.0, 1.5*93.0);
   vtkPolyDataMapper *saggitalMapper = vtkPolyDataMapper::New();
-    saggitalMapper->SetInput(saggitalSection->GetOutput());
-    saggitalMapper->ScalarVisibilityOn();
-    saggitalMapper->SetScalarRange (0, 2000);
-    saggitalMapper->SetLookupTable (bwLut);
+    saggitalMapper->SetInput(saggitalPlane->GetOutput());
+    saggitalMapper->ImmediateModeRenderingOn();
   vtkActor *sagittal = vtkActor::New();
     sagittal->SetMapper(saggitalMapper);
+    sagittal->SetTexture(saggitalTexture);
 
   // axial
-  vtkStructuredPointsGeometryFilter *axialSection = 
-      vtkStructuredPointsGeometryFilter::New();
-    axialSection->SetExtent (0,63, 0,63, 46, 46);
+  vtkExtractVOI *axialSection = vtkExtractVOI::New();
+    axialSection->SetVOI (0,63, 0,63, 46,46);
     axialSection->SetInput (v16->GetOutput());
+  vtkTexture *axialTexture = vtkTexture::New();
+    axialTexture->SetInput(axialSection->GetOutput());
+    axialTexture->InterpolateOn();
+    axialTexture->SetLookupTable (hueLut);
+    axialTexture->MapColorScalarsThroughLookupTableOn();
+  vtkPlaneSource *axialPlane = vtkPlaneSource::New();
+    axialPlane->SetXResolution(1);
+    axialPlane->SetYResolution(1);
+    axialPlane->SetOrigin(0.0, 0.0, 1.5*46);
+    axialPlane->SetPoint1(3.2*63, 0.0, 1.5*46);
+    axialPlane->SetPoint2(0.0, 3.2*63, 1.5*46.0);
   vtkPolyDataMapper *axialMapper = vtkPolyDataMapper::New();
-    axialMapper->SetInput(axialSection->GetOutput());
-    axialMapper->ScalarVisibilityOn();
-    axialMapper->SetScalarRange (0, 2000);
-    axialMapper->SetLookupTable (hueLut);
+    axialMapper->SetInput(axialPlane->GetOutput());
+    axialMapper->ImmediateModeRenderingOn();
   vtkActor *axial = vtkActor::New();
     axial->SetMapper(axialMapper);
+    axial->SetTexture(axialTexture);
 
   // coronal
-  vtkStructuredPointsGeometryFilter *coronalSection = 
-      vtkStructuredPointsGeometryFilter::New();
-    coronalSection->SetExtent (0,63, 32, 32, 0, 92);
+  vtkExtractVOI *coronalSection = vtkExtractVOI::New();
+    coronalSection->SetVOI (0,63, 32,32, 0,92);
     coronalSection->SetInput (v16->GetOutput());
+  vtkTexture *coronalTexture = vtkTexture::New();
+    coronalTexture->SetInput(coronalSection->GetOutput());
+    coronalTexture->InterpolateOn();
+    coronalTexture->SetLookupTable (hueLut);
+    coronalTexture->MapColorScalarsThroughLookupTableOn();
+  vtkPlaneSource *coronalPlane = vtkPlaneSource::New();
+    coronalPlane->SetXResolution(1);
+    coronalPlane->SetYResolution(1);
+    coronalPlane->SetOrigin(0.0, 3.2*32, 0.0);
+    coronalPlane->SetPoint1(3.2*63, 3.2*32, 0.0);
+    coronalPlane->SetPoint2(0.0, 3.2*32, 1.5*92.0);
   vtkPolyDataMapper *coronalMapper = vtkPolyDataMapper::New();
-    coronalMapper->SetInput(coronalSection->GetOutput());
-    coronalMapper->ScalarVisibilityOn();
-    coronalMapper->SetScalarRange (0, 2000);
-    coronalMapper->SetLookupTable (satLut);
+    coronalMapper->SetInput(coronalPlane->GetOutput());
+    coronalMapper->ImmediateModeRenderingOn();
   vtkActor *coronal = vtkActor::New();
     coronal->SetMapper(coronalMapper);
+    coronal->SetTexture(coronalTexture);
 
   // create a camera with the correct view up
   vtkCamera *aCamera = vtkCamera::New();
