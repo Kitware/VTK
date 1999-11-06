dd2f6c0400c77f7cfdf2899c4dc9b4114e6085fb
Bill Lorensen <lorensen@crd.ge.com>
1999-11-06 08:59:07 -0500

    ERR: memory leak in SetWindowName repaired.

--- imaging/vtkXImageWindow.cxx
+++ imaging/vtkXImageWindow.cxx
@@ -386,12 +386,14 @@ void vtkXImageWindow::SetWindowName(char* name)
     {
     if( XStringListToTextProperty( &name, 1, &win_name_text_prop ) == 0 )
       {
+      XFree (win_name_text_prop.value);
       vtkWarningMacro(<< "Can't rename window"); 
       return;
       }
     
     XSetWMName( this->DisplayId, this->WindowId, &win_name_text_prop );
     XSetWMIconName( this->DisplayId, this->WindowId, &win_name_text_prop );
+    XFree (win_name_text_prop.value);
     }
 }
 
