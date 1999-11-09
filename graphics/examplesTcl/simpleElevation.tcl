97f959aed4474d35d0619e7a8b23f48e89a4591f
Will Schroeder <will.schroeder@kitware.com>
1999-11-09 15:41:01 -0500

    ENH:Test vtkSimpleElevationFilter

---
+++ graphics/examplesTcl/simpleElevation.tcl
@@ -0,0 +1,39 @@
+catch {load vtktcl}
+# Example demonstrates generating scalars based on elevation.
+#
+source ../../examplesTcl/vtkInt.tcl
+
+vtkSphereSource sphere
+    sphere SetPhiResolution 12
+    sphere SetThetaResolution 12
+
+vtkSimpleElevationFilter colorIt
+  colorIt SetInput [sphere GetOutput]
+  colorIt SetVector 0 1 0; #do this to get better coverage :)
+  colorIt SetVector 0 0 1
+
+vtkPolyDataMapper mapper
+  mapper SetInput [colorIt GetPolyDataOutput] 
+  mapper SetScalarRange -1 1
+
+vtkActor actor
+  actor SetMapper mapper
+
+vtkRenderer ren1
+vtkRenderWindow renWin
+    renWin AddRenderer ren1
+vtkRenderWindowInteractor iren
+    iren SetRenderWindow renWin
+
+ren1 AddActor actor
+ren1 SetBackground 1 1 1
+renWin SetSize 400 400
+[ren1 GetActiveCamera] Zoom 1.4
+
+iren SetUserMethod {wm deiconify .vtkInteract}
+iren Initialize
+
+renWin SetFileName simpleElevation.tcl.ppm
+#renWin SaveImageAsPPM
+
+wm withdraw .
