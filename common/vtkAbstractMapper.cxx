0591a248c78098eb004df8409635aa51b9e43146
Bill Lorensen <lorensen@crd.ge.com>
1999-11-21 17:58:45 -0500

    ENH: Put New() back because of Python Problems.

--- common/vtkAbstractMapper.cxx
+++ common/vtkAbstractMapper.cxx
@@ -45,6 +45,21 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
 
 //------------------------------------------------------------------------------
+vtkAbstractMapper* vtkAbstractMapper::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkAbstractMapper");
+  if(ret)
+    {
+    return (vtkAbstractMapper*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkAbstractMapper;
+}
+
+
+
+
 // Construct object.
 vtkAbstractMapper::vtkAbstractMapper()
 {
