fbf8aa0c95aa0a543b47d6f71ab3a2fa819d26ba
Bill Lorensen <lorensen@crd.ge.com>
1999-11-24 08:30:02 -0500

    ERR: viewer SetZSlice 0.

--- imaging/examplesTcl/TestBlend.tcl
+++ imaging/examplesTcl/TestBlend.tcl
@@ -36,26 +36,19 @@ vtkImageViewer viewer
 viewer SetInput [blend GetOutput]
 viewer SetColorWindow 255.0
 viewer SetColorLevel 127.5
-viewer SetZSlice 50
+viewer SetZSlice 0
 
 viewer Render
 set opacity 1
-
-#make interface
-source WindowLevelInterface.tcl
-
-frame .wl.f3
-label .wl.f3.opacityLabel -text "Opacity"
-scale .wl.f3.opacity -from 0.0 -to 1.0 \
-     -orient horizontal -command SetOpacity -variable opacity -resolution .01
-pack .wl.f3 -side top
-pack .wl.f3.opacityLabel .wl.f3.opacity -side left
 
 proc SetOpacity opacity {
     blend SetOpacity 1 $opacity
     viewer Render
 }
 
+SetOpacity 1
+
+#make interface
 vtkWindowToImageFilter windowToimage
   windowToimage SetInput [viewer GetImageWindow]
 
@@ -63,4 +56,18 @@ vtkPNMWriter pnmWriter
   pnmWriter SetInput [windowToimage GetOutput]
   pnmWriter SetFileName "TestBlend.tcl.ppm"
 #  pnmWriter Write
+
+source ../../imaging/examplesTcl/WindowLevelInterface.tcl
+
+# only show ui if not testing
+if {[info commands rtExMath] != "rtExMath"} {
+frame .wl.f3
+label .wl.f3.opacityLabel -text "Opacity"
+scale .wl.f3.opacity -from 0.0 -to 1.0 \
+     -orient horizontal -command SetOpacity -variable opacity -resolution .01
+pack .wl.f3 -side top
+pack .wl.f3.opacityLabel .wl.f3.opacity -side left
+}
+
+
 
