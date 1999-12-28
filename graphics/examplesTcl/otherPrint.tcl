37333ad4e8335b80ac03c8e7fb5ae9b5854918ed
Bill Lorensen <lorensen@crd.ge.com>
1999-12-28 14:03:52 -0500

    ENH: first check in.

---
+++ graphics/examplesTcl/otherPrint.tcl
@@ -0,0 +1,41 @@
+# All tests need to:
+# Define 2 variables and 1 proc
+#     var 1: rtSelector - a shell command line to select the output to be compared
+#     var 2: rtComparator - a shell command to compare the selected output with the baseline
+#     proc 1: rtOtherTest (fileid)
+# A few statements at the end to run outside the regression testing framework
+#
+
+catch {load vtktcl}
+
+set rtSelector "grep -v 'Number of Threads' | grep -v 0x | grep -v Modified"
+set rtComparator "diff"
+
+proc rtOtherTest { fileid } {
+#actual test
+    puts $fileid "Printf test started"
+
+    set all [lsort [info command vtk*]]
+    foreach a $all {
+	puts $fileid "$a --------------"
+	if {$a == "vtkIndent"} {
+	    continue
+	}
+	if {$a == "vtkOutputPort"} {
+	    continue
+	}
+	if {$a == "vtkTimeStamp"} {
+	    continue
+	}
+	catch {$a b; puts $fileid "[b Print]"; b Delete}
+    }
+}
+
+# All tests should end with the following...
+
+if {![info exists rtOutId]} {
+    rtOtherTest stdout
+    exit
+}
+
+wm withdraw .
