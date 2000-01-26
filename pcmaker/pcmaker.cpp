0b00911910f05956265c5476d56ea6f37440b8e6
Ken Martin <ken.martin@kitware.com>
2000-01-26 10:48:35 -0500

    fixed nightly

--- pcmaker/pcmaker.cpp
+++ pcmaker/pcmaker.cpp
@@ -298,10 +298,10 @@ BOOL CPcmakerApp::InitInstance()
     dlg.adlg.m_WhereTk  = "";
     dlg.adlg.m_EXTRA_LINK_FLAGS = "";
     dlg.adlg.m_LibPrefix  = "vtk";
-    dlg.Create(IDD_PCMAKER_DIALOG,NULL);
     dlg.m_BuildJava = TRUE;
     dlg.m_BuildPython = TRUE;
     dlg.m_BuildTcl = TRUE;
+    dlg.Create(IDD_PCMAKER_DIALOG,NULL);
     dlg.DoOKStuff();
     }
    else if (strncmp(m_lpCmdLine, "qualityNT", 9) == 0)  // skip any trailing characters
