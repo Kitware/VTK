51d13950d48810b1ddcee944af92c08b12009812
John Biddiscombe <jbiddiscombe@skippingmouse.co.uk>
2000-02-25 13:28:58 -0500

    ENH: Added SetInstance for convenient redirection of messages

--- common/vtkOutputWindow.h
+++ common/vtkOutputWindow.h
@@ -71,6 +71,11 @@ public:
   // Description:
   // Return the singleton instance with no reference counting.
   static vtkOutputWindow* GetInstance();
+  // Description:
+  // Supply a user defined output window. Call ->Delete() on the supplied
+  // instance after setting it.
+  static void SetInstance(vtkOutputWindow *instance);
+  //
   virtual void DisplayText(const char*);
   // Description:
   // If PromptUser is set to true then each time a line of text
@@ -79,7 +84,7 @@ public:
   vtkBooleanMacro(PromptUser,int);
   vtkSetMacro(PromptUser, int);
 protected:
-  vtkOutputWindow(); 
+  vtkOutputWindow();
   virtual ~vtkOutputWindow(); 
   vtkOutputWindow(const vtkOutputWindow&) {};
   void operator=(const vtkOutputWindow&) {};
