87f490b2fb92ff6fa5041b2ca5bc0160b08924c0
David Gobbi <david.gobbi@gmail.com>
2000-03-12 23:21:58 -0500

    update vtkParseJavaBeans to match vtkParseJava

--- wrap/vtkParseJavaBeans.c
+++ wrap/vtkParseJavaBeans.c
@@ -51,7 +51,6 @@ FunctionInfo *currentFunction;
 void output_temp(FILE *fp,int i)
 {
   /* ignore void */
-
   if (((currentFunction->ArgTypes[i] % 10) == 2)&&
       (!((currentFunction->ArgTypes[i]%1000)/100)))
     {
@@ -64,7 +63,7 @@ void output_temp(FILE *fp,int i)
     return;
     }
   
-  if (currentFunction->ArgTypes[i] == 303)
+  if (currentFunction->ArgTypes[i]%1000 == 303)
     {
     fprintf(fp,"String ");
     }
@@ -86,8 +85,8 @@ void output_temp(FILE *fp,int i)
 
   fprintf(fp,"id%i",i);
   if (((currentFunction->ArgTypes[i]%1000)/100 == 3)&&
-      (currentFunction->ArgTypes[i] != 303)&&
-      (currentFunction->ArgTypes[i] != 309))
+      (currentFunction->ArgTypes[i]%1000 != 303)&&
+      (currentFunction->ArgTypes[i]%1000 != 309))
     {
     fprintf(fp,"[]");
     }
@@ -137,16 +136,32 @@ int DoneOne()
       for (j = 0; j < fi->NumberOfArguments; j++)
 	{
 	if ((fi->ArgTypes[j] != currentFunction->ArgTypes[j]) &&
-	    !(((fi->ArgTypes[j] == 309)&&
-	       (currentFunction->ArgTypes[j] == 109)) ||
-	      ((fi->ArgTypes[j] == 109)&&
-	       (currentFunction->ArgTypes[j] == 309))))
+	    !(((fi->ArgTypes[j]%1000 == 309)&&
+	       (currentFunction->ArgTypes[j]%1000 == 109)) ||
+	      ((fi->ArgTypes[j]%1000 == 109)&&
+	       (currentFunction->ArgTypes[j]%1000 == 309)) ||
+	      ((fi->ArgTypes[j]%1000 == 301)&&
+	       (currentFunction->ArgTypes[j]%1000 == 307)) ||
+	      ((fi->ArgTypes[j]%1000 == 307)&&
+	       (currentFunction->ArgTypes[j]%1000 == 301)) ||
+	      ((fi->ArgTypes[j]%1000 == 304)&&
+	       (currentFunction->ArgTypes[j]%1000 == 306)) ||
+	      ((fi->ArgTypes[j]%1000 == 306)&&
+	       (currentFunction->ArgTypes[j]%1000 == 304)) ||
+	      ((fi->ArgTypes[j]%1000 == 1)&&
+	       (currentFunction->ArgTypes[j]%1000 == 7)) ||
+	      ((fi->ArgTypes[j]%1000 == 7)&&
+	       (currentFunction->ArgTypes[j]%1000 == 1)) ||
+	      ((fi->ArgTypes[j]%1000 == 4)&&
+	       (currentFunction->ArgTypes[j]%1000 == 6)) ||
+	      ((fi->ArgTypes[j]%1000 == 6)&&
+	       (currentFunction->ArgTypes[j]%1000 == 4))))
 	  {
 	  match = 0;
 	  }
 	else
 	  {
-	  if (fi->ArgTypes[j] == 309 || fi->ArgTypes[j] == 109)
+	  if (fi->ArgTypes[j]%1000 == 309 || fi->ArgTypes[j]%1000 == 109)
 	    {
 	    if (strcmp(fi->ArgClasses[j],currentFunction->ArgClasses[j]))
 	      {
@@ -156,14 +171,32 @@ int DoneOne()
 	  }
 	}
       if ((fi->ReturnType != currentFunction->ReturnType) &&
-	  !(((fi->ReturnType == 309)&&(currentFunction->ReturnType == 109)) ||
-	    ((fi->ReturnType == 109)&&(currentFunction->ReturnType == 309))))
+	  !(((fi->ReturnType%1000 == 309)&&
+	     (currentFunction->ReturnType%1000 == 109)) ||
+	    ((fi->ReturnType%1000 == 109)&&
+	     (currentFunction->ReturnType%1000 == 309)) ||
+	    ((fi->ReturnType%1000 == 301)&&
+	     (currentFunction->ReturnType%1000 == 307)) ||
+	    ((fi->ReturnType%1000 == 307)&&
+	     (currentFunction->ReturnType%1000 == 301)) ||
+	    ((fi->ReturnType%1000 == 304)&&
+	     (currentFunction->ReturnType%1000 == 306)) ||
+	    ((fi->ReturnType%1000 == 306)&&
+	     (currentFunction->ReturnType%1000 == 304)) ||
+	    ((fi->ReturnType%1000 == 1)&&
+	     (currentFunction->ReturnType%1000 == 7)) ||
+	    ((fi->ReturnType%1000 == 7)&&
+	     (currentFunction->ReturnType%1000 == 1)) ||
+	    ((fi->ReturnType%1000 == 4)&&
+	     (currentFunction->ReturnType%1000 == 6)) ||
+	    ((fi->ReturnType%1000 == 6)&&
+	     (currentFunction->ReturnType%1000 == 4))))
 	{
 	match = 0;
 	}
       else
 	{
-	if (fi->ReturnType == 309 || fi->ReturnType == 109)
+	if (fi->ReturnType%1000 == 309 || fi->ReturnType%1000 == 109)
 	  {
 	  if (strcmp(fi->ReturnClass,currentFunction->ReturnClass))
 	    {
@@ -201,28 +234,28 @@ void outputFunction(FILE *fp, FileInfo *data)
   /* check to see if we can handle the args */
   for (i = 0; i < currentFunction->NumberOfArguments; i++)
     {
-    if (currentFunction->ArgTypes[i] == 9) args_ok = 0;
+    if (currentFunction->ArgTypes[i]%1000 == 9) args_ok = 0;
     if ((currentFunction->ArgTypes[i]%10) == 8) args_ok = 0;
     if (((currentFunction->ArgTypes[i]%1000)/100 != 3)&&
 	(currentFunction->ArgTypes[i]%1000 != 109)&&
 	((currentFunction->ArgTypes[i]%1000)/100)) args_ok = 0;
-    if (currentFunction->ArgTypes[i] == 313) args_ok = 0;
-    if (currentFunction->ArgTypes[i] == 314) args_ok = 0;
-    if (currentFunction->ArgTypes[i] == 315) args_ok = 0;
-    if (currentFunction->ArgTypes[i] == 316) args_ok = 0;
+    if (currentFunction->ArgTypes[i]%1000 == 313) args_ok = 0;
+    if (currentFunction->ArgTypes[i]%1000 == 314) args_ok = 0;
+    if (currentFunction->ArgTypes[i]%1000 == 315) args_ok = 0;
+    if (currentFunction->ArgTypes[i]%1000 == 316) args_ok = 0;
     }
   if ((currentFunction->ReturnType%10) == 8) args_ok = 0;
-  if (currentFunction->ReturnType == 9) args_ok = 0;
+  if (currentFunction->ReturnType%1000 == 9) args_ok = 0;
   if (((currentFunction->ReturnType%1000)/100 != 3)&&
       (currentFunction->ReturnType%1000 != 109)&&
       ((currentFunction->ReturnType%1000)/100)) args_ok = 0;
 
 
   /* eliminate unsigned char * and unsigned short * */
-  if (currentFunction->ReturnType == 313) args_ok = 0;
-  if (currentFunction->ReturnType == 314) args_ok = 0;
-  if (currentFunction->ReturnType == 315) args_ok = 0;
-  if (currentFunction->ReturnType == 316) args_ok = 0;
+  if (currentFunction->ReturnType%1000 == 313) args_ok = 0;
+  if (currentFunction->ReturnType%1000 == 314) args_ok = 0;
+  if (currentFunction->ReturnType%1000 == 315) args_ok = 0;
+  if (currentFunction->ReturnType%1000 == 316) args_ok = 0;
 
   if (currentFunction->NumberOfArguments && 
       (currentFunction->ArgTypes[0] == 5000)
@@ -233,8 +266,8 @@ void outputFunction(FILE *fp, FileInfo *data)
     {
     if (((currentFunction->ArgTypes[i]%1000)/100 == 3)&&
 	(currentFunction->ArgCounts[i] <= 0)&&
-	(currentFunction->ArgTypes[i] != 309)&&
-	(currentFunction->ArgTypes[i] != 303)) args_ok = 0;
+	(currentFunction->ArgTypes[i]%1000 != 309)&&
+	(currentFunction->ArgTypes[i]%1000 != 303)) args_ok = 0;
     }
 
   /* if we need a return type hint make sure we have one */
@@ -313,8 +346,8 @@ void outputFunction(FILE *fp, FileInfo *data)
 	/* only care about set methods and On/Off methods */
 	if (!strncmp(beanfunc,"set",3) && 
 	    currentFunction->NumberOfArguments == 1 && 
-	    (currentFunction->ArgTypes[0] < 10 || 
-	     currentFunction->ArgTypes[0] == 303 || 
+	    (currentFunction->ArgTypes[0]%1000 < 10 || 
+	     currentFunction->ArgTypes[0]%1000 == 303 || 
 	     currentFunction->ArgTypes[0]%10 == 9))
 	  {
 	  char prop[256];
@@ -325,7 +358,7 @@ void outputFunction(FILE *fp, FileInfo *data)
 	  fprintf(fp,");\n      changes.firePropertyChange(\"%s\",null,",prop);
 	  
 	  /* handle basic types */
-	  if (currentFunction->ArgTypes[0] == 303)
+	  if (currentFunction->ArgTypes[0]%1000 == 303)
 	    {
 	    fprintf(fp," id0");
 	    }
@@ -411,7 +444,6 @@ if (strcmp("vtkObject",data->ClassName))
     currentFunction = data->Functions + i;
     outputFunction(fp, data);
     }
-
   
 if (!data->NumberOfSuperClasses)
     {
