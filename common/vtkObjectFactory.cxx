bfc8083f2a59150fe2a8aa83c07d96b952c726d3
Bill Hoffman <bill.hoffman@kitware.com>
2000-03-15 15:39:48 -0500

    ENH: remove warning

--- common/vtkObjectFactory.cxx
+++ common/vtkObjectFactory.cxx
@@ -167,7 +167,6 @@ void vtkObjectFactory::LoadLibrariesInPath(const char* path)
     // for a shared library in it.
     if(vtkNameIsSharedLibrary(file))
       {
-      vtkGenericWarningMacro("Attempt to load dll: " << file);
       char* fullpath = CreateFullPath(path, file);
       vtkLibHandle lib = vtkDynamicLoader::OpenLibrary(fullpath);
       if(lib)
