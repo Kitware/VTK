ea7a648ee61c428b9311248490c9fb5d8f756ed2
Bill Lorensen <lorensen@crd.ge.com>
2000-04-25 07:27:24 -0400

    ERR: removed warnings.

--- graphics/vtkExtractGrid.cxx
+++ graphics/vtkExtractGrid.cxx
@@ -122,7 +122,7 @@ void vtkExtractGrid::ExecuteInformation()
   if ( this->IncludeBoundary && 
        (rate[0] != 1 || rate[1] != 1 || rate[2] != 1) )
     {
-    int i, diff;
+    int diff;
     for (i=0; i<3; i++)
       {
       if ( ((diff=voi[2*i+1]-voi[2*i]) > 0) && rate[i] != 1 &&
@@ -152,7 +152,7 @@ void vtkExtractGrid::Execute()
   vtkStructuredGrid *output= this->GetOutput();
   vtkPointData *outPD=output->GetPointData();
   vtkCellData *outCD=output->GetCellData();
-  int i, j, k, dims[3], outDims[3], voi[6], extent[6], dim, idx, newIdx;
+  int i, j, k, dims[3], outDims[3], voi[6], dim, idx, newIdx;
   int newCellId;
   int sliceSize, outSize, jOffset, kOffset, rate[3];
   vtkPoints *newPts, *inPts;
