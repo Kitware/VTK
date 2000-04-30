32250987754472607213d0b6ba40df54403247ed
David Gobbi <david.gobbi@gmail.com>
2000-04-30 19:03:57 -0400

    ERR: the wrong base class was specified in vtkTypeMacro

--- contrib/vtkThinPlateSplineTransform.h
+++ contrib/vtkThinPlateSplineTransform.h
@@ -66,7 +66,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 class VTK_EXPORT vtkThinPlateSplineTransform : public vtkWarpTransform
 {
 public:
-  vtkTypeMacro(vtkThinPlateSplineTransform,vtkGeneralTransform);
+  vtkTypeMacro(vtkThinPlateSplineTransform,vtkWarpTransform);
   void PrintSelf(ostream& os, vtkIndent indent);
   static vtkThinPlateSplineTransform *New();
 
