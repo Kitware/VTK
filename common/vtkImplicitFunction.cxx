57ff442446b9e06e8c249728722ee6e8642e7b06
David Gobbi <david.gobbi@gmail.com>
2000-05-05 14:40:06 -0400

    Reverse the earlier change to negate the function value when the determinant of the Jacobian (the 3x3 portion of the transformation) is negative.  Takes too many CPU cycles, and it's a really picky point about how flip transformations should be handled.

--- common/vtkImplicitFunction.cxx
+++ common/vtkImplicitFunction.cxx
@@ -63,13 +63,23 @@ float vtkImplicitFunction::FunctionValue(const float x[3])
   else //pass point through transform
     {
     float pt[3];
+    this->Transform->TransformPoint(x,pt);
+    return this->EvaluateFunction(pt);
+    }
+
+  /* Return negative if determinant of Jacobian matrix is negative,
+     i.e. if the transformation has a flip.  This is more 'correct'
+     than the above behaviour, because it turns the implicit surface
+     inside-out in the same way that polygonal surfaces are turned
+     inside-out by a flip.  It takes up too many valuable CPU cycles
+     to check the determinant on every function evaluation, though.
+  {
+    float pt[3];
     float A[3][3];
     this->Transform->Update();
     this->Transform->InternalTransformDerivative(x,pt,A);
     float val = this->EvaluateFunction((float *)pt);
 
-    // return negative if determinant of Jacobian matrix is negative,
-    // i.e. if the transformation has a flip
     if (vtkMath::Determinant3x3(A) < 0)
       {
       return -val;
@@ -79,6 +89,7 @@ float vtkImplicitFunction::FunctionValue(const float x[3])
       return +val;
       }
     }
+  */
 }
 
 // Evaluate function gradient at position x-y-z and pass back vector. Point
@@ -97,11 +108,27 @@ void vtkImplicitFunction::FunctionGradient(const float x[3], float g[3])
     this->Transform->InternalTransformDerivative(x,pt,A);
     this->EvaluateGradient((float *)pt,g);
 
-    // the gradient must be multiplied by the inverse of the
-    // transposed inverse of the Jacobian matrix of the transform,
-    // which is (of course) just the transpose of the Jacobian.
+    // The gradient must be transformed using the same math as is
+    // use for a normal to a surface: it must be multiplied by the
+    // inverse of the transposed inverse of the Jacobian matrix of 
+    // the transform, which is just the transpose of the Jacobian.
     vtkMath::Transpose3x3(A,A);
     vtkMath::Multiply3x3(A,g,g);
+
+    /* If the determinant of the Jacobian matrix is negative,
+       then the gradient points in the opposite direction.  This
+       behaviour is actually incorrect, but is necessary to
+       balance the incorrect behaviour of FunctionValue.  Otherwise,
+       if you feed certain VTK filters a transform with a flip
+       the gradient will point in the wrong direction and they
+       will never converge to a result */
+       
+    if (vtkMath::Determinant3x3(A) < 0)
+      {
+      g[0] = -g[0];
+      g[1] = -g[1];
+      g[2] = -g[2];
+      }
     }
 }
 
