b386e0e574dd535ff9b68893fb1a97461057daae
Matt Turek <turek@crd.ge.com>
2000-05-24 05:06:51 -0400

    ENH: Captures debug/warning/error messages in a log file instead of on the console.

---
+++ common/vtkFileOutputWindow.cxx
@@ -0,0 +1,116 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkFileOutputWindow.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#include "vtkFileOutputWindow.h"
+#include "vtkObjectFactory.h"
+
+vtkFileOutputWindow* vtkFileOutputWindow::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkFileOutputWindow");
+  if(ret)
+    {
+    return (vtkFileOutputWindow*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkFileOutputWindow;
+}
+
+vtkFileOutputWindow::vtkFileOutputWindow()
+{
+  this->OStream = NULL;
+  this->FileName = NULL;
+  this->Append = 0;
+}
+
+vtkFileOutputWindow::~vtkFileOutputWindow()
+{
+  if (this->FileName) 
+    {
+    delete[] this->FileName;
+    }
+  if (this->OStream)
+    {
+    delete this->OStream;
+    }
+}
+
+void vtkFileOutputWindow::Initialize() 
+{
+  if (!this->OStream)
+    {
+    if (!this->FileName)
+      {
+      char* fileName = "vtkMessageLog.log";
+      this->FileName = new char[strlen(fileName)+1];
+      strcpy(this->FileName, fileName);
+      }
+    if (this->Append)
+      {
+      this->OStream = new ofstream(this->FileName, ios::app);
+      }
+    else
+      {
+      this->OStream = new ofstream(this->FileName);
+      }
+    }
+}
+
+void vtkFileOutputWindow::DisplayText(const char* text)
+{
+  if(!text)
+    {
+    return;
+    }
+
+  if (!this->OStream)
+    {
+    this->Initialize();
+    }
+  *this->OStream << text << endl;
+  
+  if (this->Flush)
+    {
+    this->OStream->flush();
+    }
+}
+
+
---
+++ common/vtkFileOutputWindow.h
@@ -0,0 +1,98 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkFileOutputWindow.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkFileOutputWindow - File Specific output window class
+// .SECTION Description
+// Writes debug/warning/error output to a log file instead of the console.
+// To use this class, instatiate it and then call SetInstance(this).
+// 
+
+#ifndef __vtkFileOutputWindow_h
+#define __vtkFileOutputWindow_h
+
+#include "vtkOutputWindow.h"
+
+
+class VTK_EXPORT vtkFileOutputWindow : public vtkOutputWindow
+{
+public:
+  vtkTypeMacro(vtkFileOutputWindow, vtkOutputWindow);
+
+  static vtkFileOutputWindow* New();
+
+  // Description:  Put the text into the log file
+  // New lines are converted to carriage return new lines.
+  virtual void DisplayText(const char*);
+
+  // Description: Sets the name for the log file.
+  vtkSetStringMacro(FileName);
+  vtkGetStringMacro(FileName);
+
+  // Description: Turns on buffer flushing for the output 
+  // to the log file.
+  vtkSetMacro(Flush, int);
+  vtkGetMacro(Flush, int);
+  vtkBooleanMacro(Flush, int);
+	
+  // Description: Setting append will cause the log file to be 
+  // opened in append mode.  Otherwise, if the log file exists,
+  // it will be overwritten each time the vtkFileOutputWindow 
+  // is created.
+  vtkSetMacro(Append, int);
+  vtkGetMacro(Append, int);
+  vtkBooleanMacro(Append, int);
+
+private: 
+  vtkFileOutputWindow(); 
+  virtual ~vtkFileOutputWindow(); 
+  vtkFileOutputWindow(const vtkFileOutputWindow&) {};
+  void operator=(const vtkFileOutputWindow&) {};
+  
+  void Initialize();
+  char* FileName;
+  ofstream* OStream;
+  int Flush;
+  int Append;
+};
+
+
+
+#endif
