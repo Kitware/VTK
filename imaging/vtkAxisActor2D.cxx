a9e987490813046d1ea26cd4eeed856072bf9fb3
Charles Law <charles.law@kitware.com>
2000-05-26 07:33:48 -0400

    Hanks fix for the SUN

--- imaging/vtkAxisActor2D.cxx
+++ imaging/vtkAxisActor2D.cxx
@@ -375,8 +375,16 @@ void vtkAxisActor2D::BuildAxis(vtkViewport *viewport)
   // generate point along axis (as well as tick points)
   deltaX = p2[0] - p1[0];
   deltaY = p2[1] - p1[1];
-  theta = atan2 (deltaY, deltaX);
   
+  if (deltaX == 0. && deltaY == 0.)
+    {
+    theta = 0.;
+    }
+  else
+    {
+    theta = atan2(deltaY, deltaX);
+    }
+
   ptIds[0] = pts->InsertNextPoint(p1); //first axis point
   xTick[0] = p1[0] + this->TickLength*sin(theta);
   xTick[1] = p1[1] - this->TickLength*cos(theta);
