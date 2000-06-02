d9c20405be3c058263fcf6ccd74cf7dd014772a2
Ken Martin <ken.martin@kitware.com>
2000-06-02 10:15:53 -0400

    ERR:Use viewport GetOrigin() GetSize() calls instead of OpenGL call

--- common/vtkgluPickMatrix.h
+++ common/vtkgluPickMatrix.h
@@ -1,21 +1,65 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkgluPickMatrix.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkObject - implement selected glu functionality
+// .SECTION Description
+// This file implements selected glu functionality to avoid 
+// system dependencies on glu.
+
 #ifndef vtkgluPickMatrix_h
 #define vtkgluPickMatrix_h
 
 // This function was copied from Mesa and sets up the pick matrix
-
 inline void vtkgluPickMatrix( GLdouble x, GLdouble y,
-			      GLdouble width, GLdouble height,
-			      const GLint viewport[4] )
+                              GLdouble width, GLdouble height,
+                              int *origin, int *size )
 {
-  
   GLfloat m[16];
   GLfloat sx, sy;
   GLfloat tx, ty;
   
-  sx = viewport[2] / width;
-  sy = viewport[3] / height;
-  tx = (viewport[2] + 2.0 * (viewport[0] - x)) / width;
-  ty = (viewport[3] + 2.0 * (viewport[1] - y)) / height;
+  sx = size[0] / width;
+  sy = size[1] / height;
+  tx = (size[0] + 2.0 * (origin[0] - x)) / width;
+  ty = (size[1] + 2.0 * (origin[1] - y)) / height;
 
 #define M(row,col)  m[col*4+row]
    M(0,0) = sx;   M(0,1) = 0.0;  M(0,2) = 0.0;  M(0,3) = tx;
