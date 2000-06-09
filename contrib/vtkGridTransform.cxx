ddb261c2e63788118084e44ef465056bde29e01a
David Gobbi <david.gobbi@gmail.com>
2000-06-09 10:31:11 -0400

    ERR: inverse transformation of double points was not working (silly typo)

--- contrib/vtkGridTransform.cxx
+++ contrib/vtkGridTransform.cxx
@@ -1211,7 +1211,7 @@ void vtkGridTransform::InverseTransformPoint(const float inPoint[3],
 void vtkGridTransform::InverseTransformPoint(const double point[3], 
 					     double output[3])
 {
-  double fpoint[3];
+  float fpoint[3];
   fpoint[0] = point[0]; 
   fpoint[1] = point[1]; 
   fpoint[2] = point[2];
