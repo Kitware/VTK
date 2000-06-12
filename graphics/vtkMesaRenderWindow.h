a8fb492457526a141657f8d3b055d797c1bef482
Charles Law <charles.law@kitware.com>
2000-06-12 05:52:39 -0400

    Trying to get better speed

--- graphics/vtkMesaRenderWindow.h
+++ graphics/vtkMesaRenderWindow.h
@@ -151,6 +151,12 @@ public:
                                 int blend=0);
 
   // Description:
+  // Set/Get the pixel data of an image, transmitted as RGBARGBA...
+  virtual unsigned char *GetRGBACharPixelData(int x,int y,int x2,int y2,int front);
+  virtual void SetRGBACharPixelData(int x,int y,int x2,int y2,unsigned char *,
+				    int front, int blend=0);  
+  
+  // Description:
   // Set/Get the zbuffer data from an image
   virtual float *GetZbufferData( int x1, int y1, int x2, int y2 );
   virtual void SetZbufferData( int x1, int y1, int x2, int y2, float *buffer );
