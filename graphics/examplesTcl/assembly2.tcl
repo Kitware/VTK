8bcfb6e3800abca1cb7c301511aec425064732cb
Will Schroeder <will.schroeder@kitware.com>
2000-07-13 06:43:18 -0400

    ERR:Updated for changes to vtkAssembly

--- graphics/examplesTcl/assembly2.tcl
+++ graphics/examplesTcl/assembly2.tcl
@@ -45,25 +45,27 @@ vtkPolyDataMapper cylinderMapper
     cylinderMapper SetInput [cylinder GetOutput]
 vtkActor cylActor
     cylActor SetMapper cylinderMapper
+    [cylActor GetProperty] SetColor 1 0 0
 vtkAssembly cylinderActor
-    cylinderActor SetMapper cylinderMapper
+    cylinderActor AddPart cylActor
     cylinderActor AddPart sphereActor
     cylinderActor AddPart cubeActor
     cylinderActor AddPart coneActor
     cylinderActor SetOrigin 5 10 15
     cylinderActor AddPosition 5 0 0
     cylinderActor RotateX 45
-    [cylinderActor GetProperty] SetColor 1 0 0
 
+vtkActor cylActor2
+    cylActor2 SetMapper cylinderMapper
+    [cylActor2 GetProperty] SetColor 0 1 0
 vtkAssembly cylinderActor2
-    cylinderActor2 SetMapper cylinderMapper
+    cylinderActor2 AddPart cylActor2
     cylinderActor2 AddPart sphereActor
     cylinderActor2 AddPart cubeActor
     cylinderActor2 AddPart coneActor
     cylinderActor2 SetOrigin 5 10 15
     cylinderActor2 AddPosition 6 0 0
     cylinderActor2 RotateX 50
-    [cylinderActor2 GetProperty] SetColor 0 1 0
 
 vtkAssembly twoGroups
     twoGroups AddPart cylinderActor
@@ -75,7 +77,6 @@ vtkAssembly twoGroups2
     twoGroups2 AddPart cylinderActor
     twoGroups2 AddPart cylinderActor2
     twoGroups2 AddPosition 3 0 0
-
 
 vtkAssembly twoGroups3
     twoGroups3 AddPart cylinderActor
@@ -100,7 +101,6 @@ vtkAssembly topLevel
 
 # Add the actors to the renderer, set the background and size
 #
-
 ren1 AddActor threeGroups
 ren1 AddActor threeGroups2
 ren1 SetBackground 0.1 0.2 0.4
@@ -109,7 +109,14 @@ renWin SetSize 450 450
 # Get handles to some useful objects
 #
 iren SetUserMethod {wm deiconify .vtkInteract}
-iren Initialize
+vtkCamera camera
+    camera SetClippingRange 29.8577 59.9358
+    camera SetFocalPoint 10.4922 18.7858 3.11418
+    camera SetPosition 10.4922 18.7858 46.2418
+    camera SetViewAngle 30
+    camera SetViewUp 0 1 0
+#ren1 SetActiveCamera camera
+renWin Render
 
 # prevent the tk window from showing up then start the event loop
 wm withdraw .
