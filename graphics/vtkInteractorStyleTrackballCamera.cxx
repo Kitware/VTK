64a2f93e24e151793a67e973aba596dcb5ec8257
Amy Squillacote <amy.squillacote@kitware.com>
2000-07-19 05:20:38 -0400

    This is a separate class to handle the interactor style where the actor mode is camera and the trackball mode is trackball.

---
+++ graphics/vtkInteractorStyleTrackballCamera.cxx
@@ -0,0 +1,284 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkInteractorStyleTrackballCamera.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#include "vtkInteractorStyleTrackballCamera.h"
+#include "vtkObjectFactory.h"
+
+//----------------------------------------------------------------------------
+vtkInteractorStyleTrackballCamera *vtkInteractorStyleTrackballCamera::New() 
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkInteractorStyleTrackball");
+  if(ret)
+    {
+    return (vtkInteractorStyleTrackballCamera*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkInteractorStyleTrackballCamera;
+}
+
+
+//----------------------------------------------------------------------------
+vtkInteractorStyleTrackballCamera::vtkInteractorStyleTrackballCamera() 
+{
+  this->MotionFactor = 10.0;
+  this->State = VTK_INTERACTOR_STYLE_CAMERA_NONE;
+}
+
+//----------------------------------------------------------------------------
+vtkInteractorStyleTrackballCamera::~vtkInteractorStyleTrackballCamera() 
+{
+}
+
+//----------------------------------------------------------------------------
+void vtkInteractorStyleTrackballCamera::OnMouseMove(int vtkNotUsed(ctrl), 
+					   int vtkNotUsed(shift),
+					   int x, int y) 
+{
+  if (this->State == VTK_INTERACTOR_STYLE_CAMERA_ROTATE)
+    {
+    this->FindPokedCamera(x, y);
+    this->RotateXY(x - this->LastPos[0], y - this->LastPos[1]);
+    }
+  else if (this->State == VTK_INTERACTOR_STYLE_CAMERA_PAN)
+    {
+    this->FindPokedCamera(x, y);
+    this->PanXY(x, y, this->LastPos[0], this->LastPos[1]);
+    }
+  else if (this->State == VTK_INTERACTOR_STYLE_CAMERA_ZOOM)
+    {
+    this->FindPokedCamera(x, y);
+    this->DollyXY(x - this->LastPos[0], y - this->LastPos[1]);
+    }
+
+  this->LastPos[0] = x;
+  this->LastPos[1] = y;
+}
+
+
+//----------------------------------------------------------------------------
+void vtkInteractorStyleTrackballCamera::RotateXY(int dx, int dy)
+{
+  double rxf;
+  double ryf;
+  vtkCamera *cam;
+  
+  if (this->CurrentRenderer == NULL)
+    {
+    return;
+    }
+  
+  int *size = this->CurrentRenderer->GetRenderWindow()->GetSize();
+  this->DeltaElevation = -20.0 / size[1];
+  this->DeltaAzimuth = -20.0 / size[0];
+  
+  rxf = (double)dx * this->DeltaAzimuth *  this->MotionFactor;
+  ryf = (double)dy * this->DeltaElevation * this->MotionFactor;
+  
+  cam = this->CurrentRenderer->GetActiveCamera();
+  cam->Azimuth(rxf);
+  cam->Elevation(ryf);
+  cam->OrthogonalizeViewUp();
+  this->CurrentRenderer->ResetCameraClippingRange();
+  vtkRenderWindowInteractor *rwi = this->Interactor;
+  if (this->CurrentLight)
+    {
+    // get the first light
+    this->CurrentLight->SetPosition(cam->GetPosition());
+    this->CurrentLight->SetFocalPoint(cam->GetFocalPoint());
+    }	
+  rwi->Render();
+}
+
+//----------------------------------------------------------------------------
+void vtkInteractorStyleTrackballCamera::PanXY(int x, int y, int oldX, int oldY)
+{
+  vtkCamera *cam;
+  double viewFocus[4], focalDepth, viewPoint[3];
+  float newPickPoint[4], oldPickPoint[4], motionVector[3];
+  
+  if (this->CurrentRenderer == NULL)
+    {
+    return;
+    }
+
+  // calculate the focal depth since we'll be using it a lot
+  cam = this->CurrentRenderer->GetActiveCamera();
+  cam->GetFocalPoint(viewFocus);
+  this->ComputeWorldToDisplay(viewFocus[0], viewFocus[1],
+			      viewFocus[2], viewFocus);
+  focalDepth = viewFocus[2];
+
+  this->ComputeDisplayToWorld(double(x), double(y),
+			      focalDepth, newPickPoint);
+    
+  // has to recalc old mouse point since the viewport has moved,
+  // so can't move it outside the loop
+  this->ComputeDisplayToWorld(double(oldX),double(oldY),
+			      focalDepth, oldPickPoint);
+  
+  // camera motion is reversed
+  motionVector[0] = oldPickPoint[0] - newPickPoint[0];
+  motionVector[1] = oldPickPoint[1] - newPickPoint[1];
+  motionVector[2] = oldPickPoint[2] - newPickPoint[2];
+  
+  cam->GetFocalPoint(viewFocus);
+  cam->GetPosition(viewPoint);
+  cam->SetFocalPoint(motionVector[0] + viewFocus[0],
+		     motionVector[1] + viewFocus[1],
+		     motionVector[2] + viewFocus[2]);
+  cam->SetPosition(motionVector[0] + viewPoint[0],
+		   motionVector[1] + viewPoint[1],
+		   motionVector[2] + viewPoint[2]);
+      
+  vtkRenderWindowInteractor *rwi = this->Interactor;
+  if (this->CurrentLight)
+    {
+    /* get the first light */
+    this->CurrentLight->SetPosition(cam->GetPosition());
+    this->CurrentLight->SetFocalPoint(cam->GetFocalPoint());
+    }
+    
+  rwi->Render();
+}
+
+//----------------------------------------------------------------------------
+void vtkInteractorStyleTrackballCamera::DollyXY(int dx, int dy)
+{
+  vtkCamera *cam;
+  double dyf = this->MotionFactor * (double)(dy) / (double)(this->Center[1]);
+  double zoomFactor = pow((double)1.1, dyf);
+  
+  if (this->CurrentRenderer == NULL)
+    {
+    return;
+    }
+  
+  cam = this->CurrentRenderer->GetActiveCamera();
+  if (cam->GetParallelProjection())
+    {
+    cam->SetParallelScale(cam->GetParallelScale()/zoomFactor);
+    }
+  else
+    {
+    cam->Dolly(zoomFactor);
+    this->CurrentRenderer->ResetCameraClippingRange();
+    }
+  
+  vtkRenderWindowInteractor *rwi = this->Interactor;
+  if (this->CurrentLight)
+    {
+    /* get the first light */
+    this->CurrentLight->SetPosition(cam->GetPosition());
+    this->CurrentLight->SetFocalPoint(cam->GetFocalPoint());
+    }
+  
+  rwi->Render();
+}
+
+//----------------------------------------------------------------------------
+void vtkInteractorStyleTrackballCamera::OnLeftButtonDown(int ctrl, int shift, 
+						int x, int y) 
+{
+  cerr << "**** Left Button Down\n";
+  this->FindPokedRenderer(x, y);
+  if (this->CurrentRenderer == NULL)
+    {
+    return;
+    }
+
+  this->State = VTK_INTERACTOR_STYLE_CAMERA_ROTATE;
+}
+
+//----------------------------------------------------------------------------
+void vtkInteractorStyleTrackballCamera::OnLeftButtonUp(int ctrl, int shift, 
+					      int x, int y) 
+{
+  this->State = VTK_INTERACTOR_STYLE_CAMERA_NONE;
+}
+
+//----------------------------------------------------------------------------
+void vtkInteractorStyleTrackballCamera::OnMiddleButtonDown(int ctrl, int shift, 
+						 int x, int y) 
+{
+  cerr << "**** Middle Button Down\n";
+  this->FindPokedRenderer(x, y);
+  if (this->CurrentRenderer == NULL)
+    {
+    return;
+    }
+  
+  this->State = VTK_INTERACTOR_STYLE_CAMERA_PAN;
+}
+//----------------------------------------------------------------------------
+void vtkInteractorStyleTrackballCamera::OnMiddleButtonUp(int ctrl, int shift, 
+					       int x, int y) 
+{
+  this->State = VTK_INTERACTOR_STYLE_CAMERA_NONE;
+}
+
+//----------------------------------------------------------------------------
+void vtkInteractorStyleTrackballCamera::OnRightButtonDown(int ctrl, int shift, 
+						int x, int y) 
+{
+  cerr << "**** Right Button Down\n";
+  this->FindPokedRenderer(x, y);
+  if (this->CurrentRenderer == NULL)
+    {
+    return;
+    }
+  
+  this->State = VTK_INTERACTOR_STYLE_CAMERA_ZOOM;
+}
+
+//----------------------------------------------------------------------------
+void vtkInteractorStyleTrackballCamera::OnRightButtonUp(int ctrl, int shift, 
+					      int x, int y) 
+{
+  this->State = VTK_INTERACTOR_STYLE_CAMERA_NONE;
+}
+
+//----------------------------------------------------------------------------
+void vtkInteractorStyleTrackballCamera::PrintSelf(ostream& os, vtkIndent indent)
+{
+  vtkInteractorStyle::PrintSelf(os,indent);
+
+}
---
+++ graphics/vtkInteractorStyleTrackballCamera.h
@@ -0,0 +1,99 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkInteractorStyleTrackballCamera.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+
+// .NAME vtkInteractorStyleTrackballCamera - Manipulation of an implicit plane.
+
+// .SECTION Description
+// vtkInteractorStyleTrackballCamera Allows interactive definition of a plane
+// by manipulating a vtkPlane's parameters.  
+// The center of the plane is hot.  The center provides
+// rotation (left mouse button), XY translation relative to camera (middle),
+// and translation along camera's view-plane normal (right).
+
+
+#ifndef __vtkInteractorStyleTrackballCamera_h
+#define __vtkInteractorStyleTrackballCamera_h
+
+#include "vtkInteractorStyle.h"
+
+
+#define VTK_INTERACTOR_STYLE_CAMERA_NONE    0
+#define VTK_INTERACTOR_STYLE_CAMERA_ROTATE  1
+#define VTK_INTERACTOR_STYLE_CAMERA_PAN     2
+#define VTK_INTERACTOR_STYLE_CAMERA_ZOOM    3
+
+class VTK_EXPORT vtkInteractorStyleTrackballCamera : public vtkInteractorStyle
+{
+public:
+  // Description:
+  // This class must be supplied with a vtkRenderWindowInteractor wrapper or
+  // parent. This class should not normally be instantiated by application
+  // programmers.
+  static vtkInteractorStyleTrackballCamera *New();
+
+  vtkTypeMacro(vtkInteractorStyleTrackballCamera, vtkObject);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Generic event bindings must be overridden in subclasses
+  void OnMouseMove  (int ctrl, int shift, int x, int y);
+  void OnLeftButtonDown(int ctrl, int shift, int x, int y);
+  void OnLeftButtonUp  (int ctrl, int shift, int x, int y);
+  void OnMiddleButtonDown(int ctrl, int shift, int x, int y);
+  void OnMiddleButtonUp  (int ctrl, int shift, int x, int y);
+  void OnRightButtonDown(int ctrl, int shift, int x, int y);
+  void OnRightButtonUp  (int ctrl, int shift, int x, int y);
+
+protected:
+  vtkInteractorStyleTrackballCamera();
+  ~vtkInteractorStyleTrackballCamera();
+  vtkInteractorStyleTrackballCamera(const vtkInteractorStyleTrackballCamera&) {};
+  void operator=(const vtkInteractorStyleTrackballCamera&) {};
+
+  void RotateXY(int dx, int dy);
+  void PanXY(int x, int y, int oldx, int oldy);
+  void DollyXY(int dx, int dy);
+  
+  int State;
+  float MotionFactor;
+};
+
+#endif
