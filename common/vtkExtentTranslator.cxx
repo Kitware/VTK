0db50c59bab38da5d8eef0c4b68d815a0a448765
Charles Law <charles.law@kitware.com>
2000-07-26 07:43:11 -0400

    object that will change pieces to extents

---
+++ common/vtkExtentTranslator.cxx
@@ -0,0 +1,170 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkExtentTranslator.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+
+#include "vtkExtentTranslator.h"
+#include "vtkObjectFactory.h"
+
+
+
+//------------------------------------------------------------------------------
+vtkExtentTranslator* vtkExtentTranslator::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkExtentTranslator");
+  if(ret)
+    {
+    return (vtkExtentTranslator*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkExtentTranslator;
+}
+
+
+//----------------------------------------------------------------------------
+vtkExtentTranslator::vtkExtentTranslator()
+{
+  int idx;
+
+  this->MinimumPieceSize[0] = 2;
+  this->MinimumPieceSize[1] = 2;
+  this->MinimumPieceSize[2] = 2;  
+
+}
+
+//----------------------------------------------------------------------------
+vtkExtentTranslator::~vtkExtentTranslator()
+{
+}
+
+//----------------------------------------------------------------------------
+int vtkExtentTranslator::SplitExtent(int piece, int numPieces, int *ext)
+{
+  int numPiecesInFirstHalf;
+  int size[3], mid, splitAxis;
+
+  // keep splitting until we have only one piece.
+  // piece and numPieces will always be relative to the current ext. 
+  while (numPieces > 1)
+    {
+    // Get the dimensions for each axis.
+    size[0] = ext[1]-ext[0];
+    size[1] = ext[3]-ext[2];
+    size[2] = ext[5]-ext[4];
+    // choose the biggest axis
+    if (size[2] >= size[1] && size[2] >= size[0] && 
+	size[2]/2 >= this->MinimumPieceSize[2])
+      {
+      splitAxis = 2;
+      }
+    else if (size[1] >= size[0] && size[1]/2 >= this->MinimumPieceSize[1])
+      {
+      splitAxis = 1;
+      }
+    else if (size[0]/2 >= this->MinimumPieceSize[0])
+      {
+      splitAxis = 0;
+      }
+    else
+      {
+      // signal no more splits possible
+      splitAxis = -1;
+      }
+
+    if (splitAxis == -1)
+      {
+      // can not split any more.
+      if (piece == 0)
+        {
+        // just return the remaining piece
+        numPieces = 1;
+        }
+      else
+        {
+        // the rest must be empty
+        return 0;
+        }
+      }
+    else
+      {
+      // split the chosen axis into two pieces.
+      numPiecesInFirstHalf = (numPieces / 2);
+      mid = (size[splitAxis] * numPiecesInFirstHalf / numPieces) 
+	+ ext[splitAxis*2];
+      if (piece < numPiecesInFirstHalf)
+        {
+        // piece is in the first half
+        // set extent to the first half of the previous value.
+        ext[splitAxis*2+1] = mid;
+        // piece must adjust.
+        numPieces = numPiecesInFirstHalf;
+        }
+      else
+        {
+        // piece is in the second half.
+        // set the extent to be the second half. (two halves share points)
+        ext[splitAxis*2] = mid;
+        // piece must adjust
+        numPieces = numPieces - numPiecesInFirstHalf;
+        piece -= numPiecesInFirstHalf;
+        }
+      }
+    } // end of while
+
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+void vtkExtentTranslator::PrintSelf(ostream& os, vtkIndent indent)
+{
+  vtkObject::PrintSelf(os,indent);
+
+  os << indent << "MiniumuPieceSize: " << this->MinimumPieceSize[0] << ", "
+     << this->MinimumPieceSize[1] << ", " 
+     << this->MinimumPieceSize[2] << ", " << "\n";
+}
+
+
+
+
+
+
+
---
+++ common/vtkExtentTranslator.h
@@ -0,0 +1,95 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkExtentTranslator.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+    THIS CLASS IS PATENT PENDING.
+
+    Application of this software for commercial purposes requires 
+    a license grant from Kitware. Contact:
+        Ken Martin
+        Kitware
+        469 Clifton Corporate Parkway,
+        Clifton Park, NY 12065
+        Phone:1-518-371-3971 
+    for more information.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkExtentTranslator - Generates a structured extent from unstructured.
+
+// .SECTION Description
+// vtkExtentTranslator generates a structured extent from an unstructured extent.
+// It uses a recursive cheme that splits the largest axis.  A hard coded extent can
+// be used fro a starting point.
+
+// .SECTION Caveats
+// This object is still under development.
+
+#ifndef __vtkExtentTranslator_h
+#define __vtkExtentTranslator_h
+
+#include "vtkObject.h"
+
+
+class VTK_EXPORT vtkExtentTranslator : public vtkObject
+{
+public:
+  static vtkExtentTranslator *New();
+
+  vtkTypeMacro(vtkExtentTranslator,vtkObject);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Returns 0 if no data exist for a piece.
+  // The whole extent SHould be passed in as the ext.
+  // It is modified to return the result.
+  int SplitExtent(int piece, int numPieces, int *ext);  
+
+protected:
+  vtkExtentTranslator();
+  ~vtkExtentTranslator();
+  vtkExtentTranslator(const vtkExtentTranslator&) {};
+  void operator=(const vtkExtentTranslator&) {};
+
+  int MinimumPieceSize[3];
+
+};
+
+#endif
+
