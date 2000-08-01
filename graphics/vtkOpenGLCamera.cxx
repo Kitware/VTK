05c03345a095e8196fbd6db0759e68a4a1583a10
Hank Childs <childs@forum.llnl.gov>
2000-08-01 13:52:28 -0400

    Only make renderers issue a Clear if they are in the back layer, so transparent renderers don't write over each other.

--- graphics/vtkOpenGLCamera.cxx
+++ graphics/vtkOpenGLCamera.cxx
@@ -164,7 +164,7 @@ void vtkOpenGLCamera::Render(vtkRenderer *ren)
   // insert camera view transformation 
   glMultMatrixd(matrix->Element[0]);
 
-  if ((ren->GetRenderWindow())->GetErase() && ! ren->GetTransparentRenderer())
+  if ((ren->GetRenderWindow())->GetErase() && !(ren->Transparent()))
     {
     ren->Clear();
     }
