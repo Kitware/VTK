a7c84669c8460c53b790fec66ec1d60fe4522e9a
Sebastien Barre <sebastien.barre@kitware.com>
2000-08-01 09:03:27 -0400

    vtkPolyData::ComputeBounds() now uses cell's points

--- common/vtkPolyData.cxx
+++ common/vtkPolyData.cxx
@@ -534,6 +534,76 @@ void vtkPolyData::GetCellBounds(int cellId, float bounds[6])
     }
 }
 
+
+//----------------------------------------------------------------------------
+void vtkPolyData::ComputeBounds()
+{
+    if (this->GetMTime() > this->ComputeTime)
+    {
+        // If there are no cells, but there are points, back to the
+        // bounds of the points set.
+
+        if (this->GetNumberOfCells() == 0 && this->GetNumberOfPoints()) 
+        {
+            vtkPointSet::ComputeBounds();
+            return;
+        } 
+
+        int t, i, npts, *pts;
+        float x[3];
+      
+        this->Bounds[0] = this->Bounds[2] = this->Bounds[4] =  VTK_LARGE_FLOAT;
+        this->Bounds[1] = this->Bounds[3] = this->Bounds[5] = -VTK_LARGE_FLOAT;
+
+        vtkCellArray *cella[4];
+
+        cella[0] = this->GetVerts();
+        cella[1] = this->GetLines();
+        cella[2] = this->GetPolys();
+        cella[3] = this->GetStrips();
+
+        // Iterate over cells's points
+
+        for (t = 0; t < 4; t++) 
+        {
+            for (cella[t]->InitTraversal(); cella[t]->GetNextCell(npts,pts); )
+            {
+                for (i = 0;  i < npts; i++)
+                {
+                    this->Points->GetPoint( pts[i], x );
+
+                    if (x[0] < this->Bounds[0])
+                    {
+                        this->Bounds[0] = x[0];
+                    }
+                    if (x[0] > this->Bounds[1])
+                    {
+                        this->Bounds[1] = x[0];
+                    }
+                    if (x[1] < this->Bounds[2])
+                    {
+                        this->Bounds[2] = x[1];
+                    }
+                    if (x[1] > this->Bounds[3])
+                    {
+                        this->Bounds[3] = x[1];
+                    }
+                    if (x[2] < this->Bounds[4])
+                    {
+                        this->Bounds[4] = x[2];
+                    }
+                    if (x[2] > this->Bounds[5])
+                    {
+                        this->Bounds[5] = x[2];
+                    }
+                }
+            }
+        }
+
+        this->ComputeTime.Modified();
+    }
+}
+
 //----------------------------------------------------------------------------
 // Set the cell array defining vertices.
 void vtkPolyData::SetVerts (vtkCellArray* v) 
