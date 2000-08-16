2e119f7ee9204b33db32acc3e29855293ef626a0
Bill Lorensen <lorensen@crd.ge.com>
2000-08-16 03:09:59 -0400

    ENH: filter Compute Time from output.

--- graphics/examplesTcl/otherPrint.tcl
+++ graphics/examplesTcl/otherPrint.tcl
@@ -8,7 +8,7 @@
 
 catch {load vtktcl}
 
-set rtSelector "sed -e s/0x0/0/ | sed -e s/-0/0/ | grep -v -i thread | grep -v StartTime: | grep -v 0x | grep -v Modified "
+set rtSelector "sed -e s/0x0/0/ | sed -e s/-0/0/ | grep -v -i thread | grep -v StartTime: | grep -v 0x | grep -v Modified | grep -v 'Compute Time:'"
 set rtComparator "diff -b"
 
 proc rtOtherTest { fileid } {
