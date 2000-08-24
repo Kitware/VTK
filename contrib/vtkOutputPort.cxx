10560d69e24bced596524521b13ed9deede48a00
Charles Law <charles.law@kitware.com>
2000-08-24 14:03:34 -0400

    change comment

--- contrib/vtkOutputPort.cxx
+++ contrib/vtkOutputPort.cxx
@@ -45,7 +45,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 
-//------------------------------------------------------------------------------
+//----------------------------------------------------------------------------
 vtkOutputPort* vtkOutputPort::New()
 {
   // First try to create the object from the vtkObjectFactory
