0bc6af09598390c950262002c3f90efaff0b341c
Charles Law <charles.law@kitware.com>
2000-08-24 14:43:10 -0400

    Mis spelled ivar in printself

--- graphics/vtkRenderWindow.cxx
+++ graphics/vtkRenderWindow.cxx
@@ -731,7 +731,7 @@ void vtkRenderWindow::PrintSelf(ostream& os, vtkIndent indent)
   os << indent << "NeverRendered: " << this->NeverRendered << "\n";
   os << indent << "Interactor: " << this->Interactor << "\n";
   os << indent << "Motion Blur Frames: " << this->SubFrames << "\n";
-  os << indent << "Swapbuffers: " << (this->SwapBuffers ? "On\n":"Off\n");
+  os << indent << "SwapBuffers: " << (this->SwapBuffers ? "On\n":"Off\n");
   os << indent << "StereoType: " << this->GetStereoTypeAsString() << "\n";
   os << indent << "Number of Layers: " << this->NumLayers << "\n";
 
