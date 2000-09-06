0373599a943b41f0c3c96c4250e1ecabca307c24
Michalel Halle <halazar@media.mit.edu>
2000-09-06 05:35:39 -0400

    Initial commit of vtkLightKit.

---
+++ contrib/vtkLightKit.cxx
@@ -0,0 +1,346 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $ $
+  Language:  C++
+  Date:      $ $
+  Version:   $ $
+  Thanks:    Michael Halle, Brigham and Women's Hospital
+
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+/* vtkLightKit
+*/
+
+#include <math.h>
+#include "vtkLightKit.h"
+#include "vtkLight.h"
+#include "vtkPiecewiseFunction.h"
+#include "vtkObjectFactory.h"
+#include "vtkRenderer.h"
+
+//------------------------------------------------------------------------------
+vtkLightKit* vtkLightKit::New() {
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkLightKit");
+  if(ret)
+    {
+    return (vtkLightKit*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkLightKit;
+}
+
+vtkLightKit::vtkLightKit() {
+  // create members
+  this->KeyLight = vtkLight::New();
+  this->KeyLight->Register(this);
+
+  this->FillLight = vtkLight::New();
+  this->FillLight->Register(this);
+
+  this->Headlight = vtkLight::New();
+  this->Headlight->Register(this);
+
+  for(int i = 0; i < 4; i++) {
+    this->WarmthFunction[i] = vtkPiecewiseFunction::New();
+  }
+  this->InitializeWarmthFunctions();
+
+  // initialize values
+  this->KeyLight->SetLightTypeToCameraLight();
+  this->FillLight->SetLightTypeToCameraLight();
+  this->Headlight->SetLightTypeToHeadlight();
+
+  this->SetKeyLightAngle(50.0, 10.0);
+  this->SetFillLightAngle(-75.0, -10.0);
+
+  this->KeyLightWarmth  = 0.6;
+  this->FillLightWarmth = 0.4;
+  this->HeadlightWarmth = 0.5;
+
+  this->KeyLightIntensity = 1.0;
+  this->KeyToFillRatio = 5.0;
+  this->KeyToHeadRatio = 7.0;
+
+  this->MaintainLuminance = 0;
+
+  // push initial values out
+  this->Modified();
+}  
+
+vtkLightKit::~vtkLightKit() {
+
+  if(this->KeyLight != NULL) {
+    this->KeyLight->UnRegister(this);
+    this->KeyLight = NULL;
+  }
+
+  if(this->FillLight != NULL) {
+    this->FillLight->UnRegister(this);
+    this->FillLight = NULL;
+  }
+
+  if(this->Headlight != NULL) {
+    this->Headlight->UnRegister(this);
+    this->Headlight = NULL;
+  }
+
+  for(int i = 0; i < 4; i++) {
+    this->WarmthFunction[i]->Delete();
+  }
+}
+
+void vtkLightKit::SetKeyLightAngle(float elevation, float azimuth) {
+  this->KeyLightAngle[0] = elevation;
+  this->KeyLightAngle[1] = azimuth;
+
+  this->KeyLight->SetDirectionAngle(elevation, azimuth);
+}
+
+void vtkLightKit::SetFillLightAngle(float elevation, float azimuth) {
+  this->FillLightAngle[0] = elevation;
+  this->FillLightAngle[1] = azimuth;
+
+  this->FillLight->SetDirectionAngle(elevation, azimuth);
+}
+
+void vtkLightKit::WarmthToRGB(float w, float rgb[3]) {
+  rgb[0] = this->WarmthFunction[0]->GetValue(w);
+  rgb[1] = this->WarmthFunction[1]->GetValue(w);
+  rgb[2] = this->WarmthFunction[2]->GetValue(w);
+}
+
+float vtkLightKit::WarmthToIntensity(float w) {
+  return this->WarmthFunction[3]->GetValue(w);
+}
+
+void vtkLightKit::WarmthToRGBI(float w, float rgb[3], float& i) {
+  rgb[0] = this->WarmthFunction[0]->GetValue(w);
+  rgb[1] = this->WarmthFunction[1]->GetValue(w);
+  rgb[2] = this->WarmthFunction[2]->GetValue(w);
+  i = this->WarmthFunction[3]->GetValue(w);
+}
+
+void vtkLightKit::AddLightsToRenderer(vtkRenderer *renderer) {
+  if(renderer != NULL) {
+    renderer->AddLight(this->Headlight);
+    renderer->AddLight(this->KeyLight);
+    renderer->AddLight(this->FillLight);
+  }
+}
+
+void vtkLightKit::RemoveLightsFromRenderer(vtkRenderer *renderer) {
+  if(renderer != NULL) {
+    renderer->RemoveLight(this->Headlight);
+    renderer->RemoveLight(this->KeyLight);
+    renderer->RemoveLight(this->FillLight);
+  }
+}
+
+void vtkLightKit::Modified() {
+  this->Update();
+  this->MTime.Modified();
+}
+
+void vtkLightKit::Update() {
+
+  float *fillLightColor = this->FillLightColor;
+  float fillLightPI;
+
+  float *keyLightColor = this->KeyLightColor;
+  float keyLightPI;
+
+  float *headlightColor = this->HeadlightColor;
+  float headlightPI;
+
+  float fillLightIntensity, keyLightIntensity, headlightIntensity;
+
+  this->WarmthToRGBI(this->KeyLightWarmth,  keyLightColor,  keyLightPI);
+  this->WarmthToRGBI(this->FillLightWarmth, fillLightColor, fillLightPI);
+  this->WarmthToRGBI(this->HeadlightWarmth, headlightColor, headlightPI);
+
+  keyLightIntensity = this->KeyLightIntensity;
+
+  fillLightIntensity = keyLightIntensity / this->KeyToFillRatio;
+  headlightIntensity = keyLightIntensity / this->KeyToHeadRatio;
+
+  // This is sort of interesting: the fill light intensity is weighted
+  // by the perceptual brightness of the color of each light.  Since
+  // the fill light will often be a cooler color than the key light,
+  // the bluer color would otherwise seem less bright than the neutral
+  // and this bias the key-to-fill ratio.  This factor compensates for
+  // this problem.  Note we always do this correction, no matter what
+  // the MaintainLuminance flag says.  That flag's for controlling
+  // the intensity of the entire scene, not just the fill light.
+
+  if(this->MaintainLuminance) {
+    fillLightIntensity /= fillLightPI;
+    headlightIntensity /= headlightPI;
+    keyLightIntensity  /= keyLightPI;
+  } 
+  this->KeyLight->SetColor(keyLightColor);
+  this->KeyLight->SetIntensity(keyLightIntensity);
+  
+  this->FillLight->SetColor(fillLightColor);
+  this->FillLight->SetIntensity(fillLightIntensity);
+
+  this->Headlight->SetColor(headlightColor);
+  this->Headlight->SetIntensity(headlightIntensity);
+}
+
+void vtkLightKit::PrintSelf(ostream& os, vtkIndent indent) {
+  vtkObject::PrintSelf(os,indent);
+
+  os << indent << "KeyLightIntensity: " << this->KeyLightIntensity << "\n";
+  os << indent << "KeyToFillRatio: " << this->KeyToFillRatio << "\n";
+  os << indent << "KeyToHeadRatio: " << this->KeyToHeadRatio << "\n";
+
+  os << indent << "KeyLightWarmth: " << this->KeyLightWarmth << "\n";
+  os << indent << "KeyLightAngle: (" 
+     << this->KeyLightAngle[0] << ", "
+     << this->KeyLightAngle[1] << ")\n";
+
+  os << indent << "FillLightWarmth: " << this->FillLightWarmth << "\n";
+  os << indent << "FillLightAngle: (" 
+     << this->FillLightAngle[0] << ", "
+     << this->FillLightAngle[1] << ")\n";
+
+  os << indent << "HeadlightWarmth: " << this->HeadlightWarmth << "\n";
+
+  os << indent << "MaintainLuminance: " << 
+    (this->MaintainLuminance ? "On" : "Off") << "\n";
+}
+
+void vtkLightKit::DeepCopy( vtkLightKit *k ) {
+ this->KeyLightIntensity = k->KeyLightIntensity;
+ this->KeyToFillRatio = k->KeyToFillRatio;
+ this->KeyToHeadRatio = k->KeyToHeadRatio;
+
+ this->KeyLightWarmth = k->KeyLightWarmth;
+ this->FillLightWarmth = k->FillLightWarmth;
+ this->HeadlightWarmth = k->HeadlightWarmth;
+
+ this->KeyLightAngle[0] = k->KeyLightAngle[0];
+ this->KeyLightAngle[1] = k->KeyLightAngle[1];
+
+ this->FillLightAngle[0] = k->FillLightAngle[0];
+ this->FillLightAngle[1] = k->FillLightAngle[1];
+
+ this->MaintainLuminance = k->MaintainLuminance;
+
+ this->KeyLight->DeepCopy(k->KeyLight);
+ this->FillLight->DeepCopy(k->FillLight);
+ this->Headlight->DeepCopy(k->Headlight);
+}  
+
+// r, g, b, sqrt(color length)
+static float warmthTable[] = {
+  0.1674, 0.3065, 1.0000, 0.5865,
+  0.1798, 0.3204, 1.0000, 0.5965,
+  0.1935, 0.3352, 1.0000, 0.6071,
+  0.2083, 0.3511, 1.0000, 0.6184,
+  0.2245, 0.3679, 1.0000, 0.6302,
+  0.2422, 0.3859, 1.0000, 0.6426,
+  0.2614, 0.4050, 1.0000, 0.6556,
+  0.2822, 0.4252, 1.0000, 0.6693,
+  0.3049, 0.4467, 1.0000, 0.6837,
+  0.3293, 0.4695, 1.0000, 0.6986,
+  0.3557, 0.4935, 1.0000, 0.7142,
+  0.3841, 0.5188, 1.0000, 0.7303,
+  0.4144, 0.5454, 1.0000, 0.7470,
+  0.4468, 0.5731, 1.0000, 0.7642,
+  0.4811, 0.6020, 1.0000, 0.7818,
+  0.5173, 0.6320, 1.0000, 0.7998,
+  0.5551, 0.6628, 1.0000, 0.8179,
+  0.5943, 0.6942, 1.0000, 0.8362,
+  0.6346, 0.7261, 1.0000, 0.8544,
+  0.6756, 0.7581, 1.0000, 0.8724,
+  0.7168, 0.7898, 1.0000, 0.8899,
+  0.7575, 0.8209, 1.0000, 0.9068,
+  0.7972, 0.8508, 1.0000, 0.9229,
+  0.8351, 0.8791, 1.0000, 0.9379,
+  0.8705, 0.9054, 1.0000, 0.9517,
+  0.9026, 0.9290, 1.0000, 0.9640,
+  0.9308, 0.9497, 1.0000, 0.9746,
+  0.9546, 0.9671, 1.0000, 0.9834,
+  0.9734, 0.9808, 1.0000, 0.9903,
+  0.9872, 0.9907, 1.0000, 0.9954,
+  0.9958, 0.9970, 1.0000, 0.9985,
+  0.9996, 0.9997, 1.0000, 0.9999,
+  1.0000, 0.9999, 0.9996, 0.9999,
+  1.0000, 0.9988, 0.9958, 0.9994,
+  1.0000, 0.9964, 0.9871, 0.9982,
+  1.0000, 0.9925, 0.9730, 0.9962,
+  1.0000, 0.9869, 0.9532, 0.9935,
+  1.0000, 0.9796, 0.9275, 0.9898,
+  1.0000, 0.9705, 0.8959, 0.9853,
+  1.0000, 0.9595, 0.8584, 0.9798,
+  1.0000, 0.9466, 0.8150, 0.9734,
+  1.0000, 0.9317, 0.7660, 0.9660,
+  1.0000, 0.9147, 0.7116, 0.9576,
+  1.0000, 0.8956, 0.6522, 0.9482,
+  1.0000, 0.8742, 0.5881, 0.9377,
+  1.0000, 0.8506, 0.5199, 0.9261,
+  1.0000, 0.8247, 0.4483, 0.9134,
+  1.0000, 0.7964, 0.3739, 0.8995,
+  1.0000, 0.7656, 0.2975, 0.8845,
+  1.0000, 0.7324, 0.2201, 0.8683,
+  1.0000, 0.6965, 0.1426, 0.8509,
+  1.0000, 0.6580, 0.0662, 0.8323,
+  1.0000, 0.6179, 0.0000, 0.8134,
+  1.0000, 0.5832, 0.0000, 0.8008,
+  1.0000, 0.5453, 0.0000, 0.7868,
+  1.0000, 0.5042, 0.0000, 0.7713,
+  1.0000, 0.4595, 0.0000, 0.7541,
+  1.0000, 0.4111, 0.0000, 0.7350,
+  1.0000, 0.3588, 0.0000, 0.7139,
+  1.0000, 0.3025, 0.0000, 0.6904,
+  1.0000, 0.2423, 0.0000, 0.6643,
+  1.0000, 0.1782, 0.0000, 0.6353,
+  1.0000, 0.1104, 0.0000, 0.6032,
+  1.0000, 0.0396, 0.0000, 0.5677,
+};
+
+void vtkLightKit::InitializeWarmthFunctions() {
+
+  const int len = sizeof(warmthTable) / sizeof(float) / 4;
+
+  for(int i = 0; i < 4; i++) {
+    this->WarmthFunction[i]->BuildFunctionFromTable(0.0, 1.0, len,
+						   &warmthTable[i], 4);
+  }
+}
+
+
---
+++ contrib/vtkLightKit.h
@@ -0,0 +1,273 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    $ $
+  Language:  C++
+  Date:      $ $
+  Version:   $ $
+  Thanks:    Michael Halle, Brigham and Women's Hospital
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkLightKit - a simple but quality lighting kit
+// .SECTION Description
+// vtkLightKit is designed to make general purpose lighting of vtk
+// scenes simple, flexible, and attractive (or at least not horribly
+// ugly without significant effort).  Use a LightKit when you want
+// more control over your lighting than you can get with the default
+// vtk light, which is a headlight located at the camera. (Headlights
+// are very simple to use, but they don't show the shape of objects very 
+// well, don't give a good sense of "up" and "down", and don't evenly
+// light the object.)
+//
+// A LightKit consists of three lights, a key light, a fill light, and
+// a headlight.  The main light is the key light.  It is usually
+// positioned so that it appears like an overhead light (like the sun,
+// or a ceiling light).  It is generally positioned to shine down on the
+// scene from about a 45 degree angle vertically and at least a little
+// offset side to side.  The key light usually at least about twice as 
+// bright as the total of all other lights in the scene to provide good
+// modeling of object features.
+// 
+// The other two lights in the kit, the fill light and headlight, are
+// weaker sources that provide extra illumination to fill in the spots
+// that the key light misses.  The fill light is usually positioned
+// across from or opposite from the key light (though still on the
+// same side of the object as the camera) in order to simulate diffuse
+// reflections from other objects in the scene.  The headlight, always
+// located at the position of the camera, reduces the contrast between
+// areas lit by the key and fill light. To enforce the relationship
+// between the three lights, the intensity of the fill and headlights
+// are set as a ratio to the key light brightness.  Thus, the brightness
+// of all the lights in the scene can be changed by changing the key light 
+// intensity.
+//
+// For simplicity, the position of lights in the LightKit can only be
+// specified using angles: the elevation (latitude) and azimuth
+// (longitude) of each light with respect to the camera.  All lights
+// are directional lights (infinitely far away with no falloff).
+//
+// vtkLightKit limits the colors that can be assigned to any light to
+// those of incandescent sources such as light bulbs and sunlight.  It
+// defines a special color spectrum called "warmth" from which light
+// colors can be chosen, where 0 is cold blue, 0.5 is neutral white,
+// and 1 is deep sunset red.  Colors close to 0.5 are "cool whites" and
+// "warm whites," respectively. 
+//
+// Since colors far from white on the warmth scale appear less bright,
+// key-to-fill and key-to-headlight ratios are skewed by 
+// key, fill, and headlight colors.  If the flag MaintainLuminance
+// is set, vtkLightKit will attempt to compensate for these perceptual
+// differences by increasing the brightness of more saturated colors.
+//
+// A LightKit is not explicitly part of the vtk pipeline.  Rather, it
+// is a composite object that controls the behavior of lights using a
+// unified user interface.  Every time a parameter of vtkLightKit is
+// adjusted, the properties of its lights are modified.
+//
+// .SECTION Credits
+// vtkLightKit was originally written and contributed to vtk by
+// Michael Halle (mhalle@bwh.harvard.edu) at the Surgical Planning
+// Lab, Brigham and Women's Hospital.
+
+#ifndef __vtkLightKit_h
+#define __vtkLightKit_h
+
+#include "vtkObject.h"
+class vtkLight;
+class vtkPiecewiseFunction;
+class vtkRenderer;
+
+class VTK_EXPORT vtkLightKit : public vtkObject
+{
+public:
+  static vtkLightKit *New();
+  vtkTypeMacro(vtkLightKit, vtkObject);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Set/Get the intensity of the key light.  The key light is the
+  // brightest light in the scene.  The intensities of the other two
+  // lights are ratios of the key light's intensity.  
+  vtkSetMacro(KeyLightIntensity, float);  
+  vtkGetMacro(KeyLightIntensity, float);
+
+  // Description: 
+  // Set/Get the key-to-fill ratio.  This ratio controls
+  // how bright the fill light is compared to the key light: larger
+  // values correspond to a dimmer fill light.  The purpose of the
+  // fill light is to light parts of the object not lit by the key
+  // light, while still maintaining constrast.  This type of lighting
+  // may correspond to indirect illumination from the key light, bounced
+  // off a wall, floor, or other object.  The fill light should never
+  // be brighter than the key light:  a good range for the key-to-fill
+  // ratio is between 3 and 10.
+  vtkSetClampMacro(KeyToFillRatio, float, 0.5, VTK_FLOAT_MAX);
+  vtkGetMacro(KeyToFillRatio, float);
+
+  // Description: 
+  // Set/Get the key-to-headlight ratio.  Similar to the key-to-fill
+  // ratio, this ratio controls how bright the headlight light is
+  // compared to the key light: larger values correspond to a dimmer
+  // headlight light.  The headlight is special kind of fill light,
+  // lighting only the parts of the object that the camera can see.
+  // As such, a headlight tends to reduce the contrast of a scene.  It
+  // can be used to fill in "shadows" of the object missed by the key
+  // and fill lights.  The headlight should always be significantly
+  // dimmer than the key light:  ratios of 3 to 15 are typical.
+  vtkSetClampMacro(KeyToHeadRatio, float, 0.5, VTK_FLOAT_MAX);
+  vtkGetMacro(KeyToHeadRatio, float);
+
+  // Description: 
+  // Set the warmth of each the lights.  Warmth is a parameter that
+  // varies from 0 to 1, where 0 is "cold" (looks icy or lit by a very
+  // blue sky), 1 is "warm" (the red of a very red sunset, or the
+  // embers of a campfire), and 0.5 is a neutral white.  The warmth
+  // scale is non-linear. Warmth values close to 0.5 are subtly
+  // "warmer" or "cooler," much like a warmer tungsten incandescent
+  // bulb, a cooler halogen, or daylight (cooler still).  Moving
+  // further away from 0.5, colors become more quickly varying towards
+  // blues and reds.  With regards to aesthetics, extremes of warmth
+  // should be used sparingly.
+  vtkSetMacro(KeyLightWarmth, float);
+  vtkGetMacro(KeyLightWarmth, float);
+
+  vtkSetMacro(FillLightWarmth, float);
+  vtkGetMacro(FillLightWarmth, float);
+
+  vtkSetMacro(HeadlightWarmth, float);
+  vtkGetMacro(HeadlightWarmth, float);
+
+  // Description:
+  // Returns the floating-point RGB values of each of the light's color.
+  vtkGetVectorMacro(KeyLightColor,  float, 3);
+  vtkGetVectorMacro(FillLightColor, float, 3);
+  vtkGetVectorMacro(HeadlightColor, float, 3);
+
+  // Description:
+  // If MaintainLuminance is set, the LightKit will attempt to maintain
+  // the apparent intensity of lights based on their perceptual brightnesses.
+  // By default, MaintainLuminance is off.
+  vtkBooleanMacro(MaintainLuminance, int);
+  vtkGetMacro(MaintainLuminance, int);
+  vtkSetMacro(MaintainLuminance, int);
+
+  // Description: 
+  // Get/Set the position of the key light and fill
+  // light, using angular methods.  Elevation corresponds to latitude,
+  // azimuth to longitude.  It is recommended that the key light
+  // always be on the viewer's side of the object and above the
+  // object, while the fill light generally lights the part of the object
+  // not lit by the fill light.  The headlight, which is always located
+  // at the viewer, can then be used to reduce the contrast in the image.
+  void SetKeyLightAngle(float elevation, float azimuth);
+  void SetKeyLightAngle(float angle[2]) { 
+    this->SetKeyLightAngle(angle[0], angle[1]); };
+
+  void SetKeyLightElevation(float x) {
+    SetKeyLightAngle(x, this->KeyLightAngle[1]); };
+
+  void SetKeyLightAzimuth(float x) {
+    SetKeyLightAngle(this->KeyLightAngle[0], x); };
+
+  vtkGetVectorMacro(KeyLightAngle, float, 2);
+  float GetKeyLightElevation() {
+    float ang[2]; GetKeyLightAngle(ang); return ang[0]; };
+
+  float GetKeyLightAzimuth() {
+    float ang[2]; GetKeyLightAngle(ang); return ang[1]; };
+
+  void SetFillLightAngle(float elevation, float azimuth);
+  void SetFillLightAngle(float angle[2]) { 
+    this->SetFillLightAngle(angle[0], angle[1]); };
+
+  void SetFillLightElevation(float x) {
+    SetFillLightAngle(x, this->FillLightAngle[1]); };
+
+  void SetFillLightAzimuth(float x) {
+    SetFillLightAngle(this->FillLightAngle[0], x); };
+
+  vtkGetVectorMacro(FillLightAngle, float, 2);
+  float GetFillLightElevation() {
+    float ang[2]; GetFillLightAngle(ang); return ang[0]; };
+
+  float GetFillLightAzimuth() {
+    float ang[2]; GetFillLightAngle(ang); return ang[1]; };
+
+  // Description:
+  // Add lights to, or remove lights from, a renderer.  
+  // Lights may be added to more than one renderer, if desired.
+  void AddLightsToRenderer(vtkRenderer *renderer);
+  void RemoveLightsFromRenderer(vtkRenderer *renderer);
+
+  void DeepCopy(vtkLightKit *kit);
+
+  void Modified();
+  void Update();
+
+protected:
+  vtkLightKit();
+  ~vtkLightKit();
+
+  void WarmthToRGBI(float w, float rgb[3], float& i);
+  void WarmthToRGB(float w, float rgb[3]);
+  void InitializeWarmthFunctions();
+  float WarmthToIntensity(float w);
+
+  vtkLightKit(const vtkLightKit&) {};
+  void operator=(const vtkLightKit&) {};
+
+  float KeyLightIntensity;
+  float KeyToFillRatio;
+  float KeyToHeadRatio;
+  
+  vtkLight *KeyLight;
+  float KeyLightWarmth;
+  float KeyLightAngle[2];
+  float KeyLightColor[3];
+
+  vtkLight *FillLight;
+  float FillLightWarmth;
+  float FillLightAngle[2];
+  float FillLightColor[3];
+
+  vtkLight *Headlight;
+  float HeadlightWarmth;
+  float HeadlightColor[3];
+
+  int MaintainLuminance;
+
+  vtkPiecewiseFunction *WarmthFunction[4]; // r, g, b, perceptual length
+};
+
+#endif
