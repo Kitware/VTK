471a177fbd6e7ce0b8277e4751ecf99891b2d70e
Will Schroeder <will.schroeder@kitware.com>
2000-09-07 03:40:23 -0400

    ENH:Specialization of vtkGlyph2D to vtkGlyph3D

---
+++ graphics/vtkGlyph2D.cxx
@@ -0,0 +1,456 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkGlyph2D.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#include "vtkGlyph2D.h"
+#include "vtkTransform.h"
+#include "vtkMath.h"
+#include "vtkObjectFactory.h"
+
+//------------------------------------------------------------------------
+vtkGlyph2D* vtkGlyph2D::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkGlyph2D");
+  if(ret)
+    {
+    return (vtkGlyph2D*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkGlyph2D;
+}
+
+void vtkGlyph2D::Execute()
+{
+  vtkPointData *pd;
+  vtkScalars *inScalars;
+  vtkVectors *inVectors;
+  vtkGhostLevels *inGhostLevels;
+  vtkNormals *inNormals, *sourceNormals;
+  vtkDataArray *newScalarsData, *inScalarsData;
+  int numPts, numSourcePts, numSourceCells;
+  int inPtId, i, index;
+  vtkPoints *sourcePts;
+  vtkPoints *newPts;
+  vtkScalars *newScalars=NULL;
+  vtkVectors *newVectors=NULL;
+  vtkNormals *newNormals=NULL;
+  float *x, *v, s, vMag, value, theta;
+  vtkTransform *trans = vtkTransform::New();
+  vtkCell *cell;
+  vtkIdList *cellPts;
+  int npts;
+  vtkIdList *pts;
+  int haveVectors, haveNormals, ptIncr, cellId;
+  float scalex,scaley, den;
+  vtkPolyData *output = this->GetOutput();
+  vtkPointData *outputPD = output->GetPointData();
+  vtkDataSet *input = this->GetInput();
+  int numberOfSources = this->GetNumberOfSources();
+
+  vtkDebugMacro(<<"Generating 2D glyphs");
+
+  pts = vtkIdList::New();
+  pts->Allocate(VTK_CELL_SIZE);
+
+  pd = input->GetPointData();
+
+  inScalars = pd->GetScalars();
+  inVectors = pd->GetVectors();
+  inNormals = pd->GetNormals();
+  inGhostLevels = pd->GetGhostLevels();
+
+  numPts = input->GetNumberOfPoints();
+  if (numPts < 1)
+    {
+    vtkErrorMacro(<<"No points to glyph!");
+    return;
+    }
+
+  // Check input for consistency
+  //
+  if ( (den = this->Range[1] - this->Range[0]) == 0.0 )
+    {
+    den = 1.0;
+    }
+  if ( this->VectorMode != VTK_VECTOR_ROTATION_OFF &&
+       ((this->VectorMode == VTK_USE_VECTOR && inVectors != NULL) ||
+        (this->VectorMode == VTK_USE_NORMAL && inNormals != NULL)) )
+    {
+    haveVectors = 1;
+    }
+  else
+    {
+    haveVectors = 0;
+    }
+
+  if ( (this->IndexMode == VTK_INDEXING_BY_SCALAR && !inScalars) ||
+       (this->IndexMode == VTK_INDEXING_BY_VECTOR &&
+       ((!inVectors && this->VectorMode == VTK_USE_VECTOR) ||
+        (!inNormals && this->VectorMode == VTK_USE_NORMAL))) )
+    {
+    if ( this->GetSource(0) == NULL )
+      {
+      vtkErrorMacro(<<"Indexing on but don't have data to index with");
+      pts->Delete();
+      return;
+      }
+    else
+      {
+      vtkWarningMacro(<<"Turning indexing off: no data to index with");
+      this->IndexMode = VTK_INDEXING_OFF;
+      }
+    }
+
+  // Allocate storage for output PolyData
+  //
+  outputPD->CopyScalarsOff();
+  outputPD->CopyVectorsOff();
+  outputPD->CopyNormalsOff();
+  if ( this->IndexMode != VTK_INDEXING_OFF )
+    {
+    pd = NULL;
+    numSourcePts = numSourceCells = 0;
+    haveNormals = 1;
+    for (numSourcePts=numSourceCells=i=0; i < numberOfSources; i++)
+      {
+      if ( this->GetSource(i) != NULL )
+        {
+        numSourcePts += this->GetSource(i)->GetNumberOfPoints();
+        numSourceCells += this->GetSource(i)->GetNumberOfCells();
+        sourceNormals = this->GetSource(i)->GetPointData()->GetNormals();
+        if ( !sourceNormals )
+          {
+          haveNormals = 0;
+          }
+        }
+      }
+    }
+  else
+    {
+    sourcePts = this->GetSource(0)->GetPoints();
+    numSourcePts = sourcePts->GetNumberOfPoints();
+    numSourceCells = this->GetSource(0)->GetNumberOfCells();
+
+    sourceNormals = this->GetSource(0)->GetPointData()->GetNormals();
+    if ( sourceNormals )
+      {
+      haveNormals = 1;
+      }
+    else
+      {
+      haveNormals = 0;
+      }
+
+    // Prepare to copy output.
+    pd = this->GetSource(0)->GetPointData();
+    outputPD->CopyAllocate(pd,numPts*numSourcePts);
+    }
+
+  newPts = vtkPoints::New();
+  newPts->Allocate(numPts*numSourcePts);
+  if ( this->ColorMode == VTK_COLOR_BY_SCALAR && inScalars )
+    {
+    newScalars = (vtkScalars *) inScalars->MakeObject ();
+    newScalars->Allocate(numPts*numSourcePts);
+    newScalarsData = newScalars->GetData ();
+    inScalarsData = inScalars->GetData ();
+    }
+  else if ( (this->ColorMode == VTK_COLOR_BY_SCALE) && inScalars)
+    {
+    newScalars = vtkScalars::New();
+    newScalars->Allocate(numPts*numSourcePts);
+    }
+  else if ( (this->ColorMode == VTK_COLOR_BY_VECTOR) && haveVectors)
+    {
+    newScalars = vtkScalars::New();
+    newScalars->Allocate(numPts*numSourcePts);
+    }
+  if ( haveVectors )
+    {
+    newVectors = vtkVectors::New();
+    newVectors->Allocate(numPts*numSourcePts);
+    }
+  if ( haveNormals )
+    {
+    newNormals = vtkNormals::New();
+    newNormals->Allocate(numPts*numSourcePts);
+    }
+
+  // Setting up for calls to PolyData::InsertNextCell()
+  output->Allocate(3*numPts*numSourceCells,numPts*numSourceCells);
+
+  // Traverse all Input points, transforming Source points and copying
+  // point attributes.
+  //
+  ptIncr=0;
+  for (inPtId=0; inPtId < numPts; inPtId++)
+    {
+    scalex = scaley = 1.0;
+    if ( ! (inPtId % 10000) )
+      {
+      this->UpdateProgress ((float)inPtId/numPts);
+      if (this->GetAbortExecute())
+        {
+        break;
+        }
+      }
+
+    // Get the scalar and vector data
+    if ( inScalars )
+      {
+      s = inScalars->GetScalar(inPtId);
+      if ( this->ScaleMode == VTK_SCALE_BY_SCALAR ||
+                this->ScaleMode == VTK_DATA_SCALING_OFF )
+        {
+        scalex = scaley = s;
+        }
+      }
+    
+    if ( haveVectors )
+      {
+      if ( this->VectorMode == VTK_USE_NORMAL )
+        {
+        v = inNormals->GetNormal(inPtId);
+        }
+      else
+        {
+        v = inVectors->GetVector(inPtId);
+        }
+      vMag = vtkMath::Norm(v);
+      if ( this->ScaleMode == VTK_SCALE_BY_VECTORCOMPONENTS )
+        {
+        scalex = v[0];
+        scaley = v[1];
+        }
+      else if ( this->ScaleMode == VTK_SCALE_BY_VECTOR )
+        {
+        scalex = scaley = vMag;
+        }
+      }
+    
+    // Clamp data scale if enabled
+    if ( this->Clamping )
+      {
+      scalex = (scalex < this->Range[0] ? this->Range[0] :
+                (scalex > this->Range[1] ? this->Range[1] : scalex));
+      scalex = (scalex - this->Range[0]) / den;
+      scaley = (scaley < this->Range[0] ? this->Range[0] :
+                (scaley > this->Range[1] ? this->Range[1] : scaley));
+      scaley = (scaley - this->Range[0]) / den;
+      }
+    
+    // Compute index into table of glyphs
+    if ( this->IndexMode == VTK_INDEXING_OFF )
+      {
+      index = 0;
+      }
+    else 
+      {
+      if ( this->IndexMode == VTK_INDEXING_BY_SCALAR )
+        {
+        value = s;
+        }
+      else
+        {
+        value = vMag;
+        }
+      
+      index = (int) ((float)(value - this->Range[0]) * numberOfSources / den);
+      index = (index < 0 ? 0 :
+              (index >= numberOfSources ? (numberOfSources-1) : index));
+      
+      if ( this->GetSource(index) != NULL )
+        {
+        sourcePts = this->GetSource(index)->GetPoints();
+        sourceNormals = this->GetSource(index)->GetPointData()->GetNormals();
+        numSourcePts = sourcePts->GetNumberOfPoints();
+        numSourceCells = this->GetSource(index)->GetNumberOfCells();
+        }
+      }
+    
+    // Make sure we're not indexing into empty glyph
+    if ( this->GetSource(index) == NULL )
+      {
+      continue;
+      }
+
+    // Check ghost points.
+    if (inGhostLevels && inGhostLevels->GetGhostLevel(inPtId) > 0)
+      {
+      continue;
+      }
+    
+    // Now begin copying/transforming glyph
+    trans->Identity();
+    
+    // Copy all topology (transformation independent)
+    for (cellId=0; cellId < numSourceCells; cellId++)
+      {
+      cell = this->GetSource(index)->GetCell(cellId);
+      cellPts = cell->GetPointIds();
+      npts = cellPts->GetNumberOfIds();
+      for (pts->Reset(), i=0; i < npts; i++) 
+        {
+        pts->InsertId(i,cellPts->GetId(i) + ptIncr);
+        }
+      output->InsertNextCell(cell->GetCellType(),pts);
+      }
+    
+    // translate Source to Input point
+    x = input->GetPoint(inPtId);
+    trans->Translate(x[0], x[1], 0.0);
+    
+    if ( haveVectors )
+      {
+      // Copy Input vector
+      for (i=0; i < numSourcePts; i++) 
+        {
+        newVectors->InsertVector(i+ptIncr, v);
+        }
+      if (this->Orient && (vMag > 0.0))
+        {
+        theta = atan2(v[0],v[1])/vtkMath::DegreesToRadians();
+        trans->RotateWXYZ(theta, 0.0, 0.0, 1.0);
+        }
+      }
+    
+    // determine scale factor from scalars if appropriate
+    if ( inScalars )
+      {
+      // Copy scalar value
+      if (this->ColorMode == VTK_COLOR_BY_SCALE)
+        {
+        for (i=0; i < numSourcePts; i++) 
+          {
+          newScalars->InsertScalar(i+ptIncr, scalex); 
+          }
+        }
+      else if (this->ColorMode == VTK_COLOR_BY_SCALAR)
+        {
+        for (i=0; i < numSourcePts; i++)
+          {
+          outputPD->CopyTuple(inScalarsData, newScalarsData, inPtId, ptIncr+i);
+          }
+        }
+      }
+    if (haveVectors && this->ColorMode == VTK_COLOR_BY_VECTOR)
+      {
+      for (i=0; i < numSourcePts; i++) 
+        {
+        newScalars->InsertScalar(i+ptIncr, vMag);
+        }
+      }
+    
+    // scale data if appropriate
+    if ( this->Scaling )
+      {
+      if ( this->ScaleMode == VTK_DATA_SCALING_OFF )
+        {
+        scalex = scaley = this->ScaleFactor;
+        }
+      else
+        {
+        scalex *= this->ScaleFactor;
+        scaley *= this->ScaleFactor;
+        }
+      
+      if ( scalex == 0.0 )
+        {
+        scalex = 1.0e-10;
+        }
+      if ( scaley == 0.0 )
+        {
+        scaley = 1.0e-10;
+        }
+      trans->Scale(scalex,scaley,1.0);
+      }
+    
+    // multiply points and normals by resulting matrix
+    trans->MultiplyPoints(sourcePts,newPts);
+    
+    if ( haveNormals )
+      {
+      trans->MultiplyNormals(sourceNormals,newNormals);
+      }
+    
+    // Copy point data from source (if possible)
+    if ( pd ) 
+      {
+      for (i=0; i < numSourcePts; i++)
+        {
+        outputPD->CopyData(pd,i,ptIncr+i);
+        }
+      }
+    ptIncr += numSourcePts;
+    } 
+
+  // Update ourselves and release memory
+  //
+  output->SetPoints(newPts);
+  newPts->Delete();
+
+  if (newScalars)
+    {
+    outputPD->SetScalars(newScalars);
+    newScalars->Delete();
+    }
+
+  if (newVectors)
+    {
+    outputPD->SetVectors(newVectors);
+    newVectors->Delete();
+    }
+
+  if (newNormals)
+    {
+    outputPD->SetNormals(newNormals);
+    newNormals->Delete();
+    }
+
+  output->Squeeze();
+  trans->Delete();
+  pts->Delete();
+}
+
+void vtkGlyph2D::PrintSelf(ostream& os, vtkIndent indent)
+{
+  vtkGlyph3D::PrintSelf(os,indent);
+}
---
+++ graphics/vtkGlyph2D.h
@@ -0,0 +1,81 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkGlyph2D.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkGlyph2D - copy oriented and scaled glyph geometry to every input point (2D specialization)
+// .SECTION Description
+// This subclass of vtkGlyph3D is a specialization to 2D. Transformations
+// (i.e., translation, scaling, and rotation) are constrained to the plane.
+// For example, rotations due to a vector are computed from the x-y
+// coordinates of the vector only, and are assumed to occur around the
+// z-axis. (See vtkGlyph3D for documentation on the interface to this
+// class.)
+// 
+// .SECTION See Also
+// vtkTensorGlyph vtkGlyph3D vtkProgrammableGlyphFilter
+
+#ifndef __vtkGlyph2D_h
+#define __vtkGlyph2D_h
+
+#include "vtkGlyph3D.h"
+
+class VTK_EXPORT vtkGlyph2D : public vtkGlyph3D
+{
+public:
+  vtkTypeMacro(vtkGlyph2D,vtkGlyph3D);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description
+  // Construct object with scaling on, scaling mode is by scalar value, 
+  // scale factor = 1.0, the range is (0,1), orient geometry is on, and
+  // orientation is by vector. Clamping and indexing are turned off. No
+  // initial sources are defined.
+  static vtkGlyph2D *New();
+
+protected:
+  vtkGlyph2D() {};
+  ~vtkGlyph2D() {};
+  vtkGlyph2D(const vtkGlyph2D&) {};
+  void operator=(const vtkGlyph2D&) {};
+
+  void Execute();
+};
+
+#endif
