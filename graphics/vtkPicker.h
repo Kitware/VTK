c3c25c2cb9585e4fce89722d5a10cc63d38d3c6d
David Gobbi <david.gobbi@gmail.com>
2000-09-12 18:55:21 -0400

    ERR: vtkTypeMacro() -> derived from vtkAbstractPicker, not from vtkObject

--- graphics/vtkPicker.h
+++ graphics/vtkPicker.h
@@ -75,7 +75,7 @@ class VTK_EXPORT vtkPicker : public vtkAbstractPropPicker
 {
 public:
   static vtkPicker *New();
-  vtkTypeMacro(vtkPicker,vtkObject);
+  vtkTypeMacro(vtkPicker,vtkAbstractPropPicker);
   void PrintSelf(ostream& os, vtkIndent indent);
 
   // Description:
