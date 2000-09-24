831ada223392fd00762ff260b33b82598b292f06
Will Schroeder <will.schroeder@kitware.com>
2000-09-24 06:24:02 -0400

    ENH:ShallowCopy missed members

--- contrib/vtkLegendBoxActor.cxx
+++ contrib/vtkLegendBoxActor.cxx
@@ -699,9 +699,16 @@ void vtkLegendBoxActor::ShallowCopy(vtkProp *prop)
     this->SetShadow(a->GetShadow());
     this->SetFontFamily(a->GetFontFamily());
     this->SetBorder(a->GetBorder());
-    this->SetPadding(a->GetPadding());
     this->SetLockBorder(a->GetLockBorder());
-    this->SetLockBorder(a->GetScalarVisibility());
+    this->SetPadding(a->GetPadding());
+    this->SetScalarVisibility(a->GetScalarVisibility());
+    this->SetNumberOfEntries(a->GetNumberOfEntries());
+    for (int i=0; i<this->NumberOfEntries; i++)
+      {
+      this->SetEntrySymbol(i,a->GetEntrySymbol(i));
+      this->SetEntryString(i,a->GetEntryString(i));
+      this->SetEntryColor(i,a->GetEntryColor(i));
+      }
     }
 
   // Now do superclass
