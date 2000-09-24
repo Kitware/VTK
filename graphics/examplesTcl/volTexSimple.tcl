321f4a224ae3a984492ac5188a1f397552849c61
Bill Lorensen <lorensen@crd.ge.com>
2000-09-24 07:28:46 -0400

    ENH: better coverage.

--- graphics/examplesTcl/volTexSimple.tcl
+++ graphics/examplesTcl/volTexSimple.tcl
@@ -35,14 +35,73 @@ vtkVolume volume
 
 # Okay now the graphics stuff
 vtkRenderer ren1
+vtkRenderer ren2
+vtkRenderer ren3
+vtkRenderer ren4
+vtkRenderer ren5
+vtkRenderer ren6
 vtkRenderWindow renWin
     renWin AddRenderer ren1
-    renWin SetSize 256 256
+    renWin AddRenderer ren2
+    renWin AddRenderer ren3
+    renWin AddRenderer ren4
+    renWin AddRenderer ren5
+    renWin AddRenderer ren6
+    renWin SetSize 768 512
 vtkRenderWindowInteractor iren
     iren SetRenderWindow renWin
 
 ren1 AddVolume volume
+ren2 AddVolume volume
+ren3 AddVolume volume
+ren4 AddVolume volume
+ren5 AddVolume volume
+ren6 AddVolume volume
+
 ren1 SetBackground 0.1 0.2 0.4
+ren2 SetBackground 0.1 0.2 0.4
+ren3 SetBackground 0.1 0.2 0.4
+ren4 SetBackground 0.1 0.2 0.4
+ren5 SetBackground 0.1 0.2 0.4
+ren6 SetBackground 0.1 0.2 0.4
+
+ren1 SetViewport 0 0 .3 .5
+ren2 SetViewport .3 0 .6 .5
+ren3 SetViewport .6 0 1 .5
+ren4 SetViewport 0 .5 .3 1
+ren5 SetViewport .3 .5 .6 1
+ren6 SetViewport .6 .5 1 1
+
+[ren1 GetActiveCamera] SetPosition 1 0 0
+[ren1 GetActiveCamera] SetFocalPoint 0 0 0
+[ren1 GetActiveCamera] SetViewUp 0 1 0
+ren1 ResetCamera
+
+[ren4 GetActiveCamera] SetPosition -1 0 0
+[ren4 GetActiveCamera] SetFocalPoint 0 0 0
+[ren4 GetActiveCamera] SetViewUp 0 1 0
+ren4 ResetCamera
+
+[ren2 GetActiveCamera] SetPosition 0 0 1
+[ren2 GetActiveCamera] SetFocalPoint 0 0 0
+[ren2 GetActiveCamera] SetViewUp 0 1 0
+ren2 ResetCamera
+
+[ren5 GetActiveCamera] SetPosition 0 0 -1
+[ren5 GetActiveCamera] SetFocalPoint 0 0 0
+[ren5 GetActiveCamera] SetViewUp 0 1 0
+ren5 ResetCamera
+
+[ren3 GetActiveCamera] SetPosition 0 1 0
+[ren3 GetActiveCamera] SetFocalPoint 0 0 0
+[ren3 GetActiveCamera] SetViewUp 0 0 1
+ren3 ResetCamera
+
+[ren6 GetActiveCamera] SetPosition 0 -1 0
+[ren6 GetActiveCamera] SetFocalPoint 0 0 0
+[ren6 GetActiveCamera] SetViewUp 0 0 1
+ren6 ResetCamera
+
 renWin Render
 
 proc TkCheckAbort {} {
