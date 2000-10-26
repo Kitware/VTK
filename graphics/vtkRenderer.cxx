1874c0e118962321a3ed41ae26af4716349593fb
John Biddiscombe <jbiddiscombe@skippingmouse.co.uk>
2000-10-26 10:28:20 -0400

    ERR: When BBox of scene passes in front of nearest clipping plane, dubious flickering occurs on 32bit zbuffer cards. Have made 24&32bit same as 16bit until I work out a better fix.

--- graphics/vtkRenderer.cxx
+++ graphics/vtkRenderer.cxx
@@ -891,11 +891,11 @@ void vtkRenderer::ResetCameraClippingRange( float bounds[6] )
     }
   else if ( ZBufferDepth <= 24 )
     {
-    range[0] = (range[0] < 0.001*range[1])?(0.001*range[1]):(range[0]);
+    range[0] = (range[0] < 0.01*range[1])?(0.01*range[1]):(range[0]);
     }
   else
     {
-    range[0] = (range[0] < 0.0001*range[1])?(0.0001*range[1]):(range[0]);
+    range[0] = (range[0] < 0.01*range[1])?(0.01*range[1]):(range[0]);
     }
 
   this->ActiveCamera->SetClippingRange( range );
