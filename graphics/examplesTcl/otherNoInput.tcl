d265eb12e02b2db9c4ab8cfcc1a836fa50773c9a
Bill Lorensen <lorensen@crd.ge.com>
2000-11-19 18:54:32 -0500

    ENH: a new test on the horizon.

---
+++ graphics/examplesTcl/otherNoInput.tcl
@@ -0,0 +1,46 @@
+# All tests need to:
+# Define 2 variables and 1 proc
+#     var 1: rtSelector - a shell command line to select the output to be compared
+#     var 2: rtComparator - a shell command to compare the selected output with the baseline
+#     proc 1: rtOtherTest (fileid)
+# A few statements at the end to run outside the regression testing framework
+#
+
+catch {load vtktcl}
+
+set rtSelector "sed -e s/0x0/0/ | sed -e s/-0/0/ | grep -v -i thread | grep -v StartTime: | grep -v 0x | grep -v Modified | grep -v 'Compute Time:'"
+set rtSelector "sed -e s/0x0/0/ | sed -e s/-0/0/ | grep -v -i thread | grep -v Time: | grep -v 0x | grep -v Modified"
+set rtComparator "diff -b"
+
+proc rtOtherTest { fileid } {
+#actual test
+    vtkFileOutputWindow w
+    w SetInstance w
+    puts $fileid "No Input test started"
+
+    set all [lsort [info command vtk*]]
+    foreach a $all {
+	puts $fileid "$a --------------"
+	if {$a == "vtkIndent"} {
+	    continue
+	}
+	if {$a == "vtkTimeStamp"} {
+	    continue
+	}
+	if {$a == "vtkSynchronizedTemplates3D"} {
+	    continue
+	}
+	w DisplayText "$a-----------"
+	catch {$a b; puts $fileid "[b Update]"}
+	catch {b Delete}
+    }
+}
+
+# All tests should end with the following...
+
+if {![info exists rtOutId]} {
+    rtOtherTest stdout
+    exit
+}
+
+wm withdraw .
