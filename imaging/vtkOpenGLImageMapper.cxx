0ea3ed61e90c3905e1ca562e46b70385b7a444bd
John Biddiscombe <jbiddiscombe@skippingmouse.co.uk>
2000-11-21 21:21:15 -0500

    ERR:Corrected Width calculation +1

--- imaging/vtkOpenGLImageMapper.cxx
+++ imaging/vtkOpenGLImageMapper.cxx
@@ -219,8 +219,8 @@ static void vtkOpenGLImageMapperRender(
 
   if (self->GetRenderToRectangle())
     {
-    int rectwidth  = actorPos2[0] - actorPos[0];
-    int rectheight = actorPos2[1] - actorPos[1];
+    int rectwidth  = (actorPos2[0] - actorPos[0]) + 1;
+    int rectheight = (actorPos2[1] - actorPos[1]) + 1;
     float xscale = (float)rectwidth/width;
     float yscale = (float)rectheight/height;
     glPixelZoom(xscale, yscale);
@@ -355,8 +355,8 @@ static void vtkOpenGLImageMapperRenderShort(
 
   if (self->GetRenderToRectangle())
     {
-    int rectwidth  = actorPos2[0] - actorPos[0];
-    int rectheight = actorPos2[1] - actorPos[1];
+    int rectwidth  = (actorPos2[0] - actorPos[0]) + 1;
+    int rectheight = (actorPos2[1] - actorPos[1]) + 1;
     float xscale = (float)rectwidth/width;
     float yscale = (float)rectheight/height;
     glPixelZoom(xscale, yscale);
@@ -401,8 +401,8 @@ static void vtkOpenGLImageMapperRenderChar(
 
   if (self->GetRenderToRectangle())
     {
-    int rectwidth  = actorPos2[0] - actorPos[0];
-    int rectheight = actorPos2[1] - actorPos[1];
+    int rectwidth  = (actorPos2[0] - actorPos[0]) + 1;
+    int rectheight = (actorPos2[1] - actorPos[1]) + 1;
     float xscale = (float)rectwidth/width;
     float yscale = (float)rectheight/height;
     glPixelZoom(xscale, yscale);
