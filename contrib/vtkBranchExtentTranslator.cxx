30fac2292870a09a6712d78509658931d7f72d0b
Charles Law <charles.law@kitware.com>
2000-11-29 15:44:56 -0500

    From ParaView

---
+++ contrib/vtkBranchExtentTranslator.cxx
@@ -0,0 +1,178 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkBranchExtentTranslator.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+
+#include "vtkBranchExtentTranslator.h"
+#include "vtkObjectFactory.h"
+#include "vtkSource.h"
+
+
+
+//------------------------------------------------------------------------------
+vtkBranchExtentTranslator* vtkBranchExtentTranslator::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkBranchExtentTranslator");
+  if(ret)
+    {
+    return (vtkBranchExtentTranslator*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkBranchExtentTranslator;
+}
+
+
+//----------------------------------------------------------------------------
+vtkBranchExtentTranslator::vtkBranchExtentTranslator()
+{
+  this->OriginalSource = NULL;
+}
+
+//----------------------------------------------------------------------------
+vtkBranchExtentTranslator::~vtkBranchExtentTranslator()
+{
+  this->SetOriginalSource(NULL);
+}
+
+//----------------------------------------------------------------------------
+int vtkBranchExtentTranslator::PieceToExtent()
+{
+  //cerr << this << " PieceToExtent: " << this->Piece << " of " << this->NumberOfPieces << endl;
+  //cerr << "OriginalData: " << this->OriginalSource << endl;
+  //cerr << "OriginalData is of type " << this->OriginalSource->GetClassName() << endl;
+  //cerr << "OriginalSource: " << this->OriginalSource->GetSource() << endl;
+  //cerr << "OriginalSource is of type: " << this->OriginalSource->GetSource()->GetClassName() << endl;
+  
+  
+  if (this->OriginalSource == NULL)
+    { // If the user has not set the original source, then just default
+    // to the method in the superclass.
+    return this->vtkExtentTranslator::PieceToExtent();
+    }
+
+  this->OriginalSource->UpdateInformation();
+  this->OriginalSource->GetWholeExtent(this->Extent);
+
+  //cerr << this << "WholeExtent: " << this->Extent[0] << ", " << this->Extent[1] << ", " 
+  //     << this->Extent[2] << ", " << this->Extent[3] << ", " 
+  //     << this->Extent[4] << ", " << this->Extent[5] << endl;
+  
+  if (this->SplitExtent(this->Piece, this->NumberOfPieces, this->Extent) == 0)
+    {
+    //cerr << "Split thinks nothing is in the piece" << endl;
+    //cerr << this << " Split: " << this->Extent[0] << ", " << this->Extent[1] << ", " 
+    //   << this->Extent[2] << ", " << this->Extent[3] << ", "
+    //   << this->Extent[4] << ", " << this->Extent[5] << endl;    
+    // Nothing in this piece.
+    this->Extent[0] = this->Extent[2] = this->Extent[4] = 0;
+    this->Extent[1] = this->Extent[3] = this->Extent[5] = -1;
+    //cerr << this << " Extent: " << this->Extent[0] << ", " << this->Extent[1] << ", " 
+    //	 << this->Extent[2] << ", " << this->Extent[3] << ", " 
+    //	 << this->Extent[4] << ", " << this->Extent[5] << endl;    
+    return 0;
+    }
+
+  //cerr << this << " Split: " << this->Extent[0] << ", " << this->Extent[1] << ", " 
+  //     << this->Extent[2] << ", " << this->Extent[3] << ", " 
+  //     << this->Extent[4] << ", " << this->Extent[5] << endl;    
+  
+  // Clip with the whole extent passed in.
+  if (this->Extent[0] < this->WholeExtent[0])
+    {
+    this->Extent[0] = this->WholeExtent[0];
+    }  
+  if (this->Extent[1] > this->WholeExtent[1])
+    {
+    this->Extent[1] = this->WholeExtent[1];
+    }
+  if (this->Extent[2] < this->WholeExtent[2])
+    {
+    this->Extent[2] = this->WholeExtent[2];
+    }  
+  if (this->Extent[3] > this->WholeExtent[3])
+    {
+    this->Extent[3] = this->WholeExtent[3];
+    }
+  if (this->Extent[4] < this->WholeExtent[4])
+    {
+    this->Extent[4] = this->WholeExtent[4];
+    }  
+  if (this->Extent[5] > this->WholeExtent[5])
+    {
+    this->Extent[5] = this->WholeExtent[5];
+    }
+  
+  
+  if (this->Extent[0] > this->Extent[1] ||
+      this->Extent[2] > this->Extent[3] ||
+      this->Extent[4] > this->Extent[5])
+    {
+    this->Extent[0] = this->Extent[2] = this->Extent[4] = 0;
+    this->Extent[1] = this->Extent[3] = this->Extent[5] = -1;
+    //cerr << this << " Extent: " << this->Extent[0] << ", " << this->Extent[1] << ", " 
+    //	 << this->Extent[2] << ", " << this->Extent[3] << ", " 
+    //	 << this->Extent[4] << ", " << this->Extent[5] << endl;
+    return 0;
+    }  
+  
+  //cerr << this << " Extent: " << this->Extent[0] << ", " << this->Extent[1] << ", " 
+  //     << this->Extent[2] << ", " << this->Extent[3] << ", " 
+  //     << this->Extent[4] << ", " << this->Extent[5] << endl;
+  
+  return 1;
+}
+
+
+
+//----------------------------------------------------------------------------
+void vtkBranchExtentTranslator::PrintSelf(ostream& os, vtkIndent indent)
+{
+  vtkObject::PrintSelf(os,indent);
+
+  os << indent << "Original Source: (" << this->OriginalSource << ")\n";
+}
+
+
+
+
+
+
+
---
+++ contrib/vtkBranchExtentTranslator.h
@@ -0,0 +1,98 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkBranchExtentTranslator.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+
+Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+    THIS CLASS IS PATENT PENDING.
+
+    Application of this software for commercial purposes requires 
+    a license grant from Kitware. Contact:
+        Ken Martin
+        Kitware
+        469 Clifton Corporate Parkway,
+        Clifton Park, NY 12065
+        Phone:1-518-371-3971 
+    for more information.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkBranchExtentTranslator - Uses alternative source for whole extent.
+// .SECTION Description
+// vtkBranchExtentTranslator is like extent translator, but it uses an 
+// alternative source as a whole extent. The whole extent passed is assumed 
+// to be a subextent of the original source.  we simply take the intersection 
+// of the split extent and the whole extdent passed in.  We are attempting to
+// make branching pipelines request consistent extents with the same piece 
+// requests.  
+
+// .SECTION Caveats
+// This object is still under development.
+
+#ifndef __vtkBranchExtentTranslator_h
+#define __vtkBranchExtentTranslator_h
+
+#include "vtkExtentTranslator.h"
+#include "vtkImageData.h"
+
+
+class VTK_EXPORT vtkBranchExtentTranslator : public vtkExtentTranslator
+{
+public:
+  static vtkBranchExtentTranslator *New();
+
+  vtkTypeMacro(vtkBranchExtentTranslator,vtkExtentTranslator);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // This is the original upstream image source.
+  vtkSetObjectMacro(OriginalSource,vtkImageData);
+  vtkGetObjectMacro(OriginalSource,vtkImageData);
+
+  int PieceToExtent();
+
+protected:
+  vtkBranchExtentTranslator();
+  ~vtkBranchExtentTranslator();
+  vtkBranchExtentTranslator(const vtkBranchExtentTranslator&) {};
+  void operator=(const vtkBranchExtentTranslator&) {};
+
+  vtkImageData *OriginalSource;
+};
+
+#endif
+
