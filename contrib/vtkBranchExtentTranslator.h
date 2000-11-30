38e78fddda7f566b0b5e3859ea9e77c9989ceb61
Charles Law <charles.law@kitware.com>
2000-11-30 09:50:20 -0500

    Cut and paste error.  The class is not patent pending.

--- contrib/vtkBranchExtentTranslator.h
+++ contrib/vtkBranchExtentTranslator.h
@@ -11,17 +11,6 @@
 Copyright (c) 1993-2000 Ken Martin, Will Schroeder, Bill Lorensen 
 All rights reserved.
 
-    THIS CLASS IS PATENT PENDING.
-
-    Application of this software for commercial purposes requires 
-    a license grant from Kitware. Contact:
-        Ken Martin
-        Kitware
-        469 Clifton Corporate Parkway,
-        Clifton Park, NY 12065
-        Phone:1-518-371-3971 
-    for more information.
-
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
 
