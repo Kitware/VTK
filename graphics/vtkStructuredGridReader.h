c4aeda7639923c2d95980ce7d545c5662aa2a988
Bill Lorensen <lorensen@crd.ge.com>
2000-12-07 08:11:13 -0500

    ERR: ALll signatures of a member function must be overridden.

--- graphics/vtkStructuredGridReader.h
+++ graphics/vtkStructuredGridReader.h
@@ -68,6 +68,8 @@ public:
   // Description:
   // Get the output of this reader.
   vtkStructuredGrid *GetOutput();
+  vtkStructuredGrid *GetOutput(int idx)
+    {return (vtkStructuredGrid *) this->vtkSource::GetOutput(idx); };
   void SetOutput(vtkStructuredGrid *output);  
 
 protected:
