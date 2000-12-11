91a884da94dd4dd0fde551b52fe88baf37cdf1e9
Charles Law <charles.law@kitware.com>
2000-12-11 09:09:54 -0500

    LANL header

--- contrib/vtkTreeComposite.h
+++ contrib/vtkTreeComposite.h
@@ -38,6 +38,34 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =========================================================================*/
+
+// This software and ancillary information known as vtk_ext (and
+// herein called "SOFTWARE") is made available under the terms
+// described below.  The SOFTWARE has been approved for release with
+// associated LA_CC Number 99-44, granted by Los Alamos National
+// Laboratory in July 1999.
+//
+// Unless otherwise indicated, this SOFTWARE has been authored by an
+// employee or employees of the University of California, operator of
+// the Los Alamos National Laboratory under Contract No. W-7405-ENG-36
+// with the United States Department of Energy.
+//
+// The United States Government has rights to use, reproduce, and
+// distribute this SOFTWARE.  The public may copy, distribute, prepare
+// derivative works and publicly display this SOFTWARE without charge,
+// provided that this Notice and any statement of authorship are
+// reproduced on all copies.
+//
+// Neither the U. S. Government, the University of California, nor the
+// Advanced Computing Laboratory makes any warranty, either express or
+// implied, nor assumes any liability or responsibility for the use of
+// this SOFTWARE.
+//
+// If SOFTWARE is modified to produce derivative works, such modified
+// SOFTWARE should be clearly marked, so as not to confuse it with the
+// version available from Los Alamos National Laboratory.
+
+
 // .NAME vtkTreeComposite - An object to composite multiple render windows.
 // .SECTION Description
 // vtkTreeComposite operates in multiple processes.  Each compositer has 
