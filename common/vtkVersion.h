d9da4de740ca29fd3755e67ea8d3d4cbf79696f1
Jim Miller <millerjv@crd.ge.com>
2001-01-28 20:02:38 -0500

    Automated commit to force versioning.

--- common/vtkVersion.h
+++ common/vtkVersion.h
@@ -59,7 +59,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define VTK_MINOR_VERSION 2
 #define VTK_BUILD_VERSION 0
 #define VTK_VERSION "3.2.0"
-#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.762 $, $Date: 2001-01-28 01:02:49 $ (GMT)"
+#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.763 $, $Date: 2001-01-29 01:02:38 $ (GMT)"
 
 
 class VTK_EXPORT vtkVersion : public vtkObject {
