7b8bc5bda08e917a5bfbf990b0f6212cda40453c
Michalel Halle <halazar@media.mit.edu>
2001-03-07 11:41:44 -0500

    Changed to use "" instead of <> for includes.  (Why does vtk use ""?  It looks dorky :))

--- contrib/vtkWeightedTransformFilter.cxx
+++ contrib/vtkWeightedTransformFilter.cxx
@@ -40,9 +40,9 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =========================================================================*/
-#include <vtkWeightedTransformFilter.h>
-#include <vtkObjectFactory.h>
-#include <vtkMath.h>
+#include "vtkWeightedTransformFilter.h"
+#include "vtkObjectFactory.h"
+#include "vtkMath.h"
 
 //----------------------------------------------------------------------------
 
