ff8cc1d0d2a8467d5170bb5e779216c22991e81d
Ken Martin <ken.martin@kitware.com>
2001-03-08 15:21:04 -0500

    simplified image reader

---
+++ imaging/vtkImageReader2.cxx
@@ -0,0 +1,714 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkImageReader2.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "vtkByteSwap.h"
+
+#include "vtkImageReader2.h"
+#include "vtkObjectFactory.h"
+
+
+
+//-----------------------------------------------------------------------------
+vtkImageReader2* vtkImageReader2::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkImageReader2");
+  if(ret)
+    {
+    return (vtkImageReader2*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkImageReader2;
+}
+
+#ifdef read
+#undef read
+#endif
+
+#ifdef close
+#undef close
+#endif
+
+//----------------------------------------------------------------------------
+vtkImageReader2::vtkImageReader2()
+{
+  int idx;
+  
+  this->FilePrefix = NULL;
+  this->FilePattern = new char[strlen("%s.%d") + 1];
+  strcpy (this->FilePattern, "%s.%d");
+  this->File = NULL;
+
+  this->DataScalarType = VTK_SHORT;
+  this->NumberOfScalarComponents = 1;
+  
+  for (idx = 0; idx < 3; ++idx)
+    {
+    this->DataIncrements[idx] = 1;
+    this->DataExtent[idx*2] = this->DataExtent[idx*2 + 1] = 0;
+    this->DataSpacing[idx] = 1.0;
+    this->DataOrigin[idx] = 0.0;
+    }
+  this->DataIncrements[3] = 1;
+  
+  this->FileName = NULL;
+  this->InternalFileName = NULL;
+  
+  this->HeaderSize = 0;
+  this->ManualHeaderSize = 0;
+  
+  // Left over from short reader
+  this->SwapBytes = 0;
+  this->FileLowerLeft = 0;
+  this->FileDimensionality = 2;
+}
+
+//----------------------------------------------------------------------------
+vtkImageReader2::~vtkImageReader2()
+{ 
+  if (this->File)
+    {
+    this->File->close();
+    delete this->File;
+    this->File = NULL;
+    }
+  
+  if (this->FileName)
+    {
+    delete [] this->FileName;
+    this->FileName = NULL;
+    }
+  if (this->FilePrefix)
+    {
+    delete [] this->FilePrefix;
+    this->FilePrefix = NULL;
+    }
+  if (this->FilePattern)
+    {
+    delete [] this->FilePattern;
+    this->FilePattern = NULL;
+    }
+  if (this->InternalFileName)
+    {
+    delete [] this->InternalFileName;
+    this->InternalFileName = NULL;
+    }
+}
+
+//----------------------------------------------------------------------------
+// This function sets the name of the file. 
+void vtkImageReader2::ComputeInternalFileName(int slice)
+{
+  // delete any old filename
+  if (this->InternalFileName)
+    {
+    delete [] this->InternalFileName;
+    }
+  
+  if (!this->FileName && !this->FilePattern)
+    {
+    vtkErrorMacro(<<"Either a FileName or FilePattern must be specified.");
+    return;
+    }
+
+  // make sure we figure out a filename to open
+  if (this->FileName)
+    {
+    this->InternalFileName = new char [strlen(this->FileName) + 10];
+    sprintf(this->InternalFileName,"%s",this->FileName);
+    }
+  else 
+    {
+    if (this->FilePrefix)
+      {
+      this->InternalFileName = new char [strlen(this->FilePrefix) +
+                                        strlen(this->FilePattern) + 10];
+      sprintf (this->InternalFileName, this->FilePattern, 
+               this->FilePrefix, slice);
+      }
+    else
+      {
+      this->InternalFileName = new char [strlen(this->FilePattern) + 10];
+      sprintf (this->InternalFileName, this->FilePattern, slice);
+      }
+    }
+}
+
+
+//----------------------------------------------------------------------------
+// This function sets the name of the file. 
+void vtkImageReader2::SetFileName(const char *name)
+{
+  if ( this->FileName && name && (!strcmp(this->FileName,name)))
+    {
+    return;
+    }
+  if (!name && !this->FileName)
+    {
+    return;
+    }
+  if (this->FileName)
+    {
+    delete [] this->FileName;
+    }
+  if (this->FilePrefix)
+    {
+    delete [] this->FilePrefix;
+    this->FilePrefix = NULL;
+    }
+  if (name)
+    {
+    this->FileName = new char[strlen(name) + 1];
+    strcpy(this->FileName, name);
+    }
+  else
+    {
+    this->FileName = NULL;
+    }
+
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+// This function sets the prefix of the file name. "image" would be the
+// name of a series: image.1, image.2 ...
+void vtkImageReader2::SetFilePrefix(const char *prefix)
+{
+  if ( this->FilePrefix && prefix && (!strcmp(this->FilePrefix,prefix)))
+    {
+    return;
+    }
+  if (!prefix && !this->FilePrefix)
+    {
+    return;
+    }
+  if (this->FilePrefix)
+    {
+    delete [] this->FilePrefix;
+    }
+  if (this->FileName)
+    {
+    delete [] this->FileName;
+    this->FileName = NULL;
+    }  
+  this->FilePrefix = new char[strlen(prefix) + 1];
+  strcpy(this->FilePrefix, prefix);
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+// This function sets the pattern of the file name which turn a prefix
+// into a file name. "%s.%3d" would be the
+// pattern of a series: image.001, image.002 ...
+void vtkImageReader2::SetFilePattern(const char *pattern)
+{
+  if ( this->FilePattern && pattern && 
+       (!strcmp(this->FilePattern,pattern)))
+    {
+    return;
+    }
+  if (!pattern && !this->FilePattern)
+    {
+    return;
+    }
+  if (this->FilePattern)
+    {
+    delete [] this->FilePattern;
+    }
+  if (this->FileName)
+    {
+    delete [] this->FileName;
+    this->FileName = NULL;
+    }
+  this->FilePattern = new char[strlen(pattern) + 1];
+  strcpy(this->FilePattern, pattern);
+  this->Modified();
+}
+
+void vtkImageReader2::SetDataByteOrderToBigEndian()
+{
+#ifndef VTK_WORDS_BIGENDIAN
+  this->SwapBytesOn();
+#else
+  this->SwapBytesOff();
+#endif
+}
+
+void vtkImageReader2::SetDataByteOrderToLittleEndian()
+{
+#ifdef VTK_WORDS_BIGENDIAN
+  this->SwapBytesOn();
+#else
+  this->SwapBytesOff();
+#endif
+}
+
+void vtkImageReader2::SetDataByteOrder(int byteOrder)
+{
+  if ( byteOrder == VTK_FILE_BYTE_ORDER_BIG_ENDIAN )
+    {
+    this->SetDataByteOrderToBigEndian();
+    }
+  else
+    {
+    this->SetDataByteOrderToLittleEndian();
+    }
+}
+
+int vtkImageReader2::GetDataByteOrder()
+{
+#ifdef VTK_WORDS_BIGENDIAN
+  if ( this->SwapBytes )
+    {
+    return VTK_FILE_BYTE_ORDER_LITTLE_ENDIAN;
+    }
+  else
+    {
+    return VTK_FILE_BYTE_ORDER_BIG_ENDIAN;
+    }
+#else
+  if ( this->SwapBytes )
+    {
+    return VTK_FILE_BYTE_ORDER_BIG_ENDIAN;
+    }
+  else
+    {
+    return VTK_FILE_BYTE_ORDER_LITTLE_ENDIAN;
+    }
+#endif
+}
+
+const char *vtkImageReader2::GetDataByteOrderAsString()
+{
+#ifdef VTK_WORDS_BIGENDIAN
+  if ( this->SwapBytes )
+    {
+    return "LittleEndian";
+    }
+  else
+    {
+    return "BigEndian";
+    }
+#else
+  if ( this->SwapBytes )
+    {
+    return "BigEndian";
+    }
+  else
+    {
+    return "LittleEndian";
+    }
+#endif
+}
+
+
+//----------------------------------------------------------------------------
+void vtkImageReader2::PrintSelf(ostream& os, vtkIndent indent)
+{
+  int idx;
+  
+  vtkImageSource::PrintSelf(os,indent);
+
+  // this->File, this->Colors need not be printed  
+  os << indent << "FileName: " <<
+    (this->FileName ? this->FileName : "(none)") << "\n";
+  os << indent << "FilePrefix: " << 
+    (this->FilePrefix ? this->FilePrefix : "(none)") << "\n";
+  os << indent << "FilePattern: " << 
+    (this->FilePattern ? this->FilePattern : "(none)") << "\n";
+
+  os << indent << "DataScalarType: " 
+     << vtkImageScalarTypeNameMacro(this->DataScalarType) << "\n";
+  os << indent << "NumberOfScalarComponents: " 
+     << this->NumberOfScalarComponents << "\n";
+ 
+  os << indent << "File Dimensionality: " << this->FileDimensionality << "\n";
+
+  os << indent << "File Lower Left: " << 
+    (this->FileLowerLeft ? "On\n" : "Off\n");
+
+  os << indent << "Swap Bytes: " << (this->SwapBytes ? "On\n" : "Off\n");
+
+  os << indent << "DataIncrements: (" << this->DataIncrements[0];
+  for (idx = 1; idx < 2; ++idx)
+    {
+    os << ", " << this->DataIncrements[idx];
+    }
+  os << ")\n";
+  
+  os << indent << "DataExtent: (" << this->DataExtent[0];
+  for (idx = 1; idx < 6; ++idx)
+    {
+    os << ", " << this->DataExtent[idx];
+    }
+  os << ")\n";
+  
+  os << indent << "DataSpacing: (" << this->DataSpacing[0];
+  for (idx = 1; idx < 3; ++idx)
+    {
+    os << ", " << this->DataSpacing[idx];
+    }
+  os << ")\n";
+  
+  os << indent << "DataOrigin: (" << this->DataOrigin[0];
+  for (idx = 1; idx < 3; ++idx)
+    {
+    os << ", " << this->DataOrigin[idx];
+    }
+  os << ")\n";
+  
+  os << indent << "HeaderSize: " << this->HeaderSize << "\n";
+
+  if ( this->InternalFileName )
+    {
+    os << indent << "Internal File Name: " << this->InternalFileName << "\n";
+    }
+  else
+    {
+    os << indent << "Internal File Name: (none)\n";
+    }
+}
+
+
+//----------------------------------------------------------------------------
+// This method returns the largest data that can be generated.
+void vtkImageReader2::ExecuteInformation()
+{
+  vtkImageData *output = this->GetOutput();
+  
+  output->SetWholeExtent(this->DataExtent);
+  output->SetSpacing(this->DataSpacing);
+  output->SetOrigin(this->DataOrigin);
+
+  output->SetScalarType(this->DataScalarType);
+  output->SetNumberOfScalarComponents(this->NumberOfScalarComponents);
+}
+
+
+//----------------------------------------------------------------------------
+// Manual initialization.
+void vtkImageReader2::SetHeaderSize(int size)
+{
+  if (size != this->HeaderSize)
+    {
+    this->HeaderSize = size;
+    this->Modified();
+    }
+  this->ManualHeaderSize = 1;
+}
+  
+
+//----------------------------------------------------------------------------
+// This function opens a file to determine the file size, and to
+// automatically determine the header size.
+void vtkImageReader2::ComputeDataIncrements()
+{
+  int idx;
+  unsigned long fileDataLength;
+  
+  // Determine the expected length of the data ...
+  switch (this->DataScalarType)
+    {
+    case VTK_FLOAT:
+      fileDataLength = sizeof(float);
+      break;
+    case VTK_DOUBLE:
+      fileDataLength = sizeof(double);
+      break;
+    case VTK_INT:
+      fileDataLength = sizeof(int);
+      break;
+    case VTK_SHORT:
+      fileDataLength = sizeof(short);
+      break;
+    case VTK_UNSIGNED_SHORT:
+      fileDataLength = sizeof(unsigned short);
+      break;
+    case VTK_UNSIGNED_CHAR:
+      fileDataLength = sizeof(unsigned char);
+      break;
+    default:
+      vtkErrorMacro(<< "Unknown DataScalarType");
+      return;
+    }
+
+  fileDataLength *= this->NumberOfScalarComponents;
+  
+  // compute the fileDataLength (in units of bytes)
+  for (idx = 0; idx < 3; ++idx)
+    {
+    this->DataIncrements[idx] = fileDataLength;
+    fileDataLength = fileDataLength *
+      (this->DataExtent[idx*2+1] - this->DataExtent[idx*2] + 1);
+    }
+  this->DataIncrements[3] = fileDataLength;
+}
+
+
+void vtkImageReader2::OpenFile()
+{
+  if (!this->FileName && !this->FilePattern)
+    {
+    vtkErrorMacro(<<"Either a FileName or FilePattern must be specified.");
+    return;
+    }
+
+  // Close file from any previous image
+  if (this->File)
+    {
+    this->File->close();
+    delete this->File;
+    this->File = NULL;
+    }
+  
+  // Open the new file
+  vtkDebugMacro(<< "Initialize: opening file " << this->InternalFileName);
+#ifdef _WIN32
+  this->File = new ifstream(this->InternalFileName, ios::in | ios::binary);
+#else
+  this->File = new ifstream(this->InternalFileName, ios::in);
+#endif
+  if (! this->File || this->File->fail())
+    {
+    vtkErrorMacro(<< "Initialize: Could not open file " << 
+    this->InternalFileName);
+    return;
+    }
+}
+
+
+int vtkImageReader2::GetHeaderSize()
+{
+  return this->GetHeaderSize(this->DataExtent[4]);
+}
+
+int vtkImageReader2::GetHeaderSize(int idx)
+{
+  if (!this->FileName && !this->FilePattern)
+    {
+    vtkErrorMacro(<<"Either a FileName or FilePattern must be specified.");
+    return 0;
+    }
+  if ( ! this->ManualHeaderSize)
+    {
+    this->ComputeDataIncrements();
+
+    // make sure we figure out a filename to open
+    this->ComputeInternalFileName(idx);
+    this->OpenFile();
+    
+    // Get the size of the header from the size of the image
+    this->File->seekg(0,ios::end);
+    
+    return (int)(this->File->tellg() - 
+      (long)this->DataIncrements[this->GetFileDimensionality()]);
+    }
+  
+  return this->HeaderSize;
+}
+
+void vtkImageReader2::SeekFile(int i, int j, int k)
+{
+  unsigned long streamStart;
+
+  // convert data extent into constants that can be used to seek.
+  streamStart = 
+    (i - this->DataExtent[0]) * this->DataIncrements[0];
+  
+  if (this->FileLowerLeft)
+    {
+    streamStart = streamStart + 
+      (j - this->DataExtent[2]) * this->DataIncrements[1];
+    }
+  else
+    {
+    streamStart = streamStart + 
+      (this->DataExtent[3] - this->DataExtent[2] - j) * 
+      this->DataIncrements[1];
+    }
+  
+  // handle three and four dimensional files
+  if (this->GetFileDimensionality() >= 3)
+    {
+    streamStart = streamStart + 
+      (k - this->DataExtent[4]) * this->DataIncrements[2];
+    }
+  
+  streamStart += this->GetHeaderSize(k);
+  
+  // error checking
+  this->File->seekg((long)streamStart, ios::beg);
+  if (this->File->fail())
+    {
+    vtkWarningMacro("File operation failed.");
+    return;
+    }
+}
+
+//----------------------------------------------------------------------------
+// This function reads in one data of data.
+// templated to handle different data types.
+template <class OT>
+static void vtkImageReader2Update(vtkImageReader2 *self, vtkImageData *data,
+				  OT *outPtr)
+{
+  int outIncr[3];
+  OT *outPtr1, *outPtr2;
+  long streamRead;
+  int idx0, idx1, idx2, nComponents;
+  int outExtent[6];
+  int comp;
+  unsigned long count = 0;
+  unsigned long target;
+  
+  // Get the requested extents and increments
+  data->GetExtent(outExtent);
+  data->GetIncrements(outIncr);
+  nComponents = data->GetNumberOfScalarComponents();
+  
+  // length of a row, num pixels read at a time
+  int pixelRead = outExtent[1] - outExtent[0] + 1; 
+  streamRead = (long)(pixelRead*nComponents*sizeof(OT));  
+  
+  // create a buffer to hold a row of the data
+  target = (unsigned long)((outExtent[5]-outExtent[4]+1)*
+			   (outExtent[3]-outExtent[2]+1)/50.0);
+  target++;
+
+  // read the data row by row
+  if (self->GetFileDimensionality() == 3)
+    {
+    self->ComputeInternalFileName(0);
+    self->OpenFile();
+    }
+  outPtr2 = outPtr;
+  for (idx2 = outExtent[4]; idx2 <= outExtent[5]; ++idx2)
+    {
+    if (self->GetFileDimensionality() == 2)
+      {
+      self->ComputeInternalFileName(idx2);
+      self->OpenFile();
+      }
+    outPtr1 = outPtr2;
+    for (idx1 = outExtent[2]; 
+	 !self->AbortExecute && idx1 <= outExtent[3]; ++idx1)
+      {
+      if (!(count%target))
+	{
+	self->UpdateProgress(count/(50.0*target));
+	}
+      count++;
+      
+      // seek to the correct row
+      self->SeekFile(outExtent[0],idx1,idx2);
+      // read the row.
+      if ( !self->GetFile()->read((char *)outPtr1, streamRead))
+	{
+	vtkGenericWarningMacro("File operation failed. row = " << idx1
+			       << ", Read = " << streamRead
+			       << ", FilePos = " << self->GetFile()->tellg());
+	return;
+	}
+      // handle swapping
+      if (self->GetSwapBytes() && sizeof(OT) > 1)
+	{
+	vtkByteSwap::SwapVoidRange(outPtr1, pixelRead*nComponents, sizeof(OT));
+	}
+      outPtr1 += outIncr[1];
+      }
+    // move to the next image in the file and data
+    outPtr2 += outIncr[2];
+    }
+}
+
+//----------------------------------------------------------------------------
+// This function reads a data from a file.  The datas extent/axes
+// are assumed to be the same as the file extent/order.
+void vtkImageReader2::ExecuteData(vtkDataObject *output)
+{
+  vtkImageData *data = this->AllocateOutputData(output);
+  
+  void *ptr = NULL;
+  int *ext;
+  
+  if (!this->FileName && !this->FilePattern)
+    {
+    vtkErrorMacro(<<"Either a FileName or FilePattern must be specified.");
+    return;
+    }
+
+  ext = data->GetExtent();
+
+  vtkDebugMacro("Reading extent: " << ext[0] << ", " << ext[1] << ", " 
+	<< ext[2] << ", " << ext[3] << ", " << ext[4] << ", " << ext[5]);
+  
+  this->ComputeDataIncrements();
+  
+  // Call the correct templated function for the output
+  ptr = data->GetScalarPointer();
+  switch (this->GetDataScalarType())
+    {
+    vtkTemplateMacro3(vtkImageReader2Update, this, data, (VTK_TT *)(ptr));
+    default:
+      vtkErrorMacro(<< "UpdateFromFile: Unknown data type");
+    }   
+}
+
+
+//----------------------------------------------------------------------------
+// Set the data type of pixles in the file.  
+// If you want the output scalar type to have a different value, set it
+// after this method is called.
+void vtkImageReader2::SetDataScalarType(int type)
+{
+  if (type == this->DataScalarType)
+    {
+    return;
+    }
+  
+  this->Modified();
+  this->DataScalarType = type;
+  // Set the default output scalar type
+  this->GetOutput()->SetScalarType(this->DataScalarType);
+}
+
---
+++ imaging/vtkImageReader2.h
@@ -0,0 +1,213 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkImageReader2.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkImageReader2 - Superclass of binary file readers.
+// .SECTION Description
+// vtkImageReader2 is a slightly simplified version of vtkImageReader, it 
+// is a good superclass for streaming readers 
+
+// .SECTION See Also
+// vtkBMPReader vtkPNMReader vtkTIFFReader
+
+#ifndef __vtkImageReader2_h
+#define __vtkImageReader2_h
+
+#include "vtkImageSource.h"
+
+#define VTK_FILE_BYTE_ORDER_BIG_ENDIAN 0
+#define VTK_FILE_BYTE_ORDER_LITTLE_ENDIAN 1
+
+class VTK_EXPORT vtkImageReader2 : public vtkImageSource
+{
+public:
+  static vtkImageReader2 *New();
+  vtkTypeMacro(vtkImageReader2,vtkImageSource);
+  void PrintSelf(ostream& os, vtkIndent indent);   
+
+  // Description:
+  // Specify file name for the image file. You should specify either
+  // a FileName or a FilePrefix. Use FilePrefix if the data is stored 
+  // in multiple files.
+  void SetFileName(const char *);
+  vtkGetStringMacro(FileName);
+
+  // Description:
+  // Specify file prefix for the image file(s).You should specify either
+  // a FileName or FilePrefix. Use FilePrefix if the data is stored
+  // in multiple files.
+  void SetFilePrefix(const char *);
+  vtkGetStringMacro(FilePrefix);
+
+  // Description:
+  // The sprintf format used to build filename from FilePrefix and number.
+  void SetFilePattern(const char *);
+  vtkGetStringMacro(FilePattern);
+
+  // Description:
+  // Set the data type of pixels in the file.  
+  // If you want the output scalar type to have a different value, set it
+  // after this method is called.
+  void SetDataScalarType(int type);
+  void SetDataScalarTypeToFloat(){this->SetDataScalarType(VTK_FLOAT);}
+  void SetDataScalarTypeToDouble(){this->SetDataScalarType(VTK_DOUBLE);}
+  void SetDataScalarTypeToInt(){this->SetDataScalarType(VTK_INT);}
+  void SetDataScalarTypeToShort(){this->SetDataScalarType(VTK_SHORT);}
+  void SetDataScalarTypeToUnsignedShort()
+    {this->SetDataScalarType(VTK_UNSIGNED_SHORT);}
+  void SetDataScalarTypeToUnsignedChar()
+    {this->SetDataScalarType(VTK_UNSIGNED_CHAR);}
+
+  // Description:
+  // Get the file format.  Pixels are this type in the file.
+  vtkGetMacro(DataScalarType, int);
+
+  // Description:
+  // Set/Get the number of scalar components
+  vtkSetMacro(NumberOfScalarComponents,int);
+  vtkGetMacro(NumberOfScalarComponents,int);
+  
+  // Description:
+  // Get/Set the extent of the data on disk.  
+  vtkSetVector6Macro(DataExtent,int);
+  vtkGetVector6Macro(DataExtent,int);
+  
+  // Description:
+  // The number of dimensions stored in a file. This defaults to two.
+  vtkSetMacro(FileDimensionality, int);
+  int GetFileDimensionality() {return this->FileDimensionality;}
+  
+  // Description:
+  // Set/Get the spacing of the data in the file.
+  vtkSetVector3Macro(DataSpacing,float);
+  vtkGetVector3Macro(DataSpacing,float);
+  
+  // Description:
+  // Set/Get the origin of the data (location of first pixel in the file).
+  vtkSetVector3Macro(DataOrigin,float);
+  vtkGetVector3Macro(DataOrigin,float);
+
+  // Description:
+  // Get the size of the header computed by this object.
+  int GetHeaderSize();
+  int GetHeaderSize(int slice);
+
+  // Description:
+  // If there is a tail on the file, you want to explicitly set the
+  // header size.
+  void SetHeaderSize(int size);
+  
+  // Description:
+  // These methods should be used instead of the SwapBytes methods.
+  // They indicate the byte ordering of the file you are trying
+  // to read in. These methods will then either swap or not swap
+  // the bytes depending on the byte ordering of the machine it is
+  // being run on. For example, reading in a BigEndian file on a
+  // BigEndian machine will result in no swapping. Trying to read
+  // the same file on a LittleEndian machine will result in swapping.
+  // As a quick note most UNIX machines are BigEndian while PC's
+  // and VAX tend to be LittleEndian. So if the file you are reading
+  // in was generated on a VAX or PC, SetDataByteOrderToLittleEndian 
+  // otherwise SetDataByteOrderToBigEndian. 
+  void SetDataByteOrderToBigEndian();
+  void SetDataByteOrderToLittleEndian();
+  int GetDataByteOrder();
+  void SetDataByteOrder(int);
+  const char *GetDataByteOrderAsString();
+
+  // Description:
+  // Set/Get the byte swapping to explicitly swap the bytes of a file.
+  vtkSetMacro(SwapBytes,int);
+  int GetSwapBytes() {return this->SwapBytes;}
+  vtkBooleanMacro(SwapBytes,int);
+
+//BTX
+  ifstream *GetFile() {return this->File;}
+  vtkGetVectorMacro(DataIncrements,unsigned long,4);
+//ETX
+
+  void OpenFile();
+  void SeekFile(int i, int j, int k);
+
+  // Description:
+  // Set/Get whether the data comes from the file starting in the lower left
+  // corner or upper left corner.
+  vtkBooleanMacro(FileLowerLeft, int);
+  vtkGetMacro(FileLowerLeft, int);
+  vtkSetMacro(FileLowerLeft, int);
+
+  // Description:
+  // Set/Get the internal file name
+  void ComputeInternalFileName(int slice);
+  vtkGetStringMacro(InternalFileName);
+  
+protected:
+  vtkImageReader2();
+  ~vtkImageReader2();
+  vtkImageReader2(const vtkImageReader2&) {};
+  void operator=(const vtkImageReader2&) {};
+
+  char *InternalFileName;
+  char *FileName;
+  char *FilePrefix;
+  char *FilePattern;
+  int NumberOfScalarComponents;
+  int FileLowerLeft;
+
+  ifstream *File;
+  unsigned long DataIncrements[4];
+  int DataExtent[6];
+  int SwapBytes;
+
+  int FileDimensionality;
+  int HeaderSize;
+  int DataScalarType;
+  int ManualHeaderSize;
+  int Initialized;
+
+  int DataDimensions[3];
+  float DataSpacing[3];
+  float DataOrigin[3];
+
+  virtual void ExecuteInformation();
+  virtual void ExecuteData(vtkDataObject *data);
+  virtual void ComputeDataIncrements();
+};
+
+#endif
