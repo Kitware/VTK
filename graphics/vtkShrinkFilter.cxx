23b9a3c2e78aa158095075fbd59fdd098b9899a5
Jeff Lee <jeff@cdnorthamerica.com>
2001-03-17 17:42:53 -0500

    set progress correctly and avoid div by zero

--- graphics/vtkShrinkFilter.cxx
+++ graphics/vtkShrinkFilter.cxx
@@ -106,6 +106,8 @@ void vtkShrinkFilter::Execute()
   //
   tenth   = numCells / 10;
   decimal = 0.0;
+  if (tenth == 0)
+    tenth = 1;
 
   for (cellId=0; cellId < numCells; cellId++)
     {
@@ -113,17 +115,17 @@ void vtkShrinkFilter::Execute()
     numIds = ptIds->GetNumberOfIds();
 
     //abort/progress methods
-    if (tenth != 0) {
-      if (cellId % tenth == 0) 
-	{
-	  decimal += 0.1;
-	  this->UpdateProgress (decimal);
-	  if (this->GetAbortExecute())
-	    {
-	      break; //out of cell loop
-	    }
-	}
-    }
+  
+    if (cellId % tenth == 0) 
+      {
+	decimal += 0.1;
+	this->UpdateProgress (decimal);
+	if (this->GetAbortExecute())
+	  {
+	    break; //out of cell loop
+	  }
+      }
+
 
     // get the center of the cell
     center[0] = center[1] = center[2] = 0.0;
