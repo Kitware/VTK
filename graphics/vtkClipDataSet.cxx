8956901c61822c8b375514eeac39344742a4964c
Will Schroeder <will.schroeder@kitware.com>
2001-04-11 13:53:56 -0400

    ENH:Added vtkClipDataSet in preparation for 3D cell clipping

---
+++ graphics/vtkClipDataSet.cxx
@@ -0,0 +1,413 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkClipDataSet.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#include <math.h>
+#include "vtkClipDataSet.h"
+#include "vtkMergePoints.h"
+#include "vtkObjectFactory.h"
+
+//--------------------------------------------------------------------------
+vtkClipDataSet* vtkClipDataSet::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkClipDataSet");
+  if(ret)
+    {
+    return (vtkClipDataSet*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkClipDataSet;
+}
+
+//----------------------------------------------------------------------------
+// Construct with user-specified implicit function; InsideOut turned off; value
+// set to 0.0; and generate clip scalars turned off.
+vtkClipDataSet::vtkClipDataSet(vtkImplicitFunction *cf)
+{
+  this->ClipFunction = cf;
+  this->InsideOut = 0;
+  this->Locator = NULL;
+  this->Value = 0.0;
+  this->GenerateClipScalars = 0;
+
+  this->GenerateClippedOutput = 0;
+  this->vtkSource::SetNthOutput(1,vtkUnstructuredGrid::New());
+  this->Outputs[1]->Delete();
+}
+
+//----------------------------------------------------------------------------
+vtkClipDataSet::~vtkClipDataSet()
+{
+  if ( this->Locator )
+    {
+    this->Locator->UnRegister(this);
+    this->Locator = NULL;
+    }
+  this->SetClipFunction(NULL);
+}
+
+//----------------------------------------------------------------------------
+// Overload standard modified time function. If Clip functions is modified,
+// then this object is modified as well.
+unsigned long vtkClipDataSet::GetMTime()
+{
+  unsigned long mTime=this->vtkDataSetToUnstructuredGridFilter::GetMTime();
+  unsigned long time;
+
+  if ( this->ClipFunction != NULL )
+    {
+    time = this->ClipFunction->GetMTime();
+    mTime = ( time > mTime ? time : mTime );
+    }
+  if ( this->Locator != NULL )
+    {
+    time = this->Locator->GetMTime();
+    mTime = ( time > mTime ? time : mTime );
+    }
+
+  return mTime;
+}
+
+vtkUnstructuredGrid *vtkClipDataSet::GetClippedOutput()
+{
+  if (this->NumberOfOutputs < 2)
+    {
+    return NULL;
+    }
+  
+  return (vtkUnstructuredGrid *)(this->Outputs[1]);
+}
+
+
+//----------------------------------------------------------------------------
+//
+// Clip through data generating surface.
+//
+void vtkClipDataSet::Execute()
+{
+  vtkDataSet *input = this->GetInput();
+  vtkUnstructuredGrid *output = this->GetOutput();
+  vtkUnstructuredGrid *clippedOutput = this->GetClippedOutput();
+  int numPts = input->GetNumberOfPoints();
+  int numCells = input->GetNumberOfCells();
+  vtkPointData *inPD=input->GetPointData(), *outPD = output->GetPointData();
+  vtkCellData *inCD=input->GetCellData();
+  vtkCellData *outCD[2];
+  vtkPoints *newPoints;
+  vtkScalars *cellScalars; 
+  vtkScalars *clipScalars;
+  vtkPoints *cellPts;
+  vtkIdList *cellIds;
+  float s;
+  int npts, *pts;
+  int cellType;
+  int i, j;
+  int estimatedSize;
+  vtkUnsignedCharArray *types[2];
+  vtkIntArray *locs[2];
+  
+ 
+  vtkDebugMacro(<< "Clipping dataset");
+  
+  // Initialize self; create output objects
+  //
+  if ( numPts < 1 )
+    {
+    //vtkErrorMacro(<<"No data to clip");
+    return;
+    }
+
+  if ( !this->ClipFunction && this->GenerateClipScalars )
+    {
+    vtkErrorMacro(<<"Cannot generate clip scalars if no clip function defined");
+    return;
+    }
+
+  // allocate the output and associated helper classes
+  estimatedSize = numCells;
+  estimatedSize = estimatedSize / 1024 * 1024; //multiple of 1024
+  if (estimatedSize < 1024)
+    {
+    estimatedSize = 1024;
+    }
+  cellScalars = vtkScalars::New();
+  cellScalars->Allocate(VTK_CELL_SIZE);
+  vtkCellArray *conn[2];
+  conn[0] = vtkCellArray::New();
+  conn[0]->Allocate(estimatedSize,estimatedSize/2);
+  conn[0]->InitTraversal();
+  types[0] = vtkUnsignedCharArray::New();
+  types[0]->Allocate(estimatedSize,estimatedSize/2);
+  locs[0] = vtkIntArray::New();
+  locs[0]->Allocate(estimatedSize,estimatedSize/2);
+  if ( this->GenerateClippedOutput )
+    {
+    conn[1] = vtkCellArray::New();
+    conn[1]->Allocate(estimatedSize,estimatedSize/2);
+    conn[1]->InitTraversal();
+    types[1] = vtkUnsignedCharArray::New();
+    types[1]->Allocate(estimatedSize,estimatedSize/2);
+    locs[1] = vtkIntArray::New();
+    locs[1]->Allocate(estimatedSize,estimatedSize/2);
+    }
+  newPoints = vtkPoints::New();
+  newPoints->Allocate(numPts,numPts/2);
+  
+  // locator used to merge potentially duplicate points
+  if ( this->Locator == NULL )
+    {
+    this->CreateDefaultLocator();
+    }
+  this->Locator->InitPointInsertion (newPoints, input->GetBounds());
+
+  // Determine whether we're clipping with input scalars or a clip function
+  // and do necessary setup.
+  if ( this->ClipFunction )
+    {
+    vtkScalars *tmpScalars = vtkScalars::New();
+    tmpScalars->SetNumberOfScalars(numPts);
+    inPD = vtkPointData::New();
+    inPD->ShallowCopy(input->GetPointData());//copies original
+    if ( this->GenerateClipScalars )
+      {
+      inPD->SetScalars(tmpScalars);
+      }
+    for ( i=0; i < numPts; i++ )
+      {
+      s = this->ClipFunction->FunctionValue(input->GetPoint(i));
+      tmpScalars->SetScalar(i,s);
+      }
+    clipScalars = (vtkScalars *)tmpScalars;
+    }
+  else //using input scalars
+    {
+    clipScalars = inPD->GetScalars();
+    if ( !clipScalars )
+      {
+      vtkErrorMacro(<<"Cannot clip without clip function or input scalars");
+      return;
+      }
+    }
+    
+  if ( !this->GenerateClipScalars && !input->GetPointData()->GetScalars())
+    {
+    outPD->CopyScalarsOff();
+    }
+  else
+    {
+    outPD->CopyScalarsOn();
+    }
+  outPD->InterpolateAllocate(inPD,estimatedSize,estimatedSize/2);
+  outCD[0] = output->GetCellData();
+  outCD[0]->CopyAllocate(inCD,estimatedSize,estimatedSize/2);
+  if ( this->GenerateClippedOutput )
+    {
+    outCD[1] = clippedOutput->GetCellData();
+    outCD[1]->CopyAllocate(inCD,estimatedSize,estimatedSize/2);
+    }
+
+  //Process all cells and clip each in turn
+  //
+  int abort=0;
+  int updateTime = numCells/20 + 1;  // update roughly every 5%
+  vtkGenericCell *cell = vtkGenericCell::New();
+  int num[2]; num[0]=num[1]=0;
+  int numNew[2]; numNew[0]=numNew[1]=0;
+  for (int cellId=0; cellId < numCells && !abort; cellId++)
+    {
+    if ( !(cellId % updateTime) )
+      {
+      this->UpdateProgress((float)cellId / numCells);
+      abort = this->GetAbortExecute();
+      }
+
+    input->GetCell(cellId,cell);
+    cellPts = cell->GetPoints();
+    cellIds = cell->GetPointIds();
+    npts = cellPts->GetNumberOfPoints();
+
+    // evaluate implicit cutting function
+    for ( i=0; i < npts; i++ )
+      {
+      s = clipScalars->GetScalar(cellIds->GetId(i));
+      cellScalars->InsertScalar(i, s);
+      }
+
+    // perform the clipping
+    cell->Clip(this->Value, cellScalars, this->Locator, conn[0],
+               inPD, outPD, inCD, cellId, outCD[0], this->InsideOut);
+    numNew[0] = conn[0]->GetNumberOfCells() - num[0];
+    num[0] = conn[0]->GetNumberOfCells();
+
+    if ( this->GenerateClippedOutput )
+      {
+      cell->Clip(this->Value, cellScalars, this->Locator, conn[1],
+                 inPD, outPD, inCD, cellId, outCD[1], !this->InsideOut);
+      numNew[1] = conn[1]->GetNumberOfCells() - num[1];
+      num[1] = conn[1]->GetNumberOfCells();
+      }
+
+    for (i=0; i<2; i++) //for both outputs
+      {
+      for (j=0; j < numNew[i]; j++) 
+        {
+        locs[i]->InsertNextValue(conn[i]->GetTraversalLocation());
+        conn[i]->GetNextCell(npts,pts);
+        
+        //For each new cell added, got to set the type of the cell
+        switch ( cell->GetCellDimension() )
+          {
+          case 0: //points are generated-------------------------------
+            cellType = (npts > 1 ? VTK_POLY_VERTEX : VTK_VERTEX);
+            break;
+
+          case 1: //lines are generated----------------------------------
+            cellType = (npts > 2 ? VTK_POLY_LINE : VTK_LINE);
+            break;
+
+          case 2: //polygons are generated------------------------------
+            cellType = (npts == 3 ? VTK_TRIANGLE : 
+                        (npts == 4 ? VTK_QUAD : VTK_POLYGON));
+            break;
+
+          case 3: //tetrahedra are generated------------------------------
+            cellType = VTK_TETRA;
+            break;
+          } //switch
+        types[i]->InsertNextValue(cellType);
+        } //for each new cell
+      } //for both outputs
+    } //for each cell
+
+  cell->Delete();
+  cellScalars->Delete();
+
+  if ( this->ClipFunction ) 
+    {
+    clipScalars->Delete();
+    inPD->Delete();
+    }
+  
+  output->SetPoints(newPoints);
+  output->SetCells(types[0], locs[0], conn[0]);
+  conn[0]->Delete();
+  types[0]->Delete();
+  locs[0]->Delete();
+
+  if ( this->GenerateClippedOutput )
+    {
+    clippedOutput->SetPoints(newPoints);
+    clippedOutput->SetCells(types[1], locs[1], conn[1]);
+    conn[1]->Delete();
+    types[1]->Delete();
+    locs[1]->Delete();
+    }
+  
+  newPoints->Delete();
+  this->Locator->Initialize();//release any extra memory
+  output->Squeeze();
+}
+
+//----------------------------------------------------------------------------
+// Specify a spatial locator for merging points. By default, 
+// an instance of vtkMergePoints is used.
+void vtkClipDataSet::SetLocator(vtkPointLocator *locator)
+{
+  if ( this->Locator == locator)
+    {
+    return;
+    }
+  
+  if ( this->Locator )
+    {
+    this->Locator->UnRegister(this);
+    this->Locator = NULL;
+    }
+
+  if ( locator )
+    {
+    locator->Register(this);
+    }
+
+  this->Locator = locator;
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+void vtkClipDataSet::CreateDefaultLocator()
+{
+  if ( this->Locator == NULL )
+    {
+    this->Locator = vtkMergePoints::New();
+    }
+}
+
+
+//----------------------------------------------------------------------------
+void vtkClipDataSet::PrintSelf(ostream& os, vtkIndent indent)
+{
+  vtkDataSetToUnstructuredGridFilter::PrintSelf(os,indent);
+
+  if ( this->ClipFunction )
+    {
+    os << indent << "Clip Function: " << this->ClipFunction << "\n";
+    }
+  else
+    {
+    os << indent << "Clip Function: (none)\n";
+    }
+  os << indent << "InsideOut: " << (this->InsideOut ? "On\n" : "Off\n");
+  os << indent << "Value: " << this->Value << "\n";
+  if ( this->Locator )
+    {
+    os << indent << "Locator: " << this->Locator << "\n";
+    }
+  else
+    {
+    os << indent << "Locator: (none)\n";
+    }
+
+  os << indent << "Generate Clip Scalars: " 
+     << (this->GenerateClipScalars ? "On\n" : "Off\n");
+
+  os << indent << "Generate Clipped Output: " 
+     << (this->GenerateClippedOutput ? "On\n" : "Off\n");
+}
---
+++ graphics/vtkClipDataSet.h
@@ -0,0 +1,178 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkClipDataSet.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkClipDataSet - clip any dataset with user-specified implicit function or input scalar data
+// .SECTION Description
+// vtkClipDataSet is a filter that any type of dataset using either
+// any subclass of vtkImplicitFunction, or the input scalar
+// data. Clipping means that it actually "cuts" through the cells of
+// the dataset, returning everything inside of the specified implicit
+// function (or greater than the scalar value) including "pieces" of
+// a cell. (Compare this with vtkExtractGeometry, which pulls out
+// entire, uncut cells.) The output of this filter is an unstructured
+// grid.
+//
+// To use this filter, you must decide if you will be clipping with an
+// implicit function, or whether you will be using the input scalar
+// data.  If you want to clip with an implicit function, you must:
+// 1) define an implicit function
+// 2) set it with the SetClipFunction method
+// 3) apply the GenerateClipScalarsOn method
+// If a ClipFunction is not specified, or GenerateClipScalars is off
+// (the default), then the input's scalar data will be used to clip
+// the polydata.
+//
+// You can also specify a scalar value, which is used to decide what is
+// inside and outside of the implicit function. You can also reverse the
+// sense of what inside/outside is by setting the InsideOut instance
+// variable. (The clipping algorithm proceeds by computing an implicit
+// function value or using the input scalar data for each point in the
+// dataset. This is compared to the scalar value to determine
+// inside/outside.)
+//
+// This filter can be configured to compute a second output. The
+// second output is the part of the cell that is clipped away. Set the
+// GenerateClippedData boolean on if you wish to access this output data.
+
+// .SECTION Caveats
+// vtkClipDataSet will triangulate all types of 3D cells (i.e., create
+// tetrahedra). This is true even if the cell is not actually cut. This
+// is necessary to preserve compatibility across face neighbors. 2D cells
+// will only be triangulated if the cutting function passes through them.
+
+// .SECTION See Also
+// vtkImplicitFunction vtkCutter vtkClipVolume vtkClipPolyData
+
+#ifndef __vtkClipDataSet_h
+#define __vtkClipDataSet_h
+
+#include "vtkDataSetToUnstructuredGridFilter.h"
+#include "vtkImplicitFunction.h"
+
+class VTK_EXPORT vtkClipDataSet : public vtkDataSetToUnstructuredGridFilter
+{
+public:
+  vtkTypeMacro(vtkClipDataSet,vtkDataSetToUnstructuredGridFilter);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Construct with user-specified implicit function; InsideOut turned off;
+  // value set to 0.0; and generate clip scalars turned off.
+  static vtkClipDataSet *New();
+
+  // Description:
+  // Set the clipping value of the implicit function (if clipping with
+  // implicit function) or scalar value (if clipping with
+  // scalars). The default value is 0.0. 
+  vtkSetMacro(Value,float);
+  vtkGetMacro(Value,float);
+  
+  // Description:
+  // Set/Get the InsideOut flag. When off, a vertex is considered
+  // inside the implicit function if its value is greater than the
+  // Value ivar. When InsideOutside is turned on, a vertex is
+  // considered inside the implicit function if its implicit function
+  // value is less than or equal to the Value ivar.  InsideOut is off
+  // by default.
+  vtkSetMacro(InsideOut,int);
+  vtkGetMacro(InsideOut,int);
+  vtkBooleanMacro(InsideOut,int);
+
+  // Description
+  // Specify the implicit function with which to perform the
+  // clipping. If you do not define an implicit function, then the input
+  // scalar data will be used for clipping.
+  vtkSetObjectMacro(ClipFunction,vtkImplicitFunction);
+  vtkGetObjectMacro(ClipFunction,vtkImplicitFunction);
+
+  // Description:
+  // If this flag is enabled, then the output scalar values will be 
+  // interpolated from the implicit function values, and not the 
+  // input scalar data. If you enable this flag but do not provide an
+  // implicit function an error will be reported.
+  vtkSetMacro(GenerateClipScalars,int);
+  vtkGetMacro(GenerateClipScalars,int);
+  vtkBooleanMacro(GenerateClipScalars,int);
+
+  // Description:
+  // Control whether a second output is generated. The second output
+  // contains the polygonal data that's been clipped away.
+  vtkSetMacro(GenerateClippedOutput,int);
+  vtkGetMacro(GenerateClippedOutput,int);
+  vtkBooleanMacro(GenerateClippedOutput,int);
+
+  // Description:
+  // Return the Clipped output.
+  vtkUnstructuredGrid *GetClippedOutput();
+
+  // Description:
+  // Specify a spatial locator for merging points. By default, an
+  // instance of vtkMergePoints is used.
+  void SetLocator(vtkPointLocator *locator);
+  vtkGetObjectMacro(Locator,vtkPointLocator);
+
+  // Description:
+  // Create default locator. Used to create one when none is specified. The 
+  // locator is used to merge coincident points.
+  void CreateDefaultLocator();
+
+  // Description:
+  // Return the mtime also considering the locator and clip function.
+  unsigned long GetMTime();
+
+protected:
+  vtkClipDataSet(vtkImplicitFunction *cf=NULL);
+  ~vtkClipDataSet();
+  vtkClipDataSet(const vtkClipDataSet&) {}
+  void operator=(const vtkClipDataSet&) {}
+
+  void Execute();
+  vtkImplicitFunction *ClipFunction;
+  
+  vtkPointLocator *Locator;
+  int InsideOut;
+  float Value;
+  int GenerateClipScalars;
+
+  int GenerateClippedOutput;
+};
+
+#endif
