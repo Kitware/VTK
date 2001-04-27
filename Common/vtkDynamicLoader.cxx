9f0b153ca1fde05c8c924c21e8b489074dea0e1f
Yves Starreveld <ystarrev@uwo.ca>
2001-04-27 13:38:06 -0400

    ENH: dynamic loader for OSX

--- Common/vtkDynamicLoader.cxx
+++ Common/vtkDynamicLoader.cxx
@@ -1,7 +1,7 @@
 #include "vtkDynamicLoader.h"
 // This file is actually 3 different implementations.
 // 1. HP machines which uses shl_load
-// 2. Power PC MAC which uses GetSharedLibrary
+// 2. Apple OSX which uses NSLinkModule
 // 3. Windows which uses LoadLibrary
 // 4. Most unix systems which use dlopen (default )
 // Each part of the ifdef contains a complete implementation for
@@ -51,27 +51,21 @@ const char* vtkDynamicLoader::LastError()
 #endif
 
 
+
 // ---------------------------------------------------------------
-// 2. Implementation for the Power PC (MAC)
-#ifdef __powerc
-#define VTKDYNAMICLOADER_DEFINED 1
-#include <CodeFragments.h>
-#include <Errors.h>
-#include <Threads.h>
-#include <Timer.h>
+// 2. Implementation for Darwin (OSX) Machines
+
+#ifdef DARWIN
+#define VTKDYNAMICLOADER_DEFINED
+#include <mach-o/dyld.h>
 
 vtkLibHandle vtkDynamicLoader::OpenLibrary(const char* libname )
 {
-    Str63 libName;
-    libName[0] = strlen(libname);
-    strcpy((char*) &libName[1], libname);
-    ConnectionID connID;
-    Ptr mainAddr;
-    Str255 errName;
-    OSErr err = GetSharedLibrary(
-        libName, kPowerPCArch, kLoadLib, &connID, &mainAddr, errName
-    );
-    return err == fragNoErr ? connID : 0;
+  NSObjectFileImageReturnCode rc;
+  NSObjectFileImage image;
+
+  rc = NSCreateObjectFileImageFromFile(libname, &image);
+  return NSLinkModule(image, libname, TRUE);
 }
 
 int vtkDynamicLoader::CloseLibrary(vtkLibHandle lib)
@@ -80,31 +74,39 @@ int vtkDynamicLoader::CloseLibrary(vtkLibHandle lib)
 }
 
 void* vtkDynamicLoader::GetSymbolAddress(vtkLibHandle lib, const char* sym)
-{ 
-  Str255 symName;
-  symName[0] = strlen(sym);
-  strcpy((char*) &symName[1], sym);
-  Ptr symAddr;
-  SymClass symClass;
-  OSErr err = FindSymbol(lib, symName, &symAddr, &symClass);
-  return (void*) symAddr; 
+{
+    void *result=0;
+    if(NSIsSymbolNameDefined(sym)){
+         cout << sym << " is defined!" << endl;
+         NSSymbol symbol= NSLookupAndBindSymbol(sym);
+         if(symbol){
+                result = NSAddressOfSymbol(symbol);
+         }
+  }else{
+        cout << sym << " is not defined!" << endl;
+ }
+  return result;
 }
 
 const char* vtkDynamicLoader::LibPrefix()
-{ 
+{
   return "";
 }
 
 const char* vtkDynamicLoader::LibExtension()
 {
-  return ".lib";
+  return ".dylib";
 }
 
 const char* vtkDynamicLoader::LastError()
 {
   return 0;
 }
+
 #endif
+
+
+
 
 // ---------------------------------------------------------------
 // 3. Implementation for Windows win32 code
