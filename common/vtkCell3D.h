069dea4c4d26c514cda3dc480f7ddb167a841be7
Will Schroeder <will.schroeder@kitware.com>
2001-04-30 16:24:39 -0400

    ENH:Rearranged documentation

--- common/vtkCell3D.h
+++ common/vtkCell3D.h
@@ -41,9 +41,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 =========================================================================*/
 // .NAME vtkCell3D - abstract class to specify 3D cell interface
 // .SECTION Description
-// vtkCell3D is an abstract class that extends the interfaces for 3D data cells,
-// and implements methods needed to satisfy the vtkCell API. The 3D cells 
-// include hexehedra, tetrahedra, wedge, pyramid, and voxel.
+// vtkCell3D is an abstract class that extends the interfaces for 3D data 
+// cells, and implements methods needed to satisfy the vtkCell API. The 
+// 3D cells include hexehedra, tetrahedra, wedge, pyramid, and voxel.
 
 // .SECTION See Also
 // vtkTetra vtkHexahedron vtkVoxel vtkWedge vtkPyramid
@@ -61,10 +61,10 @@ public:
 
   // Description:
   // Get the pair of vertices that define an edge. The method returns the
-  // number of vertices, along with an array of vertices. Note that the vertices
-  // are 0-offset; that is, they refer to the ids of the cell, not the
-  // point ids of the mesh that the cell belongs to. The edgeId must range
-  // between 0<=edgeId<this->GetNumberOfEdges().
+  // number of vertices, along with an array of vertices. Note that the
+  // vertices are 0-offset; that is, they refer to the ids of the cell, not
+  // the point ids of the mesh that the cell belongs to. The edgeId must
+  // range between 0<=edgeId<this->GetNumberOfEdges().
   virtual void GetEdgePoints(int edgeId, int* &pts) = 0;
   
   // Description:
@@ -76,17 +76,16 @@ public:
   virtual void GetFacePoints(int faceId, int* &pts) = 0;
 
   // Description:
-  // Cut (or clip) the cell based on the input cellScalars and the
-  // specified value. The output of the clip operation will be one or
-  // more cells of the same topological dimension as the original cell. 
-  // The flag insideOut controls what part of the cell is considered inside - 
-  // normally cell points whose scalar value is greater than "value" are
-  // considered inside. If insideOut is on, this is reversed. Also, if the 
-  // output cell data is non-NULL, the cell data from the clipped cell is 
-  // passed to the generated contouring primitives. (Note: the CopyAllocate() 
-  // method must be invoked on both the output cell and point data. The
-  // cellId refers to the cell from which the cell data is copied.)
-  // (Satisfies vtkCell API.)
+  // Cut (or clip) the cell based on the input cellScalars and the specified
+  // value. The output of the clip operation will be one or more cells of the
+  // same topological dimension as the original cell.  The flag insideOut
+  // controls what part of the cell is considered inside - normally cell
+  // points whose scalar value is greater than "value" are considered
+  // inside. If insideOut is on, this is reversed. Also, if the output cell
+  // data is non-NULL, the cell data from the clipped cell is passed to the
+  // generated contouring primitives. (Note: the CopyAllocate() method must
+  // be invoked on both the output cell and point data. The cellId refers to
+  // the cell from which the cell data is copied.)  (Satisfies vtkCell API.)
   virtual void Clip(float value, vtkScalars *cellScalars, 
                     vtkPointLocator *locator, vtkCellArray *connectivity,
                     vtkPointData *inPd, vtkPointData *outPd,
