b81dced7c15b43271d189d6fa8d7445d2093c137
Berk Geveci <berk.geveci@kitware.com>
2001-05-08 10:12:50 -0400

    Adding event logging with mpe.

---
+++ Parallel/vtkMPIEventLog.cxx
@@ -0,0 +1,136 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkMPIEventLog.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+
+#include "vtkMPIEventLog.h"
+#include "vtkMPIController.h"
+#include "vtkObjectFactory.h"
+#include "mpe.h"
+
+int vtkMPIEventLog::LastEventId = 0;
+
+vtkMPIEventLog* vtkMPIEventLog::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkMPIEventLog");
+  if(ret)
+    {
+    return (vtkMPIEventLog*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkMPIEventLog;
+}
+
+void vtkMPIEventLog::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->vtkObject::PrintSelf(os,indent);
+}
+
+vtkMPIEventLog::vtkMPIEventLog()
+{
+
+  this->Active = 0;
+  
+}
+
+void vtkMPIEventLog::InitializeLogging()
+{
+  MPE_Init_log();
+}
+
+void vtkMPIEventLog::FinalizeLogging(const char* fname)
+{
+  MPE_Finish_log(const_cast<char*>(fname));
+}
+
+int vtkMPIEventLog::SetDescription(const char* name, const char* desc)
+{
+  // Trying to make sure that the object is created on all
+  // processes
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  int err, processId;
+  if ( (err = MPI_Comm_rank(MPI_COMM_WORLD,&processId)) 
+       != MPI_SUCCESS)
+    {
+    char *msg = vtkMPIController::ErrorString(err);
+    vtkErrorMacro("MPI error occured: " << msg);
+    delete[] msg;
+    return 0;
+    }
+
+  this->Active = 1;
+  this->Id = LastEventId;
+  if (processId == 0)
+    {
+    MPE_Describe_state(this->Id, this->Id+1, const_cast<char*>(name), 
+		       const_cast<char*>(desc));
+    }
+
+  LastEventId += 2;
+  return 1;
+} 
+
+void vtkMPIEventLog::StartLogging()
+{
+  if (!this->Active)
+    {
+    vtkWarningMacro("This vtkMPIEventLog has not been initialized. Can not log event.");
+    return;
+    }
+
+  MPE_Log_event(this->Id, 0, "begin");
+}
+
+void vtkMPIEventLog::StopLogging()
+{
+  if (!this->Active)
+    {
+    vtkWarningMacro("This vtkMPIEventLog has not been initialized. Can not log event.");
+    return;
+    }
+  MPE_Log_event(this->Id+1, 0, "end");
+}
+
+vtkMPIEventLog::~vtkMPIEventLog()
+{
+}
+
---
+++ Parallel/vtkMPIEventLog.h
@@ -0,0 +1,108 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkMPIEventLog.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkMPIEventLog - Class for creating MPI groups.
+
+// .SECTION Description
+
+// .SECTION See Also
+
+#ifndef __vtkMPIEventLog_h
+#define __vtkMPIEventLog_h
+
+#include "vtkObject.h"
+#include "vtkMPIController.h"
+
+class VTK_EXPORT vtkMPIEventLog : public vtkObject
+{
+
+public:
+
+  vtkTypeMacro(vtkMPIEventLog,vtkObject);
+  
+  // Description:
+  // Construct a vtkMPIEventLog with the following initial state:
+  // Processes = 0, MaximumNumberOfProcesses = 0.
+  static vtkMPIEventLog* New();
+  
+  // Description:
+  // Used to initialize the underlying mpe event.
+  // HAS TO BE CALLED BY ALL PROCESSES before any event 
+  // logging is done.
+  // It takes a name and a description for the graphical
+  // representation, for example, "red:vlines3". See
+  // mpe documentation for details.
+  // Returns 0 on MPI failure (or aborts depending on
+  // MPI error handlers)
+  int SetDescription(const char* name, const char* desc);
+
+  // Description:
+  // These methods have to be called once on all processors
+  // before and after invoking any logging events.
+  // The name of the logfile is given by fileName.
+  // See mpe documentation for file formats.
+  static void InitializeLogging();
+  static void FinalizeLogging(const char* fileName);
+
+  // Description:
+  // Issue start and stop events for this log entry.
+  void StartLogging();
+  void StopLogging();
+
+  virtual void PrintSelf(ostream& os, vtkIndent indent);
+
+protected:
+
+  vtkMPIEventLog();
+  ~vtkMPIEventLog();
+  vtkMPIEventLog(const vtkMPIEventLog&) {};
+  void operator=(const vtkMPIEventLog&) {};
+
+  static int LastEventId;
+  int Active;
+  int Id;
+};
+
+#endif
+
+
+
+
