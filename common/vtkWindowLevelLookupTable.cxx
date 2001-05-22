16c97334a8685a71d6fda05c4440aebf4e777321
David Gobbi <david.gobbi@gmail.com>
2001-05-22 11:55:10 -0400

    ERR: fencepost-counting errors in the math ENH: SetMinimum/MaximumTableValue for consistency with vtkLookupTable      The table is now a ramp between the min/max table values, and      SetWindow/SetLevel work by adjusting the TableRange rather than      by changing the table values.

--- common/vtkWindowLevelLookupTable.cxx
+++ common/vtkWindowLevelLookupTable.cxx
@@ -44,7 +44,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 
-//------------------------------------------------------------------------------
+//----------------------------------------------------------------------------
 vtkWindowLevelLookupTable* vtkWindowLevelLookupTable::New()
 {
   // First try to create the object from the vtkObjectFactory
@@ -57,149 +57,145 @@ vtkWindowLevelLookupTable* vtkWindowLevelLookupTable::New()
   return new vtkWindowLevelLookupTable;
 }
 
-
-
-
-// Construct with range=(0,1); and hsv ranges set up for rainbow color table 
-// (from red to blue).
-vtkWindowLevelLookupTable::vtkWindowLevelLookupTable(int sze, int ext):
-vtkLookupTable(sze,ext)
+vtkWindowLevelLookupTable::vtkWindowLevelLookupTable(int sze, int ext)
+  : vtkLookupTable(sze, ext)
 {
-  this->NumberOfColors = sze;
-  this->Level = sze / 2.0;
-  this->Window = sze;
+  this->Level = (this->TableRange[0] + this->TableRange[1])/2;
+  this->Window = (this->TableRange[1] - this->TableRange[0]);
+
   this->InverseVideo = 0;
   
-  this->Table->Allocate(sze,ext);
+  this->MinimumTableValue[0] = 0.0f;
+  this->MinimumTableValue[1] = 0.0f;
+  this->MinimumTableValue[2] = 0.0f;
+  this->MinimumTableValue[3] = 1.0f;
 
-  this->MinimumColor[0] = this->MinimumColor[1] = this->MinimumColor[2] = 0;
+  this->MaximumTableValue[0] = 1.0f;
+  this->MaximumTableValue[1] = 1.0f;
+  this->MaximumTableValue[2] = 1.0f;
+  this->MaximumTableValue[3] = 1.0f;
+
+  this->MinimumColor[0] = 0;
+  this->MinimumColor[1] = 0;
+  this->MinimumColor[2] = 0;
   this->MinimumColor[3] = 255;
-  this->MaximumColor[0] = this->MaximumColor[1] = this->MaximumColor[2] = 255;
+
+  this->MaximumColor[0] = 255;
+  this->MaximumColor[1] = 255;
+  this->MaximumColor[2] = 255;
   this->MaximumColor[3] = 255;
 }
 
-// Generate lookup table from window and level.
-// Table is built as a linear ramp, centered at Level and of width Window.
+// Table is built as a linear ramp between MinimumTableValue and
+// MaximumTableValue.
 void vtkWindowLevelLookupTable::Build()
 {
-  float rampStartsAt, rampEndsAt;
-  float lowValue, highValue, incrValue;
-  int indxStartsAt, indxEndsAt;
-  int indx;
-  int i;
-  unsigned char *rgba;
-  unsigned char *minimum, *maximum;
-
-  if ( this->Table->GetNumberOfTuples() < 1 ||
-  (this->GetMTime() > this->BuildTime && this->InsertTime < this->BuildTime) )
+  if (this->Table->GetNumberOfTuples() < 1 ||
+      (this->GetMTime() > this->BuildTime && 
+       this->InsertTime < this->BuildTime))
     {
-    // determine where ramp starts and ends
-    rampStartsAt = this->Level - this->Window / 2.0;
-    rampEndsAt = this->Level + this->Window / 2.0;
+    int i, j;
+    unsigned char *rgba;
+    float start[4], incr[4];
 
-    vtkDebugMacro (<< "Ramp starts at: " << rampStartsAt << "\n");
-    vtkDebugMacro (<< "Ramp ends at: " << rampEndsAt << "\n");
-
-    // calculate table indices from values
-    indxStartsAt = this->MapScalarToIndex (rampStartsAt);
-    if (indxStartsAt >= this->NumberOfColors)
+    for (j = 0; j < 4; j++)
       {
-      indxStartsAt = this->NumberOfColors - 1;
-      }
-    if (indxStartsAt < 1)
-      {
-      indxStartsAt = 1;
+      start[j] = this->MinimumTableValue[j]*255;
+      incr[j] = ((this->MaximumTableValue[j]-this->MinimumTableValue[j]) / 
+		 (this->NumberOfColors - 1) * 255);
       }
 
-    indxEndsAt = this->MapScalarToIndex (rampEndsAt);
-    if (indxEndsAt >= this->NumberOfColors)
+    if (this->InverseVideo)
       {
-      indxStartsAt = this->NumberOfColors - 1;
-      }
-    if (indxEndsAt < 1)
-      {
-      indxEndsAt = 1;
-      }
-
-    vtkDebugMacro (<< "Index starts at: " << indxStartsAt << "\n");
-    vtkDebugMacro (<< "Index ends at: " << indxEndsAt << "\n");
-
-    if (this->InverseVideo) 
-      {
-      lowValue = 255;
-      highValue = 0;
-      minimum = this->MaximumColor;
-      maximum = this->MinimumColor;
+      for (i = 0; i < this->NumberOfColors; i++)
+	{
+        rgba = this->Table->WritePointer(4*i,4);
+	for (j = 0; j < 4; j++)
+	  {
+	  rgba[j] = (unsigned char) \
+	    (start[j] + (this->NumberOfColors - i - 1)*incr[j] + 0.5);
+	  }
+	}
       }
     else
       {
-      lowValue = 0;
-      highValue = 255;
-      minimum = this->MinimumColor;
-      maximum = this->MaximumColor;
-      }
-
-    // first do below the ramp
-    for (indx=0; indx < indxStartsAt; indx++)
-      {
-      rgba = this->Table->WritePointer(4*indx,4);
-      for (i=0; i < 4; i++)
+      for (i = 0; i < this->NumberOfColors; i++)
 	{
-	rgba[i] = minimum[i];
+        rgba = this->Table->WritePointer(4*i,4);
+	for (j = 0; j < 4; j++)
+	  {
+	  rgba[j] = (unsigned char)(start[j] + i*incr[j] + 0.5);
+	  }
 	}
       }
-
-    // now do the ramp
-    incrValue = (float) (highValue - lowValue) / (indxEndsAt - indxStartsAt);
-    for (indx=indxStartsAt; indx < indxEndsAt; indx++)
-      {
-      rgba = this->Table->WritePointer(4*indx,4);
-      rgba[0] = rgba[1] = rgba[2] = (unsigned char) (lowValue + incrValue * (indx - indxStartsAt) + .5);
-      rgba[3] = 255;
-      }
-
-    // finally do above the ramp
-    for (indx=indxEndsAt; indx < this->NumberOfColors; indx++)
-      {
-      rgba = this->Table->WritePointer(4*indx,4);
-      for (i=0; i < 4; i++)
-	{
-	rgba[i] = maximum[i];
-	}
-      }
-  }
+    }
   this->BuildTime.Modified();
 }
 
-// Convert scalar value to a table index
-int vtkWindowLevelLookupTable:: MapScalarToIndex (float scalar)
+// Reverse the color table (don't rebuild in case someone has
+// been adjusting the table values by hand)
+// This is a little ugly ... it might be best to remove
+// SetInverseVideo altogether and just use a negative Window.
+void vtkWindowLevelLookupTable::SetInverseVideo(int iv)
 {
-  int indx;
+  if (this->InverseVideo == iv)
+    {
+    return;
+    }
 
-  indx = (int)((scalar-this->TableRange[0])/(this->TableRange[1]-this->TableRange[0]) * this->NumberOfColors);
-  indx = (indx < 0 ? 0 : (indx >= this->NumberOfColors ? this->NumberOfColors-1 : indx));
+  this->InverseVideo = iv;
 
-  return indx;
+  if (this->Table->GetNumberOfTuples() < 1)
+    {
+    return;
+    }
+
+  unsigned char *rgba, *rgba2;
+  unsigned char tmp[4];
+  int i;
+  int n = this->NumberOfColors-1;
+
+  for (i = 0; i < this->NumberOfColors/2; i++)
+    {
+    rgba = this->Table->WritePointer(4*i,4);
+    rgba2 = this->Table->WritePointer(4*(n-i),4);
+    tmp[0]=rgba[0]; tmp[1]=rgba[1]; tmp[2]=rgba[2]; tmp[3]=rgba[3];
+    rgba[0]=rgba2[0]; rgba[1]=rgba2[1]; rgba[2]=rgba2[2]; rgba[3]=rgba2[3]; 
+    rgba2[0]=tmp[0]; rgba2[1]=tmp[1]; rgba2[2]=tmp[2]; rgba2[3]=tmp[3]; 
+    }
+  this->Modified();
 }
-  
+
 void vtkWindowLevelLookupTable::PrintSelf(ostream& os, vtkIndent indent)
 {
   vtkLookupTable::PrintSelf(os,indent);
 
-  os << indent << "Window: " <<this->Window << "\n";
-  os << indent << "Level: " <<this->Level << "\n";
-  os << indent << "Inverse Video: " << (this->InverseVideo ? "On\n" : "Off\n");
-
-  os << indent << "Minimum Color : ("
-     << (int) this->MinimumColor[0] << ", "
-     << (int) this->MinimumColor[1] << ", "
-     << (int) this->MinimumColor[2] << ", "
-     << (int) this->MinimumColor[3] << ")\n";
+  os << indent << "Window: " << this->Window << "\n";
+  os << indent << "Level: " << this->Level << "\n";
+  os << indent << "InverseVideo: " 
+     << (this->InverseVideo ? "On\n" : "Off\n");
+  os << indent << "MinimumTableValue : ("
+     << this->MinimumTableValue[0] << ", "
+     << this->MinimumTableValue[1] << ", "
+     << this->MinimumTableValue[2] << ", "
+     << this->MinimumTableValue[3] << ")\n";  
+  os << indent << "MaximumTableValue : ("
+     << this->MaximumTableValue[0] << ", "
+     << this->MaximumTableValue[1] << ", "
+     << this->MaximumTableValue[2] << ", "
+     << this->MaximumTableValue[3] << ")\n";  
+  this->GetMinimumColor();
+  os << indent << "MinimumColor : ("
+     << this->MinimumColor[0] << ", "
+     << this->MinimumColor[1] << ", "
+     << this->MinimumColor[2] << ", "
+     << this->MinimumColor[3] << ")\n";
+  this->GetMaximumColor();
   os << indent << "MaximumColor : ("
-     << (int) this->MaximumColor[0] << ", "
-     << (int) this->MaximumColor[1] << ", "
-     << (int) this->MaximumColor[2] << ", "
-     << (int) this->MaximumColor[3] << ")\n";
+     << this->MaximumColor[0] << ", "
+     << this->MaximumColor[1] << ", "
+     << this->MaximumColor[2] << ", "
+     << this->MaximumColor[3] << ")\n";
 }
 
 
