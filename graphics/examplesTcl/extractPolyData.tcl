9730dc06986b7203915111d093f49233a73c6a37
Will Schroeder <will.schroeder@kitware.com>
2001-07-03 10:16:15 -0400

    ENH:Support extraction of boundary cells

--- graphics/examplesTcl/extractPolyData.tcl
+++ graphics/examplesTcl/extractPolyData.tcl
@@ -2,43 +2,43 @@ catch {load vtktcl}
 if { [catch {set VTK_TCL $env(VTK_TCL)}] != 0} { set VTK_TCL "../../examplesTcl" }
 if { [catch {set VTK_DATA $env(VTK_DATA)}] != 0} { set VTK_DATA "../../../vtkdata" }
 
-# this is a tcl version of the Mace example
+# Demonstrate how to extract polygonal cells with an implicit function
 # get the interactor ui
 source $VTK_TCL/vtkInt.tcl
 
 # create a sphere source and actor
 #
 vtkSphereSource sphere
-  sphere SetThetaResolution 36
-  sphere SetPhiResolution 18
+  sphere SetThetaResolution 8
+  sphere SetPhiResolution 16
   sphere SetRadius 1.5
 
+# Extraction stuff
+vtkTransform t
+    t RotateX 90
+vtkCylinder cylfunc
+    cylfunc SetRadius 0.5
+    cylfunc SetTransform t
+vtkExtractPolyDataGeometry extract
+    extract SetInput [sphere GetOutput]
+    extract SetImplicitFunction cylfunc
+    extract ExtractBoundaryCellsOn
+
 vtkPolyDataMapper  sphereMapper
-    sphereMapper SetInput [sphere GetOutput]
+    sphereMapper SetInput [extract GetOutput]
     sphereMapper GlobalImmediateModeRenderingOn
+
 vtkActor sphereActor
     sphereActor SetMapper sphereMapper
-vtkActor sphereActor2
-    sphereActor2 SetMapper sphereMapper
-    [sphereActor2 GetProperty] SetRepresentationToWireframe
-    [sphereActor2 GetProperty] SetColor 0 0 0
 
 # Create the RenderWindow, Renderer and both Actors
 #
 vtkRenderer ren1
 vtkRenderWindow renWin
     renWin AddRenderer ren1
-    renWin SetWindowName "vtk - Mace"
+    renWin SetWindowName "vtk - extractPolyData"
 vtkRenderWindowInteractor iren
     iren SetRenderWindow renWin
-
-# Extraction stuff
-vtkCylinder cylfunc
-  cylfunc SetRadius 0.5
-
-vtkExtractPolyDataGeometry extract
-    extract SetInput [sphere GetOutput]
-    extract SetImplicitFunction cylfunc
 
 # Add the actors to the renderer, set the background and size
 #
@@ -46,30 +46,12 @@ ren1 AddActor sphereActor
 
 ren1 SetBackground 0.1 0.2 0.4
 renWin SetSize 300 300
-
-sphereMapper SetInput [extract GetOutput]
-
-vtkCamera cam1
-  cam1 SetPosition 4 4 3
-
-ren1 SetActiveCamera cam1
-
 renWin Render
-
 
 # render the image
 #
 iren SetUserMethod {wm deiconify .vtkInteract}
-
 iren Initialize
-renWin SetFileName "extractPolyData.tcl.ppm"
-#renWin SaveImageAsPPM
-
-proc TkCheckAbort {} {
-  set foo [renWin GetEventPending]
-  if {$foo != 0} {renWin SetAbortRender 1}
-}
-renWin SetAbortCheckMethod {TkCheckAbort}
 
 # prevent the tk window from showing up then start the event loop
 wm withdraw .
