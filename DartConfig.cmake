c467fdd67195266a6d6a5c60e2bab72fbf32a4fa
Berk Geveci <berk.geveci@kitware.com>
2001-07-03 14:13:00 -0400

    Setting up testing.

---
+++ DartConfig.cmake
@@ -0,0 +1,13 @@
+# Dart server to submit results (used by client)
+SET (DROP_SITE "public.kitware.com")
+SET (DROP_LOCATION "/incoming")
+SET (DROP_SITE_USER "anonymous")
+SET (DROP_SITE_PASSWORD "vtk-tester@somewhere.com")
+SET (TRIGGER_SITE 
+       "http://${DROP_SITE}/cgi-bin/Submit-vtk-TestingResults.pl")
+
+# Dart server configuration 
+SET (CVS_WEB_URL "http://${DROP_SITE}/cgi-bin/cvsweb.cgi/vtknew/")
+SET (CVS_WEB_CVSROOT "vtknew")
+SET (DOXYGEN_URL "http://${DROP_SITE}/" )
+SET (GNATS_WEB_URL "http://${DROP_SITE}/")
