d36e9f5da74a0f1aa83aa3e5eed8c645c42cead8
Ken Martin <ken.martin@kitware.com>
2001-07-17 10:39:19 -0400

    added utility header

---
+++ Common/Testing/Cxx/vtkRegressionTestImage.h
@@ -0,0 +1,59 @@
+
+// Includes and a macro necessary for saving the image produced by a cxx 
+// example program. This capability is critical for regression testing.
+
+#include "vtkWindowToImageFilter.h"
+#include "vtkPNGReader.h"
+#include "vtkPNGWriter.h"
+#include "vtkImageDifference.h"
+
+void vtkRegressionTestImage2(int argc, char *argv[], vtkWindow *rw ) 
+{
+  if( (argc >= 3) && (strcmp("-V", argv[argc-2]) == 0) ) 
+    { 
+    vtkWindowToImageFilter *rt_w2if = vtkWindowToImageFilter::New(); 
+    rt_w2if->SetInput(rw);
+    FILE *rt_fin = fopen(argv[argc-1],"r"); 
+    if (rt_fin) 
+      { 
+      fclose(rt_fin);
+      }
+    else
+      {
+      FILE *rt_fout = fopen(argv[argc-1],"wb"); 
+      if (rt_fout) 
+        { 
+        fclose(rt_fout);
+        vtkPNGWriter *rt_pngw = vtkPNGWriter::New();
+        rt_pngw->SetFileName(argv[argc-1]);
+        rt_pngw->SetInput(rt_w2if->GetOutput());
+        rt_pngw->Write();
+        rt_pngw->Delete();
+        }
+      else
+        {
+        fprintf(stderr,"Unable to find valid image!!!");
+        exit(1);
+        }
+      }
+    vtkPNGReader *rt_png = vtkPNGReader::New(); 
+    rt_png->SetFileName(argv[argc-1]); 
+    vtkImageDifference *rt_id = vtkImageDifference::New(); 
+    rt_id->SetInput(rt_w2if->GetOutput()); 
+    rt_id->SetImage(rt_png->GetOutput()); 
+    rt_id->Update(); 
+    rt_w2if->Delete(); 
+    rt_png->Delete(); 
+    if (rt_id->GetThresholdedError() <= 10) 
+      { 
+      rt_id->Delete(); 
+      exit(0); 
+      } 
+    fprintf(stderr,"Failed Image Test : %f", rt_id->GetThresholdedError()); 
+    rt_id->Delete(); 
+    exit( 1 );
+    }
+}
+
+#define vtkRegressionTestImage(rw) \
+vtkRegressionTestImage2(argc, argv, rw)
