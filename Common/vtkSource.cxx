dc9496d237dde213db5f52b844e6272fa2401b14
Charles Law <charles.law@kitware.com>
2001-07-25 07:44:38 -0400

    fix: Fixed problem running LOx.tcl

--- Common/vtkSource.cxx
+++ Common/vtkSource.cxx
@@ -265,6 +265,8 @@ void vtkSource::UpdateInformation()
 
 void vtkSource::PropagateUpdateExtent(vtkDataObject *output)
 {
+  int idx;
+
   // check flag to avoid executing forever if there is a loop
   if (this->Updating)
     {
@@ -284,6 +286,20 @@ void vtkSource::PropagateUpdateExtent(vtkDataObject *output)
 	<< " method and allocate your own data.");  
     }
   
+  // If the user defines a ComputeInputUpdateExtent method,
+  // I want RequestExactUpdateExtent to be off by default (User does nothing else).
+  // Otherwise, the ComputeInputUpdateExtent in this superclass sets
+  // RequestExactExtent to on.  The reason for this initialization here is 
+  // if this sources shares an input with another, we do not want the input's
+  // RequestExactExtent "state" to interfere with each other.
+  for (idx = 0; idx < this->NumberOfInputs; ++idx)
+    {
+    if (this->Inputs[idx] != NULL)
+      {
+      this->Inputs[idx]->RequestExactExtentOff();
+      }
+    }  
+      
   // Give the subclass a chance to request a larger extent on 
   // the inputs. This is necessary when, for example, a filter
   // requires more data at the "internal" boundaries to 
@@ -295,7 +311,7 @@ void vtkSource::PropagateUpdateExtent(vtkDataObject *output)
   // Now that we know the input update extent, propogate this
   // through all the inputs.
   this->Updating = 1;
-  for (int idx = 0; idx < this->NumberOfInputs; ++idx)
+  for (idx = 0; idx < this->NumberOfInputs; ++idx)
     {
     if (this->Inputs[idx] != NULL)
       {
