c7762c268d4eeca0e76c22edcb79d22630884f22
Ken Martin <ken.martin@kitware.com>
2001-07-26 16:10:57 -0400

    more tests

---
+++ Imaging/Testing/Tcl/TestInPlaceFilter.tcl
@@ -0,0 +1,61 @@
+package require vtktcl
+
+# Derived from Cursor3D.  This script increases the coverage of the
+# vtkImageInplaceFilter super class.
+
+
+
+# global values
+set CURSOR_X 20
+set CURSOR_Y 20
+set CURSOR_Z 20
+
+set IMAGE_MAG_X 2
+set IMAGE_MAG_Y 2
+set IMAGE_MAG_Z 1
+
+
+
+# pipeline stuff
+vtkSLCReader reader
+reader SetFileName "$VTK_DATA_ROOT/Data/nut.slc"
+
+# make the image a little biger
+vtkImageMagnify magnify1
+  magnify1 SetInput [reader GetOutput]
+magnify1 SetMagnificationFactors $IMAGE_MAG_X $IMAGE_MAG_Y $IMAGE_MAG_Z
+  magnify1 ReleaseDataFlagOn
+
+vtkImageMagnify magnify2
+  magnify2 SetInput [reader GetOutput]
+  magnify2 SetMagnificationFactors $IMAGE_MAG_X $IMAGE_MAG_Y $IMAGE_MAG_Z
+  magnify2 ReleaseDataFlagOn
+
+# a filter that does in place processing (magnify ReleaseDataFlagOn)
+vtkImageCursor3D cursor
+cursor SetInput [magnify1 GetOutput]
+cursor SetCursorPosition [expr $CURSOR_X * $IMAGE_MAG_X] \
+    [expr $CURSOR_Y * $IMAGE_MAG_Y] [expr $CURSOR_Z * $IMAGE_MAG_Z]
+cursor SetCursorValue 255
+cursor SetCursorRadius [expr 50 * $IMAGE_MAG_X]
+# stream to increase coverage of in place filter.
+
+# put thge two together in one image
+vtkImageAppend imageAppend
+imageAppend SetAppendAxis 0
+imageAppend AddInput [magnify2 GetOutput]
+imageAppend AddInput [cursor GetOutput]
+
+vtkImageViewer viewer
+viewer SetInput [imageAppend GetOutput]
+viewer SetZSlice [expr $CURSOR_Z * $IMAGE_MAG_Z]
+viewer SetColorWindow 200
+viewer SetColorLevel 80
+#viewer DebugOn
+viewer Render
+
+viewer SetPosition 50 50
+
+#make interface
+viewer Render
+
