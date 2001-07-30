ef09c8ba14106adca3ab314d7b3509d7b7097053
Will Schroeder <will.schroeder@kitware.com>
2001-07-30 10:45:29 -0400

    ENH:Also test new vtkBlankStructuredGrid class

--- graphics/examplesTcl/blankGrid.tcl
+++ graphics/examplesTcl/blankGrid.tcl
@@ -1,24 +1,19 @@
 catch {load vtktcl}
+
 if { [catch {set VTK_TCL $env(VTK_TCL)}] != 0} { set VTK_TCL "../../examplesTcl" }
 if { [catch {set VTK_DATA $env(VTK_DATA)}] != 0} { set VTK_DATA "../../../vtkdata" }
 
-# Demonstrate how to use structured grid blanking with an image
+# Demonstrate how to use structured grid blanking with an image. There are two
+# techniques demonstrated: one uses an image to perform the blanking;
+# the other uses scalar values to do the same thing. Both images should
+# be identical.
 #
 
 # get the interactor ui
 source $VTK_TCL/vtkInt.tcl
 source $VTK_TCL/colors.tcl
 
-# create planes
-# Create the RenderWindow, Renderer and both Actors
-#
-vtkRenderer ren1
-vtkRenderWindow renWin
-    renWin AddRenderer ren1
-vtkRenderWindowInteractor iren
-    iren SetRenderWindow renWin
-
-# create pipeline
+# create pipeline - start by extracting a single plane from the grid
 #
 vtkPLOT3DReader pl3d
     pl3d SetXYZFileName "$VTK_DATA/combxyz.bin"
@@ -29,41 +24,60 @@ vtkExtractGrid plane
     plane SetInput [pl3d GetOutput]
     plane SetVOI 0 57 0 33 0 0
     plane Update
-vtkStructuredPoints blankImage
-   blankImage SetScalarType 3
+
+# Create some data to use for the (image) blanking
+#
+vtkImageData blankImage
+   blankImage SetScalarTypeToUnsignedChar
    blankImage SetDimensions 57 33 1
+   blankImage AllocateScalars
+set blanking [[blankImage GetPointData] GetScalars]
 set numBlanks [expr 57*33]
-vtkScalars blanking
-   blanking SetDataTypeToUnsignedChar
-   blanking SetNumberOfScalars $numBlanks
 for {set i 0} {$i<$numBlanks} {incr i} {
-   blanking SetScalar $i 1
+   $blanking SetScalar $i 1
 }
-
 # Manually blank out areas corresponding to dilution holes
-blanking SetScalar 318 0
-blanking SetScalar 945 0
-blanking SetScalar 1572 0
+$blanking SetScalar 318 0
+$blanking SetScalar 945 0
+$blanking SetScalar 1572 0
+$blanking SetScalar 641 0
+$blanking SetScalar 1553 0
 
-blanking SetScalar 641 0
-blanking SetScalar 1553 0
-
-[blankImage GetPointData] SetScalars blanking
-
+# The first blanking technique uses the image to set the blanking values
+#
 vtkBlankStructuredGridWithImage blankIt
     blankIt SetInput [plane GetOutput]
     blankIt SetBlankingInput blankImage
-
 vtkStructuredGridGeometryFilter blankedPlane
     blankedPlane SetInput [blankIt GetOutput]
     blankedPlane SetExtent 0 100 0 100 0 0
-
 vtkPolyDataMapper planeMapper
     planeMapper SetInput [blankedPlane GetOutput]
     planeMapper SetScalarRange 0.197813 0.710419
 vtkActor planeActor
     planeActor SetMapper planeMapper
 
+# The second blanking technique uses grid data values to create the blanking.
+# Here we borrow the image data and threshold on that.
+#
+vtkStructuredGrid anotherGrid
+    anotherGrid CopyStructure [plane GetOutput]
+    [anotherGrid GetPointData] SetScalars [blankImage GetPointData]
+vtkBlankStructuredGrid blankGrid
+    blankGrid SetInput anotherGrid 
+    blankGrid SetMinBlankValue -0.5
+    blankGrid SetMaxBlankValue  0.5
+vtkStructuredGridGeometryFilter blankedPlane2
+    blankedPlane2 SetInput [blankIt GetOutput]
+    blankedPlane2 SetExtent 0 100 0 100 0 0
+vtkPolyDataMapper planeMapper2
+    planeMapper2 SetInput [blankedPlane2 GetOutput]
+    planeMapper2 SetScalarRange 0.197813 0.710419
+vtkActor planeActor2
+    planeActor2 SetMapper planeMapper2
+
+# An outline around the data
+#
 vtkStructuredGridOutlineFilter outline
     outline SetInput [pl3d GetOutput]
 vtkPolyDataMapper outlineMapper
@@ -71,26 +85,45 @@ vtkPolyDataMapper outlineMapper
 vtkActor outlineActor
     outlineActor SetMapper outlineMapper
     eval [outlineActor GetProperty] SetColor $black
+vtkPolyDataMapper outlineMapper2
+    outlineMapper2 SetInput [outline GetOutput]
+vtkActor outlineActor2
+    outlineActor2 SetMapper outlineMapper2
+    eval [outlineActor2 GetProperty] SetColor $black
+
+# create planes
+# Create the RenderWindow, Renderer and both Actors
+#
+vtkRenderer ren1
+vtkRenderer ren2
+vtkRenderWindow renWin
+    renWin AddRenderer ren1
+    renWin AddRenderer ren2
+vtkRenderWindowInteractor iren
+    iren SetRenderWindow renWin
 
 # Add the actors to the renderer, set the background and size
 #
 ren1 AddActor outlineActor
 ren1 AddActor planeActor
+ren2 AddActor outlineActor2
+ren2 AddActor planeActor2
+
 ren1 SetBackground 1 1 1
-ren1 TwoSidedLightingOff
+ren2 SetBackground 1 1 1
 
-renWin SetSize 500 500
-iren Initialize
-iren LightFollowCameraOff
-
-renWin Render
-ren1 LightFollowCameraOff
+renWin SetSize 500 250
 
 set cam1 [ren1 GetActiveCamera]
     $cam1 SetClippingRange 3.95297 50
     $cam1 SetFocalPoint 8.88908 0.595038 29.3342
     $cam1 SetPosition -12.3332 31.7479 41.2387
     $cam1 SetViewUp 0.060772 -0.319905 0.945498
+set cam2 [ren2 GetActiveCamera]
+    $cam2 SetClippingRange 3.95297 50
+    $cam2 SetFocalPoint 8.88908 0.595038 29.3342
+    $cam2 SetPosition -12.3332 31.7479 41.2387
+    $cam2 SetViewUp 0.060772 -0.319905 0.945498
 
 # render the image
 #
