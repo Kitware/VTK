2de924c4c594f9397981efd9d67e0ce051d0ea07
Will Schroeder <will.schroeder@kitware.com>
2001-07-30 07:34:57 -0400

    ENH:Convert attribute data into blanking field

---
+++ Graphics/vtkBlankStructuredGrid.cxx
@@ -0,0 +1,176 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkBlankStructuredGrid.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#include "vtkBlankStructuredGrid.h"
+#include "vtkObjectFactory.h"
+
+//------------------------------------------------------------------------
+vtkBlankStructuredGrid* vtkBlankStructuredGrid::New()
+{
+  // First try to create the object from the vtkObjectFactory
+  vtkObject* ret = vtkObjectFactory::CreateInstance("vtkBlankStructuredGrid");
+  if(ret)
+    {
+    return (vtkBlankStructuredGrid*)ret;
+    }
+  // If the factory was unable to create the object, then create it here.
+  return new vtkBlankStructuredGrid;
+}
+
+// Construct object to extract all of the input data.
+vtkBlankStructuredGrid::vtkBlankStructuredGrid()
+{
+  this->MinBlankingValue = VTK_LARGE_FLOAT;
+  this->MaxBlankingValue = VTK_LARGE_FLOAT;
+  this->ArrayName = NULL;
+  this->ArrayId = -1;
+  this->Component = 0;
+}
+
+vtkBlankStructuredGrid::~vtkBlankStructuredGrid()
+{
+  if ( this->ArrayName )
+    {
+    delete [] this->ArrayName;
+    this->ArrayName = NULL;
+    }
+}
+
+
+template <class T>
+static void vtkBlankStructuredGridExecute(vtkBlankStructuredGrid *self,
+                                          T *dptr, int numPts, int numComp,
+                                          int comp, float min, float max,
+                                          vtkUnsignedCharArray *blanking)
+{
+  T compValue;
+  dptr += comp;
+
+  for ( int ptId=0; ptId < numPts; ptId++, dptr+=numComp)
+    {
+    compValue = *dptr;
+    if ( compValue >= min && compValue <= max )
+      {
+      blanking->SetValue(ptId,0); //make it invisible
+      }
+    else
+      {
+      blanking->SetValue(ptId,1);
+      }
+    }
+}
+
+
+void vtkBlankStructuredGrid::Execute()
+{
+  vtkStructuredGrid *input= this->GetInput();
+  vtkPointData *pd=input->GetPointData();
+  vtkCellData *cd=input->GetCellData();
+  vtkStructuredGrid *output= this->GetOutput();
+  vtkPointData *outPD=output->GetPointData();
+  vtkCellData *outCD=output->GetCellData();
+  int numPts = input->GetNumberOfPoints();
+  vtkDataArray *dataArray=NULL;
+
+  vtkDebugMacro(<< "Blanking Grid");
+
+  // Get the appropriate data array
+  //
+  if ( this->ArrayName != NULL )
+    {
+    dataArray = pd->GetArray(this->ArrayName);
+    }
+  else if ( this->ArrayId >= 0 )
+    {
+    dataArray = pd->GetArray(this->ArrayId);
+    }
+    
+  int numComp = dataArray->GetNumberOfComponents();
+  if ( !dataArray || this->Component >= numComp )
+    {
+    vtkWarningMacro(<<"Data array (used for coloring) not found");
+    return;
+    }
+  void *dptr = dataArray->GetVoidPointer(0);
+  
+  // Loop over the data array setting anything within the data range specified
+  // to be blanked.
+  //
+  vtkUnsignedCharArray *blanking = vtkUnsignedCharArray::New();
+  blanking->SetNumberOfValues(numPts);
+
+  // call templated function
+  switch (dataArray->GetDataType())
+    {
+    vtkTemplateMacro8(vtkBlankStructuredGridExecute, this, (VTK_TT *)(dptr), numComp,
+                      numPts, this->Component, this->MinBlankingValue, 
+                      this->MaxBlankingValue, blanking);
+    default:
+      break;
+    }
+
+  // Clean up and get out
+  output->SetPointVisibility(blanking);
+  blanking->Delete();
+}
+
+
+void vtkBlankStructuredGrid::PrintSelf(ostream& os, vtkIndent indent)
+{
+  vtkStructuredGridToStructuredGridFilter::PrintSelf(os,indent);
+
+  os << indent << "Min Blanking Value: " << this->MinBlankingValue << "\n";
+  os << indent << "Max Blanking Value: " << this->MaxBlankingValue << "\n";
+  os << indent << "Array Name: ";
+  if ( this->ArrayName )
+    {
+    os << this->ArrayName << "\n";
+    }
+  else
+    {
+    os << "(none)\n";
+    }
+  os << indent << "Array ID: " << this->ArrayId << "\n";
+  os << indent << "Component: " << this->Component << "\n";
+  
+}
+
+
---
+++ Graphics/vtkBlankStructuredGrid.h
@@ -0,0 +1,117 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkBlankStructuredGrid.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkBlankStructuredGrid - translate point attribute data into a blanking field
+
+// .SECTION Description
+// vtkBlankStructuredGrid is a filter that sets the blanking field in a 
+// vtkStructuredGrid dataset. The blanking field is set by examining a
+// specified point attribute data array (e.g., scalars) and converting
+// values in the data array to either a "1" (visible) or "0" (blanked) value
+// in the blanking array. The values to be blanked are specified by giving
+// a min/max range. All data values in the data array indicated and laying
+// within the range specified (inclusive on both ends) are translated to 
+// a "off" blanking value.
+
+// .SECTION See Also
+// vtkStructuredGrid
+
+#ifndef __vtkBlankStructuredGrid_h
+#define __vtkBlankStructuredGrid_h
+
+#include "vtkStructuredGridToStructuredGridFilter.h"
+
+class VTK_EXPORT vtkBlankStructuredGrid : public vtkStructuredGridToStructuredGridFilter
+{
+public:
+  static vtkBlankStructuredGrid *New();
+  vtkTypeMacro(vtkBlankStructuredGrid,vtkStructuredGridToStructuredGridFilter);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Specify the lower data value in the data array specified which will be converted
+  // into a "blank" (or off) value in the blanking array.
+  vtkSetMacro(MinBlankingValue,int);
+  vtkGetMacro(MinBlankingValue,int);
+
+  // Description:
+  // Specify the upper data value in the data array specified which will be converted
+  // into a "blank" (or off) value in the blanking array.
+  vtkSetMacro(MaxBlankingValue,int);
+  vtkGetMacro(MaxBlankingValue,int);
+
+  // Description:
+  // Specify the data array name to use to generate the blanking field. Alternatively,
+  // you can specify the array id. (If both are set, the array name takes precedence.)
+  vtkSetStringMacro(ArrayName);
+  vtkGetStringMacro(ArrayName);
+
+  // Description:
+  // Specify the data array id to use to generate the blanking field. Alternatively,
+  // you can specify the array name. (If both are set, the array name 
+  // takes precedence.)
+  vtkSetMacro(ArrayId,int);
+  vtkGetMacro(ArrayId,int);
+
+  // Description:
+  // Specify the component in the data array to use to generate the blanking field.
+  vtkSetClampMacro(Component,int,0,VTK_LARGE_INTEGER);
+  vtkGetMacro(Component,int);
+
+protected:
+  vtkBlankStructuredGrid();
+  ~vtkBlankStructuredGrid();
+  vtkBlankStructuredGrid(const vtkBlankStructuredGrid&) {}
+  void operator=(const vtkBlankStructuredGrid&) {}
+
+  void Execute();
+  
+  float MinBlankingValue;
+  float MaxBlankingValue;
+  char  *ArrayName;
+  int   ArrayId;
+  int   Component;
+  
+};
+
+#endif
+
+
