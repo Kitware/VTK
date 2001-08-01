2f4af279fd35d9bc0df965f2cea4c6452a9b163a
Will Schroeder <will.schroeder@kitware.com>
2001-08-01 09:40:27 -0400

    ENH:Test vtkImageEuclideanDistance

---
+++ Imaging/Testing/Tcl/TestEuclideanDistance.tcl
@@ -0,0 +1,34 @@
+package require vtktcl
+
+# This script shows how to use vtkImageEuclideanDistance
+
+# Image pipeline
+
+vtkPNGReader reader
+reader SetFileName $VTK_DATA_ROOT/Data/fullhead15.png
+
+vtkImageCast cast
+cast SetOutputScalarTypeToShort
+cast SetInput [reader GetOutput]
+
+vtkImageThreshold thresh
+thresh SetInput [cast GetOutput]
+thresh ThresholdByUpper 2000.0
+thresh SetInValue 0
+thresh SetOutValue 200
+thresh ReleaseDataFlagOff
+
+vtkImageCityBlockDistance dist
+dist SetInput [thresh GetOutput]
+
+vtkImageViewer viewer
+viewer SetInput [dist GetOutput]
+viewer SetColorWindow 117
+viewer SetColorLevel 43
+
+viewer Render
+
+
+
+
+
