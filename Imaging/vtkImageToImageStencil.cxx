c23418e7ff0a90e218e87b09d21bbc30869adf20
Berk Geveci <berk.geveci@kitware.com>
2001-08-03 09:10:38 -0400

    Should not include something in graphics.

--- Imaging/vtkImageToImageStencil.cxx
+++ Imaging/vtkImageToImageStencil.cxx
@@ -43,7 +43,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <math.h>
 #include "vtkImageToImageStencil.h"
 #include "vtkPolyData.h"
-#include "vtkOBBTree.h"
 #include "vtkObjectFactory.h"
 
 
