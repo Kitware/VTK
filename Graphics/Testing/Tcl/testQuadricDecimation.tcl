3ff3898be2293d1a1bb05517ead3f71c3d885132
Amy Squillacote <amy.squillacote@kitware.com>
2001-08-10 15:00:43 -0400

    new tcl script for testing vtkQuadricDecimation to produce uniform results across platforms

--- Graphics/Testing/Tcl/testQuadricDecimation.tcl
+++ Graphics/Testing/Tcl/testQuadricDecimation.tcl
@@ -13,11 +13,20 @@ vtkRenderWindowInteractor iren
 
 vtkSphereSource sphere
   sphere SetPhiResolution 5
-  sphere SetThetaResolution 6
+  sphere SetThetaResolution 5
   sphere SetRadius 1
 
+vtkTransform xform
+  xform Scale 1 2 3
+  xform RotateWXYZ 32 3 1 2
+  xform Scale 2 5 4
+
+vtkTransformFilter xformFilter
+  xformFilter SetInput [sphere GetOutput]
+  xformFilter SetTransform xform
+
 vtkElevationFilter el
-  el SetInput [sphere GetOutput]
+  el SetInput [xformFilter GetOutput]
   el SetLowPoint 0 0 0
   el SetHighPoint 0 0 1
 
@@ -31,6 +40,17 @@ vtkPolyDataMapper mapper
 
 vtkActor actor
   actor SetMapper mapper
+
+# Set up the camera parameters
+#
+vtkCamera camera
+  camera SetPosition -58.3274 -.948032 17.7715
+  camera SetFocalPoint -.172376 .389125 -.100301
+  camera SetViewUp -.0374429 .998186 -.0471559
+  camera SetViewAngle 30
+  camera SetClippingRange 48.8511 76.1099
+
+ren1 SetActiveCamera camera
 
 # Add the actors to the renderer, set the background and size
 #
