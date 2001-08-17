39a28e26f2706cfa76e2ffd66ba0d93ee3c251e8
Berk Geveci <berk.geveci@kitware.com>
2001-08-17 16:44:17 -0400

    Tests for sockets.

---
+++ Parallel/Testing/Cxx/SocketClient.cxx
@@ -0,0 +1,338 @@
+#include "vtkSocketController.h"
+#include "vtkUnstructuredGrid.h"
+#include "vtkRectilinearGrid.h"
+#include "vtkStructuredGrid.h"
+#include "vtkDataSetMapper.h"
+#include "vtkActor.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderer.h"
+#include "vtkDoubleArray.h"
+#include "vtkRegressionTestImage.h"
+#include "vtkInputPort.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkContourFilter.h"
+#include "vtkDebugLeaks.h"
+
+static const int scMsgLength = 10;
+
+static void CleanUp(vtkSocketCommunicator* comm, vtkSocketController* contr)
+{
+  comm->CloseConnection();
+  comm->Delete();
+  contr->Delete();
+}
+
+int main(int argc, char** argv)
+{
+  vtkDebugLeaks::PromptUserOff();
+
+  vtkSocketController* contr = vtkSocketController::New();
+  contr->Initialize();
+
+  vtkSocketCommunicator* comm = vtkSocketCommunicator::New();
+
+  int i;
+
+  // Get the host name from the command line arguments
+  char* hostname = new char[30];
+  strcpy(hostname, "localhost");
+  int dataIndex=-1;
+  for (i=0; i<argc; i++)
+    {
+    if ( strcmp("-H", argv[i]) == 0 )
+      {
+      if ( i < argc-1 )
+	{
+	dataIndex = i+1;
+	}
+      }
+    }
+  if (dataIndex != -1)
+    {
+    delete[] hostname;
+    hostname = new char[strlen(argv[dataIndex])+1];
+    strcpy(hostname, argv[dataIndex]);
+    }
+
+  // Get the port from the command line arguments
+  int port=11111;
+
+  dataIndex=-1;
+  for (i=0; i<argc; i++)
+    {
+    if ( strcmp("-P", argv[i]) == 0 )
+      {
+      if ( i < argc-1 )
+	{
+	dataIndex = i+1;
+	}
+      }
+    }
+  if (dataIndex != -1)
+    {
+    port = atoi(argv[dataIndex]);
+    }
+
+  // Establish connection
+  if (!comm->ConnectTo(hostname, port))
+    {
+    cerr << "Client error: Could not connect to the server."
+	 << endl;
+    comm->Delete();
+    contr->Delete();
+    delete[] hostname;
+    return 1;
+    }
+  delete[] hostname;
+
+  // Test sending all supported types of arrays
+  int datai[scMsgLength];
+  for (i=0; i<scMsgLength; i++)
+    {
+    datai[i] = i;
+    }
+  if (!comm->Send(datai, scMsgLength, 1, 11))
+    {
+    cerr << "Client error: Error sending data." << endl;
+    CleanUp(comm, contr);
+    return 1;
+    }
+
+  unsigned long dataul[scMsgLength];
+  for (i=0; i<scMsgLength; i++)
+    {
+    dataul[i] = static_cast<unsigned long>(i);
+    }
+  if (!comm->Send(dataul, scMsgLength, 1, 22))
+    {
+    cerr << "Client error: Error sending data." << endl;
+    CleanUp(comm, contr);
+    return 1;
+    }
+
+  char datac[scMsgLength];
+  for (i=0; i<scMsgLength; i++)
+    {
+    datac[i] = static_cast<char>(i);
+    }
+  if (!comm->Send(datac, scMsgLength, 1, 33))
+    {
+    cerr << "Client error: Error sending data." << endl;
+    CleanUp(comm, contr);
+    return 1;
+    }
+
+  unsigned char datauc[scMsgLength];
+  for (i=0; i<scMsgLength; i++)
+    {
+    datauc[i] = static_cast<unsigned char>(i);
+    }
+  if (!comm->Send(datauc, scMsgLength, 1, 44))
+    {
+    cerr << "Client error: Error sending data." << endl;
+    CleanUp(comm, contr);
+    return 1;
+    }
+
+  float dataf[scMsgLength];
+  for (i=0; i<scMsgLength; i++)
+    {
+    dataf[i] = static_cast<float>(i);
+    }
+  if (!comm->Send(dataf, scMsgLength, 1, 7))
+    {
+    cerr << "Client error: Error sending data." << endl;
+    CleanUp(comm, contr);
+    return 1;
+    }
+
+
+  double datad[scMsgLength];
+  for (i=0; i<scMsgLength; i++)
+    {
+    datad[i] = static_cast<double>(i);
+    }
+  if (!comm->Send(datad, scMsgLength, 1, 7))
+    {
+    cerr << "Client error: Error sending data." << endl;
+    CleanUp(comm, contr);
+    return 1;
+    }
+
+  vtkIdType datait[scMsgLength];
+  for (i=0; i<scMsgLength; i++)
+    {
+    datait[i] = static_cast<vtkIdType>(i);
+    }
+  if (!comm->Send(datait, scMsgLength, 1, 7))
+    {
+    cerr << "Client error: Error sending data." << endl;
+    CleanUp(comm, contr);
+    return 1;
+    }
+
+  // Test receiving vtkDataObject
+  vtkUnstructuredGrid* ugrid = vtkUnstructuredGrid::New();
+
+  if (!comm->Receive(ugrid, 1, 9))
+    {
+    cerr << "Client error: Error receiving data." << endl;
+    CleanUp(comm, contr);
+    ugrid->Delete();
+    return 1;
+    }
+
+  vtkDataSetMapper* umapper = vtkDataSetMapper::New();
+  umapper->SetInput(ugrid);
+
+  vtkActor* uactor = vtkActor::New();
+  uactor->SetMapper(umapper);
+  uactor->SetPosition(5, 0, 0);
+  uactor->SetScale(0.2, 0.2, 0.2);
+  umapper->UnRegister(0);
+
+  // Test receiving vtkDataArray
+  vtkDoubleArray* da = vtkDoubleArray::New();
+  if (!comm->Receive(da, 1, 9))
+    {
+    cerr << "Client error: Error receiving data." << endl;
+    CleanUp(comm, contr);
+    ugrid->Delete();
+    uactor->Delete();
+    da->Delete();
+    return 1;
+    }
+  for (i=0; i<40; i++)
+    {
+    if (da->GetValue(i) != static_cast<double>(i))
+      {
+      cerr << "Server error: Corrupt vtkDoubleArray." << endl;
+      CleanUp(comm, contr);
+      ugrid->Delete();
+      uactor->Delete();
+      da->Delete();
+      return 1;
+      }
+    }
+
+  da->Delete();
+
+  contr->SetCommunicator(comm);
+
+  // Test the ports
+  vtkInputPort* ip = vtkInputPort::New();
+  ip->SetController(contr);
+  ip->SetTag(45);
+  ip->SetRemoteProcessId(1);
+
+  // Get polydata
+  ip->GetPolyDataOutput()->Update();
+
+  vtkPolyDataMapper* pmapper = vtkPolyDataMapper::New();
+  pmapper->SetInput(ip->GetPolyDataOutput());
+  // Break the connection and tell the server to fall out of loop
+  ip->GetPolyDataOutput()->SetSource(0);
+  contr->TriggerRMI(1, vtkMultiProcessController::BREAK_RMI_TAG);
+
+  vtkActor* pactor = vtkActor::New();
+  pactor->SetMapper(pmapper);
+  pmapper->UnRegister(0);
+
+  // Get rectilinear grid
+  ip->GetRectilinearGridOutput()->Update();
+
+  vtkDataSetMapper* rgmapper = vtkDataSetMapper::New();
+  rgmapper->SetInput(ip->GetRectilinearGridOutput());
+  ip->GetRectilinearGridOutput()->SetSource(0);
+  contr->TriggerRMI(1, vtkMultiProcessController::BREAK_RMI_TAG);
+
+  vtkActor* rgactor = vtkActor::New();
+  rgactor->SetMapper(rgmapper);
+  rgactor->SetPosition(0, -5, 0);
+  rgactor->SetScale(2, 2, 2);
+  rgmapper->UnRegister(0);
+
+  // Get structured points
+  ip->GetStructuredPointsOutput()->Update();
+
+  vtkContourFilter* iso = vtkContourFilter::New();
+  iso->SetInput(ip->GetStructuredPointsOutput());
+  iso->SetValue(0, 128);
+  ip->GetStructuredPointsOutput()->SetSource(0);
+  contr->TriggerRMI(1, vtkMultiProcessController::BREAK_RMI_TAG);
+
+  vtkPolyDataMapper* spmapper = vtkPolyDataMapper::New();
+  spmapper->SetInput(iso->GetOutput());
+  iso->UnRegister(0);
+
+  vtkActor* spactor = vtkActor::New();
+  spactor->SetMapper(spmapper);
+  spactor->SetPosition(5, -5, 0);
+  spactor->SetScale(0.1, 0.1, 0.1);
+  spmapper->UnRegister(0);
+
+  // Get structured grid
+  ip->GetStructuredGridOutput()->Update();
+
+  vtkContourFilter* iso2 = vtkContourFilter::New();
+  iso2->SetInput(ip->GetStructuredGridOutput());
+  iso2->SetValue(0, .205);
+  ip->GetStructuredGridOutput()->SetSource(0);
+  contr->TriggerRMI(1, vtkMultiProcessController::BREAK_RMI_TAG);
+
+  vtkPolyDataMapper* sgmapper = vtkPolyDataMapper::New();
+  sgmapper->SetInput(iso2->GetOutput());
+  iso2->UnRegister(0);
+
+  vtkActor* sgactor = vtkActor::New();
+  sgactor->SetMapper(sgmapper);
+  sgactor->SetPosition(10, -5, 0);
+  sgmapper->UnRegister(0);
+
+  // Get image data
+  ip->GetImageDataOutput()->Update();
+
+  vtkContourFilter* iso3 = vtkContourFilter::New();
+  iso3->SetInput(ip->GetImageDataOutput());
+  iso3->SetValue(0, .205);
+  ip->GetImageDataOutput()->SetSource(0);
+  contr->TriggerRMI(1, vtkMultiProcessController::BREAK_RMI_TAG);
+
+  vtkPolyDataMapper* immapper = vtkPolyDataMapper::New();
+  immapper->SetInput(iso3->GetOutput());
+  iso3->UnRegister(0);
+
+  vtkActor* imactor = vtkActor::New();
+  imactor->SetMapper(immapper);
+  imactor->SetPosition(10, 0, 0);
+  imactor->SetScale(0.02, 0.02, 0.02);
+  immapper->UnRegister(0);
+
+  vtkRenderer* ren = vtkRenderer::New();
+  ren->AddActor(uactor);
+  ren->AddActor(pactor);
+  ren->AddActor(rgactor);
+  ren->AddActor(spactor);
+  ren->AddActor(sgactor);
+  ren->AddActor(imactor);
+  uactor->UnRegister(0);
+  pactor->UnRegister(0);
+  rgactor->UnRegister(0);
+  spactor->UnRegister(0);
+  sgactor->UnRegister(0);
+  imactor->UnRegister(0);
+
+  vtkRenderWindow* renWin = vtkRenderWindow::New();
+  renWin->AddRenderer(ren);
+  ren->UnRegister(0);
+
+  renWin->Render();
+
+  int retVal = vtkRegressionTestImage( renWin );
+
+  ip->Delete();
+  renWin->Delete();
+  ugrid->Delete();
+  CleanUp(comm, contr);
+  return 0;
+}
---
+++ Parallel/Testing/Cxx/SocketServer.cxx
@@ -0,0 +1,288 @@
+#include "vtkSocketController.h"
+#include "vtkUnstructuredGridReader.h"
+#include "vtkRectilinearGridReader.h"
+#include "vtkStructuredPointsReader.h"
+#include "vtkRegressionTestImage.h"
+#include "vtkDoubleArray.h"
+#include "vtkOutputPort.h"
+#include "vtkBYUReader.h"
+#include "vtkPLOT3DReader.h"
+#include "vtkPNMReader.h"
+#include "vtkDebugLeaks.h"
+
+static const int scMsgLength = 10;
+
+static void CleanUp(vtkSocketCommunicator* comm, vtkSocketController* contr)
+{
+  comm->CloseConnection();
+  comm->Delete();
+  contr->Delete();
+}
+
+int main(int argc, char** argv)
+{
+  vtkDebugLeaks::PromptUserOff();
+
+  vtkSocketController* contr = vtkSocketController::New();
+  contr->Initialize();
+
+  vtkSocketCommunicator* comm = vtkSocketCommunicator::New();
+
+  int port=11111;
+
+  int i;
+
+  // Get the port from the command line arguments
+  int dataIndex=-1;
+  for (i=0; i<argc; i++)
+    {
+    if ( strcmp("-P", argv[i]) == 0 )
+      {
+      if ( i < argc-1 )
+	{
+	dataIndex = i+1;
+	}
+      }
+    }
+  if (dataIndex != -1)
+    {
+    port = atoi(argv[dataIndex]);
+    }
+
+  // Establish connection
+  if (!comm->WaitForConnection(port))
+    {
+    cerr << "Server error: Wait timed out or could not initialize socket."
+	 << endl;
+    comm->Delete();
+    contr->Delete();
+    return 1;
+    }
+
+
+  // Test receiving all supported types of arrays
+  int datai[scMsgLength];
+  if (!comm->Receive(datai, scMsgLength, 1, 11))
+    {
+    cerr << "Server error: Error receiving data." << endl;
+    CleanUp(comm, contr);
+    return 1;
+    }
+  for (i=0; i<scMsgLength; i++)
+    {
+    if (datai[i] != i)
+      {
+      cerr << "Server error: Corrupt integer array." << endl;
+      CleanUp(comm, contr);
+      return 1;
+      }
+    }
+
+  unsigned long dataul[scMsgLength];
+  if (!comm->Receive(dataul, scMsgLength, 1, 22))
+    {
+    cerr << "Server error: Error receiving data." << endl;
+    CleanUp(comm, contr);
+    return 1;
+    }
+  for (i=0; i<scMsgLength; i++)
+    {
+    if (dataul[i] != static_cast<unsigned long>(i))
+      {
+      cerr << "Server error: Corrupt unsigned long array." << endl;
+      CleanUp(comm, contr);
+      return 1;
+      }
+    }
+
+  char datac[scMsgLength];
+  if (!comm->Receive(datac, scMsgLength, 1, 33))
+    {
+    cerr << "Server error: Error receiving data." << endl;
+    CleanUp(comm, contr);
+    return 1;
+    }
+  for (i=0; i<scMsgLength; i++)
+    {
+    if (datac[i] != static_cast<char>(i))
+      {
+      cerr << "Server error: Corrupt char array." << endl;
+      CleanUp(comm, contr);
+      return 1;
+      }
+    }
+
+  unsigned char datauc[scMsgLength];
+  if (!comm->Receive(datauc, scMsgLength, 1, 44))
+    {
+    cerr << "Server error: Error receiving data." << endl;
+    CleanUp(comm, contr);
+    return 1;
+    }
+  for (i=0; i<scMsgLength; i++)
+    {
+    if (datauc[i] != static_cast<unsigned char>(i))
+      {
+      cerr << "Server error: Corrupt unsigned char array." << endl;
+      CleanUp(comm, contr);
+      return 1;
+      }
+    }
+
+  float dataf[scMsgLength];
+  if (!comm->Receive(dataf, scMsgLength, 1, 7))
+    {
+    cerr << "Server error: Error receiving data." << endl;
+    CleanUp(comm, contr);
+    return 1;
+    }
+  for (i=0; i<scMsgLength; i++)
+    {
+    if (dataf[i] != static_cast<float>(i))
+      {
+      cerr << "Server error: Corrupt float array." << endl;
+      CleanUp(comm, contr);
+      return 1;
+      }
+    }
+
+
+  double datad[scMsgLength];
+  if (!comm->Receive(datad, scMsgLength, 1, 7))
+    {
+    cerr << "Server error: Error receiving data." << endl;
+    CleanUp(comm, contr);
+    return 1;
+    }
+  for (i=0; i<scMsgLength; i++)
+    {
+    if (datad[i] != static_cast<double>(i))
+      {
+      cerr << "Server error: Corrupt double array." << endl;
+      CleanUp(comm, contr);
+      return 1;
+      }
+    }
+
+  vtkIdType datait[scMsgLength];
+  if (!comm->Receive(datait, scMsgLength, 1, 7))
+    {
+    cerr << "Server error: Error receiving data." << endl;
+    CleanUp(comm, contr);
+    return 1;
+    }
+  for (i=0; i<scMsgLength; i++)
+    {
+    if (datait[i] != static_cast<vtkIdType>(i))
+      {
+      cerr << "Server error: Corrupt vtkIdType array." << endl;
+      CleanUp(comm, contr);
+      return 1;
+      }
+    }
+
+
+  // Test sending vtkDataObject
+  vtkUnstructuredGridReader* ugrid = vtkUnstructuredGridReader::New();
+  char* fname = vtkExpandDataFileName(argc, argv, "Data/blow.vtk");
+  ugrid->SetFileName(fname);
+  delete[] fname;
+
+  ugrid->Update();
+
+  if (!comm->Send(ugrid->GetOutput(), 1, 9))
+    {
+    cerr << "Server error: Error sending data." << endl;
+    CleanUp(comm, contr);
+    ugrid->Delete();
+    return 1;
+    }
+  ugrid->Delete();
+
+  // Test receiving vtkDataArray
+  vtkDoubleArray* da = vtkDoubleArray::New();
+  da->SetNumberOfComponents(4);
+  da->SetNumberOfTuples(10);
+  for(i=0; i<40; i++)
+    {
+    da->SetValue(i,static_cast<double>(i));
+    }
+  if (!comm->Send(da, 1, 9))
+    {
+    cerr << "Client error: Error sending data." << endl;
+    CleanUp(comm, contr);
+    da->Delete();
+    return 1;
+    }
+  da->Delete();
+
+  contr->SetCommunicator(comm);
+
+  vtkOutputPort* op = vtkOutputPort::New();
+  op->SetController(contr);
+  op->SetTag(45);
+
+  vtkBYUReader* pd = vtkBYUReader::New();
+  fname = vtkExpandDataFileName(argc, argv, "Data/teapot.g");
+  pd->SetGeometryFileName(fname);
+  delete[] fname;
+
+  pd->Update();
+
+  op->SetInput(pd->GetOutput());
+  op->WaitForUpdate();
+  pd->Delete();
+
+  vtkRectilinearGridReader* rgrid = vtkRectilinearGridReader::New();
+  fname = vtkExpandDataFileName(argc, argv, "Data/RectGrid2.vtk");
+  rgrid->SetFileName(fname);
+  delete[] fname;
+
+  rgrid->Update();
+
+  op->SetInput(rgrid->GetOutput());
+  op->WaitForUpdate();
+  rgrid->Delete();
+
+  vtkStructuredPointsReader* spgrid = vtkStructuredPointsReader::New();
+  fname = vtkExpandDataFileName(argc, argv, "Data/ironProt.vtk");
+  spgrid->SetFileName(fname);
+  delete[] fname;
+
+  spgrid->Update();
+
+  op->SetInput(spgrid->GetOutput());
+  op->WaitForUpdate();
+  spgrid->Delete();
+
+  vtkPLOT3DReader* pl3d = vtkPLOT3DReader::New();
+  fname = vtkExpandDataFileName(argc, argv, "Data/combxyz.bin");
+  pl3d->SetXYZFileName(fname);
+  delete[] fname;
+  fname = vtkExpandDataFileName(argc, argv, "Data/combq.bin");
+  pl3d->SetQFileName(fname);
+  delete[] fname;
+  pl3d->SetScalarFunctionNumber(100);
+  pl3d->SetVectorFunctionNumber(202);
+
+  pl3d->Update();
+
+  op->SetInput(pl3d->GetOutput());
+  op->WaitForUpdate();
+  pl3d->Delete();
+
+  vtkPNMReader* imageData = vtkPNMReader::New();
+  fname = vtkExpandDataFileName(argc, argv, "Data/earth.ppm");
+  imageData->SetFileName(fname);
+  delete[] fname;
+
+  imageData->Update();
+
+  op->SetInput(imageData->GetOutput());
+  op->WaitForUpdate();
+  imageData->Delete();
+
+  op->Delete();
+  CleanUp(comm, contr);
+  return 0;
+}
