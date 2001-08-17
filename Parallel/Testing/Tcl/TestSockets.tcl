372a22305bbe27dc614ba3d2b5a064447637d04c
Berk Geveci <berk.geveci@kitware.com>
2001-08-17 16:51:37 -0400

    Better error handling.

--- Parallel/Testing/Tcl/TestSockets.tcl
+++ Parallel/Testing/Tcl/TestSockets.tcl
@@ -1,5 +1,2 @@
 exec [lindex $argv 0] -D [lindex $argv 2] &
-set [catch {exec [lindex $argv 1] -D [lindex $argv 2] -V [lindex $argv 3]}] errs
-if {[info exists errs]} {
-    puts stderr "Sockets test failed: $errs"
-}
+exec [lindex $argv 1] -D [lindex $argv 2] -V [lindex $argv 3]
