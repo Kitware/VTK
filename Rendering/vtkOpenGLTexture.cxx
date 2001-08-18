13253f6f141bb6367f10302ad455ae54934363af
Yves Starreveld <ystarrev@uwo.ca>
2001-08-18 12:08:41 -0400

    maxDimGL type definition changed from int to GLint to make call to glGetIntegerv happy

--- Rendering/vtkOpenGLTexture.cxx
+++ Rendering/vtkOpenGLTexture.cxx
@@ -210,7 +210,7 @@ void vtkOpenGLTexture::Load(vtkRenderer *ren)
       {
       resampleNeeded = 1;
       }
-    int maxDimGL;
+    GLint maxDimGL;
     glGetIntegerv(GL_MAX_TEXTURE_SIZE,&maxDimGL);
     // if larger than permitted by the graphics library then must resample
     if ( xsize > maxDimGL || ysize > maxDimGL )
@@ -390,7 +390,7 @@ static int FindPowerOfTwo(int i)
   // [these lines added by Tim Hutton (implementing Joris Vanden Wyngaerd's suggestions)]
   // limit the size of the texture to the maximum allowed by OpenGL
   // (slightly more graceful than texture failing but not ideal)
-  int maxDimGL;
+  GLint maxDimGL;
   glGetIntegerv(GL_MAX_TEXTURE_SIZE,&maxDimGL);
   if ( size > maxDimGL )
     {
