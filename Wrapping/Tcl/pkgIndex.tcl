a0ac9fe26f4c2ac39f9a00041aa27ed18ea6f14c
Sebastien Barre <sebastien.barre@kitware.com>
2001-08-20 11:46:08 -0400

    Split the package into initialization (vtktcl) and interaction code (vtktcl_widgets)

--- Wrapping/Tcl/pkgIndex.tcl
+++ Wrapping/Tcl/pkgIndex.tcl
@@ -43,43 +43,52 @@ package ifneeded vtktcl 3.3 {
             puts $errormsg
             set ok 0
         } else {
-           # Try to load the other components
-           __temp_try_to_load_vtk_lib vtkFilteringTCL
-           __temp_try_to_load_vtk_lib vtkGraphicsTCL
-           __temp_try_to_load_vtk_lib vtkImagingTCL
-           __temp_try_to_load_vtk_lib vtkHybridTCL
-           __temp_try_to_load_vtk_lib vtkIOTCL
-           __temp_try_to_load_vtk_lib vtkParallelTCL
-           __temp_try_to_load_vtk_lib vtkPatentedTCL
-           __temp_try_to_load_vtk_lib vtkRenderingTCL
+            # Try to load the other components
+            __temp_try_to_load_vtk_lib vtkFilteringTCL
+            __temp_try_to_load_vtk_lib vtkGraphicsTCL
+            __temp_try_to_load_vtk_lib vtkImagingTCL
+            __temp_try_to_load_vtk_lib vtkHybridTCL
+            __temp_try_to_load_vtk_lib vtkIOTCL
+            __temp_try_to_load_vtk_lib vtkParallelTCL
+            __temp_try_to_load_vtk_lib vtkPatentedTCL
+            __temp_try_to_load_vtk_lib vtkRenderingTCL
         }
-     }
-     
-     # Try to set the exit method of the interactor (needs Rendering)
-     if {[info commands vtkWin32RenderWindowInteractor] != ""} {
-        if {[catch {
-           vtkWin32RenderWindowInteractor __temp_vtkwin32iren__
-           __temp_vtkwin32iren__ SetClassExitMethod exit
-           __temp_vtkwin32iren__ Delete
-        } errormsg ]} {
-           # warn the user
-           puts $errormsg
-        }
-     }
-  
-     # set VTK_DATA if we can, first look at environment vars
-     if { [catch {set VTK_DATA_ROOT $env(VTK_DATA_ROOT)}] != 0} { 
-       # then look at command line args
-       set vtkDataFound 0
-       for {set i 0} {$i < [expr $argc - 1]} {incr i} {
-          if {[lindex $argv $i] == "-D"} {
-             set vtkDataFound 1
-             set VTK_DATA_ROOT [lindex $argv [expr $i + 1]]
-          }
-       }
-       # make a final guess at a relativepath
-       if {$vtkDataFound == 0} {set VTK_DATA_ROOT "../../../../VTKData" }
     }
+    
+    # Try to set the exit method of the interactor (needs Rendering)
+    if {[info commands vtkWin32RenderWindowInteractor] != ""} {
+        if {[catch {
+            vtkWin32RenderWindowInteractor __temp_vtkwin32iren__
+            __temp_vtkwin32iren__ SetClassExitMethod exit
+            __temp_vtkwin32iren__ Delete
+        } errormsg ]} {
+            # warn the user
+            puts $errormsg
+        }
+    }
+    
+    # set VTK_DATA if we can, first look at environment vars
+    if { [catch {set VTK_DATA_ROOT $env(VTK_DATA_ROOT)}] != 0} { 
+        # then look at command line args
+        set vtkDataFound 0
+        for {set i 0} {$i < [expr $argc - 1]} {incr i} {
+            if {[lindex $argv $i] == "-D"} {
+                set vtkDataFound 1
+                set VTK_DATA_ROOT [lindex $argv [expr $i + 1]]
+            }
+        }
+        # make a final guess at a relativepath
+        if {$vtkDataFound == 0} {set VTK_DATA_ROOT "../../../../VTKData" }
+    }
+    
+    if {$ok} {
+        package provide vtktcl 3.3
+    }
+}
+
+package ifneeded vtktcl_widgets 1.0 {
+
+    package require vtktcl
 
     set widgetObjectFound 0
     foreach dir $auto_path {
@@ -87,7 +96,7 @@ package ifneeded vtktcl 3.3 {
 	if {[file exists $fname]} {
 	    set widgetObjectFound 1
 	    source $fname
-           break
+            break
 	}
     }
 
@@ -96,7 +105,7 @@ package ifneeded vtktcl 3.3 {
 	    set fname $dir/TkInteractor.tcl
 	    if {[file exists $fname]} {
 		source $fname
-               break
+                break
 	    }
 	}
     }
@@ -113,96 +122,96 @@ package ifneeded vtktcl 3.3 {
     set vtkInteractCommandIndex 0
     
     proc vtkInteract {} {
-       global vtkInteractCommandList vtkInteractCommandIndex
-       global vtkInteractTagcount
-       
-       proc dovtk {s w} {
-          global vtkInteractBold vtkInteractNormal vtkInteractTagcount 
-          global vtkInteractCommandList vtkInteractCommandIndex
-          
-          set tag [append tagnum $vtkInteractTagcount]
-          set vtkInteractCommandIndex $vtkInteractTagcount
-          incr vtkInteractTagcount 1
-          .vtkInteract.display.text configure -state normal
-          .vtkInteract.display.text insert end $s $tag
-          set vtkInteractCommandList [linsert $vtkInteractCommandList end $s]
-          eval .vtkInteract.display.text tag configure $tag $vtkInteractNormal
-          .vtkInteract.display.text tag bind $tag <Any-Enter> \
-                ".vtkInteract.display.text tag configure $tag $vtkInteractBold"
-          .vtkInteract.display.text tag bind $tag <Any-Leave> \
-                ".vtkInteract.display.text tag configure $tag $vtkInteractNormal"
-          .vtkInteract.display.text tag bind $tag <1> "dovtk [list $s] .vtkInteract"
-          .vtkInteract.display.text insert end \n;
-          .vtkInteract.display.text insert end [uplevel 1 $s]
-          .vtkInteract.display.text insert end \n\n
-          .vtkInteract.display.text configure -state disabled
-          .vtkInteract.display.text yview end
-       }
-       
-       catch {destroy .vtkInteract}
-       toplevel .vtkInteract -bg #bbbbbb
-       wm title .vtkInteract "vtk Interactor"
-       wm iconname .vtkInteract "vtk"
-       
-       frame .vtkInteract.buttons -bg #bbbbbb
-       pack  .vtkInteract.buttons -side bottom -fill both -expand 0 -pady 2m
-       button .vtkInteract.buttons.dismiss -text Dismiss \
-             -command "wm withdraw .vtkInteract" \
-             -bg #bbbbbb -fg #221133 -activebackground #cccccc -activeforeground #221133
-       pack .vtkInteract.buttons.dismiss -side left -expand 1 -fill x
-       
-       frame .vtkInteract.file -bg #bbbbbb
-       label .vtkInteract.file.label -text "Command:" -width 10 -anchor w \
-             -bg #bbbbbb -fg #221133
-       entry .vtkInteract.file.entry -width 40 \
-             -bg #dddddd -fg #221133 -highlightthickness 1 -highlightcolor #221133
-       bind .vtkInteract.file.entry <Return> {
-          dovtk [%W get] .vtkInteract; %W delete 0 end}
-          pack .vtkInteract.file.label -side left
-          pack .vtkInteract.file.entry -side left -expand 1 -fill x
-          
-          frame .vtkInteract.display -bg #bbbbbb
-          text .vtkInteract.display.text -yscrollcommand ".vtkInteract.display.scroll set" \
+        global vtkInteractCommandList vtkInteractCommandIndex
+        global vtkInteractTagcount
+        
+        proc dovtk {s w} {
+            global vtkInteractBold vtkInteractNormal vtkInteractTagcount 
+            global vtkInteractCommandList vtkInteractCommandIndex
+            
+            set tag [append tagnum $vtkInteractTagcount]
+            set vtkInteractCommandIndex $vtkInteractTagcount
+            incr vtkInteractTagcount 1
+            .vtkInteract.display.text configure -state normal
+            .vtkInteract.display.text insert end $s $tag
+            set vtkInteractCommandList [linsert $vtkInteractCommandList end $s]
+            eval .vtkInteract.display.text tag configure $tag $vtkInteractNormal
+            .vtkInteract.display.text tag bind $tag <Any-Enter> \
+                    ".vtkInteract.display.text tag configure $tag $vtkInteractBold"
+            .vtkInteract.display.text tag bind $tag <Any-Leave> \
+                    ".vtkInteract.display.text tag configure $tag $vtkInteractNormal"
+            .vtkInteract.display.text tag bind $tag <1> "dovtk [list $s] .vtkInteract"
+            .vtkInteract.display.text insert end \n;
+            .vtkInteract.display.text insert end [uplevel 1 $s]
+            .vtkInteract.display.text insert end \n\n
+            .vtkInteract.display.text configure -state disabled
+            .vtkInteract.display.text yview end
+        }
+        
+        catch {destroy .vtkInteract}
+        toplevel .vtkInteract -bg #bbbbbb
+        wm title .vtkInteract "vtk Interactor"
+        wm iconname .vtkInteract "vtk"
+        
+        frame .vtkInteract.buttons -bg #bbbbbb
+        pack  .vtkInteract.buttons -side bottom -fill both -expand 0 -pady 2m
+        button .vtkInteract.buttons.dismiss -text Dismiss \
+                -command "wm withdraw .vtkInteract" \
+                -bg #bbbbbb -fg #221133 -activebackground #cccccc -activeforeground #221133
+        pack .vtkInteract.buttons.dismiss -side left -expand 1 -fill x
+        
+        frame .vtkInteract.file -bg #bbbbbb
+        label .vtkInteract.file.label -text "Command:" -width 10 -anchor w \
+                -bg #bbbbbb -fg #221133
+        entry .vtkInteract.file.entry -width 40 \
+                -bg #dddddd -fg #221133 -highlightthickness 1 -highlightcolor #221133
+        bind .vtkInteract.file.entry <Return> {
+            dovtk [%W get] .vtkInteract; %W delete 0 end
+        }
+        pack .vtkInteract.file.label -side left
+        pack .vtkInteract.file.entry -side left -expand 1 -fill x
+        
+        frame .vtkInteract.display -bg #bbbbbb
+        text .vtkInteract.display.text -yscrollcommand ".vtkInteract.display.scroll set" \
                 -setgrid true -width 60 -height 8 -wrap word -bg #dddddd -fg #331144 \
                 -state disabled
-          scrollbar .vtkInteract.display.scroll \
+        scrollbar .vtkInteract.display.scroll \
                 -command ".vtkInteract.display.text yview" -bg #bbbbbb \
                 -troughcolor #bbbbbb -activebackground #cccccc -highlightthickness 0 
-          pack .vtkInteract.display.text -side left -expand 1 -fill both
-          pack .vtkInteract.display.scroll -side left -expand 0 -fill y
-          
-          pack .vtkInteract.display -side bottom -expand 1 -fill both
-          pack .vtkInteract.file -pady 3m -padx 2m -side bottom -fill x 
-          
-          set vtkInteractCommandIndex 0
-          
-          bind .vtkInteract <Down> {
-             if { $vtkInteractCommandIndex < [expr $vtkInteractTagcount - 1] } {
+        pack .vtkInteract.display.text -side left -expand 1 -fill both
+        pack .vtkInteract.display.scroll -side left -expand 0 -fill y
+        
+        pack .vtkInteract.display -side bottom -expand 1 -fill both
+        pack .vtkInteract.file -pady 3m -padx 2m -side bottom -fill x 
+        
+        set vtkInteractCommandIndex 0
+        
+        bind .vtkInteract <Down> {
+            if { $vtkInteractCommandIndex < [expr $vtkInteractTagcount - 1] } {
                 incr vtkInteractCommandIndex
                 set command_string [lindex $vtkInteractCommandList $vtkInteractCommandIndex]
                 .vtkInteract.file.entry delete 0 end
                 .vtkInteract.file.entry insert end $command_string
-             } elseif { $vtkInteractCommandIndex == [expr $vtkInteractTagcount - 1] } {
+            } elseif { $vtkInteractCommandIndex == [expr $vtkInteractTagcount - 1] } {
                 .vtkInteract.file.entry delete 0 end
-             }
-          }
-          
-          bind .vtkInteract <Up> {
-             if { $vtkInteractCommandIndex > 0 } { 
+            }
+        }
+        
+        bind .vtkInteract <Up> {
+            if { $vtkInteractCommandIndex > 0 } { 
                 set vtkInteractCommandIndex [expr $vtkInteractCommandIndex - 1]
                 set command_string [lindex $vtkInteractCommandList $vtkInteractCommandIndex]
                 .vtkInteract.file.entry delete 0 end
                 .vtkInteract.file.entry insert end $command_string
-             }
-          }
-          
-          wm withdraw .vtkInteract
-       }
-       vtkInteract
-       
-    if {$ok} {
-        package provide vtktcl 3.3
+            }
+        }
+        
+        wm withdraw .vtkInteract
     }
+
+    vtkInteract
+
+    package provide vtktcl_widgets 1.0
 }
 
 package ifneeded vtkpython 3.3 {
@@ -237,11 +246,11 @@ package ifneeded vtkpython 3.3 {
 
     set errormsg [__ptemp_try_to_load_vtk_lib vtkPythonTkWidgets]
     if {$errormsg != ""} {
-	puts $errormsg
-	set ok 0
+        puts $errormsg
+        set ok 0
     }
 
     if {$ok} {
-	package provide vtkpython 3.3
+        package provide vtkpython 3.3
     }
 }
