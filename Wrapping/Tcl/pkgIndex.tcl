aaa341e73eab6548c79560f418890350e73900a6
Sebastien Barre <sebastien.barre@kitware.com>
2001-08-21 19:10:20 -0400

    create the ::vtktcl namespace, and put our stuff right there

--- Wrapping/Tcl/pkgIndex.tcl
+++ Wrapping/Tcl/pkgIndex.tcl
@@ -1,13 +1,18 @@
 # Tcl package index file, version 1.0
-uplevel #0 "set vtkpackagehome $dir"
+
+namespace eval ::vtktcl {
+    variable package_index_dir [file dirname [info script]] 
+}
 
 package ifneeded vtktcl 3.3 {
 
-    # Windows: 'load' looks in the same dir as the Tcl/Tk app, the current dir,
-    # c:\window\system[32], c:\windows and the dirs listed in the PATH env var.
-    # Unix: 'load' looks in dirs listed in the LD_LIBRARY_PATH env var.
+    # Windows: the 'load' command looks in the same dir as the Tcl/Tk app,
+    # the current dir, c:\window\system[32], c:\windows and the dirs 
+    # listed in the PATH env var.
+    # Unix: the 'load' command looks in dirs listed in the 
+    # LD_LIBRARY_PATH env var.
 
-    proc __temp_try_to_load_vtk_lib {name} {
+    proc ::vtktcl::load_component {name} {
         global tcl_platform
         # First dir is empty, to let Tcl try a relative name
         set dirs {""}
@@ -33,33 +38,35 @@ package ifneeded vtktcl 3.3 {
         return "$name could not be found!"
     }
 
-    # I won't 'rename __temp_try_to_load_vtk_lib ""' because it will be quite
-    # useful for user-defined packages depending on vtktcl.
+    # I won't 'rename ::vtktcl::load_component ""' because it will be 
+    # quite useful for user-defined packages depending on vtktcl.
 
     set ok 1
 
     # Check if vtk commands are already there and try to
     # load the libraries only if they are not
+
     if {[catch {vtkCommand ListAllInstances}]} {
         # Try to load at least the Common part
-        set errormsg [__temp_try_to_load_vtk_lib vtkCommonTCL]
+        set errormsg [::vtktcl::load_component vtkCommonTCL]
         if {$errormsg != ""} {
             puts $errormsg
             set ok 0
         } else {
             # Try to load the other components
-            __temp_try_to_load_vtk_lib vtkFilteringTCL
-            __temp_try_to_load_vtk_lib vtkGraphicsTCL
-            __temp_try_to_load_vtk_lib vtkImagingTCL
-            __temp_try_to_load_vtk_lib vtkHybridTCL
-            __temp_try_to_load_vtk_lib vtkIOTCL
-            __temp_try_to_load_vtk_lib vtkParallelTCL
-            __temp_try_to_load_vtk_lib vtkPatentedTCL
-            __temp_try_to_load_vtk_lib vtkRenderingTCL
+            ::vtktcl::load_component vtkFilteringTCL
+            ::vtktcl::load_component vtkGraphicsTCL
+            ::vtktcl::load_component vtkImagingTCL
+            ::vtktcl::load_component vtkHybridTCL
+            ::vtktcl::load_component vtkIOTCL
+            ::vtktcl::load_component vtkParallelTCL
+            ::vtktcl::load_component vtkPatentedTCL
+            ::vtktcl::load_component vtkRenderingTCL
         }
     }
     
     # Try to set the exit method of the interactor (needs Rendering)
+
     if {[info commands vtkWin32RenderWindowInteractor] != ""} {
         if {[catch {
             vtkWin32RenderWindowInteractor __temp_vtkwin32iren__
@@ -72,6 +79,7 @@ package ifneeded vtktcl 3.3 {
     }
     
     # set VTK_DATA if we can, first look at environment vars
+
     if { [catch {set VTK_DATA_ROOT $env(VTK_DATA_ROOT)}] != 0} { 
         # then look at command line args
         set vtkDataFound 0
@@ -82,7 +90,9 @@ package ifneeded vtktcl 3.3 {
             }
         }
         # make a final guess at a relativepath
-        if {$vtkDataFound == 0} {set VTK_DATA_ROOT "../../../../VTKData" }
+        if {$vtkDataFound == 0} {
+            set VTK_DATA_ROOT "../../../../VTKData" 
+        }
     }
 
     if {$ok} {
@@ -90,13 +100,11 @@ package ifneeded vtktcl 3.3 {
     }
 }
 
-   
 package ifneeded vtktcl_interactor 1.0 {
 
     package require vtktcl
-    global vtkpackagehome
 
-    source [file join $vtkpackagehome Interactor.tcl]
+    source [file join $::vtktcl::package_index_dir Interactor.tcl]
 
     package provide vtktcl_interactor 1.0
 }
@@ -105,19 +113,17 @@ package ifneeded vtktcl_interactor 1.0 {
 package ifneeded vtktcl_widgets 1.0 {
 
     package require vtktcl_interactor
-    global vtkpackagehome
 
-    source [file join $vtkpackagehome WidgetObject.tcl]
-    source [file join $vtkpackagehome TkInteractor.tcl]
+    source [file join $::vtktcl::package_index_dir WidgetObject.tcl]
+    source [file join $::vtktcl::package_index_dir TkInteractor.tcl]
 
     package provide vtktcl_widgets 1.0
 }
 
 
 package ifneeded vtktcl_colors 1.0 {
- 
-    global vtkpackagehome
-    source [file join $vtkpackagehome colors.tcl]
+    
+    source [file join $::vtktcl::package_index_dir colors.tcl]
 
     package provide vtktcl_colors 1.0
 }
