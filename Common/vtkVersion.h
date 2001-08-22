640301424521c63d8711ffa7961e4dc19d1cb78a
Ken Martin <ken.martin@kitware.com>
2001-08-22 15:32:43 -0400

    updated version number

--- Common/vtkVersion.h
+++ Common/vtkVersion.h
@@ -55,11 +55,11 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "vtkObject.h"
 
-#define VTK_MAJOR_VERSION 3
-#define VTK_MINOR_VERSION 2
+#define VTK_MAJOR_VERSION 4
+#define VTK_MINOR_VERSION 0
 #define VTK_BUILD_VERSION 0
-#define VTK_VERSION "3.2.0"
-#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.946 $, $Date: 2001-08-13 23:59:06 $ (GMT)"
+#define VTK_VERSION "4.0.0"
+#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.947 $, $Date: 2001-08-22 19:32:43 $ (GMT)"
 
 
 class VTK_EXPORT vtkVersion : public vtkObject {
