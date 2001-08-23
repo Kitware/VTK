a3930d0a568fe80015aa1015b13d65f0c7376b09
Jim Miller <millerjv@crd.ge.com>
2001-08-23 11:25:18 -0400

    new test

---
+++ Graphics/Testing/Cxx/CellLocator.cxx
@@ -0,0 +1,104 @@
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderer.h"
+#include "vtkSphereSource.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkActor.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkCellLocator.h"
+
+#include "vtkRegressionTestImage.h"
+#include "vtkDebugLeaks.h"
+
+int main( int argc, char *argv[] )
+{
+  vtkDebugLeaks::PromptUserOff();
+
+  vtkRenderer *renderer = vtkRenderer::New();
+  vtkRenderWindow *renWin = vtkRenderWindow::New();
+    renWin->AddRenderer(renderer);
+  vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
+    iren->SetRenderWindow(renWin);
+
+  vtkSphereSource *sphere = vtkSphereSource::New();
+    sphere->SetThetaResolution(8); sphere->SetPhiResolution(8);
+    sphere->SetRadius(1.0);
+    sphere->Update();
+  vtkPolyDataMapper *sphereMapper = vtkPolyDataMapper::New();
+    sphereMapper->SetInput(sphere->GetOutput());
+  vtkActor *sphereActor = vtkActor::New();
+    sphereActor->SetMapper(sphereMapper);
+    
+  vtkSphereSource *spot = vtkSphereSource::New();
+    spot->SetPhiResolution(6);
+    spot->SetThetaResolution(6);
+    spot->SetRadius(0.1);
+
+  vtkPolyDataMapper *spotMapper = vtkPolyDataMapper::New();
+    spotMapper->SetInput(spot->GetOutput());
+
+  // Build a locator 
+  vtkCellLocator *cellLocator = vtkCellLocator::New();
+  cellLocator->SetDataSet(sphere->GetOutput());
+  cellLocator->BuildLocator();
+
+  // Intersect with line
+  float p1[] = {2.0, 1.0, 3.0};
+  float p2[] = {0.0, 0.0, 0.0};
+  float t, ptline[3], pcoords[3];
+  int subId;
+  cellLocator->IntersectWithLine(p1, p2, 0.001, t, ptline, pcoords, subId);
+
+  vtkActor *intersectLineActor = vtkActor::New();
+    intersectLineActor->SetMapper(spotMapper);
+    intersectLineActor->SetPosition(ptline);
+    intersectLineActor->GetProperty()->SetColor(1.0, 0.0, 0.0);
+
+  // Find closest point
+  vtkIdType cellId;
+  float dist;
+  p1[0] = -2.4; p1[1] = -0.9;
+  cellLocator->FindClosestPoint(p1, ptline, cellId, subId, dist);
+  vtkActor *closestPointActor = vtkActor::New();
+    closestPointActor->SetMapper(spotMapper);
+    closestPointActor->SetPosition(ptline);
+    closestPointActor->GetProperty()->SetColor(0.0, 1.0, 0.0);
+
+  // Find closest point within radius
+  float radius = 5.0;
+  p1[0] = .2; p1[1] = 1.0; p1[2] = 1.0;
+  cellLocator->FindClosestPointWithinRadius(p1, radius, ptline, cellId, subId, dist);
+  vtkActor *closestPointActor2 = vtkActor::New();
+    closestPointActor2->SetMapper(spotMapper);
+    closestPointActor2->SetPosition(ptline);
+    closestPointActor2->GetProperty()->SetColor(0.0, 1.0, 0.0);
+  
+  renderer->AddActor(sphereActor);
+  renderer->AddActor(intersectLineActor);
+  renderer->AddActor(closestPointActor);
+  renderer->AddActor(closestPointActor2);
+  renderer->SetBackground(1,1,1);
+  renWin->SetSize(300,300);
+
+  // interact with data
+  renWin->Render();
+
+  int retVal = vtkRegressionTestImage( renWin );
+
+  // Clean up
+  renderer->Delete();
+  renWin->Delete();
+  iren->Delete();
+  sphere->Delete();
+  sphereMapper->Delete();
+  sphereActor->Delete();
+  spot->Delete();
+  spotMapper->Delete();
+  intersectLineActor->Delete();
+  closestPointActor->Delete();
+  closestPointActor2->Delete();
+  cellLocator->FreeSearchStructure();
+  cellLocator->Delete();
+
+  return !retVal;
+}
