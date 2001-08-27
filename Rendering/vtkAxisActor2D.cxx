1ed2d9bc182f4d003748a1fc08fc3a629b42da96
Charles Law <charles.law@kitware.com>
2001-08-27 11:16:30 -0400

    SGI Compile problem

--- Rendering/vtkAxisActor2D.cxx
+++ Rendering/vtkAxisActor2D.cxx
@@ -496,7 +496,7 @@ void vtkAxisActor2D::ComputeRange(float inRange[2], float outRange[2],
   static float divs[VTK_NUM_DIVS] = {10.0, 8.0, 5.0, 4.0, 2.0, 1.0,
 				     0.5, 0.25, 0.20, 0.125, 0.10};
   float range = fabs(inRange[1]-inRange[0]), sRange[2];
-  float logFactor = (float)pow(10.0, floor(log10(range)));
+  float logFactor = (float)pow((double)(10.0), (double)(floor(log10(range))));
   float lastInterval;
   int j;
 
@@ -527,7 +527,7 @@ void vtkAxisActor2D::ComputeRange(float inRange[2], float outRange[2],
       //recompute range
       range = fabs(sRange[1]-sRange[0]);
       //recompute logfactor
-      logFactor = (float)pow(10.0, floor(log10(range)));
+      logFactor = (float)pow((double)(10.0), (double)(floor(log10(range))));
       //recompute inRange so things get flipped correctly at the end
       inRange[0] = sRange[0];
       inRange[1] = sRange[1];
