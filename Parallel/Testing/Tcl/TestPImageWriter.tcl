5ed7abbd3fe6ad3635aa7df74a11b6bc7b60076f
Berk Geveci <berk.geveci@kitware.com>
2001-08-29 10:45:06 -0400

    New test

---
+++ Parallel/Testing/Tcl/TestPImageWriter.tcl
@@ -0,0 +1,35 @@
+package require vtktcl
+
+# Image pipeline
+
+vtkTIFFReader image1
+  image1 SetFileName "$VTK_DATA_ROOT/Data/beach.tif"
+  image1 Update
+
+#
+# If the current directory is writable, then test the witers
+#
+
+if {[catch {set channel [open test.tmp w]}] == 0 } {
+    close $channel
+    file delete -force test.tmp
+    
+    vtkPImageWriter piw
+    piw SetInput [image1 GetOutput]
+    piw SetFileName piw.raw
+    
+    piw Write
+
+    file delete -force piw.raw
+}
+
+vtkImageViewer viewer
+viewer SetInput [image1 GetOutput]
+viewer SetColorWindow 255
+viewer SetColorLevel 127.5
+
+wm withdraw .
+
+viewer Render
+
+
