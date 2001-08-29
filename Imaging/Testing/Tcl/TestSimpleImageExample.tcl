22a93ab34d87bc1c70ecb223aca044fb353e76d5
Ken Martin <ken.martin@kitware.com>
2001-08-29 16:43:38 -0400

    added test

---
+++ Imaging/Testing/Tcl/TestSimpleImageExample.tcl
@@ -0,0 +1,20 @@
+package require vtktcl
+
+
+
+# Image pipeline
+
+vtkPNGReader reader
+reader SetFileName $VTK_DATA_ROOT/Data/fullhead15.png
+
+vtkSimpleImageFilterExample gradient
+gradient SetInput [reader GetOutput]
+
+vtkImageViewer viewer
+#viewer DebugOn
+viewer SetInput [gradient GetOutput]
+viewer SetColorWindow 1000
+viewer SetColorLevel 500
+
+
+viewer Render
