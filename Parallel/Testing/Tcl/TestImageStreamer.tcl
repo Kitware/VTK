df52aa38c8c5dd2de2848c2ed037987999888cf5
Berk Geveci <berk.geveci@kitware.com>
2001-08-29 10:07:05 -0400

    Adding new tests.

---
+++ Parallel/Testing/Tcl/TestImageStreamer.tcl
@@ -0,0 +1,55 @@
+package require vtktcl
+
+vtkImageReader reader
+reader ReleaseDataFlagOff
+reader SetDataByteOrderToLittleEndian
+reader SetDataExtent 0 63 0 63 1 93
+reader SetFilePrefix "$VTK_DATA_ROOT/Data/headsq/quarter"
+reader SetDataMask 0x7fff
+
+set rangeStart 0.0
+set rangeEnd   0.2
+
+vtkLookupTable LUT 
+LUT SetTableRange 0 1800
+LUT SetSaturationRange 1 1
+LUT SetHueRange $rangeStart $rangeEnd
+LUT SetValueRange 1 1  
+LUT SetAlphaRange 0 0
+LUT Build
+
+proc changeLUT { } {
+    global rangeStart rangeEnd
+    
+    set rangeStart [expr $rangeStart + 0.1]
+    set rangeEnd   [expr $rangeEnd   + 0.1]
+    if { $rangeEnd > 1.0 } {
+	set rangeStart 0.0
+	set rangeEnd   0.2
+    }
+
+    LUT SetHueRange $rangeStart $rangeEnd
+    LUT Build
+}
+
+vtkImageMapToColors mapToRGBA
+mapToRGBA SetInput [reader GetOutput]
+mapToRGBA SetOutputFormatToRGBA
+mapToRGBA SetLookupTable LUT
+mapToRGBA SetEndMethod changeLUT
+
+vtkMemoryLimitImageDataStreamer streamer
+streamer SetInput [mapToRGBA GetOutput]
+streamer SetMemoryLimit 100
+streamer UpdateWholeExtent
+
+# set the window/level to 255.0/127.5 to view full range
+vtkImageViewer viewer
+viewer SetInput [streamer GetOutput]
+viewer SetColorWindow 255.0
+viewer SetColorLevel 127.5
+viewer SetZSlice 50
+
+wm withdraw .
+viewer Render
+
---
+++ Parallel/Testing/Tcl/TestUnstructuredPieces.tcl
@@ -0,0 +1,55 @@
+package require vtktcl_interactor
+
+vtkMath math
+math RandomSeed 22
+
+vtkParallelFactory pf
+pf RegisterFactory pf
+
+vtkPLOT3DReader pl3d
+pl3d SetXYZFileName "$VTK_DATA_ROOT/Data/combxyz.bin"
+pl3d SetQFileName "$VTK_DATA_ROOT/Data/combq.bin"
+pl3d SetScalarFunctionNumber 100
+
+vtkDataSetTriangleFilter dst
+dst SetInput [pl3d GetOutput]
+
+vtkExtractUnstructuredGridPiece extract
+extract SetInput [dst GetOutput]
+
+vtkContourFilter cf
+cf SetInput [extract GetOutput]
+cf SetValue 0 0.24
+
+vtkPolyDataNormals pdn
+pdn SetInput [cf GetOutput]
+
+vtkPieceScalars ps
+ps SetInput [pdn GetOutput]
+
+vtkPolyDataMapper mapper
+mapper SetInput [ps GetOutput]
+mapper SetNumberOfPieces 3
+
+vtkActor actor
+actor SetMapper mapper
+
+vtkRenderer ren
+ren AddActor actor
+set camera [ren GetActiveCamera]
+
+#$camera SetPosition 68.1939 -23.4323 12.6465
+#$camera SetViewUp 0.46563 0.882375 0.0678508  
+#$camera SetFocalPoint 3.65707 11.4552 1.83509 
+#$camera SetClippingRange 59.2626 101.825 
+
+vtkRenderWindow renWin
+renWin AddRenderer ren
+
+vtkRenderWindowInteractor iren
+iren SetRenderWindow renWin
+iren Initialize
+iren SetUserMethod {wm deiconify .vtkInteract}
+
+wm withdraw .
+
