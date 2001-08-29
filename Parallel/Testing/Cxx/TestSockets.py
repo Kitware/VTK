38e12039fe64c818c19e99402418e9e1334f4cd0
Berk Geveci <berk.geveci@kitware.com>
2001-08-29 13:19:51 -0400

    Driver for socket test.

---
+++ Parallel/Testing/Cxx/TestSockets.py
@@ -0,0 +1,21 @@
+""" Driver script for testing sockets
+    Unix only
+"""
+import os, sys, time
+
+# Fork, run server in child, client in parent
+pid = os.fork()
+if pid == 0:
+    # exec the parent
+    os.execv(sys.argv[1], ('-D', sys.argv[3]))
+else:
+    # wait a little to make sure that the server is ready
+    time.sleep(10)
+    # run the client
+    os.system('%s -D %s -V %s' % ( sys.argv[2], sys.argv[3],
+                                   sys.argv[4] ))
+    # in case the client fails, we need to kill the server
+    # or it will stay around
+    time.sleep(20)
+    os.kill(pid, 15)
+
