408c5b3d7317630bea2e14977af71d38100bd1d4
Will Schroeder <will.schroeder@kitware.com>
2001-08-30 14:51:04 -0400

    ENH:Cursor3D coverage

---
+++ Graphics/Testing/Tcl/cursor3D.tcl
@@ -0,0 +1,73 @@
+package require vtktcl_interactor
+
+vtkRenderer ren1
+vtkRenderWindow renWin
+  renWin AddRenderer ren1
+vtkRenderWindowInteractor iren
+  iren SetRenderWindow renWin
+
+  # read data
+vtkPLOT3DReader reader
+  reader SetXYZFileName "$VTK_DATA_ROOT/Data/combxyz.bin"
+  reader SetQFileName "$VTK_DATA_ROOT/Data/combq.bin"
+  reader SetScalarFunctionNumber 210
+  reader Update
+
+  # create outline
+vtkStructuredGridOutlineFilter outlineF
+  outlineF SetInput [reader GetOutput]
+vtkPolyDataMapper outlineMapper
+  outlineMapper SetInput [outlineF GetOutput]
+vtkActor outline
+  outline SetMapper outlineMapper
+  [outline GetProperty] SetColor 0 0 0
+
+  # create cursor
+vtkCursor3D cursor
+  eval cursor SetFocalPoint [[reader GetOutput] GetCenter]
+  eval cursor SetModelBounds [[reader GetOutput] GetBounds]
+  cursor AllOff
+  cursor AxesOn
+  cursor OutlineOn
+  cursor XShadowsOn
+  cursor YShadowsOn
+  cursor ZShadowsOn
+vtkPolyDataMapper cursorMapper
+  cursorMapper SetInput [cursor GetOutput]
+vtkActor cursorActor
+  cursorActor SetMapper cursorMapper
+  [cursorActor GetProperty] SetColor 1 0 0
+
+  # create probe
+vtkProbeFilter probe
+  probe SetSource [reader GetOutput]
+  probe SetInput [cursor GetFocus]
+
+  # create a cone geometry for glyph
+vtkConeSource cone
+  cone SetResolution 16
+  cone SetRadius 0.25
+
+  # create glyph
+vtkGlyph3D glyph
+  glyph SetInput [probe GetOutput]
+  glyph SetSource [cone GetOutput]
+  glyph SetVectorModeToUseVector
+  glyph SetScaleModeToScaleByScalar
+  glyph SetScaleFactor .0002
+vtkPolyDataMapper glyphMapper
+  glyphMapper SetInput [glyph GetOutput]
+vtkActor glyphActor
+  glyphActor SetMapper glyphMapper
+
+ren1 AddActor outline
+ren1 AddActor cursorActor
+ren1 AddActor glyphActor
+ren1 SetBackground 1.0 1.0 1.0
+renWin SetSize 200 200
+[ren1 GetActiveCamera] Elevation 60
+ren1 ResetCameraClippingRange
+renWin Render
+
+# prevent the tk window from showing up then start the event loop
+wm withdraw .
