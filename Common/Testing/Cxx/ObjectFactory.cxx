f2dc8617d828fd7a49d9f99635b9b51125f7d5ac
Ken Martin <ken.martin@kitware.com>
2001-08-30 10:22:09 -0400

    cleaned up some

--- Common/Testing/Cxx/ObjectFactory.cxx
+++ Common/Testing/Cxx/ObjectFactory.cxx
@@ -10,25 +10,23 @@ public:
    // Methods from vtkObject
   ~vtkTestVertex() 
     {
-      cout << "delete vtkTestVertex " << endl;
     }
   
   vtkTypeMacro(vtkTestVertex,vtkVertex);
   static vtkTestVertex* New() { return new vtkTestVertex; }
-  vtkTestVertex() { cout << "Create vtkTestVertex " << endl; }
+  vtkTestVertex() {  }
 };
 class vtkTestVertex2 : public vtkVertex
 {
 public:
   ~vtkTestVertex2() 
     {
-      cout << "delete vtkTestVertex2 " << endl;
     }
 
    // Methods from vtkObject
   vtkTypeMacro(vtkTestVertex2,vtkVertex);
   static vtkTestVertex2* New() { return new vtkTestVertex2; }
-  vtkTestVertex2() { cout << "Create vtkTestVertex2 " << endl; }
+  vtkTestVertex2() { }
 };
 
 VTK_CREATE_CREATE_FUNCTION(vtkTestVertex);
@@ -86,32 +84,23 @@ int main()
   vtkObjectFactory::RegisterFactory(factory);
   factory->Delete();
   vtkVertex* v = vtkVertex::New();
-  cout << v->GetClassName() << endl;
   TestNewVertex(v, "vtkTestVertex");
   v->Delete();
 
-  factory->Print(cout);
   // disable all vtkVertex creation with the
   factory->Disable("vtkVertex");
-  factory->Print(cout);
   v = vtkVertex::New();
-  cout << v->GetClassName() << endl;
   TestNewVertex(v, "vtkVertex");
   
   factory->SetEnableFlag(1, "vtkVertex", "vtkTestVertex2");
-  factory->Print(cout);
   v->Delete();
   v = vtkVertex::New();
-  cout << v->GetClassName() << endl;
   TestNewVertex(v, "vtkTestVertex2");
   
   factory->SetEnableFlag(0, "vtkVertex", "vtkTestVertex2");
-  factory->Print(cout);
   factory->SetEnableFlag(1, "vtkVertex", "vtkTestVertex");
-  factory->Print(cout);
   v->Delete();
   v = vtkVertex::New();
-  cout << v->GetClassName() << endl;
   TestNewVertex(v, "vtkTestVertex");
   v->Delete();
   vtkObjectFactory::UnRegisterAllFactories();
