bf5bf707359ecdad63becf476548837e0d606c3d
Bill Lorensen <lorensen@crd.ge.com>
2001-09-02 20:48:02 -0400

    ENH: better coverage.

--- Imaging/Testing/Tcl/voxelModel.tcl
+++ Imaging/Testing/Tcl/voxelModel.tcl
@@ -1,15 +1,12 @@
 package require vtktcl_interactor
 
-
-# get the interactor ui
-
 # Now create the RenderWindow, Renderer and Interactor
 #
 vtkRenderer ren1
 vtkRenderWindow renWin
-    renWin AddRenderer ren1
+  renWin AddRenderer ren1
 vtkRenderWindowInteractor iren
-    iren SetRenderWindow renWin
+  iren SetRenderWindow renWin
 
 vtkSphereSource sphereModel
   sphereModel SetThetaResolution 10
@@ -18,10 +15,13 @@ vtkSphereSource sphereModel
 vtkVoxelModeller voxelModel
   voxelModel SetInput [sphereModel GetOutput]
   voxelModel SetSampleDimensions 21 21 21
+  voxelModel SetModelBounds -1.5 1.5 -1.5 1.5 -1.5 1.5
 
 if {[catch {set channel [open voxelModel.vtk w]}] == 0 } {
    close $channel
-   file delete -force test.tmp
+   file delete -force voxelModel.vtk
+
+   voxelModel Write voxelDataFile
 
    vtkDataSetWriter aWriter
      aWriter SetFileName voxelModel.vtk
@@ -67,6 +67,7 @@ if {[catch {set channel [open voxelModel.vtk w]}] == 0 } {
 # prevent the tk window from showing up then start the event loop
    wm withdraw .
    file delete -force voxelModel.vtk
+   file delete -force voxelDataFile
 }
 
 
