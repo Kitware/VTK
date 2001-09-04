bec5fcf40a6eb2b6600c84858d3b65e28bf709a9
Ken Martin <ken.martin@kitware.com>
2001-09-04 09:30:20 -0400

    Nightly Version

--- Common/vtkVersion.h
+++ Common/vtkVersion.h
@@ -59,7 +59,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define VTK_MINOR_VERSION 0
 #define VTK_BUILD_VERSION 0
 #define VTK_VERSION "4.0.0"
-#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.963 $, $Date: 2001-09-04 13:05:37 $ (GMT)"
+#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.964 $, $Date: 2001-09-04 13:30:20 $ (GMT)"
 
 
 class VTK_EXPORT vtkVersion : public vtkObject {
