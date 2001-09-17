e1fc6cd020572544e5699ce0ace5f3456b63e8d6
Berk Geveci <berk.geveci@kitware.com>
2001-09-17 16:13:30 -0400

    I wonder why some compilers don't support ANSI

--- Common/vtkDataSetAttributes.cxx
+++ Common/vtkDataSetAttributes.cxx
@@ -256,7 +256,7 @@ void vtkDataSetAttributes::DeepCopy(vtkFieldData *fd)
       newData->Delete();
       }
     // Copy the copy flags
-    for(int attributeType=0; attributeType<NUM_ATTRIBUTES; attributeType++)
+    for(attributeType=0; attributeType<NUM_ATTRIBUTES; attributeType++)
       {
       this->CopyAttributeFlags[attributeType] = 
 	dsa->CopyAttributeFlags[attributeType];
@@ -305,7 +305,7 @@ void vtkDataSetAttributes::ShallowCopy(vtkFieldData *fd)
 	}
       }
     // Copy the copy flags
-    for(int attributeType=0; attributeType<NUM_ATTRIBUTES; attributeType++)
+    for(attributeType=0; attributeType<NUM_ATTRIBUTES; attributeType++)
       {
       this->CopyAttributeFlags[attributeType] = 
 	dsa->CopyAttributeFlags[attributeType];
