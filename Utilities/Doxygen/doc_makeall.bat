4f3165092f9f7f8298c7905ed4c7feb3d9e75ef7
Sebastien Barre <sebastien.barre@kitware.com>
2001-10-01 11:32:19 -0400

    Add some options explicitly

--- Utilities/Doxygen/doc_makeall.bat
+++ Utilities/Doxygen/doc_makeall.bat
@@ -1,6 +1,6 @@
-perl doc_header2doxygen.pl
-perl doc_version.pl --logo "vtk-logo.gif"
-perl doc_class2example.pl --link http://public.kitware.com/cgi-bin/cvsweb.cgi/~checkout~/VTK/Utilities/Doxygen
-perl doc_class2example.pl --dirmatch "^Testing$" --label "Tests" --title "Class To Tests" --store "doc_class2tests.dox" --unique "t" --link ../../VTK/Utilities/Doxygen
-perl doc_index.pl
+perl doc_header2doxygen.pl --to ../../../VTK-doxygen
+perl doc_version.pl --logo "vtk-logo.gif" --to ../../../VTK-doxygen
+perl doc_class2example.pl --link http://public.kitware.com/cgi-bin/cvsweb.cgi/~checkout~/VTK/Utilities/Doxygen --to ../../../VTK-doxygen
+perl doc_class2example.pl --dirmatch "^Testing$" --label "Tests" --title "Class To Tests" --store "doc_class2tests.dox" --unique "t" --link ../../VTK/Utilities/Doxygen --to ../../../VTK-doxygen
+perl doc_index.pl --to ../../../VTK-doxygen
 doxygen
