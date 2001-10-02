d9d605b98c1e45b3009b3277c7031ae6bcce25be
Ken Martin <ken.martin@kitware.com>
2001-10-02 10:58:46 -0400

    now looks at identiy flag

--- Rendering/vtkOpenGLActor.cxx
+++ Rendering/vtkOpenGLActor.cxx
@@ -97,37 +97,43 @@ void vtkOpenGLActor::Render(vtkRenderer *ren, vtkMapper *mapper)
     }
 
   // build transformation 
-  double *mat = this->GetMatrix()->Element[0];
-  double mat2[16];
-  mat2[0] = mat[0];
-  mat2[1] = mat[4];
-  mat2[2] = mat[8];
-  mat2[3] = mat[12];
-  mat2[4] = mat[1];
-  mat2[5] = mat[5];
-  mat2[6] = mat[9];
-  mat2[7] = mat[13];
-  mat2[8] = mat[2];
-  mat2[9] = mat[6];
-  mat2[10] = mat[10];
-  mat2[11] = mat[14];
-  mat2[12] = mat[3];
-  mat2[13] = mat[7];
-  mat2[14] = mat[11];
-  mat2[15] = mat[15];
+  if (!this->IsIdentity)
+    {
+    double *mat = this->GetMatrix()->Element[0];
+    double mat2[16];
+    mat2[0] = mat[0];
+    mat2[1] = mat[4];
+    mat2[2] = mat[8];
+    mat2[3] = mat[12];
+    mat2[4] = mat[1];
+    mat2[5] = mat[5];
+    mat2[6] = mat[9];
+    mat2[7] = mat[13];
+    mat2[8] = mat[2];
+    mat2[9] = mat[6];
+    mat2[10] = mat[10];
+    mat2[11] = mat[14];
+    mat2[12] = mat[3];
+    mat2[13] = mat[7];
+    mat2[14] = mat[11];
+    mat2[15] = mat[15];
+    
+    // insert model transformation 
+    glMatrixMode( GL_MODELVIEW );
+    glPushMatrix();
+    glMultMatrixd(mat2);
+    }
   
-  // insert model transformation 
-  glMatrixMode( GL_MODELVIEW );
-  glPushMatrix();
-  glMultMatrixd(mat2);
-
   // send a render to the mapper; update pipeline
   mapper->Render(ren,this);
 
   // pop transformation matrix
-  glMatrixMode( GL_MODELVIEW );
-  glPopMatrix();
-
+  if (!this->IsIdentity)
+    {
+    glMatrixMode( GL_MODELVIEW );
+    glPopMatrix();
+    }
+  
   if (opacity != 1.0)
     {
     glDepthMask (GL_TRUE);
