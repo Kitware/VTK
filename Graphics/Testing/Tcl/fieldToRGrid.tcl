1c77acd488bb51799c4a5b0985a70d132b61f461
Bill Lorensen <lorensen@crd.ge.com>
2001-10-11 08:31:55 -0400

    ENH: new test.

---
+++ Graphics/Testing/Tcl/fieldToRGrid.tcl
@@ -0,0 +1,153 @@
+package require vtk
+package require vtkinteraction
+package require vtktesting
+
+## Generate a rectilinear grid from a field.
+##
+
+# get the interactor ui
+
+# Create a reader and write out the field
+vtkDataSetReader reader
+    reader SetFileName "$VTK_DATA_ROOT/Data/RectGrid2.vtk"
+vtkDataSetToDataObjectFilter ds2do
+    ds2do SetInput [reader GetOutput]
+if {[catch {set channel [open RGridField.vtk w]}] == 0 } {
+   close $channel
+vtkDataObjectWriter writer
+    writer SetInput [ds2do GetOutput]
+    writer SetFileName "RGridField.vtk"
+    writer Write
+
+# Read the field
+#
+vtkDataObjectReader dor
+    dor SetFileName "RGridField.vtk"
+vtkDataObjectToDataSetFilter do2ds
+    do2ds SetInput [dor GetOutput]
+    do2ds SetDataSetTypeToRectilinearGrid
+    do2ds SetDimensionsComponent Dimensions 0 
+    do2ds SetPointComponent 0 XCoordinates 0
+    do2ds SetPointComponent 1 YCoordinates 0 
+    do2ds SetPointComponent 2 ZCoordinates 0
+vtkFieldDataToAttributeDataFilter fd2ad
+    fd2ad SetInput [do2ds GetRectilinearGridOutput]
+    fd2ad SetInputFieldToDataObjectField
+    fd2ad SetOutputAttributeDataToPointData
+    fd2ad SetVectorComponent 0 vectors 0 
+    fd2ad SetVectorComponent 1 vectors 1 
+    fd2ad SetVectorComponent 2 vectors 2 
+    fd2ad SetScalarComponent 0 scalars 0 
+    fd2ad Update
+
+# create pipeline
+#
+vtkRectilinearGridGeometryFilter plane
+    plane SetInput [fd2ad GetRectilinearGridOutput]
+    plane SetExtent 0 100 0 100 15 15 
+vtkWarpVector warper
+    warper SetInput [plane GetOutput]
+    warper SetScaleFactor 0.05
+vtkDataSetMapper planeMapper
+    planeMapper SetInput [warper GetOutput]
+    planeMapper SetScalarRange 0.197813 0.710419
+vtkActor planeActor
+    planeActor SetMapper planeMapper
+
+vtkPlane cutPlane
+    eval cutPlane SetOrigin [[fd2ad GetOutput] GetCenter]
+    cutPlane SetNormal 1 0 0
+vtkCutter planeCut
+    planeCut SetInput [fd2ad GetRectilinearGridOutput]
+    planeCut SetCutFunction cutPlane
+vtkDataSetMapper cutMapper
+    cutMapper SetInput [planeCut GetOutput]
+    eval cutMapper SetScalarRange \
+    [[[[fd2ad GetOutput] GetPointData] GetScalars] GetRange]
+vtkActor cutActor
+    cutActor SetMapper cutMapper
+
+vtkContourFilter iso
+    iso SetInput [fd2ad GetRectilinearGridOutput]
+    iso SetValue 0 0.7
+vtkPolyDataNormals normals
+    normals SetInput [iso GetOutput]
+    normals SetFeatureAngle 45
+vtkPolyDataMapper isoMapper
+    isoMapper SetInput [normals GetOutput]
+    isoMapper ScalarVisibilityOff
+vtkActor isoActor
+    isoActor SetMapper isoMapper
+    eval [isoActor GetProperty] SetColor $bisque
+    eval [isoActor GetProperty] SetRepresentationToWireframe
+
+vtkStreamLine streamer
+    streamer SetInput [fd2ad GetOutput]
+    streamer SetStartPosition -1.2 -0.1 1.3
+    streamer SetMaximumPropagationTime 500
+    streamer SetStepLength 0.05
+    streamer SetIntegrationStepLength 0.05
+    streamer SetIntegrationDirectionToIntegrateBothDirections
+
+vtkTubeFilter streamTube
+    streamTube SetInput [streamer GetOutput]
+    streamTube SetRadius 0.025
+    streamTube SetNumberOfSides 6
+    streamTube SetVaryRadiusToVaryRadiusByVector
+vtkPolyDataMapper mapStreamTube
+    mapStreamTube SetInput [streamTube GetOutput]
+    eval mapStreamTube SetScalarRange \
+       [[[[fd2ad GetOutput] GetPointData] GetScalars] GetRange]
+vtkActor streamTubeActor
+    streamTubeActor SetMapper mapStreamTube
+    [streamTubeActor GetProperty] BackfaceCullingOn
+
+vtkOutlineFilter outline
+    outline SetInput [fd2ad GetRectilinearGridOutput]
+vtkPolyDataMapper outlineMapper
+    outlineMapper SetInput [outline GetOutput]
+vtkActor outlineActor
+    outlineActor SetMapper outlineMapper
+    eval [outlineActor GetProperty] SetColor $black
+
+# Graphics stuff
+# Create the RenderWindow, Renderer and both Actors
+#
+vtkRenderer ren1
+vtkRenderWindow renWin
+    renWin AddRenderer ren1
+vtkRenderWindowInteractor iren
+    iren SetRenderWindow renWin
+
+# Add the actors to the renderer, set the background and size
+#
+ren1 AddActor outlineActor
+ren1 AddActor planeActor
+ren1 AddActor cutActor
+ren1 AddActor isoActor
+ren1 AddActor streamTubeActor
+
+ren1 SetBackground 1 1 1
+renWin SetSize 300 300
+
+[ren1 GetActiveCamera] SetPosition 0.0390893 0.184813 -3.94026 
+[ren1 GetActiveCamera] SetFocalPoint -0.00578326 0 0.701967 
+[ren1 GetActiveCamera] SetViewAngle 30
+[ren1 GetActiveCamera] SetViewUp 0.00850257 0.999169 0.0398605 
+[ren1 GetActiveCamera] SetClippingRange 3.08127 6.62716 
+
+iren Initialize
+
+# render the image
+#
+iren SetUserMethod {wm deiconify .vtkInteract}
+if {[info commands rtExMath] != ""} { 
+    file delete -force RGridField.vtk
+}
+}
+
+# prevent the tk window from showing up then start the event loop
+wm withdraw .
+
+
+
---
+++ Graphics/Testing/Tcl/fieldToUGrid.tcl
@@ -0,0 +1,117 @@
+package require vtk
+package require vtkinteraction
+
+# Read a field representing unstructured grid and display it (similar to blow.tcl)
+
+# create a reader and write out field daya
+vtkUnstructuredGridReader reader
+    reader SetFileName "$VTK_DATA_ROOT/Data/blow.vtk"
+    reader SetScalarsName "thickness9"
+    reader SetVectorsName "displacement9"
+vtkDataSetToDataObjectFilter ds2do
+    ds2do SetInput [reader GetOutput]
+
+# we must be able to write here
+if {[catch {set channel [open UGridField.vtk w]}] == 0 } {
+   close $channel
+
+vtkDataObjectWriter write
+    write SetInput [ds2do GetOutput]
+    write SetFileName "UGridField.vtk"
+    write Write
+
+# Read the field and convert to unstructured grid.
+vtkDataObjectReader dor
+    dor SetFileName "UGridField.vtk"
+vtkDataObjectToDataSetFilter do2ds
+    do2ds SetInput [dor GetOutput]
+    do2ds SetDataSetTypeToUnstructuredGrid
+    do2ds SetPointComponent 0 Points 0 
+    do2ds SetPointComponent 1 Points 1 
+    do2ds SetPointComponent 2 Points 2 
+    do2ds SetCellTypeComponent CellTypes 0
+    do2ds SetCellConnectivityComponent Cells 0
+vtkFieldDataToAttributeDataFilter fd2ad
+    fd2ad SetInput [do2ds GetUnstructuredGridOutput]
+    fd2ad SetInputFieldToDataObjectField
+    fd2ad SetOutputAttributeDataToPointData
+    fd2ad SetVectorComponent 0 displacement9 0 
+    fd2ad SetVectorComponent 1 displacement9 1 
+    fd2ad SetVectorComponent 2 displacement9 2 
+    fd2ad SetScalarComponent 0 thickness9 0 
+    
+# Now start visualizing
+vtkWarpVector warp
+    warp SetInput [fd2ad GetUnstructuredGridOutput]
+
+# extract mold from mesh using connectivity
+vtkConnectivityFilter connect
+    connect SetInput [warp GetOutput]
+    connect SetExtractionModeToSpecifiedRegions
+    connect AddSpecifiedRegion 0
+    connect AddSpecifiedRegion 1
+vtkDataSetMapper moldMapper
+    moldMapper SetInput [connect GetOutput]
+    moldMapper ScalarVisibilityOff
+vtkActor moldActor
+    moldActor SetMapper moldMapper
+    [moldActor GetProperty] SetColor .2 .2 .2
+    [moldActor GetProperty] SetRepresentationToWireframe
+
+# extract parison from mesh using connectivity
+vtkConnectivityFilter connect2
+    connect2 SetInput [warp GetOutput]
+    connect2 SetExtractionModeToSpecifiedRegions
+    connect2 AddSpecifiedRegion 2
+vtkGeometryFilter parison
+    parison SetInput [connect2 GetOutput]
+vtkPolyDataNormals normals2
+    normals2 SetInput [parison GetOutput]
+    normals2 SetFeatureAngle 60
+vtkLookupTable lut
+    lut SetHueRange 0.0 0.66667
+vtkPolyDataMapper parisonMapper
+    parisonMapper SetInput [normals2 GetOutput]
+    parisonMapper SetLookupTable lut
+    parisonMapper SetScalarRange 0.12 1.0
+vtkActor parisonActor
+    parisonActor SetMapper parisonMapper
+
+vtkContourFilter cf
+    cf SetInput [connect2 GetOutput]
+    cf SetValue 0 .5
+vtkPolyDataMapper contourMapper
+    contourMapper SetInput [cf GetOutput]
+vtkActor contours
+    contours SetMapper contourMapper
+
+# Create graphics stuff
+vtkRenderer ren1
+vtkRenderWindow renWin
+    renWin AddRenderer ren1
+vtkRenderWindowInteractor iren
+    iren SetRenderWindow renWin
+
+# Add the actors to the renderer, set the background and size
+ren1 AddActor moldActor
+ren1 AddActor parisonActor
+ren1 AddActor contours
+[ren1 GetActiveCamera] Azimuth 60
+[ren1 GetActiveCamera] Roll -90
+[ren1 GetActiveCamera] Dolly 3
+ren1 ResetCameraClippingRange
+ren1 SetBackground 1 1 1
+renWin SetSize 375 200
+
+iren Initialize
+iren SetUserMethod {wm deiconify .vtkInteract}
+if {[info commands rtExMath] != ""} { 
+    file delete -force UGridField.vtk
+}
+}
+
+# prevent the tk window from showing up then start the event loop
+wm withdraw .
+
+
+
