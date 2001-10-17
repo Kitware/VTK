2d8ed58e7904e8f04c6d681d1c6d14fb69014f30
Amy Squillacote <amy.squillacote@kitware.com>
2001-10-17 14:38:33 -0400

    moving vtkPVGeometryFilter from ParaView

---
+++ Parallel/vtkPVGeometryFilter.h
@@ -0,0 +1,84 @@
+/*=========================================================================
+
+  Program:   ParaView
+  Module:    vtkPVGeometryFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2000-2001 Kitware Inc. 469 Clifton Corporate Parkway,
+Clifton Park, NY, 12065, USA.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither the name of Kitware nor the names of any contributors may be used
+   to endorse or promote products derived from this software without specific 
+   prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkPVGeometryFilter - Geometry filter that does outlines for volumes.
+// .SECTION Description
+// This filter defaults to using the outline filter unless the input
+// is a structured volume.
+
+#ifndef __vtkPVGeometryFilter_h
+#define __vtkPVGeometryFilter_h
+
+#include "vtkDataSetSurfaceFilter.h"
+#include "vtkImageData.h"
+#include "vtkStructuredGrid.h"
+#include "vtkRectilinearGrid.h"
+#include "vtkUnstructuredGrid.h"
+
+class VTK_EXPORT vtkPVGeometryFilter : public vtkDataSetSurfaceFilter
+{
+public:
+  static vtkPVGeometryFilter *New();
+  vtkTypeMacro(vtkPVGeometryFilter,vtkDataSetSurfaceFilter);
+
+  // Description:
+  // This flag is set during the execute method.  It indicates
+  // that the input was 3d and an outline representation was used.
+  vtkGetMacro(OutlineFlag, int);
+
+protected:
+  vtkPVGeometryFilter();
+  ~vtkPVGeometryFilter();
+  vtkPVGeometryFilter(const vtkPVGeometryFilter&) {};
+  void operator=(const vtkPVGeometryFilter&) {};
+
+  void Execute();
+  void ImageDataExecute(vtkImageData *input);
+  void StructuredGridExecute(vtkStructuredGrid *input);
+  void RectilinearGridExecute(vtkRectilinearGrid *input);
+  void UnstructuredGridExecute(vtkUnstructuredGrid *input);
+
+  int OutlineFlag;
+};
+
+#endif
+
+
