b77acbe8e71ae2aa51b913ab326c54dfe325848c
Will Schroeder <will.schroeder@kitware.com>
2001-10-30 11:34:13 -0500

    UG Examples

---
+++ Examples/VisualizationAlgorithms/Tcl/imageWarp.tcl
@@ -0,0 +1,62 @@
+# This example shows how to combine data from both the imaging
+# and graphics pipelines. The vtkMergeData filter is used to
+# merge the data from each together.
+
+package require vtk
+package require vtkinteraction
+
+# Read in an image and compute a luminance value. The image is extracted
+# as a set of polygons (vtkStructuredPointsGeometryFilter). We then will
+# warp the plane using the scalar (luminance) values.
+#
+vtkBMPReader reader
+  reader SetFileName $VTK_DATA_ROOT/Data/masonry.bmp
+vtkImageLuminance luminance
+  luminance SetInput [reader GetOutput]
+vtkStructuredPointsGeometryFilter geometry
+  geometry SetInput [luminance GetOutput]
+vtkWarpScalar warp
+  warp SetInput [geometry GetOutput]
+  warp SetScaleFactor -0.1
+
+# Use vtkMergeFilter to combine the original image with the warped geometry.
+#
+vtkMergeFilter merge
+  merge SetGeometry [warp GetOutput]
+  merge SetScalars  [reader GetOutput]
+vtkDataSetMapper mapper
+  mapper SetInput [merge GetOutput]
+  mapper SetScalarRange 0 255
+  mapper ImmediateModeRenderingOff
+vtkActor actor
+  actor SetMapper mapper
+
+# Create renderer stuff
+#
+vtkRenderer ren1
+vtkRenderWindow renWin
+    renWin AddRenderer ren1
+vtkRenderWindowInteractor iren
+    iren SetRenderWindow renWin
+
+# Add the actors to the renderer, set the background and size
+#
+ren1 AddActor actor
+[ren1 GetActiveCamera] Azimuth 20
+[ren1 GetActiveCamera] Elevation 30
+ren1 SetBackground 0.1 0.2 0.4
+ren1 ResetCameraClippingRange
+
+renWin SetSize 250 250
+
+# render the image
+#
+iren SetUserMethod {wm deiconify .vtkInteract}
+set cam1 [ren1 GetActiveCamera]
+$cam1 Zoom 1.4
+renWin Render
+
+# prevent the tk window from showing up then start the event loop
+wm withdraw .
+
+
