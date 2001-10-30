0c108c5e26bb2f935528547df3d6c6790f0167b6
Will Schroeder <will.schroeder@kitware.com>
2001-10-30 11:10:45 -0500

    UG Examples

---
+++ Examples/VisualizationAlgorithms/Tcl/CutCombustor.tcl
@@ -0,0 +1,87 @@
+# This example shows how to use cutting (vtkCutter) and how it compares
+# with extracting a plane from a computational grid.
+#
+
+package require vtk
+package require vtkinteraction
+
+# Read some data.
+vtkPLOT3DReader pl3d
+    pl3d SetXYZFileName "$VTK_DATA_ROOT/Data/combxyz.bin"
+    pl3d SetQFileName "$VTK_DATA_ROOT/Data/combq.bin"
+    pl3d SetScalarFunctionNumber 100
+    pl3d SetVectorFunctionNumber 202
+    pl3d Update
+
+# The cutter uses an implicit function to perform the cutting.
+# Here we define a plane, specifying its center and normal.
+# Then we assign the plane to the cutter.
+vtkPlane plane
+    eval plane SetOrigin [[pl3d GetOutput] GetCenter]
+    plane SetNormal -0.287 0 0.9579
+vtkCutter planeCut
+    planeCut SetInput [pl3d GetOutput]
+    planeCut SetCutFunction plane
+vtkPolyDataMapper cutMapper
+    cutMapper SetInput [planeCut GetOutput]
+    eval cutMapper SetScalarRange \
+      [[[[pl3d GetOutput] GetPointData] GetScalars] GetRange]
+vtkActor cutActor
+    cutActor SetMapper cutMapper
+
+# Here we extract a computational plane from the structured grid.
+# We render it as wireframe.
+vtkStructuredGridGeometryFilter compPlane
+    compPlane SetInput [pl3d GetOutput]
+    compPlane SetExtent 0 100 0 100 9 9
+vtkPolyDataMapper planeMapper
+    planeMapper SetInput [compPlane GetOutput]
+    planeMapper ScalarVisibilityOff
+vtkActor planeActor
+    planeActor SetMapper planeMapper
+    [planeActor GetProperty] SetRepresentationToWireframe
+    [planeActor GetProperty] SetColor 0 0 0
+
+# The outline of the data puts the data in context.
+vtkStructuredGridOutlineFilter outline
+    outline SetInput [pl3d GetOutput]
+vtkPolyDataMapper outlineMapper
+    outlineMapper SetInput [outline GetOutput]
+vtkActor outlineActor
+    outlineActor SetMapper outlineMapper
+set outlineProp [outlineActor GetProperty]
+eval $outlineProp SetColor 0 0 0
+
+# Create the RenderWindow, Renderer and both Actors
+#
+vtkRenderer ren1
+vtkRenderWindow renWin
+    renWin AddRenderer ren1
+vtkRenderWindowInteractor iren
+    iren SetRenderWindow renWin
+
+# Add the actors to the renderer, set the background and size
+#
+ren1 AddActor outlineActor
+ren1 AddActor planeActor
+ren1 AddActor cutActor
+
+ren1 SetBackground 1 1 1
+renWin SetSize 400 300
+
+set cam1 [ren1 GetActiveCamera]
+$cam1 SetClippingRange 11.1034 59.5328
+$cam1 SetFocalPoint 9.71821 0.458166 29.3999
+$cam1 SetPosition -2.95748 -26.7271 44.5309
+$cam1 SetViewUp 0.0184785 0.479657 0.877262
+iren Initialize
+
+# render the image
+#
+iren SetUserMethod {wm deiconify .vtkInteract}
+
+# prevent the tk window from showing up then start the event loop
+wm withdraw .
+
+
+
---
+++ Examples/VisualizationAlgorithms/Tcl/streamSurface.tcl
@@ -0,0 +1,86 @@
+# This example demonstrates the generation of a streamsurface.
+
+#
+# First we include the VTK Tcl packages which will make available
+# all of the vtk commands from Tcl. The vtkinteraction package defines
+# a simple Tcl/Tk interactor widget.
+#
+package require vtk
+package require vtkinteraction
+package require vtktesting
+
+# create pipeline
+#
+vtkPLOT3DReader pl3d
+  pl3d SetXYZFileName "$VTK_DATA_ROOT/Data/combxyz.bin"
+  pl3d SetQFileName "$VTK_DATA_ROOT/Data/combq.bin"
+  pl3d SetScalarFunctionNumber 100 
+  pl3d SetVectorFunctionNumber 202
+  pl3d Update
+
+# We use a rake to generate a series of streamline starting points
+# scattered along a line. Each point will generate a streamline. These
+# streamlines are then fed to the vtkRuledSurfaceFilter which stitches
+# the lines together to form a surface.
+vtkLineSource rake
+  rake SetPoint1 15 -5 32
+  rake SetPoint2 15 5 32
+  rake SetResolution 21
+vtkPolyDataMapper rakeMapper
+  rakeMapper SetInput [rake GetOutput]
+vtkActor rakeActor
+  rakeActor SetMapper rakeMapper 
+
+vtkRungeKutta4 integ
+vtkStreamLine sl
+  sl SetInput [pl3d GetOutput]
+  sl SetSource [rake GetOutput] 
+  sl SetIntegrator integ 
+  sl SetMaximumPropagationTime 0.1 
+  sl SetIntegrationStepLength 0.1 
+  sl SetIntegrationDirectionToBackward
+  sl SetStepLength 0.001 
+
+# Note the SetOnRation method. It turns on every other strip that
+# the filter generates (only when multiple lines are input).
+vtkRuledSurfaceFilter scalarSurface
+  scalarSurface SetInput [sl GetOutput]
+  scalarSurface SetOffset 0 
+  scalarSurface SetOnRatio 2 
+  scalarSurface PassLinesOn
+  scalarSurface SetRuledModeToPointWalk
+  scalarSurface SetDistanceFactor 30 
+vtkPolyDataMapper mapper
+  mapper SetInput [scalarSurface GetOutput]
+  eval mapper SetScalarRange [[pl3d GetOutput] GetScalarRange]
+vtkActor actor
+  actor SetMapper mapper 
+
+vtkStructuredGridOutlineFilter outline
+  outline SetInput [pl3d GetOutput]
+vtkPolyDataMapper outlineMapper
+  outlineMapper SetInput [outline GetOutput]
+vtkActor outlineActor
+  outlineActor SetMapper outlineMapper 
+  [outlineActor GetProperty] SetColor 0 0 0
+
+# Now create the usual graphics stuff.
+vtkRenderer ren
+vtkRenderWindow renWin
+    renWin AddRenderer ren
+vtkRenderWindowInteractor iren
+    iren SetRenderWindow renWin
+
+ren AddActor rakeActor 
+ren AddActor actor 
+ren AddActor outlineActor 
+ren SetBackground 1 1 1
+
+renWin SetSize 300 300
+
+iren SetUserMethod {wm deiconify .vtkInteract}
+iren Initialize
+
+# interact with data
+wm withdraw .
+
