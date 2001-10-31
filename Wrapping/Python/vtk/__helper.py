b6f7305e60e7f0923add9d23eb673bdbd52580d4
Prabhu Ramachandran <prabhu_r@users.sourceforge.net>
2001-10-31 15:30:50 -0500

    ENH: If a module is not importable the appropriate entry in sys.modules is removed.

--- Wrapping/Python/vtk/__helper.py
+++ Wrapping/Python/vtk/__helper.py
@@ -1,14 +1,15 @@
 """ This provides some useful code used by other modules.  This is not to be
 used by the end user which is why it is hidden. """
 
-import string
+import string, sys
 class LinkError(Exception):
     pass
 
 def refine_import_err(mod_name, exc):
     """ Checks to see if the ImportError was because the library
     itself was not there or because there was a link error.  If there
-    was a link error it raises a LinkError if not it does nothing."""    
+    was a link error it raises a LinkError if not it does nothing."""
+    del sys.modules['vtk.%s'%mod_name]
     mod = 'vtk' + string.upper(mod_name[0]) + mod_name[1:] +'Python'
     if string.find(str(exc), mod) == -1:
 	raise LinkError, str(exc)
