8873091956e8ed138254bd8c1e5ee0b814471aef
Bill Lorensen <lorensen@crd.ge.com>
2001-11-14 07:21:02 -0500

    ERR: need to define where packages exist.

--- Common/Testing/Tcl/otherInterp.tcl
+++ Common/Testing/Tcl/otherInterp.tcl
@@ -1,3 +1,8 @@
+for {set i  0} {$i < [expr $argc - 1]} {incr i} {
+   if {[lindex $argv $i] == "-A"} {
+      set auto_path "$auto_path [lindex $argv [expr $i +1]]"
+   }
+}
 package require vtk
 
 proc rtOtherTest { fileid } {
