d1cfa1f10b5289945a3d689514216c18afa3497d
Sebastien Barre <sebastien.barre@kitware.com>
2001-12-14 18:20:43 -0500

    Fix crash in some config if Tk was not found. Complain more gracefully.

--- Wrapping/Tcl/vtkrendering/vtkrendering.tcl
+++ Wrapping/Tcl/vtkrendering/vtkrendering.tcl
@@ -1,14 +1,23 @@
 package require vtkgraphics
 package require vtkimaging
 
-if {[catch {
-    package require Tk
-}]} {
-    ::vtk::load_component tk
+catch {
+    unset __tk_error
 }
 
-if {[info commands vtkRenderWindow] != "" ||
-    [::vtk::load_component vtkRenderingTCL] == ""} {
+if {[catch {
+    package require Tk
+} errormsg]} {
+    if {[::vtk::load_component tk] != ""} {
+        puts $errormsg
+        puts "Tk was not found: the VTK rendering package can not be used... Please check that your Tcl/Tk installation is correct. Windows users should also check that the program used to open/execute Tcl files is the Tk shell (wish), not the Tcl shell (tclsh)."
+        set __tk_error 1
+    }
+}
+
+if {![info exists __tk_error] && \
+        ([info commands vtkRenderWindow] != "" || \
+        [::vtk::load_component vtkRenderingTCL] == "")} {
 
     # Set the default exit method of vtkWin32RenderWindowInteractor to
     # call the Tcl 'exit' command
@@ -18,10 +27,10 @@ if {[info commands vtkRenderWindow] != "" ||
             vtkWin32RenderWindowInteractor __temp_vtkwin32iren__
             __temp_vtkwin32iren__ SetClassExitMethod exit
             __temp_vtkwin32iren__ Delete
-        } errormsg ]} {
+        } errormsg]} {
             puts $errormsg
         }
     }
-    
+
     package provide vtkrendering 4.0
 }
