4a16693758c7f73f2b5e87159ad3eaa6d10a489a
Will Schroeder <will.schroeder@kitware.com>
2001-12-17 10:28:23 -0500

    ENH:New legal contact point

--- Patented/vtkSweptSurface.cxx
+++ Patented/vtkSweptSurface.cxx
@@ -14,13 +14,14 @@ All rights reserved.
     "Implicit Modeling of Swept Volumes and Swept Surfaces"
     Application of this software for commercial purposes requires 
     a license grant from GE. Contact:
-        Jerald Roehling
-        GE Licensing
-        One Independence Way
-        PO Box 2023
-        Princeton, N.J. 08540
-        phone 609-734-9823
-        e-mail:Roehlinj@gerlmo.ge.com
+
+        Carl B. Horton
+        Sr. Counsel, Intellectual Property
+        3000 N. Grandview Blvd., W-710
+        Waukesha, WI  53188
+        Phone:  (262) 513-4022
+        E-Mail: Carl.Horton@med.ge.com
+
     for more information.
 
 Redistribution and use in source and binary forms, with or without
