4efb271b9693ca64232a197e161ff46382da13b3
Charles Law <charles.law@kitware.com>
2001-12-19 11:01:03 -0500

    TkRenderWidget causes leaks.

--- Graphics/Testing/Tcl/TestSpherePuzzleArrows.tcl
+++ Graphics/Testing/Tcl/TestSpherePuzzleArrows.tcl
@@ -4,49 +4,12 @@ package require vtkinteraction
 # prevent the tk window from showing up then start the event loop
 wm withdraw .
 
-# Create the toplevel window
-toplevel .top
-wm title .top {Sphere Puzzle}
-
-# Create some frames
-frame .top.f1 
-frame .top.f2
-pack .top.f1 .top.f2 -side top -expand 1 -fill both
-
 vtkRenderWindow renWin
-vtkTkRenderWidget .top.f1.rw -width 400 -height 400 -rw renWin
-BindTkRenderWidget .top.f1.rw
-pack .top.f1.rw -expand 1 -fill both
 
 # create a rendering window and renderer
 vtkRenderer ren1
     renWin AddRenderer ren1
-
-label .top.f2.l1 -text "Position cursor over the rotation plane."
-label .top.f2.l2 -text "Moving pieces will be highlighted."
-label .top.f2.l3 -text "Press 'm' to make a move."
-
-button .top.f2.reset -text "Reset" \
-	-command "puzzle Reset; renWin Render"
-
-pack .top.f2.l1 .top.f2.l2 .top.f2.l3 .top.f2.reset \
-	-side top -expand 1 -fill both
-
-button .top.f2.b1 -text "Quit" -command {vtkCommand DeleteAllObjects; exit}
-pack .top.f2.b1  -expand 1 -fill x
-
-proc ToggleGhostCells {} {
-   renWin Render
-}
-
-
-
-proc test {slab} {
-    for { set i 0} {$i <= 100} {set i [expr $i + 5]} {
-	puzzle MoveVertical $slab $i 1
-	renWin Render
-    }
-}
+    renWin SetSize 400 400
 
 
 vtkSpherePuzzle puzzle
@@ -72,15 +35,12 @@ ren1 AddActor actor2
 ren1 SetBackground 0.1 0.2 0.4
 
 
-bind .top.f1.rw <Motion> {MotionCallback %x %y}
-bind .top.f1.rw <KeyPress-m> {ButtonCallback %x %y}
-
 set LastVal -1
 proc MotionCallback {x y} {
     global LastVal
 
     # Compute display point from Tk display point.
-    set WindowY [lindex [.top.f1.rw configure -height] 4]
+    set WindowY 400
     set y [expr $WindowY - $y]
     set z [ren1 GetZ $x $y]
 
@@ -104,7 +64,7 @@ proc MotionCallback {x y} {
 proc ButtonCallback {x y} {
 
     # Compute display point from Tk display point.
-    set WindowY [lindex [.top.f1.rw configure -height] 4]
+    set WindowY 400
     set y [expr $WindowY - $y]
     set z [ren1 GetZ $x $y]
 
