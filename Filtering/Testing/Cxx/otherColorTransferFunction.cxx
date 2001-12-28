b4273890d7fc6976266717a3ee9728820e009308
Bill Lorensen <lorensen@crd.ge.com>
2001-12-28 12:44:07 -0500

    ENH: Better coverage.

--- Filtering/Testing/Cxx/otherColorTransferFunction.cxx
+++ Filtering/Testing/Cxx/otherColorTransferFunction.cxx
@@ -63,14 +63,76 @@ void Test(ostream& strm)
   vtkColorTransferFunction *ctf2 = vtkColorTransferFunction::New();
   ctf2->BuildFunctionFromTable(0, 15, 256, &table[0][0]);
 
+  ctf2->SetColorSpaceToRGB();
+  ctf2->GetTable(0,15,512);
+
+  ctf2->SetColorSpaceToHSV();
+  ctf2->GetTable(0,15,512);
+
+  ctf1->DeepCopy(ctf2);
+  strm << "ctf1->DeepCopy(ctf2)" << endl;
+  strm << *ctf1;
+  
   ctf1->RemovePoint (10);
   strm << *ctf1;
 
   ctf1->RemoveAllPoints();
   strm << *ctf1;
 
+  char *cData = new char[128];
+  unsigned char *ucData = new unsigned char[128];
+  short *sData = new short[128];
+  unsigned short *usData = new unsigned short[128];
+  int *iData = new int[128];
+  unsigned int *uiData = new unsigned int[128];
+  long *lData = new long[128];
+  unsigned long *ulData = new unsigned long[128];
+  float *fData = new float[128];
+  double *dData = new double[128];
+
+  for (int k = 0; k < 128; k++)
+    {
+    *(cData+k) = static_cast<char>(static_cast<float>(k)/255.0);
+    *(ucData+k) = static_cast<unsigned char>(static_cast<float>(k)/255.0);
+    *(sData+k) = static_cast<short>(static_cast<float>(k)/255.0);
+    *(usData+k) = static_cast<unsigned short>(static_cast<float>(k)/255.0);
+    *(iData+k) = static_cast<int>(static_cast<float>(k)/255.0);
+    *(uiData+k) = static_cast<unsigned int>(static_cast<float>(k)/255.0);
+    *(lData+k) = static_cast<long>(static_cast<float>(k)/255.0);
+    *(ulData+k) = static_cast<unsigned long>(static_cast<float>(k)/255.0);
+    *(fData+k) = static_cast<float>(static_cast<float>(k)/255.0);
+    *(dData+k) = static_cast<double>(static_cast<float>(k)/255.0);
+    }
+
+  unsigned char *ucResult = new unsigned char[128*4];
+  for (k = 1; k <= 4; k++)
+    {
+    ctf2->MapScalarsThroughTable2((void *) cData, ucResult, VTK_CHAR, 128, 1, k);
+    ctf2->MapScalarsThroughTable2((void *) ucData, ucResult, VTK_UNSIGNED_CHAR, 128, 1, k);
+    ctf2->MapScalarsThroughTable2((void *) sData, ucResult, VTK_SHORT, 128, 1, k);
+    ctf2->MapScalarsThroughTable2((void *) usData, ucResult, VTK_UNSIGNED_SHORT, 128, 1, k);
+    ctf2->MapScalarsThroughTable2((void *) iData, ucResult, VTK_INT, 128, 1, k);
+    ctf2->MapScalarsThroughTable2((void *) uiData, ucResult, VTK_UNSIGNED_INT, 128, 1, k);
+    ctf2->MapScalarsThroughTable2((void *) lData, ucResult, VTK_LONG, 128, 1, k);
+    ctf2->MapScalarsThroughTable2((void *) ulData, ucResult, VTK_UNSIGNED_LONG, 128, 1, k);
+    ctf2->MapScalarsThroughTable2((void *) fData, ucResult, VTK_FLOAT, 128, 1, k);
+    ctf2->MapScalarsThroughTable2((void *) dData, ucResult, VTK_DOUBLE, 128, 1, k);
+    }
+
   ctf1->Delete();
   ctf2->Delete();
+
+  delete []ucResult;
+  delete []cData;
+  delete []ucData;
+  delete []sData;
+  delete []usData;
+  delete []iData;
+  delete []uiData;
+  delete []lData;
+  delete []ulData;
+  delete []fData;
+  delete []dData;
 
   strm << "Test vtkColorTransferFunction End" << endl;
 }
