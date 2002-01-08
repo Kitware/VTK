af6cfce82ee4fa670c95522a29083250e792d652
Sebastien Barre <sebastien.barre@kitware.com>
2002-01-08 10:05:20 -0500

    Update ex1 and add ex2

--- Examples/Build/vtkMy/Examples/Cxx/Ex1/vtkmyEx1.cxx
+++ Examples/Build/vtkMy/Examples/Cxx/Ex1/vtkmyEx1.cxx
@@ -1,3 +1,11 @@
+//
+// This example creates a couple of class instances and print them to
+// the standard output. No rendering window is created.
+//
+
+//
+// First include the required header files for the vtk classes we are using
+//
 #include "vtkBar.h"
 #include "vtkBar2.h"
 #include "vtkImageFoo.h"
@@ -5,27 +13,38 @@
 int main( int argc, char *argv[] )
 {
 
+  //
+  // Next we create an instance of vtkBar
+  //
   cout << "Create vtkBar object and print it." << endl;
-  
+
   vtkBar *bar = vtkBar::New();
   bar->Print(cout);
-  bar->Delete();
   
+  //
+  // Then we create an instance of vtkBar2
+  //
   cout << "Create vtkBar2 object and print it." << endl;
   
   vtkBar2 *bar2 = vtkBar2::New();
   bar2->Print(cout);
-  bar2->Delete();
   
+  //
+  // And we create an instance of vtkImageFoo
+  //
   cout << "Create vtkImageFoo object and print it." << endl;
   
   vtkImageFoo *imagefoo = vtkImageFoo::New();
   imagefoo->Print(cout);
-  imagefoo->Delete();
   
   cout << "Looks good ?" << endl;
 
+  //
+  // Free up any objects we created
+  //
+  bar->Delete();
+  bar2->Delete();
+  imagefoo->Delete();
+
   return 0;
 }
-
-
