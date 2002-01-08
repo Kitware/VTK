4e22a1075b42f6cd504afcfa54881ea85d0b1731
John Biddiscombe <jbiddiscombe@skippingmouse.co.uk>
2002-01-08 18:03:17 -0500

    ERR: Fix clumsy mistake in function def

--- Rendering/vtkRenderWindow.h
+++ Rendering/vtkRenderWindow.h
@@ -297,10 +297,14 @@ public:
   vtkSetMacro(InAbortCheck,int);
   virtual int CheckAbortStatus();
   virtual int GetEventPending() { return 0;};
+
   // Description:
-  // Are we rendering at the moment 
-  virtual int CheckInRenderStatus() { return this->InRender; }
-  virtual int ClearInRenderStatus() { return (this->InRender = 0); }
+  // Are we rendering at the moment
+  virtual int  CheckInRenderStatus() { return this->InRender; }
+
+  // Description:
+  // Clear status (after an exception was thrown for example)
+  virtual void ClearInRenderStatus() { this->InRender = 0; }
 
   // Description:
   // Specify a function to be called to check and see if an abort
