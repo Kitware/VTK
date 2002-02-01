14bdc711402ea603d1ed7369868db463c146bcca
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-02-01 12:10:32 -0500

    Fix typo in the test

--- IO/Testing/Tcl/TestTIFFReader.tcl
+++ IO/Testing/Tcl/TestTIFFReader.tcl
@@ -3,7 +3,7 @@ package require vtk
 # Image pipeline
 
 vtkImageReader2Factory createReader
-set reader [createReader CreateImageReader2 "$VTK_DATA_ROOT/Data/libtif/test.tif"]
+set reader [createReader CreateImageReader2 "$VTK_DATA_ROOT/Data/libtiff/test.tif"]
 $reader SetFileName "$VTK_DATA_ROOT/Data/libtiff/test.tif"
 
 vtkImageViewer viewer
