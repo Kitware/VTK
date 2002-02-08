2339473471058eb565cd70052d484f1e4775d083
Will Schroeder <will.schroeder@kitware.com>
2002-02-08 17:00:21 -0500

    ENH:Adding Clip() method

--- Common/vtkPixel.cxx
+++ Common/vtkPixel.cxx
@@ -25,7 +25,7 @@
 #include "vtkLine.h"
 #include "vtkPointLocator.h"
 
-vtkCxxRevisionMacro(vtkPixel, "1.70");
+vtkCxxRevisionMacro(vtkPixel, "1.71");
 vtkStandardNewMacro(vtkPixel);
 
 // Construct the pixel with four points.
@@ -488,15 +488,179 @@ void vtkPixel::Derivatives(int vtkNotUsed(subId),
     }
 }
 
-void vtkPixel::Clip(float vtkNotUsed(value), 
-                    vtkDataArray *vtkNotUsed(cellScalars), 
-                    vtkPointLocator *vtkNotUsed(locator), 
-                    vtkCellArray *vtkNotUsed(tetras),
-                    vtkPointData *vtkNotUsed(inPd),
-                    vtkPointData *vtkNotUsed(outPd),
-                    vtkCellData *vtkNotUsed(inCd), vtkIdType vtkNotUsed(cellId),
-                    vtkCellData *vtkNotUsed(outCd), int vtkNotUsed(insideOut))
-{
+// support pixel clipping
+typedef int PIXEL_EDGE_LIST;
+typedef struct {
+       PIXEL_EDGE_LIST edges[14];
+} PIXEL_CASES;
+ 
+static PIXEL_CASES pixelCases[] = { 
+{{  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 0
+{{   3, 100,   0,   3,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 1
+{{   3, 101,   1,   0,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 2
+{{   4, 100, 101,   1,   3,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 3
+{{   3, 102,   2,   1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 4
+{{   3, 100,   0,   3,   3, 102,   2,   1,   4,   0,   1,   2,   3,  -1}}, // 5
+{{   4, 101, 102,   2,   0,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 6
+{{   3, 100, 101,   3,   3, 101,   2,   3,   3, 101, 102,   2,  -1,  -1}}, // 7
+{{   3, 103,   3,   2,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 8
+{{   4, 100,   0,   2, 103,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 9
+{{   3, 101,   1,   0,   3, 103,   3,   2,   4,   0,   1,   2,   3,  -1}}, // 10
+{{   3, 100, 101,   1,   3, 100,   1,   2,   3, 100,   2, 103,  -1,  -1}}, // 11
+{{   4, 102, 103,   3,   1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 12
+{{   3, 100,   0, 103,   3,   0,   1, 103,   3,   1, 102, 103,  -1,  -1}}, // 13
+{{   3,   0, 101, 102,   3,   0, 102,   3,   3, 102, 103,   3,  -1,  -1}}, // 14
+{{   4, 100, 101, 102, 103,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 15
+};
 
+static PIXEL_CASES pixelCasesComplement[] = { 
+{{  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 0
+{{   3, 100,   0,   3,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 1
+{{   3, 101,   1,   0,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 2
+{{   4, 100, 101,   1,   3,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 3
+{{   3, 102,   2,   1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 4
+{{   3, 100,   0,   3,   3, 102,   2,   1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 5
+{{   4, 101, 102,   2,   0,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 6
+{{   3, 100, 101,   3,   3, 101,   2,   3,   3, 101, 102,   2,  -1,  -1}}, // 7
+{{   3, 103,   3,   2,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 8
+{{   4, 100,   0,   2, 103,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 9
+{{   3, 101,   1,   0,   3, 103,   3,   2,  -1,  -1,  -1,  -1,  -1,  -1}}, // 10
+{{   3, 100, 101,   1,   3, 100,   1,   2,   3, 100,   2, 103,  -1,  -1}}, // 11
+{{   4, 102, 103,   3,   1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 12
+{{   3, 100,   0, 103,   3,   0,   1, 103,   3,   1, 102, 103,  -1,  -1}}, // 13
+{{   3,   0, 101, 102,   3,   0, 102,   3,   3, 102, 103,   3,  -1,  -1}}, // 14
+{{   4, 100, 101, 102, 103,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1}}, // 15
+};
+
+// Clip this pixel using scalar value provided. Like contouring, except
+// that it cuts the pixel to produce quads and/or triangles.
+void vtkPixel::Clip(float value, vtkDataArray *cellScalars, 
+                   vtkPointLocator *locator, vtkCellArray *polys,
+                   vtkPointData *inPd, vtkPointData *outPd,
+                   vtkCellData *inCd, vtkIdType cellId, vtkCellData *outCd,
+                   int insideOut)
+{
+  static int CASE_MASK[4] = {1,2,4,8};
+  PIXEL_CASES *pixelCase;
+  PIXEL_EDGE_LIST  *edge;
+  int i, j, index, *vert;
+  int e1, e2;
+  int newCellId;
+  vtkIdType pts[4];
+  int vertexId;
+  float t, x1[3], x2[3], x[3], deltaScalar;
+  float scalar0, scalar1, e1Scalar;
+
+  // Build the index into the case table
+  if ( insideOut )
+    {    
+    for ( i=0, index = 0; i < 4; i++)
+      {
+      if (cellScalars->GetComponent(i,0) <= value)
+        {
+        index |= CASE_MASK[i];
+        }
+      }
+    // Select case based on the index and get the list of edges for this case
+    pixelCase = pixelCases + index;
+    }    
+  else
+    {
+    for ( i=0, index = 0; i < 4; i++)
+      {
+      if (cellScalars->GetComponent(i,0) > value)
+        {
+        index |= CASE_MASK[i];
+        }
+      }
+    // Select case based on the index and get the list of edges for this case
+    pixelCase = pixelCasesComplement + index;
+    }
+
+  edge = pixelCase->edges;
+
+  // generate each pixel
+  for ( ; edge[0] > -1; edge += edge[0]+1 )
+    {
+    for (i=0; i < edge[0]; i++) // insert pixel or triangle
+      {
+      // vertex exists, and need not be interpolated
+      if (edge[i+1] >= 100)
+        {
+        vertexId = edge[i+1] - 100;
+        this->Points->GetPoint(vertexId, x);
+        if ( locator->InsertUniquePoint(x, pts[i]) )
+          {
+          outPd->CopyData(inPd,this->PointIds->GetId(vertexId),pts[i]);
+          }
+        }
+
+      else //new vertex, interpolate
+        {
+        vert = edges[edge[i+1]];
+
+        // calculate a preferred interpolation direction
+        scalar0 = cellScalars->GetComponent(vert[0],0);
+        scalar1 = cellScalars->GetComponent(vert[1],0);
+        deltaScalar = scalar1 - scalar0;
+
+        if (deltaScalar > 0)
+          {
+          e1 = vert[0]; e2 = vert[1];
+          e1Scalar = scalar0;
+          }
+        else
+          {
+          e1 = vert[1]; e2 = vert[0];
+          e1Scalar = scalar1;
+          deltaScalar = -deltaScalar;
+          }
+
+        // linear interpolation
+        if (deltaScalar == 0.0)
+          {
+          t = 0.0;
+          }
+        else
+          {
+          t = (value - e1Scalar) / deltaScalar;
+          }
+
+        this->Points->GetPoint(e1, x1);
+        this->Points->GetPoint(e2, x2);
+
+        for (j=0; j<3; j++)
+          {
+          x[j] = x1[j] + t * (x2[j] - x1[j]);
+          }
+
+        if ( locator->InsertUniquePoint(x, pts[i]) )
+          {
+          int p1 = this->PointIds->GetId(e1);
+          int p2 = this->PointIds->GetId(e2);
+          outPd->InterpolateEdge(inPd,pts[i],p1,p2,t);
+          }
+        }
+      }
+    // check for degenerate output
+    if ( edge[0] == 3 ) //i.e., a triangle
+      {
+      if (pts[0] == pts[1] || pts[0] == pts[2] || pts[1] == pts[2] )
+        {
+        continue;
+        }
+      }
+    else // a pixel
+      {
+      if ((pts[0] == pts[3] && pts[1] == pts[2]) || 
+          (pts[0] == pts[1] && pts[3] == pts[2]) )
+        {
+        continue;
+        }
+      }
+
+    newCellId = polys->InsertNextCell(edge[0],pts);
+    outCd->CopyData(inCd,cellId,newCellId);
+    }
 }
 
