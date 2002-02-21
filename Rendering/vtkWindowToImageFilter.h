084c06c1a2a09696e74cbc5c69315fea73ea6bd6
Michalel Halle <halazar@media.mit.edu>
2002-02-21 17:14:26 -0500

    Added documentation comments describing the use of vtkWindowToImageFilter for writing windows to files, and  documenting the need to call Modified() after Rendering.  Hopefully, the documentation will keep people from tripping on this frequent problem.

--- Rendering/vtkWindowToImageFilter.h
+++ Rendering/vtkWindowToImageFilter.h
@@ -17,10 +17,30 @@
 =========================================================================*/
 // .NAME vtkWindowToImageFilter - Use a vtkWindow as input to image pipeline
 // .SECTION Description
-// vtkWindowToImageFilter provides methods needed to read the data in a 
-// vtkWindow and use it as input to the imaging pipeline. This is
-// useful for saving an image to a file for example. Use this filter to
-// convert RenderWindows or ImageWindows to an image format.
+// vtkWindowToImageFilter provides methods needed to read the data in
+// a vtkWindow and use it as input to the imaging pipeline. This is
+// useful for saving an image to a file for example. Use this filter
+// to convert RenderWindows or ImageWindows to an image format.  
+
+// .SECTION Caveats
+// A vtkWindow doesn't behave like other parts of the VTK pipeline: its
+// modification time doesn't get updated when an image is rendered.  As a
+// result, naive use of vtkWindowToImageFilter will produce an image of
+// the first image that the window rendered, but which is never updated
+// on subsequent window updates.  This behavior is unexpected and in 
+// general undesirable. 
+//
+// To force an update of the output image, call vtkWindowToImageFilter's 
+// Modified method after rendering to the window.
+//
+// In VTK versions 4 and later, this filter is part of the canonical
+// way to output an image of a window to a file (replacing the
+// obsolete SaveImageAsPPM method for vtkRenderWindows that existed in
+// 3.2 and earlier).  Connect this filter to the output of the window,
+// and filter's output to a writer such as vtkPNGWriter.
+
+// .SECTION see also
+// vtkWindow
 
 #ifndef __vtkWindowToImageFilter_h
 #define __vtkWindowToImageFilter_h
