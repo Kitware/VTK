82b39e13e73647c16ed363ba981bb35c6f96011d
Amy Squillacote <amy.squillacote@kitware.com>
2002-02-26 09:35:16 -0500

    adding vtkReflectionFilter

---
+++ Graphics/vtkReflectionFilter.cxx
@@ -0,0 +1,116 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkReflectionFilter.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkReflectionFilter.h"
+#include "vtkObjectFactory.h"
+
+vtkCxxRevisionMacro(vtkReflectionFilter, "1.1");
+vtkStandardNewMacro(vtkReflectionFilter);
+
+//---------------------------------------------------------------------------
+vtkReflectionFilter::vtkReflectionFilter()
+{
+}
+
+//---------------------------------------------------------------------------
+vtkReflectionFilter::~vtkReflectionFilter()
+{
+}
+
+//---------------------------------------------------------------------------
+void vtkReflectionFilter::Execute()
+{
+  vtkPolyData *input = this->GetInput();
+  vtkPolyData *output = this->GetOutput();
+  
+  output->CopyStructure(input);
+  output->BuildCells();
+  
+  vtkIdType numPts = input->GetNumberOfPoints();
+  vtkIdType numCells = input->GetNumberOfCells();
+  vtkPoints *points = input->GetPoints();
+  vtkPoints *outPoints = output->GetPoints();
+  
+  float bounds[6];
+  input->GetBounds(bounds);
+  
+  vtkIdType i;
+  float point[3];
+  vtkGenericCell *cell = vtkGenericCell::New();
+  
+  for (i = 0; i < numPts; i++)
+    {
+    points->GetPoint(i, point);
+    
+    switch (this->Plane)
+      {
+      case VTK_USE_X_MIN:
+        outPoints->InsertNextPoint(-point[0] + 2*bounds[0], point[1],
+                                   point[2]);
+        break;
+      case VTK_USE_X_MAX:
+        outPoints->InsertNextPoint(-point[0] + 2*bounds[1], point[1],
+                                   point[2]);
+        break;
+      case VTK_USE_Y_MIN:
+        outPoints->InsertNextPoint(point[0], -point[1] + 2*bounds[2],
+                                   point[2]);
+        break;
+      case VTK_USE_Y_MAX:
+        outPoints->InsertNextPoint(point[0], -point[1] + 2*bounds[3],
+                                   point[2]);
+        break;
+      case VTK_USE_Z_MIN:
+        outPoints->InsertNextPoint(point[0], point[1],
+                                   -point[2] + 2*bounds[4]);
+        break;
+      case VTK_USE_Z_MAX:
+        outPoints->InsertNextPoint(point[0], point[1],
+                                   -point[2] + 2*bounds[5]);
+        break;
+      }
+    }
+  
+  int numCellPts, j, cellType;
+  vtkIdType *newCellPts;
+  vtkIdList *cellPts;
+  
+  for (i = 0; i < numCells; i++)
+    {
+    input->GetCell(i, cell);
+    numCellPts = cell->GetNumberOfPoints();
+    cellType = cell->GetCellType();
+    newCellPts = new vtkIdType[numCellPts];
+    cellPts = cell->GetPointIds();
+    for (j = 0; j < numCellPts; j++)
+      {
+      newCellPts[j] = cellPts->GetId(j) + numPts;
+      }
+    output->InsertNextCell(cellType, numCellPts, newCellPts);
+    delete [] newCellPts;
+    }
+  
+  cell->Delete();
+}
+
+//---------------------------------------------------------------------------
+void vtkReflectionFilter::PrintSelf(ostream &os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+
+  os << indent << "Plane: " << this->Plane << endl;
+}
---
+++ Graphics/vtkReflectionFilter.h
@@ -0,0 +1,67 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkReflectionFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkReflectionFilter - reflects a data set across a plane
+// .SECTION Description
+// The vtkReflectionFilter reflects a polygonal data set across one of the
+// planes formed by the data set's bounding box.
+
+#ifndef __vtkReflectionFilter_h
+#define __vtkReflectionFilter_h
+
+#include "vtkPolyDataToPolyDataFilter.h"
+
+#define VTK_USE_X_MIN 0
+#define VTK_USE_Y_MIN 1
+#define VTK_USE_Z_MIN 2
+#define VTK_USE_X_MAX 3
+#define VTK_USE_Y_MAX 4
+#define VTK_USE_Z_MAX 5
+
+class VTK_GRAPHICS_EXPORT vtkReflectionFilter : public vtkPolyDataToPolyDataFilter
+{
+public:
+  static vtkReflectionFilter *New();
+  
+  vtkTypeRevisionMacro(vtkReflectionFilter, vtkPolyDataToPolyDataFilter);
+  void PrintSelf(ostream &os, vtkIndent indent);
+  
+  vtkSetClampMacro(Plane, int, 0, 5);
+  vtkGetMacro(Plane, int);
+  void SetPlaneToXMin() { this->SetPlane(VTK_USE_X_MIN); };
+  void SetPlaneToYMin() { this->SetPlane(VTK_USE_Y_MIN); };
+  void SetPlaneToZMin() { this->SetPlane(VTK_USE_Z_MIN); };
+  void SetPlaneToXMax() { this->SetPlane(VTK_USE_X_MAX); };
+  void SetPlaneToYMax() { this->SetPlane(VTK_USE_Y_MAX); };
+  void SetPlaneToZMax() { this->SetPlane(VTK_USE_Z_MAX); };
+  
+protected:
+  vtkReflectionFilter();
+  ~vtkReflectionFilter();
+  
+  void Execute();
+
+  int Plane;
+  
+private:
+  vtkReflectionFilter(const vtkReflectionFilter&);  // Not implemented
+  void operator=(const vtkReflectionFilter&);  // Not implemented
+};
+
+#endif
+
+
