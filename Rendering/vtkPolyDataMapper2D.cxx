454f782c8408325b7a9d1f1925aab1e8eccbd5b8
Charles Law <charles.law@kitware.com>
2002-02-27 07:51:05 -0500

    Consistent Register/UnRegisters.

--- Rendering/vtkPolyDataMapper2D.cxx
+++ Rendering/vtkPolyDataMapper2D.cxx
@@ -20,7 +20,7 @@
 
 #include "vtkLookupTable.h"
 
-vtkCxxRevisionMacro(vtkPolyDataMapper2D, "1.36");
+vtkCxxRevisionMacro(vtkPolyDataMapper2D, "1.37");
 
 vtkPolyDataMapper2D::vtkPolyDataMapper2D()
 {
@@ -77,7 +77,7 @@ vtkPolyDataMapper2D::~vtkPolyDataMapper2D()
     }
   if ( this->Colors != NULL )
     {
-    this->Colors->Delete();
+    this->Colors->UnRegister(this);
     }
   
   this->SetInput(NULL);
@@ -144,8 +144,12 @@ vtkUnsignedCharArray *vtkPolyDataMapper2D::MapScalars(float alpha)
         this->LookupTable->SetRange(this->ScalarRange);
         }
       this->LookupTable->SetAlpha(alpha);
+      // Map Scalar constructs a array and returns it.
+      // Not having "New" or "Make" in the name breaks VTK conventions but, ...
       this->Colors = this->LookupTable->
         MapScalars(scalars, this->ColorMode, this->ArrayComponent);
+      this->Colors->Register(this);
+      this->Colors->Delete();
       }
     }
 
