0a75cb4d9c7a4dd43d9bf7f75793b86c60adca04
David Gobbi <david.gobbi@gmail.com>
2002-03-25 14:37:50 -0500

    NEW: vtkSphericalTransform and vtkCylindricalTransform

---
+++ Common/vtkCylindricalTransform.cxx
@@ -0,0 +1,172 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkCylindricalTransform.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkCylindricalTransform.h"
+#include "vtkObjectFactory.h"
+#include "vtkMath.h"
+#include <math.h>
+#include <stdlib.h>
+
+vtkCxxRevisionMacro(vtkCylindricalTransform, "1.1");
+vtkStandardNewMacro(vtkCylindricalTransform);
+
+//----------------------------------------------------------------------------
+vtkCylindricalTransform::vtkCylindricalTransform()
+{
+}
+
+vtkCylindricalTransform::~vtkCylindricalTransform()
+{
+}
+
+void vtkCylindricalTransform::PrintSelf(ostream& os, vtkIndent indent)
+{
+  vtkWarpTransform::PrintSelf(os,indent);
+}
+
+void vtkCylindricalTransform::InternalDeepCopy(vtkAbstractTransform *transform)
+{
+  vtkCylindricalTransform *cylindricalTransform = 
+    (vtkCylindricalTransform *)transform;
+
+  // copy these even though they aren't used
+  this->SetInverseTolerance(cylindricalTransform->InverseTolerance);
+  this->SetInverseIterations(cylindricalTransform->InverseIterations);
+
+  // copy the inverse flag, which is used
+  if (this->InverseFlag != cylindricalTransform->InverseFlag)
+    {
+    this->InverseFlag = cylindricalTransform->InverseFlag;
+    this->Modified();
+    }
+}
+
+vtkAbstractTransform *vtkCylindricalTransform::MakeTransform()
+{
+  return vtkCylindricalTransform::New();
+}
+
+template<class T>
+static void vtkCylindricalToRectangular(const T inPoint[3], T outPoint[3],
+                                        T derivative[3][3])
+{
+  T r = inPoint[0];
+  T sintheta = sin(inPoint[1]);
+  T costheta = cos(inPoint[1]);
+  T z = inPoint[2];
+
+  outPoint[0] = r*costheta;
+  outPoint[1] = r*sintheta;
+  outPoint[2] = z;
+
+  if (derivative)
+    {
+    derivative[0][0] =    costheta;
+    derivative[0][1] = -r*sintheta;
+    derivative[0][2] =           0;
+
+    derivative[1][0] =    sintheta;
+    derivative[1][1] =  r*costheta;
+    derivative[1][2] =           0;
+
+    derivative[2][0] =           0;
+    derivative[2][1] =           0;
+    derivative[2][2] =           1;
+    }
+}
+
+template<class T>
+static void vtkRectangularToCylindrical(const T inPoint[3], T outPoint[3])
+{
+  T x = inPoint[0];
+  T y = inPoint[1];
+  T z = inPoint[2];
+  
+  T rr = x*x + y*y;
+
+  outPoint[0] = sqrt(rr);
+  if (rr == 0)
+    {
+    outPoint[1] = 0;
+    }
+  else
+    {
+    outPoint[1] = atan2(y, x);
+    }
+  outPoint[2] = z;
+}
+
+void vtkCylindricalTransform::ForwardTransformPoint(const float inPoint[3],
+                                                    float outPoint[3])
+{
+  vtkCylindricalToRectangular(inPoint, outPoint, (float (*)[3])0);
+}
+
+void vtkCylindricalTransform::ForwardTransformPoint(const double inPoint[3],
+                                                    double outPoint[3])
+{
+  vtkCylindricalToRectangular(inPoint, outPoint, (double (*)[3])0);
+}
+
+void vtkCylindricalTransform::ForwardTransformDerivative(
+                                                       const float inPoint[3],
+                                                       float outPoint[3],
+                                                       float derivative[3][3])
+{
+  vtkCylindricalToRectangular(inPoint, outPoint, derivative);
+}
+
+void vtkCylindricalTransform::ForwardTransformDerivative(
+                                                       const double inPoint[3],
+                                                       double outPoint[3],
+                                                       double derivative[3][3])
+{
+  vtkCylindricalToRectangular(inPoint, outPoint, derivative);
+}
+
+void vtkCylindricalTransform::InverseTransformPoint(const float inPoint[3],
+                                                    float outPoint[3])
+{
+  vtkRectangularToCylindrical(inPoint, outPoint);
+}
+
+void vtkCylindricalTransform::InverseTransformPoint(const double inPoint[3],
+                                                    double outPoint[3])
+{
+  vtkRectangularToCylindrical(inPoint, outPoint);
+}
+
+void vtkCylindricalTransform::InverseTransformDerivative(
+                                                       const float inPoint[3],
+                                                       float outPoint[3],
+                                                       float derivative[3][3])
+{
+  float tmp[3];
+  vtkRectangularToCylindrical(inPoint, outPoint);
+  vtkCylindricalToRectangular(outPoint, tmp, derivative);
+}
+
+void vtkCylindricalTransform::InverseTransformDerivative(
+                                                       const double inPoint[3],
+                                                       double outPoint[3],
+                                                       double derivative[3][3])
+{
+  double tmp[3];
+  vtkRectangularToCylindrical(inPoint, outPoint);
+  vtkCylindricalToRectangular(outPoint, tmp, derivative);
+}
+
---
+++ Common/vtkSphericalTransform.cxx
@@ -0,0 +1,177 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkSphericalTransform.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkSphericalTransform.h"
+#include "vtkObjectFactory.h"
+#include "vtkMath.h"
+#include <math.h>
+#include <stdlib.h>
+
+vtkCxxRevisionMacro(vtkSphericalTransform, "1.1");
+vtkStandardNewMacro(vtkSphericalTransform);
+
+//----------------------------------------------------------------------------
+vtkSphericalTransform::vtkSphericalTransform()
+{
+}
+
+vtkSphericalTransform::~vtkSphericalTransform()
+{
+}
+
+void vtkSphericalTransform::PrintSelf(ostream& os, vtkIndent indent)
+{
+  vtkWarpTransform::PrintSelf(os,indent);
+}
+
+void vtkSphericalTransform::InternalDeepCopy(vtkAbstractTransform *transform)
+{
+  vtkSphericalTransform *sphericalTransform = 
+    (vtkSphericalTransform *)transform;
+
+  // copy these even though they aren't used
+  this->SetInverseTolerance(sphericalTransform->InverseTolerance);
+  this->SetInverseIterations(sphericalTransform->InverseIterations);
+
+  // copy the inverse flag, which is used
+  if (this->InverseFlag != sphericalTransform->InverseFlag)
+    {
+    this->InverseFlag = sphericalTransform->InverseFlag;
+    this->Modified();
+    }
+}
+
+vtkAbstractTransform *vtkSphericalTransform::MakeTransform()
+{
+  return vtkSphericalTransform::New();
+}
+
+template<class T>
+static void vtkSphericalToRectangular(const T inPoint[3], T outPoint[3],
+                                      T derivative[3][3])
+{
+  T r = inPoint[0];
+  T sinphi = sin(inPoint[1]);
+  T cosphi = cos(inPoint[1]);
+  T sintheta = sin(inPoint[2]);
+  T costheta = cos(inPoint[2]);
+
+  outPoint[0] = r*sinphi*costheta;
+  outPoint[1] = r*sinphi*sintheta;
+  outPoint[2] = r*cosphi;
+
+  if (derivative)
+    {
+    derivative[0][0] =    sinphi*costheta;
+    derivative[0][1] =  r*cosphi*costheta;
+    derivative[0][2] = -r*sinphi*sintheta;
+
+    derivative[1][0] =    sinphi*sintheta;
+    derivative[1][1] =  r*cosphi*sintheta;
+    derivative[1][2] =  r*sinphi*costheta;
+
+    derivative[2][0] =    cosphi;
+    derivative[2][1] = -r*sinphi;
+    derivative[2][2] =    0;
+    }
+}
+
+template<class T>
+static void vtkRectangularToSpherical(const T inPoint[3], T outPoint[3])
+{
+  T x = inPoint[0];
+  T y = inPoint[1];
+  T z = inPoint[2];
+  
+  T RR = x*x + y*y;
+  T rr = RR + z*z;
+
+  outPoint[0] = sqrt(rr);
+  if (rr == 0)
+    {
+    outPoint[1] = 0;
+    }
+  else
+    {
+    outPoint[1] = atan2(sqrt(RR), z);
+    }
+  if (RR == 0)
+    {
+    outPoint[2] = 0;
+    }
+  else
+    {
+    outPoint[2] = atan2(y, x);
+    }
+}
+
+void vtkSphericalTransform::ForwardTransformPoint(const float inPoint[3],
+                                                  float outPoint[3])
+{
+  vtkSphericalToRectangular(inPoint, outPoint, (float (*)[3])0);
+}
+
+void vtkSphericalTransform::ForwardTransformPoint(const double inPoint[3],
+                                                  double outPoint[3])
+{
+  vtkSphericalToRectangular(inPoint, outPoint, (double (*)[3])0);
+}
+
+void vtkSphericalTransform::ForwardTransformDerivative(const float inPoint[3],
+                                                       float outPoint[3],
+                                                       float derivative[3][3])
+{
+  vtkSphericalToRectangular(inPoint, outPoint, derivative);
+}
+
+void vtkSphericalTransform::ForwardTransformDerivative(const double inPoint[3],
+                                                       double outPoint[3],
+                                                       double derivative[3][3])
+{
+  vtkSphericalToRectangular(inPoint, outPoint, derivative);
+}
+
+void vtkSphericalTransform::InverseTransformPoint(const float inPoint[3],
+                                                  float outPoint[3])
+{
+  vtkRectangularToSpherical(inPoint, outPoint);
+}
+
+void vtkSphericalTransform::InverseTransformPoint(const double inPoint[3],
+                                                  double outPoint[3])
+{
+  vtkRectangularToSpherical(inPoint, outPoint);
+}
+
+void vtkSphericalTransform::InverseTransformDerivative(const float inPoint[3],
+                                                       float outPoint[3],
+                                                       float derivative[3][3])
+{
+  float tmp[3];
+  vtkRectangularToSpherical(inPoint, outPoint);
+  vtkSphericalToRectangular(outPoint, tmp, derivative);
+}
+
+void vtkSphericalTransform::InverseTransformDerivative(const double inPoint[3],
+                                                       double outPoint[3],
+                                                       double derivative[3][3])
+{
+  double tmp[3];
+  vtkRectangularToSpherical(inPoint, outPoint);
+  vtkSphericalToRectangular(outPoint, tmp, derivative);
+}
+
