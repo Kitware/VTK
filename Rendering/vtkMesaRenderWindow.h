76bc36ee03aea5c0e9f2c3c0a221db9f35df0dcd
Berk Geveci <berk.geveci@kitware.com>
2002-03-27 09:29:25 -0500

    Mesa headers should always reflect changes to OpenGL headers.

--- Rendering/vtkMesaRenderWindow.h
+++ Rendering/vtkMesaRenderWindow.h
@@ -61,24 +61,41 @@ public:
   // Description:
   // Set/Get the pixel data of an image, transmitted as RGBRGB... 
   virtual unsigned char *GetPixelData(int x,int y,int x2,int y2,int front);
-  virtual void SetPixelData(int x,int y,int x2,int y2,unsigned char *,
-                            int front);
+  virtual int GetPixelData(int x,int y,int x2,int y2, int front,
+			   vtkUnsignedCharArray*);
+  virtual int SetPixelData(int x,int y,int x2,int y2,unsigned char *,
+			   int front);
+  virtual int SetPixelData(int x,int y,int x2,int y2, vtkUnsignedCharArray*,
+			   int front);
 
   // Description:
   // Set/Get the pixel data of an image, transmitted as RGBARGBA... 
   virtual float *GetRGBAPixelData(int x,int y,int x2,int y2,int front);
-  virtual void SetRGBAPixelData(int x,int y,int x2,int y2,float *,int front,
+  virtual int GetRGBAPixelData(int x,int y,int x2,int y2, int front,
+			       vtkFloatArray* data);
+  virtual int SetRGBAPixelData(int x,int y,int x2,int y2,float *,int front,
                                 int blend=0);
+  virtual int SetRGBAPixelData(int x,int y,int x2,int y2, vtkFloatArray*,
+				int front, int blend=0);
   virtual void ReleaseRGBAPixelData(float *data);
   virtual unsigned char *GetRGBACharPixelData(int x,int y,int x2,int y2,
                                               int front);
-  virtual void SetRGBACharPixelData(int x,int y,int x2,int y2,unsigned char *,
+  virtual int GetRGBACharPixelData(int x,int y,int x2,int y2, int front,
+				   vtkUnsignedCharArray* data);
+  virtual int SetRGBACharPixelData(int x,int y,int x2,int y2,unsigned char *,
                                     int front, int blend=0);  
+  virtual int SetRGBACharPixelData(int x,int y,int x2,int y2,
+				   vtkUnsignedCharArray *,
+				   int front, int blend=0);  
 
   // Description:
   // Set/Get the zbuffer data from an image
   virtual float *GetZbufferData( int x1, int y1, int x2, int y2 );
-  virtual void SetZbufferData( int x1, int y1, int x2, int y2, float *buffer );
+  virtual int GetZbufferData( int x1, int y1, int x2, int y2, 
+			      vtkFloatArray* z );
+  virtual int SetZbufferData( int x1, int y1, int x2, int y2, float *buffer );
+  virtual int SetZbufferData( int x1, int y1, int x2, int y2, 
+			      vtkFloatArray *buffer );
 
   // Description:
   // Make this window the current Mesa context.
