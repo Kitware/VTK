3ba5361e7396420142c61180be20888f7fb267ee
Bill Hoffman <bill.hoffman@kitware.com>
2002-03-27 14:03:30 -0500

    BUG: fix reference count problems

--- Rendering/vtkInteractorStyle.cxx
+++ Rendering/vtkInteractorStyle.cxx
@@ -24,7 +24,7 @@
 #include "vtkOldStyleCallbackCommand.h"
 #include "vtkCallbackCommand.h"
 
-vtkCxxRevisionMacro(vtkInteractorStyle, "1.55");
+vtkCxxRevisionMacro(vtkInteractorStyle, "1.56");
 
 //----------------------------------------------------------------------------
 vtkInteractorStyle *vtkInteractorStyle::New() 
@@ -99,6 +99,11 @@ vtkInteractorStyle::~vtkInteractorStyle()
   if ( this->CurrentRenderer)
     {
     this->CurrentRenderer->UnRegister(this);
+    this->CurrentRenderer = NULL;
+    }
+  if (this->CurrentCamera)
+    {
+    this->CurrentCamera->UnRegister(this);
     this->CurrentRenderer = NULL;
     }
 
@@ -304,9 +309,17 @@ void vtkInteractorStyle::FindPokedRenderer(int x,int y)
 
 //----------------------------------------------------------------------------
 void  vtkInteractorStyle::FindPokedCamera(int x,int y) 
-{
-  this->CurrentRenderer = this->Interactor->FindPokedRenderer(x,y);
+{  
+  if (this->CurrentCamera)
+    {
+    this->CurrentCamera->UnRegister(this);
+    }
+  this->FindPokedRenderer(x,y);
   this->CurrentCamera = this->CurrentRenderer->GetActiveCamera();
+  if(this->CurrentCamera)
+    {
+    this->CurrentCamera->Register(this);
+    }
   
   // side effect stuff
   float *vp = this->CurrentRenderer->GetViewport();
