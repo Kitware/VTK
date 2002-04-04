f254de237e90df386abe88b441ec17775f65c821
Prabhu Ramachandran <prabhu_r@users.sourceforge.net>
2002-04-04 12:23:19 -0500

    Modified to use the new vtk package structure.  These files exist for backwards compatibility.

--- Wrapping/Python/vtkpython.py
+++ Wrapping/Python/vtkpython.py
@@ -4,46 +4,32 @@ This module loads the various shared object files that make up
 the vtk-python interface. 
 """
 
-#-----------------------------------------------------------------------------
-# load all of the kit modules into python 
-
-import os
-
-required_kits = ['Common','Filtering','IO','Imaging','Graphics']
-optional_kits = ['Rendering','Hybrid','Parallel','Patented']
-
-# the vtkpython.kits variable tells us which kits we actually have
-kits = []
-
-def vtkCreateKitModuleName(kit):
-    """vtkCreateKitModuleName(kit) -- create module name from kit name
-    """
-    if os.name == 'posix':
-        return 'libvtk'+kit+'Python'
-    else:
-        return 'vtk'+kit+'Python'
-
-# load the required kits
-for kit in required_kits:
-    kit_name = vtkCreateKitModuleName(kit)
-    exec "from "+kit_name+" import *"
-    kits.append(kit)
-
-# try to load the optional kits
-for kit in optional_kits:
-    kit_name = vtkCreateKitModuleName(kit)
-    try:
-        exec "from "+kit_name+" import *"
-        kits.append(kit)
-    except ImportError:
-        pass
+from vtk import *
 
 #-----------------------------------------------------------------------------
 # the following pieces are for the vtk regression testing and examples
 
-import sys, os.path
-from vtkGetDataRoot import vtkGetDataRoot
-    
+import sys, os
+
+def vtkGetDataRoot():
+    """vtkGetDataRoot() -- return vtk example data directory
+    """
+    dataIndex=-1;
+    for i in range(0, len(sys.argv)):
+        if sys.argv[i] == '-D' and i < len(sys.argv)-1:
+            dataIndex = i+1
+
+    if dataIndex != -1:
+        dataRoot = sys.argv[dataIndex]
+    else:
+        try:
+            dataRoot = os.environ['VTK_DATA_ROOT']
+        except KeyError:
+            dataRoot = '../../../../VTKData'
+
+    return dataRoot
+
+
 def vtkRegressionTestImage( renWin ):
     """vtkRegressionTestImage(renWin) -- produce regression image for window
 
