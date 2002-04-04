d19af7d037f233ebeda4b8df79e0ae907da4cc29
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-04-03 19:13:03 -0500

    Add simple error check

--- Rendering/vtkMapper.cxx
+++ Rendering/vtkMapper.cxx
@@ -18,7 +18,7 @@
 #include "vtkMapper.h"
 #include "vtkLookupTable.h"
 
-vtkCxxRevisionMacro(vtkMapper, "1.96");
+vtkCxxRevisionMacro(vtkMapper, "1.97");
 
 // Initialize static member that controls global immediate mode rendering
 static int vtkMapperGlobalImmediateModeRendering = 0;
@@ -271,7 +271,7 @@ void vtkMapper::ColorByArrayComponent(int arrayNum, int component)
 
 void vtkMapper::ColorByArrayComponent(const char* arrayName, int component)
 {
-  if (strcmp(this->ArrayName, arrayName) == 0 &&
+  if (arrayName || strcmp(this->ArrayName, arrayName) == 0 &&
       component == this->ArrayComponent &&
       this->ArrayAccessMode == VTK_GET_ARRAY_BY_ID)
     {
