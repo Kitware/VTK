a6b5330468e24141559c59d9dce8e09680ee5848
Will Schroeder <will.schroeder@kitware.com>
2002-04-04 13:51:12 -0500

    ENH:Move the light example

--- Examples/GUI/Tcl/SphereWidget.tcl
+++ Examples/GUI/Tcl/SphereWidget.tcl
@@ -2,59 +2,61 @@ package require vtk
 package require vtkinteraction
 package require vtktesting
 
-# This example demonstrates how to use the vtkSphereWidget to seed
-# streamlines in a dataset.
+# This example demonstrates how to use the vtkSphereWidget to control the
+# position of a light.
 
 # Start by loading some data.
 #
-vtkPLOT3DReader pl3d
-    pl3d SetXYZFileName "$VTK_DATA_ROOT/Data/combxyz.bin"
-    pl3d SetQFileName "$VTK_DATA_ROOT/Data/combq.bin"
-    pl3d SetScalarFunctionNumber 100
-    pl3d SetVectorFunctionNumber 202
-    pl3d Update
+vtkDEMReader dem
+    dem SetFileName "$VTK_DATA_ROOT/Data/SainteHelens.dem"
+    dem Update
 
-# The sphere widget is used in conjunction with vtkPointSource to
-# generate seeds for streamribbons.
-#
-vtkPointSource pointSource
-    pointSource SetNumberOfPoints 20
-vtkSphereWidget sphereWidget
-    sphereWidget SetInput [pl3d GetOutput]
-    sphereWidget SetRepresentationToWireframe
-    sphereWidget PlaceWidget
-vtkPolyData sphere
-    sphereWidget GetPolyData sphere
+set Scale 2
+vtkLookupTable lut
+  lut SetHueRange 0.6 0
+  lut SetSaturationRange 1.0 0
+  lut SetValueRange 0.5 1.0
+set lo [expr $Scale * [lindex [dem GetElevationBounds] 0]]
+set hi [expr $Scale * [lindex [dem GetElevationBounds] 1]]
 
-vtkRungeKutta4 rk4
-vtkStreamLine streamer
-    streamer SetInput [pl3d GetOutput]
-    streamer SetSource [pointSource GetOutput]
-    streamer SetMaximumPropagationTime 100
-    streamer SetIntegrationStepLength .2
-    streamer SetStepLength .001
-    streamer SetNumberOfThreads 1
-    streamer SetIntegrationDirectionToForward
-    streamer VorticityOn
-    streamer SetIntegrator rk4
-vtkRibbonFilter rf
-    rf SetInput [streamer GetOutput]
-    rf SetWidth 0.1
-    rf SetWidthFactor 5
-vtkPolyDataMapper streamMapper
-    streamMapper SetInput [rf GetOutput]
-    eval streamMapper SetScalarRange [[pl3d GetOutput] GetScalarRange]
-vtkActor streamlines
-    streamlines SetMapper streamMapper
-    streamlines VisibilityOff
+vtkImageShrink3D shrink
+  shrink SetShrinkFactors 4 4 1
+  shrink SetInput [dem GetOutput]
+  shrink AveragingOn
 
-# An outline is shown for context.
-vtkStructuredGridOutlineFilter outline
-    outline SetInput [pl3d GetOutput]
-vtkPolyDataMapper outlineMapper
-    outlineMapper SetInput [outline GetOutput]
-vtkActor outlineActor
-    outlineActor SetMapper outlineMapper
+vtkImageDataGeometryFilter geom
+  geom SetInput [shrink GetOutput]
+  geom ReleaseDataFlagOn
+
+vtkWarpScalar warp
+  warp SetInput [geom GetOutput]
+  warp SetNormal 0 0 1
+  warp UseNormalOn
+  warp SetScaleFactor $Scale
+  warp ReleaseDataFlagOn
+
+vtkElevationFilter elevation
+  elevation SetInput [warp GetOutput]
+  elevation SetLowPoint 0 0 $lo
+  elevation SetHighPoint 0 0 $hi
+  eval elevation SetScalarRange $lo $hi
+  elevation ReleaseDataFlagOn
+
+vtkPolyDataNormals normals
+  normals SetInput [elevation GetPolyDataOutput]
+  normals SetFeatureAngle 60
+  normals ConsistencyOff
+  normals SplittingOff
+  normals ReleaseDataFlagOn
+
+vtkPolyDataMapper demMapper
+  demMapper SetInput [normals GetOutput]
+  eval demMapper SetScalarRange $lo $hi
+  demMapper SetLookupTable lut
+  demMapper ImmediateModeRenderingOn
+
+vtkLODActor demActor
+  demActor SetMapper demMapper
 
 # Create the RenderWindow, Renderer and both Actors
 #
@@ -63,26 +65,39 @@ vtkRenderWindow renWin
     renWin AddRenderer ren1
 vtkRenderWindowInteractor iren
     iren SetRenderWindow renWin
+    iren LightFollowCameraOff
+    iren SetInteractorStyle ""
 
 # Associate the line widget with the interactor
+vtkSphereWidget sphereWidget
 sphereWidget SetInteractor iren
-sphereWidget AddObserver StartInteractionEvent BeginInteraction
-sphereWidget AddObserver InteractionEvent GenerateStreamlines
+sphereWidget SetProp3D demActor
+sphereWidget SetPlaceFactor 4
+sphereWidget PlaceWidget
+sphereWidget TranslationOff
+sphereWidget ScaleOff
+sphereWidget HandleVisibilityOn
+sphereWidget AddObserver InteractionEvent MoveLight
 
 # Add the actors to the renderer, set the background and size
 #
-ren1 AddActor outlineActor
-ren1 AddActor streamlines
-
+ren1 AddActor demActor
 ren1 SetBackground 1 1 1
 renWin SetSize 300 300
 ren1 SetBackground 0.1 0.2 0.4
 
 set cam1 [ren1 GetActiveCamera]
-$cam1 SetClippingRange 3.95297 50
-$cam1 SetFocalPoint 9.71821 0.458166 29.3999
-$cam1 SetPosition 2.7439 -37.3196 38.7167
-$cam1 SetViewUp -0.16123 0.264271 0.950876
+$cam1 SetViewUp 0 0 1
+eval $cam1 SetFocalPoint [[dem GetOutput] GetCenter]
+$cam1 SetPosition 1 0 0
+ren1 ResetCamera
+$cam1 Elevation 25
+$cam1 Azimuth 125
+$cam1 Zoom 1.25
+
+vtkLight light
+eval light SetFocalPoint [[dem GetOutput] GetCenter]
+ren1 AddLight light
 
 # render the image
 #
@@ -93,13 +108,8 @@ renWin Render
 wm withdraw .
 
 # Actually probe the data
-proc BeginInteraction {} {
-    streamlines VisibilityOn
-    renWin Render
-}
 
-proc GenerateStreamlines {} {
-    pointSource SetRadius [sphereWidget GetRadius]
-    eval pointSource SetCenter [sphereWidget GetCenter]
+proc MoveLight {} {
+    eval light SetPosition [sphereWidget GetHandlePosition]
 }
 
