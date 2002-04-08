6ff1017126e43cce78ad6f29ddf4bf4aead0e14e
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-04-07 21:04:34 -0400

    Even though some people do not agree with me, this class will help several people.

---
+++ Common/vtkString.cxx
@@ -0,0 +1,101 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkString.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkString.h"
+#include "vtkObjectFactory.h"
+
+vtkCxxRevisionMacro(vtkString, "1.1");
+vtkStandardNewMacro(vtkString);
+ 
+// Description:
+// This method returns the size of string. If the string is empty,
+// it returns 0. It can handle null pointers.
+vtkIdType vtkString::Length(const char* str)
+{
+  if ( !str )
+    {
+    return 0;
+    }
+  return static_cast<vtkIdType>(strlen(str));
+}
+
+  
+// Description:
+// Copy string to the other string.
+void vtkString::Copy(char* dest, const char* src)
+{
+  if ( !dest )
+    {
+    return;
+    }
+  if ( !src )
+    {
+    *dest = 0;
+    return;
+    }
+  strcpy(dest, src);
+}
+
+// Description:
+// This method makes a duplicate of the string similar to
+// C function strdup but it uses new to create new string, so
+// you can use delete to remove it. It returns empty string 
+// "" if the input is empty.
+char* vtkString::Duplicate(const char* str)
+{  
+  char *newstr = new char [ vtkString::Length(str) + 1 ];
+  vtkString::Copy(newstr, str);
+  return newstr;
+}
+
+// Description:
+// This method compare two strings. It is similar to strcmp,
+// but it can handle null pointers.
+int vtkString::Compare(const char* str1, const char* str2)
+{
+  if ( !str1 )
+    {
+    return -1;
+    }
+  if ( !str2 )
+    {
+    return 1;
+    }
+  return strcmp(str1, str2);
+}
+
+// Description:
+// Check if the first string starts with the second one.
+int vtkString::StartsWith(const char* str1, const char* str2)
+{
+  if ( !str1 || !str2 || strlen(str1) < strlen(str2) )
+    {
+    return 0;
+    }
+  return !strncmp(str1, str2, strlen(str2));  
+}
+
+// Description:
+// Check if the first string starts with the second one.
+int vtkString::EndsWith(const char* str1, const char* str2)
+{
+  if ( !str1 || !str2 || strlen(str1) < strlen(str2) )
+    {
+    return 0;
+    }
+  return !strncmp(str1 + (strlen(str1)-strlen(str2)), str2, strlen(str2));
+}
---
+++ Common/vtkString.h
@@ -0,0 +1,74 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkString.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkString - performs common string operations
+// .SECTION Description
+// vtkString is a collection of methods to perform common string operations. 
+// The strings in this case are not some reference counted objects but
+// the traditional c-strings (char*). This class provides platform 
+// independent methods for creating, copying, ... of strings.
+
+#ifndef __vtkString_h
+#define __vtkString_h
+
+#include "vtkObject.h"
+
+class VTK_COMMON_EXPORT vtkString : public vtkObject
+{
+public:
+  static vtkString *New();
+  vtkTypeRevisionMacro(vtkString,vtkObject);
+
+  // Description:
+  // This method returns the size of string. If the string is empty,
+  // it returns 0. It can handle null pointers.
+  static vtkIdType Length(const char* str);
+
+  // Description:
+  // Copy string to the other string.
+  static void Copy(char* dest, const char* src);
+
+  // Description:
+  // This method makes a duplicate of the string similar to
+  // C function strdup but it uses new to create new string, so
+  // you can use delete to remove it. It returns empty string 
+  // "" if the input is empty.
+  static char* Duplicate(const char* str);
+
+  // Description:
+  // This method compare two strings. It is similar to strcmp,
+  // but it can handle null pointers.
+  static int Compare(const char* str1, const char* str2);
+
+  // Description:
+  // Check if the first string starts with the second one.
+  static int StartsWith(const char* str1, const char* str2);
+
+  // Description:
+  // Check if the first string starts with the second one.
+  static int EndsWith(const char* str1, const char* str2);
+  
+protected:
+  vtkString() {};
+  ~vtkString() {};
+
+private:
+  vtkString(const vtkString&);  // Not implemented.
+  void operator=(const vtkString&);  // Not implemented.
+};
+
+#endif
