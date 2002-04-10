87e2430ca9a14b4d6f1edea72048a8f0a704ce15
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-04-10 14:40:24 -0400

    Remove warnings on Visual Studio using /W4

--- Common/vtkContainer.h
+++ Common/vtkContainer.h
@@ -182,20 +182,20 @@ static inline void* vtkContainerCreateMethod(void* d1)
 
 static inline void vtkContainerDeleteMethod(vtkObject* d1) 
 { d1->UnRegister(0); /* cout << "UR(d1)" << endl; */ }
-static inline void vtkContainerDeleteMethod(char d1) {}
-static inline void vtkContainerDeleteMethod(short d1) {}
-static inline void vtkContainerDeleteMethod(int d1) {}
-static inline void vtkContainerDeleteMethod(long d1) {}
-static inline void vtkContainerDeleteMethod(unsigned char d1) {}
-static inline void vtkContainerDeleteMethod(unsigned short d1) {}
-static inline void vtkContainerDeleteMethod(unsigned int d1) {}
-static inline void vtkContainerDeleteMethod(unsigned long d1) {}
-static inline void vtkContainerDeleteMethod(float d1) {}
-static inline void vtkContainerDeleteMethod(double d1) {}
+static inline void vtkContainerDeleteMethod(char) {}
+static inline void vtkContainerDeleteMethod(short) {}
+static inline void vtkContainerDeleteMethod(int) {}
+static inline void vtkContainerDeleteMethod(long) {}
+static inline void vtkContainerDeleteMethod(unsigned char) {}
+static inline void vtkContainerDeleteMethod(unsigned short) {}
+static inline void vtkContainerDeleteMethod(unsigned int) {}
+static inline void vtkContainerDeleteMethod(unsigned long) {}
+static inline void vtkContainerDeleteMethod(float) {}
+static inline void vtkContainerDeleteMethod(double) {}
 static inline void vtkContainerDeleteMethod(const char* d1) 
 { char *ch = const_cast<char*>(d1); delete [] ch; } 
 static inline void vtkContainerDeleteMethod(char* d1) { delete [] d1; }
-static inline void vtkContainerDeleteMethod(void* d1) {}
+static inline void vtkContainerDeleteMethod(void*) {}
 
 
 #endif 
