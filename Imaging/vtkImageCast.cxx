0c33d3afb75c5cc1ebd8c4a13827f77da54e12c3
Ken Martin <ken.martin@kitware.com>
2002-04-16 18:42:45 -0400

    updated to use iterators

--- Imaging/vtkImageCast.cxx
+++ Imaging/vtkImageCast.cxx
@@ -17,9 +17,10 @@
 =========================================================================*/
 #include "vtkImageCast.h"
 #include "vtkObjectFactory.h"
+#include "vtkImageProgressIterator.h"
 
 
-vtkCxxRevisionMacro(vtkImageCast, "1.41");
+vtkCxxRevisionMacro(vtkImageCast, "1.42");
 vtkStandardNewMacro(vtkImageCast);
 
 //----------------------------------------------------------------------------
@@ -57,84 +58,58 @@ void vtkImageCast::UpdateData(vtkDataObject *data)
 // This templated function executes the filter for any type of data.
 template <class IT, class OT>
 static void vtkImageCastExecute(vtkImageCast *self,
-                                vtkImageData *inData, IT *inPtr,
-                                vtkImageData *outData, OT *outPtr,
-                                int outExt[6], int id)
+                                vtkImageData *inData,
+                                vtkImageData *outData,
+                                int outExt[6], int id, IT *, OT *)
 {
+  vtkImageIterator<IT> inIt(inData, outExt);
+  vtkImageProgressIterator<OT> outIt(outData, outExt, self, id);
+
   double typeMin, typeMax, val;
   int clamp;
-  int idxR, idxY, idxZ;
-  int maxY, maxZ;
-  int inIncX, inIncY, inIncZ;
-  int outIncX, outIncY, outIncZ;
-  int rowLength;
-  unsigned long count = 0;
-  unsigned long target;
 
   // for preventing overflow
   typeMin = outData->GetScalarTypeMin();
   typeMax = outData->GetScalarTypeMax();
   clamp = self->GetClampOverflow();
 
-  // find the region to loop over
-  rowLength = (outExt[1] - outExt[0]+1)*inData->GetNumberOfScalarComponents();
-  maxY = outExt[3] - outExt[2]; 
-  maxZ = outExt[5] - outExt[4];
-  target = (unsigned long)((maxZ+1)*(maxY+1)/50.0);
-  target++;
-  
-  // Get increments to march through data 
-  inData->GetContinuousIncrements(outExt, inIncX, inIncY, inIncZ);
-  outData->GetContinuousIncrements(outExt, outIncX, outIncY, outIncZ);
-
   // Loop through ouput pixels
-  for (idxZ = 0; idxZ <= maxZ; idxZ++)
+  while (!outIt.IsAtEnd())
     {
-    for (idxY = 0; !self->AbortExecute && idxY <= maxY; idxY++)
+    IT* inSI = inIt.BeginSpan();
+    OT* outSI = outIt.BeginSpan();
+    OT* outSIEnd = outIt.EndSpan();
+    if (clamp)
       {
-      if (!id) 
+      while (outSI != outSIEnd)
         {
-        if (!(count%target))
+        // Pixel operation
+        val = static_cast<double>(*inSI);
+        if (val > typeMax)
           {
-          self->UpdateProgress(count/(50.0*target));
+          val = typeMax;
           }
-        count++;
-        }
-      // put the test for clamp to avoid the innermost loop
-      if (clamp)
-        {
-        for (idxR = 0; idxR < rowLength; idxR++)
+        if (val < typeMin)
           {
-          // Pixel operation
-          val = (float)(*inPtr);
-          if (val > typeMax)
-            {
-            val = typeMax;
-            }
-          if (val < typeMin)
-            {
-            val = typeMin;
-            }
-          *outPtr = (OT)(val);
-          outPtr++;
-          inPtr++;
+          val = typeMin;
           }
+        *outSI = static_cast<OT>(val);
+        ++outSI;
+        ++inSI;
         }
-      else
-        {
-        for (idxR = 0; idxR < rowLength; idxR++)
-          {
-          // Pixel operation
-          *outPtr = (OT)(*inPtr);
-          outPtr++;
-          inPtr++;
-          }
-        }
-      outPtr += outIncY;
-      inPtr += inIncY;
       }
-    outPtr += outIncZ;
-    inPtr += inIncZ;
+    else
+      {
+      while (outSI != outSIEnd)
+        {
+        // now process the components
+        *outSI = static_cast<OT>(*inSI);
+        ++outSI;
+        ++inSI;
+        }
+      }
+    inIt.NextSpan();
+    outIt.NextSpan();
     }
 }
 
@@ -143,16 +118,15 @@ static void vtkImageCastExecute(vtkImageCast *self,
 //----------------------------------------------------------------------------
 template <class T>
 static void vtkImageCastExecute(vtkImageCast *self,
-                                vtkImageData *inData, T *inPtr,
-                                vtkImageData *outData, int outExt[6], int id)
+                                vtkImageData *inData,
+                                vtkImageData *outData, int outExt[6], int id,
+                                T *)
 {
-  void *outPtr = outData->GetScalarPointerForExtent(outExt);
-
   switch (outData->GetScalarType())
     {
     vtkTemplateMacro7(vtkImageCastExecute, self, 
-                      inData, (T *)(inPtr), 
-                      outData, (VTK_TT *)(outPtr),outExt, id);
+                      inData, outData, outExt, id,
+                      static_cast<T *>(0), static_cast<VTK_TT *>(0));
     default:
       vtkGenericWarningMacro("Execute: Unknown output ScalarType");
       return;
@@ -171,15 +145,13 @@ void vtkImageCast::ThreadedExecute(vtkImageData *inData,
                                    vtkImageData *outData,
                                    int outExt[6], int id)
 {
-  void *inPtr = inData->GetScalarPointerForExtent(outExt);
-  
   vtkDebugMacro(<< "Execute: inData = " << inData 
                 << ", outData = " << outData);
   
   switch (inData->GetScalarType())
     {
-    vtkTemplateMacro6(vtkImageCastExecute, this, inData, (VTK_TT *)(inPtr), 
-                      outData, outExt, id);
+    vtkTemplateMacro6(vtkImageCastExecute, this, inData, 
+                      outData, outExt, id, static_cast<VTK_TT *>(0));
     default:
       vtkErrorMacro(<< "Execute: Unknown input ScalarType");
       return;
