21d8631242e89b18c4a49322115753f75fb90fb4
Will Schroeder <will.schroeder@kitware.com>
2002-04-19 12:39:38 -0400

    ENH:Additional functionality

--- Hybrid/vtkInteractorStyleTerrain.cxx
+++ Hybrid/vtkInteractorStyleTerrain.cxx
@@ -18,9 +18,12 @@
 #include "vtkInteractorStyleTerrain.h"
 #include "vtkMath.h"
 #include "vtkCallbackCommand.h"
+#include "vtkPropPicker.h"
+#include "vtkSphereSource.h"
+#include "vtkPolyDataMapper.h"
 #include "vtkObjectFactory.h"
 
-vtkCxxRevisionMacro(vtkInteractorStyleTerrain, "1.1");
+vtkCxxRevisionMacro(vtkInteractorStyleTerrain, "1.2");
 vtkStandardNewMacro(vtkInteractorStyleTerrain);
 
 vtkInteractorStyleTerrain::vtkInteractorStyleTerrain()
@@ -29,10 +32,61 @@ vtkInteractorStyleTerrain::vtkInteractorStyleTerrain()
   this->EventCallbackCommand->SetCallback(
     vtkInteractorStyleTerrain::ProcessEvents);
   
+  this->LatLongLines = 0;
+  this->Picker = vtkPropPicker::New();
+  this->LatLongSphere = vtkSphereSource::New();
+  this->LatLongSphere->SetPhiResolution(13);
+  this->LatLongSphere->SetThetaResolution(25);
+  this->LatLongSphere->LatLongTessellationOn();
+  this->LatLongMapper = vtkPolyDataMapper::New();
+  this->LatLongMapper->SetInput(this->LatLongSphere->GetOutput());
+  this->LatLongActor = vtkActor::New();
+  this->LatLongActor->SetMapper(this->LatLongMapper);
+  this->LatLongActor->PickableOff();
+
 }
 
 vtkInteractorStyleTerrain::~vtkInteractorStyleTerrain()
 {
+  this->Picker->Delete();
+  this->LatLongSphere->Delete();
+  this->LatLongMapper->Delete();
+  this->LatLongActor->Delete();
+}
+
+void vtkInteractorStyleTerrain::SetEnabled(int enabling)
+{
+  if ( ! this->Interactor )
+    {
+    vtkErrorMacro(<<"The interactor must be set prior to enabling/disabling widget");
+    return;
+    }
+
+  if ( enabling ) //----------------------------------------------------------
+    {
+    vtkDebugMacro(<<"Enabling widget");
+
+    if ( this->Enabled ) //already enabled, just return
+      {
+      return;
+      }
+    
+    this->Enabled = 1;
+    this->InvokeEvent(vtkCommand::EnableEvent,NULL);
+    }
+
+  else //disabling-------------------------------------------------------------
+    {
+    vtkDebugMacro(<<"Disabling widget");
+
+    if ( ! this->Enabled ) //already disabled, just return
+      {
+      return;
+      }
+    
+    this->Enabled = 0;
+    this->InvokeEvent(vtkCommand::DisableEvent,NULL);
+    }
 }
 
 // NOTE!!! This does not do any reference counting!!!
@@ -113,12 +167,12 @@ void vtkInteractorStyleTerrain::ProcessEvents(vtkObject* object,
     }
 }
 
-
-
 void vtkInteractorStyleTerrain::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
   
+  os << indent << "Latitude/Longitude Lines: " 
+     << (this->LatLongLines ? "On\n" : "Off\n");
 }
 
 
@@ -127,7 +181,7 @@ void vtkInteractorStyleTerrain::OnLeftButtonDown (int vtkNotUsed(ctrl),
                                                   int X, int Y)
 {
   // We're only here is we are enabled
-  this->State = vtkInteractorStyleTerrain::LeftDown;
+  this->State = vtkInteractorStyleTerrain::Rotating;
   
   this->EventCallbackCommand->SetAbortFlag(1);
   this->InvokeEvent(vtkCommand::StartInteractionEvent,NULL);
@@ -136,53 +190,6 @@ void vtkInteractorStyleTerrain::OnLeftButtonDown (int vtkNotUsed(ctrl),
   this->OldX = X;
   this->OldY = Y;
 }
-
-
-void vtkInteractorStyleTerrain::OnMouseMove (int vtkNotUsed(ctrl), int shift, 
-                                             int X, int Y)
-{
-  // See whether we're active
-  if ( this->State == vtkInteractorStyleTerrain::Outside || 
-       this->State == vtkInteractorStyleTerrain::Start )
-    {
-    return;
-    }
-
-  // Get the vector of motion
-  int *size = this->Interactor->GetSize();
-  float a = (float)(OldX-X) / size[0] * 180.0f;
-  float e = (float)(OldY-Y) / size[1] * 180.0f;
-  
-  if ( shift )
-    {
-    if ( fabs(OldX-X) >= fabs(OldY-Y) )
-      {
-      e = 0.0;
-      }
-    else
-      {
-      a = 0.0;
-      }
-    }
-
-  this->CurrentCamera = this->Interactor->FindPokedCamera(X,Y);
-  if ( !this->CurrentCamera )
-    {
-    return;
-    }
-
-  this->CurrentCamera->Elevation(e);
-  this->CurrentCamera->Azimuth(a);
-
-  // Interact, if desired
-  this->EventCallbackCommand->SetAbortFlag(1);
-  this->InvokeEvent(vtkCommand::InteractionEvent,NULL);
-  
-  this->Interactor->Render();
-  this->OldX = X;
-  this->OldY = Y;
-}
-
 
 void vtkInteractorStyleTerrain::OnLeftButtonUp (int vtkNotUsed(ctrl), 
                                                 int vtkNotUsed(shift), 
@@ -200,6 +207,127 @@ void vtkInteractorStyleTerrain::OnLeftButtonUp (int vtkNotUsed(ctrl),
   this->Interactor->Render();
 }
 
+void vtkInteractorStyleTerrain::OnMiddleButtonDown (int vtkNotUsed(ctrl), 
+                                                  int vtkNotUsed(shift), 
+                                                  int X, int Y)
+{
+  // We're only here is we are enabled
+  this->State = vtkInteractorStyleTerrain::Panning;
+  
+  this->EventCallbackCommand->SetAbortFlag(1);
+  this->InvokeEvent(vtkCommand::StartInteractionEvent,NULL);
+  this->Interactor->Render();
+
+  this->OldX = X;
+  this->OldY = Y;
+}
+
+void vtkInteractorStyleTerrain::OnMiddleButtonUp (int vtkNotUsed(ctrl), 
+                                                int vtkNotUsed(shift), 
+                                                int vtkNotUsed(X), 
+                                                int vtkNotUsed(Y))
+{
+  if ( this->State == vtkInteractorStyleTerrain::Outside )
+    {
+    return;
+    }
+
+  this->State = vtkInteractorStyleTerrain::Start;
+  this->EventCallbackCommand->SetAbortFlag(1);
+  this->InvokeEvent(vtkCommand::EndInteractionEvent,NULL);
+  this->Interactor->Render();
+}
+
+void vtkInteractorStyleTerrain::OnRightButtonDown (int vtkNotUsed(ctrl), 
+                                                  int vtkNotUsed(shift), 
+                                                  int X, int Y)
+{
+  // We're only here is we are enabled
+  this->State = vtkInteractorStyleTerrain::Zooming;
+  
+  this->EventCallbackCommand->SetAbortFlag(1);
+  this->InvokeEvent(vtkCommand::StartInteractionEvent,NULL);
+  this->Interactor->Render();
+
+  this->OldX = X;
+  this->OldY = Y;
+}
+
+void vtkInteractorStyleTerrain::OnRightButtonUp (int vtkNotUsed(ctrl), 
+                                                int vtkNotUsed(shift), 
+                                                int vtkNotUsed(X), 
+                                                int vtkNotUsed(Y))
+{
+  if ( this->State == vtkInteractorStyleTerrain::Outside )
+    {
+    return;
+    }
+
+  this->State = vtkInteractorStyleTerrain::Start;
+  this->EventCallbackCommand->SetAbortFlag(1);
+  this->InvokeEvent(vtkCommand::EndInteractionEvent,NULL);
+  this->Interactor->Render();
+}
+
+void vtkInteractorStyleTerrain::OnMouseMove (int vtkNotUsed(ctrl), int shift, 
+                                             int X, int Y)
+{
+  // See whether we're active
+  if ( this->State == vtkInteractorStyleTerrain::Outside || 
+       this->State == vtkInteractorStyleTerrain::Start )
+    {
+    return;
+    }
+
+  // Make sure that we have a camera
+  this->CurrentCamera = this->Interactor->FindPokedCamera(X,Y);
+  if ( !this->CurrentCamera )
+    {
+    return;
+    }
+
+  // Gather necessary information
+  int *size = this->Interactor->GetSize();
+
+  // Do the right thing depending on the mouse button
+  if ( this->State == vtkInteractorStyleTerrain::Rotating ) //left mouse
+    {
+    float a = (float)(OldX-X) / size[0] * 180.0f;
+    float e = (float)(OldY-Y) / size[1] * 180.0f;
+  
+    if ( shift )
+      {
+      if ( fabs(OldX-X) >= fabs(OldY-Y) )
+        {
+        e = 0.0;
+        }
+      else
+        {
+        a = 0.0;
+        }
+      }
+    // Move the camera
+    this->CurrentCamera->Elevation(e);
+    this->CurrentCamera->Azimuth(a);
+    }
+
+  else if ( this->State == vtkInteractorStyleTerrain::Panning ) //middle mouse
+    {
+    }
+
+  else if ( this->State == vtkInteractorStyleTerrain::Zooming ) //right mouse
+    {
+    }
+
+  // Interact, if desired
+  this->EventCallbackCommand->SetAbortFlag(1);
+  this->InvokeEvent(vtkCommand::InteractionEvent,NULL);
+  
+  this->Interactor->Render();
+  this->OldX = X;
+  this->OldY = Y;
+}
+
 
 void vtkInteractorStyleTerrain::OnChar(int vtkNotUsed(ctrl), 
                                        int vtkNotUsed(shift),
@@ -208,8 +336,6 @@ void vtkInteractorStyleTerrain::OnChar(int vtkNotUsed(ctrl),
 {
   switch (keycode)
     {
-    case 'Q' :
-    case 'q' :
     case 'e' :
     case 'E' :
       this->Interactor->ExitCallback();
@@ -224,7 +350,8 @@ void vtkInteractorStyleTerrain::OnChar(int vtkNotUsed(ctrl),
     //-----
     case 'r' :
     case 'R' :
-      this->FindPokedRenderer(this->OldX, this->OldY);
+      this->CurrentRenderer = 
+        this->Interactor->FindPokedRenderer(this->OldX,this->OldY);
       this->CurrentRenderer->ResetCamera();
       this->Interactor->Render();
       break;
@@ -241,9 +368,58 @@ void vtkInteractorStyleTerrain::OnChar(int vtkNotUsed(ctrl),
         }
       this->Interactor->Render();
       break;
+
+    //-----
+    case 'f' :
+      break;
+
+    //-----
+    case 'l' :
+      this->CurrentRenderer = 
+        this->Interactor->FindPokedRenderer(this->OldX,this->OldY);
+      if ( this->LatLongLines ) 
+        {
+        this->LatLongLinesOff();
+        }
+      else 
+        {
+        float bounds[6];
+        this->CurrentRenderer->ComputeVisiblePropBounds(bounds);
+        float radius = sqrt((bounds[1]-bounds[0])*(bounds[1]-bounds[0]) +
+                            (bounds[3]-bounds[2])*(bounds[3]-bounds[2]) +
+                            (bounds[5]-bounds[4])*(bounds[5]-bounds[4])) / 2.0;
+        this->LatLongSphere->SetRadius(radius);
+        this->LatLongSphere->SetCenter((bounds[0]+bounds[1])/2.0,
+                                       (bounds[2]+bounds[3])/2.0,
+                                       (bounds[4]+bounds[5])/2.0);        
+        this->LatLongLinesOn();
+        }
+      this->SelectRepresentation();
+
+      this->Interactor->Render();
+      break;
     }
 
   this->KeyPressCallbackCommand->SetAbortFlag(1);
+}
+
+void vtkInteractorStyleTerrain::SelectRepresentation()
+{
+  if ( ! this->CurrentRenderer )
+    {
+    return;
+    }
+
+  this->CurrentRenderer->RemoveActor(this->LatLongActor);
+  this->LatLongActor->VisibilityOff();
+  
+  if ( this->LatLongLines )
+    {
+    this->LatLongActor->VisibilityOn();
+    this->CurrentRenderer->AddActor(this->LatLongActor);
+    this->LatLongActor->GetProperty()->SetRepresentationToWireframe();
+    }
+  
 }
 
 
--- Hybrid/vtkInteractorStyleTerrain.h
+++ Hybrid/vtkInteractorStyleTerrain.h
@@ -22,18 +22,41 @@
 // scene is manipulated by specifying azimuth (angle around the view
 // up vector) and elevation (the angle from the horizon).
 //
+// The mouse binding for this class is as follows. Left mouse click followed
+// rotates the camera around the focal point using both elevation and azimuth
+// invocations on the camera. Left mouse motion in the horizontal direction
+// results in azimuth motion; left mouse motion in the vertical direction
+// results in elevation motion. Therefore, diagonal motion results in a
+// combination of azimuth and elevation. (If the shift key is held during
+// motion, then only one of elevation or azimuth is invoked, depending on the
+// whether the mouse motion is primarily horizontal or vertical.) Middle
+// mouse button pans the camera across the scene (again the shift key has a
+// similar effect on limiting the motion to the vertical or horizontal
+// direction. The right mouse is used to dolly (e.g., a type of zoom) towards
+// or away from the focal point.
+//
+// The class also supports some keypress events. The "r" key resets the
+// camera.  The "e" key invokes the exit callback and by default exits the
+// program. The "f" key sets a new camera focal point and flys towards that
+// point. The "u" key invokes the user event. The "3" key toggles between 
+// stereo and non-stero mode. The "l" key toggles on/off a latitude/longitude
+// sphere that can be used to indicate position.
+// 
 
 // .SECTION See Also
-// vtkInteractorObserver
+// vtkInteractorObserver vtkInteractorStyle vtk3DWidget
 
 #ifndef __vtkInteractorStyleTerrain_h
 #define __vtkInteractorStyleTerrain_h
 
-#include "vtkInteractorStyle.h"
+#include "vtkInteractorObserver.h"
 
 class vtkCallbackCommand;
+class vtkPropPicker;
+class vtkPolyDataMapper;
+class vtkSphereSource;
 
-class VTK_HYBRID_EXPORT vtkInteractorStyleTerrain : public vtkInteractorStyle
+class VTK_HYBRID_EXPORT vtkInteractorStyleTerrain : public vtkInteractorObserver
 {
 public:
   // Description:
@@ -47,6 +70,13 @@ public:
   // Set/Get the Interactor wrapper being controlled by this object.
   // (Satisfy superclass API.)
   virtual void SetInteractor(vtkRenderWindowInteractor *interactor);
+  virtual void SetEnabled(int);
+
+  // Description:
+  // Turn on/off the latitude/longitude lines.
+  vtkSetMacro(LatLongLines,int);
+  vtkGetMacro(LatLongLines,int);
+  vtkBooleanMacro(LatLongLines,int);
 
 protected:
   vtkInteractorStyleTerrain();
@@ -57,8 +87,9 @@ protected:
   enum WidgetState
   {
     Start=0,
-    LeftDown,
-    LeftMoving,
+    Rotating,
+    Panning,
+    Zooming,
     Outside
   };
 //ETX
@@ -70,8 +101,20 @@ protected:
   // ProcessEvents() dispatches to these methods.
   void OnLeftButtonDown(int ctrl, int shift, int X, int Y);
   void OnLeftButtonUp(int ctrl, int shift, int X, int Y);
+  void OnMiddleButtonDown(int ctrl, int shift, int X, int Y);
+  void OnMiddleButtonUp(int ctrl, int shift, int X, int Y);
+  void OnRightButtonDown(int ctrl, int shift, int X, int Y);
+  void OnRightButtonUp(int ctrl, int shift, int X, int Y);
   void OnMouseMove(int ctrl, int shift, int X, int Y);
   void OnChar(int ctrl, int shift, char keycode, int repeatcount);
+
+  // Internal helper attributes
+  int LatLongLines;
+  void SelectRepresentation();
+  vtkPropPicker *Picker;
+  vtkSphereSource *LatLongSphere;
+  vtkPolyDataMapper *LatLongMapper;
+  vtkActor *LatLongActor;
 
 private:
   vtkInteractorStyleTerrain(const vtkInteractorStyleTerrain&);  // Not implemented.
