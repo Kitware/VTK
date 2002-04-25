75a6e226e4f8804b311b08b19e5e14d5edb36a2c
Bill Lorensen <lorensen@crd.ge.com>
2002-04-25 09:58:59 -0400

    ERR: was not checking for a NULL this->Reslice.

--- Hybrid/vtkImagePlaneWidget.cxx
+++ Hybrid/vtkImagePlaneWidget.cxx
@@ -36,7 +36,7 @@
 #include "vtkTransform.h"
 #include "vtkMatrix4x4.h"
 
-vtkCxxRevisionMacro(vtkImagePlaneWidget, "1.6");
+vtkCxxRevisionMacro(vtkImagePlaneWidget, "1.7");
 vtkStandardNewMacro(vtkImagePlaneWidget);
 
 vtkImagePlaneWidget::vtkImagePlaneWidget()
@@ -108,6 +108,7 @@ vtkImagePlaneWidget::vtkImagePlaneWidget()
   this->Texture = NULL;
   this->LookupTable = NULL;
   this->ColorMap = NULL;
+  this->DummyTransform = NULL;
 }
 
 vtkImagePlaneWidget::~vtkImagePlaneWidget()
@@ -1146,6 +1147,10 @@ float vtkImagePlaneWidget::GetSlicePosition()
 
 void vtkImagePlaneWidget::SetSliceIndex(int index)
 {
+  if (!this->Reslice)
+    {
+    return;
+    }
   this->ImageData = this->Reslice->GetInput();
   this->ImageData->UpdateInformation();
   float origin[3];
@@ -1193,6 +1198,10 @@ void vtkImagePlaneWidget::SetSliceIndex(int index)
 
 int vtkImagePlaneWidget::GetSliceIndex()
 {
+  if (!this->Reslice)
+    {
+      return 0;
+    }
   this->ImageData = this->Reslice->GetInput();
   this->ImageData->UpdateInformation();
   float origin[3];
