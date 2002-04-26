4443b4bb1b9ce002c0ecf854a6cf82b305da68f5
Sebastien Barre <sebastien.barre@kitware.com>
2002-04-26 17:54:56 -0400

    ENH: cleanup, deprecate class to avoid duplicate code (now subclass of vtkInteractorStyleSwitch)

--- Rendering/vtkInteractorStyleTrackball.h
+++ Rendering/vtkInteractorStyleTrackball.h
@@ -19,135 +19,28 @@
 
 // .SECTION Description
 // vtkInteractorStyleTrackball is an implementation of vtkInteractorStyle
-// that defines the trackball style. The trackball style can be thought of
-// as a "grab and move" approach. That is, on mouse down a point on the
-// object is grabbed, and then moving the mouse cause motion in proportion
-// to the amount of motion. Note that the events that are bound by this
-// class are the same as vtkInteractorStyle (which implements joystick mode),
-// just the behavior is modified consistent with the trackball style of 
-// interaction.
+// that defines the trackball style. It is now deprecated and as such a
+// subclass of vtkInteractorStyleSwitch
 
 // .SECTION See Also
-// vtkInteractorStyle
+// vtkInteractorStyleSwitch vtkInteractorStyleTrackballActor vtkInteractorStyleJoystickCamera
 
 #ifndef __vtkInteractorStyleTrackball_h
 #define __vtkInteractorStyleTrackball_h
 
-#include "vtkInteractorStyle.h"
-#include "vtkAbstractPropPicker.h"
+#include "vtkInteractorStyleSwitch.h"
 
-#define VTKIS_JOYSTICK    0
-#define VTKIS_TRACKBALL   1
-
-#define VTKIS_CAMERA      0
-#define VTKIS_ACTOR       1
-
-#define VTKIS_CONTROL_OFF 0
-#define VTKIS_CONTROL_ON  1
-
-class VTK_RENDERING_EXPORT vtkInteractorStyleTrackball : public vtkInteractorStyle 
+class VTK_RENDERING_EXPORT vtkInteractorStyleTrackball : public vtkInteractorStyleSwitch 
 {
 public:
   static vtkInteractorStyleTrackball *New();
-  vtkTypeRevisionMacro(vtkInteractorStyleTrackball,vtkInteractorStyle);
+  vtkTypeRevisionMacro(vtkInteractorStyleTrackball,vtkInteractorStyleSwitch);
   void PrintSelf(ostream& os, vtkIndent indent);
-  
-  // Description:
-  // Concrete implementation of event bindings
-  virtual void OnRightButtonDown (int ctrl, int shift, int x, int Y);
-  virtual void OnRightButtonUp   (int ctrl, int shift, int x, int Y);
-  virtual void OnMiddleButtonDown(int ctrl, int shift, int x, int Y);
-  virtual void OnMiddleButtonUp  (int ctrl, int shift, int x, int Y);
-  virtual void OnLeftButtonDown  (int ctrl, int shift, int x, int Y);
-  virtual void OnLeftButtonUp    (int ctrl, int shift, int x, int Y);
-
-  // Description:
-  // OnChar implements keyboard functions, but subclasses can override this 
-  // behavior
-  virtual void OnChar(int ctrl, int shift, char keycode, int repeatcount);
-  
-  // Description:
-  // OnTimer calls RotateCamera, RotateActor etc which should be overridden by
-  // style subclasses.
-  virtual void OnTimer(void);
-
-  // Description:
-  // External switching between actor and camera mode.
-  virtual void SetActorModeToCamera();
-  virtual void SetActorModeToActor();
-  vtkGetMacro(ActorMode, int);
-  
-  // Description:
-  // External switching between joystick and trackball mode.
-  virtual void SetTrackballModeToTrackball();
-  virtual void SetTrackballModeToJoystick();
-  vtkGetMacro(TrackballMode, int);
 
 protected:
   vtkInteractorStyleTrackball();
   ~vtkInteractorStyleTrackball();
 
-  // used to track picked objects in actor mode
-  // reason for existence: user may use any kind of picker.  Interactor
-  //    need the high precision of cell picker at all time.
-  vtkAbstractPropPicker *InteractionPicker;
-  int PropPicked;                      // boolean: prop picked?
-  vtkProp3D *InteractionProp;
-  
-  // new interactor modes
-  int ActorMode;
-  int TrackballMode;
-  int ControlMode;
-  float MotionFactor;                // constant: for motion
-  int Preprocess;                       // boolean: was preprocessing done?
-  float RadianToDegree;                 // constant: for conv from deg to rad
-
-  // data arrays for motion
-  float NewPickPoint[4];
-  float OldPickPoint[4];
-  float MotionVector[3];                // vector used for interaction
-  float OldX;
-  float OldY;
-  
-  // this really belong in camera
-  double ViewLook[3];
-  double ViewPoint[3];
-  double ViewFocus[3];
-  double ViewUp[3];
-  double ViewRight[3];
-
-  // actor stuff
-  float Origin[3];
-  float Position[3];
-  float ObjCenter[3];                   // center of bounding box
-  float DispObjCenter[3];               // center of box in display coord
-  float Radius;                         // radius of virtual sphere
-
-  // methods for the different interactions in different modes
-  virtual void TrackballRotateCamera(int x, int y);
-  virtual void TrackballSpinCamera(int x, int y);
-  virtual void TrackballPanCamera(int x, int y);
-  virtual void TrackballDollyCamera(int x, int y);
-  
-  virtual void JoystickRotateActor(int x, int y);
-  virtual void JoystickSpinActor(int x, int y);
-  virtual void JoystickPanActor(int x, int y);
-  virtual void JoystickDollyActor(int x, int y);
-  virtual void JoystickScaleActor(int x, int y);
-  
-  virtual void TrackballRotateActor(int x, int y);
-  virtual void TrackballSpinActor(int x, int y);
-  virtual void TrackballPanActor(int x, int y);
-  virtual void TrackballDollyActor(int x, int y);
-  virtual void TrackballScaleActor(int x, int y);
-
-  void Prop3DTransform(vtkProp3D *prop3D, double *boxCenter,
-                      int numRotation, double **rotate,
-                      double *scale);
-  void Prop3DTransform(vtkProp3D *prop3D,float *boxCenter,
-                      int NumRotation,double **rotate,
-                      double *scale);
-  void FindPickedActor(int x, int Y);
 private:
   vtkInteractorStyleTrackball(const vtkInteractorStyleTrackball&);  // Not implemented.
   void operator=(const vtkInteractorStyleTrackball&);  // Not implemented.
