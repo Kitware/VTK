3423a3867140fe520a41979edb439db622200050
Sebastien Barre <sebastien.barre@kitware.com>
2002-05-02 19:31:25 -0400

    ENH: use observers instead of Tk bindings

--- Examples/Modelling/Tcl/SpherePuzzle.tcl
+++ Examples/Modelling/Tcl/SpherePuzzle.tcl
@@ -1,132 +1,210 @@
+#
+# First we include the VTK Tcl packages which will make available 
+# all of the vtk commands to Tcl
+#
 package require vtk
 package require vtkinteraction
 
-# prevent the tk window from showing up then start the event loop
+#
+# Prevent the tk window from showing up then start the event loop
+#
 wm withdraw .
 
+#
 # Create the toplevel window
+#
 toplevel .top
-wm title .top {Sphere Puzzle}
+wm title .top "Sphere Puzzle"
+wm protocol .top WM_DELETE_WINDOW ::vtk::cb_exit
 
+#
 # Create some frames
+#
 frame .top.f1 
 frame .top.f2
-pack .top.f1 .top.f2 -side top -expand 1 -fill both
 
+pack .top.f1 \
+        -side top -anchor n \
+        -expand 1 -fill both
+
+pack .top.f2 \
+        -side bottom -anchor s \
+        -expand t -fill x
+
+#
+# Create the Tk render widget, and bind the events
+#
 vtkRenderWindow renWin
-vtkTkRenderWidget .top.f1.rw -width 400 -height 400 -rw renWin
-::vtk::bind_tk_render_widget .top.f1.rw
-pack .top.f1.rw -expand 1 -fill both
 
-# create a rendering window and renderer
 vtkRenderer ren1
     renWin AddRenderer ren1
 
+vtkTkRenderWidget .top.f1.rw \
+        -width 400 \
+        -height 400 \
+        -rw renWin
+
+::vtk::bind_tk_render_widget .top.f1.rw
+
+pack .top.f1.rw \
+        -expand t -fill both
+
+#
+# Display some infos
+#
 label .top.f2.l1 -text "Position cursor over the rotation plane."
 label .top.f2.l2 -text "Moving pieces will be highlighted."
 label .top.f2.l3 -text "Press 'm' to make a move."
 
-button .top.f2.reset -text "Reset" \
-	-command "puzzle Reset; renWin Render"
+button .top.f2.reset \
+        -text "Reset" \
+	-command {puzzle Reset; renWin Render}
 
-pack .top.f2.l1 .top.f2.l2 .top.f2.l3 .top.f2.reset \
-	-side top -expand 1 -fill both
+button .top.f2.b1 \
+        -text "Quit" \
+        -command ::vtk::cb_exit
 
-button .top.f2.b1 -text "Quit" -command {vtkCommand DeleteAllObjects; exit}
-pack .top.f2.b1  -expand 1 -fill x
+pack .top.f2.l1 .top.f2.l2 .top.f2.l3 .top.f2.reset .top.f2.b1 \
+	-side top \
+        -expand t -fill x
 
-proc ToggleGhostCells {} {
-   renWin Render
-}
-
-proc test {slab} {
-    for { set i 0} {$i <= 100} {set i [expr $i + 5]} {
-	puzzle MoveVertical $slab $i 1
-	renWin Render
-    }
-}
-
+#
+# Create the pipeline
+#
 vtkSpherePuzzle puzzle
+
 vtkPolyDataMapper mapper
    mapper SetInput [puzzle GetOutput]
+
 vtkActor actor
     actor SetMapper mapper
 
 vtkSpherePuzzleArrows arrows
+
 vtkPolyDataMapper mapper2
    mapper2 SetInput [arrows GetOutput]
+
 vtkActor actor2
     actor2 SetMapper mapper2
 
+#
 # Add the actors to the renderer, set the background and size
 #
 ren1 AddActor actor
 ren1 AddActor actor2
-
 ren1 SetBackground 0.1 0.2 0.4
 
-bind .top.f1.rw <Motion> {MotionCallback %x %y}
-bind .top.f1.rw <KeyPress-m> {ButtonCallback %x %y}
+ren1 ResetCamera
+set cam [ren1 GetActiveCamera]
+$cam Elevation -40
+renWin Render
 
+#
+# Modify some bindings, use the interactor style 'switch'
+#
+set iren [renWin GetInteractor]
+set istyle [vtkInteractorStyleSwitch istyleswitch]
+$iren SetInteractorStyle $istyle
+$istyle SetCurrentStyleToTrackballCamera
+
+$iren AddObserver MouseMoveEvent MotionCallback
+$iren AddObserver CharEvent CharCallback
+
+#
+# Highlight pieces
+#
 set LastVal -1
-proc MotionCallback {x y} {
+proc MotionCallback {} {
+
+    global in_piece_rotation
+    
+    if {[info exists in_piece_rotation]} {
+        return
+    }
+
     global LastVal
 
-    # Compute display point from Tk display point.
-    set WindowY [lindex [.top.f1.rw configure -height] 4]
-    set y [expr $WindowY - $y]
-    set z [ren1 GetZ $x $y]
+    set iren [renWin GetInteractor]
+    set istyle [[$iren GetInteractorStyle] GetCurrentStyle]
 
-    ren1 SetDisplayPoint $x $y $z
+    # Return if the user is performing interaction
+
+    if {[$istyle GetState]} {
+        return
+    }
+
+    # Get mouse position
+
+    set pos [$iren GetEventPosition]
+    set x [lindex $pos 0]
+    set y [lindex $pos 1]
+
+    # Get world point
+
+    ren1 SetDisplayPoint $x $y [ren1 GetZ $x $y]
     ren1 DisplayToWorld
     set pt [ren1 GetWorldPoint]
 
-    #tk_messageBox -message "$pt"
-    set x [lindex $pt 0]
-    set y [lindex $pt 1]
-    set z [lindex $pt 2]
-
-    set val [puzzle SetPoint $x $y $z]
+    set val [puzzle SetPoint [lindex $pt 0] [lindex $pt 1] [lindex $pt 2]]
     if {$val != $LastVal} {
 	renWin Render
 	set LastVal $val
     }
 }
 
+#
+# Rotate the puzzle
+#
+proc CharCallback {} {
+
+    set iren [renWin GetInteractor]
+
+    set keycode [$iren GetKeyCode]
+    if {$keycode != "m" && $keycode != "M"} {
+        return
+    }
+
+    set pos [$iren GetEventPosition]
+    ButtonCallback [lindex $pos 0] [lindex $pos 1]
+}
 
 proc ButtonCallback {x y} {
 
-    # Compute display point from Tk display point.
-    set WindowY [lindex [.top.f1.rw configure -height] 4]
-    set y [expr $WindowY - $y]
-    set z [ren1 GetZ $x $y]
+    global in_piece_rotation
+    
+    if {[info exists in_piece_rotation]} {
+        return
+    }
+    set in_piece_rotation 1
 
-    ren1 SetDisplayPoint $x $y $z
+    # Get world point
+
+    ren1 SetDisplayPoint $x $y [ren1 GetZ $x $y]
     ren1 DisplayToWorld
     set pt [ren1 GetWorldPoint]
 
-    #tk_messageBox -message "$pt"
     set x [lindex $pt 0]
     set y [lindex $pt 1]
     set z [lindex $pt 2]
 
-    for { set i 0} {$i <= 100} {set i [expr $i + 5]} {
+    for { set i 0} {$i <= 100} {set i [expr $i + 10]} {
 	puzzle SetPoint $x $y $z
 	puzzle MovePoint $i
 	renWin Render
     }
+
+    unset in_piece_rotation
 }
 
-renWin Render
-
-set cam [ren1 GetActiveCamera]
-$cam Elevation -40
 update
 
-ButtonCallback 218 205
-ButtonCallback 261 272
-ButtonCallback 213 293
-ButtonCallback 203 238
-ButtonCallback 134 214
+#
+# Shuffle the puzzle
+#
+ButtonCallback 218 195
+ButtonCallback 261 128
+ButtonCallback 213 107
+ButtonCallback 203 162
+ButtonCallback 134 186
 
-
+tkwait window .top
