6278f5eacae9081297c85e8c5217de4dea653efb
Lisa Avila <lisa.avila@kitware.com>
2002-05-13 14:57:33 -0400

    new test and fix for vtkLODProp3D

--- Rendering/vtkLODProp3D.cxx
+++ Rendering/vtkLODProp3D.cxx
@@ -24,7 +24,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-vtkCxxRevisionMacro(vtkLODProp3D, "1.31");
+vtkCxxRevisionMacro(vtkLODProp3D, "1.32");
 vtkStandardNewMacro(vtkLODProp3D);
 
 #define VTK_INDEX_NOT_IN_USE    -1
@@ -831,7 +831,8 @@ void vtkLODProp3D::SetAllocatedRenderTime( float t, vtkViewport *vp )
 
   // update the EstimatedTime of the last LOD to be rendered
   if ( this->SelectedLODIndex >= 0 &&
-       this->SelectedLODIndex < this->NumberOfEntries )
+       this->SelectedLODIndex < this->NumberOfEntries &&
+       this->LODs[this->SelectedLODIndex].ID != VTK_INDEX_NOT_IN_USE )
     {
     // For stability, blend in the new time - 25% old + 75% new
     newTime = 
