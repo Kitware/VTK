f3d8e6378f1a56a8667d227220e38e5eeb9aaace
Berk Geveci <berk.geveci@kitware.com>
2002-05-14 09:13:00 -0400

    Blocked the wrapping of NewInstance since it creates compilation problems (see the comment in the file).

--- Wrapping/vtkWrapJava.c
+++ Wrapping/vtkWrapJava.c
@@ -434,6 +434,15 @@ void outputFunction(FILE *fp, FileInfo *data)
   int args_ok = 1;
   CurrentData = data;
 
+  /* NewInstance can not be wrapped because it is a (non-virtual)   */
+  /* method which returns a pointer of the same type as the current */ 
+  /* pointer. Since all methods are virtual in Java, this looks     */
+  /* like polymorphic return type.                                  */
+  if (!strcmp("NewInstance",currentFunction->Name))
+    {
+    return ;
+    }
+
   /* some functions will not get wrapped no matter what else */
   if (currentFunction->IsOperator || 
       currentFunction->ArrayFailure ||
