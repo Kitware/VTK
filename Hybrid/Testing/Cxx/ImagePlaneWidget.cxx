c331b7795281a90808f7ef6ac763df7b8235e863
Dean Inglis <dean.inglis@camris.ca>
2002-05-24 15:40:44 -0400

    ENH: increasing coverage

--- Hybrid/Testing/Cxx/ImagePlaneWidget.cxx
+++ Hybrid/Testing/Cxx/ImagePlaneWidget.cxx
@@ -6,31 +6,58 @@
   Date:      $Date$
   Version:   $Revision$
 
-  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
   See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
 
-     This software is distributed WITHOUT ANY WARRANTY; without even 
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
      PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
-#include "vtkVolume16Reader.h"
+#include "vtkActor.h"
+#include "vtkActor2D.h"
+#include "vtkCellPicker.h"
+#include "vtkImageActor.h"
+#include "vtkImageMapToColors.h"
+#include "vtkImagePlaneWidget.h"
+#include "vtkImageReader.h"
 #include "vtkOutlineFilter.h"
 #include "vtkPolyDataMapper.h"
-#include "vtkActor.h"
 #include "vtkRenderer.h"
 #include "vtkRenderWindow.h"
 #include "vtkRenderWindowInteractor.h"
-#include "vtkImagePlaneWidget.h"
-#include "vtkImageReader.h"
-#include "vtkCellPicker.h"
+#include "vtkTextMapper.h"
+#include "vtkVolume16Reader.h"
 
 #include "vtkRegressionTestImage.h"
 #include "vtkDebugLeaks.h"
 
+// Callback for the interaction
+class vtkMyCallback : public vtkCommand
+{
+public:
+  static vtkMyCallback *New()
+    { return new vtkMyCallback; }
+  void Delete()
+    { delete this; }
+  virtual void Execute(vtkObject *caller, unsigned long, void*)
+    {
+      vtkImagePlaneWidget *planeWidget = reinterpret_cast<vtkImagePlaneWidget*>(caller);
+
+      sprintf(this->buffer,"%.2f, %d",planeWidget->GetSlicePosition(),planeWidget->GetSliceIndex());
+      this->Mapper->SetInput(buffer);
+      this->Mapper->Modified();
+    }
+  vtkMyCallback():Mapper(0){};
+  vtkTextMapper* Mapper;
+  char buffer[128];
+};
+
 int main( int argc, char *argv[] )
 {
+  vtkDebugLeaks::PromptUserOff();
+
   const char *temp=getenv("VTK_DATA_ROOT");
   char filename[2048];
   
@@ -41,7 +68,7 @@ int main( int argc, char *argv[] )
     }
   strcpy(filename,temp);
   strcat(filename,"/Data/headsq/quarter");
-  
+
   vtkVolume16Reader* v16 =  vtkVolume16Reader::New();
     v16->SetDataDimensions( 64, 64);
     v16->SetDataByteOrderToLittleEndian();
@@ -59,61 +86,137 @@ int main( int argc, char *argv[] )
   vtkActor* outlineActor =  vtkActor::New();
     outlineActor->SetMapper( outlineMapper);
 
-  vtkRenderer*       ren1 = vtkRenderer::New();
+  vtkRenderer* ren1 = vtkRenderer::New();
+  vtkRenderer* ren2 = vtkRenderer::New();
 
   vtkRenderWindow* renWin = vtkRenderWindow::New();
-    renWin->AddRenderer( ren1);
+    renWin->AddRenderer(ren2);
+    renWin->AddRenderer(ren1);
 
   vtkRenderWindowInteractor* iren = vtkRenderWindowInteractor::New();
-    iren->SetRenderWindow( renWin);
+    iren->SetRenderWindow(renWin);
 
   vtkCellPicker* picker = vtkCellPicker::New();
-    picker->SetTolerance(0.005); 
-    picker->PickFromListOn();
+    picker->SetTolerance(0.005);
 
   vtkImagePlaneWidget* planeWidgetX = vtkImagePlaneWidget::New();
-  planeWidgetX->SetInput(v16->GetOutput());  
-  planeWidgetX->SetInteractor( iren);
-  planeWidgetX->SetPlaneOrientationToXAxes();  
-  planeWidgetX->SetSliceIndex(32);
-  planeWidgetX->SetPicker(picker);
-  planeWidgetX->GetPlaneProperty()->SetColor(1,0,0);
-  planeWidgetX->On();
+    planeWidgetX->SetInteractor( iren);
+    planeWidgetX->SetKeyPressActivationValue('x');
+    planeWidgetX->SetPicker(picker);
+    planeWidgetX->GetPlaneProperty()->SetColor(1,0,0);
+    planeWidgetX->TextureInterpolateOff();
+    planeWidgetX->SetResliceInterpolateToNearestNeighbour();
+    planeWidgetX->SetInput(v16->GetOutput());
+    planeWidgetX->SetPlaneOrientationToXAxes();
+    planeWidgetX->SetSliceIndex(32);
+    planeWidgetX->On();
 
   vtkImagePlaneWidget* planeWidgetY = vtkImagePlaneWidget::New();
-  planeWidgetY->SetInput(v16->GetOutput());
-  planeWidgetY->SetInteractor( iren);
-  planeWidgetY->SetPlaneOrientationToYAxes();
-  planeWidgetY->SetSliceIndex(32);
-  planeWidgetY->SetPicker(picker);
-  planeWidgetY->GetPlaneProperty()->SetColor(1,1,0);
-  planeWidgetY->On();
+    planeWidgetY->SetInteractor( iren);
+    planeWidgetY->SetKeyPressActivationValue('y');
+    planeWidgetY->SetPicker(picker);
+    planeWidgetY->GetPlaneProperty()->SetColor(1,1,0);
+    planeWidgetY->TextureInterpolateOn();
+    planeWidgetY->SetResliceInterpolateToLinear();
+    planeWidgetY->SetInput(v16->GetOutput());
+    planeWidgetY->SetPlaneOrientationToYAxes();
+    planeWidgetY->SetSlicePosition(102.4);
+    planeWidgetY->SetLookupTable( planeWidgetX->GetLookupTable());
+    planeWidgetY->On();
 
   vtkImagePlaneWidget* planeWidgetZ = vtkImagePlaneWidget::New();
-  planeWidgetZ->SetInput(v16->GetOutput());
-  planeWidgetZ->SetInteractor( iren);
-  planeWidgetZ->SetPlaneOrientationToZAxes();
-  planeWidgetZ->SetSliceIndex(46);
-  planeWidgetZ->SetPicker(picker);
-  planeWidgetZ->GetPlaneProperty()->SetColor(0,0,1);
-  planeWidgetZ->On();
+    planeWidgetZ->SetInteractor( iren);
+    planeWidgetZ->SetKeyPressActivationValue('z');
+    planeWidgetZ->SetPicker(picker);
+    planeWidgetZ->GetPlaneProperty()->SetColor(0,0,1);
+    planeWidgetZ->TextureInterpolateOn();
+    planeWidgetZ->SetResliceInterpolateToCubic();
+    planeWidgetZ->SetInput(v16->GetOutput());
+    planeWidgetZ->SetPlaneOrientationToZAxes();
+    planeWidgetZ->SetSliceIndex(25);
+    planeWidgetZ->SetLookupTable( planeWidgetX->GetLookupTable());
+    planeWidgetZ->On();
 
+  // add text display of planeWidgetZ's slice position and slice index to ren2
+  vtkTextMapper* textMapper = vtkTextMapper::New();
+    textMapper->SetInput("NA");
+    textMapper->SetFontSize(18);
+    textMapper->SetFontFamilyToArial();
+    textMapper->SetJustificationToLeft();
+    textMapper->BoldOn();
+    textMapper->ItalicOff();
+    textMapper->ShadowOff();
+
+  vtkActor2D* textActor = vtkActor2D::New();
+    textActor->SetMapper(textMapper);
+    textActor->GetProperty()->SetColor(0,1,1);
+
+  // link the widget to the text by callback
+  vtkMyCallback *myCallback = vtkMyCallback::New();
+    myCallback->Mapper = textMapper;
+
+  planeWidgetZ->AddObserver(vtkCommand::InteractionEvent,myCallback);
+
+  // add a 2D image to test the GetReslice method
+  vtkImageMapToColors* colorMap = vtkImageMapToColors::New();
+    colorMap->PassAlphaToOutputOff();
+    colorMap->SetActiveComponent(0);
+    colorMap->SetOutputFormatToLuminance();
+    colorMap->SetInput(planeWidgetZ->GetResliceOutput());
+    colorMap->SetLookupTable(planeWidgetX->GetLookupTable());
+
+  vtkImageActor* imageActor = vtkImageActor::New();
+    imageActor->PickableOff();
+    imageActor->SetInput(colorMap->GetOutput());
+
+  // add the actors
   ren1->AddActor( outlineActor);
+  ren2->AddActor( imageActor);
+  ren2->AddActor( textActor);
 
   ren1->SetBackground( 0.1, 0.1, 0.2);
-  renWin->SetSize( 300, 300);
+  ren2->SetBackground( 0.2, 0.1, 0.2);
 
+  renWin->SetSize( 600, 350);
+
+  ren1->SetViewport(0,0,0.58333,1);
+  ren2->SetViewport(0.58333,0,1,1);
+
+  // set the actors' postions
   renWin->Render();
+  iren->SetEventPosition( 175,175);
   iren->SetKeyCode('r');
   iren->InvokeEvent(vtkCommand::CharEvent,NULL);
+  iren->SetEventPosition( 475,175);
+  iren->SetKeyCode('r');
+  iren->InvokeEvent(vtkCommand::CharEvent,NULL);
+  int *o = ren2->GetOrigin();
+  textActor->SetDisplayPosition(o[0] + 5, o[1] + 5);
   renWin->Render();
+
+  // test SetKeyPressActivationValue for one of the widgets
+  iren->SetKeyCode('z');
+  iren->InvokeEvent(vtkCommand::CharEvent,NULL);
+  iren->SetKeyCode('z');
+  iren->InvokeEvent(vtkCommand::CharEvent,NULL);
 
   ren1->GetActiveCamera()->Elevation(110);
   ren1->GetActiveCamera()->SetViewUp(0, 0, -1);
   ren1->GetActiveCamera()->Azimuth(45);
+  ren1->GetActiveCamera()->Dolly(1.15);
   ren1->ResetCameraClippingRange();
 
+  // make the window level work
+  int pos1[2] = { 193,187};
+  iren->SetEventPosition(pos1);
+  iren->InvokeEvent(vtkCommand::RightButtonPressEvent, NULL);
+  int pos2[2] = { 105,197};
+  iren->SetEventPosition(pos2);
+  iren->InvokeEvent(vtkCommand::MouseMoveEvent, NULL);
+  iren->InvokeEvent(vtkCommand::RightButtonReleaseEvent, NULL);
+
   int retVal = vtkRegressionTestImage( renWin );
+
   if ( retVal == vtkRegressionTester::DO_INTERACTOR)
     {
     iren->Start();
@@ -122,6 +225,8 @@ int main( int argc, char *argv[] )
   planeWidgetX->Delete();
   planeWidgetY->Delete();
   planeWidgetZ->Delete();
+  colorMap->Delete();
+  imageActor->Delete();
   picker->Delete();
   outlineActor->Delete();
   outlineMapper->Delete();
@@ -129,7 +234,11 @@ int main( int argc, char *argv[] )
   iren->Delete();
   renWin->Delete();
   ren1->Delete();
+  ren2->Delete();
   v16->Delete();
-  
-  return 0;
+  textMapper->Delete();
+  textActor->Delete();
+  myCallback->Delete();
+
+  return !retVal;
 }
