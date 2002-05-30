08ff737990d50e3962f1a94cbe3fc432c2c86663
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-05-30 16:12:56 -0400

    Add vtkXMLParser test

---
+++ IO/Testing/Cxx/TestXML.cxx
@@ -0,0 +1,51 @@
+#include "vtkXMLParser.h"
+#include "vtkOutputWindow.h"
+#include "vtkObjectFactory.h"
+
+
+class vtkMyXML : public vtkXMLParser
+{
+public:
+  vtkTypeMacro(vtkMyXML, vtkXMLParser);
+  static vtkMyXML* New();
+  
+private:
+  vtkMyXML() {} 
+  void StartElement(const char*, const char**) {}
+  void EndElement(const char*) {}
+
+  vtkMyXML(const vtkMyXML*); // Not implemented
+  void operator=(const vtkMyXML*); // Not implemented    
+};
+
+vtkStandardNewMacro(vtkMyXML);
+
+int main(int argc, char **argv)
+{
+  int res = 0;
+  vtkOutputWindow::GetInstance()->PromptUserOn();
+  if ( argc <= 1 )
+    {
+    cout << "Usage: " << argv[0] << " <xml file>" << endl;
+    return 1;
+    }
+
+  vtkMyXML *parser = vtkMyXML::New();
+  parser->SetFileName(argv[1]);
+  if ( ! parser->Parse() )
+    {
+    cout << "Cannot parse the file: " << argv[1] << endl;
+    res = 1;
+    }
+  parser->SetFileName(0);
+
+  if( !parser->Parse("<xml>This is an XML file</xml>") )
+    {
+    cout << "Cannot parse message" << endl;
+    res = 1;
+    }
+  
+  parser->Delete();
+
+  return res;
+}
