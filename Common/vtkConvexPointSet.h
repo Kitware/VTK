637949c8852abf6f714ef9afa96ccbff53f2aa26
Will Schroeder <will.schroeder@kitware.com>
2002-05-30 14:41:05 -0400

    ENH:Beginning to integrate nonlinear cells

---
+++ Common/vtkConvexPointSet.h
@@ -0,0 +1,122 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkConvexPointSet.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 1993-2001 Ken Martin, Will Schroeder, Bill Lorensen 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * Neither name of Ken Martin, Will Schroeder, or Bill Lorensen nor the names
+   of any contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+ * Modified source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+// .NAME vtkConvexPointSet - a 3D cell that represents a tetrahedron
+// .SECTION Description
+// vtkConvexPointSet is a concrete implementation of vtkCell to represent a 3D
+// tetrahedron.
+
+#ifndef __vtkConvexPointSet_h
+#define __vtkConvexPointSet_h
+
+#include "vtkCell3D.h"
+#include "vtkLine.h"
+#include "vtkTriangle.h"
+
+class vtkUnstructuredGrid;
+
+class VTK_COMMON_EXPORT vtkConvexPointSet : public vtkCell3D
+{
+public:
+  static vtkConvexPointSet *New();
+  vtkTypeMacro(vtkConvexPointSet,vtkCell);
+
+  // Description:
+  // See vtkCell3D API for description of these methods.
+  virtual void GetEdgePoints(int edgeId, int* &pts) {};
+  virtual void GetFacePoints(int faceId, int* &pts) {};
+
+  // Description:
+  // See the vtkCell API for descriptions of these methods.
+  vtkCell *MakeObject();
+  int GetCellType() {return VTK_CONVEX_POINT_SET;}
+  int GetNumberOfEdges() {return 0;}
+  int GetNumberOfFaces() {return 0;}
+  vtkCell *GetEdge(int edgeId) {return NULL;};
+  vtkCell *GetFace(int faceId) {return NULL;};
+  void Contour(float value, vtkDataArray *cellScalars, 
+               vtkPointLocator *locator, vtkCellArray *verts, 
+               vtkCellArray *lines, vtkCellArray *polys,
+               vtkPointData *inPd, vtkPointData *outPd,
+               vtkCellData *inCd, vtkIdType cellId, vtkCellData *outCd);
+  int EvaluatePosition(float x[3], float* closestPoint,
+                       int& subId, float pcoords[3],
+                       float& dist2, float *weights) 
+    {return 0;};
+  void EvaluateLocation(int& subId, float pcoords[3], float x[3],
+                        float *weights) {};
+  int IntersectWithLine(float p1[3], float p2[3], float tol, float& t,
+                        float x[3], float pcoords[3], int& subId)
+    {return 0;};
+  int Triangulate(int index, vtkIdList *ptIds, vtkPoints *pts) {return 0;};
+  void Derivatives(int subId, float pcoords[3], float *values, 
+                   int dim, float *derivs) {};
+
+  // Description:
+  // Returns the set of points that are on the boundary of the tetrahedron that
+  // are closest parametrically to the point specified. This may include faces,
+  // edges, or vertices.
+  int CellBoundary(int subId, float pcoords[3], vtkIdList *pts)
+    {return 0;};
+  
+  // Description:
+  // Return the center of the tetrahedron in parametric coordinates.
+  int GetParametricCenter(float pcoords[3]);
+
+protected:
+  vtkConvexPointSet();
+  ~vtkConvexPointSet();
+
+private:
+  vtkConvexPointSet(const vtkConvexPointSet&);  // Not implemented.
+  void operator=(const vtkConvexPointSet&);  // Not implemented.
+};
+
+inline int vtkConvexPointSet::GetParametricCenter(float pcoords[3])
+{
+  pcoords[0] = pcoords[1] = pcoords[2] = 0.5;
+  return 0;
+}
+
+#endif
+
+
+
