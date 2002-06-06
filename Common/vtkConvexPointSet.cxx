49e3cabb59ce2a60e2d215b5be3733742240ce48
Will Schroeder <will.schroeder@kitware.com>
2002-06-06 16:18:18 -0400

    ENH:Scoped out clipping and contouring

--- Common/vtkConvexPointSet.cxx
+++ Common/vtkConvexPointSet.cxx
@@ -19,18 +19,24 @@
 #include "vtkMath.h"
 #include "vtkCellArray.h"
 #include "vtkPointLocator.h"
+#include "vtkOrderedTriangulator.h"
+#include "vtkTetra.h"
 #include "vtkObjectFactory.h"
 
-vtkCxxRevisionMacro(vtkConvexPointSet, "1.3");
+vtkCxxRevisionMacro(vtkConvexPointSet, "1.4");
 vtkStandardNewMacro(vtkConvexPointSet);
 
 // Construct the hexahedron with eight points.
 vtkConvexPointSet::vtkConvexPointSet()
 {
+  this->Tetras = vtkCellArray::New();
+  this->Tetra = vtkTetra::New();
 }
 
 vtkConvexPointSet::~vtkConvexPointSet()
 {
+  this->Tetras->Delete();
+  this->Tetra->Delete();
 }
 
 vtkCell *vtkConvexPointSet::MakeObject()
@@ -41,16 +47,195 @@ vtkCell *vtkConvexPointSet::MakeObject()
 }
 
 
+int vtkConvexPointSet::Triangulate(int index, vtkIdList *ptIds, vtkPoints *pts)
+{
+  int numPts=this->GetNumberOfPoints();
+  int i;
+  float *xPtr;
+  vtkIdType ptId;
+  vtkIdType npts, *cpts;
+
+  // Initialize
+  ptIds->Reset();
+  pts->Reset();
+  if ( numPts < 1 ) return 0;
+  
+  // Create a triangulator if necessary.
+  if ( ! this->Triangulator )
+    {
+    this->Triangulator = vtkOrderedTriangulator::New();
+    this->Triangulator->PreSortedOff();
+    }
+
+  // Initialize Delaunay insertion process with voxel triangulation.
+  // No more than (numPts + numEdges) points can be inserted.
+  this->Triangulator->InitTriangulation(this->GetBounds(), numPts);
+
+  // Inject cell points into triangulation. Recall that the PreSortedOff() 
+  // flag was set which means that the triangulator will order the points 
+  // according to point id.
+  for (i=0; i<numPts; i++)
+    {
+    ptId = this->PointIds->GetId(i);
+    xPtr = this->Points->GetPoint(i);
+    this->Triangulator->InsertPoint(ptId, xPtr, 0);
+    }//for eight voxel corner points
+  
+  // triangulate the points
+  this->Triangulator->Triangulate();
+
+  // Add the triangulation to the mesh
+  this->Tetras->Reset();
+  this->Triangulator->AddTetras(0,this->Tetras);
+  
+  // Copy into expected output structures
+  for ( this->Tetras->InitTraversal(); this->Tetras->GetNextCell(npts,cpts); )
+    {
+    for (i=0; i<4; i++)
+      {
+      ptIds->InsertNextId(cpts[i]);
+      pts->InsertNextPoint(this->Points->GetPoint(cpts[i]));
+      }
+    }
+  
+  return 1;
+}
+
+  
+void vtkConvexPointSet::OrderedTriangulate(vtkIdType numPts, 
+                                           float value,
+                                           vtkDataArray *cellScalars,
+                                           vtkPointLocator *locator,
+                                           vtkPointData *inPD, 
+                                           vtkPointData *outPD,
+                                           int insideOut)
+{
+  vtkIdType ptId, id;
+  int i;
+  float s1;
+  int type;
+  float *xPtr;
+
+  // allocate scratch memory
+  int *internalId = new int [numPts];
+
+  // Create a triangulator if necessary.
+  if ( ! this->Triangulator )
+    {
+    this->Triangulator = vtkOrderedTriangulator::New();
+    this->Triangulator->PreSortedOff();
+    }
+
+  // Initialize Delaunay insertion process with voxel triangulation.    
+
+  // No more than (numPts + numEdges) points can be inserted.
+  this->Triangulator->InitTriangulation(this->GetBounds(), numPts);
+
+  // Inject cell points into triangulation. Recall that the PreSortedOff() 
+  // flag was set which means that the triangulator will order the points 
+  // according to point id.
+  for (i=0; i<numPts; i++)
+    {
+    ptId = this->PointIds->GetId(i);
+      
+    // Currently all points are injected because of the possibility 
+    // of intersection point merging.
+    s1 = cellScalars->GetComponent(i,0);
+    if ( (s1 >= value && !insideOut) || (s1 < value && insideOut) )
+      {
+      type = 0; //inside
+      }
+    else
+      {
+      type = 4; //no insert, but its type might change later
+      }
+
+    xPtr = this->Points->GetPoint(i);
+    if ( locator->InsertUniquePoint(xPtr, id) )    
+
+      {
+      outPD->CopyData(inPD,ptId, id);
+      }
+    internalId[i] = this->Triangulator->InsertPoint(id, xPtr, type);
+    }//for eight voxel corner points
+  
+  // triangulate the points
+  this->Triangulator->Triangulate();
+
+  delete [] internalId;
+}
 
 void vtkConvexPointSet::Contour(float value, vtkDataArray *cellScalars, 
-                            vtkPointLocator *locator,
-                            vtkCellArray *vtkNotUsed(verts), 
-                            vtkCellArray *vtkNotUsed(lines), 
-                            vtkCellArray *polys, 
-                            vtkPointData *inPd, vtkPointData *outPd,
-                            vtkCellData *inCd, vtkIdType cellId,
-                            vtkCellData *outCd)
+                                vtkPointLocator *locator,
+                                vtkCellArray *vtkNotUsed(verts), 
+                                vtkCellArray *vtkNotUsed(lines), 
+                                vtkCellArray *polys, 
+                                vtkPointData *inPd, vtkPointData *outPd,
+                                vtkCellData *inCd, vtkIdType cellId,
+                                vtkCellData *outCd)
+{
+  // Initialize
+  vtkIdType numPts=this->GetNumberOfPoints();
+  if ( numPts < 1 ) return;
+
+  // Triangulate with cell intersection points
+  this->OrderedTriangulate(numPts, value, cellScalars, locator, 
+                           inPd, outPd, 0);
+  
+  // Add the surface triangles to the mesh
+  this->Triangulator->AddTriangles(polys);
+}
+    
+
+
+void vtkConvexPointSet::Clip(float value, vtkDataArray *cellScalars, 
+                             vtkPointLocator *locator, vtkCellArray *tets,
+                             vtkPointData *inPD, vtkPointData *outPD,
+                             vtkCellData *vtkNotUsed(inCD),
+                             vtkIdType vtkNotUsed(cellId),
+                             vtkCellData *vtkNotUsed(outCD), int insideOut)
+{
+  // Initialize
+  vtkIdType numPts=this->GetNumberOfPoints();
+  if ( numPts < 1 ) return;
+
+  // Triangulate with cell intersection points
+  this->OrderedTriangulate(numPts, value, cellScalars, locator, 
+                           inPD, outPD, insideOut);
+  
+  // Add the tetrahedra to the mesh
+  this->Triangulator->AddTetras(0,tets);
+}
+
+int vtkConvexPointSet::CellBoundary(int subId, float pcoords[3], 
+                                    vtkIdList *pts)
+{
+  return 0;
+}
+
+int vtkConvexPointSet::EvaluatePosition(float x[3], float* closestPoint,
+                                        int& subId, float pcoords[3],
+                                        float& dist2, float *weights)
+{
+  return 0;
+}
+
+void vtkConvexPointSet::EvaluateLocation(int& subId, float pcoords[3], 
+                                         float x[3], float *weights)
 {
 }
+
+int vtkConvexPointSet::IntersectWithLine(float p1[3], float p2[3], float tol, 
+                                         float& t, float x[3], 
+                                         float pcoords[3], int& subId)
+{
+  return 0;
+}
+
+void vtkConvexPointSet::Derivatives(int subId, float pcoords[3], 
+                                    float *values, int dim, float *derivs)
+{
+}
+
 
 
--- Common/vtkConvexPointSet.h
+++ Common/vtkConvexPointSet.h
@@ -20,6 +20,10 @@
 // vtkConvexPointSet is a concrete implementation that represents
 // a 3D cell defined by a convex set of points. Example of such cells are
 // octants (from an octree).
+//
+// vtkConvexPointSet uses the ordered triangulations approach 
+// (vtkOrderedTriangulator) to create triangulations guaranteed to be
+// compatible across shared faces.
 
 #ifndef __vtkConvexPointSet_h
 #define __vtkConvexPointSet_h
@@ -29,6 +33,8 @@
 #include "vtkTriangle.h"
 
 class vtkUnstructuredGrid;
+class vtkCellArray;
+class vtkTetra;
 
 class VTK_COMMON_EXPORT vtkConvexPointSet : public vtkCell3D
 {
@@ -47,42 +53,78 @@ public:
   int GetCellType() {return VTK_CONVEX_POINT_SET;}
   int GetNumberOfEdges() {return 0;}
   int GetNumberOfFaces() {return 0;}
-  vtkCell *GetEdge(int edgeId) 
-    {return NULL;}
-  vtkCell *GetFace(int faceId) 
-    {return NULL;}
+  vtkCell *GetEdge(int edgeId) {return NULL;}
+  vtkCell *GetFace(int faceId) {return NULL;}
+
+  // Description:
+  // Satisfy the vtkCell API. This method contours by triangulating the
+  // cell and then contouring the resulting tetrahedra.
   void Contour(float value, vtkDataArray *cellScalars, 
                vtkPointLocator *locator, vtkCellArray *verts, 
                vtkCellArray *lines, vtkCellArray *polys,
                vtkPointData *inPd, vtkPointData *outPd,
                vtkCellData *inCd, vtkIdType cellId, vtkCellData *outCd);
-  int EvaluatePosition(float x[3], float* closestPoint,
-                       int& subId, float pcoords[3],
-                       float& dist2, float *weights) 
-    {return 0;}
-  void EvaluateLocation(int& subId, float pcoords[3], float x[3],
-                        float *weights) {};
-  int IntersectWithLine(float p1[3], float p2[3], float tol, float& t,
-                        float x[3], float pcoords[3], int& subId)
-    {return 0;};
-  int Triangulate(int index, vtkIdList *ptIds, vtkPoints *pts) 
-    {return 0;}
-  void Derivatives(int subId, float pcoords[3], float *values, 
-                   int dim, float *derivs) {};
 
   // Description:
-  // Returns the set of points that are on the boundary of the cell that
-  // are closest parametrically to the point specified.
-  int CellBoundary(int subId, float pcoords[3], vtkIdList *pts)
-    {return 0;};
+  // Satisfy the vtkCell API. This method contours by triangulating the
+  // cell and then adding clip-edge intersection points into the
+  // triangulation; extracting the clipped region.
+  virtual void Clip(float value, vtkDataArray *cellScalars, 
+                    vtkPointLocator *locator, vtkCellArray *connectivity,
+                    vtkPointData *inPd, vtkPointData *outPd,
+                    vtkCellData *inCd, vtkIdType cellId, vtkCellData *outCd, 
+                    int insideOut);
+
+  // Description:
+  // Satisfy the vtkCell API. This method determines the subId, pcoords,
+  // and weights by triangulating the convex point set, and then 
+  // determining which tetrahedron the point lies in.
+  int EvaluatePosition(float x[3], float* closestPoint,
+                       int& subId, float pcoords[3],
+                       float& dist2, float *weights);
+
+  // Description:
+  // The inverse of EvaluatePosition.
+  void EvaluateLocation(int& subId, float pcoords[3], float x[3],
+                        float *weights);
   
   // Description:
-  // Return the center of the cell in parametric coordinates.
+  // Triangulates the cells and then intersects them to determine the
+  // intersection point.
+  int IntersectWithLine(float p1[3], float p2[3], float tol, float& t,
+                        float x[3], float pcoords[3], int& subId);
+
+  // Description:
+  // Triangulate using methods of vtkOrderedTriangulator.
+  int Triangulate(int index, vtkIdList *ptIds, vtkPoints *pts);
+  
+  // Description:
+  // Computes derivatives by triangulating and from subId and pcoords,
+  // evaluating derivatives on the resulting tetrahedron.
+  void Derivatives(int subId, float pcoords[3], float *values, 
+                   int dim, float *derivs);
+
+  // Description:
+  // Returns the set of points forming a face of the triangulation of these
+  // points that are on the boundary of the cell that are closest 
+  // parametrically to the point specified.
+  int CellBoundary(int subId, float pcoords[3], vtkIdList *pts);
+  
+  // Description:
+  // Return the center of the cell in parametric coordinates. 
   int GetParametricCenter(float pcoords[3]);
 
 protected:
   vtkConvexPointSet();
   ~vtkConvexPointSet();
+
+  vtkCellArray *Tetras;
+  vtkTetra *Tetra;
+
+  void OrderedTriangulate(vtkIdType numPts, float value,
+                          vtkDataArray *cellScalars, vtkPointLocator *locator,
+                          vtkPointData *inPD, vtkPointData *outPD,
+                          int insideOut);
 
 private:
   vtkConvexPointSet(const vtkConvexPointSet&);  // Not implemented.
