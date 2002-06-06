b0674ede2354d8666285160a0aeda8553642eb8e
Ken Martin <ken.martin@kitware.com>
2002-06-06 16:30:09 -0400

    converted to iterator

--- Imaging/vtkImageHSVToRGB.cxx
+++ Imaging/vtkImageHSVToRGB.cxx
@@ -17,10 +17,11 @@
 =========================================================================*/
 #include "vtkImageHSVToRGB.h"
 #include "vtkObjectFactory.h"
+#include "vtkImageProgressIterator.h"
 
 #include <math.h>
 
-vtkCxxRevisionMacro(vtkImageHSVToRGB, "1.23");
+vtkCxxRevisionMacro(vtkImageHSVToRGB, "1.24");
 vtkStandardNewMacro(vtkImageHSVToRGB);
 
 //----------------------------------------------------------------------------
@@ -34,126 +35,104 @@ vtkImageHSVToRGB::vtkImageHSVToRGB()
 // This templated function executes the filter for any type of data.
 template <class T>
 static void vtkImageHSVToRGBExecute(vtkImageHSVToRGB *self,
-                                    vtkImageData *inData, T *inPtr,
-                                    vtkImageData *outData, T *outPtr,
-                                    int outExt[6], int id)
+                                    vtkImageData *inData,
+                                    vtkImageData *outData,
+                                    int outExt[6], int id, T *)
 {
-  int idxC, idxX, idxY, idxZ;
-  int maxC, maxX, maxY, maxZ;
-  int inIncX, inIncY, inIncZ;
-  int outIncX, outIncY, outIncZ;
-  unsigned long count = 0;
-  unsigned long target;
+  vtkImageIterator<T> inIt(inData, outExt);
+  vtkImageProgressIterator<T> outIt(outData, outExt, self, id);
   float R, G, B, H, S, V;
   float max = self->GetMaximum();
   float temp;
   float third = max / 3.0;
+  int idxC;
   
   // find the region to loop over
-  maxC = inData->GetNumberOfScalarComponents()-1;
-  maxX = outExt[1] - outExt[0]; 
-  maxY = outExt[3] - outExt[2]; 
-  maxZ = outExt[5] - outExt[4];
-  target = (unsigned long)((maxZ+1)*(maxY+1)/50.0);
-  target++;
+  int maxC = inData->GetNumberOfScalarComponents()-1;
   
-  // Get increments to march through data 
-  inData->GetContinuousIncrements(outExt, inIncX, inIncY, inIncZ);
-  outData->GetContinuousIncrements(outExt, outIncX, outIncY, outIncZ);
-
   // Loop through ouput pixels
-  for (idxZ = 0; idxZ <= maxZ; idxZ++)
+  while (!outIt.IsAtEnd())
     {
-    for (idxY = 0; !self->AbortExecute && idxY <= maxY; idxY++)
+    T* inSI = inIt.BeginSpan();
+    T* outSI = outIt.BeginSpan();
+    T* outSIEnd = outIt.EndSpan();
+    while (outSI != outSIEnd)
       {
-      if (!id) 
+      // Pixel operation
+      H = (float)(*inSI); ++inSI;
+      S = (float)(*inSI); ++inSI;
+      V = (float)(*inSI); ++inSI;
+      
+      // compute rgb assuming S = 1.0;
+      if (H >= 0.0 && H <= third) // red -> green
         {
-        if (!(count%target))
-          {
-          self->UpdateProgress(count/(50.0*target));
-          }
-        count++;
+        G = H/third;
+        R = 1.0 - G;
+        B = 0.0;
         }
-      for (idxX = 0; idxX <= maxX; idxX++)
+      else if (H >= third && H <= 2.0*third) // green -> blue
         {
-        // Pixel operation
-        H = (float)(*inPtr); inPtr++;
-        S = (float)(*inPtr); inPtr++;
-        V = (float)(*inPtr); inPtr++;
-
-        // compute rgb assuming S = 1.0;
-        if (H >= 0.0 && H <= third) // red -> green
-          {
-          G = H/third;
-          R = 1.0 - G;
-          B = 0.0;
-          }
-        else if (H >= third && H <= 2.0*third) // green -> blue
-          {
-          B = (H - third)/third;
-          G = 1.0 - B;
-          R = 0.0;
-          }
-        else // blue -> red
-          {
-          R = (H - 2.0 * third)/third;
-          B = 1.0 - R;
-          G = 0.0;
-          }
-        
-        // add Saturation to the equation.
-        S = S / max;
-        //R = S + (1.0 - S)*R;
-        //G = S + (1.0 - S)*G;
-        //B = S + (1.0 - S)*B;
-        // what happend to this?
-        R = S*R + (1.0 - S);
-        G = S*G + (1.0 - S);
-        B = S*B + (1.0 - S);
-        
-        // Use value to get actual RGB 
-        // normalize RGB first then apply value
-        temp = R + G + B; 
-        //V = 3 * V / (temp * max);
-        // and what happend to this?
-        V = 3 * V / (temp);
-        R = R * V;
-        G = G * V;
-        B = B * V;
-        
-        // clip below 255
-        //if (R > 255.0) R = max;
-        //if (G > 255.0) G = max;
-        //if (B > 255.0) B = max;
-        // mixed constant 255 and max ?????
-        if (R > max)
-          {
-          R = max;
-          }
-        if (G > max)
-          {
-          G = max;
-          }
-        if (B > max)
-          {
-          B = max;
-          }
-        
-        // assign output.
-        *outPtr = (T)(R); outPtr++;
-        *outPtr = (T)(G); outPtr++;
-        *outPtr = (T)(B); outPtr++;
-
-        for (idxC = 3; idxC <= maxC; idxC++)
-          {
-          *outPtr++ = *inPtr++;
-          }
+        B = (H - third)/third;
+        G = 1.0 - B;
+        R = 0.0;
         }
-      outPtr += outIncY;
-      inPtr += inIncY;
+      else // blue -> red
+        {
+        R = (H - 2.0 * third)/third;
+        B = 1.0 - R;
+        G = 0.0;
+        }
+        
+      // add Saturation to the equation.
+      S = S / max;
+      //R = S + (1.0 - S)*R;
+      //G = S + (1.0 - S)*G;
+      //B = S + (1.0 - S)*B;
+      // what happend to this?
+      R = S*R + (1.0 - S);
+      G = S*G + (1.0 - S);
+      B = S*B + (1.0 - S);
+      
+      // Use value to get actual RGB 
+      // normalize RGB first then apply value
+      temp = R + G + B; 
+      //V = 3 * V / (temp * max);
+      // and what happend to this?
+      V = 3 * V / (temp);
+      R = R * V;
+      G = G * V;
+      B = B * V;
+      
+      // clip below 255
+      //if (R > 255.0) R = max;
+      //if (G > 255.0) G = max;
+      //if (B > 255.0) B = max;
+      // mixed constant 255 and max ?????
+      if (R > max)
+        {
+        R = max;
+        }
+      if (G > max)
+        {
+        G = max;
+        }
+      if (B > max)
+        {
+        B = max;
+        }
+      
+      // assign output.
+      *outSI = (T)(R); ++outSI;
+      *outSI = (T)(G); ++outSI;
+      *outSI = (T)(B); ++outSI;
+      
+      for (idxC = 3; idxC <= maxC; idxC++)
+        {
+        *outSI++ = *inSI++;
+        }
       }
-    outPtr += outIncZ;
-    inPtr += inIncZ;
+    inIt.NextSpan();
+    outIt.NextSpan();
     }
 }
 
@@ -162,17 +141,14 @@ void vtkImageHSVToRGB::ThreadedExecute(vtkImageData *inData,
                                        vtkImageData *outData,
                                        int outExt[6], int id)
 {
-  void *inPtr = inData->GetScalarPointerForExtent(outExt);
-  void *outPtr = outData->GetScalarPointerForExtent(outExt);
-  
   vtkDebugMacro(<< "Execute: inData = " << inData 
-                << ", outData = " << outData);
+  << ", outData = " << outData);
   
   // this filter expects that input is the same type as output.
   if (inData->GetScalarType() != outData->GetScalarType())
     {
     vtkErrorMacro(<< "Execute: input ScalarType, " << inData->GetScalarType()
-                  << ", must match out ScalarType " << outData->GetScalarType());
+    << ", must match out ScalarType " << outData->GetScalarType());
     return;
     }
   
@@ -190,8 +166,8 @@ void vtkImageHSVToRGB::ThreadedExecute(vtkImageData *inData,
 
   switch (inData->GetScalarType())
     {
-    vtkTemplateMacro7(vtkImageHSVToRGBExecute,this, inData, (VTK_TT *)(inPtr), 
-                      outData, (VTK_TT *)(outPtr), outExt, id);
+    vtkTemplateMacro6(vtkImageHSVToRGBExecute,this, inData, 
+                      outData, outExt, id, static_cast<VTK_TT *>(0));
     default:
       vtkErrorMacro(<< "Execute: Unknown ScalarType");
       return;
