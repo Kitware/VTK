72b991789bbb4f5650ceb45dfa0acb7a82bca735
Ken Martin <ken.martin@kitware.com>
2002-06-06 15:55:46 -0400

    converted to iterator

--- Imaging/vtkImageShiftScale.cxx
+++ Imaging/vtkImageShiftScale.cxx
@@ -17,8 +17,9 @@
 =========================================================================*/
 #include "vtkImageShiftScale.h"
 #include "vtkObjectFactory.h"
+#include "vtkImageProgressIterator.h"
 
-vtkCxxRevisionMacro(vtkImageShiftScale, "1.41");
+vtkCxxRevisionMacro(vtkImageShiftScale, "1.42");
 vtkStandardNewMacro(vtkImageShiftScale);
 
 //----------------------------------------------------------------------------
@@ -52,86 +53,59 @@ void vtkImageShiftScale::ExecuteInformation(vtkImageData *inData,
 // This templated function executes the filter for any type of data.
 template <class IT, class OT>
 static void vtkImageShiftScaleExecute(vtkImageShiftScale *self,
-                                      vtkImageData *inData, IT *inPtr,
-                                      vtkImageData *outData, OT *outPtr,
-                                      int outExt[6], int id)
+                                      vtkImageData *inData,
+                                      vtkImageData *outData,
+                                      int outExt[6], int id,  IT *, OT *)
 {
+  vtkImageIterator<IT> inIt(inData, outExt);
+  vtkImageProgressIterator<OT> outIt(outData, outExt, self, id);
   double typeMin, typeMax, val;
   int clamp;
   float shift = self->GetShift();
   float scale = self->GetScale();
-  int idxR, idxY, idxZ;
-  int maxY, maxZ;
-  int inIncX, inIncY, inIncZ;
-  int outIncX, outIncY, outIncZ;
-  int rowLength;
-  unsigned long count = 0;
-  unsigned long target;
 
   // for preventing overflow
   typeMin = outData->GetScalarTypeMin();
   typeMax = outData->GetScalarTypeMax();
   clamp = self->GetClampOverflow();
     
-  // find the region to loop over
-  rowLength = (outExt[1] - outExt[0]+1)*inData->GetNumberOfScalarComponents();
-  maxY = outExt[3] - outExt[2]; 
-  maxZ = outExt[5] - outExt[4];
-  target = (unsigned long)((maxZ+1)*(maxY+1)/50.0);
-  target++;
-  
-  // Get increments to march through data 
-  inData->GetContinuousIncrements(outExt, inIncX, inIncY, inIncZ);
-  outData->GetContinuousIncrements(outExt, outIncX, outIncY, outIncZ);
-
   // Loop through ouput pixels
-  for (idxZ = 0; idxZ <= maxZ; idxZ++)
+  while (!outIt.IsAtEnd())
     {
-    for (idxY = 0; !self->AbortExecute && idxY <= maxY; idxY++)
+    IT* inSI = inIt.BeginSpan();
+    OT* outSI = outIt.BeginSpan();
+    OT* outSIEnd = outIt.EndSpan();
+    if (clamp)
       {
-      if (!id) 
+      while (outSI != outSIEnd)
         {
-        if (!(count%target))
+        // Pixel operation
+        val = ((float)(*inSI) + shift) * scale;
+        if (val > typeMax)
           {
-          self->UpdateProgress(count/(50.0*target));
+          val = typeMax;
           }
-        count++;
-        }
-      // put the test for clamp to avoid the innermost loop
-      if (clamp)
-        {
-        for (idxR = 0; idxR < rowLength; idxR++)
+        if (val < typeMin)
           {
-          // Pixel operation
-          val = ((float)(*inPtr) + shift) * scale;
-          if (val > typeMax)
-            {
-            val = typeMax;
-            }
-          if (val < typeMin)
-            {
-            val = typeMin;
-            }
-          *outPtr = (OT)(val);
-          outPtr++;
-          inPtr++;
+          val = typeMin;
           }
+        *outSI = (OT)(val);
+        ++outSI;
+        ++inSI;
         }
-      else
-        {
-        for (idxR = 0; idxR < rowLength; idxR++)
-          {
-          // Pixel operation
-          *outPtr = (OT)(((float)(*inPtr) + shift) * scale);
-          outPtr++;
-          inPtr++;
-          }
-        }
-      outPtr += outIncY;
-      inPtr += inIncY;
       }
-    outPtr += outIncZ;
-    inPtr += inIncZ;
+    else
+      {
+      while (outSI != outSIEnd)
+        {
+        // Pixel operation
+        *outSI = (OT)(((float)(*inSI) + shift) * scale);
+        ++outSI;
+        ++inSI;
+        }
+      }
+    inIt.NextSpan();
+    outIt.NextSpan();
     }
 }
 
@@ -140,16 +114,15 @@ static void vtkImageShiftScaleExecute(vtkImageShiftScale *self,
 //----------------------------------------------------------------------------
 template <class T>
 static void vtkImageShiftScaleExecute1(vtkImageShiftScale *self,
-                                      vtkImageData *inData, T *inPtr,
+                                      vtkImageData *inData,
                                       vtkImageData *outData,
-                                      int outExt[6], int id)
+                                      int outExt[6], int id, T *)
 {
-  void *outPtr = outData->GetScalarPointerForExtent(outExt);
-  
   switch (outData->GetScalarType())
     {
-    vtkTemplateMacro7(vtkImageShiftScaleExecute, self, inData, inPtr,
-                      outData, (VTK_TT *)(outPtr),outExt, id);
+    vtkTemplateMacro7(vtkImageShiftScaleExecute, self, inData,
+                      outData,outExt, id,
+                      static_cast<T *>(0), static_cast<VTK_TT *>(0));
     default:
       vtkGenericWarningMacro("Execute: Unknown input ScalarType");
       return;
@@ -168,12 +141,10 @@ void vtkImageShiftScale::ThreadedExecute(vtkImageData *inData,
                                          vtkImageData *outData,
                                          int outExt[6], int id)
 {
-  void *inPtr = inData->GetScalarPointerForExtent(outExt);
-  
   switch (inData->GetScalarType())
     {
     vtkTemplateMacro6(vtkImageShiftScaleExecute1, this, 
-                      inData, (VTK_TT *)(inPtr), outData, outExt, id);
+                      inData, outData, outExt, id, static_cast<VTK_TT *>(0));
     default:
       vtkErrorMacro(<< "Execute: Unknown ScalarType");
       return;
