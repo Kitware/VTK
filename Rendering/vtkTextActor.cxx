e743d2e829e418b1f62d6d177994819b926a13c0
Sebastien Barre <sebastien.barre@kitware.com>
2002-06-20 15:17:01 -0400

    FIX: returns status of call to parent's method, instead of just 1

--- Rendering/vtkTextActor.cxx
+++ Rendering/vtkTextActor.cxx
@@ -18,7 +18,7 @@
 #include "vtkTextActor.h"
 #include "vtkObjectFactory.h"
 
-vtkCxxRevisionMacro(vtkTextActor, "1.4");
+vtkCxxRevisionMacro(vtkTextActor, "1.5");
 vtkStandardNewMacro(vtkTextActor);
 // ----------------------------------------------------------------------------
 vtkTextActor::vtkTextActor()
@@ -94,8 +94,7 @@ void vtkTextActor::ReleaseGraphicsResources(vtkWindow *win)
 int vtkTextActor::RenderOverlay(vtkViewport *viewport)
 {
   // Everything is built in RenderOpaqueGeometry, just have to render
-  this->vtkActor2D::RenderOverlay(viewport);
-  return 1;
+  return this->vtkActor2D::RenderOverlay(viewport);
 }
 // ----------------------------------------------------------------------------
 int vtkTextActor::RenderOpaqueGeometry(vtkViewport *viewport)
