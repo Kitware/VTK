5d40235d11d3bc2580cfbeca33ae3e34de9ee259
Bill Lorensen <lorensen@crd.ge.com>
2002-07-04 08:42:21 -0400

    ERR: the duplicate loading blocker command must not be abstract,

--- Wrapping/Tcl/vtkfiltering/vtkfiltering.tcl
+++ Wrapping/Tcl/vtkfiltering/vtkfiltering.tcl
@@ -1,6 +1,6 @@
 package require vtkcommon
 
-if {[info commands vtkDataObjectSource] != "" ||
+if {[info commands vtkCardinalSpline] != "" ||
     [::vtk::load_component vtkFilteringTCL] == ""} {
     package provide vtkfiltering 4.0
 }
