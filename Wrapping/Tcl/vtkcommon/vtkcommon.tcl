bfda439c0b9af54d1f584e6a37a98a2e1bce3ef8
Sebastien Barre <sebastien.barre@kitware.com>
2002-07-08 14:05:45 -0400

    ENH: call vtkCommand DeleteAllObjects automatically on exit (detect leaks). Brad's contrib.

--- Wrapping/Tcl/vtkcommon/vtkcommon.tcl
+++ Wrapping/Tcl/vtkcommon/vtkcommon.tcl
@@ -3,4 +3,12 @@ package require vtkbase
 if {[info commands vtkObject] != "" ||
     [::vtk::load_component vtkCommonTCL] == ""} {
     package provide vtkcommon 4.0
+
+    # Invoke DeleteAllObjects on exit
+
+    rename ::exit ::vtk::exit
+    proc ::exit {} {
+        vtkCommand DeleteAllObjects
+        return [::vtk::exit]
+    }
 }
