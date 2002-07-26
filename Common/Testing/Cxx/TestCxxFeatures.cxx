f2324be67fcc45cf3dc229adebd52c241ca20937
Brad King <brad.king@kitware.com>
2002-07-26 15:14:07 -0400

    ENH: Added macro configuration to keep tests running when compilers support them.

--- Common/Testing/Cxx/TestCxxFeatures.cxx
+++ Common/Testing/Cxx/TestCxxFeatures.cxx
@@ -25,6 +25,43 @@
 
 //----------------------------------------------------------------------------
 
+/* Check for known compilers.  */
+
+#if defined(_MSC_VER)
+# define VTK_CXX_MSVC
+#endif
+
+#if defined(__sgi) && !defined(__GNUC__) && !defined(_COMPILER_VERSION)
+# define VTK_CXX_SGI_6
+#endif
+
+//----------------------------------------------------------------------------
+
+/* Check for known compiler limitations.  */
+
+// Check for IRIX64-6.5-CC-o32 (old SGI compiler).
+#if defined(VTK_CXX_SGI_6)
+# define VTK_TYPENAME /* empty */
+# define VTK_CLASS_TEMPLATE_SPECIALIZATION /* empty */
+#endif
+
+// Check for MSVC.
+#if defined(VTK_CXX_MSVC)
+# define VTK_TYPENAME /* empty */
+#endif
+
+// Assume standard behavior if symbol is not already defined.
+#if !defined(VTK_TYPENAME)
+# define VTK_TYPENAME typename
+#endif
+
+// Assume standard behavior if symbol is not already defined.
+#if !defined(VTK_CLASS_TEMPLATE_SPECIALIZATION)
+# define VTK_CLASS_TEMPLATE_SPECIALIZATION template <>
+#endif
+
+//----------------------------------------------------------------------------
+
 /* Test full template specialization of functions.  */
 template <class T>
 int FullySpecializedFunction(T*)
@@ -71,8 +108,7 @@ int TestFullySpecializedFunction()
 
 /* Test use of standard "bool" type and values.  */
 
-#if 0
-// Fails on kulu.crd IRIX64-6.5-CC-o32 (old SGI compiler).
+#if !defined(VTK_CXX_SGI_6)
 bool GetFalse()
 {
   return false;
@@ -105,40 +141,56 @@ int TestBool()
 
 /* Test full template specialization of classes.  */
 
-#if 0
-// Fails on kulu.crd IRIX64-6.5-CC-o32 (old SGI compiler).
 template <class T>
 struct FullySpecializedClass
 {
   static int Method() { return 0; }
+  typedef T Type;
 };
 
-template <>
-struct FullySpecializedClass<int>
+VTK_CLASS_TEMPLATE_SPECIALIZATION
+struct FullySpecializedClass<float>
 {
   static int Method() { return 1; }
+  typedef int Type;
 };
+
+template <class T>
+int TestFullySpecializedClassTrait(T*)
+{
+  typedef VTK_TYPENAME FullySpecializedClass<T>::Type Type;
+  if(static_cast<Type>(3.1) == 3.1)
+    {
+    return 0;
+    }
+  return 1;
+}
 
 int TestFullySpecializedClass()
 {
   int result = 1;
-  int should_be_0 = FullySpecializedClass<float>::Method();
+  int should_be_0 = FullySpecializedClass<int>::Method();
   if(should_be_0 != 0)
     {
-    cerr << "FullySpecializedClass<float>::Method() returned "
+    cerr << "FullySpecializedClass<int>::Method() returned "
          << should_be_0 << ", not 0.\n";
     result = 0;
     }
-  int should_be_1 = FullySpecializedClass<int>::Method();
+  int should_be_1 = FullySpecializedClass<float>::Method();
   if(should_be_1 != 1)
     {    
-    cerr << "FullySpecializedClass<int>::Method() returned "
+    cerr << "FullySpecializedClass<float>::Method() returned "
          << should_be_1 << ", not 1.\n";
+    result = 0;
+    }
+  if(!TestFullySpecializedClassTrait(static_cast<float*>(0)))
+    {
+    cerr << "Trait lookup of float didn't produce int.";
     result = 0;
     }
   return result;
 }
-#endif
+
 //----------------------------------------------------------------------------
 
 /* Test if(int x = f()) style scoping.  */
@@ -181,30 +233,18 @@ int TestIfScope()
 
 //----------------------------------------------------------------------------
 
+#define DO_TEST(x) \
+  if(x()) { cout << "Passed: " #x "\n"; } \
+  else { cout << "Failed: " #x "\n"; result = 1; }
+
 int main()
 {
   int result = 0;
-  if(!TestFullySpecializedFunction())
-    {
-    result = 1;
-    }
-#if 0
-  // Fails on kulu.crd IRIX64-6.5-CC-o32 (old SGI compiler).
-  if(!TestBool())
-    {
-    result = 1;
-    }
+  DO_TEST(TestFullySpecializedFunction);
+#if !defined(VTK_CXX_SGI_6)
+  DO_TEST(TestBool);
 #endif
-#if 0
-  // Fails on kulu.crd IRIX64-6.5-CC-o32 (old SGI compiler).
-  if(!TestFullySpecializedClass())
-    {
-    result = 1;
-    }
-#endif
-  if(!TestIfScope())
-    {
-    result = 1;
-    }
+  DO_TEST(TestFullySpecializedClass);
+  DO_TEST(TestIfScope);
   return result;
 }
