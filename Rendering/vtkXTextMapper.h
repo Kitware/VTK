d8e910fd05ee4b5fe6661377b9bc23a9498e6d94
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-08-22 15:43:42 -0400

    Revert back

--- Rendering/vtkXTextMapper.h
+++ Rendering/vtkXTextMapper.h
@@ -29,7 +29,12 @@
 
 #include "vtkTextMapper.h"
 
-class vtkXTextMapperInternal;
+#include        <X11/Xlib.h>
+#include        <X11/Xutil.h>
+#include        <X11/cursorfont.h>
+#include        <X11/X.h>
+#include        <X11/keysym.h>
+
 
 class VTK_RENDERING_EXPORT vtkXTextMapper : public vtkTextMapper
 {
@@ -49,13 +54,13 @@ public:
 
 protected:
   vtkXTextMapper();
-  ~vtkXTextMapper();
+  ~vtkXTextMapper() {};
 
   // Description:
   // Actually get the size of the rectangle.
   void DetermineSize(vtkViewport *viewport, int size[2]);
 
-  vtkXTextMapperInternal *Internal;
+  Font CurrentFont;
 
   // Size of the Text.
   vtkTimeStamp  SizeMTime;
