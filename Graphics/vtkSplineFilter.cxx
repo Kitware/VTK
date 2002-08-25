b4916f652b656f1ab37eaaabce0967dc9e9798b9
Will Schroeder <will.schroeder@kitware.com>
2002-08-25 15:22:31 -0400

    ERR:Removed warning

--- Graphics/vtkSplineFilter.cxx
+++ Graphics/vtkSplineFilter.cxx
@@ -21,7 +21,7 @@
 #include "vtkFloatArray.h"
 #include "vtkMath.h"
 
-vtkCxxRevisionMacro(vtkSplineFilter, "1.6");
+vtkCxxRevisionMacro(vtkSplineFilter, "1.7");
 vtkStandardNewMacro(vtkSplineFilter);
 
 vtkSplineFilter::vtkSplineFilter()
@@ -242,7 +242,7 @@ int vtkSplineFilter::GeneratePoints(vtkIdType offset, vtkIdType npts,
   // Now compute the new points
   numNewPts = numDivs + 1;
   vtkIdType idx;
-  float s, s0;
+  float s, s0=0.0;
   if ( genTCoords == VTK_TCOORDS_FROM_SCALARS )
     {
     s0=pd->GetScalars()->GetTuple1(pts[0]);
