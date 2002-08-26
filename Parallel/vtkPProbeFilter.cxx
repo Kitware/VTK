5dc76ffe85286a7f09ac0aaaedd49eba437d2174
Andy Cedilnik <andy.cedilnik@kitware.com>
2002-08-26 13:54:34 -0400

    Add parallel probe filter

---
+++ Parallel/vtkPProbeFilter.cxx
@@ -0,0 +1,130 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkPProbeFilter.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkPProbeFilter.h"
+
+#include "vtkMultiProcessController.h"
+#include "vtkObjectFactory.h"
+#include "vtkPolyData.h"
+
+vtkCxxRevisionMacro(vtkPProbeFilter, "1.1");
+vtkStandardNewMacro(vtkPProbeFilter);
+
+vtkCxxSetObjectMacro(vtkPProbeFilter, Controller, vtkMultiProcessController);
+
+//----------------------------------------------------------------------------
+vtkPProbeFilter::vtkPProbeFilter()
+{
+  this->Controller = 0;
+}
+
+//----------------------------------------------------------------------------
+vtkPProbeFilter::~vtkPProbeFilter()
+{
+  this->SetController(0);
+}
+
+//----------------------------------------------------------------------------
+void vtkPProbeFilter::ExecuteInformation()
+{
+  vtkDataSet *output = this->GetOutput();
+  output->SetMaximumNumberOfPieces(-1);
+}
+
+//----------------------------------------------------------------------------
+void vtkPProbeFilter::Execute()
+{
+  if ( !this->Controller )
+    {
+    vtkErrorMacro("Controller not set");
+    return;
+    }
+  vtkDataSet *output = this->GetOutput();
+  //vtkDataObject *input = this->GetInput();
+  //vtkDataObject *source = this->GetSource();
+  
+  this->vtkProbeFilter::Execute();
+  int procid = this->Controller->GetLocalProcessId();
+  int numProcs = this->Controller->GetNumberOfProcesses();
+  vtkIdType numPoints = this->GetValidPoints()->GetMaxId() + 1;
+  if ( procid )
+    {
+    // Satellite node
+    this->Controller->Send(&numPoints, 1, 0, 1970);
+    if ( numPoints > 0 )
+      {
+      this->Controller->Send(this->GetValidPoints(), 0, 1971);
+      this->Controller->Send(output, 0, 1972);      
+      }
+    output->ReleaseData();
+    }
+  else
+    {
+    vtkIdType numRemotePoints = 0;
+    vtkIdTypeArray *validPoints = vtkIdTypeArray::New();
+    vtkDataSet *remoteProbeOutput = output->NewInstance();
+    vtkPointData *remotePointData;
+    vtkPointData *pointData = output->GetPointData();
+    vtkIdType i;
+    vtkIdType j;
+    vtkIdType k;
+    vtkIdType pointId;
+    vtkIdType numComponents = pointData->GetNumberOfComponents();
+    float *tuple = new float[numComponents];
+    for (i = 1; i < numProcs; i++)
+      {
+      this->Controller->Receive(&numRemotePoints, 1, i, 1970);
+      if (numRemotePoints > 0)
+        {
+        this->Controller->Receive(validPoints, i, 1971);
+        this->Controller->Receive(remoteProbeOutput, i, 1972);
+      
+        remotePointData = remoteProbeOutput->GetPointData();
+        for (j = 0; j < numRemotePoints; j++)
+          {
+          pointId = validPoints->GetValue(j);
+        
+          remotePointData->GetTuple(pointId, tuple);
+        
+          for (k = 0; k < numComponents; k++)
+            {
+            output->GetPointData()->SetComponent(pointId, k, tuple[k]);
+            }
+          }
+        }
+      }
+    delete [] tuple;
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkPProbeFilter::ComputeInputUpdateExtents( vtkDataObject *output )
+{
+  vtkDataObject *input = this->GetInput();
+  vtkDataObject *source = this->GetSource();
+
+  input->SetUpdateExtent(0, 1, 0);
+  source->SetUpdateExtent(output->GetUpdatePiece(),
+                          output->GetUpdateNumberOfPieces(),
+                          output->GetUpdateGhostLevel());
+}
+
+//----------------------------------------------------------------------------
+void vtkPProbeFilter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
---
+++ Parallel/vtkPProbeFilter.h
@@ -0,0 +1,58 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkPProbeFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkPProbeFilter - probe dataset in parallel
+// .SECTION Description
+
+#ifndef __vtkPProbeFilter_h
+#define __vtkPProbeFilter_h
+
+#include "vtkProbeFilter.h"
+
+class vtkMultiProcessController;
+
+class VTK_PARALLEL_EXPORT vtkPProbeFilter : public vtkProbeFilter
+{
+public:
+  vtkTypeRevisionMacro(vtkPProbeFilter,vtkProbeFilter);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  static vtkPProbeFilter *New();
+
+  // Description:
+  // Set and get the controller.
+  virtual void SetController(vtkMultiProcessController*);
+  vtkGetObjectMacro(Controller, vtkMultiProcessController);
+
+protected:
+  vtkPProbeFilter();
+  ~vtkPProbeFilter();
+
+  // Usual data generation method
+  virtual void Execute();
+  virtual void ExecuteInformation();
+  virtual void ComputeInputUpdateExtents(vtkDataObject *output);
+
+  vtkMultiProcessController* Controller;
+
+private:
+  vtkPProbeFilter(const vtkPProbeFilter&);  // Not implemented.
+  void operator=(const vtkPProbeFilter&);  // Not implemented.
+};
+
+#endif
