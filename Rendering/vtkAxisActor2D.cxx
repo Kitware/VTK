60f795cc093c24bb08420836b09758bf386195c5
Sebastien Barre <sebastien.barre@kitware.com>
2002-09-27 08:52:25 -0400

    FIX: warnings

--- Rendering/vtkAxisActor2D.cxx
+++ Rendering/vtkAxisActor2D.cxx
@@ -24,7 +24,7 @@
 #include "vtkTextProperty.h"
 #include "vtkViewport.h"
 
-vtkCxxRevisionMacro(vtkAxisActor2D, "1.29.4.1");
+vtkCxxRevisionMacro(vtkAxisActor2D, "1.29.4.2");
 vtkStandardNewMacro(vtkAxisActor2D);
 
 vtkCxxSetObjectMacro(vtkAxisActor2D,LabelTextProperty,vtkTextProperty);
@@ -99,7 +99,7 @@ vtkAxisActor2D::vtkAxisActor2D()
   this->LastPosition2[0] = this->LastPosition2[1] = 0;
 
   this->LastSize[0] = this->LastSize[1] = 0;
-  this->LastMaxLabelSize[0] = this->LastMaxLabelSize[1] = 0.0;
+  this->LastMaxLabelSize[0] = this->LastMaxLabelSize[1] = 0;
 }
 
 //----------------------------------------------------------------------------
