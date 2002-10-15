1c22f279033d47c02548bdd7d75942e8850b5266
Brad King <brad.king@kitware.com>
2002-10-15 10:59:26 -0400

    ENH: Adding vtkDataArraySelection class.  Moving from ParaView/Packages directory.

---
+++ Common/vtkDataArraySelection.cxx
@@ -0,0 +1,250 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkDataArraySelection.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkDataArraySelection.h"
+#include "vtkObjectFactory.h"
+#include "vtkVector.txx"
+
+vtkCxxRevisionMacro(vtkDataArraySelection, "1.1");
+vtkStandardNewMacro(vtkDataArraySelection);
+
+//----------------------------------------------------------------------------
+vtkDataArraySelection::vtkDataArraySelection()
+{
+  this->ArrayNames = ArrayNamesType::New();
+  this->ArraySettings = ArraySettingsType::New();
+}
+
+//----------------------------------------------------------------------------
+vtkDataArraySelection::~vtkDataArraySelection()
+{
+  this->ArraySettings->Delete();
+  this->ArrayNames->Delete();
+}
+
+//----------------------------------------------------------------------------
+void vtkDataArraySelection::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+  os << indent << "Number of Arrays: " << this->GetNumberOfArrays() << "\n";
+}
+
+//----------------------------------------------------------------------------
+void vtkDataArraySelection::EnableArray(const char* name)
+{
+  vtkIdType pos=0;
+  if(this->ArrayNames->FindItem(name, pos) == VTK_OK)
+    {
+    int value = 0;
+    this->ArraySettings->GetItemNoCheck(pos, value);
+    if(!value)
+      {
+      this->ArraySettings->SetItemNoCheck(pos, 1);
+      this->Modified();
+      }
+    return;
+    }
+  this->ArrayNames->AppendItem(name);
+  this->ArraySettings->AppendItem(1);
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+void vtkDataArraySelection::DisableArray(const char* name)
+{
+  vtkIdType pos=0;
+  if(this->ArrayNames->FindItem(name, pos) == VTK_OK)
+    {
+    int value = 0;
+    this->ArraySettings->GetItemNoCheck(pos, value);
+    if(value)
+      {
+      this->ArraySettings->SetItemNoCheck(pos, 0);
+      this->Modified();
+      }
+    return;
+    }
+  this->ArrayNames->AppendItem(name);
+  this->ArraySettings->AppendItem(0);
+  this->Modified();
+}
+
+//----------------------------------------------------------------------------
+int vtkDataArraySelection::ArrayIsEnabled(const char* name)
+{
+  // Check if there is a specific entry for this array.
+  vtkIdType pos=0;
+  int result=0;
+  if((this->ArrayNames->FindItem(name, pos) == VTK_OK) &&
+     (this->ArraySettings->GetItem(pos, result) == VTK_OK))
+    {
+    return result;
+    }
+  
+  // The array does not have an entry.  Assume it is disabled.
+  return 0;
+}
+
+//----------------------------------------------------------------------------
+void vtkDataArraySelection::EnableAllArrays()
+{
+  int modified = 0;
+  vtkIdType i;
+  for(i=0;i < this->ArraySettings->GetNumberOfItems();++i)
+    {
+    int value = 0;
+    this->ArraySettings->GetItemNoCheck(i, value);
+    if(!value)
+      {
+      this->ArraySettings->SetItemNoCheck(i, 1);
+      modified = 1;
+      }
+    }
+  if(modified)
+    {
+    this->Modified();
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkDataArraySelection::DisableAllArrays()
+{
+  int modified = 0;
+  vtkIdType i;
+  for(i=0;i < this->ArraySettings->GetNumberOfItems();++i)
+    {
+    int value = 0;
+    this->ArraySettings->GetItemNoCheck(i, value);
+    if(value)
+      {
+      this->ArraySettings->SetItemNoCheck(i, 0);
+      modified = 1;
+      }
+    }
+  if(modified)
+    {
+    this->Modified();
+    }
+}
+
+//----------------------------------------------------------------------------
+int vtkDataArraySelection::GetNumberOfArrays()
+{
+  return this->ArrayNames->GetNumberOfItems();
+}
+
+//----------------------------------------------------------------------------
+const char* vtkDataArraySelection::GetArrayName(int index)
+{
+  const char* n = 0;
+  if(this->ArrayNames->GetItem(index, n) == VTK_OK)
+    {
+    return n;
+    }
+  return 0;
+}
+
+//----------------------------------------------------------------------------
+int vtkDataArraySelection::GetArraySetting(int index)
+{
+  int n = 0;
+  if(this->ArraySettings->GetItem(index, n) == VTK_OK)
+    {
+    return n;
+    }
+  return 0;
+}
+
+//----------------------------------------------------------------------------
+void vtkDataArraySelection::RemoveAllArrays()
+{
+  if(this->GetNumberOfArrays() > 0)
+    {
+    this->ArrayNames->RemoveAllItems();
+    this->ArraySettings->RemoveAllItems();
+    this->Modified();
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkDataArraySelection::SetArrays(const char* const* names, int numArrays)
+{
+  // This function is called only by the filter owning the selection.
+  // It should not call Modified() because array settings are not
+  // changed.
+  
+  // Create a new map for this set of arrays.
+  ArrayNamesType* newNames = ArrayNamesType::New();
+  ArraySettingsType* newSettings = ArraySettingsType::New();
+  
+  // Allocate.
+  newNames->SetSize(numArrays);
+  newSettings->SetSize(numArrays);
+  newNames->ResizeOn();
+  newSettings->ResizeOn();
+  
+  // Fill with settings for all arrays.
+  int i;
+  for(i=0;i < numArrays; ++i)
+    {
+    // Add this array.
+    newNames->AppendItem(names[i]);
+    
+    // Fill in the setting.
+    vtkIdType pos=0;
+    int result=0;
+    if((this->ArrayNames->FindItem(names[i], pos) == VTK_OK) &&
+       (this->ArraySettings->GetItem(pos, result) == VTK_OK))
+      {
+      // Copy the old setting for this array.
+      newSettings->AppendItem(result);
+      }
+    else
+      {
+      // No setting existed, default to on.
+      newSettings->AppendItem(1);
+      }
+    }
+  
+  // Delete the old map and save the new one.
+  this->ArrayNames->Delete();
+  this->ArraySettings->Delete();
+  this->ArrayNames = newNames;
+  this->ArraySettings = newSettings;
+}
+
+//----------------------------------------------------------------------------
+void vtkDataArraySelection::CopySelections(vtkDataArraySelection* selections)
+{
+  if(this == selections)
+    {
+    return;
+    }
+  this->RemoveAllArrays();
+  
+  const char* name=0;
+  int setting=0;
+  vtkIdType i;
+  for(i=0;i < selections->ArrayNames->GetNumberOfItems();++i)
+    {
+    selections->ArrayNames->GetItemNoCheck(i, name);
+    selections->ArraySettings->GetItemNoCheck(i, setting);
+    this->ArrayNames->AppendItem(name);
+    this->ArraySettings->AppendItem(setting);
+    }
+  this->Modified();
+}
---
+++ Common/vtkDataArraySelection.h
@@ -0,0 +1,113 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkDataArraySelection.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkDataArraySelection - Store on/off settings for data arrays for a vtkSource.
+// .SECTION Description
+// vtkDataArraySelection can be used by vtkSource subclasses to store
+// on/off settings for whether each vtkDataArray in its input should
+// be passed in the source's output.  This is primarily intended to
+// allow file readers to configure what data arrays are read from the
+// file.
+
+#ifndef __vtkDataArraySelection_h
+#define __vtkDataArraySelection_h
+
+#include "vtkObject.h"
+
+//BTX
+template <class DType> class vtkVector;
+//ETX
+
+class VTK_COMMON_EXPORT vtkDataArraySelection : public vtkObject
+{
+public:
+  vtkTypeRevisionMacro(vtkDataArraySelection,vtkObject);
+  void PrintSelf(ostream& os, vtkIndent indent);
+  static vtkDataArraySelection* New();
+  
+  // Description:
+  // Enable the array with the given name.  Creates a new entry if
+  // none exists.
+  void EnableArray(const char* name);
+  
+  // Description:
+  // Disable the array with the given name.  Creates a new entry if
+  // none exists.
+  void DisableArray(const char* name);
+  
+  // Description:
+  // Return whether the array with the given name is enabled.  If
+  // there is no entry, the array is assumed to be disabled.
+  int ArrayIsEnabled(const char* name);
+  
+  // Description:
+  // Enable all arrays that currently have an entry.
+  void EnableAllArrays();
+  
+  // Description:
+  // Disable all arrays that currently have an entry.
+  void DisableAllArrays();
+  
+  // Description:
+  // Get the number of arrays that currently have an entry.
+  int GetNumberOfArrays();
+  
+  // Description:
+  // Get the name of the array entry at the given index.
+  const char* GetArrayName(int index);
+  
+  // Description:
+  // Get whether the array at the given index is enabled.
+  int GetArraySetting(int index);
+  
+  // Description:
+  // Remove all array entries.
+  void RemoveAllArrays();
+  
+  //BTX
+  // Description:
+  // Set the list of arrays that have entries.  For arrays that
+  // already have entries, the settings are copied.  For arrays that
+  // don't already have an entry, they are assumed to be enabled.
+  // There will be no more entries than the names given.
+  void SetArrays(const char* const* names, int numArrays);
+  //ETX
+  
+  // Description:
+  // Copy the selections from the given vtkDataArraySelection instance.
+  void CopySelections(vtkDataArraySelection* selections);
+protected:
+  vtkDataArraySelection();
+  ~vtkDataArraySelection();
+  
+  //BTX
+  typedef vtkVector<const char*> ArrayNamesType;
+  typedef vtkVector<int> ArraySettingsType;
+  //ETX
+  
+  // The list of array names.
+  ArrayNamesType* ArrayNames;
+  
+  // The list of array settings.
+  ArraySettingsType* ArraySettings;
+  
+private:
+  vtkDataArraySelection(const vtkDataArraySelection&);  // Not implemented.
+  void operator=(const vtkDataArraySelection&);  // Not implemented.
+};
+
+#endif
