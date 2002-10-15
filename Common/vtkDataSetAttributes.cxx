eb2fc93778bcd2e47eaeef0bf7d4327416b14568
Charles Law <charles.law@kitware.com>
2002-10-15 08:11:38 -0400

    When DataSetAttributes initializes arrays, it has to also clear the attribute indexes.  GetScalars (CopyAllocate) caused infrequent dereferencing of ->Data which was NULL.

--- Common/vtkDataSetAttributes.cxx
+++ Common/vtkDataSetAttributes.cxx
@@ -33,7 +33,7 @@
 #include "vtkIdTypeArray.h"
 #include "vtkObjectFactory.h"
 
-vtkCxxRevisionMacro(vtkDataSetAttributes, "1.63");
+vtkCxxRevisionMacro(vtkDataSetAttributes, "1.64");
 vtkStandardNewMacro(vtkDataSetAttributes);
 
 //--------------------------------------------------------------------------
@@ -182,6 +182,10 @@ void vtkDataSetAttributes::ShallowCopy(vtkFieldData *fd)
 void vtkDataSetAttributes::InitializeFields()
 {
   this->vtkFieldData::InitializeFields();
+  for(int attributeType=0; attributeType<NUM_ATTRIBUTES; attributeType++)
+    {
+    this->AttributeIndices[attributeType] = -1;
+    }
 }
 
 // Initialize all of the object's data to NULL
@@ -1546,7 +1550,7 @@ int vtkDataSetAttributes::CheckNumberOfComponents(vtkDataArray* da,
 vtkDataArray* vtkDataSetAttributes::GetAttribute(int attributeType)
 {
   int index = this->AttributeIndices[attributeType];
-  if (index == -1 || this->Data == NULL)
+  if (index == -1)
     {
     return 0;
     }
