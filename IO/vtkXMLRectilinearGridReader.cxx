282c75a2cd4c7b053298bcfbf0b19cb3062a41a2
Brad King <brad.king@kitware.com>
2002-11-22 15:49:50 -0500

    BUG: Need to use CreateDataArray for coordinate arrays instead of assuming vtkFloatArray.

--- IO/vtkXMLRectilinearGridReader.cxx
+++ IO/vtkXMLRectilinearGridReader.cxx
@@ -16,13 +16,14 @@
 
 =========================================================================*/
 #include "vtkXMLRectilinearGridReader.h"
+
+#include "vtkFloatArray.h"
 #include "vtkObjectFactory.h"
+#include "vtkRectilinearGrid.h"
 #include "vtkXMLDataElement.h"
 #include "vtkXMLDataParser.h"
-#include "vtkFloatArray.h"
-#include "vtkRectilinearGrid.h"
 
-vtkCxxRevisionMacro(vtkXMLRectilinearGridReader, "1.1");
+vtkCxxRevisionMacro(vtkXMLRectilinearGridReader, "1.2");
 vtkStandardNewMacro(vtkXMLRectilinearGridReader);
 
 //----------------------------------------------------------------------------
@@ -130,10 +131,15 @@ void vtkXMLRectilinearGridReader::SetupOutputInformation()
   this->Superclass::SetupOutputInformation();  
   vtkRectilinearGrid* output = this->GetOutput();
   
+  // Use the configuration of the first piece since all are the same.
+  vtkXMLDataElement* xc = this->CoordinateElements[0]->GetNestedElement(0);
+  vtkXMLDataElement* yc = this->CoordinateElements[0]->GetNestedElement(1);
+  vtkXMLDataElement* zc = this->CoordinateElements[0]->GetNestedElement(2);
+  
   // Create the coordinate arrays.
-  vtkDataArray* x = vtkFloatArray::New();
-  vtkDataArray* y = vtkFloatArray::New();
-  vtkDataArray* z = vtkFloatArray::New();
+  vtkDataArray* x = this->CreateDataArray(xc);
+  vtkDataArray* y = this->CreateDataArray(yc);
+  vtkDataArray* z = this->CreateDataArray(zc);
   output->SetXCoordinates(x);
   output->SetYCoordinates(y);
   output->SetZCoordinates(z);
