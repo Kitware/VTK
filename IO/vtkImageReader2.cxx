a2a3697ed86f5a1b99498b3d76c9fb745ab3cf18
John Biddiscombe <jbiddiscombe@skippingmouse.co.uk>
2002-11-25 06:17:08 -0500

    Replace Open + seek + Tellg with stat() when determining header size

--- IO/vtkImageReader2.cxx
+++ IO/vtkImageReader2.cxx
@@ -6,12 +6,12 @@
   Date:      $Date$
   Version:   $Revision$
 
-  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
   See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
 
-     This software is distributed WITHOUT ANY WARRANTY; without even 
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
      PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
@@ -23,7 +23,7 @@
 
 #include <sys/stat.h>
 
-vtkCxxRevisionMacro(vtkImageReader2, "1.21");
+vtkCxxRevisionMacro(vtkImageReader2, "1.22");
 vtkStandardNewMacro(vtkImageReader2);
 
 #ifdef read
@@ -550,15 +550,11 @@ unsigned long vtkImageReader2::GetHeaderSize(unsigned long idx)
 
     // make sure we figure out a filename to open
     this->ComputeInternalFileName(idx);
-    if ( !this->OpenFile() )
-      {
-      return 0;
-      }
-    
-    // Get the size of the header from the size of the image
-    this->File->seekg(0,ios::end);
-    
-    return (int)(this->File->tellg() - 
+
+    struct stat statbuf;
+    stat(this->InternalFileName, &statbuf);
+
+    return (int)(statbuf.st_size -
       (long)this->DataIncrements[this->GetFileDimensionality()]);
     }
   
