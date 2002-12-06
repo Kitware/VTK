ee977ccdcb3a09392c6df930cf16c5226baa1e8e
Brad King <brad.king@kitware.com>
2002-12-05 21:34:16 -0500

    ENH: Added test for vtkXMLDataSetWriter and vtkXMLPDataSetWriter.

--- IO/Testing/Tcl/TestEmptyXML.tcl
+++ IO/Testing/Tcl/TestEmptyXML.tcl
@@ -50,13 +50,44 @@ foreach pair $types {
     preader Delete
 }
 
+# Test the data set writers.
+foreach pair $types {
+    set type [lindex $pair 0]
+    set ext [lindex $pair 1]
+    vtkXMLDataSetWriter writer
+    vtkXMLPDataSetWriter pwriter
+    vtk${type} input
+
+    writer SetFileName "empty${type}DataSet.${ext}"
+    puts "Attempting DataSet ${type} write with no input."
+    catch {writer Write}
+    puts "Attempting DataSet ${type} write with empty input."
+    writer SetInput input
+    writer Write
+
+    pwriter SetFileName "emptyP${type}DataSet.p${ext}"
+    pwriter SetNumberOfPieces 1
+    puts "Attempting DataSet P${type} write with no input."
+    catch {pwriter Write}
+    puts "Attempting DataSet P${type} write with empty input."
+    pwriter SetInput input
+    pwriter Write
+
+    input Delete
+    pwriter Delete
+    writer Delete
+}
+
 # Delete the test files.
 foreach pair $types {
     set type [lindex $pair 0]
     set ext [lindex $pair 1]
     file delete -force "empty${type}.${ext}"
+    file delete -force "empty${type}DataSet.${ext}"
     file delete -force "emptyP${type}.p${ext}"
     file delete -force "emptyP${type}0.${ext}"
+    file delete -force "emptyP${type}DataSet.p${ext}"
+    file delete -force "emptyP${type}DataSet0.${ext}"
 }
 
 exit
