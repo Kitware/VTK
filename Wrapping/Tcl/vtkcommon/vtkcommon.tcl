e8d29a1d0b7d40343519f6277d5b73b410a4188c
Brad King <brad.king@kitware.com>
2002-12-20 15:39:50 -0500

    ENH: Deprecated support for pointing TCLLIBPATH to the source tree.

--- Wrapping/Tcl/vtkcommon/vtkcommon.tcl
+++ Wrapping/Tcl/vtkcommon/vtkcommon.tcl
@@ -5,6 +5,14 @@ if {[info commands ::vtk::init::require_package] != ""} {
     package provide vtkcommon 4.1
   }
 } else {
+  puts stderr "Warning: Your TCLLIBPATH points to the VTK source tree. \
+ Support for this is deprecated in VTK 4.2, and will be removed in a\
+ future version.  Please point TCLLIBPATH to\
+ {VTK_BINARY_DIR}/Wrapping/Tcl<config>, where <config>\
+ is the build configuration.  The <config> value is empty for most\
+ platforms, and /Debug, /Release, etc. for Visual Studio builds. \
+ After this is done, you no longer need to set PATH or LD_LIBRARY_PATH\
+ to point to the VTK build tree."
   if {[info commands vtkObject] != "" ||
     [::vtk::load_component vtkCommonTCL] == ""} {
     package provide vtkcommon 4.1
