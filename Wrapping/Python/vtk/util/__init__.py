e70640cd66d7d19046eb945e5dbe8bc1f96677bf
Prabhu Ramachandran <prabhu_r@users.sourceforge.net>
2002-12-21 14:17:35 -0500

    ENH: Updated to add colors.py.

--- Wrapping/Python/vtk/util/__init__.py
+++ Wrapping/Python/vtk/util/__init__.py
@@ -1,4 +1,4 @@
 """Utility modules for the VTK-Python wrappers."""
 
-__all__ = ['misc', 'vtkConstants', 'vtkImageExportToArray',
+__all__ = ['colors', 'misc', 'vtkConstants', 'vtkImageExportToArray',
            'vtkImageImportFromArray', 'vtkMethodParser']
