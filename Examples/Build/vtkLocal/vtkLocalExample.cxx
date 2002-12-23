517d4be712f891fae79f2c1b1fecc2fcd75df62e
Brad King <brad.king@kitware.com>
2002-12-23 10:56:48 -0500

    ENH: Initial checkin of simple project example to add a vtkLocal toolkit.

---
+++ Examples/Build/vtkLocal/vtkLocalExample.cxx
@@ -0,0 +1,24 @@
+/*=========================================================================
+This source has no copyright.  It is intended to be copied by users
+wishing to create their own VTK classes locally.
+=========================================================================*/
+#include "vtkLocalExample.h"
+#include "vtkObjectFactory.h"
+
+vtkStandardNewMacro(vtkLocalExample);
+
+//----------------------------------------------------------------------------
+vtkLocalExample::vtkLocalExample()
+{
+}
+
+//----------------------------------------------------------------------------
+vtkLocalExample::~vtkLocalExample()
+{
+}
+
+//----------------------------------------------------------------------------
+void vtkLocalExample::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
---
+++ Examples/Build/vtkLocal/vtkLocalExample.h
@@ -0,0 +1,32 @@
+/*=========================================================================
+This source has no copyright.  It is intended to be copied by users
+wishing to create their own VTK classes locally.
+=========================================================================*/
+// .NAME vtkLocalExample - Example class using VTK.
+// .SECTION Description
+// vtkLocalExample is a simple class that uses VTK.  This class can be
+// copied and modified to produce your own classes.
+
+#ifndef __vtkLocalExample_h
+#define __vtkLocalExample_h
+
+#include "vtkLocalConfigure.h" // Include configuration header.
+#include "vtkObject.h"
+
+class VTK_vtkLocal_EXPORT vtkLocalExample : public vtkObject
+{
+public:
+  static vtkLocalExample* New();
+  vtkTypeMacro(vtkLocalExample, vtkObject);
+  void PrintSelf(ostream& os, vtkIndent indent);
+  
+protected:
+  vtkLocalExample();
+  ~vtkLocalExample();
+  
+private:
+  vtkLocalExample(const vtkLocalExample&);  // Not implemented.
+  void operator=(const vtkLocalExample&);  // Not implemented.
+};
+
+#endif
