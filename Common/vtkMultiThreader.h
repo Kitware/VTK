7570b72b18128d7f71cd36e6b9be9cd6b693d569
Bill Lorensen <lorensen@crd.ge.com>
2002-12-28 08:16:07 -0500

    ERR: comments to justify system includes.

--- Common/vtkMultiThreader.h
+++ Common/vtkMultiThreader.h
@@ -28,13 +28,13 @@
 #include "vtkObject.h"
 
 #ifdef VTK_USE_SPROC
-#include <sys/types.h>
-#include <unistd.h>
+#include <sys/types.h> // Needed for unix implementation of sproc
+#include <unistd.h> // Needed for unix implementation of sproc
 #endif
 
 #ifdef VTK_USE_PTHREADS
-#include <sys/types.h>
-#include <unistd.h>
+#include <sys/types.h> // Needed for unix implementation of pthreads
+#include <unistd.h> // Needed for unix implementation of pthreads
 #endif
 
 // If VTK_USE_SPROC is defined, then sproc() will be used to create
