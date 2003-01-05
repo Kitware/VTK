3300a5bec71d19b8fa5093836e2675a03446f632
Yves Starreveld <ystarrev@uwo.ca>
2003-01-05 12:56:02 -0500

    ENH: add __ppc__ to the ifdef to use the fast floor function

--- Imaging/vtkImageReslice.cxx
+++ Imaging/vtkImageReslice.cxx
@@ -27,7 +27,7 @@
 #include <float.h>
 #include <math.h>
 
-vtkCxxRevisionMacro(vtkImageReslice, "1.32");
+vtkCxxRevisionMacro(vtkImageReslice, "1.33");
 vtkStandardNewMacro(vtkImageReslice);
 vtkCxxSetObjectMacro(vtkImageReslice, InformationInput, vtkImageData);
 vtkCxxSetObjectMacro(vtkImageReslice,ResliceAxes,vtkMatrix4x4);
@@ -742,7 +742,7 @@ void vtkImageReslice::ExecuteInformation(vtkImageData *input,
 // and is used a lot in this code, optimize for different CPU architectures
 inline int vtkResliceFloor(double x)
 {
-#if defined mips || defined sparc
+#if defined mips || defined sparc || defined __ppc__
   return (int)((unsigned int)(x + 2147483648.0) - 2147483648U);
 #elif defined i386 || defined _M_IX86
   unsigned int hilo[2];
