725f7388ef51995fac9a4b09252815c4350135a5
Yves Starreveld <ystarrev@uwo.ca>
2003-01-06 09:20:23 -0500

    ENH: file no longer needed on OSX

--- Patented/vtkOSXPatentedTclInit.c
+++
@@ -1,8 +0,0 @@
-//-----  This hack needed to compile using gcc3 on OSX until new stdc++.dylib
-#include <stdio.h>
-void oft_initPat() 
-{
-  extern void _ZNSt8ios_base4InitC4Ev();
-  _ZNSt8ios_base4InitC4Ev();
-}
-
