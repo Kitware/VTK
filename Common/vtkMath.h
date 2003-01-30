f832be34c3d7d1855ce230f0e65139a96a04dc49
Bill Hoffman <bill.hoffman@kitware.com>
2003-01-30 12:41:20 -0500

    BUG: remove static template function

--- Common/vtkMath.h
+++ Common/vtkMath.h
@@ -563,7 +563,7 @@ inline void vtkMath::Cross(const double x[3], const double y[3], double z[3])
 //BTX
 //----------------------------------------------------------------------------
 template<class T>
-static inline double vtkDeterminant3x3(T A[3][3])
+inline double vtkDeterminant3x3(T A[3][3])
 {
   return A[0][0]*A[1][1]*A[2][2] + A[1][0]*A[2][1]*A[0][2] + 
          A[2][0]*A[0][1]*A[1][2] - A[0][0]*A[2][1]*A[1][2] - 
