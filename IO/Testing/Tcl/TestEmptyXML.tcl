883362d41e57d097909ffe4b6a8a79529c55d420
Brad King <brad.king@kitware.com>
2003-01-31 17:19:41 -0500

    ENH: Added test reading of junk and empty files.

--- IO/Testing/Tcl/TestEmptyXML.tcl
+++ IO/Testing/Tcl/TestEmptyXML.tcl
@@ -9,6 +9,15 @@ set types {
     {UnstructuredGrid vtu}
 }
 
+# Prepare some test files.
+file delete -force "junkFile.vtk"
+file delete -force "emptyFile.vtk"
+set f [open "emptyFile.vtk" w]
+close $f
+set f [open "junkFile.vtk" w]
+puts $f "v9np7598mapwcawoiur-,rjpmW9MJV28nun-q38ynq-9 8ugujqvt-8n3-nv8"
+close $f
+
 # Test each writer/reader.
 foreach pair $types {
     set type [lindex $pair 0]
@@ -39,6 +48,22 @@ foreach pair $types {
     vtkXMLP${type}Reader preader
     preader SetFileName "emptyP${type}.p${ext}"
     puts "Attempting read from file with empty P${type}."    
+    preader Update
+
+    reader SetFileName "emptyFile.vtk"
+    preader SetFileName "emptyFile.vtk"
+
+    puts "Attempting read ${type} from empty file."
+    reader Update
+    puts "Attempting read P${type} from empty file."
+    preader Update
+
+    reader SetFileName "junkFile.vtk"
+    preader SetFileName "junkFile.vtk"
+
+    puts "Attempting read ${type} from junk file."
+    reader Update
+    puts "Attempting read P${type} from junk file."
     preader Update
     
     input Delete
@@ -87,5 +112,7 @@ foreach pair $types {
     file delete -force "emptyP${type}DataSet.p${ext}"
     file delete -force "emptyP${type}DataSet0.${ext}"
 }
+file delete -force "junkFile.vtk"
+file delete -force "emptyFile.vtk"
 
 exit
