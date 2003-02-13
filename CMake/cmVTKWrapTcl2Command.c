73b27ad7a6a80872352b21497d1577a3ad4114c0
Ken Martin <ken.martin@kitware.com>
2003-02-13 10:18:57 -0500

    some inproved packaging and tcl84 compatability

--- CMake/cmVTKWrapTcl2Command.c
+++ CMake/cmVTKWrapTcl2Command.c
@@ -46,15 +46,21 @@ static void CreateInitFile(cmLoadedCommandInfo *info,
     }
   
   fprintf(fout,"#include \"vtkTclUtil.h\"\n");
+  fprintf(fout,"#include \"vtkVersion.h\"\n");
   fprintf(fout,"#define VTK_TCL_TO_STRING(x) VTK_TCL_TO_STRING0(x)\n");
   fprintf(fout,"#define VTK_TCL_TO_STRING0(x) #x\n");
+
   fprintf(fout,
           "extern \"C\"\n"
           "{\n"
+          "#if (TCL_MAJOR_VERSION == 8) && (TCL_MINOR_VERSION >= 4) && (TCL_RELEASE_LEVEL >= TCL_FINAL_RELEASE)\n"
+          "  typedef int (*vtkTclCommandType)(ClientData, Tcl_Interp *,int, CONST84 char *[]);\n"
+          "#else\n"
           "  typedef int (*vtkTclCommandType)(ClientData, Tcl_Interp *,int, char *[]);\n"
+          "#endif\n"
           "}\n"
           "\n");
-  
+
   for (i = 0; i < numConcrete; i++)
     {
     fprintf(fout,"int %sCommand(ClientData cd, Tcl_Interp *interp,\n             int argc, char *argv[]);\n",concrete[i]);
