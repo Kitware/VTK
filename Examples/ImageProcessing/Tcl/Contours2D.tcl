dfb5dde5409b249e1cb5f35b84dd1ef8737faa3a
Ken Martin <ken.martin@kitware.com>
2003-02-14 11:30:03 -0500

    will added more comments

--- Examples/ImageProcessing/Tcl/Contours2D.tcl
+++ Examples/ImageProcessing/Tcl/Contours2D.tcl
@@ -1,24 +1,37 @@
+# This example shows how to sample a mathematical function over a
+# volume. A slice from the volume is then extracted and then contoured
+# to produce 2D contour lines.
+#
 package require vtk
 package require vtkinteraction
 
-# Quadric definition
+# Quadric definition. This is a type of implicit function. Here the 
+# coefficients to the equations are set.
 vtkQuadric quadric
   quadric SetCoefficients .5 1 .2 0 .1 0 0 .2 0 0
 
+# The vtkSampleFunction uses the quadric function and evaluates function
+# value over a regular lattice (i.e., a volume).
 vtkSampleFunction sample
   sample SetSampleDimensions 30 30 30
   sample SetImplicitFunction quadric
   sample ComputeNormalsOff
 
+# Here a single slice (i.e., image) is extracted from the volume. (Note: in
+# actuality the VOI request causes the sample function to operate on just the
+# slice.)
 vtkExtractVOI extract
   extract SetInput [sample GetOutput]
   extract SetVOI 0 29 0 29 15 15
   extract SetSampleRate 1 2 3
 
+# The image is contoured to produce contour lines. Thirteen contour values
+# ranging from (0,1.2) inclusive are produced.
 vtkContourFilter contours
   contours SetInput [extract GetOutput]
   contours GenerateValues 13 0.0 1.2
 
+# The contour lines are mapped to the graphics library.
 vtkPolyDataMapper contMapper
   contMapper SetInput [contours GetOutput]
   contMapper SetScalarRange 0.0 1.2
@@ -26,7 +39,7 @@ vtkPolyDataMapper contMapper
 vtkActor contActor
   contActor SetMapper contMapper
 
-# Create outline
+# Create outline an outline of the sampled data.
 vtkOutlineFilter outline
   outline SetInput [sample GetOutput]
 
@@ -37,19 +50,24 @@ vtkActor outlineActor
   outlineActor SetMapper outlineMapper
   eval [outlineActor GetProperty] SetColor 0 0 0
 
-# create graphics objects
+# Create the renderer, render window, and interactor.
 vtkRenderer ren1
 vtkRenderWindow renWin
     renWin AddRenderer ren1
 vtkRenderWindowInteractor iren
     iren SetRenderWindow renWin
 
+# Set the background color to white. Associate the actors with the
+# renderer.
 ren1 SetBackground 1 1 1
 ren1 AddActor contActor
 ren1 AddActor outlineActor
 
+# Zoom in a little bit. Associate the Tk interactor popup with a user
+# keypress-u (the UserEvent).
 [ren1 GetActiveCamera] Zoom 1.5
-#iren AddObserver UserEvent {wm deiconify .vtkInteract}
+iren AddObserver UserEvent {wm deiconify .vtkInteract}
 iren Initialize;
 
+# Don't show the root Tk window "."
 wm withdraw .
