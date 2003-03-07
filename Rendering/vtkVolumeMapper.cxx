29677e73f6a04726fae3e1b9ff394e4bb1c9ce1c
Sebastien Barre <sebastien.barre@kitware.com>
2003-03-07 10:55:28 -0500

    ENH: add UseImageClipper ivar to enable/disable the internal (evil) ImageClip.

--- Rendering/vtkVolumeMapper.cxx
+++ Rendering/vtkVolumeMapper.cxx
@@ -20,7 +20,7 @@
 #include "vtkImageClip.h"
 #include "vtkImageData.h"
 
-vtkCxxRevisionMacro(vtkVolumeMapper, "1.44");
+vtkCxxRevisionMacro(vtkVolumeMapper, "1.45");
 
 // Construct a vtkVolumeMapper with empty scalar input and clipping off.
 vtkVolumeMapper::vtkVolumeMapper()
@@ -39,7 +39,8 @@ vtkVolumeMapper::vtkVolumeMapper()
   this->Bounds[1] = this->Bounds[3] = this->Bounds[5] = 1.0;
   this->Center[0] = this->Center[1] = this->Center[2] = 0.0;
   this->CroppingRegionFlags = VTK_CROP_SUBVOLUME;
-  
+
+  this->UseImageClipper = 1;
   this->ImageClipper = vtkImageClip::New();
   this->ImageClipper->ClipDataOn();
   
@@ -104,10 +105,40 @@ float *vtkVolumeMapper::GetBounds()
     }
 }
 
+void vtkVolumeMapper::SetUseImageClipper(int arg)
+{
+  if (this->UseImageClipper == arg)
+    {
+    return;
+    }
+
+  this->UseImageClipper = arg;
+  this->Modified();
+
+  // Force a change of the input to reconnect the pipeline correctly
+
+  vtkImageData *input = this->GetInput();
+  this->SetInput(NULL);
+  this->SetInput(input);
+}
+
 void vtkVolumeMapper::SetInput( vtkImageData *input )
 {
-  this->ImageClipper->SetInput( input );
-  this->vtkProcessObject::SetNthInput(0, this->ImageClipper->GetOutput() );
+  if (this->UseImageClipper)
+    {
+    this->ImageClipper->SetInput(input);
+    /*
+      if (input)
+      {
+      this->ImageClipper->ResetOutputWholeExtent();
+      }
+    */
+    this->vtkProcessObject::SetNthInput(0, this->ImageClipper->GetOutput());
+    }
+  else
+    {
+    this->vtkProcessObject::SetNthInput(0, input);
+    }
 }
 
 vtkImageData *vtkVolumeMapper::GetInput()
@@ -158,5 +189,8 @@ void vtkVolumeMapper::PrintSelf(ostream& os, vtkIndent indent)
   // Don't print this->VoxelCroppingRegionPlanes
   
   os << indent << "Build Time: " <<this->BuildTime.GetMTime() << "\n";
+
+  os << indent << "UseImageClipper: " 
+     << (this->UseImageClipper ? "On\n" : "Off\n");
 }
 
