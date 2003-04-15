2cddfd9c8ab2e764d431b2734b55ae955e2fdcbc
Bill Lorensen <lorensen@crd.ge.com>
2003-04-15 09:52:15 -0400

    ENH: new class to compare to images.

---
+++ Imaging/Testing/Tcl/TestWipe.tcl
@@ -0,0 +1,66 @@
+package require vtk
+package require vtkinteraction
+
+# Image pipeline
+
+vtkRenderWindow imgWin
+
+vtkImageCanvasSource2D image1
+  image1 SetNumberOfScalarComponents 3
+  image1 SetScalarTypeToUnsignedChar
+  image1 SetExtent 0 79 0 79 0 0
+  image1 SetDrawColor 255 255 0
+  image1 FillBox 0 79 0 79
+
+vtkImageCanvasSource2D image2
+  image2 SetNumberOfScalarComponents 3
+  image2 SetScalarTypeToUnsignedChar
+  image2 SetExtent 0 79 0 79 0 0
+  image2 SetDrawColor 0 255 255
+  image2 FillBox 0 79 0 79
+
+vtkImageMapper mapper
+  mapper SetInput [image1 GetOutput]
+  mapper SetColorWindow 255
+  mapper SetColorLevel 127.5
+vtkActor2D actor
+  actor SetMapper mapper
+vtkRenderer imager
+  imager AddActor2D actor
+
+imgWin AddRenderer imager
+
+set wipes "Quad Horizontal Vertical LowerLeft LowerRight UpperLeft UpperRight"
+
+foreach wipe $wipes {
+
+    vtkImageRectilinearWipe wiper${wipe}
+      wiper${wipe} SetInput 0 [image1 GetOutput]
+      wiper${wipe} SetInput 1 [image2 GetOutput]
+      wiper${wipe} SetPosition 20 20
+      wiper${wipe} SetWipeTo${wipe}
+  
+    vtkImageMapper mapper${wipe}
+      mapper${wipe} SetInput [wiper${wipe} GetOutput]
+      mapper${wipe} SetColorWindow 255
+      mapper${wipe} SetColorLevel 127.5
+    vtkActor2D actor${wipe}
+      actor${wipe} SetMapper mapper${wipe}
+    vtkRenderer imager${wipe}
+      imager${wipe} AddActor2D actor${wipe}
+
+    imgWin AddRenderer imager${wipe}
+}
+imagerQuad SetViewport 0 .5 .25 1
+imagerHorizontal SetViewport .25 .5 .5 1
+imagerVertical SetViewport .5 .5 .75 1
+imagerLowerLeft SetViewport .75 .5 1 1
+imagerLowerRight SetViewport 0 0 .25 .5
+imagerUpperLeft SetViewport .25 0 .5 .5
+imagerUpperRight SetViewport .5 0 .75 .5
+imager SetViewport .75 0 1 .5
+
+imgWin SetSize 400 200
+imgWin Render
+
+wm withdraw .
---
+++ Imaging/vtkImageRectilinearWipe.h
@@ -0,0 +1,120 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkImageRectilinearWipe.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkImageRectilinearWipe - make a rectilinear combination of two images.
+// .SECTION Description
+// vtkImageRectilinearWipe makes a rectilinear combination of two
+// images. The two input images must correspond in size, scalar type and
+// number of components.
+// The resulting image has four possible configurations
+// called:
+//   Quad - alternate input 0 and input 1 horizontally and
+//     vertically. Select this with SetWipeModeToQuad. The Position
+//     specifies the location of the quad intersection.
+//   Corner - 3 of one input and 1 of the other. Select the location of
+//     input 0 with with SetWipeModeToLowerLeft, SetWipeModeToLowerRight,
+//     SetWipeModeToUpperLeft and SetWipeModeToUpperRight. The Position
+//     selects the location of the corner.
+//   Horizontal - alternate input 0 and input 1 with a vertical
+//     split. Select this with SetWipeModeToHorizontal. Position[0]
+//     specifies the location of the vertical transition between input 0
+//     and input 1.
+//   Vertical - alternate input 0 and input 1 with a horizontal
+//     split. Only the y The intersection point of the rectilinear points
+//     is controlled with the Point ivar.
+
+// .SECTION Thanks
+// This work was supported by PHS Research Grant No. 1 P41 RR13218-01
+// from the National Center for Research Resources.
+
+// .SECTION See Also
+// vtkImageCheckerboard
+
+#ifndef __vtkImageRectilinearWipe_h
+#define __vtkImageRectilinearWipe_h
+
+#include "vtkImageTwoInputFilter.h"
+
+#define VTK_WIPE_QUAD 0
+#define VTK_WIPE_HORIZONTAL 1
+#define VTK_WIPE_VERTICAL 2
+#define VTK_WIPE_LOWER_LEFT 3
+#define VTK_WIPE_LOWER_RIGHT 4
+#define VTK_WIPE_UPPER_LEFT 5
+#define VTK_WIPE_UPPER_RIGHT 6
+
+class VTK_IMAGING_EXPORT vtkImageRectilinearWipe : public vtkImageTwoInputFilter
+{
+public:
+  static vtkImageRectilinearWipe *New();
+  vtkTypeRevisionMacro(vtkImageRectilinearWipe,vtkImageTwoInputFilter);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Set/Get the location of the image transition.
+  vtkSetVector2Macro(Position,int);
+  vtkGetVectorMacro(Position,int,2);
+
+  // Description:
+  // Specify the wipe mode. This mode determnis how input 0 and input
+  // 1 are combined to produce the output. Each mode uses one or both
+  // of the values stored in Position.
+  //   SetWipeToQuad - alternate input 0 and input 1 horizontally and
+  //     vertically. The Position specifies the location of the quad
+  //     intersection.
+  //   SetWipeToLowerLeft{LowerRight,UpperLeft.UpperRight} - 3 of one
+  //     input and 1 of the other. Select the location of input 0 to the
+  //     LowerLeft{LowerRight,UpperLeft,UpperRight}. Position
+  //     selects the location of the corner.
+  //   SetWipeToHorizontal - alternate input 0 and input 1 with a vertical
+  //     split. Position[0] specifies the location of the vertical
+  //     transition between input 0 and input 1.
+  //   SetWipeToVertical - alternate input 0 and input 1 with a
+  //     horizontal split. Position[1] specifies the location of the
+  //     horizonal transition between input 0 and input 1.
+  vtkSetClampMacro(Wipe,int,
+                   VTK_WIPE_QUAD,VTK_WIPE_UPPER_RIGHT);
+  vtkGetMacro(Wipe,int);
+  void SetWipeToQuad()
+    {this->SetWipe(VTK_WIPE_QUAD);}
+  void SetWipeToHorizontal()
+    {this->SetWipe(VTK_WIPE_HORIZONTAL);}
+  void SetWipeToVertical()
+    {this->SetWipe(VTK_WIPE_VERTICAL);}
+  void SetWipeToLowerLeft()
+    {this->SetWipe(VTK_WIPE_LOWER_LEFT);}
+  void SetWipeToLowerRight()
+    {this->SetWipe(VTK_WIPE_LOWER_RIGHT);}
+  void SetWipeToUpperLeft()
+    {this->SetWipe(VTK_WIPE_UPPER_LEFT);}
+  void SetWipeToUpperRight()
+    {this->SetWipe(VTK_WIPE_UPPER_RIGHT);}
+
+protected:
+  vtkImageRectilinearWipe();
+  ~vtkImageRectilinearWipe() {};
+
+  void ThreadedExecute(vtkImageData **inDatas, vtkImageData *outData,
+                       int extent[6], int id);
+  int Position[2];
+  int Wipe;
+private:
+  vtkImageRectilinearWipe(const vtkImageRectilinearWipe&);  // Not implemented.
+  void operator=(const vtkImageRectilinearWipe&);  // Not implemented.
+};
+
+#endif
