147ca2822ba9c283e6af2a87c88a70512cb4f3b8
Sebastien Barre <sebastien.barre@kitware.com>
2003-06-06 16:39:20 -0400

    ENH: static Tcl/Tk support - provide TCL_TK_STATIC, - encapsulate #include tcl.h / tk.h inside vtkTcl.h / vtkTk.h so that we can support static Tcl/Tk - add missing imm32 / comctl32

---
+++ Common/vtkTcl.h
@@ -0,0 +1,32 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkTcl.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef __vtkTcl_h
+#define __vtkTcl_h
+
+#include "vtkToolkits.h"
+
+#ifdef TCL_TK_STATIC
+#ifndef STATIC_BUILD
+#define STATIC_BUILD
+#endif
+#endif
+
+#include <tcl.h>
+
+#endif
---
+++ Common/vtkTk.h
@@ -0,0 +1,32 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkTk.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 1993-2002 Ken Martin, Will Schroeder, Bill Lorensen 
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef __vtkTk_h
+#define __vtkTk_h
+
+#include "vtkToolkits.h"
+
+#ifdef TCL_TK_STATIC
+#ifndef STATIC_BUILD
+#define STATIC_BUILD
+#endif
+#endif
+
+#include <tk.h>
+
+#endif
