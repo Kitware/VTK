3ae630016440320e4d36bc23471b1d4fe7f1c20f
Berk Geveci <berk.geveci@kitware.com>
2003-06-18 10:12:44 -0400

    BUG: Was always using this->TCoordsName as the name of the array.

--- IO/vtkDataWriter.cxx
+++ IO/vtkDataWriter.cxx
@@ -38,7 +38,7 @@
 #include "vtkUnsignedLongArray.h"
 #include "vtkUnsignedShortArray.h"
 
-vtkCxxRevisionMacro(vtkDataWriter, "1.98");
+vtkCxxRevisionMacro(vtkDataWriter, "1.99");
 vtkStandardNewMacro(vtkDataWriter);
 
 // this undef is required on the hp. vtkMutexLock ends up including
@@ -853,7 +853,7 @@ int vtkDataWriter::WriteTCoordData(ostream *fp, vtkDataArray *tcoords, int num)
 
 
   *fp << "TEXTURE_COORDINATES ";
-  sprintf(format, "%s %d %s\n", this->TCoordsName, dim, "%s");
+  sprintf(format, "%s %d %s\n", tcoordsName, dim, "%s");
   delete[] tcoordsName;
 
   return this->WriteArray(fp, tcoords->GetDataType(), tcoords, format, num, 
