7a16d5ad037724d46a93cb138954943410fb0281
Berk Geveci <berk.geveci@kitware.com>
2003-06-20 13:05:57 -0400

    ENH:Fixed 4-byte alignment problems

--- Common/vtkHeap.cxx
+++ Common/vtkHeap.cxx
@@ -18,7 +18,7 @@
 #include "vtkHeap.h"
 #include "vtkObjectFactory.h"
 
-vtkCxxRevisionMacro(vtkHeap, "1.8");
+vtkCxxRevisionMacro(vtkHeap, "1.9");
 vtkStandardNewMacro(vtkHeap);
 
 class VTK_COMMON_EXPORT vtkHeapBlock
@@ -54,17 +54,21 @@ vtkHeap::~vtkHeap()
 
 void* vtkHeap::AllocateMemory(size_t n)
 {
+  if ( n%4 ) //4-byte word alignement
+    {
+    n += 4 - (n%4);
+    }
+
   size_t blockSize = (n > this->BlockSize ? n : this->BlockSize );
   this->NumberOfAllocations++;
-  char* ptr;
   
-  if ( ! this->Current || (this->Current->Data + this->Position + n) >= 
-       (this->Current->Data + this->Current->Size) )
+  if ( ! this->Current || 
+       (this->Position + n) >= this->Current->Size )
     {
     this->Add(blockSize);
     }
   
-  ptr = this->Current->Data + this->Position;
+  char *ptr = this->Current->Data + this->Position;
   this->Position += n;
 
   return ptr;
