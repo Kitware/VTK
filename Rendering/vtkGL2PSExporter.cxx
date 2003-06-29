799bf090d0bc2ec822fe190e9fd9c8f8c3631d51
Prabhu Ramachandran <prabhu_r@users.sourceforge.net>
2003-06-29 15:12:07 -0400

    ENH: Generates proper output if the user has not rendered the scene already. Also turns off sorting when mixed mode images are generated.

--- Rendering/vtkGL2PSExporter.cxx
+++ Rendering/vtkGL2PSExporter.cxx
@@ -170,7 +170,7 @@ void _Turn2DPropsOn(vtkRendererCollection *renCol, vtkIntArray *act2dVis)
     }
 }
 
-vtkCxxRevisionMacro(vtkGL2PSExporter, "1.6");
+vtkCxxRevisionMacro(vtkGL2PSExporter, "1.7");
 vtkStandardNewMacro(vtkGL2PSExporter);
 
 vtkGL2PSExporter::vtkGL2PSExporter()
@@ -246,8 +246,10 @@ void vtkGL2PSExporter::WriteData()
 
   // GL2PS versions before 0.9.1 segfault if sorting is performed when
   // TeX output is chosen.  Sorting is irrelevant for Tex output
-  // anyway.
-  if (this->FileFormat == TEX_FILE)
+  // anyway.  When Write3DPropsAsRasterImage is on sorting is of no
+  // use.
+  if ((this->FileFormat == TEX_FILE) || 
+      (this->Write3DPropsAsRasterImage == 1))
     {
     sort = GL2PS_NO_SORT;
     }
@@ -312,7 +314,11 @@ void vtkGL2PSExporter::WriteData()
 
   // Ready to write file.
 
-  // write out a raster image without the 2d actors
+  // Need to render once before rendering into the feedback buffer to
+  // prevent problems.
+  this->RenderWindow->Render();  
+
+  // Write out a raster image without the 2d actors
   if (this->Write3DPropsAsRasterImage)
     {
     _SavePropVisibility(renCol, volVis, actVis, act2dVis);
