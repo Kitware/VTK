2cbf1746f991f34b5132c2f346cef975492d9db2
Prabhu Ramachandran <prabhu_r@users.sourceforge.net>
2003-06-30 01:24:12 -0400

    ENH: Added support for controlling point size and line width.  Updated documentation.

--- Rendering/vtkGL2PSExporter.cxx
+++ Rendering/vtkGL2PSExporter.cxx
@@ -170,8 +170,11 @@ void _Turn2DPropsOn(vtkRendererCollection *renCol, vtkIntArray *act2dVis)
     }
 }
 
-vtkCxxRevisionMacro(vtkGL2PSExporter, "1.7");
+vtkCxxRevisionMacro(vtkGL2PSExporter, "1.8");
 vtkStandardNewMacro(vtkGL2PSExporter);
+
+static float vtkGL2PSExporterGlobalPointSizeFactor = 5.0/7.0;
+static float vtkGL2PSExporterGlobalLineWidthFactor = 5.0/7.0;
 
 vtkGL2PSExporter::vtkGL2PSExporter()
 {
@@ -195,6 +198,26 @@ vtkGL2PSExporter::~vtkGL2PSExporter()
     {
     delete [] this->FilePrefix;
     }
+}
+
+void vtkGL2PSExporter::SetGlobalPointSizeFactor(float val)
+{
+    vtkGL2PSExporterGlobalPointSizeFactor = fabs(val);
+}
+
+float vtkGL2PSExporter::GetGlobalPointSizeFactor()
+{
+    return vtkGL2PSExporterGlobalPointSizeFactor;
+}
+
+void vtkGL2PSExporter::SetGlobalLineWidthFactor(float val)
+{
+    vtkGL2PSExporterGlobalLineWidthFactor = fabs(val);
+}
+
+float vtkGL2PSExporter::GetGlobalLineWidthFactor()
+{
+    return vtkGL2PSExporterGlobalLineWidthFactor;
 }
 
 void vtkGL2PSExporter::WriteData()
@@ -438,4 +461,8 @@ void vtkGL2PSExporter::PrintSelf(ostream& os, vtkIndent indent)
      << (this->OcclusionCull ? "On\n" : "Off\n");
   os << indent << "Write3DPropsAsRasterImage: "
      << (this->Write3DPropsAsRasterImage ? "On\n" : "Off\n");
+  os << indent << "GlobalPointSizeFactor:" 
+     << vtkGL2PSExporterGlobalPointSizeFactor << endl;
+  os << indent << "GlobalLineWidthFactor:" 
+     << vtkGL2PSExporterGlobalLineWidthFactor << endl;
 }
