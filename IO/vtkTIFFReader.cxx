30a765af3ffe2a05cfa2d891e3e7e33fdd213b22
Ken Martin <ken.martin@kitware.com>
2003-08-22 10:46:02 -0400

    bug in reading rgb pallette images

--- IO/vtkTIFFReader.cxx
+++ IO/vtkTIFFReader.cxx
@@ -52,7 +52,7 @@ extern "C" {
 
 //-------------------------------------------------------------------------
 vtkStandardNewMacro(vtkTIFFReader);
-vtkCxxRevisionMacro(vtkTIFFReader, "1.38");
+vtkCxxRevisionMacro(vtkTIFFReader, "1.39");
 
 class vtkTIFFReaderInternal
 {
@@ -626,9 +626,9 @@ int vtkTIFFReader::EvaluateImageAt( void* out, void* in )
       break;
     case vtkTIFFReader::PALETTE_RGB:
       this->GetColor(*source, &red, &green, &blue);     
-      *(image)   = static_cast<unsigned char>(red);
-      *(image+1) = static_cast<unsigned char>(green);
-      *(image+2) = static_cast<unsigned char>(blue);
+      *(image)   = static_cast<unsigned char>(red >> 8);
+      *(image+1) = static_cast<unsigned char>(green >> 8);
+      *(image+2) = static_cast<unsigned char>(blue >> 8);
       increment = 3;
       break;
     default:
