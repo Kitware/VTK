1ad5012c99390ab9f9c930550841dafcf7b59eff
Andy Cedilnik <andy.cedilnik@kitware.com>
2003-09-15 14:37:48 -0400

    ENH: More elaborate testing

--- Common/Testing/Python/PythonSmoke.py
+++ Common/Testing/Python/PythonSmoke.py
@@ -6,7 +6,6 @@ try:
 except:
   print "Cannot import vtk"
   sys.exit(1)
-
 try:
   print dir(vtk)
 except:
@@ -14,15 +13,34 @@ except:
   sys.exit(1)
 
 try:
-  o = vtk.vtkObject()
+  try:
+    try:
+      o = vtk.vtkLineWidget()
+      print "Using Hybrid"
+    except:
+      o = vtk.vtkActor()
+      print "Using Rendering"
+  except:
+    o = vtk.vtkObject()
+    print "Using Common"
 except:
   print "Cannot create vtkObject"
   sys.exit(1)
 
 try:
   print o
+  print "Reference count: %d" % o.GetReferenceCount()
+  print "Class name: %s" % o.GetClassName()
 except:
   print "Cannot print object"
+  sys.exit(1)
+
+try:
+  b = vtk.vtkObject()
+  d = b.SafeDownCast(o)
+  print b, d
+except:
+  print "Cannot downcast"
   sys.exit(1)
 
 sys.exit(0)
