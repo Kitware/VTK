d6e4410f5632a08406a3bab1a14f575deb88affc
Berk Geveci <berk.geveci@kitware.com>
2003-11-05 09:44:35 -0500

    Fixed mem. leak

--- Graphics/vtkCutter.cxx
+++ Graphics/vtkCutter.cxx
@@ -32,7 +32,7 @@
 
 #include <math.h>
 
-vtkCxxRevisionMacro(vtkCutter, "1.73");
+vtkCxxRevisionMacro(vtkCutter, "1.74");
 vtkStandardNewMacro(vtkCutter);
 vtkCxxSetObjectMacro(vtkCutter,CutFunction,vtkImplicitFunction);
 
@@ -340,8 +340,6 @@ void vtkCutter::UnstructuredGridCutter()
   int abortExecute = 0;
 
   float range[2];
-
-  cellScalars=vtkFloatArray::New();
 
   // Create objects to hold output of contour operation
   //
