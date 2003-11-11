46961bd518b68ff4e25902c5352cf9c4ed6c409a
Amy Squillacote <amy.squillacote@kitware.com>
2003-11-11 10:45:48 -0500

    changing test so it should pass on more platforms

--- Rendering/Testing/Tcl/TestStyleRubberBandZoom.tcl
+++ Rendering/Testing/Tcl/TestStyleRubberBandZoom.tcl
@@ -1,21 +1,36 @@
 package require vtk
-package require vtkinteraction
 
-# Load base (spike and test)
+# Set up the pipeline
 
-source [file join [file dirname [info script]] TestStyleBase.tcl]
-source [file join [file dirname [info script]] TestStyleBaseSpike.tcl]
+vtkTIFFReader reader
+reader SetFileName "$VTK_DATA_ROOT/Data/beach.tif"
 
-# Set interactor style
+vtkImageActor ia
+ia SetInput [reader GetOutput]
 
-vtkInteractorStyleRubberBandZoom inStyle
-iren SetInteractorStyle inStyle
-iren Initialize
+vtkRenderer ren
+ren AddActor ia
+
+vtkRenderWindow renWin
+renWin AddRenderer ren
+renWin SetSize 400 400
+
+vtkRenderWindowInteractor iren
+iren SetRenderWindow renWin
+
+vtkInteractorStyleRubberBandZoom rbz
+rbz SetInteractor iren
+
+iren SetInteractorStyle rbz
+
+renWin Render
 
 # Test style
 
-iren SetEventInformationFlipY 150 150 0 0 0 0 0
+iren SetEventInformationFlipY 250 250 0 0 0 0 0
 iren InvokeEvent "LeftButtonPressEvent"
 iren SetEventInformationFlipY 100 100 0 0 0 0 0
 iren InvokeEvent "MouseMoveEvent"
 iren InvokeEvent "LeftButtonReleaseEvent"
+
+wm withdraw .
