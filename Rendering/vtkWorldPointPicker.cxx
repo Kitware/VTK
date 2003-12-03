d396897f580be0f6c0017f2fb5048446b4d648b0
Jeff Lee <jeff@cdnorthamerica.com>
2003-12-03 17:35:45 -0500

    ENH - Since renderers are rendered backwards from numlayers->0, the z-buffer data is wiped out for all but the last rendered layer (0).  For multiple-layer renderwindows, we need to perform a pick render before accessing z-buffer info, or else we will get renderer[0]'s z-buffer info.

--- Rendering/vtkWorldPointPicker.cxx
+++ Rendering/vtkWorldPointPicker.cxx
@@ -19,7 +19,7 @@
 #include "vtkObjectFactory.h"
 #include "vtkRenderer.h"
 
-vtkCxxRevisionMacro(vtkWorldPointPicker, "1.17");
+vtkCxxRevisionMacro(vtkWorldPointPicker, "1.18");
 vtkStandardNewMacro(vtkWorldPointPicker);
 
 vtkWorldPointPicker::vtkWorldPointPicker()
@@ -47,30 +47,45 @@ int vtkWorldPointPicker::Pick(float selectionX, float selectionY,
   // Invoke start pick method if defined
   this->InvokeEvent(vtkCommand::StartPickEvent,NULL);
 
-  z = renderer->GetZ ((int) selectionX, (int) selectionY);
-
-  // if z is 1.0, we assume the user has picked a point on the
-  // screen that has not been rendered into. Use the camera's focal
-  // point for the z value. The test value .999999 has to be used
-  // instead of 1.0 because for some reason our SGI Infinite Reality
-  // engine won't return a 1.0 from the zbuffer
-  if (z < 0.999999)
+  // Since renderers are rendered backwards from numlayers->0, the
+  // z-buffer data is wiped out for all but the last rendered layer (0).
+  // for multiple-layer renderwindows, we need to perform a pick render
+  // before accessing z-buffer info, or else we will get renderer[0]'s
+  // z-buffer info.
+  if (renderer->GetLayer() != 0)
     {
-    selectionZ = z;
-    vtkDebugMacro(<< " z from zBuffer: " << selectionZ);
+    vtkDebugMacro(<< " Renderer not on layer 0, Getting Z from PickProp() ");
+    renderer->PickProp((int) selectionX, (int) selectionY);
+    selectionZ = renderer->GetPickedZ();
+    vtkDebugMacro(<< " z from transparent renderer: " << selectionZ);
     }
   else
     {
-    // Get camera focal point and position. Convert to display (screen) 
-    // coordinates. We need a depth value for z-buffer.
-    camera = renderer->GetActiveCamera();
-    camera->GetFocalPoint((float *)cameraFP); cameraFP[3] = 1.0;
-
-    renderer->SetWorldPoint(cameraFP);
-    renderer->WorldToDisplay();
-    displayCoord = renderer->GetDisplayPoint();
-    selectionZ = displayCoord[2];
-    vtkDebugMacro(<< "computed z from focal point: " << selectionZ);
+    z = renderer->GetZ ((int) selectionX, (int) selectionY);
+    
+    // if z is 1.0, we assume the user has picked a point on the
+    // screen that has not been rendered into. Use the camera's focal
+    // point for the z value. The test value .999999 has to be used
+    // instead of 1.0 because for some reason our SGI Infinite Reality
+    // engine won't return a 1.0 from the zbuffer
+    if (z < 0.999999)
+      {
+        selectionZ = z;
+        vtkDebugMacro(<< " z from zBuffer: " << selectionZ);
+      }
+    else
+      {
+        // Get camera focal point and position. Convert to display (screen) 
+        // coordinates. We need a depth value for z-buffer.
+        camera = renderer->GetActiveCamera();
+        camera->GetFocalPoint((float *)cameraFP); cameraFP[3] = 1.0;
+        
+        renderer->SetWorldPoint(cameraFP);
+        renderer->WorldToDisplay();
+        displayCoord = renderer->GetDisplayPoint();
+        selectionZ = displayCoord[2];
+        vtkDebugMacro(<< "computed z from focal point: " << selectionZ);
+      }
     }
 
   // now convert the display point to world coordinates
