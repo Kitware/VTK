43986330bd7312f4f892331aa84cd1c6ac81d81f
Dean Inglis <dean.inglis@camris.ca>
2003-12-20 07:59:11 -0500

    FIX: printself defects

--- Hybrid/vtkImageTracerWidget.cxx
+++ Hybrid/vtkImageTracerWidget.cxx
@@ -37,7 +37,7 @@
 #include "vtkTransformPolyDataFilter.h"
 #include "vtkTransform.h"
 
-vtkCxxRevisionMacro(vtkImageTracerWidget, "1.5");
+vtkCxxRevisionMacro(vtkImageTracerWidget, "1.6");
 vtkStandardNewMacro(vtkImageTracerWidget);
 
 vtkCxxSetObjectMacro(vtkImageTracerWidget, HandleProperty, vtkProperty);
@@ -426,6 +426,8 @@ void vtkImageTracerWidget::PrintSelf(ostream& os, vtkIndent indent)
      << (this->SnapToImage ? "On\n" : "Off\n") ;
   os << indent << "CaptureRadius: " << this->CaptureRadius << "\n";
   os << indent << "GlyphAngle: " << this->GlyphAngle << "\n";
+  os << indent << "NumberOfHandles: " << this->NumberOfHandles << "\n";
+  os << indent << "IsClosed: " << this->IsClosed << "\n";
   os << indent << "AutoClose: "
      << (this->AutoClose ? "On\n" : "Off\n") ;
 }
