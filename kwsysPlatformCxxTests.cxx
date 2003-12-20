cc3228f6f1776083e98493b3d949c19fab65ca86
Brad King <brad.king@kitware.com>
2003-12-20 12:44:46 -0500

    ENH: Shortened and grouped IOS and STL feature macro names.

--- kwsysPlatformCxxTests.cxx
+++ kwsysPlatformCxxTests.cxx
@@ -1,10 +1,10 @@
-#ifdef TEST_KWSYS_HAVE_STL_STD
+#ifdef TEST_KWSYS_STL_HAVE_STD
 #include <list>
 void f(std::list<int>*) {}
 int main() { return 0; }
 #endif
 
-#ifdef TEST_KWSYS_HAVE_ANSI_STREAMS_STD
+#ifdef TEST_KWSYS_IOS_HAVE_STD
 #include <iosfwd>
 void f(std::ostream*) {}
 int main() { return 0; }
