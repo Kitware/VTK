e4b74298ce7f88741e249c6956fcd7d937a7b76d
Brad King <brad.king@kitware.com>
2003-12-22 11:20:52 -0500

    ENH: All platform tests are now in kwsysPlatformCxxTests.cxx.  This makes the listfile code much more readable.

--- kwsysPlatformCxxTests.cxx
+++ kwsysPlatformCxxTests.cxx
@@ -4,8 +4,28 @@ void f(std::list<int>*) {}
 int main() { return 0; }
 #endif
 
+#ifdef TEST_KWSYS_IOS_HAVE_ANSI
+#include <iosfwd>
+int main() { return 0; }
+#endif
+
 #ifdef TEST_KWSYS_IOS_HAVE_STD
 #include <iosfwd>
 void f(std::ostream*) {}
+int main() { return 0; }
+#endif
+
+#ifdef TEST_KWSYS_IOS_HAVE_SSTREAM
+#include <sstream>
+int main() { return 0; }
+#endif
+
+#ifdef TEST_KWSYS_IOS_HAVE_STRSTREAM_H
+#include <strstream.h>
+int main() { return 0; }
+#endif
+
+#ifdef TEST_KWSYS_IOS_HAVE_STRSTREA_H
+#include <strstrea.h>
 int main() { return 0; }
 #endif
