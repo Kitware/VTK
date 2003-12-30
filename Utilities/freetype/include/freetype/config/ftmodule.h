979fe964734820c2221b0e612394384480d08fdc
Sebastien Barre <sebastien.barre@kitware.com>
2003-12-30 11:42:11 -0500

    FIX: remove sfnt support to fix optim flags (variable might be clobbered by longjmp, etc.)

--- Utilities/freetype/include/freetype/config/ftmodule.h
+++ Utilities/freetype/include/freetype/config/ftmodule.h
@@ -7,7 +7,7 @@ FT_USE_MODULE(psaux_module_class)
 FT_USE_MODULE(psnames_module_class)
 FT_USE_MODULE(pshinter_module_class)
 FT_USE_MODULE(ft_raster1_renderer_class)
-FT_USE_MODULE(sfnt_module_class)
+  /* FT_USE_MODULE(sfnt_module_class) */
 FT_USE_MODULE(ft_smooth_renderer_class)
 FT_USE_MODULE(tt_driver_class)
 FT_USE_MODULE(t1_driver_class)
