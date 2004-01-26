576575e13bbb74d877087aa26046eb4360a27248
Charles Law <charles.law@kitware.com>
2004-01-26 12:00:13 -0500

    I was getting corrupt scalars in multidisplay/paraview.

--- Rendering/vtkMapper.cxx
+++ Rendering/vtkMapper.cxx
@@ -18,7 +18,7 @@
 #include "vtkLookupTable.h"
 #include "vtkMath.h"
 
-vtkCxxRevisionMacro(vtkMapper, "1.109");
+vtkCxxRevisionMacro(vtkMapper, "1.110");
 
 // Initialize static member that controls global immediate mode rendering
 static int vtkMapperGlobalImmediateModeRendering = 0;
@@ -221,7 +221,8 @@ vtkUnsignedCharArray *vtkMapper::MapScalars(double alpha)
         GetScalars(this->GetInput(), this->ScalarMode, this->ArrayAccessMode,
                    this->ArrayId, this->ArrayName, this->ArrayComponent);
       if (this->GetMTime() < this->Colors->GetMTime() &&
-          scalars && scalars->GetMTime() < this->Colors->GetMTime())
+          this->GetInput() && 
+          this->GetInput()->GetMTime() < this->Colors->GetMTime())
         {
         return this->Colors;
         }
