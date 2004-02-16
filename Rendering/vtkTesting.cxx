81f4e44997c00337848868b070c46bb84b3e8c66
Ken Martin <ken.martin@kitware.com>
2004-02-16 16:21:06 -0500

    fix memory leak

--- Rendering/vtkTesting.cxx
+++ Rendering/vtkTesting.cxx
@@ -29,7 +29,7 @@
 #include <sys/stat.h>
 
 vtkStandardNewMacro(vtkTesting);
-vtkCxxRevisionMacro(vtkTesting, "1.6");
+vtkCxxRevisionMacro(vtkTesting, "1.7");
 vtkCxxSetObjectMacro(vtkTesting, RenderWindow, vtkRenderWindow);
 
 // Function returning either a command line argument, an environment variable
@@ -177,10 +177,10 @@ const char *vtkTesting::GetDataRoot()
       argv[i] = strdup(this->Args[i].c_str());
       }
     }
+
   char *dr = vtkTestUtilitiesGetArgOrEnvOrDefault(
     "-D", this->Args.size(), argv, "VTK_DATA_ROOT", 
     "../../../../VTKData");
-  
   this->SetDataRoot(dr);
   delete [] dr;
   
@@ -242,10 +242,11 @@ const char *vtkTesting::GetValidImageFileName()
       }
     }
   
-  vtkstd::string viname = vtkTestUtilitiesGetArgOrEnvOrDefault(
+  char * baseline = vtkTestUtilitiesGetArgOrEnvOrDefault(
     "-B", this->Args.size(), argv, 
-    "VTK_BASELINE_ROOT", 
-    this->GetDataRoot());
+    "VTK_BASELINE_ROOT", this->GetDataRoot());
+  vtkstd::string viname = baseline;
+  delete [] baseline;
   
   for (i = 0; i < (this->Args.size() - 1); ++i)
     {
