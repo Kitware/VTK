ac16c741c75ad9886ee0ca330565bb114895885d
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-03-24 11:52:21 -0500

    FIX: Node were being read improperly

--- IO/vtkGAMBITReader.cxx
+++ IO/vtkGAMBITReader.cxx
@@ -27,7 +27,7 @@
 #include "vtkIntArray.h"
 #include "vtkCellArray.h"
 
-vtkCxxRevisionMacro(vtkGAMBITReader, "1.3");
+vtkCxxRevisionMacro(vtkGAMBITReader, "1.4");
 vtkStandardNewMacro(vtkGAMBITReader);
 
 //----------------------------------------------------------------------------
@@ -237,7 +237,15 @@ void vtkGAMBITReader::ReadBoundaryConditionSets()
       for(f=0; f < nentry; f++)
         {
         *(this->FileStream) >> node;
-        bcscalar->SetValue(node, 1);
+        node--;
+        if( node >= 0 && node < this->NumberOfNodes)
+          {
+          bcscalar->SetValue(node, 1);
+          }
+        else
+          {
+          vtkErrorMacro(<<"Node value is outside of range");
+          }
         }
       this->FileStream->get(c);
       // read here the end of section
