7bd130000f9c24ac030b813047f5fe157f374d05
Ken Martin <ken.martin@kitware.com>
2004-03-30 13:44:56 -0500

    added coverage for jpeg and png

--- IO/Testing/Tcl/TestImageWriters.tcl
+++ IO/Testing/Tcl/TestImageWriters.tcl
@@ -17,44 +17,65 @@ vtkImageLuminance luminance
   luminance SetInput sp
 
 #
-# If the current directory is writable, then test the witers
+# write to the temp directory if possible, otherwise use .
 #
+set dir "."
+if {[info commands rtTester] == "rtTester"}  {
+   set dir [rtTester GetTempDirectory]
+}
 
-if {[catch {set channel [open test.tmp w]}] == 0 } {
+# make sure it is writeable first
+if {[catch {set channel [open $dir/test.tmp w]}] == 0 } {
    close $channel
-   file delete -force test.tmp
+   file delete -force $dir/test.tmp
 
    vtkTIFFWriter tiff1
    tiff1 SetInput [image1 GetOutput]
-   tiff1 SetFileName tiff1.tif
+   tiff1 SetFileName $dir/tiff1.tif
    
    vtkTIFFWriter tiff2
    tiff2 SetInput [luminance GetOutput]
-   tiff2 SetFileName tiff2.tif
+   tiff2 SetFileName $dir/tiff2.tif
    
    vtkBMPWriter bmp1
    bmp1 SetInput [image1 GetOutput]
-   bmp1 SetFileName bmp1.bmp
+   bmp1 SetFileName $dir/bmp1.bmp
    
    vtkBMPWriter bmp2
    bmp2 SetInput [luminance GetOutput]
-   bmp2 SetFileName bmp2.bmp
+   bmp2 SetFileName $dir/bmp2.bmp
    
    vtkPNMWriter pnm1
    pnm1 SetInput [image1 GetOutput]
-   pnm1 SetFileName pnm1.pnm
+   pnm1 SetFileName $dir/pnm1.pnm
    
    vtkPNMWriter pnm2
    pnm2 SetInput [luminance GetOutput]
-   pnm2 SetFileName pnm2.pnm
+   pnm2 SetFileName $dir/pnm2.pnm
 
    vtkPostScriptWriter psw1
    psw1 SetInput [image1 GetOutput]
-   psw1 SetFileName psw1.ps
+   psw1 SetFileName $dir/psw1.ps
 
    vtkPostScriptWriter psw2
    psw2 SetInput [luminance GetOutput]
-   psw2 SetFileName psw2.ps
+   psw2 SetFileName $dir/psw2.ps
+
+   vtkPNGWriter pngw1
+   psw1 SetInput [image1 GetOutput]
+   psw1 SetFileName $dir/pngw1.png
+
+   vtkPNGWriter pngw2
+   psw2 SetInput [luminance GetOutput]
+   psw2 SetFileName $dir/pngw2.png
+
+   vtkJPEGWriter jpgw1
+   psw1 SetInput [image1 GetOutput]
+   psw1 SetFileName $dir/jpgw1.jpg
+
+   vtkJPEGWriter jpgw2
+   psw2 SetInput [luminance GetOutput]
+   psw2 SetFileName $dir/jpgw2.jpg
 
    tiff1 Write
    tiff2 Write
@@ -64,15 +85,23 @@ if {[catch {set channel [open test.tmp w]}] == 0 } {
    pnm2 Write
    psw1 Write
    psw2 Write
+   pngw1 Write
+   pngw2 Write
+   jpgw1 Write
+   jpgw2 Write
 
-   file delete -force tiff1.tif
-   file delete -force tiff2.tif
-   file delete -force bmp1.bmp
-   file delete -force bmp2.bmp
-   file delete -force pnm1.pnm
-   file delete -force pnm2.pnm
-   file delete -force psw1.ps
-   file delete -force psw2.ps
+   file delete -force $dir/tiff1.tif
+   file delete -force $dir/tiff2.tif
+   file delete -force $dir/bmp1.bmp
+   file delete -force $dir/bmp2.bmp
+   file delete -force $dir/pnm1.pnm
+   file delete -force $dir/pnm2.pnm
+   file delete -force $dir/psw1.ps
+   file delete -force $dir/psw2.ps
+   file delete -force $dir/pngw1.png
+   file delete -force $dir/pngw2.png
+   file delete -force $dir/jpgw1.jpg
+   file delete -force $dir/jpgw2.jpg
 }
 
 vtkImageViewer viewer
