d58857b320b1be8843c2ae8ed3316ae17fd9a4cf
Bill Lorensen <lorensen@crd.ge.com>
2004-04-28 18:40:11 -0400

    ENH: Places a Note on the Dashboard that contains the output of several relevant build dependent files.

--- Common/Testing/Cxx/SystemInformation.cxx
+++ Common/Testing/Cxx/SystemInformation.cxx
@@ -20,10 +20,29 @@
 #include "vtkDebugLeaks.h"
 #include <Common/Testing/Cxx/SystemInformation.h>
 #include <sys/stat.h>
+#include <time.h>
 
-void vtkSystemInformationPrintFile(const char* name, ostream& os)
+// Construct the name of the notes file.
+#define VTK_SYSTEM_INFORMATION_NOTES \
+  VTK_BINARY_DIR "/Testing/HTML/TestingResults/Sites/" \
+  VTKTesting_SITE "/" VTKTesting_BUILD_NAME "/BuildNameNotes.xml"
+
+vtkstd::string vtkGetCurrentDateTime(const char* format)
 {
-  os << "================================================================\n";
+  char buf[1024];
+  time_t t;
+  time(&t);
+  strftime(buf, sizeof(buf), format, localtime(&t));
+  return buf;
+}
+
+void vtkSystemInformationPrintFile(const char* name, ostream& os,
+                                   char note=0)
+{
+  if (!note)
+    {
+    os << "================================================================\n";
+    }
   struct stat fs;
   if(stat(name, &fs) != 0)
     {
@@ -39,10 +58,14 @@ void vtkSystemInformationPrintFile(const char* name, ostream& os)
 
   if(fin)
     {
-    os << "Contents of \"" << name << "\":\n";
-    os << "----------------------------------------------------------------\n";
+    if (!note)
+      {
+      os << "Contents of \"" << name << "\":\n";
+      os << "----------------------------------------------------------------\n";
+      }
     const int bufferSize = 4096;
-    char buffer[bufferSize];
+    char bufferIn[bufferSize];
+    char bufferOut[6*bufferSize]; // worst case scenario
     // This copy loop is very sensitive on certain platforms with
     // slightly broken stream libraries (like HPUX).  Normally, it is
     // incorrect to not check the error condition on the fin.read()
@@ -50,10 +73,40 @@ void vtkSystemInformationPrintFile(const char* name, ostream& os)
     // error occurred.  Therefore, the loop should be safe everywhere.
     while(fin)
       {
-      fin.read(buffer, bufferSize);
+      fin.read(bufferIn, bufferSize);
       if(fin.gcount())
         {
-        os.write(buffer, fin.gcount());
+        // convert buffer to an XML safe form
+        const char *s = bufferIn;
+        char *x = bufferOut;
+        *x = '\0';
+        for (int i = 0; i < static_cast<int>(fin.gcount()); i++)
+          {
+          // replace all special characters
+          switch (*s)
+            {
+            case '&':
+              strcat(x, "&amp;"); x += 5;
+              break;
+            case '"':
+              strcat(x, "&quot;"); x += 6;
+              break;
+            case '\'':
+              strcat(x, "&apos;"); x += 6;
+              break;
+            case '<':
+              strcat(x, "&lt;"); x += 4;
+              break;
+            case '>':
+              strcat(x, "&gt;"); x += 4;
+              break;
+            default:
+              *x = *s; x++;
+              *x = '\0'; // explicitly terminate the new string
+            }
+          s++;
+          }
+        os.write(bufferOut, x - bufferOut);
         }
       }
     os.flush();
@@ -86,6 +139,40 @@ int SystemInformation(int,char *[])
     vtkSystemInformationPrintFile(*f, cout);
     }
 
+  ofstream outf(VTK_SYSTEM_INFORMATION_NOTES, ios::out);
+  if(outf)
+    {
+    cout << "Also writing this information to file " << VTK_SYSTEM_INFORMATION_NOTES << "\n";
+  
+    outf << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << endl;
+    outf << "<Site BuildName=\"" << VTKTesting_BUILD_NAME << "\"  Name=\""
+         << VTKTesting_SITE << "\">" << endl;
+    outf << "<BuildNameNotes>" << endl;
+    for(f = files; *f; ++f)
+      {
+      vtkstd::string currentDateTime =  vtkGetCurrentDateTime("%a %b %d %Y %H:%M:%S %Z");
+      outf << "<Note Name=\"" << *f << "\">" << endl;
+      outf << "<DateTime>"
+           << currentDateTime.c_str()
+           << "</DateTime>" << endl;
+      outf << "<Text>" << endl;
+    
+      vtkSystemInformationPrintFile(*f, outf, 1);
+
+      outf << "</Text>" << endl;
+      outf << "</Note>" << endl;
+      }
+    
+    outf << "</BuildNameNotes>" << endl;
+    outf << "</Site>" << endl;
+    outf.close();
+    }
+  else
+    {
+    cerr << "Error writing this information to file " << VTK_SYSTEM_INFORMATION_NOTES << "\n";
+    return 1;
+    }
+  
 #if defined(__sgi) && !defined(__GNUC__) && defined(_COMPILER_VERSION)
   cout << "SGI compiler version: " << int(_COMPILER_VERSION) << endl;
 #endif
