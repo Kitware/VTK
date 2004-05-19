70434aec3082e13e155847dcfe5551a6b69d0e60
Ken Martin <ken.martin@kitware.com>
2004-05-19 13:27:24 -0400

    merges from the main tree

--- Parallel/vtkMPICommunicator.cxx
+++ Parallel/vtkMPICommunicator.cxx
@@ -23,7 +23,7 @@
 
 #include "vtkMPI.h"
 
-vtkCxxRevisionMacro(vtkMPICommunicator, "1.30.6.1");
+vtkCxxRevisionMacro(vtkMPICommunicator, "1.30.6.2");
 vtkStandardNewMacro(vtkMPICommunicator);
 
 vtkCxxSetObjectMacro(vtkMPICommunicator,Group,vtkMPIGroup);
@@ -52,7 +52,14 @@ MPI_Comm* vtkMPICommunicatorOpaqueComm::GetHandle()
 # ifdef VTK_USE_64BIT_IDS
 MPI_Datatype vtkMPICommunicatorGetMPIType()
 {
+#if VTK_SIZEOF_LONG == 8
+  return MPI_LONG;
+#elif defined(MPI_LONG_LONG)
   return MPI_LONG_LONG;
+#else
+  vtkGenericWarningMacro("This systems MPI doesnt seem to support 64 bit ids and you have 64 bit IDs turned on. Please contact VTK mailing list.");
+  return MPI_INT;
+#endif
 }
 # endif
 #else
