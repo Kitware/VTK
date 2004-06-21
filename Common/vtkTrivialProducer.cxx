abc2c226564f83cb7dd8882c3ed9871747b674df
Brad King <brad.king@kitware.com>
2004-06-21 15:34:08 -0400

    ENH: Added vtkTrivialProducer to server as a producer for stand-alone data objects in the vtkAlgorithm/vtkExecutive-based pipeline.

---
+++ Common/vtkTrivialProducer.cxx
@@ -0,0 +1,167 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkTrivialProducer.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkTrivialProducer.h"
+
+#include "vtkDataObject.h"
+#include "vtkStreamingDemandDrivenPipeline.h"
+#include "vtkGarbageCollector.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkObjectFactory.h"
+#include "vtkStructuredGrid.h"
+#include "vtkRectilinearGrid.h"
+#include "vtkImageData.h"
+
+vtkCxxRevisionMacro(vtkTrivialProducer, "1.1");
+vtkStandardNewMacro(vtkTrivialProducer);
+
+//----------------------------------------------------------------------------
+vtkTrivialProducer::vtkTrivialProducer()
+{
+  this->SetNumberOfInputPorts(0);
+  this->SetNumberOfOutputPorts(1);
+  this->Output = 0;
+  this->DebugOn();
+}
+
+//----------------------------------------------------------------------------
+vtkTrivialProducer::~vtkTrivialProducer()
+{
+  this->SetOutput(0);
+}
+
+//----------------------------------------------------------------------------
+void vtkTrivialProducer::SetOutput(vtkDataObject*)
+{
+  // Method is currently disabled.
+#if 0
+  vtkDataObject* oldOutput = this->Output;
+  if(newOutput != oldOutput)
+    {
+    this->Output = newOutput;
+    if(newOutput)
+      {
+      newOutput->Register(this);
+      newOutput->SetProducerPort(this->GetOutputPort(0));
+      }
+    if(oldOutput)
+      {
+      oldOutput->SetProducerPort(0);
+      oldOutput->UnRegister(this);
+      }
+    this->Modified();
+    }
+#endif
+}
+
+//----------------------------------------------------------------------------
+void vtkTrivialProducer::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
+
+//----------------------------------------------------------------------------
+vtkExecutive* vtkTrivialProducer::CreateDefaultExecutive()
+{
+  return vtkStreamingDemandDrivenPipeline::New();
+}
+
+//----------------------------------------------------------------------------
+int vtkTrivialProducer::FillInputPortInformation(int, vtkInformation*)
+{
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+int vtkTrivialProducer::FillOutputPortInformation(int, vtkInformation*)
+{
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+int vtkTrivialProducer::ProcessUpstreamRequest(vtkInformation*,
+                                               vtkInformationVector*,
+                                               vtkInformationVector*)
+{
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+int
+vtkTrivialProducer::ProcessDownstreamRequest(vtkInformation* request,
+                                             vtkInformationVector*,
+                                             vtkInformationVector* outputVector)
+{
+  if(request->Has(vtkDemandDrivenPipeline::REQUEST_INFORMATION()) ||
+     request->Has(vtkDemandDrivenPipeline::REQUEST_DATA()))
+    {
+    vtkInformation* outputInfo = outputVector->GetInformationObject(0);
+    outputInfo->Set(vtkInformation::DATA_OBJECT(), this->Output);
+    }
+  if(request->Has(vtkDemandDrivenPipeline::REQUEST_INFORMATION()) &&
+     this->Output)
+    {
+    vtkInformation* outputInfo = outputVector->GetInformationObject(0);
+
+    // The whole extent is just the extent because the output has no
+    // real source to change its data.
+    if(vtkStructuredGrid* sg = vtkStructuredGrid::SafeDownCast(this->Output))
+      {
+      int extent[6];
+      sg->GetExtent(extent);
+      outputInfo->Set(vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT(),
+                      extent, 6);
+      }
+    else if(vtkImageData* id = vtkImageData::SafeDownCast(this->Output))
+      {
+      int extent[6];
+      id->GetExtent(extent);
+      outputInfo->Set(vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT(),
+                      extent, 6);
+      }
+    else if(vtkRectilinearGrid* rg = vtkRectilinearGrid::SafeDownCast(this->Output))
+      {
+      int extent[6];
+      rg->GetExtent(extent);
+      outputInfo->Set(vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT(),
+                      extent, 6);
+      }
+    else if(this->Output->GetExtentType() == VTK_PIECES_EXTENT)
+      {
+      outputInfo->Set(
+        vtkStreamingDemandDrivenPipeline::MAXIMUM_NUMBER_OF_PIECES(), 1);
+      }
+    }
+  if(request->Has(vtkDemandDrivenPipeline::REQUEST_DATA()) && this->Output)
+    {
+    this->Output->DataHasBeenGenerated();
+    this->Output->Print(cout);
+    }
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+void vtkTrivialProducer::ReportReferences(vtkGarbageCollector* collector)
+{
+  this->Superclass::ReportReferences(collector);
+  collector->ReportReference(this->Output, "Output");
+}
+
+//----------------------------------------------------------------------------
+void vtkTrivialProducer::RemoveReferences()
+{
+  this->SetOutput(0);
+  this->Superclass::RemoveReferences();
+}
---
+++ Common/vtkTrivialProducer.h
@@ -0,0 +1,69 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkTrivialProducer.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkTrivialProducer - Producer for stand-alone data objects.
+// .SECTION Description
+// vtkTrivialProducer allows stand-alone data objects to be connected
+// as inputs in a pipeline.  All data objects that are connected to a
+// pipeline involving vtkAlgorithm must have a producer.  This trivial
+// producer allows data objects that are hand-constructed in a program
+// without another vtk producer to be connected.
+
+#ifndef __vtkTrivialProducer_h
+#define __vtkTrivialProducer_h
+
+#include "vtkAlgorithm.h"
+
+class vtkDataObject;
+
+class VTK_COMMON_EXPORT vtkTrivialProducer : public vtkAlgorithm
+{
+public:
+  static vtkTrivialProducer *New();
+  vtkTypeRevisionMacro(vtkTrivialProducer,vtkAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Process upstream/downstream requests trivially.  The associated
+  // output data object is never modified, but it is queried to
+  // fulfill requests.
+  virtual int ProcessUpstreamRequest(vtkInformation*,
+                                     vtkInformationVector*,
+                                     vtkInformationVector*);
+  virtual int ProcessDownstreamRequest(vtkInformation*,
+                                       vtkInformationVector*,
+                                       vtkInformationVector*);
+
+  // Description:
+  // Set the data object that is "produced" by this producer.  It is
+  // never really modified.
+  virtual void SetOutput(vtkDataObject* output);
+protected:
+  vtkTrivialProducer();
+  ~vtkTrivialProducer();
+
+  virtual int FillInputPortInformation(int, vtkInformation*);
+  virtual int FillOutputPortInformation(int, vtkInformation*);
+  virtual vtkExecutive* CreateDefaultExecutive();
+
+  vtkDataObject* Output;
+
+  virtual void ReportReferences(vtkGarbageCollector*);
+  virtual void RemoveReferences();
+private:
+  vtkTrivialProducer(const vtkTrivialProducer&);  // Not implemented.
+  void operator=(const vtkTrivialProducer&);  // Not implemented.
+};
+
+#endif
