52043113584222db213f3f8d023940b776e65aa3
Brad King <brad.king@kitware.com>
2004-06-23 16:15:03 -0400

    BUG: Removed extra DebugOn call.

--- Common/vtkTrivialProducer.cxx
+++ Common/vtkTrivialProducer.cxx
@@ -24,7 +24,7 @@
 #include "vtkRectilinearGrid.h"
 #include "vtkImageData.h"
 
-vtkCxxRevisionMacro(vtkTrivialProducer, "1.2");
+vtkCxxRevisionMacro(vtkTrivialProducer, "1.3");
 vtkStandardNewMacro(vtkTrivialProducer);
 
 //----------------------------------------------------------------------------
@@ -33,7 +33,6 @@ vtkTrivialProducer::vtkTrivialProducer()
   this->SetNumberOfInputPorts(0);
   this->SetNumberOfOutputPorts(1);
   this->Output = 0;
-  this->DebugOn();
 }
 
 //----------------------------------------------------------------------------
