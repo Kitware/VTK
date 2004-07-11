8d5203fc506ef6efa8f855378f3ea75170e4b65e
Ken Martin <ken.martin@kitware.com>
2004-07-10 21:46:16 -0400

    Nightly Version

--- Common/vtkVersion.h
+++ Common/vtkVersion.h
@@ -28,7 +28,7 @@
 
 #include "vtkObject.h"
 
-#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.1919 $, $Date: 2004-07-10 01:48:45 $ (GMT)"
+#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.1920 $, $Date: 2004-07-11 01:46:16 $ (GMT)"
 
 
 class VTK_COMMON_EXPORT vtkVersion : public vtkObject
