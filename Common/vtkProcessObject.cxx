d2ab1f1ff899ad4f2ceca363c26e3a5ba739c1f2
Ken Martin <ken.martin@kitware.com>
2004-07-17 12:00:11 -0400

    another fix

--- Common/vtkProcessObject.cxx
+++ Common/vtkProcessObject.cxx
@@ -27,7 +27,7 @@
 
 #include "vtkDebugLeaks.h"
 
-vtkCxxRevisionMacro(vtkProcessObject, "1.42");
+vtkCxxRevisionMacro(vtkProcessObject, "1.43");
 
 //----------------------------------------------------------------------------
 
@@ -44,6 +44,9 @@ public:
 #endif
     return new vtkProcessObjectDummyData;
     }
+protected:
+  vtkProcessObjectDummyData() {}; 
+  virtual ~vtkProcessObjectDummyData() {}; 
 private:
   vtkProcessObjectDummyData(const vtkProcessObjectDummyData&);
   void operator=(const vtkProcessObjectDummyData&);  // Not implemented.
