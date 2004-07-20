3a22b3e7eac7d2370c81b1ca755ef6301e932ff1
Ken Martin <ken.martin@kitware.com>
2004-07-20 10:02:39 -0400

    finally does execute information

--- IO/vtkStructuredPointsReader.cxx
+++ IO/vtkStructuredPointsReader.cxx
@@ -21,7 +21,7 @@
 #include "vtkPointData.h"
 #include "vtkStructuredPoints.h"
 
-vtkCxxRevisionMacro(vtkStructuredPointsReader, "1.56");
+vtkCxxRevisionMacro(vtkStructuredPointsReader, "1.57");
 vtkStandardNewMacro(vtkStructuredPointsReader);
 
 vtkStructuredPointsReader::vtkStructuredPointsReader()
@@ -62,27 +62,219 @@ void vtkStructuredPointsReader::ExecuteInformation()
 {
   this->SetErrorCode( vtkErrorCode::NoError );
   vtkStructuredPoints *output = this->GetOutput();
-  vtkDataArray *scalars;
-  int saveRequestFlag;
   
-  // Now here is a problem.
-  // Update Extent needs to be set incase the RequestExactExtent flag is on.
-  // Bypass to superclasses update.  
-  // Well that solution no longer works because the superclass now handles
-  // the RequestForExactExtent flag.  Lets try disabling the flag temporarily.
-  saveRequestFlag = output->GetRequestExactExtent();
-  output->SetRequestExactExtent(0);
-  output->vtkDataObject::UpdateData();
-  output->SetRequestExactExtent(saveRequestFlag);
+  char line[256];
+  int dimsRead=0, arRead=0, originRead=0;
+  int scalarTypeRead = 0;
+  int done=0;
   
-  scalars = output->GetPointData()->GetScalars();
-  if (scalars)
+  if (!this->OpenVTKFile() || !this->ReadHeader())
     {
-    output->SetScalarType(scalars->GetDataType());
-    output->SetNumberOfScalarComponents(scalars->GetNumberOfComponents());
+    return;
     }
 
-  output->SetWholeExtent(output->GetExtent());
+  // Read structured points specific stuff
+  if (!this->ReadString(line))
+    {
+    vtkErrorMacro(<<"Data file ends prematurely!");
+    this->CloseVTKFile ();
+    this->SetErrorCode( vtkErrorCode::PrematureEndOfFileError );
+    return;
+    }
+
+  if ( !strncmp(this->LowerCase(line),"dataset",(unsigned long)7) )
+    {
+    // Make sure we're reading right type of geometry
+    if (!this->ReadString(line))
+      {
+      vtkErrorMacro(<<"Data file ends prematurely!");
+      this->CloseVTKFile ();
+      this->SetErrorCode( vtkErrorCode::PrematureEndOfFileError );
+      return;
+      } 
+
+    if ( strncmp(this->LowerCase(line),"structured_points",17) )
+      {
+      vtkErrorMacro(<< "Cannot read dataset type: " << line);
+      this->CloseVTKFile ();
+      this->SetErrorCode( vtkErrorCode::UnrecognizedFileTypeError );
+      return;
+      }
+
+    // Read keyword and number of points
+    while (!done)
+      {
+      if (!this->ReadString(line))
+        {
+        break;
+        }
+      
+      if ( ! strncmp(this->LowerCase(line), "dimensions",10) )
+        {
+        int dim[3];
+        if (!(this->Read(dim) && 
+              this->Read(dim+1) && 
+              this->Read(dim+2)))
+          {
+          vtkErrorMacro(<<"Error reading dimensions!");
+          this->CloseVTKFile ();
+          this->SetErrorCode( vtkErrorCode::FileFormatError );
+          return;
+          }
+        output->SetWholeExtent(0,dim[0]-1,0,dim[1]-1,0,dim[2]-1);
+        dimsRead = 1;
+        }
+      
+      else if ( !strncmp(line,"aspect_ratio",12) || 
+                !strncmp(line,"spacing",7) )
+        {
+        double ar[3];
+        if (!(this->Read(ar) && 
+              this->Read(ar+1) && 
+              this->Read(ar+2)))
+          {
+          vtkErrorMacro(<<"Error reading spacing!");
+          this->CloseVTKFile ();
+          this->SetErrorCode( vtkErrorCode::FileFormatError );
+          return;
+          }
+        output->SetSpacing(ar);
+        arRead = 1;
+        }
+      
+      else if ( ! strncmp(line,"origin",6) )
+        {
+        double origin[3];
+        if (!(this->Read(origin) && 
+              this->Read(origin+1) && 
+              this->Read(origin+2)))
+          {
+          vtkErrorMacro(<<"Error reading origin!");
+          this->CloseVTKFile ();
+          this->SetErrorCode( vtkErrorCode::FileFormatError );
+          return;
+          }
+        
+        output->SetOrigin(origin);
+        originRead = 1;
+        }
+      
+      else if ( !strncmp(line, "cell_data", 9))
+        {
+        break; //out of this loop
+        }
+
+      else if ( ! strncmp(line, "point_data", 10) )
+        {
+        int npts;
+        if (!this->Read(&npts))
+          {
+          vtkErrorMacro(<<"Cannot read point data!");
+          this->CloseVTKFile ();
+          this->SetErrorCode( vtkErrorCode::FileFormatError );
+          return;
+          }
+
+        while (this->ReadString(line))
+          {
+          // read scalar data
+          //
+          if ( ! strncmp(this->LowerCase(line), "scalars", 7) )
+            {
+            this->ReadString(line);
+            this->ReadString(line);
+            
+            scalarTypeRead = 1;
+
+            if ( ! strncmp(line, "bit", 3) )
+              {
+              output->SetScalarType(VTK_BIT);
+              }
+            else if ( ! strncmp(line, "char", 4) )
+              {
+              output->SetScalarTypeToChar();              
+              }
+            else if ( ! strncmp(line, "unsigned_char", 13) )
+              {
+              output->SetScalarTypeToUnsignedChar();
+              }   
+            else if ( ! strncmp(line, "short", 5) )
+              {
+              output->SetScalarTypeToShort();
+              }
+            else if ( ! strncmp(line, "unsigned_short", 14) )
+              {
+              output->SetScalarTypeToUnsignedShort();
+              }
+            else if ( ! strncmp(line, "int", 3) )
+              {
+              output->SetScalarTypeToInt();
+              }
+            else if ( ! strncmp(line, "unsigned_int", 12) )
+              {
+              output->SetScalarTypeToUnsignedInt();
+              }
+            else if ( ! strncmp(line, "long", 4) )
+              {
+              output->SetScalarTypeToLong();
+              }
+            else if ( ! strncmp(line, "unsigned_long", 13) )
+              {
+              output->SetScalarTypeToUnsignedLong();
+              }
+            else if ( ! strncmp(line, "float", 5) )
+              {
+              output->SetScalarTypeToFloat();
+              }
+            else if ( ! strncmp(line, "double", 6) )
+              {
+              output->SetScalarTypeToDouble();
+              }
+            else
+              {
+              scalarTypeRead = 0;
+              }
+            
+            // the next string could be an integer number of components or a
+            // lookup table
+            this->ReadString(line);
+            if (strcmp(this->LowerCase(line), "lookup_table"))
+              {
+              int numComp = atoi(line);
+              if (numComp < 1 || !this->ReadString(line))
+                {
+                vtkErrorMacro(<<"Cannot read scalar header!" << " for file: " 
+                              << (this->FileName?this->FileName:"(Null FileName)"));
+                return;
+                }
+              output->SetNumberOfScalarComponents(numComp);
+              }
+            else
+              {
+              output->SetNumberOfScalarComponents(1);
+              }
+            break;
+            }
+          }
+        break; //out of this loop
+        }
+      
+      else if (strncmp(line, "field", 5))
+        {
+        vtkErrorMacro(<< "Unrecognized keyword: " << line);
+        this->CloseVTKFile ();
+        this->SetErrorCode( vtkErrorCode::FileFormatError );
+        return;
+        }
+      }
+    
+    if ( !dimsRead || !arRead || !originRead || !scalarTypeRead) 
+      {
+      vtkWarningMacro(<<"Not all meta data was read form the file.");
+      }
+    }
+  
+  this->CloseVTKFile ();
 }
 
 void vtkStructuredPointsReader::Execute()
