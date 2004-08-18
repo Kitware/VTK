5fbde93e0f1888900a29a7110f6cb6ba6c38e0bf
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-08-18 11:12:40 -0400

    DOC: tweaks

--- Parallel/vtkExtractUserDefinedPiece.h
+++ Parallel/vtkExtractUserDefinedPiece.h
@@ -17,17 +17,18 @@
  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
 ----------------------------------------------------------------------------*/
 
-// .NAME  vtkExtractUserDefinedPiece - Return user specified piece with ghost cells
+// .NAME vtkExtractUserDefinedPiece - Return user specified piece with ghost cells
 //
 // .SECTION Description
-//    Provided a function that determines which cells are zero-level
-//    cells ("the piece"), this class outputs the piece with the 
-//    requested number of ghost levels.  The only difference between 
-//    this class and the class it is derived from is that the 
-//    zero-level cells are specified by a function you provide, 
-//    instead of determined by dividing up the cells based on cell Id.
+// Provided a function that determines which cells are zero-level
+// cells ("the piece"), this class outputs the piece with the 
+// requested number of ghost levels.  The only difference between 
+// this class and the class it is derived from is that the 
+// zero-level cells are specified by a function you provide, 
+// instead of determined by dividing up the cells based on cell Id.
 //
 // .SECTION See Also
+// vtkExtractUnstructuredGridPiece
 
 #ifndef __vtkExtractUserDefinedPiece_h
 #define __vtkExtractUserDefinedPiece_h
@@ -47,16 +48,13 @@ public:
 
   // Set the function used to identify the piece.  The function should 
   // return 1 if the cell is in the piece, and 0 otherwise.
-
   void SetPieceFunction(UserDefFunc func) {this->InPiece = func; this->Modified();}
 
   // Set constant data to be used by the piece identifying function.
-   
   void SetConstantData(void *data, int len);
 
   // Get constant data to be used by the piece identifying function.
   // Return the length of the data buffer.
-
   int GetConstantData(void **data);
 
   // The function should return 1 if the cell
