35b4a6a039a69100201b0f20b1f416883cf69798
Brad King <brad.king@kitware.com>
2004-08-19 07:54:14 -0400

    ENH: Re-enabled new GC implementation now that compilation issues have been fixed.

--- Common/vtkGarbageCollector.cxx
+++ Common/vtkGarbageCollector.cxx
@@ -14,23 +14,105 @@
 =========================================================================*/
 #include "vtkGarbageCollector.h"
 
-#include <vtkstd/set>
 #include <vtkstd/queue>
+#include <vtkstd/set>
+#include <vtkstd/stack>
+#include <vtkstd/vector>
 
-vtkCxxRevisionMacro(vtkGarbageCollector, "1.10");
+vtkCxxRevisionMacro(vtkGarbageCollector, "1.11");
 
 //----------------------------------------------------------------------------
-class vtkGarbageCollectorQueue: public vtkstd::queue<vtkObjectBase*> {};
-class vtkGarbageCollectorQueued: public vtkstd::set<vtkObjectBase*> {};
-
-//----------------------------------------------------------------------------
-vtkGarbageCollector::vtkGarbageCollector(vtkGarbageCollectorQueue* q,
-                                         vtkGarbageCollectorQueued* qd)
+class vtkGarbageCollectorInternals
 {
-  this->Queue = q;
-  this->Queued = qd;
-  this->NetCount = 0;
-  this->Current = 0;
+public:
+  // Store garbage collection entries keyed by object.
+  struct Entry
+  {
+    Entry(vtkObjectBase* obj): Object(obj), Root(), InComponent(0),
+                               VisitOrder(0), Queued(0), References() {}
+
+    // The object corresponding to this entry.
+    vtkObjectBase* Object;
+
+    // The candidate root for the component containing this object.
+    Entry* Root;
+
+    // Mark whether the object has been assigned to a component.
+    int InComponent;
+
+    // Mark the order in which object's are visited by Tarjan's algorithm.
+    int VisitOrder;
+
+    // Whether this entry has been queued while computing net reference count.
+    int Queued;
+
+    // The list of references reported by this entry's object.
+    typedef vtkstd::vector<Entry*> ReferencesType;
+    ReferencesType References;
+  };
+
+  // Order entries by object pointer for quick lookup.
+  struct EntryCompare
+  {
+    std::less<vtkObjectBase*> Compare;
+    vtkstd_bool operator()(Entry* l, Entry* r) const
+      { return Compare(l->Object, r->Object); }
+  };
+
+  // The set of objects that have been visited.
+  typedef vtkstd::set<Entry*, EntryCompare> EntriesBase;
+  struct EntriesType: public EntriesBase
+  {
+    typedef EntriesBase::iterator iterator;
+    Entry* Find(vtkObjectBase* obj)
+      {
+      Entry e(obj);
+      iterator i = this->find(&e);
+      return (i != end())? *i : 0;
+      }
+    ~EntriesType() { for(iterator i=begin(); i != end(); ++i) { delete *i; } }
+  };
+  EntriesType Entries;
+
+  // The main garbage collector instance.
+  vtkGarbageCollector* External;
+
+  // The stack of objects forming the connected components.
+  typedef vtkstd::stack<Entry*> StackType;
+  StackType Stack;
+
+  // The object currently being explored.
+  Entry* Current;
+
+  // Count for visit order.
+  int Count;
+
+  // The objects in the root's connected component.
+  typedef vtkstd::vector<vtkObjectBase*> ComponentType;
+  ComponentType Component;
+
+  // Find the strongly connected components reachable from the given root.
+  Entry* FindStrongComponents(vtkObjectBase*);
+
+  // Callback from objects to report references.
+  void ReportReference(vtkObjectBase*);
+
+  // Node visitor for Tarjan's algorithm.
+  Entry* VisitTarjan(vtkObjectBase*);
+
+  // Find/Print/Delete the set of objects in the root's strongly
+  // connected component.
+  int FindComponent(Entry*);
+  void PrintComponent(ostream& os);
+  void DeleteComponent();
+};
+
+//----------------------------------------------------------------------------
+vtkGarbageCollector::vtkGarbageCollector(vtkGarbageCollectorInternals* internal)
+{
+  this->Internal = internal;
+  this->Internal->External = this;
+  this->Internal->Current = 0;
 }
 
 //----------------------------------------------------------------------------
@@ -58,14 +140,13 @@ void vtkGarbageCollector::UnRegister(vtkObjectBase*)
 //----------------------------------------------------------------------------
 void vtkGarbageCollector::Check(vtkObjectBase* root)
 {
-  // Allocate as much as possible on the stack.  This code runs every
-  // time UnRegister is called on an object supporting garbage
-  // collection.  It may be worth modifying the queue and set to use a
-  // local array for storage until the size grows beyond some
+  // Allocate as much as possible on the runtime stack.  This code
+  // runs every time UnRegister is called on an object supporting
+  // garbage collection.  It may be worth modifying the stack and map
+  // to use a local array for storage until the size grows beyond some
   // constant.
-  vtkGarbageCollectorQueue objectQueue;
-  vtkGarbageCollectorQueued objectQueued;
-  vtkGarbageCollector collector(&objectQueue, &objectQueued);
+  vtkGarbageCollectorInternals internal;
+  vtkGarbageCollector collector(&internal);
 
   // Uncomment these lines to get reference loop debugging for objects
   // with Debug flag set:
@@ -74,124 +155,281 @@ void vtkGarbageCollector::Check(vtkObjectBase* root)
   //  collector.SetDebug(obj->GetDebug());
   //  }
 
+  collector.SetDebug(1);
+
+  // Do collection if necessary.
   collector.CheckReferenceLoops(root);
+
+  // Avoid destruction message.
   collector.SetDebug(0);
 }
 
 //----------------------------------------------------------------------------
-void vtkGarbageCollector::CheckReferenceLoops(vtkObjectBase* root)
+#ifdef VTK_LEAN_AND_MEAN
+void vtkGarbageCollector::ReportReference(vtkObjectBase* obj, const char*)
 {
-  // This performs a BFS traversal of the reference graph associated
-  // with the given root object.  If the total reference count of the
-  // connected component is 0 when not counting internal references,
-  // the entire component is deleted.
-
-  // Fake an internal reference to the root to initialize the queue.
-  this->Queued->clear();
-  this->NetCount = 1;
-  this->ReportReference(root, "");
-
-  vtkDebugMacro("Starting reference graph walk with root "
-                << root->GetClassName() << "(" << root << ")");
-
-  // Loop until the queue is empty.
-  while(!this->Queue->empty())
+  // Forward call to the internal implementation.
+  if(obj)
     {
-    // Get the next object from the queue.
-    this->Current = this->Queue->front();
-    this->Queue->pop();
-
-    vtkDebugMacro("Requesting references from "
-                  << this->Current->GetClassName() << "("
-                  << this->Current << ") with reference count "
-                  << this->Current->GetReferenceCount());
-
-    // Tell the object to report its references to us.
-    this->Current->ReportReferences(this);
-    }
-  this->Current = 0;
-
-  vtkDebugMacro("Finished reference graph walk with net reference count "
-                << this->NetCount);
-
-  // If the net reference count is 0, delete the entire connected
-  // component of the reference graph.
-  if(this->NetCount == 0)
-    {
-    // Report the connected component to be deleted.
-    vtkstd::set<vtkObjectBase*>::iterator obj;
-#ifndef VTK_LEAN_AND_MEAN
-    if(this->Debug && vtkObject::GetGlobalWarningDisplay())
-      {
-      ostrstream msg;
-      msg << "Deleting connected component of reference graph:\n";
-      for(obj = this->Queued->begin(); obj != this->Queued->end(); ++obj)
-        {
-        msg << "  " << (*obj)->GetClassName() << "(" << *obj << ")\n";
-        }
-      msg << ends;
-      vtkDebugMacro(<< msg.str());
-      msg.rdbuf()->freeze(0);
-      }
-#endif
-
-    // Notify all objects they are about to be garbage collected.
-    // They will disable reference loop checking.
-    for(obj = this->Queued->begin(); obj != this->Queued->end(); ++obj)
-      {
-      (*obj)->GarbageCollectionStarting();
-      }
-
-    // Disconnect the reference graph.
-    for(obj = this->Queued->begin(); obj != this->Queued->end(); ++obj)
-      {
-      (*obj)->RemoveReferences();
-      }
-
-    // Notify all objects they have been garbage collected.  They will
-    // delete themselves.
-    for(obj = this->Queued->begin(); obj != this->Queued->end(); ++obj)
-      {
-      (*obj)->GarbageCollectionFinishing();
-      }
+    this->Internal->ReportReference(obj);
     }
 }
-
-//----------------------------------------------------------------------------
-#ifndef VTK_LEAN_AND_MEAN
-void vtkGarbageCollector::ReportReference(vtkObjectBase* obj, const char* desc)
 #else
-void vtkGarbageCollector::ReportReference(vtkObjectBase* obj, const char*)
-#endif
+void vtkGarbageCollector::ReportReference(vtkObjectBase* obj, const char* desc)
 {
   if(obj)
     {
-#ifndef VTK_LEAN_AND_MEAN
-    if(this->Current && this->Debug && vtkObject::GetGlobalWarningDisplay())
+    // Report debugging information if requested.
+    if(this->Debug && vtkObject::GetGlobalWarningDisplay())
       {
+      vtkObjectBase* current = this->Internal->Current->Object;
       ostrstream msg;
       msg << "ReportReference: "
-          << this->Current->GetClassName() << "(" << this->Current << ") "
+          << current->GetClassName() << "(" << current << ") "
           << (desc?desc:"")
           << " -> " << obj->GetClassName() << "(" << obj << ")";
       msg << ends;
       vtkDebugMacro(<< msg.str());
       msg.rdbuf()->freeze(0);
       }
+
+    // Forward call to the internal implementation.
+    this->Internal->ReportReference(obj);
+    }
+}
 #endif
 
-    // Add the object to the queue if it has not already been added.
-    if(this->Queued->find(obj) == this->Queued->end())
+//----------------------------------------------------------------------------
+void vtkGarbageCollector::ForwardReportReferences(vtkObjectBase* obj)
+{
+#ifndef VTK_LEAN_AND_MEAN
+  // Report debugging information if requested.
+  if(this->Debug && vtkObject::GetGlobalWarningDisplay())
+    {
+    vtkObjectBase* current = this->Internal->Current->Object;
+    vtkDebugMacro("Requesting references from "
+                  << current->GetClassName() << "("
+                  << current << ") with reference count "
+                  << current->GetReferenceCount());
+    }
+#endif
+  obj->ReportReferences(this);
+}
+
+//----------------------------------------------------------------------------
+void vtkGarbageCollector::ForwardRemoveReferences(vtkObjectBase* obj)
+{
+  obj->RemoveReferences();
+}
+
+//----------------------------------------------------------------------------
+void vtkGarbageCollector::ForwardGarbageCollectionStarting(vtkObjectBase* obj)
+{
+  obj->GarbageCollectionStarting();
+}
+
+//----------------------------------------------------------------------------
+void vtkGarbageCollector::ForwardGarbageCollectionFinishing(vtkObjectBase* obj)
+{
+  obj->GarbageCollectionFinishing();
+}
+
+//----------------------------------------------------------------------------
+void vtkGarbageCollector::CheckReferenceLoops(vtkObjectBase* root)
+{
+  // This traverses the reference graph associated with the given root
+  // object.  If the total reference count of the strongly connected
+  // component is 0 when not counting internal references, the entire
+  // component is deleted.
+
+  vtkDebugMacro("Starting reference graph walk with root "
+                << root->GetClassName() << "(" << root << ")");
+
+  // Find the strongly connected components reachable from this root.
+  vtkGarbageCollectorInternals::Entry*
+    rootEntry = this->Internal->FindStrongComponents(root);
+
+  vtkDebugMacro("Finished reference graph walk with root "
+                << root->GetClassName() << "(" << root << ")");
+
+  // Find the net reference count of the component containing the root.
+  int netCount = this->Internal->FindComponent(rootEntry);
+
+#ifndef VTK_LEAN_AND_MEAN
+  if(this->Debug && vtkObject::GetGlobalWarningDisplay())
+    {
+    ostrstream msg;
+    msg << "Identified strongly connected component with net reference count "
+        << netCount << ":";
+    this->Internal->PrintComponent(msg);
+    msg << ends;
+    vtkDebugMacro(<< msg.str());
+    msg.rdbuf()->freeze(0);
+    }
+#endif
+
+  // If the net reference count is zero, delete the component.
+  if(netCount == 0)
+    {
+    vtkDebugMacro("Deleting strongly connected component of reference graph.");
+    this->Internal->DeleteComponent();
+    }
+}
+
+//----------------------------------------------------------------------------
+int vtkGarbageCollectorInternals::FindComponent(Entry* root)
+{
+  // The queue of objects while checking the net reference count.
+  typedef vtkstd::queue<Entry*> QueueType;
+  QueueType entryQueue;
+
+  // Initialize the queue with the root object.
+  int netCount = root->Object->GetReferenceCount();
+  this->Component.push_back(root->Object);
+  root->Queued = 1;
+  entryQueue.push(root);
+
+  // Loop until the queue is empty.
+  while(!entryQueue.empty())
+    {
+    // Get the next object from the queue.
+    Entry* v = entryQueue.front();
+    entryQueue.pop();
+
+    // Process the references to objects in the component.
+    for(Entry::ReferencesType::iterator r = v->References.begin();
+        r != v->References.end(); ++r)
       {
-      // Include the references to the object.
-      this->NetCount += obj->GetReferenceCount();
+      Entry* w = *r;
+      if(w->Root == root)
+        {
+        if(!w->Queued)
+          {
+          // Include the references to this object.
+          netCount += w->Object->GetReferenceCount();
 
-      // Queue the object.
-      this->Queued->insert(obj);
-      this->Queue->push(obj);
+          // Add the object to the list of objects in the component.
+          this->Component.push_back(w->Object);
+
+          // Queue the object.
+          w->Queued = 1;
+          entryQueue.push(w);
+          }
+
+        // This is an internal reference, so decrement the net count.
+        --netCount;
+        }
       }
+    }
 
-    // This is an internal reference.
-    --this->NetCount;
+  return netCount;
+}
+
+//----------------------------------------------------------------------------
+vtkGarbageCollectorInternals::Entry*
+vtkGarbageCollectorInternals::FindStrongComponents(vtkObjectBase* root)
+{
+  // Use Tarjan's algorithm to visit the reference graph and mark
+  // strongly connected components.
+  this->Count = 0;
+  return this->VisitTarjan(root);
+}
+
+//----------------------------------------------------------------------------
+void vtkGarbageCollectorInternals::ReportReference(vtkObjectBase* obj)
+{
+  // Get the source and destination of this reference.
+  Entry* v = this->Current;
+  Entry* w = this->Entries.Find(obj);
+
+  // Visit the destination of this reference if it has not been visited.
+  if(!w)
+    {
+    w = this->VisitTarjan(obj);
+    }
+
+  // If the destination has not yet been assigned to a component,
+  // check if it is a better potential root for the current object.
+  if(!w->InComponent)
+    {
+    if(w->Root->VisitOrder < v->Root->VisitOrder)
+      {
+      v->Root = w->Root;
+      }
+    }
+
+  // Save this reference.
+  v->References.push_back(w);
+}
+
+//----------------------------------------------------------------------------
+vtkGarbageCollectorInternals::Entry*
+vtkGarbageCollectorInternals::VisitTarjan(vtkObjectBase* obj)
+{
+  // Create an entry for the object.
+  Entry* v = new Entry(obj);
+  this->Entries.insert(v);
+
+  // Initialize the entry and push it onto the stack of graph nodes.
+  v->Root = v;
+  v->InComponent = 0;
+  v->VisitOrder = ++this->Count;
+  this->Stack.push(v);
+
+  // Process the references from this node.
+  Entry* saveCurrent = this->Current;
+  this->Current = v;
+  this->External->ForwardReportReferences(v->Object);
+  this->Current = saveCurrent;
+
+  // If we have found a component mark its members.
+  if(v->Root == v)
+    {
+    Entry* w;
+    do
+      {
+      w = this->Stack.top();
+      this->Stack.pop();
+      w->InComponent = 1;
+      w->Root = v;
+      } while(w != v);
+    }
+
+  return v;
+}
+
+//----------------------------------------------------------------------------
+void vtkGarbageCollectorInternals::PrintComponent(ostream& os)
+{
+  for(ComponentType::iterator i = this->Component.begin();
+      i != this->Component.end(); ++i)
+    {
+    os << "\n  " << (*i)->GetClassName() << "(" << *i << ")";
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkGarbageCollectorInternals::DeleteComponent()
+{
+  ComponentType::iterator obj;
+
+  // Notify all objects they are about to be garbage collected.
+  // They will disable reference loop checking.
+  for(obj = this->Component.begin(); obj != this->Component.end(); ++obj)
+    {
+    vtkGarbageCollector::ForwardGarbageCollectionStarting(*obj);
+    }
+
+  // Disconnect the reference graph.
+  for(obj = this->Component.begin(); obj != this->Component.end(); ++obj)
+    {
+    vtkGarbageCollector::ForwardRemoveReferences(*obj);
+    }
+
+  // Notify all objects they have been garbage collected.  They will
+  // delete themselves.
+  for(obj = this->Component.begin(); obj != this->Component.end(); ++obj)
+    {
+    vtkGarbageCollector::ForwardGarbageCollectionFinishing(*obj);
     }
 }
