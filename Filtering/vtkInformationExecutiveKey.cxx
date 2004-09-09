eb645ad2d7fac9cbed68853bd85ff2f4cf167d25
Brad King <brad.king@kitware.com>
2004-09-09 13:23:05 -0400

    ENH: Added vtkInformationExecutiveKey.

---
+++ Filtering/vtkInformationExecutiveKey.cxx
@@ -0,0 +1,70 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkInformationExecutiveKey.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkInformationExecutiveKey.h"
+
+#include "vtkExecutive.h"
+#include "vtkGarbageCollector.h"
+
+vtkCxxRevisionMacro(vtkInformationExecutiveKey, "1.1.2.1");
+
+//----------------------------------------------------------------------------
+vtkInformationExecutiveKey::vtkInformationExecutiveKey(const char* name, const char* location):
+  vtkInformationKey(name, location)
+{
+}
+
+//----------------------------------------------------------------------------
+vtkInformationExecutiveKey::~vtkInformationExecutiveKey()
+{
+}
+
+//----------------------------------------------------------------------------
+void vtkInformationExecutiveKey::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
+
+//----------------------------------------------------------------------------
+void vtkInformationExecutiveKey::Set(vtkInformation* info,
+                                     vtkExecutive* value)
+{
+  this->SetAsObjectBase(info, value);
+}
+
+//----------------------------------------------------------------------------
+vtkExecutive* vtkInformationExecutiveKey::Get(vtkInformation* info)
+{
+  return vtkExecutive::SafeDownCast(this->GetAsObjectBase(info));
+}
+
+//----------------------------------------------------------------------------
+int vtkInformationExecutiveKey::Has(vtkInformation* info)
+{
+  return vtkExecutive::SafeDownCast(this->GetAsObjectBase(info))?1:0;
+}
+
+//----------------------------------------------------------------------------
+void vtkInformationExecutiveKey::Copy(vtkInformation* from,
+                                      vtkInformation* to)
+{
+  this->Set(to, this->Get(from));
+}
+
+//----------------------------------------------------------------------------
+void vtkInformationExecutiveKey::Report(vtkInformation* info,
+                                        vtkGarbageCollector* collector)
+{
+  collector->ReportReference(this->Get(info), this->GetName());
+}
---
+++ Filtering/vtkInformationExecutiveKey.h
@@ -0,0 +1,58 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkInformationExecutiveKey.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkInformationExecutiveKey - Key for vtkExecutive values.
+// .SECTION Description
+// vtkInformationExecutiveKey is used to represent keys in
+// vtkInformation for values that are vtkExecutive instances.
+
+#ifndef __vtkInformationExecutiveKey_h
+#define __vtkInformationExecutiveKey_h
+
+#include "vtkInformationKey.h"
+
+class vtkExecutive;
+
+class VTK_FILTERING_EXPORT vtkInformationExecutiveKey : public vtkInformationKey
+{
+public:
+  vtkTypeRevisionMacro(vtkInformationExecutiveKey,vtkInformationKey);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  vtkInformationExecutiveKey(const char* name, const char* location);
+  ~vtkInformationExecutiveKey();
+
+  // Description:
+  // Get/Set the value associated with this key in the given
+  // information object.
+  void Set(vtkInformation* info, vtkExecutive*);
+  vtkExecutive* Get(vtkInformation* info);
+  int Has(vtkInformation* info);
+
+  // Description:
+  // Copy the entry associated with this key from one information
+  // object to another.  If there is no entry in the first information
+  // object for this key, the value is removed from the second.
+  virtual void Copy(vtkInformation* from, vtkInformation* to);
+
+  // Description:
+  // Report a reference this key has in the given information object.
+  virtual void Report(vtkInformation* info, vtkGarbageCollector* collector);
+
+private:
+  vtkInformationExecutiveKey(const vtkInformationExecutiveKey&);  // Not implemented.
+  void operator=(const vtkInformationExecutiveKey&);  // Not implemented.
+};
+
+#endif
