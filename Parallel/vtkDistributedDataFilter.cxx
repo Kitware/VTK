4fc90eab04e7aa90e1fa40362889ba8c2b460774
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-09-18 00:46:15 -0400

    ENH: Fixing warnings on Dashboard (unused vars)

--- Parallel/vtkDistributedDataFilter.cxx
+++ Parallel/vtkDistributedDataFilter.cxx
@@ -61,7 +61,7 @@
 #include "vtkMPIController.h"
 #endif
 
-vtkCxxRevisionMacro(vtkDistributedDataFilter, "1.5")
+vtkCxxRevisionMacro(vtkDistributedDataFilter, "1.6")
 
 vtkStandardNewMacro(vtkDistributedDataFilter)
 
@@ -1499,7 +1499,7 @@ vtkUnstructuredGrid *
     vtkIdList ***cellIds, int *numLists, int deleteCellIds,
     vtkDataSet *myGrid, int deleteMyGrid, 
     int filterOutDuplicateCells,   // flag if different processes may send same cells
-    int ghostCellFlag,   // flag if these cells are ghost cells
+    int vtkNotUsed(ghostCellFlag),   // flag if these cells are ghost cells
     int tag)
 {
   vtkUnstructuredGrid *mergedGrid = NULL;
@@ -2024,7 +2024,7 @@ vtkUnstructuredGrid *
     vtkIdList ***cellIds, int *numLists, int deleteCellIds,
     vtkDataSet *myGrid, int deleteMyGrid, 
     int filterOutDuplicateCells,   // flag if different processes may send same cells
-    int ghostCellFlag,  // flag if these are ghost cells
+    int vtkNotUsed(ghostCellFlag),  // flag if these are ghost cells
     int tag)
 {
   vtkUnstructuredGrid *mergedGrid = NULL;
