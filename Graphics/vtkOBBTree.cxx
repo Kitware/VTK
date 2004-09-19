28e2be0e57f553c1ae14f1635e27ca352034bc16
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-09-19 14:50:12 -0400

    ENH: Some TestSetGet issues

--- Graphics/vtkOBBTree.cxx
+++ Graphics/vtkOBBTree.cxx
@@ -26,7 +26,7 @@
 #include "vtkTriangle.h"
 #include "vtkUnstructuredGrid.h"
 
-vtkCxxRevisionMacro(vtkOBBTree, "1.61");
+vtkCxxRevisionMacro(vtkOBBTree, "1.62");
 vtkStandardNewMacro(vtkOBBTree);
 
 #define vtkCELLTRIANGLES(CELLPTIDS, TYPE, IDX, PTID0, PTID1, PTID2) \
@@ -79,6 +79,7 @@ vtkOBBTree::vtkOBBTree()
   this->Tree = NULL;
   this->PointsList = NULL;
   this->InsertedPoints = NULL;
+  this->OBBCount = 0;
 }
 
 vtkOBBTree::~vtkOBBTree()
