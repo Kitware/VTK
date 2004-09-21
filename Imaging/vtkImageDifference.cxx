e567b9e23540698d47b64495cb2195835a7c8178
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-09-21 19:23:13 -0400

    ENH: Be more verbose

--- Imaging/vtkImageDifference.cxx
+++ Imaging/vtkImageDifference.cxx
@@ -17,7 +17,7 @@
 #include "vtkImageData.h"
 #include "vtkObjectFactory.h"
 
-vtkCxxRevisionMacro(vtkImageDifference, "1.34");
+vtkCxxRevisionMacro(vtkImageDifference, "1.35");
 vtkStandardNewMacro(vtkImageDifference);
 
 // Construct object to extract all of the input data.
@@ -344,7 +344,13 @@ void vtkImageDifference::ExecuteInformation(vtkImageData **inputs,
       this->ErrorPerThread[i] = 1000;
       this->ThresholdedErrorPerThread[i] = 1000;
       }
-    vtkErrorMacro("ExecuteInformation: Input are not the same size.");
+    vtkErrorMacro("ExecuteInformation: Input are not the same size.\n" 
+      << " Input1 is: " << in1Ext[0] << "," << in1Ext[1] << ","
+                        << in1Ext[2] << "," << in1Ext[3] << ","
+                        << in1Ext[4] << "," << in1Ext[5] << "\n"
+      << " Input2 is: " << in2Ext[0] << "," << in2Ext[1] << ","
+                        << in2Ext[2] << "," << in2Ext[3] << ","
+                        << in2Ext[4] << "," << in2Ext[5] );
     }
 
   // We still need to set the whole extent to be the intersection.
