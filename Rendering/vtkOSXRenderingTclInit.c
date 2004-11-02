72a46011268c8bbe3591e368280a7453e15adcbb
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-11-02 13:12:57 -0500

    ENH: This should have been removed a long time ago

--- Rendering/vtkOSXRenderingTclInit.c
+++
@@ -1,8 +0,0 @@
-//-----  This hack needed to compile using gcc3 on OSX until new stdc++.dylib
-#include <stdio.h>
-void oft_initRenOSXInit() 
-{
-  extern void _ZNSt8ios_base4InitC4Ev();
-  _ZNSt8ios_base4InitC4Ev();
-}
-
