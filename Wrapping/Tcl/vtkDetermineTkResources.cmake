5bf581cac14545693cdff6c911c829c64c5df11f
Ken Martin <ken.martin@kitware.com>
2004-11-08 12:55:00 -0500

    COMP: do not use rc files on cygwin

--- Wrapping/Tcl/vtkDetermineTkResources.cmake
+++ Wrapping/Tcl/vtkDetermineTkResources.cmake
@@ -1,12 +1,13 @@
 IF (WIN32)
   IF (NOT BORLAND)
+   IF(NOT CYGWIN)
     VTK_GET_TCL_TK_VERSION ("TCL_TK_MAJOR_VERSION" "TCL_TK_MINOR_VERSION")
     IF (TCL_TK_MAJOR_VERSION AND TCL_TK_MINOR_VERSION)
       SET (TK_RESOURCE_FILE_TRY 
            "${VTK_SOURCE_DIR}/Utilities/TclTk/resources/tk${TCL_TK_MAJOR_VERSION}.${TCL_TK_MINOR_VERSION}/win/rc/tk.rc")
       IF (EXISTS ${TK_RESOURCE_FILE_TRY})
         SET(TK_RESOURCE_FILE ${TK_RESOURCE_FILE_TRY} CACHE INTERNAL 
-	        "The tk.rc resource file required to add the proper resources to a Tk command-line interpreter (i.e. vtk.exe)")
+              "The tk.rc resource file required to add the proper resources to a Tk command-line interpreter (i.e. vtk.exe)")
       ENDIF (EXISTS ${TK_RESOURCE_FILE_TRY})
     ENDIF (TCL_TK_MAJOR_VERSION AND TCL_TK_MINOR_VERSION)
     IF (TK_RESOURCE_FILE)
@@ -19,6 +20,7 @@ IF (WIN32)
       SET(VTK_EXE_RESOURCE_FILES 
           "${VTK_BINARY_DIR}/Wrapping/Tcl/resources/vtk.rc")
     ENDIF (TK_RESOURCE_FILE)
+   ENDIF(NOT CYGWIN)
   ENDIF (NOT BORLAND)
 ENDIF (WIN32)
 
