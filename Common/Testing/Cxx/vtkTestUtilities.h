4ef951ccfbdb0360d6be9946ab2151b5068625ce
Francois Bertel <francois.bertel@kitware.com>
2004-11-16 08:47:15 -0500

    Clean comments and removed warnings

--- Common/Testing/Cxx/vtkTestUtilities.h
+++ Common/Testing/Cxx/vtkTestUtilities.h
@@ -12,43 +12,50 @@
      PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
+// .NAME vtkTestUtilities - Utility functions used for regression testing.
+// .SECTION Description
+// vtkTestUtilities provides methods to perform common testing operations.
+// These include getting a command line argument or an environment variable,
+// or a default value. Particularly, there are specialized methods to get the
+// root directory for VTK Data, expanding a filename with this root directory.
+
 #ifndef __vtkTestUtilities_h
 #define __vtkTestUtilities_h
 
-struct vtkTestUtilities
+#include "vtkSystemIncludes.h"
+
+struct VTK_COMMON_EXPORT vtkTestUtilities
 {
   // Description:
-  // Function necessary for accessing the root directory for VTK data.
-  // Try the -D command line argument or VTK_DATA_ROOT or a default value.
-  // The returned string has to be deleted (with delete[]) by the user.
+  // Function necessary for accessing the root directory for VTK data. Try the
+  // -D command line argument or VTK_DATA_ROOT or a default value. The returned
+  // string has to be deleted (with delete[]) by the user.
   static inline char* GetDataRoot(int argc, char* argv[]);
 
   // Description:
-  // Given a file name, this function returns a new string which
-  // is (in theory) the full path. This path is constructed by
-  // prepending the file name with a command line argument 
-  // (-D path) or VTK_DATA_ROOT env. variable.
-  // If slash is true, appends a slash to the resulting string.
-  // The returned string has to be deleted (with delete[]) by the user.
+  // Given a file name, this function returns a new string which is (in theory)
+  // the full path. This path is constructed by prepending the file name with a
+  // command line argument (-D path) or VTK_DATA_ROOT env. variable. If slash
+  // is true, appends a slash to the resulting string. The returned string has
+  // to be deleted (with delete[]) by the user.
   static inline char* ExpandDataFileName(int argc, char* argv[], 
                                          const char* fname,
                                          int slash = 0);
   // Description:
-  // Function returning either a command line argument, an environment 
-  // variable or a default value.
-  // The returned string has to be deleted (with delete[]) by the user.
+  // Function returning either a command line argument, an environment variable
+  // or a default value. The returned string has to be deleted (with delete[])
+  // by the user.
   static inline char* GetArgOrEnvOrDefault(const char* arg, 
                                            int argc, char* argv[], 
                                            const char* env, 
                                            const char* def);
 
   // Description:
-  // Given a file name, this function returns a new string which
-  // is (in theory) the full path. This path is constructed by
-  // prepending the file name with a command line argument, an environment 
-  // variable or a default value.
-  // If slash is true, appends a slash to the resulting string.
-  // The returned string has to be deleted (with delete[]) by the user.
+  // Given a file name, this function returns a new string which is (in theory)
+  // the full path. This path is constructed by prepending the file name with a
+  // command line argument, an environment variable or a default value. If
+  // slash is true, appends a slash to the resulting string. The returned
+  // string has to be deleted (with delete[]) by the user.
   static inline char* ExpandFileNameWithArgOrEnvOrDefault(const char* arg, 
                                                           int argc, char* argv[], 
                                                           const char* env, 
@@ -95,7 +102,7 @@ char* vtkTestUtilities::GetArgOrEnvOrDefault(const char* arg,
       }
     }
 
-  char* value = 0;
+  char* value;
 
   if (index != -1) 
     {
@@ -129,7 +136,7 @@ char* vtkTestUtilities::ExpandFileNameWithArgOrEnvOrDefault(const char* arg,
                                                             const char* fname,
                                                             int slash)
 {
-  char* fullName = 0;
+  char* fullName;
 
   char* value = vtkTestUtilities::GetArgOrEnvOrDefault(arg, argc, argv, 
                                                        env,
