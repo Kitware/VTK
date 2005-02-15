c2281eafeae1323a02554ec8bd16f8921beeb396
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2005-02-15 16:21:58 -0500

    BUG: At no point in time did Patented rely on Rendering to be built...

--- Wrapping/Tcl/vtkpatented/vtkpatented.tcl
+++ Wrapping/Tcl/vtkpatented/vtkpatented.tcl
@@ -1,5 +1,4 @@
 package require -exact vtkio 4.5
-package require -exact vtkrendering 4.5
 
 if {[info commands ::vtk::init::require_package] != ""} {
   if {[::vtk::init::require_package vtkPatentedTCL 4.5]} {
