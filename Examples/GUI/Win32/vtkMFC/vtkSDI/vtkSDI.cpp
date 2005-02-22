6002d16805d89dd0552385cec57e3fce35fb51b3
Andrew Maclean <a.maclean@cas.edu.au>
2005-02-22 18:30:17 -0500

    COMP: - In MFC 5.0, Enable3dControls and Enable3dControlsStatic are obsolete because their functionality is incorporated into Microsoft's 32-bit operating systems. So I have removed calls to  these functions. Also StdAfx.h has been revised to comply with the VS7.1 default  StdAfx.h file.

--- Examples/GUI/Win32/vtkMFC/vtkSDI/vtkSDI.cpp
+++ Examples/GUI/Win32/vtkMFC/vtkSDI/vtkSDI.cpp
@@ -68,12 +68,6 @@ BOOL CVtkSDIApp::InitInstance()
   //  of your final executable, you should remove from the following
   //  the specific initialization routines you do not need.
 
-#ifdef _AFXDLL
-  Enable3dControls();      // Call this when using MFC in a shared DLL
-#else
-  Enable3dControlsStatic();  // Call this when linking to MFC statically
-#endif
-
   // Change the registry key under which our settings are stored.
   // TODO: You should modify this string to be something appropriate
   // such as the name of your company or organization.
