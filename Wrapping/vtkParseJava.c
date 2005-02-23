87df094a187f4b04d6509b42d78c38a9d98fa877
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2005-02-23 14:39:27 -0500

    BUG: Fix Bug #1612 - java objects should implement toString(). Thanks to Steven Robbins for patch

--- Wrapping/vtkParseJava.c
+++ Wrapping/vtkParseJava.c
@@ -493,6 +493,8 @@ void vtkParseOutput(FILE *fp, FileInfo *data)
     fprintf(fp,"  public native String Print();\n");
     /* Add the PrintRevisions method to vtkObject. */
     fprintf(fp,"  public native String PrintRevisions();\n");
+    /* Add the default toString from java object */
+    fprintf(fp,"  public String toString() { return Print(); }n");
     }
 
   if (!strcmp("vtkObject",data->ClassName))
