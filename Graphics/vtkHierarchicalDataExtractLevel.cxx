edee417f049799aea1bac8b6077e23906f649aa7
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2005-03-16 19:57:43 -0500

    COMP: Fix warnings. STYLE: update PrintSelf

--- Graphics/vtkHierarchicalDataExtractLevel.cxx
+++ Graphics/vtkHierarchicalDataExtractLevel.cxx
@@ -23,7 +23,7 @@ PURPOSE.  See the above copyright notice for more information.
 #include "vtkInformationVector.h"
 #include "vtkObjectFactory.h"
 
-vtkCxxRevisionMacro(vtkHierarchicalDataExtractLevel, "1.1");
+vtkCxxRevisionMacro(vtkHierarchicalDataExtractLevel, "1.2");
 vtkStandardNewMacro(vtkHierarchicalDataExtractLevel);
 
 // Construct object with PointIds and CellIds on; and ids being generated
@@ -133,7 +133,7 @@ int vtkHierarchicalDataExtractLevel::RequestInformation(
     for (unsigned int j=0; j<numDataSets; j++)
       {
       vtkInformation* outInfo = compInfo->GetInformation(i, j);
-      vtkInformation* inInfo = inCompInfo->GetInformation(i, j);
+      inInfo = inCompInfo->GetInformation(i, j);
       outInfo->Copy(inInfo);
       }
     }
@@ -257,4 +257,10 @@ int vtkHierarchicalDataExtractLevel::RequestData(
 void vtkHierarchicalDataExtractLevel::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
+
+  os << indent << "MinLevel:" << this->MinLevel << endl;
+  os << indent << "MaxLevel:" << this->MaxLevel << endl;
+  os << indent << "InputLevels: (" << this->InputLevels[0] << "," 
+                                   << this->InputLevels[1] << ")" << endl;
+
 }
