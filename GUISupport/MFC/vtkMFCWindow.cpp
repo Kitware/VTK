21388aa03889b60c94d7c93213e70a98eb3d9b09
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2005-03-17 11:55:39 -0500

    STYLE: put back the copyright

--- GUISupport/MFC/vtkMFCWindow.cpp
+++ GUISupport/MFC/vtkMFCWindow.cpp
@@ -1,3 +1,17 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkMFCWindow.cpp
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 
 
 #ifndef WINVER    // Allow use of features specific to Windows 95 and Windows NT 4 or later.
--- GUISupport/MFC/vtkMFCWindow.h
+++ GUISupport/MFC/vtkMFCWindow.h
@@ -1,3 +1,17 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkMFCWindow.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 
 #ifndef C_VTK_MFC_WINDOW
 #define C_VTK_MFC_WINDOW
