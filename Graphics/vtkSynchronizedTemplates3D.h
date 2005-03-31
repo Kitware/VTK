de102e11426711dc039ddc8ac40083addfdcbb3f
Ken Martin <ken.martin@kitware.com>
2005-03-31 10:02:09 -0500

    STYLE: minor cleanup

--- Graphics/vtkSynchronizedTemplates3D.h
+++ Graphics/vtkSynchronizedTemplates3D.h
@@ -11,18 +11,6 @@
      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
      PURPOSE.  See the above copyright notice for more information.
 
-
-     THIS CLASS IS PATENT PENDING.
-
-     Application of this software for commercial purposes requires 
-     a license grant from Kitware. Contact:
-         Ken Martin
-         Kitware
-         28 Corporate Drive Suite 204,
-         Clifton Park, NY 12065
-         Phone:1-518-371-3971 
-     for more information.
-
 =========================================================================*/
 // .NAME vtkSynchronizedTemplates3D - generate isosurface from structured points
 
