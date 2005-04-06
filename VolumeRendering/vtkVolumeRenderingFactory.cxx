2a1a641b4a6e22601fa5a65abc4f652bcfd8dcd6
Lisa Avila <lisa.avila@kitware.com>
2005-04-06 13:54:35 -0400

    BUG: fix problem reported on developer list with access to protected variable in vtkGraphicsFactory

--- VolumeRendering/vtkVolumeRenderingFactory.cxx
+++ VolumeRendering/vtkVolumeRenderingFactory.cxx
@@ -33,7 +33,7 @@
 
 #include "stdlib.h"
 
-vtkCxxRevisionMacro(vtkVolumeRenderingFactory, "1.1");
+vtkCxxRevisionMacro(vtkVolumeRenderingFactory, "1.2");
 vtkStandardNewMacro(vtkVolumeRenderingFactory);
 
 
@@ -60,7 +60,7 @@ vtkObject* vtkVolumeRenderingFactory::CreateInstance(const char* vtkclassname )
     if(strcmp(vtkclassname, "vtkVolumeTextureMapper2D") == 0)
       {
 #if defined(VTK_USE_MANGLED_MESA)
-      if ( vtkGraphicsFactory::UseMesaClasses )
+      if ( vtkGraphicsFactory::GetUseMesaClasses() )
         {
         return vtkMesaVolumeTextureMapper2D::New();
         }
@@ -70,7 +70,7 @@ vtkObject* vtkVolumeRenderingFactory::CreateInstance(const char* vtkclassname )
     if(strcmp(vtkclassname, "vtkRayCastImageDisplayHelper") == 0)
       {
 #if defined(VTK_USE_MANGLED_MESA)
-      if ( vtkGraphicsFactory::UseMesaClasses )
+      if ( vtkGraphicsFactory::GetUseMesaClasses() )
         {
         return vtkMesaRayCastImageDisplayHelper::New();
         }
