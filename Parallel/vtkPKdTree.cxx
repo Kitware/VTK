786da5efbf2a316342d87e5eb6017a9682aeeb2f
Lee Ann Fisk <lafisk@sandia.gov>
2005-04-13 11:11:21 -0400

    BUG: Ken Moreland discovered static_cast was surrounding an extra term, changing the order of operation.

--- Parallel/vtkPKdTree.cxx
+++ Parallel/vtkPKdTree.cxx
@@ -76,7 +76,7 @@ static char * makeEntry(const char *s)
 
 // Timing data ---------------------------------------------
 
-vtkCxxRevisionMacro(vtkPKdTree, "1.13");
+vtkCxxRevisionMacro(vtkPKdTree, "1.14");
 vtkStandardNewMacro(vtkPKdTree);
 
 const int vtkPKdTree::NoRegionAssignment = 0;   // default
@@ -818,8 +818,8 @@ void vtkPKdTree::_select(int L, int R, int K, int dim)
       Z = static_cast<float>(log(float(N))); 
       S = static_cast<int>(.5 * exp(2*Z/3));
       SD = static_cast<int>(.5 * sqrt(Z*S*((float)(N-S)/N)) * sign(I - N/2));
-      LL = max(L, K - static_cast<int>((I*((float)S/N)) + SD));
-      RR = min(R, K + static_cast<int>((N-I) * ((float)S/N) + SD));
+      LL = max(L, K - static_cast<int>((I*((float)S/N))) + SD);
+      RR = min(R, K + static_cast<int>((N-I) * ((float)S/N)) + SD);
       this->_select(LL, RR, K, dim);
       }
 
