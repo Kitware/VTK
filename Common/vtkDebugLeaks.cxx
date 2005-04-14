00879957c1ad5b68a144a4698b738c6e4192b8d8
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2005-04-14 10:00:06 -0400

    ENH: Fix Bug #1772 - Patch for vtkDebugLeaks spelling error

--- Common/vtkDebugLeaks.cxx
+++ Common/vtkDebugLeaks.cxx
@@ -40,7 +40,7 @@ int vtkDebugLeaksIgnoreClassesCheck(const char* s)
   return 0;
 }
 
-vtkCxxRevisionMacro(vtkDebugLeaks, "1.36");
+vtkCxxRevisionMacro(vtkDebugLeaks, "1.37");
 vtkStandardNewMacro(vtkDebugLeaks);
 
 //----------------------------------------------------------------------------
@@ -71,7 +71,7 @@ public:
         {
         char tmp[55];
         sprintf(tmp," has %i %s still around.\n",this->Count,
-                (this->Count == 1) ? "instances" : "instance");
+                (this->Count == 1) ? "instance" : "instances");
         os += "Class ";
         os += this->Key;
         os += tmp;
