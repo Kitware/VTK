1b757e5844763893496d886bed8fc2a70014ea4d
Lisa Avila <lisa.avila@kitware.com>
2005-05-13 14:49:24 -0400

    ENH: A new hierarhcical polydata mapper that can be used to render vtkPolyData or vtkHierarchicalDataSet composited of vtkPolyData

---
+++ Rendering/vtkHierarchicalPolyDataMapper.cxx
@@ -0,0 +1,236 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkHierarchicalPolyDataMapper.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "vtkInformation.h"
+#include "vtkMapper.h"
+#include "vtkExecutive.h"
+#include "vtkCompositeDataPipeline.h"
+#include "vtkCompositeDataSet.h"
+#include "vtkCompositeDataIterator.h"
+#include "vtkHierarchicalDataSet.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkObjectFactory.h"
+#include "vtkHierarchicalPolyDataMapper.h"
+#include "vtkPolyData.h"
+#include "vtkMath.h"
+
+vtkCxxRevisionMacro(vtkHierarchicalPolyDataMapper, "1.1");
+vtkStandardNewMacro(vtkHierarchicalPolyDataMapper);
+
+vtkHierarchicalPolyDataMapper::vtkHierarchicalPolyDataMapper()
+{
+}
+vtkHierarchicalPolyDataMapper::~vtkHierarchicalPolyDataMapper()
+{
+  for(unsigned int i=0;i<this->InternalMappers.size();i++)
+    {
+    this->InternalMappers[i]->Delete();
+    }
+  this->InternalMappers.clear();
+}
+
+// Specify the type of data this mapper can handle. If we are
+// working with a regular (not hierarchical) pipeline, then we
+// need vtkPolyData. For composite data pipelines, then
+// vtkHierarchicalDataSet is required, and we'll check when
+// building our structure whether all the part of the composite
+// data set are polydata.
+int vtkHierarchicalPolyDataMapper::FillInputPortInformation(
+  int vtkNotUsed(port), vtkInformation* info)
+{
+  info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPolyData");
+  info->Set(vtkCompositeDataPipeline::INPUT_REQUIRED_COMPOSITE_DATA_TYPE(), 
+            "vtkHierarchicalDataSet");
+  return 1;
+}    
+
+// When the structure is out-of-date, recreate it by
+// creating a mapper for each input data.
+void vtkHierarchicalPolyDataMapper::BuildPolyDataMapper()
+{
+  int warnOnce = 0;
+  
+  //Delete pdmappers if they already exist.
+  for(unsigned int i=0;i<this->InternalMappers.size();i++)
+    {
+    this->InternalMappers[i]->Delete();
+    }
+  this->InternalMappers.clear();
+  
+  //Get the HierarchicalDataSet from the input
+  vtkInformation* inInfo = this->GetExecutive()->GetInputInformation(0,0);
+  vtkHierarchicalDataSet *input = vtkHierarchicalDataSet::SafeDownCast(
+    inInfo->Get(vtkCompositeDataSet::COMPOSITE_DATA_SET()));
+  
+  // If it isn't hierarchical, maybe it is just a plain vtkPolyData
+  if(!input) 
+    {
+    vtkPolyData *pd = vtkPolyData::SafeDownCast(
+      this->GetExecutive()->GetInputData(0, 0));
+    if ( pd )
+      {
+      vtkPolyDataMapper *pdmapper = vtkPolyDataMapper::New();
+      pdmapper->SetInput(pd);
+      this->InternalMappers.push_back(pdmapper);
+      }
+    }
+  else
+    {
+    //for each data set build a vtkPolyDataMapper
+    vtkCompositeDataIterator* iter = input->NewIterator();
+    iter->GoToFirstItem();  
+    while (!iter->IsDoneWithTraversal())
+      {
+      vtkPolyData* pd = vtkPolyData::SafeDownCast(iter->GetCurrentDataObject());      
+      if (pd)
+        {
+        vtkPolyDataMapper *pdmapper = vtkPolyDataMapper::New();
+        pdmapper->SetInput(pd);
+        this->InternalMappers.push_back(pdmapper);
+        }
+      // This is not polydata - warn the user that there are non-polydata
+      // parts to this data set which will not be rendered by this mapper
+      else
+        {
+        if ( !warnOnce )
+          {
+          vtkErrorMacro("All data in the hierachical dataset must be polydata.");
+          warnOnce = 1;
+          }
+        }
+      iter->GoToNextItem();
+      }
+    }
+  
+  this->InternalMappersBuildTime.Modified();
+  
+}
+
+void vtkHierarchicalPolyDataMapper::Render(vtkRenderer *ren, vtkActor *a)
+{
+  //If the PolyDataMappers are not up-to-date then rebuild them
+  vtkCompositeDataPipeline * executive = 
+    vtkCompositeDataPipeline::SafeDownCast(this->GetExecutive());
+  
+  if(executive->GetPipelineMTime() > this->InternalMappersBuildTime.GetMTime())
+    {
+    this->BuildPolyDataMapper();    
+    }
+  
+  //Call Render() on each of the PolyDataMappers
+  for(unsigned int i=0;i<this->InternalMappers.size();i++)
+    {
+    this->InternalMappers[i]->SetClippingPlanes( this->GetClippingPlanes() );
+    this->InternalMappers[i]->Render(ren,a);    
+    }
+}
+vtkExecutive* vtkHierarchicalPolyDataMapper::CreateDefaultExecutive()
+{
+  return vtkCompositeDataPipeline::New();
+}
+
+//Looks at each DataSet and finds the union of all the bounds
+void vtkHierarchicalPolyDataMapper::ComputeBounds()
+{
+  vtkMath::UninitializeBounds(this->Bounds);
+  
+  vtkInformation* inInfo = this->GetExecutive()->GetInputInformation(0,0);
+  vtkHierarchicalDataSet *input = vtkHierarchicalDataSet::SafeDownCast(
+    inInfo->Get(vtkCompositeDataSet::COMPOSITE_DATA_SET()));
+
+  // If we don't have hierarchical data, test to see if we have
+  // plain old polydata. In this case, the bounds are simply
+  // the bounds of the input polydata.
+  if(!input) 
+    {
+    vtkPolyData *pd = vtkPolyData::SafeDownCast(
+      this->GetExecutive()->GetInputData(0, 0));
+    if ( pd )
+      {
+      pd->GetBounds( this->Bounds );
+      }
+    return;
+    }
+  
+  // We do have hierarchical data - so we need to loop over
+  // it and get the total bounds.
+  vtkCompositeDataIterator* iter = input->NewIterator();
+  iter->GoToFirstItem();  
+  double bounds[6];
+  int i;
+  
+  while (!iter->IsDoneWithTraversal())
+    {
+    vtkPolyData *pd = vtkPolyData::SafeDownCast(iter->GetCurrentDataObject());    
+    if (pd)
+      {
+      // If this isn't the first time through, expand bounds
+      // we've compute so far based on the bounds of this
+      // block
+      if ( vtkMath::AreBoundsInitialized(this->Bounds) )
+        {
+        pd->GetBounds(bounds);
+        for(i=0; i<3; i++)
+          {
+          this->Bounds[i*2] = 
+            (bounds[i*2]<this->Bounds[i*2])?
+            (bounds[i*2]):(this->Bounds[i*2]);
+          this->Bounds[i*2+1] = 
+            (bounds[i*2+1]>this->Bounds[i*2+1])?
+            (bounds[i*2+1]):(this->Bounds[i*2+1]);
+          }
+        }
+      // If this is our first time through, just get the bounds
+      // of the data as the initial bounds
+      else
+        {
+        pd->GetBounds(this->Bounds);
+        }
+      }
+    iter->GoToNextItem();
+    }
+  
+  this->BoundsMTime.Modified();
+}
+
+double* vtkHierarchicalPolyDataMapper::GetBounds()
+{
+  static double bounds[] = {-1.0,1.0, -1.0,1.0, -1.0,1.0};
+  
+  if ( ! this->GetInput() ) 
+    {
+    return bounds;
+    }
+  else
+    {
+
+    this->Update();
+    
+    //only compute bounds when the input data has changed
+    vtkCompositeDataPipeline * executive = vtkCompositeDataPipeline::SafeDownCast(this->GetExecutive());
+    if( executive->GetPipelineMTime() > this->BoundsMTime.GetMTime() )
+      {
+      ComputeBounds();
+      }
+    
+    return this->Bounds;
+    }
+}
+
+void vtkHierarchicalPolyDataMapper::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
+
---
+++ Rendering/vtkHierarchicalPolyDataMapper.h
@@ -0,0 +1,100 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkHierarchicalPolyDataMapper.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkHierarchicalPolyDataMapper - a class that renders hierarchical polygonal data
+// .SECTION Description
+// This class uses a set of vtkPolyDataMappers to render input data
+// which may be hierarchical. The input to this mapper may be
+// either vtkPolyData or a vtkHierarchicalDataSet built from 
+// polydata. If something other than vtkPolyData is encountered,
+// an error message will be produced.
+//
+// .SECTION see also
+// vtkPolyDataMapper
+
+#ifndef __vtkHierarchicalPolyDataMapper_h
+#define __vtkHierarchicalPolyDataMapper_h
+
+#include "vtkMapper.h"
+#include <vtkstd/vector>
+
+class vtkPolyDataMapper;
+class vtkInformation;
+class vtkRenderer;
+class vtkActor;
+
+class VTK_RENDERING_EXPORT vtkHierarchicalPolyDataMapper : public vtkMapper 
+{
+
+public:
+  static vtkHierarchicalPolyDataMapper *New();
+  vtkTypeRevisionMacro(vtkHierarchicalPolyDataMapper, vtkMapper);
+  virtual void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Standard method for rendering a mapper. This method will be 
+  // called by the actor.
+  void Render(vtkRenderer *ren, vtkActor *a);
+  
+  // Description:
+  // Standard vtkProp method to get 3D bounds of a 3D prop
+  double *GetBounds();
+  void GetBounds(double bounds[6]) { this->Superclass::GetBounds( bounds ); };  
+  
+protected:
+  vtkHierarchicalPolyDataMapper();
+  ~vtkHierarchicalPolyDataMapper();
+  
+  // Description:
+  // We need to override this method because the standard streaming
+  // demand driven pipeline is not what we want - we are expecting
+  // hierarchical data as input
+  vtkExecutive* CreateDefaultExecutive();
+
+  // Description:
+  // Need to define the type of data handled by this mapper.
+  virtual int FillInputPortInformation(int port, vtkInformation* info);
+  
+  // Description:
+  // This is the build method for creating the internal polydata
+  // mapper that do the actual work
+  void BuildPolyDataMapper();
+  
+  // Description:
+  // Need to loop over the hierarchy to compute bounds
+  void ComputeBounds();
+
+  // Description:
+  // Time stamp for computation of bounds.
+  vtkTimeStamp BoundsMTime;
+
+//BTX  
+  // Description:
+  // These are the internal polydata mapper that do the
+  // rendering. We save then so that they can keep their
+  // display lists.
+  vtkstd::vector<vtkPolyDataMapper*> InternalMappers;
+//ETX
+  
+  // Description:
+  // Time stamp for when we need to update the 
+  // internal mappers
+  vtkTimeStamp InternalMappersBuildTime; 
+  
+private:
+  vtkHierarchicalPolyDataMapper(const vtkHierarchicalPolyDataMapper&);  // Not implemented.
+  void operator=(const vtkHierarchicalPolyDataMapper&);    // Not implemented.
+};
+
+#endif
