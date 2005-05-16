8001d4cb27b3b390bbf83c731268160085a05bfb
Andy Wilson <atwilso@sandia.gov>
2005-05-16 09:37:31 -0400

    ENH: New subclass of vtkAbstractArray meant to hold strings instead of constant-length data.  Nothing uses this as yet, nor will it until I'm certain it doesn't cause problems on the dashboards.  Not yet supported by Tcl/Python/Java wrappers.

---
+++ Common/vtkStringArray.cxx
@@ -0,0 +1,556 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkStringArray.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright 2004 Sandia Corporation.
+  Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+  license for use of this work by or on behalf of the
+  U.S. Government. Redistribution and use in source and binary forms, with
+  or without modification, are permitted provided that this Notice and any
+  statement of authorship are reproduced on all copies.
+
+=========================================================================*/
+
+
+
+// We do not provide a definition for the copy constructor or
+// operator=.  Block the warning.
+#ifdef _MSC_VER
+# pragma warning (disable: 4661)
+#endif
+
+#include <vtkObjectFactory.h>
+
+#include "vtkStringArray.h"
+#include "vtkStdString.h"
+
+#include "vtkCharArray.h"
+#include "vtkIdList.h"
+#include "vtkIdTypeArray.h"
+
+vtkCxxRevisionMacro(vtkStringArray, "1.2");
+vtkStandardNewMacro(vtkStringArray);
+
+//----------------------------------------------------------------------------
+
+vtkStringArray::vtkStringArray(vtkIdType numComp) :
+  vtkAbstractArray( numComp )
+{
+  this->Array = NULL;
+  this->SaveUserArray = 0;
+}
+
+//----------------------------------------------------------------------------
+
+vtkStringArray::~vtkStringArray()
+{
+  if ((this->Array) && (!this->SaveUserArray))
+    {
+    delete [] this->Array;
+    }
+}
+
+//----------------------------------------------------------------------------
+// This method lets the user specify data to be held by the array.  The
+// array argument is a pointer to the data.  size is the size of
+// the array supplied by the user.  Set save to 1 to keep the class
+// from deleting the array when it cleans up or reallocates memory.
+// The class uses the actual array provided; it does not copy the data
+// from the suppled array.
+
+void vtkStringArray::SetArray(vtkStdString *array, vtkIdType size, int save)
+{
+  if ((this->Array) && (!this->SaveUserArray))
+    {
+    vtkDebugMacro (<< "Deleting the array...");
+    delete [] this->Array;
+    }
+  else
+    {
+    vtkDebugMacro (<<"Warning, array not deleted, but will point to new array.");
+    }
+
+  vtkDebugMacro(<<"Setting array to: " << array);
+
+  this->Array = array;
+  this->Size = size;
+  this->MaxId = size-1;
+  this->SaveUserArray = save;
+}
+
+//----------------------------------------------------------------------------
+// Allocate memory for this array. Delete old storage only if necessary.
+
+int vtkStringArray::Allocate(vtkIdType sz, vtkIdType)
+{
+  if(sz > this->Size)
+    {
+    if(this->Array && !this->SaveUserArray)
+      {
+      delete [] this->Array;
+      }
+
+    this->Size = ( sz > 0 ? sz : 1);
+    this->Array = new vtkStdString[this->Size];
+    if(!this->Array)
+      {
+      return 0;
+      }
+    this->SaveUserArray = 0;
+    }
+
+  this->MaxId = -1;
+
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+// Release storage and reset array to initial state.
+
+void vtkStringArray::Initialize()
+{
+  if(this->Array && !this->SaveUserArray)
+    {
+    delete [] this->Array;
+    }
+  this->Array = 0;
+  this->Size = 0;
+  this->MaxId = -1;
+  this->SaveUserArray = 0;
+}
+
+//----------------------------------------------------------------------------
+// Deep copy of another string array.
+
+void vtkStringArray::DeepCopy(vtkAbstractArray* aa)
+{
+  // Do nothing on a NULL input.
+  if(!aa)
+    {
+    return;
+    }
+
+  // Avoid self-copy.
+  if(this == aa)
+    {
+    return;
+    }
+
+  // If data type does not match, we can't copy. 
+  if(aa->GetDataType() != this->GetDataType())
+    {
+    vtkErrorMacro(<< "Incompatible types: tried to copy an array of type "
+                  << aa->GetDataTypeAsString()
+                  << " into a string array ");
+    return;
+    }
+
+  vtkStringArray *fa = vtkStringArray::SafeDownCast( aa );
+  if ( fa == NULL )
+    {
+    vtkErrorMacro(<< "Shouldn't Happen: Couldn't downcast array into a vtkStringArray." );
+    return;
+    }
+
+  // Free our previous memory.
+  if(this->Array && !this->SaveUserArray)
+    {
+    delete [] this->Array;
+    }
+
+  // Copy the given array into new memory.
+  this->MaxId = fa->GetMaxId();
+  this->Size = fa->GetSize();
+  this->SaveUserArray = 0;
+  this->Array = new vtkStdString[this->Size];
+
+  for (int i = 0; i < this->Size; ++i)
+    {
+    this->Array[i] = fa->Array[i];
+    }
+}
+
+//----------------------------------------------------------------------------
+
+void vtkStringArray::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+  if(this->Array)
+    {
+    os << indent << "Array: " << this->Array << "\n";
+    }
+  else
+    {
+    os << indent << "Array: (null)\n";
+    }
+}
+
+//----------------------------------------------------------------------------
+// Protected function does "reallocate"
+
+vtkStdString * vtkStringArray::ResizeAndExtend(vtkIdType sz)
+{
+  vtkStdString * newArray;
+  vtkIdType newSize;
+
+  if(sz > this->Size)
+    {
+    // Requested size is bigger than current size.  Allocate enough
+    // memory to fit the requested size and be more than double the
+    // currently allocated memory.
+    newSize = this->Size + sz;
+    }
+  else if (sz == this->Size)
+    {
+    // Requested size is equal to current size.  Do nothing.
+    return this->Array;
+    }
+  else
+    {
+    // Requested size is smaller than current size.  Squeeze the
+    // memory.
+    newSize = sz;
+    }
+
+  if(newSize <= 0)
+    {
+    this->Initialize();
+    return 0;
+    }
+
+  newArray = new vtkStdString[newSize];
+  if(!newArray)
+    {
+    vtkErrorMacro("Cannot allocate memory\n");
+    return 0;
+    }
+
+  if(this->Array)
+    {
+    // can't use memcpy here
+    int numCopy = (newSize < this->Size ? newSize : this->Size);
+    for (int i = 0; i < numCopy; ++i)
+      {
+      newArray[i] = this->Array[i];
+      }
+    if(!this->SaveUserArray)
+      {
+      delete [] this->Array;
+      }
+    }
+
+  if(newSize < this->Size)
+    {
+    this->MaxId = newSize-1;
+    }
+  this->Size = newSize;
+  this->Array = newArray;
+  this->SaveUserArray = 0;
+
+  return this->Array;
+}
+
+//----------------------------------------------------------------------------
+
+void vtkStringArray::Resize(vtkIdType sz)
+{
+  vtkStdString * newArray;
+  vtkIdType newSize = sz;
+
+  if(newSize == this->Size)
+    {
+    return;
+    }
+
+  if(newSize <= 0)
+    {
+    this->Initialize();
+    return;
+    }
+
+  newArray = new vtkStdString[newSize];
+  if(!newArray)
+    {
+    vtkErrorMacro(<< "Cannot allocate memory\n");
+    return;
+    }
+
+  if(this->Array)
+    {
+    int numCopy = (newSize < this->Size ? newSize : this->Size);
+
+    for (int i = 0; i < numCopy; ++i)
+      {
+      newArray[i] = this->Array[i];
+      }
+    
+    if(!this->SaveUserArray)
+      {
+      delete[] this->Array;
+      }
+    }
+
+  if(newSize < this->Size)
+    {
+    this->MaxId = newSize-1;
+    }
+  this->Size = newSize;
+  this->Array = newArray;
+  this->SaveUserArray = 0;
+}
+
+
+//----------------------------------------------------------------------------
+
+void vtkStringArray::SetNumberOfValues(vtkIdType number)
+{
+  this->Allocate(number);
+  this->MaxId = number - 1;
+}
+
+//----------------------------------------------------------------------------
+
+vtkStdString * vtkStringArray::WritePointer(vtkIdType id,
+                                     vtkIdType number)
+{
+  vtkIdType newSize=id+number;
+  if ( newSize > this->Size )
+    {
+    this->ResizeAndExtend(newSize);
+    }
+  if ( (--newSize) > this->MaxId )
+    {
+    this->MaxId = newSize;
+    }
+  return this->Array + id;
+}
+
+//----------------------------------------------------------------------------
+
+void vtkStringArray::InsertValue(vtkIdType id, vtkStdString f)
+{
+  if ( id >= this->Size )
+    {
+    this->ResizeAndExtend(id+1);
+    }
+  this->Array[id] = f;
+  if ( id > this->MaxId )
+    {
+    this->MaxId = id;
+    }
+}
+
+//----------------------------------------------------------------------------
+
+vtkIdType vtkStringArray::InsertNextValue(vtkStdString f)
+{
+  this->InsertValue (++this->MaxId,f);
+  return this->MaxId;
+}
+
+// ----------------------------------------------------------------------
+
+int
+vtkStringArray::GetDataTypeSize( void )
+{ return static_cast<int>(sizeof(vtkStdString)); }
+
+
+// ----------------------------------------------------------------------
+
+unsigned long
+vtkStringArray::GetActualMemorySize( void )
+{
+  unsigned long totalSize = 0;
+  unsigned long  numPrims = this->GetSize();
+
+  for (unsigned long i = 0; i < numPrims; ++i)
+    {
+    totalSize += sizeof( vtkStdString );
+    totalSize += this->Array[i].size() * sizeof( vtkStdString::value_type );
+    }
+
+  return (unsigned long) ceil( totalSize / 1000.0 ); // kilobytes
+}
+
+// ----------------------------------------------------------------------
+
+vtkStdString
+vtkStringArray::GetValue( vtkIdType id )
+{
+  return this->Array[id];
+}
+
+// ----------------------------------------------------------------------
+
+void
+vtkStringArray::GetValues(vtkIdList *indices, vtkAbstractArray *aa)
+{
+  if (aa == NULL)
+    {
+    vtkErrorMacro(<<"GetValues: Output array is null!");
+    return;
+    }
+
+  vtkStringArray *output = vtkStringArray::SafeDownCast(aa);
+
+  if (output == NULL)
+    {
+    vtkErrorMacro(<< "Can't copy values from a string array into an array "
+                  << "of type " << aa->GetDataTypeAsString());
+    return;
+    }
+
+  for (vtkIdType i = 0; i < indices->GetNumberOfIds(); ++i)
+    {
+    vtkIdType index = indices->GetId(i);
+    output->SetValue(i, this->GetValue(index));
+    }
+}
+
+// ----------------------------------------------------------------------
+
+void
+vtkStringArray::GetValues(vtkIdType startIndex,
+                          vtkIdType endIndex, 
+                          vtkAbstractArray *aa)
+{
+  if (aa == NULL)
+    {
+    vtkErrorMacro(<<"GetValues: Output array is null!");
+    return;
+    }
+
+  vtkStringArray *output = vtkStringArray::SafeDownCast(aa);
+
+  if (output == NULL)
+    {
+    vtkErrorMacro(<< "Can't copy values from a string array into an array "
+                  << "of type " << aa->GetDataTypeAsString());
+    return;
+    }
+
+  for (vtkIdType i = 0; i < (endIndex - startIndex) + 1; ++i)
+    {
+    vtkIdType index = startIndex + i;
+    output->SetValue(i, this->GetValue(index));
+    }
+}
+
+
+// ----------------------------------------------------------------------
+
+void
+vtkStringArray::CopyValue(int toIndex, int fromIndex,
+                          vtkAbstractArray *source)
+{
+  if (source == NULL)
+    {
+    vtkErrorMacro(<<"CopyValue: Input array is null!");
+    return;
+    }
+
+  vtkStringArray *realSource = vtkStringArray::SafeDownCast(source);
+
+  if (realSource == NULL)
+    {
+    vtkErrorMacro(<< "Can't copy values from an array of type " 
+                  << source->GetDataTypeAsString()
+                  << " into a string array!");
+    return;
+    }
+
+  this->SetValue(toIndex, realSource->GetValue(fromIndex));
+}
+
+// ----------------------------------------------------------------------
+
+void
+vtkStringArray::ConvertToContiguous(vtkDataArray **Data, 
+                                    vtkIdTypeArray **Offsets)
+{
+  vtkCharArray *data = vtkCharArray::New();
+  vtkIdTypeArray *offsets = vtkIdTypeArray::New();
+  int currentPosition = 0;
+
+  for (vtkIdType i = 0; i < this->GetNumberOfValues(); ++i)
+    {
+    vtkStdString thisString = this->Array[i];
+    for (unsigned int j = 0; j < this->Array[i].length(); ++j)
+      {
+      data->InsertNextValue(thisString[j]);
+      ++currentPosition;
+      }
+    offsets->InsertNextValue(currentPosition); 
+    }
+  
+  *Data = data;
+  *Offsets = offsets;
+}
+
+// ----------------------------------------------------------------------
+
+// This will work with any sort of data array, but if you call it with
+// anything other than a char array you might get strange results.
+// You have been warned...
+
+void
+vtkStringArray::ConvertFromContiguous(vtkDataArray *Data,
+                                      vtkIdTypeArray *Offsets)
+{
+  this->Reset();
+
+  vtkIdType currentStringStart = 0;
+
+  for (vtkIdType i = 0; i < Offsets->GetNumberOfTuples(); ++i)
+    {
+    // YOU ARE HERE
+    vtkStdString newString;
+    vtkIdType stringEnd = Offsets->GetValue(i);
+
+    for (vtkIdType here = currentStringStart; 
+         here < stringEnd;
+         ++here)
+      {
+      newString.push_back(static_cast<char>(Data->GetTuple1(here)));
+      }
+    this->InsertNextValue(newString);
+    currentStringStart = stringEnd;
+    }
+}
+
+
+
+// ----------------------------------------------------------------------
+
+// 
+//
+// Below here are interface methods to allow values to be inserted as
+// const char * instead of vtkStdString.  Yes, they're trivial.  The
+// wrapper code needs them.
+//
+//
+
+
+void
+vtkStringArray::SetValue( vtkIdType id, const char *value )
+{
+  this->SetValue( id, vtkStdString(value) );
+}
+
+void
+vtkStringArray::InsertValue( vtkIdType id, const char *value )
+{
+  this->InsertValue( id, vtkStdString( value ) );
+}
+
+vtkIdType
+vtkStringArray::InsertNextValue( const char *value )
+{
+  return this->InsertNextValue( vtkStdString( value ) );
+}
+
+// ----------------------------------------------------------------------
---
+++ Common/vtkStringArray.h
@@ -0,0 +1,215 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkStringArray.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright 2004 Sandia Corporation.
+  Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+  license for use of this work by or on behalf of the
+  U.S. Government. Redistribution and use in source and binary forms, with
+  or without modification, are permitted provided that this Notice and any
+  statement of authorship are reproduced on all copies.
+
+=========================================================================*/
+
+// .NAME vtkStringArray - Subclass of vtkAbstractArray that holds vtkStdStrings
+//
+// .SECTION Description
+// Points and cells may sometimes have associated data that are stored
+// as strings, e.g. many information visualization projects.  This
+// class provides a reasonably clean way to store and access those.
+//
+//
+// .SECTION Caveats
+// 
+// Wrapping support for the Set/Get/Insert methods is a little
+// strange.  The Tcl/Python/Java wrappers treat vtkStdString as const
+// char * right now instead of dealing with native strings.  This is
+// why there are two versions of every Set method: one (which you
+// should use in C++ code) taking a vtkStdString argument and one
+// (used by the wrapper) taking a const char * argument.
+//
+// If you'd like to look into modifying the wrappers to handle this
+// properly, go right ahead...
+//
+// .SECTION Thanks
+// Andy Wilson (atwilso@sandia.gov) wrote this class.
+
+#ifndef __vtkStringArray_h
+#define __vtkStringArray_h
+
+#include "vtkAbstractArray.h"
+#include "vtkStdString.h" // needed for vtkStdString definition
+
+class VTK_COMMON_EXPORT vtkStringArray : public vtkAbstractArray
+{
+public:
+  static vtkStringArray* New();
+  vtkTypeRevisionMacro(vtkStringArray,vtkAbstractArray);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  //
+  // 
+  // Functions required by vtkAbstractArray
+  //
+  //
+
+  // Description:
+  // Get the data type.
+  int GetDataType()
+    { return VTK_STRING; }
+
+  bool IsNumeric() { return false; } 
+
+  // Description:
+  // Release storage and reset array to initial state.
+  void Initialize();
+
+  // Description:
+  // Return the size of the data type.  WARNING: This may not mean
+  // what you expect with strings.  It will return
+  // sizeof(vtkstd::string) and not take into account the data
+  // included in any particular string.
+  int GetDataTypeSize();
+
+  // Description:
+  // Free any unnecessary memory.
+  // Resize object to just fit data requirement. Reclaims extra memory.
+  void Squeeze() { this->ResizeAndExtend (this->MaxId+1); }
+
+  // Description:
+  // Resize the array while conserving the data.
+  void Resize(vtkIdType numTuples);
+
+  // Description:
+  // Given a list of indices, return an array of values.  You must
+  // insure that the output array has been previously allocated with
+  // enough space to hold the data and that the types match
+  // sufficiently to allow conversion (if necessary).
+  void GetValues(vtkIdList *ptIds, vtkAbstractArray *output);
+
+  // Description:
+  // Get the values for the range of indices specified (i.e.,
+  // p1->p2 inclusive). You must insure that the output array has been
+  // previously allocated with enough space to hold the data and that
+  // the type of the output array is compatible with the type of this
+  // array.
+  void GetValues(vtkIdType p1, vtkIdType p2, vtkAbstractArray *output);
+
+  // Description:
+  // Copy a value from a given source array into this array.
+  void CopyValue(int toIndex, int fromIndex, vtkAbstractArray *sourceArray);
+
+  // Description:
+  // Allocate memory for this array. Delete old storage only if necessary.
+  // Note that ext is no longer used.
+  int Allocate( vtkIdType sz, vtkIdType ext=1000 );
+
+  // Description:
+  // Get the data at a particular index.
+  vtkStdString GetValue(vtkIdType id);
+
+  // Description:
+  // Set the data at a particular index. Does not do range checking. Make sure
+  // you use the method SetNumberOfValues() before inserting data.
+//BTX
+  void SetValue(vtkIdType id, vtkStdString value)
+    { this->Array[id] = value; }
+//ETX
+  void SetValue(vtkIdType id, const char *value);
+
+  // Description:
+  // Specify the number of values for this object to hold. Does an
+  // allocation as well as setting the MaxId ivar. Used in conjunction with
+  // SetValue() method for fast insertion.
+  void SetNumberOfValues(vtkIdType number);
+
+  int GetNumberOfValues() { return this->MaxId + 1; }
+
+  int GetNumberOfElementComponents() { return 0; }
+  int GetElementComponentSize() { return static_cast<int>(sizeof(vtkStdString::value_type)); }
+
+  // Description:
+  // Insert data at a specified position in the array.
+//BTX
+  void InsertValue(vtkIdType id, vtkStdString f);
+//ETX
+  void InsertValue(vtkIdType id, const char *val);
+
+  // Description:
+  // Insert data at the end of the array. Return its location in the array.
+//BTX
+  vtkIdType InsertNextValue(vtkStdString f);
+//ETX
+  vtkIdType InsertNextValue(const char *f);
+
+//BTX
+  // Description:
+  // Get the address of a particular data index. Make sure data is allocated
+  // for the number of items requested. Set MaxId according to the number of
+  // data values requested.
+  vtkStdString* WritePointer(vtkIdType id, vtkIdType number);
+//ETX
+
+//BTX
+  // Description:
+  // Get the address of a particular data index. Performs no checks
+  // to verify that the memory has been allocated etc.
+  vtkStdString* GetPointer(vtkIdType id) { return this->Array + id; }
+  void* GetVoidPointer(vtkIdType id) { return this->GetPointer(id); }
+//BTX
+
+  // Description:
+  // Deep copy of another string array.  Will complain and change nothing
+  // if the array passed in is not a vtkStringArray.
+  void DeepCopy( vtkAbstractArray* aa );
+
+
+//BTX
+  // Description:
+  // This method lets the user specify data to be held by the array.  The
+  // array argument is a pointer to the data.  size is the size of
+  // the array supplied by the user.  Set save to 1 to keep the class
+  // from deleting the array when it cleans up or reallocates memory.
+  // The class uses the actual array provided; it does not copy the data
+  // from the suppled array.
+  void SetArray(vtkStdString* array, vtkIdType size, int save);
+  void SetVoidArray(void* array, vtkIdType size, int save)
+    { this->SetArray(static_cast<vtkStdString*>(array), size, save); }
+//ETX
+
+  // Description:
+  // Return the memory in kilobytes consumed by this data array. Used to
+  // support streaming and reading/writing data. The value returned is
+  // guaranteed to be greater than or equal to the memory required to
+  // actually represent the data represented by this object. The 
+  // information returned is valid only after the pipeline has 
+  // been updated.
+  //
+  // This function takes into account the size of the contents of the
+  // strings as well as the string containers themselves.
+  unsigned long GetActualMemorySize();
+
+  void ConvertToContiguous(vtkDataArray **Data, vtkIdTypeArray **Offsets);
+  void ConvertFromContiguous(vtkDataArray *Data, vtkIdTypeArray *Offsets);
+
+protected:
+  vtkStringArray(vtkIdType numComp=1);
+  ~vtkStringArray();
+
+  vtkStdString* Array;   // pointer to data
+  vtkStdString* ResizeAndExtend(vtkIdType sz);  // function to resize data
+
+  int SaveUserArray;
+
+private:
+  vtkStringArray(const vtkStringArray&);  // Not implemented.
+  void operator=(const vtkStringArray&);  // Not implemented.
+};
+
+
+
+#endif
