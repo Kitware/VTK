dfdc38026ed1bb5826525646233dff1d9f783c82
Andrew Maclean <a.maclean@cas.edu.au>
2005-05-23 19:55:49 -0400

    COMP:  Fixing a MSV6 compiler warning. Removed the last else statement in DetermineFileType() and  replaced it with a return statement.

--- IO/vtkParticleReader.cxx
+++ IO/vtkParticleReader.cxx
@@ -32,7 +32,7 @@
 #include <vtkstd/vector>
 #include <vtkstd/string>
 
-vtkCxxRevisionMacro(vtkParticleReader, "1.26");
+vtkCxxRevisionMacro(vtkParticleReader, "1.27");
 vtkStandardNewMacro(vtkParticleReader);
 
 namespace {
@@ -349,10 +349,8 @@ int vtkParticleReader::DetermineFileType()
     {
     return FILE_TYPE_IS_TEXT;
     }
-  else
-    {
-    return FILE_TYPE_IS_BINARY;
-    }
+
+  return FILE_TYPE_IS_BINARY;
 }
 
 //----------------------------------------------------------------------------
