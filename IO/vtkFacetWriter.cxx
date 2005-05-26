c22efd329c3e8d755e696e9fbe94a125b2c3e9e3
Brad King <brad.king@kitware.com>
2005-05-26 10:28:04 -0400

    COMP: Fixed unreachable code warning.

--- IO/vtkFacetWriter.cxx
+++ IO/vtkFacetWriter.cxx
@@ -34,7 +34,7 @@
 #include <vtkstd/string>
 #include <vtkstd/vector>
 
-vtkCxxRevisionMacro(vtkFacetWriter, "1.4");
+vtkCxxRevisionMacro(vtkFacetWriter, "1.5");
 vtkStandardNewMacro(vtkFacetWriter);
 
 //----------------------------------------------------------------------------
@@ -153,11 +153,15 @@ int vtkFacetWriter::WriteDataToStream(ostream* ost, vtkPolyData* data)
 
   if ( data->GetVerts()->GetNumberOfCells() )
     {
-    if ( written )
-      {
-      vtkErrorMacro("Multiple different cells in the poly data");
-      return 0;
-      }
+    // This test is needed if another cell type is written above this
+    // block.  We must remove it here because it produces an
+    // unreachable code warning.
+    //
+    //if ( written )
+    //  {
+    //  vtkErrorMacro("Multiple different cells in the poly data");
+    //  return 0;
+    //  }
     ca = data->GetVerts();
     numCells = ca->GetNumberOfCells();
     vtkIdType numPts = 0;
