a41f46225fc55fae32b1722df84f9de340e3887a
Brad King <brad.king@kitware.com>
2005-05-26 10:23:41 -0400

    COMP: Added missing initializer to local variable.

--- Parallel/vtkInputPort.cxx
+++ Parallel/vtkInputPort.cxx
@@ -28,7 +28,7 @@
 #include "vtkStructuredPoints.h"
 #include "vtkUnstructuredGrid.h"
 
-vtkCxxRevisionMacro(vtkInputPort, "1.20");
+vtkCxxRevisionMacro(vtkInputPort, "1.21");
 vtkStandardNewMacro(vtkInputPort);
 
 vtkCxxSetObjectMacro(vtkInputPort,Controller, vtkMultiProcessController);
@@ -306,7 +306,7 @@ int vtkInputPort::RequestDataObject(
   this->Controller->TriggerRMI(this->RemoteProcessId, this->Tag+2);
   
   // Now receive the information
-  int dataType;
+  int dataType = 0;
   this->Controller->Receive( &dataType, 1, 
                              this->RemoteProcessId,
                              vtkInputPort::DATA_TYPE_TAG);
