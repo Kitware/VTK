1c6f8364ab2a978f724bdf1e0c518567c923ee7f
Lisa Avila <lisa.avila@kitware.com>
2005-06-01 09:35:54 -0400

    BUG: Ooops - moved method to public but forgot to remove it from protected...

--- Rendering/vtkMesaRenderWindow.h
+++ Rendering/vtkMesaRenderWindow.h
@@ -115,7 +115,6 @@ protected:
   vtkIdList *TextureResourceIds;
 
   int GetPixelData(int x,int y,int x2,int y2,int front, unsigned char* data);
-  int GetZbufferData( int x1, int y1, int x2, int y2, float* z );
   int GetRGBAPixelData(int x,int y,int x2,int y2, int front, float* data);
   int GetRGBACharPixelData(int x,int y,int x2,int y2, int front,
                            unsigned char* data);
