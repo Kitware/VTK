f91c776b2435757b18a7a54d34ceb0b1380d8141
Andy Cedilnik <andy.cedilnik@kitware.com>
2005-07-17 11:45:04 -0400

    ENH: Add Exodus2

---
+++ Utilities/vtk_netcdf.h
@@ -0,0 +1,21 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtk_netcdf.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#ifndef __vtk_netcdf_h
+#define __vtk_netcdf_h
+
+/* Use the netcdf library configured for VTK.  */
+#include <vtknetcdf/netcdf.h>
+
+#endif
