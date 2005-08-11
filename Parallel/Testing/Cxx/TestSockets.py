b496d98336f31845712dd5d479196bf5db96cbab
Berk Geveci <berk.geveci@kitware.com>
2005-08-11 17:02:54 -0400

    BUG: Test should take temporary dir as argument

--- Parallel/Testing/Cxx/TestSockets.py
+++ Parallel/Testing/Cxx/TestSockets.py
@@ -12,8 +12,8 @@ else:
     # wait a little to make sure that the server is ready
     time.sleep(10)
     # run the client
-    retVal = os.system('"%s" -D "%s" -V "%s"' % ( sys.argv[2], sys.argv[3],
-                                            sys.argv[4] ))
+    retVal = os.system('"%s" -D "%s" -V "%s" -T "%s"' %
+                       ( sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5] ))
     # in case the client fails, we need to kill the server
     # or it will stay around
     time.sleep(20)
