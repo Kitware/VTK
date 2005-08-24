f2511d86c430cb28baaf55d169f3faa02043a034
Brad King <brad.king@kitware.com>
2005-08-24 19:27:11 -0400

    BUG: Fix for display in Qt 4.0.1 designer during interaction (from Clinton Stimpson).

--- GUISupport/Qt/Q4VTKWidgetPlugin.cxx
+++ GUISupport/Qt/Q4VTKWidgetPlugin.cxx
@@ -65,7 +65,16 @@ QString QVTKWidgetPlugin::name() const
 
 QString QVTKWidgetPlugin::domXml() const
 {
-  return QLatin1String("<widget class=\"QVTKWidget\" name=\"qvtkWidget\"></widget>");
+  return QLatin1String("<widget class=\"QVTKWidget\" name=\"qvtkWidget\">\n"
+                       " <property name=\"geometry\">\n"
+                       "  <rect>\n"
+                       "   <x>0</x>\n"
+                       "   <y>0</y>\n"
+                       "   <width>100</width>\n"
+                       "   <height>100</height>\n"
+                       "  </rect>\n"
+                       " </property>\n"
+                       "</widget>\n");
 }
 
 QWidget* QVTKWidgetPlugin::createWidget(QWidget* parent)
