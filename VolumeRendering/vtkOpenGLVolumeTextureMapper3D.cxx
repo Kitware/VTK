3ef9221b495a88edf2f4aa2c7f8ecd37383a1822
Brad King <brad.king@kitware.com>
2005-08-30 14:58:46 -0400

    ENH: Removed need for CMake 2.0 VolumeRendering asm headers hack by changing the headers to .cxx files.  The needed symbols are extern-declared where they are used.  The generated sources are no longer #include-d so they are now sources and not headers.

--- VolumeRendering/vtkOpenGLVolumeTextureMapper3D.cxx
+++ VolumeRendering/vtkOpenGLVolumeTextureMapper3D.cxx
@@ -33,12 +33,12 @@
 #include "vtkOpenGLExtensionManager.h"
 #include "vtkgl.h"
 
-#include "vtkVolumeTextureMapper3D_OneComponentShadeFP.h"
-#include "vtkVolumeTextureMapper3D_OneComponentNoShadeFP.h"
-#include "vtkVolumeTextureMapper3D_TwoDependentNoShadeFP.h"
-#include "vtkVolumeTextureMapper3D_TwoDependentShadeFP.h"
-#include "vtkVolumeTextureMapper3D_FourDependentNoShadeFP.h"
-#include "vtkVolumeTextureMapper3D_FourDependentShadeFP.h"
+extern const char* vtkVolumeTextureMapper3D_OneComponentShadeFP;
+extern const char* vtkVolumeTextureMapper3D_OneComponentNoShadeFP;
+extern const char* vtkVolumeTextureMapper3D_TwoDependentNoShadeFP;
+extern const char* vtkVolumeTextureMapper3D_TwoDependentShadeFP;
+extern const char* vtkVolumeTextureMapper3D_FourDependentNoShadeFP;
+extern const char* vtkVolumeTextureMapper3D_FourDependentShadeFP;
 
 //extern "C" void (*glXGetProcAddressARB(const GLubyte *procName))( void );
 
@@ -66,7 +66,7 @@
     }}
 
 //#ifndef VTK_IMPLEMENT_MESA_CXX
-vtkCxxRevisionMacro(vtkOpenGLVolumeTextureMapper3D, "1.6");
+vtkCxxRevisionMacro(vtkOpenGLVolumeTextureMapper3D, "1.7");
 vtkStandardNewMacro(vtkOpenGLVolumeTextureMapper3D);
 //#endif
 
