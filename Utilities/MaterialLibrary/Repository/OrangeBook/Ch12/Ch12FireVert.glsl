9baa9986c65346056b2eb5b606c2f385ad57e0b6
Gary Templet <gjtempl@sandia.gov>
2005-10-13 14:20:54 -0400

    ENH: Materials and Shaders from Chapter 12 of      "OpenGL Shading Language", Randi J Rost,

---
+++ Utilities/MaterialLibrary/Repository/OrangeBook/Ch12/Ch12FireVert.glsl
@@ -0,0 +1,25 @@
+//
+// Vertex shader for producing a turbulent fire effect
+//
+// Authors: John Kessenich, Randi Rost
+//
+// Copyright (c) 2002-2004 3Dlabs Inc. Ltd. 
+//
+// See 3Dlabs-License.txt for license information
+//
+
+varying float LightIntensity;
+varying vec3  MCposition;
+
+uniform vec3  LightPos;
+uniform float Scale;
+
+void main(void)
+{
+    vec3 ECposition = vec3 (gl_ModelViewMatrix * gl_Vertex);
+    MCposition      = vec3 (gl_Vertex) * Scale;
+    vec3 tnorm      = normalize(vec3 (gl_NormalMatrix * gl_Normal));
+    LightIntensity  = dot(normalize(LightPos - ECposition), tnorm);
+    LightIntensity *= 1.5;
+    gl_Position     = ftransform();
+}
---
+++ Utilities/MaterialLibrary/Repository/OrangeBook/Ch12/Ch12GraniteFrag.glsl
@@ -0,0 +1,24 @@
+//
+// Fragment shader for producing a granite effect
+//
+// Author: Randi Rost
+//
+// Copyright (c) 2002-2004 3Dlabs Inc. Ltd. 
+//
+// See 3Dlabs-License.txt for license information
+//
+
+varying float LightIntensity; 
+varying vec3  MCposition;
+
+uniform sampler3D Noise;
+uniform float NoiseScale;
+
+void main(void)
+{
+    vec4  noisevec  = texture3D(Noise, NoiseScale * MCposition);
+    float intensity = min(1.0, noisevec[3] * 18.0);
+    vec3  color     = vec3 (intensity * LightIntensity);
+    
+    gl_FragColor    = vec4 (color, 1.0);
+}
