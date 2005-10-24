494b7cae1fbcc32a1e1e7e3613fe0f065dadaecf
Brad King <brad.king@kitware.com>
2005-10-24 18:33:37 -0400

    COMP: Fixed uninitialized variable warning.  In fact the case the compiler was considering can never happen anyway.

--- Filtering/vtkCompositeDataPipeline.cxx
+++ Filtering/vtkCompositeDataPipeline.cxx
@@ -35,7 +35,7 @@ PURPOSE.  See the above copyright notice for more information.
 #include "vtkStructuredGrid.h"
 #include "vtkUniformGrid.h"
 
-vtkCxxRevisionMacro(vtkCompositeDataPipeline, "1.31");
+vtkCxxRevisionMacro(vtkCompositeDataPipeline, "1.32");
 vtkStandardNewMacro(vtkCompositeDataPipeline);
 
 vtkInformationKeyMacro(vtkCompositeDataPipeline,BEGIN_LOOP,Integer);
@@ -701,8 +701,8 @@ int vtkCompositeDataPipeline::UpdateBlocks(
   vtkInformation* inInfo)
 {
   // Get the producer and its output port for input connection i,j.
-  vtkExecutive* producer;
-  int producerPort;
+  vtkExecutive* producer = 0;
+  int producerPort = -1;
   if(vtkInformation* info = this->GetInputInformation(i, j))
     {
     // Get the executive producing this input.  If there is none, then
