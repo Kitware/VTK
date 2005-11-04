926bd7ad97da75119d9d7d9125af29053b8ec0b2
David Cole <david.cole@kitware.com>
2005-11-04 14:07:52 -0500

    ENH: Merge changes from main tree into VTK-5-0 branch. (cvs -q up -j1.11 -j1.13 Common/vtkSmartPointer.h)

--- Common/vtkSmartPointer.h
+++ Common/vtkSmartPointer.h
@@ -99,6 +99,29 @@ public:
     {
     return vtkSmartPointer<T>(t->NewInstance(), NoReference());
     }
+
+  // Work-around for HP overload resolution bug.  Since
+  // NullPointerOnly is a private type the only pointer value that can
+  // be passed by user code is a null pointer.  This operator will be
+  // chosen by the compiler when comparing against null explicitly and
+  // avoid the bogus ambiguous overload error.
+#if defined(__HP_aCC)
+# define VTK_SMART_POINTER_DEFINE_OPERATOR_WORKAROUND(op) \
+  vtkstd_bool operator op (NullPointerOnly*) const        \
+    {                                                     \
+    return ::operator op (*this, 0);                      \
+    }
+private:
+  class NullPointerOnly {};
+public:
+  VTK_SMART_POINTER_DEFINE_OPERATOR_WORKAROUND(==)
+  VTK_SMART_POINTER_DEFINE_OPERATOR_WORKAROUND(!=)
+  VTK_SMART_POINTER_DEFINE_OPERATOR_WORKAROUND(<)
+  VTK_SMART_POINTER_DEFINE_OPERATOR_WORKAROUND(<=)
+  VTK_SMART_POINTER_DEFINE_OPERATOR_WORKAROUND(>)
+  VTK_SMART_POINTER_DEFINE_OPERATOR_WORKAROUND(>=)
+# undef VTK_SMART_POINTER_DEFINE_OPERATOR_WORKAROUND
+#endif
 protected:
   vtkSmartPointer(T* r, const NoReference& n): vtkSmartPointerBase(r, n) {}
 };
