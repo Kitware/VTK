5a78b73c4a1623f7dab8d253f599b6c49be5c601
David Cole <david.cole@kitware.com>
2005-11-08 10:49:07 -0500

    ENH: Merge changes from main tree into VTK-5-0 branch. (cvs -q up -j1.1 -j1.2 Examples/Build/vtkLocal/vtkLocalExample.cxx)

--- Examples/Build/vtkLocal/vtkLocalExample.cxx
+++ Examples/Build/vtkLocal/vtkLocalExample.cxx
@@ -3,8 +3,10 @@ This source has no copyright.  It is intended to be copied by users
 wishing to create their own VTK classes locally.
 =========================================================================*/
 #include "vtkLocalExample.h"
+
 #include "vtkObjectFactory.h"
 
+vtkCxxRevisionMacro(vtkLocalExample, "1.1.34.1");
 vtkStandardNewMacro(vtkLocalExample);
 
 //----------------------------------------------------------------------------
