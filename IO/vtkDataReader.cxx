726d599972e1ff2a4b0054ec0e8826a5a0cf72ef
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2005-11-16 11:25:34 -0500

    BUG: Fix Bug #1672 - Rounding error in vtkDataReader

--- IO/vtkDataReader.cxx
+++ IO/vtkDataReader.cxx
@@ -41,7 +41,7 @@
 #include <ctype.h>
 #include <sys/stat.h>
 
-vtkCxxRevisionMacro(vtkDataReader, "1.133");
+vtkCxxRevisionMacro(vtkDataReader, "1.134");
 vtkStandardNewMacro(vtkDataReader);
 
 vtkCxxSetObjectMacro(vtkDataReader, InputArray, vtkCharArray);
@@ -1449,7 +1449,7 @@ int vtkDataReader::ReadCoScalarData(vtkDataSetAttributes *a, int numPts)
           for (j=0; j<numComp; j++)
             {
             idx = i*numComp + j;
-            scalars->SetValue(idx,(unsigned char)(255.0*data->GetValue(idx)));
+            scalars->SetValue(idx,(unsigned char)(255.0*data->GetValue(idx)+0.5));
             }
           }
         if ( ! skipScalar )
