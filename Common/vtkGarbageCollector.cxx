9554f172ec99fc6d4f4490767d6740d59c425cf8
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2005-11-25 15:10:58 -0500

    COMP: Fix warning on gcc295

--- Common/vtkGarbageCollector.cxx
+++ Common/vtkGarbageCollector.cxx
@@ -35,7 +35,7 @@
 
 #include <assert.h>
 
-vtkCxxRevisionMacro(vtkGarbageCollector, "1.29");
+vtkCxxRevisionMacro(vtkGarbageCollector, "1.30");
 vtkStandardNewMacro(vtkGarbageCollector);
 
 #if VTK_GARBAGE_COLLECTOR_HASH
@@ -506,10 +506,6 @@ void vtkGarbageCollectorImpl::FindComponents(vtkObjectBase* root)
 }
 
 //----------------------------------------------------------------------------
-#if defined(_MSC_VER) && _MSC_VER < 1300
-# pragma warning (push)
-# pragma warning (disable: 4702) // bogus unreachable code warning
-#endif
 vtkGarbageCollectorImpl::Entry*
 vtkGarbageCollectorImpl::MaybeVisit(vtkObjectBase* obj)
 {
@@ -522,15 +518,9 @@ vtkGarbageCollectorImpl::MaybeVisit(vtkObjectBase* obj)
     // Visit the object to create the entry.
     return this->VisitTarjan(obj);
     }
-  else
-    {
-    // Return the existing entry.
-    return *i;
-    }
+  // else Return the existing entry.
+  return *i;
 }
-#if defined(_MSC_VER) && _MSC_VER < 1300
-# pragma warning (pop)
-#endif
 
 //----------------------------------------------------------------------------
 vtkGarbageCollectorImpl::Entry*
