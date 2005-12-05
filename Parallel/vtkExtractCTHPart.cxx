aa3ff0ffc032677c17595d5b4f6007c719985e5b
Berk Geveci <berk.geveci@kitware.com>
2005-12-05 11:08:38 -0500

    ENH: Pass the volume fraction data

--- Parallel/vtkExtractCTHPart.cxx
+++ Parallel/vtkExtractCTHPart.cxx
@@ -49,7 +49,7 @@
 #include <vtkstd/vector>
 #include <assert.h>
 
-vtkCxxRevisionMacro(vtkExtractCTHPart, "1.11");
+vtkCxxRevisionMacro(vtkExtractCTHPart, "1.12");
 vtkStandardNewMacro(vtkExtractCTHPart);
 vtkCxxSetObjectMacro(vtkExtractCTHPart,ClipPlane,vtkPlane);
 vtkCxxSetObjectMacro(vtkExtractCTHPart,Controller,vtkMultiProcessController);
@@ -852,9 +852,6 @@ void vtkExtractCTHPart::ExecutePartOnUniformGrid(
   
   this->Data->CopyStructure(input);
 
-  // Do not pass cell volume fraction data.
-  this->Data->GetCellData()->CopyFieldOff(arrayName);
-  
   vtkDataArray* scalars = input->GetCellData()->GetScalars();
   if (scalars && strcmp(arrayName, scalars->GetName()) == 0)
     { // I do not know why the reader sets attributes, but ....
@@ -1124,9 +1121,6 @@ void vtkExtractCTHPart::ExecutePartOnRectilinearGrid(
  
   this->RData->CopyStructure(input);
 
-  // Do not pass cell volume fraction data.
-  this->RData->GetCellData()->CopyFieldOff(arrayName);
-  
   vtkDataArray* scalars = input->GetCellData()->GetScalars();
   if (scalars && strcmp(arrayName, scalars->GetName()) == 0)
     { // I do not know why the reader sets attributes, but ....
