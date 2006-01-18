50bd8b79070f85aad1052dc6cd2e806575dbfd4b
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-01-18 18:52:15 -0500

    STYLE: Fix doc

--- Filtering/vtkImageAlgorithm.h
+++ Filtering/vtkImageAlgorithm.h
@@ -18,9 +18,9 @@
 // pipeline  complexity. It handles breaking the pipeline execution 
 // into smaller extents so that the vtkImageData limits are observed. It 
 // also provides support for multithreading. If you don't need any of this
-// functionality, consider using vtkSimpleImageToImageAlgorithm instead.
+// functionality, consider using vtkSimpleImageToImageFilter instead.
 // .SECTION See also
-// vtkSimpleImageToImageAlgorithm
+// vtkSimpleImageToImageFilter
 
 #ifndef __vtkImageAlgorithm_h
 #define __vtkImageAlgorithm_h
