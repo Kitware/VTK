4476d587a9c5985896a64cd0ff9495aa71fb8041
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2006-02-07 16:35:05 -0500

    STYLE: reverting to comments from the previous version, since removed the new opertor changes in vtkDataArrayTemplate.txx

--- Common/vtkDataArrayTemplate.h
+++ Common/vtkDataArrayTemplate.h
@@ -178,9 +178,7 @@ public:
   // the array supplied by the user.  Set save to 1 to keep the class
   // from deleting the array when it cleans up or reallocates memory.
   // The class uses the actual array provided; it does not copy the data
-  // from the suppled array. If save is 0, then this class is free to delete
-  // the array when it cleans up or reallocates. In that case, it is required 
-  // that the array was allocated using the C++ new operator (and not malloc).
+  // from the suppled array. 
   void SetArray(T* array, vtkIdType size, int save);
   virtual void SetVoidArray(void* array, vtkIdType size, int save)
     { this->SetArray(static_cast<T*>(array), size, save); }
