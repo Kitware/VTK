a24676e21b95f4e596c1cc145edb8b6b4b729cd4
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-02-09 18:05:32 -0500

    BUG: Need both avcodec and avformat

--- CMake/FindFFMPEG.cmake
+++ CMake/FindFFMPEG.cmake
@@ -7,7 +7,7 @@
 # FFMPEG_FOUND, If false, do not try to use FFMPEG.
 
 # also defined, but not for general use are
-# FFMPEG_LIBRARY, where to find the FFMPEG library.
+# FFMPEG_avformat_LIBRARY and FFMPEG_avcodec_LIBRARY, where to find the FFMPEG library.
 # This is usefull to do it this way so that we can always add more libraries
 # if needed to FFMPEG_LIBRARIES if ffmpeg ever changes...
 
@@ -16,7 +16,11 @@ FIND_PATH(FFMPEG_INCLUDE_DIR ffmpeg/avformat.h
   /usr/include
 )
 
-FIND_LIBRARY(FFMPEG_LIBRARY avformat
+FIND_LIBRARY(FFMPEG_avformat_LIBRARY avformat
+  /usr/local/lib
+  /usr/lib
+)
+FIND_LIBRARY(FFMPEG_avcodec_LIBRARY avcodec
   /usr/local/lib
   /usr/lib
 )
@@ -24,7 +28,7 @@ FIND_LIBRARY(FFMPEG_LIBRARY avformat
 IF(FFMPEG_INCLUDE_DIR)
   IF(FFMPEG_LIBRARY)
     SET( FFMPEG_FOUND "YES" )
-    SET( FFMPEG_LIBRARIES ${FFMPEG_LIBRARY} )
+    SET( FFMPEG_LIBRARIES ${FFMPEG_avcodec_LIBRARY} ${FFMPEG_avformat_LIBRARY} )
   ENDIF(FFMPEG_LIBRARY)
 ENDIF(FFMPEG_INCLUDE_DIR)
 
