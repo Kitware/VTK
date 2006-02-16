b62ce641d1000639f8041c00b18b7251948d40a9
Dave Demarle <dave.demarle@kitware.com>
2006-02-16 11:30:34 -0500

    BUG: FFMPEG linking fixes. Give ParaView access to VTK's FindFFMPEG. Don't fail if FIND can't find the library, instead give the user a chance to specify where it is. Don't automatically link against the library if it is found, because FFMPEG is likely to be too old to use. Give the user one variable to control whether it should be linked, and only show that if the lib is found.

--- CMake/FindFFMPEG.cmake
+++ CMake/FindFFMPEG.cmake
@@ -26,6 +26,11 @@ FIND_LIBRARY(FFMPEG_avcodec_LIBRARY avcodec
   /usr/lib
 )
 
+FIND_LIBRARY(FFMPEG_avutil_LIBRARY avutil
+  /usr/local/lib
+  /usr/lib
+)
+
 FIND_LIBRARY(FFMPEG_vorbis_LIBRARY vorbis
   /usr/local/lib
   /usr/lib
@@ -42,11 +47,6 @@ FIND_LIBRARY(FFMPEG_vorbisenc_LIBRARY vorbisenc
 )
 
 FIND_LIBRARY(FFMPEG_theora_LIBRARY theora
-  /usr/local/lib
-  /usr/lib
-)
-
-FIND_LIBRARY(FFMPEG_avutil_LIBRARY avutil
   /usr/local/lib
   /usr/lib
 )
@@ -68,36 +68,38 @@ FIND_LIBRARY(FFMPEG_z_LIBRARY z
 
 
 IF(FFMPEG_INCLUDE_DIR)
-  IF(FFMPEG_avcodec_LIBRARY)
-    IF(FFMPEG_avformat_LIBRARY)
-      SET( FFMPEG_FOUND "YES" )
-      SET( FFMPEG_BASIC_LIBRARIES 
-        ${FFMPEG_avcodec_LIBRARY} 
-        ${FFMPEG_avformat_LIBRARY}
-        )
-      SET( FFMPEG_LIBRARIES 
-        ${FFMPEG_BASIC_LIBRARIES}
-        ${FFMPEG_vorbis_LIBRARY} 
-        ${FFMPEG_dc1394_LIBRARY} 
-        ${FFMPEG_vorbisenc_LIBRARY} 
-        ${FFMPEG_theora_LIBRARY} 
-        ${FFMPEG_avutil_LIBRARY} 
-        ${FFMPEG_dts_LIBRARY} 
-        ${FFMPEG_gsm_LIBRARY} 
-        ${FFMPEG_z_LIBRARY})
-    ENDIF(FFMPEG_avformat_LIBRARY)
-  ENDIF(FFMPEG_avcodec_LIBRARY)
+  IF(FFMPEG_avformat_LIBRARY)
+    IF(FFMPEG_avcodec_LIBRARY)
+      IF(FFMPEG_avutil_LIBRARY)
+        SET( FFMPEG_FOUND "YES" )
+        SET( FFMPEG_BASIC_LIBRARIES 
+          ${FFMPEG_avcodec_LIBRARY} 
+          ${FFMPEG_avformat_LIBRARY}
+          ${FFMPEG_avutil_LIBRARY} 
+          )
+        SET( FFMPEG_LIBRARIES 
+          ${FFMPEG_BASIC_LIBRARIES}
+          ${FFMPEG_vorbis_LIBRARY} 
+          ${FFMPEG_dc1394_LIBRARY} 
+          ${FFMPEG_vorbisenc_LIBRARY} 
+          ${FFMPEG_theora_LIBRARY} 
+          ${FFMPEG_dts_LIBRARY} 
+          ${FFMPEG_gsm_LIBRARY} 
+          ${FFMPEG_z_LIBRARY})
+      ENDIF(FFMPEG_avutil_LIBRARY)
+    ENDIF(FFMPEG_avcodec_LIBRARY)
+  ENDIF(FFMPEG_avformat_LIBRARY)
 ENDIF(FFMPEG_INCLUDE_DIR)
 
 MARK_AS_ADVANCED(
   FFMPEG_INCLUDE_DIR
-  FFMPEG_avcodec_LIBRARY
   FFMPEG_avformat_LIBRARY
+  FFMPEG_avcodec_LIBRARY
+  FFMPEG_avutil_LIBRARY
   FFMPEG_vorbis_LIBRARY
   FFMPEG_dc1394_LIBRARY
   FFMPEG_vorbisenc_LIBRARY
   FFMPEG_theora_LIBRARY
-  FFMPEG_avutil_LIBRARY
   FFMPEG_dts_LIBRARY
   FFMPEG_gsm_LIBRARY
   FFMPEG_z_LIBRARY
