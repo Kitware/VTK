97d9020f77efad8d33e2820170da582df1336db5
Sebastien Barre <sebastien.barre@kitware.com>
2006-02-21 12:19:20 -0500

    BUG: this is required to build Python wrappers with Microsoft Visual 8/2005, it is not fooled by the _DEBUG trick anymore

--- Common/vtkPython.h
+++ Common/vtkPython.h
@@ -19,12 +19,25 @@
 
 #include "vtkToolkits.h"
 
-/* Use the real python debugging library if it is provided.  */
+/* 
+   Use the real python debugging library if it is provided.  
+   Otherwise use the "documented" trick involving checking for _DEBUG
+   and undefined that symbol while we include Python headers.
+   Update: this method does not fool Microsoft Visual C++ 8 anymore; two
+   of its header files (crtdefs.h and use_ansi.h) check if _DEBUG was set
+   or not, and set flags accordingly (_CRT_MANIFEST_RETAIL, 
+   _CRT_MANIFEST_DEBUG, _CRT_MANIFEST_INCONSISTENT). The next time the
+   check is performed in the same compilation unit, and the flags are found,
+   and error is triggered. Let's prevent that by setting _CRT_NOFORCE_MANIFEST.
+*/
 #if defined(VTK_WINDOWS_PYTHON_DEBUGGABLE)
 # include <Python.h>
 #else
 # ifdef _DEBUG
 #  undef _DEBUG
+#  if defined(_MSC_VER) && _MSC_VER >= 1400
+#    define _CRT_NOFORCE_MANIFEST 1
+#  endif
 #  include <Python.h>
 #  define _DEBUG
 # else
