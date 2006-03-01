d2f1972f0e7045f125853a15b18c0cb10af83b53
Zack Galbreath <zack.galbreath@kitware.com>
2006-03-01 15:04:23 -0500

    COMP:Fix compile errors

--- Rendering/vtkFreeTypeUtilities.cxx
+++ Rendering/vtkFreeTypeUtilities.cxx
@@ -40,7 +40,7 @@
 #define VTK_FTFC_DEBUG_CD 0
 
 //----------------------------------------------------------------------------
-vtkCxxRevisionMacro(vtkFreeTypeUtilities, "1.21");
+vtkCxxRevisionMacro(vtkFreeTypeUtilities, "1.22");
 vtkInstantiatorNewMacro(vtkFreeTypeUtilities);
 
 //----------------------------------------------------------------------------
@@ -1215,7 +1215,7 @@ int vtkFreeTypeUtilitiesRenderString(
       unsigned char *glyph_ptr_row = bitmap->buffer;
       unsigned char *glyph_ptr;
 
-      float t_alpha;
+      float t_alpha, data_alpha, t_1_m_alpha;
 
       int i, j;
       for (j = 0; j < bitmap->rows; j++)
