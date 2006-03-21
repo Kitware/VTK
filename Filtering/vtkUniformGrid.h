af9dfc5976d09c31313cc9a3d511aff2540186af
David Cole <david.cole@kitware.com>
2006-03-21 17:06:05 -0500

    ENH: Merge changes from main tree into VTK-5-0 branch. (cvs -q up -j1.9 -j1.10 Filtering/vtkUniformGrid.h)

--- Filtering/vtkUniformGrid.h
+++ Filtering/vtkUniformGrid.h
@@ -133,10 +133,12 @@ public:
 
   virtual vtkImageData* NewImageDataCopy();
 
+  //BTX
   // Description:
   // Retrieve an instance of this class from an information object.
   static vtkUniformGrid* GetData(vtkInformation* info);
   static vtkUniformGrid* GetData(vtkInformationVector* v, int i=0);
+  //ETX
 
 protected:
   vtkUniformGrid();
