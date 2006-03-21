3d269052f504d41885648ded8241a9ca8984942d
David Cole <david.cole@kitware.com>
2006-03-21 16:28:16 -0500

    ENH: Merge changes from main tree into VTK-5-0 branch. (cvs -q up -j1.47 -j1.48 Graphics/vtkShrinkFilter.h)

--- Graphics/vtkShrinkFilter.h
+++ Graphics/vtkShrinkFilter.h
@@ -14,11 +14,11 @@
 =========================================================================*/
 // .NAME vtkShrinkFilter - shrink cells composing an arbitrary data set
 // .SECTION Description
-// vtkShrinkFilter shrinks cells composing an arbitrary data set 
-// towards their centroid. The centroid of a cell is computed as 
-// the average position of the cell points. Shrinking results in 
-// disconnecting the cells from one another. The output of this filter is
-// of general dataset type vtkUnstructuredGrid.
+// vtkShrinkFilter shrinks cells composing an arbitrary data set
+// towards their centroid. The centroid of a cell is computed as the
+// average position of the cell points. Shrinking results in
+// disconnecting the cells from one another. The output of this filter
+// is of general dataset type vtkUnstructuredGrid.
 
 // .SECTION Caveats
 // It is possible to turn cells inside out or cause self intersection
@@ -40,26 +40,27 @@ public:
   void PrintSelf(ostream& os, vtkIndent indent);
 
   // Description:
-  // Set the fraction of shrink for each cell.
-  vtkSetClampMacro(ShrinkFactor,double,0.0,1.0);
-
-  // Description:
-  // Get the fraction of shrink for each cell.
-  vtkGetMacro(ShrinkFactor,double);
+  // Get/Set the fraction of shrink for each cell.
+  vtkSetClampMacro(ShrinkFactor, double, 0.0, 1.0);
+  vtkGetMacro(ShrinkFactor, double);
 
 protected:
-  vtkShrinkFilter(double sf=0.5);
-  ~vtkShrinkFilter() {};
+  vtkShrinkFilter();
+  ~vtkShrinkFilter();
 
-  virtual int RequestData(vtkInformation *, vtkInformationVector **, vtkInformationVector *);
-  virtual int FillInputPortInformation(int port, vtkInformation *info);
+  // Override to specify support for any vtkDataSet input type.
+  virtual int FillInputPortInformation(int port, vtkInformation* info);
+
+  // Main implementation.
+  virtual int RequestData(vtkInformation*,
+                          vtkInformationVector**,
+                          vtkInformationVector*);
 
   double ShrinkFactor;
+
 private:
   vtkShrinkFilter(const vtkShrinkFilter&);  // Not implemented.
   void operator=(const vtkShrinkFilter&);  // Not implemented.
 };
 
 #endif
-
-
