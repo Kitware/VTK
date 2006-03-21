068d9716b344f24a8e78d28d9fa6934c9410ebf8
David Cole <david.cole@kitware.com>
2006-03-21 16:10:02 -0500

    ENH: Merge changes from main tree into VTK-5-0 branch. (cvs -q up -j1.5 -j1.6 Filtering/vtkRectilinearGrid.h)

--- Filtering/vtkRectilinearGrid.h
+++ Filtering/vtkRectilinearGrid.h
@@ -169,6 +169,11 @@ public:
   // and the source generated more than the update extent. 
   virtual void Crop();
 
+  // Description:
+  // Retrieve an instance of this class from an information object.
+  static vtkRectilinearGrid* GetData(vtkInformation* info);
+  static vtkRectilinearGrid* GetData(vtkInformationVector* v, int i=0);
+
 protected:
   vtkRectilinearGrid();
   ~vtkRectilinearGrid();
