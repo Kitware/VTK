72409b5e203daeb963c846fd01ccb9768a3715ec
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2006-04-01 14:40:43 -0500

    BUG: backing out commits to set parent Id, since vtkRenderWindow::SetSize is anyways not encouraged for QVTKWidget.

--- GUISupport/Qt/QVTKWidget.cxx
+++ GUISupport/Qt/QVTKWidget.cxx
@@ -218,16 +218,6 @@ void QVTKWidget::SetRenderWindow(vtkRenderWindow* w)
     // give the qt window id to the vtk window
     this->mRenWin->SetWindowId( reinterpret_cast<void*>(this->winId()));
 
-#if defined(WIN32)
-    // If this widget has a parent, we must set the parent Id for the render
-    // window, otherwise SetSize on renderwindow produces incorrect
-    // results.
-    if (this->parentWidget())
-      {
-      this->mRenWin->SetParentId(reinterpret_cast<void*>(
-        this->parentWidget()->winId()));
-      }
-#endif // defined (WIN32)
     // mac compatibility issues
 #ifdef Q_WS_MAC
 # ifdef QVTK_HAVE_VTK_4_5
@@ -881,21 +871,6 @@ void QVTKWidget::reparent(QWidget* parent, Qt::WFlags f, const QPoint& p, bool s
     QWidget::reparent(parent, f, p, false);
 
     x11_setup_window();
-
-#if defined(WIN32)
-    // If this widget has a parent, we must set the parent Id for the render
-    // window, otherwise SetSize on renderwindow produces incorrect
-    // results.
-    if (this->parentWidget())
-      {
-      this->mRenWin->SetParentId(reinterpret_cast<void*>(
-        this->parentWidget()->winId()));
-      }
-    else
-      {
-      this->mRenWin->SetParentId(0);
-      }
-#endif // defined (WIN32)
 
     // connect to new window
 #if defined(Q_WS_MAC)
