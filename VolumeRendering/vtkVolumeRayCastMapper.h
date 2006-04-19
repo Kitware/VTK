65644f1c07eabf4b08a4a0d535404fa42d7c26f1
David Cole <david.cole@kitware.com>
2006-04-19 14:32:23 -0400

    ENH: Merge changes from main tree into VTK-5-0 branch. (cvs -q up -j1.1 -j1.2 VolumeRendering/vtkVolumeRayCastMapper.h)

--- VolumeRendering/vtkVolumeRayCastMapper.h
+++ VolumeRendering/vtkVolumeRayCastMapper.h
@@ -134,7 +134,7 @@ public:
   // Set/Get the number of threads to use. This by default is equal to
   // the number of available processors detected.
   void SetNumberOfThreads( int num );
-  vtkGetMacro( NumberOfThreads, int );
+  int GetNumberOfThreads();
 
   // Description:
   // If IntermixIntersectingGeometry is turned on, the zbuffer will be
@@ -204,7 +204,6 @@ protected:
   friend VTK_THREAD_RETURN_TYPE VolumeRayCastMapper_CastRays( void *arg );
 
   vtkMultiThreader  *Threader;
-  int               NumberOfThreads;
 
   vtkMatrix4x4 *PerspectiveMatrix;
   vtkMatrix4x4 *ViewToWorldMatrix;
