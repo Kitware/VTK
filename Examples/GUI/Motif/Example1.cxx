f7f585b66bf28e1682fa46b37cd452c309661639
David Cole <david.cole@kitware.com>
2006-04-30 10:28:13 -0400

    ENH: Merge changes from main tree into VTK-5-0 branch. (cvs -q up -j1.5 -j1.6 Examples/GUI/Motif/Example1.cxx)

--- Examples/GUI/Motif/Example1.cxx
+++ Examples/GUI/Motif/Example1.cxx
@@ -25,13 +25,16 @@
 #include "vtkXRenderWindowInteractor.h"
 
 #include <Xm/PushB.h>
+
 void quit_cb(Widget,XtPointer,XtPointer);
+
 main (int argc, char *argv[])
 {
   // X window stuff
   XtAppContext app;
   Widget toplevel, button;
   Display *display;
+
   // VTK stuff
   vtkRenderWindow *renWin;
   vtkRenderer *ren1;
@@ -41,7 +44,6 @@ main (int argc, char *argv[])
   vtkGlyph3D *glyph;
   vtkPolyDataMapper *sphereMapper, *spikeMapper;
   vtkXRenderWindowInteractor *iren;
-
 
   renWin = vtkRenderWindow::New();
   ren1 = vtkRenderer::New();
@@ -55,7 +57,7 @@ main (int argc, char *argv[])
   cone = vtkConeSource::New();
   glyph = vtkGlyph3D::New();
   glyph->SetInputConnection(sphere->GetOutputPort());
-  glyph->SetSource(cone->GetOutput());
+  glyph->SetSourceConnection(cone->GetOutputPort());
   glyph->SetVectorModeToUseNormal();
   glyph->SetScaleModeToScaleByVector();
   glyph->SetScaleFactor(0.25);
@@ -67,32 +69,32 @@ main (int argc, char *argv[])
   ren1->AddActor(spikeActor1);
   ren1->SetBackground(0.4,0.1,0.2);
 
-
-// do the xwindow ui stuff
+  // do the xwindow ui stuff
   XtSetLanguageProc(NULL,NULL,NULL);
   toplevel = XtVaAppInitialize(&app,"Sample",NULL,0,
-                               &argc,argv,NULL, NULL);
-  
+    &argc,argv,NULL, NULL);
+
   // get the display connection and give it to the renderer
   display = XtDisplay(toplevel);
   renWin->SetDisplayId(display);
-  
-  // we typecast to an X specific interactor
-  // Since we have decided to make this an X program
+
+  // We use an X specific interactor
+  // since we have decided to make this an X program
   iren = vtkXRenderWindowInteractor::New();
   iren->SetRenderWindow(renWin);
   iren->Initialize(app);
-  
+
   button = XtVaCreateManagedWidget("Exit",
-                                   xmPushButtonWidgetClass, 
-                                   toplevel,
-                                   XmNwidth, 50, 
-                                   XmNheight, 50,NULL);
-  
+    xmPushButtonWidgetClass,
+    toplevel,
+    XmNwidth, 50,
+    XmNheight, 50, NULL);
+
   XtRealizeWidget(toplevel);
   XtAddCallback(button,XmNactivateCallback,quit_cb,NULL);
   XtAppMainLoop(app);
 }
+
 // simple quit callback
 void quit_cb(Widget w,XtPointer client_data, XtPointer call_data)
 {
