860afe9f4fe0a40e9731b79772805a45d103f311
Ken Martin <ken.martin@kitware.com>
2006-05-04 12:34:45 -0400

    ENH: remove use of loaded commands

--- CMake/vtkCompileCMakeExtensions.cmake
+++ CMake/vtkCompileCMakeExtensions.cmake
@@ -3,34 +3,5 @@
 # The arguments are the source and build tree locations for the loaded
 # commands project, and the variable in which success/failure should
 # be stored.
-MACRO(VTK_COMPILE_CMAKE_EXTENSIONS source_dir build_dir result_var)
-  # do we need the extensions
-  SET (VTK_NEED_LOADED_COMMANDS 0)
-  IF (VTK_WRAP_TCL OR VTK_WRAP_PYTHON)
-    SET (VTK_NEED_LOADED_COMMANDS 1)
-  ENDIF (VTK_WRAP_TCL OR VTK_WRAP_PYTHON)
-  IF (VTK_WRAP_JAVA)
-    SET (VTK_NEED_LOADED_COMMANDS 1)
-  ENDIF (VTK_WRAP_JAVA)
-  
-  # if we need them
-  IF (VTK_NEED_LOADED_COMMANDS)
-    # if they are not already loaded
-    IF(COMMAND VTK_WRAP_TCL2)
-    ELSE(COMMAND VTK_WRAP_TCL2)
-      MESSAGE(STATUS "Compiling VTK CMake commands")
-      TRY_COMPILE("${result_var}" "${build_dir}" "${source_dir}"
-        VTK_LOADED_COMMANDS      
-        CMAKE_FLAGS -DVTK_BUILD_FROM_TRY_COMPILE:BOOL=TRUE
-        OUTPUT_VARIABLE VTK_COMPILE_CMAKE_EXTENSIONS_OUTPUT)
-      IF("${result_var}")
-        MESSAGE(STATUS "Compiling VTK CMake commands - done")
-      ELSE("${result_var}")
-        WRITE_FILE(${CMAKE_BINARY_DIR}/CMakeFiles/CMakeError.log 
-          "Building of VTK extensions failed with the following output:\n"
-          "${VTK_COMPILE_CMAKE_EXTENSIONS_OUTPUT}\n" APPEND)
-        MESSAGE(FATAL_ERROR "Compiling VTK CMake commands - failed")
-      ENDIF("${result_var}")
-    ENDIF(COMMAND VTK_WRAP_TCL2)
-  ENDIF (VTK_NEED_LOADED_COMMANDS)
-ENDMACRO(VTK_COMPILE_CMAKE_EXTENSIONS)
+
+# no longer used
--- CMake/vtkLoadCMakeExtensions.cmake
+++ CMake/vtkLoadCMakeExtensions.cmake
@@ -1,33 +1,5 @@
 #-----------------------------------------------------------------------------
 # Macro to load VTK's CMake extension commands.  The argument should
 # be the directory containing the command modules.
-MACRO(VTK_LOAD_SINGLE_CMAKE_EXTENSION name dir)
-  IF(COMMAND ${name})
-  ELSE(COMMAND ${name})
-    LOAD_COMMAND(${name} ${dir} ${dir}/Debug)
-  ENDIF(COMMAND ${name})
-  IF(COMMAND ${name})
-  ELSE(COMMAND ${name})
-    MESSAGE(FATAL_ERROR "Loading VTK command ${name} - failed")
-  ENDIF(COMMAND ${name})
-ENDMACRO(VTK_LOAD_SINGLE_CMAKE_EXTENSION)
 
-MACRO(VTK_LOAD_CMAKE_EXTENSIONS ext_dir)
-  # do we need the extensions
-  SET (VTK_NEED_LOADED_COMMANDS 0)
-  IF (VTK_WRAP_TCL OR VTK_WRAP_PYTHON)
-    SET (VTK_NEED_LOADED_COMMANDS 1)
-  ENDIF (VTK_WRAP_TCL OR VTK_WRAP_PYTHON)
-  IF (VTK_WRAP_JAVA)
-    SET (VTK_NEED_LOADED_COMMANDS 1)
-  ENDIF (VTK_WRAP_JAVA)
-
-  IF (VTK_NEED_LOADED_COMMANDS)
-    MESSAGE(STATUS "Loading VTK CMake commands")
-    VTK_LOAD_SINGLE_CMAKE_EXTENSION(VTK_WRAP_TCL2 ${ext_dir})
-    VTK_LOAD_SINGLE_CMAKE_EXTENSION(VTK_WRAP_PYTHON2 ${ext_dir})
-    VTK_LOAD_SINGLE_CMAKE_EXTENSION(VTK_WRAP_JAVA2 ${ext_dir})
-    VTK_LOAD_SINGLE_CMAKE_EXTENSION(VTK_GENERATE_JAVA_DEPENDENCIES ${ext_dir})
-    MESSAGE(STATUS "Loading VTK CMake commands - done")
-  ENDIF (VTK_NEED_LOADED_COMMANDS)
-ENDMACRO(VTK_LOAD_CMAKE_EXTENSIONS)
+# no longer used
