3bc0322494098dd01b6090c5bab3f89fe1b6d68f
Ken Martin <ken.martin@kitware.com>
2006-05-09 11:42:46 -0400

    ENH: no longer used

--- CMake/vtkCompileCMakeExtensions.cmake
+++
@@ -1,7 +0,0 @@
-#-----------------------------------------------------------------------------
-# Macro to compile VTK's CMake extension commands.
-# The arguments are the source and build tree locations for the loaded
-# commands project, and the variable in which success/failure should
-# be stored.
-
-# no longer used
--- CMake/vtkLoadCMakeExtensions.cmake
+++
@@ -1,5 +0,0 @@
-#-----------------------------------------------------------------------------
-# Macro to load VTK's CMake extension commands.  The argument should
-# be the directory containing the command modules.
-
-# no longer used
