83e96c31f87cedc00678ceee6884bc04cc97739e
David Cole <david.cole@kitware.com>
2006-05-26 00:11:03 -0400

    ENH: Merge changes from main tree into VTK-5-0 branch. (cvs -q up -j1.89 -j1.90 Common/vtkTclUtil.cxx)

--- Common/vtkTclUtil.cxx
+++ Common/vtkTclUtil.cxx
@@ -770,7 +770,6 @@ void vtkTclApplicationInitTclTk(Tcl_Interp* interp,
     VTK_TCL_TK_COPY_SUPPORT_LIBRARY is ON.
   */
 
-#ifdef VTK_TCL_TK_COPY_SUPPORT_LIBRARY
   int has_tcllibpath_env = getenv("TCL_LIBRARY") ? 1 : 0;
   int has_tklibpath_env = getenv("TK_LIBRARY") ? 1 : 0;
   vtkstd::string selfdir;
@@ -831,8 +830,4 @@ void vtkTclApplicationInitTclTk(Tcl_Interp* interp,
         }
       }
     }
-#else
-  (void)interp;
-  (void)relative_dirs;
-#endif
 }
