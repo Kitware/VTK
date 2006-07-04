5157124baf5f8b61403620b882cc7fe00c350350
David Gobbi <david.gobbi@gmail.com>
2006-07-04 07:43:00 -0400

    BUG: attempt to get TestSetFileNames.tcl to pass on more platforms

--- IO/Testing/Tcl/TestSetFileNames.tcl
+++ IO/Testing/Tcl/TestSetFileNames.tcl
@@ -10,6 +10,8 @@ sortFileNames NumericSortOn
 vtkImageReader2 reader
 reader SetFileNames [sortFileNames GetFileNames]
 reader SetDataExtent 0 63 0 63 1 1
+reader SetDataByteOrderToLittleEndian
+reader SetDataMask 0x7fff
 
 # set Z slice to 2: if output is not numerically sorted, the wrong
 # slice will be shown
