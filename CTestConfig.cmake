f82160d62c0e32ffb3e6865b45470358b04f87a8
Andy Cedilnik <andy.cedilnik@kitware.com>
2006-07-07 14:51:30 -0400

    ENH: Initial import

---
+++ CTestConfig.cmake
@@ -0,0 +1,6 @@
+SET (CTEST_PROJECT_NAME "VTK")
+SET (CTEST_NIGHTLY_START_TIME "23:00:00 EDT")
+SET (CTEST_DROP_METHOD "http")
+SET (CTEST_DROP_SITE "www.vtk.org")
+SET (CTEST_DROP_LOCATION "/cgi-bin/HTTPUploadDartFile.cgi")
+SET (CTEST_TRIGGER_SITE "http://${CTEST_DROP_SITE}/cgi-bin/Submit-vtk-TestingResults.cgi")
