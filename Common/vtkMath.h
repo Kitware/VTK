b67e2667706c74652ab98871aa922e35418b5ab9
Brad Davis <brad.davis@kitware.com>
2006-07-11 16:52:29 -0400

    BUG: in Floor function, use union to avoid violation of aliasing rules

--- Common/vtkMath.h
+++ Common/vtkMath.h
@@ -717,15 +717,15 @@ inline vtkTypeInt64 vtkMath::Factorial( int N )
 inline int vtkMath::Floor(double x)
 {
 #if defined i386 || defined _M_IX86
-  double tempval;
+  union { int i[2]; double d; } u;
   // use 52-bit precision of IEEE double to round (x - 0.25) to 
   // the nearest multiple of 0.5, according to prevailing rounding
   // mode which is IEEE round-to-nearest,even
-  tempval = (x - 0.25) + 3377699720527872.0; // (2**51)*1.5
+  u.d = (x - 0.25) + 3377699720527872.0; // (2**51)*1.5
   // extract mantissa, use shift to divide by 2 and hence get rid
   // of the bit that gets messed up because the FPU uses
   // round-to-nearest,even mode instead of round-to-nearest,+infinity
-  return ((int*)&tempval)[0] >> 1;
+  return u.i[0] >> 1;
 #else
   return (int)floor(x);
 #endif
