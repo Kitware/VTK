56733cc47aa553dea16ba9e63946052a3ec19ef3
David Gobbi <david.gobbi@gmail.com>
2006-09-24 08:22:29 -0400

    BUG: Fix bugs 3798 and 3799.  There were interpolation problems, and the textured image was shifted by half a pixel.

--- Widgets/vtkImagePlaneWidget.cxx
+++ Widgets/vtkImagePlaneWidget.cxx
@@ -40,7 +40,7 @@
 #include "vtkTexture.h"
 #include "vtkTransform.h"
 
-vtkCxxRevisionMacro(vtkImagePlaneWidget, "1.7");
+vtkCxxRevisionMacro(vtkImagePlaneWidget, "1.8");
 vtkStandardNewMacro(vtkImagePlaneWidget);
 
 vtkCxxSetObjectMacro(vtkImagePlaneWidget, PlaneProperty, vtkProperty);
@@ -1376,6 +1376,7 @@ void vtkImagePlaneWidget::SetInput(vtkDataSet* input)
   this->OriginalLevel = 0.5*(range[0] + range[1]);
 
   this->Reslice->SetInput(this->ImageData);
+  this->ResliceInterpolate = -1;
   this->SetResliceInterpolate(this->ResliceInterpolate);
 
   this->ColorMap->SetInput(this->Reslice->GetOutput());
@@ -1482,14 +1483,14 @@ void vtkImagePlaneWidget::UpdatePlane()
 
   double planeOrigin[4];
   this->PlaneSource->GetOrigin(planeOrigin);
+
   planeOrigin[3] = 1.0;
   double originXYZW[4];
-  this->ResliceAxes->MultiplyPoint(planeOrigin,originXYZW);
+  this->ResliceAxes->MultiplyPoint(planeOrigin, originXYZW);
 
   this->ResliceAxes->Transpose();
   double neworiginXYZW[4];
-  double point[] =  {originXYZW[0],originXYZW[1],originXYZW[2],originXYZW[3]};
-  this->ResliceAxes->MultiplyPoint(point,neworiginXYZW);
+  this->ResliceAxes->MultiplyPoint(originXYZW, neworiginXYZW);
 
   this->ResliceAxes->SetElement(0,3,neworiginXYZW[0]);
   this->ResliceAxes->SetElement(1,3,neworiginXYZW[1]);
@@ -1549,9 +1550,11 @@ void vtkImagePlaneWidget::UpdatePlane()
       }
     }
 
-  this->Reslice->SetOutputSpacing(planeSizeX/extentX,planeSizeY/extentY,1);
-  this->Reslice->SetOutputOrigin(0.0,0.0,0.0);
-  this->Reslice->SetOutputExtent(0,extentX-1,0,extentY-1,0,0);
+  double outputSpacingX = planeSizeX/extentX;
+  double outputSpacingY = planeSizeY/extentY;
+  this->Reslice->SetOutputSpacing(outputSpacingX, outputSpacingY, 1);
+  this->Reslice->SetOutputOrigin(0.5*outputSpacingX, 0.5*outputSpacingY, 0);
+  this->Reslice->SetOutputExtent(0, extentX-1, 0, extentY-1, 0, 0);
 }
 
 //----------------------------------------------------------------------------
