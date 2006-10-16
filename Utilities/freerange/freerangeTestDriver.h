24accc12574648d6755816db416540b730054cc9
David Thompson <dcthomp@sandia.gov>
2006-10-16 17:22:32 -0400

    COMP: Guaranteed to fix gcc-2.9.5 build problems. Hopefully won't break       Windows in the process.

---
+++ Utilities/freerange/freerangeTestDriver.h
@@ -0,0 +1,7 @@
+#ifndef __freerangeTestDriver_h
+#define __freerangeTestDriver_h
+
+#include "vtkObject.h" // for vtkstd
+#include "vtkTestDriver.h" // for vtkstd/exception include and warning pragmas
+
+#endif // __freerangeTestDriver_h
