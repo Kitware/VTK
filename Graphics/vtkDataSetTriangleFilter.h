2a8bb44c36c4e09702941063a1fb87106489146f
Dave Demarle <dave.demarle@kitware.com>
2006-10-28 12:10:16 -0400

    BUG: Give Paraview's 'tetrahedralizer' a facility to cull non tetrahedral elements in its output. This lets the volume rendering classes handle non tet inputs. See bug#2736

--- Graphics/vtkDataSetTriangleFilter.h
+++ Graphics/vtkDataSetTriangleFilter.h
@@ -42,6 +42,13 @@ public:
   vtkTypeRevisionMacro(vtkDataSetTriangleFilter,vtkUnstructuredGridAlgorithm);
   void PrintSelf(ostream& os, vtkIndent indent);
 
+  // Description:
+  // When On this filter will cull all 1D and 2D cells from the output.
+  // The default is Off.
+  vtkSetMacro(TetrahedraOnly, int);
+  vtkGetMacro(TetrahedraOnly, int);
+  vtkBooleanMacro(TetrahedraOnly, int);
+
 protected:
   vtkDataSetTriangleFilter();
   ~vtkDataSetTriangleFilter();
@@ -57,7 +64,9 @@ protected:
   // Different execute methods depending on whether input is structured or not
   void StructuredExecute(vtkDataSet *, vtkUnstructuredGrid *);
   void UnstructuredExecute(vtkDataSet *, vtkUnstructuredGrid *);
-  
+
+  int TetrahedraOnly;
+
 private:
   vtkDataSetTriangleFilter(const vtkDataSetTriangleFilter&);  // Not implemented.
   void operator=(const vtkDataSetTriangleFilter&);  // Not implemented.
