be0b32fb7a0d25bd03e2b106f09de1abac49a82b
Andy Wilson <atwilso@sandia.gov>
2006-11-06 17:41:37 -0500

    BUG: Don't let the strip_whitespace routine fall off the end of the string
    BUG: DOS encodes newline as 0x0d 0x0a.  Unix uses 0x0a.  In order to try to reconcile these I'm going to silently ignore 0x0d.  This seems a better solution than having the reader return different results on different platforms...

--- Infovis/vtkFixedWidthTextReader.cxx
+++ Infovis/vtkFixedWidthTextReader.cxx
@@ -33,7 +33,7 @@
 #include <vtkstd/string>
 #include <fstream>
 
-vtkCxxRevisionMacro(vtkFixedWidthTextReader, "1.1");
+vtkCxxRevisionMacro(vtkFixedWidthTextReader, "1.2");
 vtkStandardNewMacro(vtkFixedWidthTextReader);
 
 // Function body at bottom of file
@@ -48,6 +48,24 @@ static int splitString(const vtkStdString& input,
 // some platforms but not others so I'm afraid I have to write it
 // myself.
 static int my_getline(istream& stream, vtkStdString &output, char delim='\n');
+
+
+// On MS-DOS, a newline is 0x0d 0x0a.  On Unix machines, it's just
+// 0x0a.  This makes me sad.  
+
+static vtkStdString simplify_newlines(const vtkStdString &input)
+{
+  vtkStdString output;
+  for (int i = 0; i < input.size(); ++i)
+    {
+    if (input.at(i) != 0x0d)
+      {
+      output += input.at(i);
+      }
+    }
+  return output;
+}
+
 
 // ----------------------------------------------------------------------
 
@@ -235,6 +253,7 @@ splitString(const vtkStdString& input,
   vtkStdString thisFieldText;
   vtkStdString parsedField;
 
+
   while (thisField * fieldWidth < input.size())
     {
     thisFieldText = input.substr(thisField*fieldWidth, fieldWidth);
@@ -242,11 +261,13 @@ splitString(const vtkStdString& input,
     if (stripWhitespace)
       {
       int startIndex = 0, endIndex = thisFieldText.size() - 1;
-      while (isspace(static_cast<int>(thisFieldText.at(startIndex))))
+      while (startIndex < thisFieldText.size() && 
+             isspace(static_cast<int>(thisFieldText.at(startIndex))))
         {
         ++startIndex;
         }
-      while (isspace(static_cast<int>(thisFieldText.at(endIndex))))
+      while (endIndex > 0 && 
+             isspace(static_cast<int>(thisFieldText.at(endIndex))))
         {
         -- endIndex;
         }
@@ -290,7 +311,7 @@ my_getline(istream& in, vtkStdString &out, char delimiter)
     ++numCharactersRead;
 
     char downcast = static_cast<char>(nextValue);
-    if (downcast != delimiter)
+    if (downcast != delimiter && downcast != 0x0d)
       {
       out += downcast;
       }
