05b7ee6cc4a0f49d3ba4085ce4fe6db479f6ee1c
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-11-13 10:24:23 -0500

    BUG: Fix Bug #3330 - vtkBorderRepresentation - virtuals not called that only impact VS6 poor implementation of the standard

--- Widgets/vtkWidgetRepresentation.h
+++ Widgets/vtkWidgetRepresentation.h
@@ -92,9 +92,9 @@ public:
   // Note that subclasses may ignore some of these methods and implement their own
   // depending on the specifics of the widget.
   virtual void PlaceWidget(double* vtkNotUsed(bounds[6])) {}
-  virtual void StartWidgetInteraction(double* vtkNotUsed(eventPos[2])) {}
-  virtual void WidgetInteraction(double* vtkNotUsed(newEventPos[2])) {}
-  virtual void EndWidgetInteraction(double* vtkNotUsed(newEventPos[2])) {}
+  virtual void StartWidgetInteraction(double eventPos[2]) { (void)eventPos; }
+  virtual void WidgetInteraction(double newEventPos[2]) { (void)newEventPos; }
+  virtual void EndWidgetInteraction(double newEventPos[2]) { (void)newEventPos; }
   virtual int ComputeInteractionState(int X, int Y, int modify=0);
   virtual int GetInteractionState()
     {return this->InteractionState;}
