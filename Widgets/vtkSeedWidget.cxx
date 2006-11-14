5f79c2f09c2520cc771ab10f78d53cdf62d6cec0
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2006-11-14 10:24:20 -0500

    STYLE: Bug #4044 - STYLE: cout should be removed

--- Widgets/vtkSeedWidget.cxx
+++ Widgets/vtkSeedWidget.cxx
@@ -28,7 +28,7 @@
 #include "vtkEvent.h"
 #include <vtkstd/vector>
 
-vtkCxxRevisionMacro(vtkSeedWidget, "1.7");
+vtkCxxRevisionMacro(vtkSeedWidget, "1.8");
 vtkStandardNewMacro(vtkSeedWidget);
 
 
@@ -244,7 +244,6 @@ void vtkSeedWidget::EndSelectAction(vtkAbstractWidget *w)
 //-------------------------------------------------------------------------
 void vtkSeedWidget::DeleteAction(vtkAbstractWidget *w)
 {
-  cout << "Delete action\n";
   vtkSeedWidget *self = reinterpret_cast<vtkSeedWidget*>(w);
 
   // Do nothing if outside
