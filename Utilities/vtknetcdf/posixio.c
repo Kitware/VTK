5a367247f0586fb577d0a73f47c2fed0513dd28e
David Thompson <dcthomp@sandia.gov>
2006-11-30 04:20:53 -0500

    COMP: Can't seem to eliminate this bogus Borland warning, so turn it off.

--- Utilities/vtknetcdf/posixio.c
+++ Utilities/vtknetcdf/posixio.c
@@ -11,6 +11,7 @@
 
 #if defined(__BORLANDC__)
 #pragma warn -8004 /* "assigned a value that is never used" */
+#pragma warn -8065 /* "Call to function 'XXX' with no prototype" */
 #endif
 
 #include <assert.h>
