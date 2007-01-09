4fe2b4815ee9a941bc552d4bd36d9d386eaa9dca
Andy Cedilnik <andy.cedilnik@kitware.com>
2007-01-08 21:01:09 -0500

    STYLE: Nightly Version update

--- Common/vtkVersion.h
+++ Common/vtkVersion.h
@@ -28,7 +28,7 @@
 
 #include "vtkObject.h"
 
-#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.2649 $, $Date: 2007-01-08 02:05:40 $ (GMT)"
+#define VTK_SOURCE_VERSION "vtk version " VTK_VERSION ", vtk source $Revision: 1.2650 $, $Date: 2007-01-09 02:01:09 $ (GMT)"
 
 
 class VTK_COMMON_EXPORT vtkVersion : public vtkObject
