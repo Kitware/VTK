608f11312419ce6bf0f5b5a7784c04f239a84b20
David Cole <david.cole@kitware.com>
2007-01-24 07:00:21 -0500

    ENH: Remove building the MPEG2 library from VTK's build instructions. This library has patents associated with it, so it should not be *included* in VTK. Make it available separately for clients who are willing to accept using patented code. Change use of the MPEG2 library to be based on a CMake configure-time option: VTK_USE_MPEG2_ENCODER. This set of changes is to conform with VTK's "patent free" policy which prohibits including patented code in the VTK source tree.

--- IO/vtkMPEG2Writer.h
+++ IO/vtkMPEG2Writer.h
@@ -13,11 +13,30 @@
 
 =========================================================================*/
 // .NAME vtkMPEG2Writer - Writes MPEG2 Movie files.
+//
 // .SECTION Description
 // vtkMPEG2Writer writes Movie files. The data type
 // of the file is unsigned char regardless of the input type.
+//
+// This class is conditionally compiled into VTK only if VTK's CMake
+// option VTK_USE_MPEG2_ENCODER is ON. It is OFF by default.
+//
+// Portions of the mpeg2 library are patented. VTK does not enable linking to
+// this library by default so VTK can remain "patent free". Users who wish to
+// link in mpeg2 functionality must build that library separately and then
+// turn on VTK_USE_MPEG2_ENCODER when configuring VTK. After turning on
+// VTK_USE_MPEG2_ENCODER, you must also set the CMake variables
+// vtkMPEG2Encode_INCLUDE_PATH and vtkMPEG2Encode_LIBRARIES.
+//
+// You are solely responsible for any legal issues associated with using
+// patented code in your software.
+//
+// You can download a "CMake-ified" source tree of the MPEG2 library by
+// visiting the download page at http://www.vtk.org and scrolling down to
+// the "Download Additional Components" section.
+//
 // .SECTION See Also
-// vtkGenericMovieWriter vtkAVIWriter
+// vtkGenericMovieWriter vtkAVIWriter vtkFFMPEGWriter
 
 #ifndef __vtkMPEG2Writer_h
 #define __vtkMPEG2Writer_h
@@ -34,14 +53,14 @@ public:
   static vtkMPEG2Writer *New();
   vtkTypeRevisionMacro(vtkMPEG2Writer,vtkGenericMovieWriter);
   void PrintSelf(ostream& os, vtkIndent indent);
-  
+
   // Description:
   // These methods start writing an Movie file, write a frame to the file
   // and then end the writing process.
   void Start();
   void Write();
   void End();
-  
+
 protected:
   vtkMPEG2Writer();
   ~vtkMPEG2Writer();
@@ -63,6 +82,3 @@ private:
 };
 
 #endif
-
-
-
