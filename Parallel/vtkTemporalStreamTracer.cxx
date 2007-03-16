05ff4cf8e37ba7a82ea4a83b7cf859dc60ab38bd
Dave Demarle <dave.demarle@kitware.com>
2007-03-16 11:47:38 -0400

    COMP: Fix yet another comp warning.

--- Parallel/vtkTemporalStreamTracer.cxx
+++ Parallel/vtkTemporalStreamTracer.cxx
@@ -68,7 +68,7 @@ PURPOSE.  See the above copyright notice for more information.
 #include <algorithm>
 
 //---------------------------------------------------------------------------
-vtkCxxRevisionMacro(vtkTemporalStreamTracer, "1.10");
+vtkCxxRevisionMacro(vtkTemporalStreamTracer, "1.11");
 vtkStandardNewMacro(vtkTemporalStreamTracer);
 vtkCxxSetObjectMacro(vtkTemporalStreamTracer, Controller, vtkMultiProcessController);
 //---------------------------------------------------------------------------
@@ -966,7 +966,7 @@ void vtkTemporalStreamTracer::GenerateOutputLines(vtkPolyData *output)
     ParticleLifetime       &P = (*it);
     ParticleInformation &info = P.Information;
     // create Point Id's 
-    double *coord = &info.CurrentPosition.x[0];
+    //double *coord = &info.CurrentPosition.x[0];
     //tempId = this->OutputCoordinates->InsertNextPoint(coord);
     ParticleIds->InsertNextTuple1(info.UniqueParticleId);
     SourceIds->InsertNextTuple1(info.SourceID);
