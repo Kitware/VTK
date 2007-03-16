6e2206c7dc034ef75c1b8b77a5582051a24f5605
Dave Demarle <dave.demarle@kitware.com>
2007-03-16 10:32:13 -0400

    COMP: Fix comp warning.

--- Parallel/vtkTemporalStreamTracer.cxx
+++ Parallel/vtkTemporalStreamTracer.cxx
@@ -68,7 +68,7 @@ PURPOSE.  See the above copyright notice for more information.
 #include <algorithm>
 
 //---------------------------------------------------------------------------
-vtkCxxRevisionMacro(vtkTemporalStreamTracer, "1.9");
+vtkCxxRevisionMacro(vtkTemporalStreamTracer, "1.10");
 vtkStandardNewMacro(vtkTemporalStreamTracer);
 vtkCxxSetObjectMacro(vtkTemporalStreamTracer, Controller, vtkMultiProcessController);
 //---------------------------------------------------------------------------
@@ -954,7 +954,7 @@ void vtkTemporalStreamTracer::GenerateOutputLines(vtkPolyData *output)
   ParticleIds->SetName("ParticleId");
   SourceIds->SetName("SourceId");
   InjectedPointIds->SetName("InjectedPointId");
-  vtkIdType tempId; // only need 1 now we have removed trails
+  //vtkIdType tempId; // only need 1 now we have removed trails
   vtkIdType Np = this->ParticleHistories.size();
   vtkIdType *cells = this->ParticleCells->WritePointer(Np, Np*2);
   //
@@ -967,7 +967,7 @@ void vtkTemporalStreamTracer::GenerateOutputLines(vtkPolyData *output)
     ParticleInformation &info = P.Information;
     // create Point Id's 
     double *coord = &info.CurrentPosition.x[0];
-    tempId = this->OutputCoordinates->InsertNextPoint(coord);
+    //tempId = this->OutputCoordinates->InsertNextPoint(coord);
     ParticleIds->InsertNextTuple1(info.UniqueParticleId);
     SourceIds->InsertNextTuple1(info.SourceID);
     InjectedPointIds->InsertNextTuple1(info.InjectedPointId);
