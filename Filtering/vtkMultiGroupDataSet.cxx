1bf0bd870024890e847989b32a94b709aa386ad8
Berk Geveci <berk.geveci@kitware.com>
2007-04-07 16:48:35 -0400

    BUG: Fixed memory leak.

--- Filtering/vtkMultiGroupDataSet.cxx
+++ Filtering/vtkMultiGroupDataSet.cxx
@@ -24,7 +24,7 @@
 
 #include "vtkObjectFactory.h"
 
-vtkCxxRevisionMacro(vtkMultiGroupDataSet, "1.5");
+vtkCxxRevisionMacro(vtkMultiGroupDataSet, "1.6");
 vtkStandardNewMacro(vtkMultiGroupDataSet);
 
 vtkInformationKeyMacro(vtkMultiGroupDataSet,GROUP,Integer);
@@ -41,7 +41,14 @@ vtkMultiGroupDataSet::~vtkMultiGroupDataSet()
 {
   this->InitializeDataSets();
   delete this->Internal;
-  this->SetMultiGroupDataInformation(0);
+
+  // Cannot be released with SetMultiGroupDataInformation(0) because
+  // that method always allocates a new one.
+  if (this->MultiGroupDataInformation)
+    {
+    this->MultiGroupDataInformation->Delete();
+    this->MultiGroupDataInformation = 0;
+    }
 }
 
 //----------------------------------------------------------------------------
