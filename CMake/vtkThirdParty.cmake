79c6715ff3d4b9173d1025cf468806d5ae5b12a6
Brad King <brad.king@kitware.com>
2007-06-01 09:59:14 -0400

    COMP: Suppress warnings in vtkfreetype and vtklibxml2 build with a new VTK_THIRD_PARTY_WARNING_SUPPRESS macro.

--- CMake/vtkThirdParty.cmake
+++ CMake/vtkThirdParty.cmake
@@ -51,3 +51,28 @@ MACRO(VTK_THIRD_PARTY_SUBDIR upper lower)
     SUBDIRS(${lower})
   ENDIF(NOT VTK_USE_SYSTEM_${upper})
 ENDMACRO(VTK_THIRD_PARTY_SUBDIR)
+
+#-----------------------------------------------------------------------------
+MACRO(VTK_THIRD_PARTY_WARNING_SUPPRESS upper lang)
+  IF(NOT ${upper}_WARNINGS_ALLOW)
+    # MSVC uses /w to suppress warnings.  It also complains if another
+    # warning level is given, so remove it.
+    IF(MSVC)
+      SET(${upper}_WARNINGS_BLOCKED 1)
+      STRING(REGEX REPLACE "(^| )([/-])W[0-9]( |$)" " "
+        CMAKE_${lang}_FLAGS "${CMAKE_${lang}_FLAGS}")
+      SET(CMAKE_${lang}_FLAGS "${CMAKE_${lang}_FLAGS} /w")
+    ENDIF(MSVC)
+
+    # Borland uses -w- to suppress warnings.
+    IF(BORLAND)
+      SET(${upper}_WARNINGS_BLOCKED 1)
+      SET(CMAKE_${lang}_FLAGS "${CMAKE_${lang}_FLAGS} -w-")
+    ENDIF(BORLAND)
+
+    # Most compilers use -w to suppress warnings.
+    IF(NOT ${upper}_WARNINGS_BLOCKED)
+      SET(CMAKE_${lang}_FLAGS "${CMAKE_${lang}_FLAGS} -w")
+    ENDIF(NOT ${upper}_WARNINGS_BLOCKED)
+  ENDIF(NOT ${upper}_WARNINGS_ALLOW)
+ENDMACRO(VTK_THIRD_PARTY_WARNING_SUPPRESS)
