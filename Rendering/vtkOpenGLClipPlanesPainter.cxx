2a76d8f6d770b903d941aa681a555f936c7063aa
Charl Botha <c.p.botha@ewi.tudelft.nl>
2007-07-04 11:48:28 -0400

    BUG: Patch contributed by Stef Busking (s.busking |at| tudelft.nl) that fixes the transformation of the clipping plane from object space to world space.  See http://public.kitware.com/pipermail/vtkusers/2007-February/089807.html for more details.

--- Rendering/vtkOpenGLClipPlanesPainter.cxx
+++ Rendering/vtkOpenGLClipPlanesPainter.cxx
@@ -27,7 +27,7 @@
 
 #ifndef VTK_IMPLEMENT_MESA_CXX
 vtkStandardNewMacro(vtkOpenGLClipPlanesPainter);
-vtkCxxRevisionMacro(vtkOpenGLClipPlanesPainter, "1.2");
+vtkCxxRevisionMacro(vtkOpenGLClipPlanesPainter, "1.3");
 #endif
 //-----------------------------------------------------------------------------
 vtkOpenGLClipPlanesPainter::vtkOpenGLClipPlanesPainter()
@@ -73,6 +73,8 @@ void vtkOpenGLClipPlanesPainter::RenderInternal(vtkRenderer* renderer,
     vtkMatrix4x4 *actorMatrix = vtkMatrix4x4::New();
     actor->GetMatrix( actorMatrix );
     actorMatrix->Invert();
+    // OpenGL matrices are transposed compared to VTK matrices
+    actorMatrix->Transpose();
 
     double origin[4], normal[3], point[4];
 
@@ -83,32 +85,9 @@ void vtkOpenGLClipPlanesPainter::RenderInternal(vtkRenderer* renderer,
       plane->GetOrigin(origin);
       plane->GetNormal(normal);
 
-      point[0] = origin[0] + normal[0];
-      point[1] = origin[1] + normal[1];
-      point[2] = origin[2] + normal[2];
-
-      origin[3] = point[3] = 1.0;
-
-      actorMatrix->MultiplyPoint( origin, origin );
-      actorMatrix->MultiplyPoint( point, point );
-
-      if ( origin[3] != 1.0 )
-        {
-        origin[0] /= origin[3];
-        origin[1] /= origin[3];
-        origin[2] /= origin[3];
-        }
-
-      if ( point[3] != 1.0 )
-        {
-        point[0] /= point[3];
-        point[1] /= point[3];
-        point[2] /= point[3];
-        }
-
-      normal[0] = point[0] - origin[0];
-      normal[1] = point[1] - origin[1];
-      normal[2] = point[2] - origin[2];
+      glMatrixMode(GL_MODELVIEW);
+      glPushMatrix();
+      glMultMatrixd(actorMatrix->Element[0]);
 
       planeEquation[0] = normal[0];
       planeEquation[1] = normal[1];
@@ -117,6 +96,8 @@ void vtkOpenGLClipPlanesPainter::RenderInternal(vtkRenderer* renderer,
         planeEquation[1]*origin[1]+
         planeEquation[2]*origin[2]);
       glClipPlane((GLenum)(GL_CLIP_PLANE0+i),planeEquation);
+
+      glPopMatrix();
       }
 
     actorMatrix->Delete();  
