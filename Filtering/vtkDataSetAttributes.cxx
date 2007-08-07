a1150532d173024ddabe763acc98b892c3f77869
Jeffrey Baumes <jeff.baumes@kitware.com>
2007-08-07 16:45:34 -0400

    BUG: DeepCopy() now works when the pedigree id array is a string array.

--- Filtering/vtkDataSetAttributes.cxx
+++ Filtering/vtkDataSetAttributes.cxx
@@ -30,7 +30,7 @@
 #include "vtkIdTypeArray.h"
 #include "vtkObjectFactory.h"
 
-vtkCxxRevisionMacro(vtkDataSetAttributes, "1.23");
+vtkCxxRevisionMacro(vtkDataSetAttributes, "1.24");
 vtkStandardNewMacro(vtkDataSetAttributes);
 
 //--------------------------------------------------------------------------
@@ -147,7 +147,7 @@ void vtkDataSetAttributes::DeepCopy(vtkFieldData *fd)
         {
         // If this array is an attribute in the source, make it so
         // in the target as well.
-        this->SetAttribute(vtkDataArray::SafeDownCast(newData), attributeType);
+        this->SetAttribute(newData, attributeType);
         }
       else
         {
