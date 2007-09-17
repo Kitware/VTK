d014bab13417045701bdc80b68611e65ccafd0bf
David Cole <david.cole@kitware.com>
2007-09-17 17:07:21 -0400

    BUG: Generated documentation page does not show up correctly on Kitware web site when URL ends with a "/"... Remove the trailing slash.

--- IO/vtkPLYReader.h
+++ IO/vtkPLYReader.h
@@ -16,7 +16,7 @@
 // .SECTION Description
 // vtkPLYReader is a source object that reads polygonal data in
 // Stanford University PLY file format (see 
-// http://graphics.stanford.edu/data/3Dscanrep/). It requires that
+// http://graphics.stanford.edu/data/3Dscanrep). It requires that
 // the elements "vertex" and "face" are defined. The "vertex" element
 // must have the properties "x", "y", and "z". The "face" element must
 // have the property "vertex_indices" defined. Optionally, if the "face"
