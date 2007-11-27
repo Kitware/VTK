81d870ee2879cc7aa126cd56926bc37c1108d8ed
Jeffrey Baumes <jeff.baumes@kitware.com>
2007-11-27 09:37:31 -0500

    ENH: Adding XML file open menu.

--- Examples/Infovis/Java/TreeViews.java
+++ Examples/Infovis/Java/TreeViews.java
@@ -1,20 +1,33 @@
-
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
 import java.util.Random;
 
-import javax.swing.JComponent;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.KeyStroke;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import vtk.*;
 
-public class TreeViews extends JComponent {
+public class TreeViews extends JFrame {
 
   private vtkRenderWindowPanel panel1 = new vtkRenderWindowPanel();
   private vtkRenderWindowPanel panel2 = new vtkRenderWindowPanel();
   private vtkRenderWindowPanel panel3 = new vtkRenderWindowPanel();
   private vtkRenderWindowPanel panel4 = new vtkRenderWindowPanel();
+  
+  private vtkXMLTreeReader reader = new vtkXMLTreeReader();
+  private vtkTreeLevelsFilter levels = new vtkTreeLevelsFilter();
 
   private vtkGraphLayoutView view1;
   private vtkTreeLayoutView view2;
@@ -26,23 +39,20 @@ public class TreeViews extends JComponent {
     // The tree has a "name" field to use for labels.
     // It also has a "level" field to use for colors.
     vtkStringArray nameArr = new vtkStringArray();
-    nameArr.SetName("name");    
-    vtkIntArray levelArr = new vtkIntArray();
-    levelArr.SetName("level");
-    vtkTree tree = new vtkTree();
+    nameArr.SetName("name");
     // Start by adding a root to the tree
+    vtkTree tree = new vtkTree();
     tree.AddRoot();
     nameArr.InsertNextValue("0");
-    levelArr.InsertNextValue(0);
     Random r = new Random();
     for (int i = 1; i < 100; ++i) {
       // Add a child to a random vertex in the tree.
       tree.AddChild(r.nextInt(i));
       nameArr.InsertNextValue("" + i);
-      levelArr.InsertNextValue(tree.GetLevel(i));
     }
     tree.GetVertexData().AddArray(nameArr);
-    tree.GetVertexData().AddArray(levelArr);
+    
+    levels.SetInput(tree);
 
     // Create the selection link.
     // This is shared among all views in order to link the selection.
@@ -52,7 +62,7 @@ public class TreeViews extends JComponent {
     // This view performs an automatic embedding of the graph in 2D.
     // Label by name, color by level.
     view1 = new vtkGraphLayoutView();
-    vtkDataRepresentation rep1 = view1.AddRepresentationFromInput(tree);
+    vtkDataRepresentation rep1 = view1.AddRepresentationFromInputConnection(levels.GetOutputPort());
     view1.SetupRenderWindow(panel1.GetRenderWindow());
     view1.SetVertexLabelArrayName("name");
     view1.VertexLabelVisibilityOn();
@@ -62,10 +72,11 @@ public class TreeViews extends JComponent {
     rep1.SetSelectionLink(link);
 
     // Create a standard tree layout view.
-    // The standard tree layout arranges the vertices of a tree in horizontal levels.
+    // The standard tree layout arranges the vertices of a tree in horizontal
+    // levels.
     // Label by name, color by level.
     view2 = new vtkTreeLayoutView();
-    vtkDataRepresentation rep2 = view2.AddRepresentationFromInput(tree);
+    vtkDataRepresentation rep2 = view2.AddRepresentationFromInputConnection(levels.GetOutputPort());
     view2.SetupRenderWindow(panel2.GetRenderWindow());
     view2.SetLabelArrayName("name");
     view2.LabelVisibilityOn();
@@ -75,10 +86,11 @@ public class TreeViews extends JComponent {
     rep2.SetSelectionLink(link);
 
     // Create a radial tree layout view.
-    // The radial tree layout arranges the vertices of a tree in concentric circles.
+    // The radial tree layout arranges the vertices of a tree in concentric
+    // circles.
     // Label by name, color by level.
     view3 = new vtkTreeLayoutView();
-    vtkDataRepresentation rep3 = view3.AddRepresentationFromInput(tree);
+    vtkDataRepresentation rep3 = view3.AddRepresentationFromInputConnection(levels.GetOutputPort());
     view3.SetupRenderWindow(panel3.GetRenderWindow());
     view3.RadialOn();
     view3.SetAngle(360);
@@ -93,7 +105,7 @@ public class TreeViews extends JComponent {
 
     // Create a tree map view.
     view4 = new vtkTreeMapView();
-    vtkDataRepresentation rep4 = view4.AddRepresentationFromInput(tree);
+    vtkDataRepresentation rep4 = view4.AddRepresentationFromInputConnection(levels.GetOutputPort());
     view4.SetupRenderWindow(panel4.GetRenderWindow());
     // Link the selection.
     rep4.SetSelectionLink(link);
@@ -101,10 +113,7 @@ public class TreeViews extends JComponent {
     // Use a simple ViewChangedObserver to render all views
     // when the selection in one view changes.
     ViewChangedObserver obs = new ViewChangedObserver();
-    rep1.AddObserver("SelectionChangedEvent", obs, "SelectionChanged");
-    rep2.AddObserver("SelectionChangedEvent", obs, "SelectionChanged");
-    rep3.AddObserver("SelectionChangedEvent", obs, "SelectionChanged");
-    rep4.AddObserver("SelectionChangedEvent", obs, "SelectionChanged");
+    link.AddObserver("SelectionChangedEvent", obs, "SelectionChanged");
 
     // Arrange the views in a grid.
     GridLayout layout = new GridLayout(2, 2);
@@ -115,6 +124,44 @@ public class TreeViews extends JComponent {
     this.add(panel2);
     this.add(panel3);
     this.add(panel4);
+
+    // Create menu
+    JMenuBar menuBar;
+    JMenu menu;
+    JMenuItem menuItem;
+
+    // Create the menu bar.
+    menuBar = new JMenuBar();
+
+    // Build the first menu.
+    menu = new JMenu("File");
+    menu.setMnemonic(KeyEvent.VK_F);
+    menuBar.add(menu);
+
+    // A JMenuItem
+    menuItem = new JMenuItem("Open XML...", KeyEvent.VK_X);
+    menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O,
+        ActionEvent.CTRL_MASK));
+    menuItem.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        // Create a file chooser
+        final JFileChooser fc = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("XML files (*.xml)", "xml");
+        fc.setFileFilter(filter);
+        // In response to a button click:
+        int returnVal = fc.showOpenDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+          File f = fc.getSelectedFile();
+          reader.SetFileName(f.getAbsolutePath());
+          levels.SetInputConnection(reader.GetOutputPort());
+          ViewChangedObserver obs = new ViewChangedObserver();
+          obs.SelectionChanged();
+        }
+      }});
+    menu.add(menuItem);
+
+    this.setJMenuBar(menuBar);
   }
 
   private class ViewChangedObserver {
@@ -141,14 +188,15 @@ public class TreeViews extends JComponent {
   }
 
   public static void main(String args[]) {
-    TreeViews panel = new TreeViews();
-    JFrame frame = new JFrame("Titan Demo");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.getContentPane().add("Center", panel);
-    frame.pack();
-    frame.setVisible(true);
+    JPopupMenu.setDefaultLightWeightPopupEnabled(false);
 
-    frame.addWindowListener(new WindowAdapter() {
+    TreeViews app = new TreeViews();
+    app.setTitle("Tree Views");
+    app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    app.pack();
+    app.setVisible(true);
+
+    app.addWindowListener(new WindowAdapter() {
       @Override
       public void windowClosing(WindowEvent e) {
         // Calling vtkGlobalJavaHash.DeleteAll() will clean up
