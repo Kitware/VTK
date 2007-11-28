c454abdb72fa43ef862189509050f6bc576ffdff
Brad King <brad.king@kitware.com>
2007-11-28 13:01:22 -0500

    STYLE: The Date and Revision keywords do not actually need to be replaced in the comments for ExtractDateFromCVS and ExtractVersionFromCVS.

--- Utilities/vtkmetaio/metaCommand.cxx
+++ Utilities/vtkmetaio/metaCommand.cxx
@@ -47,7 +47,7 @@ MetaCommand()
 }
 
 
-/** Extract the date from the $Date: 2007-10-25 19:01:05 $ cvs command */
+/** Extract the date from a CVS Date keyword/value pair.  */
 METAIO_STL::string MetaCommand::
 ExtractDateFromCVS(METAIO_STL::string date)
 {
@@ -70,7 +70,7 @@ SetDateFromCVS(METAIO_STL::string cvsDate)
   this->SetDate( this->ExtractDateFromCVS( cvsDate ).c_str() );
 }
 
-/** Extract the version from the $Revision: 1.15 $ cvs command */
+/** Extract the version from a CVS Revision keyword/value pair.  */
 METAIO_STL::string MetaCommand::
 ExtractVersionFromCVS(METAIO_STL::string version)
 {
