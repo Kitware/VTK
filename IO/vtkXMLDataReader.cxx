9fc9900ebdd4afccb74a87c123fc42645a3ad1f9
David Thompson <dcthomp@sandia.gov>
2008-01-16 23:05:58 -0500

    BUG: Use GetAbstractArray() instead of GetArray() when printing error      so we don't segfault if we're reading a vtkStringArray.

--- IO/vtkXMLDataReader.cxx
+++ IO/vtkXMLDataReader.cxx
@@ -29,7 +29,7 @@
 
 #include "assert.h"
 
-vtkCxxRevisionMacro(vtkXMLDataReader, "1.34");
+vtkCxxRevisionMacro(vtkXMLDataReader, "1.35");
 
 //----------------------------------------------------------------------------
 vtkXMLDataReader::vtkXMLDataReader()
@@ -527,7 +527,7 @@ int vtkXMLDataReader::ReadPieceData()
           if(!this->ReadArrayForCells(eNested, cellData->GetAbstractArray(a++)))
             {
             vtkErrorMacro("Cannot read cell data array \""
-              << cellData->GetArray(a-1)->GetName() << "\" from "
+              << cellData->GetAbstractArray(a-1)->GetName() << "\" from "
               << ePointData->GetName() << " in piece " << this->Piece
               << ".  The data array in the element may be too short.");
             return 0;
