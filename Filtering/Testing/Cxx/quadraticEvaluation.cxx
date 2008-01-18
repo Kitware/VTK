8c700f254e589ae949db353d7ee966e002476538
David Thompson <dcthomp@sandia.gov>
2008-01-18 16:49:14 -0500

    BUG: Attempt a fix for the memory corruption in the test and      take out debug printouts.

--- Filtering/Testing/Cxx/quadraticEvaluation.cxx
+++ Filtering/Testing/Cxx/quadraticEvaluation.cxx
@@ -69,61 +69,47 @@ int TestQE(ostream& strm)
   double *paramcoor;
 
   //-----------------------------------------------------------
-  cout << "Instantiation...\n"; cout.flush();
   strm << "Test instantiation New() and NewInstance() Start" << endl;
-  cout << "  QEdge...\n"; cout.flush();
   vtkQuadraticEdge *edge = vtkQuadraticEdge::New();
   vtkQuadraticEdge *edge2 = edge->NewInstance();
 
-  cout << "  QTri...\n"; cout.flush();
   vtkQuadraticTriangle *tri = vtkQuadraticTriangle::New();
   vtkQuadraticTriangle *tri2 = tri->NewInstance();
 
-  cout << "  QQuad...\n"; cout.flush();
   vtkQuadraticQuad *quad = vtkQuadraticQuad::New();
   vtkQuadraticQuad *quad2 = quad->NewInstance();
 
-  cout << "  QTet...\n"; cout.flush();
   vtkQuadraticTetra *tetra = vtkQuadraticTetra::New();
   vtkQuadraticTetra *tetra2 = tetra->NewInstance();
 
-  cout << "  QHex...\n"; cout.flush();
   vtkQuadraticHexahedron *hex = vtkQuadraticHexahedron::New();
   vtkQuadraticHexahedron *hex2 = hex->NewInstance();
 
-  cout << "  QWedge...\n"; cout.flush();
   vtkQuadraticWedge *wedge = vtkQuadraticWedge::New();
   vtkQuadraticWedge *wedge2 = wedge->NewInstance();
 
-  cout << "  QPyramid...\n"; cout.flush();
   vtkQuadraticPyramid *pyra = vtkQuadraticPyramid::New();
   vtkQuadraticHexahedron *pyra2 = hex->NewInstance();
   
   // New quadratic cells
   
-  cout << "  QLQuad...\n"; cout.flush();
   vtkQuadraticLinearQuad *quadlin = vtkQuadraticLinearQuad::New();
   vtkQuadraticLinearQuad *quadlin2 = quadlin->NewInstance();
 
-  cout << "  QQuad...\n"; cout.flush();
   vtkBiQuadraticQuad *biquad = vtkBiQuadraticQuad::New();
   vtkBiQuadraticQuad *biquad2 = biquad->NewInstance();
 
-  cout << "  QLWedge...\n"; cout.flush();
   vtkQuadraticLinearWedge *wedgelin = vtkQuadraticLinearWedge::New();
   vtkQuadraticLinearWedge *wedgelin2 = wedgelin->NewInstance();
 
-  cout << "  QQWedge...\n"; cout.flush();
   vtkBiQuadraticQuadraticWedge *biwedge = vtkBiQuadraticQuadraticWedge::New();
   vtkBiQuadraticQuadraticWedge *biwedge2 = biwedge->NewInstance();
 
 
-  cout << "  QQQHex...\n"; cout.flush();
   vtkTriQuadraticHexahedron *trihex = vtkTriQuadraticHexahedron::New();
   vtkTriQuadraticHexahedron *trihex2 = trihex->NewInstance();
 
 
-  cout << "  QQHex...\n"; cout.flush();
   vtkBiQuadraticQuadraticHexahedron *bihex = vtkBiQuadraticQuadraticHexahedron::New();
   vtkBiQuadraticQuadraticHexahedron *bihex2 = bihex->NewInstance();
 
@@ -147,11 +133,9 @@ int TestQE(ostream& strm)
 
 
   //-------------------------------------------------------------
-  cout << "EvalPosn...\n"; cout.flush();
   strm << "Test vtkCell::EvaluatePosition Start" << endl;
 
   // vtkQuadraticEdge
-  cout << "  QEdge...\n"; cout.flush();
   double edgePCoords[3], edgeWeights[3], edgePosition[3];
   double edgePoint[1][3] = {{0.25, 0.125, 0.0}};
   double edgeClosest[3];
@@ -168,7 +152,6 @@ int TestQE(ostream& strm)
                          dist2, edgeWeights);
 
   // vtkQuadraticTriangle
-  cout << "  QTri...\n"; cout.flush();
   double triPCoords[3], triWeights[6], triPosition[3];
   double triPoint[1][3] = {{0.5, 0.266667, 0.0}};
   double triClosest[3];
@@ -192,7 +175,6 @@ int TestQE(ostream& strm)
 
 
   // vtkQuadraticQuad
-  cout << "  QQuad...\n"; cout.flush();
   double quadPCoords[3], quadWeights[8], quadPosition[3];
   double quadPoint[1][3] = {{0.25, 0.33, 0.0}};
   double quadClosest[3];
@@ -219,7 +201,6 @@ int TestQE(ostream& strm)
                          dist2, quadWeights);
 
   // vtkQuadraticTetra
-  cout << "  QTet...\n"; cout.flush();
   double tetraPCoords[3], tetraWeights[10], tetraPosition[3];
   double tetraPoint[1][3] = {{0.5, 0.266667, 0.333333}};
   double tetraClosest[3];
@@ -251,7 +232,6 @@ int TestQE(ostream& strm)
 
 
   // vtkQuadraticHexahedron
-  cout << "  QHex...\n"; cout.flush();
   double hexPCoords[3], hexWeights[20], hexPosition[3];
   double hexPoint[1][3] = {{0.25, 0.33333, 0.666667}};
   double hexClosest[3];
@@ -302,7 +282,6 @@ int TestQE(ostream& strm)
                          dist2, hexWeights);
 
   // vtkQuadraticWedge
-  cout << "  QWedge...\n"; cout.flush();
   double wedgePCoords[3], wedgeWeights[20], wedgePosition[3];
   double wedgePoint[1][3] = {{0.25, 0.33333, 0.666667}};
   double wedgeClosest[3];
@@ -343,7 +322,6 @@ int TestQE(ostream& strm)
                          dist2, wedgeWeights);
 
   // vtkQuadraticPyramid
-  cout << "  QPyramid...\n"; cout.flush();
   double pyraPCoords[3], pyraWeights[20], pyraPosition[3];
   double pyraPoint[1][3] = {{0.25, 0.33333, 0.666667}};
   double pyraClosest[3];
@@ -382,7 +360,6 @@ int TestQE(ostream& strm)
   // New quadratic cells
 
   // vtkQuadraticLinearQuad
-  cout << "  QLQuad...\n"; cout.flush();
   double quadlinPCoords[3], quadlinWeights[6], quadlinPosition[3];
   double quadlinPoint[1][3] = {{0.25, 0.33, 0.0}};
   double quadlinClosest[3];
@@ -398,8 +375,7 @@ int TestQE(ostream& strm)
                          dist2, quadlinWeights);
 
   // vtkBiQuadraticQuad
-  cout << "  QQQuad...\n"; cout.flush();
-  double biquadPCoords[3], biquadWeights[6], biquadPosition[3];
+  double biquadPCoords[3], biquadWeights[9], biquadPosition[3];
   double biquadPoint[1][3] = {{0.25, 0.33, 0.0}};
   double biquadClosest[3];
   paramcoor = biquad->GetParametricCoords();
@@ -414,7 +390,6 @@ int TestQE(ostream& strm)
                          dist2, biquadWeights);
 
   // vtkQuadraticLinearWedge
-  cout << "  QLWedge...\n"; cout.flush();
   double wedgelinPCoords[3], wedgelinWeights[12], wedgelinPosition[3];
   double wedgelinPoint[1][3] = {{0.25, 0.33333, 0.666667}};
   double wedgelinClosest[3];
@@ -430,7 +405,6 @@ int TestQE(ostream& strm)
                          dist2, wedgelinWeights);
 
   // vtkBiQuadraticQuadraticWedge
-  cout << "  QQWedge...\n"; cout.flush();
   double biwedgePCoords[3], biwedgeWeights[18], biwedgePosition[3];
   double biwedgePoint[1][3] = {{0.25, 0.33333, 0.666667}};
   double biwedgeClosest[3];
@@ -446,7 +420,6 @@ int TestQE(ostream& strm)
                          dist2, biwedgeWeights);
 
   // vtkBiQuadraticQuadraticHexahedron
-  cout << "  QQHex...\n"; cout.flush();
   double bihexPCoords[3], bihexWeights[24], bihexPosition[3];
   double bihexPoint[1][3] = {{0.25, 0.33333, 0.666667}};
   double bihexClosest[3];
@@ -462,7 +435,6 @@ int TestQE(ostream& strm)
                          dist2, bihexWeights);
 
   // vtkTriQuadraticHexahedron
-  cout << "  QQQHex...\n"; cout.flush();
   double trihexPCoords[3], trihexWeights[27], trihexPosition[3];
   double trihexPoint[1][3] = {{0.25, 0.33333, 0.666667}};
   double trihexClosest[3];
@@ -481,67 +453,52 @@ int TestQE(ostream& strm)
   strm << "Test vtkCell::EvaluatePosition End" << endl;
 
   //-------------------------------------------------------------
-  cout << "EvalLocn...\n"; cout.flush();
   strm << "Test vtkCell::EvaluateLocation Start" << endl;
   // vtkQuadraticEdge
-  cout << "  QEdge...\n"; cout.flush();
   edge->EvaluateLocation(subId, edgePCoords, edgePosition, edgeWeights);
 
   // vtkQuadraticTriangle
-  cout << "  QTri...\n"; cout.flush();
   tri->EvaluateLocation(subId, triPCoords, triPosition, triWeights);
 
   // vtkQuadraticQuad
-  cout << "  QQuad...\n"; cout.flush();
   quad->EvaluateLocation(subId, quadPCoords, quadPosition, quadWeights);
 
   // vtkQuadraticTetra
-  cout << "  QTet...\n"; cout.flush();
   tetra->EvaluateLocation(subId, tetraPCoords, tetraPosition, tetraWeights);
 
   // vtkQuadraticHexahedron
-  cout << "  QHex...\n"; cout.flush();
   hex->EvaluateLocation(subId, hexPCoords, hexPosition, hexWeights);
 
   // vtkQuadraticWedge
-  cout << "  QWedge...\n"; cout.flush();
   wedge->EvaluateLocation(subId, wedgePCoords, wedgePosition, wedgeWeights);
 
   // vtkQuadraticPyramid
-  cout << "  QPyramid...\n"; cout.flush();
   pyra->EvaluateLocation(subId, pyraPCoords, pyraPosition, pyraWeights);
 
   // New quadratic cells
 
   // vtkQuadraticLinearQuad
-  cout << "  QLQuad...\n"; cout.flush();
   quadlin->EvaluateLocation(subId, quadlinPCoords, quadlinPosition, quadlinWeights);
 
   // vtkBiQuadraticQuad
-  cout << "  QQQuad...\n"; cout.flush();
   biquad->EvaluateLocation(subId, biquadPCoords, biquadPosition, biquadWeights);
 
 
   // vtkQuadraticLinearWedge
-  cout << "  QLWedge...\n"; cout.flush();
   wedgelin->EvaluateLocation(subId, wedgelinPCoords, wedgelinPosition, wedgelinWeights);
 
   // vtkBiQuadraticQuadraticWedge
-  cout << "  QQWedge...\n"; cout.flush();
   biwedge->EvaluateLocation(subId, biwedgePCoords, biwedgePosition, biwedgeWeights);
 
   // vtkQuadraticLinearQuad
-  cout << "  QLQuad...\n"; cout.flush();
   bihex->EvaluateLocation(subId, bihexPCoords, bihexPosition, bihexWeights);
 
   // vtkTriQuadraticHexahedron
-  cout << "  QQQHex...\n"; cout.flush();
   trihex->EvaluateLocation(subId, trihexPCoords, trihexPosition, trihexWeights);
 
   strm << "Test vtkCell::EvaluateLocation End" << endl;
 
   //-------------------------------------------------------------
-  cout << "Derivs...\n"; cout.flush();
   strm << "Test vtkCell::CellDerivs Start" << endl;
 
   // vtkQuadraticEdge - temporarily commented out
@@ -550,37 +507,31 @@ int TestQE(ostream& strm)
   //edge->Derivatives(subId, edgePCoords, edgeValues, 1, edgeDerivs);
 
   // vtkQuadraticTriangle
-  cout << "  QTri...\n"; cout.flush();
   double triValues[6], triDerivs[3];
   ComputeDataValues(tri->Points,triValues);
   tri->Derivatives(subId, triPCoords, triValues, 1, triDerivs);
 
   // vtkQuadraticQuad
-  cout << "  QQuad...\n"; cout.flush();
   double quadValues[8], quadDerivs[3];
   ComputeDataValues(quad->Points,quadValues);
   quad->Derivatives(subId, quadPCoords, quadValues, 1, quadDerivs);
 
   // vtkQuadraticTetra
-  cout << "  QTet...\n"; cout.flush();
   double tetraValues[10], tetraDerivs[3];
   ComputeDataValues(tetra->Points,tetraValues);
   tetra->Derivatives(subId, tetraPCoords, tetraValues, 1, tetraDerivs);
 
   // vtkQuadraticHexahedron
-  cout << "  QHex...\n"; cout.flush();
   double hexValues[20], hexDerivs[3];
   ComputeDataValues(hex->Points,hexValues);
   hex->Derivatives(subId, hexPCoords, hexValues, 1, hexDerivs);
 
   // vtkQuadraticWedge
-  cout << "  QWedge...\n"; cout.flush();
   double wedgeValues[15], wedgeDerivs[3];
   ComputeDataValues(wedge->Points,wedgeValues);
   wedge->Derivatives(subId, wedgePCoords, wedgeValues, 1, wedgeDerivs);
 
   // vtkQuadraticPyramid
-  cout << "  QPyramid...\n"; cout.flush();
   double pyraValues[13], pyraDerivs[3];
   ComputeDataValues(pyra->Points,pyraValues);
   pyra->Derivatives(subId, pyraPCoords, pyraValues, 1, pyraDerivs);
@@ -588,37 +539,31 @@ int TestQE(ostream& strm)
   // New quadratic cells
 
   // vtkQuadraticLinearQuad
-  cout << "  QLQuad...\n"; cout.flush();
   double quadlinValues[6], quadlinDerivs[3];
   ComputeDataValues(quadlin->Points,quadlinValues);
   quadlin->Derivatives(subId, quadlinPCoords, quadlinValues, 1, quadlinDerivs);
 
   // vtkBiQuadraticQuad
-  cout << "  QQQuad...\n"; cout.flush();
   double biquadValues[9], biquadDerivs[3];
   ComputeDataValues(biquad->Points,biquadValues);
   biquad->Derivatives(subId, biquadPCoords, biquadValues, 1, biquadDerivs);
 
   // vtkQuadraticLinearWedge
-  cout << "  QLWedge...\n"; cout.flush();
   double wedgelinValues[12], wedgelinDerivs[3];
   ComputeDataValues(wedgelin->Points, wedgelinValues);
   wedgelin->Derivatives(subId, wedgelinPCoords, wedgelinValues, 1, wedgelinDerivs);
 
   // vtkBiQuadraticQuadraticWedge
-  cout << "  QQWedge...\n"; cout.flush();
   double biwedgeValues[18], biwedgeDerivs[3];
   ComputeDataValues(biwedge->Points, biwedgeValues);
   biwedge->Derivatives(subId, biwedgePCoords, biwedgeValues, 1, biwedgeDerivs);
 
   // vtkBiQuadraticQuadraticHexahedron
-  cout << "  QQHex...\n"; cout.flush();
   double bihexValues[24], bihexDerivs[3];
   ComputeDataValues(bihex->Points, bihexValues);
   bihex->Derivatives(subId, bihexPCoords, bihexValues, 1, bihexDerivs);
 
   // vtkTriQuadraticHexahedron
-  cout << "  QQQHex...\n"; cout.flush();
   double trihexValues[27], trihexDerivs[3];
   ComputeDataValues(trihex->Points, trihexValues);
   trihex->Derivatives(subId, trihexPCoords, trihexValues, 1, trihexDerivs);
@@ -626,33 +571,19 @@ int TestQE(ostream& strm)
 
   strm << "Test vtkCell::CellDerivs End" << endl;
 
-  cout << "  QEdge...\n"; cout.flush();
   edge->Delete();
-  cout << "  QTri...\n"; cout.flush();
   tri->Delete();
-  cout << "  QTet...\n"; cout.flush();
   tetra->Delete();
-  cout << "  QWedge...\n"; cout.flush();
   wedge->Delete();
-  cout << "  QLWedge...\n"; cout.flush();
   wedgelin->Delete();
-  cout << "  QQWedge...\n"; cout.flush();
   biwedge->Delete();
-  cout << "  QPyramid...\n"; cout.flush();
   pyra->Delete();
-  cout << "  QQuad...\n"; cout.flush();
   quad->Delete();
-  cout << "  QLQuad...\n"; cout.flush();
   quadlin->Delete();
-  cout << "  QQQuad...\n"; cout.flush();
   biquad->Delete();
-  cout << "  QHex...\n"; cout.flush();
   hex->Delete();
-  cout << "  QQHex...\n"; cout.flush();
   bihex->Delete();
-  cout << "  QQQHex...\n"; cout.flush();
   trihex->Delete();
-  cout << "End...\n"; cout.flush();
 
   return 0;
 }
@@ -660,6 +591,5 @@ int TestQE(ostream& strm)
 int quadraticEvaluation(int,char *[])
 {
   vtksys_ios::ostringstream vtkmsg_with_warning_C4701;
-  cout << "Starting...\n"; cout.flush();
   return TestQE(vtkmsg_with_warning_C4701);
 }
