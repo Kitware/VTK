d357edf41849b50ee04975c1c1c3f9b4dde14469
Sebastien Barre <sebastien.barre@kitware.com>
2008-01-23 16:50:13 -0500

    ENH: update for Tcl/Tk 8.5

---
+++ CMake/FindTCL.cmake
@@ -0,0 +1,183 @@
+# - Find Tcl includes and libraries.
+# This module finds if Tcl is installed and determines where the
+# include files and libraries are. It also determines what the name of
+# the library is. This code sets the following variables:
+#  TCL_FOUND              = Tcl was found
+#  TK_FOUND               = Tk was found
+#  TCLTK_FOUND            = Tcl and Tk were found
+#  TCL_LIBRARY            = path to Tcl library (tcl tcl80)
+#  TCL_INCLUDE_PATH       = path to where tcl.h can be found
+#  TCL_TCLSH              = path to tclsh binary (tcl tcl80)
+#  TK_LIBRARY             = path to Tk library (tk tk80 etc)
+#  TK_INCLUDE_PATH        = path to where tk.h can be found
+#  TK_WISH                = full path to the wish executable
+#
+# In an effort to remove some clutter and clear up some issues for people
+# who are not necessarily Tcl/Tk gurus/developpers, some variables were
+# moved or removed. Changes compared to CMake 2.4 are:
+# - The stub libraries are now found in FindTclStub.cmake
+#   => they were only useful for people writing Tcl/Tk extensions.
+# - TCL_LIBRARY_DEBUG and TK_LIBRARY_DEBUG were removed.
+#   => these libs are not packaged by default with Tcl/Tk distributions. 
+#      Even when Tcl/Tk is built from source, several flavors of debug libs
+#      are created and there is no real reason to pick a single one
+#      specifically (say, amongst tcl84g, tcl84gs, or tcl84sgx). 
+#      Let's leave that choice to the user by allowing him to assign 
+#      TCL_LIBRARY to any Tcl library, debug or not.
+# - TK_INTERNAL_PATH was removed.
+#   => this ended up being only a Win32 variable, and there is a lot of
+#      confusion regarding the location of this file in an installed Tcl/Tk
+#      tree anyway (see 8.5 for example). If you need the internal path at
+#      this point it is safer you ask directly where the *source* tree is
+#      and dig from there.
+
+INCLUDE(CMakeFindFrameworks)
+INCLUDE(FindTclsh)
+INCLUDE(FindWish)
+
+GET_FILENAME_COMPONENT(TCL_TCLSH_PATH "${TCL_TCLSH}" PATH)
+GET_FILENAME_COMPONENT(TCL_TCLSH_PATH_PARENT "${TCL_TCLSH_PATH}" PATH)
+
+GET_FILENAME_COMPONENT(TK_WISH_PATH "${TK_WISH}" PATH)
+GET_FILENAME_COMPONENT(TK_WISH_PATH_PARENT "${TK_WISH_PATH}" PATH)
+
+GET_FILENAME_COMPONENT(TCL_INCLUDE_PATH_PARENT "${TCL_INCLUDE_PATH}" PATH)
+GET_FILENAME_COMPONENT(TK_INCLUDE_PATH_PARENT "${TK_INCLUDE_PATH}" PATH)
+
+GET_FILENAME_COMPONENT(TCL_LIBRARY_PATH "${TCL_LIBRARY}" PATH)
+GET_FILENAME_COMPONENT(TCL_LIBRARY_PATH_PARENT "${TCL_LIBRARY_PATH}" PATH)
+
+GET_FILENAME_COMPONENT(TK_LIBRARY_PATH "${TK_LIBRARY}" PATH)
+GET_FILENAME_COMPONENT(TK_LIBRARY_PATH_PARENT "${TK_LIBRARY_PATH}" PATH)
+
+GET_FILENAME_COMPONENT(
+  ActiveTcl_CurrentVersion 
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\ActiveState\\ActiveTcl;CurrentVersion]" 
+  NAME)
+
+SET (TCLTK_POSSIBLE_LIB_PATHS
+  "${TCL_INCLUDE_PATH_PARENT}/lib"
+  "${TK_INCLUDE_PATH_PARENT}/lib"
+  "${TCL_LIBRARY_PATH}"
+  "${TK_LIBRARY_PATH}"
+  "${TCL_TCLSH_PATH_PARENT}/lib"
+  "${TK_WISH_PATH_PARENT}/lib"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\ActiveState\\ActiveTcl\\${ActiveTcl_CurrentVersion}]/lib"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.6;Root]/lib"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.5;Root]/lib"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.4;Root]/lib"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.3;Root]/lib"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.2;Root]/lib"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.0;Root]/lib"
+  "$ENV{ProgramFiles}/Tcl/Lib"
+  "C:/Program Files/Tcl/lib" 
+  "C:/Tcl/lib" 
+  /usr/lib 
+  /usr/local/lib
+)
+
+FIND_LIBRARY(TCL_LIBRARY
+  NAMES tcl 
+  tcl86 tcl8.6 
+  tcl85 tcl8.5 
+  tcl84 tcl8.4 
+  tcl83 tcl8.3 
+  tcl82 tcl8.2 
+  tcl80 tcl8.0
+  PATHS ${TCLTK_POSSIBLE_LIB_PATHS}
+)
+
+FIND_LIBRARY(TK_LIBRARY 
+  NAMES tk 
+  tk86 tk8.6
+  tk85 tk8.5 
+  tk84 tk8.4 
+  tk83 tk8.3 
+  tk82 tk8.2 
+  tk80 tk8.0
+  PATHS ${TCLTK_POSSIBLE_LIB_PATHS}
+)
+
+CMAKE_FIND_FRAMEWORKS(Tcl)
+CMAKE_FIND_FRAMEWORKS(Tk)
+
+SET(TCL_FRAMEWORK_INCLUDES)
+IF(Tcl_FRAMEWORKS)
+  IF(NOT TCL_INCLUDE_PATH)
+    FOREACH(dir ${Tcl_FRAMEWORKS})
+      SET(TCL_FRAMEWORK_INCLUDES ${TCL_FRAMEWORK_INCLUDES} ${dir}/Headers)
+    ENDFOREACH(dir)
+  ENDIF(NOT TCL_INCLUDE_PATH)
+ENDIF(Tcl_FRAMEWORKS)
+
+SET(TK_FRAMEWORK_INCLUDES)
+IF(Tk_FRAMEWORKS)
+  IF(NOT TK_INCLUDE_PATH)
+    FOREACH(dir ${Tk_FRAMEWORKS})
+      SET(TK_FRAMEWORK_INCLUDES ${TK_FRAMEWORK_INCLUDES}
+        ${dir}/Headers ${dir}/PrivateHeaders)
+    ENDFOREACH(dir)
+  ENDIF(NOT TK_INCLUDE_PATH)
+ENDIF(Tk_FRAMEWORKS)
+
+SET (TCLTK_POSSIBLE_INCLUDE_PATHS
+  "${TCL_LIBRARY_PATH_PARENT}/include"
+  "${TK_LIBRARY_PATH_PARENT}/include"
+  "${TCL_INCLUDE_PATH}"
+  "${TK_INCLUDE_PATH}"
+  "${TCL_TCLSH_PATH_PARENT}/include"
+  "${TK_WISH_PATH_PARENT}/include"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\ActiveState\\ActiveTcl\\${ActiveTcl_CurrentVersion}]/include"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.6;Root]/include"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.5;Root]/include"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.4;Root]/include"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.3;Root]/include"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.2;Root]/include"
+  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Scriptics\\Tcl\\8.0;Root]/include"
+  "$ENV{ProgramFiles}/Tcl/include"
+  "C:/Program Files/Tcl/include"
+  "C:/Tcl/include"
+  /usr/include
+  /usr/local/include
+  /usr/include/tcl8.6
+  /usr/include/tcl8.5
+  /usr/include/tcl8.4
+  /usr/include/tcl8.3
+  /usr/include/tcl8.2
+  /usr/include/tcl8.0
+)
+
+FIND_PATH(TCL_INCLUDE_PATH tcl.h
+  ${TCL_FRAMEWORK_INCLUDES} ${TCLTK_POSSIBLE_INCLUDE_PATHS}
+)
+
+FIND_PATH(TK_INCLUDE_PATH tk.h
+  ${TK_FRAMEWORK_INCLUDES} ${TCLTK_POSSIBLE_INCLUDE_PATHS}
+)
+
+MARK_AS_ADVANCED(
+  TCL_TCLSH_PATH
+  TK_WISH_PATH
+  TCL_INCLUDE_PATH
+  TK_INCLUDE_PATH
+  TCL_LIBRARY
+  TK_LIBRARY
+  )
+
+IF(TCL_INCLUDE_PATH AND TCL_LIBRARY)
+  SET(TCL_FOUND 1)
+ENDIF(TCL_INCLUDE_PATH AND TCL_LIBRARY)
+  SET(TCL_FOUND 0)
+ENDIF(TCL_INCLUDE_PATH AND TCL_LIBRARY)
+
+IF(TK_INCLUDE_PATH AND TK_LIBRARY)
+  SET(TK_FOUND 1)
+ELSE(TK_INCLUDE_PATH AND TK_LIBRARY)
+  SET(TK_FOUND 0)
+ENDIF(TK_INCLUDE_PATH AND TK_LIBRARY)
+
+IF(TCL_FOUND AND TK_FOUND)
+  SET(TCLTK_FOUND 1)
+ELSE(TCL_FOUND AND TK_FOUND)
+  SET(TCLTK_FOUND 0)
+ENDIF(TCL_FOUND AND TK_FOUND)
--- CMake/vtkTclTkMacros.cmake
+++ CMake/vtkTclTkMacros.cmake
@@ -20,7 +20,7 @@ MACRO (VTK_GET_TCL_TK_VERSION tcl_tk_major_version tcl_tk_minor_version)
   ELSE (TK_INTERNAL_PATH)
     SET (try_tk_internal_path ${VTK_TK_INTERNAL_DIR})
   ENDIF (TK_INTERNAL_PATH)
-  FOREACH (tcl_tk_minor_version_try "2" "3" "4")
+  FOREACH (tcl_tk_minor_version_try "2" "3" "4" "5")
     IF ("${try_tk_internal_path}" MATCHES "tk8\\.?${tcl_tk_minor_version_try}")
       SET (${tcl_tk_major_version} "8")
       SET (${tcl_tk_minor_version} ${tcl_tk_minor_version_try})
@@ -42,14 +42,14 @@ MACRO (VTK_GET_TCL_TK_VERSION tcl_tk_major_version tcl_tk_minor_version)
 
   FOREACH(dir ${TCL_INCLUDE_PATH})
     IF(EXISTS "${dir}/tcl.h")
-      FILE(READ "${TCL_INCLUDE_PATH}/tcl.h" tcl_include_file)
+      FILE(READ "${dir}/tcl.h" tcl_include_file)
       STRING(REGEX REPLACE
         ".*#define TCL_VERSION[ \t]*\"([0-9][0-9]*\\.[0-9][0-9]*)\".*" "\\1"
         tcl_include_file "${tcl_include_file}")
       IF(${tcl_include_file} MATCHES "^[0-9]*\\.[0-9]*$")
-        STRING(REGEX REPLACE "^([0-9]*)\\.([0-9]*)$" "\\1" "${tcl_tk_major_version}"
+        STRING(REGEX REPLACE "^([0-9]*)\\.([0-9]*)$" "\\1" ${tcl_tk_major_version}
           "${tcl_include_file}")
-        STRING(REGEX REPLACE "^([0-9]*)\\.([0-9]*)$" "\\2" "${tcl_tk_minor_version}"
+        STRING(REGEX REPLACE "^([0-9]*)\\.([0-9]*)$" "\\2" ${tcl_tk_minor_version}
           "${tcl_include_file}")
       ENDIF(${tcl_include_file} MATCHES "^[0-9]*\\.[0-9]*$")
     ENDIF(EXISTS "${dir}/tcl.h")
@@ -85,12 +85,15 @@ MACRO (VTK_GET_TCL_SUPPORT_FILES tcl_support_lib_dir list)
 
   # Tcl support files (*.tcl + encoding + tclIndex, etc.)
 
-  FILE (GLOB TCL_SUPPORT_FILES_TCL "${tcl_support_lib_dir}/*.tcl")
-  FILE (GLOB TCL_SUPPORT_FILES_ENC "${tcl_support_lib_dir}/encoding/*.enc")
+  FILE (GLOB TCL_SUPPORT_FILES_TCL  "${tcl_support_lib_dir}/*.tcl")
+  FILE (GLOB TCL_SUPPORT_FILES_ENC  "${tcl_support_lib_dir}/encoding/*.enc")
+  FILE (GLOB TCL_SUPPORT_FILES_MSGS "${tcl_support_lib_dir}/msgs/*.msg")
   SET (${list}
     "${tcl_support_lib_dir}/tclIndex" 
     ${TCL_SUPPORT_FILES_TCL} 
-    ${TCL_SUPPORT_FILES_ENC})
+    ${TCL_SUPPORT_FILES_ENC}
+    ${TCL_SUPPORT_FILES_MSGS}
+    )
 
 ENDMACRO (VTK_GET_TCL_SUPPORT_FILES)
 
@@ -98,10 +101,15 @@ MACRO (VTK_GET_TK_SUPPORT_FILES tk_support_lib_dir list)
 
   # Tk support files (*.tcl + tclIndex, etc.)
 
-  FILE (GLOB TK_SUPPORT_FILES_TCL "${tk_support_lib_dir}/*.tcl")
+  FILE (GLOB TK_SUPPORT_FILES_TCL  "${tk_support_lib_dir}/*.tcl")
+  FILE (GLOB TK_SUPPORT_FILES_MSGS "${tk_support_lib_dir}/msgs/*.msg")
+  FILE (GLOB TK_SUPPORT_FILES_TTK  "${tk_support_lib_dir}/ttk/*.tcl")
   SET (${list}
     "${tk_support_lib_dir}/tclIndex" 
-    ${TK_SUPPORT_FILES_TCL})
+    ${TK_SUPPORT_FILES_TCL}
+    ${TK_SUPPORT_FILES_MSGS}
+    ${TK_SUPPORT_FILES_TTK}
+    )
 
 ENDMACRO (VTK_GET_TK_SUPPORT_FILES)
 
