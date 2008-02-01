bb6b5ac5bec6fb7789fc8bcaff8fff35ff4e2dbb
Jeffrey Baumes <jeff.baumes@kitware.com>
2008-02-01 09:17:31 -0500

    STYLE: Adding Chaco reader comments.

--- Infovis/vtkChacoGraphReader.h
+++ Infovis/vtkChacoGraphReader.h
@@ -16,6 +16,30 @@
  Copyright (c) Sandia Corporation
  See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
 ----------------------------------------------------------------------------*/
+// .NAME vtkChacoGraphReader - Reads chaco graph files.
+//
+// .SECTION Description
+// vtkChacoGraphReader reads in files in the Chaco format into a vtkGraph.
+// An example is the following
+// <code>
+// 10 13
+// 2 6 10
+// 1 3
+// 2 4 8
+// 3 5
+// 4 6 10
+// 1 5 7
+// 6 8
+// 3 7 9
+// 8 10
+// 1 5 9
+// </code>
+// The first line specifies the number of vertices and edges
+// in the graph. Each additional line contains the vertices adjacent
+// to a particular vertex.  In this example, vertex 1 is adjacent to
+// 2, 6 and 10, vertex 2 is adjacent to 1 and 3, etc.  Since Chaco ids
+// start at 1 and VTK ids start at 0, the vertex ids in the vtkGraph
+// will be 1 less than the Chaco ids.
 
 #ifndef _vtkChacoGraphReader_h
 #define _vtkChacoGraphReader_h
