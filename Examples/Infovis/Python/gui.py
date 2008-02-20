4724060cd835a5fe7cfbee05f5abc136bebdc27b
Timothy M. Shead <tshead@sandia.gov>
2008-02-20 11:56:06 -0500

    STYLE: Improved documentation for the Tython PyQt example

--- Examples/Infovis/Python/gui.py
+++ Examples/Infovis/Python/gui.py
@@ -4,16 +4,19 @@ from PyQt4 import uic
 from vtk import *
 import sys
 
+# Create a PyQt window using a .ui file generated with Qt Designer ...
 application = QtGui.QApplication(sys.argv)
 
 window = uic.loadUi("gui.ui")
 vertex_slider = window.findChild(QtGui.QSlider, "vertexCount")
 edge_slider = window.findChild(QtGui.QSlider, "edgeCount")
 
+# Create a simple pipeline source ...
 source = vtkRandomGraphSource()
 source.SetNumberOfVertices(vertex_slider.value())
 source.SetNumberOfEdges(edge_slider.value())
 
+# Define event-handlers that update the pipeline in response to widget events ...
 def change_vertex_count(count):
   source.SetNumberOfVertices(count)
   edge_slider.setValue(source.GetNumberOfEdges())
@@ -26,17 +29,20 @@ def change_edge_count(count):
   view.GetRenderer().ResetCamera()
   render_window.Render()
 
+# Connect the GUI widgets to the event-handlers ...
 QtCore.QObject.connect(vertex_slider, QtCore.SIGNAL("valueChanged(int)"), change_vertex_count)
 QtCore.QObject.connect(edge_slider, QtCore.SIGNAL("valueChanged(int)"), change_edge_count)
 window.show()
 
-# Setup a VTK window, but don't start an event-loop ...
+# Setup a VTK view ...
 view = vtkGraphLayoutView()
 view.AddRepresentationFromInputConnection(source.GetOutputPort())
 
 render_window = vtkRenderWindow()
 render_window.SetSize(600, 600)
 view.SetupRenderWindow(render_window)
+
+# This initializes the VTK window for interaction, but doesn't start an event-loop ...
 render_window.Start()
 
 # Start the Qt event-loop ...
