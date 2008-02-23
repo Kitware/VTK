a4621b67ae04a1442713267bbe8b6f6e6c4f0d0d
Philippe Pebay <pppebay@sandia.gov>
2008-02-23 17:44:04 -0500

    ENH: by default (and this is the case e.g. for SQLite), index names      are not supported when creating either a PRIMARY or a UNIQUE index      within a CREATE TABLE statement. Make this the default implementation      at the parent class level, in order to avoid potential problems with      yet unknown database backends.

--- IO/vtkSQLDatabase.cxx
+++ IO/vtkSQLDatabase.cxx
@@ -39,7 +39,7 @@ PURPOSE.  See the above copyright notice for more information.
 #include <vtksys/SystemTools.hxx>
 #include <vtksys/ios/sstream>
 
-vtkCxxRevisionMacro(vtkSQLDatabase, "1.34");
+vtkCxxRevisionMacro(vtkSQLDatabase, "1.35");
 
 // ----------------------------------------------------------------------
 vtkSQLDatabase::vtkSQLDatabase()
@@ -218,7 +218,11 @@ vtkStdString vtkSQLDatabase::GetIndexSpecification( vtkSQLDatabaseSchema* schema
       return vtkStdString();
     }
   
-  queryStr += schema->GetIndexNameFromHandle( tblHandle, idxHandle );
+  // No index_name for PRIMARY KEYs nor UNIQUEs
+  if ( skipped )
+    {
+    queryStr += schema->GetIndexNameFromHandle( tblHandle, idxHandle );
+    }
 
   // CREATE INDEX <index name> ON <table name> syntax
   if ( skipped )
