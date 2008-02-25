a6e8129c3cafb64cfce7cfd8264105ec5b9b684b
David Thompson <dcthomp@sandia.gov>
2008-02-25 02:02:39 -0500

    BUG: Do not derive vtkMySQLDatabasePrivate from vtkObject. It is a private      class used only by vtkMySQLDatabase and vtkMySQLQuery. It is now      excluded from HeaderTesting-IO so there should be no complaints from      that test and *not* inheriting vtkObject should prevent error messages      about deleting a vtkObject with non-zero reference count.

--- IO/vtkMySQLDatabasePrivate.h
+++ IO/vtkMySQLDatabasePrivate.h
@@ -1,21 +1,19 @@
 #ifndef __vtkMySQLDatabasePrivate_h
 #define __vtkMySQLDatabasePrivate_h
 
-#include "vtkObject.h"
-
 #ifdef _WIN32
 # include <winsock.h> // mysql.h relies on the typedefs from here
 #endif
 
 #include <mysql.h> // needed for MYSQL typedefs
 
-class VTK_IO_EXPORT vtkMySQLDatabasePrivate : public vtkObject
+class VTK_IO_EXPORT vtkMySQLDatabasePrivate
 {
 public:
   vtkMySQLDatabasePrivate() :
-    Connection(NULL)
+    Connection( NULL )
   {
-    mysql_init(&this->NullConnection);
+  mysql_init( &this->NullConnection );
   }
 
   MYSQL NullConnection;
