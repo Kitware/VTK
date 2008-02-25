334fe0b1ad46782dc513085bafd0fa20db30e9f9
Philippe Pebay <pppebay@sandia.gov>
2008-02-25 12:22:36 -0500

    ENH: added tests regarding for ExecuteSchema when MySQL is used. Several      aspects of the resulting DB are tested and compared to the original      schema.

--- IO/Testing/Cxx/TestMySQLDatabase.cxx
+++ IO/Testing/Cxx/TestMySQLDatabase.cxx
@@ -18,24 +18,24 @@
   See Copyright.txt or http://www.paraview.org/HTML/Copyright.html for details.
   ----------------------------------------------------------------------------*/
 // .SECTION Thanks
-// Thanks to Andrew Wilson from Sandia National Laboratories for implementing
-// this test.
+// Thanks to Andrew Wilson and Philippe Pebay from Sandia National Laboratories 
+// for implementing this test.
 
 #include "vtkMySQLDatabase.h"
-#include "vtkRowQueryToTable.h"
 #include "vtkSQLQuery.h"
-#include "vtkSmartPointer.h"
+#include "vtkRowQueryToTable.h"
+#include "vtkSQLDatabaseSchema.h"
 #include "vtkStdString.h"
 #include "vtkTable.h"
-#include "vtkToolkits.h"
 #include "vtkVariant.h"
 #include "vtkVariantArray.h"
+#include "vtkToolkits.h"
+
+#include <vtkstd/vector>
 
 int TestMySQLDatabase( int, char ** const )
 {
-  vtkSmartPointer<vtkMySQLDatabase> db;
-  db.TakeReference(vtkMySQLDatabase::SafeDownCast( vtkSQLDatabase::CreateFromURL( VTK_MYSQL_TEST_URL ) ) );
-
+  vtkMySQLDatabase* db = vtkMySQLDatabase::SafeDownCast( vtkSQLDatabase::CreateFromURL( VTK_MYSQL_TEST_URL ) );
   bool status = db->Open();
 
   if ( ! status )
@@ -44,10 +44,7 @@ int TestMySQLDatabase( int, char ** const )
     return 1;
     }
 
-  vtkSmartPointer<vtkSQLQuery> query;
-  query.TakeReference(db->GetQueryInstance());
-
-/*  
+  vtkSQLQuery* query = db->GetQueryInstance();
   vtkStdString createQuery( "CREATE TABLE IF NOT EXISTS people (name TEXT, age INTEGER, weight FLOAT)" );
   cout << createQuery << endl;
   query->SetQuery( createQuery.c_str() );
@@ -119,8 +116,7 @@ int TestMySQLDatabase( int, char ** const )
     cerr << query->GetFieldName( col );
     }
   cerr << endl;
-
-  vtkSmartPointer<vtkVariantArray> va = vtkSmartPointer<vtkVariantArray>::New();
+  vtkVariantArray* va = vtkVariantArray::New();
   while ( query->NextRow( va ) )
     {
     for ( int field = 0; field < va->GetNumberOfValues(); ++ field )
@@ -133,9 +129,10 @@ int TestMySQLDatabase( int, char ** const )
       }
     cerr << endl;
     }
+  va->Delete();
 
   cerr << endl << "Using vtkRowQueryToTable to execute query:" << endl;
-  vtkSmartPointer<vtkRowQueryToTable> reader = vtkSmartPointer<vtkRowQueryToTable>::New();
+  vtkRowQueryToTable* reader = vtkRowQueryToTable::New();
   reader->SetQuery( query );
   reader->Update();
   vtkTable* table = reader->GetOutput();
@@ -156,7 +153,227 @@ int TestMySQLDatabase( int, char ** const )
 
   query->SetQuery( "DROP TABLE people" );
   query->Execute();
-*/
+
+  reader->Delete();
+  query->Delete();
+  db->Delete();
+
+// ----------------------------------------------------------------------
+// Testing transformation of a schema into a MySQL database
+
+  // 1. Create the schema
+  cerr << "@@ Creating a schema...";
+
+  vtkSQLDatabaseSchema* schema = vtkSQLDatabaseSchema::New();
+  schema->SetName( "TestSchema" );
+
+  int tblHandle = schema->AddTableMultipleArguments( "StrangeTable",
+    vtkSQLDatabaseSchema::COLUMN_TOKEN, vtkSQLDatabaseSchema::SERIAL,  "TableKey",  0, "",
+    vtkSQLDatabaseSchema::COLUMN_TOKEN, vtkSQLDatabaseSchema::VARCHAR, "SomeName", 11, "NOT NULL",
+    vtkSQLDatabaseSchema::COLUMN_TOKEN, vtkSQLDatabaseSchema::BIGINT,  "SomeNmbr", 17, "DEFAULT 0",
+    vtkSQLDatabaseSchema::INDEX_TOKEN,  vtkSQLDatabaseSchema::PRIMARY_KEY, "BigKey",
+    vtkSQLDatabaseSchema::INDEX_COLUMN_TOKEN, "TableKey",
+    vtkSQLDatabaseSchema::END_INDEX_TOKEN,
+    vtkSQLDatabaseSchema::INDEX_TOKEN,  vtkSQLDatabaseSchema::UNIQUE, "ReverseLookup",
+    vtkSQLDatabaseSchema::INDEX_COLUMN_TOKEN, "SomeName",
+    vtkSQLDatabaseSchema::INDEX_COLUMN_TOKEN, "SomeNmbr",
+    vtkSQLDatabaseSchema::END_INDEX_TOKEN,
+    vtkSQLDatabaseSchema::TRIGGER_TOKEN,  vtkSQLDatabaseSchema::AFTER_INSERT,
+      "InsertTrigger", "INSERT INTO OtherTable ( Value ) VALUES NEW.SomeNmbr",
+    vtkSQLDatabaseSchema::END_TABLE_TOKEN
+  );
+
+  if ( tblHandle < 0 )
+    {
+    cerr << "Could not create test schema.\n";
+    return 1;
+    }
+  cerr << " done." << endl;
+  
+  // 2. Convert the schema into a MySQL database
+  cerr << "@@ Converting the schema into a MySQL database...";
+
+  db = vtkMySQLDatabase::SafeDownCast( vtkSQLDatabase::CreateFromURL( VTK_MYSQL_TEST_URL ) );
+  status = db->Open();
+
+  if ( ! status )
+    {
+    cerr << "Couldn't open database.\n";
+    return 1;
+    }
+
+  status = db->EffectSchema( schema ); 
+  if ( ! status )
+    {
+    cerr << "Could not effect test schema.\n";
+    return 1;
+    }
+  cerr << " done." << endl;
+
+  // 3. Count tables of the newly created database
+  cerr << "@@ Counting tables of the newly created database... ";
+
+  query = db->GetQueryInstance();
+  query->SetQuery( "SHOW TABLES" );
+  if ( ! query->Execute() )
+    {
+    cerr << "Query failed" << endl;
+    return 1;
+    }
+
+  int numTbl = query->GetNumberOfFields();
+  if ( numTbl != schema->GetNumberOfTables() )
+    {
+    cerr << "Found an incorrect number of tables: " 
+         << numTbl 
+         << " != " 
+         << schema->GetNumberOfTables()
+         << endl;
+    return 1;
+    }
+  
+  cerr << numTbl 
+       << " found.\n";
+
+  vtkstd::vector<vtkStdString> tables;
+  while ( query->NextRow() )
+    {
+    for ( int field = 0; field < numTbl; ++ field )
+      {
+      tables.push_back( query->DataValue( field ).ToString() );
+      }
+    }
+
+  // 4. Inspect these tables
+  cerr << "@@ Inspecting these tables..." << "\n";
+
+  for ( int tblHandle = 0; tblHandle < numTbl; ++ tblHandle )
+    {
+    vtkStdString tblName( schema->GetTableNameFromHandle( tblHandle ) );
+    cerr << "   Table: " 
+         << tblName
+         << "\n";
+
+    if ( tblName != tables[tblHandle] )
+      {
+      cerr << "Fetched an incorrect name: " 
+           << tables[tblHandle]
+           << " != " 
+           << tblName
+           << endl;
+      return 1;
+      }
+                       
+    // 4.1 Check columns
+    vtkStdString queryStr ("DESCRIBE ");
+    queryStr += tblName;
+    query->SetQuery( queryStr );
+    if ( ! query->Execute() )
+      {
+      cerr << "Query failed" << endl;
+      return 1;
+      }
+    
+    int numFields = query->GetNumberOfFields();
+    int colHandle = 0;
+    for ( ; query->NextRow(); ++ colHandle )
+      {
+      for ( int field = 0; field < numFields; ++ field )
+        {
+        if ( field )
+          {
+          cerr << ", ";
+          }
+        else // if ( field )
+          {
+          vtkStdString colName ( schema->GetColumnNameFromHandle( tblHandle, colHandle ) );
+          if ( colName != query->DataValue( field ).ToString() )
+            {
+            cerr << "Found an incorrect column name: " 
+                 << query->DataValue( field ).ToString()
+                 << " != " 
+                 << colName
+                 << endl;
+            return 1;
+            }
+          cerr << "     Column: ";
+          }
+        cerr << query->DataValue( field ).ToString().c_str();
+        }
+      cerr << endl;
+      }
+    
+    if ( colHandle != schema->GetNumberOfColumnsInTable( tblHandle ) )
+      {
+      cerr << "Found an incorrect number of columns: " 
+           << colHandle
+           << " != " 
+           << schema->GetNumberOfColumnsInTable( tblHandle )
+           << endl;
+      return 1;
+      }
+
+    // 4.2 Check indices
+    queryStr = "SHOW INDEX FROM ";
+    queryStr += tblName;
+    query->SetQuery( queryStr );
+    if ( ! query->Execute() )
+      {
+      cerr << "Query failed" << endl;
+      return 1;
+      }
+
+    numFields = query->GetNumberOfFields();
+    int idxHandle = -1;
+    while ( query->NextRow() )
+      {
+      int cnmHandle = atoi( query->DataValue( 3 ).ToString() ) - 1;
+      if ( ! cnmHandle )
+        {
+        ++ idxHandle;
+        }
+
+      vtkStdString colName ( schema->GetIndexColumnNameFromHandle( tblHandle, idxHandle, cnmHandle ) );
+      for ( int field = 0; field < numFields; ++ field )
+        {
+        if ( field )
+          {
+          cerr << ", ";
+          }
+        else // if ( field )
+          {
+          cerr << "     Index: ";
+          }
+        cerr << query->DataValue( field ).ToString().c_str();
+        }
+      cerr << endl;
+
+      if ( colName != query->DataValue( 4 ).ToString() )
+        {
+        cerr << "Fetched an incorrect column name: " 
+             << query->DataValue( 4 ).ToString()
+             << " != " 
+             << colName
+             << endl;
+        return 1;
+        }
+      }
+    
+    if ( idxHandle + 1 != schema->GetNumberOfIndicesInTable( tblHandle ) )
+      {
+      cerr << "Found an incorrect number of indices: " 
+           << idxHandle + 1
+           << " != " 
+           << schema->GetNumberOfIndicesInTable( tblHandle )
+           << endl;
+      return 1;
+      }
+    }
+
+  // Clean up
+  db->Delete();
+  schema->Delete();
+  query->Delete();
 
   return 0;
 }
