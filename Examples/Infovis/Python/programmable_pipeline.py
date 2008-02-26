230b0655cfdfc5c4e0accb47f42c618ebac42b12
Timothy M. Shead <tshead@sandia.gov>
2008-02-26 14:03:45 -0500

    ENH: Update the programmable_pipeline.py example to include creation of an edge attribute array

--- Examples/Infovis/Python/programmable_pipeline.py
+++ Examples/Infovis/Python/programmable_pipeline.py
@@ -14,19 +14,32 @@ def create_index_callback():
   output = create_index.GetOutput()
 
   output.ShallowCopy(input)
-  index_array = vtkIntArray()
-  index_array.SetName("index")
-  index_array.SetNumberOfTuples(output.GetNumberOfVertices())
+
+  vertex_id_array = vtkIdTypeArray()
+  vertex_id_array.SetName("vertex_id")
+  vertex_id_array.SetNumberOfTuples(output.GetNumberOfVertices())
   for i in range(output.GetNumberOfVertices()):
-    index_array.SetValue(i, i)
-  output.GetVertexData().AddArray(index_array)
+    vertex_id_array.SetValue(i, i)
+  output.GetVertexData().AddArray(vertex_id_array)
+
+  edge_target_array = vtkIdTypeArray()
+  edge_target_array.SetName("edge_target")
+  edge_target_array.SetNumberOfTuples(output.GetNumberOfEdges())
+  edge_iterator = vtkEdgeListIterator()
+  output.GetEdges(edge_iterator)
+  while edge_iterator.HasNext():
+    edge = edge_iterator.NextGraphEdge()
+    edge_target_array.SetValue(edge.GetId(), edge.GetTarget())
+  output.GetEdgeData().AddArray(edge_target_array)
 
 create_index.SetExecuteMethod(create_index_callback)
 
 view = vtkGraphLayoutView()
 view.AddRepresentationFromInputConnection(create_index.GetOutputPort())
-view.SetVertexLabelArrayName("index")
+view.SetVertexLabelArrayName("vertex_id")
 view.SetVertexLabelVisibility(True)
+view.SetEdgeLabelArrayName("edge_target")
+view.SetEdgeLabelVisibility(True)
 
 theme = vtkViewTheme.CreateMellowTheme()
 view.ApplyViewTheme(theme)
