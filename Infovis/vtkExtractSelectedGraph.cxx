ee004b5b6aefa7bc25e766dd39b40285f03ec2a8
Timothy M. Shead <tshead@sandia.gov>
2008-02-27 10:44:37 -0500

    BUG: vtkExtractSelectedGraph did not work with edge selections.

--- Infovis/vtkExtractSelectedGraph.cxx
+++ Infovis/vtkExtractSelectedGraph.cxx
@@ -44,7 +44,7 @@
 
 #include <vtksys/stl/map>
 
-vtkCxxRevisionMacro(vtkExtractSelectedGraph, "1.17");
+vtkCxxRevisionMacro(vtkExtractSelectedGraph, "1.18");
 vtkStandardNewMacro(vtkExtractSelectedGraph);
 //----------------------------------------------------------------------------
 vtkExtractSelectedGraph::vtkExtractSelectedGraph()
@@ -203,7 +203,7 @@ int vtkExtractSelectedGraph::RequestData(
     vtkSmartPointer<vtkMutableUndirectedGraph>::New();
 
   if (selection->GetProperties()->Has(vtkSelection::FIELD_TYPE()) && 
-      selection->GetProperties()->Get(vtkSelection::FIELD_TYPE()) == vtkSelection::CELL)
+      selection->GetProperties()->Get(vtkSelection::FIELD_TYPE()) == vtkSelection::EDGE)
     {
     //
     // Edge selection
@@ -242,6 +242,7 @@ int vtkExtractSelectedGraph::RequestData(
     while (edges->HasNext())
       {
       vtkEdgeType e = edges->Next();
+
       if ((inverse && selectArr->LookupValue(e.Id) < 0) ||
           (!inverse && selectArr->LookupValue(e.Id) >= 0))
         {
