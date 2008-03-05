e8a025ae20cd4bccf0b8a02e018af3e6fd36ea49
Francois Bertel <francois.bertel@kitware.com>
2008-03-05 13:15:17 -0500

    STYLE:Fixed missing start equation marker.

--- Graphics/vtkMeshQuality.h
+++ Graphics/vtkMeshQuality.h
@@ -584,7 +584,7 @@ public:
   // member function. Use at your own risk with nonplanar quadrilaterals.
   // The aspect ratio of a planar quadrilateral \f$q\f$ is: 
   // \f$\frac{|q|_1|q|_\infty}{4{\cal A}}\f$,
-  // where \f$|q|_1\f$, $|q|_\infty\f$ and \f${\cal A}\f$ respectively denote the 
+  // where \f$|q|_1\f$, \f$|q|_\infty\f$ and \f${\cal A}\f$ respectively denote the 
   // perimeter, the greatest edge length and the area of \f$q\f$.
   static double QuadAspectRatio( vtkCell* cell );
 
