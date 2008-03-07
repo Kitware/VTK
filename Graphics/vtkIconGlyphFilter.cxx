09237ad65409b835cc2ce0dfa77b9401c04e2e5c
Dave Partyka <dave.partyka@kitware.com>
2008-03-07 14:33:53 -0500

    ENH: Fix copyright notice for IconGlyphFilter

--- Graphics/vtkIconGlyphFilter.cxx
+++ Graphics/vtkIconGlyphFilter.cxx
@@ -1,11 +1,17 @@
-/*
- * Copyright 2004 Sandia Corporation.
- * Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
- * license for use of this work by or on behalf of the
- * U.S. Government. Redistribution and use in source and binary forms, with
- * or without modification, are permitted provided that this Notice and any
- * statement of authorship are reproduced on all copies.
- */
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkIconGlyphFilter.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
 
 
 #include "vtkIconGlyphFilter.h"
@@ -23,7 +29,7 @@
 #include "vtkPoints.h"
 #include "vtkPolyData.h"
 
-vtkCxxRevisionMacro(vtkIconGlyphFilter, "1.2");
+vtkCxxRevisionMacro(vtkIconGlyphFilter, "1.3");
 vtkStandardNewMacro(vtkIconGlyphFilter);
 
 //-----------------------------------------------------------------------------
