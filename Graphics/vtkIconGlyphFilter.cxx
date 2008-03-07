c543ba0f7a9b4fa4f89002fc92b257e4c23b7500
Dave Partyka <dave.partyka@kitware.com>
2008-03-07 02:38:50 -0500

    ENH: Add new IconGlyphFilter class.

---
+++ Graphics/vtkIconGlyphFilter.cxx
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2004 Sandia Corporation.
+ * Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+ * license for use of this work by or on behalf of the
+ * U.S. Government. Redistribution and use in source and binary forms, with
+ * or without modification, are permitted provided that this Notice and any
+ * statement of authorship are reproduced on all copies.
+ */
+
+
+#include "vtkIconGlyphFilter.h"
+
+#include "vtkCellArray.h"
+#include "vtkCellData.h"
+#include "vtkFloatArray.h"
+#include "vtkIdList.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkMath.h"
+#include "vtkObjectFactory.h"
+#include "vtkPointData.h"
+#include "vtkPointLocator.h"
+#include "vtkPoints.h"
+#include "vtkPolyData.h"
+
+vtkCxxRevisionMacro(vtkIconGlyphFilter, "1.1");
+vtkStandardNewMacro(vtkIconGlyphFilter);
+
+//-----------------------------------------------------------------------------
+vtkIconGlyphFilter::vtkIconGlyphFilter()
+{
+  this->IconSize[0] = 1;
+  this->IconSize[1] = 1;
+  this->IconSheetSize[0] = 1;
+  this->IconSheetSize[1] = 1;
+
+  this->SetInputArrayToProcess(0, 0, 0, 
+    vtkDataObject::FIELD_ASSOCIATION_POINTS, vtkDataSetAttributes::SCALARS);
+}
+
+//-----------------------------------------------------------------------------
+vtkIconGlyphFilter::~vtkIconGlyphFilter()
+{
+}
+
+//-----------------------------------------------------------------------------
+void vtkIconGlyphFilter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+
+  os << indent << "IconSize: " << IconSize[0] << " " << IconSize[1] << endl;
+  os << indent << "IconSheetSize: " << IconSheetSize[0] << " " << IconSheetSize[1] << endl;
+}
+
+//-----------------------------------------------------------------------------
+int vtkIconGlyphFilter::RequestData(vtkInformation *vtkNotUsed(request),
+                                  vtkInformationVector **inputVector,
+                                  vtkInformationVector *outputVector)
+{
+  // Get the information object.
+  vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
+  vtkInformation *outInfo = outputVector->GetInformationObject(0);
+
+  // Get the data objects.
+  vtkPointSet *input = vtkPointSet::SafeDownCast(
+                                     inInfo->Get(vtkDataObject::DATA_OBJECT()));
+  vtkPolyData *output = vtkPolyData::SafeDownCast(
+                                    outInfo->Get(vtkDataObject::DATA_OBJECT()));
+
+  int numPoints = input->GetNumberOfPoints();
+
+  if (numPoints <= 0)
+    {
+    // nothing to do...
+    return 1;
+    }
+
+  vtkIntArray* scalars = vtkIntArray::SafeDownCast(
+    this->GetInputArrayToProcess(0, inputVector));
+  if (!scalars)
+    {
+    vtkErrorMacro("Input Scalars must be specified to index into the icon sheet.");
+    return 0;
+    }
+
+  double point[3], textureCoord[2];
+  double halfX = this->IconSize[0]/2.0;
+  double halfY = this->IconSize[1]/2.0;
+  double sheetXDim = this->IconSheetSize[0]/this->IconSize[0];
+  double sheetYDim = this->IconSheetSize[1]/this->IconSize[1];
+  int iconIndex = 0;
+  int j, k;
+
+  vtkPoints * outPoints = vtkPoints::New();
+  outPoints->Allocate(4 * numPoints);
+
+  vtkCellArray * outCells = vtkCellArray::New();
+  outCells->Allocate(outCells->EstimateSize(numPoints, 4));
+
+  vtkFloatArray *outTCoords = vtkFloatArray::New();
+  outTCoords->SetNumberOfComponents(2);
+  outTCoords->Allocate(8*numPoints);
+
+  for(int i = 0; i < numPoints; i++)
+    {
+    iconIndex = scalars->GetValue(i);
+    input->GetPoint(i, point);
+    outPoints->InsertNextPoint(point[0] - 0.5, point[1] - 0.5, point[2]);
+
+    this->IconConvertIndex(iconIndex, j, k);
+    textureCoord[0] = j/sheetXDim;
+    textureCoord[1] = k/sheetYDim;
+    outTCoords->InsertTuple(i * 4, textureCoord);
+
+    outPoints->InsertNextPoint(point[0] + 0.5, point[1] - 0.5, point[2]);
+
+    this->IconConvertIndex(iconIndex, j, k);
+    textureCoord[0] = (j + 1.0)/sheetXDim;
+    textureCoord[1] = k/sheetYDim;
+    outTCoords->InsertTuple(i * 4 + 1, textureCoord);
+
+    outPoints->InsertNextPoint(point[0] + 0.5, point[1] + 0.5, point[2]);
+
+    this->IconConvertIndex(iconIndex, j, k);
+    textureCoord[0] = (j + 1.0)/sheetXDim;
+    textureCoord[1] = (k + 1.0)/sheetYDim;
+    outTCoords->InsertTuple(i * 4 + 2, textureCoord);
+
+    outPoints->InsertNextPoint(point[0] - 0.5, point[1] + 0.5, point[2]);
+
+    this->IconConvertIndex(iconIndex, j, k);
+    textureCoord[0] = j/sheetXDim;
+    textureCoord[1] = (k + 1.0)/sheetYDim;
+    outTCoords->InsertTuple(i * 4 + 3, textureCoord);
+
+    outCells->InsertNextCell(4);
+    outCells->InsertCellPoint(i * 4);
+    outCells->InsertCellPoint(i * 4 + 1);
+    outCells->InsertCellPoint(i * 4 + 2);
+    outCells->InsertCellPoint(i * 4 + 3);
+    }
+
+  output->SetPoints(outPoints);
+  outPoints->Delete();
+
+  outTCoords->SetName("TextureCoordinates");
+  output->GetPointData()->SetTCoords(outTCoords);
+  outTCoords->Delete();
+
+  output->SetPolys(outCells);
+  outCells->Delete();
+
+  return 1;
+}
+
---
+++ Graphics/vtkIconGlyphFilter.h
@@ -0,0 +1,70 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkIconGlyphFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright 2004 Sandia Corporation.
+  Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+  license for use of this work by or on behalf of the
+  U.S. Government. Redistribution and use in source and binary forms, with
+  or without modification, are permitted provided that this Notice and any
+  statement of authorship are reproduced on all copies.
+
+=========================================================================*/
+
+#ifndef __vtkIconGlyphFilter_h
+#define __vtkIconGlyphFilter_h
+
+#include "vtkPolyDataAlgorithm.h"
+
+
+class VTK_GRAPHICS_EXPORT vtkIconGlyphFilter : public vtkPolyDataAlgorithm
+{
+public:
+
+  // Description
+  static vtkIconGlyphFilter *New();
+  vtkTypeRevisionMacro(vtkIconGlyphFilter,vtkPolyDataAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Specify the Width and Height, in pixels, of an icon in the icon sheet
+  vtkSetVector2Macro(IconSize,int);
+  vtkGetVectorMacro(IconSize,int,2);
+
+  // Description:
+  // Specify the Width and Height, in pixels, of an icon in the icon sheet
+  vtkSetVector2Macro(IconSheetSize,int);
+  vtkGetVectorMacro(IconSheetSize,int,2);
+
+protected:
+  vtkIconGlyphFilter();
+  ~vtkIconGlyphFilter();
+
+  virtual int RequestData(vtkInformation *,
+                          vtkInformationVector **,
+                          vtkInformationVector *);
+
+  int IconSize[2]; // Size in pixels of an icon in an icon sheet
+  int IconSheetSize[2]; // Size in pixels of the icon sheet
+
+private:
+  vtkIconGlyphFilter(const vtkIconGlyphFilter&);  // Not implemented.
+  void operator=(const vtkIconGlyphFilter&);  // Not implemented.
+
+  void IconConvertIndex(int id, int & j, int & k);
+};
+
+void vtkIconGlyphFilter::IconConvertIndex(int id, int & j, int & k)
+{
+  int dimX = this->IconSheetSize[0]/this->IconSize[0];
+  int dimY = this->IconSheetSize[1]/this->IconSize[1];
+
+  j = id - dimX * (int)(id/dimX);
+  k = dimY - (int)(id/dimX) - 1;
+}
+
+#endif
