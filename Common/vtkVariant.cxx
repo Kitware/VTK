74345c91f7686b88775f352bc8f7f99309af01cc
David Cole <david.cole@kitware.com>
2008-03-26 10:53:34 -0400

    COMP: Fine. Be that way. Fixing issue #5791 and still supporting VS6 forces us to include the vtkVariant ToNumeric definition *after* all its callers with VS6, but *before* all its callers with reasonable compilers. So, split ToNumeric out into its own file and include it in the right place based on _MSC_VER value.

--- Common/vtkVariant.cxx
+++ Common/vtkVariant.cxx
@@ -564,112 +564,15 @@ T vtkVariantStringToNumeric(vtkStdString str, bool* valid, T* vtkNotUsed(ignored
 //----------------------------------------------------------------------------
 // Definition of ToNumeric
 
-template <typename T>
-T vtkVariant::ToNumeric(bool* valid, T* vtkNotUsed(ignored)) const
-{
-  if (valid)
-    {
-    *valid = true;
-    }
-  if (this->IsString())
-    {
-    return vtkVariantStringToNumeric<T>(* this->Data.String, valid);
-    }
-  if (this->IsFloat())
-    {
-    return static_cast<T>(this->Data.Float);
-    }
-  if (this->IsDouble())
-    {
-    return static_cast<T>(this->Data.Double);
-    }
-  if (this->IsChar())
-    {
-    return static_cast<T>(this->Data.Char);
-    }
-  if (this->IsUnsignedChar())
-    {
-    return static_cast<T>(this->Data.UnsignedChar);
-    }
-  if (this->IsSignedChar())
-    {
-    return static_cast<T>(this->Data.SignedChar);
-    }
-  if (this->IsShort())
-    {
-    return static_cast<T>(this->Data.Short);
-    }
-  if (this->IsUnsignedShort())
-    {
-    return static_cast<T>(this->Data.UnsignedShort);
-    }
-  if (this->IsInt())
-    {
-    return static_cast<T>(this->Data.Int);
-    }
-  if (this->IsUnsignedInt())
-    {
-    return static_cast<T>(this->Data.UnsignedInt);
-    }
-  if (this->IsLong())
-    {
-    return static_cast<T>(this->Data.Long);
-    }
-  if (this->IsUnsignedLong())
-    {
-    return static_cast<T>(this->Data.UnsignedLong);
-    }
-#if defined(VTK_TYPE_USE___INT64)
-  if (this->Is__Int64())
-    {
-    return static_cast<T>(this->Data.__Int64);
-    }
-  if (this->IsUnsigned__Int64())
-    {
-    return static_cast<T>(static_cast<__int64>(this->Data.Unsigned__Int64));
-    }
+// Visual Studio 6 chokes with "error C2893: Failed to specialize function
+// template" when the definition of ToNumeric is included here and it
+// encounters the various callers of ToNumeric below. So for VS6 include the
+// the implementation of ToNumeric at the bottom of the file.
+//
+#if !defined(_MSC_VER) || (_MSC_VER >= 1300)
+# define VTK_VARIANT_TO_NUMERIC_CXX_INCLUDED
+# include "vtkVariantToNumeric.cxx"
 #endif
-#if defined(VTK_TYPE_USE_LONG_LONG)
-  if (this->IsLongLong())
-    {
-    return static_cast<T>(this->Data.LongLong);
-    }
-  if (this->IsUnsignedLongLong())
-    {
-    return static_cast<T>(this->Data.UnsignedLongLong);
-    }
-#endif
-  // For arrays, convert the first value to the appropriate type.
-  if (this->IsArray())
-    {
-    if (this->Data.VTKObject->IsA("vtkDataArray"))
-      {
-      // Note: This are not the best conversion.
-      //       We covert the first value to double, then
-      //       cast it back to the appropriate numeric type.
-      vtkDataArray* da = vtkDataArray::SafeDownCast(this->Data.VTKObject);
-      return static_cast<T>(da->GetTuple1(0));
-      }
-    if (this->Data.VTKObject->IsA("vtkVariantArray"))
-      {
-      // Note: This are not the best conversion.
-      //       We covert the first value to double, then
-      //       cast it back to the appropriate numeric type.
-      vtkVariantArray* va = vtkVariantArray::SafeDownCast(this->Data.VTKObject);
-      return static_cast<T>(va->GetValue(0).ToDouble());
-      }
-    if (this->Data.VTKObject->IsA("vtkStringArray"))
-      {
-      vtkStringArray* sa = vtkStringArray::SafeDownCast(this->Data.VTKObject);
-      return vtkVariantStringToNumeric<T>(sa->GetValue(0), valid);
-      }
-    }
-  if (valid)
-    {
-    *valid = false;
-    }
-  return static_cast<T>(0);
-}
 
 //----------------------------------------------------------------------------
 // Explicit instantiations of ToNumeric
@@ -677,7 +580,7 @@ T vtkVariant::ToNumeric(bool* valid, T* vtkNotUsed(ignored)) const
 // Visual Studio 6 chokes with "fatal error C1001: INTERNAL COMPILER ERROR"
 // when trying to use the vtkVariantToNumericInstantiateMacro, so don't...
 //
-#if defined(_MSC_VER) && (_MSC_VER <= 1200)
+#if defined(_MSC_VER) && (_MSC_VER < 1300)
 # define VTK_VARIANT_NO_INSTANTIATE
 #endif
 
@@ -806,3 +709,11 @@ vtkTypeUInt64 vtkVariant::ToTypeUInt64(bool* valid) const
 {
   return this->ToNumeric(valid, static_cast<vtkTypeUInt64 *>(0));
 }
+
+//----------------------------------------------------------------------------
+// Definition of ToNumeric if not already included above:
+
+#if !defined(VTK_VARIANT_TO_NUMERIC_CXX_INCLUDED)
+# define VTK_VARIANT_TO_NUMERIC_CXX_INCLUDED
+# include "vtkVariantToNumeric.cxx"
+#endif
