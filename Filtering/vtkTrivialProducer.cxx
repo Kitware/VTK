cd87c5d1e65bbe0e1190f6fe9d891d1751786d74
Berk Geveci <berk.geveci@kitware.com>
2008-03-27 14:15:59 -0400

    ENH: Added support for using serial polydata writers in parallel. Geometry is collected to the first node and written from there.

--- Filtering/vtkTrivialProducer.cxx
+++ Filtering/vtkTrivialProducer.cxx
@@ -21,7 +21,7 @@
 #include "vtkInformationVector.h"
 #include "vtkObjectFactory.h"
 
-vtkCxxRevisionMacro(vtkTrivialProducer, "1.10");
+vtkCxxRevisionMacro(vtkTrivialProducer, "1.11");
 vtkStandardNewMacro(vtkTrivialProducer);
 
 // This compile-time switch determines whether the update extent is
@@ -123,7 +123,7 @@ vtkTrivialProducer::ProcessRequest(vtkInformation* request,
       {
       // There is no real source to  change the output data, so we can
       // produce exactly one piece.
-      outputInfo->Set(vtkStreamingDemandDrivenPipeline::MAXIMUM_NUMBER_OF_PIECES(), 1);
+      outputInfo->Set(vtkStreamingDemandDrivenPipeline::MAXIMUM_NUMBER_OF_PIECES(), -1);
       }
     else if(dataInfo->Get(vtkDataObject::DATA_EXTENT_TYPE()) == VTK_3D_EXTENT)
       {
