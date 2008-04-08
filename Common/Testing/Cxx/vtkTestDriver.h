595895c662b0d24b38abbd2ce326edbc39ac9535
David Cole <david.cole@kitware.com>
2008-04-08 14:15:54 -0400

    COMP: Eliminate warnings on dash13 nightly dashboard.

--- Common/Testing/Cxx/vtkTestDriver.h
+++ Common/Testing/Cxx/vtkTestDriver.h
@@ -28,6 +28,14 @@
 # pragma warning (disable: 4701) /* local variable 'x' may be used
                                     without having been initialized */
 
+// Warnings that show up on vc6 dashboards since floating point
+// exception handling was added to the test driver...
+//
+# pragma warning (disable: 4018) /* signed/unsigned mismatch */
+# pragma warning (disable: 4146) /* unary minus operator applied unsigned */
+# pragma warning (disable: 4284) /* return type for operator-> not a UDT */
+# pragma warning (disable: 4290) /* C++ exception specification ignored */
+
 #endif
 
 #include <vtkstd/exception> // for vtkstd::exception
