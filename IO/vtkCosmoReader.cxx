b9cd929444bb225777b5c6b977592c57fc95c9be
Dave Demarle <dave.demarle@kitware.com>
2008-04-17 14:01:25 -0400

    STYLE: Add Kitware copyright to contributed code and give props to Pat Fasel.

--- IO/vtkCosmoReader.cxx
+++ IO/vtkCosmoReader.cxx
@@ -1,5 +1,19 @@
 /*=========================================================================
 
+  Program:   Visualization Toolkit
+  Module:    vtkCosmoReader.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+/*=========================================================================
+
   Program:   VTK/ParaView Los Alamos National Laboratory Modules (PVLANL)
   Module:    vtkCosmoReader.cxx
 
@@ -63,7 +77,7 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "vtkStdString.h"
 
-vtkCxxRevisionMacro(vtkCosmoReader, "1.7");
+vtkCxxRevisionMacro(vtkCosmoReader, "1.8");
 vtkStandardNewMacro(vtkCosmoReader);
 
 namespace
