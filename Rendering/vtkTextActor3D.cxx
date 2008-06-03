bba1be74c32668108cbb90aaa885fb2618889569
Charl Botha <c.p.botha@ewi.tudelft.nl>
2008-06-03 11:15:18 -0400

    BUG: Fixed extent bug http://www.vtk.org/Bug/view.php?id=7098 by simplifying UpdateImageActor and letting vtkFTU::RenderString (and PrepareImageData) do their work. ENH: Added GetBoundingBox method with which freetype text bounding box can be queried.

--- Rendering/vtkTextActor3D.cxx
+++ Rendering/vtkTextActor3D.cxx
@@ -26,7 +26,7 @@
 #include "vtkMatrix4x4.h"
 #include "vtkMath.h"
 
-vtkCxxRevisionMacro(vtkTextActor3D, "1.7");
+vtkCxxRevisionMacro(vtkTextActor3D, "1.8");
 vtkStandardNewMacro(vtkTextActor3D);
 
 vtkCxxSetObjectMacro(vtkTextActor3D, TextProperty, vtkTextProperty);
@@ -89,6 +89,40 @@ double* vtkTextActor3D::GetBounds()
     }
 
   return NULL;
+}
+
+// --------------------------------------------------------------------------
+
+int vtkTextActor3D::GetBoundingBox(int bbox[4])
+{
+  if (!this->TextProperty)
+  {
+    vtkErrorMacro(<<"Need valid vtkTextProperty.");
+    return 0;
+  }
+
+  if (!bbox)
+  {
+    vtkErrorMacro(<<"Need 4-element int array for bounding box.");
+  }
+
+  vtkFreeTypeUtilities *fu = vtkFreeTypeUtilities::GetInstance();
+  if (!fu)
+  {
+    vtkErrorMacro(<<"Failed getting the FreeType utilities instance");
+    return 0;
+  }
+
+  fu->GetBoundingBox(this->TextProperty, this->Input, bbox);
+  if (!fu->IsBoundingBoxValid(bbox))
+  {
+    vtkErrorMacro(<<"No text in input.");
+    return 0;
+  }
+
+  return 1;
+
+
 }
 
 // --------------------------------------------------------------------------
@@ -162,7 +196,6 @@ int vtkTextActor3D::RenderOpaqueGeometry(vtkViewport *viewport)
 int vtkTextActor3D::UpdateImageActor()
 {
   // Need text prop
-
   if (!this->TextProperty)
     {
     vtkErrorMacro(<<"Need a text property to render text actor");
@@ -170,7 +203,6 @@ int vtkTextActor3D::UpdateImageActor()
     }
 
   // No input, the assign the image actor a zilch input
-
   if (!this->Input || !*this->Input)
     {
     if (this->ImageActor)
@@ -185,49 +217,14 @@ int vtkTextActor3D::UpdateImageActor()
   //  - instance has been modified since last build
   //  - text prop has been modified since last build
   //  - ImageData ivar has not been allocated yet
-
   if (this->GetMTime() > this->BuildTime ||
       this->TextProperty->GetMTime() > this->BuildTime ||
       !this->ImageData)
     {
-    //cout << "Need to re-render " << this->GetMTime() << ", " <<  this->TextProperty->GetMTime() << ", " << this->BuildTime << endl;
 
     this->BuildTime.Modified();
 
-    // Get the bounding box of the text to render
-    // If the bounding box is invalid, then there is nothing to render
-    // assign the image actor a nada input
-
-    vtkFreeTypeUtilities *fu = vtkFreeTypeUtilities::GetInstance();
-    if (!fu)
-      {
-      vtkErrorMacro(<<"Failed getting the FreeType utilities instance");
-      return 0;
-      }
-
-    int text_bbox[4];
-    fu->GetBoundingBox(this->TextProperty, this->Input, text_bbox);
-    if (!fu->IsBoundingBoxValid(text_bbox))
-      {
-      if (this->ImageActor)
-        {
-        this->ImageActor->SetInput(NULL);
-        }
-      return 1;
-      }
-
-    // The bounding box was the area that is going to be filled with pixels
-    // given a text origin of (0, 0). Now get the real size we need, i.e.
-    // the full extent from the origin to the bounding box.
-
-    int text_size[2];
-    text_size[0] = (text_bbox[1] - text_bbox[0] + 1) + abs(text_bbox[0]);
-    text_size[1] = (text_bbox[3] - text_bbox[2] + 1) + abs(text_bbox[2]);
-
-    // If the RGBA image data is too small, resize it to the next power of 2
-    // WARNING: at this point, since this image is going to be a texture
-    // we should limit its size or query the hardware
-
+    // we have to give vtkFTU::RenderString something to work with
     if (!this->ImageData)
       {
       this->ImageData = vtkImageData::New();
@@ -236,87 +233,30 @@ int vtkTextActor3D::UpdateImageActor()
       this->ImageData->SetSpacing(1.0, 1.0, 1.0);
       }
 
-    // If the current image data is too small to render the text,
-    // or more than twice as big (too hungry), then resize
-
-    int img_dims[3], new_img_dims[3];
-    this->ImageData->GetDimensions(img_dims);
-
-    if (img_dims[0] < text_size[0] || img_dims[1] < text_size[1] ||
-        text_size[0] * 2 < img_dims[0] || text_size[1] * 2 < img_dims[0])
+    vtkFreeTypeUtilities *fu = vtkFreeTypeUtilities::GetInstance();
+    if (!fu)
       {
-      new_img_dims[0] = 1 << static_cast<int>(ceil(log(static_cast<double>(text_size[0])) / log(2.0)));
-      new_img_dims[1] = 1 << static_cast<int>(ceil(log(static_cast<double>(text_size[1])) / log(2.0)));
-      new_img_dims[2] = 1;
-      if (new_img_dims[0] != img_dims[0] || 
-          new_img_dims[1] != img_dims[1] ||
-          new_img_dims[2] != img_dims[2])
-        {
-        //cout << "I need: " << text_size[0] << "x" << text_size[1] << ", I resize to: " << new_img_dims[0] << "x" << new_img_dims[1] << endl;
-        this->ImageData->SetDimensions(new_img_dims);
-        this->ImageData->AllocateScalars();
-        this->ImageData->UpdateInformation();
-        this->ImageData->SetUpdateExtent(this->ImageData->GetWholeExtent());
-        this->ImageData->PropagateUpdateExtent();
-        if (this->ImageActor)
-          {
-          this->ImageActor->SetDisplayExtent(
-            this->ImageData->GetWholeExtent());
-          }
-        }
+      vtkErrorMacro(<<"Failed getting the FreeType utilities instance");
+      return 0;
       }
-
-    // Render inside the image data
-    
-    int x = (text_bbox[0] < 0 ? -text_bbox[0] : 0);
-    int y = (text_bbox[2] < 0 ? -text_bbox[2] : 0);
-
-#if 0
-    char *ptr = (char*)this->ImageData->GetScalarPointer();
-    int nb_comp = this->ImageData->GetNumberOfScalarComponents();
-    char *ptr_end = ptr + this->ImageData->GetNumberOfPoints() * nb_comp;
-    memset(ptr, 0, ptr_end - ptr);
-    ptr += nb_comp - 1;
-    while (ptr < ptr_end)
-      {
-      *ptr = 255.0;
-      ptr += nb_comp;
-      }
-#else
-    memset(this->ImageData->GetScalarPointer(), 
-           0, 
-           (this->ImageData->GetNumberOfPoints() *
-            this->ImageData->GetNumberOfScalarComponents()));
-#endif
-
+  
     if (!fu->RenderString(
-          this->TextProperty, this->Input, x, y, this->ImageData))
+          this->TextProperty, this->Input, this->ImageData))
       {
       vtkErrorMacro(<<"Failed rendering text to buffer");
       return 0;
       }
 
-    // Set the image data origin so that oriented text is placed properly
-    // Also take shadow into account, we want the text to be on the baseline
-
-    if (this->TextProperty->GetShadow())
-      {
-      // this is wrong
-      //x += this->TextProperty->GetShadowOffset()[0];
-      //y += this->TextProperty->GetShadowOffset()[1];
-      }
-    this->ImageData->SetOrigin(-x, -y, 0);
-
     // Associate the image data (should be up to date now) to the image actor
-
     if (this->ImageActor)
       {
       this->ImageActor->SetInput(this->ImageData);
+      this->ImageActor->SetDisplayExtent(this->ImageData->GetWholeExtent());
       }
-    }
+
+    } // if (this->GetMTime() ...
 
   // Position the actor
-
   if (this->ImageActor)
     {
     // Which one is faster ?
