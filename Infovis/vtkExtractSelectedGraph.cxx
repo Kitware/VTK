e4732b6dd67d37ba473633d3e1bb90f96baddec8
Jeffrey Baumes <jeff.baumes@kitware.com>
2008-06-10 16:19:07 -0400

    BUG: Getting wrong selection size with inverted selections.

--- Infovis/vtkExtractSelectedGraph.cxx
+++ Infovis/vtkExtractSelectedGraph.cxx
@@ -47,7 +47,7 @@
 #include <vtksys/stl/map>
 #include <vtkstd/vector>
 
-vtkCxxRevisionMacro(vtkExtractSelectedGraph, "1.24");
+vtkCxxRevisionMacro(vtkExtractSelectedGraph, "1.25");
 vtkStandardNewMacro(vtkExtractSelectedGraph);
 //----------------------------------------------------------------------------
 vtkExtractSelectedGraph::vtkExtractSelectedGraph()
@@ -186,10 +186,9 @@ int vtkExtractSelectedGraph::RequestData(
     vtkErrorMacro("Selection list must be of type vtkIdTypeArray.");
     return 0;
     }
-  vtkIdType selectSize = selectArr->GetNumberOfTuples();
 
   // Invert the selection if necessary
-  int inverse = indexSelection->GetProperties()->Get(vtkSelection::INVERSE());
+  int inverse = selection->GetProperties()->Get(vtkSelection::INVERSE());
   vtkSmartPointer<vtkIdTypeArray> inverted = 0;
   if (inverse)
     {
@@ -204,6 +203,7 @@ int vtkExtractSelectedGraph::RequestData(
       }
     selectArr = inverted;
     }
+  vtkIdType selectSize = selectArr->GetNumberOfTuples();
   
   vtkSmartPointer<vtkMutableDirectedGraph> dirBuilder = 
     vtkSmartPointer<vtkMutableDirectedGraph>::New();
