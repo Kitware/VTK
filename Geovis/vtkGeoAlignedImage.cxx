9a38ae803c0c370648f5596773d56f41815301a8
Ken Martin <ken.martin@kitware.com>
2008-06-13 12:55:12 -0400

    ENH: some cleanup

--- Geovis/vtkGeoAlignedImage.cxx
+++ Geovis/vtkGeoAlignedImage.cxx
@@ -27,11 +27,12 @@
 #include "vtkActor.h"
 #include "vtkGeoAlignedImage.h"
 #include "vtkGeoImageNode.h"
+#include "vtkGeoPatch.h"
 #include "vtkGeoTerrainNode.h"
 #include "vtkGeoTerrain.h"
 
 
-vtkCxxRevisionMacro(vtkGeoAlignedImage, "1.1");
+vtkCxxRevisionMacro(vtkGeoAlignedImage, "1.2");
 vtkStandardNewMacro(vtkGeoAlignedImage);
 
 //----------------------------------------------------------------------------
--- Geovis/vtkGeoAlignedImage.h
+++ Geovis/vtkGeoAlignedImage.h
@@ -30,13 +30,12 @@
 #include <vtkstd/vector>
 #include <vtkstd/stack>
 #include "vtkSmartPointer.h" // for SP
-//#include "vtkGeoPatch.h" // for SP
 #include "vtkGeoAlignedImageCache.h"
 
 
 class vtkGeoTerrain;
 class vtkAssembly;
-class vtkGeoPatch'
+class vtkGeoPatch;
 
 class VTK_GEOVIS_EXPORT vtkGeoAlignedImage : public vtkObject
 {
