de6a5f7078fed0c5acf1de2647d37afaa0455361
David Cole <david.cole@kitware.com>
2008-06-16 12:25:27 -0400

    ENH: Merge changes from main tree into VTK-5-2 branch. (cvs -q up -j1.27 -j1.29 Imaging/vtkImageStencilData.cxx)

--- Imaging/vtkImageStencilData.cxx
+++ Imaging/vtkImageStencilData.cxx
@@ -25,7 +25,7 @@
 
 #include <math.h>
 
-vtkCxxRevisionMacro(vtkImageStencilData, "1.27");
+vtkCxxRevisionMacro(vtkImageStencilData, "1.27.2.1");
 vtkStandardNewMacro(vtkImageStencilData);
 
 //----------------------------------------------------------------------------
@@ -347,6 +347,14 @@ int vtkImageStencilData::GetNextExtent(int &r1, int &r2,
 
   if (yIdx < 0 || yIdx >= yExt || zIdx < 0 || zIdx >= zExt)
     { // out-of-bounds in y or z, use null extent
+    // if iter was set to negative, reverse the result
+    if (iter < 0)
+      {
+      iter = 0;
+      r1 = rmin;
+      r2 = rmax;
+      return 1;
+      }
     return 0;
     }
 
@@ -717,6 +725,13 @@ void vtkImageStencilData::Add( vtkImageStencilData * stencil1 )
   int extent[6], extent1[6], extent2[6], r1, r2, idy, idz, iter=0;
   stencil1->GetExtent(extent1);
   this->GetExtent(extent2);
+
+  if (extent1[0] > extent1[1] || 
+      extent1[2] > extent1[3] || 
+      extent1[4] > extent1[5])
+    {
+    return;
+    }
 
   if (vtkMath::ExtentIsWithinOtherExtent(extent1,extent2))
     {
