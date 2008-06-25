a3e968166facf3f77bea44acd001aa2a59e98716
Dave Partyka <dave.partyka@kitware.com>
2008-06-25 15:53:04 -0400

    BUG: fix crashing from calling function on a null shader.

--- Rendering/vtkStandardPolyDataPainter.cxx
+++ Rendering/vtkStandardPolyDataPainter.cxx
@@ -63,7 +63,7 @@ public:
 };
 
 //-----------------------------------------------------------------------------
-vtkCxxRevisionMacro(vtkStandardPolyDataPainter, "1.11");
+vtkCxxRevisionMacro(vtkStandardPolyDataPainter, "1.12");
 vtkStandardNewMacro(vtkStandardPolyDataPainter);
 //-----------------------------------------------------------------------------
 static inline int vtkStandardPolyDataPainterGetTotalCells(vtkPolyData* pd,
@@ -166,7 +166,10 @@ void vtkStandardPolyDataPainter::UpdateGenericAttributesCache(
         // This caches the attribute index.
         // This is essential since we don't want to call GetAttributeLocation in
         // glBegin()/glEnd().
-        shaderDevice->SendAttribute(vertexAttributeName, 0, 0, 0, 0);
+        if(shaderDevice)
+          {
+          shaderDevice->SendAttribute(vertexAttributeName, 0, 0, 0, 0);
+          }
         }
       }
     }
