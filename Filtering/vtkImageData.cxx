b6439715ce2aaa3bb3ff06ba0bfb93074cbd2f34
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-08-18 16:13:07 -0400

    BUG: Reverting previous commit. The change was causing issues with subPixelPositionMin test (vtkImageToStructuredPoints ended up overwriting arrays).

--- Filtering/vtkImageData.cxx
+++ Filtering/vtkImageData.cxx
@@ -32,7 +32,7 @@
 #include "vtkVertex.h"
 #include "vtkVoxel.h"
 
-vtkCxxRevisionMacro(vtkImageData, "1.33");
+vtkCxxRevisionMacro(vtkImageData, "1.34");
 vtkStandardNewMacro(vtkImageData);
 
 //----------------------------------------------------------------------------
@@ -1580,9 +1580,6 @@ void vtkImageData::AllocateScalars()
   // allocate enough memory
   scalars->SetNumberOfTuples(imageSize);
 
-  // Assign some default name. Filters generally override this to something more
-  // sensible.
-  scalars->SetName("vtkScalars");
   this->PointData->SetScalars(scalars);
   scalars->Delete();
 }
