2d472ee1b8d15f724eaf20f26b765a179d29aebb
Dean Inglis <dean.inglis@camris.ca>
2008-08-26 14:28:37 -0400

    BUG: fix printself defect

--- Widgets/vtkDijkstraImageContourLineInterpolator.cxx
+++ Widgets/vtkDijkstraImageContourLineInterpolator.cxx
@@ -27,7 +27,7 @@
 #include "vtkPoints.h"
 #include "vtkPolyData.h"
 
-vtkCxxRevisionMacro(vtkDijkstraImageContourLineInterpolator,"1.1");
+vtkCxxRevisionMacro(vtkDijkstraImageContourLineInterpolator,"1.2");
 vtkStandardNewMacro(vtkDijkstraImageContourLineInterpolator);
 
 //----------------------------------------------------------------------
@@ -129,5 +129,7 @@ int vtkDijkstraImageContourLineInterpolator::InterpolateLine(
 void vtkDijkstraImageContourLineInterpolator::PrintSelf(
                               ostream& os, vtkIndent indent)
 {
-  this->Superclass::PrintSelf(os,indent);  
+  this->Superclass::PrintSelf(os,indent);
+  os << indent << "DijkstraImageGeodesicPath: " << this->DijkstraImageGeodesicPath << endl;
+  os << indent << "CostImage: " << this->GetCostImage() << endl;
 }
