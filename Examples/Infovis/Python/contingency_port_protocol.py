f431d288374e8b99049cc90fe7de883c45f62349
Philippe Pebay <pppebay@sandia.gov>
2008-10-07 20:24:02 -0400

    ENH: progress

--- Examples/Infovis/Python/contingency_port_protocol.py
+++ Examples/Infovis/Python/contingency_port_protocol.py
@@ -1,19 +1,20 @@
 from vtk import *
 
-data_dir = "../../../../VTKData/Data/Infovis/SQLite/"
+data_dir = "../../../../../VTKData/Data/Infovis/SQLite/"
 sqlite_file = data_dir + "ports_protocols.db"
 database = vtkSQLDatabase.CreateFromURL("sqlite://" + sqlite_file)
 database.Open("")
 
 edge_query = database.GetQueryInstance()
-edge_query.SetQuery("select * from tcp")
+edge_query.SetQuery("select src, dst, dport, protocol from tcp")
 
 edge_table = vtkRowQueryToTable()
 edge_table.SetQuery(edge_query)
 
 cs = vtkContingencyStatistics()
 cs.AddInputConnection(edge_table.GetOutputPort())
-contingency_table = cs.GetOutputPort()
+cs.AddColumnPair("dport","protocol")
+cs.SetAssess(1)
 
 vertex_query = database.GetQueryInstance()
 vertex_query.SetQuery("select ip, hostname from dnsnames")
@@ -22,13 +23,15 @@ vertex_table = vtkRowQueryToTable()
 vertex_table.SetQuery(vertex_query)
 
 graph = vtkTableToGraph()
-graph.AddInputConnection(edge_table.GetOutputPort())
+graph.AddInputConnection(cs.GetOutputPort())
 graph.AddLinkVertex("src", "ip", False)
 graph.AddLinkVertex("dst", "ip", False)
 graph.AddLinkEdge("src", "dst")
 graph.SetVertexTableConnection(vertex_table.GetOutputPort())
 graph.Update()
 print graph.GetOutput()
+
+print cs.GetOutput()
 
 view = vtkGraphLayoutView()
 view.AddRepresentationFromInputConnection(graph.GetOutputPort())
@@ -40,7 +43,7 @@ view.SetColorVertices(True)
 
 view.SetEdgeLabelArrayName("protocol")
 view.SetEdgeLabelVisibility(True)
-view.SetEdgeColorArrayName("port")
+view.SetEdgeColorArrayName("p(dport | protocol)")
 view.SetColorEdges(True)
 
 view.SetLayoutStrategyToSimple2D()
