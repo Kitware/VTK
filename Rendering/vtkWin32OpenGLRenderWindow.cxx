c20395a1b1500620eca852ad57187bd11ce49646
David Cole <david.cole@kitware.com>
2008-10-14 13:06:38 -0400

    BUG: Add missing code to handle WM_SETCURSOR in terms of the vtkRenderWindow::GetCurrentCursor() value.

--- Rendering/vtkWin32OpenGLRenderWindow.cxx
+++ Rendering/vtkWin32OpenGLRenderWindow.cxx
@@ -32,7 +32,7 @@ PURPOSE.  See the above copyright notice for more information.
 
 #include "vtkOpenGL.h"
 
-vtkCxxRevisionMacro(vtkWin32OpenGLRenderWindow, "1.157");
+vtkCxxRevisionMacro(vtkWin32OpenGLRenderWindow, "1.158");
 vtkStandardNewMacro(vtkWin32OpenGLRenderWindow);
 
 #define VTK_MAX_LIGHTS 8
@@ -680,6 +680,9 @@ LRESULT vtkWin32OpenGLRenderWindow::MessageProc(HWND hWnd, UINT message,
     }
     break;
     case WM_ERASEBKGND:
+      return TRUE;
+    case WM_SETCURSOR:
+      this->SetCurrentCursor(this->GetCurrentCursor());
       return TRUE;
     default:
       this->InvokeEvent(vtkCommand::RenderWindowMessageEvent, &message);
@@ -1466,6 +1469,7 @@ void vtkWin32OpenGLRenderWindow::SetCursorPosition(int x, int y)
     }
 };
 
+//----------------------------------------------------------------------------
 void vtkWin32OpenGLRenderWindow::SetCurrentCursor(int shape)
 {
   if ( this->InvokeEvent(vtkCommand::CursorChangedEvent,&shape) )
