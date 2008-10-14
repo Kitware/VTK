3f6ca47c48dedcf0d861e8ff6a1d357697577146
Brian Wylie <bnwylie@sandia.gov>
2008-10-14 15:47:12 -0400

    ENH: Adding a python example that demonstrates the use of vtkSQLDatabaseGraphSource

---
+++ Examples/Infovis/Python/databases2.py
@@ -0,0 +1,32 @@
+from vtk import *
+
+data_dir = "../../../../VTKData/Data/Infovis/SQLite/"
+sqlite_file = data_dir + "SmallEmailTest.db"
+
+databaseToGraph = vtkSQLDatabaseGraphSource()
+databaseToGraph.SetURL("sqlite://" + sqlite_file)
+databaseToGraph.SetEdgeQuery("select source, target from emails")
+databaseToGraph.SetVertexQuery("select Name, Job, Age from employee")
+databaseToGraph.AddLinkVertex("source", "Name", False)
+databaseToGraph.AddLinkVertex("target", "Name", False)
+databaseToGraph.AddLinkEdge("source", "target")
+
+view = vtkGraphLayoutView()
+view.AddRepresentationFromInputConnection(databaseToGraph.GetOutputPort())
+view.SetVertexLabelArrayName("label")
+view.SetVertexLabelVisibility(True)
+view.SetVertexColorArrayName("Age")
+view.SetColorVertices(True)
+view.SetLayoutStrategyToSimple2D()
+
+
+theme = vtkViewTheme.CreateMellowTheme()
+theme.SetCellColor(.2,.2,.6)
+theme.SetLineWidth(4)
+view.ApplyViewTheme(theme)
+
+window = vtkRenderWindow()
+window.SetSize(600, 600)
+view.SetupRenderWindow(window)
+view.GetRenderer().ResetCamera()
+window.GetInteractor().Start()
