0e389358627a6269e01a3abb1c56bd406c225f69
Jason Shepherd <jfsheph@sandia.gov>
2008-10-17 19:01:59 -0400

    ENH: adding an example python file for generating a treemap

---
+++ Examples/Infovis/Python/treemap_view.py
@@ -0,0 +1,27 @@
+from vtk import *
+
+reader1 = vtkXMLTreeReader()
+reader1.SetFileName("treetest.xml")
+reader1.Update()
+
+dummy = vtkTreeMapView()
+view = vtkTreeMapView()
+view.SetSizeArrayName("size")
+view.SetColorArrayName("level")
+view.SetLabelArrayName("name")
+view.SetHoverArrayName("name")
+view.SetLayoutStrategyToSquarify()
+view.SetRepresentationFromInput(reader1.GetOutput())
+
+# Apply a theme to the views
+theme = vtkViewTheme.CreateMellowTheme()
+view.ApplyViewTheme(theme)
+theme.FastDelete()
+
+win = vtkRenderWindow()
+dummy.SetupRenderWindow(win)
+view.SetupRenderWindow(win)
+view.GetRenderer().ResetCamera()
+
+win.GetInteractor().Initialize()
+win.GetInteractor().Start()
