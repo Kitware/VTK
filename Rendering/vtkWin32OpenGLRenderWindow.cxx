4bd887956fa62e30a75fa85b89467aec854852a8
David Cole <david.cole@kitware.com>
2008-10-21 14:05:14 -0400

    ENH: Merge changes from main tree into VTK-5-2 branch. (cvs -q up -j1.154 -j1.158 Rendering/vtkWin32OpenGLRenderWindow.cxx)

--- Rendering/vtkWin32OpenGLRenderWindow.cxx
+++ Rendering/vtkWin32OpenGLRenderWindow.cxx
@@ -32,7 +32,7 @@ PURPOSE.  See the above copyright notice for more information.
 
 #include "vtkOpenGL.h"
 
-vtkCxxRevisionMacro(vtkWin32OpenGLRenderWindow, "1.154");
+vtkCxxRevisionMacro(vtkWin32OpenGLRenderWindow, "1.154.2.1");
 vtkStandardNewMacro(vtkWin32OpenGLRenderWindow);
 
 #define VTK_MAX_LIGHTS 8
@@ -680,6 +680,9 @@ LRESULT vtkWin32OpenGLRenderWindow::MessageProc(HWND hWnd, UINT message,
     }
     break;
     case WM_ERASEBKGND:
+      return TRUE;
+    case WM_SETCURSOR:
+      this->SetCurrentCursor(this->GetCurrentCursor());
       return TRUE;
     default:
       this->InvokeEvent(vtkCommand::RenderWindowMessageEvent, &message);
@@ -1405,11 +1408,19 @@ void vtkWin32OpenGLRenderWindow::CleanUpOffScreenRendering(void)
       {
       vtkErrorMacro("wglDeleteContext failed in CleanUpOffScreenRendering(), error: " << GetLastError());
       }
+    this->ContextId=0;
     }
 }
 
 void vtkWin32OpenGLRenderWindow::ResumeScreenRendering(void)
-{  
+{
+  // release OpenGL graphics resources before switch back to on-screen. 
+  if(this->ContextId!=0)
+    {
+      this->MakeCurrent();
+      this->CleanUpRenderers();
+    }
+
   this->Mapped = this->ScreenMapped;
   this->Size[0] = this->ScreenWindowSize[0];
   this->Size[1] = this->ScreenWindowSize[1];
@@ -1458,6 +1469,7 @@ void vtkWin32OpenGLRenderWindow::SetCursorPosition(int x, int y)
     }
 };
 
+//----------------------------------------------------------------------------
 void vtkWin32OpenGLRenderWindow::SetCurrentCursor(int shape)
 {
   if ( this->InvokeEvent(vtkCommand::CursorChangedEvent,&shape) )
