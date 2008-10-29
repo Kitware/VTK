693c9b4a412e627519b2ec4b80ea0655ddae4535
David Cole <david.cole@kitware.com>
2008-10-29 14:51:51 -0400

    STYLE: Remove DartConfig.cmake file. It is no longer necessary when building with the minimum required version of CMake 2.4. CTestConfig.cmake has the settings you're looking for.

--- DartConfig.cmake
+++
@@ -1,4 +0,0 @@
-# This file is no longer strictly necessary, since VTK's CMakeLists.txt file
-# uses "INCLUDE(CTest)" and requires building with CMake 2.4 or greater...
-#
-# Remove this file from CVS after tonight's Nightly dashboards.
