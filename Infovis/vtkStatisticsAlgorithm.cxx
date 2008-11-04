7486b20fa09ef87635b0a773adcb9add0bffa2af
Philippe Pebay <pppebay@sandia.gov>
2008-11-04 18:14:04 -0500

    COMP: with some compilers, must initialize a variable even when it       is marked with vtkNotUsed.

--- Infovis/vtkStatisticsAlgorithm.cxx
+++ Infovis/vtkStatisticsAlgorithm.cxx
@@ -28,7 +28,7 @@
 #include "vtkStringArray.h"
 #include "vtkTable.h"
 
-vtkCxxRevisionMacro(vtkStatisticsAlgorithm, "1.21");
+vtkCxxRevisionMacro(vtkStatisticsAlgorithm, "1.22");
 
 // ----------------------------------------------------------------------
 vtkStatisticsAlgorithm::vtkStatisticsAlgorithm()
@@ -111,7 +111,7 @@ int vtkStatisticsAlgorithm::RequestData( vtkInformation*,
   // Extract output tables
   vtkTable* outData = vtkTable::GetData( outputVector, 0 );
   vtkTable* outMeta1 = vtkTable::GetData( outputVector, 1 );
-  vtkTable* outMeta2; // Unused for now
+  vtkTable* outMeta2 = 0; // Unused for now
 
   outData->ShallowCopy( inData );
 
