879e86132acbcc07d57d2f25153f0381455b9361
Philippe Pebay <pppebay@sandia.gov>
2008-11-06 14:18:14 -0500

    STYLE: copyright notices

--- Infovis/Testing/Cxx/TestCorrelativeStatistics.cxx
+++ Infovis/Testing/Cxx/TestCorrelativeStatistics.cxx
@@ -1,11 +1,14 @@
 /*
- * Copyright 2007 Sandia Corporation.
+ * Copyright 2008 Sandia Corporation.
  * Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
  * license for use of this work by or on behalf of the
  * U.S. Government. Redistribution and use in source and binary forms, with
  * or without modification, are permitted provided that this Notice and any
  * statement of authorship are reproduced on all copies.
  */
+// .SECTION Thanks
+// Thanks to Philippe Pebay from Sandia National Laboratories 
+// for implementing this test.
 
 #include "vtkDoubleArray.h"
 #include "vtkStringArray.h"
