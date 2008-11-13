479fc9ed7fcc3fdeadeb9c8f467202374697c4d1
Philippe Pebay <pppebay@sandia.gov>
2008-11-13 18:50:07 -0500

    STYLE: a more appropriate test name, since it is not limited to parallel        descriptive statistics.

--- Infovis/Testing/Cxx/TestParallelRandomStatistics.cxx
+++ Infovis/Testing/Cxx/TestParallelRandomStatistics.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    TestPDescriptiveStatistics.cxx
+  Module:    TestParallelRandomStatistics.cxx
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
