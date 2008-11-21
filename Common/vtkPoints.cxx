53eb1ce4a6d53278c5d1127fa9f0cb1c32f3c1ab
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2008-11-21 13:09:32 -0500

    BUG: Assign some default name to points array.

--- Common/vtkPoints.cxx
+++ Common/vtkPoints.cxx
@@ -29,7 +29,7 @@
 #include "vtkUnsignedLongArray.h"
 #include "vtkUnsignedShortArray.h"
 
-vtkCxxRevisionMacro(vtkPoints, "1.55");
+vtkCxxRevisionMacro(vtkPoints, "1.56");
 
 //----------------------------------------------------------------------------
 // Needed when we don't use the vtkStandardNewMacro.
@@ -182,6 +182,10 @@ void vtkPoints::SetData(vtkDataArray *data)
     this->Data->UnRegister(this);
     this->Data = data;
     this->Data->Register(this);
+    if (!this->Data->GetName())
+      {
+      this->Data->SetName("Points");
+      }
     this->Modified();
     }
 }
