7517ff9d5062067bdf30970c4c1c8d64cb3c6fe2
David Cole <david.cole@kitware.com>
2008-11-25 15:17:25 -0500

    COMP: Trigger continuous rebuild of proj_list.o to get rid of "incremental build does not work when just a non-vtk header file changes" problem.

--- Utilities/vtklibproj4/proj_list.c
+++ Utilities/vtklibproj4/proj_list.c
@@ -31,8 +31,19 @@ LIBPROJ_ID[] = "Id";
 */
 #define PROJ_LIST_H "proj_list.h"
 #include "lib_proj.h"
+
 /*
 ** Log: proj_list.c
+** Revision 1.1  2008-11-07 16:41:14  jeff
+** ENH: Adding a 2D geoview. Adding the geographic projection library libproj4
+** to Utilities. Updating the architecture of the geospatial views. All
+** multi-resolution sources are now subclasses of vtkGeoSource. Each source
+** has its own worker thread for fetching refined images or geometry.
+** On the 3D side, vtkGeoGlobeSource is an appropriate source for vtkGeoTerrain,
+** and vtkGeoAlignedImageSource is an appropriate source for
+** vtkGeoAlignedImageRepresentation. On the 2D side, vtkGeoProjectionSource is an
+** appropriate source for vtkGeoTerrain2D, and the image source is the same.
+**
 ** Revision 3.1  2006/01/11 01:38:18  gie
 ** Initial
 **
