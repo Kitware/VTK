aee7e17ac2653daf84d68f2fddd88ba2b88a7826
David Thompson <dcthomp@sandia.gov>
2009-01-11 15:17:12 -0500

    COMP: Perhaps AIX #defines Z2? Can't think of why else this error       'The text "100" is unexpected' would be flagged on line 166.

--- Rendering/vtkLabelHierarchyIterator.cxx
+++ Rendering/vtkLabelHierarchyIterator.cxx
@@ -35,7 +35,7 @@
 #include "vtkPointData.h"
 #include "vtkPolyData.h"
 
-vtkCxxRevisionMacro(vtkLabelHierarchyIterator, "1.4");
+vtkCxxRevisionMacro(vtkLabelHierarchyIterator, "1.5");
 vtkCxxSetObjectMacro(vtkLabelHierarchyIterator,Hierarchy,vtkLabelHierarchy);
 vtkCxxSetObjectMacro(vtkLabelHierarchyIterator,TraversedBounds,vtkPolyData);
 
@@ -163,6 +163,9 @@ void vtkLabelHierarchyIterator::BoxAllNodes( vtkPolyData* boxes )
     {
     vtkLabelHierarchy::Implementation::HierarchyIterator2 iter;
     double ctr[3];
+#ifdef Z2
+#  undef Z2
+#endif // see if this fixes AIX
     double hz = this->Hierarchy->GetImplementation()->Z2;
     for (
       iter = this->Hierarchy->GetImplementation()->Hierarchy2->begin( true );
