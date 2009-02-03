8931ac3cb9fa01b51f5d512dc2bbf67321c51b94
Francois Bertel <francois.bertel@kitware.com>
2009-02-03 11:00:05 -0500

    STYLE:Fixed missing information about initial values of XResolution and YResolution.

--- Graphics/vtkPlaneSource.h
+++ Graphics/vtkPlaneSource.h
@@ -19,10 +19,11 @@
 // origin point, and then two other points that, together with the
 // origin, define two axes for the plane. These axes do not have to be
 // orthogonal - so you can create a parallelogram. (The axes must not
-// be parallel.) By default, the plane is centered at the origin and
-// perpendicular to the z-axis, with width and height of length 1. The
-// resolution of the plane (i.e., number of subdivisions) is
+// be parallel.) The resolution of the plane (i.e., number of subdivisions) is
 // controlled by the ivars XResolution and YResolution.
+//
+// By default, the plane is centered at the origin and perpendicular to the
+// z-axis, with width and height of length 1 and resolutions set to 1.
 //
 // There are three convenience methods that allow you to easily move the
 // plane.  The first, SetNormal(), allows you to specify the plane
