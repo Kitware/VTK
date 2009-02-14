53461abda280d068a9b2c8a70222efecebf613f5
Philippe Pebay <pppebay@sandia.gov>
2009-02-13 19:20:37 -0500

    STYLE: identify clearly that this test uses MPI

--- Infovis/Testing/Cxx/TestParallelRandomStatisticsMPI.cxx
+++ Infovis/Testing/Cxx/TestParallelRandomStatisticsMPI.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    TestParallelRandomStatistics.cxx
+  Module:    TestParallelRandomStatisticsMPI.cxx
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
