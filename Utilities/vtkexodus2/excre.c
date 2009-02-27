bae996c8aa063ccadb356f5d06fb5ce6829b67a0
David Cole <david.cole@kitware.com>
2009-02-27 17:39:02 -0500

    COMP: Fix compiler warning introduced by yesterday's commit.

--- Utilities/vtkexodus2/excre.c
+++ Utilities/vtkexodus2/excre.c
@@ -205,7 +205,7 @@ int ex_create_int (const char *path,
  */
 
 /* store Exodus API version # as an attribute */
-  vers = EX_API_VERS;
+  vers = (float) EX_API_VERS;
   if ((status=nc_put_att_float(exoid, NC_GLOBAL, ATT_API_VERSION,
 			       NC_FLOAT, 1, &vers)) != NC_NOERR)
   {
@@ -218,7 +218,7 @@ int ex_create_int (const char *path,
   }
 
 /* store Exodus file version # as an attribute */
-  vers = EX_VERS;
+  vers = (float) EX_VERS;
   if ((status=nc_put_att_float(exoid, NC_GLOBAL, ATT_VERSION, NC_FLOAT, 1, &vers)) != NC_NOERR)
   {
     exerrval = status;
