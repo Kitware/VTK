4bf8979e731c3d9415f05b5b7c0720e4a5d84e30
Francois Bertel <francois.bertel@kitware.com>
2009-03-03 12:46:38 -0500

    BUG:8659.

--- Rendering/vtkActor.cxx
+++ Rendering/vtkActor.cxx
@@ -31,7 +31,7 @@
 
 #include <math.h>
 
-vtkCxxRevisionMacro(vtkActor, "1.139");
+vtkCxxRevisionMacro(vtkActor, "1.140");
 
 vtkCxxSetObjectMacro(vtkActor,Texture,vtkTexture);
 vtkCxxSetObjectMacro(vtkActor,Mapper,vtkMapper);
@@ -117,6 +117,13 @@ void vtkActor::GetActors(vtkPropCollection *ac)
 // should be called from the render methods only
 int vtkActor::GetIsOpaque()
 {
+  // make sure we have a property
+  if(!this->Property)
+    {
+    // force creation of a property
+    this->GetProperty();
+    }
+  
   int result=this->Property->GetOpacity() >= 1.0;
   
   if(result)
