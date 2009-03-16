42a49e55489a3267eed02cec51b5470b5b74db2c
Ken Moreland <kmorel@sandia.gov>
2009-03-16 12:15:19 -0400

    STYLE: Add a warning when vtkFunctionParser fails to help users trace back where the problem originated from.

--- Graphics/vtkArrayCalculator.cxx
+++ Graphics/vtkArrayCalculator.cxx
@@ -28,7 +28,7 @@
 #include "vtkPolyData.h"
 #include "vtkUnstructuredGrid.h"
 
-vtkCxxRevisionMacro(vtkArrayCalculator, "1.43");
+vtkCxxRevisionMacro(vtkArrayCalculator, "1.44");
 vtkStandardNewMacro(vtkArrayCalculator);
 
 vtkArrayCalculator::vtkArrayCalculator()
@@ -391,6 +391,7 @@ int vtkArrayCalculator::RequestData(
   else
     {
     // Error occurred in vtkFunctionParser.
+    vtkWarningMacro("An error occured when parsing the calculator's function.  See previous errors.");
     return 1;
     }
 
