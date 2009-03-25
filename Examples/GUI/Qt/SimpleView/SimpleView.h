66b4f2232b0b2d402e9a68d43e097c68ca0c6a55
Brian Wylie <bnwylie@sandia.gov>
2009-03-25 09:56:14 -0400

    ENH:  Adding new simpleview example

---
+++ Examples/GUI/Qt/SimpleView/SimpleView.h
@@ -0,0 +1,57 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    SimpleView.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright 2009 Sandia Corporation.
+  Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+  license for use of this work by or on behalf of the
+  U.S. Government. Redistribution and use in source and binary forms, with
+  or without modification, are permitted provided that this Notice and any
+  statement of authorship are reproduced on all copies.
+
+=========================================================================*/
+#ifndef SimpleView_H
+#define SimpleView_H
+
+#include "vtkSmartPointer.h"    // Required for smart pointer internal ivars.
+#include <QMainWindow>
+
+// Forward Qt class declarations
+class Ui_SimpleView;
+
+// Forward VTK class declarations
+class vtkQtTableView;
+
+
+class SimpleView : public QMainWindow
+{
+  Q_OBJECT
+
+public:
+
+  // Constructor/Destructor
+  SimpleView(); 
+  ~SimpleView();
+
+public slots:
+
+  virtual void slotOpenFile();
+  virtual void slotExit();
+
+protected:
+   
+protected slots:
+
+private:
+
+  vtkSmartPointer<vtkQtTableView>         TableView;
+    
+  // Designer form
+  Ui_SimpleView *ui;
+};
+
+#endif // SimpleView_H
