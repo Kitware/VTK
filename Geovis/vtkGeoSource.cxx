75d7ceb179f3cbe0315312c5c76dbef5dc94e959
Dave Partyka <dave.partyka@kitware.com>
2009-03-25 17:05:08 -0400

    ENH: Fix threading/debug leaks of vtkCollection.

--- Geovis/vtkGeoSource.cxx
+++ Geovis/vtkGeoSource.cxx
@@ -72,7 +72,7 @@ public:
   vtksys_stl::vector<int> ThreadIds;
 };
 
-vtkCxxRevisionMacro(vtkGeoSource, "1.7");
+vtkCxxRevisionMacro(vtkGeoSource, "1.8");
 vtkGeoSource::vtkGeoSource()
 {
   this->InputSet = vtkCollection::New();
@@ -137,6 +137,7 @@ void vtkGeoSource::ShutDown()
       this->Threader->TerminateThread(*iter);
       }
     this->Implementation->ThreadIds.clear();
+    this->Implementation->OutputMap.clear();
     }
   this->Initialized = false;
 }
