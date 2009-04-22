74860f7a1676cc8ce7c1f22e4672397589b14a98
Jason Shepherd <jfsheph@sandia.gov>
2009-04-22 15:37:14 -0400

    ENH: Add the option to use the new Qt based labeler in the tree ring view, which allows text rotations, ellipsis, unicode, and markup support to the tree ring views.

--- Views/vtkTreeRingView.cxx
+++ Views/vtkTreeRingView.cxx
@@ -25,7 +25,13 @@
 #include "vtkStackedTreeLayoutStrategy.h"
 #include "vtkTreeRingToPolyData.h"
 
-vtkCxxRevisionMacro(vtkTreeRingView, "1.7");
+#ifdef VTK_USE_QT
+#include "vtkTexturedActor2D.h"
+#include "vtkAreaLayout.h"
+#include "vtkQtTreeRingLabelMapper.h"
+#endif
+
+vtkCxxRevisionMacro(vtkTreeRingView, "1.8");
 vtkStandardNewMacro(vtkTreeRingView);
 //----------------------------------------------------------------------------
 vtkTreeRingView::vtkTreeRingView()
@@ -37,6 +43,23 @@ vtkTreeRingView::~vtkTreeRingView()
 {
 }
 
+//----------------------------------------------------------------------------
+void vtkTreeRingView::UseFittedLabeling()
+{
+#ifdef VTK_USE_QT
+  this->AreaLabelActor = vtkSmartPointer<vtkTexturedActor2D>::New();
+  this->AreaLabelActor->PickableOff();
+  
+  vtkSmartPointer<vtkQtTreeRingLabelMapper> mapper = 
+    vtkSmartPointer<vtkQtTreeRingLabelMapper>::New();
+  mapper->SetRenderer( this->Renderer );
+  this->SetAreaLabelMapper(mapper);
+  
+  this->AreaLabelMapper->SetInputConnection(this->AreaLayout->GetOutputPort());
+  this->AreaLabelActor->SetMapper(this->AreaLabelMapper);
+#endif
+}
+  
 //----------------------------------------------------------------------------
 void vtkTreeRingView::SetRootAngles(double start, double end)
 {
