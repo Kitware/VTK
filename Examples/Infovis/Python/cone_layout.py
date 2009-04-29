99d8f82f81fb23cafb4e17013a5b395c1b721353
Brian Wylie <bnwylie@sandia.gov>
2009-04-29 14:55:34 -0400

    ENH: Revamp the python cone layout example to take advantage of the new 3D layout support in views.

--- Examples/Infovis/Python/cone_layout.py
+++ Examples/Infovis/Python/cone_layout.py
@@ -4,97 +4,28 @@ from vtk import *
 reader = vtkXMLTreeReader()
 reader.SetFileName("vtkclasses.xml")
 reader.Update()
+print reader.GetOutput()
 
-# Layout: use 3D cone-tree - large subtrees so compress spacing...
-view = vtkConeLayoutStrategy()
-view.SetSpacing(0.3);
-layout = vtkGraphLayout()
-layout.SetLayoutStrategy(view)
-layout.SetInputConnection(reader.GetOutputPort())
+view = vtkGraphLayoutView()
+view.AddRepresentationFromInputConnection(reader.GetOutputPort())
+view.SetVertexLabelArrayName("id")
+view.SetVertexLabelVisibility(True)
+view.SetVertexColorArrayName("vertex id")
+view.SetColorVertices(True)
+view.SetLayoutStrategyToCone()
+view.SetInteractionModeTo3D() # Left mouse button causes 3D rotate instead of zoom
+view.SetLabelPlacementModeToLabelPlacer()
 
-geom = vtkGraphToPolyData()
-geom.SetInputConnection(layout.GetOutputPort())
+theme = vtkViewTheme.CreateMellowTheme()
+theme.SetCellColor(.2,.2,.6)
+theme.SetLineWidth(2)
+theme.SetPointSize(10)
+view.ApplyViewTheme(theme)
+theme.FastDelete()
 
-# Vertex pipeline - mark each vertex with a cube glyph
-cube = vtkCubeSource()
-cube.SetXLength(1.0)
-cube.SetYLength(1.0)
-cube.SetZLength(1.0)
-
-glyph = vtkGlyph3D()
-glyph.SetInputConnection(geom.GetOutputPort())
-glyph.SetSource(cube.GetOutput())
-
-gmap = vtkPolyDataMapper()
-gmap.SetInputConnection(glyph.GetOutputPort())
-
-gact = vtkActor()
-gact.SetMapper(gmap)
-gact.GetProperty().SetColor(0,0,1)
-
-
-# Edge pipeline - map edges to lines
-mapper = vtkPolyDataMapper()
-mapper.SetInputConnection(geom.GetOutputPort())
-
-actor = vtkActor()
-actor.SetMapper(mapper)
-actor.GetProperty().SetColor(0.4,0.4,0.6)
-
-
-# Label pipeline.
-# 1.  Find vertex degrees
-# 2.  Select vertices with degree >= 10 (could include parent)
-# 3.  Extract those vertices form graph
-# 4.  Render labels
-
-degree = vtkVertexDegree()
-degree.SetInputConnection(layout.GetOutputPort())
-
-sel = vtkSelectionSource()
-sel.SetContentType(7)
-sel.SetFieldType(3)
-sel.SetArrayName("VertexDegree")
-sel.AddThreshold(10,10000)    # classes with "large" numbers of children...
-sel.Update()
-
-# Take selection and extract a graph
-extract_graph = vtkExtractSelectedGraph()
-extract_graph.AddInputConnection(degree.GetOutputPort())
-extract_graph.SetSelectionConnection(sel.GetOutputPort())
-
-geom2 = vtkGraphToPolyData()
-geom2.SetInputConnection(extract_graph.GetOutputPort())
-
-labels = vtkLabeledDataMapper()
-labels.SetFieldDataArray(1)
-labels.SetInputConnection(geom2.GetOutputPort())
-labels.SetLabelModeToLabelFieldData()
-
-prop = labels.GetLabelTextProperty()
-prop.SetColor(1,1,1)
-prop.SetJustificationToCentered()
-prop.SetVerticalJustificationToCentered()
-prop.SetFontSize(10)
-
-text = vtkActor2D()
-text.SetMapper(labels)
-
-
-# Renderer, window, and interaction
-
-ren = vtkRenderer()
-ren.AddActor(actor)
-ren.AddActor(gact)
-ren.AddActor(text)
-ren.ResetCamera()
-
-renWin = vtkRenderWindow()
-renWin.AddRenderer(ren)
-renWin.SetSize(800,550)
-
-iren = vtkRenderWindowInteractor()
-iren.SetRenderWindow(renWin)
-iren.Initialize()
-iren.Start()
-
+window = vtkRenderWindow()
+window.SetSize(600, 600)
+view.SetupRenderWindow(window)
+view.GetRenderer().ResetCamera()
+window.Render()
+window.GetInteractor().Start()
