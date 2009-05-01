400355085a2162574030d5f4dcb9f13b6108c559
Zack Galbreath <zack.galbreath@kitware.com>
2009-05-01 10:57:47 -0400

    ENH: Comment only changes for VTK-5-4-1-rc1

--- Common/vtkVersion.h
+++ Common/vtkVersion.h
@@ -21,6 +21,7 @@
 // This file will change frequently to update the VTKSourceVersion which
 // timestamps a particular source release.
 //
+// Updated for cvs tag VTK-5-4-1-rc1
 
 
 #ifndef __vtkVersion_h
