7622b0e039f69a0265add5e333c156f2c287bf44
Timothy M. Shead <tshead@sandia.gov>
2009-05-14 13:50:30 -0400

    BUG: vtkUnicodeStringArray::InsertTuple() didn't automatically grow the array as specified in vtkAbstractArray.

--- Common/vtkUnicodeStringArray.cxx
+++ Common/vtkUnicodeStringArray.cxx
@@ -28,7 +28,7 @@ public:
   StorageT Storage;
 };
 
-vtkCxxRevisionMacro(vtkUnicodeStringArray, "1.4");
+vtkCxxRevisionMacro(vtkUnicodeStringArray, "1.5");
 vtkStandardNewMacro(vtkUnicodeStringArray);
 
 vtkUnicodeStringArray::vtkUnicodeStringArray(vtkIdType)
@@ -102,7 +102,10 @@ void vtkUnicodeStringArray::InsertTuple(vtkIdType i, vtkIdType j, vtkAbstractArr
     return;
     }
 
-  this->Implementation->Storage.insert(this->Implementation->Storage.begin() + i, array->Implementation->Storage[j]);
+  if(this->Implementation->Storage.size() <= i)
+    this->Implementation->Storage.resize(i + 1);
+
+  this->Implementation->Storage[i] = array->Implementation->Storage[j];
   this->DataChanged();
 }
 
