e68770fedad15d8f80a2ff418f69e8a0609e6f6e
Zack Galbreath <zack.galbreath@kitware.com>
2009-06-03 09:31:28 -0400

    ENH: Final edit for tagging VTK 5-4-2

--- Common/vtkVersion.h
+++ Common/vtkVersion.h
@@ -20,8 +20,6 @@
 // .SECTION Caveats
 // This file will change frequently to update the VTKSourceVersion which
 // timestamps a particular source release.
-//
-// Updated for cvs tag VTK-5-4-2-rc3
 
 #ifndef __vtkVersion_h
 #define __vtkVersion_h
