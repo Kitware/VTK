ea948278c47b44958dbd94a9076cd70084ca5a4d
Zack Galbreath <zack.galbreath@kitware.com>
2009-06-30 14:10:43 -0400

    ENH: Merge changes from main tree into VTK-5-4 branch. (cvs -q up -j1.3 -j1.4 Widgets/Testing/Cxx/TestOrthoPlanes.cxx)

--- Widgets/Testing/Cxx/TestOrthoPlanes.cxx
+++ Widgets/Testing/Cxx/TestOrthoPlanes.cxx
@@ -398,6 +398,7 @@ int TestOrthoPlanes( int argc, char *argv[] )
   vtkRenderer* ren2 = vtkRenderer::New();
 
   vtkRenderWindow* renWin = vtkRenderWindow::New();
+  renWin->SetMultiSamples(0);
     renWin->AddRenderer(ren2);
     renWin->AddRenderer(ren1);
 
