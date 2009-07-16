efc0e90e31984f6a05fb808b33f54d33f1be76af
Timothy M. Shead <tshead@sandia.gov>
2009-07-16 14:35:12 -0400

    COMP: Added regression tests for Mime Type identification.

---
+++ TextAnalysis/Testing/Cxx/TestMimeTypes.cxx
@@ -0,0 +1,53 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    TestMimeTypes.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright 2008 Sandia Corporation.
+  Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+  license for use of this work by or on behalf of the
+  U.S. Government. Redistribution and use in source and binary forms, with
+  or without modification, are permitted provided that this Notice and any
+  statement of authorship are reproduced on all copies.
+
+=========================================================================*/
+
+#include <vtkMimeTypes.h>
+#include <vtkSmartPointer.h>
+
+#include <vtkstd/stdexcept>
+#include <vtksys/ios/sstream>
+
+#define test_expression(expression) \
+{ \
+  if(!(expression)) \
+    { \
+    vtkstd::ostringstream buffer; \
+    buffer << "Expression failed at line " << __LINE__ << ": " << #expression; \
+    throw vtkstd::runtime_error(buffer.str()); \
+    } \
+}
+
+int TestMimeTypes(int argc, char* argv[])
+{
+  try
+    {
+    vtkSmartPointer<vtkMimeTypes> mime_types = vtkSmartPointer<vtkMimeTypes>::New();
+
+    test_expression(mime_types->Lookup("file:///home/bob/a.foo") == "");
+    test_expression(mime_types->Lookup("file:///home/bob/b.txt") == "text/plain");
+    test_expression(mime_types->Lookup("file:///home/bob/c.doc") == "application/msword");
+    test_expression(mime_types->Lookup("file:///home/bob/d.pdf") == "application/pdf");
+ 
+    return 0;
+    }
+  catch(vtkstd::exception& e)
+    {
+    cerr << e.what() << endl;
+    return 1;
+    }
+}
+
