c07c5fa58d354e5980e22e76cf81408df8147c47
Francois Bertel <francois.bertel@kitware.com>
2009-07-21 10:33:22 -0400

    COMP:Fixed warning.

--- TextAnalysis/Testing/Cxx/TestTokenValueFilter.cxx
+++ TextAnalysis/Testing/Cxx/TestTokenValueFilter.cxx
@@ -46,41 +46,41 @@
     } \
 }
 
-int TestTokenValueFilter(int argc, char* argv[])
+int TestTokenValueFilter(int vtkNotUsed(argc), char *vtkNotUsed(argv)[])
 {
   try
     {
     vtkIdTypeArray* const document_array = vtkIdTypeArray::New();
     document_array->SetName("document");
-      
+    
     vtkStringArray* const uri_array = vtkStringArray::New();
     uri_array->SetName("uri");
-
+    
     vtkStringArray* const mime_type_array = vtkStringArray::New();
     mime_type_array->SetName("mime_type");
-
+    
     vtkStringArray* const content_array = vtkStringArray::New();
     content_array->SetName("content");
-
+    
     document_array->InsertNextValue(0);
     uri_array->InsertNextValue("data:text/plain");
     mime_type_array->InsertNextValue("text/plain");
     content_array->InsertNextValue("The quick brown fox jumped over the lazy dogs.");
-   
+    
     vtkSmartPointer<vtkTable> documents = vtkSmartPointer<vtkTable>::New();
     documents->AddColumn(document_array);
     documents->AddColumn(uri_array);
     documents->AddColumn(mime_type_array);
     documents->AddColumn(content_array);
-
+    
     document_array->Delete();
     uri_array->Delete();
     mime_type_array->Delete();
     content_array->Delete();
-
+    
     vtkSmartPointer<vtkDocumentTextExtraction> text_extraction = vtkSmartPointer<vtkDocumentTextExtraction>::New();
     text_extraction->SetInputConnection(0, documents->GetProducerPort());
-
+    
     vtkSmartPointer<vtkTokenizer> tokenizer = vtkSmartPointer<vtkTokenizer>::New();
     tokenizer->SetInputConnection(0, text_extraction->GetOutputPort());
     tokenizer->AddDroppedDelimiters(vtkTokenizer::Whitespace());
@@ -92,17 +92,17 @@ int TestTokenValueFilter(int argc, char* argv[])
     vtkSmartPointer<vtkTokenValueFilter> token_value = vtkSmartPointer<vtkTokenValueFilter>::New();
     token_value->SetInputConnection(0, fold_case->GetOutputPort());
     token_value->AddStopWordValues();
-  
+    
     token_value->Update();
     vtkUnicodeStringArray* const tokens = vtkUnicodeStringArray::SafeDownCast(token_value->GetOutput()->GetColumnByName("text"));
-
+    
     for(vtkIdType i = 0; i != tokens->GetNumberOfTuples(); ++i)
       cerr << tokens->GetValue(i).utf8_str() << endl;
-
+    
     test_expression(tokens->GetNumberOfTuples() == 7);
     test_expression(tokens->GetValue(0) == vtkUnicodeString::from_utf8("quick"));
     test_expression(tokens->GetValue(6) == vtkUnicodeString::from_utf8("."));
- 
+    
     return 0;
     }
   catch(vtkstd::exception& e)
@@ -111,4 +111,3 @@ int TestTokenValueFilter(int argc, char* argv[])
     return 1;
     }
 }
-
