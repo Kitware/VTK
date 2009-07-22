49339407d44abffb0103bed0537cedabbd9b4f12
Nikhil Shetty <nikhil.shetty@kitware.com>
2009-07-22 14:36:26 -0400

    ENH: Introducing Introspection and a few more speedups.
    CS wrapper mechanism changed to use map <string, funptr>
    
    Changed interpreter mechanism to use maps. Introduced another init generator which generates vtkParaviewMOP.cxx
    
    Adding vtkParse.h in the right place (its a symbolic link)
    
    A working version with (a) Hashmaps for CLASS and resp METHODS called by interpreter (b) Stuff to gather info for minimal image
    
    Test cases failing. Trying to undo changes in cmake files and see
    
    Minor redo in CMakeList.txt, Using the vtkParaviewMINIInit to initialize (Commented the rest) (removed comment)
    
    BTX ETX added to classes to compile. Test cases do not complete however pv client comes up and is usable
    
    Put in PARAVIEW_MINIMAL_BUILD predefined macro. Few more BTX and ETX. Restored Servers/Common/CMakeList.txt to original state
    
    Resolved compilation errors and also generated wrappers for PVCommandOptions_SRC
    
    removing soft links created earlier. Look for better cmake solution
    
    to use vtkParse.h within paraview
    
    COMP: use vtkstd instead of std
    
    BUG: Fixed bug in vtkWrapClientServer.cmake + IF conditions added to build PARAVIEW_MINI + more BTX-ETX
    
    ENH: Adding a MetaObjectInfoMap in interpreter for introspection.
    
    ENH: ClientServerInterpreter with introspection
    
    trying to clear rebase errors
    
    To retract form an update
    
    to handle mpi4pi warnings

--- Filtering/vtkMutableDirectedGraph.h
+++ Filtering/vtkMutableDirectedGraph.h
@@ -22,8 +22,8 @@
 // .SECTION Description
 // vtkMutableDirectedGraph is a directed graph which has additional methods
 // for adding edges and vertices. AddChild() is a convenience method for
-// constructing trees. ShallowCopy(), DeepCopy(), CheckedShallowCopy() and 
-// CheckedDeepCopy() will succeed for instances of vtkDirectedGraph, 
+// constructing trees. ShallowCopy(), DeepCopy(), CheckedShallowCopy() and
+// CheckedDeepCopy() will succeed for instances of vtkDirectedGraph,
 // vtkMutableDirectedGraph and vtkTree.
 //
 // .SECTION See Also
@@ -62,7 +62,7 @@ public:
   // If non-null, propertyArr provides properties that will be attached
   // to this vertex. The values in propertyArr must match up with the
   // arrays in the vertex data retrieved by GetVertexData.
-  // If the graph has pedigree IDs for its vertices, and a vertex with 
+  // If the graph has pedigree IDs for its vertices, and a vertex with
   // the given pedigree ID already exists, its properties will be
   // overwritten with the properties in propertyArr and its ID will be
   // returned. Note that, in a distributed graph with pedigree IDs,
@@ -80,7 +80,7 @@ public:
   // vertex with that pedigree ID does not already exist) and returns
   // the id the vertex with that pedigree ID. If a vertex with the
   // given pedigree ID already exists, its ID will be returned. Note
-  // that, in a distributed graph, 
+  // that, in a distributed graph,
   // the vertex added or found might not be local. In this case,
   // AddVertex will wait until the vertex can be added or found
   // remotely, so that the proper vertex ID can be returned. If you
@@ -97,8 +97,10 @@ public:
   // in propertyArr must match up with the arrays in the edge data
   // returned by GetEdgeData. Both u and v must refer to vertices
   // already in the graph.
+//BTX
   vtkEdgeType AddEdge(vtkIdType u, vtkIdType v);
-  
+//ETX
+
   // Description:
   // Adds a directed edge from u to v to the graph and returns a
   // vtkEdgeType structure for that edge. propertyArr
@@ -106,10 +108,12 @@ public:
   // in propertyArr must match up with the arrays in the edge data
   // returned by GetEdgeData. Both u and v must refer to vertices
   // already in the graph.
-  vtkEdgeType AddEdge(vtkIdType u, vtkIdType v, 
+//BTX
+  vtkEdgeType AddEdge(vtkIdType u, vtkIdType v,
                       vtkVariantArray *propertyArr);
+//ETX
 
-  //BTX  
+  //BTX
   // Description:
   // Adds a directed edge from u to v to the graph and returns a
   // vtkEdgeType structure for that edge. uPedigreeId is the pedigree
@@ -118,7 +122,7 @@ public:
   // If provided, propertyArr provides edge properties
   // for the newly-created edge. The values in propertyArr must match
   // up with the arrays in the edge data returned by GetEdgeData.
-  vtkEdgeType AddEdge(const vtkVariant& uPedigreeId, vtkIdType v, 
+  vtkEdgeType AddEdge(const vtkVariant& uPedigreeId, vtkIdType v,
                       vtkVariantArray *propertyArr = 0);
 
   // Description:
@@ -129,7 +133,7 @@ public:
   // If provided, propertyArr provides edge properties
   // for the newly-created edge. The values in propertyArr must match
   // up with the arrays in the edge data returned by GetEdgeData.
-  vtkEdgeType AddEdge(vtkIdType u, const vtkVariant& vPedigreeId, 
+  vtkEdgeType AddEdge(vtkIdType u, const vtkVariant& vPedigreeId,
                       vtkVariantArray *propertyArr = 0);
 
   // Description:
@@ -140,8 +144,8 @@ public:
   // propertyArr provides edge properties for the newly-created
   // edge. The values in propertyArr must match up with the arrays in
   // the edge data returned by GetEdgeData.
-  vtkEdgeType AddEdge(const vtkVariant& uPedigreeId, 
-                      const vtkVariant& vPedigreeId, 
+  vtkEdgeType AddEdge(const vtkVariant& uPedigreeId,
+                      const vtkVariant& vPedigreeId,
                       vtkVariantArray *propertyArr = 0);
   //ETX
 
@@ -151,7 +155,7 @@ public:
   // to this vertex. The values in propertyArr must match up with the
   // arrays in the vertex data retrieved by GetVertexData.
   void LazyAddVertex();
-  
+
   // Description:
   // Adds a vertex to the graph, and returns the id of that vertex.
   // If non-null, propertyArr provides properties that will be attached
@@ -185,7 +189,7 @@ public:
   // already exist. If provided, propertyArr provides edge properties
   // for the newly-created edge. The values in propertyArr must match
   // up with the arrays in the edge data returned by GetEdgeData.
-  void LazyAddEdge(const vtkVariant& uPedigreeId, vtkIdType v, 
+  void LazyAddEdge(const vtkVariant& uPedigreeId, vtkIdType v,
                    vtkVariantArray *propertyArr = 0);
 
   // Description:
@@ -197,7 +201,7 @@ public:
   // already exist. If provided, propertyArr provides edge properties
   // for the newly-created edge. The values in propertyArr must match
   // up with the arrays in the edge data returned by GetEdgeData.
-  void LazyAddEdge(vtkIdType u, const vtkVariant& vPedigreeId, 
+  void LazyAddEdge(vtkIdType u, const vtkVariant& vPedigreeId,
                    vtkVariantArray *propertyArr = 0);
 
   // Description:
@@ -210,8 +214,8 @@ public:
   // propertyArr provides edge properties for the newly-created
   // edge. The values in propertyArr must match up with the arrays in
   // the edge data returned by GetEdgeData.
-  void LazyAddEdge(const vtkVariant& uPedigreeId, 
-                   const vtkVariant& vPedigreeId, 
+  void LazyAddEdge(const vtkVariant& uPedigreeId,
+                   const vtkVariant& vPedigreeId,
                    vtkVariantArray *propertyArr = 0);
   //ETX
 
--- Rendering/vtkOpenGLRenderWindow.h
+++ Rendering/vtkOpenGLRenderWindow.h
@@ -50,7 +50,7 @@ public:
   virtual void StereoUpdate();
 
   // Description:
-  // Set/Get the pixel data of an image, transmitted as RGBRGB... 
+  // Set/Get the pixel data of an image, transmitted as RGBRGB...
   virtual unsigned char *GetPixelData(int x,int y,int x2,int y2,int front);
   virtual int GetPixelData(int x,int y,int x2,int y2, int front,
                            vtkUnsignedCharArray *data);
@@ -60,7 +60,7 @@ public:
                            vtkUnsignedCharArray *data, int front);
 
   // Description:
-  // Set/Get the pixel data of an image, transmitted as RGBARGBA... 
+  // Set/Get the pixel data of an image, transmitted as RGBARGBA...
   virtual float *GetRGBAPixelData(int x,int y,int x2,int y2,int front);
   virtual int GetRGBAPixelData(int x,int y,int x2,int y2, int front,
                                vtkFloatArray* data);
@@ -75,10 +75,10 @@ public:
                                    vtkUnsignedCharArray *data);
   virtual int SetRGBACharPixelData(int x, int y, int x2, int y2,
                                    unsigned char *data, int front,
-                                   int blend=0);  
+                                   int blend=0);
   virtual int SetRGBACharPixelData(int x,int y,int x2,int y2,
                                    vtkUnsignedCharArray *data, int front,
-                                   int blend=0);  
+                                   int blend=0);
 
   // Description:
   // Set/Get the zbuffer data from an image
@@ -89,7 +89,7 @@ public:
   virtual int SetZbufferData( int x1, int y1, int x2, int y2, float *buffer );
   virtual int SetZbufferData( int x1, int y1, int x2, int y2,
                               vtkFloatArray *buffer );
-  
+
   // Description:
   // Register a texture name with this render window.
   void RegisterTextureResource (GLuint id);
@@ -102,7 +102,7 @@ public:
   // Get the size of the color buffer.
   // Returns 0 if not able to determine otherwise sets R G B and A into buffer.
   int GetColorBufferSizes(int *rgba);
-  
+
   // Description:
   // Initialize OpenGL for this window.
   virtual void OpenGLInit();
@@ -114,7 +114,7 @@ public:
   // application-created framebuffer object (GPU-based offscreen rendering)
   // It is used by vtkOpenGLCamera.
   unsigned int GetBackLeftBuffer();
-  
+
   // Description:
   // Return the OpenGL name of the back right buffer.
   // It is GL_BACK_RIGHT if GL is bound to the window-system-provided
@@ -122,7 +122,7 @@ public:
   // application-created framebuffer object (GPU-based offscreen rendering)
   // It is used by vtkOpenGLCamera.
   unsigned int GetBackRightBuffer();
-  
+
   // Description:
   // Return the OpenGL name of the front left buffer.
   // It is GL_FRONT_LEFT if GL is bound to the window-system-provided
@@ -130,7 +130,7 @@ public:
   // application-created framebuffer object (GPU-based offscreen rendering)
   // It is used by vtkOpenGLCamera.
   unsigned int GetFrontLeftBuffer();
-  
+
   // Description:
   // Return the OpenGL name of the front right buffer.
   // It is GL_FRONT_RIGHT if GL is bound to the window-system-provided
@@ -138,7 +138,7 @@ public:
   // application-created framebuffer object (GPU-based offscreen rendering)
   // It is used by vtkOpenGLCamera.
   unsigned int GetFrontRightBuffer();
-  
+
   // Description:
   // Return the OpenGL name of the back left buffer.
   // It is GL_BACK if GL is bound to the window-system-provided
@@ -146,7 +146,7 @@ public:
   // application-created framebuffer object (GPU-based offscreen rendering)
   // It is used by vtkOpenGLCamera.
   unsigned int GetBackBuffer();
-  
+
   // Description:
   // Return the OpenGL name of the front left buffer.
   // It is GL_FRONT if GL is bound to the window-system-provided
@@ -154,24 +154,26 @@ public:
   // application-created framebuffer object (GPU-based offscreen rendering)
   // It is used by vtkOpenGLCamera.
   unsigned int GetFrontBuffer();
-  
+
   // Description:
   // Update graphic error status, regardless of ReportGraphicErrors flag.
   // It means this method can be used in any context and is not restricted to
   // debug mode.
   virtual void CheckGraphicError();
-  
+
   // Description:
   // Return the last graphic error status. Initial value is false.
   virtual int HasGraphicError();
-  
+
   // Description:
   // Return a string matching the last graphic error status.
   virtual const char *GetLastGraphicErrorString();
 
   // Description:
   // Get the time when the OpenGL context was created.
+//BTX
   vtkGetMacro(ContextCreationTime, vtkTimeStamp);
+//ETX
 
   // Description:
   // Returns the extension manager. A new one will be created if one hasn't
@@ -182,14 +184,14 @@ public:
   // Returns an Hardware Support object. A new one will be created if one
   // hasn't already been set up.
   vtkOpenGLHardwareSupport* GetHardwareSupport();
- 
+
   //BTX
   // Description:
   // Returns its texture unit manager object. A new one will be created if one
   // hasn't already been set up.
   vtkTextureUnitManager *GetTextureUnitManager();
   //ETX
-  
+
 protected:
   vtkOpenGLRenderWindow();
   ~vtkOpenGLRenderWindow();
@@ -200,7 +202,7 @@ protected:
   int GetRGBAPixelData(int x,int y,int x2,int y2, int front, float* data);
   int GetRGBACharPixelData(int x,int y,int x2,int y2, int front,
                            unsigned char* data);
-  
+
   // Description:
   // Create an offScreen window based on OpenGL framebuffer extension.
   // Return if the creation was successful or not.
@@ -210,50 +212,50 @@ protected:
   // \post valid_result: (result==0 || result==1)
   //                     && (result implies OffScreenUseFrameBuffer)
   int CreateHardwareOffScreenWindow(int width, int height);
-  
+
   // Description:
   // Destroy an offscreen window based on OpenGL framebuffer extension.
   // \pre initialized: OffScreenUseFrameBuffer
   // \post destroyed: !OffScreenUseFrameBuffer
   void DestroyHardwareOffScreenWindow();
-  
+
   // Description:
   // Flag telling if a framebuffer-based offscreen is currently in use.
   int OffScreenUseFrameBuffer;
-  
+
   // Description:
   // Variables used by the framebuffer-based offscreen method.
   int NumberOfFrameBuffers;
   unsigned int TextureObjects[4]; // really GLuint
   unsigned int FrameBufferObject; // really GLuint
   unsigned int DepthRenderBufferObject; // really GLuint
-  
+
   // Description:
   // Create a not-off-screen window.
   virtual void CreateAWindow()=0;
-  
+
   // Description:
   // Destroy a not-off-screen window.
   virtual void DestroyWindow()=0;
-  
+
   // Description:
   // Set the texture unit manager.
   void SetTextureUnitManager(vtkTextureUnitManager *textureUnitManager);
-  
+
   unsigned int BackLeftBuffer;
   unsigned int BackRightBuffer;
   unsigned int FrontLeftBuffer;
   unsigned int FrontRightBuffer;
   unsigned int FrontBuffer;
   unsigned int BackBuffer;
-  
+
   // Actual type is GLenum. Last value returned by glGetError().
   unsigned int LastGraphicError;
- 
+
   vtkTimeStamp ContextCreationTime;
-  
+
   vtkTextureUnitManager *TextureUnitManager;
-  
+
 private:
   vtkOpenGLRenderWindow(const vtkOpenGLRenderWindow&);  // Not implemented.
   void operator=(const vtkOpenGLRenderWindow&);  // Not implemented.
