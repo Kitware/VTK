1ca582eb65d00dc55e5e24c99441443638368fb2
Jason Shepherd <jfsheph@sandia.gov>
2009-08-06 17:51:37 -0400

    ENH: Moving a filter for creating a graph from a VTK dataflow network (created by Jeff Baumes and Tim Shead) out of our sandbox and into the main repository so that others can play around with it.

---
+++ Views/vtkPipelineGraphSource.h
@@ -0,0 +1,65 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkPipelineGraphSource.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkPipelineGraphSource - a graph constructed from a VTK pipeline
+//
+// .SECTION Description
+
+#ifndef __vtkPipelineGraphSource_h
+#define __vtkPipelineGraphSource_h
+
+#include "vtkDirectedGraphAlgorithm.h"
+
+class vtkCollection;
+
+class VTK_VIEWS_EXPORT vtkPipelineGraphSource : public vtkDirectedGraphAlgorithm
+{
+public:
+  static vtkPipelineGraphSource* New();
+  vtkTypeRevisionMacro(vtkPipelineGraphSource,vtkDirectedGraphAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  void AddSink(vtkObject* object);
+  void RemoveSink(vtkObject* object);
+
+//BTX
+
+  // Description:
+  // Generates a GraphViz DOT file that describes the VTK pipeline
+  // terminating at the given sink.
+  static void PipelineToDot(vtkAlgorithm* sink, ostream& output);
+  // Description:
+  // Generates a GraphViz DOT file that describes the VTK pipeline
+  // terminating at the given sinks.
+  static void PipelineToDot(vtkCollection* sinks, ostream& output);
+
+protected:
+  vtkPipelineGraphSource();
+  ~vtkPipelineGraphSource();
+
+  int RequestData(
+    vtkInformation*, 
+    vtkInformationVector**, 
+    vtkInformationVector*);
+
+  vtkCollection* Sinks;
+
+private:
+  vtkPipelineGraphSource(const vtkPipelineGraphSource&); // Not implemented
+  void operator=(const vtkPipelineGraphSource&);   // Not implemented
+//ETX
+};
+
+#endif
+
