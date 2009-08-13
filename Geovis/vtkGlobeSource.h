85325d6c4f8f190ed5ef58b1971da9a0761239e7
Francois Bertel <francois.bertel@kitware.com>
2009-08-13 17:29:45 -0400

    STYLE:Added figure to build the equation from this specific spherical coordinate system to this specific cartesian coordinate system.

--- Geovis/vtkGlobeSource.h
+++ Geovis/vtkGlobeSource.h
@@ -27,7 +27,9 @@
 // to generate texture coordinates for any texture map.  This source is 
 // imperfect near the poles as implmented.  It should really reduce the
 // longitude resolution as the triangles become slivers.
-
+//
+// \image html vtkGlobeSourceSphericalToCartesianFigure.png
+// \image latex vtkGlobeSourceSphericalToCartesianFigure.eps
 #ifndef __vtkGlobeSource_h
 #define __vtkGlobeSource_h
 
