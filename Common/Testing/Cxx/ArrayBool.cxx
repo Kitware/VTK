50b2034b3815ebbf199a225bcdaca8c1526ca2cb
Timothy M. Shead <tshead@sandia.gov>
2009-11-13 14:02:14 -0500

    COMP: Fix some warnings building the ArrayBool test on VS91.

--- Common/Testing/Cxx/ArrayBool.cxx
+++ Common/Testing/Cxx/ArrayBool.cxx
@@ -45,25 +45,25 @@ int ArrayBool(int vtkNotUsed(argc), char *vtkNotUsed(argv)[])
     vtkSmartPointer<vtkDenseArray<char> > dense = vtkSmartPointer<vtkDenseArray<char> >::New();
     vtkDenseArray<char>& dense_ref = *dense;
     dense->Resize(2, 2);
-    dense->Fill(false);
+    dense->Fill(0);
 
-    test_expression(dense->GetValue(1, 1) == false);
-    dense->SetValue(1, 1, true);
-    test_expression(dense->GetValue(1, 1) == true);
+    test_expression(dense->GetValue(1, 1) == 0);
+    dense->SetValue(1, 1, 1);
+    test_expression(dense->GetValue(1, 1) == 1);
     
-    test_expression(dense->GetValue(0, 1) == false);
-    test_expression(dense_ref[vtkArrayCoordinates(0, 1)] == false);
-    dense_ref[vtkArrayCoordinates(0, 1)] = true;
-    test_expression(dense_ref[vtkArrayCoordinates(0, 1)] == true);
-    test_expression(dense->GetValue(0, 1) == true);
+    test_expression(dense->GetValue(0, 1) == 0);
+    test_expression(dense_ref[vtkArrayCoordinates(0, 1)] == 0);
+    dense_ref[vtkArrayCoordinates(0, 1)] = 1;
+    test_expression(dense_ref[vtkArrayCoordinates(0, 1)] == 1);
+    test_expression(dense->GetValue(0, 1) == 1);
 
     // Confirm that we can work with sparse arrays of bool values
     vtkSmartPointer<vtkSparseArray<char> > sparse = vtkSmartPointer<vtkSparseArray<char> >::New();
     sparse->Resize(2, 2);
 
-    test_expression(sparse->GetValue(1, 1) == false);
-    sparse->SetValue(1, 1, true);
-    test_expression(sparse->GetValue(1, 1) == true);
+    test_expression(sparse->GetValue(1, 1) == 0);
+    sparse->SetValue(1, 1, 1);
+    test_expression(sparse->GetValue(1, 1) == 1);
     
     return 0;
     }
