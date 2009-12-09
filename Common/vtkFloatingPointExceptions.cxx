8ca337baaeaf77990fbf6f4a49e8d583e56a7bf6
Marcus Hanwell <marcus.hanwell@kitware.com>
2009-12-09 16:09:01 -0500

    ENH: Enable floating point warnings on all Linux architectures.
    Spoke with Francois about enabling floating point warnings on all Linux
    architectures, as there are no continuous 64 bit Linux builds it will need to be
    tested on the Nightly builds. Works locally, but it is not ideal as it calls
    exit. Currently tests are segfaulting, or exiting on Windows. On x86_64 Linux
    machines no errors were raised before.

--- Common/vtkFloatingPointExceptions.cxx
+++ Common/vtkFloatingPointExceptions.cxx
@@ -15,8 +15,28 @@
 
 #include "vtkFloatingPointExceptions.h"
 
+#if defined(__linux__)
+#include <csignal>
+#include <fenv.h>
+#endif
+
 #ifdef _MSC_VER
 #include <float.h>
+#endif
+
+#if defined(__linux__)
+//-----------------------------------------------------------------------------
+// Signal handler for floating point exceptions in anonymous namespace
+namespace {
+
+void signal_handler(int signal)
+{
+  cerr << "Error: Floating point error detected.\n";
+  // This should possibly throw an exception rather than exit.
+  exit(1);
+}
+
+} // End anonymous namespace
 #endif
 
 //-----------------------------------------------------------------------------
@@ -28,9 +48,14 @@ void vtkFloatingPointExceptions::Enable()
   // enable floating point exceptions on MSVC
   _controlfp(_EM_DENORMAL | _EM_UNDERFLOW | _EM_INEXACT, _MCW_EM);
 #endif  //_MSC_VER
-#if defined(__linux__) && defined(__i386__)
+#if defined(__linux__)
+  // This should work on all platforms
+  feenableexcept(FE_DIVBYZERO | FE_INVALID);
+  // Set the signal handler
+  signal(SIGFPE, signal_handler);
   // This only works on linux x86
-  unsigned int fpucw= 0x1372;
-  __asm__ ("fldcw %0" : : "m" (fpucw));
+//  unsigned int fpucw= 0x1372;
+//  __asm__ ("fldcw %0" : : "m" (fpucw));
 #endif  //__linux__
 }
+
