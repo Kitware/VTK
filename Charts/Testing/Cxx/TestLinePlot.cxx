5fa888fb875f98ae1a791a49b01e5e7852944924
Marcus Hanwell <marcus.hanwell@kitware.com>
2009-12-10 16:37:58 -0500

    BUG: Increased the threshold for this test, small differences in aliased lines.

--- Charts/Testing/Cxx/TestLinePlot.cxx
+++ Charts/Testing/Cxx/TestLinePlot.cxx
@@ -85,6 +85,6 @@ int TestLinePlot( int argc, char * argv [] )
   //Finally render the scene and compare the image to a reference image
   view->Render();
   view->GetRenderWindow()->SetMultiSamples(0);
-  int retVal = vtkRegressionTestImage(view->GetRenderWindow());
+  int retVal = vtkRegressionTestImageThreshold(view->GetRenderWindow(), 25);
   return !retVal;
 }
