c5eab3a54c82001a447cd7faacf170d65d92695d
David Thompson <dcthomp@sandia.gov>
2009-12-11 10:55:36 -0500

    COMP: Do not remove old enum when renaming... just add the new name       so old code still compiles.

--- Infovis/vtkStatisticsAlgorithm.h
+++ Infovis/vtkStatisticsAlgorithm.h
@@ -77,6 +77,7 @@ public:
     {
     OUTPUT_DATA  = 0,       //!< Output 0 mirrors the input data, plus optional assessment columns
     OUTPUT_MODEL = 1,       //!< Output 1 contains any generated model
+    ASSESSMENT   = 2,       //!< This is an old, deprecated name for OUTPUT_TEST.
     OUTPUT_TEST  = 2        //!< Output 2 contains result of statistical test(s)
     };
 //ETX
