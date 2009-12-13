31a16d8d9c0ef4f6abebd914f33bcbcb70a741f2
Timothy M. Shead <tshead@sandia.gov>
2009-12-13 01:28:21 -0500

    ENH: Created vtkTableToArray, and added a Python example.

---
+++ Examples/Infovis/Python/adjacency_matrix_to_graph.py
@@ -0,0 +1,58 @@
+from vtk import *
+import os.path
+
+data_dir = "../../../../VTKData/Data/Infovis/"
+if not os.path.exists(data_dir):
+  data_dir = "../../../../../VTKData/Data/Infovis/"
+csv_file = data_dir + "matrix.csv"
+
+reader = vtkDelimitedTextReader()
+reader.SetFileName(csv_file)
+reader.SetHaveHeaders(True)
+
+array = vtkTableToArray()
+array.SetInputConnection(0, reader.GetOutputPort())
+array.AddColumn("A")
+array.AddColumn("B")
+array.AddColumn("C")
+array.AddColumn("D")
+array.AddColumn("E")
+array.AddColumn("F")
+
+edges = vtkAdjacencyMatrixToEdgeTable()
+edges.SetInputConnection(0, array.GetOutputPort())
+edges.SetSourceDimension(0)
+edges.SetMinimumThreshold(0.5)
+
+graph = vtkTableToGraph()
+graph.SetInputConnection(0, edges.GetOutputPort())
+graph.AddLinkVertex("row", "index", False)
+graph.AddLinkVertex("column", "index", False)
+graph.AddLinkEdge("row", "column")
+
+graph.Update()
+reader.GetOutput().Dump(10)
+edges.GetOutput().Dump(10)
+
+view = vtkGraphLayoutView()
+view.AddRepresentationFromInputConnection(graph.GetOutputPort())
+view.SetVertexLabelArrayName("label")
+view.SetVertexLabelVisibility(True)
+view.SetEdgeLabelArrayName("value")
+view.SetEdgeLabelVisibility(True)
+view.SetLayoutStrategyToSimple2D()
+
+theme = vtkViewTheme.CreateMellowTheme()
+theme.SetCellColor(.2,.2,.6)
+theme.SetLineWidth(5)
+theme.SetPointSize(10)
+view.ApplyViewTheme(theme)
+view.SetVertexLabelFontSize(20)
+theme.FastDelete()
+
+view.GetRenderWindow().SetSize(600, 600)
+view.ResetCamera()
+view.Render()
+view.GetInteractor().Start()
+
+
---
+++ Infovis/vtkTableToArray.h
@@ -0,0 +1,75 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkTableToArray.h
+  
+-------------------------------------------------------------------------
+  Copyright 2008 Sandia Corporation.
+  Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+  the U.S. Government retains certain rights in this software.
+-------------------------------------------------------------------------
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef __vtkTableToArray_h
+#define __vtkTableToArray_h
+
+#include "vtkArrayDataAlgorithm.h"
+
+// .NAME vtkTableToArray - converts a vtkTable to a matrix.
+//
+// .SECTION Description
+// Converts a vtkTable into a dense matrix.  Use AddColumn() to
+// designate one-to-many table columns that will become columns in the
+// output matrix.
+//
+// .SECTION Caveats
+// Only produces vtkDenseArray<double>, regardless of the input table column types.
+//
+// .SECTION Thanks
+// Developed by Timothy M. Shead (tshead@sandia.gov) at Sandia National Laboratories.
+
+class VTK_INFOVIS_EXPORT vtkTableToArray : public vtkArrayDataAlgorithm
+{
+public:
+  static vtkTableToArray* New();
+  vtkTypeRevisionMacro(vtkTableToArray, vtkArrayDataAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  // Description:
+  // Specify the set of input table columns that will be mapped to columns
+  // in the output matrix.
+  void ClearColumns();
+  void AddColumn(const char* name);
+
+//BTX
+protected:
+  vtkTableToArray();
+  ~vtkTableToArray();
+
+  int FillInputPortInformation(int, vtkInformation*);
+
+  int RequestData(
+    vtkInformation*, 
+    vtkInformationVector**, 
+    vtkInformationVector*);
+
+private:
+  vtkTableToArray(const vtkTableToArray&); // Not implemented
+  void operator=(const vtkTableToArray&);   // Not implemented
+
+  class implementation;
+  implementation* const Implementation;
+//ETX
+};
+
+#endif
+
