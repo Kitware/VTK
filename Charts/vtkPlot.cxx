1ccf02164613b3c8dbd3ff1cf79ffd8ce53ccec5
Marcus Hanwell <marcus.hanwell@kitware.com>
2009-12-17 09:42:01 -0500

    ENH: Default to a fully opaque black when constructed.

--- Charts/vtkPlot.cxx
+++ Charts/vtkPlot.cxx
@@ -23,7 +23,7 @@
 
 #include "vtkStdString.h"
 
-vtkCxxRevisionMacro(vtkPlot, "1.2");
+vtkCxxRevisionMacro(vtkPlot, "1.3");
 vtkCxxSetObjectMacro(vtkPlot, Selection, vtkIdTypeArray);
 
 //-----------------------------------------------------------------------------
@@ -32,7 +32,7 @@ vtkPlot::vtkPlot()
   this->Color[0] = 0;
   this->Color[1] = 0;
   this->Color[2] = 0;
-  this->Color[3] = 0;
+  this->Color[3] = 255;
   this->Width = 1.0;
   this->Data = vtkContextMapper2D::New();
   this->Selection = NULL;
