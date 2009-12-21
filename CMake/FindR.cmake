7835d860efad775101b83a45704c0bb20ea8b66c
Wes Turner <turner@crd.ge.com>
2009-12-21 16:55:03 -0500

    BUG: Fixing the R interface to correctly parse and handle R component paths.

--- CMake/FindR.cmake
+++ CMake/FindR.cmake
@@ -9,11 +9,22 @@
 # R_COMMAND     - Path to R command
 #
 
-FIND_PATH(R_INCLUDE_DIR "R.h" DOC "Path to file R.h")
-
-FIND_LIBRARY(R_LIBRARY R DOC "R library (example libR.a, libR.dylib, etc.).")
-
-FIND_PATH(R_UTILITY_LIBRARY_PATH Rblas DOC "Directory containing Rblas and Rlapack libraries.")
-
+SET(TEMP_CMAKE_FIND_APPBUNDLE ${CMAKE_FIND_APPBUNDLE})
+SET(CMAKE_FIND_APPBUNDLE "NEVER")
 FIND_PROGRAM(R_COMMAND R DOC "R executable.")
+IF (R_COMMAND)
+  EXECUTE_PROCESS(WORKING_DIRECTORY . COMMAND ${R_COMMAND} RHOME OUTPUT_VARIABLE R_BASE_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
+ENDIF (R_COMMAND)
+SET(CMAKE_FIND_APPBUNDLE ${TEMP_CMAKE_FIND_APPBUNDLE})
+
+FIND_PATH(R_INCLUDE_DIR R.h PATHS /usr/local/lib /usr/local/lib64 PATH_SUFFIXES R/include DOC "Path to file R.h")
+FIND_LIBRARY(R_LIBRARY_BASE R PATHS ${R_BASE_DIR} PATH_SUFFIXES /lib DOC "R library (example libR.a, libR.dylib, etc.).")
+FIND_LIBRARY(R_LIBRARY_BLAS Rblas PATHS ${R_BASE_DIR} PATH_SUFFIXES /lib DOC "Rblas library (example libRblas.a, libRblas.dylib, etc.).")
+FIND_LIBRARY(R_LIBRARY_LAPACK Rlapack PATHS ${R_BASE_DIR} PATH_SUFFIXES /lib  DOC "Rlapack library (example libRlapack.a, libRlapack.dylib, etc.).")
+FIND_LIBRARY(R_LIBRARY_READLINE readline DOC "(Optional) system readline library. Only required if the R libraries were build with readline support.")
+
+SET(R_LIBRARIES ${R_LIBRARY_BASE} ${R_LIBRARY_BLAS} ${R_LIBRARY_LAPACK} ${R_LIBRARY_BASE})
+IF (R_LIBRARY_READLINE)
+  SET(R_LIBRARIES ${R_LIBRARIES} ${R_LIBRARY_READLINE})
+ENDIF (R_LIBRARY_READLINE)
 
