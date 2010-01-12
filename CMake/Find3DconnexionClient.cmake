53374d6d4ff9921fc0aaa861186276475b2e64e0
Francois Bertel <francois.bertel@kitware.com>
2010-01-12 11:06:01 -0500

    STYLE:Fixed wrong license terms.

--- CMake/Find3DconnexionClient.cmake
+++ CMake/Find3DconnexionClient.cmake
@@ -1,23 +1,17 @@
-##=============================================================================
-##   This file is part of VTKEdge. See vtkedge.org for more information.
-##
-##   Copyright (c) 2008 Kitware, Inc.
-##
-##   VTKEdge may be used under the terms of the GNU General Public License 
-##   version 3 as published by the Free Software Foundation and appearing in 
-##   the file LICENSE.txt included in the top level directory of this source
-##   code distribution. Alternatively you may (at your option) use any later 
-##   version of the GNU General Public License if such license has been 
-##   publicly approved by Kitware, Inc. (or its successors, if any).
-##
-##   VTKEdge is distributed "AS IS" with NO WARRANTY OF ANY KIND, INCLUDING
-##   THE WARRANTIES OF DESIGN, MERCHANTABILITY, AND FITNESS FOR A PARTICULAR
-##   PURPOSE. See LICENSE.txt for additional details.
-##
-##   VTKEdge is available under alternative license terms. Please visit
-##   vtkedge.org or contact us at kitware@kitware.com for further information.
-##
-##=============================================================================
+# =============================================================================
+#
+#  Program:   Visualization Toolkit
+#  Module:    Find3DconnexionClient.cmake
+#
+#  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+#  All rights reserved.
+#  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+#
+#     This software is distributed WITHOUT ANY WARRANTY; without even
+#     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#     PURPOSE.  See the above copyright notice for more information.
+#
+# =============================================================================
 
 # -----------------------------------------------------------------------------
 # Find 3DconnexionClient framework (Mac OS X).
