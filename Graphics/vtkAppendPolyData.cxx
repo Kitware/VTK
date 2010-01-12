58c1b8b6b001b563493da2614014f5097dd14e7b
David Doria <daviddoria@gmail.com>
2010-01-12 15:01:16 -0500

    BUG: reverted to 1.104 because of a failing test on the Paraview dashboard

--- Graphics/vtkAppendPolyData.cxx
+++ Graphics/vtkAppendPolyData.cxx
@@ -25,7 +25,7 @@
 #include "vtkPolyData.h"
 #include "vtkStreamingDemandDrivenPipeline.h"
 
-vtkCxxRevisionMacro(vtkAppendPolyData, "1.105");
+vtkCxxRevisionMacro(vtkAppendPolyData, "1.106");
 vtkStandardNewMacro(vtkAppendPolyData);
 
 //----------------------------------------------------------------------------
@@ -236,7 +236,7 @@ int vtkAppendPolyData::RequestData(vtkInformation *vtkNotUsed(request),
       } // for a non NULL input
     } // for each input
 
-  if ( numPts == 0 )
+  if ( numPts < 1 || numCells < 1 )
     {
     vtkDebugMacro(<<"No data to append!");
     return 1;
