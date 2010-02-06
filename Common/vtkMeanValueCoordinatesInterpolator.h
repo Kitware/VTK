a0829cb39c914313821f646a3c2288e563de3fc1
Will Schroeder <will.schroeder@kitware.com>
2010-02-06 07:07:46 -0500

    Tweaks.

--- Common/vtkMeanValueCoordinatesInterpolator.h
+++ Common/vtkMeanValueCoordinatesInterpolator.h
@@ -15,11 +15,11 @@ PURPOSE.  See the above copyright notice for more information.
 // .NAME vtkMeanValueCoordinatesInterpolator - compute interpolation computes for closed triangular mesh
 // .SECTION Description
 // vtkMeanValueCoordinatesInterpolator computes interpolation weights for a
-// closed triangular mesh.  Once computed, the interpolation weights can be
-// used to interpolate data anywhere interior or exterior to the mesh. This
-// work is documented in the Siggraph paper by Tao Ju, Scot Schaefer and Joe
-// Warren from Rice University "Mean Value Coordinates for Closed Triangular
-// Meshes".
+// closed, manifold triangular mesh.  Once computed, the interpolation
+// weights can be used to interpolate data anywhere interior or exterior to
+// the mesh. This work is documented in the Siggraph paper by Tao Ju, Scot
+// Schaefer and Joe Warren from Rice University "Mean Value Coordinates for
+// Closed Triangular Meshes".
 //
 // In VTK this class was initially created to interpolate data across
 // polyhedral cells. In addition, the class can be used to interpolate
@@ -34,6 +34,15 @@ PURPOSE.  See the above copyright notice for more information.
 
 #include "vtkObject.h"
 
+class vtkPoints;
+class vtkIdList;
+class vtkDataArray;
+class vtkIdTypeArray;
+
+//Special internal class for iterating over data
+class vtkTriIterator;
+
+
 class VTK_COMMON_EXPORT vtkMeanValueCoordinatesInterpolator : public vtkObject
 {
 public:
@@ -44,13 +53,36 @@ public:
   void PrintSelf(ostream& os, vtkIndent indent);
 
   // Description:
-  // Method to generate interpolation weights from a list of triangles.
-  // In this method, the triangles are defined by a vtkPoints array plus 
-  // a vtkIdList, where the vtkId.
+  // Method to generate interpolation weights from a list of triangles.  In
+  // this version of the method, the triangles are defined by a vtkPoints
+  // array plus a vtkIdList, where the vtkIdList is organized such that three
+  // ids in order define a triangle.  Note that the data type (float,double)
+  // of the points will determine the type of the weights (this function only
+  // allows float and double point types).
+  static void ComputeInterpolationWeights(vtkPoints *pts, vtkIdList *tris,
+                                          vtkDataArray *weights);
+  
+  // Description:
+  // Method to generate interpolation weights from a list of triangles.  In
+  // this version of the method, the triangles are defined by a vtkPoints
+  // array plus a vtkIdTypeArray, where the vtkIdTypeArray is organized
+  // according to a vtkCellArray assuming that the cells are all triangles.
+  // Note that the data type (float,double) of the points will
+  // determine the type of the weights (this function only allows float
+  // and double point types).
+  static void ComputeInterpolationWeights(vtkPoints *pts, vtkIdTypeArray *tris,
+                                          vtkDataArray *weights);
+  
 
 protected:
   vtkMeanValueCoordinatesInterpolator();
   ~vtkMeanValueCoordinatesInterpolator();
+
+  // Description:
+  // Internal method that sets up the processing of the data.
+  static void ComputeInterpolationWeights(vtkPoints *pts, vtkIdType *tris,
+                                          vtkTriIterator& iter, vtkDataArray *weights);
+  
 
 private:
   vtkMeanValueCoordinatesInterpolator(const vtkMeanValueCoordinatesInterpolator&);  // Not implemented.
