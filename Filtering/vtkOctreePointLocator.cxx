315c150358d90de7007056e97b8b90087bbb53b0
Andy Bauer <andy.bauer@kitware.com>
2010-02-09 13:58:44 -0500

    STYLE: Fixing PrintSelf to properly print out all class variables.

--- Filtering/vtkOctreePointLocator.cxx
+++ Filtering/vtkOctreePointLocator.cxx
@@ -39,7 +39,7 @@
 #include <vtkstd/stack>
 #include <vtkstd/vector>
 
-vtkCxxRevisionMacro(vtkOctreePointLocator, "1.1");
+vtkCxxRevisionMacro(vtkOctreePointLocator, "1.2");
 vtkStandardNewMacro(vtkOctreePointLocator);
 
 // helper class for ordering the points in 
@@ -1299,19 +1299,16 @@ void vtkOctreePointLocator::PrintSelf(ostream& os, vtkIndent indent)
 { 
   this->Superclass::PrintSelf(os,indent);
 
-  os << indent << "NumberOfRegions: " << this->NumberOfLeafNodes << endl;
-
+  os << indent << "MaximumPointsPerRegion: " << 
+    this->MaximumPointsPerRegion << endl;
+  os << indent << "NumberOfLeafNodes: " << this->NumberOfLeafNodes << endl;
   os << indent << "Top: " << this->Top << endl;
   os << indent << "LeafNodeList: " << this->LeafNodeList << endl;
-
   os << indent << "LocatorPoints: " << this->LocatorPoints << endl;
   os << indent << "NumberOfLocatorPoints: " 
      << this->NumberOfLocatorPoints << endl;
-
   os << indent << "LocatorIds: " << this->LocatorIds << endl;
-
   os << indent << "FudgeFactor: " << this->FudgeFactor << endl;
   os << indent << "MaxWidth: " << this->MaxWidth << endl;
-
   os << indent << "CreateCubicOctants: " << this->CreateCubicOctants << endl;
 }
