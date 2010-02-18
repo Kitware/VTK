62cf3225f5854104b8821cb868582b7d9dc0fd64
Lisa Avila <lisa.avila@kitware.com>
2010-02-18 14:57:27 -0500

    BUG: fixing printself defects

--- VolumeRendering/vtkGPUVolumeRayCastMapper.cxx
+++ VolumeRendering/vtkGPUVolumeRayCastMapper.cxx
@@ -33,7 +33,7 @@
 #include "vtkGPUInfoList.h"
 #include "vtkGPUInfo.h"
 
-vtkCxxRevisionMacro(vtkGPUVolumeRayCastMapper, "1.1");
+vtkCxxRevisionMacro(vtkGPUVolumeRayCastMapper, "1.2");
 vtkInstantiatorNewMacro(vtkGPUVolumeRayCastMapper);
 vtkCxxSetObjectMacro(vtkGPUVolumeRayCastMapper, MaskInput, vtkImageData);
 
@@ -513,6 +513,17 @@ void vtkGPUVolumeRayCastMapper::PrintSelf(ostream& os, vtkIndent indent)
   this->Superclass::PrintSelf(os,indent);
   
   os << indent << "AutoAdjustSampleDistances: " << this->AutoAdjustSampleDistances << endl;
+  os << indent << "MinimumImageSampleDistance: " << this->MinimumImageSampleDistance << endl;
+  os << indent << "MaximumImageSampleDistance: " << this->MaximumImageSampleDistance << endl;
+  os << indent << "ImageSampleDistance: " << this->ImageSampleDistance << endl;
+  os << indent << "SampleDistance: " << this->SampleDistance << endl;
+  os << indent << "FinalColorWindow: " << this->FinalColorWindow << endl;
+  os << indent << "FinalColorLevel: " << this->FinalColorLevel << endl;
+  os << indent << "MaskInput: " << this->MaskInput << endl;
+  os << indent << "MaskBlendFactor: " << this->MaskBlendFactor << endl;
+  os << indent << "MaxMemoryInBytes: " << this->MaxMemoryInBytes << endl;
+  os << indent << "MaxMemoryFraction: " << this->MaxMemoryFraction << endl;
+  
 }
 
 // ----------------------------------------------------------------------------
