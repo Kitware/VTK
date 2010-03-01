827827ba7fb1bcfd1059b788943cf6cde7baa3c8
Ken Moreland <kmorel@sandia.gov>
2010-03-01 15:55:04 -0500

    ENH: If compiling with OSMesa ON, make compiling with X optional.  It is not uncommon to use OSMesa in place of X11.

--- CMake/vtkUseX.cmake
+++ CMake/vtkUseX.cmake
@@ -5,7 +5,7 @@
 # if set to ON.
 FIND_PACKAGE(X11)
 SET(VTK_USE_X_OPTIONAL 0)
-SET(VTK_USE_X_FORCE 0)
+SET(VTK_USE_X_DEFAULT 0)
 IF(X11_FOUND)
   IF(CYGWIN)
     SET(VTK_USE_X_OPTIONAL 1)
@@ -13,11 +13,15 @@ IF(X11_FOUND)
   IF(APPLE)
     SET(VTK_USE_X_OPTIONAL 1)
   ELSE(APPLE)
-    SET(VTK_USE_X_FORCE ${VTK_USE_RENDERING})
+    IF (VTK_OPENGL_HAS_OSMESA)
+      SET(VTK_USE_X_OPTIONAL 1)
+    ENDIF (VTK_OPENGL_HAS_OSMESA)
+    SET(VTK_USE_X_DEFAULT ${VTK_USE_RENDERING})
   ENDIF(APPLE)
 ENDIF(X11_FOUND)
 
 VTK_DEPENDENT_OPTION(VTK_USE_X
-                     "Build classes for the X11 window system." OFF
+                     "Build classes for the X11 window system."
+                     "${VTK_USE_X_DEFAULT}"
                      "X11_FOUND;VTK_USE_RENDERING;VTK_USE_X_OPTIONAL"
-                     "${VTK_USE_X_FORCE}")
+                     "${VTK_USE_X_DEFAULT}")
