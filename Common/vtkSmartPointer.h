5b524d533881965b1d4aa03fc23c357f427289c6
Brad King <brad.king@kitware.com>
2010-03-22 09:16:15 -0400

    ENH: Add vtkNew<> to create objects locally
    This simple template creates and owns one instance of its template
    argument for its lifetime.  Ownership can be shared with smart or raw
    pointers but cannot be taken away from vtkNew<> until destruction.  This
    approximates use of VTK objects as stack variables.

--- Common/vtkSmartPointer.h
+++ Common/vtkSmartPointer.h
@@ -22,6 +22,8 @@
 
 #include "vtkSmartPointerBase.h"
 
+template <class T> class vtkNew;
+
 template <class T>
 class vtkSmartPointer: public vtkSmartPointerBase
 {
@@ -43,6 +45,11 @@ public:
     vtkSmartPointerBase(CheckType(r.GetPointer())) {}
 
   // Description:
+  // Initialize smart pointer to given object.
+  template <class U>
+  vtkSmartPointer(const vtkNew<U>& r): vtkSmartPointerBase(CheckType(r)) {}
+
+  // Description:
   // Assign object to reference.  This removes any reference to an old
   // object.
   vtkSmartPointer& operator=(T* r)
@@ -58,6 +65,16 @@ public:
   vtkSmartPointer& operator=(const vtkSmartPointer<U>& r)
     {
     this->vtkSmartPointerBase::operator=(CheckType(r.GetPointer()));
+    return *this;
+    }
+
+  // Description:
+  // Assign object to reference.  This removes any reference to an old
+  // object.
+  template <class U>
+  vtkSmartPointer& operator=(const vtkNew<U>& r)
+    {
+    this->vtkSmartPointerBase::operator=(CheckType(r));
     return *this;
     }
 
