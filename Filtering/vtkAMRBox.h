c82cd08bf49ae7f80a0fbf682038a34757fce500
Ken Moreland <kmorel@sandia.gov>
2010-03-31 10:57:19 -0400

    STYLE: Add spacing between method declarations so that the documentation shows up properly in Doxygen.

--- Filtering/vtkAMRBox.h
+++ Filtering/vtkAMRBox.h
@@ -31,8 +31,8 @@
 #define __vtkAMRBox_h
 
 #include "vtkObject.h"
-#include "vtkType.h"//For utility functions.
-#include <vtkstd/vector>//For utility functions.
+#include "vtkType.h" //For utility functions.
+#include <vtkstd/vector> // STL Header
 
 class VTK_FILTERING_EXPORT vtkAMRBox
 {
@@ -40,72 +40,90 @@ public:
   // Description:
   // Construct the empty box.
   vtkAMRBox(int dim=3);
+
   // Description:
   // Construct a specific 3D box.
   vtkAMRBox(
       int ilo,int jlo,int klo,
       int ihi,int jhi,int khi);
+
   // Description:
   // Construct a specific 2D box.
   vtkAMRBox(
       int ilo,int jlo,
       int ihi,int jhi);
+
   // Description:
   // Construct a specific box. (ilo,jlo,klo,)(ihi,jhi,khi)
   vtkAMRBox(int dim, const int lo[3], const int hi[3]);
   vtkAMRBox(const int lo[3], const int hi[3]);
+
   // Description:
   // Construct a specific box. (ilo,ihi, jlo,jhi, klo,khi)
   vtkAMRBox(int dim, const int dims[6]);
   vtkAMRBox(const int dims[6]);
+
   // Description:
   // Copy construct this box from another.
   vtkAMRBox(const vtkAMRBox &other);
+
   // Description:
   // Copy the other box to this box.
   vtkAMRBox &operator=(const vtkAMRBox &other);
+
   // Description:
   // Set the box to null;
   void Invalidate();
+
   // Description:
   // Get/Set the spatial dimension of the box. Only 2 and 3 
   // are valid.
   int GetDimensionality() const { return this->Dimension; }
   void SetDimensionality(int dim);
+
   // Description:
   // Set the dimensions of the box. ilo,jlo,klo,ihi,jhi,khi
   void SetDimensions(
       int ilo, int jlo, int klo,
       int ihi, int jhi, int khi);
+
   // Description:
   // Set the dimensions of the box. (ilo,jlo,klo),(ihi,jhi,khi)
   void SetDimensions(const int lo[3], const int hi[3]);
+
   // Description:
   // Set the dimensions of the box. (ilo,ihi,jlo,jhi,klo,khi)
   void SetDimensions(const int dims[6]);
+
   // Description:
   // Get the dimensions of this box. (ilo,jlo,jhi),(ihi,jhi,khi)
   void GetDimensions(int lo[3], int hi[3]) const;
+
   // Description:
   // Get the dimensions of this box. (ilo,ihi, jlo,jhi, klo,khi)
   void GetDimensions(int dims[6]) const;
+
   // Description:
   // Get the low corner index.
   void GetLoCorner(int lo[3]) const;
   const int *GetLoCorner() const { return this->LoCorner; }
+
   // Description:
   // Copy the high corner index.
   void GetHiCorner(int hi[3]) const;
   const int *GetHiCorner() const { return this->HiCorner; }
+
   // Description:
   // Gets the number of cells enclosed by the box.
   void GetNumberOfCells(int ext[3]) const;
   vtkIdType GetNumberOfCells() const;
+
   // Description:
   // Gets the number of nodes required to construct
   // a physical representation of the box.
   void GetNumberOfNodes(int ext[3]) const;
   vtkIdType GetNumberOfNodes() const;
+
   // Description:
   // Set/Get grid spacing. Refine/coarsen operations update
   // the grid spacing.
@@ -114,6 +132,7 @@ public:
   void SetGridSpacing(double dx);
   void SetGridSpacing(const double dX[3]);
   void SetGridSpacing(double dx, double dy, double dz);
+
   // Description:
   // Set/Get world space origin of the data set, The origin
   // is the location of the low corner cell's low corner node
@@ -123,6 +142,7 @@ public:
   void GetDataSetOrigin(double X0[3]) const;
   void SetDataSetOrigin(const double X0[3]);
   void SetDataSetOrigin(double x0, double y0, double z0);
+
   // Description:
   // Get the world space origin of this box. The origin is the
   // location of the lower corner cell's lower corner node,
@@ -130,39 +150,50 @@ public:
   // that use GetDataSetOrigin(). The value is computed each time,
   // so that operations on the box are are appropriately reflected.
   void GetBoxOrigin(double x0[3]) const;
+
   // Description:
   // Grows the box in all directions.
   void Grow(int byN);
+
   // Description:
   // Shrinks the box in all directions.
   void Shrink(int byN);
+
   // Description:
   // Shifts the box in index space.
   void Shift(int i, int j);
   void Shift(int i, int j, int k);
   void Shift(const int I[3]);
+
   // Description:
   // Test if this box is empty/valid.
   bool Empty() const;
+
   // Description:
   // Test if this box has the same dimensions as another.
   bool operator==(const vtkAMRBox &other);
+
   // Description:
   // Intersect this box with another box in place.
   void operator&=(const vtkAMRBox &rhs);
+
   // Description:
   // Test to see if a given cell index is inside this box.
   bool Contains(int i,int j,int k) const;
   bool Contains(const int I[3]) const;
+
   // Description:
   // Test to see if a given box is inside this box.
   bool Contains(const vtkAMRBox &other) const;
+
   // Description:
   // Refine the box.
   void Refine(int r);
+
   // Description:
   // Coarsen the box.
   void Coarsen(int r);
+
   // Description:
   // Send the box to a stream. "(ilo,jlo,jhi),(ihi,jhi,khi)"
   ostream &Print(ostream &os) const;
@@ -184,6 +215,7 @@ public:
   // Get/SetXCorner, and the many constructors above.
   int LoCorner[3]; // lo corner cell id.
   int HiCorner[3]; // hi corner cell id.
+
 private:
   int Dimension;         // 2 or 3.
   double X0[3];          // Dataset origin (not box origin). low corner cell's, low corner node.
