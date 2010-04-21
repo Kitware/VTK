cd89938702bec01326b63ec5cf9ca79b67eae6bc
Kenneth Moreland <kmorel@sandia.gov>
2010-04-21 15:56:41 -0600

    ENH: vtkNetCDFCFReader was not recognizing time units with "after".

--- IO/vtkNetCDFCFReader.cxx
+++ IO/vtkNetCDFCFReader.cxx
@@ -166,7 +166,8 @@ int vtkNetCDFCFReader::vtkDimensionInfo::LoadMetaData(int ncFD)
       // correspond to strings formatted with the Unidata udunits package.  I'm
       // not sure if these checks are complete, but they matches all of the
       // examples I have seen.
-      if (units.find(" since ") != vtkStdString::npos)
+      if (   (units.find(" since ") != vtkStdString::npos)
+          || (units.find(" after ") != vtkStdString::npos) )
         {
         this->Units = TIME_UNITS;
         }
