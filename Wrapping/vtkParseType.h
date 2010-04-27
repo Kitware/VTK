4a7700ba1fb7f7581f0e386ebf5d64ee7346706e
David Gobbi <david.gobbi@gmail.com>
2010-04-27 13:10:46 -0600

    DOC: remove RCS

--- Wrapping/vtkParseType.h
+++ Wrapping/vtkParseType.h
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    $RCSfile: vtkParseUtils.h,v $
+  Module:    vtkParseUtils.h
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
