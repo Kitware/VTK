1216a2f1329dd4d8c91ada87efcf69007b3e4b86
Philippe Pébay <pppebay@sandia.gov>
2010-04-30 11:47:07 -0700

    ENH: behaviour with empty (but not NULL) input
    Do not bail out early from Assess() when input data has no rows but is not NULL.

--- Infovis/vtkUnivariateStatisticsAlgorithm.cxx
+++ Infovis/vtkUnivariateStatisticsAlgorithm.cxx
@@ -72,13 +72,7 @@ void vtkUnivariateStatisticsAlgorithm::Assess( vtkTable* inData,
                                                vtkMultiBlockDataSet* inMeta,
                                                vtkTable* outData )
 {
-  if ( ! inData || inData->GetNumberOfColumns() <= 0 )
-    {
-    return;
-    }
-
-  vtkIdType nRowData = inData->GetNumberOfRows();
-  if ( nRowData <= 0 )
+  if ( ! inData )
     {
     return;
     }
@@ -110,6 +104,7 @@ void vtkUnivariateStatisticsAlgorithm::Assess( vtkTable* inData,
     // Store names to be able to use SetValueByName, and create the outData columns
     int nv = this->AssessNames->GetNumberOfValues();
     vtkStdString* names = new vtkStdString[nv];
+    vtkIdType nRowData = inData->GetNumberOfRows();
     for ( int v = 0; v < nv; ++ v )
       {
       vtksys_ios::ostringstream assessColName;
