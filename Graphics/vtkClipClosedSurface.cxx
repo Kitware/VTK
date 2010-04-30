a16fa979d51c428aeec913d1ead058bf3637e933
Zhanping Liu <zhanping.liu@kitware.com>
2010-04-30 17:05:37 -0400

    change the style.

--- Graphics/vtkClipClosedSurface.cxx
+++ Graphics/vtkClipClosedSurface.cxx
@@ -370,7 +370,7 @@ int vtkClipClosedSurface::RequestData(
     {
     // Make the scalars
     lineScalars = vtkUnsignedCharArray::New();
-    lineScalars->SetName( "CellOriginColor" );
+    lineScalars->SetName("CellOriginColor");
     lineScalars->SetNumberOfComponents(3);
 
     vtkDataArray *tryInputScalars = input->GetCellData()->GetScalars();
@@ -422,7 +422,7 @@ int vtkClipClosedSurface::RequestData(
     if (lineScalars)
       {
       polyScalars = vtkUnsignedCharArray::New();
-      polyScalars->SetName( "CellOriginColor" );
+      polyScalars->SetName("CellOriginColor");
       polyScalars->SetNumberOfComponents(3);
       }
 
