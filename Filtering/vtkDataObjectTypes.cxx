532a980bfdd9ebdbb66ed6a64c799448fe9b5c70
Julien Tierny <julien@marge.sci.utah.edu>
2010-05-12 14:47:03 -0600

    Subsequent edits on data object types

--- Filtering/vtkDataObjectTypes.cxx
+++ Filtering/vtkDataObjectTypes.cxx
@@ -35,6 +35,7 @@ PURPOSE.  See the above copyright notice for more information.
 #include  "vtkPointSet.h"
 #include  "vtkPolyData.h"
 #include  "vtkRectilinearGrid.h"
+#include  "vtkReebGraph.h"
 #include  "vtkSelection.h"
 #include  "vtkStructuredGrid.h"
 #include  "vtkStructuredPoints.h"
@@ -83,6 +84,7 @@ static const char* vtkDataObjectTypesStrings[] = {
   "vtkMultiPieceDataSet",
   "vtkDirectedAcyclicGraph",
   "vtkArrayData",
+  "vtkReebGraph",
   NULL
 };
 
@@ -237,6 +239,10 @@ vtkDataObject* vtkDataObjectTypes::NewDataObject(const char* type)
   else if(strcmp(type, "vtkAnnotationLayers") == 0)
     {
     return vtkAnnotationLayers::New();
+    }
+  else if(strcmp(type, "vtkReebGraph") == 0)
+    {
+      return vtkReebGraph::New();
     }
 #ifdef VTK_USE_N_WAY_ARRAYS
   else if(strcmp(type, "vtkArrayData") == 0)
