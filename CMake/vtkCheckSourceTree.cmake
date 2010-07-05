3e06a5377d61d2fb7be72ca3dc37e5d6b822e37b
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2010-07-05 10:34:41 -0400

    ENH: Added check for local git hooks.
    Added a check for locally installed Git hooks, as discussed on the
    developer mailing list,
    http://www.vtk.org/pipermail/vtk-developers/2010-June/008016.html
    
    The check is self documenting, and can be disabled by setting
    VTK_IGNORE_HOOKS to ON in the cache. If the build is being driven by
    CTest the checks are disabled, and for tarballs that have been archived
    (equivalent of cvs export) the checks will also not run.

---
+++ CMake/vtkCheckSourceTree.cmake
@@ -0,0 +1,25 @@
+# Emit a fatal error and inform the user if they have not enabled hooks.
+option(VTK_IGNORE_HOOKS "Should the VTK hooks check be ignored?" OFF)
+if(DEFINED ENV{DASHBOARD_TEST_FROM_CTEST})
+  set(VTK_FROM_CTEST TRUE)
+endif()
+if(NOT VTK_IGNORE_HOOKS AND NOT VTK_FROM_CTEST AND
+    EXISTS "${VTK_SOURCE_DIR}/.git/config")
+  if(NOT EXISTS "${VTK_SOURCE_DIR}/.git/hooks/.git/config")
+    message(FATAL_ERROR "
+ Please initialize your local Git hooks, paste the following into a shell:
+
+ $ cd ${VTK_SOURCE_DIR}/.git/hooks
+ $ git init
+ $ git pull .. remotes/origin/hooks
+ $ cd ../..
+
+ See http://www.vtk.org/Wiki/VTK/Git#Hooks for more details.
+
+ If you wish to ignore this check for a build set the CMake cache variable
+ VTK_IGNORE_HOOKS to ON. To ignore this check in all builds either archive
+ your clone, or create the file ${VTK_SOURCE_DIR}/.git/hooks/.git/config
+ in your source tree.
+ ")
+  endif()
+endif()
