eb4a548e9d07b409e9cea1e9f45b7358ef97220b
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2010-07-06 11:42:38 -0400

    STYLE: Cleaned up format of message, thanks to Brad King.

--- CMake/vtkCheckSourceTree.cmake
+++ CMake/vtkCheckSourceTree.cmake
@@ -6,20 +6,13 @@ endif()
 if(NOT VTK_IGNORE_HOOKS AND NOT VTK_FROM_CTEST AND
     EXISTS "${VTK_SOURCE_DIR}/.git/config")
   if(NOT EXISTS "${VTK_SOURCE_DIR}/.git/hooks/.git/config")
-    message(FATAL_ERROR "
- Please initialize your local Git hooks, paste the following into a shell:
-
- cd ${VTK_SOURCE_DIR}/.git/hooks
+    message(FATAL_ERROR
+"Please initialize your local Git hooks, paste the following into a shell:
+ cd \"${VTK_SOURCE_DIR}/.git/hooks\"
  git init
  git pull .. remotes/origin/hooks
  cd ../..
-
- See http://www.vtk.org/Wiki/VTK/Git#Hooks for more details.
-
- If you wish to ignore this check for a build set the CMake cache variable
- VTK_IGNORE_HOOKS to ON. To ignore this check in all builds either archive
- your clone, or create the file ${VTK_SOURCE_DIR}/.git/hooks/.git/config
- in your source tree.
- ")
+See http://www.vtk.org/Wiki/VTK/Git#Hooks for more details.
+If you wish to ignore this check for a build set the CMake cache variable VTK_IGNORE_HOOKS to ON. To ignore this check in all builds either archive your clone, or create the file\n ${VTK_SOURCE_DIR}/.git/hooks/.git/config\nin your source tree.")
   endif()
 endif()
