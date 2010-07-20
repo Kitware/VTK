a7266ea2db6d2637d69238a093e5630724b0b85d
David Partyka <dave.partyka@kitware.com>
2010-07-20 09:32:35 -0400

    ENH: Remove unused warning file.

--- CMake/InstallDisabled.cmake
+++
@@ -1,3 +0,0 @@
-MESSAGE(FATAL_ERROR
-  "ERROR: VTK installation is disabled because VTK_USE_RPATH is ON and you are using a CMake version earlier than 2.4.  It is not safe to install binaries that have a runtime path pointing to the build tree.  In order to install safely you need to delete the libraries from your build tree, set VTK_USE_RPATH to OFF, and rebuild.  See comments in VTK/CMake/vtkSelectSharedLibraries.cmake for more information."
-)
