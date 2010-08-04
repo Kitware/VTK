3b7aef9223cfa567de4271b9a71ab271c3ab74b3
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2010-08-04 13:21:00 -0400

    ENH: Draw scatter plot symbols in the legend.

--- Charts/vtkPlotLine.cxx
+++ Charts/vtkPlotLine.cxx
@@ -58,6 +58,7 @@ bool vtkPlotLine::PaintLegend(vtkContext2D *painter, float rect[4], int )
   painter->ApplyPen(this->Pen);
   painter->DrawLine(rect[0], rect[1]+0.5*rect[3],
                     rect[0]+rect[2], rect[1]+0.5*rect[3]);
+  this->Superclass::PaintLegend(painter, rect, 0);
   return true;
 }
 
