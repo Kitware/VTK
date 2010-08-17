8313f706a980adad616dc3264155f56d69f94993
Robert Maynard <robert.maynard@kitware.com>
2010-08-17 13:04:07 -0400

    COMP: Fix Warnings with incorrect cast.

--- IO/vtkTecplotReader.cxx
+++ IO/vtkTecplotReader.cxx
@@ -1603,7 +1603,7 @@ void vtkTecplotReader::ReadFile( vtkMultiBlockDataSet * multZone )
             do
               {
               //remove left square bracket, if it exists
-              unsigned int brack_pos = var_format_type.find("[");
+              size_t brack_pos = var_format_type.find("[");
               if ( brack_pos != vtkstd::string::npos )
                 var_format_type.erase(brack_pos, brack_pos+1);
 
