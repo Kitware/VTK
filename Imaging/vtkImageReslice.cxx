68ecdd42bb8cd89d44f5f7a8a5c75628c172fe64
David Gobbi <david.gobbi@gmail.com>
2010-09-21 22:05:43 -0600

    ENH: Use offset cast as a faster floor() on x86_64 and ppc64.

--- Imaging/vtkImageReslice.cxx
+++ Imaging/vtkImageReslice.cxx
@@ -42,47 +42,89 @@ vtkCxxSetObjectMacro(vtkImageReslice,ResliceAxes,vtkMatrix4x4);
 vtkCxxSetObjectMacro(vtkImageReslice,ResliceTransform,vtkAbstractTransform);
 
 //--------------------------------------------------------------------------
-// The 'floor' function on x86 and mips is many times slower than these
-// and is used a lot in this code, optimize for different CPU architectures
+// The 'floor' function is slow, so we want to do an integer
+// cast but keep the "floor" behavior of always rounding down,
+// rather than truncating, i.e. we want -0.6 to become -1.
+// The easiest way to do this is to add a large value in
+// order to make the value "unsigned", then cast to int, and
+// then subtract off the large value.
+
+// On the old i386 architecture, even a cast to int is very
+// expensive because it requires changing the rounding mode
+// on the FPU.  So we use a bit-trick similar to the one
+// described at http://www.stereopsis.com/FPU.html
+
+#if defined ia64 || defined __ia64__ || defined _M_IA64
+#define VTK_RESLICE_64BIT_FLOOR
+#elif defined __ppc64__ || defined __x86_64__ || defined _M_X64
+#define VTK_RESLICE_64BIT_FLOOR
+#elif defined __ppc__ || defined sparc || defined mips
+#define VTK_RESLICE_32BIT_FLOOR
+#elif defined i386 || defined _M_IX86
+#define VTK_RESLICE_I386_FLOOR
+#endif
+
+
+// We add a tolerance of 2^-17 (around 7.6e-6) so that float
+// values that are just less than the closest integer are
+// rounded up.  This adds robustness against rounding errors.
+
+#define VTK_RESLICE_FLOOR_TOL 7.62939453125e-06
+
+
 template<class F>
 inline int vtkResliceFloor(double x, F &f)
 {
-#if defined mips || defined sparc || defined __ppc__
-  x += 2147483648.0;
+#if defined VTK_RESLICE_64BIT_FLOOR
+  x += (103079215104.0 + VTK_RESLICE_FLOOR_TOL);
+#ifdef VTK_TYPE_USE___INT64
+  __int64 i = static_cast<__int64>(x);
+  f = x - i;
+  return static_cast<int>(i - 103079215104i64);
+#else
+  long long i = static_cast<long long>(x);
+  f = x - i;
+  return static_cast<int>(i - 103079215104LL);
+#endif
+#elif defined VTK_RESLICE_32BIT_FLOOR
+  x += (2147483648.0 + VTK_RESLICE_FLOOR_TOL);
   unsigned int i = static_cast<unsigned int>(x);
   f = x - i;
   return static_cast<int>(i - 2147483648U);
-#elif defined i386 || defined _M_IX86
+#elif defined VTK_RESLICE_I386_FLOOR
   union { double d; unsigned short s[4]; unsigned int i[2]; } dual;
   dual.d = x + 103079215104.0;  // (2**(52-16))*1.5
   f = dual.s[0]*0.0000152587890625; // 2**(-16)
   return static_cast<int>((dual.i[1]<<16)|((dual.i[0])>>16));
-#elif defined ia64 || defined __ia64__ || defined IA64
-  x += 103079215104.0;
-  long long i = static_cast<long long>(x);
-  f = x - i;
-  return static_cast<int>(i - 103079215104LL);
 #else
-  double y = floor(x);
+  double y = floor(x + VTK_RESLICE_FLOOR_TOL);
   f = x - y;
   return static_cast<int>(y);
 #endif
 }
 
+
 inline int vtkResliceRound(double x)
 {
-#if defined mips || defined sparc || defined __ppc__
-  return static_cast<int>(static_cast<unsigned int>(x + 2147483648.5) - 2147483648U);
-#elif defined i386 || defined _M_IX86
+#if defined VTK_RESLICE_64BIT_FLOOR
+  x += (103079215104.5 + VTK_RESLICE_FLOOR_TOL);
+#ifdef VTK_TYPE_USE___INT64
+  __int64 i = static_cast<__int64>(x);
+  return static_cast<int>(i - 103079215104i64);
+#else
+  long long i = static_cast<long long>(x);
+  return static_cast<int>(i - 103079215104LL);
+#endif
+#elif defined VTK_RESLICE_32BIT_FLOOR
+  x += (2147483648.5 + VTK_RESLICE_FLOOR_TOL);
+  unsigned int i = static_cast<unsigned int>(x);
+  return static_cast<int>(i - 2147483648U);
+#elif defined VTK_RESLICE_I386_FLOOR
   union { double d; unsigned int i[2]; } dual;
   dual.d = x + 103079215104.5;  // (2**(52-16))*1.5
   return static_cast<int>((dual.i[1]<<16)|((dual.i[0])>>16));
-#elif defined ia64 || defined __ia64__ || defined IA64
-  x += 103079215104.5;
-  long long i = static_cast<long long>(x);
-  return static_cast<int>(i - 103079215104LL);
 #else
-  return static_cast<int>(floor(x+0.5));
+  return static_cast<int>(floor(x + (0.5 + VTK_RESLICE_FLOOR_TOL)));
 #endif
 }
 
