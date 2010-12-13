5ddaa0e64f3d3f484b3d00df11ebc0c05f41e043
Brad King <brad.king@kitware.com>
2010-12-13 13:53:24 -0500

    hdf5: Disable hdf5 library name encoding

--- Resources/HDF5Macros.cmake
+++ Resources/HDF5Macros.cmake
@@ -78,13 +78,7 @@ MACRO (H5_SET_LIB_OPTIONS libtarget libname libtype)
     ENDIF (WIN32 AND NOT MINGW)
   ENDIF (${libtype} MATCHES "SHARED")
   
-  SET_TARGET_PROPERTIES (${libtarget}
-      PROPERTIES
-      DEBUG_OUTPUT_NAME          ${LIB_DEBUG_NAME}
-      RELEASE_OUTPUT_NAME        ${LIB_RELEASE_NAME}
-      MINSIZEREL_OUTPUT_NAME     ${LIB_RELEASE_NAME}
-      RELWITHDEBINFO_OUTPUT_NAME ${LIB_RELEASE_NAME}
-  )
+  # library names not mangled in this reduced distribution
   
   #----- Use MSVC Naming conventions for Shared Libraries
   IF (MINGW AND BUILD_SHARED_LIBS)
