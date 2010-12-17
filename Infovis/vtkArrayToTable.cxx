7d8a86ad60c6cb7f6ce044be4ce3300264a40922
Warren L. Davis IV <wldavis@sandia.gov>
2010-12-17 10:20:43 -0700

    Adding support for long unsigned int to vtkArrayToTable

--- Infovis/vtkArrayToTable.cxx
+++ Infovis/vtkArrayToTable.cxx
@@ -25,6 +25,7 @@
 #include "vtkDoubleArray.h"
 #include "vtkIdTypeArray.h"
 #include "vtkUnsignedIntArray.h"
+#include "vtkUnsignedLongArray.h"
 #include "vtkTypeUInt64Array.h"
 #include "vtkInformation.h"
 #include "vtkInformationVector.h"
@@ -175,6 +176,7 @@ int vtkArrayToTable::RequestData(
     if(ConvertVector<double, vtkDoubleArray>(input_array, output_table)) return 1;
     if(ConvertVector<vtkTypeUInt32, vtkUnsignedIntArray>(input_array, output_table)) return 1;
     if(ConvertVector<vtkTypeUInt64, vtkTypeUInt64Array>(input_array, output_table)) return 1;
+    if(ConvertVector<long unsigned int, vtkUnsignedLongArray>(input_array, output_table)) return 1;
     if(ConvertVector<vtkIdType, vtkIdTypeArray>(input_array, output_table)) return 1;
     if(ConvertVector<vtkStdString, vtkStringArray>(input_array, output_table)) return 1;
     if(ConvertVector<vtkUnicodeString, vtkUnicodeStringArray>(input_array, output_table)) return 1;
@@ -182,6 +184,7 @@ int vtkArrayToTable::RequestData(
     if(ConvertMatrix<double, vtkDoubleArray>(input_array, output_table)) return 1;
     if(ConvertMatrix<vtkTypeUInt32, vtkUnsignedIntArray>(input_array, output_table)) return 1;
     if(ConvertMatrix<vtkTypeUInt64, vtkTypeUInt64Array>(input_array, output_table)) return 1;
+    if(ConvertMatrix<long unsigned int, vtkUnsignedLongArray>(input_array, output_table)) return 1;
     if(ConvertMatrix<vtkIdType, vtkIdTypeArray>(input_array, output_table)) return 1;
     if(ConvertMatrix<vtkStdString, vtkStringArray>(input_array, output_table)) return 1;
     if(ConvertMatrix<vtkUnicodeString, vtkUnicodeStringArray>(input_array, output_table)) return 1;
