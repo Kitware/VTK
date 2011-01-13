5d4166083669297e2930ad6d23150e17edcb4eac
Brad King <brad.king@kitware.com>
2011-01-13 15:33:33 -0500

    Update copyright notices for year 2011

--- hooks-chain.bash
+++ hooks-chain.bash
@@ -1,5 +1,5 @@
 #=============================================================================
-# Copyright 2010 Kitware, Inc.
+# Copyright 2010-2011 Kitware, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
