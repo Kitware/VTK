39cdd63f8a9bd7cd5de028b18889d7581a89b12e
Andrew Bauer <andy.bauer@kitware.com>
2011-01-27 11:22:32 -0500

    Working in parallel with extract surface and XMLPPolyDataWriter.

--- Parallel/Testing/Cxx/TestTemporalParallelWriter.cxx
+++ Parallel/Testing/Cxx/TestTemporalParallelWriter.cxx
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <mpi.h>
 
+#include <vtkDataSetSurfaceFilter.h>
 #include <vtkMPIController.h>
 #include <vtkNetCDFPOPReader.h>
-#include <vtkXMLPRectilinearGridWriter.h>
+#include <vtkXMLPPolyDataWriter.h>
 
 // read in a set of NetCDFPOP files in parallel where
 // we partition both in space and time and write them out
@@ -12,22 +13,26 @@
 // this gets called by all processes
 void MyMain(vtkMultiProcessController* controller, void* vtkNotUsed(arg) )
 {
-  
   vtkGenericWarningMacro(<< getpid() << " is rank " << controller->GetLocalProcessId() << " of " << controller->GetNumberOfProcesses());
   vtkNetCDFPOPReader* reader = vtkNetCDFPOPReader::New();
   reader->SetStride(4, 4, 4);
   reader->SetFileName("/home/acbauer/DATA/UVCDAT/t.x1_SAMOC_greenland.008910.bin.nc");
 
-  vtkXMLPRectilinearGridWriter* writer = vtkXMLPRectilinearGridWriter::New();
-  writer->SetInputConnection(reader->GetOutputPort());
-  writer->SetFileName("/media/ssddrive/BUILDS/VTK/temporal-parallelism/test.pvtr");
+  vtkDataSetSurfaceFilter* surfaceFilter = vtkDataSetSurfaceFilter::New();
+  surfaceFilter->SetInputConnection(reader->GetOutputPort());
+  surfaceFilter->UseStripsOff();
+
+  vtkXMLPPolyDataWriter* writer = vtkXMLPPolyDataWriter::New();
+  writer->SetInputConnection(surfaceFilter->GetOutputPort());
+  writer->SetFileName("/media/ssddrive/BUILDS/VTK/temporal-parallelism/test.pvtp");
   writer->SetNumberOfPieces(controller->GetNumberOfProcesses());
   writer->SetStartPiece(controller->GetLocalProcessId());
   writer->SetEndPiece(controller->GetLocalProcessId());
   writer->Write();
+  writer->Delete();
+  surfaceFilter->Delete();
 
   reader->Delete();
-  writer->Delete();
   cerr << "finished\n";
 }
 
