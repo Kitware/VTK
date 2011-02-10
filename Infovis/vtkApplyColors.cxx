c83503cec36bd756e82e4dd983cb2c3f1a739705
Jeffrey Baumes <jeff.baumes@kitware.com>
2011-02-10 15:22:58 -0500

    Fixing bug causing vtkApplyColors to crash
    Cell and point lookup tables may not be initialized, check
    for that case.
    
    Change-Id: Ife740060130b289df20110f8f8777c71caff26d4

--- Infovis/vtkApplyColors.cxx
+++ Infovis/vtkApplyColors.cxx
@@ -454,11 +454,11 @@ void vtkApplyColors::ProcessColorArray(
 long unsigned int vtkApplyColors::GetMTime()
 {
   long unsigned int mtime = Superclass::GetMTime();
-  if (this->PointLookupTable->GetMTime() > mtime)
+  if (this->PointLookupTable && this->PointLookupTable->GetMTime() > mtime)
     {
     mtime = this->PointLookupTable->GetMTime();
     }
-  if (this->CellLookupTable->GetMTime() > mtime)
+  if (this->CellLookupTable && this->CellLookupTable->GetMTime() > mtime)
     {
     mtime = this->CellLookupTable->GetMTime();
     }
