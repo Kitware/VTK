56d160c47f84bb7fa88b2e8d924e5cbf541e9cf7
George Zagaris <george.zagaris@kitware.com>
2011-02-25 09:36:39 -0500

    BUGFIX: Fix GetNumberOfNodes & GetNumberOfCells
    Fixed GetNumberOfNodes() and GetNumberOfCelss() methods
    given the cell based layout of the vtkAMRBox.

--- Filtering/vtkAMRBox.h
+++ Filtering/vtkAMRBox.h
@@ -289,7 +289,7 @@ public:
 
   // Description:
   // Intersect this box with another box in place.
-  //void operator&=(const vtkAMRBox &rhs);
+  void operator&=(const vtkAMRBox &rhs);
 
   // Description:
   // Test to see if a given cell index is inside this box.
