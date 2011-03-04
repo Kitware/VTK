b348e25b71966a4e4b808627f79116514aaa8ead
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2011-03-03 19:47:15 -0500

    Fixed asserts on exit on windows on server side.
    The problem was that callback were erased as connection was closed while the
    callbacks were bring processed which led to the iterators being invalid. Fixed
    that.

--- Parallel/vtkMultiProcessController.cxx
+++ Parallel/vtkMultiProcessController.cxx
@@ -623,27 +623,35 @@ void vtkMultiProcessController::ProcessRMI(int remoteProcessId,
                                            void *arg, int argLength,
                                            int rmiTag)
 {
-  bool found = false;
+  // we build the list of callbacks to call and then invoke them to handle the
+  // case where the callback removes the callback.
+  vtkstd::vector<vtkInternal::vtkRMICallback> callbacks;
+
   vtkInternal::RMICallbackMap::iterator iter =
     this->Internal->RMICallbacks.find(rmiTag);
   if (iter != this->Internal->RMICallbacks.end())
     {
     vtkInternal::RMICallbackVector::iterator iterVec;
     for (iterVec = iter->second.begin();
-      iterVec != iter->second.end(); ++iterVec)
+      iterVec != iter->second.end(); iterVec++)
       {
-      found = true;
       if (iterVec->Function)
         {
-        (*iterVec->Function)(iterVec->LocalArgument, arg, argLength, remoteProcessId);
+        callbacks.push_back(*iterVec);
         }
       }
     }
 
-  if ( ! found)
+  if (callbacks.size()==0)
     {
     vtkErrorMacro("Process " << this->GetLocalProcessId() << 
                   " Could not find RMI with tag " << rmiTag);
+    }
+  
+  vtkstd::vector<vtkInternal::vtkRMICallback>::iterator citer;
+  for (citer = callbacks.begin(); citer != callbacks.end(); citer++)
+    {
+    (*citer->Function)(citer->LocalArgument, arg, argLength, remoteProcessId);
     }
 }
 
