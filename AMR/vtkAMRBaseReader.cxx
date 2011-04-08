028201737476d007dc1da2293a04277961aa758b
George Zagaris <george.zagaris@kitware.com>
2011-04-08 17:05:50 -0400

    ENH: AMR base reader abstract class
    Adding base AMR reader class which encapsulates
    common functionality, i.e, UI components and
    Point/Cell data array selection that all concrete
    AMR readers can inherit.

---
+++ AMR/vtkAMRBaseReader.cxx
@@ -0,0 +1,213 @@
+/*=========================================================================
+
+ Program:   Visualization Toolkit
+ Module:    vtkAMRBaseReader.cxx
+
+ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+ All rights reserved.
+ See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notice for more information.
+
+ =========================================================================*/
+#include "vtkAMRBaseReader.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkDataObject.h"
+#include "vtkMultiProcessController.h"
+#include "vtkHierarchicalBoxDataSet.h"
+#include "vtkDataArraySelection.h"
+#include "vtkCallbackCommand.h"
+
+#include "vtkAMRUtilities.h"
+
+#include <cassert>
+
+vtkAMRBaseReader::vtkAMRBaseReader()
+{
+  this->MaxLevel   = 0; // By default load just the low-res data at level 0
+  this->Controller = NULL;
+}
+
+//------------------------------------------------------------------------------
+vtkAMRBaseReader::~vtkAMRBaseReader()
+{
+  this->PointDataArraySelection->RemoveObserver( this->SelectionObserver );
+  this->CellDataArraySelection->RemoveObserver( this->SelectionObserver );
+  this->SelectionObserver->Delete( );
+  this->CellDataArraySelection->Delete( );
+  this->PointDataArraySelection->Delete( );
+}
+
+//------------------------------------------------------------------------------
+int vtkAMRBaseReader::FillOutputPortInformation(
+    int vtkNotUsed(port),vtkInformation *info )
+{
+  info->Set( vtkDataObject::DATA_TYPE_NAME(), "vtkHierarchicalBoxDataSet" );
+  return 1;
+}
+
+//------------------------------------------------------------------------------
+void vtkAMRBaseReader::PrintSelf( std::ostream &os, vtkIndent indent )
+{
+  this->Superclass::PrintSelf( os, indent );
+}
+
+//------------------------------------------------------------------------------
+void vtkAMRBaseReader::Initialize()
+{
+  this->SetNumberOfInputPorts( 0 );
+  this->FileName      = NULL;
+  this->MaxLevel      = 0;
+  this->LoadParticles = 1;
+
+  this->CellDataArraySelection  = vtkDataArraySelection::New();
+  this->PointDataArraySelection = vtkDataArraySelection::New();
+  this->SelectionObserver       = vtkCallbackCommand::New();
+  this->SelectionObserver->SetCallback(
+      &vtkAMRBaseReader::SelectionModifiedCallback);
+  this->SelectionObserver->SetClientData( this );
+  this->CellDataArraySelection->AddObserver(
+     vtkCommand::ModifiedEvent,this->SelectionObserver );
+  this->PointDataArraySelection->AddObserver(
+     vtkCommand::ModifiedEvent, this->SelectionObserver );
+
+}
+
+//----------------------------------------------------------------------------
+void vtkAMRBaseReader::SelectionModifiedCallback(
+    vtkObject*, unsigned long, void* clientdata, void*)
+{
+  static_cast<vtkAMRBaseReader*>(clientdata)->Modified();
+}
+
+//------------------------------------------------------------------------------
+int vtkAMRBaseReader::GetNumberOfPointArrays()
+{
+  return( this->PointDataArraySelection->GetNumberOfArrays() );
+}
+
+//------------------------------------------------------------------------------
+int vtkAMRBaseReader::GetNumberOfCellArrays()
+{
+  return( this->CellDataArraySelection->GetNumberOfArrays() );
+}
+
+//------------------------------------------------------------------------------
+const char* vtkAMRBaseReader::GetPointArrayName(int index)
+{
+  return( this->PointDataArraySelection->GetArrayName( index ) );
+}
+
+//------------------------------------------------------------------------------
+const char* vtkAMRBaseReader::GetCellArrayName(int index)
+{
+  return( this->CellDataArraySelection->GetArrayName( index )  );
+}
+
+//------------------------------------------------------------------------------
+int vtkAMRBaseReader::GetPointArrayStatus(const char* name)
+{
+  return( this->PointDataArraySelection->ArrayIsEnabled( name ) );
+}
+
+//------------------------------------------------------------------------------
+int vtkAMRBaseReader::GetCellArrayStatus(const char* name)
+{
+  return( this->CellDataArraySelection->ArrayIsEnabled( name ) );
+}
+
+//------------------------------------------------------------------------------
+void vtkAMRBaseReader::SetPointArrayStatus(const char* name, int status)
+{
+
+  if( status )
+    {
+    this->PointDataArraySelection->EnableArray(name);
+    }
+  else
+    {
+    this->PointDataArraySelection->DisableArray(name);
+    }
+
+}
+
+//------------------------------------------------------------------------------
+void vtkAMRBaseReader::SetCellArrayStatus(const char* name, int status)
+{
+  if( status )
+    {
+    this->CellDataArraySelection->EnableArray( name );
+    }
+  else
+    {
+    this->CellDataArraySelection->DisableArray( name );
+    }
+}
+
+//------------------------------------------------------------------------------
+int vtkAMRBaseReader::GetBlockProcessId( const int blockIdx )
+{
+  // If this is reader instance is serial, return Process 0
+  // as the Process ID for the corresponding block.
+  if( this->Controller == NULL )
+    return 0;
+
+  int N = this->Controller->GetNumberOfProcesses();
+  return( blockIdx%N );
+}
+
+//------------------------------------------------------------------------------
+bool vtkAMRBaseReader::IsBlockMine( const int blockIdx )
+{
+  // If this reader instance does not run in parallel, then,
+  // all blocks are owned by this reader.
+  if( this->Controller == NULL )
+    return true;
+
+  int myRank = this->Controller->GetLocalProcessId();
+  if( myRank == this->GetBlockProcessId( blockIdx ) )
+    return true;
+  return false;
+}
+
+//------------------------------------------------------------------------------
+int vtkAMRBaseReader::RequestData(
+        vtkInformation* vtkNotUsed(request),
+        vtkInformationVector** vtkNotUsed(inputVector),
+        vtkInformationVector* outputVector )
+{
+  vtkInformation            *outInf = outputVector->GetInformationObject( 0 );
+  vtkHierarchicalBoxDataSet *output =
+    vtkHierarchicalBoxDataSet::SafeDownCast(
+     outInf->Get( vtkDataObject::DATA_OBJECT() ) );
+  assert( "pre: output AMR dataset is NULL" && ( output != NULL ) );
+
+  this->ReadMetaData();
+  this->GenerateBlockMap();
+
+  vtkstd::vector< int > idxcounter;
+  idxcounter.resize(this->GetNumberOfLevels(), 0);
+  for( int block=0; block < this->GetNumberOfBlocks(); ++block )
+    {
+
+      if( this->IsBlockMine(block) )
+        {
+          this->GetBlock( block, output, idxcounter );
+        }
+
+    } // END for all blocks
+
+  // Generate all the AMR metadata & the visibility arrays
+  vtkAMRUtilities::GenerateMetaData( output, this->Controller );
+  output->GenerateVisibilityArrays();
+
+  // If this instance of the reader is not parallel, block until all processes
+  // read their blocks.
+  if( this->Controller != NULL )
+    this->Controller->Barrier();
+
+  return 1;
+}
---
+++ AMR/vtkAMRBaseReader.h
@@ -0,0 +1,180 @@
+/*=========================================================================
+
+ Program:   Visualization Toolkit
+ Module:    vtkAMRBaseReader.h
+
+ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+ All rights reserved.
+ See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notice for more information.
+
+ =========================================================================*/
+// .NAME vtkAMRBaseReader.h -- Base class for all AMR Readers
+//
+// .SECTION Description
+// An abstract class that encapsulates common functionality for all AMR readers.
+
+#ifndef VTKAMRBASEREADER_H_
+#define VTKAMRBASEREADER_H_
+
+#include "vtkHierarchicalBoxDataSetAlgorithm.h"
+#include <vtkstd/vector> // STL Header
+
+// Forward Declarations
+class vtkHierarchicalBoxDataSet;
+class vtkMultiProcessController;
+class vtkDataArraySelection;
+class vtkCallbackCommand;
+
+class VTK_AMR_EXPORT vtkAMRBaseReader :
+  public vtkHierarchicalBoxDataSetAlgorithm
+{
+  public:
+     vtkTypeMacro( vtkAMRBaseReader, vtkHierarchicalBoxDataSetAlgorithm );
+     void PrintSelf(std::ostream &os, vtkIndent indent);
+
+     // Description:
+     // Initializes the AMR reader.
+     // All concrete instances must call this method in their constructor.
+     void Initialize();
+
+    // Description:
+    // Set/Get a multiprocess-controller for reading in parallel.
+    // By default this parameter is set to NULL by the constructor.
+    vtkSetMacro( Controller, vtkMultiProcessController* );
+    vtkGetMacro( Controller, vtkMultiProcessController* );
+
+    // Description:
+    // Set the level, up to which the blocks are loaded.
+    vtkSetMacro( MaxLevel,int);
+
+    // Description:
+    // Set/Get whether the particles, if any, are loaded
+    vtkSetMacro( LoadParticles, int );
+    vtkGetMacro( LoadParticles, int );
+    vtkBooleanMacro( LoadParticles, int );
+
+    // Description:
+    // Get the data array selection tables used to configure which data
+    // arrays are loaded by the reader.
+    vtkGetObjectMacro(CellDataArraySelection, vtkDataArraySelection);
+    vtkGetObjectMacro(PointDataArraySelection, vtkDataArraySelection);
+
+    // Description:
+    // Get the number of point or cell arrays available in the input.
+    int GetNumberOfPointArrays();
+    int GetNumberOfCellArrays();
+
+    // Description:
+    // Get the name of the point or cell array with the given index in
+    // the input.
+    const char* GetPointArrayName(int index);
+    const char* GetCellArrayName(int index);
+
+   // Description:
+   // Get/Set whether the point or cell array with the given name is to
+   // be read.
+   int GetPointArrayStatus(const char* name);
+   int GetCellArrayStatus(const char* name);
+   void SetPointArrayStatus(const char* name, int status);
+   void SetCellArrayStatus(const char* name, int status);
+
+    // Description:
+    // Set/Get the filename. Concrete instances of this class must implement
+    // the SetFileName method accordingly.
+    vtkGetStringMacro( FileName );
+    virtual void SetFileName()=0;
+
+  protected:
+    vtkAMRBaseReader();
+    virtual ~vtkAMRBaseReader();
+
+    // Description:
+    // Determines if the block is owned by this process based on the
+    // the block index and total number of processes.
+    bool IsBlockMine( const int blockIdx );
+
+    // Description:
+    // Returns the block process ID for the block corresponding to the
+    // given block index. If this reader instance is serial, i.e., there
+    // is no controller associated, the method returns 0. Otherwise, static
+    // block-cyclic-distribution is assumed and each block is assigned to
+    // a process according to blockIdx%N, where N is the total number of
+    // processes.
+    int GetBlockProcessId( const int blockIdx );
+
+    // Description:
+    // Reads all the metadata from the file. Implemented by concrete classes.
+    virtual void ReadMetaData()=0;
+
+    // Description:
+    // Generates a linear index for each block. Implemented by concrete
+    // instances.
+    //
+    // Note this method must create a block map for all blocks
+    // that are to be rendered according to the max level argument. For
+    // example, a sample implementation may look as follows:
+    //<code>
+    //  for(int i=0;i < this->Internal->NumberOfBlocks;i++ )
+    //   {
+    //    if ( this->GetBlockLevel( i ) <= this->MaxLevel  )
+    //     {
+    //      this->BlockMap.push_back( i );
+    //     }
+    //   }
+    //</code>
+    virtual void GenerateBlockMap()=0;
+
+    // Description:
+    // Returns the total number of blocks. Implemented by concrete instances.
+    virtual int GetNumberOfBlocks()=0;
+
+    // Description:
+    // Returns the total number of levels. Implemented by concrete instances.
+    virtual int GetNumberOfLevels()=0;
+
+    // Description:
+    // Loads the block according to the index w.r.t. the generated BlockMap.
+    virtual void GetBlock( int index, vtkHierarchicalBoxDataSet *hbds,
+                           vtkstd::vector< int > &idxcounter )=0;
+
+    // Description:
+    // Standard Pipeline methods, subclasses may override this method if needed.
+    virtual int RequestData(
+        vtkInformation* vtkNotUsed(request),
+        vtkInformationVector** vtkNotUsed(inputVector),
+        vtkInformationVector* outputVector );
+    int FillOutputPortInformation(int port,vtkInformation *info);
+
+    // Array selection member variables and methods
+    vtkDataArraySelection *PointDataArraySelection;
+    vtkDataArraySelection *CellDataArraySelection;
+    vtkCallbackCommand    *SelectionObserver;
+
+    // Description:
+    // Initializes the PointDataArraySelection & CellDataArraySelection
+    virtual void SetUpDataArraySelections()=0;
+
+    // Descriptions
+    // Call-back registered with the SelectionObserver.
+    static void SelectionModifiedCallback(
+      vtkObject *caller,unsigned long eid,void *clientdata,void *calldata );
+
+    int LoadParticles;
+    int MaxLevel;
+    char *FileName;
+    vtkMultiProcessController *Controller;
+
+    //BTX
+      vtkstd::vector<int> BlockMap;
+    //ETX
+
+  private:
+    vtkAMRBaseReader( const vtkAMRBaseReader& ); // Not implemented
+    void operator=( const vtkAMRBaseReader& ); // Not implemented
+};
+
+#endif /* VTKAMRBASEREADER_H_ */
