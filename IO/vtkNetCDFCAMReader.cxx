600a39061b7abf72abaf724c22961a52adf4b0a4
Andrew Bauer <andy.bauer@kitware.com>
2011-04-13 02:21:08 -0400

    Reader now working with reading point data at first level.
    Change-Id: I522235aa91bc78fece38fd18a61a153fcc794412

--- IO/vtkNetCDFCAMReader.cxx
+++ IO/vtkNetCDFCAMReader.cxx
@@ -235,12 +235,9 @@ int vtkNetCDFCAMReader::RequestData(
     }
 
   // read in the points first
-  NcToken dimensionToken = "ncol";
-  NcDim* dimension = this->PointsFile->get_dim(dimensionToken);
-  NcToken lonToken = "lon";
-  NcVar* lon = this->PointsFile->get_var(lonToken);
-  NcToken latToken = "lat";
-  NcVar* lat = this->PointsFile->get_var(latToken);
+  NcDim* dimension = this->PointsFile->get_dim("ncol");
+  NcVar* lon = this->PointsFile->get_var("lon");
+  NcVar* lat = this->PointsFile->get_var("lat");
   vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
   long numPoints = dimension->size();
   if(lat == NULL || lon == NULL)
@@ -253,8 +250,14 @@ int vtkNetCDFCAMReader::RequestData(
     points->SetDataTypeToDouble();
     points->SetNumberOfPoints(numPoints);
     std::vector<double> array(numPoints*2);
-    lon->get(&array[0], numPoints);
-    lat->get(&array[numPoints], numPoints);
+    if(!lon->get(&array[0], numPoints))
+      {
+      return 0;
+      }
+    if(!lat->get(&array[numPoints], numPoints))
+      {
+      return 0;
+      }
     for(long i=0;i<numPoints;i++)
       {
       points->SetPoint(i, array[i], array[i+numPoints], 0.);
@@ -265,8 +268,18 @@ int vtkNetCDFCAMReader::RequestData(
     points->SetDataTypeToFloat();
     points->SetNumberOfPoints(numPoints);
     std::vector<float> array(numPoints*2);
-    lon->get(&array[0], numPoints);
-    lat->get(&array[numPoints], numPoints);
+    if(!lon->get(&array[0], numPoints))
+      {
+      return 0;
+      }
+    if(!lat->get(&array[numPoints], numPoints))
+      {
+      return 0;
+      }
+    for(long i=0;i<numPoints;i++)
+      {
+      points->SetPoint(i, array[i], array[i+numPoints], 0.);
+      }
     for(long i=0;i<numPoints;i++)
       {
       points->SetPoint(i, array[i], array[i+numPoints], 0.);
@@ -275,33 +288,48 @@ int vtkNetCDFCAMReader::RequestData(
   output->SetPoints(points);
 
   // read in any point data with dimensions (time, lev, ncol)
-//   for(int i=0;i<this->PointsFile->num_vars();i++)
-//     {
-//     NcVar* variable = this->PointsFile->get_var(i);
-//     NcToken variableName = variable->name();
-//     if(variable->num_dims() != 3)
-//       { // not a field variable
-//       continue;
-//       }
-//     vtkDataArray* dataArray = NULL;
-//     if(variable->type() == ncDouble)
-//       {
-//       vtkDoubleArray* doubleArray = vtkDoubleArray::New();
-//       doubleArray->SetNumberOfTuples(numPoints);
-//       variable->get(doubleArray->GetPointer(0), 0, 0, numPoints);
-//       dataArray = doubleArray;
-//       }
-//     else
-//       {
-//       vtkFloatArray* floatArray = vtkFloatArray::New();
-//       floatArray->SetNumberOfTuples(numPoints);
-//       variable->get(floatArray->GetPointer(0), 0, 0, numPoints);
-//       dataArray = floatArray;
-//       }
-//     dataArray->SetName(variableName);
-//     output->GetPointData()->AddArray(dataArray);
-//     dataArray->Delete();
-//     }
+  NcDim* levelsDimension = this->PointsFile->get_dim("lev");
+  long numLevels = levelsDimension->size();
+  for(int i=0;i<this->PointsFile->num_vars();i++)
+    {
+    NcVar* variable = this->PointsFile->get_var(i);
+    if(variable->num_dims() != 3 || variable->get_dim(0)->size() != 1 ||
+       variable->get_dim(1)->size() != numLevels ||
+       variable->get_dim(2)->size() != numPoints)
+      { // not a field variable
+      continue;
+      }
+    NcToken variableName = variable->name();
+    // if we have a different level, that should be set here
+    long level = 0;
+    variable->set_cur(0, level, 0);
+    vtkDataArray* dataArray = NULL;
+    if(variable->type() == ncDouble)
+      {
+      vtkDoubleArray* doubleArray = vtkDoubleArray::New();
+      doubleArray->SetNumberOfTuples(numPoints);
+      if(!variable->get(doubleArray->GetPointer(0),
+                        1, 1, numPoints))
+        {
+        return 0;
+        }
+      dataArray = doubleArray;
+      }
+    else
+      {
+      vtkFloatArray* floatArray = vtkFloatArray::New();
+      floatArray->SetNumberOfTuples(numPoints);
+      if(!variable->get(floatArray->GetPointer(0),
+                        1, 1, numPoints))
+        {
+        return 0;
+        }
+      dataArray = floatArray;
+      }
+    dataArray->SetName(variableName);
+    output->GetPointData()->AddArray(dataArray);
+    dataArray->Delete();
+    }
 
   // now read in the cell connectivity.  note that this is a periodic
   // domain and only the points on the left boundary are included in
@@ -362,17 +390,23 @@ int vtkNetCDFCAMReader::RequestData(
             }
           else
             { // need to make point on the right boundary
-            vtkIdType newId =
+            pointIds[j] = boundaryPoints[pointIds[j]] =
               points->InsertNextPoint(point[0]+360., point[1], point[2]);
-            boundaryPoints[pointIds[j]] = newId;
-            output->GetPointData()->CopyData(output->GetPointData(),
-                                             pointIds[j], newId);
-            pointIds[j] = newId;
             }
           }
         }
       }
     output->InsertNextCell(VTK_QUAD, 4, pointIds);
+    }
+
+  output->GetPointData()->CopyAllOn();
+  output->GetPointData()->CopyAllocate(output->GetPointData(),
+                                       output->GetNumberOfPoints());
+  vtkPointData* pointData = output->GetPointData();
+  for(std::map<vtkIdType, vtkIdType>::const_iterator it=
+        boundaryPoints.begin();it!=boundaryPoints.end();it++)
+    {
+    pointData->CopyData(pointData, it->first, it->second);
     }
 
   vtkDebugMacro(<<"Read " <<output->GetNumberOfPoints() <<" points,"
