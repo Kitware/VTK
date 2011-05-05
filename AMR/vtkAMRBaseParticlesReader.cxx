541f14a4f6ac9ab9d09472202e60d253106a4dc2
George Zagaris <george.zagaris@kitware.com>
2011-05-05 18:27:29 -0400

    ENH: AMR Base Particles Reader
    Initial commit of AMR base particles reader.

---
+++ AMR/vtkAMRBaseParticlesReader.cxx
@@ -0,0 +1,36 @@
+/*=========================================================================
+
+ Program:   Visualization Toolkit
+ Module:    vtkAMRBaseParticlesReader.cxx
+
+ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+ All rights reserved.
+ See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notice for more information.
+
+ =========================================================================*/
+#include "vtkAMRBaseParticlesReader.h"
+
+
+vtkAMRBaseParticlesReader::vtkAMRBaseParticlesReader()
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+//------------------------------------------------------------------------------
+vtkAMRBaseParticlesReader::~vtkAMRBaseParticlesReader()
+{
+  // TODO Auto-generated destructor stub
+}
+
+//------------------------------------------------------------------------------
+void vtkAMRBaseParticlesReader::PrintSelf( std::ostream &os, vtkIndent indent )
+{
+  this->Superclass::PrintSelf( os, indent );
+}
+
+//------------------------------------------------------------------------------
