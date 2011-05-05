4029ecea55f962700e2994b00cbdc148cffe75fa
Julien Finet <julien.finet@kitware.com>
2011-05-05 13:23:29 -0400

    Support extent changes by cleaning up COMBINED_UPDATE_EXTENT.
    When no execute is needed (in REQUEST_UPDATE_EXTENT), we still need to
    reset COMBINED_UPDATE_EXTENT otherwise, if the UPDATE_EXTENT is later
     changed, COMBINED_UPDATE_EXTENT still contains old/obsolete bounds.
    
    Change-Id: I071f5130dc798da6a403fc34112b21388e53a943

---
+++ Imaging/Testing/Cxx/TestUpdateExtentReset.cxx
@@ -0,0 +1,54 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    ImageWeightedSum.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include <vtkColorTransferFunction.h>
+#include <vtkImageData.h>
+#include <vtkImageReslice.h>
+#include <vtkImageMapToColors.h>
+#include <vtkImageAppendComponents.h>
+#include <vtkSmartPointer.h>
+
+int TestUpdateExtentReset(int argc, char * argv [] )
+{
+  vtkSmartPointer<vtkImageData> img = vtkSmartPointer<vtkImageData>::New();
+  img->SetDimensions(100, 100, 100);
+  img->SetScalarTypeToFloat();
+  img->SetNumberOfScalarComponents(1);
+  img->AllocateScalars();
+
+  vtkSmartPointer<vtkImageReslice> reslicer = vtkSmartPointer<vtkImageReslice>::New();
+  reslicer->SetInput(img);
+  reslicer->SetOutputExtent(0, 100, 0, 100, 0, 0);
+
+  vtkSmartPointer<vtkImageMapToColors> colors =
+    vtkSmartPointer<vtkImageMapToColors>::New();
+  colors->SetInput(reslicer->GetOutput());
+
+  vtkSmartPointer<vtkColorTransferFunction> ctf =
+    vtkSmartPointer<vtkColorTransferFunction>::New();
+  ctf->AddRGBPoint(0, 1., 0., 0.);
+  colors->SetLookupTable(ctf);
+
+  vtkSmartPointer<vtkImageAppendComponents> append =
+    vtkSmartPointer<vtkImageAppendComponents>::New();
+  append->SetInputConnection(0 ,colors->GetOutput()->GetProducerPort());
+
+  colors->Update();
+  append->Update();
+  colors->Update();
+  reslicer->SetOutputExtent(0, 100, 0, 80, 0, 0);
+  colors->Update();
+
+  return EXIT_SUCCESS;
+}
