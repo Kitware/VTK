d88d5cbd3ab6083fbb388079f20a5f01b2d520d8
Philippe Pébay <pppebay@sandia.gov>
2011-05-09 16:55:09 -0700

    Updated CR information
    Change-Id: I7274c321ed5d448cd5210bd01ceeb1ee8e78f2b2

--- Infovis/Testing/Cxx/TestRandomPKMeansStatisticsMPI.cxx
+++ Infovis/Testing/Cxx/TestRandomPKMeansStatisticsMPI.cxx
@@ -13,7 +13,7 @@
 
 =========================================================================*/
 /*
- * Copyright 2008 Sandia Corporation.
+ * Copyright 2011 Sandia Corporation.
  * Under the terms of Contract DE-AC04-94AL85000, there is a non-exclusive
  * license for use of this work by or on behalf of the
  * U.S. Government. Redistribution and use in source and binary forms, with
