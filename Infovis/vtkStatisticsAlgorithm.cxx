ed9fda3f36ee872245800c2a2d789633bad4f3ef
Philippe Pébay <pppebay@sandia.gov>
2011-05-26 16:39:44 -0700

    Updated CR

--- Infovis/vtkStatisticsAlgorithm.cxx
+++ Infovis/vtkStatisticsAlgorithm.cxx
@@ -13,7 +13,7 @@
 
 =========================================================================*/
 /*-------------------------------------------------------------------------
-  Copyright 2008 Sandia Corporation.
+  Copyright 2011 Sandia Corporation.
   Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
   the U.S. Government retains certain rights in this software.
 -------------------------------------------------------------------------*/
