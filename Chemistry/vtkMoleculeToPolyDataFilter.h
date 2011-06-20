bf86b66055c7dc5b9beee61de9f40418f4072e6c
David C. Lonie <loniedavid@gmail.com>
2011-06-20 18:52:51 -0400

    Added some polydata generators to the Chemisty kit.
    Using the vtkMoleculeMapper from the next patch is the preferred
    rendering method, but these may still be useful at some point.
    
    Change-Id: Id55ebacf6f88df2c9b2ddc37c7f6749d450a1e3c

---
+++ Chemistry/vtkMoleculeToPolyDataFilter.h
@@ -0,0 +1,48 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkMoleculeToPolyDataFilter.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkMoleculeToPolyDataFilter - abstract filter class
+// .SECTION Description
+// vtkMoleculeToPolyDataFilter is an abstract filter class whose
+// subclasses take as input datasets of type vtkMolecule and
+// generate polygonal data on output.
+
+#ifndef __vtkMoleculeToPolyDataFilter_h
+#define __vtkMoleculeToPolyDataFilter_h
+
+#include "vtkPolyDataAlgorithm.h"
+
+class vtkMolecule;
+
+class VTK_CHEMISTRY_EXPORT vtkMoleculeToPolyDataFilter
+: public vtkPolyDataAlgorithm
+{
+public:
+  vtkTypeMacro(vtkMoleculeToPolyDataFilter,vtkPolyDataAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  vtkMolecule * GetInput();
+
+protected:
+  vtkMoleculeToPolyDataFilter();
+  ~vtkMoleculeToPolyDataFilter();
+
+  virtual int FillInputPortInformation(int, vtkInformation*);
+
+private:
+  vtkMoleculeToPolyDataFilter(const vtkMoleculeToPolyDataFilter&);  // Not implemented.
+  void operator=(const vtkMoleculeToPolyDataFilter&);  // Not implemented.
+};
+
+#endif
