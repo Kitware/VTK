fa7a15aecf519bffa0576a6be4752e2074fb515f
Philippe Pébay <pppebay@sandia.gov>
2011-06-21 16:16:29 -0700

    Header clean-up following overhaul

--- Infovis/vtkDescriptiveStatistics.h
+++ Infovis/vtkDescriptiveStatistics.h
@@ -103,9 +103,9 @@ protected:
   // Description:
   // Execute the calculations required by the Learn option, given some input Data
   // NB: input parameters are unused.
-  virtual void Learn( vtkTable* inData,
-                      vtkTable* inParameters,
-                      vtkMultiBlockDataSet* outMeta );
+  virtual void Learn( vtkTable*,
+                      vtkTable*,
+                      vtkMultiBlockDataSet* );
 
   // Description:
   // Execute the calculations required by the Derive option.
@@ -113,9 +113,9 @@ protected:
 
   // Description:
   // Execute the calculations required by the Test option.
-  virtual void Test( vtkTable* inData,
-                     vtkMultiBlockDataSet* inMeta,
-                     vtkTable* outMeta ); 
+  virtual void Test( vtkTable*,
+                     vtkMultiBlockDataSet*,
+                     vtkTable* ); 
 
   // Description:
   // Execute the calculations required by the Assess option.
@@ -123,11 +123,6 @@ protected:
                        vtkMultiBlockDataSet* inMeta,
                        vtkTable* outData ) 
   { this->Superclass::Assess( inData, inMeta, outData, 1 ); }
-
-  int UnbiasedVariance;
-  int G1Skewness;
-  int G2Kurtosis;
-  int SignedDeviations;
 
 //BTX  
   // Description:
@@ -137,6 +132,11 @@ protected:
                                     vtkStringArray* rowNames,
                                     AssessFunctor*& dfunc );
 //ETX
+
+  int UnbiasedVariance;
+  int G1Skewness;
+  int G2Kurtosis;
+  int SignedDeviations;
 
 private:
   vtkDescriptiveStatistics( const vtkDescriptiveStatistics& ); // Not implemented
