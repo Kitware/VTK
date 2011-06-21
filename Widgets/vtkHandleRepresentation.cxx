7b626b3974d291f5dff207fba3d61be261016b14
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-06-21 14:58:41 +0200

    WorldPosition is the ultimate maintainer.
    Need to update displayposition first before accessing its internal value
    
    Change-Id: Ibe384e04217bbdf030782cefdb23834a18f628c7

--- Widgets/vtkHandleRepresentation.cxx
+++ Widgets/vtkHandleRepresentation.cxx
@@ -90,6 +90,12 @@ void vtkHandleRepresentation::GetDisplayPosition(double pos[3])
 //----------------------------------------------------------------------
 double* vtkHandleRepresentation::GetDisplayPosition()
 {
+  // The world position maintains the position
+  if ( this->Renderer && (this->WorldPositionTime > this->DisplayPositionTime) )
+    {
+    int *p = this->WorldPosition->GetComputedDisplayValue(this->Renderer);
+    this->DisplayPosition->SetValue(p[0],p[1],0.0);
+    }
   return this->DisplayPosition->GetValue();
 }
 
