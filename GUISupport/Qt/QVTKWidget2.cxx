32e2a318f2ada60f14807c14239e93395c179e63
Clinton Stimpson <clinton@elemtech.com>
2011-07-01 13:30:20 -0600

    Fix buffer swap issue in QVTKWidget2

--- GUISupport/Qt/QVTKWidget2.cxx
+++ GUISupport/Qt/QVTKWidget2.cxx
@@ -42,6 +42,7 @@ QVTKWidget2::QVTKWidget2(QWidget* p, const QGLWidget* shareWidget, Qt::WindowFla
   mIrenAdapter = new QVTKInteractorAdapter(this);
   mConnect = vtkSmartPointer<vtkEventQtSlotConnect>::New();
   this->setMouseTracking(true);       
+  this->setAutoBufferSwap(false);
 }
 
 QVTKWidget2::QVTKWidget2(QGLContext* ctx, QWidget* p, const QGLWidget* shareWidget, Qt::WindowFlags f)
@@ -51,6 +52,7 @@ QVTKWidget2::QVTKWidget2(QGLContext* ctx, QWidget* p, const QGLWidget* shareWidg
   mIrenAdapter = new QVTKInteractorAdapter(this);
   mConnect = vtkSmartPointer<vtkEventQtSlotConnect>::New();
   this->setMouseTracking(true);       
+  this->setAutoBufferSwap(false);
 }
 
 QVTKWidget2::QVTKWidget2(const QGLFormat& fmt, QWidget* p, const QGLWidget* shareWidget, Qt::WindowFlags f)
@@ -60,6 +62,7 @@ QVTKWidget2::QVTKWidget2(const QGLFormat& fmt, QWidget* p, const QGLWidget* shar
   mIrenAdapter = new QVTKInteractorAdapter(this);
   mConnect = vtkSmartPointer<vtkEventQtSlotConnect>::New();
   this->setMouseTracking(true);       
+  this->setAutoBufferSwap(false);
 }
 
 /*! destructor */
@@ -253,20 +256,7 @@ void QVTKWidget2::paintGL()
     return;
     }
 
-  // A callback from VTK will call swapBuffers() for us
-  // this is because sometimes VTK does a render without coming through this paintGL()
-
-  // if you want paintGL to always be called for each time VTK renders
-  // 1. turn off EnableRender on the interactor,
-  // 2. turn off SwapBuffers on the render window,
-  // 3. add an observer for the RenderEvent coming from the interactor
-  // 4. implement the callback on the observer to call updateGL() on this widget
-  // 5. implement this function to call mRenWin->Render() instead of going through interactor
-
-  bool autoswap = this->autoBufferSwap();
-  this->setAutoBufferSwap(false);
   iren->Render();
-  this->setAutoBufferSwap(autoswap);
 }
 
 /*! handle mouse press event
@@ -432,4 +422,15 @@ void QVTKWidget2::Frame()
 {
   if(mRenWin->GetSwapBuffers())
     this->swapBuffers();
+  
+  // This callback will call swapBuffers() for us
+  // because sometimes VTK does a render without coming through this paintGL()
+
+  // if you want paintGL to always be called for each time VTK renders
+  // 1. turn off EnableRender on the interactor,
+  // 2. turn off SwapBuffers on the render window,
+  // 3. add an observer for the RenderEvent coming from the interactor
+  // 4. implement the callback on the observer to call updateGL() on this widget
+  // 5. overload QVTKWidget2::paintGL() to call mRenWin->Render() instead iren->Render()
+
 }
