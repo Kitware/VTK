98588b78dde8580be1b3a0e06a67686d8d2551c1
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2011-07-21 08:32:06 -0400

    Replaced keyword deering with offaxis as it is a common term
    Change-Id: I364c35ef19763d0238954bd4242e0e142560175b

--- Rendering/vtkCamera.h
+++ Rendering/vtkCamera.h
@@ -262,7 +262,7 @@ class VTK_RENDERING_EXPORT vtkCamera : public vtkObject
   vtkGetMacro(FocalDisk,double);
 
   // Description:
-  // Set/Get use deering frustum.
+  // Set/Get use offaxis frustum.
   // OffAxis frustum is used for off-axis frustum calculations specificly
   // for stereo rendering.
   // For reference see "High Resolution Virtual Reality", in Proc.
@@ -273,28 +273,28 @@ class VTK_RENDERING_EXPORT vtkCamera : public vtkObject
 
   // Description:
   // Set/Get top left corner point of the screen.
-  // This will be used only for deering frustum calculation.
+  // This will be used only for offaxis frustum calculation.
   // Default is (-1.0, -1.0, -1.0).
   vtkSetVector3Macro(ScreenBottomLeft, double);
   vtkGetVector3Macro(ScreenBottomLeft, double);
 
   // Description:
   // Set/Get bottom left corner point of the screen.
-  // This will be used only for deering frustum calculation.
+  // This will be used only for offaxis frustum calculation.
   // Default is (1.0, -1.0, -1.0).
   vtkSetVector3Macro(ScreenBottomRight, double);
   vtkGetVector3Macro(ScreenBottomRight, double);
 
   // Description:
   // Set/Get top right corner point of the screen.
-  // This will be used only for deering frustum calculation.
+  // This will be used only for offaxis frustum calculation.
   // Default is (1.0, 1.0, -1.0).
   vtkSetVector3Macro(ScreenTopRight, double);
   vtkGetVector3Macro(ScreenTopRight, double);
 
   // Description:
   // Set/Get distance between the eyes.
-  // This will be used only for deering frustum calculation.
+  // This will be used only for offaxis frustum calculation.
   // Default is 0.06.
   vtkSetMacro(EyeSeparation, double);
   vtkGetMacro(EyeSeparation, double);
@@ -303,19 +303,19 @@ class VTK_RENDERING_EXPORT vtkCamera : public vtkObject
   // Set/Get the eye position (center point between two eyes).
   // This is a convenience function that sets the translation
   // component of EyeTransformMatrix.
-  // This will be used only for deering frustum calculation.
+  // This will be used only for offaxis frustum calculation.
   void SetEyePosition(double eyePosition[3]);
   void GetEyePosition(double eyePosition[3]);
 
   // Description:
   // Get normal vector from eye to screen rotated by EyeTransformMatrix.
-  // This will be used only for deering frustum calculation.
+  // This will be used only for offaxis frustum calculation.
   void GetEyePlaneNormal(double normal[3]);
 
   // Description:
   // Set/Get eye transformation matrix.
   // This is the transformation matrix for the point between eyes.
-  // This will be used only for deering frustum calculation.
+  // This will be used only for offaxis frustum calculation.
   // Default is identity.
   void SetEyeTransformMatrix(vtkMatrix4x4* matrix);
   vtkGetObjectMacro(EyeTransformMatrix, vtkMatrix4x4);
@@ -550,7 +550,7 @@ protected:
   void ComputeWorldToScreenMatrix();
 
   // Description:
-  // Compute and use frustum using deering method.
+  // Compute and use frustum using offaxis method.
   void ComputeOffAxisProjectionFrustum();
 
   // Description:
