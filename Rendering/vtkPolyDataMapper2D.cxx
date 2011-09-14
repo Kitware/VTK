e005b7affbbef616db40a6643f5590fd91d24044
Julien Finet <julien.finet@kitware.com>
2011-09-14 01:15:43 -0400

    Add vtkPolyDataMapper2D::TransformCoordinateUseDouble
    vtkOpenGLPolyDataMapper was rounding up transformed coordinates into
    integers. Add an option to use doubles instead.

--- Rendering/vtkPolyDataMapper2D.cxx
+++ Rendering/vtkPolyDataMapper2D.cxx
@@ -43,6 +43,7 @@ vtkPolyDataMapper2D::vtkPolyDataMapper2D()
   this->ScalarMode = VTK_SCALAR_MODE_DEFAULT;
   
   this->TransformCoordinate = NULL;
+  this->TransformCoordinateUseDouble = false;
 
   strcpy(this->ArrayName, "");
   this->ArrayId = -1;
