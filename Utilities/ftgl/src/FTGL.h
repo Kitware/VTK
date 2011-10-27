7eafb31c075d1d5f1a32b8be90076601370a1663
David Partyka <david.partyka@kitware.com>
2011-10-27 14:05:21 -0400

    Fix ftgl export macro.

--- Utilities/ftgl/src/FTGL.h
+++ Utilities/ftgl/src/FTGL.h
@@ -75,7 +75,7 @@ struct FTGLRenderContext
   #ifdef FTGL_LIBRARY_STATIC    // static lib - no special export required
   #  define FTGL_EXPORT
   #else
-    #ifdef FTGL_LIBRARY        // dynamic lib - must export/import symbols appropriately.
+    #ifdef vtkftgl_EXPORTS        // dynamic lib - must export/import symbols appropriately.
   #  define FTGL_EXPORT   __declspec(dllexport)
   #else
   #  define FTGL_EXPORT   __declspec(dllimport)
