423f8ea8440c3d7f581de53fbf61db94d6926256
Philippe Pébay <philippe.pebay@kitware.com>
2011-11-09 17:53:55 +0100

    Better and more legible formating and test flow
    Change-Id: Ie79bf366477f0f96f086d3626e3956a329c28cfa

--- Hybrid/Testing/Cxx/TestPolarAxes.cxx
+++ Hybrid/Testing/Cxx/TestPolarAxes.cxx
@@ -37,68 +37,68 @@
 //----------------------------------------------------------------------------
 int TestPolarAxes( int argc, char * argv [] )
 {
-  vtkNew<vtkBYUReader> fohe;
-  char* fname = vtkTestUtilities::ExpandDataFileName(argc, argv, "Data/teapot.g");
-  fohe->SetGeometryFileName(fname);
+  vtkNew<vtkBYUReader> reader;
+  char* fname = vtkTestUtilities::ExpandDataFileName( argc, argv, "Data/teapot.g" );
+  reader->SetGeometryFileName( fname );
   delete [] fname;
 
   vtkNew<vtkPolyDataNormals> normals;
-  normals->SetInputConnection(fohe->GetOutputPort());
+  normals->SetInputConnection( reader->GetOutputPort() );
 
-  vtkNew<vtkPolyDataMapper> foheMapper;
-  foheMapper->SetInputConnection(normals->GetOutputPort());
+  vtkNew<vtkPolyDataMapper> readerMapper;
+  readerMapper->SetInputConnection( normals->GetOutputPort() );
 
-  vtkNew<vtkLODActor> foheActor;
-  foheActor->SetMapper(foheMapper.GetPointer());
-  foheActor->GetProperty()->SetDiffuseColor(0.7, 0.3, 0.0);
+  vtkNew<vtkLODActor> readerActor;
+  readerActor->SetMapper( readerMapper.GetPointer() );
+  readerActor->GetProperty()->SetDiffuseColor( 0.7, 0.3, 0.0 );
 
   vtkNew<vtkOutlineFilter> outline;
-  outline->SetInputConnection(normals->GetOutputPort());
+  outline->SetInputConnection(normals->GetOutputPort() );
 
   vtkNew<vtkPolyDataMapper> mapOutline;
-  mapOutline->SetInputConnection(outline->GetOutputPort());
+  mapOutline->SetInputConnection(outline->GetOutputPort() );
 
   vtkNew<vtkActor> outlineActor;
-  outlineActor->SetMapper(mapOutline.GetPointer());
-  outlineActor->GetProperty()->SetColor(0.0 ,0.0 ,0.0);
+  outlineActor->SetMapper( mapOutline.GetPointer() );
+  outlineActor->GetProperty()->SetColor( 0.0, 0.0, 0.0 );
 
   vtkNew<vtkCamera> camera;
-  camera->SetClippingRange(1.0, 100.0);
-  camera->SetFocalPoint(0.9, 1.0, 0.0);
-  camera->SetPosition(11.63, 6.0, 10.77);
+  camera->SetClippingRange( 1.0, 100.0 );
+  camera->SetFocalPoint( 0.9, 1.0, 0.0 );
+  camera->SetPosition( 11.63, 6.0, 10.77 );
 
   vtkNew<vtkLight> light;
-  light->SetFocalPoint(0.21406, 1.5, 0.0);
-  light->SetPosition(8.3761, 4.94858, 4.12505);
+  light->SetFocalPoint( 0.21406, 1.5, 0.0 );
+  light->SetPosition( 8.3761, 4.94858, 4.12505 );
 
-  vtkNew<vtkRenderer> ren2;
-  ren2->SetActiveCamera(camera.GetPointer());
-  ren2->AddLight(light.GetPointer());
+  vtkNew<vtkRenderer> renderer;
+  renderer->SetActiveCamera( camera.GetPointer() );
+  renderer->AddLight( light.GetPointer() );
 
-  vtkNew<vtkRenderWindow> renWin;
-  renWin->SetMultiSamples(0);
-  renWin->AddRenderer(ren2.GetPointer());
-  renWin->SetWindowName("VTK - Polar Axes custom range");
-  renWin->SetSize(600, 600);
-
-  vtkNew<vtkRenderWindowInteractor> iren;
-  iren->SetRenderWindow(renWin.GetPointer());
-
-  ren2->AddViewProp(foheActor.GetPointer());
-  ren2->AddViewProp(outlineActor.GetPointer());
-  ren2->SetBackground(0.1, 0.2, 0.4);
-
+  // Update normals in order to get correct bounds for polar axes
   normals->Update();
 
   vtkNew<vtkPolarAxesActor> polaxes;
   polaxes->SetBounds( normals->GetOutput()->GetBounds() );
   polaxes->SetPole( 0., 0., 0. );
   polaxes->SetMaximumRadius( 300 );
-  polaxes->SetCamera( ren2->GetActiveCamera() );
+  polaxes->SetCamera( renderer->GetActiveCamera() );
   polaxes->SetRadialLabelFormat( "%6.1f" );
   polaxes->SetScreenSize( 15.0 );
 
-  ren2->AddViewProp(polaxes.GetPointer());
+  vtkNew<vtkRenderWindow> renWin;
+  renWin->SetMultiSamples( 0 );
+  renWin->AddRenderer( renderer.GetPointer() );
+  renWin->SetWindowName( "VTK - Polar Axes custom range" );
+  renWin->SetSize( 600, 600 );
+
+  vtkNew<vtkRenderWindowInteractor> iren;
+  iren->SetRenderWindow( renWin.GetPointer() );
+
+  renderer->SetBackground( 0.1, 0.2, 0.4 );
+  renderer->AddViewProp( readerActor.GetPointer() );
+  renderer->AddViewProp( outlineActor.GetPointer() );
+  renderer->AddViewProp( polaxes.GetPointer() );
   renWin->Render();
 
   int retVal = vtkRegressionTestImage( renWin.GetPointer() );
