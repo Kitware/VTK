3ee3f25cf8bc3e63df5a1fc8128f975b7f2518cf
Philippe Pébay <philippe.pebay@kitware.com>
2011-11-21 17:03:38 +0100

    Clarfication, coding style
    Change-Id: I4bd472921546412c9f963493871c5c3ff8e43006

--- Filtering/vtkLinearExtractionFilter.cxx
+++ Filtering/vtkLinearExtractionFilter.cxx
@@ -24,7 +24,7 @@
 vtkStandardNewMacro(vtkLinearExtractionFilter);
 
 // ----------------------------------------------------------------------
-vtkLinearExtractionFilter::vtkLinearExtractionFilter ( )
+vtkLinearExtractionFilter::vtkLinearExtractionFilter ()
 {
   this->Tolerance = 0. ;
   this->SetStartPoint( 0., 0., 0. );
@@ -32,137 +32,133 @@ vtkLinearExtractionFilter::vtkLinearExtractionFilter ( )
 }
 
 // ----------------------------------------------------------------------
-vtkLinearExtractionFilter::~vtkLinearExtractionFilter ( )
+vtkLinearExtractionFilter::~vtkLinearExtractionFilter ()
 {
 }
 
 // ----------------------------------------------------------------------
-void vtkLinearExtractionFilter::PrintSelf (ostream& os, vtkIndent indent)
+void vtkLinearExtractionFilter::PrintSelf ( ostream& os, vtkIndent indent )
 {
-  Superclass::PrintSelf(os,indent);
+  Superclass::PrintSelf( os,indent );
 
-  os << indent 
-     << "Point 1   : (" 
-     << this->StartPoint[0] 
-     << ", " 
-     << this->StartPoint [1] 
-     << ", " 
+  os << indent
+     << "Point 1   : ("
+     << this->StartPoint[0]
+     << ", "
+     << this->StartPoint [1]
+     << ", "
      << this->StartPoint [2]
      << ")\n";
 
-  os << indent 
+  os << indent
      << "Point 2   : ("
-     << this->EndPoint [0] 
-     << ", " 
-     << this->EndPoint [1] 
-     << ", " 
-     << this->EndPoint [2] 
+     << this->EndPoint [0]
+     << ", "
+     << this->EndPoint [1]
+     << ", "
+     << this->EndPoint [2]
      << ")\n";
 
-  os << indent 
-     << "Tolerance : " 
+  os << indent
+     << "Tolerance : "
      << this->Tolerance
      << "\n";
 }
 
 // ----------------------------------------------------------------------
-int vtkLinearExtractionFilter::FillInputPortInformation(int port, vtkInformation *info)
+int vtkLinearExtractionFilter::FillInputPortInformation( int port, vtkInformation *info )
 {
-  info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(),
+  info->Set( vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(),
             "vtkCompositeDataSet");
-  //info->Set(vtkAlgorithm::INPUT_IS_OPTIONAL(), 0);
+  //info->Set( vtkAlgorithm::INPUT_IS_OPTIONAL(), 0 );
   return 1;
 }
 
 // ----------------------------------------------------------------------
-int vtkLinearExtractionFilter::RequestData(
-                                           vtkInformation *vtkNotUsed(request),
-                                           vtkInformationVector **inputVector,
-                                           vtkInformationVector *outputVector)
+int vtkLinearExtractionFilter::RequestData( vtkInformation *vtkNotUsed( request ),
+                                            vtkInformationVector **inputVector,
+                                            vtkInformationVector *outputVector )
 {
-  // get the info objects
-  vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
-  vtkInformation *outInfo = outputVector->GetInformationObject(0);
+  // Get information objects
+  vtkInformation *inInfo = inputVector[0]->GetInformationObject( 0 );
+  vtkInformation *outInfo = outputVector->GetInformationObject( 0 );
 
-  // get the input and ouptut
-  vtkCompositeDataSet *compositeInput = vtkCompositeDataSet::SafeDownCast(
-                                                                          inInfo->Get(vtkDataObject::DATA_OBJECT()));
+  // Get input and ouptut
+  vtkCompositeDataSet *compositeInput
+    = vtkCompositeDataSet::SafeDownCast( inInfo->Get( vtkDataObject::DATA_OBJECT() ) );
 
-  vtkSelection *output = vtkSelection::SafeDownCast(
-                                                    outInfo->Get(vtkDataObject::DATA_OBJECT()));
+  vtkSelection *output =
+    vtkSelection::SafeDownCast( outInfo->Get( vtkDataObject::DATA_OBJECT() ) );
 
   // preparation de l'output
-  if (0 == output)
+  if ( ! output )
     {
-    vtkErrorMacro(<<"vtkLinearExtractionFilter: filter does not have any output.");
+    vtkErrorMacro( <<"vtkLinearExtractionFilter: filter does not have any output." );
     return 0;
-    }	// if (0 == output)
+    }	// if ( ! output )
 
-  if (0 == compositeInput)
+  if ( ! compositeInput )
     {
-    vtkErrorMacro(<<"vtkLinearExtractionFilter: filter does not have any input.");
+    vtkErrorMacro( <<"vtkLinearExtractionFilter: filter does not have any input." );
     return 0;
-    }	// if (0 == input)
+    }	// if ( ! compositeInput )
 
   vtkSmartPointer<vtkCompositeDataIterator> inputIterator = vtkCompositeDataIterator::New();
-  inputIterator->SetDataSet(compositeInput);
+  inputIterator->SetDataSet( compositeInput );
   inputIterator->VisitOnlyLeavesOn();
   inputIterator->SkipEmptyNodesOn();
   inputIterator->InitTraversal();
   inputIterator->GoToFirstItem();
-  while (inputIterator->IsDoneWithTraversal() == 0)
+  while ( ! inputIterator->IsDoneWithTraversal() )
     {
-    vtkDataSet * input = vtkDataSet::SafeDownCast(
-                                                  inputIterator->GetCurrentDataObject());
-
-    int conmposite_index = inputIterator->GetCurrentFlatIndex(); // et oui, les composite_index commencent a 1 :(
-    int partNumber = -1;
-
-    if( partNumber == -1 ) partNumber = conmposite_index - 1;
+    vtkDataSet* input = vtkDataSet::SafeDownCast( inputIterator->GetCurrentDataObject() );
+    // Composite indices begin at 1
+    int partNumber = inputIterator->GetCurrentFlatIndex() - 1;
 
     inputIterator->GoToNextItem();
-
     vtkSmartPointer<vtkIdTypeArray> indices = vtkSmartPointer<vtkIdTypeArray>::New();
-
     this->RequestDataInternal( input, indices );
 
     vtkSmartPointer<vtkSelectionNode> outSelNode = vtkSelectionNode::New();
-    outSelNode->SetContentType(vtkSelectionNode::INDICES);
-    outSelNode->SetFieldType(vtkSelectionNode::CELL);
-    outSelNode->GetProperties()->Set(vtkSelectionNode::COMPOSITE_INDEX(),partNumber+1);
-    outSelNode->SetSelectionList(indices);
-    output->AddNode(outSelNode);
-    //vtkDebugMacro(<<"vtkLinearExtractionFilter: Ajout du node :\n");
-    //outSelNode->PrintSelf(cout,vtkIndent(2));
+    outSelNode->SetContentType( vtkSelectionNode::INDICES );
+    outSelNode->SetFieldType( vtkSelectionNode::CELL );
+    outSelNode->GetProperties()->Set( vtkSelectionNode::COMPOSITE_INDEX(), partNumber + 1 );
+    outSelNode->SetSelectionList( indices );
+    output->AddNode( outSelNode );
     }
 
   return 1;
 }
 
+// ----------------------------------------------------------------------
 void vtkLinearExtractionFilter::RequestDataInternal ( vtkDataSet* input, vtkIdTypeArray* outIndices )
 {
-  // La liste des données retenues / distance à P1 :
-  vector< pair<vtkIdType, double> >	keptData;
-  const vtkIdType	cellNum	= input->GetNumberOfCells ( );
-  for (vtkIdType id = 0; id < cellNum; id++)
+  // Storage for retained data with and distance à P1 :
+  vector< pair<vtkIdType, double> > keptData;
+  const vtkIdType cellNum = input->GetNumberOfCells();
+  for ( vtkIdType id = 0; id < cellNum; ++ id )
     {
-    vtkCell*	cell	= input->GetCell (id);
-    if (0 != cell)
+    vtkCell* cell = input->GetCell ( id );
+    if ( 0 != cell )
       {
-      // Coordonnée paramétrique du point intersécant sur la ligne :
-      double	coords [3], pcoords [3];
+      // Storage for coordinates of intersection with the line
+      double coords [3];
+      double pcoords [3];
       double t = 0;
-      int						subId	= 0;
+      int subId	= 0;
 
-      // IntersectWithLine : intersection avec un segment et non avec une
-      // ligne.
-      if (0 != cell->IntersectWithLine (
-                                        this->StartPoint, this->EndPoint, this->Tolerance, t, coords, pcoords,
-                                        subId))
+      // Intersection with a segment and not a line
+      if ( cell->IntersectWithLine ( this->StartPoint, 
+                                     this->EndPoint, 
+                                     this->Tolerance, 
+                                     t, 
+                                     coords, 
+                                     pcoords,
+                                     subId ) )
         {
-        outIndices->InsertNextValue(id);
+        outIndices->InsertNextValue( id );
         }
-      }	// if (0 != cell)
-    }	// for (vtkIdType id = 0; id < cellNum; id++)
+      }	// if ( cell )
+    } // for ( vtkIdType id = 0; id < cellNum; ++ id )
 
-}	// vtkLinearExtractionFilter::ExecuteDistanceSorted
+}
