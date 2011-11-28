8830631d6e6264ff9768fdc4716a795aa93f38af
Philippe Pébay <philippe.pebay@kitware.com>
2011-11-28 17:14:22 +0100

    Fixed memory leak: undeleted char array
    Change-Id: I7ddf03b2e51c97f9b53dc06ae290fea4609afd67

--- Filtering/Testing/Cxx/TestLinearExtractor3D.cxx
+++ Filtering/Testing/Cxx/TestLinearExtractor3D.cxx
@@ -128,6 +128,7 @@ int TestLinearExtractor3D( int argc, char * argv [] )
   vtkSmartPointer<vtkUnstructuredGridReader> reader = vtkSmartPointer<vtkUnstructuredGridReader>::New();
   reader->SetFileName( fileName );
   reader->Update();
+  delete [] fileName;
 
   // Create multi-block mesh for linear extractor
   vtkSmartPointer<vtkMultiBlockDataSet> mesh = vtkSmartPointer<vtkMultiBlockDataSet>::New();
