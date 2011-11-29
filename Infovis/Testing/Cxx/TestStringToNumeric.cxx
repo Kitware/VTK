e551c8566c492742b11de7259cf9a9fd90adcc15
Scott Fowler <s.fowler@tecplot.com>
2011-11-28 16:44:17 -0800

    Added tests to vtkStringToNumeric
    Change-Id: Ic593eedb02e4377c4557b07cd6312131e9f07a33

--- Infovis/Testing/Cxx/TestStringToNumeric.cxx
+++ Infovis/Testing/Cxx/TestStringToNumeric.cxx
@@ -25,104 +25,180 @@
 #include "vtkStringToNumeric.h"
 #include "vtkTable.h"
 #include "vtkTestUtilities.h"
+#include "vtkMath.h"
 
 #include "vtkSmartPointer.h"
 #define VTK_CREATE(type,name) \
   vtkSmartPointer<type> name = vtkSmartPointer<type>::New()
 
+namespace
+{
+    int ArrayTypesTest(int argc, char* argv[])
+    {
+      char* file = vtkTestUtilities::ExpandDataFileName(argc, argv,
+                                                        "Data/authors.csv");
+      
+      VTK_CREATE(vtkDelimitedTextReader, reader);
+      reader->SetFileName(file);
+      reader->SetHaveHeaders(true);
+
+      delete [] file;
+
+      VTK_CREATE(vtkStringToNumeric, numeric);
+      numeric->SetInputConnection(reader->GetOutputPort());
+      numeric->Update();
+      
+      vtkTable* table = vtkTable::SafeDownCast(numeric->GetOutput());
+      
+      cerr << "Testing array types..." << endl;
+      int errors = 0;
+      if (!vtkStringArray::SafeDownCast(table->GetColumnByName("Author")))
+        {
+        cerr << "ERROR: Author array missing" << endl;
+        ++errors;
+        }
+      if (!vtkStringArray::SafeDownCast(table->GetColumnByName("Affiliation")))
+        {
+        cerr << "ERROR: Affiliation array missing" << endl;
+        ++errors;
+        }
+      if (!vtkStringArray::SafeDownCast(table->GetColumnByName("Alma Mater")))
+        {
+        cerr << "ERROR: Alma Mater array missing" << endl;
+        ++errors;
+        }
+      if (!vtkStringArray::SafeDownCast(table->GetColumnByName("Categories")))
+        {
+        cerr << "ERROR: Categories array missing" << endl;
+        ++errors;
+        }
+      if (!vtkIntArray::SafeDownCast(table->GetColumnByName("Age")))
+        {
+        cerr << "ERROR: Age array missing or not converted to int" << endl;
+        ++errors;
+        }
+      else
+        {
+        vtkIntArray* age = vtkIntArray::SafeDownCast(table->GetColumnByName("Age"));
+        int sum = 0;
+        for (vtkIdType i = 0; i < age->GetNumberOfTuples(); i++)
+          {
+          sum += age->GetValue(i);
+          }
+        if (sum != 181)
+          {
+          cerr << "ERROR: Age sum is incorrect" << endl;
+          ++errors;
+          }
+        }
+      if (!vtkDoubleArray::SafeDownCast(table->GetColumnByName("Coolness")))
+        {
+        cerr << "ERROR: Coolness array missing or not converted to double" << endl;
+        ++errors;
+        }
+      else
+        {
+        vtkDoubleArray* cool = vtkDoubleArray::SafeDownCast(table->GetColumnByName("Coolness"));
+        double sum = 0;
+        for (vtkIdType i = 0; i < cool->GetNumberOfTuples(); i++)
+          {
+          sum += cool->GetValue(i);
+          }
+        double eps = 10e-8;
+        double diff = (2.35 > sum) ? (2.35 - sum) : (sum - 2.35);
+        if (diff > eps)
+          {
+          cerr << "ERROR: Coolness sum is incorrect" << endl;
+          ++errors;
+          }
+        }
+      
+      return errors;
+
+    }
+
+    int WhitespaceAndEmptyCellsTest()
+    {
+      // Setup a table of string columns, which is to get converted to numeric
+      VTK_CREATE(vtkTable, inputTable);
+      VTK_CREATE(vtkStringArray, integerColumn);
+      integerColumn->SetName("IntegerColumn");
+      integerColumn->SetNumberOfTuples(2);
+      integerColumn->SetValue(0, " ");
+      integerColumn->SetValue(1, " 1 ");
+
+      VTK_CREATE(vtkStringArray, doubleColumn);
+      doubleColumn->SetName("DoubleColumn");
+      doubleColumn->SetNumberOfTuples(2);
+      doubleColumn->SetValue(0, " ");
+      doubleColumn->SetValue(1, " 1.1 ");
+
+      inputTable->AddColumn(integerColumn);
+      inputTable->AddColumn(doubleColumn);
+
+      // Setup the vtkStringToNumeric which is under test
+      VTK_CREATE(vtkStringToNumeric, numeric);
+      int const defaultIntValue = 100;
+      numeric->SetDefaultIntegerValue(defaultIntValue);
+      numeric->SetDefaultDoubleValue(vtkMath::Nan());
+      numeric->SetTrimWhitespacePriorToNumericConversion(true);
+      numeric->SetInput(inputTable);
+      numeric->Update();
+      vtkTable* table = vtkTable::SafeDownCast(numeric->GetOutput());
+      table->Dump();
+
+      cerr << "Testing handling whitespace and empty cells..." << endl;
+      int errors = 0;
+      if (!vtkIntArray::SafeDownCast(table->GetColumnByName("IntegerColumn")))
+        {
+        cerr << "ERROR: IntegerColumn array missing or not converted to int" << endl;
+        ++errors;
+        }
+      else
+        {
+        vtkIntArray* column = vtkIntArray::SafeDownCast(table->GetColumnByName("IntegerColumn"));
+        if (defaultIntValue != column->GetValue(0))
+          {
+              cerr << "ERROR: Empty cell value is: " << column->GetValue(0) << ". Expected: " << defaultIntValue;
+              ++errors;
+          }
+        if (1 != column->GetValue(1))
+          {
+              cerr << "ERROR: Cell with whitespace value is: " << column->GetValue(1) << ". Expected: 1";
+              ++errors;
+          }
+        }
+
+      if (!vtkDoubleArray::SafeDownCast(table->GetColumnByName("DoubleColumn")))
+        {
+        cerr << "ERROR: DoubleColumn array missing or not converted to double" << endl;
+        ++errors;
+        }
+      else
+        {
+        vtkDoubleArray* column = vtkDoubleArray::SafeDownCast(table->GetColumnByName("DoubleColumn"));
+        if (!vtkMath::IsNan(column->GetValue(0)))
+          {
+              cerr << "ERROR: Empty cell value is: " << column->GetValue(0) << ". Expected: " << vtkMath::Nan();
+              ++errors;
+          }
+        if (1.1 != column->GetValue(1))
+          {
+              cerr << "ERROR: Cell with whitespace value is: " << column->GetValue(1) << ". Expected: 1.1";
+              ++errors;
+          }
+        }
+      return errors;
+    }
+}
 int TestStringToNumeric(int argc, char* argv[])
 {
-  char* file = vtkTestUtilities::ExpandDataFileName(argc, argv,
-                                                    "Data/authors.csv");
-  
-  VTK_CREATE(vtkDelimitedTextReader, reader);
-  reader->SetFileName(file);
-  reader->SetHaveHeaders(true);
+    int errors = ArrayTypesTest(argc, argv);
+    errors += WhitespaceAndEmptyCellsTest();
 
-  delete [] file;
+    cerr << "...done testing" << endl;
+    cerr << errors << " errors found." << endl;
 
-  VTK_CREATE(vtkStringToNumeric, numeric);
-  numeric->SetInputConnection(reader->GetOutputPort());
-  numeric->Update();
-  
-  vtkTable* table = vtkTable::SafeDownCast(numeric->GetOutput());
-  
-  cerr << "Testing array types..." << endl;
-  int errors = 0;
-  if (!vtkStringArray::SafeDownCast(table->GetColumnByName("Author")))
-    {
-    cerr << "ERROR: Author array missing" << endl;
-    ++errors;
-    }
-  if (!vtkStringArray::SafeDownCast(table->GetColumnByName("Affiliation")))
-    {
-    cerr << "ERROR: Affiliation array missing" << endl;
-    ++errors;
-    }
-  if (!vtkStringArray::SafeDownCast(table->GetColumnByName("Alma Mater")))
-    {
-    cerr << "ERROR: Alma Mater array missing" << endl;
-    ++errors;
-    }
-  if (!vtkStringArray::SafeDownCast(table->GetColumnByName("Categories")))
-    {
-    cerr << "ERROR: Categories array missing" << endl;
-    ++errors;
-    }
-  if (!vtkIntArray::SafeDownCast(table->GetColumnByName("Age")))
-    {
-    cerr << "ERROR: Age array missing or not converted to int" << endl;
-    ++errors;
-    }
-  else
-    {
-    vtkIntArray* age = vtkIntArray::SafeDownCast(table->GetColumnByName("Age"));
-    int sum = 0;
-    for (vtkIdType i = 0; i < age->GetNumberOfTuples(); i++)
-      {
-      sum += age->GetValue(i);
-      }
-    if (sum != 181)
-      {
-      cerr << "ERROR: Age sum is incorrect" << endl;
-      ++errors;
-      }
-    }
-  if (!vtkDoubleArray::SafeDownCast(table->GetColumnByName("Coolness")))
-    {
-    cerr << "ERROR: Coolness array missing or not converted to double" << endl;
-    ++errors;
-    }
-  else
-    {
-    vtkDoubleArray* cool = vtkDoubleArray::SafeDownCast(table->GetColumnByName("Coolness"));
-    double sum = 0;
-    for (vtkIdType i = 0; i < cool->GetNumberOfTuples(); i++)
-      {
-      sum += cool->GetValue(i);
-      }
-    double eps = 10e-8;
-    double diff = (2.35 > sum) ? (2.35 - sum) : (sum - 2.35);
-    if (diff > eps)
-      {
-      cerr << "ERROR: Coolness sum is incorrect" << endl;
-      ++errors;
-      }
-    }
-
-  cerr << "Testing ForceDouble..." << endl;
-  numeric->ForceDoubleOn();
-  numeric->Update();
-  table = vtkTable::SafeDownCast(numeric->GetOutput());
-  if (!vtkDoubleArray::SafeDownCast(table->GetColumnByName("Age")))
-    {
-    cerr << "ERROR: ForceDouble did not force Age to double array" << endl;
-    ++errors;
-    }
-
-  cerr << "...done testing" << endl;
-  cerr << errors << " errors found." << endl;
-  
-  return errors;
+    return errors;
 }
 
