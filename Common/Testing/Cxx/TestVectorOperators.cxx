65e1b047391ebc6581c5589ff5c4d905e12e9977
David Gobbi <david.gobbi@gmail.com>
2011-12-13 14:58:54 -0700

    COMP: AIX is picky about include order for streams.
    Change-Id: I471bc9549fc6e34bef26e3e89040f2f07d16fc46

--- Common/Testing/Cxx/TestVectorOperators.cxx
+++ Common/Testing/Cxx/TestVectorOperators.cxx
@@ -13,9 +13,9 @@
 
 =========================================================================*/
 
+#include "vtkSetGet.h"
 #include "vtkVector.h"
 #include "vtkVectorOperators.h"
-#include "vtkSetGet.h"
 
 //----------------------------------------------------------------------------
 int TestVectorOperators(int, char*[])
