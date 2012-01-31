Kitware/VTK

commit 5c3682292d900f805c98f90ca41b405f721a7e2d

Apply patch to wrap ftgl in a namespace.

Utilities/ftgl/src/FTGL.h
@@ -5,6 +5,8 @@
 #include "vtkftglConfig.h"
 #endif
 
+#define FTGL_USE_NAMESPACE
+
 // To include debug memory manager by Paul Nettle (midnight@FluidStudios.com)
 // http://www.FluidStudios.com/publications.html
 // Get this code and use it. It will open your eyes:)

Rendering/vtkFreeTypeTools.cxx
@@ -31,6 +31,10 @@
 // The embedded fonts
 #include "fonts/vtkEmbeddedFonts.h"
 
+#ifdef FTGL_USE_NAMESPACE
+using namespace ftgl;
+#endif
+
 // Print debug info
 #define VTK_FTFC_DEBUG 0
 #define VTK_FTFC_DEBUG_CD 0

Rendering/vtkFreeTypeUtilities.cxx
@@ -32,6 +32,10 @@
 
 #include <sys/stat.h>
 
+#ifdef FTGL_USE_NAMESPACE
+using namespace ftgl;
+#endif
+
 // Print debug info
 
 #define VTK_FTFC_DEBUG 0

Rendering/vtkFreeTypeUtilities.h
@@ -32,6 +32,8 @@ class vtkTextProperty;
 
 // FreeType
 
+#include <FTGL.h>
+
 #include "vtk_freetype.h"  //since ft2build.h could be in the path
 #include FT_FREETYPE_H
 #include FT_GLYPH_H
@@ -42,7 +44,14 @@ class vtkTextProperty;
 # define VTK_FREETYPE_CACHING_SUPPORTED
 #endif
 
+#ifdef FTGL_USE_NAMESPACE
+namespace ftgl
+{
+#endif
 class FTFont;
+#ifdef FTGL_USE_NAMESPACE
+}
+#endif
 
 //----------------------------------------------------------------------------
 // Singleton cleanup
@@ -243,7 +252,11 @@ public:
   struct Entry
   {
     vtkTextProperty *TextProperty;
+#ifdef FTGL_USE_NAMESPACE
+    ftgl::FTFont *Font;
+#else
     FTFont *Font;
+#endif
     float LargestAscender;
     float LargestDescender;
   };

Rendering/vtkOpenGLFreeTypeTextMapper.cxx
@@ -33,6 +33,9 @@
 #include "vtk_gl2ps.h"
 #endif // VTK_USE_GL2PS
 
+#ifdef FTGL_USE_NAMESPACE
+using namespace ftgl;
+#endif
 
 //----------------------------------------------------------------------------
 // Print debug info

Rendering/vtkOpenGLFreeTypeTextMapper.h
@@ -26,9 +26,18 @@
 
 #include "vtkTextMapper.h"
 
+//#include <FTGL.h>
+//
+//#ifdef FTGL_USE_NAMESPACE
+//namespace ftgl
+//{
+//#endif
 //BTX
-class FTFont;
+//class FTFont;
 //ETX
+//#ifdef FTGL_USE_NAMESPACE
+//}
+//#endif
 
 class VTK_RENDERING_EXPORT vtkOpenGLFreeTypeTextMapper : public vtkTextMapper
 {

Utilities/ftgl/src/FTBitmapGlyph.cpp
@@ -3,6 +3,10 @@
   #include "mmgr.h"
 #endif
 
+#ifdef FTGL_USE_NAMESPACE
+namespace ftgl
+{
+#endif
 
 FTBitmapGlyph::FTBitmapGlyph( FT_Glyph _glyph)
 :  FTGlyph(),
@@ -110,3 +114,7 @@ float FTBitmapGlyph::Render( const FT_Vector& pen,
 
   return advance;
 }
+
+#ifdef FTGL_USE_NAMESPACE
+} // namespace ftgl
+#endif

Utilities/ftgl/src/FTBitmapGlyph.h
@@ -9,6 +9,10 @@
 #include "FTGL.h"
 #include  "FTGlyph.h"
 
+#ifdef FTGL_USE_NAMESPACE
+namespace ftgl
+{
+#endif
 
 /**
  * FTBitmapGlyph is a specialisation of FTGlyph for creating bitmaps.
@@ -72,6 +76,9 @@ class FTGL_EXPORT FTBitmapGlyph : public FTGlyph
 
 };
 
+#ifdef FTGL_USE_NAMESPACE
+} // namespace ftgl
+#endif
 
 #endif  //  __FTBitmapGlyph__
 

commit 1ce50f33037a6b8b15c2935a7733084e6bd991ef

Fix static compilation on windows.
This is a fix for an issue introduced in 5c3682292.
VTKFTGL is only defined inside of VTK/Utilities/ftgl, so Rendering was picking
up mismatching symbol definitions and executables using statically
compiled VTK libs would not link.

Change-Id: Ie766ca88e499d9f98dfa0f31f27a3f9aaf2ac3d8

Utilities/ftgl/src/FTGL.h
@@ -1,9 +1,9 @@
 #ifndef    __FTGL__
 #define    __FTGL__
 
-#if defined(VTKFTGL)
+//#if defined(VTKFTGL)
 #include "vtkftglConfig.h"
-#endif
+//#endif
 
 #define FTGL_USE_NAMESPACE
 