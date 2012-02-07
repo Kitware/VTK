31dd722a1c87a7eb0e69dbf38498d570e166c069
Brad King <brad.king@kitware.com>
2012-02-07 14:59:18 -0500

    Add VTK-specific local commit hooks
    Add ".hooks-config.bash" to tell the main hooks to chain to our hooks.
    Add a pre-commit hook that cooperates with SetupForDevelopment.sh to
    ensure the user has run the most recent hook setup script.  Add a
    prepare-commit-msg hook to mention the "WIP:" prefix in the standard
    commented commit message instructions.
    
    Change-Id: I034afb64aa4fbbcaafd70d8a57493495d8736181

--- Utilities/SetupForDevelopment.sh
+++ Utilities/SetupForDevelopment.sh
@@ -11,3 +11,7 @@ Utilities/Scripts/GitTips.sh
 # Rebase master by default
 git config rebase.stat true
 git config branch.master.rebase true
+
+# Record the version of this setup so Scripts/pre-commit can check it.
+SetupForDevelopment_VERSION=1
+git config hooks.SetupForDevelopment ${SetupForDevelopment_VERSION}
