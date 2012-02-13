97502f2b934b03093baf1a2bc7539b88dc82d2f9
George Zagaris <george.zagaris@kitware.com>
2012-02-13 17:27:35 -0500

    ENH: vtkLevelIdScalars for backwards compatibility
    Added vtkLevelIdScalars filter, an empty class that inherits
    from vtkOverlappingAMRLevelIdScalars for backwards compatibility.
    
    Change-Id: I0ac7426ea2fff014ca2c78a57aa04c8db1bbe32e

---
+++ Graphics/vtkLevelIdScalars.cxx
@@ -0,0 +1,37 @@
+/*=========================================================================
+
+ Program:   Visualization Toolkit
+ Module:    vtkLevelIdScalars.cxx
+
+ Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+ All rights reserved.
+ See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+ This software is distributed WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ PURPOSE.  See the above copyright notice for more information.
+
+ =========================================================================*/
+#include "vtkLevelIdScalars.h"
+#include "vtkObjectFactory.h"
+
+
+vtkStandardNewMacro(vtkLevelIdScalars);
+
+//------------------------------------------------------------------------------
+vtkLevelIdScalars::vtkLevelIdScalars()
+{
+
+}
+
+//------------------------------------------------------------------------------
+vtkLevelIdScalars::~vtkLevelIdScalars()
+{
+
+}
+
+//------------------------------------------------------------------------------
+void vtkLevelIdScalars::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
