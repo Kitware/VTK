d938f17ae493c46ed1af59dba90727411e9ff4ba
George Zagaris <george.zagaris@kitware.com>
2012-03-02 16:31:20 -0500

    BUGFIX: Revert to old code for blanked cells
    Revert to old function for non-visibile cells:
    (1) A cell is non-visible iff: (a) it has been marked as
    non-visible or any of it's points are not visible.
    
    (2) Return empty cell if the cell is not visible
    
    Change-Id: If20ffee6e291f5c03fc1777c267813fa470c2086

--- Filtering/vtkUniformGrid.cxx
+++ Filtering/vtkUniformGrid.cxx
@@ -233,12 +233,12 @@ vtkCell *vtkUniformGrid::GetCell(vtkIdType cellId)
     }
 
   // see whether the cell is blanked
-//  if ( (this->PointVisibility->IsConstrained() ||
-//        this->CellVisibility->IsConstrained())
-//       && !this->IsCellVisible(cellId) )
-//    {
-//    return this->GetEmptyCell();
-//    }
+  if ( (this->PointVisibility->IsConstrained() ||
+        this->CellVisibility->IsConstrained())
+       && !this->IsCellVisible(cellId) )
+    {
+    return this->GetEmptyCell();
+    }
 
   switch (this->GetDataDescription())
     {
@@ -934,99 +934,97 @@ unsigned char vtkUniformGrid::IsPointVisible(vtkIdType pointId)
 // Return non-zero if the specified cell is visible (i.e., not blanked)
 unsigned char vtkUniformGrid::IsCellVisible(vtkIdType cellId)
 {
+  if ( !this->CellVisibility->IsVisible(cellId) )
+    {
+    return 0;
+    }
 
-  return( this->CellVisibility->IsVisible( cellId )  );
-//  if ( !this->CellVisibility->IsVisible(cellId) )
-//    {
-//    return 0;
-//    }
-//
-//  int iMin, iMax, jMin, jMax, kMin, kMax;
-//  int *dims = this->GetDimensions();
-//
-//  iMin = iMax = jMin = jMax = kMin = kMax = 0;
-//
-//  switch (this->GetDataDescription())
-//    {
-//    case VTK_EMPTY:
-//      return 0;
-//
-//    case VTK_SINGLE_POINT: // cellId can only be = 0
-//      break;
-//
-//    case VTK_X_LINE:
-//      iMin = cellId;
-//      iMax = cellId + 1;
-//      break;
-//
-//    case VTK_Y_LINE:
-//      jMin = cellId;
-//      jMax = cellId + 1;
-//      break;
-//
-//    case VTK_Z_LINE:
-//      kMin = cellId;
-//      kMax = cellId + 1;
-//      break;
-//
-//    case VTK_XY_PLANE:
-//      iMin = cellId % (dims[0]-1);
-//      iMax = iMin + 1;
-//      jMin = cellId / (dims[0]-1);
-//      jMax = jMin + 1;
-//      break;
-//
-//    case VTK_YZ_PLANE:
-//      jMin = cellId % (dims[1]-1);
-//      jMax = jMin + 1;
-//      kMin = cellId / (dims[1]-1);
-//      kMax = kMin + 1;
-//      break;
-//
-//    case VTK_XZ_PLANE:
-//      iMin = cellId % (dims[0]-1);
-//      iMax = iMin + 1;
-//      kMin = cellId / (dims[0]-1);
-//      kMax = kMin + 1;
-//      break;
-//
-//    case VTK_XYZ_GRID:
-//      iMin = cellId % (dims[0] - 1);
-//      iMax = iMin + 1;
-//      jMin = (cellId / (dims[0] - 1)) % (dims[1] - 1);
-//      jMax = jMin + 1;
-//      kMin = cellId / ((dims[0] - 1) * (dims[1] - 1));
-//      kMax = kMin + 1;
-//      break;
-//    }
-//
-//  // Extract point ids
-//  // Ids are relative to extent min.
-//  vtkIdType idx[8];
-//  vtkIdType npts = 0;
-//  int loc[3];
-//  int d01 = dims[0]*dims[1];
-//  for (loc[2]=kMin; loc[2]<=kMax; loc[2]++)
-//    {
-//    for (loc[1]=jMin; loc[1]<=jMax; loc[1]++)
-//      {
-//      for (loc[0]=iMin; loc[0]<=iMax; loc[0]++)
-//        {
-//        idx[npts] = loc[0] + loc[1]*dims[0] + loc[2]*d01;
-//        npts++;
-//        }
-//      }
-//    }
-//
-//  for (int i=0; i<npts; i++)
-//    {
-//    if ( !this->IsPointVisible(idx[i]) )
-//      {
-//      return 0;
-//      }
-//    }
-//
-//  return 1;
+  int iMin, iMax, jMin, jMax, kMin, kMax;
+  int *dims = this->GetDimensions();
+
+  iMin = iMax = jMin = jMax = kMin = kMax = 0;
+
+  switch (this->GetDataDescription())
+    {
+    case VTK_EMPTY:
+      return 0;
+
+    case VTK_SINGLE_POINT: // cellId can only be = 0
+      break;
+
+    case VTK_X_LINE:
+      iMin = cellId;
+      iMax = cellId + 1;
+      break;
+
+    case VTK_Y_LINE:
+      jMin = cellId;
+      jMax = cellId + 1;
+      break;
+
+    case VTK_Z_LINE:
+      kMin = cellId;
+      kMax = cellId + 1;
+      break;
+
+    case VTK_XY_PLANE:
+      iMin = cellId % (dims[0]-1);
+      iMax = iMin + 1;
+      jMin = cellId / (dims[0]-1);
+      jMax = jMin + 1;
+      break;
+
+    case VTK_YZ_PLANE:
+      jMin = cellId % (dims[1]-1);
+      jMax = jMin + 1;
+      kMin = cellId / (dims[1]-1);
+      kMax = kMin + 1;
+      break;
+
+    case VTK_XZ_PLANE:
+      iMin = cellId % (dims[0]-1);
+      iMax = iMin + 1;
+      kMin = cellId / (dims[0]-1);
+      kMax = kMin + 1;
+      break;
+
+    case VTK_XYZ_GRID:
+      iMin = cellId % (dims[0] - 1);
+      iMax = iMin + 1;
+      jMin = (cellId / (dims[0] - 1)) % (dims[1] - 1);
+      jMax = jMin + 1;
+      kMin = cellId / ((dims[0] - 1) * (dims[1] - 1));
+      kMax = kMin + 1;
+      break;
+    }
+
+  // Extract point ids
+  // Ids are relative to extent min.
+  vtkIdType idx[8];
+  vtkIdType npts = 0;
+  int loc[3];
+  int d01 = dims[0]*dims[1];
+  for (loc[2]=kMin; loc[2]<=kMax; loc[2]++)
+    {
+    for (loc[1]=jMin; loc[1]<=jMax; loc[1]++)
+      {
+      for (loc[0]=iMin; loc[0]<=iMax; loc[0]++)
+        {
+        idx[npts] = loc[0] + loc[1]*dims[0] + loc[2]*d01;
+        npts++;
+        }
+      }
+    }
+
+  for (int i=0; i<npts; i++)
+    {
+    if ( !this->IsPointVisible(idx[i]) )
+      {
+      return 0;
+      }
+    }
+
+  return 1;
 }
 
 //----------------------------------------------------------------------------
