be670cd466545ad6c655cebf3c24fc34171d23bc
Philippe Pébay <philippe.pebay@kitware.com>
2012-03-20 22:50:18 +0100

    Added a HyperTree fractal source written by Charles Law
    Change-Id: Ide6c35c94dc8bcbf441e87258f2a63ac1d00e34d

---
+++ Graphics/vtkHyperTreeFractalSource.cxx
@@ -0,0 +1,180 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkHyperTreeFractalSource.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkHyperTreeFractalSource.h"
+
+#include "vtkHyperTree.h"
+#include "vtkHyperTreeCursor.h"
+#include "vtkObjectFactory.h"
+#include "vtkInformationVector.h"
+#include "vtkInformation.h"
+#include <assert.h>
+#include "vtkMath.h"
+#include "vtkPointData.h"
+#include "vtkDataArray.h"
+#include "vtkUnsignedCharArray.h"
+#include "vtkFloatArray.h"
+#include "vtkGarbageCollector.h"
+
+vtkStandardNewMacro(vtkHyperTreeFractalSource);
+
+//----------------------------------------------------------------------------
+vtkHyperTreeFractalSource::vtkHyperTreeFractalSource()
+{
+  this->AxisBranchFactor = 2;
+  this->MaximumLevel = 1;
+  this->Dimension = 3;
+  this->Dual = false;
+}
+
+//----------------------------------------------------------------------------
+vtkHyperTreeFractalSource::~vtkHyperTreeFractalSource()
+{
+}
+
+
+
+//----------------------------------------------------------------------------
+vtkHyperTree* vtkHyperTreeFractalSource::NewHyperTree()
+{
+  vtkHyperTree* output = vtkHyperTree::New();
+
+  output->SetDimension(this->Dimension);
+  output->SetAxisBranchFactor(this->AxisBranchFactor);
+  int ii;
+  for (ii = 0; ii < 3; ++ii)
+    {
+    this->Size[ii] = 1.0;
+    this->Origin[ii] = 0.0;
+    }
+  if (this->Dimension == 2)
+    {
+    this->Size[2] = 0.0;
+    }
+  output->SetSize(this->Size);
+  output->SetOrigin(this->Origin);
+  
+  vtkFloatArray *scalars=vtkFloatArray::New();
+  scalars->SetNumberOfComponents(1);
+
+  vtkIdType fact = 1;
+  for (int i = 1; i < this->MaximumLevel; ++i)
+    {
+    fact *= this->AxisBranchFactor;
+    }
+  vtkIdType maxNumberOfCells=fact*fact*fact;
+
+  scalars->Allocate(maxNumberOfCells/fact);
+  scalars->SetName("Test");
+  output->GetLeafData()->SetScalars(scalars);
+  scalars->UnRegister(this);
+  
+  vtkHyperTreeCursor *cursor=output->NewCellCursor();
+  cursor->ToRoot();
+  
+  int idx[3];
+  idx[0] = idx[1] = idx[2] = 0;
+  this->Subdivide(cursor,1,output, this->Origin,this->Size, idx);
+  
+  cursor->UnRegister(this);
+
+  output->SetDualGridFlag(this->Dual);
+
+  scalars->Squeeze();
+  assert("post: dataset_and_data_size_match" && output->CheckAttributes()==0);
+  
+  return output;
+}
+
+//----------------------------------------------------------------------------
+void vtkHyperTreeFractalSource::Subdivide(vtkHyperTreeCursor *cursor,
+                                            int level, vtkHyperTree *output,
+                                            double* origin, double* size,
+                                            int idx[3])
+{
+  // Determine whether to subdivide.
+  int subdivide = 1;
+
+  if (idx[0] != 0 || idx[1] != 0 || idx[2] != 0)
+    {
+    subdivide = 0;
+    }
+  
+  // Check for hard coded minimum and maximum level restrictions.
+  if (level >= this->MaximumLevel)
+    {
+    subdivide = 0;
+    }
+  
+  if (subdivide)
+    {
+    output->SubdivideLeaf(cursor);
+    double newOrigin[3];
+    double newSize[3];
+    newSize[0] = size[0] / this->AxisBranchFactor;
+    newSize[1] = size[1] / this->AxisBranchFactor;
+    newSize[2] = size[2] / this->AxisBranchFactor;
+    
+    // Now traverse to children.
+    int xDim, yDim, zDim;
+    xDim = yDim = zDim = 1;
+    if (this->Dimension == 1)
+      {
+      xDim = this->AxisBranchFactor;
+      }
+    else if (this->Dimension == 2)
+      {
+      xDim = yDim = this->AxisBranchFactor;
+      }
+    else if (this->Dimension == 3)
+      {
+      xDim = yDim = zDim = this->AxisBranchFactor;
+      }
+    int childIdx = 0;
+    int newIdx[3];
+    for (int z = 0; z < zDim; ++z)
+      {
+      newIdx[2] = idx[2] * zDim + z;
+      for (int y = 0; y < yDim; ++y)
+        {
+        newIdx[1] = idx[1] * yDim + y;
+        for (int x = 0; x < xDim; ++x)
+          {
+          newIdx[0] = idx[0] * xDim + x;
+          newOrigin[0] = origin[0] + static_cast<double>(x)*newSize[0];
+          newOrigin[1] = origin[1] + static_cast<double>(y)*newSize[1];
+          newOrigin[2] = origin[2] + static_cast<double>(z)*newSize[2];
+          cursor->ToChild(childIdx);
+          this->Subdivide(cursor,level+1,output, 
+                          newOrigin,newSize,newIdx);
+          cursor->ToParent();
+          ++childIdx;
+          }
+        }
+      }
+    }
+  else
+    {
+    float val = idx[0] + idx[1] + idx[2];
+    // Weight cell values for smoother iso surface.
+    vtkIdType id=cursor->GetLeafId();
+    output->GetLeafData()->GetScalars()->InsertTuple1(id,val);
+    }
+}
+
+//-----------------------------------------------------------------------------
+void vtkHyperTreeFractalSource::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
---
+++ Graphics/vtkHyperTreeFractalSource.h
@@ -0,0 +1,77 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkHyperTreeFractalSource.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkHyperTreeFractalSource - Create an Tree from a fractal.
+// hyperTree
+// .SECTION Description
+//
+// .SECTION See Also
+// vtkHyperTreeSampleFunction
+
+#ifndef __vtkHyperTreeFractalSource_h
+#define __vtkHyperTreeFractalSource_h
+
+#include "vtkObject.h"
+
+class vtkImplicitFunction;
+class vtkHyperTree;
+class vtkHyperTreeCursor;
+
+class VTK_EXPORT vtkHyperTreeFractalSource : public vtkObject
+{
+public:
+  vtkTypeMacro(vtkHyperTreeFractalSource,vtkObject);
+  void PrintSelf(ostream& os, vtkIndent indent);
+  
+  static vtkHyperTreeFractalSource *New();
+
+  vtkSetMacro(MaximumLevel, int);
+  vtkGetMacro(MaximumLevel, int);
+
+  vtkSetMacro(Dimension, int);
+  vtkGetMacro(Dimension, int);
+
+  vtkSetMacro(AxisBranchFactor, int);
+  vtkGetMacro(AxisBranchFactor, int);
+
+  vtkSetMacro(Dual, int);
+  vtkGetMacro(Dual, int);
+  vtkBooleanMacro(Dual, int);
+
+  // Bypass the pipeline
+  vtkHyperTree* NewHyperTree();
+
+protected:
+  vtkHyperTreeFractalSource();
+  ~vtkHyperTreeFractalSource();
+
+  void Subdivide(vtkHyperTreeCursor *cursor,
+                 int level, vtkHyperTree *output,
+                 double* origin, double* size,
+                 int idx[3]);
+
+  int MaximumLevel;
+  int Dimension;
+  int AxisBranchFactor;
+  double Size[3];
+  double Origin[3];
+
+  int Dual;
+
+private:
+  vtkHyperTreeFractalSource(const vtkHyperTreeFractalSource&);  // Not implemented.
+  void operator=(const vtkHyperTreeFractalSource&);  // Not implemented.
+};
+
+#endif
