072cf0d6bcc8d47cd837b90e0b830ac7eccbd1ac
Pavel Pokutnev <pavel.pokutnev@googlemail.com>
2012-03-22 23:29:47 +0100

    Fix for MFC compatibility issue with bad_alloc
    To be able to catch allocation failures, when VTK is running in the context of other framework (e.g. MFC), we should use (...) in the catch handler instead of std::bad_alloc. If hosting framework implements its own operator new, it's possible that it will throw own defined exception and not the standard std::bad_alloc! In the case of MFC this is CMemoryException, so it would not be handled correctly and crash.
    
    BUG ID 0013018: http://vtk.org/Bug/view.php?id=13018
    
    Change-Id: Ic47b1666116a4299d85e9b86a5d1b13a72822a94

--- VolumeRendering/vtkFixedPointVolumeRayCastMapper.cxx
+++ VolumeRendering/vtkFixedPointVolumeRayCastMapper.cxx
@@ -2849,11 +2849,17 @@ void vtkFixedPointVolumeRayCastMapper::ComputeGradients( vtkVolume *vol )
 
   // first, attempt contiguous memory. If this fails, then go
   // for non-contiguous
+  // NOTE: To be able to catch allocation failures, when VTK is running in the
+  //       context of other framework (e.g. MFC), we should use (...) in the
+  //       catch handler instead of std::bad_alloc. If hosting framework implements
+  //       its own operator new, it's possible that it will throw own defined exception
+  //       and not the standard std::bad_alloc! In the case of MFC this is CMemoryException,
+  //       so it would not be handled correctly and crash.
   try
     {
     this->ContiguousGradientNormal = new unsigned short [numSlices * sliceSize];
     }
-  catch(std::bad_alloc &)
+  catch(...)
     {
     this->ContiguousGradientNormal = NULL;
     }
@@ -2861,7 +2867,7 @@ void vtkFixedPointVolumeRayCastMapper::ComputeGradients( vtkVolume *vol )
     {
     this->ContiguousGradientMagnitude = new unsigned char [numSlices * sliceSize];
     }
-  catch(std::bad_alloc &)
+  catch(...)
     {
     this->ContiguousGradientMagnitude = NULL;
     }
