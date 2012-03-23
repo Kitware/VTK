59e9ec14119837252bd02a738a51233b00f4db2d
Philippe Pébay <philippe.pebay@kitware.com>
2012-03-23 18:26:42 +0100

    Fixed memory leaks
    Change-Id: Ibee9ac969e2e37346962b88af035c43c01a9f78e

--- Graphics/Testing/Cxx/TestHyperTree.cxx
+++ Graphics/Testing/Cxx/TestHyperTree.cxx
@@ -13,15 +13,18 @@
 #include "vtkHyperTree.h"
 #include "vtkHyperTreeFractalSource.h"
 
+#include "vtkContourFilter.h"
 #include "vtkCutter.h"
+#include "vtkDataSetMapper.h"
+#include "vtkGeometryFilter.h"
+#include "vtkNew.h"
 #include "vtkPlane.h"
 #include "vtkShrinkFilter.h"
-#include "vtkContourFilter.h"
-
-#include "vtkThreshold.h"
-
-
 #include "vtkPolyDataWriter.h"
+#include "vtkRegressionTestImage.h"
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
 #include "vtkUnstructuredGridWriter.h"
 #include "vtkXMLUnstructuredGridWriter.h"
 #include "vtkUnstructuredGrid.h"
@@ -29,25 +32,26 @@
 
 int TestHyperTree(int argc, char** argv)
 {
-  vtkHyperTreeFractalSource* fractal = vtkHyperTreeFractalSource::New();
+  vtkNew<vtkHyperTreeFractalSource> fractal;
   fractal->SetMaximumLevel(3);
   fractal->DualOn();
   fractal->SetDimension(3);
   fractal->SetAxisBranchFactor(3);
   vtkHyperTree* tree = fractal->NewHyperTree();
 
-  vtkCutter* cut = vtkCutter::New();
-  vtkPlane* plane = vtkPlane::New();
+  vtkNew<vtkCutter> cut;
+  vtkNew<vtkPlane> plane;
   plane->SetOrigin(0.5, 0.5, 0.3333333);
   plane->SetNormal(0,0,1);
   cut->SetInputData(tree);
-  cut->SetCutFunction(plane);
+  cut->SetCutFunction(plane.GetPointer());
   vtkPolyDataWriter* writer = vtkPolyDataWriter::New();
   writer->SetFileName("./hyperTreeCut.vtk");
   writer->SetInputConnection(cut->GetOutputPort());
   writer->Write();
+  writer->Delete();
   
-  vtkContourFilter *contour = vtkContourFilter::New();
+  vtkNew<vtkContourFilter> contour;
   contour->SetInputData( tree );
   contour->SetNumberOfContours( 2 );
   contour->SetValue( 0, 2. );
@@ -58,14 +62,46 @@ int TestHyperTree(int argc, char** argv)
   writer2->SetFileName("./hyperTreeContour.vtk");
   writer2->SetInputConnection(contour->GetOutputPort());
   writer2->Write();
+  writer2->Delete();
 
-  vtkShrinkFilter *shrink = vtkShrinkFilter::New();
+  vtkNew<vtkShrinkFilter> shrink;
   shrink->SetInputData(tree);
   shrink->SetShrinkFactor(0.7);
   vtkUnstructuredGridWriter* writer3 = vtkUnstructuredGridWriter::New();
   writer3->SetFileName("./hyperTreeShrink.vtk");
   writer3->SetInputConnection(shrink->GetOutputPort());
   writer3->Write();
+  writer3->Delete();
 
+  vtkNew<vtkDataSetMapper> treeMapper;
+  treeMapper->SetInputConnection( shrink->GetOutputPort() );
+  vtkNew<vtkActor> treeActor;
+  treeActor->SetMapper( treeMapper.GetPointer() );
+
+  // Create a renderer, add actors to it
+  vtkNew<vtkRenderer> ren1;
+  ren1->AddActor( treeActor.GetPointer() );
+  ren1->SetBackground( 1., 1., 1. );
+
+  // Create a renderWindow
+  vtkNew<vtkRenderWindow> renWin;
+  renWin->AddRenderer( ren1.GetPointer() );
+  renWin->SetSize( 300, 300 );
+  renWin->SetMultiSamples( 0 );
+  
+  // Create interactor
+  vtkNew<vtkRenderWindowInteractor> iren;
+  iren->SetRenderWindow( renWin.GetPointer() );
+
+  // Render and test
+  renWin->Render();
+
+  int retVal = vtkRegressionTestImage( renWin.GetPointer() );
+  if ( retVal == vtkRegressionTester::DO_INTERACTOR)
+    {
+    iren->Start();
+    }
+
+  tree->Delete();
   return 0;
 }
