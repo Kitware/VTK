11a711ef2084686d048de820dc66787851be81bf
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2012-04-10 10:37:44 -0400

    Moved Hybrid rendering code into its own module
    This removes the hard OpenGL dependency from the FiltersHybrid module.
    
    Change-Id: I5bffaa07d2ef69be99ce05b8c8ab31833f34d7d0

--- Filters/Hybrid/module.cmake
+++ Filters/Hybrid/module.cmake
@@ -3,11 +3,9 @@ vtk_module(vtkFiltersHybrid
     StandAlone
   DEPENDS
     vtkImagingSources
-    vtkRenderingOpenGL # This should not be allowed - FIXME - move classes.
     vtkFiltersGeneral
-  COMPILE_DEPENDS
-    vtkUtilitiesEncodeString # This is only here as GL is in this module. FIXME.
   TEST_DEPENDS
+    vtkRenderingOpenGL
     vtkImagingCore
     vtkTestingRendering
   )
