098bb19afcff15870e1bd1c64d45991d5e38d795
David Gobbi <david.gobbi@gmail.com>
2012-04-11 13:15:48 -0600

    COMP: Remove unused vtkEllipsoidSource header from vtkImageConvolve.
    Change-Id: Ic87e7bcbf4812b65123a4b56b5836d22d0c230ac

--- Imaging/General/vtkImageConvolve.cxx
+++ Imaging/General/vtkImageConvolve.cxx
@@ -14,7 +14,6 @@
 =========================================================================*/
 #include "vtkImageConvolve.h"
 #include "vtkImageData.h"
-#include "vtkImageEllipsoidSource.h"
 #include "vtkInformation.h"
 #include "vtkInformationVector.h"
 #include "vtkObjectFactory.h"
