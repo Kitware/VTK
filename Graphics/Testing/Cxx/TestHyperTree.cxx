3fcf63077addb8b7eed0291df4b16b7f5fa413ab
Philippe Pébay <philippe.pebay@kitware.com>
2012-04-12 10:25:53 +0200

    Test with 3-D HT source
    Change-Id: I705b90280c7891fe6f46946f696dd91bbdf8ee12

--- Graphics/Testing/Cxx/TestHyperTree.cxx
+++ Graphics/Testing/Cxx/TestHyperTree.cxx
@@ -35,7 +35,7 @@ int TestHyperTree( int argc, char** argv )
   vtkNew<vtkHyperTreeFractalSource> fractal;
   fractal->SetMaximumLevel( 3 );
   fractal->DualOn();
-  fractal->SetGridSize( 2, 1, 1 );
+  fractal->SetGridSize( 3, 2, 2 );
   fractal->SetDimension( 3 );
   fractal->SetAxisBranchFactor( 3 );
   vtkHyperTreeGrid* tree = fractal->NewHyperTreeGrid();
