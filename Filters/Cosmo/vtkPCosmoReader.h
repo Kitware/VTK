aa8bda15e940beaf445f141011a5ad3ba314a859
Chris Harris <chris.harris@kitware.com>
2012-05-04 09:40:02 -0400

    Update export headers after Cosmo move
    Change-Id: I98f1d66f627808ceea01918e19aa63e108613bad

--- Filters/Cosmo/vtkPCosmoReader.h
+++ Filters/Cosmo/vtkPCosmoReader.h
@@ -83,13 +83,13 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef __vtkPCosmoReader_h
 #define __vtkPCosmoReader_h
 
-#include "vtkParallelIOModule.h" // For export macro
+#include "vtkFiltersCosmoModule.h" // For export macro
 #include "vtkUnstructuredGridAlgorithm.h"
 
 class vtkMultiProcessController;
 class vtkStdString;
 
-class VTKPARALLELIO_EXPORT vtkPCosmoReader : public vtkUnstructuredGridAlgorithm
+class VTKFILTERSCOSMO_EXPORT vtkPCosmoReader : public vtkUnstructuredGridAlgorithm
 {
 public:
   static vtkPCosmoReader *New();
