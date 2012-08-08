dacb634f9e77bfdf226290ac76ba69a534615bd7
Brad King <brad.king@kitware.com>
2012-08-08 13:16:29 -0400

    vtknetcdf: Fix NULL filename case in NC_check_file_type
    Port fix from commit 1ed55f13 (Handle case of null filename, 2011-12-08)
    to new netcdf import.
    
    Change-Id: I00fc17789feb22adacce2fd762034ec32dfdcf49

--- ThirdParty/netcdf/vtknetcdf/libdispatch/file.c
+++ ThirdParty/netcdf/vtknetcdf/libdispatch/file.c
@@ -46,6 +46,8 @@ NC_check_file_type(const char *path, int use_parallel, void *mpi_info,
       FILE *fp;
       int i;
 
+      if (!path)
+	 return 0;
       if (!(fp = fopen(path, "r")))
 	 return errno;
       i = fread(magic, MAGIC_NUMBER_LEN, 1, fp);
