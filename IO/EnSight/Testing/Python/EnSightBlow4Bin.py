d54ee8ccb41e8dc59242fd33f75b28c96ae71a66
Nikhil Shetty <nikhil.shetty@kitware.com>
2012-08-20 11:39:32 -0400

    Adding IOEnSight python tests
    Change-Id: Id372fcf924015bbec70b42a66c342ee782730e5b

---
+++ IO/EnSight/Testing/Python/EnSightBlow4Bin.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import vtk
+from vtk.test import Testing
+from vtk.util.misc import vtkGetDataRoot
+VTK_DATA_ROOT = vtkGetDataRoot()
+
+# create a rendering window and renderer
+ren1 = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren1)
+renWin.StereoCapableWindowOn()
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+reader = vtk.vtkGenericEnSightReader()
+# Make sure all algorithms use the composite data pipeline
+cdp = vtk.vtkCompositeDataPipeline()
+reader.SetDefaultExecutivePrototype(cdp)
+reader.SetCaseFileName("" + str(VTK_DATA_ROOT) + "/Data/EnSight/blow4_bin.case")
+reader.SetTimeValue(1.0)
+geom = vtk.vtkGeometryFilter()
+geom.SetInputConnection(reader.GetOutputPort())
+mapper = vtk.vtkHierarchicalPolyDataMapper()
+mapper.SetInputConnection(geom.GetOutputPort())
+mapper.SetColorModeToMapScalars()
+mapper.SetScalarModeToUsePointFieldData()
+mapper.ColorByArrayComponent("displacement",0)
+mapper.SetScalarRange(0,2.08)
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+# assign our actor to the renderer
+ren1.AddActor(actor)
+# enable user interface interactor
+iren.Initialize()
+ren1.GetActiveCamera().SetPosition(99.3932,17.6571,-22.6071)
+ren1.GetActiveCamera().SetFocalPoint(3.5,12,1.5)
+ren1.GetActiveCamera().SetViewAngle(30)
+ren1.GetActiveCamera().SetViewUp(0.239617,-0.01054,0.97081)
+ren1.ResetCameraClippingRange()
+renWin.Render()
+# prevent the tk window from showing up then start the event loop
+reader.SetDefaultExecutivePrototype(None)
+# --- end of script --
---
+++ IO/EnSight/Testing/Python/EnSightMandelbrot.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import vtk
+from vtk.test import Testing
+from vtk.util.misc import vtkGetDataRoot
+VTK_DATA_ROOT = vtkGetDataRoot()
+
+# create a rendering window and renderer
+ren1 = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren1)
+renWin.StereoCapableWindowOn()
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+reader1 = vtk.vtkEnSightMasterServerReader()
+# Make sure all algorithms use the composite data pipeline
+cdp = vtk.vtkCompositeDataPipeline()
+reader1.SetDefaultExecutivePrototype(cdp)
+reader1.SetCaseFileName("" + str(VTK_DATA_ROOT) + "/Data/EnSight/mandelbrot.sos")
+reader1.SetCurrentPiece(0)
+geom0 = vtk.vtkGeometryFilter()
+geom0.SetInputConnection(reader1.GetOutputPort())
+mapper0 = vtk.vtkHierarchicalPolyDataMapper()
+mapper0.SetInputConnection(geom0.GetOutputPort())
+mapper0.SetColorModeToMapScalars()
+mapper0.SetScalarModeToUsePointFieldData()
+mapper0.ColorByArrayComponent("Iterations",0)
+mapper0.SetScalarRange(0,112)
+actor0 = vtk.vtkActor()
+actor0.SetMapper(mapper0)
+reader2 = vtk.vtkEnSightMasterServerReader()
+reader2.SetCaseFileName("" + str(VTK_DATA_ROOT) + "/Data/EnSight/mandelbrot.sos")
+reader2.SetCurrentPiece(1)
+geom2 = vtk.vtkGeometryFilter()
+geom2.SetInputConnection(reader2.GetOutputPort())
+mapper2 = vtk.vtkHierarchicalPolyDataMapper()
+mapper2.SetInputConnection(geom2.GetOutputPort())
+mapper2.SetColorModeToMapScalars()
+mapper2.SetScalarModeToUsePointFieldData()
+mapper2.ColorByArrayComponent("Iterations",0)
+mapper2.SetScalarRange(0,112)
+actor2 = vtk.vtkActor()
+actor2.SetMapper(mapper2)
+# assign our actor to the renderer
+ren1.AddActor(actor0)
+ren1.AddActor(actor2)
+# enable user interface interactor
+iren.Initialize()
+# prevent the tk window from showing up then start the event loop
+reader1.SetDefaultExecutivePrototype(None)
+# --- end of script --
