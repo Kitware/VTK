034bcdbaf02e12e0f090532ddace7b62aa7c4993
Zack Galbreath <zack.galbreath@kitware.com>
2012-08-23 12:09:35 -0400

    documentation, automatic text resizing, support for coloring string columns
    Updated documentation in the header file.
    
    Re-worked the font size issue in light of Jeff Baumes' improvements to this
    functionality.  Font size now scales up or down as the zoom level changes.
    
    Also re-worked the way vtkLookupTable is used.  We now maintain a separate
    lookup table per column, so each column is colored according to its own
    range of data.  Also added functionality to color string columns in addition
    to numerical columns.
    
    Change-Id: I7e814656d2f759e7eaacdb1a898c17cf27d5e7ad

--- Views/Infovis/vtkTreeHeatmapItem.cxx
+++ Views/Infovis/vtkTreeHeatmapItem.cxx
@@ -40,6 +40,7 @@ vtkCxxSetObjectMacro(vtkTreeHeatmapItem, Table, vtkTable);
 
 vtkTreeHeatmapItem::vtkTreeHeatmapItem()
 {
+  this->TreeHeatmapBuildTime = 0;
   this->Tree = 0;
   this->Table = 0;
   this->Multiplier = 100.0;
@@ -55,6 +56,12 @@ vtkTreeHeatmapItem::~vtkTreeHeatmapItem()
   if (this->Table)
     {
     this->Table->Delete();
+    }
+
+  while (!this->LookupTables.empty())
+    {
+    this->LookupTables.back()->Delete();
+    this->LookupTables.pop_back();
     }
 }
 
@@ -82,7 +89,6 @@ bool vtkTreeHeatmapItem::IsDirty()
     }
   if (this->Tree->GetMTime() > this->TreeHeatmapBuildTime ||
       this->Table->GetMTime() > this->GetMTime())
-  
     {
     return true;
     }
@@ -102,7 +108,7 @@ void vtkTreeHeatmapItem::RebuildBuffers()
   this->LayoutTree = vtkTree::SafeDownCast(this->Layout->GetOutput());
 
   this->ComputeMultiplier();
-  this->InitializeLookupTable();
+  this->InitializeLookupTables();
 
   if(this->Tree->GetMTime() > this->Table->GetMTime())
     {
@@ -139,17 +145,28 @@ void vtkTreeHeatmapItem::ComputeMultiplier()
     }
 }
 
-void vtkTreeHeatmapItem::InitializeLookupTable()
+void vtkTreeHeatmapItem::InitializeLookupTables()
 {
-  double min = DBL_MAX;
-  double max = DBL_MIN;
-  int numberOfValues = (this->Table->GetNumberOfColumns() -1) *
-                       this->Table->GetNumberOfRows();
-      
-  for (vtkIdType row = 0; row < this->Table->GetNumberOfRows(); ++row)
+  while (!this->LookupTables.empty())
     {
-    for (vtkIdType column = 1; column < this->Table->GetNumberOfColumns();
-         ++column)
+    this->LookupTables.back()->Delete();
+    this->LookupTables.pop_back();
+    }
+  this->LookupTables.reserve( this->Table->GetNumberOfColumns() + 1 );
+
+  for (vtkIdType column = 1; column < this->Table->GetNumberOfColumns();
+       ++column)
+    {
+    if (this->Table->GetValue(0, column).IsString())
+      {
+      this->GenerateLookupTableForStringColumn(column);
+      continue;
+      }
+    double min = DBL_MAX;
+    double max = DBL_MIN;
+    vtkLookupTable *lookupTable = vtkLookupTable::New();
+    this->LookupTables.push_back(lookupTable);
+    for (vtkIdType row = 0; row < this->Table->GetNumberOfRows(); ++row)
       {
       double value = this->Table->GetValue(row, column).ToDouble();
       if (value > max)
@@ -161,14 +178,44 @@ void vtkTreeHeatmapItem::InitializeLookupTable()
         min = value;
         }
       }
+    this->LookupTables[column-1]->SetNumberOfTableValues(256);
+    this->LookupTables[column-1]->SetRange(min, max);
+    this->LookupTables[column-1]->Build();
     }
+}
+      
+void vtkTreeHeatmapItem::GenerateLookupTableForStringColumn(vtkIdType column)
+{
+  //generate a sorted vector of all the strings in this column
+  std::vector< std::string > sortedStrings;
+  for (vtkIdType row = 0; row < this->Table->GetNumberOfRows(); ++row)
+    {
+    std::string value = this->Table->GetValue(row, column).ToString();
+    // no duplicates
+    if (std::find(sortedStrings.begin(), sortedStrings.end(), value) ==
+        sortedStrings.end())
+      {
+      sortedStrings.push_back(value);
+      }
+    }
+  std::sort(sortedStrings.begin(), sortedStrings.end());
 
-  this->LookupTable->SetNumberOfTableValues(256);
-  this->LookupTable->SetRange(min, max);
-  this->LookupTable->Build();
-  this->LookupTable->SetTableValue(0, 1, 0, 0);
-  this->LookupTable->SetTableValue(128, 0.5, 0.5, 0.5);
-  this->LookupTable->SetTableValue(255, 0, 1, 0);
+  // map each string to a double value based on its position
+  // in alphabetical order
+  std::map< std::string, double> stringToDouble;
+  for (int i = 0; i < sortedStrings.size(); ++i)
+    {
+    stringToDouble[ sortedStrings[i] ] = (double)i;
+    }
+ 
+  // associate this mapping with the column number
+  this->StringToDoubleMaps[column] = stringToDouble;
+   
+  // generate a lookup table for this column
+  this->LookupTables.push_back(vtkLookupTable::New());
+  this->LookupTables[column-1]->SetNumberOfTableValues(256);
+  this->LookupTables[column-1]->SetRange(0, sortedStrings.size() - 1);
+  this->LookupTables[column-1]->Build();
 }
 
 void vtkTreeHeatmapItem::PaintBuffers(vtkContext2D *painter)
@@ -220,17 +267,47 @@ void vtkTreeHeatmapItem::PaintBuffers(vtkContext2D *painter)
   // and the row/column labels
   double spacing = cellWidth * 0.25;
 
+  // get array of node names from the tree
   vtkStringArray *nodeNames = vtkStringArray::SafeDownCast(
     this->LayoutTree->GetVertexData()->GetAbstractArray("node name"));
+
+  // get array of row names from the table.  We assume this is the first row.
   vtkStringArray *tableNames = vtkStringArray::SafeDownCast(
-    this->Table->GetColumnByName("name"));
+    this->Table->GetColumn(0));
 
   // set up our text property to draw row names
   painter->GetTextProp()->SetColor(0.0, 0.0, 0.0);
-  painter->GetTextProp()->SetFontSize(floor(cellHeight));
   painter->GetTextProp()->SetJustificationToLeft();
   painter->GetTextProp()->SetVerticalJustificationToCentered();
   painter->GetTextProp()->SetOrientation(0);
+  
+  // calculate a font size that's appropriate for this zoom level
+  float stringBounds[4];
+  stringBounds[3] = FLT_MAX;
+  std::string testString = "Igq"; //selected for range of height
+  int currentFontSize = floor(cellHeight);
+  painter->GetTextProp()->SetFontSize(currentFontSize);
+  painter->ComputeStringBounds(testString, stringBounds);
+  if (stringBounds[3] > cellHeight)
+    {
+    while (stringBounds[3] > cellHeight && currentFontSize > 0)
+      {
+      --currentFontSize;
+      painter->GetTextProp()->SetFontSize(currentFontSize);
+      painter->ComputeStringBounds(testString, stringBounds);
+      }
+    }
+  else
+    {
+      while (stringBounds[3] < cellHeight)
+      {
+      ++currentFontSize;
+      painter->GetTextProp()->SetFontSize(currentFontSize);
+      painter->ComputeStringBounds(testString, stringBounds);
+      }
+    --currentFontSize;
+    painter->GetTextProp()->SetFontSize(currentFontSize);
+    }
   
   double xStart, yStart;
   double yMaxTable = DBL_MIN;
@@ -243,6 +320,7 @@ void vtkTreeHeatmapItem::PaintBuffers(vtkContext2D *painter)
       continue;
       }
 
+
     // find the row in the table that corresponds to this vertex
     double point[3];
     this->LayoutTree->GetPoint(vertex, point);
@@ -253,9 +331,24 @@ void vtkTreeHeatmapItem::PaintBuffers(vtkContext2D *painter)
          ++column)
       {
       // get the color for this cell from the lookup table
-      vtkVariant value = this->Table->GetValue(tableRow, column);
       double color[3];
-      this->LookupTable->GetColor(value.ToDouble(), color);
+      if (this->Table->GetValue(tableRow, column).IsString())
+        {
+        // get the string to int mapping for this column
+        std::map< std::string, double> stringToDouble = this->StringToDoubleMaps[column];
+
+        // get the integer value for the current string
+        std::string cellStr = this->Table->GetValue(tableRow, column).ToString();
+        double colorKey = stringToDouble[cellStr];
+        
+        // now we can lookup the appropriate color for this string
+        this->LookupTables[column-1]->GetColor(colorKey, color);
+        }
+      else
+        {
+        vtkVariant value = this->Table->GetValue(tableRow, column);
+        this->LookupTables[column-1]->GetColor(value.ToDouble(), color);
+        }
       painter->GetBrush()->SetColorF(color[0], color[1], color[2]);
 
       // draw this cell of the table
--- Views/Infovis/vtkTreeHeatmapItem.h
+++ Views/Infovis/vtkTreeHeatmapItem.h
@@ -12,23 +12,28 @@
      PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
-// .NAME vtkTreeHeatmapItem - A 2D graphics item for rendering a tree.
+// .NAME vtkTreeHeatmapItem - A 2D graphics item for rendering a tree and
+// an associated heatmap.
 //
 // .SECTION Description
-// This item draws a tree as a part of a vtkContextScene. This simple
-// class has minimal state and delegates the determination of visual
-// vertex and edge properties like color, size, width, etc. to
-// a set of virtual functions. To influence the rendering of the tree,
-// subclass this item and override the property functions you wish to
-// customize.
+// This item draws a tree and a heatmap as a part of a vtkContextScene.
+// The input tree's vertex data must contain a vtkStringArray called 
+// "node name".  This array corresponds to the first column of the input
+// table.  The vtkNewickTreeReader automatically initializes this required
+// array in its output tree.
+//
+// .SEE ALSO
+// vtkTree vtkTable vtkNewickTreeReader
 
 #ifndef __vtkTreeHeatmapItem_h
 #define __vtkTreeHeatmapItem_h
 
 #include "vtkViewsInfovisModule.h" // For export macro
 #include "vtkContextItem.h"
- 
+
 #include "vtkNew.h" // For vtkNew ivars
+#include <vector>   // For lookup tables
+#include <map>      // For string lookup tables
 
 class vtkGraphLayout;
 class vtkLookupTable;
@@ -43,29 +48,64 @@ public:
   virtual void PrintSelf(ostream &os, vtkIndent indent);
 
   // Description:
-  // The tree that this item draws.
+  // Set the tree that this item draws.  Note that this tree's vertex data
+  // must contain a vtkStringArray called "node name".  Additionally, this
+  // array must contain the same values as the first column of the input
+  // table.  See SetTable for more information.  The vtkNewickTreeReader
+  // automatically creates this required array for you.
   virtual void SetTree(vtkTree *tree);
+
+  // Description:
+  // Get the tree that this item draws.
   vtkGetObjectMacro(Tree, vtkTree);
   
   // Description:
-  // The table that this item draws.
+  // Set the table that this item draws.  The first column of the table
+  // must contain the names of the rows.  These names, in turn, must correspond
+  // with the nodes names in the input tree.  See SetTree for more information.
   virtual void SetTable(vtkTable *table);
+
+  // Description:
+  // Get the table that this item draws.
   vtkGetObjectMacro(Table, vtkTable);
 
 protected:
   vtkTreeHeatmapItem();
   ~vtkTreeHeatmapItem();
     
+  // Description:
+  // Generate some data needed for painting.  We cache this information as
+  // it only needs to be generated when the input data changes.
   virtual void RebuildBuffers();
-  virtual void PaintBuffers(vtkContext2D *painter);
-  virtual bool IsDirty();
-
-  void ComputeMultiplier();
-  void InitializeLookupTable();
 
   // Description:
-  // Paints the tree.
+  // This function does the bulk of the actual work in rendering our tree &
+  // heatmap data.
+  virtual void PaintBuffers(vtkContext2D *painter);
+
+  // Description:
+  // This function returns a bool indicating whether or not we need to rebuild
+  // our cached data before painting.
+  virtual bool IsDirty();
+
+  // Description:
+  // Compute how to scale our data so that text labels will fit within the
+  // bounds determined by the table's cells.
+  void ComputeMultiplier();
+
+  // Description:
+  // Generate a separate vtkLookupTable for each column in the table.
+  void InitializeLookupTables();
+
+  // Description:
+  // Paints the tree & associated table as a heatmap.
   virtual bool Paint(vtkContext2D *painter);
+
+  // Description:
+  // Helper function.  Generates a vtkLookupTable for a Table column that
+  // contains only strings.  Each string will be assigned a separate color.
+  // This is useful for visualizing categorical data.
+  void GenerateLookupTableForStringColumn(vtkIdType column);
 
 private:
   vtkTreeHeatmapItem(const vtkTreeHeatmapItem&); // Not implemented
@@ -76,8 +116,10 @@ private:
   vtkTable *Table;
   unsigned long TreeHeatmapBuildTime;
   vtkNew<vtkGraphLayout> Layout;
-  vtkNew<vtkLookupTable> LookupTable;
+  std::vector< vtkLookupTable * > LookupTables;
   double Multiplier;
+  
+  std::map< int, std::map< std::string, double> > StringToDoubleMaps;
 };
 
 #endif
