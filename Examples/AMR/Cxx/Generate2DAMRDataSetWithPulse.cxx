6587bda6bdbfbda22d4ab7e5e0317489233421b8
George Zagaris <george.zagaris@kitware.com>
2012-09-06 13:06:50 -0400

    ENH: Fix warnings and update to new AMR datatypes
    Change-Id: Icc35277e37710d48388c9f8b863ad407b707fff7

--- Examples/AMR/Cxx/Generate2DAMRDataSetWithPulse.cxx
+++ Examples/AMR/Cxx/Generate2DAMRDataSetWithPulse.cxx
@@ -24,16 +24,16 @@
 #include <sstream>
 #include <cassert>
 
-#include "vtkUniformGrid.h"
+#include "vtkAMRUtilities.h"
+#include "vtkCell.h"
+#include "vtkCellData.h"
 #include "vtkDataArray.h"
 #include "vtkDoubleArray.h"
-#include "vtkCell.h"
-#include "vtkPoints.h"
+#include "vtkOverlappingAMR.h"
 #include "vtkPointData.h"
-#include "vtkCellData.h"
-#include "vtkHierarchicalBoxDataSet.h"
-#include "vtkXMLHierarchicalBoxDataWriter.h"
-#include "vtkAMRUtilities.h"
+#include "vtkPoints.h"
+#include "vtkUniformGrid.h"
+
 #include "AMRCommon.h"
 
 struct PulseAttributes {
@@ -52,7 +52,7 @@ void SetPulse();
 
 // Description:
 // Constructs the vtkHierarchicalBoxDataSet.
-vtkHierarchicalBoxDataSet* GetAMRDataSet();
+vtkOverlappingAMR* GetAMRDataSet();
 
 // Description:
 // Attaches the pulse to the given grid.
@@ -71,7 +71,7 @@ int main( int argc, char **argv )
   SetPulse();
 
   // STEP 1: Get the AMR dataset
-  vtkHierarchicalBoxDataSet *amrDataSet = GetAMRDataSet();
+  vtkOverlappingAMR *amrDataSet = GetAMRDataSet();
   assert( "pre: NULL AMR dataset" && ( amrDataSet != NULL ) );
 
   AMRCommon::WriteAMRData( amrDataSet, "Gaussian2D" );
@@ -100,8 +100,8 @@ void AttachPulseToGrid( vtkUniformGrid *grid )
   xyz->SetNumberOfComponents( 1 );
   xyz->SetNumberOfTuples( grid->GetNumberOfCells() );
 
-  unsigned int cellIdx = 0;
-  for( ; cellIdx < grid->GetNumberOfCells(); ++cellIdx )
+
+  for(int cellIdx=0; cellIdx < grid->GetNumberOfCells(); ++cellIdx )
     {
       double center[3];
       AMRCommon::ComputeCellCenter( grid, cellIdx, center );
@@ -122,12 +122,17 @@ void AttachPulseToGrid( vtkUniformGrid *grid )
 }
 
 //------------------------------------------------------------------------------
-vtkHierarchicalBoxDataSet* GetAMRDataSet()
+vtkOverlappingAMR* GetAMRDataSet()
 {
-  vtkHierarchicalBoxDataSet *data = vtkHierarchicalBoxDataSet::New();
-  data->Initialize();
-
+  int NumLevels = 2;
+  int BlocksPerLevel[2] = {1,2};
   double origin[3];
+  origin[0] = origin[1] = -2.0; origin[2] = 0.0;
+
+  vtkOverlappingAMR *data = vtkOverlappingAMR::New();
+  data->Initialize(NumLevels,BlocksPerLevel,origin,VTK_XY_PLANE);
+
+
   double h[3];
   int    ndim[3];
   int    blockId = -1;
@@ -136,15 +141,17 @@ vtkHierarchicalBoxDataSet* GetAMRDataSet()
   // Root Block -- Block 0,0
   ndim[0]   = 6; ndim[1]   = 5; ndim[2] = 1;
   h[0]      = h[1]      = h[2]      = 1.0;
-  origin[0] = origin[1] = -2.0; origin[2] = 0.0;
+
   blockId   = 0;
   level     = 0;
   vtkUniformGrid *root = AMRCommon::GetGrid(origin, h, ndim);
   AttachPulseToGrid( root );
-  data->SetDataSet( level, blockId,root);
+  data->SetAMRBox(
+    level,blockId,root->GetOrigin(),root->GetDimensions(),root->GetSpacing());
+  data->SetDataSet(level,blockId,root);
   root->Delete();
 
-  // Block 0,1
+  // Block 1,0
   ndim[0]   = ndim[1]   = 9; ndim[2] = 1;
   h[0]      = h[1]      = h[2]       = 0.25;
   origin[0] = origin[1] = -2.0; origin[2] = 0.0;
@@ -152,6 +159,9 @@ vtkHierarchicalBoxDataSet* GetAMRDataSet()
   level     = 1;
   vtkUniformGrid *grid1 = AMRCommon::GetGrid(origin, h, ndim);
   AttachPulseToGrid( grid1 );
+  data->SetAMRBox(
+   level,blockId,grid1->GetOrigin(),
+   grid1->GetDimensions(),grid1->GetSpacing());
   data->SetDataSet( level, blockId,grid1);
   grid1->Delete();
 
@@ -163,6 +173,9 @@ vtkHierarchicalBoxDataSet* GetAMRDataSet()
   level     = 1;
   vtkUniformGrid *grid3 = AMRCommon::GetGrid(origin, h, ndim);
   AttachPulseToGrid( grid3 );
+  data->SetAMRBox(
+      level,blockId,grid3->GetOrigin(),
+      grid3->GetDimensions(),grid3->GetSpacing());
   data->SetDataSet( level, blockId,grid3);
   grid3->Delete();
 
