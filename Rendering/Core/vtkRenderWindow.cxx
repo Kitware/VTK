1e27d086dc8b3f3e91175f551d7fa7dde75e6123
Burlen Loring <bloring@lbl.gov>
2012-09-28 09:35:50 -0700

    RenderWindow::RemoveRenderer invalid access
    Change in order of operations. release the reference to the renderer
    being removed after we are finished modifying it.
    
    Change-Id: I19a1cbbf230655e81771431e7516f6841cc1bb83

--- Rendering/Core/vtkRenderWindow.cxx
+++ Rendering/Core/vtkRenderWindow.cxx
@@ -823,11 +823,11 @@ void vtkRenderWindow::AddRenderer(vtkRenderer *ren)
 void vtkRenderWindow::RemoveRenderer(vtkRenderer *ren)
 {
   // we are its parent
-  this->Renderers->RemoveItem(ren);
   if (ren->GetRenderWindow() == this)
     {
     ren->SetRenderWindow(NULL);
     }
+  this->Renderers->RemoveItem(ren);
 }
 
 int vtkRenderWindow::HasRenderer(vtkRenderer *ren)
