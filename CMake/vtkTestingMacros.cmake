4d05b66e3efa430c86beaa7c2f3663b206edfa64
Dave DeMarle <dave.demarle@kitware.com>
2012-10-02 16:57:20 -0400

    Fix most of the parallel tests.
    Add optional test macro arguments to allow for some of the tests to
    depend on VTK_LARGE_DATA_ROOT and for most of the tests to have
    their correct images in an pre-modularized vtkdata directory layout.
    NOTE: This requires some VTKDATA file movement because old tests
    have names like tname.cxx.png and this assumes tname.png.
    
    Also make TestParallelRendering return some sort of pass/fail result.
    
    Change-Id: Id76a92ac1d7f5eedb15baff91e6849b590961d5e

--- CMake/vtkTestingMacros.cmake
+++ CMake/vtkTestingMacros.cmake
@@ -1,17 +1,40 @@
+macro(parse_optional_arguments)
+  # Look for optional baseline image directory argument to use instead of module
+  # name in which to look for correct image result
+  set (BASELINEDIR ${vtk-module})
+
+  # Look for optional data directory argument that lets you choose VTK_DATA_ROOT
+  # or VTK_LARGE_DATA_ROOT or nothing.
+  #set (DATADIR ${VTK_DATA_ROOT}) #don't automatically use it, some tests
+  #don't want it specified at all
+
+  set(argv ${ARGV})
+  set(MYARGV)
+  set (i 0)
+  while (${i} LESS ${ARGC})
+    math(EXPR iplus1 "${i}+1")
+    list(GET argv ${i} ARG)
+    if (${ARG} STREQUAL "BASELINEDIR")
+      list(GET argv ${iplus1} BASELINEDIR)
+      set (i ${iplus1})
+      math(EXPR iplus1 "${i}+1")
+    elseif(${ARG} STREQUAL "DATADIR")
+      list(GET argv ${iplus1} DATADIR)
+      set (i ${iplus1})
+      math(EXPR iplus1 "${i}+1")
+    else()
+      list(APPEND MYARGV ${ARG})
+    endif()
+    set(i ${iplus1})
+  endwhile()
+endmacro(parse_optional_arguments)
+
 # -----------------------------------------------------------------------------
 # Macro vtk_tests() takes a list of cxx files which will be driven by the modules
 # test driver. This helps reduce a lot of boiler place code in each module
 macro(vtk_tests)
-  # Look for an optional baseline image directory argument
-  # to use instead of the module name
-  set (BASELINEDIR ${vtk-module})
-  set (MYARGV ${ARGV})
-  if ((${ARGC} GREATER 1) AND (${ARGV0} STREQUAL "BASELINEDIR"))
-    set (BASELINEDIR ${ARGV1})
-    list(REMOVE_AT MYARGV 0)
-    list(REMOVE_AT MYARGV 0)
-  endif()
 
+  parse_optional_arguments(${ARGV})
   create_test_sourcelist(Tests ${vtk-module}CxxTests.cxx
     ${MYARGV}
     EXTRA_INCLUDE vtkTestDriver.h)
@@ -24,10 +47,10 @@ macro(vtk_tests)
   # Add all the executables
   foreach(test ${TestsToRun})
     get_filename_component(TName ${test} NAME_WE)
-    if(VTK_DATA_ROOT)
+    if(DATADIR)
       add_test(NAME ${vtk-module}Cxx-${TName}
         COMMAND ${vtk-module}CxxTests ${TName}
-        -D ${VTK_DATA_ROOT}
+        -D ${DATADIR}
         -T ${VTK_TEST_OUTPUT_DIR}
         -V Baseline/${BASELINEDIR}/${TName}.png)
     else()
@@ -42,18 +65,36 @@ endmacro(vtk_tests)
 # add_test_mpi macro take one or more files. It uses the files name for the name
 # of the test. The test will be run using MPI.
 macro (add_test_mpi fileName)
+
+  parse_optional_arguments(${ARGV})
+
   get_filename_component(name ${fileName} NAME_WE)
+  list(REMOVE_AT MYARGV 0)
   vtk_module_test_executable(
     ${name}
     ${name}.cxx
-    ${ARGN})
-  add_test(
-    NAME ${vtk-module}Cxx-MPI-${name}
-    COMMAND ${VTK_MPIRUN_EXE}
-    ${VTK_MPI_PRENUMPROC_FLAGS} ${VTK_MPI_NUMPROC_FLAG} ${VTK_MPI_MAX_NUMPROCS}
-    ${VTK_MPI_PREFLAGS}
-    $<TARGET_FILE:${name}>
-    ${VTK_MPI_POSTFLAGS})
+    ${MYARGV})
+
+  if(DATADIR)
+    add_test(
+      NAME ${vtk-module}Cxx-MPI-${name}
+      COMMAND ${VTK_MPIRUN_EXE}
+      ${VTK_MPI_PRENUMPROC_FLAGS} ${VTK_MPI_NUMPROC_FLAG} ${VTK_MPI_MAX_NUMPROCS}
+      ${VTK_MPI_PREFLAGS}
+      $<TARGET_FILE:${name}>
+      -D ${DATADIR}
+      -T ${VTK_BINARY_DIR}/Testing/Temporary
+      -V ${DATADIR}/Baseline/Parallel/${name}.png
+      ${VTK_MPI_POSTFLAGS})
+  else()
+    add_test(
+      NAME ${vtk-module}Cxx-MPI-${name}
+      COMMAND ${VTK_MPIRUN_EXE}
+      ${VTK_MPI_PRENUMPROC_FLAGS} ${VTK_MPI_NUMPROC_FLAG} ${VTK_MPI_MAX_NUMPROCS}
+      ${VTK_MPI_PREFLAGS}
+      $<TARGET_FILE:${name}>
+      ${VTK_MPI_POSTFLAGS})
+  endif()
 endmacro()
 
 # -----------------------------------------------------------------------------
