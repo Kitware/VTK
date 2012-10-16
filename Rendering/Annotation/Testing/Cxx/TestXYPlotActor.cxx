8e5e0f6f2f2c8e84b42c5f0bad6c9709f3d099dc
Philippe Pébay <philippe.pebay@kitware.com>
2012-10-16 14:51:53 +0200

    Testing with synthetic data, with no recourse to IO
    Change-Id: I367acc558bb31436ddb0280db6ee40650a93dc45

--- Rendering/Annotation/Testing/Cxx/TestXYPlotActor.cxx
+++ Rendering/Annotation/Testing/Cxx/TestXYPlotActor.cxx
@@ -2,39 +2,47 @@
 
 #include "vtkActor.h"
 #include "vtkCamera.h"
-#include "vtkImageData.h"
-#include "vtkLineSource.h"
+#include "vtkDoubleArray.h"
+#include "vtkPoints.h"
 #include "vtkPointData.h"
 #include "vtkPolyData.h"
-#include "vtkPolyDataMapper.h"
-#include "vtkProbeFilter.h"
 #include "vtkProperty.h"
 #include "vtkProperty2D.h"
 #include "vtkRenderWindow.h"
 #include "vtkRenderWindowInteractor.h"
 #include "vtkRenderer.h"
 #include "vtkTextProperty.h"
-#include "vtkVolume16Reader.h"
 #include "vtkXYPlotActor.h"
 
 #include "vtkTestUtilities.h"
 
-int TestXYPlotActor( int argc, char *argv[] )
+int TestXYPlotActor( int, char *[] )
 {
-  char* fname = vtkTestUtilities::ExpandDataFileName( argc, argv, "Data/headsq/quarter" );
+  // Create container for points
+  vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
 
-  vtkSmartPointer<vtkVolume16Reader> v16 =
-    vtkSmartPointer<vtkVolume16Reader>::New();
-  v16->SetDataDimensions( 64, 64 );
-  v16->SetDataByteOrderToLittleEndian();
-  v16->SetImageRange( 1, 93 );
-  v16->SetDataSpacing( 3.2, 3.2, 1.5 );
-  v16->SetFilePrefix( fname );
-  v16->SetDataMask( 0x7fff );
-  v16->Update();
+  // Create container for data
+  vtkSmartPointer<vtkDoubleArray> data = vtkSmartPointer<vtkDoubleArray>::New();
+  data->SetNumberOfComponents( 1 );
+  data->SetName( "Test Data" );
 
-  delete[] fname;
+  // Fill in points and data
+  unsigned int nVals = 500;
+  for ( unsigned int i = 0; i < nVals; ++ i )
+    {
+    double val = sin( i ) * cos( 2 * i ) * sqrt( i );
+    data->InsertNextValue( val );
+    points->InsertNextPoint( i, 0., 0. );
+    }
+  double* range = data->GetRange();
 
+  // Create data set with created points and data
+  vtkSmartPointer<vtkPolyData> polydata = vtkSmartPointer<vtkPolyData>::New();
+  polydata->SetPoints( points );
+  //  polydata->SetPolys(triangles);
+  polydata->GetPointData()->SetScalars( data );
+
+  // Set up rendering contraption
   vtkSmartPointer<vtkRenderer> ren1 = vtkSmartPointer<vtkRenderer>::New();
   vtkSmartPointer<vtkRenderWindow> renWin = vtkSmartPointer<vtkRenderWindow>::New();
   renWin->SetMultiSamples( 0 );
@@ -42,17 +50,9 @@ int TestXYPlotActor( int argc, char *argv[] )
   vtkSmartPointer<vtkRenderWindowInteractor> iren = vtkSmartPointer<vtkRenderWindowInteractor>::New();
   iren->SetRenderWindow( renWin );
 
-  vtkSmartPointer<vtkLineSource> line = vtkSmartPointer<vtkLineSource>::New();
-
-  vtkSmartPointer<vtkProbeFilter> probe = vtkSmartPointer<vtkProbeFilter>::New();
-  probe->SetInputConnection( line->GetOutputPort() );
-  probe->SetSourceConnection( v16->GetOutputPort() );
-
-  vtkImageData* data = v16->GetOutput();
-  double* range = data->GetPointData()->GetScalars()->GetRange();
-
+  // Set XY plot actor
   vtkSmartPointer<vtkXYPlotActor> xyPlot = vtkSmartPointer<vtkXYPlotActor>::New();
-  xyPlot->AddDataSetInputConnection( probe->GetOutputPort() );
+  xyPlot->AddDataSetInput( polydata );
   xyPlot->GetPositionCoordinate()->SetValue( 0.05, 0.05, 0 );
   xyPlot->GetPosition2Coordinate()->SetValue( 0.95, 0.95, 0 );
   xyPlot->SetXValuesToNormalizedArcLength();
@@ -60,8 +60,8 @@ int TestXYPlotActor( int argc, char *argv[] )
   xyPlot->SetNumberOfYLabels( 8 );
   xyPlot->SetTitle( "XY Plot Actor Test");
   xyPlot->SetYTitlePositionToTop();
-  xyPlot->SetXTitle( "x value");
-  xyPlot->SetYTitle( "y value");
+  xyPlot->SetXTitle( "x");
+  xyPlot->SetYTitle( "f(x)");
   xyPlot->SetXRange( 0, 1 );
   xyPlot->SetYRange( range[0], range[1] );
   xyPlot->GetProperty()->SetColor( 0, 0, 0 );
