a0dbee4fd6d0c2a707f8178aa4ed5c96a23ee33c
Philippe Pébay <philippe.pebay@kitware.com>
2012-10-27 23:36:16 +0200

    Checkpoint towards a much better axis cut test
    Change-Id: I5e35c883a8fbdfdb186a7f57a26ad25ca4c83bca

--- Filters/HyperTree/Testing/Cxx/TestHyperTreeGridTernary3DAxisCut.cxx
+++ Filters/HyperTree/Testing/Cxx/TestHyperTreeGridTernary3DAxisCut.cxx
@@ -15,6 +15,8 @@
 #include "vtkCamera.h"
 #include "vtkCellData.h"
 #include "vtkNew.h"
+#include "vtkOutlineFilter.h"
+#include "vtkProperty.h"
 #include "vtkPolyDataMapper.h"
 #include "vtkRegressionTestImage.h"
 #include "vtkRenderer.h"
@@ -23,28 +25,78 @@
 
 int TestHyperTreeGridTernary3DAxisCut( int argc, char* argv[] )
 {
-  vtkNew<vtkHyperTreeGridSource> fractal;
-  fractal->SetMaximumLevel( 3 );
-  fractal->SetGridSize( 3, 4, 2 );
-  fractal->SetDimension( 3 );
-  fractal->SetAxisBranchFactor( 3 );
+  // Hyper tree grid
+  vtkNew<vtkHyperTreeGridSource> htGrid;
+  int maxLevel = 5;
+  htGrid->SetMaximumLevel( maxLevel );
+  htGrid->SetGridSize( 3, 3, 2 );
+  htGrid->SetDimension( 3 );
+  htGrid->SetAxisBranchFactor( 3 );
+  htGrid->DualOn();
+  htGrid->SetDescriptor( "RRR .R. .RR ..R ..R .R.|R.......................... ........................... ........................... .............R............. ....RR.RR........R......... .....RRRR.....R.RR......... ........................... ........................... ...........................|........................... ........................... ........................... ...RR.RR.......RR.......... ........................... RR......................... ........................... ........................... ........................... ........................... ........................... ........................... ........................... ............RRR............|........................... ........................... .......RR.................. ........................... ........................... ........................... ........................... ........................... ........................... ........................... ...........................|........................... ..........................." );
 
-  vtkNew<vtkHyperTreeGridAxisCut> axisCut;
-  axisCut->SetInputConnection( fractal->GetOutputPort() );
-  axisCut->SetPlaneNormalAxis( 2 );
-  axisCut->SetPlanePosition( .1 );
-  axisCut->Update();
-  vtkPolyData* pd = axisCut->GetOutput();
+  // Outline
+  vtkNew<vtkOutlineFilter> outline;
+  outline->SetInputConnection( htGrid->GetOutputPort() );
 
-  vtkNew<vtkPolyDataMapper> mapper;
-  mapper->SetInputConnection( axisCut->GetOutputPort() );
-  mapper->SetScalarRange( pd->GetCellData()->GetScalars()->GetRange() );
+  // Axis cuts
+  vtkNew<vtkHyperTreeGridAxisCut> axisCut1;
+  axisCut1->SetInputConnection( htGrid->GetOutputPort() );
+  axisCut1->SetPlaneNormalAxis( 0 );
+  axisCut1->SetPlanePosition( 2. );
+  axisCut1->Update();
+  vtkNew<vtkHyperTreeGridAxisCut> axisCut2;
+  axisCut2->SetInputConnection( htGrid->GetOutputPort() );
+  axisCut2->SetPlaneNormalAxis( 2 );
+  axisCut2->SetPlanePosition( .35 );
+  axisCut2->Update();
+  vtkPolyData* pd = axisCut2->GetOutput();
 
-  vtkNew<vtkActor> actor;
-  actor->SetMapper( mapper.GetPointer() );
+  // Mappers
+  vtkNew<vtkPolyDataMapper> mapper1;
+  mapper1->SetInputConnection( axisCut1->GetOutputPort() );
+  mapper1->SetScalarRange( pd->GetCellData()->GetScalars()->GetRange() );
+  mapper1->SetResolveCoincidentTopologyToPolygonOffset();
+  mapper1->SetResolveCoincidentTopologyPolygonOffsetParameters( 0, 1 );
+  vtkNew<vtkPolyDataMapper> mapper2;
+  mapper2->SetInputConnection( axisCut1->GetOutputPort() );
+  mapper2->ScalarVisibilityOff();
+  mapper2->SetResolveCoincidentTopologyToPolygonOffset();
+  mapper2->SetResolveCoincidentTopologyPolygonOffsetParameters( 1, 1 );
+  vtkNew<vtkPolyDataMapper> mapper3;
+  mapper3->SetInputConnection( outline->GetOutputPort() );
+  mapper3->ScalarVisibilityOff();
+  vtkNew<vtkPolyDataMapper> mapper4;
+  mapper4->SetInputConnection( axisCut2->GetOutputPort() );
+  mapper4->SetScalarRange( pd->GetCellData()->GetScalars()->GetRange() );
+  mapper4->SetResolveCoincidentTopologyToPolygonOffset();
+  mapper4->SetResolveCoincidentTopologyPolygonOffsetParameters( 0, 1 );
+  vtkNew<vtkPolyDataMapper> mapper5;
+  mapper5->SetInputConnection( axisCut2->GetOutputPort() );
+  mapper5->ScalarVisibilityOff();
+  mapper5->SetResolveCoincidentTopologyToPolygonOffset();
+  mapper5->SetResolveCoincidentTopologyPolygonOffsetParameters( 1, 1 );
+ 
+  // Actors
+  vtkNew<vtkActor> actor1;
+  actor1->SetMapper( mapper1.GetPointer() );
+  vtkNew<vtkActor> actor2;
+  actor2->SetMapper( mapper2.GetPointer() );
+  actor2->GetProperty()->SetRepresentationToWireframe();
+  actor2->GetProperty()->SetColor( .7, .7, .7 );
+  vtkNew<vtkActor> actor3;
+  actor3->SetMapper( mapper3.GetPointer() );
+  actor3->GetProperty()->SetColor( .1, .1, .1 );
+  actor3->GetProperty()->SetLineWidth( 1 );
+  vtkNew<vtkActor> actor4;
+  actor4->SetMapper( mapper4.GetPointer() );
+  vtkNew<vtkActor> actor5;
+  actor5->SetMapper( mapper5.GetPointer() );
+  actor5->GetProperty()->SetRepresentationToWireframe();
+  actor5->GetProperty()->SetColor( .7, .7, .7 );
 
-  // Create camera
-  vtkHyperTreeGrid* ht = fractal->GetOutput();
+  // Camera
+  vtkHyperTreeGrid* ht = htGrid->GetOutput();
   double bd[6];
   ht->GetBounds( bd );
   vtkNew<vtkCamera> camera;
@@ -52,19 +104,23 @@ int TestHyperTreeGridTernary3DAxisCut( int argc, char* argv[] )
   camera->SetFocalPoint( ht->GetCenter() );
   camera->SetPosition( -.8 * bd[1], 2.1 * bd[3], -4.8 * bd[5] );
 
-  // Create a renderer, add actors to it
+  // Renderer
   vtkNew<vtkRenderer> renderer;
   renderer->SetActiveCamera( camera.GetPointer() );
   renderer->SetBackground( 1., 1., 1. );
-  renderer->AddActor( actor.GetPointer() );
+  renderer->AddActor( actor1.GetPointer() );
+  renderer->AddActor( actor2.GetPointer() );
+  renderer->AddActor( actor3.GetPointer() );
+  renderer->AddActor( actor4.GetPointer() );
+  renderer->AddActor( actor5.GetPointer() );
 
-  // Create a renderWindow
+  // Render window
   vtkNew<vtkRenderWindow> renWin;
   renWin->AddRenderer( renderer.GetPointer() );
   renWin->SetSize( 300, 300 );
   renWin->SetMultiSamples( 0 );
 
-  // Create interactor
+  // Interactor
   vtkNew<vtkRenderWindowInteractor> iren;
   iren->SetRenderWindow( renWin.GetPointer() );
 
