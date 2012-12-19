bad79e6f7acd8b07e86232bb674056b363468165
Philippe Pébay <philippe.pebay@kitware.com>
2012-12-19 09:15:19 +0100

    This sphere has a Material in it -- say it!
    Change-Id: I87af96e1e6fac3021e76fb3cec33652533a7068a

--- Filters/HyperTree/Testing/Cxx/TestHyperTreeGridTernarySphereMaterial.cxx
+++ Filters/HyperTree/Testing/Cxx/TestHyperTreeGridTernarySphereMaterial.cxx
@@ -1,7 +1,7 @@
 /*==================================================================
 
   Program:   Visualization Toolkit
-  Module:    TestHyperTreeGridTernarySphere.cxx
+  Module:    TestHyperTreeGridTernarySphereMaterial.cxx
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
@@ -29,7 +29,7 @@
 #include "vtkRenderWindow.h"
 #include "vtkRenderWindowInteractor.h"
 
-int TestHyperTreeGridTernarySphere( int argc, char* argv[] )
+int TestHyperTreeGridTernarySphereMaterial( int argc, char* argv[] )
 {
   // Hyper tree grid
   vtkNew<vtkHyperTreeGridSource> htGrid;
