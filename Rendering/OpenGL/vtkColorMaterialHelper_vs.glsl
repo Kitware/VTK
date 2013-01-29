fb1f4dd76a0064e28d3dd047d46a5f4c5015fe9c
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2013-01-29 14:07:55 -0500

    Macros beginning with GL_* are reserved by GLSL.
    Fixing code to not use reserved keywords. From GLSL 4.3 spec (page 11):
    
      All macro names containing two consecutive underscores ( __ ) are reserved for
      future use as predefined macro names. All macro names prefixed with “GL_”
      (“GL” followed by a single underscore) are also reserved.
    
    Change-Id: I6e0fa27385b1aaef50b18a383f381224100e673d

--- Rendering/OpenGL/vtkColorMaterialHelper_vs.glsl
+++ Rendering/OpenGL/vtkColorMaterialHelper_vs.glsl
@@ -16,11 +16,11 @@
 
 #version 110
 
-#define GL_AMBIENT 1
-#define GL_DIFFUSE 2
-#define GL_SPECULAR 3
-#define GL_AMBIENT_AND_DIFFUSE 4
-#define GL_EMISSION 5
+#define VTK_GL_AMBIENT 1
+#define VTK_GL_DIFFUSE 2
+#define VTK_GL_SPECULAR 3
+#define VTK_GL_AMBIENT_AND_DIFFUSE 4
+#define VTK_GL_EMISSION 5
 
 uniform int vtkColorMaterialHelper_Mode;
 
@@ -32,24 +32,24 @@ gl_MaterialParameters getMaterialParameters()
     }
 
   gl_MaterialParameters materialParams = gl_FrontMaterial;
-  if (vtkColorMaterialHelper_Mode == GL_AMBIENT)
+  if (vtkColorMaterialHelper_Mode == VTK_GL_AMBIENT)
     {
     materialParams.ambient = gl_Color;
     }
-  else if (vtkColorMaterialHelper_Mode == GL_DIFFUSE)
+  else if (vtkColorMaterialHelper_Mode == VTK_GL_DIFFUSE)
     {
     materialParams.diffuse = gl_Color;
     }
-  else if (vtkColorMaterialHelper_Mode == GL_SPECULAR)
+  else if (vtkColorMaterialHelper_Mode == VTK_GL_SPECULAR)
     {
     materialParams.specular = gl_Color;
     }
-  else if (vtkColorMaterialHelper_Mode == GL_AMBIENT_AND_DIFFUSE)
+  else if (vtkColorMaterialHelper_Mode == VTK_GL_AMBIENT_AND_DIFFUSE)
     {
     materialParams.ambient = gl_Color;
     materialParams.diffuse = gl_Color;
     }
-  else if (vtkColorMaterialHelper_Mode == GL_EMISSION)
+  else if (vtkColorMaterialHelper_Mode == VTK_GL_EMISSION)
     {
     materialParams.emission = gl_Color;
     }
