e4220a10eb450ec59e580b3c28e7c103ef510a6c
Sean McBride <sean@rogue-research.com>
2013-02-17 02:25:00 -0500

    Fixed clang analyzer warning about dead store.
    An 'abort' variable to exit a loop early was redundant with a 'break' statement in the loop.
    
    Change-Id: Iad88d4b4fbf4d3fea9ee996c6c43a4aa55fc09cd

--- Filters/Core/vtkSmoothPolyDataFilter.cxx
+++ Filters/Core/vtkSmoothPolyDataFilter.cxx
@@ -194,7 +194,7 @@ int vtkSmoothPolyDataFilter::RequestData(
   double CosFeatureAngle; //Cosine of angle between adjacent polys
   double CosEdgeAngle; // Cosine of angle between adjacent edges
   double closestPt[3], dist2, *w = NULL;
-  int iterationNumber, abortExecute;
+  int iterationNumber;
   vtkIdType numSimple=0, numBEdges=0, numFixed=0, numFEdges=0;
   vtkPolyData *inMesh, *Mesh;
   vtkPoints *inPts;
@@ -565,8 +565,8 @@ int vtkSmoothPolyDataFilter::RequestData(
     }
 
   factor = this->RelaxationFactor;
-  for ( maxDist=VTK_DOUBLE_MAX, iterationNumber=0, abortExecute=0;
-  maxDist > conv && iterationNumber < this->NumberOfIterations && !abortExecute;
+  for ( maxDist=VTK_DOUBLE_MAX, iterationNumber=0;
+  maxDist > conv && iterationNumber < this->NumberOfIterations;
   iterationNumber++ )
     {
 
@@ -575,7 +575,6 @@ int vtkSmoothPolyDataFilter::RequestData(
       this->UpdateProgress (0.5 + 0.5*iterationNumber/this->NumberOfIterations);
       if (this->GetAbortExecute())
         {
-        abortExecute = 1;
         break;
         }
       }
