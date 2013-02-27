52a286e5c50dc393d63aa93b660b6d2112595bfc
Kyle Lutz <kyle.lutz@kitware.com>
2013-02-27 16:40:25 -0500

    Fix memory leak in TestAMRReadWrite
    This fixes a memory leak from the fname string in the
    CreateTestAMR test.
    
    Change-Id: Idd6d1aaf1205adcc97581e175534497f33905840

--- IO/Geometry/Testing/Cxx/TestAMRReadWrite.cxx
+++ IO/Geometry/Testing/Cxx/TestAMRReadWrite.cxx
@@ -33,6 +33,7 @@ namespace
      char* fname = vtkTestUtilities::ExpandDataFileName(argc, argv,"Data/AMR/Enzo/DD0010/moving7_0010.hierarchy");
      vtkNew<vtkAMREnzoReader> reader;
      reader->SetFileName(fname);
+     delete [] fname;
      reader->SetMaxLevel(8);
      reader->SetCellArrayStatus( "TotalEnergy",1);
      reader->Update();
