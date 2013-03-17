ac24b78141ad108acf1bc7f11b604ed6630413c6
Sean McBride <sean@rogue-research.com>
2013-03-16 22:04:25 -0400

    Put RenderString() in VTK_LEGACY
    The comments say its deprecated, put it in
    VTK_LEGACY.  API seems unused in VTK itself.
    
    Change-Id: If043e635408a89b01a1186307e9fa8c2dd627d71

--- Rendering/FreeType/vtkFreeTypeUtilities.cxx
+++ Rendering/FreeType/vtkFreeTypeUtilities.cxx
@@ -1280,13 +1280,16 @@ int vtkFreeTypeUtilities::PopulateImageData(vtkTextProperty *tprop,
 }
 
 //----------------------------------------------------------------------------
+#ifndef VTK_LEGACY_REMOVE
 int vtkFreeTypeUtilities::RenderString(vtkTextProperty *tprop,
                                        const char *str,
                                        int, int,
                                        vtkImageData *data)
 {
+  VTK_LEGACY_BODY(vtkFreeTypeUtilities::RenderString, "VTK 6.0");
   return this->RenderString(tprop, str, data);
 }
+#endif
 
 //----------------------------------------------------------------------------
 int vtkFreeTypeUtilities::RenderString(vtkTextProperty *tprop,
