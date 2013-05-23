57569cc9d0cb500a85b924f8a4ee30b222183171
Brad King <brad.king@kitware.com>
2013-05-23 08:52:53 -0400

    Use vtk_test_cxx_executable in Rendering/GL2PS/Testing/Cxx
    Replace the last use of vtkTestingObjectFactory.cmake and remove it.
    CMake modules that have immediate inline effects are hard to follow, so
    drop this one.
    
    Change-Id: I813644b0c31c1eecd3415f11e57e703d035940fd

--- CMake/vtkTestingObjectFactory.cmake
+++
@@ -1,4 +0,0 @@
-include(vtkTestingRenderingDriver)
-CREATE_TEST_SOURCELIST(Tests ${KIT}CxxTests.cxx ${MyTests}
-                       EXTRA_INCLUDE ${vtkTestingRendering_SOURCE_DIR}/vtkTestingObjectFactory.h)
-
