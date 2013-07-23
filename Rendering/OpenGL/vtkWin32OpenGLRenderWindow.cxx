Kitware/VTK

commit a6fa3e375c17771864345763ae2283d47be7aae8

OpenGL error hunt
This patch implements OpenGL error checking in VTK.

OpenGL's error handling implementation error is designed such
that internal error flags remain set with the first error that
occurred until they are explicitly checked. With this design
it's important to check and clear the error flags regularly
else they become unusable as code checking for errors ends
up reporting earlier undetected unrelated errors.

This patch takes the following approach:

1) at public entry points into code that uses OpenGL clear the
error flags without reporting errors. This guards against
reportinig unrealted errors, such as those caused by code
outside of VTK. See vtkOpenGLClearErrorMacro

2) before returning from functions that made OpenGL calls check
for and report OpenGL errors. This detects Open GL errors in the
function/method where they occurred facilitating debugging and it
clears error flags so that user code doesn't detect errors caused
by VTK. See vtkOpenGLCheckErrorMacro

This patch cleans up a number of bugs that were detected by
the new error checking and reporting.

This patch also contains improvements for OpenGL pixel buffers, a
renderbuffer object, and fast paths through framebuffer objects,
and texture objects, and fast path for setting uniform variables,
all of which are needed in vtkSurfaceLICPainter and
vtkLineIntegralConvolution2D GPGPU code.

Change-Id: I4ecefe9a444a74128bf73ef2ddfd5d4acea387ec

Rendering/OpenGL/vtkWin32OpenGLRenderWindow.cxx
@@ -24,6 +24,8 @@ PURPOSE.  See the above copyright notice for more information.
 #include "vtkOpenGLProperty.h"
 #include "vtkOpenGLRenderer.h"
 #include "vtkOpenGLTexture.h"
+#include "vtkOpenGLRenderWindow.h"
+#include "vtkOpenGLError.h"
 #include "vtkRendererCollection.h"
 #include "vtkWin32RenderWindowInteractor.h"
 
@@ -31,6 +33,7 @@ PURPOSE.  See the above copyright notice for more information.
 #include <vtksys/ios/sstream>
 
 #include "vtkOpenGL.h"
+#include "vtkOpenGLError.h"
 #include "vtkgl.h"
 
 vtkStandardNewMacro(vtkWin32OpenGLRenderWindow);
@@ -88,6 +91,7 @@ void vtkWin32OpenGLRenderWindow::Clean()
   if (this->OwnContext && this->ContextId)
     {
     this->MakeCurrent();
+    vtkOpenGLClearErrorMacro();
 
      /* first delete all the old lights */
     for (short cur_light = GL_LIGHT0; cur_light < GL_LIGHT0+VTK_MAX_LIGHTS; cur_light++)
@@ -112,6 +116,7 @@ void vtkWin32OpenGLRenderWindow::Clean()
         }
 #endif
       }
+    vtkOpenGLCheckErrorMacro("failed in Clean");
 
     this->CleanUpRenderers();
 
@@ -149,7 +154,6 @@ void vtkWin32OpenGLRenderWindow::CleanUpRenderers()
        (ren = this->Renderers->GetNextRenderer(rsit));)
     {
     ren->SetRenderWindow(NULL);
-    ren->SetRenderWindow(this);
     }
 }
 
@@ -645,7 +649,7 @@ void vtkWin32OpenGLRenderWindow::SetupPixelFormat(HDC hDC, DWORD dwFlags,
   wglMakeCurrent(tempDC, 0);
   wglDeleteContext(tempContext);
   ReleaseDC(tempId, tempDC);
-  ::DestroyWindow(tempId);
+  ::DestroyWindow(tempId); // windows api
 
   // If we got a valid pixel format in the process, we are done.
   // Otherwise, we use the old approach of using ChoosePixelFormat.
@@ -1144,7 +1148,7 @@ void vtkWin32OpenGLRenderWindow::DestroyWindow()
         vtkSetWindowLong(this->WindowId,sizeof(vtkLONG),(vtkLONG)0);
         if(this->OwnWindow)
           {
-          ::DestroyWindow(this->WindowId);
+          ::DestroyWindow(this->WindowId); // windows api
           this->WindowId=0;
           }
         }
@@ -1522,7 +1526,14 @@ void vtkWin32OpenGLRenderWindow::CreateOffScreenDC(HBITMAP hbmp, HDC aHdc)
   SelectObject(this->MemoryHdc, this->MemoryBuffer);
 
   // Renderers will need to redraw anything cached in display lists
-  this->CleanUpRenderers();
+  vtkRenderer *ren;
+  vtkCollectionSimpleIterator rsit;
+  for (this->Renderers->InitTraversal(rsit);
+       (ren = this->Renderers->GetNextRenderer(rsit));)
+    {
+    ren->SetRenderWindow(NULL);
+    ren->SetRenderWindow(this);
+    }
 
   // adjust settings for renderwindow
   this->Mapped =0;

commit f7f82536aa867acc64b9a458c60810dd3fbf2b00

Fixed regression introduced by a6fa3e375c.
a6fa3e375c changed the logic in vtkWin32OpenGLRenderWindow::CleanUpRenderers().
That method was used when swithcing from offscreen to onscreen. That resulted in
the renderers not having correct references to render window after switching
from offscreen to onscreen context causing dashboard failures on ParaView
dashboards. This fixes that issue.

Since the a6fa3e375c changed the logic in CreateOffScreenDC() by not using
CleanUpRenderers() to reset renderers, it makes sence to change the logic in
ResumeScreenRendering() to not use CleanUpRenderers() for resetting renderers as
well.

Change-Id: I32e58854fa8720c99212cee5d7a2652f11dc9b21

Rendering/OpenGL/vtkWin32OpenGLRenderWindow.cxx
@@ -1628,7 +1628,15 @@ void vtkWin32OpenGLRenderWindow::ResumeScreenRendering(void)
   if(this->ContextId!=0)
     {
       this->MakeCurrent();
-      this->CleanUpRenderers();
+      // Renderers will need to redraw anything cached in display lists
+      vtkRenderer *ren;
+      vtkCollectionSimpleIterator rsit;
+      for (this->Renderers->InitTraversal(rsit);
+           (ren = this->Renderers->GetNextRenderer(rsit));)
+        {
+        ren->SetRenderWindow(NULL);
+        ren->SetRenderWindow(this);
+        }
     }
 
   this->Mapped = this->ScreenMapped;