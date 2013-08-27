dd403de2d319de67a423e1da228f102ec4cc8698
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2013-08-27 15:49:04 -0400

    STYLE: Minor fixes to documentation strings
    Change-Id: I5d9b4ca87539172df2ee58730035bfdf7afdbde3

--- Charts/Core/Testing/Cxx/TestLinePlot3D.cxx
+++ Charts/Core/Testing/Cxx/TestLinePlot3D.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    TestLinePlot.cxx
+  Module:    TestLinePlot3D.cxx
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
--- Charts/Core/Testing/Cxx/TestLinePlotDouble.cxx
+++ Charts/Core/Testing/Cxx/TestLinePlotDouble.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    TestLinePlot.cxx
+  Module:    TestLinePlotDouble.cxx
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
