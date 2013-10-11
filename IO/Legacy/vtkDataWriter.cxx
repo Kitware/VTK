f3f0214156c236927be545a365ea0021eec7d7e3
Chris Harris <chris.harris@kitware.com>
2013-10-11 11:52:03 -0400

    OutputString should include null terminator
    The allocation of OutputString in vtkDataWriter needs to be
    size()+1 to leave room for the null terminator.
    
    Change-Id: I96af49cc7307f1e0cefd2edc3be8617edfc3a8e3

--- IO/Legacy/vtkDataWriter.cxx
+++ IO/Legacy/vtkDataWriter.cxx
@@ -1920,9 +1920,9 @@ void vtkDataWriter::CloseVTKFile(ostream *fp)
 
       delete [] this->OutputString;
       this->OutputStringLength = static_cast<int>(ostr->str().size());
-      this->OutputString = new char[ostr->str().size()];
+      this->OutputString = new char[ostr->str().size()+1];
       memcpy(this->OutputString, ostr->str().c_str(),
-        this->OutputStringLength);
+        ostr->str().size()+1);
       }
     delete fp;
     }
