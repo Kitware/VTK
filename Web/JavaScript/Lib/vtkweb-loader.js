5611f9cb56edba742f6c8047d710820bf2633212
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2013-10-18 17:22:04 -0600

    Migrate ParaViewWeb widgets into more generic VTK ones
    Change-Id: I9ed311236e82aee369a451a35848068ee4b648f8

--- Web/JavaScript/Lib/vtkweb-loader.js
+++ Web/JavaScript/Lib/vtkweb-loader.js
@@ -12,48 +12,98 @@
 
     var vtkWebLibs = {
         "core-min" : [
-        "ext/js-core/jquery-1.8.3.min.js",
-        "ext/js-core/autobahn.min.js",
-        "lib/js/vtkweb-all.min.js"
+        "ext/core/jquery-1.8.3.min.js",
+        "ext/core/autobahn.min.js",
+        "lib/core/vtkweb-all.min.js"
         ],
         "core" : [
-        "ext/js-core/jquery-1.8.3.min.js",
-        "ext/js-core/autobahn.js",
-        "lib/js/vtkweb-all.js"
+        "ext/core/jquery-1.8.3.min.js",
+        "ext/core/autobahn.js",
+        "lib/core/vtkweb-all.js"
         ],
         "webgl-min" : [
-        "ext/js-core/gl-matrix-min.js"
+        "ext/core/gl-matrix-min.js"
         ],
         "webgl" : [
-        "ext/js-core/gl-matrix.js"
+        "ext/core/gl-matrix.js"
         ],
         "mobile-min" : [
-        "ext/js-core/jquery.hammer.min.js"
+        "ext/core/jquery.hammer.min.js"
         ],
         "mobile" : [
-        "ext/js-core/jquery.hammer.js"
+        "ext/core/jquery.hammer.js"
         ],
         "all" : [
-        "ext/js-core/jquery-1.8.3.min.js",
-        "ext/js-core/autobahn.js",
-        "lib/js/vtkweb-all.js",
-        "ext/js-core/gl-matrix.js",
-        "ext/js-core/jquery.hammer.js"
+        "ext/core/jquery-1.8.3.min.js",
+        "ext/core/autobahn.js",
+        "lib/core/vtkweb-all.js",
+        "ext/core/gl-matrix.js",
+        "ext/core/jquery.hammer.js"
         ],
         "all-min": [
-        "ext/js-core/jquery-1.8.3.min.js",
-        "ext/js-core/autobahn.min.js",
-        "lib/js/vtkweb-all.min.js",
-        "ext/js-core/gl-matrix-min.js",
-        "ext/js-core/jquery.hammer.min.js"
-        ]
+        "ext/core/jquery-1.8.3.min.js",
+        "ext/core/autobahn.min.js",
+        "lib/core/vtkweb-all.min.js",
+        "ext/core/gl-matrix-min.js",
+        "ext/core/jquery.hammer.min.js"
+        ],
+        "bootstrap": [
+        "ext/bootstrap/js/bootstrap.min.js",
+        "ext/bootstrap/css/bootstrap-responsive.min.css",
+        "ext/bootstrap/css/bootstrap.min.css"
+        ],
+        "fontello": [
+        "ext/fontello/css/animation.css",
+        "ext/fontello/css/fontello.css"
+        ],
+        "color": [
+        "ext/jscolor/jscolor.js"
+        ],
+        "filebrowser": [
+        "ext/pure/pure.min.js",
+        "lib/widgets/vtkweb-widget-filebrowser.js",
+        "lib/widgets/vtkweb-widget-filebrowser.tpl",
+        "lib/widgets/vtkweb-widget-filebrowser.css"
+        ],
     },
     modules = [],
     script = document.getElementsByTagName("script")[document.getElementsByTagName("script").length - 1],
     basePath = "",
     extraScripts = [];
 
+    // ---------------------------------------------------------------------
+    function loadCss(url) {
+        var link = document.createElement("link");
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        link.href = url;
+        document.getElementsByTagName("head")[0].appendChild(link);
+    }
+
+    // ---------------------------------------------------------------------
+    function loadJavaScript(url) {
+        document.write('<script src="' + url + '"></script>');
+    }
+
+    // ---------------------------------------------------------------------
+    function _endWith(string, end) {
+        return string.lastIndexOf(end) === (string.length - end.length);
+    }
+
+    // ---------------------------------------------------------------------
+    function loadFile(url) {
+        if(_endWith(url, ".js")) {
+            loadJavaScript(url);
+        } else if(_endWith(url, ".css")) {
+            loadCss(url);
+        } else if(_endWith(url, ".tpl")) {
+            // template to load
+        }
+    }
+
+    // ---------------------------------------------------------------------
     // Extract modules to load
+    // ---------------------------------------------------------------------
     try {
         modules = script.getAttribute("load").split(",");
         for(var j in modules) {
@@ -63,7 +113,9 @@
     // We don't care we will use the default setup
     }
 
+    // ---------------------------------------------------------------------
     // Extract extra script to load
+    // ---------------------------------------------------------------------
     try {
         extraScripts = script.getAttribute("extra").split(",");
         for(var j in extraScripts) {
@@ -73,29 +125,40 @@
     // We don't care we will use the default setup
     }
 
+    // ---------------------------------------------------------------------
     // If no modules have been defined, just pick the default
+    // ---------------------------------------------------------------------
     if(modules.length == 0) {
         modules = [ "all-min" ];
     }
 
+    // ---------------------------------------------------------------------
     // Extract basePath
+    // ---------------------------------------------------------------------
     var lastSlashIndex = script.getAttribute("src").lastIndexOf('lib/js/vtkweb-loader');
     if(lastSlashIndex != -1) {
         basePath = script.getAttribute("src").substr(0, lastSlashIndex);
     }
 
+    // ---------------------------------------------------------------------
     // Add missing libs
+    // ---------------------------------------------------------------------
     for(var i in modules) {
         for(var j in vtkWebLibs[modules[i]]) {
-            document.write('<script src="' + basePath + vtkWebLibs[modules[i]][j] + '"></script>');
+            var path = basePath + vtkWebLibs[modules[i]][j];
+            loadFile(path);
         }
     }
 
+    // ---------------------------------------------------------------------
     // Add extra libs
+    // ---------------------------------------------------------------------
     for(var i in extraScripts) {
-        document.write('<script src="' + extraScripts[i] + '"></script>');
+        loadFile(extraScripts[i]);
     }
 
+    // ---------------------------------------------------------------------
     // Remove loader
+    // ---------------------------------------------------------------------
     script.parentNode.removeChild(script);
 }(window));
---
+++ Web/JavaScript/Widgets/FileBrowser/vtkweb-widget-filebrowser.js
@@ -0,0 +1,175 @@
+/**
+ * VTK-Web Widget Library.
+ *
+ * This module extend jQuery object to add support for graphical components
+ * related to File Browsing.
+ *
+ * @class jQuery.vtk.ui.FileBrowser
+ */
+(function ($) {
+
+    // =======================================================================
+    // ==== Defaults constant values =========================================
+    // =======================================================================
+    var pathSeparator = '/',
+    directives = {
+         '.vtk-directory': {
+            'directory <-': {
+                '@path': function(arg) {
+                    return pathToStr(arg.item.path);
+                },
+                '@class+': function(arg) {
+                    return (arg.item.path.length === 1) ? ' active' : '';
+                },
+                '.vtk-label': 'directory.label',
+                'li.vtk-files': {
+                    'file <- directory.files': {
+                        'div': 'file.label'
+                    }
+                },
+                'li.vtk-dirs': {
+                    'dir <- directory.dirs': {
+                        'div': 'dir'
+                    }
+                },
+                'li.vtk-path': {
+                    'i <- directory.path': {
+                        'div': 'i'
+                    }//,
+                    // filter : function(a) {
+                    //     return a.pos < (a.items.length - 1);
+                    // }
+                }
+            }
+        }
+    },
+    fileBrowserGenerator = null;
+
+    $.fn.fileBrowser = function(options) {
+        // Handle data with default values
+        var opts = $.extend({},$.fn.fileBrowser.defaults, options);
+
+        // Compile template only once
+        if(fileBrowserGenerator === null) {
+            template = $(opts.template);
+            console.log(template);
+            fileBrowserGenerator = template.compile(directives);
+        }
+
+        return this.each(function() {
+            var me = $(this).empty().addClass('vtk-filebrowser'),
+            container = $('<div/>');
+            me.append(container);
+
+            // Generate HTML
+            container.render(opts.data, fileBrowserGenerator);
+
+            // Initialize pipelineBrowser (Visibility + listeners)
+            initializeListener(me);
+        });
+    };
+
+    $.fn.fileBrowser.defaults = {
+        template: ".vtk-templates > .vtkweb-widget-filebrowser > div",
+        root: 'Root',
+        data: [
+            { 'label': 'Root', 'path': ['Root'], 'files': [
+                {'label': 'can.ex2', 'size': 2345},
+                {'label': 'diskout.ex2', 'size': 23345},
+            ], 'dirs': ['Cosmo', 'Ensight']},
+            { 'label': 'Cosmo',
+              'path': ['Root','Cosmo'],
+              'files': [
+                {'label': 'can.ex2', 'size': 2345},
+                {'label': 'diskout.ex2', 'size': 23345},
+              ],
+              'dirs': ['a','b','c'] },
+            { 'label': 'Ensight',
+              'path': ['Root','Ensight'],
+              'files': [
+                {'label': 'can.case', 'size': 2345},
+                {'label': 'diskout.case', 'size': 23345},
+              ],
+              'dirs': [] },
+            { 'label': 'a',
+               'path': ['Root','Cosmo', 'a'],
+               'files': [
+                {'label': 'x', 'size': 2345},
+                {'label': 'y', 'size': 23345},
+               ],
+               'dirs': []}
+        ]
+    };
+
+    // =======================================================================
+
+    function strToPath(pathId) {
+        var path = pathId.split(pathSeparator);
+        return path.slice(1, path.length);
+    }
+
+    // =======================================================================
+
+    function getParent(path) {
+        return path.slice(0, path.length - 2);
+    }
+
+    // =======================================================================
+
+    function getPath(parentPath, child) {
+        return [].concat(parentPath).concat(child);
+    }
+
+    // =======================================================================
+
+    function pathToStr(path) {
+        //console.log(path);
+        var str = pathSeparator + path.join(pathSeparator);
+        return str;
+    }
+
+    // =======================================================================
+
+    function initializeListener(container) {
+        $('.action', container).click(function(){
+            var me = $(this), item = $('div', me), pathStr = me.closest('.vtk-directory').attr('path'), type = me.closest('ul').attr('type');
+
+            if(type === 'path') {
+                // Find out the panel to show
+                var newPath = pathToStr(strToPath(pathStr).slice(0, me.index() + 1));
+                selector = '.vtk-directory[path="' + newPath + '"]';
+                var newActive = $(selector , container).addClass('active');
+                if(newActive.length === 1) {
+                    $('.vtk-directory', container).removeClass('active');
+                    newActive.addClass('active');
+                }
+            } else if(type === 'dir') {
+                // Swicth active panel
+                var str = '.vtk-directory[path="' + pathStr + pathSeparator + item.html() + '"]';
+                var newActive = $(str, container);
+                if(newActive.length === 1) {
+                    $('.vtk-directory', container).removeClass('active');
+                    newActive.addClass('active');
+                    container.trigger({
+                        type: 'directory-click',
+                        path: pathStr,
+                        name: me.text()
+                    });
+                } else {
+                    container.trigger({
+                        type: 'directory-not-found',
+                        path: pathStr,
+                        name: me.text()
+                    });
+                }
+            } else if(type === 'files') {
+                container.trigger({
+                    type: 'file-click',
+                    path: pathStr,
+                    name: me.text()
+                });
+            }
+        })
+    }
+
+}(jQuery));
