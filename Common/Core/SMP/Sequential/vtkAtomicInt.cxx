75062ae47a605998f27948960076c98f0426c313
Berk Geveci <berk.geveci@kitware.com>
2013-10-28 15:07:30 -0400

    Fixed atomic int compile issues on Windows.
    It turns out that forward declaring various atomic functions
    (_InterlockedXXX) is not straightforward. I ran into situations
    where this did not link. So I moved all of the atomic
    implementation for Windows to the cxx file and properly
    included vtkWindows.h. This should fix all compile/link issues
    of atomic ints on Windows. Note that the main reason that I
    chose not to include vtkWindows.h in the header is that
    atomic ints are used by all subclasses of vtkObjectBase and
    including vtkWindows.h all over the place would degrade
    compile performance significantly.
    
    Change-Id: I00bf173140587b07665d7d5d84ab0cad5bfda7fe

--- Common/Core/SMP/Sequential/vtkAtomicInt.cxx
+++ Common/Core/SMP/Sequential/vtkAtomicInt.cxx
@@ -16,10 +16,126 @@
 
 #if !defined(VTK_HAS_ATOMIC64) || !defined(VTK_HAS_ATOMIC32)
 # include "vtkSimpleCriticalSection.h"
+#elif defined(VTK_WINDOWS_ATOMIC)
+# include "vtkWindows.h"
 #endif
 
 namespace detail
 {
+
+#if !defined(VTK_HAS_ATOMIC32)
+vtkAtomicIntImpl<vtkTypeInt32>::vtkAtomicIntImpl()
+{
+  this->AtomicInt32CritSec = new vtkSimpleCriticalSection;
+}
+
+vtkAtomicIntImpl<vtkTypeInt32>::~vtkAtomicIntImpl()
+{
+  delete this->AtomicInt32CritSec;
+}
+
+vtkTypeInt32 vtkAtomicIntImpl<vtkTypeInt32>::operator++()
+{
+  if (!this->AtomicInt32CritSec)
+    {
+    return 0;
+    }
+  vtkTypeInt32 val;
+  this->AtomicInt32CritSec->Lock();
+  val = ++this->Value;
+  this->AtomicInt32CritSec->Unlock();
+  return val;
+}
+
+vtkTypeInt32 vtkAtomicIntImpl<vtkTypeInt32>::operator--()
+{
+  if (!this->AtomicInt32CritSec)
+    {
+    return 0;
+    }
+  vtkTypeInt32 val;
+  this->AtomicInt32CritSec->Lock();
+  val = --this->Value;
+  this->AtomicInt32CritSec->Unlock();
+  return val;
+}
+
+vtkTypeInt32 vtkAtomicIntImpl<vtkTypeInt32>::operator+=(vtkTypeInt32 val)
+{
+  if (!this->AtomicInt32CritSec)
+    {
+    return 0;
+    }
+  vtkTypeInt32 val2;
+  this->AtomicInt32CritSec->Lock();
+  val2 = (this->Value += val);
+  this->AtomicInt32CritSec->Unlock();
+  return val2;
+}
+
+vtkTypeInt32 vtkAtomicIntImpl<vtkTypeInt32>::load() const
+{
+  if (!this->AtomicInt32CritSec)
+    {
+    return 0;
+    }
+  vtkTypeInt32 val;
+  this->AtomicInt32CritSec->Lock();
+  val = this->Value;
+  this->AtomicInt32CritSec->Unlock();
+  return val;
+}
+
+void vtkAtomicIntImpl<vtkTypeInt32>::store(vtkTypeInt32 val)
+{
+  if (!this->AtomicInt32CritSec)
+    {
+    return;
+    }
+  this->AtomicInt32CritSec->Lock();
+  this->Value = val;
+  this->AtomicInt32CritSec->Unlock();
+}
+#elif defined (VTK_WINDOWS_ATOMIC)
+vtkAtomicIntImpl<vtkTypeInt32>::vtkAtomicIntImpl()
+{
+}
+
+vtkAtomicIntImpl<vtkTypeInt32>::~vtkAtomicIntImpl()
+{
+}
+
+vtkTypeInt32 vtkAtomicIntImpl<vtkTypeInt32>::operator++()
+{
+  return InterlockedIncrement((long*)&this->Value);
+}
+
+vtkTypeInt32 vtkAtomicIntImpl<vtkTypeInt32>::operator--()
+{
+  return InterlockedDecrement((long*)&this->Value);
+}
+
+vtkTypeInt32 vtkAtomicIntImpl<vtkTypeInt32>::operator+=(vtkTypeInt32 val)
+{
+# if defined(VTK_HAS_INTERLOCKEDADD)
+  return InterlockedAdd((long*)&this->Value, val);
+# else
+  return InterlockedExchangeAdd((long*)&this->Value, val) + val;
+# endif
+}
+
+vtkTypeInt32 vtkAtomicIntImpl<vtkTypeInt32>::load() const
+{
+  long retval;
+  InterlockedExchange(&retval, this->Value);
+  return retval;
+}
+
+void vtkAtomicIntImpl<vtkTypeInt32>::store(vtkTypeInt32 val)
+{
+  InterlockedExchange((long*)&this->Value, val);
+}
+#endif // !defined(VTK_HAS_ATOMIC32)
 
 #if !defined(VTK_HAS_ATOMIC64)
 vtkAtomicIntImpl<vtkTypeInt64>::vtkAtomicIntImpl()
@@ -95,80 +211,45 @@ void vtkAtomicIntImpl<vtkTypeInt64>::store(vtkTypeInt64 val)
   this->Value = val;
   this->AtomicInt64CritSec->Unlock();
 }
+#elif defined (VTK_WINDOWS_ATOMIC)
+vtkAtomicIntImpl<vtkTypeInt64>::vtkAtomicIntImpl()
+{
+}
+
+vtkAtomicIntImpl<vtkTypeInt64>::~vtkAtomicIntImpl()
+{
+}
+
+vtkTypeInt64 vtkAtomicIntImpl<vtkTypeInt64>::operator++()
+{
+  return InterlockedIncrement64(&this->Value);
+}
+
+
+vtkTypeInt64 vtkAtomicIntImpl<vtkTypeInt64>::operator--()
+{
+  return InterlockedDecrement64(&this->Value);
+}
+
+vtkTypeInt64 vtkAtomicIntImpl<vtkTypeInt64>::operator+=(vtkTypeInt64 val)
+{
+# if defined(VTK_HAS_INTERLOCKEDADD)
+  return InterlockedAdd64(&this->Value, val);
+# else
+  return InterlockedExchangeAdd64(&this->Value, val) + val;
+# endif
+}
+
+vtkTypeInt64 vtkAtomicIntImpl<vtkTypeInt64>::load() const
+{
+  vtkTypeInt64 retval;
+  InterlockedExchange64(&retval, this->Value);
+  return retval;
+}
+
+void vtkAtomicIntImpl<vtkTypeInt64>::store(vtkTypeInt64 val)
+{
+  InterlockedExchange64(&this->Value, val);
+}
 #endif // !defined(VTK_HAS_ATOMIC64)
-
-#if !defined(VTK_HAS_ATOMIC32)
-vtkAtomicIntImpl<vtkTypeInt32>::vtkAtomicIntImpl()
-{
-  this->AtomicInt32CritSec = new vtkSimpleCriticalSection;
-}
-
-vtkAtomicIntImpl<vtkTypeInt32>::~vtkAtomicIntImpl()
-{
-  delete this->AtomicInt32CritSec;
-}
-
-vtkTypeInt32 vtkAtomicIntImpl<vtkTypeInt32>::operator++()
-{
-  if (!this->AtomicInt32CritSec)
-    {
-    return 0;
-    }
-  vtkTypeInt32 val;
-  this->AtomicInt32CritSec->Lock();
-  val = ++this->Value;
-  this->AtomicInt32CritSec->Unlock();
-  return val;
-}
-
-vtkTypeInt32 vtkAtomicIntImpl<vtkTypeInt32>::operator--()
-{
-  if (!this->AtomicInt32CritSec)
-    {
-    return 0;
-    }
-  vtkTypeInt32 val;
-  this->AtomicInt32CritSec->Lock();
-  val = --this->Value;
-  this->AtomicInt32CritSec->Unlock();
-  return val;
-}
-
-vtkTypeInt32 vtkAtomicIntImpl<vtkTypeInt32>::operator+=(vtkTypeInt32 val)
-{
-  if (!this->AtomicInt32CritSec)
-    {
-    return 0;
-    }
-  vtkTypeInt32 val2;
-  this->AtomicInt32CritSec->Lock();
-  val2 = (this->Value += val);
-  this->AtomicInt32CritSec->Unlock();
-  return val2;
-}
-
-vtkTypeInt32 vtkAtomicIntImpl<vtkTypeInt32>::load() const
-{
-  if (!this->AtomicInt32CritSec)
-    {
-    return 0;
-    }
-  vtkTypeInt32 val;
-  this->AtomicInt32CritSec->Lock();
-  val = this->Value;
-  this->AtomicInt32CritSec->Unlock();
-  return val;
-}
-
-void vtkAtomicIntImpl<vtkTypeInt32>::store(vtkTypeInt32 val)
-{
-  if (!this->AtomicInt32CritSec)
-    {
-    return;
-    }
-  this->AtomicInt32CritSec->Lock();
-  this->Value = val;
-  this->AtomicInt32CritSec->Unlock();
-}
-#endif // !defined(VTK_HAS_ATOMIC32)
 }
