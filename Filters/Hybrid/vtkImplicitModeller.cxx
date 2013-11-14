a3231167f03e9dbac5711f26f27ac9a2c9f38f98
David Gobbi <david.gobbi@gmail.com>
2013-11-14 06:17:46 -0700

    Function call resolution failed because function was static.
    The IBM XL compiler complained about the SetOutputDistance function
    in vtkImplicitModeller.cxx: "Static declarations are not considered
    for a function call if the function is not qualified."  I turned the
    function into a template so that it would be considered.
    
    Change-Id: I24a11bdaa7a36c90d03e02cc4cc6e639a0bcdc0f

--- Filters/Hybrid/vtkImplicitModeller.cxx
+++ Filters/Hybrid/vtkImplicitModeller.cxx
@@ -220,8 +220,9 @@ void SetOutputDistance(double distance, OT *outputValue, double capValue, double
 
 // Convert distance as stored in output (could be scaled and/or non-double
 // type) to double distance with correct scaling
-static void ConvertToDoubleDistance(double inDistance, double &distance,
-                                    double &distance2, double scaleFactor)
+template <class OT>
+void ConvertToDoubleDistance(const OT &inDistance, double &distance,
+                             double &distance2, double scaleFactor)
 {
   if (scaleFactor)
     {
