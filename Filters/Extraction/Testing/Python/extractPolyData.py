0567a19cf4483a5c247b853efd7e068f44e0267a
Will Schroeder <will.schroeder@kitware.com>
2014-01-02 13:49:03 -0500

    Extended filter to support culling unused points.
    Previously the filter just passed points through. This behavior is an
    option now (PassPointsOn) and instead unused points (after extraction) are
    discarded. This implementation is consistent with vtkExtractGeometry and
    is the new default behavior (PassPointsOff).
    
    Change-Id: I8fb396caa236e54c0afdb69f94a0ea785e8bca97

--- Filters/Extraction/Testing/Python/extractPolyData.py
+++ Filters/Extraction/Testing/Python/extractPolyData.py
@@ -12,6 +12,7 @@ sphere = vtk.vtkSphereSource()
 sphere.SetThetaResolution(8)
 sphere.SetPhiResolution(16)
 sphere.SetRadius(1.5)
+
 # Extraction stuff
 t = vtk.vtkTransform()
 t.RotateX(90)
@@ -22,11 +23,48 @@ extract = vtk.vtkExtractPolyDataGeometry()
 extract.SetInputConnection(sphere.GetOutputPort())
 extract.SetImplicitFunction(cylfunc)
 extract.ExtractBoundaryCellsOn()
+extract.PassPointsOn()
 sphereMapper = vtk.vtkPolyDataMapper()
 sphereMapper.SetInputConnection(extract.GetOutputPort())
 sphereMapper.GlobalImmediateModeRenderingOn()
 sphereActor = vtk.vtkActor()
 sphereActor.SetMapper(sphereMapper)
+
+# Extraction stuff - now cull points
+extract2 = vtk.vtkExtractPolyDataGeometry()
+extract2.SetInputConnection(sphere.GetOutputPort())
+extract2.SetImplicitFunction(cylfunc)
+extract2.ExtractBoundaryCellsOn()
+extract2.PassPointsOff()
+sphereMapper2 = vtk.vtkPolyDataMapper()
+sphereMapper2.SetInputConnection(extract2.GetOutputPort())
+sphereActor2 = vtk.vtkActor ()
+sphereActor2.SetMapper(sphereMapper2)
+sphereActor2.AddPosition(2.5, 0, 0)
+
+# Put some glyphs on the points
+glyphSphere = vtk.vtkSphereSource()
+glyphSphere.SetRadius(0.05)
+glyph = vtk.vtkGlyph3D()
+glyph.SetInputConnection(extract.GetOutputPort())
+glyph.SetSourceConnection(glyphSphere.GetOutputPort())
+glyph.SetScaleModeToDataScalingOff()
+glyphMapper = vtk.vtkPolyDataMapper()
+glyphMapper.SetInputConnection(glyph.GetOutputPort())
+glyphActor = vtk.vtkActor()
+glyphActor.SetMapper(glyphMapper)
+
+glyph2 = vtk.vtkGlyph3D()
+glyph2.SetInputConnection(extract2.GetOutputPort())
+glyph2.SetSourceConnection(glyphSphere.GetOutputPort())
+glyph2.SetScaleModeToDataScalingOff()
+glyphMapper2 = vtk.vtkPolyDataMapper()
+glyphMapper2.SetInputConnection(glyph2.GetOutputPort())
+glyphActor2 = vtk.vtkActor()
+glyphActor2.SetMapper(glyphMapper2)
+glyphActor2.AddPosition(2.5, 0, 0)
+
+
 # Create the RenderWindow, Renderer and both Actors
 #
 ren1 = vtk.vtkRenderer()
@@ -38,6 +76,9 @@ iren.SetRenderWindow(renWin)
 # Add the actors to the renderer, set the background and size
 #
 ren1.AddActor(sphereActor)
+ren1.AddActor(glyphActor)
+ren1.AddActor(sphereActor2)
+ren1.AddActor(glyphActor2)
 ren1.ResetCamera()
 ren1.GetActiveCamera().Azimuth(30)
 ren1.SetBackground(0.1,0.2,0.4)
--- Filters/Extraction/Testing/Tcl/extractPolyData.tcl
+++ Filters/Extraction/Testing/Tcl/extractPolyData.tcl
@@ -11,7 +11,7 @@ vtkSphereSource sphere
   sphere SetPhiResolution 16
   sphere SetRadius 1.5
 
-# Extraction stuff
+# Extraction stuff - first just pass points
 vtkTransform t
     t RotateX 90
 vtkCylinder cylfunc
@@ -21,13 +21,46 @@ vtkExtractPolyDataGeometry extract
     extract SetInputConnection [sphere GetOutputPort]
     extract SetImplicitFunction cylfunc
     extract ExtractBoundaryCellsOn
-
+    extract PassPointsOn
 vtkPolyDataMapper  sphereMapper
     sphereMapper SetInputConnection [extract GetOutputPort]
     sphereMapper GlobalImmediateModeRenderingOn
-
 vtkActor sphereActor
     sphereActor SetMapper sphereMapper
+
+# Extraction stuff - now cull points
+vtkExtractPolyDataGeometry extract2
+    extract2 SetInputConnection [sphere GetOutputPort]
+    extract2 SetImplicitFunction cylfunc
+    extract2 ExtractBoundaryCellsOn
+    extract2 PassPointsOff
+vtkPolyDataMapper  sphereMapper2
+    sphereMapper2 SetInputConnection [extract2 GetOutputPort]
+vtkActor sphereActor2
+    sphereActor2 SetMapper sphereMapper2
+    sphereActor2 AddPosition 2.5 0 0
+
+# Put some glyphs on the points
+vtkSphereSource glyphSphere
+   glyphSphere SetRadius 0.05
+vtkGlyph3D glyph
+    glyph SetInputConnection [extract GetOutputPort]
+    glyph SetSourceConnection [glyphSphere GetOutputPort]
+    glyph SetScaleModeToDataScalingOff
+vtkPolyDataMapper  glyphMapper
+    glyphMapper SetInputConnection [glyph GetOutputPort]
+vtkActor glyphActor
+    glyphActor SetMapper glyphMapper
+
+vtkGlyph3D glyph2
+    glyph2 SetInputConnection [extract2 GetOutputPort]
+    glyph2 SetSourceConnection [glyphSphere GetOutputPort]
+    glyph2 SetScaleModeToDataScalingOff
+vtkPolyDataMapper  glyphMapper2
+    glyphMapper2 SetInputConnection [glyph2 GetOutputPort]
+vtkActor glyphActor2
+    glyphActor2 SetMapper glyphMapper2
+    glyphActor2 AddPosition 2.5 0 0
 
 # Create the RenderWindow, Renderer and both Actors
 #
@@ -41,6 +74,9 @@ vtkRenderWindowInteractor iren
 # Add the actors to the renderer, set the background and size
 #
 ren1 AddActor sphereActor
+ren1 AddActor glyphActor
+ren1 AddActor sphereActor2
+ren1 AddActor glyphActor2
 
 ren1 ResetCamera
 [ren1 GetActiveCamera] Azimuth 30
