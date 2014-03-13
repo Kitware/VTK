d8a74423e43c5fa0a5e0114fe6c45915ad8004b7
Brad King <brad.king@kitware.com>
2014-03-13 10:30:34 -0400

    IO/XML: Remove unnecessary <sstream> include
    The VTK standard headers include <iosfwd> and make std::istringstream
    available for use as an incomplete type.  That is all vtkXMLReader.h
    needs.  Remove the extra <sstream> include.
    
    In particular, by including it before the VTK headers it causes system
    headers to be included with inconsistent Large File Support definitions
    on AIX, leading to runtime shared library unresolved symbols.  Remove
    the inclusion to fix this.
    
    Change-Id: I78ec9efae24c8f721f079ab8946afcde413fe17f

--- IO/XML/vtkXMLReader.h
+++ IO/XML/vtkXMLReader.h
@@ -22,7 +22,6 @@
 #define __vtkXMLReader_h
 
 #include "vtkIOXMLModule.h" // For export macro
-#include <sstream>          // For istringstream ivar.
 #include "vtkAlgorithm.h"
 
 class vtkAbstractArray;
