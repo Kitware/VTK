5d973c6485d6499757a4342d741d4d6ddbd2d193
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2014-05-07 14:38:11 -0400

    Moved the VBO and IBO creation to helper functions
    Get straight to the types, separate them out and generalize a little.
    
    Change-Id: Id2d3c61e854265c9bf8db1bb2903822596afce85

---
+++ Rendering/OpenGL2/vtkglVBOHelper.h
@@ -0,0 +1,124 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#ifndef __vtkGLVBOHelpher_h
+#define __vtkGLVBOHelpher_h
+
+#include "vtkglBufferObject.h"
+#include <vector>
+
+namespace vtkgl
+{
+
+// Sizes/offsets are all in bytes as OpenGL API expects them.
+struct VBOLayout
+{
+  size_t VertexCount; // Number of vertices in the VBO
+  int Stride;       // The size of a complete vertex + attributes
+  int VertexOffset; // Offset of the vertex
+  int NormalOffset; // Offset of the normal
+  int ColorOffset;  // Offset of the color
+  int ColorComponents; // Number of color components
+};
+
+// Take the points, and pack them into the VBO object supplied. This currently
+// takes whatever the input type might be and packs them into a VBO using
+// floats for the vertices and normals, and unsigned char for the colors (if
+// the array is non-null).
+template<typename T>
+VBOLayout CreateTriangleVBO(T* points, T* normals, vtkIdType numPts,
+                            unsigned char *colors, int colorComponents,
+                            BufferObject &vertexBuffer)
+{
+  VBOLayout layout;
+  // Figure out how big each block will be, currently 6 or 7 floats.
+  int blockSize = 6;
+  layout.VertexOffset = 0;
+  layout.NormalOffset = sizeof(float) * 3;
+  if (colors)
+    {
+    ++blockSize;
+    layout.ColorComponents = colorComponents;
+    layout.ColorOffset = sizeof(float) * 6;
+    }
+  else
+    {
+    layout.ColorComponents = 0;
+    layout.ColorOffset = 0;
+    }
+  layout.Stride = sizeof(float) * blockSize;
+
+  // Create a buffer, and copy the data over.
+  std::vector<float> packedVBO;
+  packedVBO.resize(blockSize * numPts);
+  std::vector<float>::iterator it = packedVBO.begin();
+
+  for (vtkIdType i = 0; i < numPts; ++i)
+    {
+    // Vertices
+    *(it++) = *(points++);
+    *(it++) = *(points++);
+    *(it++) = *(points++);
+    *(it++) = *(normals++);
+    *(it++) = *(normals++);
+    *(it++) = *(normals++);
+    if (colors)
+      {
+      if (colorComponents == 4)
+        {
+        *(it++) = *reinterpret_cast<float *>(colors);
+        colors += 4;
+        }
+      else
+        {
+        unsigned char c[4] = { *(colors++), *(colors++), *(colors++), 255 };
+        *(it++) = *reinterpret_cast<float *>(c);
+        }
+      }
+    if (it == packedVBO.end())
+      {
+      cout << "Error!!!" << endl;
+      break;
+      }
+    }
+  vertexBuffer.upload(packedVBO, vtkgl::BufferObject::ArrayBuffer);
+  layout.VertexCount = numPts;
+  return layout;
+}
+
+size_t CreateIndexBuffer(vtkCellArray *cells, BufferObject &indexBuffer,
+                         int num)
+{
+  std::vector<unsigned int> indexArray;
+  vtkIdType* indices(NULL);
+  vtkIdType points(0);
+  indexArray.reserve(cells->GetNumberOfCells() * num);
+  for (cells->InitTraversal(); cells->GetNextCell(points, indices); )
+    {
+    if (points != num)
+      {
+      exit(-1); // assert(points == num);
+      }
+    for (int j = 0; j < num; ++j)
+      {
+      indexArray.push_back(static_cast<unsigned int>(*(indices++)));
+      }
+    }
+  indexBuffer.upload(indexArray, vtkgl::BufferObject::ElementArrayBuffer);
+  return indexArray.size();
+}
+
+} // End namespace
+
+#endif // __vtkGLVBOHelpher_h
