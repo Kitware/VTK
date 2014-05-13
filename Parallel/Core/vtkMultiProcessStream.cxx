063b174d3f441a2ecd3c34c1b1fdd948b0342799
Dan Lipsa <dan.lipsa@kitware.com>
2014-05-13 11:53:26 -0400

    vtkMultiProcessStream: Add insert and extract operators for bool
    Change-Id: I9c2c618373600e289b4917fa03fd042359099d67

--- Parallel/Core/vtkMultiProcessStream.cxx
+++ Parallel/Core/vtkMultiProcessStream.cxx
@@ -560,6 +560,16 @@ vtkMultiProcessStream& vtkMultiProcessStream::operator << (char value)
 }
 
 //----------------------------------------------------------------------------
+vtkMultiProcessStream& vtkMultiProcessStream::operator << (bool v)
+{
+  char value = v;
+  this->Internals->Data.push_back(vtkInternals::char_value);
+  this->Internals->Push(reinterpret_cast<unsigned char*>(&value), sizeof(char));
+  return (*this);
+}
+
+
+//----------------------------------------------------------------------------
 vtkMultiProcessStream& vtkMultiProcessStream::operator << (unsigned int value)
 {
   this->Internals->Data.push_back(vtkInternals::uint32_value);
@@ -693,6 +703,18 @@ vtkMultiProcessStream& vtkMultiProcessStream::operator >> (char &value)
   this->Internals->Pop(reinterpret_cast<unsigned char*>(&value), sizeof(char));
   return (*this);
 }
+
+//----------------------------------------------------------------------------
+vtkMultiProcessStream& vtkMultiProcessStream::operator >> (bool &v)
+{
+  char value;
+  assert(this->Internals->Data.front() == vtkInternals::char_value);
+  this->Internals->Data.pop_front();
+  this->Internals->Pop(reinterpret_cast<unsigned char*>(&value), sizeof(char));
+  v = static_cast<bool>(value);
+  return (*this);
+}
+
 
 //----------------------------------------------------------------------------
 vtkMultiProcessStream& vtkMultiProcessStream::operator >> (unsigned int &value)
