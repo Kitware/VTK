f1ab26253e03009a850d62998fbb43fbc0c37fd8
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-05-21 22:49:40 -0400

    Added copyright
    Change-Id: I9089bcc85f0dfc724d0077d08933f5b2741f274b

--- Rendering/Volume2/vtkSinglePassVolumeMapper.cxx
+++ Rendering/Volume2/vtkSinglePassVolumeMapper.cxx
@@ -1,4 +1,19 @@
-﻿#include "vtkSinglePassVolumeMapper.h"
+﻿/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkOpenGLProjectedTetrahedraMapper.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "vtkSinglePassVolumeMapper.h"
 
 #include "vtkGLSLShader.h"
 #include "vtkOpenGLOpacityTable.h"
