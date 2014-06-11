523e3c06d283586e6aade42503c53ad574e4e24b
Ben Boeckel <ben.boeckel@kitware.com>
2014-06-11 10:03:27 -0400

    vtkFiltersPython: Disable without Python wrapping
    Change-Id: Ib90ccaa7ea9c7dfc2ea4e7647f423a0c45f31b04

--- Filters/Python/module.cmake
+++ Filters/Python/module.cmake
@@ -1,9 +1,11 @@
-vtk_module(vtkFiltersPython
-  GROUPS
-    StandAlone
-  DEPENDS
-    vtkCommonExecutionModel
-    vtkPython
-  PRIVATE_DEPENDS
-    vtkWrappingPythonCore
-  )
+if (VTK_WRAP_PYTHON)
+  vtk_module(vtkFiltersPython
+    GROUPS
+      StandAlone
+    DEPENDS
+      vtkCommonExecutionModel
+      vtkPython
+    PRIVATE_DEPENDS
+      vtkWrappingPythonCore
+    )
+endif ()
