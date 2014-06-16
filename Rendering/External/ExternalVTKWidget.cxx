f9f4ec95a81178db09832403a3692ec1a439d55f
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2014-06-16 15:15:19 -0400

    ENH: Add ExternalVTKWidget
    This widget makes it easy to use the VTK rendering framework in external
    window managers and/or external applications that do not require VTK to draw its
    own window. ExternalVTKWidget uses the vtkExternalOpenGLRenderWindow and
    vtkExternalOpenGLRenderer classes to use the OpenGL context managed by the
    external application.
    
    Change-Id: I629ee93d427e0d0c791748c010b597b6e327f2a9

---
+++ Rendering/External/ExternalVTKWidget.cxx
@@ -0,0 +1,52 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    ExternalVTKWidget.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "ExternalVTKWidget.h"
+#include "vtkObjectFactory.h"
+
+vtkStandardNewMacro(ExternalVTKWidget);
+
+//----------------------------------------------------------------------------
+ExternalVTKWidget::ExternalVTKWidget()
+{
+  this->RenderWindow = vtkExternalOpenGLRenderWindow::New();
+  this->Renderer = vtkExternalOpenGLRenderer::New();
+  this->RenderWindow->AddRenderer(this->Renderer);
+}
+
+//----------------------------------------------------------------------------
+ExternalVTKWidget::~ExternalVTKWidget()
+{
+  this->Renderer->Delete();
+  this->RenderWindow->Delete();
+}
+
+//----------------------------------------------------------------------------
+vtkExternalOpenGLRenderWindow* ExternalVTKWidget::GetRenderWindow()
+{
+  return this->RenderWindow;
+}
+
+//----------------------------------------------------------------------------
+vtkExternalOpenGLRenderer* ExternalVTKWidget::GetRenderer()
+{
+  return this->Renderer;
+}
+
+//----------------------------------------------------------------------------
+void ExternalVTKWidget::PrintSelf(ostream &os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
---
+++ Rendering/External/ExternalVTKWidget.h
@@ -0,0 +1,58 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    ExternalVTKWidget.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME ExternalVTKWidget - Use VTK rendering in an external window/application
+// .SECTION Description
+// ExternalVTKWidget provides an easy way to render VTK objects in an external
+// environment using the VTK rendering framework without drawing a new window.
+
+#ifndef __ExternalVTKWidget_h
+#define __ExternalVTKWidget_h
+
+#include "vtkExternalOpenGLRenderer.h"
+#include "vtkExternalOpenGLRenderWindow.h"
+#include "vtkObject.h"
+#include "vtkRenderingExternalModule.h" // For export macro
+
+// Forward Declarations
+class vtkRenderer;
+class vtkRenderWindow;
+
+class VTKRENDERINGEXTERNAL_EXPORT ExternalVTKWidget :
+  public vtkObject
+{
+public:
+  static ExternalVTKWidget *New();
+  vtkTypeMacro(ExternalVTKWidget, vtkObject);
+  void PrintSelf(ostream &os, vtkIndent indent);
+
+  // Get the render window
+  vtkExternalOpenGLRenderWindow* GetRenderWindow();
+
+  // Get the renderer
+  vtkExternalOpenGLRenderer* GetRenderer();
+
+protected:
+  ExternalVTKWidget();
+  ~ExternalVTKWidget();
+
+  vtkExternalOpenGLRenderWindow* RenderWindow;
+  vtkExternalOpenGLRenderer* Renderer;
+
+private:
+  ExternalVTKWidget(const ExternalVTKWidget&); // Not implemented
+  void operator=(const ExternalVTKWidget&); // Not implemented
+};
+
+#endif //__ExternalVTKWidget_h
