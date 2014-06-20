59c382c141b935fcd62956f95c9c49f99ff1740d
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2014-06-20 11:41:42 -0400

    vtk_common: Changed the CDash submission url for VTK
    This was moved quite some time ago, go directly to the current URL.

--- vtk_common.cmake
+++ vtk_common.cmake
@@ -366,8 +366,8 @@ endif()
   # CTestConfig.cmake info here.
   set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
   set(CTEST_DROP_METHOD "http")
-  set(CTEST_DROP_SITE "www.cdash.org")
-  set(CTEST_DROP_LOCATION "/CDash/submit.php?project=VTK")
+  set(CTEST_DROP_SITE "open.cdash.org")
+  set(CTEST_DROP_LOCATION "/submit.php?project=VTK")
   set(CTEST_DROP_SITE_CDASH TRUE)
 endif()
 
