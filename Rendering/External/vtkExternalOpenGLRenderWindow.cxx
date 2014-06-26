a75302c392e085d763c033d24beb3f1e9a99f87f
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-06-26 00:53:29 -0400

    Fixed splitviewport stereo rendering
    Change-Id: Ife6f8a416eedbaadde9c927a473a4cfd02563584

--- Rendering/External/vtkExternalOpenGLRenderWindow.cxx
+++ Rendering/External/vtkExternalOpenGLRenderWindow.cxx
@@ -39,12 +39,34 @@ void vtkExternalOpenGLRenderWindow::Start(void)
   this->MakeCurrent();
 }
 
+#include <vtkCamera.h>
+#include <vtkRenderer.h>
+#include <vtkRendererCollection.h>
 //----------------------------------------------------------------------------
 void vtkExternalOpenGLRenderWindow::Render()
 {
   GLint viewport[4];
+  GLint bufferType;
+
+  // NOTE: We have to consider the size of the window as summation of
+  // viewport[2] + viewport[0] (and same for vertical dimension) because
+  // it is possible that this code gets called by external system twice. In the
+  // first case you may get (0, 0, 960, 1080) viewport dimensions and in the
+  // second case you will get (960, 0, 960, 1080). In the second case, the window
+  // diemsion needs to be 960+960 (initial pos + width) or else the rendering on
+  // the second viewport won't show anything.
   glGetIntegerv(GL_VIEWPORT, viewport);
-  this->SetSize(viewport[2], viewport[3]);
+  this->SetSize(viewport[2] + viewport[0], viewport[3] + viewport[1]);
+
+  glGetIntegerv(GL_DRAW_BUFFER, &bufferType);
+  if (bufferType == GL_BACK_RIGHT || bufferType == GL_RIGHT || bufferType == GL_FRONT_RIGHT)
+    {
+    this->GetRenderers()->GetFirstRenderer()->GetActiveCamera()->SetLeftEye(0);
+    }
+  else
+    {
+    this->GetRenderers()->GetFirstRenderer()->GetActiveCamera()->SetLeftEye(1);
+    }
   this->Superclass::Render();
 }
 
