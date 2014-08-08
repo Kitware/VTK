980d64882d3d46a7de412c5db44da271368ab289
Matthew Woehlke <matthew.woehlke@kitware.com>
2014-08-08 15:43:09 -0400

    Fix rotation handling in vtkTransformInterpolator.
    Overhaul the handling of rotations in vtkTransformInterpolator, which
    was being done in a completely wrong manner due to performing
    interpolations on angle-axis rotation values rather than actual
    quaternions.
    
    Change-Id: I593a2a8baeeab4b3734cbaece99dbe5da497fe98

--- Rendering/Core/vtkTransformInterpolator.cxx
+++ Rendering/Core/vtkTransformInterpolator.cxx
@@ -15,9 +15,11 @@
 #include "vtkTransformInterpolator.h"
 #include "vtkObjectFactory.h"
 #include "vtkTransform.h"
+#include "vtkMath.h"
 #include "vtkMatrix4x4.h"
 #include "vtkProp3D.h"
 #include "vtkTupleInterpolator.h"
+#include "vtkQuaternion.h"
 #include "vtkQuaternionInterpolator.h"
 #include <list>
 
@@ -31,14 +33,13 @@ struct vtkQTransform
   double Time;
   double P[3];
   double S[3];
-  double Q[4];
+  vtkQuaterniond Q;
 
   vtkQTransform()
     {
       this->Time = 0.0;
       this->P[0] = this->P[1] = this->P[2] = 0.0;
       this->S[0] = this->S[1] = this->S[2] = 0.0;
-      this->Q[0] = this->Q[1] = this->Q[2] = this->Q[3] = 0.0;
     }
   vtkQTransform(double t, vtkTransform *xform)
     {
@@ -47,13 +48,15 @@ struct vtkQTransform
         {
         xform->GetPosition(this->P);
         xform->GetScale(this->S);
-        xform->GetOrientationWXYZ(this->Q); //Rotation (in degrees) around unit vector
+        double q[4];
+        xform->GetOrientationWXYZ(q); //Rotation (in degrees) around unit vector
+        q[0] = vtkMath::RadiansFromDegrees(q[0]);
+        this->Q.SetRotationAngleAndAxis(q[0], q+1);
         }
       else
         {
         this->P[0] = this->P[1] = this->P[2] = 0.0;
         this->S[0] = this->S[1] = this->S[2] = 0.0;
-        this->Q[0] = this->Q[1] = this->Q[2] = this->Q[3] = 0.0;
         }
     }
 };
@@ -380,9 +383,11 @@ void vtkTransformInterpolator::InterpolateTransform(double t,
     }
 
   double P[3],S[3],Q[4];
+  vtkQuaterniond q;
   this->PositionInterpolator->InterpolateTuple(t,P);
   this->ScaleInterpolator->InterpolateTuple(t,S);
-  this->RotationInterpolator->InterpolateQuaternion(t,Q);
+  this->RotationInterpolator->InterpolateQuaternion(t,q);
+  Q[0] = vtkMath::DegreesFromRadians(q.GetRotationAngleAndAxis(Q+1));
 
   xform->Translate(P);
   xform->RotateWXYZ(Q[0],Q+1);
