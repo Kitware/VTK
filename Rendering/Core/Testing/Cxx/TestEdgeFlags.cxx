c09b0cf1e5663dbfa449fbaf3a0fe1b57c46e175
Ken Martin <ken.martin@kitware.com>
2014-10-06 14:50:06 -0400

    temporarily turn of mesa test until Joachim ets a chance to look at it
    Change-Id: Ie8d3c17a5bfcabf40d80cd7e41a0c7510194e0fd

--- Rendering/Core/Testing/Cxx/TestEdgeFlags.cxx
+++ Rendering/Core/Testing/Cxx/TestEdgeFlags.cxx
@@ -20,8 +20,8 @@
 #include "vtkCamera.h"
 #include "vtkCellArray.h"
 #include "vtkNew.h"
-#include "vtkOpenGLRenderWindow.h"
-#include "vtkOpenGLExtensionManager.h"
+//#include "vtkOpenGLRenderWindow.h"
+//#include "vtkOpenGLExtensionManager.h"
 #include "vtkPointData.h"
 #include "vtkPoints.h"
 #include "vtkPolyData.h"
@@ -34,12 +34,12 @@
 #include "vtkTestUtilities.h"
 #include "vtkUnsignedCharArray.h"
 
-bool IsDriverMesa(vtkRenderWindow *renwin)
-{
-  vtkOpenGLRenderWindow *context
-    = vtkOpenGLRenderWindow::SafeDownCast(renwin);
-  return context->GetExtensionManager()->DriverIsMesa();
-}
+// bool IsDriverMesa(vtkRenderWindow *renwin)
+// {
+//   vtkOpenGLRenderWindow *context
+//     = vtkOpenGLRenderWindow::SafeDownCast(renwin);
+//   return context->GetExtensionManager()->DriverIsMesa();
+// }
 
 int TestEdgeFlags(int argc, char *argv[])
 {
@@ -154,12 +154,12 @@ int TestEdgeFlags(int argc, char *argv[])
 
   renWin->Render();
 
-  if (IsDriverMesa(renWin.Get()))
-    {
-    // Mesa support for edge flags is buggy.
-    std::cout << "Mesa detected. Skip the test." << std::endl;
-    return !vtkRegressionTester::PASSED;
-    }
+  // if (IsDriverMesa(renWin.Get()))
+  //   {
+  //   // Mesa support for edge flags is buggy.
+  //   std::cout << "Mesa detected. Skip the test." << std::endl;
+  //   return !vtkRegressionTester::PASSED;
+  //   }
   // Compare image
   int retVal = vtkRegressionTestImage(renWin.Get());
   if (retVal == vtkRegressionTester::DO_INTERACTOR)
