fd385ce88b790ed150e480fdebc0755bfb1ee06c
Sean McBride <sean@rogue-research.com>
2014-11-26 11:07:11 -0500

    Hopefully supress dashboard compiler warning in QT header
    Suppression used since we can't change QT itself easily:
    
    In file included from /Users/kitware/Dashboards/My Tests/Nightly/VTK/build-gcc/GUISupport/Qt/moc_Q4VTKWidgetPlugin.cxx:10:
    In file included from /Users/kitware/Dashboards/My Tests/Nightly/VTK/build-gcc/GUISupport/Qt/../../../source/GUISupport/Qt/Q4VTKWidgetPlugin.h:28:
    In file included from /Users/kitware/Dashboards/Support/Qt-4.8.0/include/QtDesigner/QDesignerCustomWidgetInterface:1:
    In file included from /Users/kitware/Dashboards/Support/Qt-4.8.0/include/QtDesigner/customwidget.h:45:
    /Users/kitware/Dashboards/Support/Qt-4.8.0/lib/QtDesigner.framework/Headers/extension.h:94:50: warning: unused parameter 'manager' [-Wunused-parameter]
    inline T qt_extension(QAbstractExtensionManager* manager, QObject *object)
                                                     ^
    /Users/kitware/Dashboards/Support/Qt-4.8.0/lib/QtDesigner.framework/Headers/extension.h:94:68: warning: unused parameter 'object' [-Wunused-parameter]
    inline T qt_extension(QAbstractExtensionManager* manager, QObject *object)
                                                                       ^
    
    Change-Id: Ia645327ecffe1ef06e5d4349d01dd5da80ecf295

--- GUISupport/Qt/Q4VTKWidgetPlugin.cxx
+++ GUISupport/Qt/Q4VTKWidgetPlugin.cxx
@@ -22,10 +22,13 @@
  !!! license.
 =========================================================================*/
 
-#ifdef _MSC_VER
 // Disable warnings that Qt headers give.
+#ifdef _MSC_VER
 #pragma warning(disable:4127)
 #pragma warning(disable:4512)
+#endif
+#if defined(__GNUC__) && (__GNUC__>4) || (__GNUC__==4 && __GNUC_MINOR__>=2)
+#pragma GCC diagnostic ignored "-Wunused-parameter"
 #endif
 
 #if !defined(_DEBUG)
