08e8c63d2334299aff8fed521adba4f0b2ee4cdf
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-12-05 13:16:16 -0500

    Fixed bad sampling issues when dealing with large sample distances
    Change-Id: Ic4b339a0cd1d73841d2dd4f0f33de4ee80a908e1

--- Rendering/VolumeOpenGL2/vtkOpenGLGPUVolumeRayCastMapper.cxx
+++ Rendering/VolumeOpenGL2/vtkOpenGLGPUVolumeRayCastMapper.cxx
@@ -949,7 +949,7 @@ void vtkOpenGLGPUVolumeRayCastMapper::vtkInternal::UpdateNoiseTexture()
         while(i < size)
           {
           this->NoiseTextureData[j * size + i] =
-            static_cast<float>(noiseGenerator->EvaluateFunction(i, j, 0.0));
+            static_cast<float>(noiseGenerator->EvaluateFunction(i, j, 0.0) * 2.0 + 1);
           ++i;
           }
         ++j;
@@ -960,6 +960,8 @@ void vtkOpenGLGPUVolumeRayCastMapper::vtkInternal::UpdateNoiseTexture()
 
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    GLfloat borderColor[4]={0.0,0.0,0.0,0.0};
+    glTexParameterfv(GL_TEXTURE_2D,GL_TEXTURE_BORDER_COLOR,borderColor);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glActiveTexture(GL_TEXTURE0);
