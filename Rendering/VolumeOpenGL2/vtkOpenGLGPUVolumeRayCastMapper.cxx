84644d21711af197cf980aaef890f81411000443
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2014-12-17 02:57:39 -0500

    Update texture with the data
    Change-Id: Id63a7203c11fb8e8c75ba2b68c848ce3e54143f5

--- Rendering/VolumeOpenGL2/vtkOpenGLGPUVolumeRayCastMapper.cxx
+++ Rendering/VolumeOpenGL2/vtkOpenGLGPUVolumeRayCastMapper.cxx
@@ -1060,6 +1060,11 @@ void vtkOpenGLGPUVolumeRayCastMapper::vtkInternal::UpdateDepthTexture(
     }
 
   this->DepthTextureObject->Activate();
+  glCopyTexSubImage2D(GL_TEXTURE_2D, 0, 0 , 0,
+                      this->WindowLowerLeft[0], this->WindowLowerLeft[1],
+                      this->WindowSize[0], this->WindowSize[1]);
+  this->DepthTextureObject->Deactivate();
+
 //  glActiveTexture(GL_TEXTURE4);
 //  if (!this->DepthTextureId)
 //    {
