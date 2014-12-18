b65dde23f25b22bb50084b62fbe43f3ac19ef130
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2014-12-18 13:59:49 -0500

    Initial work to reformat the window level test
    Change-Id: I6682a41da3857e306436f4ef5c93c9d4fee56f4e

--- Rendering/Volume/Testing/Cxx/TestSmartVolumeMapperWindowLevel.cxx
+++ Rendering/Volume/Testing/Cxx/TestSmartVolumeMapperWindowLevel.cxx
@@ -17,211 +17,81 @@
 // This test volume renders a synthetic dataset with unsigned char values,
 // with the composite method.
 
-#include "vtkSphere.h"
-#include "vtkSampleFunction.h"
-
-#include "vtkSmartVolumeMapper.h"
-#include "vtkTestUtilities.h"
 #include "vtkColorTransferFunction.h"
+#include "vtkDataArray.h"
+#include "vtkImageData.h"
+#include "vtkInteractorStyleTrackballCamera.h"
+#include "vtkMetaImageReader.h"
+#include "vtkNew.h"
 #include "vtkPiecewiseFunction.h"
+#include "vtkPointData.h"
+#include "vtkRegressionTestImage.h"
 #include "vtkRenderer.h"
 #include "vtkRenderWindow.h"
 #include "vtkRenderWindowInteractor.h"
+#include "vtkSmartVolumeMapper.h"
+#include "vtkTestUtilities.h"
 #include "vtkVolumeProperty.h"
-#include "vtkCamera.h"
-#include "vtkRegressionTestImage.h"
-#include "vtkImageShiftScale.h"
-#include "vtkImageData.h"
-#include "vtkPointData.h"
-#include "vtkDataArray.h"
-
-#include "vtkContourFilter.h"
-#include "vtkPolyDataMapper.h"
-#include "vtkActor.h"
 
 int TestSmartVolumeMapperWindowLevel(int argc,
                                      char *argv[])
 {
   cout << "CTEST_FULL_OUTPUT (Avoid ctest truncation of output)" << endl;
 
-  // Create a spherical implicit function.
-  vtkSphere *shape=vtkSphere::New();
-  shape->SetRadius(0.1);
-  shape->SetCenter(0.0,0.0,0.0);
+  vtkNew<vtkRenderWindow> renWin;
+  renWin->SetSize(400, 401);
+  renWin->SetMultiSamples(0);
 
-  vtkSampleFunction *source=vtkSampleFunction::New();
-  source->SetImplicitFunction(shape);
-  shape->Delete();
-  source->SetOutputScalarTypeToDouble();
-  source->SetSampleDimensions(127,127,127); // intentional NPOT dimensions.
-  source->SetModelBounds(-1.0,1.0,-1.0,1.0,-1.0,1.0);
-  source->SetCapping(false);
-  source->SetComputeNormals(false);
-  source->SetScalarArrayName("values");
+  vtkNew<vtkRenderer> ren;
+  renWin->AddRenderer(ren.GetPointer());
 
-  source->Update();
+  vtkNew<vtkRenderWindowInteractor> iren;
+  iren->SetRenderWindow(renWin.GetPointer());
 
-  vtkDataArray *a=source->GetOutput()->GetPointData()->GetScalars("values");
-  double range[2];
-  a->GetRange(range);
+  vtkNew<vtkInteractorStyleTrackballCamera> style;
+  iren->SetInteractorStyle(style.GetPointer());
 
-  vtkImageShiftScale *t=vtkImageShiftScale::New();
-  t->SetInputConnection(source->GetOutputPort());
-  source->Delete();
-  t->SetShift(-range[0]);
-  double magnitude=range[1]-range[0];
-  if(magnitude==0.0)
-    {
-    magnitude=1.0;
-    }
-  t->SetScale(255.0/magnitude);
-  t->SetOutputScalarTypeToUnsignedChar();
+  char* fname = vtkTestUtilities::ExpandDataFileName(argc, argv,
+                                                     "Data/HeadMRVolume.mhd");
 
-  t->Update();
+  vtkNew<vtkMetaImageReader> reader;
+  reader->SetFileName(fname);
+  reader->Update();
+  delete [] fname;
 
-  vtkRenderWindow *renWin=vtkRenderWindow::New();
+  vtkNew<vtkSmartVolumeMapper> mapper;
+  mapper->SetInputConnection(reader->GetOutputPort());
 
-  vtkRenderer *ren1=vtkRenderer::New();
-  ren1->SetBackground(0.0,0.0,0.5);
-  ren1->SetViewport(0.0,0.0,0.33,1.0);
-  renWin->AddRenderer(ren1);
-  ren1->Delete();
+  vtkNew<vtkColorTransferFunction> ctf;
+  ctf->AddRGBPoint(0.0, 0.0, 0.0, 0.0);
+  ctf->AddRGBPoint(98.70, 169/255.0, 176/255.0, 107/255.0);
+  ctf->AddRGBPoint(269.04, 100/255.0, 116/255.0, 191/255.0);
+  ctf->AddRGBPoint(449.88, 0.0, 128/255.0, 174/255.0);
+  ctf->AddRGBPoint(641.00, 1.0, 1.0, 1.0);
 
-  vtkRenderer *ren2=vtkRenderer::New();
-  ren2->SetBackground(0.5,0.5,0.5);
-  ren2->SetViewport(0.33,0.0,0.66,1.0);
-  renWin->AddRenderer(ren2);
-  ren2->Delete();
+  vtkNew<vtkPiecewiseFunction> pwf;
+  pwf->AddPoint(0.0, 0.0);
+  pwf->AddPoint(36.05, 0.0);
+  pwf->AddPoint(218.30, 0.17);
+  pwf->AddPoint(412.41, 1.0);
+  pwf->AddPoint(641.0, 1.0);
 
-  vtkRenderer *ren3=vtkRenderer::New();
-  ren3->SetBackground(0.0,0.5,0.0);
-  ren3->SetViewport(0.66,0.0,1.0,1.0);
-  renWin->AddRenderer(ren3);
-  ren3->Delete();
+  vtkNew<vtkVolumeProperty> volumeProperty;
+  volumeProperty->SetScalarOpacity(pwf.GetPointer());
+  volumeProperty->SetColor(ctf.GetPointer());
 
-  renWin->SetSize(301,300); // intentional odd and NPOT  width/height
-
-  vtkRenderWindowInteractor *iren=vtkRenderWindowInteractor::New();
-  iren->SetRenderWindow(renWin);
-  renWin->Delete();
-
-  renWin->Render(); // make sure we have an OpenGL context.
-
-  vtkSmartVolumeMapper *volumeMapper;
-  vtkVolumeProperty *volumeProperty;
-  vtkVolume *volume;
-
-  volumeMapper=vtkSmartVolumeMapper::New();
-  volumeMapper->SetBlendModeToComposite();
-  volumeMapper->SetInputConnection(
-    t->GetOutputPort());
-
-  volumeProperty=vtkVolumeProperty::New();
-  volumeProperty->ShadeOff();
-  volumeProperty->SetInterpolationType(VTK_LINEAR_INTERPOLATION);
-
-  vtkPiecewiseFunction *compositeOpacity = vtkPiecewiseFunction::New();
-  compositeOpacity->AddPoint(0.0,0.0);
-  compositeOpacity->AddPoint(80.0,1.0);
-  compositeOpacity->AddPoint(80.1,0.0);
-  compositeOpacity->AddPoint(255.0,0.0);
-  volumeProperty->SetScalarOpacity(compositeOpacity); // composite first.
-
-  vtkColorTransferFunction *color=vtkColorTransferFunction::New();
-  color->AddRGBPoint(0.0  ,0.0,0.0,1.0);
-  color->AddRGBPoint(40.0  ,1.0,0.0,0.0);
-  color->AddRGBPoint(255.0,1.0,1.0,1.0);
-  volumeProperty->SetColor(color);
-  color->Delete();
-
-  volume=vtkVolume::New();
-  volume->SetMapper(volumeMapper);
-  volume->SetProperty(volumeProperty);
-  ren1->AddViewProp(volume);
-
-//  vtkSmartVolumeMapper *volumeMapper2;
-//  vtkVolume *volume2;
-//
-//  volumeMapper2=vtkSmartVolumeMapper::New();
-//  volumeMapper2->SetBlendModeToComposite();
-//  volumeMapper2->SetInputConnection(
-//    t->GetOutputPort());
-//#ifndef VTK_OPENGL2
-//  // 3D texture mode.
-//  volumeMapper2->SetRequestedRenderModeToRayCastAndTexture();
-//#else
-//  volumeMapper2->SetRequestedRenderModeToDefault();
-//#endif
-//  volume2=vtkVolume::New();
-//  volume2->SetMapper(volumeMapper2);
-//  volume2->SetProperty(volumeProperty);
-//  ren2->AddViewProp(volume2);
-
-  vtkContourFilter * cf = vtkContourFilter::New();
-  cf->SetInputConnection(t->GetOutputPort());
-  cf->SetValue(0, 80);
-  vtkPolyDataMapper * pm = vtkPolyDataMapper::New();
-  pm->SetInputConnection(cf->GetOutputPort());
-  vtkActor * ac = vtkActor::New();
-  ac->SetMapper(pm);
-  ren2->AddActor(ac);
-  ac->Delete();
-  pm->Delete();
-  cf->Delete();
-
-
-  vtkSmartVolumeMapper *volumeMapper3;
-  vtkVolume *volume3;
-
-  volumeMapper3=vtkSmartVolumeMapper::New();
-  volumeMapper3->SetBlendModeToComposite();
-  volumeMapper3->SetInputConnection(
-    t->GetOutputPort());
-  // Software mode
-  volumeMapper3->SetRequestedRenderModeToRayCast();
-
-  volume3=vtkVolume::New();
-  volume3->SetMapper(volumeMapper3);
-  volume3->SetProperty(volumeProperty);
-  ren3->AddViewProp(volume3);
-
-
-  int retVal;
-
-  ren1->ResetCamera();
-  ren2->ResetCamera();
-  ren3->ResetCamera();
-
-  volumeMapper->SetFinalColorLevel(0.25);
-  volumeMapper->SetFinalColorWindow(0.5);
-
-//  volumeMapper2->SetFinalColorLevel(0.25);
-//  volumeMapper2->SetFinalColorWindow(0.5);
-
-  volumeMapper3->SetFinalColorLevel(0.25);
-  volumeMapper3->SetFinalColorWindow(0.5);
+  vtkNew<vtkVolume> volume;
+  volume->SetMapper(mapper.GetPointer());
+  volume->SetProperty(volumeProperty.GetPointer());
+  ren->AddVolume(volume.GetPointer());
 
   renWin->Render();
 
-  retVal = vtkTesting::Test(argc, argv, renWin, 90);
+  int retVal = vtkTesting::Test(argc, argv, renWin.GetPointer(), 90);
   if (retVal == vtkRegressionTester::DO_INTERACTOR)
     {
     iren->Start();
     }
-
-  volumeMapper3->Delete();
-  volume3->Delete();
-
-//  volumeMapper2->Delete();
-//  volume2->Delete();
-
-  volumeMapper->Delete();
-  volumeProperty->Delete();
-  volume->Delete();
-
-  iren->Delete();
-  t->Delete();
-  compositeOpacity->Delete();
 
   return !((retVal == vtkTesting::PASSED) || (retVal == vtkTesting::DO_INTERACTOR));
 }
