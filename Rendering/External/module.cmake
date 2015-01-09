dd4e27dfb7ce185b7934a428100bdf81e83f1f57
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2015-01-09 11:13:28 -0500

    Disable vtkRenderingExternal module for OpenGL2 backend
    vtkRenderingExternal depends on vtkGenericOpenGLRenderWindow which is not
    available for the OpenGL2 backend yet.
    
    Change-Id: I2f141f7ef7e28d27ebd73762228eeb12d356d987

--- Rendering/External/module.cmake
+++ Rendering/External/module.cmake
@@ -1,3 +1,15 @@
+# This module cannot be built when the rendering backend is OpenGL2
+if (Module_vtkRenderingExternal AND
+    ("${VTK_RENDERING_BACKEND}" STREQUAL "OpenGL2"))
+  message (SEND_ERROR "
+  vtkRenderingExternal cannot be built
+  when the rendering backend is OpenGL2.
+  Please disable vtkRenderingExternal or
+  change VTK_RENDERING_BACKEND to OpenGL.
+  ")
+  return()
+endif()
+
 vtk_module(vtkRenderingExternal
   DEPENDS
     vtkRenderingCore
