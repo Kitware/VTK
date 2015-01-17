9ea802002a9b64c2f8a3b16e2538fa834077db82
Scott Wittenburg <scott.wittenburg@kitware.com>
2015-01-16 17:17:26 -0700

    In the vtkweb loader, add css specific to the proxy editor widget.
    Change-Id: I00c69664e393c667f88dfaa3989aff1a0ac36250

--- Web/JavaScript/Lib/vtkweb-loader.js
+++ Web/JavaScript/Lib/vtkweb-loader.js
@@ -98,6 +98,7 @@
         "lib/js/paraview.ui.files.js",
         "lib/js/paraview.ui.data.js",
         "lib/js/paraview.ui.proxy.editor.js",
+        "lib/css/paraview.ui.proxy.editor.css",
         "lib/js/paraview.ui.svg.pipeline.js",
         "lib/js/paraview.ui.opacity.editor.js",
         "lib/css/paraview.ui.opacity.editor.css"
