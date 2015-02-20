80ace5f8f63668b43a34b83f56c1b6b7054373ce
Brad King <brad.king@kitware.com>
2015-02-20 17:07:44 -0500

    Avoid bash history expansion in developer setup scripts
    Protect '!' characters in single quotes, or remove them if not needed.
    
    Change-Id: Ie8e53a8d8aa86362c4ba122075d15cc9a5bd6521

--- Utilities/Scripts/SetupGitAliases.sh
+++ Utilities/Scripts/SetupGitAliases.sh
@@ -3,17 +3,17 @@
 echo "Setting up useful Git aliases..." &&
 
 # General aliases that could be global
-git config alias.pullall "!bash -c \"git pull && git submodule update --init\"" &&
+git config alias.pullall '!bash -c "git pull && git submodule update --init"' &&
 git config alias.prepush 'log --graph --stat origin/master..' &&
 
 # Staging aliases
 stage_deprecated="The stage aliases have been deprecated. Please use Gerrit." &&
-git config alias.stage-cmd "!sh -c \"echo ${stage_deprecated}\"" &&
-git config alias.stage-push "!sh -c \"echo ${stage_deprecated}\"" &&
-git config alias.stage-branch "!sh -c \"echo ${stage_deprecated}\"" &&
-git config alias.stage-merge "!sh -c \"echo ${stage_deprecated}\"" &&
+git config alias.stage-cmd '!sh -c "echo ${stage_deprecated}"' &&
+git config alias.stage-push '!sh -c "echo ${stage_deprecated}"' &&
+git config alias.stage-branch '!sh -c "echo ${stage_deprecated}"' &&
+git config alias.stage-merge '!sh -c "echo ${stage_deprecated}"' &&
 
 # Alias to push the current topic branch to Gerrit
-git config alias.gerrit-push "!bash Utilities/Scripts/git-gerrit-push" &&
+git config alias.gerrit-push '!bash Utilities/Scripts/git-gerrit-push' &&
 
 true
