52178c808cc5c8ea3c38e131307194fd0a928f61
Sean McBride <sean@rogue-research.com>
2015-03-10 00:53:27 -0400

    Removed code dead since 2006.
    Fixes clang -Wunreachable-code warning.
    
    Change-Id: I70e251edbcb5d788d190a803c5d6f5337adb27c2

--- Rendering/Label/vtkLabeledTreeMapDataMapper.cxx
+++ Rendering/Label/vtkLabeledTreeMapDataMapper.cxx
@@ -731,21 +731,11 @@ int vtkLabeledTreeMapDataMapper::AnalyseLabel(char * string, int level,
   trueLevel =
     (trueLevel > this->MaxFontLevel) ? this->MaxFontLevel : trueLevel;
    fsize = this->GetStringSize(string, trueLevel);
-  // Determine the orientation of the label
-  // Vertical Label - THIS DOESN'T WORK DUE TO ISSUES WITH vtkTextActor
-  // so always do the following - hence the 1 in the condition
-  if (1 || (sizes[0] >= sizes[1]))
-    {
-    // Horizontal label
-    oDir = 0;
-    *tprop = this->HLabelProperties[trueLevel];
-    }
-  else
-    {
-    // Vertical Label - THIS DOESN'T WORK DUE TO ISSUES WITH vtkTextActor
-    oDir = 1;
-    *tprop = this->VerticalLabelProperty;
-    }
+
+  // Horizontal label.
+  // (Vertical labels don't work due to issues with vtkTextActor.)
+  oDir = 0;
+  *tprop = this->HLabelProperties[trueLevel];
 
   // Is this level dynamic or static?
   if (level >= this->DynamicLevel)
