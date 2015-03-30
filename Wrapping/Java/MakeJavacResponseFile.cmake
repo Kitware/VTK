1175385fa40cddc5a5de5de47647ebd48f412982
Ben Boeckel <ben.boeckel@kitware.com>
2015-03-30 10:11:08 -0400

    java: refactor out writing paths to the file

--- Wrapping/Java/MakeJavacResponseFile.cmake
+++ Wrapping/Java/MakeJavacResponseFile.cmake
@@ -1,33 +1,29 @@
 cmake_minimum_required(VERSION 2.8.8)
 
-# Clear the contents of the file
-file(WRITE "${RSP_FILENAME}.tmp" "")
-
 file(GLOB vtk_java_files "${VTK_BINARY_DIR}/java/vtk/*.java")
 file(GLOB vtk_java_rendering_files "${VTK_BINARY_DIR}/java/vtk/rendering/*.java")
 file(GLOB vtk_java_rendering_awt_files "${VTK_BINARY_DIR}/java/vtk/rendering/awt/*.java")
 file(GLOB vtk_java_sample_files "${VTK_BINARY_DIR}/java/vtk/sample/*.java")
 
-foreach( java_file IN LISTS vtk_java_files vtk_java_rendering_files
-                            vtk_java_rendering_awt_files vtk_java_sample_files)
-  file(APPEND "${RSP_FILENAME}.tmp" "${java_file}\n")
-endforeach()
+set(all_vtk_java_files vtk_java_rendering_files vtk_java_rendering_awt_files vtk_java_sample_files)
 
 if(VTK_JAVA_SWT_COMPONENT)
   file(GLOB vtk_java_swt_files "${VTK_BINARY_DIR}/java/vtk/rendering/swt/*.java")
-  foreach( java_file IN LISTS vtk_java_swt_files )
-    file(APPEND "${RSP_FILENAME}.tmp" "${java_file}\n")
-  endforeach()
+  list(APPEND all_vtk_java_files vtk_java_swt_files)
 endif()
 
 if(VTK_JAVA_JOGL_COMPONENT)
   file(GLOB vtk_java_jogl_files "${VTK_BINARY_DIR}/java/vtk/rendering/jogl/*.java")
   list(APPEND vtk_java_jogl_files "${VTK_BINARY_DIR}/java/vtk/sample/rendering/JoglConeRendering.java")
-  foreach( java_file IN LISTS vtk_java_jogl_files )
-    file(APPEND "${RSP_FILENAME}.tmp" "${java_file}\n")
-  endforeach()
+  list(APPEND all_vtk_java_files vtk_java_jogl_files)
 endif()
 
-configure_file("${RSP_FILENAME}.tmp" "${RSP_FILENAME}" COPYONLY)
+set(CMAKE_CONFIGURABLE_FILE_CONTENT)
+foreach( java_file IN LISTS ${all_vtk_java_files} )
+  set(CMAKE_CONFIGURABLE_FILE_CONTENT "${CMAKE_CONFIGURABLE_FILE_CONTENT}${java_file}\n")
+endforeach()
 
-file(REMOVE "${RSP_FILENAME}.tmp")
+configure_file(
+  "${CMAKE_ROOT}/Modules/CMakeConfigurableFile.in"
+  "${RSP_FILENAME}"
+  @ONLY)
