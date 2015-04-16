ce26403abbc1e4f1084358bb636b9dc7aaaa1ffd
KWSys Robot <kwrobot@kitware.com>
2015-04-16 10:16:09 -0400

    KWSys 2015-04-16 (1ea01a46)
    Extract upstream KWSys using the following shell commands.
    
    $ git archive --prefix=upstream-kwsys/ 1ea01a46 | tar x
    $ git shortlog --no-merges --abbrev=8 --format='%h %s' 5843f590..1ea01a46
    Ben Boeckel (1):
          425fa73e Add missing malloc return value casts
    
    Brad King (11):
          dec9c30b SharedForward: Hard-code the ldpath buffer size to below VS 14 limit
          9c3eacf8 Add CONTRIBUTING.rst file with instructions to contribute
          cc4046a8 Directory: Work around PGI problem with Linux Large File Support
          5a15cb3b Base64: Use size_t for lenghts in API
          57c3ef1d Suppress deprecation warnings for GetVersionEx on Intel compiler
          b33e7b96 FStream: Fix exception spec on our standard stream replacements
          d4e7f08e Directory: Check opendir return value before using it
          4a698414 hashtable: Give prime number table functions internal linkage
          20f50727 SystemInformation: Add missing 'break' in StacktraceSignalHandler
          69bccf2e SystemTools: Teach Touch with !create to succeed on missing file
          1ea01a46 Tell Git to export '.gitattributes'
    
    Domen Vrankar (5):
          2b042ff6 SystemTools: Optionally report error from GetRealPath
          7c9a970a Glob: Remove dead code
          1b75ad3d Glob: Remove addition of extra '/'
          5d6204e9 Glob: Handle symlink cycles in directory paths
          4890f30c Glob: Add support for directory listing
    
    Jean-Christophe Fillion-Robin (2):
          c38ba638 SystemInformation: Improve Get(Available/Total)VirtualMemory for windows
          58308145 SystemInformation: Improve comments in private implementation
    
    Paul Martin (1):
          d2aa1afd SystemTools: Update CopyFileAlways stream library workarounds
    
    Sean McBride (3):
          87c65319 SharedForward: Suppress clang -Wcast-qual warnings with conditional pragma
          093eae34 SystemInformation: Fix -Wswitch-enum warnings
          9367a33b testHashSTL: Fix warnings by marking private functions static
    
    Simon Gomizelj (1):
          2f0165f1 Terminal: Add xterm-termite to VT100 color support whitelist
    
    Stephen Kelly (3):
          e4fe1d1a SystemTools: Refactor selection of Windows directory APIs
          af86ac7d SystemTools: Fix build with SunCC/stlport.
          d30c9b03 Workaround SolarisStudio bug with libstdc++.

--- SystemInformation.cxx
+++ SystemInformation.cxx
@@ -443,7 +443,7 @@ public:
     };
 
 protected:
-  // Functions.
+  // For windows
   bool RetrieveCPUFeatures();
   bool RetrieveCPUIdentity();
   bool RetrieveCPUCacheDetails();
@@ -457,6 +457,7 @@ protected:
   bool RetrieveClassicalCPUIdentity();
   bool RetrieveExtendedCPUIdentity();
 
+  // Processor information
   Manufacturer  ChipManufacturer;
   CPUFeatures   Features;
   ID            ChipID;
@@ -464,11 +465,11 @@ protected:
   unsigned int  NumberOfLogicalCPU;
   unsigned int  NumberOfPhysicalCPU;
 
-  int CPUCount();
+  int CPUCount(); // For windows
   unsigned char LogicalCPUPerPhysicalCPU();
-  unsigned char GetAPICId();
+  unsigned char GetAPICId(); // For windows
   bool IsHyperThreadingSupported();
-  static LongLong GetCyclesDifference(DELAY_FUNC, unsigned int);
+  static LongLong GetCyclesDifference(DELAY_FUNC, unsigned int); // For windows
 
   // For Linux and Cygwin, /proc/cpuinfo formats are slightly different
   bool RetreiveInformationFromCpuInfoFile();
@@ -1233,6 +1234,7 @@ void StacktraceSignalHandler(
 
         case ILL_ILLTRP:
           oss << "illegal trap";
+          break;
 
         case ILL_PRVOPC:
           oss << "privileged opcode";
@@ -1822,6 +1824,7 @@ const char * SystemInformationImplementation::GetVendorID()
       return "Motorola";
     case HP:
       return "Hewlett-Packard";
+    case UnknownManufacturer:
     default:
       return "Unknown Manufacturer";
     }
@@ -3063,6 +3066,12 @@ bool SystemInformationImplementation::RetrieveClassicalCPUIdentity()
     case NSC:
       this->ChipID.ProcessorName = "Cx486SLC \\ DLC \\ Cx486S A-Step";
       break;
+
+    case Sun:
+    case IBM:
+    case Motorola:
+    case HP:
+    case UnknownManufacturer:
     default:
       this->ChipID.ProcessorName = "Unknown family"; // We cannot identify the processor.
       return false;
@@ -3753,9 +3762,9 @@ bool SystemInformationImplementation::QueryWindowsMemory()
   }
 #  define MEM_VAL(value) ull##value
 # endif
-  tv = ms.MEM_VAL(TotalVirtual);
+  tv = ms.MEM_VAL(TotalPageFile);
   tp = ms.MEM_VAL(TotalPhys);
-  av = ms.MEM_VAL(AvailVirtual);
+  av = ms.MEM_VAL(AvailPageFile);
   ap = ms.MEM_VAL(AvailPhys);
   this->TotalVirtualMemory = tv>>10>>10;
   this->TotalPhysicalMemory = tp>>10>>10;
@@ -5068,7 +5077,11 @@ bool SystemInformationImplementation::QueryOSInformation()
   osvi.dwOSVersionInfoSize = sizeof (OSVERSIONINFOEXW);
 #ifdef KWSYS_WINDOWS_DEPRECATED_GetVersionEx
 # pragma warning (push)
-# pragma warning (disable:4996)
+# ifdef __INTEL_COMPILER
+#  pragma warning (disable:1478)
+# else
+#  pragma warning (disable:4996)
+# endif
 #endif
   bOsVersionInfoEx = GetVersionExW ((OSVERSIONINFOW*)&osvi);
   if (!bOsVersionInfoEx)
