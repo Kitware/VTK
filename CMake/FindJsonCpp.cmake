e57d5aae0fa170b4f8b967d49a48b8a21586daa7
Ben Boeckel <ben.boeckel@kitware.com>
2015-05-14 09:29:07 -0400

    JsonCpp: the VERSION_VAR is a separate signature
    Also fix regex madness and a typo.

--- CMake/FindJsonCpp.cmake
+++ CMake/FindJsonCpp.cmake
@@ -21,9 +21,9 @@ set(JsonCpp_LIBRARIES "${JsonCpp_LIBRARY}")
 
 set(_JsonCpp_version_args)
 if (EXISTS "${JsonCpp_INCLUDE_DIR}/json/version.h")
-  file(STRINGS "${JsonCpp_INCLUDE_DIR}/json/version.h" _JsonCpp_version_contents REGEX "#\\s+define\\s+JSONCPP_VERSION_[A-Z]+")
+  file(STRINGS "${JsonCpp_INCLUDE_DIR}/json/version.h" _JsonCpp_version_contents REGEX "JSONCPP_VERSION_[A-Z]+")
   foreach (_JsonCpp_version_part MAJOR MINOR PATCH)
-    string(REGEX REPLACE ".*#\\s*define JSONCPP_VERSION_${_JsonCpp_version_part}\\s+([0-9]+).*" "\\1" JsonCpp_VERSION_${_JsonCpp_version_part} "${_JsonCpp_version_contents}")
+    string(REGEX REPLACE ".*# *define +JSONCPP_VERSION_${_JsonCpp_version_part} +([0-9]+).*" "\\1" JsonCpp_VERSION_${_JsonCpp_version_part} "${_JsonCpp_version_contents}")
   endforeach ()
 
   set(JsonCpp_VERSION_STRING "${JsonCpp_VERSION_MAJOR}.${JsonCpp_VERSION_MINOR}.${JsonCpp_VERSION_PATCH}")
@@ -32,8 +32,8 @@ if (EXISTS "${JsonCpp_INCLUDE_DIR}/json/version.h")
 endif ()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(JsonCpp DEFAULT_MSG
-  JsonCpp_LIBRARIES JsonCpp_INCLUDE_DIRS
+find_package_handle_standard_args(JsonCpp
+  REQUIRED_VARS JsonCpp_LIBRARIES JsonCpp_INCLUDE_DIRS
   ${_JsonCpp_version_args})
 
 mark_as_advanced(JsonCpp_INCLUDE_DIR JsonCpp_LIBRARY)
