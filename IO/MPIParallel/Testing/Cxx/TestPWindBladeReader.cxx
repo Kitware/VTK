2bfd5b8b26f8c0123536a2bb6dfba94eb8978513
Ken Martin <ken.martin@kitware.com>
2015-06-12 10:04:58 -0400

    Fix test that was creating an ill defined camera

--- IO/MPIParallel/Testing/Cxx/TestPWindBladeReader.cxx
+++ IO/MPIParallel/Testing/Cxx/TestPWindBladeReader.cxx
@@ -143,6 +143,7 @@ int TestPWindBladeReader( int argc, char *argv[] )
   bounds[2] -= 150;
   ren->ResetCamera(bounds);
   camera->Elevation(-90);
+  camera->SetViewUp(0,0,1);
   camera->Zoom(1.2);
 
   ren->AddActor(fieldActor);
