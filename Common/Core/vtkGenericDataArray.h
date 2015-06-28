b1891221ad4bb6bab2cbd72dd7edb3f7550864c6
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2015-06-27 22:57:48 -0400

    Correct MaxId usage.
    Grrrr...MaxId is prone to misinterpretation!!!

--- Common/Core/vtkGenericDataArray.h
+++ Common/Core/vtkGenericDataArray.h
@@ -173,11 +173,12 @@ public:
     this->MaxId = (this->Size - 1);
     return 1;
     }
+
   virtual void SetNumberOfTuples(vtkIdType number)
     {
-    if (this->Allocate(number, 0))
+    if (this->Allocate(number*this->NumberOfComponents, 0))
       {
-      this->MaxId = number - 1;
+      this->MaxId = this->Size - 1;
       }
     }
   virtual void Initialize()
@@ -187,7 +188,7 @@ public:
     }
   virtual void Squeeze()
     {
-    this->Resize(this->MaxId+1);
+    this->Resize(this->GetNumberOfTuples());
     }
 
   //----------------------------------------------------------------------------
@@ -210,18 +211,21 @@ public:
     }
   virtual vtkIdType InsertNextTuple(vtkIdType j, vtkAbstractArray *source)
     {
-    this->InsertTuple(this->MaxId, j, source);
-    return this->MaxId;
+    vtkIdType nextTuple = this->GetNumberOfTuples();
+    this->InsertTuple(nextTuple, j, source);
+    return nextTuple;
     }
   virtual vtkIdType InsertNextTuple(const float *source)
     {
-    this->InsertTuple(this->MaxId, source);
-    return this->MaxId;
+    vtkIdType nextTuple = this->GetNumberOfTuples();
+    this->InsertTuple(nextTuple, source);
+    return nextTuple;
     }
   virtual vtkIdType InsertNextTuple(const double *source)
     {
-    this->InsertTuple(this->MaxId, source);
-    return this->MaxId;
+    vtkIdType nextTuple = this->GetNumberOfTuples();
+    this->InsertTuple(nextTuple, source);
+    return nextTuple;
     }
   virtual void InsertTuples(vtkIdList *dstIds, vtkIdList *srcIds, vtkAbstractArray *source);
   virtual void InsertTuples(vtkIdType dstStart, vtkIdType n, vtkIdType srcStart, vtkAbstractArray* source);
@@ -277,6 +281,116 @@ public:
   // TODO:
   virtual vtkArrayIterator* NewIterator() { return NULL;}
 
+  //----------------------------------------------------------------------------
+  // API provided by vtkDataArrayTemplate/vtkTypedDataArray. These methods used
+  // to be virtual. They are no longer virtual and use TupleIterator mechanism
+  // to access values.
+
+  // Description:
+  // Get the data at a particular index. Index is value/component index assuming
+  // traditional VTK memory layout (array-of-structures).
+  ScalarType GetValue(vtkIdType idx)
+    {
+    return this->GetValueReference(idx);
+    }
+
+  // Description:
+  // Get a reference to the scalar value at a particular index.
+  // Index is value/component index assuming
+  // traditional VTK memory layout (array-of-structures).
+  ScalarReturnType GetValueReference(vtkIdType idx)
+    {
+    vtkIdType tuple = idx / this->NumberOfComponents;
+    int comp = static_cast<int>(idx % this->NumberOfComponents);
+    return this->Begin(tuple)[comp];
+    }
+
+  // Description:
+  // Set the data at a particular index. Does not do range checking. Make sure
+  // you use the method SetNumberOfValues() before inserting data.
+  // Index is value/component index assuming
+  // traditional VTK memory layout (array-of-structures).
+  void SetValue(vtkIdType idx, ScalarType value)
+    {
+    vtkIdType tuple = idx / this->NumberOfComponents;
+    int comp = static_cast<int>(idx % this->NumberOfComponents);
+    this->Begin(tuple)[comp] = value;
+    this->DataChanged();
+    }
+
+  // Description:
+  // Copy the tuple value into a user-provided array.
+  void GetTupleValue(vtkIdType tupleIdx, ScalarType *t)
+    {
+    TupleIteratorType iter = this->Begin(tupleIdx);
+    for (int cc=0; cc < this->NumberOfComponents; ++cc)
+      {
+      t[cc] = iter[cc];
+      }
+    }
+
+  // Description:
+  // Set the tuple value at the ith location in the array.
+  void SetTupleValue(vtkIdType tupleIdx, const ScalarType *t)
+    {
+    TupleIteratorType iter = this->Begin(tupleIdx);
+    for (int cc=0; cc < this->NumberOfComponents; ++cc)
+      {
+      iter[cc] = t[cc];
+      }
+    this->DataChanged();
+    }
+
+
+  // Description:
+  // Insert data at the end of the array. Return its location in the array.
+  vtkIdType InsertNextValue(ScalarType v)
+    {
+    vtkIdType nextValueIdx = this->MaxId + 1;
+    if (nextValueIdx >= this->Size)
+      {
+      vtkIdType tuple = nextValueIdx / this->NumberOfComponents;
+      this->EnsureAccessToTuple(tuple);
+      // Since EnsureAccessToTuple will update the MaxId to point to the last
+      // component in the last tuple, we move it back to support this method on
+      // multi-component arrays.
+      this->MaxId = nextValueIdx;
+      }
+    this->SetValue(nextValueIdx, v);
+    }
+
+  // Description:
+  // Insert data at a specified position in the array.
+  void InsertValue(vtkIdType idx, ScalarType v)
+    {
+    vtkIdType tuple = idx / this->NumberOfComponents;
+    if (this->EnsureAccessToTuple(tuple))
+      {
+      this->SetValue(idx, v);
+      }
+    }
+
+  // Description:
+  // Insert (memory allocation performed) the tuple into the ith location
+  // in the array.
+  void InsertTupleValue(vtkIdType idx, const ScalarType *t)
+    {
+    vtkIdType tuple = idx / this->NumberOfComponents;
+    if (this->EnsureAccessToTuple(tuple))
+      {
+      this->SetTupleValue(idx, t);
+      }
+    }
+
+  // Description:
+  // Insert (memory allocation performed) the tuple onto the end of the array.
+  virtual vtkIdType InsertNextTupleValue(const ScalarType *t)
+    {
+    vtkIdType nextTuple = this->GetNumberOfTuples();
+    this->InsertTupleValue(nextTuple, t);
+    return nextTuple;
+    }
+
 protected:
   vtkGenericDataArray()
     : Lookup(*this)
@@ -289,13 +403,13 @@ protected:
 
   // This method resizes the array if needed so that the given tuple index is
   // valid/accessible.
-  bool EnsureAccessToTuple(vtkIdType tuple)
+  bool EnsureAccessToTuple(vtkIdType tupleIdx)
     {
-    if (tuple < 0) { return false; }
-    vtkIdType expectedMaxId = (1 + tuple) * this->GetNumberOfComponents() - 1;
+    if (tupleIdx < 0) { return false; }
+    vtkIdType expectedMaxId = (1 + tupleIdx) * this->GetNumberOfComponents() - 1;
     if (this->MaxId < expectedMaxId)
       {
-      return this->Resize(tuple + 1) != 0;
+      return this->Resize(tupleIdx + 1) != 0;
       }
     return true;
     }
