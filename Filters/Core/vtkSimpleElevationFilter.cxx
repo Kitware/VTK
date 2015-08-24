799a1481d53cd85e7eaac2a03851649c0b8b8885
Will Schroeder <will.schroeder@kitware.com>
2015-08-24 11:45:48 -0400

    Threaded and templated elevation filters
    Used vtkSMPTools and templates to speed up the elevation filters.
    A fast path was created for processing vtkPointSet input data.

--- Filters/Core/vtkSimpleElevationFilter.cxx
+++ Filters/Core/vtkSimpleElevationFilter.cxx
@@ -15,18 +15,87 @@
 #include "vtkSimpleElevationFilter.h"
 
 #include "vtkCellData.h"
-#include "vtkDataSet.h"
+#include "vtkPointSet.h"
 #include "vtkFloatArray.h"
 #include "vtkInformation.h"
 #include "vtkInformationVector.h"
 #include "vtkMath.h"
 #include "vtkObjectFactory.h"
 #include "vtkPointData.h"
+#include "vtkSMPTools.h"
 
 vtkStandardNewMacro(vtkSimpleElevationFilter);
 
-// Construct object with LowPoint=(0,0,0) and HighPoint=(0,0,1). Scalar
-// range is (0,1).
+// The heart of the algorithm plus interface to the SMP tools. Double templated
+// over point and scalar types.
+template <class TP>
+class vtkSimpleElevationAlgorithm
+{
+public:
+  vtkIdType NumPts;
+  double Vector[3];
+  const TP *Points;
+  float *Scalars;
+
+  // Contructor
+  vtkSimpleElevationAlgorithm();
+
+  // Interface between VTK and templated functions
+  static void Elevate(vtkSimpleElevationFilter *self, vtkIdType numPts,
+                      const TP *points, float *scalars);
+
+  // Interface implicit function computation to SMP tools.
+  template <class TP> class ElevationOp
+    {
+    public:
+      ElevationOp(vtkSimpleElevationAlgorithm<TP> *algo)
+        { this->Algo = algo;}
+      vtkSimpleElevationAlgorithm *Algo;
+      void  operator() (vtkIdType k, vtkIdType end)
+        {
+        const double *v = this->Algo->Vector;
+        const TP *p = this->Algo->Points + 3*k;
+        float *s = this->Algo->Scalars + k;
+        for ( ; k < end; ++k)
+          {
+          *s = v[0]*p[0] + v[1]*p[1] + v[2]*p[2];
+          p+=3;
+          ++s;
+          }
+        }
+    };
+};
+
+//----------------------------------------------------------------------------
+// Initialized mainly to eliminate compiler warnings.
+template <class TP> vtkSimpleElevationAlgorithm<TP>::
+vtkSimpleElevationAlgorithm():Points(NULL),Scalars(NULL)
+{
+  this->Vector[0] = this->Vector[1] = this->Vector[2] = 0.0;
+}
+
+//----------------------------------------------------------------------------
+// Templated class is glue between VTK and templated algorithms.
+template <class TP> void vtkSimpleElevationAlgorithm<TP>::
+Elevate(vtkSimpleElevationFilter *self, vtkIdType numPts,
+        const TP *points, float *scalars)
+{
+  // Populate data into local storage
+  vtkSimpleElevationAlgorithm<TP> algo;
+  algo.NumPts = numPts;
+  self->GetVector(algo.Vector);
+  algo.Points = points;
+  algo.Scalars = scalars;
+
+  // Okay now generate samples using SMP tools
+  ElevationOp<TP> values(&algo);
+  vtkSMPTools::For(0,algo.NumPts, values);
+}
+
+
+// Okay begin the class proper
+//----------------------------------------------------------------------------
+// Construct object with Vector=(0,0,1).
 vtkSimpleElevationFilter::vtkSimpleElevationFilter()
 {
   this->Vector[0] = 0.0;
@@ -34,7 +103,8 @@ vtkSimpleElevationFilter::vtkSimpleElevationFilter()
   this->Vector[2] = 1.0;
 }
 
-// Convert position along ray into scalar value.  Example use includes
+//----------------------------------------------------------------------------
+// Convert position along the ray into scalar value.  Example use includes
 // coloring terrain by elevation.
 //
 int vtkSimpleElevationFilter::RequestData(
@@ -60,7 +130,7 @@ int vtkSimpleElevationFilter::RequestData(
   //
   vtkDebugMacro(<<"Generating elevation scalars!");
 
- // First, copy the input to the output as a starting point
+  // First, copy the input to the output as a starting point
   output->CopyStructure( input );
 
   if ( ((numPts=input->GetNumberOfPoints()) < 1) )
@@ -74,7 +144,7 @@ int vtkSimpleElevationFilter::RequestData(
   newScalars = vtkFloatArray::New();
   newScalars->SetNumberOfTuples(numPts);
 
-  // Set up 1D parametric system
+  // Ensure that there is a valid vector
   //
   if ( vtkMath::Dot(this->Vector,this->Vector) == 0.0)
     {
@@ -82,21 +152,42 @@ int vtkSimpleElevationFilter::RequestData(
     this->Vector[0] = this->Vector[1] = 0.0; this->Vector[2] = 1.0;
     }
 
-  // Compute dot product
+  // Create a fast path for point set input
   //
-  int abort=0;
-  vtkIdType progressInterval=numPts/20 + 1;
-  for (i=0; i<numPts && !abort; i++)
+  vtkPointSet *ps = vtkPointSet::SafeDownCast(input);
+//  if ( ps )
+  if ( 0 )
     {
-    if ( ! (i % progressInterval) )
+    float *scalars =
+      static_cast<float*>(newScalars->GetVoidPointer(0));
+    vtkPoints *points = ps->GetPoints();
+    void *pts = points->GetData()->GetVoidPointer(0);
+    switch ( points->GetDataType() )
       {
-      this->UpdateProgress ((double)i/numPts);
-      abort = this->GetAbortExecute();
+      vtkTemplateMacro(vtkSimpleElevationAlgorithm<VTK_TT>::
+                       Elevate(this,numPts,(VTK_TT *)pts,scalars));
       }
+    }//fast path
 
-    input->GetPoint(i,x);
-    s = vtkMath::Dot(this->Vector,x);
-    newScalars->SetComponent(i,0,s);
+  else
+    {
+    // Too bad, got to take the scenic route.
+    // Compute dot product.
+    //
+    int abort=0;
+    vtkIdType progressInterval=numPts/20 + 1;
+    for (i=0; i<numPts && !abort; i++)
+      {
+      if ( ! (i % progressInterval) )
+        {
+        this->UpdateProgress ((double)i/numPts);
+        abort = this->GetAbortExecute();
+        }
+
+      input->GetPoint(i,x);
+      s = vtkMath::Dot(this->Vector,x);
+      newScalars->SetComponent(i,0,s);
+      }
     }
 
   // Update self
@@ -114,6 +205,7 @@ int vtkSimpleElevationFilter::RequestData(
   return 1;
 }
 
+//----------------------------------------------------------------------------
 void vtkSimpleElevationFilter::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
