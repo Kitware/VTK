fd1acd3adb4e4deeac6add9c5b1cae47c5eb2a0b
David C. Lonie <david.lonie@kitware.com>
2015-08-24 14:29:59 -0400

    Add vtkPropItem, to embed any vtkProp in a Context2D scene.

---
+++ Rendering/ContextOpenGL2/vtkOpenGLPropItem.h
@@ -0,0 +1,59 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkOpenGLPropItem.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+// .NAME vtkOpenGLPropItem - Sync Context2D state with vtk camera.
+//
+// .SECTION Description
+// The vtkContext2D framework modifies the GL state directly, while some actors
+// and mappers rely on the modelview/projection matrices from vtkCamera. This
+// class is a layer between the two that updates the camera with the current
+// OpenGL state.
+
+#ifndef __vtkOpenGLPropItem_h
+#define __vtkOpenGLPropItem_h
+
+#include "vtkRenderingContextOpenGL2Module.h" // For export macro
+#include "vtkPropItem.h"
+#include "vtkNew.h" // for vtkNew
+
+class vtkCamera;
+
+class VTKRENDERINGCONTEXTOPENGL2_EXPORT vtkOpenGLPropItem: public vtkPropItem
+{
+public:
+  static vtkOpenGLPropItem *New();
+  vtkTypeMacro(vtkOpenGLPropItem, vtkPropItem)
+
+  virtual bool Paint(vtkContext2D *painter);
+
+protected:
+  vtkOpenGLPropItem();
+  ~vtkOpenGLPropItem();
+
+  // Sync the active vtkCamera with the GL state set by the painter.
+  virtual void UpdateTransforms();
+
+  // Restore the vtkCamera state.
+  virtual void ResetTransforms();
+
+private:
+  vtkNew<vtkCamera> CameraCache;
+  vtkContext2D *Painter;
+
+  vtkOpenGLPropItem(const vtkOpenGLPropItem &); // Not implemented.
+  void operator=(const vtkOpenGLPropItem &); // Not implemented.
+};
+
+#endif //__vtkOpenGLPropItem_h
