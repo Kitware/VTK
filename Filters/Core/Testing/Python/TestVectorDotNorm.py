67b7b4b6d5b0ba0fd7bd18172bb479759a9d9e35
Will Schroeder <will.schroeder@kitware.com>
2015-08-28 11:40:04 -0400

    Threaded and templated vector dot and vector norm
    Added vtkSMPTools and templating to improve performance. Also
    added more controls on normalizing and/or mapping the output
    scalars.

---
+++ Filters/Core/Testing/Python/TestVectorDotNorm.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+import vtk
+from vtk.test import Testing
+from vtk.util.misc import vtkGetDataRoot
+VTK_DATA_ROOT = vtkGetDataRoot()
+
+# this is a tcl version of plate vibration
+ren1 = vtk.vtkRenderer()
+ren2 = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren1)
+renWin.AddRenderer(ren2)
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+
+# read a vtk file
+#
+plate = vtk.vtkPolyDataReader()
+plate.SetFileName(VTK_DATA_ROOT + "/Data/plate.vtk")
+plate.SetVectorsName("mode8")
+
+warp = vtk.vtkWarpVector()
+warp.SetInputConnection(plate.GetOutputPort())
+warp.SetScaleFactor(0.5)
+
+normals = vtk.vtkPolyDataNormals()
+normals.SetInputConnection(warp.GetOutputPort())
+
+color = vtk.vtkVectorDot()
+color.SetInputConnection(normals.GetOutputPort())
+
+lut = vtk.vtkLookupTable()
+lut.SetNumberOfColors(256)
+lut.Build()
+
+i = 0
+while i < 128:
+    lut.SetTableValue(i, (128.0 - i) / 128.0, (128.0 - i) / 128.0,
+      (128.0 - i) / 128.0, 1)
+    i += 1
+
+i = 128
+while i < 256:
+    lut.SetTableValue(i, (i - 128.0) / 128.0, (i - 128.0) / 128.0,
+      (i - 128.0) / 128.0, 1)
+    i += 1
+
+plateMapper = vtk.vtkDataSetMapper()
+plateMapper.SetInputConnection(color.GetOutputPort())
+plateMapper.SetLookupTable(lut)
+plateMapper.SetScalarRange(-1, 1)
+
+plateActor = vtk.vtkActor()
+plateActor.SetMapper(plateMapper)
+
+color2 = vtk.vtkVectorNorm()
+color2.SetInputConnection(plate.GetOutputPort())
+color2.NormalizeOn()
+
+plateMapper2 = vtk.vtkDataSetMapper()
+plateMapper2.SetInputConnection(color2.GetOutputPort())
+plateMapper2.SetLookupTable(lut)
+plateMapper2.SetScalarRange(0, 1)
+
+plateActor2 = vtk.vtkActor()
+plateActor2.SetMapper(plateMapper2)
+
+# Add the actors to the renderer, set the background and size
+#
+ren1.SetViewport(0, 0, .5, 1)
+ren2.SetViewport(.5, 0, 1, 1)
+
+ren1.AddActor(plateActor)
+ren1.SetBackground(1, 1, 1)
+ren2.AddActor(plateActor2)
+ren2.SetBackground(1, 1, 1)
+
+renWin.SetSize(500, 250)
+
+camera = vtk.vtkCamera()
+camera.SetPosition(1,1,1)
+ren1.SetActiveCamera(camera)
+ren2.SetActiveCamera(camera)
+
+ren1.ResetCamera()
+renWin.Render()
+
+# render the image
+#
+iren.Initialize()
+#iren.Start()
--- Filters/Core/vtkVectorNorm.cxx
+++ Filters/Core/vtkVectorNorm.cxx
@@ -21,11 +21,124 @@
 #include "vtkInformationVector.h"
 #include "vtkObjectFactory.h"
 #include "vtkPointData.h"
+#include "vtkSMPTools.h"
 
 #include <math.h>
 
 vtkStandardNewMacro(vtkVectorNorm);
 
+
+// The heart of the algorithm plus interface to the SMP tools. Double templated
+// over point and scalar types.
+template <class TV>
+class vtkVectorNormAlgorithm
+{
+public:
+  vtkIdType Num;
+  double Max;
+  const TV *Vectors;
+  float *Scalars;
+
+  // Constructor
+  vtkVectorNormAlgorithm();
+
+  // Interface between VTK and templated functions.
+  static void Norm(vtkVectorNorm *self, vtkIdType num, TV *vectors,
+                   float *scalars);
+
+  // Interface dot product computation to SMP tools.
+  template <class T> class NormOp
+    {
+    public:
+      vtkVectorNormAlgorithm *Algo;
+      vtkSMPThreadLocal<double> Max;
+      NormOp(vtkVectorNormAlgorithm<T> *algo)
+        {
+        this->Algo = algo;
+        this->Max = VTK_DOUBLE_MIN;
+        }
+      void  operator() (vtkIdType k, vtkIdType end)
+        {
+        double &max = this->Max.Local();
+        const T *v = this->Algo->Vectors + 3*k;
+        float *s = this->Algo->Scalars + k;
+        for ( ; k < end; ++k)
+          {
+          *s = static_cast<float>(
+            sqrt( static_cast<double>(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]) ) );
+          max = ( *s > max ? *s : max );
+          s++;
+          v += 3;
+          }
+        }
+    };
+
+  // Interface normalize computation to SMP tools.
+  template <class T> class MapOp
+    {
+    public:
+      vtkVectorNormAlgorithm *Algo;
+      MapOp(vtkVectorNormAlgorithm<T> *algo)
+        { this->Algo = algo; }
+      void  operator() (vtkIdType k, vtkIdType end)
+        {
+        const double max = this->Algo->Max;
+        float *s = this->Algo->Scalars + k;
+        for ( ; k < end; ++k)
+          {
+          *s++ /= max;
+          }
+        }
+    };
+};
+
+//----------------------------------------------------------------------------
+// Initialized mainly to eliminate compiler warnings.
+template <class TV> vtkVectorNormAlgorithm<TV>::
+vtkVectorNormAlgorithm():Vectors(NULL),Scalars(NULL)
+{
+  this->Num = 0;
+  this->Max = 0.0;
+}
+
+//----------------------------------------------------------------------------
+// Templated class is glue between VTK and templated algorithms.
+template <class TV> void vtkVectorNormAlgorithm<TV>::
+Norm(vtkVectorNorm *self, vtkIdType num, TV *vectors, float *scalars)
+{
+  // Populate data into local storage
+  vtkVectorNormAlgorithm<TV> algo;
+
+  algo.Num = num;
+  algo.Vectors = vectors;
+  algo.Scalars = scalars;
+
+  // Okay now generate samples using SMP tools
+  NormOp<TV> norm(&algo);
+  vtkSMPTools::For(0,algo.Num, norm);
+
+  // Have to roll up the thread local storage and get the overall range
+  double max = VTK_DOUBLE_MIN;
+  vtkSMPThreadLocal<double>::iterator itr;
+  for ( itr=norm.Max.begin(); itr != norm.Max.end(); ++itr )
+    {
+    if ( *itr > max )
+      {
+      *itr = max;
+      }
+    }
+  algo.Max = max;
+
+  if ( max > 0.0 && self->GetNormalize() )
+    {
+    MapOp<TV> mapValues(&algo);
+    vtkSMPTools::For(0,algo.Num, mapValues);
+    }
+}
+
+
+//=================================Begin class proper=========================
+//----------------------------------------------------------------------------
 // Construct with normalize flag off.
 vtkVectorNorm::vtkVectorNorm()
 {
@@ -33,6 +146,7 @@ vtkVectorNorm::vtkVectorNorm()
   this->AttributeMode = VTK_ATTRIBUTE_MODE_DEFAULT;
 }
 
+//----------------------------------------------------------------------------
 int vtkVectorNorm::RequestData(
   vtkInformation *vtkNotUsed(request),
   vtkInformationVector **inputVector,
@@ -48,10 +162,9 @@ int vtkVectorNorm::RequestData(
   vtkDataSet *output = vtkDataSet::SafeDownCast(
     outInfo->Get(vtkDataObject::DATA_OBJECT()));
 
-  vtkIdType numVectors, i;
+  vtkIdType numVectors;
   int computePtScalars=1, computeCellScalars=1;
   vtkFloatArray *newScalars;
-  double v[3], s, maxScalar;
   vtkDataArray *ptVectors, *cellVectors;
   vtkPointData *pd=input->GetPointData(), *outPD=output->GetPointData();
   vtkCellData *cd=input->GetCellData(), *outCD=output->GetCellData();
@@ -81,48 +194,21 @@ int vtkVectorNorm::RequestData(
     }
 
   // Allocate / operate on point data
-  int abort=0;
-  vtkIdType progressInterval;
   if ( computePtScalars )
     {
     numVectors = ptVectors->GetNumberOfTuples();
     newScalars = vtkFloatArray::New();
     newScalars->SetNumberOfTuples(numVectors);
 
-    progressInterval=numVectors/10+1;
-    for (maxScalar=0.0, i=0; i < numVectors && !abort; i++)
-      {
-      ptVectors->GetTuple(i, v);
-      s = sqrt((double)v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
-      if ( s > maxScalar )
-        {
-        maxScalar = s;
-        }
-      newScalars->SetComponent(i,0,s);
-
-      if ( ! (i % progressInterval) )
-        {
-        vtkDebugMacro(<<"Computing point vector norm #" << i);
-        this->UpdateProgress (0.5*i/numVectors);
-        }
-      }
-
-    // If necessary, normalize
-    if ( this->Normalize && maxScalar > 0.0 )
-      {
-      for (i=0; i < numVectors; i++)
-        {
-        s = newScalars->GetComponent(i,0);
-        s /= maxScalar;
-        newScalars->SetComponent(i,0,s);
-        }
-      }
+    this->GenerateScalars(numVectors,ptVectors,newScalars);
 
     int idx = outPD->AddArray(newScalars);
     outPD->SetActiveAttribute(idx, vtkDataSetAttributes::SCALARS);
     newScalars->Delete();
     outPD->CopyScalarsOff();
     }//if computing point scalars
+
+  this->UpdateProgress (0.50);
 
   // Allocate / operate on cell data
   if ( computeCellScalars )
@@ -131,33 +217,7 @@ int vtkVectorNorm::RequestData(
     newScalars = vtkFloatArray::New();
     newScalars->SetNumberOfTuples(numVectors);
 
-    progressInterval=numVectors/10+1;
-    for (maxScalar=0.0, i=0; i < numVectors && !abort; i++)
-      {
-      cellVectors->GetTuple(i, v);
-      s = sqrt((double)v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
-      if ( s > maxScalar )
-        {
-        maxScalar = s;
-        }
-      newScalars->SetComponent(i,0,s);
-      if ( ! (i % progressInterval) )
-        {
-        vtkDebugMacro(<<"Computing cell vector norm #" << i);
-        this->UpdateProgress (0.5+0.5*i/numVectors);
-        }
-      }
-
-    // If necessary, normalize
-    if ( this->Normalize && maxScalar > 0.0 )
-      {
-      for (i=0; i < numVectors; i++)
-        {
-        s = newScalars->GetComponent(i,0);
-        s /= maxScalar;
-        newScalars->SetComponent(i,0,s);
-        }
-      }
+    this->GenerateScalars(numVectors,cellVectors,newScalars);
 
     int idx = outCD->AddArray(newScalars);
     outCD->SetActiveAttribute(idx, vtkDataSetAttributes::SCALARS);
@@ -172,6 +232,25 @@ int vtkVectorNorm::RequestData(
   return 1;
 }
 
+
+//----------------------------------------------------------------------------
+// All this does it wrap up templated code.
+void vtkVectorNorm::
+GenerateScalars(vtkIdType num, vtkDataArray *v, vtkFloatArray *s)
+{
+  float *scalars = static_cast<float*>(s->GetVoidPointer(0));
+  void *vectors = v->GetVoidPointer(0);
+  switch ( v->GetDataType() )
+    {
+    vtkTemplateMacro(vtkVectorNormAlgorithm<VTK_TT>::
+                     Norm(this,num,(VTK_TT*)vectors,scalars));
+
+    default:
+      break;
+    }
+}
+
+//----------------------------------------------------------------------------
 // Return the method for generating scalar data as a string.
 const char *vtkVectorNorm::GetAttributeModeAsString(void)
 {
@@ -189,6 +268,7 @@ const char *vtkVectorNorm::GetAttributeModeAsString(void)
     }
 }
 
+//----------------------------------------------------------------------------
 void vtkVectorNorm::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
