ebac805b7be66d1e94055b8d0ceda9b1a8a953bc
David Gobbi <david.gobbi@gmail.com>
2015-09-11 23:56:48 -0600

    Consolidate the qt4 and qt5 python interactor widgets.

--- Wrapping/Python/vtk/qt5/__init__.py
+++ Wrapping/Python/vtk/qt5/__init__.py
@@ -1,3 +1,7 @@
-"""PyQt v5 widget for VTK."""
+"""PyQt v5 widget for VTK.
+
+This module is a version of the vtk.qt module that requires Qt5.
+If you do not specifically require Qt5, then use vtk.qt instead.
+"""
 
 __all__ = ['QVTKRenderWindowInteractor']
