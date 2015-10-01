78222ad53109952cf237ae02bd584e396942cd77
Ben Boeckel <ben.boeckel@kitware.com>
2015-10-01 13:36:59 -0400

    jsoncpp: add script to update jsoncpp

---
+++ ThirdParty/jsoncpp/update.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+shopt -s dotglob
+
+readonly name="jsoncpp"
+readonly ownership="JsonCpp Upstream <kwrobot@kitware.com>"
+readonly subtree="ThirdParty/$name/vtk$name"
+readonly update="ThirdParty/$name/update.sh"
+readonly repo="https://gitlab.kitware.com/third-party/jsoncpp.git"
+readonly tag="for/vtk"
+readonly paths="
+.gitattributes
+LICENSE
+" # We amalgamate jsoncpp
+
+readonly basehash='' # NEWHASH
+
+extract_source () {
+    python amalgamate.py
+    [ -n "$paths" ] && \
+        mv -v $paths "dist"
+    mv "dist" "$name-reduced"
+    tar -cv "$name-reduced/" | \
+        tar -C "$extractdir" -x
+}
+
+. "$( dirname "${BASH_SOURCE[0]}" )/../update-common.sh"
