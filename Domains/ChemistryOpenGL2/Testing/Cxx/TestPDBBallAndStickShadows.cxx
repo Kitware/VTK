4a6ace8249ee5a61298a22b4b0b317a42b6f3b4e
Ken Martin <ken.martin@kitware.com>
2015-10-29 12:46:30 -0400

    Fix accidental commit of one test
    Forgot to revert this back to normal

--- Domains/ChemistryOpenGL2/Testing/Cxx/TestPDBBallAndStickShadows.cxx
+++ Domains/ChemistryOpenGL2/Testing/Cxx/TestPDBBallAndStickShadows.cxx
@@ -19,8 +19,7 @@
 #include "vtkCamera.h"
 #include "vtkMolecule.h"
 #include "vtkLight.h"
-#include "vtkOpenGLMoleculeMapper.h"
-#include "vtkOpenGLSphereMapper.h"
+#include "vtkMoleculeMapper.h"
 #include "vtkNew.h"
 #include "vtkProperty.h"
 #include "vtkRenderWindowInteractor.h"
@@ -29,19 +28,6 @@
 #include "vtkPDBReader.h"
 #include "vtkPlaneSource.h"
 #include "vtkPolyDataMapper.h"
-
-#include "vtkDepthOfFieldPass.h"
-#include "vtkRenderStepsPass.h"
-#include "vtkSequencePass.h"
-#include "vtkShadowMapBakerPass.h"
-#include "vtkShadowMapPass.h"
-#include "vtkOpenGLRenderer.h"
-#include "vtkCameraPass.h"
-#include "vtkRenderPassCollection.h"
-#include "vtkSSAAPass.h"
-
-#include "vtkLookupTable.h"
-#include "vtkPeriodicTable.h"
 
 #include "vtkTimerLog.h"
 #include "vtkCamera.h"
@@ -58,43 +44,21 @@ int TestPDBBallAndStickShadows(int argc, char *argv[])
 
   delete [] fileName;
 
-  vtkNew<vtkOpenGLMoleculeMapper> molmapper;
+  vtkNew<vtkMoleculeMapper> molmapper;
   molmapper->SetInputConnection(reader->GetOutputPort(1));
-  molmapper->SetRenderBonds(false);
-  molmapper->SetAtomicRadiusType( vtkMoleculeMapper::VDWRadius );
-  molmapper->SetAtomicRadiusScaleFactor( 0.9 );
-  molmapper->SetScalarMaterialModeToAmbientAndDiffuse();
 
-  // get the default lookup table and desaturate it to be
-  // more pleasing
-  vtkNew<vtkPeriodicTable> pt;
-  vtkNew<vtkLookupTable> lut;
-  pt->GetDefaultLUT(lut.Get());
-  const unsigned short numColors = lut->GetNumberOfColors();
-  double rgb[4];
-  for (vtkIdType i = 0; static_cast<unsigned int>(i) < numColors; ++i)
-    {
-    lut->GetTableValue(i, rgb);
-    lut->SetTableValue(i, 0.45 + rgb[0]*0.55,
-      0.45 + rgb[1]*0.55, 0.45 + rgb[2]*0.55);
-    }
-  molmapper->GetFastAtomMapper()->SetLookupTable(lut.Get());
+  cerr << "Class: " << molmapper->GetClassName() << endl;
+  cerr << "Atoms: " << molmapper->GetInput()->GetNumberOfAtoms() << endl;
+  cerr << "Bonds: " << molmapper->GetInput()->GetNumberOfBonds() << endl;
 
+  molmapper->UseBallAndStickSettings();
 
   vtkNew<vtkActor> actor;
   actor->SetMapper(molmapper.GetPointer());
+  actor->GetProperty()->SetAmbient(0.2);
   actor->GetProperty()->SetDiffuse(0.7);
-
-  // we override the default shader very slightly so that
-  // the ambient color component is scaled off the diffuse
-  molmapper->GetFastAtomMapper()->AddShaderReplacement(
-    vtkShader::Fragment,  // in the fragment shader
-    "//VTK::Color::Impl",
-    true, // before the standard replacements
-    "//VTK::Color::Impl\n" // we still want the default
-    "  ambientColor = diffuseColor*0.2;\n", //but we add this
-    false // only do it once
-    );
+  actor->GetProperty()->SetSpecular(0.3);
+  actor->GetProperty()->SetSpecularPower(40);
 
   vtkNew<vtkRenderer> ren;
   vtkNew<vtkRenderWindow> win;
@@ -105,11 +69,8 @@ int TestPDBBallAndStickShadows(int argc, char *argv[])
   ren->AddActor(actor.GetPointer());
   ren->ResetCamera();
   ren->GetActiveCamera()->Zoom(1.7);
-  ren->GetActiveCamera()->SetFocalDisk(ren->GetActiveCamera()->GetDistance()*0.05);
-  ren->SetBackground2(0.2, 0.2, 0.3);
-  ren->SetBackground(0.1, 0.1, 0.15);
-  ren->GradientBackgroundOn();
-  win->SetSize(600, 600);
+  ren->SetBackground(0.4, 0.5, 0.6);
+  win->SetSize(450, 450);
 
   // add a plane
   vtkNew<vtkPlaneSource> plane;
@@ -125,53 +86,20 @@ int TestPDBBallAndStickShadows(int argc, char *argv[])
 
   vtkNew<vtkLight> light1;
   light1->SetFocalPoint(0,0,0);
-  light1->SetPosition(0, 0.9, 0.3);
-  light1->SetIntensity(0.5);
-  light1->SetShadowAttenuation(0.6);
+  light1->SetPosition(0,1,0.2);
+  light1->SetColor(0.95,0.97,1.0);
+  light1->SetIntensity(0.8);
   ren->AddLight(light1.Get());
 
   vtkNew<vtkLight> light2;
   light2->SetFocalPoint(0,0,0);
-  light2->SetPosition(0.0,0.9,-0.3);
-  light2->SetIntensity(0.5);
-  light2->SetShadowAttenuation(0.6);
+  light2->SetPosition(1.0,1.0,1.0);
+  light2->SetColor(1.0,0.8,0.7);
+  light2->SetIntensity(0.3);
   ren->AddLight(light2.Get());
 
-  vtkNew<vtkShadowMapPass> shadows;
+  ren->UseShadowsOn();
 
-  vtkNew<vtkSequencePass> seq;
-  vtkNew<vtkRenderPassCollection> passes;
-  passes->AddItem(shadows->GetShadowMapBakerPass());
-  passes->AddItem(shadows.Get());
-  seq->SetPasses(passes.Get());
-
-  vtkNew<vtkCameraPass> cameraP;
-  cameraP->SetDelegatePass(seq.Get());
-
-  // create the basic VTK render steps
-  vtkNew<vtkRenderStepsPass> basicPasses;
-
-  vtkOpenGLRenderer *glrenderer =
-      vtkOpenGLRenderer::SafeDownCast(ren.GetPointer());
-
-  // finally add the DOF passs
-  vtkNew<vtkDepthOfFieldPass> dofp;
-  dofp->AutomaticFocalDistanceOff();
-  dofp->SetDelegatePass(cameraP.Get());
-
-  // finally blur the resulting image
-  // The blur delegates rendering the unblured image
-  // to the basicPasses
-  vtkNew<vtkSSAAPass> ssaa;
-  ssaa->SetDelegatePass(dofp.Get());
-  //ssaa->SetDelegatePass(cameraP.Get());
-
-  // tell the renderer to use our render pass pipeline
-//  glrenderer->SetPass(ssaa.Get());
-  glrenderer->SetPass(dofp.Get());
-//  glrenderer->SetPass(cameraP.Get());
-
-  // tell the renderer to use our render pass pipeline
   vtkNew<vtkTimerLog> timer;
   timer->StartTimer();
   win->Render();
@@ -179,7 +107,8 @@ int TestPDBBallAndStickShadows(int argc, char *argv[])
   double firstRender = timer->GetElapsedTime();
   cerr << "first render time: " << firstRender << endl;
 
-  int numRenders = 5;
+/*
+  int numRenders = 500;
   timer->StartTimer();
   for (int i = 0; i < numRenders; ++i)
     {
@@ -190,6 +119,7 @@ int TestPDBBallAndStickShadows(int argc, char *argv[])
   timer->StopTimer();
   double elapsed = timer->GetElapsedTime();
   cerr << "interactive render time: " << elapsed / numRenders << endl;
+*/
 
   ren->GetActiveCamera()->SetPosition(0,0,1);
   ren->GetActiveCamera()->SetFocalPoint(0,0,0);
