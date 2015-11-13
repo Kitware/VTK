a8a45720d77a57451e88a106e2ca6d8a83cefa77
Antonella Cascitelli <antonellacascitelli@gmail.com>
2015-11-13 11:19:10 +0100

    Changed the formatting of if/else

--- Rendering/OpenGL/vtkOpenGLCamera.cxx
+++ Rendering/OpenGL/vtkOpenGLCamera.cxx
@@ -112,14 +112,15 @@ void vtkOpenGLCamera::Render(vtkRenderer *ren)
   glViewport(lowerLeft[0], lowerLeft[1], usize, vsize);
   glEnable(GL_SCISSOR_TEST);
   if (this->UseScissor)
-  {
+    {
     glScissor(this->ScissorRect.GetX(),this->ScissorRect.GetY(),
               this->ScissorRect.GetWidth(), this->ScissorRect.GetHeight());
     this->UseScissor = false;
-  }
-  else{
+    }
+  else
+    {
     glScissor(lowerLeft[0], lowerLeft[1], usize, vsize);
-  }
+    }
 
   // some renderer subclasses may have more complicated computations for the
   // aspect ratio. So take that into account by computing the difference
@@ -187,14 +188,15 @@ void vtkOpenGLCamera::UpdateViewport(vtkRenderer *ren)
   glViewport(lowerLeft[0], lowerLeft[1], usize, vsize);
   glEnable(GL_SCISSOR_TEST);
   if (this->UseScissor)
-  {
+    {
     glScissor(this->ScissorRect.GetX(),this->ScissorRect.GetY(),
               this->ScissorRect.GetWidth(), this->ScissorRect.GetHeight());
     this->UseScissor = false;
-  }
-  else{
+    }
+  else
+    {
     glScissor(lowerLeft[0], lowerLeft[1], usize, vsize);
-  }
+    }
 
   vtkOpenGLCheckErrorMacro("failed after UpdateViewport");
 }
--- Rendering/OpenGL2/vtkOpenGLCamera.cxx
+++ Rendering/OpenGL2/vtkOpenGLCamera.cxx
@@ -125,14 +125,15 @@ void vtkOpenGLCamera::Render(vtkRenderer *ren)
   glViewport(lowerLeft[0], lowerLeft[1], usize, vsize);
   glEnable(GL_SCISSOR_TEST);
   if (this->UseScissor)
-  {
+    {
     glScissor(this->ScissorRect.GetX(),this->ScissorRect.GetY(),
               this->ScissorRect.GetWidth(), this->ScissorRect.GetHeight());
     this->UseScissor = false;
-  }
-  else{
+    }
+  else
+    {
     glScissor(lowerLeft[0], lowerLeft[1], usize, vsize);
-  }
+    }
 
   if ((ren->GetRenderWindow())->GetErase() && ren->GetErase()
       && !ren->GetIsPicking())
@@ -155,14 +156,15 @@ void vtkOpenGLCamera::UpdateViewport(vtkRenderer *ren)
   glViewport(lowerLeft[0], lowerLeft[1], usize, vsize);
   glEnable(GL_SCISSOR_TEST);
   if (this->UseScissor)
-  {
+    {
     glScissor(this->ScissorRect.GetX(),this->ScissorRect.GetY(),
               this->ScissorRect.GetWidth(), this->ScissorRect.GetHeight());
     this->UseScissor = false;
-  }
-  else{
+    }
+  else
+    {
     glScissor(lowerLeft[0], lowerLeft[1], usize, vsize);
-  }
+    }
 
   vtkOpenGLCheckErrorMacro("failed after UpdateViewport");
 }
