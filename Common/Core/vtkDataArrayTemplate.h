e4c4919cb1879676e8acb8496c9bc982ed6bd33d
David C. Lonie <david.lonie@kitware.com>
2015-12-11 14:24:15 -0500

    Forward vtkDataArrayTemplate to vtkAOSDataArrayTemplate (legacy only).
    vtkDataArrayTemplate has been deprecated.

---
+++ Common/Core/vtkDataArrayTemplate.h
@@ -0,0 +1,51 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkDataArrayTemplate.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkDataArrayTemplate
+// .SECTION Description
+// vtkDataArrayTemplate is deprecated, use vtkAOSDataArrayTemplate instead.
+
+#ifndef vtkDataArrayTemplate_h
+#define vtkDataArrayTemplate_h
+
+#include "vtkAOSDataArrayTemplate.h"
+
+#ifndef VTK_LEGACY_REMOVE
+
+template <typename ValueType>
+class vtkDataArrayTemplate : public vtkAOSDataArrayTemplate<ValueType>
+{
+public:
+  vtkTemplateTypeMacro(vtkDataArrayTemplate<ValueType>,
+                       vtkAOSDataArrayTemplate<ValueType>)
+
+  static vtkDataArrayTemplate<ValueType>* New()
+  {
+    VTK_STANDARD_NEW_BODY(vtkDataArrayTemplate<ValueType>);
+  }
+
+protected:
+  vtkDataArrayTemplate() {}
+  ~vtkDataArrayTemplate() {}
+
+private:
+  vtkDataArrayTemplate(const vtkDataArrayTemplate&); // Not implemented.
+  void operator=(const vtkDataArrayTemplate&); // Not implemented.
+};
+
+#endif // VTK_LEGACY_REMOVE
+
+#endif // vtkDataArrayTemplate_h
+
+// VTK-HeaderTest-Exclude: vtkDataArrayTemplate.h
