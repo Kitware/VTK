5d3bc16861f6d5c2cd1ad0ceca79c6599f892bb3
Cory Quammen <cory.quammen@kitware.com>
2016-01-28 22:50:43 -0500

    Added LabelOffset member variable
    This can be used to adjust the text position relative to the axis
    and hence the spacing between the tick mark and label.

--- Charts/Core/vtkAxis.h
+++ Charts/Core/vtkAxis.h
@@ -278,6 +278,11 @@ public:
   vtkGetMacro(RangeLabelsVisible, bool);
 
   // Description:
+  // Get/set the offset (in pixels) of the label text position from the axis
+  vtkSetMacro(LabelOffset, float);
+  vtkGetMacro(LabelOffset, float);
+
+  // Description:
   // Get/set whether the tick marks should be visible.
   vtkSetMacro(TicksVisible, bool);
   vtkGetMacro(TicksVisible, bool);
@@ -525,6 +530,7 @@ protected:
   bool GridVisible;    // Whether the grid for the axis should be drawn
   bool LabelsVisible;  // Should the axis labels be visible
   bool RangeLabelsVisible; // Should range labels be visible?
+  float LabelOffset;   // Offset of label from the tick mark
   bool TicksVisible;   // Should the tick marks be visible.
   bool AxisVisible;    // Should the axis line be visible.
   int Precision;       // Numerical precision to use, defaults to 2.
