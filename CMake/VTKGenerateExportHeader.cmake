77bd9900662770d83042f3ca8198d7908bba1d63
Orion Poplawski <orion@cora.nwra.com>
2016-02-02 11:37:22 -0700

    Support gcc versions up to 9.X

--- CMake/VTKGenerateExportHeader.cmake
+++ CMake/VTKGenerateExportHeader.cmake
@@ -174,7 +174,7 @@ macro(_vtk_test_compiler_hidden_visibility)
     execute_process(COMMAND ${CMAKE_C_COMPILER} --version
       OUTPUT_VARIABLE _gcc_version_info
       ERROR_VARIABLE _gcc_version_info)
-    string(REGEX MATCH "[3456]\\.[0-9]\\.[0-9]*"
+    string(REGEX MATCH "[3-9]\\.[0-9]\\.[0-9]*"
       _gcc_version "${_gcc_version_info}")
     # gcc on mac just reports: "gcc (GCC) 3.3 20030304 ..." without the
     # patch level, handle this here:
