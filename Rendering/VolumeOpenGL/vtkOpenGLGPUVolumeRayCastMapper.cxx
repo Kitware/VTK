Kitware/VTK

commit da150781515735ea4493319b82b373ae1c601217

Cleanup/correct workaround for old OS X 10.6 bug
- correct backwards logic recently introduced by 56a17fafa4e389058c89401bdffd524ba873e622,
  the workaround was being applied to non-nVidia instead of for nVidia!
- removed test code, as no one will be debugging ancient 10.6 bugs anymore.
- replaced fragile version string tests, instead check host OS version

Rendering/VolumeOpenGL/vtkOpenGLGPUVolumeRayCastMapper.cxx
@@ -89,8 +89,9 @@
 #include "vtkOpenGLRenderWindow.h"
 #include "vtkOpenGLError.h"
 
-// Uncomment the following line to debug Snow Leopard
-//#define APPLE_SNOW_LEOPARD_BUG
+#if defined(__APPLE__)
+#include <CoreServices/CoreServices.h>
+#endif
 
 //-----------------------------------------------------------------------------
 //
@@ -1506,11 +1507,6 @@ void vtkOpenGLGPUVolumeRayCastMapper::CheckFrameBufferStatus()
     default:
       cout << "Unknown framebuffer status=0x" << hex<< status << dec << endl;
     }
-  // DO NOT REMOVE THE FOLLOWING COMMENTED LINE. FOR DEBUGGING PURPOSE.
-#ifdef APPLE_SNOW_LEOPARD_BUG
-  this->DisplayFrameBufferAttachments();
-  this->DisplayReadAndDrawBuffers();
-#endif
 }
 
 //-----------------------------------------------------------------------------
@@ -2153,15 +2149,19 @@ void vtkOpenGLGPUVolumeRayCastMapper::LoadExtensions(
     }
   vtkOpenGLExtensionManager *extensions = context->GetExtensionManager();
 
-  // It does not work on Apple OS X Snow Leopard with nVidia.
-  // However it works with Apple OS X Lion with nVidia
-  // There is a bug in the OpenGL driver with an error in the
+  // It does not work on Mac OS X 10.6 (Snow Leopard) with nVidia.
+  // There is a bug in that OpenGL driver with an error in the
   // Cg compiler about an infinite loop.
-#if defined(__APPLE__) && !defined(APPLE_SNOW_LEOPARD_BUG)
-  const char *minNVIDIAVersion = "2.1 NVIDIA-8";
-  const char *version = extensions->GetDriverGLVersion();
-  if (!extensions->DriverIsNvidia() ||
-      strncmp(version, minNVIDIAVersion, strlen(minNVIDIAVersion)) < 0)
+  // However it works with Mac OS X 10.7 (Lion) with nVidia.
+#if defined(__APPLE__) && (MAC_OS_X_VERSION_MIN_REQUIRED <= 1060)
+  // Gestalt() is deprecated, but all this code will go away when 10.7 is VTK's minimum.
+  #pragma clang diagnostic push
+  #pragma clang diagnostic ignored "-Wdeprecated-declarations"
+  SInt32 major, minor;
+  Gestalt (gestaltSystemVersionMajor, &major);
+  Gestalt (gestaltSystemVersionMinor, &minor);
+  #pragma clang diagnostic pop
+  if (extensions->DriverIsNvidia() && (major == 10) && (minor == 6))
     {
     this->UnsupportedRequiredExtensions->Stream <<
       " Disabled on unsupported Apple OS X driver.";
@@ -4259,10 +4259,6 @@ void vtkOpenGLGPUVolumeRayCastMapper::PreRender(vtkRenderer *ren,
                      parallelProjection,rayCastMethod,shadeMethod,
                      componentMethod);
 
-#ifdef APPLE_SNOW_LEOPARD_BUG
-  this->Program->Build();
-#endif
-
   vtkUniformVariables *v=this->Program->GetUniformVariables();
 
   // for active texture 0, dataset
@@ -4332,18 +4328,6 @@ void vtkOpenGLGPUVolumeRayCastMapper::PreRender(vtkRenderer *ren,
 
   this->CheckFrameBufferStatus();
 
-#ifdef APPLE_SNOW_LEOPARD_BUG
-  this->Program->SendUniforms();
-  cout << "BEFORE isValid0"  << endl;
-  if(!this->Program->IsValid())
-    {
-    cout <<this->Program->GetLastValidateLog() << endl;
-    this->Program->PrintActiveUniformVariablesOnCout();
-    v->Print(cout);
-    }
-  cout << "AFTER isValid0"  << endl;
-#endif
-
   if(this->NumberOfCroppingRegions>1)
     {
     // framebuffer texture
@@ -4359,18 +4343,6 @@ void vtkOpenGLGPUVolumeRayCastMapper::PreRender(vtkRenderer *ren,
       }
     this->CheckFrameBufferStatus();
 
-#ifdef APPLE_SNOW_LEOPARD_BUG
-    this->Program->SendUniforms();
-    cout << "BEFORE isValid1"  << endl;
-    if(!this->Program->IsValid())
-      {
-      cout <<this->Program->GetLastValidateLog() << endl;
-      this->Program->PrintActiveUniformVariablesOnCout();
-      v->Print(cout);
-      }
-    cout << "AFTER isValid1"  << endl;
-#endif
-
     // max scalar value framebuffer texture
     if(this->BlendMode==vtkVolumeMapper::MAXIMUM_INTENSITY_BLEND
        || this->BlendMode==vtkGPUVolumeRayCastMapper::MINIMUM_INTENSITY_BLEND
@@ -4385,19 +4357,6 @@ void vtkOpenGLGPUVolumeRayCastMapper::PreRender(vtkRenderer *ren,
     }
 
   this->CheckFrameBufferStatus();
-
-#ifdef APPLE_SNOW_LEOPARD_BUG
-  this->Program->SendUniforms();
-  cout << "BEFORE isValid2"  << endl;
-  if(!this->Program->IsValid())
-    {
-    cout <<this->Program->GetLastValidateLog() << endl;
-    this->Program->PrintActiveUniformVariablesOnCout();
-    v->Print(cout);
-    }
-  cout << "AFTER isValid2"  << endl;
-#endif
-
 
   fvalue[0]=static_cast<float>(lowerLeft[0]);
   fvalue[1]=static_cast<float>(lowerLeft[1]);
@@ -5934,14 +5893,6 @@ int vtkOpenGLGPUVolumeRayCastMapper::RenderSubVolume(vtkRenderer *ren,
   this->LoadProjectionParameters(ren,volume);
   this->ClipBoundingBox(ren,bounds,volume);
   this->Program->SendUniforms();
-#ifdef APPLE_SNOW_LEOPARD_BUG
-  if(!this->Program->IsValid())
-    {
-    cout << "line " << __LINE__ << " " <<
-      this->Program->GetLastValidateLog() << endl;
-    }
-  this->Program->PrintActiveUniformVariablesOnCout();
-#endif
   int abort=this->RenderClippedBoundingBox(1,0,1,ren->GetRenderWindow());
   if (!abort)
     {

commit 0c166ca032a979007abe53d819cddbab9efb38e1

Fixed regression from da150781, correct OS X deployment check
Deployment can in fact be 1068, which I’d entirely forgotten.

Rendering/VolumeOpenGL/vtkOpenGLGPUVolumeRayCastMapper.cxx
@@ -2153,7 +2153,7 @@ void vtkOpenGLGPUVolumeRayCastMapper::LoadExtensions(
   // There is a bug in that OpenGL driver with an error in the
   // Cg compiler about an infinite loop.
   // However it works with Mac OS X 10.7 (Lion) with nVidia.
-#if defined(__APPLE__) && (MAC_OS_X_VERSION_MIN_REQUIRED <= 1060)
+#if defined(__APPLE__) && (MAC_OS_X_VERSION_MIN_REQUIRED < 1070)
   // Gestalt() is deprecated, but all this code will go away when 10.7 is VTK's minimum.
   #pragma clang diagnostic push
   #pragma clang diagnostic ignored "-Wdeprecated-declarations"