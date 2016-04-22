6ba3090e5a06dd8d9d17c61fea0be64a8f073f1f
David Gobbi <david.gobbi@gmail.com>
2016-04-21 23:12:17 -0600

    Update the vtkDICOM remote module to v0.7.7.

--- Remote/vtkDICOM.remote.cmake
+++ Remote/vtkDICOM.remote.cmake
@@ -5,6 +5,6 @@
 vtk_fetch_module(vtkDICOM
   "Dicom classes and utilities"
   GIT_REPOSITORY https://github.com/dgobbi/vtk-dicom
-  # vtk-dicom tag v0.7.6 (Mar 23, 2016)
-  GIT_TAG 67b8b50a1a68e520a307565ee2a39e2e350422c6
+  # vtk-dicom tag v0.7.7 (Apr 21, 2016)
+  GIT_TAG 1b2085814fc7f246ddb3f27e487233fe1f02dcb0
   )
