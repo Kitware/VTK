49290fb9b782acd24328a9624878b7f42342336a
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2016-05-02 16:20:09 -0400

    Fix color buffer clear color for RenderToImage mode
    Color buffer was being cleared to (1, 0, 1, 0) instead of
    (1, 1, 1, 0)

--- Rendering/VolumeOpenGL2/vtkOpenGLGPUVolumeRayCastMapper.cxx
+++ Rendering/VolumeOpenGL2/vtkOpenGLGPUVolumeRayCastMapper.cxx
@@ -2315,7 +2315,7 @@ void vtkOpenGLGPUVolumeRayCastMapper::vtkInternal::SetupRenderToTexture(
 
     this->FBO->CheckFrameBufferStatus(GL_FRAMEBUFFER);
 
-    glClearColor(1.0, 0.0, 1.0, 0.0);
+    glClearColor(1.0, 1.0, 1.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   }
 }
