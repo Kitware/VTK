d97bea99f15a4113ecac5c3e381542b92fdbbd2b
David C. Lonie <david.lonie@kitware.com>
2016-05-06 10:04:05 -0400

    Allow NULL inputs to array FastDownCast methods.

--- Common/Core/vtkDataArray.h
+++ Common/Core/vtkDataArray.h
@@ -413,17 +413,21 @@ private:
 //------------------------------------------------------------------------------
 inline vtkDataArray* vtkDataArray::FastDownCast(vtkAbstractArray *source)
 {
-  switch (source->GetArrayType())
+  if (source)
     {
-    case AoSDataArrayTemplate:
-    case SoADataArrayTemplate:
-    case TypedDataArray:
-    case DataArray:
-    case MappedDataArray:
-      return static_cast<vtkDataArray*>(source);
-    default:
-      return NULL;
+    switch (source->GetArrayType())
+      {
+      case AoSDataArrayTemplate:
+      case SoADataArrayTemplate:
+      case TypedDataArray:
+      case DataArray:
+      case MappedDataArray:
+        return static_cast<vtkDataArray*>(source);
+      default:
+        break;
+      }
     }
+  return NULL;
 }
 
 vtkArrayDownCast_FastCastMacro(vtkDataArray)
