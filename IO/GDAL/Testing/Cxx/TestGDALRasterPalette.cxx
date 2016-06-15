fc28e36ea20b9280d2ca55c0ace89f1b21a1f741
John Tourtellott <john.tourtellott@kitware.com>
2016-06-15 17:30:37 -0400

    Add test for vtkGDALRasterReader with image using color palette

--- IO/GDAL/Testing/Cxx/TestGDALRasterPalette.cxx
+++ IO/GDAL/Testing/Cxx/TestGDALRasterPalette.cxx
@@ -12,14 +12,16 @@
      PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
+#include <vtkDataArray.h>
 #include <vtkGDALRasterReader.h>
 #include <vtkNew.h>
+#include <vtkPointData.h>
 #include <vtkUniformGrid.h>
 
 #include <iostream>
 
 // Main program
-int TestGDALRasterNoDataValue(int argc, char** argv)
+int TestGDALRasterPalette(int argc, char** argv)
 {
   if (argc < 3)
     {
@@ -33,40 +35,14 @@ int TestGDALRasterNoDataValue(int argc, char** argv)
   vtkNew<vtkGDALRasterReader> reader;
   reader->SetFileName(inputFileName.c_str());
   reader->Update();
+  vtkUniformGrid *image = vtkUniformGrid::SafeDownCast(reader->GetOutput());
 
-  vtkUniformGrid *rasterImage = vtkUniformGrid::SafeDownCast(
-    reader->GetOutput());
-
-  int numErrors = 0;
-
-  if (!rasterImage->HasAnyBlankPoints())
+  // Check that reader generated point scalars
+  if (image->GetPointData()->GetNumberOfArrays() < 1)
     {
-    std::cerr << "Error image has no blank points" << std::endl;
-    ++numErrors;
+    std::cerr << "ERROR: No point data scalars generated" << std::endl;
+    return 1;
     }
 
-  if (!rasterImage->HasAnyBlankCells())
-    {
-    std::cerr << "Error image has no blank cells" << std::endl;
-    ++numErrors;
-    }
-
-  double *scalarRange = rasterImage->GetScalarRange();
-
-  if ((scalarRange[0] < -888.5) || (scalarRange[0]) > -887.5)
-    {
-    std::cerr << "Error scalarRange[0] should be -888.0, not "
-              << scalarRange[0] << std::endl;
-    ++numErrors;
-    }
-
-  if ((scalarRange[1] < 9998.5) || (scalarRange[1] > 9999.5))
-    {
-    std::cerr << "Error scalarRange[1] should be 9999.0, not "
-              << scalarRange[1] << std::endl;
-    ++numErrors;
-    }
-
-  //std::cout << "numErrors: " << numErrors << std::endl;
-  return numErrors;
+  return 0;
 }
