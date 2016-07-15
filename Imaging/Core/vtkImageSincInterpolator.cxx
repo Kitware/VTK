4bf5dd6bafbdd8e72bcd4795589aa62fdf9b4686
David Gobbi <david.gobbi@gmail.com>
2016-07-15 16:34:44 -0600

    Consistently specify interpolator invalid clip extent
    The clip extent provides the intersection of the output extent and the
    input extent, taking the kernel size into account.  If it is empty, it
    should always be specified the same way: the lower bound should be the
    same as the output's lower bound, and the upper bound should be one unit
    less than the lower bound (so that the computed size in any dimension is
    zero).

--- Imaging/Core/vtkImageSincInterpolator.cxx
+++ Imaging/Core/vtkImageSincInterpolator.cxx
@@ -1078,6 +1078,7 @@ void vtkImageSincInterpolatorPrecomputeWeights(
   blur[2] = ((mode & VTK_INTERPOLATION_WINDOW_ZBLUR_MASK) != 0);
 
   // set up input positions table for interpolation
+  bool validClip = true;
   for (int j = 0; j < 3; j++)
     {
     // set k to the row for which the element in column j is nonzero,
@@ -1234,9 +1235,19 @@ void vtkImageSincInterpolatorPrecomputeWeights(
         }
       }
 
-    if (region == 0)
+    if (region == 0 || clipExt[2*j] > clipExt[2*j+1])
       { // never entered input extent!
-      clipExt[2*j] = clipExt[2*j+1] + 1;
+      validClip = false;
+      }
+    }
+
+  if (!validClip)
+    {
+    // output extent doesn't itersect input extent
+    for (int j = 0; j < 3; j++)
+      {
+      clipExt[2*j] = outExt[2*j];
+      clipExt[2*j + 1] = outExt[2*j] - 1;
       }
     }
 }
