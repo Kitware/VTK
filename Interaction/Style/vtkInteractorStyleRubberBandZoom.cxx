ed7f0444f98bef66b935a32a5efad6e12cebf05d
Eloi Gaudry <eg@fft.be>
2016-08-25 15:30:56 +0200

    Add missing Frame() call on the renderWindow
    (mandatory for proper drawing of the RubberBand with OGL2 backend)

--- Interaction/Style/vtkInteractorStyleRubberBandZoom.cxx
+++ Interaction/Style/vtkInteractorStyleRubberBandZoom.cxx
@@ -99,6 +99,7 @@ void vtkInteractorStyleRubberBandZoom::OnMouseMove()
     }
 
   this->Interactor->GetRenderWindow()->SetPixelData(0, 0, size[0]-1, size[1]-1, pixels, 1);
+  this->Interactor->GetRenderWindow()->Frame();
 
   tmpPixelArray->Delete();
 }
