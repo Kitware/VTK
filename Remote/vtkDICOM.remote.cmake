057462bf3360877bb6841dd8dcae7f8ae3ac7022
David Gobbi <david.gobbi@gmail.com>
2016-09-12 23:17:38 -0600

    Update the vtkDICOM remote module to 0.7.9.
    Changes since 0.7.8:
    - Update dictionary using 2016c Part 6.
    - Update private dictionaries from dicom3tools.
    - Automatically perform YBR to RGB conversion in reader.
    - Add a ShowHidden option to vtkDICOMDirectory.
    - Move UID generation into its own class.
    - Make dicomfind {} substitution more flexible.
    - Allow adding to the directory input file names.
    - Change the brace style to non-indented.
    - Change all header documentation to doxygen.
    
    Bug fixes since 0.7.8:
    - Fix broken SetInputFileName() method of FileSorter.
    - If duplicate instances are found, split into different series.
    - Made the IsDICOMFile() check more lenient.
    - Use UID, not Instance, to find duplicates in vtkDICOMFileSorter.
    - Skip localizers at the beginning of a series.
    - Do not reject unrecognized VRs that include a 'space' character.
    - Check for repeated filenames when building vtkDICOMDirectory.

--- Remote/vtkDICOM.remote.cmake
+++ Remote/vtkDICOM.remote.cmake
@@ -5,6 +5,6 @@
 vtk_fetch_module(vtkDICOM
   "Dicom classes and utilities"
   GIT_REPOSITORY https://github.com/dgobbi/vtk-dicom
-  # vtk-dicom tag v0.7.8 (May 27, 2016)
-  GIT_TAG b21075408c74ef8a2211ed7b51bc9de85e2238c1
+  # vtk-dicom tag v0.7.9 (Sep 12, 2016)
+  GIT_TAG f1e78955a98c5d66cbe16a0e649c59a66d57ac59
   )
