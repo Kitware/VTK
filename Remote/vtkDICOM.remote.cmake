b6a9c94b86f3049caec540254f93ce4dc2e50dfc
David Gobbi <david.gobbi@gmail.com>
2016-09-14 15:15:57 -0600

    Update the vtkDICOM remote module to 0.7.10.
    The dependencies in the module.cmake files for the various VTK were
    recently cleaned up to minimize module inter-dependencies, and this
    unfortunately caused the unmarked dependency of vtkDICOM on the VTK
    module vtkCommonSystem to be exposed.  So the v0.7.10 release of
    vtk-dicom exists to remove this dependency.

--- Remote/vtkDICOM.remote.cmake
+++ Remote/vtkDICOM.remote.cmake
@@ -5,6 +5,6 @@
 vtk_fetch_module(vtkDICOM
   "Dicom classes and utilities"
   GIT_REPOSITORY https://github.com/dgobbi/vtk-dicom
-  # vtk-dicom tag v0.7.9 (Sep 12, 2016)
-  GIT_TAG f1e78955a98c5d66cbe16a0e649c59a66d57ac59
+  # vtk-dicom tag v0.7.10 (Sep 14, 2016)
+  GIT_TAG cd765aa469c3d592ca62ef40e08692cee6849129
   )
