322f967c4ca3ee387c47a433db6dc8f1a7ac19b7
Ben Boeckel <ben.boeckel@kitware.com>
2016-09-16 16:21:52 -0400

    vtkOSPRayPass: avoid variable length stack arrays
    Not supported everywhere.

--- Rendering/OSPRay/vtkOSPRayPass.cxx
+++ Rendering/OSPRay/vtkOSPRayPass.cxx
@@ -79,7 +79,7 @@ vtkOSPRayPass::vtkOSPRayPass()
       args.push_back(arg);
       }
     int ac =args.size()+1;
-    const char* av[ac];
+    const char** av = new const char*[ac];
     av[0] = new char[512];
     strcpy(av[0],"pvOSPRay\0");
     for(int i=1;i < ac; i++)
@@ -95,6 +95,7 @@ vtkOSPRayPass::vtkOSPRayPass()
       {
       //todo: request addition of ospFinalize() to ospray
       }
+    delete [] av;
     }
   else
     {
