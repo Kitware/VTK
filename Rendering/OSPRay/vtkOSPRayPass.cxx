e8d03b9c8d137b14b6334ab18a2bff951769afa3
Ben Boeckel <ben.boeckel@kitware.com>
2016-09-16 16:22:17 -0400

    vtkOSPRayPass: avoid needless memory gymnastics

--- Rendering/OSPRay/vtkOSPRayPass.cxx
+++ Rendering/OSPRay/vtkOSPRayPass.cxx
@@ -80,12 +80,10 @@ vtkOSPRayPass::vtkOSPRayPass()
       }
     int ac =args.size()+1;
     const char** av = new const char*[ac];
-    av[0] = new char[512];
-    strcpy(av[0],"pvOSPRay\0");
+    av[0] = "pvOSPRay";
     for(int i=1;i < ac; i++)
       {
-      av[i] = new char[args[i-1].size()+1];
-      strcpy(av[i], args[i-1].c_str());
+      av[i] = args[i - 1].c_str();
       }
     try
       {
