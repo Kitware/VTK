Kitware/VTK

commit 6e113ad4e80298b0ad1be944c20259490006bd34

Rewrite all public&private dependency in module.cmake file
The current dependency relationship in vtk is unclear and misleading.
This MR tries to rewrite them based on header files inclusion of headers
and source files in each module. Corner cases are considered and
modules are sorted in alphabetical order to facilitate future reference.
See details in my gitlab python based script project. In future we can continue
using this script to clean the VTK Dependency easily from
time to time.

Rendering/External/module.cmake
@@ -1,9 +1,14 @@
 vtk_module(vtkRenderingExternal
-  DEPENDS
-    vtkRenderingCore
-    vtkRendering${VTK_RENDERING_BACKEND}
   TEST_DEPENDS
     vtkglew
     vtkTestingRendering
   EXCLUDE_FROM_ALL
+  DEPENDS
+    vtkCommonCore
+    vtkRendering${VTK_RENDERING_BACKEND}
+    vtkRenderingCore
+  PRIVATE_DEPENDS
+    vtkCommonMath
+    vtkCommonTransforms
+    vtkRenderingOpenGL
   )

Accelerators/Dax/module.cmake
@@ -2,12 +2,6 @@ if(NOT VTK_LEGACY_REMOVE)
   vtk_module(vtkAcceleratorsDax
     IMPLEMENTS
       vtkFiltersCore
-    DEPENDS
-      vtkCommonCore
-      vtkCommonDataModel
-      vtkCommonExecutionModel
-      vtkFiltersCore
-      vtkFiltersGeometry
     TEST_DEPENDS
       vtkTestingCore
       vtkTestingRendering
@@ -17,5 +11,9 @@ if(NOT VTK_LEGACY_REMOVE)
       vtkIOXML
       vtkImagingSources
     EXCLUDE_FROM_ALL
+    DEPENDS
+      vtkCommonCore
+      vtkCommonDataModel
+      vtkFiltersCore
     )
 endif()

Accelerators/Piston/module.cmake
@@ -1,13 +1,5 @@
 if(NOT VTK_LEGACY_REMOVE)
   vtk_module(vtkAcceleratorsPiston
-    DEPENDS
-      vtkCommonCore
-      vtkCommonDataModel
-      vtkCommonExecutionModel
-      vtkRenderingCore
-      vtkRenderingOpenGL
-      vtkIOImage #from piston
-      vtkImagingHybrid #from piston
     TEST_DEPENDS
       vtkTestingRendering
       vtkInteractionStyle
@@ -17,5 +9,11 @@ if(NOT VTK_LEGACY_REMOVE)
       vtkParallelMPI
       vtkRenderingParallel
     EXCLUDE_FROM_ALL
+    DEPENDS
+      vtkCommonCore
+      vtkCommonDataModel
+      vtkCommonExecutionModel
+      vtkRenderingCore
+      vtkRenderingOpenGL
     )
 endif()

Charts/Core/module.cmake
@@ -1,12 +1,6 @@
 vtk_module(vtkChartsCore
   GROUPS
     StandAlone
-  DEPENDS
-    vtkRenderingContext2D
-    vtksys
-  PRIVATE_DEPENDS
-    vtkCommonColor
-    vtkInfovisCore # Needed for plot parallel coordinates vtkStringToCategory
   TEST_DEPENDS
     vtkTestingCore
     vtkTestingRendering
@@ -14,4 +8,16 @@ vtk_module(vtkChartsCore
     vtkRenderingContext${VTK_RENDERING_BACKEND}
     #vtkIOExport
     vtkIOInfovis
+  DEPENDS
+    vtkCommonCore
+    vtkCommonDataModel
+    vtkFiltersGeneral
+    vtkRenderingContext2D
+    vtkRenderingCore
+    vtksys
+  PRIVATE_DEPENDS
+    vtkCommonColor
+    vtkCommonExecutionModel
+    vtkCommonTransforms
+    vtkInfovisCore
   )

Common/Color/module.cmake
@@ -1,6 +1,4 @@
 vtk_module(vtkCommonColor
-  DEPENDS
-    vtkCommonDataModel # For vtkColor
   TEST_DEPENDS
     vtkIOImage
     vtkCommonExecutionModel
@@ -10,4 +8,7 @@ vtk_module(vtkCommonColor
     vtkInteractionStyle
   KIT
     vtkCommon
+  DEPENDS
+    vtkCommonCore
+    vtkCommonDataModel
 )

Common/ComputationalGeometry/module.cmake
@@ -1,13 +1,12 @@
 vtk_module(vtkCommonComputationalGeometry
   GROUPS
     StandAlone
-  DEPENDS
-    vtkCommonSystem
-    vtkCommonMath
-    vtkCommonDataModel
   TEST_DEPENDS
     vtkTestingCore
     vtkRendering${VTK_RENDERING_BACKEND}
   KIT
     vtkCommon
+  DEPENDS
+    vtkCommonCore
+    vtkCommonDataModel
   )

Common/Core/module.cmake
@@ -1,10 +1,6 @@
 vtk_module(vtkCommonCore
   GROUPS
     StandAlone
-  DEPENDS
-    vtkkwiml
-  PRIVATE_DEPENDS
-    vtksys
   TEST_DEPENDS
     vtkTestingCore
     vtkCommonSystem
@@ -12,4 +8,8 @@ vtk_module(vtkCommonCore
     vtksys
   KIT
     vtkCommon
+  DEPENDS
+    vtkkwiml
+  PRIVATE_DEPENDS
+    vtksys
   )

Common/DataModel/module.cmake
@@ -1,13 +1,6 @@
 vtk_module(vtkCommonDataModel
   GROUPS
     StandAlone
-  DEPENDS
-    vtkCommonSystem
-    vtkCommonMath
-    vtkCommonMisc
-    vtkCommonTransforms
-  PRIVATE_DEPENDS
-    vtksys
   TEST_DEPENDS
     vtkTestingCore
     vtkTestingRendering
@@ -22,4 +15,12 @@ vtk_module(vtkCommonDataModel
     vtkRendering${VTK_RENDERING_BACKEND}
   KIT
     vtkCommon
+  DEPENDS
+    vtkCommonCore
+    vtkCommonMath
+    vtkCommonTransforms
+  PRIVATE_DEPENDS
+    vtkCommonMisc
+    vtkCommonSystem
+    vtksys
   )

commit c0dcaf3342873c9ccf8d862194293c595c880907

Fix rendering external configure issues introduced by 6e113ad4

Rendering/External/module.cmake
@@ -10,5 +10,5 @@ vtk_module(vtkRenderingExternal
   PRIVATE_DEPENDS
     vtkCommonMath
     vtkCommonTransforms
-    vtkRenderingOpenGL
+    vtkRendering${VTK_RENDERING_BACKEND}
   )