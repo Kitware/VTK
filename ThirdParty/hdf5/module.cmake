452cfde95d704af966bc6799d963df3962446963
Ben Boeckel <ben.boeckel@kitware.com>
2016-09-22 10:42:38 -0400

    hdf5: link against MPI if a parallel HDF5 is found
    See paraview/paraview#16814.

--- ThirdParty/hdf5/module.cmake
+++ ThirdParty/hdf5/module.cmake
@@ -5,6 +5,17 @@ else()
 endif()
 if(VTK_USE_SYSTEM_HDF5)
   set(vtkhdf5_LIBRARIES ${HDF5_LIBRARIES} ${HDF5_HL_LIBRARIES})
+  if(NOT HDF5_IS_PARALLEL)
+    include(CheckSymbolExists)
+    set(CMAKE_REQUIRED_INCLUDES ${HDF5_INCLUDE_DIRS})
+    check_symbol_exists(H5_HAVE_PARALLEL hdf5.h HDF5_IS_PARALLEL)
+    unset(CMAKE_REQUIRED_INCLUDES)
+  endif()
+  if (HDF5_IS_PARALLEL)
+    include(vtkMPI)
+    list(APPEND vtkhdf5_LIBRARIES
+      ${MPI_C_LIBRARIES} ${MPI_CXX_LIBRARIES})
+  endif ()
 endif()
 vtk_module(vtkhdf5
   EXCLUDE_FROM_WRAPPING
