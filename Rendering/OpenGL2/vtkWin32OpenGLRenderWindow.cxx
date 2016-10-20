275da290d77480bf862d5593660121c318835432
Ken Martin <ken.martin@kitware.com>
2016-10-20 15:17:40 -0400

    try fix for offscreen rendering on Windows
    The context was getting cleaned too often maybe.
    This fix reverts one line from commit
    https://gitlab.kitware.com/vtk/vtk/commit/
    3a8e4497f83de76b455159dc897f3157079b868a
    
    This commit fixes issue
    
    https://gitlab.kitware.com/vtk/vtk/issues/16876
    
    and adds a test for that condition as well as
    a few other cases that should work but could
    cause problems.
    
    This change could introduce other issues limited
    to windows as the code is fairly complex and
    the use cases vary widely. At some point the whole
    process of creating windows and handling offscreen
    drawing needs to be reworked. It is a mishmash of 20
    years of code and overdue for a real workover for all
    platforms. But this topic is intended to fix this one
    case until we have time to really look at it and rework
    it.
    
    Currently the new test is only active for OpenGL2 and
    Windows. OpenGL1 and other platforms has issues with
    it that need to be fixed in the future. But for now this
    change fixes the reported Windows issues and adds a
    test to maintain that fix.

--- Rendering/OpenGL2/vtkWin32OpenGLRenderWindow.cxx
+++ Rendering/OpenGL2/vtkWin32OpenGLRenderWindow.cxx
@@ -1071,12 +1071,12 @@ void vtkWin32OpenGLRenderWindow::Finalize (void)
 
 void vtkWin32OpenGLRenderWindow::DestroyWindow()
 {
-  this->Clean();
   if(this->WindowIdReferenceCount > 0)
   {
     --this->WindowIdReferenceCount;
     if(this->WindowIdReferenceCount == 0)
     {
+      this->Clean();
       if (this->WindowId)
       {
         ReleaseDC(this->WindowId, this->DeviceContext);
