ee844561c656c4539e18addfc424816f251cd0b8
Ben Boeckel <ben.boeckel@kitware.com>
2016-10-26 13:28:39 -0400

    ThirdParty: add an import script for expat

---
+++ ThirdParty/expat/update.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+shopt -s dotglob
+
+readonly name="expat"
+readonly ownership="Expat Upstream <kwrobot@kitware.com>"
+readonly subtree="ThirdParty/$name/vtk$name"
+readonly repo="https://gitlab.kitware.com/third-party/expat.git"
+readonly tag="for/vtk"
+readonly paths="
+expat/.gitattributes
+expat/CMakeLists.txt
+expat/COPYING
+expat/ConfigureChecks.cmake
+expat/README
+expat/expat_config.h.cmake
+expat/lib/*.c
+expat/lib/*.h
+expat/lib/libexpat.def
+"
+
+extract_source () {
+    git_archive
+    # Everything is under an extra expat/ directory; remove it.
+    mv "$extractdir/$name-reduced/expat/"* "$extractdir/$name-reduced/"
+    rmdir "$extractdir/$name-reduced/expat"
+}
+
+. "${BASH_SOURCE%/*}/../update-common.sh"
