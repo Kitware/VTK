ba4813f28340c5c04ae61413eb954dfd5fb1b145
Ken Martin <ken.martin@kitware.com>
2017-01-05 09:14:17 -0500

    fix unsafe memory write in vtkImageGradientMagnitude etc
    The class was writing to a memory location from
    multiple threads bro. Also fix thread issues in
    the TestAtomic and TestConditionVariable tests.

--- Common/Core/Testing/Cxx/TestAtomic.cxx
+++ Common/Core/Testing/Cxx/TestAtomic.cxx
@@ -19,8 +19,6 @@
 
 #include <algorithm>
 
-static int Total = 0;
-static vtkTypeInt64 Total64 = 0;
 static vtkAtomicInt32 TotalAtomic(0);
 static vtkAtomicInt64 TotalAtomic64(0);
 static const int Target = 1000000;
@@ -29,17 +27,27 @@ static int Values64[Target+1];
 static vtkMTimeType MTimeValues[Target];
 static int NumThreads = 5;
 
+// uncomment the following line if you want to see
+// the difference between using atomics and not
+//#define SHOW_DIFFERENCE
+#ifdef SHOW_DIFFERENCE
+static int Total = 0;
+static vtkTypeInt64 Total64 = 0;
+#endif
 
 VTK_THREAD_RETURN_TYPE MyFunction(void *)
 {
   vtkNew<vtkObject> AnObject;
   for (int i=0; i<Target/NumThreads; i++)
   {
+#ifdef SHOW_DIFFERENCE
     Total++;
+    Total64++;
+#endif
+
     int idx = ++TotalAtomic;
     Values32[idx] = 1;
 
-    Total64++;
     idx = ++TotalAtomic64;
     Values64[idx] = 1;
 
@@ -96,9 +104,12 @@ VTK_THREAD_RETURN_TYPE MyFunction4(void *)
 
 int TestAtomic(int, char*[])
 {
+#ifdef SHOW_DIFFERENCE
   Total = 0;
-  TotalAtomic = 0;
   Total64 = 0;
+#endif
+
+  TotalAtomic = 0;
   TotalAtomic64 = 0;
 
   for (int i=0; i<=Target; i++)
@@ -160,8 +171,10 @@ int TestAtomic(int, char*[])
   mt->SetSingleMethod(MyFunction4, NULL);
   mt->SingleMethodExecute();
 
+#ifdef SHOW_DIFFERENCE
   cout << Total << " " << TotalAtomic.load() << endl;
   cout << Total64 << " " << TotalAtomic64.load() << endl;
+#endif
 
   if (TotalAtomic.load() != Target)
   {
