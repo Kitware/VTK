82dde4a1da05e77dd36e899cd922d698c9e71a38
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2017-01-10 12:13:22 -0500

    Add test for QVTKOpenGLWidget.

---
+++ GUISupport/Qt/Testing/Cxx/TestQVTKOpenGLWidget.cxx
@@ -0,0 +1,71 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    TestQVTKOpenGLWidget.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// Tests QVTKOpenGLWidget
+
+#include "QVTKOpenGLWidget.h"
+#include "vtkActor.h"
+#include "vtkGenericOpenGLRenderWindow.h"
+#include "vtkNew.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkRenderer.h"
+#include "vtkSphereSource.h"
+#include "vtkTesting.h"
+
+#include <QApplication>
+#include <QSurfaceFormat>
+
+int TestQVTKOpenGLWidget(int argc, char* argv[])
+{
+  // disable multisampling.
+  vtkOpenGLRenderWindow::SetGlobalMaximumNumberOfMultiSamples(0);
+  QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
+
+  QApplication app(argc, argv);
+
+  vtkNew<vtkTesting> vtktesting;
+  vtktesting->AddArguments(argc, argv);
+
+  vtkNew<vtkGenericOpenGLRenderWindow> window;
+
+  QVTKOpenGLWidget widget;
+  widget.SetRenderWindow(window.Get());
+
+  vtkNew<vtkRenderer> ren;
+  ren->SetGradientBackground(1);
+  ren->SetBackground2(0.7, 0.7, 0.7);
+  window->AddRenderer(ren.Get());
+
+  vtkNew<vtkSphereSource> sphere;
+  vtkNew<vtkPolyDataMapper> mapper;
+  mapper->SetInputConnection(sphere->GetOutputPort());
+  vtkNew<vtkActor> actor;
+  actor->SetMapper(mapper.Get());
+  ren->AddActor(actor.Get());
+
+  vtktesting->SetRenderWindow(window.Get());
+  widget.show();
+  app.processEvents();
+
+  int retVal = vtktesting->RegressionTest(10);
+  switch (retVal)
+  {
+    case vtkTesting::DO_INTERACTOR:
+      return app.exec();
+    case vtkTesting::FAILED:
+    case vtkTesting::NOT_RUN:
+      return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
