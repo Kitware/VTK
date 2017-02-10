46f8c6a5badf16823f03510ab826c8d1e518e349
David Gobbi <david.gobbi@gmail.com>
2017-02-10 14:07:59 -0700

    Update the vtk-dicom module to fix Cpp11 warnings
    This brings the vtk-dicom remote module to 9a4c5c3c23, which is the
    patch that adds VTK_OVERRIDE and VTK_DELETE_FUNCTION.  This is needed
    to avoid warnings from some c++11 compilers.

--- Remote/vtkDICOM.remote.cmake
+++ Remote/vtkDICOM.remote.cmake
@@ -5,6 +5,6 @@
 vtk_fetch_module(vtkDICOM
   "Dicom classes and utilities"
   GIT_REPOSITORY https://github.com/dgobbi/vtk-dicom
-  # vtk-dicom tag v0.7.10 (Sep 14, 2016)
-  GIT_TAG cd765aa469c3d592ca62ef40e08692cee6849129
+  # vtk-dicom with override, delete for C++11 (Feb 10, 2017)
+  GIT_TAG 9a4c5c3c23b952f17f2ed540cf60f932f9e1262c
   )
