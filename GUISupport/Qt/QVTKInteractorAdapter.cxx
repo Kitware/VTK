b6bed6689478964c7ab99170eb70579e159fc98d
David Gobbi <david.gobbi@gmail.com>
2017-03-14 01:18:27 -0600

    Fix QVTKInteractorAdaptor mouse wheel events
    There were two problems that needed to be fixed for the wheel events.
    The first problem had to do with changing direction: since the
    AccumulatedDelta kept the residual after the threshold was passed,
    the first wheel click in the opposite direction was sometimes ignored.
    Not keeping any residual fixes this, though it makes scrolling on a Mac
    trackpad a bit less smooth.  The second problem is that the delta()
    method is deprecated for Qt5, and angleDelta() should be used instead.

--- GUISupport/Qt/QVTKInteractorAdapter.cxx
+++ GUISupport/Qt/QVTKInteractorAdapter.cxx
@@ -291,29 +291,23 @@ bool QVTKInteractorAdapter::ProcessEvent(QEvent* e, vtkRenderWindowInteractor* i
                                (e2->modifiers() & Qt::ShiftModifier ) > 0 ? 1 : 0);
     iren->SetAltKey((e2->modifiers() & Qt::AltModifier) > 0 ? 1 : 0);
 
+#if QT_VERSION >= 0x050000
+    this->AccumulatedDelta += e2->angleDelta().y();
+#else
     this->AccumulatedDelta += e2->delta();
+#endif
     const int threshold = 120;
 
     // invoke vtk event when accumulated delta passes the threshold
     if(this->AccumulatedDelta >= threshold)
     {
       iren->InvokeEvent(vtkCommand::MouseWheelForwardEvent, e2);
-      this->AccumulatedDelta -= threshold;
-      // avoid accumulating too much delta per event
-      if (this->AccumulatedDelta > threshold/2)
-      {
-        this->AccumulatedDelta = threshold/2;
-      }
+      this->AccumulatedDelta = 0;
     }
     else if(this->AccumulatedDelta <= -threshold)
     {
       iren->InvokeEvent(vtkCommand::MouseWheelBackwardEvent, e2);
-      this->AccumulatedDelta += threshold;
-      // avoid accumulating too much delta per event
-      if (this->AccumulatedDelta < -threshold/2)
-      {
-        this->AccumulatedDelta = -threshold/2;
-      }
+      this->AccumulatedDelta = 0;
     }
     return true;
   }
