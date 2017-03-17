4104d30db1688954596a44052cfa3108bd912d69
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-03-17 11:59:28 -0600

    Java: Fix stack trace printing on Linux

--- Wrapping/Java/vtk/rendering/vtkInteractorForwarder.java
+++ Wrapping/Java/vtk/rendering/vtkInteractorForwarder.java
@@ -379,7 +379,7 @@ public class vtkInteractorForwarder implements MouseListener, MouseMotionListene
           System.out.println("Invalid scale type: " + scale);
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      // Don't care, at least we tried.
     }
     return 1;
   }
