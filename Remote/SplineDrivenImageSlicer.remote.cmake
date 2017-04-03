e3648a644752e7329856a936bd0f3b9e11b9778d
Bill Lorensen <bill.lorensen@gmail.com>
2017-04-03 13:58:20 -0400

    ENH: Add Spline Driven Image Slicer as a remote module
    From Spline Driven Image Slicer
    http://www.vtkjournal.org/browse/publication/838"

---
+++ Remote/SplineDrivenImageSlicer.remote.cmake
@@ -0,0 +1,10 @@
+#
+# Midas Journal 838
+#
+
+vtk_fetch_module(SplineDrivenImageSlicer
+  "Spline Driven Image Slicer - http://www.vtkjournal.org/browse/publication/838"
+  GIT_REPOSITORY https://github.com/lorensen/midas-journal-838.git
+# First working as a remote module
+  GIT_TAG 7994999c1e4b56ab546527788b7c894a138e97a1
+  )
