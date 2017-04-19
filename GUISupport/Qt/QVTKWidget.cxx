c5902e18d982e33fb5b83f35377f216625531406
Fabian Wenzel <fabian.wenzel@philips.com>
2017-04-19 15:00:35 +0200

    Fixes reparenting bug of QVTKWidget

--- GUISupport/Qt/QVTKWidget.cxx
+++ GUISupport/Qt/QVTKWidget.cxx
@@ -359,15 +359,6 @@ bool QVTKWidget::event(QEvent* e)
   if(e->type() == QEvent::ParentAboutToChange)
   {
     this->markCachedImageAsDirty();
-    if (this->mRenWin)
-    {
-      // Finalize the window to remove graphics resources associated with
-      // this window
-      if(this->mRenWin->GetMapped())
-      {
-        this->mRenWin->Finalize();
-      }
-    }
   }
   else if(e->type() == QEvent::ParentChange)
   {
@@ -376,12 +367,6 @@ bool QVTKWidget::event(QEvent* e)
       x11_setup_window();
       // connect to new window
       this->mRenWin->SetWindowId( reinterpret_cast<void*>(this->winId()));
-
-      // start up the window to create graphics resources for this window
-      if(isVisible())
-      {
-        this->mRenWin->Start();
-      }
     }
   }
   else if(e->type() == QEvent::TouchBegin ||
