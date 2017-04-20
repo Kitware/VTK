cba198e54bc30aaf40fd68aedd790ab86d453fe1
Ben Boeckel <ben.boeckel@kitware.com>
2017-04-20 13:41:00 -0400

    KWIML: generate forwarding headers during the import

--- Utilities/KWIML/update.sh
+++ Utilities/KWIML/update.sh
@@ -14,6 +14,14 @@ readonly paths="
 
 extract_source () {
     git_archive
+    cat > "$extractdir/$name-reduced/abi.h" <<EOF
+/* Forward include for source-tree layout.  */
+#include "include/kwiml/abi.h"
+EOF
+    cat > "$extractdir/$name-reduced/int.h" <<EOF
+/* Forward include for source-tree layout.  */
+#include "include/kwiml/int.h"
+EOF
 }
 
 . "${BASH_SOURCE%/*}/../../ThirdParty/update-common.sh"
