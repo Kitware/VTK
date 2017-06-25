a057249c3ffb5e8d95c97b4e88c31948487943a3
Bill Lorensen <bill.lorensen@gmail.com>
2017-06-25 11:42:03 -0400

    ENH: Introduce the new VTKExamples Remote module
    The WikiExamples remote module will now reference the new VTKExamples
    git repository. Users who have enabled the old WikiExamples remote
    module will need to remove the Remote/WikiExamples directory.
    
    Rather than create a new remote, we use the existing remote module
    name to discourage users from accessing the old wiki
    examples. Although the old examples still exist, they will no longer
    be supported.

--- Remote/WikiExamples.remote.cmake
+++ Remote/WikiExamples.remote.cmake
@@ -6,14 +6,14 @@
 
 # If the environement var WikiExamplesTag exists, use it
 if (NOT DEFINED ENV{WikiExamplesTag})
-  # April 5, 2017
-  set(GIT_TAG 82917c9afd5982cfa15e4be561dd708feed28394)
+  # June 25, 2017
+  set(GIT_TAG ad4b196c719e511c7e90632f58272248a465c931)
 else()
   set(GIT_TAG $ENV{WikiExamplesTag})
 endif()
 
 vtk_fetch_module(WikiExamples
   "A collection of examples that illustrate how to use VTK."
-  GIT_REPOSITORY https://github.com/lorensen/VTKWikiExamples.git
+  GIT_REPOSITORY https://github.com/lorensen/VTKExamples.git
   GIT_TAG ${GIT_TAG}
   )
