d61bcfb8936d3ccd2cd8f63ec76206203494bfc4
Bill Lorensen <bill.lorensen@gmail.com>
2017-06-28 13:27:55 -0400

    ENH: Bump examples version

--- Remote/WikiExamples.remote.cmake
+++ Remote/WikiExamples.remote.cmake
@@ -6,8 +6,8 @@
 
 # If the environement var WikiExamplesTag exists, use it
 if (NOT DEFINED ENV{WikiExamplesTag})
-  # June 25, 2017
-  set(GIT_TAG ad4b196c719e511c7e90632f58272248a465c931)
+  # June 28, 2017
+  set(GIT_TAG be2938dade7b6676e449db075632b53472ccd8a8)
 else()
   set(GIT_TAG $ENV{WikiExamplesTag})
 endif()
