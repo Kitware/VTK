5e139b3099a694e9b66674ff0a500b7d842e650a
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2017-07-24 21:39:32 -0400

    Renable charts example and update them to use Qt5.

--- Examples/Charts/Cxx/QScalarsToColors.cxx
+++ Examples/Charts/Cxx/QScalarsToColors.cxx
@@ -13,22 +13,21 @@
 
 =========================================================================*/
 
-#include "vtkFloatArray.h"
-#include "vtkMath.h"
-#include "vtkRenderer.h"
-#include "vtkRenderWindow.h"
-#include "vtkRenderWindowInteractor.h"
-
-#include "vtkSmartPointer.h"
-
-#include "vtkColorTransferFunction.h"
-#include "vtkPiecewiseFunction.h"
-#include "vtkCompositeTransferFunctionItem.h"
-#include "vtkContextView.h"
-#include "vtkContextScene.h"
+#include "QVTKOpenGLWidget.h"
 #include "vtkChartXY.h"
+#include "vtkColorTransferFunction.h"
+#include "vtkCompositeTransferFunctionItem.h"
+#include "vtkContextScene.h"
+#include "vtkContextView.h"
+#include "vtkFloatArray.h"
+#include "vtkGenericOpenGLRenderWindow.h"
+#include "vtkMath.h"
+#include "vtkNew.h"
+#include "vtkPiecewiseFunction.h"
 #include "vtkPlot.h"
-
+#include "vtkQtTableView.h"
+#include "vtkRenderer.h"
+#include "vtkRenderWindowInteractor.h"
 #include "vtkTimerLog.h"
 
 #include <QApplication>
@@ -36,61 +35,50 @@
 #include <QMainWindow>
 #include <QHBoxLayout>
 
-#include "QVTKWidget.h"
-#include "vtkQtTableView.h"
-
-#define VTK_CREATE(type, name) \
-  vtkSmartPointer<type> name = vtkSmartPointer<type>::New()
 
 //----------------------------------------------------------------------------
 int main( int argc, char * argv [] )
 {
+  // needed to ensure appropriate OpenGL context is created for VTK rendering.
+  QSurfaceFormat::setDefaultFormat(QVTKOpenGLWidget::defaultFormat());
+
   // Qt initialization
   QApplication app(argc, argv);
 
   // QVTK set up and initialization
-  QVTKWidget qvtkWidget(0);
+  QVTKOpenGLWidget qvtkWidget(nullptr);
+
+  vtkNew<vtkGenericOpenGLRenderWindow> renderWindow;
+  qvtkWidget.SetRenderWindow(renderWindow);
 
   // Set up my 2D world...
-  VTK_CREATE(vtkContextView, view); // This contains a chart object
+  vtkNew<vtkContextView> view;; // This contains a chart object
+  view->SetRenderWindow(qvtkWidget.GetRenderWindow());
   view->SetInteractor(qvtkWidget.GetInteractor());
-  qvtkWidget.SetRenderWindow(view->GetRenderWindow());
 
-  vtkSmartPointer<vtkChartXY> chart = vtkSmartPointer<vtkChartXY>::New();
+  vtkNew<vtkChartXY> chart;
   chart->SetTitle("Chart");
   view->GetScene()->AddItem(chart);
 
-  vtkSmartPointer<vtkColorTransferFunction> colorTransferFunction =
-    vtkSmartPointer<vtkColorTransferFunction>::New();
+  vtkNew<vtkColorTransferFunction> colorTransferFunction;
   colorTransferFunction->AddHSVSegment(0.,0.,1.,1.,0.3333,0.3333,1.,1.);
   colorTransferFunction->AddHSVSegment(0.3333,0.3333,1.,1.,0.6666,0.6666,1.,1.);
   colorTransferFunction->AddHSVSegment(0.6666,0.6666,1.,1.,1.,0.,1.,1.);
-
   colorTransferFunction->Build();
 
-  vtkSmartPointer<vtkPiecewiseFunction> opacityFunction =
-    vtkSmartPointer<vtkPiecewiseFunction>::New();
+  vtkNew<vtkPiecewiseFunction> opacityFunction;
   opacityFunction->AddPoint(0.,0.);
   opacityFunction->AddPoint(0.5,0.5);
   opacityFunction->AddPoint(1.,1.);
 
-  vtkSmartPointer<vtkCompositeTransferFunctionItem> item3 =
-    vtkSmartPointer<vtkCompositeTransferFunctionItem>::New();
+  vtkNew<vtkCompositeTransferFunctionItem> item3;
   item3->SetColorTransferFunction(colorTransferFunction);
   item3->SetOpacityFunction(opacityFunction);
   item3->SetOpacity(0.2);
   item3->SetMaskAboveCurve(true);
   chart->AddPlot(item3);
 
-  // Now lets try to add a table view
-  //QWidget *widget = new QWidget(mainWindow);
-  //QHBoxLayout *layout = new QHBoxLayout(widget);
-  //layout->addWidget(qvtkWidget);
-  //mainWindow->setCentralWidget(widget);
-
   // Now show the application and start the event loop
   qvtkWidget.show();
-  //view->GetRenderWindow()->SetMultiSamples(0);
-
   return app.exec();
 }
