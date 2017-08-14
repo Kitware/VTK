080165ea8bfa9837e2613c090020abd6d9f511c5
Dan Lipsa <dan.lipsa@kitware.com>
2017-08-14 10:20:18 -0400

    Replace ObjectIdMap trick for two way mapping.
    ObjectIdMap used a trick to simulate two way mapping. This trick
    failed if one inserted an object object2, followed by an object with
    smaller address object3 followed by the same object object3.
    
    The id returned for object3 on the second insertion would be
    different than the id on the first insertion.
    
    Test ObjectIdMap rather than vtkWebApplication::GetObjectId

--- Web/Core/Testing/Python/TestObjectId.py
+++ Web/Core/Testing/Python/TestObjectId.py
@@ -2,25 +2,37 @@ from vtkWebCorePython import vtkWebApplication
 import vtk
 from vtk.test import Testing
 
-
 class TestObjectId(Testing.vtkTest):
     def testObjId(self):
+        map = vtk.vtkObjectIdMap()
         # Just make sure if we call it twice with None, the results match
-        objId1 = vtkWebApplication.GetObjectId(None)
-        objId1b = vtkWebApplication.GetObjectId(None)
+        objId1 = map.GetGlobalId(None)
+        objId1b = map.GetGlobalId(None)
         print('Object ids for None: objId1 => ',objId1,', objId1b => ',objId1b)
         self.assertTrue(objId1 == objId1b)
 
-        polyData = vtk.vtkPolyData()
-        objId2 = vtkWebApplication.GetObjectId(polyData)
-        objId2b = vtkWebApplication.GetObjectId(polyData)
-        print('Object ids for polydata: objId2 => ',objId2,', objId2b => ',objId2b)
+        object2 = vtk.vtkObject()
+        addr2 = object2.__this__
+        addr2 = addr2[1:addr2.find('_', 1)]
+        addr2 = int(addr2, 16)
+
+        object3 = vtk.vtkObject()
+        addr3 = object3.__this__
+        addr3 = addr3[1:addr3.find('_', 1)]
+        addr3 = int(addr3, 16)
+
+        # insert the bigger address first
+        if (addr2 < addr3):
+            object2, object3 = object3, object2
+
+        objId2 = map.GetGlobalId(object2)
+        objId2b = map.GetGlobalId(object2)
+        print('Object ids for object2: objId2 => ',objId2,', objId2b => ',objId2b)
         self.assertTrue(objId2 == objId2b)
 
-        points = polyData.GetPoints()
-        objId3 = vtkWebApplication.GetObjectId(points)
-        objId3b = vtkWebApplication.GetObjectId(points)
-        print('Object ids for polydata points: objId3 => ',objId3,', objId3b => ',objId3b)
+        objId3 = map.GetGlobalId(object3)
+        objId3b = map.GetGlobalId(object3)
+        print('Object ids for object3: objId3 => ',objId3,', objId3b => ',objId3b)
         self.assertTrue(objId3 == objId3b)
 
 if __name__ == "__main__":
