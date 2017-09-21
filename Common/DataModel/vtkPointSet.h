00af54766bde5ebab4cd7d559425151a75fbe4db
Boris Basic <baljci@hotmail.com>
2017-09-21 17:32:14 +0200

    Fix vktPointSet comment

--- Common/DataModel/vtkPointSet.h
+++ Common/DataModel/vtkPointSet.h
@@ -19,7 +19,7 @@
  * vtkPointSet is an abstract class that specifies the interface for
  * datasets that explicitly use "point" arrays to represent geometry.
  * For example, vtkPolyData and vtkUnstructuredGrid require point arrays
- * to specify point position, while vtkStructuredPoints generates point
+ * to specify point position, while vtkStructuredGrid generates point
  * positions implicitly.
  *
  * @sa
