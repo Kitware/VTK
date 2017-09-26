5b1b3139a2d5fe55b21bc2215f942ab56bb3c01b
Bill Lorensen <bill.lorensen@gmail.com>
2017-09-26 07:56:58 -0400

    EN: Bump PoissonReconstructiom remote module

--- Remote/PoissonReconstruction.remote.cmake
+++ Remote/PoissonReconstruction.remote.cmake
@@ -6,5 +6,5 @@ vtk_fetch_module(PoissonReconstruction
   "Poisson Surface reconstruction from unorganized points"
   GIT_REPOSITORY https://github.com/lorensen/PoissonReconstruction
   # September 7, 2017 - refactored remote module
-  GIT_TAG 137d751c218ffbd946e9b9de6c108666af54c2d3
+  GIT_TAG ffaa97c271b8dfb91d7742dd3c05d7377d864afe
   )
