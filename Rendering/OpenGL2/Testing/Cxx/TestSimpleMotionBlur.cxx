0c2608481c98d1e4517bcf52b15e604dcbd33a6f
Ken Martin <ken.martin@kitware.com>
2017-10-02 10:49:09 -0400

    add simple motion blur pass
    A simple replacement for the old SubFrames options

---
+++ Rendering/OpenGL2/Testing/Cxx/TestSimpleMotionBlur.cxx
@@ -0,0 +1,129 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// The command line arguments are:
+// -I        => run in interactive mode; unless this is used, the program will
+//              not allow interaction and exit
+
+#include "vtkActor.h"
+#include "vtkCamera.h"
+#include "vtkSimpleMotionBlurPass.h"
+#include "vtkNew.h"
+#include "vtkOpenGLRenderer.h"
+#include "vtkPLYReader.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkProperty.h"
+#include "vtkRegressionTestImage.h"
+#include "vtkRenderStepsPass.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkRenderer.h"
+#include "vtkTestUtilities.h"
+#include "vtkTextureObject.h"
+#include "vtk_glew.h"
+
+//----------------------------------------------------------------------------
+int TestSimpleMotionBlur(int argc, char *argv[])
+{
+  vtkNew<vtkRenderer> renderer;
+  renderer->SetBackground(0.3, 0.4, 0.6);
+  vtkNew<vtkRenderWindow> renderWindow;
+  renderWindow->SetSize(500, 500);
+  renderWindow->AddRenderer(renderer);
+  vtkNew<vtkRenderWindowInteractor>  iren;
+  iren->SetRenderWindow(renderWindow);
+
+  const char* fileName =
+    vtkTestUtilities::ExpandDataFileName(argc, argv, "Data/dragon.ply");
+  vtkNew<vtkPLYReader> reader;
+  reader->SetFileName(fileName);
+  reader->Update();
+
+  vtkNew<vtkPolyDataMapper> mapper;
+  mapper->SetInputConnection(reader->GetOutputPort());
+
+  // create three dragons
+  {
+  vtkNew<vtkActor> actor;
+  actor->SetMapper(mapper);
+  actor->GetProperty()->SetAmbientColor(1.0, 0.0, 0.0);
+  actor->GetProperty()->SetDiffuseColor(1.0, 0.8, 0.3);
+  actor->GetProperty()->SetSpecular(0.0);
+  actor->GetProperty()->SetDiffuse(0.5);
+  actor->GetProperty()->SetAmbient(0.3);
+  actor->SetPosition(-0.1, 0.0, -0.1);
+  renderer->AddActor(actor);
+  }
+
+  {
+  vtkNew<vtkActor> actor;
+  actor->SetMapper(mapper);
+  actor->GetProperty()->SetAmbientColor(0.2, 0.2, 1.0);
+  actor->GetProperty()->SetDiffuseColor(0.2, 1.0, 0.8);
+  actor->GetProperty()->SetSpecularColor(1.0, 1.0, 1.0);
+  actor->GetProperty()->SetSpecular(0.2);
+  actor->GetProperty()->SetDiffuse(0.9);
+  actor->GetProperty()->SetAmbient(0.1);
+  actor->GetProperty()->SetSpecularPower(10.0);
+  renderer->AddActor(actor);
+  }
+
+  {
+  vtkNew<vtkActor> actor;
+  actor->SetMapper(mapper);
+  actor->GetProperty()->SetDiffuseColor(0.5, 0.65, 1.0);
+  actor->GetProperty()->SetSpecularColor(1.0, 1.0, 1.0);
+  actor->GetProperty()->SetSpecular(0.7);
+  actor->GetProperty()->SetDiffuse(0.4);
+  actor->GetProperty()->SetSpecularPower(60.0);
+  actor->SetPosition(0.1, 0.0, 0.1);
+  renderer->AddActor(actor);
+  }
+
+  renderWindow->SetMultiSamples(0);
+
+  // create the basic VTK render steps
+  vtkNew<vtkRenderStepsPass> basicPasses;
+
+  vtkNew<vtkSimpleMotionBlurPass> motion;
+  motion->SetDelegatePass(basicPasses);
+
+  // tell the renderer to use our render pass pipeline
+  vtkOpenGLRenderer *glrenderer =
+    vtkOpenGLRenderer::SafeDownCast(renderer);
+  glrenderer->SetPass(motion);
+
+  int numRenders = 30;
+  renderer->GetActiveCamera()->SetPosition(0,0,1);
+  renderer->GetActiveCamera()->SetFocalPoint(0,0,0);
+  renderer->GetActiveCamera()->SetViewUp(0,1,0);
+  renderer->ResetCamera();
+  renderer->GetActiveCamera()->Azimuth(15.0);
+  renderer->GetActiveCamera()->Zoom(1.8);
+  renderWindow->Render();
+
+  for (int i = 0; i < numRenders; ++i)
+  {
+    renderer->GetActiveCamera()->Azimuth(10.0/numRenders);
+    renderer->GetActiveCamera()->Elevation(10.0/numRenders);
+    renderWindow->Render();
+  }
+
+  int retVal = vtkRegressionTestImageThreshold( renderWindow, 15 );
+  if ( retVal == vtkRegressionTester::DO_INTERACTOR)
+  {
+    iren->Start();
+  }
+
+  return !retVal;
+}
