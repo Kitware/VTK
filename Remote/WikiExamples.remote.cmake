836466a1aac1838cdfa216044c3c38d3fa0815a0
Bill Lorensen <bill.lorensen@gmail.com>
2017-10-03 15:52:23 -0700

    ENH: Introducing VTKBookExamples

--- Remote/WikiExamples.remote.cmake
+++ Remote/WikiExamples.remote.cmake
@@ -6,8 +6,8 @@
 
 # If the environement var WikiExamplesTag exists, use it
 if (NOT DEFINED ENV{WikiExamplesTag})
-  # September 30, 2017 New baselines
-  set(GIT_TAG 1d99830ee6983e5221f415f6ed296fa15d9f985e)
+  # October 3, 2017 VTKBookExamples alpha
+  set(GIT_TAG 1fac27078b72f25c68528e8287b36cb81256b1d3)
 else()
   set(GIT_TAG $ENV{WikiExamplesTag})
 endif()
