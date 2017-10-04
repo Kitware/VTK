a4ede27f6b7497579e1b4e6a47e7ea96e82f6d95
Bill Lorensen <bill.lorensen@gmail.com>
2017-10-03 22:34:23 -0400

    Update WikiExamples.remote.cmake

--- Remote/WikiExamples.remote.cmake
+++ Remote/WikiExamples.remote.cmake
@@ -7,7 +7,7 @@
 # If the environement var WikiExamplesTag exists, use it
 if (NOT DEFINED ENV{WikiExamplesTag})
   # October 3, 2017 VTKBookExamples alpha
-  set(GIT_TAG 1fac27078b72f25c68528e8287b36cb81256b1d3)
+  set(GIT_TAG 26fc7cbd9032453f9b792a26d3d8189e3c59aec3)
 else()
   set(GIT_TAG $ENV{WikiExamplesTag})
 endif()
