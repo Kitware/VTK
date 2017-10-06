4d0485d3183ffef309c3d69d56e22ab63f0ab024
David E. DeMarle <dave.demarle@kitware.com>
2017-10-06 09:58:49 -0400

    fix a crash when showing an empty scene
    Seemed to be just the pathtracer, but in reality it happened
    because of the switch from scivis to pathtracer with the empty
    scene. The other direction also crashed but wasn't as easy to do.

--- Rendering/OSPRay/vtkOSPRayRendererNode.cxx
+++ Rendering/OSPRay/vtkOSPRayRendererNode.cxx
@@ -808,6 +808,8 @@ void vtkOSPRayRendererNode::Traverse(int operation)
   else
   {
     oModel = (OSPModel)this->OModel;
+    ospSetObject(oRenderer,"model", oModel);
+    ospCommit(oRenderer);
   }
   it->Delete();
 
