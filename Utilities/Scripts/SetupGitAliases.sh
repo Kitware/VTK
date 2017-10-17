2e0912361958305b529983636d8b53b874d02bf2
Kenneth Moreland <kmorel@sandia.gov>
2017-10-17 13:08:30 -0600

    Add recursive flag to git pullall macro
    The pullall macro does a git submodule update. Add the --recursive
    flag so that submodules of submodules also get added.

--- Utilities/Scripts/SetupGitAliases.sh
+++ Utilities/Scripts/SetupGitAliases.sh
@@ -3,7 +3,7 @@
 echo "Setting up useful Git aliases..." &&
 
 # General aliases that could be global
-git config alias.pullall '!bash -c "git pull && git submodule update --init"' &&
+git config alias.pullall '!bash -c "git pull && git submodule update --init --recursive"' &&
 git config alias.prepush 'log --graph --stat origin/master..' &&
 
 # Staging aliases
