85f8fba286e0e6ebc42076693ddfbb24ae9ec80a
Ben Boeckel <ben.boeckel@kitware.com>
2017-11-13 14:16:56 -0500

    cmake: remove conditionals for Borland
    VTK doesn't support Borland anymore.

--- CMake/vtkThirdParty.cmake
+++ CMake/vtkThirdParty.cmake
@@ -9,12 +9,6 @@ macro(vtk_third_party_warning_suppress upper lang)
       set(CMAKE_${lang}_FLAGS "${CMAKE_${lang}_FLAGS} /W0")
     endif()
 
-    # Borland uses -w- to suppress warnings.
-    if(BORLAND)
-      set(${upper}_WARNINGS_BLOCKED 1)
-      set(CMAKE_${lang}_FLAGS "${CMAKE_${lang}_FLAGS} -w-")
-    endif()
-
     # Most compilers use -w to suppress warnings.
     if(NOT ${upper}_WARNINGS_BLOCKED)
       set(CMAKE_${lang}_FLAGS "${CMAKE_${lang}_FLAGS} -w")
