37382e96e0d8ded18d51cb3ae6d7cc1b989a8775
Alexis Girault <alexis.girault@kitware.com>
2017-11-16 18:11:42 -0500

    UI: Hide status bar in VTKViewer

--- Examples/iOS/VTKViewer/VTKViewer/AppDelegate.swift
+++ Examples/iOS/VTKViewer/VTKViewer/AppDelegate.swift
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Hide status bar for App
+        application.isStatusBarHidden = true
+
         return true
     }
 
