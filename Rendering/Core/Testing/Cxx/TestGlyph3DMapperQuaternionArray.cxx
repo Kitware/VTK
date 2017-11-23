d2754605e52c8f97278fde9ad16183f7d76dc8a3
Michael Migliore <michael.migliore@kitware.com>
2017-11-23 16:02:30 +0100

    Cosmetic changes

--- Rendering/Core/Testing/Cxx/TestGlyph3DMapperQuaternionArray.cxx
+++ Rendering/Core/Testing/Cxx/TestGlyph3DMapperQuaternionArray.cxx
@@ -12,39 +12,33 @@
 
 =========================================================================*/
 
-
-#include "vtkTestUtilities.h"
 #include "vtkRegressionTestImage.h"
 
-#include "vtkRenderWindowInteractor.h"
-#include "vtkRenderWindow.h"
-#include "vtkRenderer.h"
 #include "vtkActor.h"
 #include "vtkCamera.h"
-#include "vtkPlaneSource.h"
+#include "vtkConeSource.h"
 #include "vtkElevationFilter.h"
+#include "vtkFloatArray.h"
+#include "vtkGlyph3DMapper.h"
+#include "vtkInteractorStyleSwitch.h"
+#include "vtkPlaneSource.h"
+#include "vtkPointData.h"
 #include "vtkPolyDataMapper.h"
 #include "vtkProperty.h"
-#include "vtkSuperquadricSource.h"
-#include "vtkSphereSource.h"
-#include "vtkPolyDataReader.h"
-#include "vtkInteractorStyleSwitch.h"
-#include "vtkArrayCalculator.h"
-#include "vtkPointData.h"
-#include "vtkConeSource.h"
-#include "vtkGlyph3DMapper.h"
-#include "vtkFloatArray.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkRenderer.h"
 
-int TestGlyph3DMapperQuaternionArray(int argc, char *argv[])
+int TestGlyph3DMapperQuaternionArray(int argc, char* argv[])
 {
-  int res=30;
+  int res = 30;
   vtkNew<vtkPlaneSource> plane;
-  plane->SetResolution(res,res);
+  plane->SetResolution(res, res);
 
   vtkNew<vtkElevationFilter> colors;
   colors->SetInputConnection(plane->GetOutputPort());
-  colors->SetLowPoint(-0.25,-0.25,-0.25);
-  colors->SetHighPoint(0.25,0.25,0.25);
+  colors->SetLowPoint(-0.25, -0.25, -0.25);
+  colors->SetHighPoint(0.25, 0.25, 0.25);
   colors->Update();
 
   vtkNew<vtkPolyDataMapper> planeMapper;
@@ -71,10 +65,10 @@ int TestGlyph3DMapperQuaternionArray(int argc, char *argv[])
     float s = sin(0.5 * angle);
     float c = cos(0.5 * angle);
 
-    *quatPtr++ = c*c*c + s*s*s;
-    *quatPtr++ = s*c*c - c*s*s;
-    *quatPtr++ = c*s*c + s*c*s;
-    *quatPtr++ = c*c*s - s*s*c;
+    *quatPtr++ = c * c * c + s * s * s;
+    *quatPtr++ = s * c * c - c * s * s;
+    *quatPtr++ = c * s * c + s * c * s;
+    *quatPtr++ = c * c * s - s * s * c;
   }
 
   pointData->AddArray(quatData);
@@ -87,7 +81,7 @@ int TestGlyph3DMapperQuaternionArray(int argc, char *argv[])
   squad->SetHeight(10.0);
   squad->SetRadius(1.0);
   squad->SetResolution(50);
-  squad->SetDirection(0.0,0.0,1.0);
+  squad->SetDirection(0.0, 0.0, 1.0);
 
   vtkNew<vtkGlyph3DMapper> glypher;
   glypher->SetInputConnection(colors->GetOutputPort());
@@ -101,28 +95,29 @@ int TestGlyph3DMapperQuaternionArray(int argc, char *argv[])
 
   glyphActor->SetMapper(glypher);
 
-  //Create the rendering stuff
+  // Create the rendering stuff
 
   vtkNew<vtkRenderer> ren;
   vtkNew<vtkRenderWindow> win;
   win->SetMultiSamples(0); // make sure regression images are the same on all platforms
   win->AddRenderer(ren);
   vtkNew<vtkRenderWindowInteractor> iren;
-  vtkInteractorStyleSwitch::SafeDownCast(iren->GetInteractorStyle())->SetCurrentStyleToTrackballCamera();
+  vtkInteractorStyleSwitch::SafeDownCast(iren->GetInteractorStyle())
+    ->SetCurrentStyleToTrackballCamera();
 
   iren->SetRenderWindow(win);
 
   ren->AddActor(planeActor);
   ren->AddActor(glyphActor);
-  ren->SetBackground(0.5,0.5,0.5);
-  win->SetSize(450,450);
+  ren->SetBackground(0.5, 0.5, 0.5);
+  win->SetSize(450, 450);
   win->Render();
   ren->GetActiveCamera()->Zoom(1.5);
 
   win->Render();
 
   int retVal = vtkRegressionTestImage(win);
-  if ( retVal == vtkRegressionTester::DO_INTERACTOR)
+  if (retVal == vtkRegressionTester::DO_INTERACTOR)
   {
     iren->Start();
   }
