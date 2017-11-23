4da13e2e8e0d2313b4303f9e5e93bce0220fa07d
Bill Lorensen <bill.lorensen@gmail.com>
2017-11-23 08:35:13 -0500

    Update WikiExamples.remote.cmake

--- Remote/WikiExamples.remote.cmake
+++ Remote/WikiExamples.remote.cmake
@@ -7,7 +7,7 @@
 # If the environement var WikiExamplesTag exists, use it
 if (NOT DEFINED ENV{WikiExamplesTag})
   # November 15, 2017
-  set(GIT_TAG d0fd7393ac89fe4d27009b54fc0064ceb7640679)
+  set(GIT_TAG 31d0a40ce6816a96423b1ce69065a734f468ca9e)
 else()
   set(GIT_TAG $ENV{WikiExamplesTag})
 endif()
