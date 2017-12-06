f213a299aecf51363804d4e59c73d5618e3224e6
Ben Boeckel <ben.boeckel@kitware.com>
2017-12-06 10:49:01 -0500

    Rendering/Tk: fix for post-Tcl world
    Tk rendering is somewhat Tcl independent and was broken when Tcl support
    was removed. This puts back a minimum of support to make the module
    behave properly for Python's Tk support.

--- Rendering/Tk/vtkTkImageViewerWidget.cxx
+++ Rendering/Tk/vtkTkImageViewerWidget.cxx
@@ -443,7 +443,7 @@ extern "C"
 //----------------------------------------------------------------------------
 // vtkTkImageViewerWidget_Init
 // Called upon system startup to create vtkTkImageViewerWidget command.
-extern "C" {VTK_TK_EXPORT int Vtktkimageviewerwidget_Init(Tcl_Interp *interp);}
+extern "C" {VTK_EXPORT int Vtktkimageviewerwidget_Init(Tcl_Interp *interp);}
 
 #define VTKTK_TO_STRING(x) VTKTK_TO_STRING0(x)
 #define VTKTK_TO_STRING0(x) VTKTK_TO_STRING1(x)
