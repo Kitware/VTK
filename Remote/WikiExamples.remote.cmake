16f82e2233a1fc1bfced126e06f40cdbda6936ce
Bill Lorensen <bill.lorensen@gmail.com>
2017-12-08 07:03:32 -0500

    Update WikiExamples.remote.cmake

--- Remote/WikiExamples.remote.cmake
+++ Remote/WikiExamples.remote.cmake
@@ -7,7 +7,7 @@
 # If the environement var WikiExamplesTag exists, use it
 if (NOT DEFINED ENV{WikiExamplesTag})
   # December 8, 2017 Address VTK api changes
-  set(GIT_TAG b34a7a8ef937ac6ff2b6b916a067802ffa23c738)
+  set(GIT_TAG afd60adb939267bcb5d02c4af629750c1d0e8e5a)
 else()
   set(GIT_TAG $ENV{WikiExamplesTag})
 endif()
