e4d5c1612eca6449b2d8af7785cd8b77b4045530
Ben Boeckel <ben.boeckel@kitware.com>
2017-12-28 09:49:14 -0500

    jsoncpp: update to the new module system

--- ThirdParty/jsoncpp/update.sh
+++ ThirdParty/jsoncpp/update.sh
@@ -8,7 +8,7 @@ readonly name="jsoncpp"
 readonly ownership="JsonCpp Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/jsoncpp.git"
-readonly tag="for/vtk-old"
+readonly tag="for/vtk-20181227-1.8.4"
 readonly paths="
 .gitattributes
 CMakeLists.vtk.txt
@@ -17,7 +17,7 @@ LICENSE
 " # We amalgamate jsoncpp
 
 extract_source () {
-    python amalgamate.py
+    python2 amalgamate.py
     [ -n "$paths" ] && \
         mv -v $paths "dist"
     mv "json/vtkjsoncpp_config.h.in" "dist/json"
