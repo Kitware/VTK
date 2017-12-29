127448ac3015f6ab13c44c62444444167086814d
Ben Boeckel <ben.boeckel@kitware.com>
2017-12-29 08:41:09 -0500

    zlib: update to the new module system

--- ThirdParty/zlib/update.sh
+++ ThirdParty/zlib/update.sh
@@ -8,7 +8,7 @@ readonly name="zlib"
 readonly ownership="Zlib Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/zlib.git"
-readonly tag="for/vtk-old"
+readonly tag="for/vtk-20181015-1.2.11"
 readonly paths="
 CMakeLists.txt
 
