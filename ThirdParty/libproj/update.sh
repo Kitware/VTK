395066fb2387c564981c0e917b49dd10dbe14b81
Ben Boeckel <ben.boeckel@kitware.com>
2018-01-05 14:50:58 -0500

    libproj: update the import source listing

--- ThirdParty/libproj/update.sh
+++ ThirdParty/libproj/update.sh
@@ -10,21 +10,45 @@ readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/proj.git"
 readonly tag="for/vtk"
 readonly paths="
-AUTHORS
-ChangeLog
-cmake/*.cmake
-cmake/*.cmake.in
-CMakeLists.txt
-COPYING
-src/CMakeLists.txt
-src/proj.def
-src/*.c
-src/*.cmake
-src/*.h
-NEWS
-README
 .gitattributes
-.gitignore
+ChangeLog
+COPYING
+CMakeLists.txt
+README
+README.kitware.md
+
+cmake/Proj4Config.cmake
+cmake/Proj4SystemInfo.cmake
+cmake/Proj4Test.cmake
+cmake/Proj4Utilities.cmake
+cmake/Proj4Version.cmake
+cmake/proj_config.cmake.in
+
+src/CMakeLists.txt
+src/lib_proj.cmake
+src/PJ_*.c
+src/aasincos.c
+src/adjlon.c
+src/bch2bps.c
+src/bchgen.c
+src/biveval.c
+src/dmstor.c
+src/emess.c
+src/emess.h
+src/geocent.c
+src/geocent.h
+src/geodesic.c
+src/mk_cheby.c
+src/nad_*.c
+src/pj_*.c
+src/pj_*.h
+src/proj_*.c
+src/rtodms.c
+src/vector1.c
+src/projects.h
+src/proj_api.h
+src/geodesic.h
+src/vtk_libproj_mangle.h
 "
 
 extract_source () {
