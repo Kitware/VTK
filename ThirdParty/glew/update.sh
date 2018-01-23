48dbbe2d7e786ac51b17a3a02cf1f1db9ff77ed7
Shawn Waldon <shawn.waldon@kitware.com>
2018-01-23 16:38:40 -0500

    glew: change branch to import so current module system is still used

--- ThirdParty/glew/update.sh
+++ ThirdParty/glew/update.sh
@@ -8,7 +8,7 @@ readonly name="glew"
 readonly ownership="glew Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/glew.git"
-readonly tag="for/vtk"
+readonly tag="for/vtk-old"
 readonly paths="
 auto
 config/version
