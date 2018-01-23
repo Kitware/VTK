b12f5cea5a30648edc185471073d4fb69ce23141
Shawn Waldon <shawn.waldon@kitware.com>
2018-01-23 14:36:50 -0500

    png: change branch to import so current module system is still used

--- ThirdParty/png/update.sh
+++ ThirdParty/png/update.sh
@@ -8,7 +8,7 @@ readonly name="png"
 readonly ownership="Libpng Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/png.git"
-readonly tag="for/vtk"
+readonly tag="for/vtk-old"
 readonly paths="
 .gitattributes
 CMakeLists.vtk.txt
