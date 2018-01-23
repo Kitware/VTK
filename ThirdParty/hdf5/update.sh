ec244e649e194f1fbe85abb3c80d2072b80397ee
Shawn Waldon <shawn.waldon@kitware.com>
2018-01-23 11:51:36 -0500

    hdf5: change branch to import so current module system is still used

--- ThirdParty/hdf5/update.sh
+++ ThirdParty/hdf5/update.sh
@@ -8,7 +8,7 @@ readonly name="hdf5"
 readonly ownership="HDF Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/hdf5.git"
-readonly tag="for/vtk"
+readonly tag="for/vtk-old"
 readonly paths="
 CMakeFilters.cmake
 CMakeInstallation.cmake
