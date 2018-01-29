2960d26834c264ee721120f251734d42f76a2f85
Will Schroeder <will.schroeder@kitware.com>
2018-01-29 15:04:55 -0500

    Add maximum error; handle closed loops
    A maximum error value can be set. Also, the filter crashed on
    closed loops.

--- Filters/Core/vtkDecimatePolylineFilter.h
+++ Filters/Core/vtkDecimatePolylineFilter.h
@@ -21,7 +21,14 @@
  * vertex (i.e., the distance of the vertex to a line defined from the two
  * vertices on either side of the vertex). Then, these vertices are placed
  * into a priority queue, and those with larger errors are deleted first.
- * The decimation continues until the target reduction is reached.
+ * The decimation continues until the target reduction is reached. While the
+ * filter will not delete end points, it will decimate closed loops down to a
+ * single line, thereby changing topology.
+ *
+ * Note that a maximum error value (expressed in world coordinates) can also
+ * be specified. This may limit the amount of decimation so the target
+ * reduction may not be met. By setting the maximum error value to a very
+ * small number, colinear points can be eliminated.
  *
  * @warning
  * This algorithm is a very simple implementation that overlooks some
@@ -73,6 +80,17 @@ public:
 
   //@{
   /**
+   * Set the largest decimation error that is allowed during the decimation
+   * process. This may limit the maximum reduction that may be achieved. The
+   * maximum error is specified as a fraction of the maximum length of
+   * the input data bounding box.
+   */
+  vtkSetClampMacro(MaximumError,double,0.0,VTK_DOUBLE_MAX);
+  vtkGetMacro(MaximumError,double);
+  //@}
+
+  //@{
+  /**
    * Set/get the desired precision for the output types. See the documentation
    * for the vtkAlgorithm::DesiredOutputPrecision enum for an explanation of
    * the available precision settings.
@@ -88,12 +106,11 @@ protected:
   int RequestData(vtkInformation *, vtkInformationVector **, vtkInformationVector *) override;
 
   class Polyline;
-  double ComputeError( vtkPolyData* input,
-                       Polyline* polyline,
-                       vtkIdType id );
+  double ComputeError( vtkPolyData* input, Polyline* polyline, vtkIdType id );
 
   vtkSmartPointer< vtkPriorityQueue >   PriorityQueue;
   double                                TargetReduction;
+  double                                MaximumError;
   int                                   OutputPointsPrecision;
 
 private:
@@ -102,5 +119,3 @@ private:
 };
 
 #endif
-
-
