8d61ac62bfb3c532d1d3506d5f7fd495e49c4295
Ben Boeckel <ben.boeckel@kitware.com>
2018-01-30 13:19:23 -0500

    cmake: remove unused CMake modules

--- CMake/FindPackageHandleStandardArgs.cmake
+++
@@ -1,7 +0,0 @@
-
-# include the latest version of FindPackageHandleStandardArgs.
-if(CMAKE_VERSION VERSION_LESS 3.6.0)
-  include(${CMAKE_CURRENT_LIST_DIR}/NewCMake/FindPackageHandleStandardArgs.cmake)
-else()
-  include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
-endif()
