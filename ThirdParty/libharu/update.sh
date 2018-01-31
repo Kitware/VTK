24f701ad3fcb5cc35d23b22298775589b164a3b7
Shawn Waldon <shawn.waldon@kitware.com>
2018-01-31 11:21:58 -0500

    libharu: change branch to import so old module system is still used

--- ThirdParty/libharu/update.sh
+++ ThirdParty/libharu/update.sh
@@ -8,7 +8,7 @@ readonly name="libharu"
 readonly ownership="Libharu Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/libharu.git"
-readonly tag="for/vtk"
+readonly tag="for/vtk-old"
 readonly paths="
 .gitattributes
 CMakeLists.txt
