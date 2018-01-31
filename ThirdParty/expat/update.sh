da8b4a4d3f228363e071f760944e8093788925c2
Shawn Waldon <shawn.waldon@kitware.com>
2018-01-31 14:45:24 -0500

    expat: change branch to import so old module system is still used

--- ThirdParty/expat/update.sh
+++ ThirdParty/expat/update.sh
@@ -8,7 +8,7 @@ readonly name="expat"
 readonly ownership="Expat Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/expat.git"
-readonly tag="for/vtk"
+readonly tag="for/vtk-old"
 readonly paths="
 expat/.gitattributes
 expat/CMakeLists.txt
