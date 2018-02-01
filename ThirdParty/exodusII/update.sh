8b64943b94fc40e68065272216316422ccd2b8a1
Shawn Waldon <shawn.waldon@kitware.com>
2018-02-01 14:18:52 -0500

    exodusII: change branch to import so old module system is still used

--- ThirdParty/exodusII/update.sh
+++ ThirdParty/exodusII/update.sh
@@ -8,7 +8,7 @@ readonly name="exodusII"
 readonly ownership="Seacas Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/seacas.git"
-readonly tag="for/vtk"
+readonly tag="for/vtk-old"
 readonly paths="
 packages/seacas/libraries/exodus/CMakeLists.vtk.txt
 packages/seacas/libraries/exodus/cmake/exodus_config.h.in
