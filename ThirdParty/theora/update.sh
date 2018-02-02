30e840f700644e375fa2276889aeb7766976747d
Shawn Waldon <shawn.waldon@kitware.com>
2018-02-02 14:31:56 -0500

    theora: change branch to import so old module system is still used

--- ThirdParty/theora/update.sh
+++ ThirdParty/theora/update.sh
@@ -8,7 +8,7 @@ readonly name="theora"
 readonly ownership="theora Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/theora.git"
-readonly tag="for/vtk"
+readonly tag="for/vtk-old"
 readonly paths="
 .gitattributes
 COPYING
