3d50f57bc3c1d0bd85ee6ae2a077d46b1b9a893f
Shawn Waldon <shawn.waldon@kitware.com>
2018-02-08 16:37:16 -0500

    libproj: change branch to import so old module system is still used

--- ThirdParty/libproj/update.sh
+++ ThirdParty/libproj/update.sh
@@ -8,7 +8,7 @@ readonly name="libproj"
 readonly ownership="Proj Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/proj.git"
-readonly tag="for/vtk"
+readonly tag="for/vtk-old"
 readonly paths="
 .gitattributes
 ChangeLog
