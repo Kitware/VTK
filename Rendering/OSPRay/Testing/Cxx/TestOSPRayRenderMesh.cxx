8892f18b34235550c342f61f9de1fa397db0e400
David E. DeMarle <dave.demarle@kitware.com>
2018-02-27 12:16:37 -0500

    include texture which was until recently exposed by polydatamapper
    now that mapper is cleaned up, we must include it

--- Rendering/OSPRay/Testing/Cxx/TestOSPRayRenderMesh.cxx
+++ Rendering/OSPRay/Testing/Cxx/TestOSPRayRenderMesh.cxx
@@ -53,6 +53,7 @@
 #include "vtkSmartPointer.h"
 #include "vtkSphereSource.h"
 #include "vtkStripper.h"
+#include "vtkTexture.h"
 #include "vtkTextureMapToSphere.h"
 #include "vtkTransformTextureCoords.h"
 #include "vtkUnsignedCharArray.h"
