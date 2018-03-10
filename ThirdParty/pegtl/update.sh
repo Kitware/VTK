427e9b7f2cf627f9935759a8dedd843bc08d62a3
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2018-03-10 08:46:39 -0500

    add update.sh and other files for pegtl 3rd party module.

---
+++ ThirdParty/pegtl/update.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+shopt -s dotglob
+
+readonly name="pegtl"
+readonly ownership="PEGTL Upstream <kwrobot@kitware.com>"
+readonly subtree="ThirdParty/$name/vtk$name"
+readonly repo="https://gitlab.kitware.com/third-party/pegtl.git"
+readonly tag="for/vtk"
+readonly paths="
+.gitattributes
+CMakeLists.vtk.txt
+include/
+LICENSE
+README.md
+README.kitware.md
+"
+
+extract_source () {
+    git_archive
+    pushd "$extractdir/$name-reduced"
+    mv -v CMakeLists.vtk.txt CMakeLists.txt
+    popd
+}
+
+. "${BASH_SOURCE%/*}/../update-common.sh"
