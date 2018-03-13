a62f1b8ff9835a1c8a44591c353f9c121f4a16d4
HDF Upstream <kwrobot@kitware.com>
2018-03-13 16:37:42 -0400

    hdf5 2018-03-13 (adc31b21)
    Code extracted from:
    
        https://gitlab.kitware.com/third-party/hdf5.git
    
    at commit adc31b21b067c3ea398f18d99f0196e382215e46 (for/vtk-old).

--- CMakeInstallation.cmake
+++ CMakeInstallation.cmake
@@ -286,6 +286,7 @@ if (NOT HDF5_EXTERNALLY_CONFIGURED)
   endif ()
 endif ()
 
+if (FALSE)
 if (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
   if (CMAKE_HOST_UNIX)
     set (CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}/HDF_Group/${HDF5_PACKAGE_NAME}/${HDF5_PACKAGE_VERSION}"
@@ -297,6 +298,7 @@ if (CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
       CACHE PATH "Install path prefix, prepended onto install directories." FORCE)
     set (CMAKE_GENERIC_PROGRAM_FILES)
   endif ()
+endif ()
 endif ()
 
 #-----------------------------------------------------------------------------
