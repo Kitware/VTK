081ee5fa5c325121b0aa23381237e30cc02f7b8d
Will Schroeder <will.schroeder@kitware.com>
2018-03-21 15:22:34 -0400

    Name normals array to support applications
    The output normals array was not named making it hard to access in
    ParaView and other applications.

--- Filters/Points/vtkPCANormalEstimation.cxx
+++ Filters/Points/vtkPCANormalEstimation.cxx
@@ -238,6 +238,7 @@ int vtkPCANormalEstimation::RequestData(
   vtkFloatArray *normals = vtkFloatArray::New();
   normals->SetNumberOfComponents(3);
   normals->SetNumberOfTuples(numPts);
+  normals->SetName("PCANormals");
   float *n = static_cast<float*>(normals->GetVoidPointer(0));
 
   void *inPtr = input->GetPoints()->GetVoidPointer(0);
