631e2ad77c5213a8bef54356d5e90758b0509e90
Kyle Edwards <kyle.edwards@kitware.com>
2018-03-21 14:39:46 -0400

    Test GL2PS version
    VTK uses GL2PS features from 1.4.0 and later, but the version wasn't
    being checked properly. If building with VTK_USE_SYSTEM_GL2PS and
    GL2PS is < 1.4.0, configuring succeeds but building fails. This patch
    checks the GL2PS version at configure time.
    
    FindGL2PS.cmake has also been lowercased.

--- CMake/FindGL2PS.cmake
+++ CMake/FindGL2PS.cmake
@@ -21,17 +21,33 @@
 # (To distributed this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
-FIND_PATH(GL2PS_INCLUDE_DIR gl2ps.h)
+find_path(GL2PS_INCLUDE_DIR gl2ps.h)
 
-FIND_LIBRARY(GL2PS_LIBRARY NAMES gl2ps )
+find_library(GL2PS_LIBRARY NAMES gl2ps)
+
+if(GL2PS_INCLUDE_DIR)
+  file(STRINGS "${GL2PS_INCLUDE_DIR}/gl2ps.h" _GL2PS_H_CONTENTS REGEX "#define[ \t]+GL2PS_(MAJOR|MINOR|PATCH)_VERSION[ \t]+")
+  foreach(v MAJOR MINOR PATCH)
+    if("${_GL2PS_H_CONTENTS}" MATCHES "#define[ \t]+GL2PS_${v}_VERSION[ \t]+([0-9]+)")
+      set(GL2PS_${v}_VERSION "${CMAKE_MATCH_1}")
+    else()
+      set(GL2PS_${v}_VERSION 0)
+    endif()
+  endforeach()
+  unset(_GL2PS_H_CONTENTS)
+  set(GL2PS_VERSION "${GL2PS_MAJOR_VERSION}.${GL2PS_MINOR_VERSION}.${GL2PS_PATCH_VERSION}")
+endif()
 
 # handle the QUIETLY and REQUIRED arguments and set GL2PS_FOUND to TRUE if
 # all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(GL2PS  DEFAULT_MSG  GL2PS_LIBRARY  GL2PS_INCLUDE_DIR)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GL2PS
+  REQUIRED_VARS GL2PS_LIBRARY GL2PS_INCLUDE_DIR
+  VERSION_VAR GL2PS_VERSION
+  )
 
-IF(GL2PS_FOUND)
-  SET( GL2PS_LIBRARIES ${GL2PS_LIBRARY} )
-ENDIF()
+if(GL2PS_FOUND)
+  set( GL2PS_LIBRARIES ${GL2PS_LIBRARY} )
+endif()
 
-MARK_AS_ADVANCED(GL2PS_INCLUDE_DIR GL2PS_LIBRARY)
+mark_as_advanced(GL2PS_INCLUDE_DIR GL2PS_LIBRARY)
