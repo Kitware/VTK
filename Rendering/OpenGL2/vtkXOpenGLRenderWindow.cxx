61694e1f74d6d7cb22100cea6f3a7928c6193dfd
Francois Budin <francois.budin@kitware.com>
2018-03-21 09:51:02 -0400

    Check that context exists before trying to pop context.
    On some linux drivers (such as nvidia version 384.111 and 387.34) setting the
    context to zero causes a segfault so check before setting in cases where the
    destruction may be trying to pop to a zero context.
    
    (cherry picked from commit a7988f5c530d6db31d6d9a774049c5247a9ac80c)

--- Rendering/OpenGL2/vtkXOpenGLRenderWindow.cxx
+++ Rendering/OpenGL2/vtkXOpenGLRenderWindow.cxx
@@ -1151,7 +1151,7 @@ void vtkXOpenGLRenderWindow::PopContext()
   GLXContext current = glXGetCurrentContext();
   GLXContext target = static_cast<GLXContext>(this->ContextStack.top());
   this->ContextStack.pop();
-  if (target != current)
+  if (target && target != current)
   {
     glXMakeCurrent(this->DisplayStack.top(),
       this->DrawableStack.top(),
