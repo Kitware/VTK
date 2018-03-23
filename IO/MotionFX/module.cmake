4add63946763d0a03d5605529506b4b01ba49676
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2018-03-23 12:47:14 -0400

    Make `vtkIOMotionFX` non-StandAlone.
    Since vtkpegtl requires VS2015, we'll make this module non-default till
    VS2013 support is dropped.

--- IO/MotionFX/module.cmake
+++ IO/MotionFX/module.cmake
@@ -1,6 +1,9 @@
 vtk_module(vtkIOMotionFX
-  GROUPS
-    StandAlone
+# leaving this out of StandAlone for now since
+# vtkpegtl doesn't support VS2013. Once we drop VS2013 we can make this
+# module a StandAlone module.
+#  GROUPS
+#  StandAlone
   DEPENDS
     vtkCommonExecutionModel
   PRIVATE_DEPENDS
