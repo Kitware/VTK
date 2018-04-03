521898a2b5969bdb930ed8d4bc2fdc9631473383
Nicolas Vuaille <nicolas.vuaille@kitware.com>
2018-04-03 14:25:56 +0200

    Molecule to polydata conversion
     * introduce vtkMoleculeToLinesFilter
     ** Create polydata with atom == point, bond == line cell. Copy data.
    
     * Fix AtomBall and BondStick conversion:
     ** set array name for bond orders and atomic numbers
     ** fix allocated size

---
+++ Domains/Chemistry/vtkMoleculeToLinesFilter.cxx
@@ -0,0 +1,50 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkMoleculeToLinesFilter.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkMoleculeToLinesFilter.h"
+
+#include "vtkCellArray.h"
+#include "vtkCellData.h"
+#include "vtkInformation.h"
+#include "vtkMolecule.h"
+#include "vtkPointData.h"
+
+vtkStandardNewMacro(vtkMoleculeToLinesFilter);
+
+//----------------------------------------------------------------------------
+int vtkMoleculeToLinesFilter::RequestData(vtkInformation*,
+  vtkInformationVector** inputVector,
+  vtkInformationVector* outputVector)
+{
+  vtkMolecule* input = vtkMolecule::SafeDownCast(vtkDataObject::GetData(inputVector[0]));
+  vtkPolyData* output = vtkPolyData::SafeDownCast(vtkDataObject::GetData(outputVector));
+
+  vtkNew<vtkCellArray> bonds;
+  // 2 point ids + 1 VTKCellType = 3 values per bonds
+  bonds->Allocate(3 * input->GetNumberOfBonds());
+
+  for (vtkIdType bondInd = 0; bondInd < input->GetNumberOfBonds(); ++bondInd)
+  {
+    vtkBond bond = input->GetBond(bondInd);
+    vtkIdType ids[2] = { bond.GetBeginAtomId(), bond.GetEndAtomId() };
+    bonds->InsertNextCell(2, ids);
+  }
+
+  output->SetPoints(input->GetAtomicPositionArray());
+  output->SetLines(bonds);
+  output->GetPointData()->DeepCopy(input->GetAtomData());
+  output->GetCellData()->DeepCopy(input->GetBondData());
+
+  return 1;
+}
