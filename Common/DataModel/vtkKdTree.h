960f7875db0e0c584be962c359242ca60c2ff66a
luz.paz <luzpaz@users.noreply.github.com>
2018-04-24 14:18:25 -0400

    Removed duplicate line
    The line removed is actually a typo. See line 875.

--- Common/DataModel/vtkKdTree.h
+++ Common/DataModel/vtkKdTree.h
@@ -847,7 +847,6 @@ protected:
   static int __ConvexSubRegions(int *ids, int len, vtkKdNode *tree, vtkKdNode **nodes);
   static int FoundId(vtkIntArray *idArray, int id);
 
-  void NewParitioningRequest(int req);
   void SetInputDataInfo(int i,
        int dims[3], double origin[3], double spacing[3]);
   int CheckInputDataInfo(int i,
