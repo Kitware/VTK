6f5dcab1b72f01192b44e88afaa041cffde90b24
Yuri <yuri@rawbw.com>
2018-04-27 20:48:43 -0700

    Correction to when stat64 is used: FreeBSD doesn't use stat64.

--- IO/EnSight/vtkEnSight6BinaryReader.cxx
+++ IO/EnSight/vtkEnSight6BinaryReader.cxx
@@ -36,8 +36,8 @@
 #if defined(_WIN32)
 # define VTK_STAT_STRUCT struct _stat64
 # define VTK_STAT_FUNC _stat64
-#elif defined _DARWIN_FEATURE_64_BIT_INODE && !defined __FreeBSD__
-// FreeBSD and OSX now deprecate stat64
+#elif defined _DARWIN_FEATURE_64_BIT_INODE || defined __FreeBSD__
+// FreeBSD and OSX use stat
 # define VTK_STAT_STRUCT struct stat
 # define VTK_STAT_FUNC stat
 #else
