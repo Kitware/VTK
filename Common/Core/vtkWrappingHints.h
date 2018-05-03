726d9b5107c959458509b017005157062147e08b
Todd Martin <nztoddler@yahoo.com>
2018-05-03 22:17:02 +1200

    Add custom wrap compiler directive

--- Common/Core/vtkWrappingHints.h
+++ Common/Core/vtkWrappingHints.h
@@ -24,6 +24,7 @@
 #define vtkWrappingHints_h
 
 #ifdef __VTK_WRAP__
+#define __USE_CUSTOM_VTK_WRAP__
 // The return value points to a newly-created VTK object.
 #define VTK_NEWINSTANCE [[vtk::newinstance]]
 // The parameter is a pointer to a zerocopy buffer.
@@ -32,7 +33,9 @@
 #define VTK_EXPECTS(x) [[vtk::expects(x)]]
 // Set size hint for parameter or return value
 #define VTK_SIZEHINT(...) [[vtk::sizehint(__VA_ARGS__)]]
-#else
+#endif
+
+#ifndef __USE_CUSTOM_VTK_WRAP__
 #define VTK_NEWINSTANCE
 #define VTK_ZEROCOPY
 #define VTK_EXPECTS(x)
