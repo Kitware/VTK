3249d4e839789537261f432bbb1d404f290f9242
Aron Helser <aron.helser@kitware.com>
2018-05-17 13:58:11 -0400

    Remote Modules, add MomentInvariants, and ParallelMI

---
+++ Remote/vtkFiltersMomentInvariants.remote.cmake
@@ -0,0 +1,9 @@
+#
+# vtkFiltersMomentInvariants
+#
+
+vtk_fetch_module(vtkFiltersMomentInvariants
+  "MomentInvariants filter"
+  GIT_REPOSITORY https://gitlab.kitware.com/aron.helser/MomentInvariants.git
+  GIT_TAG master
+  )
---
+++ Remote/vtkFiltersParallelMomentInvariants.remote.cmake
@@ -0,0 +1,9 @@
+#
+# vtkFiltersParallelMomentInvariants
+#
+
+vtk_fetch_module(vtkFiltersParallelMomentInvariants
+  "ParallelMomentInvariants filter"
+  GIT_REPOSITORY https://gitlab.kitware.com/aron.helser/ParallelMomentInvariants.git
+  GIT_TAG master
+  )
