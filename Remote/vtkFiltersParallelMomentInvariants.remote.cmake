aa8b56982ca960bc417b1e3c08bcbe3a6132edea
Aron Helser <aron.helser@kitware.com>
2018-05-22 09:47:51 -0400

    Remote: change ParallelMoments to use branch
    Use a branch of MomentInvariants instead of a separate repo.

--- Remote/vtkFiltersParallelMomentInvariants.remote.cmake
+++ Remote/vtkFiltersParallelMomentInvariants.remote.cmake
@@ -4,6 +4,6 @@
 
 vtk_fetch_module(vtkFiltersParallelMomentInvariants
   "ParallelMomentInvariants filter"
-  GIT_REPOSITORY https://gitlab.kitware.com/aron.helser/ParallelMomentInvariants.git
-  GIT_TAG master
+  GIT_REPOSITORY https://gitlab.kitware.com/aron.helser/MomentInvariants.git
+  GIT_TAG master_ParallelMomentInvariants
   )
