0aeb5bab0bbcffe632faf6900961ef168cbe52fe
Aron Helser <aron.helser@kitware.com>
2018-05-23 11:04:39 -0400

    Remote: parallel code included in single module
    Remove separate module for parallel - all code contained
    in vtkFiltersMomentInvariants

--- Remote/vtkFiltersParallelMomentInvariants.remote.cmake
+++
@@ -1,9 +0,0 @@
-#
-# vtkFiltersParallelMomentInvariants
-#
-
-vtk_fetch_module(vtkFiltersParallelMomentInvariants
-  "ParallelMomentInvariants filter"
-  GIT_REPOSITORY https://gitlab.kitware.com/aron.helser/MomentInvariants.git
-  GIT_TAG master_ParallelMomentInvariants
-  )
