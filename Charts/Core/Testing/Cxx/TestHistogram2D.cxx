89f977ce8517f199ccc69f6659d0b481f890cf3f
Marcus D. Hanwell <marcus.hanwell@kitware.com>
2018-05-31 09:47:35 -0400

    Update incorrect file name in test

--- Charts/Core/Testing/Cxx/TestHistogram2D.cxx
+++ Charts/Core/Testing/Cxx/TestHistogram2D.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    TestLinePlot.cxx
+  Module:    TestHistogram2D.cxx
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
