0f7ea66bde9b774d1d003c8692507e7ed4087adf
Aron Helser <aron.helser@kitware.com>
2018-05-31 12:26:29 -0400

    Testing: force remote module on for buildbot test

--- Remote/vtkFiltersMomentInvariants.remote.cmake
+++ Remote/vtkFiltersMomentInvariants.remote.cmake
@@ -5,5 +5,5 @@
 vtk_fetch_module(vtkFiltersMomentInvariants
   "MomentInvariants filter"
   GIT_REPOSITORY https://gitlab.kitware.com/vtk/MomentInvariants.git
-  GIT_TAG 8338daad6ca2f4b9bc9d67ce3b7dda1ebeb16608
+  GIT_TAG a5a360559efc62c7d54af85dd2dcad28b71dabc7
   )
