353f5156a2ff11eaf132b15441e7e0ad737e46cc
Aron Helser <aron.helser@kitware.com>
2018-06-13 13:53:47 -0400

    Update MomentInvariants remote module for test fix.

--- Remote/vtkFiltersMomentInvariants.remote.cmake
+++ Remote/vtkFiltersMomentInvariants.remote.cmake
@@ -5,5 +5,5 @@
 vtk_fetch_module(vtkFiltersMomentInvariants
   "MomentInvariants filter"
   GIT_REPOSITORY https://gitlab.kitware.com/vtk/MomentInvariants.git
-  GIT_TAG a5a360559efc62c7d54af85dd2dcad28b71dabc7
+  GIT_TAG 5abf1c1a1ff4e57bc1a7554d041ca8de84322c91
   )
