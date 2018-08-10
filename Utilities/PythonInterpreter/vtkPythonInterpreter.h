e8a252524287b764b0e5972d202fe11bc58bb1c4
Cory Quammen <cory.quammen@kitware.com>
2018-08-10 13:49:53 -0400

    Make copy constructor and operator= private
    Fixes warning about deprecated implicit copy constructor in
    vtkPythonGlobalInterpreters.

--- Utilities/PythonInterpreter/vtkPythonInterpreter.h
+++ Utilities/PythonInterpreter/vtkPythonInterpreter.h
@@ -202,6 +202,9 @@ class VTKPYTHONINTERPRETER_EXPORT vtkPythonGlobalInterpreters
 public:
   vtkPythonGlobalInterpreters();
   ~vtkPythonGlobalInterpreters();
+private:
+  vtkPythonGlobalInterpreters(const vtkPythonGlobalInterpreters&);
+  vtkPythonGlobalInterpreters& operator=(const vtkPythonGlobalInterpreters&);
 };
 
 // This is here to implement the Schwarz counter idiom.
