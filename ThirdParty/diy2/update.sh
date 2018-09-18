ad9f7b293fcf5f1de6f9892ffe3ccb0bbcd447f5
Ben Boeckel <ben.boeckel@kitware.com>
2018-09-18 14:10:14 -0400

    diy2: update the reference tag

--- ThirdParty/diy2/update.sh
+++ ThirdParty/diy2/update.sh
@@ -8,7 +8,7 @@ readonly name="diy2"
 readonly ownership="Diy2 Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/diy2.git"
-readonly tag="for/vtk"
+readonly tag="for/vtk-20180918-g8359566d"
 readonly paths="
 .gitattributes
 CMakeLists.vtk.txt
