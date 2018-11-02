95364e36cac7d9cf5764d3151b1f7336f6b823b1
Andrew J. P. Maclean <andrew.amaclean@gmail.com>
2018-11-02 15:08:20 +1100

    Upgrading these examples to C++11.

--- Examples/DataManipulation/Cxx/SGrid.cxx
+++ Examples/DataManipulation/Cxx/SGrid.cxx
@@ -20,106 +20,101 @@
 // can be added to the dataset.
 //
 //
-#include "vtkActor.h"
-#include "vtkCamera.h"
-#include "vtkFloatArray.h"
-#include "vtkHedgeHog.h"
-#include "vtkMath.h"
-#include "vtkPointData.h"
-#include "vtkPoints.h"
-#include "vtkPolyDataMapper.h"
-#include "vtkProperty.h"
-#include "vtkRenderWindow.h"
-#include "vtkRenderWindowInteractor.h"
-#include "vtkRenderer.h"
-#include "vtkStructuredGrid.h"
+#include <vtkActor.h>
+#include <vtkCamera.h>
+#include <vtkFloatArray.h>
+#include <vtkHedgeHog.h>
+#include <vtkMath.h>
+#include <vtkNamedColors.h>
+#include <vtkNew.h>
+#include <vtkPointData.h>
+#include <vtkPoints.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkStructuredGrid.h>
+
+#include <array>
 
 int main()
 {
-  int i, j, k, kOffset, jOffset, offset;
-  float x[3], v[3], rMin=0.5, rMax=1.0, deltaRad, deltaZ;
-  float radius, theta;
-  static const int dims[3]={13,11,11};
+  vtkNew<vtkNamedColors> colors;
+
+  float rMin = 0.5, rMax = 1.0, deltaRad, deltaZ;
+  std::array<int, 3> dims = {{13, 11, 11}};
 
   // Create the structured grid.
-  vtkStructuredGrid *sgrid = vtkStructuredGrid::New();
-      sgrid->SetDimensions(dims);
+  vtkNew<vtkStructuredGrid> sgrid;
+  sgrid->SetDimensions(dims.data());
 
   // We also create the points and vectors. The points
   // form a hemi-cylinder of data.
-  vtkFloatArray *vectors = vtkFloatArray::New();
-    vectors->SetNumberOfComponents(3);
-    vectors->SetNumberOfTuples(dims[0]*dims[1]*dims[2]);
-  vtkPoints *points = vtkPoints::New();
-    points->Allocate(dims[0]*dims[1]*dims[2]);
+  vtkNew<vtkFloatArray> vectors;
+  vectors->SetNumberOfComponents(3);
+  vectors->SetNumberOfTuples(dims[0] * dims[1] * dims[2]);
+  vtkNew<vtkPoints> points;
+  points->Allocate(dims[0] * dims[1] * dims[2]);
 
-  deltaZ = 2.0 / (dims[2]-1);
-  deltaRad = (rMax-rMin) / (dims[1]-1);
-  v[2]=0.0;
-  for ( k=0; k<dims[2]; k++)
+  deltaZ = 2.0 / (dims[2] - 1);
+  deltaRad = (rMax - rMin) / (dims[1] - 1);
+  float x[3], v[3];
+  v[2] = 0.0;
+  for (auto k = 0; k < dims[2]; k++)
   {
-    x[2] = -1.0 + k*deltaZ;
-    kOffset = k * dims[0] * dims[1];
-    for (j=0; j<dims[1]; j++)
+    x[2] = -1.0 + k * deltaZ;
+    int kOffset = k * dims[0] * dims[1];
+    for (auto j = 0; j < dims[1]; j++)
     {
-      radius = rMin + j*deltaRad;
-      jOffset = j * dims[0];
-      for (i=0; i<dims[0]; i++)
+      float radius = rMin + j * deltaRad;
+      int jOffset = j * dims[0];
+      for (auto i = 0; i < dims[0]; i++)
       {
-        theta = i * vtkMath::RadiansFromDegrees(15.0);
+        float theta = i * vtkMath::RadiansFromDegrees(15.0);
         x[0] = radius * cos(theta);
         x[1] = radius * sin(theta);
         v[0] = -x[1];
         v[1] = x[0];
-        offset = i + jOffset + kOffset;
-        points->InsertPoint(offset,x);
-        vectors->InsertTuple(offset,v);
+        int offset = i + jOffset + kOffset;
+        points->InsertPoint(offset, x);
+        vectors->InsertTuple(offset, v);
       }
     }
   }
   sgrid->SetPoints(points);
-  points->Delete();
   sgrid->GetPointData()->SetVectors(vectors);
-  vectors->Delete();
 
   // We create a simple pipeline to display the data.
-  vtkHedgeHog *hedgehog = vtkHedgeHog::New();
-      hedgehog->SetInputData(sgrid);
-      hedgehog->SetScaleFactor(0.1);
+  vtkNew<vtkHedgeHog> hedgehog;
+  hedgehog->SetInputData(sgrid);
+  hedgehog->SetScaleFactor(0.1);
 
-  vtkPolyDataMapper *sgridMapper = vtkPolyDataMapper::New();
-      sgridMapper->SetInputConnection(hedgehog->GetOutputPort());
-  vtkActor *sgridActor = vtkActor::New();
-      sgridActor->SetMapper(sgridMapper);
-      sgridActor->GetProperty()->SetColor(0,0,0);
+  vtkNew<vtkPolyDataMapper> sgridMapper;
+  sgridMapper->SetInputConnection(hedgehog->GetOutputPort());
+  vtkNew<vtkActor> sgridActor;
+  sgridActor->SetMapper(sgridMapper);
+  sgridActor->GetProperty()->SetColor(colors->GetColor3d("Indigo").GetData());
 
   // Create the usual rendering stuff
-  vtkRenderer *renderer = vtkRenderer::New();
-  vtkRenderWindow *renWin = vtkRenderWindow::New();
-    renWin->AddRenderer(renderer);
+  vtkNew<vtkRenderer> renderer;
+  vtkNew<vtkRenderWindow> renWin;
+  renWin->AddRenderer(renderer);
 
-  vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
-    iren->SetRenderWindow(renWin);
+  vtkNew<vtkRenderWindowInteractor> iren;
+  iren->SetRenderWindow(renWin);
 
   renderer->AddActor(sgridActor);
-  renderer->SetBackground(1,1,1);
+  renderer->SetBackground(colors->GetColor3d("Cornsilk").GetData());
   renderer->ResetCamera();
   renderer->GetActiveCamera()->Elevation(60.0);
   renderer->GetActiveCamera()->Azimuth(30.0);
-  renderer->GetActiveCamera()->Zoom(1.25);
-  renWin->SetSize(300,300);
+  renderer->GetActiveCamera()->Zoom(1.0);
+  renWin->SetSize(600, 600);
 
   // interact with data
   renWin->Render();
   iren->Start();
 
-  renderer->Delete();
-  renWin->Delete();
-  iren->Delete();
-  sgrid->Delete();
-  hedgehog->Delete();
-  sgridMapper->Delete();
-  sgridActor->Delete();
-
-  return 0;
+  return EXIT_SUCCESS;
 }
