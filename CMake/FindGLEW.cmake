fd9aca948eeea542e356028ea20395690dad83d4
Ben Boeckel <ben.boeckel@kitware.com>
2018-11-07 10:32:48 -0500

    FindGLEW: use imported targets

--- CMake/FindGLEW.cmake
+++ CMake/FindGLEW.cmake
@@ -1,18 +1,27 @@
-# Find the GLEW library
-#
-# Defines:
-#
-#  GLEW_FOUND        - system has GLEW
-#  GLEW_INCLUDE_DIRS - the GLEW include directories
-#  GLEW_LIBRARY      - The GLEW library
-#
-find_path(GLEW_INCLUDE_DIR GL/glew.h)
-find_library(GLEW_LIBRARY NAMES GLEW glew32)
-
-set(GLEW_INCLUDE_DIRS "${GLEW_INCLUDE_DIR}")
-set(GLEW_LIBRARIES "${GLEW_LIBRARY}")
+find_path(GLEW_INCLUDE_DIR
+  NAMES GL/glew.h
+  DOC "glew include directory")
+mark_as_advanced(GLEW_INCLUDE_DIR)
+find_library(GLEW_LIBRARY
+  NAMES GLEW glew32
+  DOC "glew library")
+mark_as_advanced(GLEW_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GLEW DEFAULT_MSG GLEW_INCLUDE_DIR GLEW_LIBRARY)
+find_package_handle_standard_args(GLEW REQUIRED_VARS GLEW_LIBRARY GLEW_INCLUDE_DIR)
 
-mark_as_advanced(GLEW_INCLUDE_DIR GLEW_LIBRARY)
+if (GLEW_FOUND)
+  set(GLEW_INCLUDE_DIRS "${GLEW_INCLUDE_DIR}")
+  set(GLEW_LIBRARIES "${GLEW_LIBRARY}")
+
+  if (NOT TARGET GLEW::GLEW)
+    include(vtkDetectLibraryType)
+    vtk_detect_library_type(glew_library_type
+      PATH "${GLEW_LIBRARY}")
+    add_library(GLEW::GLEW "${glew_library_type}" IMPORTED)
+    unset(glew_library_type)
+    set_target_properties(GLEW::GLEW PROPERTIES
+      IMPORTED_LOCATION "${GLEW_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${GLEW_INCLUDE_DIR}")
+  endif ()
+endif ()
