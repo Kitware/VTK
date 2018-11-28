7ceeea6429d7fa633189b41eba54975ffa7f69bb
Aron Helser <aron.helser@kitware.com>
2018-11-28 16:46:53 -0500

    Avatar: generate geometry headers using binary flag
    Use BINARY and NUL_TERMINATE cmake flags to generate a char array in a
    header, instead of hand-editing to avoid static string length limits.

--- Rendering/OpenVR/vtkAvatarHead.h
+++
@@ -1,9 +0,0 @@
-#ifndef vtkAvatarHead_h
-#define vtkAvatarHead_h
-
-#include "vtkRenderingOpenVRModule.h"
-
-VTKRENDERINGOPENVR_EXPORT extern const char *vtkAvatarHead;
-
-#endif
-// VTK-HeaderTest-Exclude: vtkAvatarHead.h
