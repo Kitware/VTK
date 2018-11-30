fc373db2f47cfcfbc8881e75507737c11f413420
Michael Migliore <michael.migliore@kitware.com>
2018-11-30 11:55:22 +0100

    Pass clipping range to new camera in panoramic pass

--- Rendering/OpenGL2/vtkPanoramicProjectionPass.cxx
+++ Rendering/OpenGL2/vtkPanoramicProjectionPass.cxx
@@ -319,6 +319,7 @@ void vtkPanoramicProjectionPass::RenderOnFace(const vtkRenderState* s, int faceI
 
   double range[2];
   oldCamera->GetClippingRange(range);
+  newCamera->SetClippingRange(range);
   vtkNew<vtkPerspectiveTransform> perspectiveTransform;
 
   // the fov is 90 degree in each direction, the frustum can be simplified
