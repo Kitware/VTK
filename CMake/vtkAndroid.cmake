36c8b46dc6a0f158485e0097bedecfaba577173d
Ben Boeckel <ben.boeckel@kitware.com>
2019-01-25 15:30:59 -0500

    vtkAndroid: update for the new module system

--- CMake/vtkAndroid.cmake
+++ CMake/vtkAndroid.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.7 FATAL_ERROR)
+cmake_minimum_required(VERSION 3.8 FATAL_ERROR)
 
 #
 # Instructions:
@@ -55,7 +55,7 @@ if (NOT EXISTS ${CMAKE_INSTALL_PREFIX})
 endif()
 
 # make sure we have a CTestCustom.cmake file
-configure_file("${VTK_CMAKE_DIR}/CTestCustom.cmake.in"
+configure_file("${vtk_cmake_dir}/CTestCustom.cmake.in"
   "${CMAKE_CURRENT_BINARY_DIR}/CTestCustom.cmake" @ONLY)
 
 # Compile a minimal VTK for its compile tools
@@ -66,16 +66,14 @@ macro(compile_vtk_tools)
     PREFIX ${CMAKE_BINARY_DIR}/CompileTools
     BINARY_DIR ${CMAKE_BINARY_DIR}/CompileTools
     INSTALL_COMMAND ""
-    BUILD_COMMAND ${CMAKE_COMMAND} --build . --config $<CONFIGURATION> --target vtkCompileTools
     BUILD_ALWAYS 1
     CMAKE_CACHE_ARGS
+      -DVTK_BUILD_COMPILE_TOOLS_ONLY:BOOL=ON
       -DCMAKE_BUILD_TYPE:STRING=Release
       -DVTK_BUILD_ALL_MODULES:BOOL=OFF
-      -DVTK_Group_Rendering:BOOL=OFF
-      -DVTK_Group_StandAlone:BOOL=ON
       -DBUILD_SHARED_LIBS:BOOL=ON
-      -DBUILD_EXAMPLES:BOOL=OFF
-      -DBUILD_TESTING:BOOL=OFF
+      -DVTK_BUILD_EXAMPLES:BOOL=OFF
+      -DVTK_BUILD_TESTING:BOOL=OFF
       -DCMAKE_MAKE_PROGRAM:FILEPATH=${CMAKE_MAKE_PROGRAM}
   )
 endmacro()
@@ -94,34 +92,27 @@ set(android_cmake_flags
   -DANDROID_EXECUTABLE:FILE=${ANDROID_EXECUTABLE}
   -DANT_EXECUTABLE:FILE=${ANT_EXECUTABLE}
   -DBUILD_SHARED_LIBS:BOOL=OFF
-  -DBUILD_TESTING:BOOL=OFF
+  -DVTK_BUILD_TESTING:STRING=OFF
   -DVTK_BUILD_EXAMPLES:BOOL=${VTK_BUILD_EXAMPLES}
-  -DVTK_RENDERING_BACKEND:STRING=OpenGL2
-  -DVTK_Group_Rendering:BOOL=OFF
-  -DVTK_Group_StandAlone:BOOL=OFF
-  -DVTK_Group_Imaging:BOOL=OFF
-  -DVTK_Group_MPI:BOOL=OFF
-  -DVTK_Group_Views:BOOL=OFF
-  -DVTK_Group_Qt:BOOL=OFF
-  -DVTK_Group_Tk:BOOL=OFF
-  -DVTK_Group_Web:BOOL=OFF
-  -DModule_vtkFiltersCore:BOOL=ON
-  -DModule_vtkFiltersModeling:BOOL=ON
-  -DModule_vtkFiltersSources:BOOL=ON
-  -DModule_vtkFiltersGeometry:BOOL=ON
-  -DModule_vtkIOGeometry:BOOL=ON
-  -DModule_vtkIOLegacy:BOOL=ON
-  -DModule_vtkIOImage:BOOL=OFF
-  -DModule_vtkIOPLY:BOOL=ON
-  -DModule_vtkIOInfovis:BOOL=ON
-  -DModule_vtkImagingCore:BOOL=ON
-  -DModule_vtkInteractionStyle:BOOL=ON
-  -DModule_vtkParallelCore:BOOL=ON
-  -DModule_vtkRenderingCore:BOOL=ON
-  -DModule_vtkRenderingFreeType:BOOL=ON
-  -DModule_vtkTestingCore:BOOL=ON
-  -DModule_vtkTestingRendering:BOOL=ON
-  -DModule_vtkRenderingVolumeOpenGL2:BOOL=ON
+  -DVTK_GROUP_ENABLE_Rendering:STRING=DONT_WANT
+  -DVTK_GROUP_ENABLE_StandAlone:STRING=DONT_WANT
+  -DVTK_MODULE_ENABLE_VTK_FiltersCore:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_FiltersModeling:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_FiltersSources:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_FiltersGeometry:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_IOGeometry:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_IOLegacy:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_IOImage:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_IOPLY:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_IOInfovis:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_ImagingCore:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_InteractionStyle:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_ParallelCore:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_RenderingCore:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_RenderingFreeType:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_TestingCore:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_TestingRendering:STRING=YES
+  -DVTK_MODULE_ENABLE_VTK_RenderingVolumeOpenGL2:STRING=YES
 )
 
 macro(crosscompile target api abi out_build_dir)
@@ -143,6 +134,7 @@ macro(crosscompile target api abi out_build_dir)
       -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR}/${target}
       -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
       -DCMAKE_TOOLCHAIN_FILE:PATH=${_ANDROID_TOOLCHAIN}
+      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
       -DVTKCompileTools_DIR:PATH=${CMAKE_BINARY_DIR}/CompileTools
       -DCMAKE_MAKE_PROGRAM:FILEPATH=${CMAKE_MAKE_PROGRAM}
       ${android_cmake_flags}
