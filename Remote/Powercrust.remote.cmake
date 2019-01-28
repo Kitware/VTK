60c6ddb3b884845e398784f1a77b58362dbe1872
Bill Lorensen <bill.lorensen@gmail.com>
2019-01-28 15:44:04 -0800

    ENH: Reinstate Remote modules.
    The new vtk module system required changes to
    vtkModuleRemote.cmake. The old system used a cmake option,
    Build_modulename:BOOL ON/OFF to enable a remote module. The new module
    system uses a cmake variable, VTK_MODULE_ENABLE_VTK with values YES,
    WANT, DONT_WANT, NO and DEFAULT.
    
    Also, CMakeLists.txt now includes add_subdirectory(Remote) and the Remote directory is added to vtk_source_directories.
    
    To test the new remote macros, we enabled three remote modules:
    PoissonReconstruction, Powercrust and SplineDrivenImageSlicer.
    
    The WikiExamples are no longer available as a remote module. They will instead be added as an external project in the future, WikiExamples.remote.cmake has been removed.

---
+++ Remote/Powercrust.remote.cmake
@@ -0,0 +1,10 @@
+#
+# Powercrust Reconstruction
+#
+
+vtk_fetch_module(Powercrust
+  "Powercrust Surface reconstruction from unorganized points"
+  GIT_REPOSITORY https://github.com/lorensen/Powercrust
+  # February 1, 2019 - updated for new vtk module API
+  GIT_TAG fb3d08d06fb504a431b9f5882e8086b932815cf0
+  )
