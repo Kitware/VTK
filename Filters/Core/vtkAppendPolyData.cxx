bfd6d19b49b15f80f9b87c50b61bfdf6e3c5dd37
Cory Quammen <cory.quammen@kitware.com>
2019-01-29 19:26:53 -0500

    Mark vtkAppendPolyData::ExecuteAppend() as legacy

--- Filters/Core/vtkAppendPolyData.cxx
+++ Filters/Core/vtkAppendPolyData.cxx
@@ -127,10 +127,13 @@ void vtkAppendPolyData::SetInputConnectionByNumber(int num,
   this->SetNthInputConnection(0, num, input);
 }
 
+#if !defined(VTK_LEGACY_REMOVE)
 //----------------------------------------------------------------------------
 int vtkAppendPolyData::ExecuteAppend(vtkPolyData* output,
     vtkPolyData* inputs[], int numInputs)
 {
+  VTK_LEGACY_BODY(vtkAppendPolyData::ExecuteAppend, "VTK 9.0");
+
   int idx;
   vtkPolyData *ds;
   vtkPoints  *inPts;
@@ -445,6 +448,8 @@ int vtkAppendPolyData::ExecuteAppend(vtkPolyData* output,
   return 1;
 }
 
+#endif // !defined(VTK_LEGACY_REMOVE)
+
 //----------------------------------------------------------------------------
 // This method is much too long, and has to be broken up!
 // Append data sets into single polygonal data set.
@@ -574,6 +579,7 @@ void vtkAppendPolyData::PrintSelf(ostream& os, vtkIndent indent)
      << endl;
 }
 
+#if !defined(VTK_LEGACY_REMOVE)
 //----------------------------------------------------------------------------
 namespace {
 struct AppendDataWorker
@@ -607,6 +613,8 @@ struct AppendDataWorker
 void vtkAppendPolyData::AppendData(vtkDataArray *dest, vtkDataArray *src,
                                    vtkIdType offset)
 {
+  VTK_LEGACY_BODY(vtkAppendPolyData::ExecuteAppend, "VTK 9.0");
+
   assert("Arrays have same number of components." &&
          src->GetNumberOfComponents() == dest->GetNumberOfComponents());
   assert("Destination array has enough tuples." &&
@@ -625,6 +633,8 @@ void vtkAppendPolyData::AppendData(vtkDataArray *dest, vtkDataArray *src,
 vtkIdType *vtkAppendPolyData::AppendCells(vtkIdType *pDest, vtkCellArray *src,
                                           vtkIdType offset)
 {
+  VTK_LEGACY_BODY(vtkAppendPolyData::ExecuteAppend, "VTK 9.0");
+
   vtkIdType *pSrc, *end, *pNum;
 
   if (src == nullptr)
@@ -654,6 +664,7 @@ vtkIdType *vtkAppendPolyData::AppendCells(vtkIdType *pDest, vtkCellArray *src,
 
   return pDest;
 }
+#endif // !defined(VTK_LEGACY_REMOVE)
 
 //----------------------------------------------------------------------------
 int vtkAppendPolyData::FillInputPortInformation(int port, vtkInformation *info)
--- Filters/Core/vtkAppendPolyData.h
+++ Filters/Core/vtkAppendPolyData.h
@@ -115,8 +115,11 @@ public:
   vtkGetMacro(OutputPointsPrecision,int);
   //@}
 
-  int ExecuteAppend(vtkPolyData* output,
-    vtkPolyData* inputs[], int numInputs) VTK_SIZEHINT(inputs, numInputs);
+  /**
+   * @deprecated Use this filter directly.
+   */
+  VTK_LEGACY(int ExecuteAppend(vtkPolyData* output,
+    vtkPolyData* inputs[], int numInputs) VTK_SIZEHINT(inputs, numInputs));
 
 protected:
   vtkAppendPolyData();
@@ -133,13 +136,19 @@ protected:
                           vtkInformationVector **, vtkInformationVector *) override;
   int FillInputPortInformation(int, vtkInformation *) override;
 
-  // An efficient templated way to append data.
-  void AppendData(vtkDataArray *dest, vtkDataArray *src, vtkIdType offset);
+  /**
+   *  An efficient templated way to append data.
+   * @deprecated Only needed for deprecated public member function ExecuteAppend
+   */
+  VTK_LEGACY(void AppendData(vtkDataArray *dest, vtkDataArray *src, vtkIdType offset));
 
 
-  // An efficient way to append cells.
-  vtkIdType *AppendCells(vtkIdType *pDest, vtkCellArray *src,
-                         vtkIdType offset);
+  /**
+   * An efficient way to append cells.
+   * @deprecated Only needed for deprecated public member function ExecuteAppend
+   */
+  VTK_LEGACY(vtkIdType *AppendCells(vtkIdType *pDest, vtkCellArray *src,
+                                    vtkIdType offset));
 
  private:
   // hide the superclass' AddInput() from the user and the compiler
