9242b59eaee6182af385de6314d42581e6f872e2
David Gobbi <david.gobbi@gmail.com>
2019-02-16 15:03:54 -0700

    Update vtk-dicom remote for warning fixes

--- Remote/vtkDICOM.remote.cmake
+++ Remote/vtkDICOM.remote.cmake
@@ -6,5 +6,5 @@ vtk_fetch_module(vtkDICOM
   "Dicom classes and utilities"
   GIT_REPOSITORY https://github.com/dgobbi/vtk-dicom
   # vtk-dicom with first batch of VTK 8.90 fixes
-  GIT_TAG 2538a7ad336a0afe8e2bffb4e36d2a555cd910ce
+  GIT_TAG fa403f6ee5201f4ff49ee866a843e69f86233cb1
   )
