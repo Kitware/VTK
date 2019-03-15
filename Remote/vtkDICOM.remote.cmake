9b92a264a5d6b44889bbd891a77b80c815bdcdca
David Gobbi <david.gobbi@gmail.com>
2019-03-15 07:41:18 -0600

    Update vtk-dicom remote to fix dependencies
    CommonDataModel is an interface dependency, since some vtk-dicom
    classes are derived from vtkDataObject.

--- Remote/vtkDICOM.remote.cmake
+++ Remote/vtkDICOM.remote.cmake
@@ -5,6 +5,6 @@
 vtk_fetch_module(vtkDICOM
   "Dicom classes and utilities"
   GIT_REPOSITORY https://github.com/dgobbi/vtk-dicom
-  # vtk-dicom with first batch of VTK 8.90 fixes
-  GIT_TAG fa403f6ee5201f4ff49ee866a843e69f86233cb1
+  # vtk-dicom with second batch of VTK 8.90 fixes
+  GIT_TAG 5bf860c73579a142d2bd667600a178ac153e4fc9
   )
