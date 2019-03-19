469f37a0a0231c2b1c4a04894e59e728afcfeb90
Ben Boeckel <ben.boeckel@kitware.com>
2019-03-19 13:53:39 -0400

    Findutf8cpp: add find module for utf8cpp

---
+++ CMake/Findutf8cpp.cmake
@@ -0,0 +1,18 @@
+find_path(utf8cpp_INCLUDE_DIR
+  NAMES utf8.h
+  DOC "utf8cpp include directory")
+mark_as_advanced(utf8cpp_INCLUDE_DIR)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(utf8cpp
+  REQUIRED_VARS utf8cpp_INCLUDE_DIR)
+
+if (utf8cpp_FOUND)
+  set(utf8cpp_INCLUDE_DIRS "${utf8cpp_INCLUDE_DIR}")
+
+  if (NOT TARGET utf8cpp::utf8cpp)
+    add_library(utf8cpp::utf8cpp INTERFACE IMPORTED)
+    set_target_properties(utf8cpp::utf8cpp PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${utf8cpp_INCLUDE_DIR}")
+  endif ()
+endif ()
