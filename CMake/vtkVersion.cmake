0d78e25180e4c637ae32ae08d06dde3bddf26301
Ben Boeckel <ben.boeckel@kitware.com>
2019-04-05 13:24:00 -0400

    vtkVersion: check that minor and build are within their range

--- CMake/vtkVersion.cmake
+++ CMake/vtkVersion.cmake
@@ -2,3 +2,15 @@
 set(VTK_MAJOR_VERSION 8)
 set(VTK_MINOR_VERSION 90)
 set(VTK_BUILD_VERSION 0)
+
+if (NOT VTK_MINOR_VERSION LESS 100)
+  message(FATAL_ERROR
+    "The minor version number cannot exceed 100 without changing "
+    "`VTK_VERSION_CHECK`.")
+endif ()
+
+if (NOT VTK_BUILD_VERSION LESS 100000000)
+  message(FATAL_ERROR
+    "The build version number cannot exceed 100000000 without changing "
+    "`VTK_VERSION_CHECK`.")
+endif ()
