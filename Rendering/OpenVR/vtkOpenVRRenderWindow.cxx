b7e5a1f9a406529f3457c586b76b10135171c24c
Sean McBride <sean@rogue-research.com>
2019-04-24 12:36:10 -0400

    Fixed compiler error due to incorrect header filename spelling

--- Rendering/OpenVR/vtkOpenVRRenderWindow.cxx
+++ Rendering/OpenVR/vtkOpenVRRenderWindow.cxx
@@ -60,7 +60,7 @@ https://github.com/ValveSoftware/openvr/blob/master/LICENSE
 #include "vtkXOpenGLRenderWindow.h"
 #endif
 #ifdef VTK_USE_COCOA
-#include "vtkCocoaOpenGLRenderWindow.h"
+#include "vtkCocoaRenderWindow.h"
 #endif
 
 #if !defined(_WIN32) || defined(__CYGWIN__)
@@ -98,7 +98,7 @@ vtkOpenVRRenderWindow::vtkOpenVRRenderWindow()
   this->HelperWindow = vtkXOpenGLRenderWindow::New();
 #endif
 #ifdef VTK_USE_COCOA
-  this->HelperWindow = vtkCocoaOpenGLRenderWindow::New();
+  this->HelperWindow = vtkCocoaRenderWindow::New();
 #endif
 
   this->DashboardOverlay = vtkOpenVRDefaultOverlay::New();
