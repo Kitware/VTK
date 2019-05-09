c296c2551d2f12721d704404c563df01bcdb56a0
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2019-05-09 19:22:58 -0400

    tpl: h5part
    adding h5part update scripts etc.

---
+++ ThirdParty/h5part/update.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+shopt -s dotglob
+
+readonly name="h5part"
+readonly ownership="H5part Upstream <kwrobot@kitware.com>"
+readonly subtree="ThirdParty/$name/vtk$name"
+readonly repo="https://gitlab.kitware.com/third-party/h5part.git"
+readonly tag="for/vtk-20190510-1.6.6"
+readonly paths="
+.gitattributes
+CMakeLists.vtk.txt
+COPYING
+README.kitware.md
+README
+src/*.c
+src/*.h
+"
+
+extract_source () {
+    git_archive
+    pushd "$extractdir/$name-reduced"
+    mv -v CMakeLists.vtk.txt CMakeLists.txt
+    popd
+}
+
+. "${BASH_SOURCE%/*}/../update-common.sh"
