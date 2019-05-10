467a6a80c0fc4aad5c8262bed8428a31f0385b6a
David Gobbi <david.gobbi@gmail.com>
2019-05-10 07:47:39 -0600

    Update vtk-dicom remote to fix overflow warning

--- Remote/vtkDICOM.remote.cmake
+++ Remote/vtkDICOM.remote.cmake
@@ -5,6 +5,6 @@
 vtk_fetch_module(vtkDICOM
   "Dicom classes and utilities"
   GIT_REPOSITORY https://github.com/dgobbi/vtk-dicom
-  # vtk-dicom with second batch of VTK 8.90 fixes
-  GIT_TAG 5bf860c73579a142d2bd667600a178ac153e4fc9
+  # vtk-dicom with third batch of VTK 8.90 fixes
+  GIT_TAG d9e9f0c02d2d17f1eb9cfa290f82d20a48203acf
   )
