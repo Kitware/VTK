9414d60585fa10bb002877ab4a322749ac4245af
Alexis Girault <alexis.girault@kitware.com>
2019-05-22 11:50:21 -0400

    vtkMarchingCubes: Update tests to account for orientation

--- Filters/General/Testing/Python/TestMarchingCubes.py
+++ Filters/General/Testing/Python/TestMarchingCubes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import vtk
+from math import cos, sin, pi
 from vtk.util.misc import vtkGetDataRoot
 VTK_DATA_ROOT = vtkGetDataRoot()
 
@@ -13,12 +14,20 @@ iren.SetRenderWindow(renWin)
 
 # create pipeline
 #
+angle = pi/2
+orientation = [
+  -1, 0, 0,
+  0, cos(angle), -sin(angle),
+  0, -sin(angle), -cos(angle)
+]
 v16 = vtk.vtkVolume16Reader()
 v16.SetDataDimensions(64, 64)
 v16.SetDataByteOrderToLittleEndian()
 v16.SetFilePrefix(VTK_DATA_ROOT + "/Data/headsq/quarter")
 v16.SetImageRange(1, 93)
 v16.SetDataSpacing(3.2, 3.2, 1.5)
+v16.SetDataOrigin(0.1, 10, 1000)
+v16.GetOutput().SetDirectionMatrix(orientation)
 v16.Update()
 
 iso = vtk.vtkMarchingCubes()
@@ -39,13 +48,7 @@ ren1.AddActor(isoActor)
 ren1.SetBackground(0.2, 0.3, 0.4)
 
 renWin.SetSize(200, 200)
-
 ren1.ResetCamera()
-ren1.GetActiveCamera().Elevation(90)
-ren1.GetActiveCamera().SetViewUp(0, 0, -1)
-ren1.GetActiveCamera().Azimuth(180)
-ren1.ResetCameraClippingRange()
-
 renWin.Render()
 
 # render the image
