75ed96f4e69cf1d859eca2f24830e0e7f64f3f08
Sujin Philip <sujin.philip@kitware.com>
2019-05-29 09:55:40 -0400

    Update MomentInvariants remote module

--- Remote/MomentInvariants.remote.cmake
+++ Remote/MomentInvariants.remote.cmake
@@ -5,5 +5,5 @@
 vtk_fetch_module(MomentInvariants
   "MomentInvariants filter"
   GIT_REPOSITORY https://gitlab.kitware.com/vtk/MomentInvariants.git
-  GIT_TAG 1f4e4b925b2dbe8dfc741d556c542508299a0427
+  GIT_TAG 80069c5d964dfa1cf958709dc5b6b5939d3fc69c
   )
