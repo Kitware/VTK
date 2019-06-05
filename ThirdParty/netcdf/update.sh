f5165de8cbab450303264956ebe50b9d88cc2638
Ben Boeckel <ben.boeckel@kitware.com>
2019-06-05 17:07:14 -0400

    netcdf: update to 4.7.0

--- ThirdParty/netcdf/update.sh
+++ ThirdParty/netcdf/update.sh
@@ -8,7 +8,7 @@ readonly name="netcdf"
 readonly ownership="netcdf Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/netcdf.git"
-readonly tag="for/vtk-20190520-4.6.1"
+readonly tag="for/vtk-20190607.1-4.7.0"
 readonly paths="
 include/*.h
 include/netcdf_meta.h.in
