5c7513e5869ee6ab5b0b4ca06dd016583e9e7b78
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2019-06-05 16:33:21 -0400

    zfp: update to 0.5.5

--- ThirdParty/zfp/update.sh
+++ ThirdParty/zfp/update.sh
@@ -8,7 +8,7 @@ readonly name="zfp"
 readonly ownership="Zfp Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/zfp.git"
-readonly tag="for/vtk-20190111-0.5.4"
+readonly tag="for/vtk-20190605-0.5.5"
 readonly paths="
 .gitattributes
 CMakeLists.vtk.txt
