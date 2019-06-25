d8bd771d5996550b8c6ddfec4579755134ae6ae4
Andrew Bauer <bauer.andrew.c@gmail.com>
2019-06-25 15:40:19 -0400

    Fixing parallel particle paths filter
    There was bug where Ids are incremented but shouldn't be
    when particles were migrated between MPI processes. Additionally
    when a particle migrated away from a process but then migrated
    back to that same process the generated path line cells were
    wrong. Both bugs are fixed with this change.

--- Filters/FlowPaths/vtkParticlePathFilter.cxx
+++ Filters/FlowPaths/vtkParticlePathFilter.cxx
@@ -125,11 +125,38 @@ void ParticlePathFilterInternal::Finalize()
            << "\n" << "): " <<" no lines in the output"<< "\n\n";
     return;
   }
-  for(unsigned int i=0; i<this->Paths.size(); i++)
+  // if we have a path that leaves a process and than comes back we need
+  // to add that as separate cells. we use the simulation time step to check
+  // on that assuming that the particle path filter is updated every time step.
+  vtkIntArray* sourceSimulationTimeStepArray = vtkArrayDownCast<vtkIntArray>(
+    this->Filter->Output->GetPointData()->GetArray("SimulationTimeStep"));
+  vtkNew<vtkIdList> tmpIds;
+  for(size_t i=0; i<this->Paths.size(); i++)
   {
     if(this->Paths[i]->GetNumberOfIds()>1)
     {
-      outLines->InsertNextCell(this->Paths[i]);
+      vtkIdList* ids = this->Paths[i];
+      int previousTimeStep = sourceSimulationTimeStepArray->GetTypedComponent(ids->GetId(0), 0);
+      tmpIds->Reset();
+      tmpIds->InsertNextId(ids->GetId(0));
+      for (vtkIdType j=1; j<ids->GetNumberOfIds(); j++)
+      {
+        int currentTimeStep = sourceSimulationTimeStepArray->GetTypedComponent(ids->GetId(j), 0);
+        if (currentTimeStep != (previousTimeStep + 1))
+        {
+          if (tmpIds->GetNumberOfIds() > 1)
+          {
+            outLines->InsertNextCell(tmpIds);
+          }
+          tmpIds->Reset();
+        }
+        tmpIds->InsertNextId(ids->GetId(j));
+        previousTimeStep = currentTimeStep;
+      }
+      if (tmpIds->GetNumberOfIds() > 1)
+      {
+        outLines->InsertNextCell(tmpIds);
+      }
     }
   }
 }
