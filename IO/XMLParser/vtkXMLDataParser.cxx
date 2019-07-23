b2faa58743cfa98ca4ff30b0805e898720350054
Feimi Yu <yuf2@rpi.edu>
2019-07-23 12:55:29 -0400

    XML parser now read inf and nan in Ascii data
    add cctype header

--- IO/XMLParser/vtkXMLDataParser.cxx
+++ IO/XMLParser/vtkXMLDataParser.cxx
@@ -27,6 +27,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <cctype>
 #include <memory>
 #include <sstream>
 #include <vector>
@@ -932,7 +933,8 @@ size_t vtkXMLDataParser::ReadAppendedData(vtkTypeInt64 offset,
 //----------------------------------------------------------------------------
 // Define a parsing function template.  The extra "long" argument is used
 // to help broken compilers select the non-templates below for char and
-// unsigned char by making them a better conversion than the template.
+// unsigned char, and float/double by making them a better conversion than
+// the template.
 template <class T>
 T* vtkXMLParseAsciiData(istream& is, int* length, T*, long)
 {
@@ -957,6 +959,116 @@ T* vtkXMLParseAsciiData(istream& is, int* length, T*, long)
   }
 
   if(length)
+  {
+    *length = dataLength;
+  }
+
+  return dataBuffer;
+}
+
+//----------------------------------------------------------------------------
+static float* vtkXMLParseAsciiData(istream& is, int* length, float*, int)
+{
+  int dataLength = 0;
+  int dataBufferSize = 64;
+
+  float* dataBuffer = new float[dataBufferSize];
+  std::string stringBuffer;
+  float element;
+
+  while (1)
+  {
+    is >> element;
+    if (!is.good())
+    {
+      is.clear(is.rdstate() & ~ios::failbit);
+      is >> stringBuffer;
+      if (!is.good())
+      {
+        break;
+      }
+      else
+      {
+        std::for_each(
+          stringBuffer.begin(), stringBuffer.end(), [](char& c) { c = std::tolower(c); });
+        if (stringBuffer == "inf" || stringBuffer == "nan" || stringBuffer == "-inf")
+        {
+          element = std::stof(stringBuffer);
+        }
+        else
+        {
+          break;
+        }
+      }
+    }
+    if (dataLength == dataBufferSize)
+    {
+      int newSize = dataBufferSize * 2;
+      float* newBuffer = new float[newSize];
+      memcpy(newBuffer, dataBuffer, dataLength * sizeof(float));
+      delete[] dataBuffer;
+      dataBuffer = newBuffer;
+      dataBufferSize = newSize;
+    }
+    dataBuffer[dataLength++] = element;
+  }
+
+  if (length)
+  {
+    *length = dataLength;
+  }
+
+  return dataBuffer;
+}
+
+//----------------------------------------------------------------------------
+static double* vtkXMLParseAsciiData(istream& is, int* length, double*, int)
+{
+  int dataLength = 0;
+  int dataBufferSize = 64;
+
+  double* dataBuffer = new double[dataBufferSize];
+  std::string stringBuffer;
+  double element;
+
+  while (1)
+  {
+    is >> element;
+    if (!is.good())
+    {
+      is.clear(is.rdstate() & ~ios::failbit);
+      is >> stringBuffer;
+      if (!is.good())
+      {
+        break;
+      }
+      else
+      {
+        std::for_each(
+          stringBuffer.begin(), stringBuffer.end(), [](char& c) { c = std::tolower(c); });
+        if (stringBuffer == "inf" || stringBuffer == "nan" || stringBuffer == "-inf")
+        {
+          element = std::stod(stringBuffer);
+        }
+        else
+        {
+          break;
+        }
+      }
+    }
+    if (dataLength == dataBufferSize)
+    {
+      int newSize = dataBufferSize * 2;
+      double* newBuffer = new double[newSize];
+      memcpy(newBuffer, dataBuffer, dataLength * sizeof(double));
+      delete[] dataBuffer;
+      dataBuffer = newBuffer;
+      dataBufferSize = newSize;
+    }
+    dataBuffer[dataLength++] = element;
+  }
+
+  if (length)
   {
     *length = dataLength;
   }
