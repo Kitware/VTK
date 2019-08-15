a90833544a5a9653a908f0d9d9886e55484e8ed5
Allison Vacanti <allison.vacanti@kitware.com>
2019-08-15 08:54:17 -0400

    Update MomentInvariants SHA.

--- Remote/MomentInvariants.remote.cmake
+++ Remote/MomentInvariants.remote.cmake
@@ -5,5 +5,5 @@
 vtk_fetch_module(MomentInvariants
   "MomentInvariants filter"
   GIT_REPOSITORY https://gitlab.kitware.com/vtk/MomentInvariants.git
-  GIT_TAG aef6704a849a56f3b5a6b91bfabba363097104c1
+  GIT_TAG 086a5875e229e7a9b17395d34ca9de305add27e7
   )
