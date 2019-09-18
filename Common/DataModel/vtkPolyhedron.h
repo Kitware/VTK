53581492d0d7a68fc0b538045cbd94acde5d809c
Yohann Bearzi <yohann.bearzi@kitware.com>
2019-09-18 16:18:34 -0400

    Warning macro adding in vtkPolyhedron
    vtkPolyhedron::GetEdgePoints and vtkPolyhedron::GetFacePoints are not implemented. This merge request adds a warning macro for when they are called.
    Added a warning macro for the same missing
    function in vtkConvexPointSet.
    Added doxygen info for missing implementations

--- Common/DataModel/vtkPolyhedron.h
+++ Common/DataModel/vtkPolyhedron.h
@@ -66,11 +66,18 @@ public:
   void PrintSelf(ostream& os, vtkIndent indent) override;
   //@}
 
+  //@{
   /**
    * See vtkCell3D API for description of these methods.
+   * @warning These method are unimplemented in vtkPolyhedron
    */
-  void GetEdgePoints(int vtkNotUsed(edgeId), int* &vtkNotUsed(pts)) override {}
-  void GetFacePoints(int vtkNotUsed(faceId), int* &vtkNotUsed(pts)) override {}
+  void GetEdgePoints(int vtkNotUsed(edgeId), int* &vtkNotUsed(pts)) override {vtkWarningMacro(<< "vtkPolyhedron::GetEdgePoints Not Implemented");}
+  void GetFacePoints(int vtkNotUsed(faceId), int* &vtkNotUsed(pts)) override {vtkWarningMacro(<< "vtkPolyhedron::GetFacePoints Not Implemented");}
+  //@}
+
+  /**
+   * See vtkCell3D API for description of this method.
+   */
   double *GetParametricCoords() override;
 
   /**
