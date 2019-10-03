e39dedcde1198e1aa9e3fe4a19d797111baeeeeb
Sean McBride <sean@rogue-research.com>
2019-10-03 17:04:37 -0400

    Fixed broken try compile for gcc/clang atomic built-ins
    It was also hiding a typo in the actual use of the builtins

--- Common/Core/vtkTestSyncBuiltins.cxx
+++ Common/Core/vtkTestSyncBuiltins.cxx
@@ -6,9 +6,7 @@ int main()
   /* Test whether gcc/clang __atomic_ builtins are defined */
 
   static volatile unsigned long v = 0;
-  unsigned long u = __atomic_add_fetch(&v, 1);
+  unsigned long u = __atomic_add_fetch(&v, 1, __ATOMIC_SEQ_CST);
 
-  if (u - 1 == 0) { return 0; }
-
-  return 1;
+  return 0;
 }
