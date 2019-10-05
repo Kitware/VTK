b4b51dacd58a35128560e76f5dbbaa03f4139b54
David Gobbi <david.gobbi@gmail.com>
2019-10-05 15:24:00 -0600

    Update vtk-dicom to v0.8.11

--- Remote/vtkDICOM.remote.cmake
+++ Remote/vtkDICOM.remote.cmake
@@ -5,6 +5,6 @@
 vtk_fetch_module(vtkDICOM
   "Dicom classes and utilities"
   GIT_REPOSITORY https://github.com/dgobbi/vtk-dicom
-  # vtk-dicom release 8.9.10 plus a warning fix
-  GIT_TAG 0a4a07111abda6008f5a825802d4a107b5b51396
+  # vtk-dicom release 8.9.11
+  GIT_TAG 6d72f5ccf1340d695a8fe36e9200aa60c09a910a
   )
