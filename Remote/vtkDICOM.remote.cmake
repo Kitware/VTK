fa1026418f5cf6185bae10e5d5acc25a338de463
David Gobbi <david.gobbi@gmail.com>
2019-11-05 10:33:51 -0700

    Update vtkDICOM remote to 0.8.12
    This brings in semicolon fixes for the VTK SetGet macros

--- Remote/vtkDICOM.remote.cmake
+++ Remote/vtkDICOM.remote.cmake
@@ -5,6 +5,6 @@
 vtk_fetch_module(vtkDICOM
   "Dicom classes and utilities"
   GIT_REPOSITORY https://github.com/dgobbi/vtk-dicom
-  # vtk-dicom release 8.9.11
-  GIT_TAG 6d72f5ccf1340d695a8fe36e9200aa60c09a910a
+  # vtk-dicom release 8.9.12 plus undef warning fix
+  GIT_TAG 8a337caf3765d0127e2dbc450834c8d5215f8b78
   )
