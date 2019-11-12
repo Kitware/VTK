c3f2ea433c6dd547b997bc7e3822df1efe7f526a
Ben Boeckel <ben.boeckel@kitware.com>
2019-11-12 11:02:12 -0500

    vtk-config: provide compatibility for VTK_USE_FILE
    Fixes: #17725

---
+++ CMake/vtk-use-file-deprecated.cmake
@@ -0,0 +1,2 @@
+message(DEPRECATION
+  "The `VTK_USE_FILE` is no longer used starting with 8.90.")
---
+++ CMake/vtk-use-file-error.cmake
@@ -0,0 +1,4 @@
+message(FATAL_ERROR
+  "The `VTK_USE_FILE` is no longer used starting with 8.90. The "
+  "`include(\${VTK_USE_FILE})` line is no longer necessary and can just be "
+  "removed.")
