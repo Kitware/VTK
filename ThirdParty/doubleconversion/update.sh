5c5815f975a150a3db3109c0547bb40c5c2e8384
Ben Boeckel <ben.boeckel@kitware.com>
2019-12-30 10:58:12 -0500

    doubleconversion: update to 3.1.5

--- ThirdParty/doubleconversion/update.sh
+++ ThirdParty/doubleconversion/update.sh
@@ -8,7 +8,7 @@ readonly name="doubleconversion"
 readonly ownership="double-conversion Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/double-conversion.git"
-readonly tag="for/vtk-20190605-3.1.5"
+readonly tag="for/vtk-20191226-3.1.5"
 readonly paths="
 AUTHORS
 Changelog
