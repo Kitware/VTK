31bfc2f931b24a49b4a392661263a95a074ddcb8
Ken Martin <ken.martin@kitware.com>
2020-01-07 12:15:35 -0500

    Update remote module to get compiler fixes
    Fix/suppress compiler warnings

--- Remote/SplineDrivenImageSlicer.remote.cmake
+++ Remote/SplineDrivenImageSlicer.remote.cmake
@@ -4,6 +4,6 @@
 
 vtk_fetch_module(SplineDrivenImageSlicer
         "Spline Driven Image Slicer - http://www.vtkjournal.org/browse/publication/838"
-        GIT_REPOSITORY https://github.com/lorensen/midas-journal-838.git
-        GIT_TAG ea400844f97bde1363f30180dde201fd69605dc6
+        GIT_REPOSITORY https://github.com/martinken/midas-journal-838.git
+        GIT_TAG 5a98940c705748df5c2470331541a051559956c8
         )
