7b9cc439dc93debdb7c24366e27be2086bb76e2a
Cory Quammen <cory.quammen@kitware.com>
2020-02-12 17:09:29 -0500

    Disable parallel tests when CUDA is enabled
    The tests were failing, so they have been disabled in the remote
    project. Update the commit SHA to disable these tests when the project
    is enabled.

--- Remote/MomentInvariants.remote.cmake
+++ Remote/MomentInvariants.remote.cmake
@@ -5,5 +5,5 @@
 vtk_fetch_module(MomentInvariants
   "MomentInvariants filter"
   GIT_REPOSITORY https://gitlab.kitware.com/vtk/MomentInvariants.git
-  GIT_TAG 569868df2fc283d1c8abff908f2973d63b48f536
+  GIT_TAG 9b83e572687ee84fd6c4c4c63e32807d280119be
   )
