83cffda8193e4d6b48d6e3e70853b92cee96d7e8
Kenneth Moreland <kmorel@sandia.gov>
2020-03-25 17:48:59 -0600

    Update the MomentInvariants remote module for VTK-m changes

--- Remote/MomentInvariants.remote.cmake
+++ Remote/MomentInvariants.remote.cmake
@@ -5,5 +5,5 @@
 vtk_fetch_module(MomentInvariants
   "MomentInvariants filter"
   GIT_REPOSITORY https://gitlab.kitware.com/vtk/MomentInvariants.git
-  GIT_TAG 9b83e572687ee84fd6c4c4c63e32807d280119be
+  GIT_TAG 3e726b23e33134ff6765fe2d62230300d426efcb
   )
