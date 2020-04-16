1701dbc9ad6279b23cdeed8b70edfe30b24ad507
Ben Boeckel <ben.boeckel@kitware.com>
2020-04-16 16:03:16 -0400

    vtkWheel: simplify feature accumulation

--- CMake/vtkWheelFinalization.cmake
+++ CMake/vtkWheelFinalization.cmake
@@ -1,65 +1,58 @@
-set(vtk_feature_entries "")
+set(vtk_features)
 
 if (TARGET VTK::mpi)
-  string(APPEND vtk_feature_entries
-    "    'mpi': [],\n")
+  list(APPEND vtk_features "mpi")
 endif ()
 
 if (TARGET VTK::RenderingCore)
-  string(APPEND vtk_feature_entries
-    "    'rendering': [],\n")
+  list(APPEND vtk_features "rendering")
 endif ()
 
 if (TARGET VTK::RenderingOpenVR)
-  string(APPEND vtk_feature_entries
-    "    'openvr': [],\n")
+  list(APPEND vtk_features "openvr")
 endif ()
 
 if (TARGET VTK::RenderingOpenGL2)
   set(has_onscreen OFF)
   set(has_offscreen OFF)
   if (VTK_USE_X)
-    string(APPEND vtk_feature_entries
-      "    'rendering-onscreen-x11': [],\n")
+    list(APPEND vtk_features "rendering-onscreen-x11")
     set(has_onscreen ON)
   endif ()
   if (VTK_USE_COCOA)
-    string(APPEND vtk_feature_entries
-      "    'rendering-onscreen-cocoa': [],\n")
+    list(APPEND vtk_features "rendering-onscreen-cocoa")
     set(has_onscreen ON)
   endif ()
   if (WIN32)
-    string(APPEND vtk_feature_entries
-      "    'rendering-onscreen-windows': [],\n")
+    list(APPEND vtk_features "rendering-onscreen-windows")
     set(has_onscreen ON)
   endif ()
   if (VTK_OPENGL_HAS_OSMESA)
-    string(APPEND vtk_feature_entries
-      "    'rendering-offscreen-osmesa': [],\n")
+    list(APPEND vtk_features "rendering-offscreen-osmesa")
     set(has_offscreen ON)
   endif ()
 
   if (has_onscreen)
-    string(APPEND vtk_feature_entries
-      "    'rendering-onscreen': [],\n")
+    list(APPEND vtk_features "rendering-onscreen")
   endif ()
   if (has_offscreen)
-    string(APPEND vtk_feature_entries
-      "    'rendering-offscreen': [],\n")
+    list(APPEND vtk_features "rendering-offscreen")
   endif ()
 
   if (VTK_OPENGL_USE_GLES)
-    string(APPEND vtk_feature_entries
-      "    'rendering-backend-gles': [],\n")
+    list(APPEND vtk_features "rendering-backend-gles")
   else ()
-    string(APPEND vtk_feature_entries
-      "    'rendering-backend-gl': [],\n")
+    list(APPEND vtk_features "rendering-backend-gl")
   endif ()
   if (VTK_OPENGL_HAS_EGL)
-    string(APPEND vtk_feature_entries
-      "    'rendering-backend-egl': [],\n")
+    list(APPEND vtk_features "rendering-backend-egl")
   endif ()
 endif ()
 
+set(vtk_feature_entries "")
+foreach (vtk_feature IN LISTS vtk_features)
+  string(APPEND vtk_feature_entries
+    "    '${vtk_feature}': [],\n")
+endforeach ()
 file(WRITE "${CMAKE_BINARY_DIR}/vtk_features.py"
   "FEATURES = {\n${vtk_feature_entries}}\n")
