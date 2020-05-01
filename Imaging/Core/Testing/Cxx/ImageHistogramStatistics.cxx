d068829e2726716f92fa2a1c6c574ba018ae834f
Andras Lasso <lasso@queensu.ca>
2020-05-01 01:36:14 -0400

    BUG: Fix crash in vtkImageHistogram if input contains NaN
    vtkImageHistogram caused application crash when input image contained NaN value
    using Intel TBB backend, Visual Studio 2017, x64.
    
    The problem was that when the image contained a NaN value then the computed scalar range was set to (Nan, Nan),
    which later got converted to invalid bin min/max indexes. The safety check that should have forced
    the indices to the valid range did not work, as it relied on comparison with a NaN value.
    
    Fixed computation of scalar range (similar to how it is done here:
    https://gitlab.kitware.com/vtk/vtk/-/commit/06d553fb232ac3992b99df47613506f106d2f46d), by changing
    the order of operands in the ternary operator.
    
    To make sure that similar error do not cause crash in the future, added a NaN-value check in the bin range computation, too.
    
    Added test case to verify that the results are correct and the application does not crash anymore if input image contains NaN.

--- Imaging/Core/Testing/Cxx/ImageHistogramStatistics.cxx
+++ Imaging/Core/Testing/Cxx/ImageHistogramStatistics.cxx
@@ -17,9 +17,13 @@
 // The command line arguments are:
 // -I        => run in interactive mode
 
+#include "vtkFloatingPointExceptions.h"
 #include "vtkImageAccumulate.h"
 #include "vtkImageCast.h"
+#include "vtkImageData.h"
 #include "vtkImageHistogramStatistics.h"
+#include "vtkMath.h"
+#include "vtkNew.h"
 #include "vtkPNGReader.h"
 
 #include "vtkTestUtilities.h"
@@ -28,7 +32,7 @@
 
 int ImageHistogramStatistics(int argc, char* argv[])
 {
-  vtkPNGReader* reader = vtkPNGReader::New();
+  vtkNew<vtkPNGReader> reader;
 
   char* fname = vtkTestUtilities::ExpandDataFileName(argc, argv, "Data/fullhead15.png");
 
@@ -36,7 +40,7 @@ int ImageHistogramStatistics(int argc, char* argv[])
   delete[] fname;
 
   // Use float data to get the most code coverage
-  vtkImageCast* imageCast = vtkImageCast::New();
+  vtkNew<vtkImageCast> imageCast;
   imageCast->SetOutputScalarTypeToFloat();
   imageCast->SetInputConnection(reader->GetOutputPort());
 
@@ -47,7 +51,7 @@ int ImageHistogramStatistics(int argc, char* argv[])
   double stdevTest = 660.9126299774935;
   double tol = 1e-6;
 
-  vtkImageHistogramStatistics* statistics = vtkImageHistogramStatistics::New();
+  vtkNew<vtkImageHistogramStatistics> statistics;
   statistics->SetInputConnection(imageCast->GetOutputPort());
   statistics->GenerateHistogramImageOff();
   statistics->Update();
@@ -60,7 +64,7 @@ int ImageHistogramStatistics(int argc, char* argv[])
 
   // uncomment to test vtkImageAccumulate instead
   /*
-    vtkImageAccumulate *accumulate = vtkImageAccumulate::New();
+    vtkNew<vtkImageAccumulate> accumulate;
     accumulate->SetInputConnection(reader->GetOutputPort());
     accumulate->Update();
 
@@ -104,9 +108,74 @@ int ImageHistogramStatistics(int argc, char* argv[])
     retVal = false;
   }
 
-  reader->Delete();
-  imageCast->Delete();
-  statistics->Delete();
+  // Make sure histogram computation does not crash if image has NaN pixel
+
+  // Clone the input image
+  vtkNew<vtkImageData> imageDataWitNaN;
+  imageDataWitNaN->DeepCopy(imageCast->GetOutput());
+  double rangeOriginal[2];
+  imageDataWitNaN->GetScalarRange(rangeOriginal);
+
+  // Set pixel value at (1,1,0) position to NaN
+  imageDataWitNaN->SetScalarComponentFromDouble(1, 1, 0, 0, vtkMath::Nan());
+
+  // Verify that scalar range is still computed correctly
+
+  // GetScalarRange() would crash due to invalid floating-point operation, therefore we
+  // need to disable floating-point exceptions here.
+  vtkFloatingPointExceptions::Disable();
+
+  double rangeWithNaN[2];
+  imageDataWitNaN->GetScalarRange(rangeWithNaN);
+  if (fabs((rangeOriginal[0] - rangeWithNaN[0]) / rangeOriginal[0]) > tol)
+  {
+    cout.precision(16);
+    cout << "rangeWithNaN[0] " << rangeWithNaN[0] << " should be " << rangeOriginal[0] << endl;
+    retVal = false;
+  }
+  if (fabs((rangeOriginal[1] - rangeWithNaN[1]) / rangeOriginal[1]) > tol)
+  {
+    cout.precision(16);
+    cout << "rangeWithNaN[1] " << rangeWithNaN[1] << " should be " << rangeOriginal[1] << endl;
+    retVal = false;
+  }
+
+  // Verify that the filter does not crash
+  statistics->SetInputData(imageDataWitNaN);
+  statistics->Update();
+
+  // Verify that the results are still the same (one pixel should not cause perceivable difference)
+  // (exact same tests as above)
+  if (fabs((minVal - minValTest) / maxValTest) > tol)
+  {
+    cout.precision(16);
+    cout << "minVal " << minVal << " should be " << minValTest << endl;
+    retVal = false;
+  }
+  if (fabs((maxVal - maxValTest) / maxValTest) > tol)
+  {
+    cout.precision(16);
+    cout << "maxVal " << maxVal << " should be " << maxValTest << endl;
+    retVal = false;
+  }
+  if (fabs((meanVal - meanValTest) / maxValTest) > tol)
+  {
+    cout.precision(16);
+    cout << "meanVal " << meanVal << " should be " << meanValTest << endl;
+    retVal = false;
+  }
+  if (fabs((median - medianTest) / maxValTest) > tol)
+  {
+    cout.precision(16);
+    cout << "median " << median << " should be " << medianTest << endl;
+    retVal = false;
+  }
+  if (fabs((stdev - stdevTest) / maxValTest) > tol)
+  {
+    cout.precision(16);
+    cout << "stdev " << stdev << " should be " << stdevTest << endl;
+    retVal = false;
+  }
 
   return !retVal;
 }
