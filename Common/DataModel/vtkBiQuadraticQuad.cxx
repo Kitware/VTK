5637fe7c1cdb1c18d97645570d7c512e8f6d9f30
florian maurin <florian@coreform.com>
2020-05-04 17:31:57 +0200

    add and clean Derivatives
    (cherry picked from commit 7f55485dedb0f3a95abfb12cfe19668a01f96553)

--- Common/DataModel/vtkBiQuadraticQuad.cxx
+++ Common/DataModel/vtkBiQuadraticQuad.cxx
@@ -309,16 +309,10 @@ int vtkBiQuadraticQuad::Triangulate(int vtkNotUsed(index), vtkIdList* ptIds, vtk
 void vtkBiQuadraticQuad::Derivatives(
   int vtkNotUsed(subId), const double pcoords[3], const double* values, int dim, double* derivs)
 {
-  double sum[3], weights[9];
+  double sum[2], p[3], weights[9];
   double functionDerivs[18];
-  double elemNodes[9][3];
   double *J[3], J0[3], J1[3], J2[3];
   double *JI[3], JI0[3], JI1[3], JI2[3];
-
-  for (int i = 0; i < 9; i++)
-  {
-    this->Points->GetPoint(i, elemNodes[i]);
-  }
 
   this->InterpolationFunctionsPrivate(pcoords, weights);
   this->InterpolationDerivsPrivate(pcoords, functionDerivs);
@@ -337,11 +331,12 @@ void vtkBiQuadraticQuad::Derivatives(
 
   for (int i = 0; i < 9; i++)
   {
+    this->Points->GetPoint(i, p);
     for (int j = 0; j < 2; j++)
     {
       for (int k = 0; k < 3; k++)
       {
-        J[j][k] += elemNodes[i][k] * functionDerivs[j * 9 + i];
+        J[j][k] += p[k] * functionDerivs[j * 9 + i];
       }
     }
   }
@@ -367,7 +362,7 @@ void vtkBiQuadraticQuad::Derivatives(
   // First compute derivatives in local x'-y' coordinate system
   for (int j = 0; j < dim; j++)
   {
-    sum[0] = sum[1] = sum[2] = 0.0;
+    sum[0] = sum[1] = 0.0;
     for (int i = 0; i < 9; i++) // loop over interp. function derivatives
     {
       sum[0] += functionDerivs[i] * values[dim * i + j];
