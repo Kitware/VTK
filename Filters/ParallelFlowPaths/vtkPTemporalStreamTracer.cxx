727df4458fd0ee1655fe9990dff35a3621a9c8ad
Ben Boeckel <ben.boeckel@kitware.com>
2020-05-13 15:22:35 -0400

    vtkTemporalStreamTracer: use deprecation

--- Filters/ParallelFlowPaths/vtkPTemporalStreamTracer.cxx
+++ Filters/ParallelFlowPaths/vtkPTemporalStreamTracer.cxx
@@ -12,6 +12,10 @@ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
+
+// Hide VTK_DEPRECATED_IN_9_0_0() warnings for this class.
+#define VTK_DEPRECATION_LEVEL 0
+
 #include "vtkPTemporalStreamTracer.h"
 #include "vtkTemporalStreamTracer.h"
 
