33301c5c4d24118ab8b8dc8b405949c3bf9aa2f3
Yohann Bearzi <yohann.bearzi@kitware.com>
2020-07-07 17:52:20 -0400

    Fix a bug in vtkPyramid::EvaluatePosition
    dist2 of vtkPyramid::EvaluatePosition was not set correctly if
    closestPoint == nullptr.
    
    Addresses paraview/paraview#19856

--- Filters/Core/Testing/Cxx/TestResampleToImage.cxx
+++ Filters/Core/Testing/Cxx/TestResampleToImage.cxx
@@ -17,22 +17,27 @@
 
 #include "vtkResampleToImage.h"
 
+#include "vtkCell.h"
+#include "vtkCellTypeSource.h"
+#include "vtkCharArray.h"
 #include "vtkClipDataSet.h"
+#include "vtkDataArrayRange.h"
 #include "vtkDataSetAttributes.h"
 #include "vtkExtractVOI.h"
 #include "vtkImageData.h"
 #include "vtkNew.h"
+#include "vtkPlane.h"
+#include "vtkPointData.h"
 #include "vtkRTAnalyticSource.h"
 #include "vtkResampleToImage.h"
 #include "vtkUnsignedCharArray.h"
-
-#include "vtkCell.h"
-#include "vtkCellType.h"
 #include "vtkUnstructuredGrid.h"
 #include <iostream>
 
 int TestResampleToImage(int, char*[])
 {
+  int status = 0;
+
   // Create Pipeline
   vtkNew<vtkRTAnalyticSource> wavelet;
   wavelet->SetWholeExtent(0, 16, 0, 16, 0, 16);
@@ -59,7 +64,7 @@ int TestResampleToImage(int, char*[])
   {
     std::cout << "Number of points: expecting 13824, got " << numPoints << std::endl;
     std::cout << "Number of cells: expecting 12167, got " << numCells << std::endl;
-    return 1;
+    status = 1;
   }
 
   vtkUnsignedCharArray* pointGhostArray = output->GetPointGhostArray();
@@ -75,7 +80,7 @@ int TestResampleToImage(int, char*[])
   if (numHiddenPoints != 2000)
   {
     std::cout << "Number of Hidden points: expecting 2000 got " << numHiddenPoints << std::endl;
-    return 1;
+    status = 1;
   }
 
   vtkUnsignedCharArray* cellGhostArray = output->GetCellGhostArray();
@@ -91,8 +96,44 @@ int TestResampleToImage(int, char*[])
   if (numHiddenCells != 2171)
   {
     std::cout << "Number of Hidden cells: expecting 2171 got " << numHiddenCells << std::endl;
-    return 1;
+    status = 1;
   }
 
-  return 0;
+  // Test for ParaView issue #19856
+
+  vtkNew<vtkCellTypeSource> cellTypeSource;
+  cellTypeSource->SetCellOrder(1);
+  cellTypeSource->SetCellType(VTK_HEXAHEDRON);
+  cellTypeSource->SetBlocksDimensions(10, 10, 10);
+
+  vtkNew<vtkPlane> plane;
+  plane->SetOrigin(7.786580522057762, 5.87458457259413, 6.314673922104045);
+  plane->SetNormal(0.8700294812256526, 0.27306383431551773, 0.4104690538726755);
+
+  vtkNew<vtkClipDataSet> clip2;
+  clip2->SetInputConnection(cellTypeSource->GetOutputPort());
+  clip2->SetClipFunction(plane);
+
+  vtkNew<vtkResampleToImage> resample2;
+  resample2->SetInputConnection(clip2->GetOutputPort());
+  resample2->SetSamplingDimensions(100, 100, 100);
+  resample2->Update();
+
+  // We look for the number of empty voxels
+  vtkPointData* cd = vtkDataSet::SafeDownCast(resample2->GetOutputDataObject(0))->GetPointData();
+  vtkCharArray* validPoints =
+    vtkCharArray::SafeDownCast(cd->GetAbstractArray(resample2->GetMaskArrayName()));
+  auto values = vtk::DataArrayValueRange(validPoints);
+  vtkIdType zeros = 0;
+  for (const auto val : values)
+  {
+    zeros += !val;
+  }
+  if (zeros != 744725)
+  {
+    std::cout << "Caught " << zeros << " invalid points, it should have been 744725" << std::endl;
+    status = 1;
+  }
+
+  return status;
 }
