d556228acb6c616527b1fce0e21ce8543ea92fdb
Yohann Bearzi <yohann.bearzi@kitware.com>
2020-07-15 11:37:30 -0400

    Moment Invariant bump (for Accelerator new API)

--- Remote/MomentInvariants.remote.cmake
+++ Remote/MomentInvariants.remote.cmake
@@ -5,5 +5,5 @@
 vtk_fetch_module(MomentInvariants
   "MomentInvariants filter"
   GIT_REPOSITORY https://gitlab.kitware.com/vtk/MomentInvariants.git
-  GIT_TAG 6bd847ad4851ea5bd2910233fcb307305a02f106
+  GIT_TAG 9f103291cd96c2757dbefa6a7a0bea1bb8cd9abf
   )
