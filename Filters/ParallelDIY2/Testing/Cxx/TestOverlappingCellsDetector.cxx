91cceeccafd350a2e5936d7877ed2d4beb4a5f5f
Yohann Bearzi <yohann.bearzi@kitware.com>
2020-08-03 18:38:28 -0400

    vtkOverlappingCellsDetector DataObjectTree aware
    One can now detect cell overlaps on multi block / partitioned inputs

--- Filters/ParallelDIY2/Testing/Cxx/TestOverlappingCellsDetector.cxx
+++ Filters/ParallelDIY2/Testing/Cxx/TestOverlappingCellsDetector.cxx
@@ -20,16 +20,20 @@
 #endif
 
 #include "vtkCellData.h"
+#include "vtkCompositeDataIterator.h"
 #include "vtkDataArray.h"
 #include "vtkDataArrayRange.h"
 #include "vtkDataSet.h"
 #include "vtkGenerateGlobalIds.h"
+#include "vtkLogger.h"
+#include "vtkMultiBlockDataSet.h"
 #include "vtkMultiProcessController.h"
 #include "vtkNew.h"
 #include "vtkOverlappingCellsDetector.h"
 #include "vtkRedistributeDataSetFilter.h"
 #include "vtkTestUtilities.h"
 #include "vtkUnstructuredGrid.h"
+#include "vtkXMLMultiBlockDataReader.h"
 #include "vtkXMLUnstructuredGridReader.h"
 
 namespace
@@ -44,8 +48,30 @@ static constexpr vtkIdType Collisions[72] = { 6, 0, 6, 0, 4, 4, 6, 0, 10, 7, // 
   0, 0 };
 }
 
+static constexpr vtkIdType CollisionsBlocks[144] = { 29, 13, 27, 12, 12, 13, 20, 7, 30, 22, // 0
+  13, 7, 3, 21, 25, 4, 10, 19, 20, 12,                                                      // 10
+  10, 8, 25, 29, 8, 18, 14, 23, 12, 14,                                                     // 20
+  1, 15, 1, 5, 15, 26, 25, 20, 4, 28,                                                       // 30
+  15, 1, 7, 17, 27, 24, 8, 14, 9, 12,                                                       // 40
+  12, 13, 4, 20, 5, 15, 3, 8, 8, 3,                                                         // 50
+  21, 5, 7, 10, 10, 5, 12, 23, 9, 10,                                                       // 60
+  10, 12, 26, 10, 23, 14, 12, 11, 21, 3,                                                    // 70
+  31, 21, 13, 3, 5, 19, 26, 4, 5, 22,                                                       // 80
+  20, 10, 12, 10, 18, 27, 13, 31, 12, 21,                                                   // 90
+  17, 10, 7, 20, 7, 1, 12, 28, 18, 22,                                                      // 100
+  2, 24, 26, 8, 12, 15, 25, 16, 8, 12,                                                      // 110
+  14, 11, 10, 10, 9, 18, 5, 20, 5, 4,                                                       // 120
+  4, 11, 20, 7, 3, 7, 10, 10, 14, 22,                                                       // 130
+  9, 8, 5, 10 };
+
 int TestOverlappingCellsDetector(int argc, char* argv[])
 {
+  vtkLogger::Init(argc, argv);
+
+  // Put every log message in "everything.log":
+  vtkLogger::LogToFile("~/Documents/ParaView/vtk_build_mpi_test/everything.log", vtkLogger::APPEND,
+    vtkLogger::VERBOSITY_MAX);
+
 #if VTK_MODULE_ENABLE_VTK_ParallelMPI
   vtkNew<vtkMPIController> contr;
 #else
@@ -57,12 +83,15 @@ int TestOverlappingCellsDetector(int argc, char* argv[])
   int retVal = EXIT_SUCCESS;
   int myrank = contr->GetLocalProcessId();
 
-  const char* Tetname =
-    vtkTestUtilities::ExpandDataFileName(argc, argv, "Data/overlapping_tetras.vtu");
+  const char* Tetname = vtkTestUtilities::ExpandDataFileName(
+    argc, argv, "Data/multiblock_overlapping_tetras/multiblock_overlapping_tetras_0_0.vtu");
+  const char* MultiBlockTetname =
+    vtkTestUtilities::ExpandDataFileName(argc, argv, "Data/multiblock_overlapping_tetras.vtm");
   const char* Hexname = vtkTestUtilities::ExpandDataFileName(argc, argv, "Data/linhex.vtu");
 
   vtkNew<vtkGenerateGlobalIds> globalIds;
 
+  vtkLogStartScope(TRACE, "Overlapping Tetras");
   if (myrank == 0)
   {
     vtkNew<vtkXMLUnstructuredGridReader> reader;
@@ -84,21 +113,71 @@ int TestOverlappingCellsDetector(int argc, char* argv[])
   detector->SetInputConnection(redistribute->GetOutputPort());
   detector->Update();
 
-  vtkDataSet* output = vtkDataSet::SafeDownCast(detector->GetOutput(0));
-  vtkDataArray* data =
-    output->GetCellData()->GetArray(detector->GetNumberOfOverlapsPerCellArrayName());
-  vtkDataArray* ids = output->GetCellData()->GetArray("GlobalCellIds");
-
-  auto valIt = vtk::DataArrayValueRange<1>(data).cbegin();
-  auto idIt = vtk::DataArrayValueRange<1>(ids).cbegin();
-  for (; valIt != vtk::DataArrayValueRange<1>(data).cend(); ++valIt, ++idIt)
   {
-    if (Collisions[static_cast<vtkIdType>(*idIt)] != *valIt)
+    vtkDataSet* output = vtkDataSet::SafeDownCast(detector->GetOutput(0));
+    vtkDataArray* data =
+      output->GetCellData()->GetArray(detector->GetNumberOfOverlapsPerCellArrayName());
+    vtkDataArray* ids = output->GetCellData()->GetArray("GlobalCellIds");
+
+    auto valIt = vtk::DataArrayValueRange<1>(data).cbegin();
+    auto idIt = vtk::DataArrayValueRange<1>(ids).cbegin();
+    for (; valIt != vtk::DataArrayValueRange<1>(data).cend(); ++valIt, ++idIt)
     {
-      retVal = EXIT_FAILURE;
-      break;
+      if (Collisions[static_cast<vtkIdType>(*idIt)] != *valIt)
+      {
+        std::cerr << "Overlapping cells detector failed with an unstructured grid" << std::endl;
+        retVal = EXIT_FAILURE;
+        break;
+      }
     }
   }
+
+  vtkLogEndScope("Overlapping Tetras");
+  vtkLogStartScope(TRACE, "MultiBlock Overlapping Tetras");
+
+  if (myrank == 0)
+  {
+    vtkNew<vtkXMLMultiBlockDataReader> reader;
+    reader->SetFileName(MultiBlockTetname);
+    globalIds->SetInputConnection(reader->GetOutputPort());
+    globalIds->Update();
+  }
+  else
+  {
+    vtkNew<vtkMultiBlockDataSet> mbds;
+    mbds->Initialize();
+    globalIds->SetInputDataObject(mbds);
+  }
+
+  detector->SetInputConnection(globalIds->GetOutputPort());
+  detector->Update();
+
+  vtkMultiBlockDataSet* outputs = vtkMultiBlockDataSet::SafeDownCast(detector->GetOutput(0));
+  auto iter = outputs->NewIterator();
+  for (iter->InitTraversal(); !iter->IsDoneWithTraversal(); iter->GoToNextItem())
+  {
+    if (auto output = vtkDataSet::SafeDownCast(iter->GetCurrentDataObject()))
+    {
+      vtkDataArray* data =
+        output->GetCellData()->GetArray(detector->GetNumberOfOverlapsPerCellArrayName());
+      vtkDataArray* ids = output->GetCellData()->GetArray("GlobalCellIds");
+      auto valIt = vtk::DataArrayValueRange<1>(data).cbegin();
+      auto idIt = vtk::DataArrayValueRange<1>(ids).cbegin();
+      for (; valIt != vtk::DataArrayValueRange<1>(data).cend(); ++valIt, ++idIt)
+      {
+        if (CollisionsBlocks[static_cast<vtkIdType>(*idIt)] != *valIt)
+        {
+          std::cerr << "Overlapping cells detector failed with a multi-block input" << std::endl;
+          retVal = EXIT_FAILURE;
+          break;
+        }
+      }
+    }
+  }
+  iter->Delete();
+
+  vtkLogEndScope("MultiBlock Overlapping Tetras");
+  vtkLogStartScope(TRACE, "Overlapping Hexes");
 
   // Here we test a data set that used to produce an infinite loop
   // when computing the bounding sphere.
@@ -117,18 +196,25 @@ int TestOverlappingCellsDetector(int argc, char* argv[])
   }
   detector->Update();
 
-  output = vtkDataSet::SafeDownCast(detector->GetOutput(0));
-  data = output->GetCellData()->GetArray(detector->GetNumberOfOverlapsPerCellArrayName());
-  for (const auto val : vtk::DataArrayValueRange<1>(data))
   {
-    if (val)
+    vtkDataSet* output = vtkDataSet::SafeDownCast(detector->GetOutput(0));
+    vtkDataArray* data =
+      output->GetCellData()->GetArray(detector->GetNumberOfOverlapsPerCellArrayName());
+    for (const auto val : vtk::DataArrayValueRange<1>(data))
     {
-      retVal = EXIT_FAILURE;
-      break;
+      if (val)
+      {
+        std::cerr << "Overlapping cells detector detected overlaps on a non-overlapping dataset"
+                  << std::endl;
+        retVal = EXIT_FAILURE;
+        break;
+      }
     }
   }
+  vtkLogEndScope("Overlapping Hexes");
 
   delete[] Hexname;
+  delete[] MultiBlockTetname;
   delete[] Tetname;
 
   vtkMultiProcessController::SetGlobalController(nullptr);
