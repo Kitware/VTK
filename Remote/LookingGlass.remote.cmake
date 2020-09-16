14b4fe1c83d669df9d97ce2c9586a4fdbcc5eda3
Ken Martin <ken.martin@kitware.com>
2020-09-16 15:59:01 -0400

    Add LookingGlass remote module support
    Add support for the looking glass remote module
    see https://github.com/Kitware/LookingGlassVTKModule

---
+++ Remote/LookingGlass.remote.cmake
@@ -0,0 +1,9 @@
+#
+# Looking Glass Remote Module
+#
+
+vtk_fetch_module(LookingGlass
+        "Looking Glass Support for VTK"
+        GIT_REPOSITORY https://github.com/Kitware/LookingGlassVTKModule
+        GIT_TAG fe33ca91cef360bdb53d734540209e72b77e740d
+        )
