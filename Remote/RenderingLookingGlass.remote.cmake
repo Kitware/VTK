8c376ec4e2f8a04a510d4b4a59403569759c7476
Cory Quammen <cory.quammen@kitware.com>
2020-09-18 17:31:02 -0400

    Rename the remote module to be more specific

--- Remote/RenderingLookingGlass.remote.cmake
+++ Remote/RenderingLookingGlass.remote.cmake
@@ -2,7 +2,7 @@
 # Looking Glass Remote Module
 #
 
-vtk_fetch_module(LookingGlass
+vtk_fetch_module(RenderingLookingGlass
         "Looking Glass Support for VTK"
         GIT_REPOSITORY https://github.com/Kitware/LookingGlassVTKModule
         GIT_TAG fe33ca91cef360bdb53d734540209e72b77e740d
