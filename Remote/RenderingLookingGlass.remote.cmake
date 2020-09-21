8bdd0f20d21eff8a18394f1c710020f3bb59dc83
Cory Quammen <cory.quammen@kitware.com>
2020-09-20 23:27:12 -0400

    Update the Looking Glass commit

--- Remote/RenderingLookingGlass.remote.cmake
+++ Remote/RenderingLookingGlass.remote.cmake
@@ -5,5 +5,5 @@
 vtk_fetch_module(RenderingLookingGlass
         "Looking Glass Support for VTK"
         GIT_REPOSITORY https://github.com/Kitware/LookingGlassVTKModule
-        GIT_TAG f624f3b3ffd4b00ebfa2b1fc0e50d144f1279c10
+        GIT_TAG c1e0df4842f22929a50daa0fe580a6a3dd3f015a
         )
