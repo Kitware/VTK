Kitware/VTK

commit 080ba5d74d05177200de7d48f443b365581dab6e

macos: implement setting of DisplayId
Previously, calls to SetDisplayId() were ignored. Implement it and use
the ID to pick the screen on which the vtkCocoaRenderWindow window
appears.

Rendering/OpenGL2/vtkCocoaRenderWindow.mm
@@ -21,6 +21,7 @@ PURPOSE.  See the above copyright notice for more information.
 #import "vtkCocoaMacOSXSDKCompatibility.h" // Needed to support old SDKs
 #import "vtkOpenGLRenderWindow.h"
 #import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 #import "vtkCocoaGLView.h"
 #import "vtkCocoaRenderWindow.h"
@@ -705,10 +706,22 @@ void vtkCocoaRenderWindow::CreateAWindow()
     (void)[app setActivationPolicy:NSApplicationActivationPolicyRegular];
 
     NSWindow* theWindow = nil;
-
+    NSScreen* screen = nil;
     // Get the screen's size (in points).  (If there's no mainScreen, the
     // rectangle will become all zeros and not used anyway.)
-    NSScreen* screen = [NSScreen mainScreen];
+    @try
+    {
+      screen = [[NSScreen screens] objectAtIndex:this->DisplayIndex];
+    }
+    @catch (NSException*)
+    {
+    }
+
+    if (!screen)
+    {
+      // Revert to main screen if specified screen isn't available.
+      screen = [NSScreen mainScreen];
+    }
     NSRect screenRect = [screen frame];
 
     // Convert from points to pixels.
@@ -1106,10 +1119,22 @@ int* vtkCocoaRenderWindow::GetScreenSize()
   NSWindow* window = [view window];
   NSScreen* screen = [window screen];
 
-  // If screen is nil, then fall back to mainScreen, which CreateAWindow()
-  // also uses (it could be nil too).
+  // If screen is nil, then fall back to the screen with index DisplayIndex,
+  // which CreateAWindow() also uses (it could be nil too).
   if (!screen)
   {
+    @try
+    {
+      screen = [[NSScreen screens] objectAtIndex:this->DisplayIndex];
+    }
+    @catch (NSException*)
+    {
+    }
+  }
+
+  if (!screen)
+  {
+    // Revert to main screen if specified screen isn't available.
     screen = [NSScreen mainScreen];
   }
 

Rendering/OpenGL2/vtkCocoaRenderWindow.h
@@ -153,11 +153,19 @@ public:
     vtkDebugMacro("Method not implemented.");
     return nullptr;
   }
-  void SetDisplayId(void*) override
+
+  /**
+   * Set the index of the NSScreen on which the window should be created.
+   * This is useful for creating the render window on secondary displays.
+   * By default, the DisplayId is 0, meaning the window will appear on
+   * the main screen. This function must be called before the window is
+   * created.
+   */
+  void SetDisplayId(void* displayId) override
   {
-    // no-op
-    vtkDebugMacro("Method not implemented.");
+    this->DisplayIndex = displayId ? *(reinterpret_cast<int*>(displayId)) : 0;
   }
+
   void* GetGenericDisplayId() override
   {
     vtkDebugMacro("Method not implemented.");
@@ -439,6 +447,8 @@ private:
 
   bool WantsBestResolution;
   bool ConnectContextToNSView;
+
+  int DisplayIndex = 0;
 };
 
 #endif

commit c30d90ab927a231fd5e2fae5a0bc9cbc0eff6131

Fixed out-of-bounds NSArray access by checking index first
Bug introduced in 080ba5d74d05177200de7d48f443b365581dab6e.
Unlike other languages, in Obj-C it’s invalid to access past the end of an array. Similarly, exception handling isn’t mean for normal control flow.

Rendering/OpenGL2/vtkCocoaRenderWindow.mm
@@ -21,7 +21,6 @@ PURPOSE.  See the above copyright notice for more information.
 #import "vtkCocoaMacOSXSDKCompatibility.h" // Needed to support old SDKs
 #import "vtkOpenGLRenderWindow.h"
 #import <Cocoa/Cocoa.h>
-#import <Foundation/Foundation.h>
 
 #import "vtkCocoaGLView.h"
 #import "vtkCocoaRenderWindow.h"
@@ -706,22 +705,21 @@ void vtkCocoaRenderWindow::CreateAWindow()
     (void)[app setActivationPolicy:NSApplicationActivationPolicyRegular];
 
     NSWindow* theWindow = nil;
-    NSScreen* screen = nil;
-    // Get the screen's size (in points).  (If there's no mainScreen, the
-    // rectangle will become all zeros and not used anyway.)
-    @try
-    {
-      screen = [[NSScreen screens] objectAtIndex:this->DisplayIndex];
-    }
-    @catch (NSException*)
-    {
-    }
 
-    if (!screen)
+    // Revert to main screen if specified screen isn't available.
+    NSScreen* screen;
+    NSArray* allScreens = [NSScreen screens];
+    if (this->DisplayIndex >= 0 && (NSUInteger)this->DisplayIndex < [allScreens count])
     {
-      // Revert to main screen if specified screen isn't available.
+      screen = [allScreens objectAtIndex:this->DisplayIndex];
+    }
+    else
+    {
       screen = [NSScreen mainScreen];
     }
+
+    // Get the screen's size (in points).  (If there's no screen, the
+    // rectangle will become all zeros and not used anyway.)
     NSRect screenRect = [screen frame];
 
     // Convert from points to pixels.
@@ -1123,12 +1121,14 @@ int* vtkCocoaRenderWindow::GetScreenSize()
   // which CreateAWindow() also uses (it could be nil too).
   if (!screen)
   {
-    @try
+    NSArray* allScreens = [NSScreen screens];
+    if (this->DisplayIndex >= 0 && (NSUInteger)this->DisplayIndex < [allScreens count])
     {
-      screen = [[NSScreen screens] objectAtIndex:this->DisplayIndex];
+      screen = [allScreens objectAtIndex:this->DisplayIndex];
     }
-    @catch (NSException*)
+    else
     {
+      screen = [NSScreen mainScreen];
     }
   }
 