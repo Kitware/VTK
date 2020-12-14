Kitware/VTK

commit 16530928b2a19fcc9cdf69b324b81002adbb65a0

vtkCompositeDataSet: copy-structure fixes
vtkCompositeDataSet::CopyStructure would not copy data-information and
other meta-data. Since CopyStructure is often used in executive and
other filters in place of shallow-copy where leaf datasets are to be
shallow-copied, this causes meta-data like time to be dropped. This was
manifesting as paraview/paraview#20052.

Fixed that.

Common/DataModel/vtkDataObjectTree.cxx
@@ -156,6 +156,8 @@ void vtkDataObjectTree::CopyStructure(vtkCompositeDataSet* compositeSource)
     return;
   }
 
+  this->Superclass::CopyStructure(source);
+
   this->Internals->Children.clear();
   if (!source)
   {

Common/DataModel/vtkCompositeDataSet.cxx
@@ -46,6 +46,17 @@ vtkCompositeDataSet* vtkCompositeDataSet::GetData(vtkInformationVector* v, int i
 }
 
 //------------------------------------------------------------------------------
+void vtkCompositeDataSet::CopyStructure(vtkCompositeDataSet* input)
+{
+  if (input != this)
+  {
+    // copy data-information and other common stuff by calling
+    // superclass' ShallowCopy.
+    this->Superclass::ShallowCopy(input);
+  }
+}
+
+//------------------------------------------------------------------------------
 void vtkCompositeDataSet::ShallowCopy(vtkDataObject* src)
 {
   if (src == this)

Common/DataModel/vtkCompositeDataSet.h
@@ -64,7 +64,7 @@ public:
    * data objects are initialized to nullptr. This also shallow copies the meta data
    * associated with all the nodes.
    */
-  virtual void CopyStructure(vtkCompositeDataSet* input) = 0;
+  virtual void CopyStructure(vtkCompositeDataSet* input);
 
   /**
    * Sets the data set at the location pointed by the iterator.

Common/DataModel/vtkUniformGridAMR.cxx
@@ -307,6 +307,8 @@ void vtkUniformGridAMR::CopyStructure(vtkCompositeDataSet* src)
     return;
   }
 
+  this->Superclass::CopyStructure(src);
+
   if (vtkUniformGridAMR* hbds = vtkUniformGridAMR::SafeDownCast(src))
   {
     this->SetAMRInfo(hbds->GetAMRInfo());

commit 3c3a51d05af7b5905fe17bb3ecf8b414df71c57f

vtkDataObjectTree: fix shallow-copy bug
vtkDataObjectTree had a bug in shallow-copy introduced in 16530928b2.
This was causing paraview/paraview#20386. Fixed that.

Common/DataModel/vtkDataObjectTree.cxx
@@ -156,8 +156,7 @@ void vtkDataObjectTree::CopyStructure(vtkCompositeDataSet* compositeSource)
     return;
   }
 
-  this->Superclass::CopyStructure(source);
-
+  this->Superclass::CopyStructure(compositeSource);
   this->Internals->Children.clear();
   if (!source)
   {