8d0969c51844ab9ccb95ecb43caa9fe69f29929b
Ben Boeckel <ben.boeckel@kitware.com>
2020-12-21 13:55:15 -0500

    ci: add a Dockerfile for CI builds

---
+++ .gitlab/ci/docker/fedora33/install_cuda.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Install tools to manage repositories.
+dnf install -y --setopt=install_weak_deps=False \
+    'dnf-command(config-manager)'
+
+# Install the CUDA repository.
+dnf config-manager --add-repo \
+    https://developer.download.nvidia.com/compute/cuda/repos/fedora33/x86_64/cuda-fedora33.repo
+
+# CUDA toolchain
+dnf install -y --setopt=install_weak_deps=False \
+   cuda-compiler-11-2
+
+dnf clean all
---
+++ .gitlab/ci/docker/fedora33/install_deps.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# Install extra dependencies for VTK
+dnf install -y --setopt=install_weak_deps=False \
+   bzip2 patch git-core git-lfs
+
+# Documentation tools
+dnf install -y --setopt=install_weak_deps=False \
+    doxygen perl-Digest-MD5
+
+# Development tools
+dnf install -y --setopt=install_weak_deps=False \
+    libasan libubsan clang-tools-extra
+
+# MPI dependencies
+dnf install -y --setopt=install_weak_deps=False \
+   openmpi-devel mpich-devel
+
+# Qt dependencies
+dnf install -y --setopt=install_weak_deps=False \
+   qt5-qtbase-devel qt5-qttools-devel
+
+# Mesa dependencies
+dnf install -y --setopt=install_weak_deps=False \
+   mesa-libOSMesa-devel mesa-libOSMesa mesa-dri-drivers mesa-libGL* glx-utils
+
+# External dependencies
+dnf install -y --setopt=install_weak_deps=False \
+    libXcursor-devel libharu-devel utf8cpp-devel pugixml-devel libtiff-devel \
+    eigen3-devel double-conversion-devel lz4-devel expat-devel glew-devel \
+    hdf5-devel hdf5-mpich-devel hdf5-openmpi-devel hdf5-devel netcdf-devel \
+    netcdf-mpich-devel netcdf-openmpi-devel libogg-devel libtheora-devel \
+    jsoncpp-devel gl2ps-devel protobuf-devel libxkbcommon libxcrypt-compat \
+    boost-devel tbb-devel postgresql-server-devel libpq-devel mariadb-devel \
+    libiodbc-devel PDAL-devel liblas-devel openslide-devel libarchive-devel \
+    freeglut-devel sqlite-devel PEGTL-devel cgnslib-devel proj-devel \
+    wkhtmltopdf
+
+# Python dependencies
+dnf install -y --setopt=install_weak_deps=False \
+   python3-twisted python3-autobahn python3 python3-devel python3-numpy \
+   python3-pip python3-mpi4py-mpich python3-mpi4py-openmpi
+
+python3 -m pip install wslink
+
+# Java dependencies
+dnf install -y --setopt=install_weak_deps=False \
+    java-openjdk-devel
+
+# RPMFusion (for ffmpeg)
+dnf install -y --setopt=install_weak_deps=False \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-33.noarch.rpm
+
+# RPMFusion external dependencies
+dnf install -y --setopt=install_weak_deps=False \
+    ffmpeg-devel
+
+dnf clean all
