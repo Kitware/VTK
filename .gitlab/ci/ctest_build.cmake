82a3f5d83909b77ec2b18c2648f21596bf8b0d57
Ben Boeckel <ben.boeckel@kitware.com>
2021-01-08 09:13:18 -0500

    ci: upload logs as notes to CDash

--- .gitlab/ci/ctest_build.cmake
+++ .gitlab/ci/ctest_build.cmake
@@ -30,6 +30,15 @@ foreach (target IN LISTS targets_to_build)
   ctest_submit(PARTS Build)
 endforeach ()
 
+file(GLOB logs
+  "${CTEST_SOURCE_DIRECTORY}/compile_output.log"
+  "${CTEST_SOURCE_DIRECTORY}/doxygen_output.log"
+  "${CTEST_SOURCE_DIRECTORY}/prepare_output.log")
+if (logs)
+  list(APPEND CTEST_NOTES_FILES ${logs})
+  ctest_submit(PARTS Notes)
+endif ()
+
 if (build_result)
   message(FATAL_ERROR
     "Failed to build")
