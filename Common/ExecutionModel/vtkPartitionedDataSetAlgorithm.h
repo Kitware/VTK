d2d1b52d8f5ea6391b237b87bc3cb6cbec5ffa60
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2021-01-11 17:49:27 -0500

    Add base algorithms for partitioned dataset types
    Adding vtkPartitionedDataSetCollectionAlgorithm and
    vtkPartitionedDataSetAlgorithm.

---
+++ Common/ExecutionModel/vtkPartitionedDataSetAlgorithm.h
@@ -0,0 +1,82 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkPartitionedDataSetAlgorithm.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+/**
+ * @class vtkPartitionedDataSetAlgorithm
+ * @brief Superclass for algorithms that produce vtkPartitionedDataSetAlgorithm
+ *
+ * vtkPartitionedDataSetAlgorithm is intended as a superclass for algorithms that
+ * produce a vtkPartitionedDataSet.
+ */
+
+#ifndef vtkPartitionedDataSetAlgorithm_h
+#define vtkPartitionedDataSetAlgorithm_h
+
+#include "vtkAlgorithm.h"
+#include "vtkCommonExecutionModelModule.h" // For export macro
+
+class vtkPartitionedDataSet;
+
+class VTKCOMMONEXECUTIONMODEL_EXPORT vtkPartitionedDataSetAlgorithm : public vtkAlgorithm
+{
+public:
+  vtkTypeMacro(vtkPartitionedDataSetAlgorithm, vtkAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent) override;
+
+  //@{
+  /**
+   * Get the output data object for the specified output port.
+   */
+  vtkPartitionedDataSet* GetOutput();
+  vtkPartitionedDataSet* GetOutput(int);
+  //@}
+
+  vtkTypeBool ProcessRequest(
+    vtkInformation* request, vtkInformationVector** inInfo, vtkInformationVector* outInfo) override;
+
+protected:
+  vtkPartitionedDataSetAlgorithm();
+  ~vtkPartitionedDataSetAlgorithm() override;
+
+  //@{
+  /**
+   * Methods for subclasses to override to handle different pipeline requests.
+   */
+  virtual int RequestDataObject(vtkInformation*, vtkInformationVector**, vtkInformationVector*)
+  {
+    return 1;
+  }
+  virtual int RequestInformation(vtkInformation*, vtkInformationVector**, vtkInformationVector*)
+  {
+    return 1;
+  }
+  virtual int RequestData(vtkInformation*, vtkInformationVector**, vtkInformationVector*)
+  {
+    return 1;
+  }
+  virtual int RequestUpdateExtent(vtkInformation*, vtkInformationVector**, vtkInformationVector*)
+  {
+    return 1;
+  }
+  //@}
+
+  int FillOutputPortInformation(int port, vtkInformation* info) override;
+  int FillInputPortInformation(int port, vtkInformation* info) override;
+
+private:
+  vtkPartitionedDataSetAlgorithm(const vtkPartitionedDataSetAlgorithm&) = delete;
+  void operator=(const vtkPartitionedDataSetAlgorithm&) = delete;
+};
+
+#endif
---
+++ Common/ExecutionModel/vtkPartitionedDataSetCollectionAlgorithm.cxx
@@ -0,0 +1,93 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkPartitionedDataSetCollectionCollectionAlgorithm.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkPartitionedDataSetCollectionAlgorithm.h"
+
+#include "vtkInformation.h"
+#include "vtkObjectFactory.h"
+#include "vtkPartitionedDataSetCollection.h"
+#include "vtkStreamingDemandDrivenPipeline.h"
+
+//----------------------------------------------------------------------------
+vtkPartitionedDataSetCollectionAlgorithm::vtkPartitionedDataSetCollectionAlgorithm() {}
+
+//----------------------------------------------------------------------------
+vtkPartitionedDataSetCollectionAlgorithm::~vtkPartitionedDataSetCollectionAlgorithm() {}
+
+//----------------------------------------------------------------------------
+vtkPartitionedDataSetCollection* vtkPartitionedDataSetCollectionAlgorithm::GetOutput()
+{
+  return vtkPartitionedDataSetCollection::SafeDownCast(this->GetOutputDataObject(0));
+}
+
+//----------------------------------------------------------------------------
+vtkPartitionedDataSetCollection* vtkPartitionedDataSetCollectionAlgorithm::GetOutput(int port)
+{
+  return vtkPartitionedDataSetCollection::SafeDownCast(this->GetOutputDataObject(port));
+}
+
+//------------------------------------------------------------------------------
+vtkTypeBool vtkPartitionedDataSetCollectionAlgorithm::ProcessRequest(
+  vtkInformation* request, vtkInformationVector** inputVector, vtkInformationVector* outputVector)
+{
+  // create the output
+  if (request->Has(vtkStreamingDemandDrivenPipeline::REQUEST_DATA_OBJECT()))
+  {
+    return this->RequestDataObject(request, inputVector, outputVector);
+  }
+
+  // generate the data
+  if (request->Has(vtkStreamingDemandDrivenPipeline::REQUEST_DATA()))
+  {
+    int retVal = this->RequestData(request, inputVector, outputVector);
+    return retVal;
+  }
+
+  // execute information
+  if (request->Has(vtkStreamingDemandDrivenPipeline::REQUEST_INFORMATION()))
+  {
+    return this->RequestInformation(request, inputVector, outputVector);
+  }
+
+  // set update extent
+  if (request->Has(vtkStreamingDemandDrivenPipeline::REQUEST_UPDATE_EXTENT()))
+  {
+    return this->RequestUpdateExtent(request, inputVector, outputVector);
+  }
+
+  return this->Superclass::ProcessRequest(request, inputVector, outputVector);
+}
+
+//------------------------------------------------------------------------------
+int vtkPartitionedDataSetCollectionAlgorithm::FillOutputPortInformation(
+  int vtkNotUsed(port), vtkInformation* info)
+{
+  info->Set(vtkDataObject::DATA_TYPE_NAME(), "vtkPartitionedDataSetCollection");
+  return 1;
+}
+
+//------------------------------------------------------------------------------
+int vtkPartitionedDataSetCollectionAlgorithm::FillInputPortInformation(
+  int vtkNotUsed(port), vtkInformation* info)
+{
+  // now add our info
+  info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkPartitionedDataSetCollection");
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+void vtkPartitionedDataSetCollectionAlgorithm::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
