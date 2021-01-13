afd73a3f93453e0802bbc646c58cd7c416850fde
Will Schroeder <will.schroeder@kitware.com>
2021-01-13 15:28:58 -0500

    Use modern dispatch, data array access, and threading for performance
    Filter performance has been improved via array dispatch, templated
    data array access, and threading.

--- Filters/General/vtkWarpScalar.cxx
+++ Filters/General/vtkWarpScalar.cxx
@@ -14,8 +14,10 @@
 =========================================================================*/
 #include "vtkWarpScalar.h"
 
+#include "vtkArrayDispatch.h"
 #include "vtkCellData.h"
 #include "vtkDataArray.h"
+#include "vtkDataArrayRange.h"
 #include "vtkDataSetAttributes.h"
 #include "vtkImageData.h"
 #include "vtkImageDataToPointSet.h"
@@ -29,6 +31,7 @@
 #include "vtkRectilinearGridToPointSet.h"
 
 #include "vtkNew.h"
+#include "vtkSMPTools.h"
 #include "vtkSmartPointer.h"
 
 vtkStandardNewMacro(vtkWarpScalar);
@@ -42,6 +45,7 @@ vtkWarpScalar::vtkWarpScalar()
   this->Normal[1] = 0.0;
   this->Normal[2] = 1.0;
   this->XYPlane = 0;
+  this->OutputPointsPrecision = vtkAlgorithm::DEFAULT_PRECISION;
 
   // by default process active point scalars
   this->SetInputArrayToProcess(
@@ -50,25 +54,6 @@ vtkWarpScalar::vtkWarpScalar()
 
 //------------------------------------------------------------------------------
 vtkWarpScalar::~vtkWarpScalar() = default;
-
-//------------------------------------------------------------------------------
-double* vtkWarpScalar::DataNormal(vtkIdType id, vtkDataArray* normals)
-{
-  return normals->GetTuple(id);
-}
-
-//------------------------------------------------------------------------------
-double* vtkWarpScalar::InstanceNormal(vtkIdType vtkNotUsed(id), vtkDataArray* vtkNotUsed(normals))
-{
-  return this->Normal;
-}
-
-//------------------------------------------------------------------------------
-double* vtkWarpScalar::ZNormal(vtkIdType vtkNotUsed(id), vtkDataArray* vtkNotUsed(normals))
-{
-  static double zNormal[3] = { 0.0, 0.0, 1.0 };
-  return zNormal;
-}
 
 //------------------------------------------------------------------------------
 int vtkWarpScalar::FillInputPortInformation(int vtkNotUsed(port), vtkInformation* info)
@@ -102,6 +87,101 @@ int vtkWarpScalar::RequestDataObject(
     return this->Superclass::RequestDataObject(request, inputVector, outputVector);
   }
 }
+
+// Core methods to scale points with scalars
+namespace
+{ // anonymous
+
+struct ScaleWorker
+{
+  template <typename InPT, typename OutPT, typename ST>
+  void operator()(InPT* inPts, OutPT* outPts, ST* scalars, vtkWarpScalar* self, double sf,
+    bool XYPlane, vtkDataArray* inNormals, double* normal)
+
+  {
+    vtkIdType numPts = inPts->GetNumberOfTuples();
+    const auto ipts = vtk::DataArrayTupleRange<3>(inPts);
+    auto opts = vtk::DataArrayTupleRange<3>(outPts);
+    const auto sRange = vtk::DataArrayTupleRange(scalars);
+
+    // For smaller data sizes, serial processing is faster than spinning up
+    // threads. The cutoff point between serial and threaded is empirical and
+    // is likely to change.
+    static constexpr int VTK_SMP_THRESHOLD = 750000;
+    if (numPts >= VTK_SMP_THRESHOLD)
+    {
+      vtkSMPTools::For(0, numPts, [&](vtkIdType ptId, vtkIdType endPtId) {
+        double s, *n = normal, inNormal[3];
+        for (; ptId < endPtId; ++ptId)
+        {
+          const auto xi = ipts[ptId];
+          auto xo = opts[ptId];
+
+          if (XYPlane)
+          {
+            s = xi[2];
+          }
+          else
+          {
+            const auto sval = sRange[ptId];
+            s = sval[0]; // 0th component of the tuple
+          }
+
+          if (inNormals)
+          {
+            inNormals->GetTuple(ptId, inNormal);
+            n = inNormal;
+          }
+
+          xo[0] = xi[0] + sf * s * n[0];
+          xo[1] = xi[1] + sf * s * n[1];
+          xo[2] = xi[2] + sf * s * n[2];
+        }
+      }); // lambda
+    }     // threaded
+
+    else // serial
+    {
+      double s, *n = normal, inNormal[3];
+      for (vtkIdType ptId = 0; ptId < numPts; ptId++)
+      {
+        if (!(ptId % 10000))
+        {
+          self->UpdateProgress((double)ptId / numPts);
+          if (self->GetAbortExecute())
+          {
+            break;
+          }
+        }
+
+        const auto xi = ipts[ptId];
+        auto xo = opts[ptId];
+
+        if (XYPlane)
+        {
+          s = xi[2];
+        }
+        else
+        {
+          const auto sval = sRange[ptId];
+          s = sval[0]; // 0th component of the tuple
+        }
+
+        if (inNormals)
+        {
+          inNormals->GetTuple(ptId, inNormal);
+          n = inNormal;
+        }
+
+        xo[0] = xi[0] + sf * s * n[0];
+        xo[1] = xi[1] + sf * s * n[1];
+        xo[2] = xi[2] + sf * s * n[2];
+      } // over all points
+    }   // serial processing
+  }
+};
+
+} // anonymous namespace
 
 //------------------------------------------------------------------------------
 int vtkWarpScalar::RequestData(vtkInformation* vtkNotUsed(request),
@@ -145,11 +225,7 @@ int vtkWarpScalar::RequestData(vtkInformation* vtkNotUsed(request),
   vtkPoints* inPts;
   vtkDataArray* inNormals;
   vtkDataArray* inScalars;
-  vtkPoints* newPts;
-  vtkPointData* pd;
-  int i;
-  vtkIdType ptId, numPts;
-  double x[3], *n, s, newX[3];
+  vtkIdType numPts;
 
   vtkDebugMacro(<< "Warping data with scalars");
 
@@ -157,9 +233,7 @@ int vtkWarpScalar::RequestData(vtkInformation* vtkNotUsed(request),
   output->CopyStructure(input);
 
   inPts = input->GetPoints();
-  pd = input->GetPointData();
-  inNormals = pd->GetNormals();
-
+  inNormals = input->GetPointData()->GetNormals();
   inScalars = this->GetInputArrayToProcess(0, inputVector);
   if (!inPts || !inScalars)
   {
@@ -169,53 +243,51 @@ int vtkWarpScalar::RequestData(vtkInformation* vtkNotUsed(request),
 
   numPts = inPts->GetNumberOfPoints();
 
+  // Create the output points. Backward compatibility requires the
+  // output type to be float - this can be overridden.
+  vtkNew<vtkPoints> newPts;
+  if (this->OutputPointsPrecision == vtkAlgorithm::DEFAULT_PRECISION ||
+    this->OutputPointsPrecision == vtkAlgorithm::SINGLE_PRECISION)
+  {
+    newPts->SetDataType(VTK_FLOAT);
+  }
+  else
+  {
+    newPts->SetDataType(VTK_DOUBLE);
+  }
+  newPts->SetNumberOfPoints(numPts);
+  output->SetPoints(newPts);
+
+  // Figure out what normal to use
+  double normal[3] = { 0.0, 0.0, 0.0 };
   if (inNormals && !this->UseNormal)
   {
-    this->PointNormal = &vtkWarpScalar::DataNormal;
     vtkDebugMacro(<< "Using data normals");
   }
   else if (this->XYPlane)
   {
-    this->PointNormal = &vtkWarpScalar::ZNormal;
+    inNormals = nullptr;
+    normal[2] = 1.0;
     vtkDebugMacro(<< "Using x-y plane normal");
   }
   else
   {
-    this->PointNormal = &vtkWarpScalar::InstanceNormal;
+    inNormals = nullptr;
+    normal[0] = this->Normal[0];
+    normal[1] = this->Normal[1];
+    normal[2] = this->Normal[2];
     vtkDebugMacro(<< "Using Normal instance variable");
   }
 
-  newPts = vtkPoints::New();
-  newPts->SetNumberOfPoints(numPts);
-
-  // Loop over all points, adjusting locations
-  //
-  for (ptId = 0; ptId < numPts; ptId++)
-  {
-    if (!(ptId % 10000))
-    {
-      this->UpdateProgress((double)ptId / numPts);
-      if (this->GetAbortExecute())
-      {
-        break;
-      }
-    }
-
-    inPts->GetPoint(ptId, x);
-    n = (this->*(this->PointNormal))(ptId, inNormals);
-    if (this->XYPlane)
-    {
-      s = x[2];
-    }
-    else
-    {
-      s = inScalars->GetComponent(ptId, 0);
-    }
-    for (i = 0; i < 3; i++)
-    {
-      newX[i] = x[i] + this->ScaleFactor * s * n[i];
-    }
-    newPts->SetPoint(ptId, newX);
+  // Dispatch over point and scalar types
+  using vtkArrayDispatch::Reals;
+  using ScaleDispatch = vtkArrayDispatch::Dispatch3ByValueType<Reals, Reals, Reals>;
+  ScaleWorker scaleWorker;
+  if (!ScaleDispatch::Execute(inPts->GetData(), newPts->GetData(), inScalars, scaleWorker, this,
+        this->ScaleFactor, this->XYPlane, inNormals, normal))
+  { // fallback to slowpath
+    scaleWorker(inPts->GetData(), newPts->GetData(), inScalars, this, this->ScaleFactor,
+      this->XYPlane, inNormals, normal);
   }
 
   // Update ourselves and release memory
@@ -224,9 +296,6 @@ int vtkWarpScalar::RequestData(vtkInformation* vtkNotUsed(request),
   output->GetPointData()->PassData(input->GetPointData());
   output->GetCellData()->CopyNormalsOff(); // distorted geometry
   output->GetCellData()->PassData(input->GetCellData());
-
-  output->SetPoints(newPts);
-  newPts->Delete();
 
   return 1;
 }
@@ -241,4 +310,5 @@ void vtkWarpScalar::PrintSelf(ostream& os, vtkIndent indent)
   os << indent << "Normal: (" << this->Normal[0] << ", " << this->Normal[1] << ", "
      << this->Normal[2] << ")\n";
   os << indent << "XY Plane: " << (this->XYPlane ? "On\n" : "Off\n");
+  os << indent << "Output Points Precision: " << this->OutputPointsPrecision << "\n";
 }
