Kitware/VTK

commit 5f2544d1930b549b18dba47a62ef67c3d40d8ac6

Reworked filter for performance and reduced memory usage.
Completely rewrote the filter for performance. Algorithm and
associated data structures redesigned for threaded computation.
Threading added for critical steps in the algorithm. Memory
fragmentation and excessive memory use (due to many vtkIdLists())
eliminated. ArrayDispatch and associated templated access added.
Simplified process of classifying vertices.

Filters/Core/Testing/Data/Baseline/TestWindowedSincPolyDataFilter.png.sha512
@@ -1,0 +1,1 @@
+56aef07133ffb1e6d0b66432789367a552f488b3243ed17dda7038a1d2561523e2afb893634bf3da6d8885462367e441a521b2b4c024582f7ddf4b1d892d2cf0

Filters/Core/Testing/Python/CMakeLists.txt
@@ -47,6 +47,7 @@ vtk_add_test_python(
   TestStructuredGridPlaneCutter.py
   TestSynchronizedTemplates2D.py
   TestSynchronizedTemplates3D.py
+  TestWindowedSincPolyDataFilter.py
   TestContourGrid.py,NO_VALID
   TestContourPolyData.py,NO_VALID
   TestTensorGlyph.py,NO_RT

Filters/Core/Testing/Python/TestWindowedSincPolyDataFilter.py
@@ -1,0 +1,94 @@
+#!/usr/bin/env python
+import vtk
+from vtk.util.misc import vtkGetDataRoot
+VTK_DATA_ROOT = vtkGetDataRoot()
+
+def GetRGBColor(colorName):
+    '''
+        Return the red, green and blue components for a
+        color as doubles.
+    '''
+    rgb = [0.0, 0.0, 0.0]  # black
+    vtk.vtkNamedColors().GetColorRGB(colorName, rgb)
+    return rgb
+
+# Control resolution of the test
+res = 100
+
+# Set up rendering
+ren0 = vtk.vtkRenderer()
+ren0.SetViewport(0,0,0.5,1.0)
+ren1 = vtk.vtkRenderer()
+ren1.SetViewport(0.5,0,1.0,1.0)
+
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren0)
+renWin.AddRenderer(ren1)
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+
+ren1.SetActiveCamera(ren0.GetActiveCamera())
+
+# Pipeline stuff. Create a sphere source at high resolution.
+sphere = vtk.vtkSphereSource()
+sphere.SetThetaResolution(res)
+sphere.SetPhiResolution(int(res/2))
+
+# Add some noise
+random = vtk.vtkBrownianPoints()
+random.SetInputConnection(sphere.GetOutputPort())
+random.SetMinimumSpeed(0.0)
+random.SetMaximumSpeed(0.01)
+
+# Now warp the sphere with the noise
+warp = vtk.vtkWarpVector()
+warp.SetInputConnection(random.GetOutputPort())
+warp.SetScaleFactor(1.0)
+
+# Smooth it
+smooth = vtk.vtkWindowedSincPolyDataFilter()
+smooth.SetInputConnection(warp.GetOutputPort())
+smooth.SetNumberOfIterations(20)
+smooth.FeatureEdgeSmoothingOff()
+smooth.BoundarySmoothingOff()
+smooth.NonManifoldSmoothingOff()
+smooth.SetPassBand(0.1)
+smooth.GenerateErrorScalarsOn()
+smooth.GenerateErrorVectorsOn()
+smooth.Update()
+
+mapper0 = vtk.vtkPolyDataMapper()
+mapper0.SetInputConnection(warp.GetOutputPort())
+
+actor0 = vtk.vtkActor()
+actor0.SetMapper(mapper0)
+actor0.GetProperty().SetDiffuseColor(GetRGBColor('tomato'))
+actor0.GetProperty().SetDiffuse(.8)
+actor0.GetProperty().SetSpecular(.4)
+actor0.GetProperty().SetSpecularPower(30)
+
+mapper1 = vtk.vtkPolyDataMapper()
+mapper1.SetInputConnection(smooth.GetOutputPort())
+mapper1.SetScalarRange(smooth.GetOutput().GetScalarRange())
+
+actor1 = vtk.vtkActor()
+actor1.SetMapper(mapper1)
+actor1.GetProperty().SetDiffuseColor(GetRGBColor('tomato'))
+actor1.GetProperty().SetDiffuse(.8)
+actor1.GetProperty().SetSpecular(.4)
+actor1.GetProperty().SetSpecularPower(30)
+
+# Add the actors to the renderer, set the background and size
+#
+ren0.AddActor(actor0)
+ren0.SetBackground(1, 1, 1)
+ren1.AddActor(actor1)
+ren1.SetBackground(1, 1, 1)
+
+renWin.SetSize(450, 300)
+ren0.ResetCamera()
+renWin.Render()
+ren0.GetActiveCamera().Zoom(1.25)
+
+iren.Initialize()
+iren.Start()

Filters/Core/vtkWindowedSincPolyDataFilter.h
@@ -17,10 +17,10 @@
  * @brief   adjust point positions using a windowed sinc function interpolation kernel
  *
  * vtkWindowedSincPolyDataFiler adjust point coordinate using a windowed
- * sinc function interpolation kernel.  The effect is to "relax" the mesh,
+ * sinc function interpolation kernel. The effect is to "relax" the mesh,
  * making the cells better shaped and the vertices more evenly distributed.
  * Note that this filter operates the lines, polygons, and triangle strips
- * composing an instance of vtkPolyData.  Vertex or poly-vertex cells are
+ * composing an instance of vtkPolyData. Vertex or poly-vertex cells are
  * never modified.
  *
  * The algorithm proceeds as follows. For each vertex v, a topological and
@@ -33,7 +33,7 @@
  * Taubin describes this methodology is the IBM tech report RC-20404
  * (#90237, dated 3/12/96) "Optimal Surface Smoothing as Filter Design"
  * G. Taubin, T. Zhang and G. Golub. (Zhang and Golub are at Stanford
- * University).
+ * University.)
  *
  * This report discusses using standard signal processing low-pass filters
  * (in particular windowed sinc functions) to smooth polyhedra. The
@@ -112,7 +112,7 @@
  * amount of smoothing increases.
  *
  * The second ivar is the specification of the PassBand for the windowed
- * sinc filter.  By design, the PassBand is specified as a doubleing point
+ * sinc filter.  By design, the PassBand is specified as a doubling point
  * number between 0 and 2.  Lower PassBand values produce more smoothing.
  * A good default value for the PassBand is 0.1 (for those interested, the
  * PassBand (and frequencies) for PolyData are based on the valence of the
@@ -120,10 +120,10 @@
  * between 0 and 2.)
  *
  * There are two instance variables that control the generation of error
- * data. If the ivar GenerateErrorScalars is on, then a scalar value indicating
- * the distance of each vertex from its original position is computed. If the
- * ivar GenerateErrorVectors is on, then a vector representing change in
- * position is computed.
+ * data. If the ivar GenerateErrorScalars is on, then a scalar value
+ * indicating the distance of each vertex from its original position is
+ * computed. If the ivar GenerateErrorVectors is on, then a vector
+ * representing change in position is computed.
  *
  * @warning
  * The smoothing operation reduces high frequency information in the
@@ -131,8 +131,26 @@
  * lost. Enabling FeatureEdgeSmoothing helps reduce this effect, but cannot
  * entirely eliminate it.
  *
+ * @warning
+ * For maximum performance, do not enable BoundarySmoothing,
+ * NonManifoldSmoothing, or FeatureEdgeSmoothing. FeatureEdgeSmoothing is
+ * particularly expensive as it requires building topological links and
+ * computing local polygon normals which are relatively expensive
+ * operations. BoundarySmoothing and NonManifoldSmoothing have a modest
+ * impact on performance.
+ *
+ * @warning
+ * This class has been threaded with vtkSMPTools. Using TBB or other
+ * non-sequential execution type (set in the CMake variable
+ * VTK_SMP_IMPLEMENTATION_TYPE) may improve performance significantly.
+ *
  * @sa
- * vtkSmoothPolyDataFilter vtkDecimate vtkDecimatePro
+ * Another useful documentation resource is this SIGGRAPH publication:
+ * Gabriel Taubin. "A Signal Processing Approach To Fair Surface Design".
+ *
+ * @sa
+ * vtkSmoothPolyDataFilter vtkPointSmoothingFilter vtkDecimate vtkDecimatePro
+ * vtkQuadricDecimation
  */
 
 #ifndef vtkWindowedSincPolyDataFilter_h
@@ -148,18 +166,18 @@ public:
   void PrintSelf(ostream& os, vtkIndent indent) override;
 
   /**
-   * Construct object with number of iterations 20; passband .1;
-   * feature edge smoothing turned off; feature
-   * angle 45 degrees; edge angle 15 degrees; and boundary smoothing turned
-   * on. Error scalars and vectors are not generated (by default). The
-   * convergence criterion is 0.0 of the bounding box diagonal.
+   * Construct object with number of iterations 20; passband .1; feature edge
+   * smoothing turned off; feature angle 45 degrees; edge angle 15 degrees;
+   * and boundary smoothing turned on. Error scalars and vectors are not
+   * generated (by default).
    */
   static vtkWindowedSincPolyDataFilter* New();
 
   //@{
   /**
-   * Specify the number of iterations (or degree of the polynomial
-   * approximating the windowed sinc function).
+   * Specify the number of iterations (i.e., the degree of the polynomial
+   * approximating the windowed sinc function). Typically values around 20
+   * are used.
    */
   vtkSetClampMacro(NumberOfIterations, int, 0, VTK_INT_MAX);
   vtkGetMacro(NumberOfIterations, int);
@@ -167,7 +185,7 @@ public:
 
   //@{
   /**
-   * Set the passband value for the windowed sinc filter
+   * Set the passband value for the windowed sinc filter.
    */
   vtkSetClampMacro(PassBand, double, 0.0, 2.0);
   vtkGetMacro(PassBand, double);
@@ -175,13 +193,12 @@ public:
 
   //@{
   /**
-   * Turn on/off coordinate normalization.  The positions can be
-   * translated and scaled such that they fit within a [-1, 1] prior
-   * to the smoothing computation. The default is off.  The numerical
-   * stability of the solution can be improved by turning
-   * normalization on.  If normalization is on, the coordinates will
-   * be rescaled to the original coordinate system after smoothing has
-   * completed.
+   * Turn on/off coordinate normalization.  The positions can be translated
+   * and scaled such that they fit within a [-1, 1] prior to the smoothing
+   * computation. The default is off.  The numerical stability of the
+   * solution can be improved by turning normalization on.  If normalization
+   * is on, the coordinates will be rescaled to the original coordinate
+   * system after smoothing has completed.
    */
   vtkSetMacro(NormalizeCoordinates, vtkTypeBool);
   vtkGetMacro(NormalizeCoordinates, vtkTypeBool);
@@ -190,7 +207,9 @@ public:
 
   //@{
   /**
-   * Turn on/off smoothing along sharp interior edges.
+   * Turn on/off smoothing points along sharp interior edges. Enabling this
+   * option has a performance impact on the algorithm since neihborhood
+   * information (cell links) and polygon normals must be computed.
    */
   vtkSetMacro(FeatureEdgeSmoothing, vtkTypeBool);
   vtkGetMacro(FeatureEdgeSmoothing, vtkTypeBool);
@@ -199,7 +218,8 @@ public:
 
   //@{
   /**
-   * Specify the feature angle for sharp edge identification.
+   * Specify the feature angle for sharp edge identification. It only affects
+   * the filter when FeatureEdgeSmoothing is enabled.
    */
   vtkSetClampMacro(FeatureAngle, double, 0.0, 180.0);
   vtkGetMacro(FeatureAngle, double);
@@ -216,7 +236,8 @@ public:
 
   //@{
   /**
-   * Turn on/off the smoothing of vertices on the boundary of the mesh.
+   * Turn on/off the smoothing of points on the boundary of the mesh.
+   * Enabled this option has a modest impact on performance.
    */
   vtkSetMacro(BoundarySmoothing, vtkTypeBool);
   vtkGetMacro(BoundarySmoothing, vtkTypeBool);
@@ -225,7 +246,8 @@ public:
 
   //@{
   /**
-   * Smooth non-manifold vertices.
+   * Smooth non-manifold points. Enabling this option has a modest
+   * impact on performance.
    */
   vtkSetMacro(NonManifoldSmoothing, vtkTypeBool);
   vtkGetMacro(NonManifoldSmoothing, vtkTypeBool);
@@ -258,14 +280,17 @@ protected:
 
   int NumberOfIterations;
   double PassBand;
+
+  vtkTypeBool NormalizeCoordinates;
+
   vtkTypeBool FeatureEdgeSmoothing;
   double FeatureAngle;
   double EdgeAngle;
   vtkTypeBool BoundarySmoothing;
   vtkTypeBool NonManifoldSmoothing;
+
   vtkTypeBool GenerateErrorScalars;
   vtkTypeBool GenerateErrorVectors;
-  vtkTypeBool NormalizeCoordinates;
 
 private:
   vtkWindowedSincPolyDataFilter(const vtkWindowedSincPolyDataFilter&) = delete;

Filters/Modeling/Testing/Data/Baseline/smoothCyl2_1.png.sha512
@@ -1,0 +1,1 @@
+3f9a47e47a919a719d2827833113dded0e6412b3c46ce94b44f6b3799fcd3fea9fbc46b00d180fe60212947f60a2344953500c89a5e0b240bded2a27d137106a

Filters/Modeling/Testing/Python/smoothCyl2.py
@@ -86,4 +86,4 @@ ren1.SetActiveCamera(camera)
 # render the image
 #
 iren.Initialize()
-#iren.Start()
+iren.Start()

commit 0c10a5b5d1f6f92a802cf61ac856844720baf038

Fixed NormalizeCoordinates option
NormalizeCoordinates was processing the incorrect output array. This also
affects ErrorScalars and ErrorVectors.

This commit fixes a regression introduced in 5f2544d19 (Reworked filter
for performance and reduced memory usage).

Filters/Core/vtkWindowedSincPolyDataFilter.cxx
@@ -1492,14 +1492,14 @@ int vtkWindowedSincPolyDataFilter::RequestData(vtkInformation* vtkNotUsed(reques
   // If the points were normalized, reverse the normalization process.
   if (this->NormalizeCoordinates)
   {
-    UnnormalizePoints(newPts, length, center);
+    UnnormalizePoints(outPts, length, center);
   }
 
   // If error scalars are requested, create them.
   if (this->GenerateErrorScalars)
   {
     vtkSmartPointer<vtkFloatArray> errorScalars;
-    errorScalars.TakeReference(ProduceErrorScalars(input->GetPoints(), newPts));
+    errorScalars.TakeReference(ProduceErrorScalars(input->GetPoints(), outPts));
     int idx = output->GetPointData()->AddArray(errorScalars);
     output->GetPointData()->SetActiveAttribute(idx, vtkDataSetAttributes::SCALARS);
   }
@@ -1508,7 +1508,7 @@ int vtkWindowedSincPolyDataFilter::RequestData(vtkInformation* vtkNotUsed(reques
   if (this->GenerateErrorVectors)
   {
     vtkSmartPointer<vtkFloatArray> errorVectors;
-    errorVectors.TakeReference(ProduceErrorVectors(input->GetPoints(), newPts));
+    errorVectors.TakeReference(ProduceErrorVectors(input->GetPoints(), outPts));
     output->GetPointData()->AddArray(errorVectors);
   }
 

Filters/Core/Testing/Data/Baseline/TestWindowedSincPolyDataFilter.png.sha512
@@ -1,1 +1,1 @@
-56aef07133ffb1e6d0b66432789367a552f488b3243ed17dda7038a1d2561523e2afb893634bf3da6d8885462367e441a521b2b4c024582f7ddf4b1d892d2cf0
+6a1724792695da9a9c42679c9d90756a1049fc8c2f2c6c8ca7804076e17ca80b47ad18caff42528ecee35a2fb6efd01f391eabd65b8b5cd7fd0a721bfc995c42

Filters/Core/Testing/Data/Baseline/TestWindowedSincPolyDataFilter2.png.sha512
@@ -1,0 +1,1 @@
+04633d266c128ddd4712a423b050814e405b9b70c854972a3e17d54ae213324867223793a5bc97bd95eefee1c0cfc77dda1d564199f89727093b4ea90ff991db

Filters/Core/Testing/Python/CMakeLists.txt
@@ -57,6 +57,7 @@ vtk_add_test_python(
   TestSynchronizedTemplates2D.py
   TestSynchronizedTemplates3D.py
   TestWindowedSincPolyDataFilter.py
+  TestWindowedSincPolyDataFilter2.py
   TestContourGrid.py,NO_VALID
   TestContourPolyData.py,NO_VALID
   TestTensorGlyph.py,NO_RT

Filters/Core/Testing/Python/TestWindowedSincPolyDataFilter2.py
@@ -1,0 +1,101 @@
+#!/usr/bin/env python
+import vtk
+from vtk.util.misc import vtkGetDataRoot
+VTK_DATA_ROOT = vtkGetDataRoot()
+
+def GetRGBColor(colorName):
+    '''
+        Return the red, green and blue components for a
+        color as doubles.
+    '''
+    rgb = [0.0, 0.0, 0.0]  # black
+    vtk.vtkNamedColors().GetColorRGB(colorName, rgb)
+    return rgb
+
+# Test the NormalizeCoordinates option (as compared to
+# TestWindowedSincPolyDataFilter.py). The sphere is placed
+# away from the origin, and scaled in size.
+
+# Control resolution of the test
+res = 100
+
+# Set up rendering
+ren0 = vtk.vtkRenderer()
+ren0.SetViewport(0,0,0.5,1.0)
+ren1 = vtk.vtkRenderer()
+ren1.SetViewport(0.5,0,1.0,1.0)
+
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren0)
+renWin.AddRenderer(ren1)
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+
+ren1.SetActiveCamera(ren0.GetActiveCamera())
+
+# Pipeline stuff. Create a sphere source at high resolution.
+sphere = vtk.vtkSphereSource()
+sphere.SetCenter(100,200,400)
+sphere.SetRadius(100)
+sphere.SetThetaResolution(res)
+sphere.SetPhiResolution(int(res/2))
+
+# Add some noise
+random = vtk.vtkBrownianPoints()
+random.SetInputConnection(sphere.GetOutputPort())
+random.SetMinimumSpeed(0.0)
+random.SetMaximumSpeed(1)
+
+# Now warp the sphere with the noise
+warp = vtk.vtkWarpVector()
+warp.SetInputConnection(random.GetOutputPort())
+warp.SetScaleFactor(1.0)
+
+# Smooth it
+smooth = vtk.vtkWindowedSincPolyDataFilter()
+smooth.SetInputConnection(warp.GetOutputPort())
+smooth.SetNumberOfIterations(20)
+smooth.FeatureEdgeSmoothingOff()
+smooth.BoundarySmoothingOff()
+smooth.NonManifoldSmoothingOff()
+smooth.SetPassBand(0.1)
+smooth.GenerateErrorScalarsOn()
+smooth.GenerateErrorVectorsOn()
+smooth.NormalizeCoordinatesOn()
+smooth.Update()
+
+mapper0 = vtk.vtkPolyDataMapper()
+mapper0.SetInputConnection(warp.GetOutputPort())
+
+actor0 = vtk.vtkActor()
+actor0.SetMapper(mapper0)
+actor0.GetProperty().SetDiffuseColor(GetRGBColor('tomato'))
+actor0.GetProperty().SetDiffuse(.8)
+actor0.GetProperty().SetSpecular(.4)
+actor0.GetProperty().SetSpecularPower(30)
+
+mapper1 = vtk.vtkPolyDataMapper()
+mapper1.SetInputConnection(smooth.GetOutputPort())
+mapper1.SetScalarRange(smooth.GetOutput().GetScalarRange())
+
+actor1 = vtk.vtkActor()
+actor1.SetMapper(mapper1)
+actor1.GetProperty().SetDiffuseColor(GetRGBColor('tomato'))
+actor1.GetProperty().SetDiffuse(.8)
+actor1.GetProperty().SetSpecular(.4)
+actor1.GetProperty().SetSpecularPower(30)
+
+# Add the actors to the renderer, set the background and size
+#
+ren0.AddActor(actor0)
+ren0.SetBackground(1, 1, 1)
+ren1.AddActor(actor1)
+ren1.SetBackground(1, 1, 1)
+
+renWin.SetSize(450, 300)
+ren0.ResetCamera()
+renWin.Render()
+ren0.GetActiveCamera().Zoom(1.25)
+
+iren.Initialize()
+iren.Start()