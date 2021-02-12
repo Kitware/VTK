bb0f0845d76ade5b4a8484c2fdfccb39f4051253
Ben Boeckel <ben.boeckel@kitware.com>
2021-02-12 09:14:54 -0500

    FindGDAL: fix a typo
    Found on the CMake submission of this change.

--- CMake/patches/3.20/FindGDAL.cmake
+++ CMake/patches/3.20/FindGDAL.cmake
@@ -154,7 +154,7 @@ set(_gdal_versions
 set(_gdal_libnames)
 foreach (_gdal_version IN LISTS _gdal_versions)
     string(REPLACE "." "" _gdal_version "${_gdal_version}")
-    list(APPEND _gdal_libnames "gdal${_gdal_version}" "GDAL${_gdal_libnames}")
+    list(APPEND _gdal_libnames "gdal${_gdal_version}" "GDAL${_gdal_version}")
 endforeach ()
 unset(_gdal_version)
 unset(_gdal_versions)
