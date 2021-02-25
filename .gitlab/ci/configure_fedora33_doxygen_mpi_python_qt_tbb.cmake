5f7033345967ea1513f49a21082067b6d41c63f6
Ben Boeckel <ben.boeckel@kitware.com>
2021-02-25 13:46:08 -0500

    gitlab-ci: add a job which uploads docs to the website

--- .gitlab/ci/configure_fedora33_doxygen_mpi_python_qt_tbb.cmake
+++ .gitlab/ci/configure_fedora33_doxygen_mpi_python_qt_tbb.cmake
@@ -1,4 +1,7 @@
 # Install to the DESTDIR directly.
 set(CMAKE_INSTALL_PREFIX "/" CACHE PATH "")
 
+# Simplify the install tree for upload.
+set(VTK_VERSIONED_INSTALL OFF CACHE BOOL "")
+
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora33.cmake")
