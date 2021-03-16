80e3ade6a73362657962984a8ba43d558361eec7
Ben Boeckel <ben.boeckel@kitware.com>
2021-03-16 16:27:03 -0400

    Wrapping/PythonCore: remove export macros from private headers

--- Wrapping/PythonCore/PyVTKReference.h
+++ Wrapping/PythonCore/PyVTKReference.h
@@ -33,10 +33,10 @@ struct PyVTKReference
   PyObject_HEAD PyObject* value;
 };
 
-extern VTKWRAPPINGPYTHONCORE_EXPORT PyTypeObject PyVTKReference_Type;
-extern VTKWRAPPINGPYTHONCORE_EXPORT PyTypeObject PyVTKNumberReference_Type;
-extern VTKWRAPPINGPYTHONCORE_EXPORT PyTypeObject PyVTKStringReference_Type;
-extern VTKWRAPPINGPYTHONCORE_EXPORT PyTypeObject PyVTKTupleReference_Type;
+extern PyTypeObject PyVTKReference_Type;
+extern PyTypeObject PyVTKNumberReference_Type;
+extern PyTypeObject PyVTKStringReference_Type;
+extern PyTypeObject PyVTKTupleReference_Type;
 
 #define PyVTKReference_Check(obj) PyObject_TypeCheck(obj, &PyVTKReference_Type)
 
@@ -45,12 +45,10 @@ extern "C"
   // Set the value held by a mutable object.  It steals the reference
   // of the provided value.  Only float, long, and int are allowed.
   // A return value of -1 indicates than an error occurred.
-  VTKWRAPPINGPYTHONCORE_EXPORT
   int PyVTKReference_SetValue(PyObject* self, PyObject* val);
 
   // Get the value held by a mutable object.  A borrowed reference
   // is returned.
-  VTKWRAPPINGPYTHONCORE_EXPORT
   PyObject* PyVTKReference_GetValue(PyObject* self);
 }
 
