a4ed7e678c46e4b004f15fc3c7cefe274e7571ec
Timothée Couble <timothee.couble@kitware.com>
2021-03-24 14:57:43 +0100

    Add vtkSMPTools::Transform function

--- Common/Core/Testing/Cxx/TestSMP.cxx
+++ Common/Core/Testing/Cxx/TestSMP.cxx
@@ -12,6 +12,7 @@
      PURPOSE.  See the above copyright notice for more information.
 
 =========================================================================*/
+#include "vtkDataArrayRange.h"
 #include "vtkNew.h"
 #include "vtkObject.h"
 #include "vtkObjectFactory.h"
@@ -19,6 +20,7 @@
 #include "vtkSMPThreadLocalObject.h"
 #include "vtkSMPTools.h"
 #include <functional>
+#include <set>
 #include <vector>
 
 static const int Target = 10000;
@@ -157,5 +159,65 @@ int TestSMP(int, char*[])
     }
   }
 
+  // Test transform
+  std::vector<double> transformData0 = { 51, 9, 3, -10, 27, 1, -5, 82, 31, 9, 21 };
+  std::vector<double> transformData1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  std::set<double> transformData2 = { 7, 24, 98, 256, 72, 19, 3, 21, 2, 12 };
+  std::vector<double> transformData3 = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+  vtkNew<vtkAOSDataArrayTemplate<double>> transformArray0;
+  vtkNew<vtkAOSDataArrayTemplate<double>> transformArray1;
+
+  transformArray0->SetNumberOfComponents(1);
+  transformArray0->SetArray(transformData0.data(), transformData0.size(), 1);
+  transformArray1->SetNumberOfComponents(1);
+  transformArray1->SetArray(transformData1.data(), transformData1.size(), 1);
+
+  const auto transformRange0 = vtk::DataArrayValueRange<1>(transformArray0);
+  auto transformRange1 = vtk::DataArrayValueRange<1>(transformArray1);
+  vtkSMPTools::Transform(transformRange0.cbegin(), transformRange0.cend(), transformRange1.begin(),
+    [](double x, double y) { return x * y; });
+  auto it0 = transformRange0.begin();
+  auto it1 = transformRange1.begin();
+  for (vtkIdType i = 0; i < 11; ++i, it0++, it1++)
+  {
+    if (*it1 != *it0 * i)
+    {
+      cerr << "Error: Bad comparison transform!" << endl;
+      return 1;
+    }
+  }
+
+  vtkSMPTools::Transform(transformData2.cbegin(), transformData2.cend(), transformData3.begin(),
+    [](double x, double y) { return x * y; });
+  auto it2 = transformData2.begin();
+  for (const auto& it3 : transformData3)
+  {
+    if (it3 != *it2 * -1)
+    {
+      cerr << "Error: Bad comparison transform!" << endl;
+      return 1;
+    }
+    it2++;
+  }
+
+  // Test fill
+  std::vector<double> fillData0 = { 51, 9, 3, -10, 27, 1, -5, 82, 31, 9, 21 };
+  vtkNew<vtkAOSDataArrayTemplate<double>> fillArray0;
+
+  fillArray0->SetNumberOfComponents(1);
+  fillArray0->SetArray(fillData0.data(), fillData0.size(), 1);
+
+  // Fill range2 with its first value
+  auto fillRange0 = vtk::DataArrayTupleRange<1>(fillArray0);
+  const auto value = *fillRange0.begin();
+  vtkSMPTools::Fill(fillRange0.begin(), fillRange0.end(), value);
+  for (auto it : fillRange0)
+  {
+    if (it != value)
+    {
+      cerr << "Error: Bad comparison transform!" << endl;
+      return 1;
+    }
+  }
   return 0;
 }
