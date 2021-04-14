e9f710f19ffbd3a454973d8f47afeeb5f76bb1af
Ben Boeckel <ben.boeckel@kitware.com>
2021-04-14 11:58:18 -0400

    testing: support actually skipping Python tests
    VTK now requires CMake 3.12 on its own and in consumers, so we can
    assume that `SKIP_RETURN_CODE` is supported now.

--- Wrapping/Python/vtkmodules/test/Testing.py
+++ Wrapping/Python/vtkmodules/test/Testing.py
@@ -107,10 +107,7 @@ _NO_IMAGE = 0
 
 def skip():
     '''Cause the test to be skipped due to insufficient requirements.'''
-    sys.exit(0)
-    # CMake 3.9 is required to make this not appear on CDash as a failure. See
-    # issue #17031.
-    #sys.exit(125)
+    sys.exit(125)
 
 
 class vtkTest(unittest.TestCase):
