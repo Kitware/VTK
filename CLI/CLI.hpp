7aeeaae9ecf58b1148134c24edefb1c6d084a067
CLI11 Upstream <kwrobot@kitware.com>
2021-05-13 12:55:57 -0400

    cli11 2021-05-13 (c03b6a4b)
    Code extracted from:
    
        https://gitlab.kitware.com/third-party/cli11.git
    
    at commit c03b6a4b6af56f642dd392f47a159040910c8c46 (for/vtk-2021-05-13-v1.9.1).

---
+++ CLI/CLI.hpp
@@ -0,0 +1,41 @@
+// Copyright (c) 2017-2020, University of Cincinnati, developed by Henry Schreiner
+// under NSF AWARD 1414736 and by the respective contributors.
+// All rights reserved.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+#pragma once
+
+
+// xxx(kitware): mandle CLI namespace
+#define CLI VTK_CLI
+
+
+// CLI Library includes
+// Order is important for combiner script
+
+#include "Version.hpp"
+
+#include "Macros.hpp"
+
+#include "StringTools.hpp"
+
+#include "Error.hpp"
+
+#include "TypeTools.hpp"
+
+#include "Split.hpp"
+
+#include "ConfigFwd.hpp"
+
+#include "Validators.hpp"
+
+#include "FormatterFwd.hpp"
+
+#include "Option.hpp"
+
+#include "App.hpp"
+
+#include "Config.hpp"
+
+#include "Formatter.hpp"
