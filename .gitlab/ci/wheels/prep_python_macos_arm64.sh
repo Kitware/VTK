ab3316d920ed303fe2cd488532f382122f92a49c
Ben Boeckel <ben.boeckel@kitware.com>
2021-05-24 15:36:57 -0400

    ci: support fetching and building macOS arm64 relocatable Python

---
+++ .gitlab/ci/wheels/prep_python_macos_arm64.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+readonly version="$1"
+shift
+
+if ! [ -d "relocatable-python" ]; then
+    git clone "https://github.com/gregneagle/relocatable-python"
+fi
+cd relocatable-python
+git pull
+
+readonly dirname="python-$version-macos-arm64"
+
+touch requirements.txt
+./make_relocatable_python_framework.py \
+    --destination="$dirname" \
+    --python-version="$version" \
+    --os-version="macos11.0" \
+    --pip-requirements=requirements.txt
+
+tar cJf "$dirname.tar.xz" "$dirname"
