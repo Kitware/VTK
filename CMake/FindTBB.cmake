ded89c1e3a025874bfd69feb6c67fa21d6af36d4
Mickael PHILIT <mickey.phy@gmail.com>
2021-05-27 04:44:42 +0000

    add missing {

--- CMake/FindTBB.cmake
+++ CMake/FindTBB.cmake
@@ -424,7 +424,7 @@ findpkg_finish(TBB_MALLOC_PROXY tbbmalloc_proxy)
 #=============================================================================
 #parse all the version numbers from tbb
 if(NOT TBB_VERSION)
-  if (EXISTS "$TBB_INCLUDE_DIR}/oneapi/tbb/version.h")
+  if (EXISTS "${TBB_INCLUDE_DIR}/oneapi/tbb/version.h")
     file(STRINGS
       "${TBB_INCLUDE_DIR}/oneapi/tbb/version.h"
       TBB_VERSION_CONTENTS
