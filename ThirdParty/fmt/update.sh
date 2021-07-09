8e2c37e1826d3a146bc3c6b1fc3e225dc0826990
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2021-07-09 18:43:17 -0400

    tpl: add tpl harness for {fmt}

---
+++ ThirdParty/fmt/update.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+shopt -s dotglob
+
+readonly name="fmt"
+readonly ownership="{fmt} Upstream <kwrobot@kitware.com>"
+readonly subtree="ThirdParty/$name/vtk$name"
+readonly repo="https://gitlab.kitware.com/third-party/fmt.git"
+readonly tag="for/vtk-20210711-8.0.1"
+readonly paths="
+.gitattributes
+include/fmt/*.h
+src/format.cc
+src/os.cc
+support/cmake/cxx14.cmake
+CMakeLists.vtk.txt
+LICENSE.rst
+README.kitware.md
+README.rst
+"
+
+extract_source () {
+    git_archive
+    pushd "$extractdir/$name-reduced"
+    mv -v include/fmt vtkfmt
+    mv CMakeLists.vtk.txt CMakeLists.txt
+    popd
+}
+
+. "${BASH_SOURCE%/*}/../update-common.sh"
