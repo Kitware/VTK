6475f36f3ec95d46355f5f94cd1b108a6cba3002
François Mazen <francois.mazen@kitware.com>
2021-07-29 10:39:51 +0200

    Fix small vlaue threshold.

--- Filters/ParallelDIY2/vtkRedistributeDataSetFilter.cxx
+++ Filters/ParallelDIY2/vtkRedistributeDataSetFilter.cxx
@@ -556,9 +556,9 @@ bool vtkRedistributeDataSetFilter::InitializeCuts(vtkDataObjectTree* input)
   if (this->UseExplicitCuts && this->ExpandExplicitCuts && gbounds.IsValid())
   {
     auto bbox = gbounds;
-    double xInflate = bbox.GetLength(0) == 0 ? 0.01 : 0.01 * bbox.GetLength(0);
-    double yInflate = bbox.GetLength(1) == 0 ? 0.01 : 0.01 * bbox.GetLength(1);
-    double zInflate = bbox.GetLength(2) == 0 ? 0.01 : 0.01 * bbox.GetLength(2);
+    double xInflate = bbox.GetLength(0) < 0.01 ? 0.01 : 0.01 * bbox.GetLength(0);
+    double yInflate = bbox.GetLength(1) < 0.01 ? 0.01 : 0.01 * bbox.GetLength(1);
+    double zInflate = bbox.GetLength(2) < 0.01 ? 0.01 : 0.01 * bbox.GetLength(2);
     bbox.Inflate(xInflate, yInflate, zInflate);
 
     this->Cuts = vtkRedistributeDataSetFilter::ExpandCuts(this->ExplicitCuts, bbox);
@@ -583,10 +583,13 @@ std::vector<vtkBoundingBox> vtkRedistributeDataSetFilter::GenerateCuts(vtkDataOb
     : this->GetNumberOfPartitions();
   auto bbox = vtkDIYUtilities::GetLocalBounds(dobj);
 
-  double xInflate = bbox.GetLength(0) == 0 ? 0.01 : 0.01 * bbox.GetLength(0);
-  double yInflate = bbox.GetLength(1) == 0 ? 0.01 : 0.01 * bbox.GetLength(1);
-  double zInflate = bbox.GetLength(2) == 0 ? 0.01 : 0.01 * bbox.GetLength(2);
-  bbox.Inflate(xInflate, yInflate, zInflate);
+  if (bbox.IsValid())
+  {
+    double xInflate = bbox.GetLength(0) < 0.01 ? 0.01 : 0.01 * bbox.GetLength(0);
+    double yInflate = bbox.GetLength(1) < 0.01 ? 0.01 : 0.01 * bbox.GetLength(1);
+    double zInflate = bbox.GetLength(2) < 0.01 ? 0.01 : 0.01 * bbox.GetLength(2);
+    bbox.Inflate(xInflate, yInflate, zInflate);
+  }
 
   double bds[6];
   bbox.GetBounds(bds);
