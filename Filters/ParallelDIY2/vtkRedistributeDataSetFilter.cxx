8258ccab74acd5157805dfa95960777222f2d1ce
François Mazen <francois.mazen@kitware.com>
2021-08-02 09:45:53 +0200

    Move magic numbers in definitions.

--- Filters/ParallelDIY2/vtkRedistributeDataSetFilter.cxx
+++ Filters/ParallelDIY2/vtkRedistributeDataSetFilter.cxx
@@ -60,6 +60,8 @@ namespace
 {
 const char* CELL_OWNERSHIP_ARRAYNAME = "__RDSF_CELL_OWNERSHIP__";
 const char* GHOST_CELL_ARRAYNAME = "__RDSF_GHOST_CELLS__";
+const double BOUNDING_BOX_LENGTH_TOLERANCE = 0.01;
+const double BOUNDING_BOX_INFLATION_RATIO = 0.01;
 }
 
 namespace detail
@@ -556,9 +558,15 @@ bool vtkRedistributeDataSetFilter::InitializeCuts(vtkDataObjectTree* input)
   if (this->UseExplicitCuts && this->ExpandExplicitCuts && gbounds.IsValid())
   {
     auto bbox = gbounds;
-    double xInflate = bbox.GetLength(0) < 0.01 ? 0.01 : 0.01 * bbox.GetLength(0);
-    double yInflate = bbox.GetLength(1) < 0.01 ? 0.01 : 0.01 * bbox.GetLength(1);
-    double zInflate = bbox.GetLength(2) < 0.01 ? 0.01 : 0.01 * bbox.GetLength(2);
+    double xInflate = bbox.GetLength(0) < BOUNDING_BOX_LENGTH_TOLERANCE
+      ? BOUNDING_BOX_LENGTH_TOLERANCE
+      : BOUNDING_BOX_INFLATION_RATIO * bbox.GetLength(0);
+    double yInflate = bbox.GetLength(1) < BOUNDING_BOX_LENGTH_TOLERANCE
+      ? BOUNDING_BOX_LENGTH_TOLERANCE
+      : BOUNDING_BOX_INFLATION_RATIO * bbox.GetLength(1);
+    double zInflate = bbox.GetLength(2) < BOUNDING_BOX_LENGTH_TOLERANCE
+      ? BOUNDING_BOX_LENGTH_TOLERANCE
+      : BOUNDING_BOX_INFLATION_RATIO * bbox.GetLength(2);
     bbox.Inflate(xInflate, yInflate, zInflate);
 
     this->Cuts = vtkRedistributeDataSetFilter::ExpandCuts(this->ExplicitCuts, bbox);
@@ -585,9 +593,15 @@ std::vector<vtkBoundingBox> vtkRedistributeDataSetFilter::GenerateCuts(vtkDataOb
 
   if (bbox.IsValid())
   {
-    double xInflate = bbox.GetLength(0) < 0.01 ? 0.01 : 0.01 * bbox.GetLength(0);
-    double yInflate = bbox.GetLength(1) < 0.01 ? 0.01 : 0.01 * bbox.GetLength(1);
-    double zInflate = bbox.GetLength(2) < 0.01 ? 0.01 : 0.01 * bbox.GetLength(2);
+    double xInflate = bbox.GetLength(0) < BOUNDING_BOX_LENGTH_TOLERANCE
+      ? BOUNDING_BOX_LENGTH_TOLERANCE
+      : BOUNDING_BOX_INFLATION_RATIO * bbox.GetLength(0);
+    double yInflate = bbox.GetLength(1) < BOUNDING_BOX_LENGTH_TOLERANCE
+      ? BOUNDING_BOX_LENGTH_TOLERANCE
+      : BOUNDING_BOX_INFLATION_RATIO * bbox.GetLength(1);
+    double zInflate = bbox.GetLength(2) < BOUNDING_BOX_LENGTH_TOLERANCE
+      ? BOUNDING_BOX_LENGTH_TOLERANCE
+      : BOUNDING_BOX_INFLATION_RATIO * bbox.GetLength(2);
     bbox.Inflate(xInflate, yInflate, zInflate);
   }
 
