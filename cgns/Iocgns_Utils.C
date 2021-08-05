7151ec8ca09a3d65f468b43e2255837035a2771b
Seacas Upstream <kwrobot@kitware.com>
2021-08-05 17:07:34 -0400

    ioss 2021-08-05 (6dc9922d)
    Code extracted from:
    
        https://gitlab.kitware.com/third-party/seacas.git
    
    at commit 6dc9922d561e3d99fe259222ffe32c70f5b82445 (ioss/for/vtk-20210805-v2021-05-12).

--- cgns/Iocgns_Utils.C
+++ cgns/Iocgns_Utils.C
@@ -76,6 +76,17 @@ static const char* strcasestr(const char* haystack, const char* needle)
     Iocgns::Utils::cgns_error(file_ptr, __FILE__, __func__, __LINE__, -1);                         \
   }
 
+#ifdef _WIN32
+char *strcasestr(char *haystack, const char *needle)
+{
+  char *c;
+  for (c = haystack; *c; c++)
+    if (!strncasecmp(c, needle, strlen(needle)))
+      return c;
+  return 0;
+}
+#endif
+
 namespace {
   int power_2(int count)
   {
@@ -566,7 +577,12 @@ int Iocgns::Utils::get_db_zone(const Ioss::GroupingEntity *entity)
   IOSS_ERROR(errmsg);
 }
 
-size_t Iocgns::Utils::index(const Ioss::Field &field) { return field.get_index() & 0xffffffff; }
+namespace {
+  const size_t CG_CELL_CENTER_FIELD_ID = 1ul << 30;
+  const size_t CG_VERTEX_FIELD_ID      = 1ul << 31;
+}
+
+size_t Iocgns::Utils::index(const Ioss::Field &field) { return field.get_index() & 0x00ffffff; }
 
 void Iocgns::Utils::set_field_index(const Ioss::Field &field, size_t index,
                                     CGNS_ENUMT(GridLocation_t) location)
