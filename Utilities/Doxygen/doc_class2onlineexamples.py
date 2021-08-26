2e16745b0692370fa5809b593ef177116ff8fdaf
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2021-08-26 12:21:25 -0400

    Add logging for the new class2onlineexamples script

--- Utilities/Doxygen/doc_class2onlineexamples.py
+++ Utilities/Doxygen/doc_class2onlineexamples.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
+from argparse import ArgumentParser
 from dataclasses import dataclass
+from os.path import basename
 from urllib.error import HTTPError
 from urllib.request import urlretrieve
+import logging
 import pathlib
 import re
-import fileinput
+import time
+import itertools
 
 
 # -------------------------------------------------------------------------------------------------
@@ -26,12 +30,13 @@ def parse_arguments():
 
     description = 'Cross-reference vtk-examples with classes'
     epilog = '''
-This script generates a 'Class to Examples' page cross-linking each class to a specific number of
-examples that use that class. The examples are hyperlinked to the vtk-examples GitHub site.
+This script generates a cross-reference between each class to a specific number of
+examples that use that class. The examples are hyperlinked to the vtk-examples GitHub site. The
+script displays the image from the example page in the doxygen documentation.
 '''
 
-    parser = argparse.ArgumentParser(description=description, epilog=epilog,
-                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser = ArgumentParser(description=description, epilog=epilog,
+                            formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-d', '--dir', type=pathlib.Path,
                         help='Directory containing the vtk classes')
     parser.add_argument('-t', '--tmpdir', type=pathlib.Path,
@@ -40,6 +45,9 @@ examples that use that class. The examples are hyperlinked to the vtk-examples G
                         help="Label to use in the documentation")
     parser.add_argument('-n', '--number', type=int, default=10,
                         help='Maximum number of examples to show')
+    parser.add_argument('-v', '--log-level',
+                        type=lambda x: getattr(logging, x),
+                        default=logging.INFO, help="Configure the logging level", )
     args = parser.parse_args()
     return args
 
@@ -104,8 +112,8 @@ def docblock_from_example(example_url, label):
 
     e = decompose_exampleurl(example_url)
     if e is None:
-        print("ERROR: Could not decompose example url to its language, module and name. Skipping..",
-              example_url)
+        logging.error("Could not decompose example url to its language, module and name. Skipping..",
+                      example_url)
         return None
     doc_block += " "*6 + "<a href=\"" + example_url + "\">\n"
     example_img_url = imageurl_from_exampleurl(e[0], e[1], e[2])
@@ -142,18 +150,17 @@ def decompose_exampleurl(example_url):
     example_name_matcher = re.compile(example_name_matcher_str)
     example_name_match = example_name_matcher.match(example_url)
     if example_name_match is None:
-        print("ERROR: Could not parse example name from url:",
-              example_url, example_name_matcher_str)
+        logging.error("Could not parse example name from url:",
+                      example_url, example_name_matcher_str)
         return None
     if len(example_name_match.groups()) != 3:
-        print("ERROR: Example regex matcher could not find sufficient matches:",
-              example_name_match.groups())
+        logging.error("Example regex matcher could not find sufficient matches:",
+                      example_name_match.groups())
         return None
     return [i for i in example_name_match.groups()]
 
+
 # -------------------------------------------------------------------------------------------------
-
-
 def imageurl_from_exampleurl(lang, module, name):
     """
     Return a valid url for the image on the example page for given language, module and  example
@@ -170,7 +177,15 @@ def imageurl_from_exampleurl(lang, module, name):
 
 # -------------------------------------------------------------------------------------------------
 def main(indir, tmpdir, label, max_num):
+    """
+    The main function of the script.
 
+    This method downloads the selectExamples script, finds and parses headers in the doxygen source,
+    and adds the necessary documenation blocks to the headers.
+    """
+
+    intermediate_time = time.time()
+    logging.info("Downloading and importing selectExamples...")
     # download the select examples script from the vtk-examples repository
     selUrl = refs.raw_base_url + "/" + refs.hash + "/" + refs.selectExamples
     path = download_file(tmpdir, selUrl, overwrite=False)
@@ -180,9 +195,16 @@ def main(indir, tmpdir, label, max_num):
     spec = importlib.util.spec_from_file_location("SelectExamples", path)
     selectExamples = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(selectExamples)
-
+    logging.debug("Downloaded and imported selectExamples script in {t:.2f}s."
+                  .format(t=time.time() - intermediate_time))
+    logging.info("Collecting files...")
+    intermediate_time = time.time()
     # iterate over all header files
     headerlist = pathlib.Path(indir).rglob('*.h')
+    headerlist, headerlist_backup = itertools.tee(headerlist)
+    num_headers = sum(1 for _ in headerlist_backup)
+    logging.debug("Collected {i} files in {t:.2f}s."
+                  .format(i=num_headers, t=(time.time() - intermediate_time)))
     # eliminate_matcher
     eliminate_matcher = re.compile(r'^vtkCommand.h$')
     # class name matcher
@@ -192,15 +214,19 @@ def main(indir, tmpdir, label, max_num):
     # label for the new documentation par
     label_matcher_str = r'^\s*\@par\s+{l}:\s*$'.format(l=label)
     label_matcher = re.compile(label_matcher_str)
+    logging.debug("Iterating over collected files...")
+    intermediate_time = time.time()
     for headerfile in headerlist:
         # eliminate matcher
         if eliminate_matcher.match(headerfile.name) is not None:
-            print("Ignoring header:", headerfile.name)
+            logging.warning("Ignoring header {f}".format(f=headerfile.name))
             continue
         classname = None
         classlineno = -1
         enddoclineno = -1
         labellineno = -1
+        header_time = time.time()
+        logging.debug("Parsing header {f}".format(f=headerfile.name))
         with open(headerfile, "r") as f:
             num = 0
             for line in f:
@@ -210,8 +236,8 @@ def main(indir, tmpdir, label, max_num):
                         # found the @class block
                         classlineno = num
                         classname = class_match.group(1)
-                        print("Class name found @",
-                              classlineno, ":", classname)
+                        logging.debug("Class name found @{l}: '{c}'"
+                                      .format(l=classlineno, c=classname))
                 elif enddoclineno < 0:
                     # Haven't found the end of the documentation block yet.
                     enddoc_match = enddoc_matcher.match(line)
@@ -223,30 +249,43 @@ def main(indir, tmpdir, label, max_num):
                         if label_match is not None:
                             # The xref is already there.
                             labellineno = num
-                            print("Label found", labellineno, ":", line)
+                            logging.debug("Label found @{lc}: '{l}'"
+                                          .format(lc=labellineno, l=line.strip()))
                     else:
                         # end of doc found. Record and abort
                         enddoclineno = num
+                        logging.debug("End of documentation block found @{lc}: '{l}'"
+                                      .format(lc=enddoclineno, l=line.strip()))
                         break
                 num += 1
 
             # If we reach here and couldn't find either classname, enddocline number, etc.
             # This file might not have the standard documentation
             if classname is None or classlineno < 0 or enddoclineno < 0:
-                print(
-                    "ERROR: Could not process documentation block in file:", headerfile.name)
+                logging.error("Could not process documentation block in file {f}"
+                              .format(f=headerfile.name))
                 continue
             elif labellineno > -1:
                 # This file already contains the examples block
-                print("INFO: Already contains par block:", headerfile.name)
+                logging.warning("Header {f} already contains the '@par {l}:' documentation block"
+                                .format(f=headerfile.name, l=label))
                 continue
-        print("Colleced info:", classname, classlineno, labellineno, enddoclineno)
-        t, examples = selectExamples.get_examples(selectExamples.get_crossref_dict(tmpdir),
-                                                  classname, 'Cxx', all_values=False, number=max_num)
+        logging.debug("Parsed header {f} in {t:.2f}s"
+                      .format(f=headerfile.name, t=(time.time() - header_time)))
+        logging.debug("Collected info for {f}: classname={c}, classlineno={lc},"
+                      "labellineno={ll}, enddoclineno={le}"
+                      .format(f=headerfile.name, c=classname,
+                              lc=classlineno, ll=labellineno, le=enddoclineno))
+        logging.debug("Fetching examples for class '{c}'".format(c=classname))
+        t, examples = selectExamples.get_examples(
+            selectExamples.get_crossref_dict(tmpdir), classname,
+            'Cxx', all_values=False, number=max_num)
         if not examples or examples is None:
-            print("INFO: No examples found for class", classname)
+            logging.debug(
+                "No examples found for class: {c}".format(c=classname))
             continue
-        num_found = len(examples)
+        logging.debug("Fetched {n} examples for class '{c}'"
+                      .format(c=classname, n=len(examples)))
         doxblock = preamble_for_label(label)
         # Add a new grid div
         doxblock += " "*4 + "<div class=\"examplegrid\">\n"
@@ -264,10 +303,19 @@ def main(indir, tmpdir, label, max_num):
         with open(headerfile, "w") as f:
             contents = "".join(contents)
             f.write(contents)
+        logging.debug("Added documentation block for found examples")
+    logging.info("Done processing {n} collected files in {t:.2f}s."
+                 .format(n=num_headers, t=(time.time() - intermediate_time)))
 
 
 # -------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     args = parse_arguments()
+    logging.basicConfig(level=args.log_level)
 
+    prog_name = basename(__file__)
+    logging.info(prog_name)
+
+    start_time = time.time()
     main(args.dir, args.tmpdir, args.label, args.number)
+    logging.info("Finished in {t:.2f}s.".format(t=(time.time() - start_time)))
