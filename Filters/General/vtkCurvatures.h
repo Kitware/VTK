f4850852ba3f6d2fc2f15208978150f5708fdb5b
Andrew Maclean <andrew.amaclean@gmail.com>
2021-09-01 11:10:31 +1000

    Fixing the LaTex equations

--- Filters/General/vtkCurvatures.h
+++ Filters/General/vtkCurvatures.h
@@ -19,45 +19,49 @@
  * vtkCurvatures takes a polydata input and computes the curvature of the
  * mesh at each point. Four possible methods of computation are available :
  *
- * Gauss Curvature
- * discrete Gauss curvature (K) computation,
- * \f$K(\text{vertex v}) = 2*\pi - \sum_{\text{facet neighbs f of v}} (\text{angle_f at v})\f$.
- * The contribution of every facet is for the moment weighted by \f$Area(facet)/3\f$.
- * The units of Gaussian Curvature are \f$[1/m^2]\f$.
+ * Gauss Curvature discrete Gauss curvature (\f$ K \f$),
+ * \f$K_v = 2\pi-n_vf_v(\alpha)\f$, where \f$K_v\f$ is the curvature
+ * at vertex \f$v\f$, \f$n_v\f$ the facet neighbours of the vertex \f$v\f$
+ * and \f$f_v(\alpha)\f$ is the angle of \f$f\f$ at vertex \f$v\f$.
+ * The contribution of every facet is for the moment weighted by the
+ * (area of each facet)/3 The units of Gaussian Curvature are \f$m^{-2}\f$.
  *
- * Mean Curvature
- * \f$H(vertex v) = \text{average over edges neighbs e of H(e)}\f$,
- * \f$H(edge e) = length(e) * dihedral\_angle(e)\f$.
+ * Mean Curvature \f$H_v = \overline{H_e}\f$, where \f$\overline{H_e}\f$ is
+ * the average over the edge neighbours of \f$H_e\f$.
+ * \f$H_e = l(e)*\alpha(e)\f$ where \f$e\f$ is an edge, \f$l\f$ is the length
+ * and \f$\alpha\f$ is the dihederal angle such that
+ * \f$-\pi < \alpha < \pi\f$. This means that the surface is assumed to
+ * be orientable and the computation creates the orientation. The units of
+ * Mean Curvature are \f$m^{-1}\f$.
  *
- * NB: dihedral_angle is the ORIENTED angle between -PI and PI,
- * this means that the surface is assumed to be orientable
- * the computation creates the orientation.
- * The units of Mean Curvature are [1/m].
+ * Maximum (\f$k_{max}\f$) and Minimum (\f$k_{min}\f$) Principal Curvatures
+ *  are \f$k_{max} = H + \sqrt{H^2 - K}\f$ and
+ * \f$k_{min} = H - \sqrt{H^2 - K}\f$.
+ * Excepting spherical and planar surfaces which have equal
+ * principal curvatures, the curvature at a point on a surface varies with
+ * the direction one "sets off" from the point. For all directions, the
+ * curvature will pass through two extrema: a minimum (\f$k_{min}\f$) and a
+ * maximum (\f$k_{max}\f$) which occur at mutually orthogonal directions
+ * to each other.
  *
- * Maximum (\f$k_\max\f$) and Minimum (\f$k_\min\f$) Principal Curvatures
- * \f$k_\max = H + \sqrt{H^2 - K}\f$,
- * \f$k_\min = H - \sqrt{H^2 - K}\f$
- * Excepting spherical and planar surfaces which have equal principal
- * curvatures, the curvature at a point on a surface varies with the direction
- * one "sets off" from the point. For all directions, the curvature will pass
- * through two extrema: a minimum (\f$k_\min\f$) and a maximum (\f$k_\max\f$)
- * which occur at mutually orthogonal directions to each other.
+ * The sign of the Gauss curvature is a geometric ivariant, it should be
+ * positive when the surface looks like a sphere, negative when it looks
+ * like a saddle, however, the sign of the Mean curvature is not, it depends
+ * on the convention for normals, This code assumes that normals point
+ * outwards (ie from the surface of a sphere outwards). If a given mesh
+ * produces curvatures of opposite senses then the flag InvertMeanCurvature
+ *  can be set and the Curvature reported by the Mean calculation will
+ * be inverted.
  *
- * NB. The sign of the Gauss curvature is a geometric invariant, it should be
- * positive when the surface looks like a sphere, negative when it looks like a
- * saddle, however the sign of the Mean curvature is not, it depends on the
- * convention for normals. This code assumes that normals point outwards (i.e.
- * from the surface of a sphere outwards). If a given mesh produces curvatures
- * of opposite senses then the flag InvertMeanCurvature can be set and the
- * Curvature reported by the Mean calculation will be inverted.
+ * For a little more information see
+ * <a href="https://public.kitware.com/pipermail/vtkusers/2002-July/012198.html"
+ * >Computing curvature of a surface</a>
  *
  * @par Thanks:
- * Philip Batchelor philipp.batchelor@kcl.ac.uk for creating and contributing
- * the class and Andrew Maclean a.maclean@acfr.usyd.edu.au for cleanups and
- * fixes. Thanks also to Goodwin Lawlor for contributing patch to calculate
- * principal curvatures
- *
- *
+ * <a href="https://en.wikipedia.org/wiki/Philip_Batchelor">Philip Batchelor</a>
+ * for creating and contributing the class and Andrew Maclean for cleanups and
+ * fixes. Thanks also to John Biddiscombe for adding the class and
+ * Goodwin Lawlor for contributing a patch to calculate principal curvatures
  *
  */
 
@@ -116,24 +120,24 @@ protected:
   int RequestData(vtkInformation*, vtkInformationVector**, vtkInformationVector*) override;
 
   /**
-   * discrete Gauss curvature (K) computation,
-   * cf http://www-ipg.umds.ac.uk/p.batchelor/curvatures/curvatures.html
+   * Discrete Gauss curvature (K) computation
    */
   void GetGaussCurvature(vtkPolyData* output);
 
   void ComputeGaussCurvature(vtkCellArray* facets, vtkPolyData* output, double* gaussCurvatureData);
 
-  // discrete Mean curvature (H) computation,
-  // cf http://www-ipg.umds.ac.uk/p.batchelor/curvatures/curvatures.html
+  /**
+   * Discrete Mean curvature (H) computation
+   */
   void GetMeanCurvature(vtkPolyData* output);
 
   /**
-   * Maximum principal curvature \f$k_max = H + sqrt(H^2 -K)\f$
+   * Maximum principal curvature
    */
   void GetMaximumCurvature(vtkPolyData* input, vtkPolyData* output);
 
   /**
-   * Minimum principal curvature \f$k_min = H - sqrt(H^2 -K)\f$
+   * Minimum principal curvature
    */
   void GetMinimumCurvature(vtkPolyData* input, vtkPolyData* output);
 
