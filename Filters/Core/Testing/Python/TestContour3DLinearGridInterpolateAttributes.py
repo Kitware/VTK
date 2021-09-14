d0dccc93a8c9e1a13fb374e8dc96aa51da88c998
Cory Quammen <cory.quammen@kitware.com>
2021-09-14 14:00:58 -0400

    TestContour3DLinearGridInterpolateAttributes: exercise ComputeScalars option
    Modify test to exercise the new ComputeScalars option. Also add some
    more checks on the output.

--- Filters/Core/Testing/Python/TestContour3DLinearGridInterpolateAttributes.py
+++ Filters/Core/Testing/Python/TestContour3DLinearGridInterpolateAttributes.py
@@ -23,10 +23,34 @@ contour.SetComputeNormals(1);
 contour.Update()
 
 # Ensure that the number of tuples in the output arrays matches the number of points
+print('Enabling InterpolateAttributes')
 output = contour.GetOutput()
 ptData = output.GetPointData()
+numPts = output.GetNumberOfPoints()
+expectedArrayNames = {'Polynomial', 'Normals'}
 for i in range(ptData.GetNumberOfArrays()):
-    numTuples = ptData.GetArray(i).GetNumberOfTuples()
-    numPts = output.GetNumberOfPoints()
-    print("numTuples: %d numPts: %d" % (numTuples, numPts))
+    array = ptData.GetArray(i)
+    arrayName = array.GetName()
+    numTuples = array.GetNumberOfTuples()
+    r = array.GetRange()
+    print("arrayName: '%s' numTuples: %d numPts: %d range: [%f, %f]" %
+        (arrayName, numTuples, numPts, r[0], r[1]))
+    assert(numTuples == numPts)
+    assert(arrayName in expectedArrayNames)
+
+# Enable contour scalars
+print('Enabling InterpolateAttributes and ComputeScalars')
+contour.SetComputeScalars(1)
+contour.Update()
+output = contour.GetOutput()
+ptData = output.GetPointData()
+numPts = output.GetNumberOfPoints()
+expectedArrayNames.add('DistanceToCenter')
+for i in range(ptData.GetNumberOfArrays()):
+    array = ptData.GetArray(i)
+    arrayName = array.GetName()
+    numTuples = array.GetNumberOfTuples()
+    r = array.GetRange()
+    print("arrayName: '%s' numTuples: %d numPts: %d range: [%f, %f]" %
+        (arrayName, numTuples, numPts, r[0], r[1]))
     assert(numTuples == numPts)
