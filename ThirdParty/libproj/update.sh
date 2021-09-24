77e420681bd9d021abeee3042096a123aab8d284
Ben Boeckel <ben.boeckel@kitware.com>
2021-09-24 19:30:23 -0400

    libproj: update to 8.1.0

--- ThirdParty/libproj/update.sh
+++ ThirdParty/libproj/update.sh
@@ -8,51 +8,50 @@ readonly name="libproj"
 readonly ownership="Proj Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/proj.git"
-readonly tag="for/vtk-20200507-4.9.3"
+readonly tag="for/vtk-20211111-8.1.0"
 readonly paths="
 .gitattributes
 ChangeLog
 COPYING
 CMakeLists.txt
-README
+README.md
 README.kitware.md
 
-cmake/Proj4Config.cmake
-cmake/Proj4SystemInfo.cmake
-cmake/Proj4Test.cmake
-cmake/Proj4Utilities.cmake
-cmake/Proj4Version.cmake
+cmake/ProjConfig.cmake
+cmake/ProjTest.cmake
+cmake/ProjUtilities.cmake
+cmake/ProjVersion.cmake
 cmake/proj_config.cmake.in
+
+data/
+
+include/CMakeLists.txt
+include/proj/CMakeLists.txt
+include/proj/*.hpp
+include/proj/internal/*.hpp
 
 src/CMakeLists.txt
 src/lib_proj.cmake
-src/PJ_*.c
-src/aasincos.c
-src/adjlon.c
-src/bch2bps.c
-src/bchgen.c
-src/biveval.c
-src/dmstor.c
-src/emess.c
-src/emess.h
-src/geocent.c
-src/geocent.h
-src/geodesic.c
-src/mk_cheby.c
-src/nad_*.c
-src/pj_*.c
-src/pj_*.h
-src/proj_*.c
-src/rtodms.c
-src/vector1.c
-src/projects.h
-src/proj_api.h
-src/geodesic.h
+src/projections/*.cpp
+src/conversions/*.cpp
+src/transformations/*.cpp
+src/transformations/*.hpp
+src/iso19111/*.cpp
+src/iso19111/operation/*.cpp
+src/iso19111/operation/*.hpp
+src/*.c
+src/*.h
+src/*.cpp
+src/*.hpp
 src/vtk_libproj_mangle.h
 "
 
 extract_source () {
     git_archive
+    pushd "$extractdir/$name-reduced"
+    rm -rvf data/tests/
+    rm data/Makefile.am
+    popd
 }
 
 . "${BASH_SOURCE%/*}/../update-common.sh"
