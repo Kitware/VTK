82aafb72e5448e25e5a6e6cb13583d21720e1fe5
Ben Boeckel <ben.boeckel@kitware.com>
2021-10-16 16:19:26 -0400

    ci: include headers under the source tree for `clang-tidy`

--- .gitlab/ci/configure_fedora34_mpi_python_qt_tbb_tidy.cmake
+++ .gitlab/ci/configure_fedora34_mpi_python_qt_tbb_tidy.cmake
@@ -1,5 +1,5 @@
-set(CMAKE_C_CLANG_TIDY "clang-tidy-cache" CACHE FILEPATH "")
-set(CMAKE_CXX_CLANG_TIDY "clang-tidy-cache" CACHE FILEPATH "")
+set(CMAKE_C_CLANG_TIDY "clang-tidy-cache" "--header-filter=$ENV{CI_PROJECT_DIR}/([A-SV-Z]|Testing)" CACHE FILEPATH "")
+set(CMAKE_CXX_CLANG_TIDY "clang-tidy-cache" "--header-filter=$ENV{CI_PROJECT_DIR}/([A-SV-Z]|Testing)" CACHE FILEPATH "")
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON CACHE BOOL "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora34.cmake")
