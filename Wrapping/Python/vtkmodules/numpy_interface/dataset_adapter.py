fd49ba42f02f534ff32108ee88e9775a2474dd4c
Cory Quammen <cory.quammen@kitware.com>
2021-10-25 14:40:19 -0400

    dataset_adapter.py: take array length from row data only if column exists
    If a new wrapped vtkTable was being populated with append() calls, the
    array length check added in 224d3db6 would return 0, and subsequent
    logic would treat tuples in the array as components instead. Fix that
    by only considering the length of row data if an existing column is
    present already.

--- Wrapping/Python/vtkmodules/numpy_interface/dataset_adapter.py
+++ Wrapping/Python/vtkmodules/numpy_interface/dataset_adapter.py
@@ -693,7 +693,8 @@ class DataSetAttributes(VTKObjectWrapper):
             arrLength = self.DataSet.GetNumberOfPoints()
         elif self.Association == ArrayAssociation.CELL:
             arrLength = self.DataSet.GetNumberOfCells()
-        elif self.Association == ArrayAssociation.ROW:
+        elif self.Association == ArrayAssociation.ROW \
+          and self.DataSet.GetNumberOfColumns() > 0:
             arrLength = self.DataSet.GetNumberOfRows()
         else:
             if not isinstance(narray, numpy.ndarray):
