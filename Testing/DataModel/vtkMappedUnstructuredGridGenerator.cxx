595290774f01f2dae008f4a69aafca5f7b71e471
Menno Deij - van Rijswijk <m.deij@marin.nl>
2021-10-27 08:43:14 +0200

    Fix build warning and update files with correct naming.
    Update IO/Exodus/vtkCPExodusIIElementBlock.cxx to use vtkNotUsed() to fix build warning.
    Update two recently added files to have correct file name at the top of the file.

--- Testing/DataModel/vtkMappedUnstructuredGridGenerator.cxx
+++ Testing/DataModel/vtkMappedUnstructuredGridGenerator.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    TestXMLMappedUnstructuredGridIO
+  Module:    vtkMappedUnstructuredGridGenerator.cxx
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
--- Testing/DataModel/vtkMappedUnstructuredGridGenerator.h
+++ Testing/DataModel/vtkMappedUnstructuredGridGenerator.h
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    vtkTestingInteractor.h
+  Module:    vtkMappedUnstructuredGridGenerator.h
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
