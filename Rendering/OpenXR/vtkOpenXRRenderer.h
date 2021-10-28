17b8244083d6642462d9eb6f0dc00ee15b5df91c
Cory Quammen <cory.quammen@kitware.com>
2021-10-28 18:39:51 -0400

    vtkOpenXRRenderer: remove redunancies
    Almost all the functionality is in vtkVRRenderer now,
    so gut the vtkOpenXRRenderer. The only special thing
    it still does is create a vtkOpenXRCamera.

--- Rendering/OpenXR/vtkOpenXRRenderer.h
+++ Rendering/OpenXR/vtkOpenXRRenderer.h
@@ -35,48 +35,14 @@ public:
   vtkTypeMacro(vtkOpenXRRenderer, vtkVRRenderer);
   void PrintSelf(ostream& os, vtkIndent indent) override;
 
-  using vtkVRRenderer::ResetCamera;
-
-  /**
-   * Automatically set up the camera based on a specified bounding box
-   * (xmin,xmax, ymin,ymax, zmin,zmax). Camera will reposition itself so
-   * that its focal point is the center of the bounding box, and adjust its
-   * distance and position to preserve its initial view plane normal
-   * (i.e., vector defined from camera position to focal point). Note: if
-   * the view plane is parallel to the view up axis, the view up axis will
-   * be reset to one of the three coordinate axes.
-   */
-  void ResetCamera(const double bounds[6]) override;
-
-  using vtkRenderer::ResetCameraClippingRange;
-
-  /**
-   * Reset the camera clipping range based on a bounding box.
-   */
-  void ResetCameraClippingRange(const double bounds[6]) override;
-
   /**
    * Create a new Camera suitable for use with this type of Renderer.
    */
   VTK_NEWINSTANCE vtkCamera* MakeCamera() override;
 
-  /**
-   * Concrete open gl render method.
-   */
-  void DeviceRender() override;
-
-  /**
-   * Show the floor of the VR world
-   */
-  virtual void SetShowFloor(bool);
-  virtual bool GetShowFloor() { return this->ShowFloor; }
-
 protected:
   vtkOpenXRRenderer();
   ~vtkOpenXRRenderer() override = default;
-
-  vtkNew<vtkActor> FloorActor;
-  bool ShowFloor = false;
 
 private:
   vtkOpenXRRenderer(const vtkOpenXRRenderer&) = delete;
