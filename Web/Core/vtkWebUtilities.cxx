6f8ec355a9b2ab0c853bae0f067888c87e3c7657
Ben Boeckel <ben.boeckel@kitware.com>
2021-12-09 10:25:57 -0500

    vtkWebUtilities: add semicolons to Python macros
    This makes `clang-format` behave a lot better.

--- Web/Core/vtkWebUtilities.cxx
+++ Web/Core/vtkWebUtilities.cxx
@@ -119,7 +119,7 @@ void vtkWebUtilities::ProcessRMIs()
 //------------------------------------------------------------------------------
 void vtkWebUtilities::ProcessRMIs(int reportError, int dont_loop)
 {
-  Py_BEGIN_ALLOW_THREADS vtkMultiProcessController::GetGlobalController()->ProcessRMIs(
-    reportError, dont_loop);
-  Py_END_ALLOW_THREADS
+  Py_BEGIN_ALLOW_THREADS;
+  vtkMultiProcessController::GetGlobalController()->ProcessRMIs(reportError, dont_loop);
+  Py_END_ALLOW_THREADS;
 }
