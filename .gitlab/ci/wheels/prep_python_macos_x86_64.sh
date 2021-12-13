faf500708e21d5903532b2bffaf6e899cbd9dbaf
Ben Boeckel <ben.boeckel@kitware.com>
2021-12-12 20:44:41 -0500

    ci: update OS version arguments for macOS binaries

--- .gitlab/ci/wheels/prep_python_macos_x86_64.sh
+++ .gitlab/ci/wheels/prep_python_macos_x86_64.sh
@@ -11,12 +11,26 @@ fi
 cd relocatable-python
 git pull
 
+# The prebuilt binaries on python.org target different macOS versions based on
+# the Python version. Use this to instruct which binary to download for the
+# `relocatable-python` project scripts.
+case "$version" in
+    3.?.*)
+        os_version="10.9"
+        ;;
+    3.1?.*)
+        os_version="11"
+        ;;
+esac
+readonly os_version
+
 readonly dirname="python-$version-macos-x86_64"
 
 touch requirements.txt
 ./make_relocatable_python_framework.py \
     --destination="$dirname" \
     --python-version="$version" \
+    --os-version="$os_version" \
     --pip-requirements=requirements.txt
 
 tar cJf "$dirname.tar.xz" "$dirname"
