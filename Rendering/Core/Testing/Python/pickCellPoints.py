334be4567a1c661b77bbddf22845aa59036730ac
Spiros Tsalikis <spiros.tsalikis@kitware.com>
2021-12-17 12:34:27 -0500

    vtkHardwarePicker: Testing

--- Rendering/Core/Testing/Python/pickCellPoints.py
+++ Rendering/Core/Testing/Python/pickCellPoints.py
@@ -5,14 +5,14 @@ import vtk
 # Quad
 pts = vtk.vtkPoints()
 pts.SetNumberOfPoints(6)
-coords = [(0,0,0),(2,0,0),(4,0,0),(0,4,0),(2,4,0),(4,4,0)]
-for i in range(0,6):
-    pts.InsertPoint(i,coords[i])
+coords = [(0, 0, 0), (2, 0, 0), (4, 0, 0), (0, 4, 0), (2, 4, 0), (4, 4, 0)]
+for i in range(0, 6):
+    pts.InsertPoint(i, coords[i])
 quads = vtk.vtkCellArray()
-cellpoints=[(0,1,4,3),(1,2,5,4)]
-for i in range(0,2):
+cellpoints = [(0, 1, 4, 3), (1, 2, 5, 4)]
+for i in range(0, 2):
     quads.InsertNextCell(4)
-    for j in range(0,4):
+    for j in range(0, 4):
         quads.InsertCellPoint(cellpoints[i][j])
 
 poly = vtk.vtkPolyData()
@@ -30,7 +30,7 @@ ren.AddActor(actor)
 
 renWin = vtk.vtkRenderWindow()
 renWin.AddRenderer(ren)
-renWin.SetSize(200,200)
+renWin.SetSize(200, 200)
 
 iren = vtk.vtkRenderWindowInteractor()
 iren.SetRenderWindow(renWin)
@@ -38,24 +38,49 @@ iren.SetRenderWindow(renWin)
 renWin.Render()
 iren.Initialize()
 
-pos =         [ (1,1),    (35,40),  (80,40),  (120,40), (165,40),
-                (199,160),(165,160),(120,160),(80,160), (35,160)  ]
-pickedCells = [ -1,       0,        0,        1,        1,
-                -1,       1,        1,        0,        0 ]
-pickedPoints= [ -1,       0,        1,        1,        2,
-                -1,       5,        4,        4,        3 ]
+pos = [(1, 1), (35, 40), (80, 40), (120, 40), (165, 40),
+       (199, 160), (165, 160), (120, 160), (80, 160), (35, 160)]
+pickedCells = [-1, 0, 0, 1, 1,
+               -1, 1, 1, 0, 0]
+pickedPoints = [-1, 0, 1, 1, 2,
+                -1, 5, 4, 4, 3]
 
-cellErrors  = 0
-pointErrors = 0
+cellErrorsCP = 0
+pointErrorsCP = 0
 cellPicker = vtk.vtkCellPicker()
-for i in range(0,len(pos)):
-    cellPicker.Pick(pos[i][0],pos[i][1],0,ren)
+cellErrorsHP = 0
+pointErrorsHP = 0
+hardwarePicker = vtk.vtkHardwarePicker()
+for i in range(0, len(pos)):
+    # Check cell picker
+    cellPicker.Pick(pos[i][0], pos[i][1], 0, ren)
     if cellPicker.GetCellId() != pickedCells[i]:
-        print("pos [{0}] : picked cell id={1} expected cell id={2}".format(pos[i],cellPicker.GetCellId(),pickedCells[i]))
-        cellErrors = cellErrors + 1
+        print("pos [{0}] : picked cell id={1} expected cell id={2}".format(pos[i], cellPicker.GetCellId(),
+                                                                           pickedCells[i]))
+        cellErrorsCP = cellErrorsCP + 1
     if cellPicker.GetPointId() != pickedPoints[i]:
-        print("pos [{0}] : picked point id={1} expected point id={2}".format(pos[i],cellPicker.GetPointId(),pickedPoints[i]))
-        pointErrors = pointErrors + 1
-if pointErrors or cellErrors:
-    print( "ERROR: Cell picker : {0} cell-id errors, {1} point-id errors".format(cellErrors,pointErrors) )
+        print("pos [{0}] : picked point id={1} expected point id={2}".format(pos[i], cellPicker.GetPointId(),
+                                                                             pickedPoints[i]))
+        pointErrorsCP = pointErrorsCP + 1
+    # Check hardware picker
+    hardwarePicker.SnapToMeshPointOff()
+    hardwarePicker.Pick(pos[i][0], pos[i][1], 0, ren)
+    if hardwarePicker.GetCellId() != pickedCells[i]:
+        print("pos [{0}] : picked cell id={1} expected cell id={2}".format(pos[i], hardwarePicker.GetCellId(),
+                                                                           pickedCells[i]))
+        cellErrorsHP = cellErrorsHP + 1
+    hardwarePicker.SnapToMeshPointOn()
+    hardwarePicker.SetPixelTolerance(20)
+    hardwarePicker.Pick(pos[i][0], pos[i][1], 0, ren)
+    if hardwarePicker.GetPointId() != pickedPoints[i]:
+        print("pos [{0}] : picked point id={1} expected point id={2}".format(pos[i], hardwarePicker.GetPointId(),
+                                                                             pickedPoints[i]))
+        pointErrorsHP = pointErrorsHP + 1
+
+if pointErrorsCP or cellErrorsCP:
+    print("ERROR: Cell picker : {0} cell-id errors, {1} point-id errors".format(cellErrorsCP, pointErrorsCP))
+    sys.exit(1)
+
+if pointErrorsHP or cellErrorsHP:
+    print("ERROR: Hardware picker : {0} cell-id errors, {1} point-id errors".format(cellErrorsHP, pointErrorsHP))
     sys.exit(1)
