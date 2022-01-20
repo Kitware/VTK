8260d665a8c4ac31c9c73e7fec7615cec63e9735
Spiros Tsalikis <spiros.tsalikis@kitware.com>
2022-01-20 13:12:20 -0500

    vtkOverlappingAMR: Fix typo

--- Common/DataModel/vtkOverlappingAMR.cxx
+++ Common/DataModel/vtkOverlappingAMR.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    vtkUniformGridAMR.cxx
+  Module:    vtkOverlappingAMR.cxx
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
