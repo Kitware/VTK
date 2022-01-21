6e7bff5a6ae3d5e110cff0d2582a903f54b95e45
Ben Boeckel <ben.boeckel@kitware.com>
2022-01-21 14:33:50 -0500

    vtkCompilerWarningFlags: add a function to enable extra build flags

--- CMake/vtkCompilerWarningFlags.cmake
+++ CMake/vtkCompilerWarningFlags.cmake
@@ -1,2 +1,15 @@
+include(CheckCompilerFlag)
+
+function (vtk_add_flag flag)
+  foreach (lang IN LISTS ARGN)
+    check_compiler_flag("${lang}" "${flag}" "vtk_have_compiler_flag-${lang}-${flag}")
+    if (vtk_have_compiler_flag-${lang}-${flag})
+      target_compile_options(vtkbuild
+        INTERFACE
+          "$<BUILD_INTERFACE:$<$<COMPILE_LANGUAGE:${lang}>:${flag}>>")
+    endif ()
+  endforeach ()
+endfunction ()
+
 option(VTK_ENABLE_EXTRA_BUILD_WARNINGS "Enable extra build warnings" OFF)
 mark_as_advanced(VTK_ENABLE_EXTRA_BUILD_WARNINGS)
