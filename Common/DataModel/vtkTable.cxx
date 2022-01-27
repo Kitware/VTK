Kitware/VTK

commit a6caabecb1798881ed2951f3a7e365d4e36e7a86

vtkTable + TestTable modifications

Common/DataModel/vtkTable.cxx
@@ -190,10 +190,18 @@ vtkIdType vtkTable::GetNumberOfRows()
 //------------------------------------------------------------------------------
 void vtkTable::SetNumberOfRows(vtkIdType n)
 {
-  if (this->RowData)
+  // to preserve data first call Resize() on all arrays
+  for (int i = 0; i < this->GetNumberOfColumns(); i++)
   {
-    this->RowData->SetNumberOfTuples(n);
+    this->GetColumn(i)->Resize(n);
   }
+  this->RowData->SetNumberOfTuples(n);
+}
+
+//------------------------------------------------------------------------------
+void vtkTable::SqueezeRows()
+{
+  this->RowData->Squeeze();
 }
 
 //------------------------------------------------------------------------------
@@ -231,6 +239,104 @@ void vtkTable::SetRow(vtkIdType row, vtkVariantArray* values)
   {
     this->SetValue(row, i, values->GetValue(i));
   }
+}
+
+//------------------------------------------------------------------------------
+void vtkTable::MoveRowData(vtkIdType first, vtkIdType last, vtkIdType delta)
+{
+  if ((first < 0) || (last < 0) || (first > last) || (delta == 0))
+  {
+    return;
+  }
+
+  // determine the move direction
+  // if delta is positive then we need to start at the back of the source segment
+  // and work forwards, else at the beginning of the source segment and backwards
+  vtkIdType start = first;
+  vtkIdType stop = last;
+  vtkIdType step = +1;
+  if (delta > 0)
+  {
+    start = last;
+    stop = first;
+    step = -1;
+  }
+
+  vtkIdType ncol = this->GetNumberOfColumns();
+  for (vtkIdType i = 0; i < ncol; i++)
+  {
+    vtkAbstractArray* arr = this->GetColumn(i);
+    int comps = arr->GetNumberOfComponents();
+    if (vtkArrayDownCast<vtkDataArray>(arr))
+    {
+      vtkDataArray* data = vtkArrayDownCast<vtkDataArray>(arr);
+      for (vtkIdType row = start; row * step <= stop * step; row += step)
+      {
+        data->SetTuple(row + delta, row, data);
+      }
+    }
+    else if (vtkArrayDownCast<vtkStringArray>(arr))
+    {
+      vtkStringArray* data = vtkArrayDownCast<vtkStringArray>(arr);
+      for (vtkIdType row = start; row * step <= stop * step; row += step)
+      {
+        for (int j = 0; j < comps; j++)
+        {
+          data->SetValue((row + delta) * comps + j, data->GetValue(row * comps + j));
+        }
+      }
+    }
+    else if (vtkArrayDownCast<vtkVariantArray>(arr))
+    {
+      vtkVariantArray* data = vtkArrayDownCast<vtkVariantArray>(arr);
+      for (vtkIdType row = start; row * step <= stop * step; row += step)
+      {
+        for (int j = 0; j < comps; j++)
+        {
+          data->SetValue((row + delta) * comps + j, data->GetValue(row * comps + j));
+        }
+      }
+    }
+    else if (vtkArrayDownCast<vtkUnicodeStringArray>(arr))
+    {
+      vtkUnicodeStringArray* data = vtkArrayDownCast<vtkUnicodeStringArray>(arr);
+      for (vtkIdType row = start; row * step <= stop * step; row += step)
+      {
+        for (int j = 0; j < comps; j++)
+        {
+          data->SetValue((row + delta) * comps + j, data->GetValue(row * comps + j));
+        }
+      }
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+void vtkTable::InsertRow(vtkIdType row)
+{
+  this->InsertRows(row, 1);
+}
+
+//------------------------------------------------------------------------------
+void vtkTable::InsertRows(vtkIdType row, vtkIdType n)
+{
+  if (n <= 0)
+  {
+    return;
+  }
+
+  row = std::max<vtkIdType>(0, std::min<vtkIdType>(row, this->GetNumberOfRows()));
+
+  vtkIdType NRows = this->GetNumberOfRows();
+  vtkIdType newNRows = std::max<vtkIdType>(NRows, row) + n;
+
+  // enlarge the table
+  this->SetNumberOfRows(newNRows);
+
+  // move the existing elements backwards
+  vtkIdType first = row;
+  vtkIdType last = NRows - 1;
+  this->MoveRowData(first, last, n);
 }
 
 //------------------------------------------------------------------------------
@@ -304,35 +410,48 @@ vtkIdType vtkTable::InsertNextRow(vtkVariantArray* values)
 //------------------------------------------------------------------------------
 void vtkTable::RemoveRow(vtkIdType row)
 {
+  this->RemoveRows(row, 1);
+}
+
+//------------------------------------------------------------------------------
+void vtkTable::RemoveRows(vtkIdType row, vtkIdType n)
+{
+  if (n <= 0)
+  {
+    return;
+  }
+
+  vtkIdType NRows = this->GetNumberOfRows();
+  vtkIdType NRemove = std::max<vtkIdType>(0, std::min<vtkIdType>(n, NRows - row));
+  vtkIdType newNRows = std::max<vtkIdType>(0, NRows - NRemove);
+  if (newNRows == NRows)
+  {
+    return;
+  }
+
+  // move the existing elements forwards
+  vtkIdType first = row + n;
+  vtkIdType last = NRows - 1;
+  this->MoveRowData(first, last, -n);
+
+  // shrink the table
+  this->SetNumberOfRows(newNRows);
+}
+
+//------------------------------------------------------------------------------
+void vtkTable::RemoveAllRows()
+{
   vtkIdType ncol = this->GetNumberOfColumns();
   for (vtkIdType i = 0; i < ncol; i++)
   {
     vtkAbstractArray* arr = this->GetColumn(i);
-    int comps = arr->GetNumberOfComponents();
     if (vtkArrayDownCast<vtkDataArray>(arr))
     {
-      vtkDataArray* data = vtkArrayDownCast<vtkDataArray>(arr);
-      data->RemoveTuple(row);
+      arr->SetNumberOfTuples(0);
     }
-    else if (vtkArrayDownCast<vtkStringArray>(arr))
+    else
     {
-      // Manually move all elements past the index back one place.
-      vtkStringArray* data = vtkArrayDownCast<vtkStringArray>(arr);
-      for (int j = comps * row; j < comps * data->GetNumberOfTuples() - 1; j++)
-      {
-        data->SetValue(j, data->GetValue(j + 1));
-      }
-      data->Resize(data->GetNumberOfTuples() - 1);
-    }
-    else if (vtkArrayDownCast<vtkVariantArray>(arr))
-    {
-      // Manually move all elements past the index back one place.
-      vtkVariantArray* data = vtkArrayDownCast<vtkVariantArray>(arr);
-      for (int j = comps * row; j < comps * data->GetNumberOfTuples() - 1; j++)
-      {
-        data->SetValue(j, data->GetValue(j + 1));
-      }
-      data->Resize(data->GetNumberOfTuples() - 1);
+      arr->SetNumberOfValues(0);
     }
   }
 }
@@ -360,6 +479,48 @@ void vtkTable::AddColumn(vtkAbstractArray* arr)
 }
 
 //------------------------------------------------------------------------------
+void vtkTable::InsertColumn(vtkAbstractArray* arr, vtkIdType index)
+{
+  if (this->GetNumberOfColumns() > 0 && arr->GetNumberOfTuples() != this->GetNumberOfRows())
+  {
+    vtkErrorMacro(<< "Column \"" << arr->GetName() << "\" must have " << this->GetNumberOfRows()
+                  << " rows, but has " << arr->GetNumberOfTuples() << ".");
+    return;
+  }
+  // ensure index is sensible
+  index = std::max<vtkIdType>(0, std::min<vtkIdType>(this->GetNumberOfColumns(), index));
+
+  // insert at end?
+  if (index == this->GetNumberOfColumns())
+  {
+    this->AddColumn(arr);
+    return;
+  }
+
+  // remove all arrays from RowData, then insert them again in correct order with new array inserted
+  // note: use vtkSmartPointer to preserve a reference count, else this->RowData->RemoveArray(0)
+  // will delete the array
+  vtkIdType ncols = this->GetNumberOfColumns();
+  std::vector<vtkSmartPointer<vtkAbstractArray>> store;
+  store.reserve(ncols);
+
+  for (int c = 0; c < ncols; c++)
+  {
+    if (c == index)
+    {
+      store.push_back(arr);
+    }
+    store.push_back(this->GetColumn(0));
+    this->RowData->RemoveArray(0);
+  }
+
+  for (int c = 0; c < store.size(); c++)
+  {
+    this->RowData->AddArray(store[c]);
+  }
+}
+
+//------------------------------------------------------------------------------
 void vtkTable::RemoveColumnByName(const char* name)
 {
   this->RowData->RemoveArray(name);
@@ -373,6 +534,16 @@ void vtkTable::RemoveColumn(vtkIdType col)
 }
 
 //------------------------------------------------------------------------------
+void vtkTable::RemoveAllColumns()
+{
+  int narrays = this->RowData->GetNumberOfArrays();
+  for (int i = 0; i < narrays; i++)
+  {
+    this->RowData->RemoveArray(0);
+  }
+}
+
+//------------------------------------------------------------------------------
 const char* vtkTable::GetColumnName(vtkIdType col)
 {
   int column = static_cast<int>(col);
@@ -383,6 +554,21 @@ const char* vtkTable::GetColumnName(vtkIdType col)
 vtkAbstractArray* vtkTable::GetColumnByName(const char* name)
 {
   return this->RowData->GetAbstractArray(name);
+}
+
+//------------------------------------------------------------------------------
+vtkIdType vtkTable::GetColumnIndex(const char* name)
+{
+  const char* col_name;
+  for (int i = 0; i < this->GetNumberOfColumns(); i++)
+  {
+    col_name = this->GetColumnName(i);
+    if (col_name && !strcmp(col_name, name))
+    {
+      return i;
+    }
+  }
+  return -1;
 }
 
 //------------------------------------------------------------------------------
@@ -613,8 +799,7 @@ vtkVariant vtkTable::GetValue(vtkIdType row, vtkIdType col)
 //------------------------------------------------------------------------------
 vtkVariant vtkTable::GetValueByName(vtkIdType row, const char* col)
 {
-  int colIndex = -1;
-  this->RowData->GetAbstractArray(col, colIndex);
+  int colIndex = GetColumnIndex(col);
   if (colIndex < 0)
   {
     return vtkVariant();

Common/DataModel/Testing/Cxx/TestTable.cxx
@@ -27,20 +27,27 @@
 #include <ctime>
 #include <vector>
 
-void CheckEqual(vtkTable* table, std::vector<std::vector<double>>& stdTable)
+const int size = 100;
+const double prob = 1.0 - 1.0 / size;
+const double highProb = 1.0 - 1.0 / (size * size);
+
+void CheckEqual(
+  vtkTable* table, std::vector<std::vector<double>>& stdTable, const std::string& info)
 {
   // Check sizes
   if (table->GetNumberOfRows() != static_cast<vtkIdType>(stdTable[0].size()))
   {
+    cout << "TestTable CheckEqual() fails after operation '" << info << "()':" << endl;
     cout << "Number of rows is incorrect (" << table->GetNumberOfRows() << " != " << stdTable.size()
          << ")" << endl;
-    exit(1);
+    exit(EXIT_FAILURE);
   }
   if (table->GetNumberOfColumns() != static_cast<vtkIdType>(stdTable.size()))
   {
+    cout << "TestTable CheckEqual() fails after operation '" << info << "()':" << endl;
     cout << "Number of columns is incorrect (" << table->GetNumberOfColumns()
          << " != " << stdTable.size() << ")" << endl;
-    exit(1);
+    exit(EXIT_FAILURE);
   }
 
   // Use GetValue() and GetValueByName() to check
@@ -52,9 +59,10 @@ void CheckEqual(vtkTable* table, std::vector<std::vector<double>>& stdTable)
       double stdTableVal = stdTable[j][i];
       if (stdTableVal && tableVal != stdTableVal)
       {
+        cout << "TestTable CheckEqual() fails after operation '" << info << "()':" << endl;
         cout << "Values not equal at row " << i << " column " << j << ": ";
         cout << "(" << tableVal << " != " << stdTableVal << ")" << endl;
-        exit(1);
+        exit(EXIT_FAILURE);
       }
     }
   }
@@ -89,15 +97,17 @@ void CheckEqual(vtkTable* table, std::vector<std::vector<double>>& stdTable)
       }
       else
       {
+        cout << "TestTable CheckEqual() fails after operation '" << info << "()':" << endl;
         cout << "Unknown array type" << endl;
-        exit(1);
+        exit(EXIT_FAILURE);
       }
       double stdTableVal = stdTable[j][i];
       if (stdTableVal && val != stdTableVal)
       {
+        cout << "TestTable CheckEqual() fails after operation '" << info << "()':" << endl;
         cout << "Values not equal at row " << i << " column " << j << ": ";
         cout << "(" << val << " != " << stdTableVal << ")" << endl;
-        exit(1);
+        exit(EXIT_FAILURE);
       }
     }
   }
@@ -112,30 +122,17 @@ void CheckEqual(vtkTable* table, std::vector<std::vector<double>>& stdTable)
       double stdTableVal = stdTable[j][i];
       if (stdTableVal && val != stdTableVal)
       {
+        cout << "TestTable CheckEqual() fails after operation '" << info << "()':" << endl;
         cout << "Values not equal at row " << i << " column " << j << ": ";
         cout << "(" << val << " != " << stdTableVal << ")" << endl;
-        exit(1);
+        exit(EXIT_FAILURE);
       }
     }
   }
 }
 
-int TestTable(int, char*[])
+void FillTable(vtkTable* table, std::vector<std::vector<double>>& stdTable)
 {
-  cout << "CTEST_FULL_OUTPUT" << endl;
-
-  long seed = time(nullptr);
-  cout << "Seed: " << seed << endl;
-  vtkMath::RandomSeed(seed);
-
-  // Make a table and a parallel vector of vectors
-  vtkTable* table = vtkTable::New();
-  std::vector<std::vector<double>> stdTable;
-
-  int size = 100;
-  double prob = 1.0 - 1.0 / size;
-  double highProb = 1.0 - 1.0 / (size * size);
-
   cout << "Creating columns." << endl;
   vtkIdType columnId = 0;
   bool noColumns = true;
@@ -173,6 +170,75 @@ int TestTable(int, char*[])
     columnId++;
   }
 
+  CheckEqual(table, stdTable, "FillTable");
+}
+
+void AddColumn(vtkTable* table, std::vector<std::vector<double>>& stdTable)
+{
+  vtkVariant name(table->GetNumberOfColumns());
+  vtkNew<vtkDoubleArray> arr;
+  arr->SetNumberOfComponents(1);
+  arr->SetNumberOfTuples(table->GetNumberOfRows());
+  arr->SetName((name.ToString() + " (vtkDoubleArray)").c_str());
+  arr->FillComponent(0, 0.0);
+  table->AddColumn(arr);
+
+  std::vector<double> fillvec;
+  fillvec.resize(table->GetNumberOfRows(), 0.0);
+  stdTable.push_back(fillvec);
+
+  CheckEqual(table, stdTable, "AddColumn");
+}
+
+void InsertColumn(vtkTable* table, std::vector<std::vector<double>>& stdTable, int c0)
+{
+  vtkVariant name(table->GetNumberOfColumns());
+  vtkNew<vtkDoubleArray> arr;
+  arr->SetNumberOfComponents(1);
+  arr->SetNumberOfTuples(table->GetNumberOfRows());
+  arr->SetName((name.ToString() + " (vtkDoubleArray)").c_str());
+  arr->FillComponent(0, 0.0);
+  table->InsertColumn(arr, c0);
+
+  std::vector<double> fillvec;
+  fillvec.resize(table->GetNumberOfRows(), 0.0);
+  stdTable.insert(stdTable.begin() + c0, fillvec);
+
+  CheckEqual(table, stdTable, "InsertColumn");
+}
+
+void InsertRows(
+  vtkTable* table, std::vector<std::vector<double>>& stdTable, const vtkIdType r0, const int n)
+{
+  cout << "Inserting rows in middle of table." << endl;
+  const int ncols = table->GetNumberOfColumns();
+
+  // insert rows in middle of stdTable, fill with 0.0
+  for (int c = 0; c < ncols; c++)
+  {
+    for (int r = r0; r < r0 + n; r++)
+    {
+      stdTable[c].insert(stdTable[c].begin() + r0, 0.0);
+    }
+  }
+
+  // insert rows in middle of vtkTable
+  // then fill with defined value, i.e. 0.0
+  table->InsertRows(r0, n);
+  for (int r = r0; r < r0 + n; r++)
+  {
+    for (int c = 0; c < ncols; c++)
+    {
+      table->SetValue(r, c, 0.0);
+    }
+  }
+
+  // compare if the remainder of the tables still equal each other
+  CheckEqual(table, stdTable, "InsertRowsInMiddle");
+}
+
+void InsertEmptyRows(vtkTable* table, std::vector<std::vector<double>>& stdTable)
+{
   cout << "Inserting empty rows." << endl;
   bool noRows = true;
   while (noRows || vtkMath::Random() < prob)
@@ -184,7 +250,11 @@ int TestTable(int, char*[])
       stdTable[i].push_back(0.0);
     }
   }
+  CheckEqual(table, stdTable, "InsertEmptyRows");
+}
 
+void InsertFullRows(vtkTable* table, std::vector<std::vector<double>>& stdTable)
+{
   cout << "Inserting full rows." << endl;
   while (vtkMath::Random() < prob)
   {
@@ -197,7 +267,11 @@ int TestTable(int, char*[])
     table->InsertNextRow(rowArray);
     rowArray->Delete();
   }
+  CheckEqual(table, stdTable, "InsertFullRows");
+}
 
+void RandomizeValues(vtkTable* table, std::vector<std::vector<double>>& stdTable)
+{
   cout << "Performing all kinds of inserts." << endl;
   int id = 0;
   while (vtkMath::Random() < highProb)
@@ -231,7 +305,11 @@ int TestTable(int, char*[])
 
     id++;
   }
+  CheckEqual(table, stdTable, "RandomInserts");
+}
 
+void RemoveHalfOfRows(vtkTable* table, std::vector<std::vector<double>>& stdTable)
+{
   cout << "Removing half of the rows." << endl;
   int numRowsToRemove = table->GetNumberOfRows() / 2;
   for (int i = 0; i < numRowsToRemove; i++)
@@ -249,7 +327,17 @@ int TestTable(int, char*[])
       stdTable[j].erase(rowIt);
     }
   }
+  CheckEqual(table, stdTable, "RemoveHalfRows");
+}
 
+void SqueezeRows(vtkTable* table, std::vector<std::vector<double>>& stdTable)
+{
+  table->SqueezeRows();
+  CheckEqual(table, stdTable, "SqueezeRows");
+}
+
+void RemoveHalfOfColumns(vtkTable* table, std::vector<std::vector<double>>& stdTable)
+{
   cout << "Removing half of the columns." << endl;
   int numColsToRemove = table->GetNumberOfColumns() / 2;
   for (int i = 0; i < numColsToRemove; i++)
@@ -267,15 +355,56 @@ int TestTable(int, char*[])
     std::vector<std::vector<double>>::iterator colIt = stdTable.begin() + col;
     stdTable.erase(colIt);
   }
+  CheckEqual(table, stdTable, "RemoveHalfColumns");
+}
 
-  cout << "vtkTable size: " << table->GetNumberOfRows() << "," << table->GetNumberOfColumns()
-       << endl;
-  cout << "std::vector<std::vector<double> > size: " << stdTable[0].size() << "," << stdTable.size()
-       << endl;
+int TestTable(int, char*[])
+{
+  cout << "CTEST_FULL_OUTPUT" << endl;
 
-  cout << "Checking that table matches expected table." << endl;
-  CheckEqual(table, stdTable);
+  long seed = time(nullptr);
+  cout << "Seed: " << seed << endl;
+  vtkMath::RandomSeed(seed);
 
-  table->Delete();
-  return 0;
+  // Make a table and a parallel vector of vectors containing the same data
+  vtkNew<vtkTable> table;
+  std::vector<std::vector<double>> stdTable;
+  FillTable(table, stdTable);
+
+  InsertEmptyRows(table, stdTable);
+  RandomizeValues(table, stdTable);
+
+  InsertFullRows(table, stdTable);
+  RandomizeValues(table, stdTable);
+
+  // add new column to the end of the table
+  AddColumn(table, stdTable);
+  RandomizeValues(table, stdTable);
+
+  // insert new column in the middle of the table
+  InsertColumn(table, stdTable, table->GetNumberOfColumns() / 2);
+  RandomizeValues(table, stdTable);
+
+  // insert new rows at the beginning of the table
+  InsertRows(table, stdTable, 0, 3);
+  RandomizeValues(table, stdTable);
+
+  // insert new rows in the middle of the table
+  InsertRows(table, stdTable, table->GetNumberOfRows() / 2, 3);
+  RandomizeValues(table, stdTable);
+
+  // insert new rows at the end of the table
+  InsertRows(table, stdTable, table->GetNumberOfRows() - 1, 3);
+  RandomizeValues(table, stdTable);
+
+  RemoveHalfOfRows(table, stdTable);
+  RandomizeValues(table, stdTable);
+
+  SqueezeRows(table, stdTable);
+  RandomizeValues(table, stdTable);
+
+  RemoveHalfOfColumns(table, stdTable);
+  RandomizeValues(table, stdTable);
+
+  return EXIT_SUCCESS;
 }

Common/DataModel/vtkTable.h
@@ -28,6 +28,11 @@
  * has the same number of entries, and provides row access (using vtkVariantArray)
  * and single entry access (using vtkVariant).
  *
+ * Inserting or removing rows via the class API preserves existing table data where possible.
+ *
+ * The "RemoveRow*" and SetNumberOfRows() operations will not release memory. Call on SqueezeRows()
+ * to achieve this after performing the operations.
+ *
  * The field data inherited from vtkDataObject may be used to store metadata
  * related to the table.
  *
@@ -108,9 +113,15 @@ public:
 
   /**
    * Set the number of rows in the table. Note that memory allocation might be performed
-   * as a result of this, but no memory will be released.
+   * as a result of this, but no memory will be released. Existing data is preserved if the table is
+   * expanding.
    */
   void SetNumberOfRows(const vtkIdType);
+
+  /**
+   * Release previously allocated and now unused memory after performing resizing operations.
+   */
+  void SqueezeRows();
 
   /**
    * Get a row of the table as a vtkVariantArray which has one entry for each column.
@@ -129,20 +140,41 @@ public:
   void SetRow(vtkIdType row, vtkVariantArray* values);
 
   /**
+   * Insert a single row at the index.
+   */
+  void InsertRow(vtkIdType row);
+
+  /**
+   * Insert n rows before row. If row < 0 then the rows will be prepended to the table.
+   */
+  void InsertRows(vtkIdType row, vtkIdType n);
+
+  /**
    * Insert a blank row at the end of the table.
    */
   vtkIdType InsertNextBlankRow(double default_num_val = 0.0);
 
   /**
-   * Insert a row specified by a vtkVariantArray.  The number of entries in the array
-   * should match the number of columns in the table.
+   * Insert a row at the end of the tablespecified by a vtkVariantArray. The number of entries in
+   * the array should match the number of columns in the table.
    */
   vtkIdType InsertNextRow(vtkVariantArray* values);
 
   /**
-   * Delete a row from the table.  Rows below the deleted row are shifted up.
+   * Delete a single row from the table. Rows below the deleted row are shifted up.
    */
   void RemoveRow(vtkIdType row);
+
+  /**
+   * Delete n rows from the table, starting at row. Rows below the deleted rows are shifted up.
+   */
+  void RemoveRows(vtkIdType row, vtkIdType n);
+
+  /**
+   * Delete all rows from the table. The column arrays are not delete, they are just empty
+   * after this operation.
+   */
+  void RemoveAllRows();
 
   //
   // Column functions
@@ -162,6 +194,12 @@ public:
   vtkAbstractArray* GetColumnByName(const char* name);
 
   /**
+   * Get the column index for a name.
+   * If name is not found returns -1.
+   */
+  vtkIdType GetColumnIndex(const char* name);
+
+  /**
    * Get a column of the table by its column index.
    */
   vtkAbstractArray* GetColumn(vtkIdType col);
@@ -172,6 +210,11 @@ public:
   void AddColumn(vtkAbstractArray* arr);
 
   /**
+   * Insert a column into the table at given column index.
+   */
+  void InsertColumn(vtkAbstractArray* arr, vtkIdType index);
+
+  /**
    * Remove a column from the table by its name.
    */
   void RemoveColumnByName(const char* name);
@@ -180,6 +223,11 @@ public:
    * Remove a column from the table by its column index.
    */
   void RemoveColumn(vtkIdType col);
+
+  /**
+   * Remove all columns from the table.
+   */
+  void RemoveAllColumns();
 
   //
   // Table single entry functions
@@ -255,6 +303,13 @@ protected:
    * Holds row information returned by GetRow().
    */
   vtkVariantArray* RowArray;
+
+  /**
+   * Move the content of the rows, starting first row and including last row. The rows
+   * will be moved by delta, which can be positive or negative. No checks will be performed that the
+   * arrays are correctly sized.
+   */
+  void MoveRowData(vtkIdType first, vtkIdType last, vtkIdType delta);
 
 private:
   vtkTable(const vtkTable&) = delete;

commit 76e28a28e2d5191786035ad4c059d9ad43365039

Include algorithm for std::max/std::min
Fix issue 'max' : is not a member of 'std' with MSVC 14.16 (VS2017),
introduced in a6caabe.

Common/DataModel/vtkTable.cxx
@@ -34,6 +34,7 @@
 #include "vtkUnicodeStringArray.h"
 #include "vtkVariantArray.h"
 
+#include <algorithm>
 #include <vector>
 
 //