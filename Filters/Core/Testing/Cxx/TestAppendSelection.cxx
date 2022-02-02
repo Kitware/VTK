45fbf37500aa0d37714a13391df16d944bf75c70
Spiros Tsalikis <spiros.tsalikis@kitware.com>
2022-02-02 15:52:25 -0500

    vtkAppendSelection: Allow setting an expression and input names

--- Filters/Core/Testing/Cxx/TestAppendSelection.cxx
+++ Filters/Core/Testing/Cxx/TestAppendSelection.cxx
@@ -18,13 +18,16 @@
 ----------------------------------------------------------------------------*/
 
 #include "vtkAppendSelection.h"
+#include "vtkExtractSelection.h"
+#include "vtkIOSSReader.h"
 #include "vtkIdTypeArray.h"
 #include "vtkInformation.h"
+#include "vtkNew.h"
+#include "vtkPartitionedDataSetCollection.h"
 #include "vtkSelection.h"
 #include "vtkSelectionNode.h"
-#include "vtkSmartPointer.h"
-
-#define VTK_CREATE(type, name) vtkSmartPointer<type> name = vtkSmartPointer<type>::New()
+#include "vtkSelectionSource.h"
+#include "vtkTestUtilities.h"
 
 int SelectionCompare(vtkSelectionNode* a, vtkSelectionNode* b)
 {
@@ -108,7 +111,7 @@ int SelectionCompare(vtkSelection* a, vtkSelection* b)
 
 int TestAppendSelectionCase(vtkSelection* input1, vtkSelection* input2, vtkSelection* correct)
 {
-  VTK_CREATE(vtkAppendSelection, append);
+  vtkNew<vtkAppendSelection> append;
   append->AddInputData(input1);
   append->AddInputData(input2);
   append->Update();
@@ -116,15 +119,15 @@ int TestAppendSelectionCase(vtkSelection* input1, vtkSelection* input2, vtkSelec
   return SelectionCompare(output, correct);
 }
 
-int TestAppendSelection(int, char*[])
+int TestAppendSelection(int argc, char* argv[])
 {
   int errors = 0;
 
   {
-    cerr << "Testing appending sel selections ..." << endl;
-    VTK_CREATE(vtkSelection, sel1);
-    VTK_CREATE(vtkSelectionNode, sel1Node);
-    VTK_CREATE(vtkIdTypeArray, sel1Arr);
+    cerr << "Testing appending cell-indices selections ..." << endl;
+    vtkNew<vtkSelection> sel1;
+    vtkNew<vtkSelectionNode> sel1Node;
+    vtkNew<vtkIdTypeArray> sel1Arr;
     sel1->AddNode(sel1Node);
     sel1Node->SetContentType(vtkSelectionNode::INDICES);
     sel1Node->SetFieldType(vtkSelectionNode::CELL);
@@ -132,9 +135,9 @@ int TestAppendSelection(int, char*[])
     sel1Arr->InsertNextValue(0);
     sel1Arr->InsertNextValue(1);
     sel1Arr->InsertNextValue(2);
-    VTK_CREATE(vtkSelection, sel2);
-    VTK_CREATE(vtkSelectionNode, sel2Node);
-    VTK_CREATE(vtkIdTypeArray, sel2Arr);
+    vtkNew<vtkSelection> sel2;
+    vtkNew<vtkSelectionNode> sel2Node;
+    vtkNew<vtkIdTypeArray> sel2Arr;
     sel2->AddNode(sel2Node);
     sel2Node->SetContentType(vtkSelectionNode::INDICES);
     sel2Node->SetFieldType(vtkSelectionNode::CELL);
@@ -142,9 +145,9 @@ int TestAppendSelection(int, char*[])
     sel2Arr->InsertNextValue(3);
     sel2Arr->InsertNextValue(4);
     sel2Arr->InsertNextValue(5);
-    VTK_CREATE(vtkSelection, selAppend);
-    VTK_CREATE(vtkSelectionNode, selAppendNode);
-    VTK_CREATE(vtkIdTypeArray, selAppendArr);
+    vtkNew<vtkSelection> selAppend;
+    vtkNew<vtkSelectionNode> selAppendNode;
+    vtkNew<vtkIdTypeArray> selAppendArr;
     selAppend->AddNode(selAppendNode);
     selAppendNode->SetContentType(vtkSelectionNode::INDICES);
     selAppendNode->SetFieldType(vtkSelectionNode::CELL);
@@ -160,10 +163,10 @@ int TestAppendSelection(int, char*[])
   }
 
   {
-    cerr << "Testing appending value selections ..." << endl;
-    VTK_CREATE(vtkSelection, sel1);
-    VTK_CREATE(vtkSelectionNode, sel1Node);
-    VTK_CREATE(vtkIdTypeArray, sel1Arr);
+    cerr << "Testing appending cell-values selections ..." << endl;
+    vtkNew<vtkSelection> sel1;
+    vtkNew<vtkSelectionNode> sel1Node;
+    vtkNew<vtkIdTypeArray> sel1Arr;
     sel1->AddNode(sel1Node);
     sel1Arr->SetName("arrayname");
     sel1Node->SetContentType(vtkSelectionNode::VALUES);
@@ -172,9 +175,9 @@ int TestAppendSelection(int, char*[])
     sel1Arr->InsertNextValue(0);
     sel1Arr->InsertNextValue(1);
     sel1Arr->InsertNextValue(2);
-    VTK_CREATE(vtkSelection, sel2);
-    VTK_CREATE(vtkSelectionNode, sel2Node);
-    VTK_CREATE(vtkIdTypeArray, sel2Arr);
+    vtkNew<vtkSelection> sel2;
+    vtkNew<vtkSelectionNode> sel2Node;
+    vtkNew<vtkIdTypeArray> sel2Arr;
     sel2->AddNode(sel2Node);
     sel2Arr->SetName("arrayname");
     sel2Node->SetContentType(vtkSelectionNode::VALUES);
@@ -183,9 +186,9 @@ int TestAppendSelection(int, char*[])
     sel2Arr->InsertNextValue(3);
     sel2Arr->InsertNextValue(4);
     sel2Arr->InsertNextValue(5);
-    VTK_CREATE(vtkSelection, selAppend);
-    VTK_CREATE(vtkSelectionNode, selAppendNode);
-    VTK_CREATE(vtkIdTypeArray, selAppendArr);
+    vtkNew<vtkSelection> selAppend;
+    vtkNew<vtkSelectionNode> selAppendNode;
+    vtkNew<vtkIdTypeArray> selAppendArr;
     selAppend->AddNode(selAppendNode);
     selAppendArr->SetName("arrayname");
     selAppendNode->SetContentType(vtkSelectionNode::VALUES);
@@ -202,10 +205,10 @@ int TestAppendSelection(int, char*[])
   }
 
   {
-    cerr << "Testing appending cell selections with different process ids..." << endl;
-    VTK_CREATE(vtkSelection, sel1);
-    VTK_CREATE(vtkSelectionNode, sel1Node);
-    VTK_CREATE(vtkIdTypeArray, sel1Arr);
+    cerr << "Testing appending cell-indices selections with different process ids..." << endl;
+    vtkNew<vtkSelection> sel1;
+    vtkNew<vtkSelectionNode> sel1Node;
+    vtkNew<vtkIdTypeArray> sel1Arr;
     sel1->AddNode(sel1Node);
     sel1Node->SetContentType(vtkSelectionNode::INDICES);
     sel1Node->SetFieldType(vtkSelectionNode::CELL);
@@ -214,9 +217,9 @@ int TestAppendSelection(int, char*[])
     sel1Arr->InsertNextValue(0);
     sel1Arr->InsertNextValue(1);
     sel1Arr->InsertNextValue(2);
-    VTK_CREATE(vtkSelection, sel2);
-    VTK_CREATE(vtkSelectionNode, sel2Node);
-    VTK_CREATE(vtkIdTypeArray, sel2Arr);
+    vtkNew<vtkSelection> sel2;
+    vtkNew<vtkSelectionNode> sel2Node;
+    vtkNew<vtkIdTypeArray> sel2Arr;
     sel2->AddNode(sel2Node);
     sel2Node->SetContentType(vtkSelectionNode::INDICES);
     sel2Node->SetFieldType(vtkSelectionNode::CELL);
@@ -225,10 +228,77 @@ int TestAppendSelection(int, char*[])
     sel2Arr->InsertNextValue(3);
     sel2Arr->InsertNextValue(4);
     sel2Arr->InsertNextValue(5);
-    VTK_CREATE(vtkSelection, selAppend);
+    vtkNew<vtkSelection> selAppend;
     selAppend->AddNode(sel1Node);
     selAppend->AddNode(sel2Node);
     errors += TestAppendSelectionCase(sel1, sel2, selAppend);
+    cerr << "... done." << endl;
+  }
+
+  {
+    cerr << "Testing appending cell-indices selections with expression..." << endl;
+    // create first selection
+    vtkNew<vtkSelectionSource> sel1;
+    sel1->SetNumberOfNodes(2);
+    sel1->SetFieldType(vtkSelectionNode::CELL);
+    // create first node of first selection
+    sel1->SetNodeName(0, "node0");
+    sel1->SetContentType(0, vtkSelectionNode::INDICES);
+    sel1->SetCompositeIndex(0, 2);
+    sel1->AddID(0, -1, 0);
+    sel1->AddID(0, -1, 1);
+    sel1->AddID(0, -1, 2);
+    sel1->AddID(0, -1, 5);
+    sel1->AddID(0, -1, 7);
+    // create second node of first selection
+    sel1->SetNodeName(1, "node1");
+    sel1->SetFieldType(vtkSelectionNode::CELL);
+    sel1->SetContentType(1, vtkSelectionNode::INDICES);
+    sel1->SetCompositeIndex(1, 2);
+    sel1->AddID(1, -1, 0);
+    sel1->AddID(1, -1, 1);
+    sel1->AddID(1, -1, 2);
+    sel1->AddID(1, -1, 3);
+    sel1->AddID(1, -1, 4);
+    sel1->AddID(1, -1, 6);
+    // this expression should generate only 3 ids (the common ids = 0, 1, 2)
+    sel1->SetExpression("node0&node1");
+
+    // create first selection
+    vtkNew<vtkSelectionSource> sel2;
+    sel2->SetNumberOfNodes(1);
+    sel2->SetFieldType(vtkSelectionNode::CELL);
+    // create first node of second selection
+    sel2->SetNodeName(0, "node0");
+    sel2->SetContentType(0, vtkSelectionNode::INDICES);
+    sel2->SetCompositeIndex(0, 2);
+    sel2->AddID(0, -1, 10);
+    sel2->AddID(0, -1, 11);
+    sel2->AddID(0, -1, 12);
+    // this selection should generate 3 ids (10, 11, 12)
+    sel2->SetExpression("node0");
+
+    vtkNew<vtkAppendSelection> append;
+    append->AppendByUnionOff();
+    append->AddInputConnection(sel1->GetOutputPort());
+    append->SetInputName(0, "S0");
+    append->AddInputConnection(sel2->GetOutputPort());
+    append->SetInputName(1, "S1");
+    // this selection should generate 6 cell ids (0, 1, 2, 10, 11, 12)
+    append->SetExpression("S0|S1");
+
+    char* fileNameC = vtkTestUtilities::ExpandDataFileName(argc, argv, "Data/can.ex2");
+    vtkNew<vtkIOSSReader> reader;
+    reader->AddFileName(fileNameC);
+    delete[] fileNameC;
+    reader->Update();
+
+    vtkNew<vtkExtractSelection> extract;
+    extract->SetInputConnection(0, reader->GetOutputPort());
+    extract->SetInputConnection(1, append->GetOutputPort());
+    extract->Update();
+    auto pdc = vtkPartitionedDataSetCollection::SafeDownCast(extract->GetOutput());
+    errors += pdc->GetNumberOfCells() != 6;
     cerr << "... done." << endl;
   }
 
