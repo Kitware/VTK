56b6b1c2bfb0547607140a2736848d113321e446
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-02-11 09:50:19 +0100

    Remove outdated remote modules that do not build anymore

--- Remote/SplineDrivenImageSlicer.remote.cmake
+++
@@ -1,9 +0,0 @@
-#
-# Midas Journal 838
-#
-
-vtk_fetch_module(SplineDrivenImageSlicer
-        "Spline Driven Image Slicer - http://www.vtkjournal.org/browse/publication/838"
-        GIT_REPOSITORY https://github.com/martinken/midas-journal-838.git
-        GIT_TAG 5a98940c705748df5c2470331541a051559956c8
-        )
