Kitware/VTK

commit 7db5c1954195cb4c999cbc09bcd6f14be022d336

ENH: Improvements to OBJExporter
1) Use std streams instead of file pointers
2) Use vtkNumberToString to improve accuracy of saved coordinates
3) Modulate diffuse, ambient and specular colors by corresponding multipliers
4) Handle textures from actor and from property(GLTF files).
5) Added tests

IO/Export/vtkOBJExporter.cxx
@@ -21,16 +21,24 @@
 #include "vtkDataSet.h"
 #include "vtkFloatArray.h"
 #include "vtkGeometryFilter.h"
+#include "vtkImageData.h"
+#include "vtkImageFlip.h"
 #include "vtkMapper.h"
 #include "vtkNew.h"
 #include "vtkObjectFactory.h"
+#include "vtkPNGWriter.h"
 #include "vtkPointData.h"
 #include "vtkPolyData.h"
 #include "vtkProperty.h"
 #include "vtkRendererCollection.h"
 #include "vtkRenderWindow.h"
+#include "vtkTexture.h"
 #include "vtkTransform.h"
+#include "vtkNumberToString.h"
+
 #include "vtksys/SystemTools.hxx"
+
+#include <sstream>
 
 vtkStandardNewMacro(vtkOBJExporter);
 
@@ -39,7 +47,7 @@ vtkOBJExporter::vtkOBJExporter()
   this->FilePrefix = nullptr;
   this->OBJFileComment = nullptr;
   this->MTLFileComment = nullptr;
-
+  this->FlipTexture = false;
   this->SetOBJFileComment("wavefront obj file written by the visualization toolkit");
   this->SetMTLFileComment("wavefront mtl file written by the visualization toolkit");
 }
@@ -75,17 +83,29 @@ void vtkOBJExporter::WriteData()
 
   // try opening the files
   std::string objFilePath = std::string(this->FilePrefix) + ".obj";
-  FILE *fpObj = fopen(objFilePath.c_str(), "w");
+  std::string filePrefix(this->FilePrefix);
+  // get the model name which isthe last component of the FilePrefix
+  std::string modelName;
+  if (filePrefix.find_last_of("/") != std::string::npos)
+  {
+    modelName = filePrefix.substr(filePrefix.find_last_of("/") + 1);
+  }
+  else
+  {
+    modelName = filePrefix;
+  }
+
+  std::ofstream fpObj(objFilePath, ios::out);
   if (!fpObj)
   {
-    vtkErrorMacro(<< "unable to open .obj files ");
+    vtkErrorMacro(<< "unable to open " << objFilePath);
     return;
   }
   std::string mtlFilePath = std::string(this->FilePrefix) + ".mtl";
-  FILE *fpMtl = fopen(mtlFilePath.c_str(), "w");
+  std::ofstream fpMtl(mtlFilePath, ios::out);
   if (!fpMtl)
   {
-    fclose(fpObj);
+    fpMtl.close();
     vtkErrorMacro(<< "unable to open .mtl files ");
     return;
   }
@@ -96,17 +116,15 @@ void vtkOBJExporter::WriteData()
   vtkDebugMacro("Writing wavefront files");
   if (this->GetOBJFileComment())
   {
-    fprintf(fpObj, "# %s\n\n", this->GetOBJFileComment());
+    fpObj << "#  " << this->GetOBJFileComment() << "\n\n";
   }
 
   std::string mtlFileName = vtksys::SystemTools::GetFilenameName(mtlFilePath);
-  fprintf(fpObj, "mtllib %s\n\n", mtlFileName.c_str());
-
+  fpObj << "mtllib " << mtlFileName << "\n\n";
   if (this->GetMTLFileComment())
   {
-    fprintf(fpMtl, "# %s\n\n", this->GetMTLFileComment());
+    fpMtl << "# " << this->GetMTLFileComment() << "\n\n";
   }
-
 
   vtkActorCollection *allActors = ren->GetActors();
   vtkCollectionSimpleIterator actorsIt;
@@ -118,15 +136,40 @@ void vtkOBJExporter::WriteData()
     for (anActor->InitPathTraversal(); (aPath = anActor->GetNextPath()); )
     {
       vtkActor *aPart = vtkActor::SafeDownCast(aPath->GetLastNode()->GetViewProp());
-      this->WriteAnActor(aPart, fpObj, fpMtl, idStart);
+      this->WriteAnActor(aPart, fpObj, fpMtl, modelName, idStart);
     }
   }
-
-  fclose(fpObj);
-  fclose(fpMtl);
+  // Write texture files
+  for (auto t : this->TextureFileMap)
+  {
+    std::stringstream fullFileName;
+    fullFileName << this->FilePrefix << t.first;
+    auto writeTexture =
+      vtkSmartPointer<vtkPNGWriter>::New();
+    if (this->FlipTexture)
+    {
+      auto flip =
+        vtkSmartPointer<vtkImageFlip>::New();
+      flip->SetInputData(t.second->GetInput());
+      flip->SetFilteredAxis(1);
+      flip->Update();
+      writeTexture->SetInputData(flip->GetOutput());
+    }
+    else
+    {
+      writeTexture->SetInputData(t.second->GetInput());
+    }
+    writeTexture->SetFileName(fullFileName.str().c_str());
+    writeTexture->Write();
+  }
+  fpObj.close();
+  fpMtl.close();
 }
 
-void vtkOBJExporter::WriteAnActor(vtkActor *anActor, FILE *fpObj, FILE *fpMtl,
+void vtkOBJExporter::WriteAnActor(vtkActor *anActor,
+                                  std::ofstream &fpObj,
+                                  std::ofstream &fpMtl,
+                                  std::string &modelName,
                                   int &idStart)
 {
   vtkDataSet *ds;
@@ -154,16 +197,57 @@ void vtkOBJExporter::WriteAnActor(vtkActor *anActor, FILE *fpObj, FILE *fpMtl,
   {
     return;
   }
-  fprintf(fpMtl,"newmtl mtl%i\n",idStart);
+  vtkNumberToString convert;
+  double temp;
+  fpMtl << "newmtl mtl" << idStart << "\n";
   tempd = prop->GetAmbientColor();
-  fprintf(fpMtl,"Ka %g %g %g\n",tempd[0], tempd[1], tempd[2]);
+  temp = prop->GetAmbient();
+  fpMtl << "Ka "
+        << convert(temp * tempd[0]) << " "
+        << convert(temp * tempd[1]) << " "
+        << convert(temp * tempd[2])
+        << "\n";
   tempd = prop->GetDiffuseColor();
-  fprintf(fpMtl,"Kd %g %g %g\n",tempd[0], tempd[1], tempd[2]);
+  temp = prop->GetDiffuse();
+  fpMtl << "Kd "
+        << convert(temp * tempd[0]) << " "
+        << convert(temp * tempd[1]) << " "
+        << convert(temp * tempd[2])
+        << "\n";
   tempd = prop->GetSpecularColor();
-  fprintf(fpMtl,"Ks %g %g %g\n",tempd[0], tempd[1], tempd[2]);
-  fprintf(fpMtl,"Ns %g\n",prop->GetSpecularPower());
-  fprintf(fpMtl,"Tr %g ", prop->GetOpacity());
-  fprintf(fpMtl,"illum 3\n\n");
+  temp = prop->GetSpecular();
+  fpMtl << "Ks "
+        << convert(temp * tempd[0]) << " "
+        << convert(temp * tempd[1]) << " "
+        << convert(temp * tempd[2])
+        << "\n";
+  fpMtl << "Ns " << convert(prop->GetSpecularPower()) << "\n";
+  fpMtl << "Tr " <<convert(prop->GetOpacity()) << "\n";
+  fpMtl << "illum 3\n";
+
+  // Actor has the texture
+  bool hasTexture = anActor->GetTexture() != nullptr;;
+
+  // Actor's property has the texture. We choose the albedo texture
+  // since it seems to be similar to the texture we expect
+  auto allTextures = prop->GetAllTextures();
+  bool hasTextureProp = allTextures.find("albedoTex") != allTextures.end();
+  if (hasTexture)
+  {
+    std::stringstream textureFileName;
+    textureFileName << "texture" << idStart << ".png";
+    fpMtl << "map_Kd " << modelName << textureFileName.str() << "\n\n";
+    this->TextureFileMap[textureFileName.str()] = anActor->GetTexture();
+  }
+  else if (hasTextureProp)
+  {
+    std::stringstream textureFileName;
+    textureFileName << "albedoTex" << "_" << idStart << ".png";
+    fpMtl << "map_Kd " << modelName << textureFileName.str() << "\n\n";
+    auto albedoTexture = allTextures.find("albedoTex");
+    this->TextureFileMap[textureFileName.str()] = albedoTexture->second;
+    this->FlipTexture = true;
+  }
 
   // get the mappers input and matrix
   ds = anActor->GetMapper()->GetInput();
@@ -195,7 +279,10 @@ void vtkOBJExporter::WriteAnActor(vtkActor *anActor, FILE *fpObj, FILE *fpMtl,
   for (i = 0; i < points->GetNumberOfPoints(); i++)
   {
     p = points->GetPoint(i);
-    fprintf (fpObj, "v %g %g %g\n", p[0], p[1], p[2]);
+    fpObj << "v "
+          << convert(p[0]) << " "
+          << convert(p[1]) << " "
+          << convert(p[2]) << "\n";
   }
   idNext = idStart + static_cast<int>(points->GetNumberOfPoints());
   points->Delete();
@@ -210,7 +297,10 @@ void vtkOBJExporter::WriteAnActor(vtkActor *anActor, FILE *fpObj, FILE *fpMtl,
     for (i = 0; i < normals->GetNumberOfTuples(); i++)
     {
       p = normals->GetTuple(i);
-      fprintf (fpObj, "vn %g %g %g\n", p[0], p[1], p[2]);
+      fpObj << "vn "
+            << convert(p[0]) << " "
+            << convert(p[1]) << " "
+            << convert(p[2]) << "\n";
     }
   }
 
@@ -220,27 +310,29 @@ void vtkOBJExporter::WriteAnActor(vtkActor *anActor, FILE *fpObj, FILE *fpMtl,
     for (i = 0; i < tcoords->GetNumberOfTuples(); i++)
     {
       p = tcoords->GetTuple(i);
-      fprintf (fpObj, "vt %g %g\n", p[0], p[1]);
+      fpObj << "vt "
+            << convert(p[0]) << " "
+            << convert(p[1]) << " "
+            << 0.0 << "\n";
     }
   }
 
   // write out a group name and material
-  fprintf (fpObj, "\ng grp%i\n", idStart);
-  fprintf (fpObj, "usemtl mtl%i\n", idStart);
-
+  fpObj << "\ng grp" << idStart << "\n";
+  fpObj << "usemtl mtl" << idStart << "\n";
   // write out verts if any
   if (pd->GetNumberOfVerts() > 0)
   {
     cells = pd->GetVerts();
     for (cells->InitTraversal(); cells->GetNextCell(npts,indx); )
     {
-      fprintf(fpObj,"p ");
+      fpObj << "p ";
       for (i = 0; i < npts; i++)
       {
         // treating vtkIdType as int
-        fprintf(fpObj,"%i ", static_cast<int>(indx[i])+idStart);
+        fpObj << static_cast<int>(indx[i]) + idStart << " ";
       }
-      fprintf(fpObj,"\n");
+      fpObj << "\n";
     }
   }
 
@@ -250,35 +342,34 @@ void vtkOBJExporter::WriteAnActor(vtkActor *anActor, FILE *fpObj, FILE *fpMtl,
     cells = pd->GetLines();
     for (cells->InitTraversal(); cells->GetNextCell(npts,indx); )
     {
-      fprintf(fpObj,"l ");
+      fpObj << "l ";
       if (tcoords)
       {
         for (i = 0; i < npts; i++)
         {
           // treating vtkIdType as int
-          fprintf(fpObj,"%i/%i ",static_cast<int>(indx[i])+idStart,
-                  static_cast<int>(indx[i]) + idStart);
-        }
+          fpObj << static_cast<int>(indx[i]) + idStart << "/"
+                << static_cast<int>(indx[i]) + idStart;
       }
-      else
+    }
+    else
+    {
+      for (i = 0; i < npts; i++)
       {
-        for (i = 0; i < npts; i++)
-        {
-          // treating vtkIdType as int
-          fprintf(fpObj,"%i ", static_cast<int>(indx[i])+idStart);
-        }
+        // treating vtkIdType as int
+        fpObj << static_cast<int>(indx[i]) + idStart << " ";
       }
-      fprintf(fpObj,"\n");
+    }
+    fpObj << "\n";
     }
   }
-
   // write out polys if any
   if (pd->GetNumberOfPolys() > 0)
   {
     cells = pd->GetPolys();
     for (cells->InitTraversal(); cells->GetNextCell(npts,indx); )
     {
-      fprintf(fpObj,"f ");
+      fpObj << "f ";
       for (i = 0; i < npts; i++)
       {
         if (pntData->GetNormals())
@@ -286,15 +377,15 @@ void vtkOBJExporter::WriteAnActor(vtkActor *anActor, FILE *fpObj, FILE *fpMtl,
           if (tcoords)
           {
             // treating vtkIdType as int
-            fprintf(fpObj,"%i/%i/%i ", static_cast<int>(indx[i])+idStart,
-                    static_cast<int>(indx[i])+ idStart,
-                    static_cast<int>(indx[i]) + idStart);
+            fpObj << static_cast<int>(indx[i]) + idStart << "/"
+                  << static_cast<int>(indx[i]) + idStart << "/"
+                  << static_cast<int>(indx[i]) + idStart << " ";
           }
           else
           {
             // treating vtkIdType as int
-            fprintf(fpObj,"%i//%i ",static_cast<int>(indx[i])+idStart,
-                    static_cast<int>(indx[i]) + idStart);
+            fpObj << static_cast<int>(indx[i]) + idStart << "//"
+                  << static_cast<int>(indx[i]) + idStart << " ";
           }
         }
         else
@@ -302,17 +393,17 @@ void vtkOBJExporter::WriteAnActor(vtkActor *anActor, FILE *fpObj, FILE *fpMtl,
           if (tcoords)
           {
             // treating vtkIdType as int
-            fprintf(fpObj,"%i/%i ", static_cast<int>(indx[i])+idStart,
-                    static_cast<int>(indx[i]) + idStart);
+            fpObj << static_cast<int>(indx[i]) + idStart << "/"
+                  << static_cast<int>(indx[i]) + idStart << " ";
           }
           else
           {
             // treating vtkIdType as int
-            fprintf(fpObj,"%i ", static_cast<int>(indx[i])+idStart);
+            fpObj << static_cast<int>(indx[i]) + idStart << " ";
           }
         }
       }
-      fprintf(fpObj,"\n");
+      fpObj << "\n";
     }
   }
 
@@ -339,22 +430,24 @@ void vtkOBJExporter::WriteAnActor(vtkActor *anActor, FILE *fpObj, FILE *fpMtl,
           if (tcoords)
           {
             // treating vtkIdType as int
-            fprintf(fpObj,"f %i/%i/%i ", static_cast<int>(indx[i1]) + idStart,
-                    static_cast<int>(indx[i1]) + idStart, static_cast<int>(indx[i1]) + idStart);
-            fprintf(fpObj,"%i/%i/%i ", static_cast<int>(indx[i2])+ idStart,
-                    static_cast<int>(indx[i2]) + idStart, static_cast<int>(indx[i2]) + idStart);
-            fprintf(fpObj,"%i/%i/%i\n", static_cast<int>(indx[i]) + idStart,
-                    static_cast<int>(indx[i]) + idStart, static_cast<int>(indx[i]) + idStart);
+            fpObj << "f "
+                  << static_cast<int>(indx[i1]) + idStart << "/"
+                  << static_cast<int>(indx[i1]) + idStart << "/"
+                  << static_cast<int>(indx[i1]) + idStart << " ";
+            fpObj << static_cast<int>(indx[i2]) + idStart << "/"
+                  << static_cast<int>(indx[i2]) + idStart << "/"
+                  << static_cast<int>(indx[i2]) + idStart << " ";
+            fpObj << static_cast<int>(indx[i]) + idStart << "/"
+                  << static_cast<int>(indx[i]) + idStart << "/"
+                  << static_cast<int>(indx[i]) + idStart << "\n";
           }
           else
           {
             // treating vtkIdType as int
-            fprintf(fpObj,"f %i//%i ", static_cast<int>(indx[i1]) + idStart,
-                    static_cast<int>(indx[i1]) + idStart);
-            fprintf(fpObj,"%i//%i ", static_cast<int>(indx[i2]) + idStart,
-                    static_cast<int>(indx[i2]) + idStart);
-            fprintf(fpObj,"%i//%i\n",static_cast<int>(indx[i]) + idStart,
-                    static_cast<int>(indx[i]) + idStart);
+            fpObj << static_cast<int>(indx[i2]) + idStart << "//"
+                  << static_cast<int>(indx[i2]) + idStart << " ";
+            fpObj << static_cast<int>(indx[i]) + idStart << "//"
+                  << static_cast<int>(indx[i]) + idStart << "\n";
           }
         }
         else
@@ -362,18 +455,21 @@ void vtkOBJExporter::WriteAnActor(vtkActor *anActor, FILE *fpObj, FILE *fpMtl,
           if (tcoords)
           {
             // treating vtkIdType as int
-            fprintf(fpObj,"f %i/%i ", static_cast<int>(indx[i1]) + idStart,
-                    static_cast<int>(indx[i1]) + idStart);
-            fprintf(fpObj,"%i/%i ", static_cast<int>(indx[i2]) + idStart,
-                    static_cast<int>(indx[i2]) + idStart);
-            fprintf(fpObj,"%i/%i\n", static_cast<int>(indx[i]) + idStart,
-                    static_cast<int>(indx[i]) + idStart);
+            fpObj << "f "
+                  << static_cast<int>(indx[i1]) + idStart << "/"
+                  << static_cast<int>(indx[i1]) + idStart << " ";
+            fpObj << static_cast<int>(indx[i2]) + idStart << "/"
+                  << static_cast<int>(indx[i2]) + idStart << " ";
+            fpObj << static_cast<int>(indx[i]) + idStart << "/"
+                  << static_cast<int>(indx[i]) + idStart << "\n";
           }
           else
           {
             // treating vtkIdType as int
-            fprintf(fpObj,"f %i %i %i\n", static_cast<int>(indx[i1]) + idStart,
-                    static_cast<int>(indx[i2]) + idStart, static_cast<int>(indx[i]) + idStart);
+            fpObj << "f "
+                  << static_cast<int>(indx[i1]) + idStart << " "
+                  << static_cast<int>(indx[i2]) + idStart << " "
+                  << static_cast<int>(indx[i]) + idStart << "\n";
           }
         }
       }
@@ -382,8 +478,6 @@ void vtkOBJExporter::WriteAnActor(vtkActor *anActor, FILE *fpObj, FILE *fpMtl,
 
   idStart = idNext;
 }
-
-
 
 void vtkOBJExporter::PrintSelf(ostream& os, vtkIndent indent)
 {

IO/Export/vtkOBJExporter.h
@@ -30,8 +30,12 @@
 
 #include "vtkIOExportModule.h" // For export macro
 #include "vtkExporter.h"
+#include <fstream> // For ofstream
+#include <map> // For map
+#include <vector> // For string
 
 class vtkActor;
+class vtkTexture;
 
 class VTKIOEXPORT_EXPORT vtkOBJExporter : public vtkExporter
 {
@@ -70,10 +74,16 @@ protected:
   ~vtkOBJExporter() override;
 
   void WriteData() override;
-  void WriteAnActor(vtkActor *anActor, FILE *fpObj, FILE *fpMat, int &id);
+  void WriteAnActor(vtkActor *anActor,
+                    std::ofstream &fpObj,
+                    std::ofstream &fpMat,
+                    std::string &modelName,
+                    int &id);
   char *FilePrefix;
   char *OBJFileComment;
   char *MTLFileComment;
+  bool FlipTexture;
+  std::map<std::string, vtkTexture *> TextureFileMap;
 private:
   vtkOBJExporter(const vtkOBJExporter&) = delete;
   void operator=(const vtkOBJExporter&) = delete;

IO/Import/Testing/Cxx/CMakeLists.txt
@@ -1,6 +1,8 @@
 # This is an alternative  way to add a named test that uses a common executable
 # Use this if it does not require MPI
 
+set(TEMP ${VTK_BINARY_DIR}/Testing/Temporary)
+
 vtk_add_test_cxx(vtkIOImportCxxTests tests
   OBJImporter-TwoTrianglesWithNegativeIndices,TestOBJImporter.cxx
     DATA{../Data/Input/TwoTrianglesWithNegativeIndices.obj}
@@ -72,6 +74,46 @@ vtk_add_test_cxx(vtkIOImportCxxTests tests
 
 vtk_add_test_cxx(vtkIOImportCxxTests tests
   TestGLTFImporterKHRLightsPunctual,TestGLTFImporter.cxx DATA{../Data/glTF/Lights/lights.gltf}
+)
+
+vtk_add_test_cxx(vtkIOImportCxxTests tests
+  NO_VALID
+  TestImportGLFTExportOBJ,TestImportExportOBJ.cxx
+  DATA{../Data/glTF/Avocado/Avocado.glb}
+  ${TEMP}/Avocado
+)
+
+vtk_add_test_cxx(vtkIOImportCxxTests tests
+  NO_VALID
+  TestImport3DSExportOBJ,TestImportExportOBJ.cxx
+  DATA{../../../../Testing/Data/iflamigm.3ds}
+  ${TEMP}/iflamigm
+)
+
+vtk_add_test_cxx(vtkIOImportCxxTests tests
+  TestImport3DS,TestImportOBJ.cxx
+  ${TEMP}/iflamigm.obj
+  ${TEMP}/iflamigm.mtl
+  ${TEMP}
+)
+
+set_tests_properties(VTK::IOImportCxx-TestImport3DS
+      PROPERTIES
+      DEPENDS
+      "VTK::IOImportCxx-TestImport3DSExportOBJ"
+)
+
+vtk_add_test_cxx(vtkIOImportCxxTests tests
+  TestImportGLFT,TestImportOBJ.cxx
+  ${TEMP}/Avocado.obj
+  ${TEMP}/Avocado.mtl
+  ${TEMP}
+)
+
+set_tests_properties(VTK::IOImportCxx-TestImportGLFT
+      PROPERTIES
+      DEPENDS
+      "VTK::IOImportCxx-TestImportGLFTExportOBJ"
 )
 
 vtk_test_cxx_executable(vtkIOImportCxxTests tests

IO/Import/Testing/Cxx/TestImportExportOBJ.cxx
@@ -1,0 +1,93 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    TestImportExportOBJ.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include <vtkSmartPointer.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkTestUtilities.h>
+
+// Importers
+#include <vtk3DSImporter.h>
+#include <vtkGLTFImporter.h>
+
+// Exporters
+#include <vtkOBJExporter.h>
+
+#include <algorithm> // For transform()
+#include <string> // For find_last_of()
+#include <cctype> // For to_lower
+#include <sstream> // For stringstream
+
+int TestImportExportOBJ(int argc, char *argv[])
+{
+  char *tname =
+    vtkTestUtilities::GetArgOrEnvOrDefault("-T", argc, argv, "VTK_TEMP_DIR", "Testing/Temporary");
+  std::string tmpDir(tname);
+  delete [] tname;
+  std::string filename = tmpDir + "/TestOBJPolyDataWriter_write.obj";
+
+  auto renderWindow =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  auto renderer =
+    vtkSmartPointer<vtkRenderer>::New();
+
+  std::string fileName = argv[1];
+  std::string extension = "";
+  // Make the extension lowercase
+  std::transform(extension.begin(), extension.end(), extension.begin(),
+                 ::tolower);
+  if (fileName.find_last_of(".") != std::string::npos)
+  {
+    extension = fileName.substr(fileName.find_last_of(".") + 1);
+  }
+  if (extension == "3ds")
+  {
+    auto importer =
+      vtkSmartPointer<vtk3DSImporter>::New();
+    importer->SetFileName(argv[1]);
+    importer->SetRenderWindow(renderWindow);
+    renderWindow = importer->GetRenderWindow();
+    renderer = importer->GetRenderer();
+    importer->Read();
+  }
+  else if (extension == "gltf" || extension == "glb")
+  {
+    auto importer =
+      vtkSmartPointer<vtkGLTFImporter>::New();
+    importer->SetFileName(argv[1]);
+    importer->SetRenderWindow(renderWindow);
+    renderWindow = importer->GetRenderWindow();
+    renderer = importer->GetRenderer();
+    importer->Read();
+  }
+  else
+  {
+    std::cout << "Error: Extension " << extension << " is not supported" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::stringstream comment;
+  auto exporter =
+    vtkSmartPointer<vtkOBJExporter>::New();
+  comment << "Converted by ImportExport from " << fileName;
+  exporter->SetFilePrefix(argv[2]);
+  exporter->SetOBJFileComment(comment.str().c_str());
+  exporter->SetMTLFileComment(comment.str().c_str());
+  exporter->SetActiveRenderer(renderer);
+  exporter->SetRenderWindow(renderWindow);
+  exporter->Write();
+
+  return EXIT_SUCCESS;
+}

IO/Import/Testing/Cxx/TestImportOBJ.cxx
@@ -1,0 +1,71 @@
+#include <vtkNamedColors.h>
+#include <vtkOBJImporter.h>
+#include <vtkPolyData.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkSmartPointer.h>
+#include <vtkTexture.h>
+
+int TestImportOBJ(int argc, char* argv[])
+{
+  if (argc < 4)
+  {
+    std::cout << "Usage: " << argv[0] << " objfile mtlfile texturepath"
+              << std::endl;
+    return EXIT_FAILURE;
+  }
+  auto importer = vtkSmartPointer<vtkOBJImporter>::New();
+  importer->SetFileName(argv[1]);
+  importer->SetFileNameMTL(argv[2]);
+  importer->SetTexturePath(argv[3]);
+
+  auto colors = vtkSmartPointer<vtkNamedColors>::New();
+
+  auto renderer = vtkSmartPointer<vtkRenderer>::New();
+  auto renWin = vtkSmartPointer<vtkRenderWindow>::New();
+  auto iren = vtkSmartPointer<vtkRenderWindowInteractor>::New();
+
+  renderer->SetBackground2(colors->GetColor3d("Silver").GetData());
+  renderer->SetBackground(colors->GetColor3d("Gold").GetData());
+  renderer->GradientBackgroundOn();
+  renWin->AddRenderer(renderer);
+  renderer->UseHiddenLineRemovalOn();
+  renWin->AddRenderer(renderer);
+  renWin->SetSize(640, 480);
+
+  iren->SetRenderWindow(renWin);
+  importer->SetRenderWindow(renWin);
+  importer->Update();
+
+  auto actors = renderer->GetActors();
+  actors->InitTraversal();
+  std::cout << "There are " << actors->GetNumberOfItems() << " actors"
+            << std::endl;
+
+  for (vtkIdType a = 0; a < actors->GetNumberOfItems(); ++a)
+  {
+    std::cout << importer->GetOutputDescription(a) << std::endl;
+
+    vtkActor* actor = actors->GetNextActor();
+
+    // OBJImporter turns texture interpolation off
+    if (actor->GetTexture())
+    {
+      std::cout << "Has texture\n";
+      actor->GetTexture()->InterpolateOn();
+    }
+
+    vtkPolyData* pd =
+        dynamic_cast<vtkPolyData*>(actor->GetMapper()->GetInput());
+
+    vtkPolyDataMapper* mapper =
+        dynamic_cast<vtkPolyDataMapper*>(actor->GetMapper());
+    mapper->SetInputData(pd);
+  }
+  renWin->Render();
+  iren->Start();
+
+  return EXIT_SUCCESS;
+}

IO/Import/Testing/Data/Baseline/TestImport3DS.png.sha512
@@ -1,0 +1,1 @@
+e0f4610e66a163122e2a59c94e9364b2a4558a705f723c33e402747d86f8ef4f55327971358387b069866c2b4c4619e8ff0dc8c43964ff94c42d04d1250b724d

IO/Import/Testing/Data/Baseline/TestImportGLFT.png.sha512
@@ -1,0 +1,1 @@
+dd9fb6c671f7417b79aa99033d1e00cc5d8c4023a933e93bf1c5dc9ebc41bb1d4af701f01590b84a1ec510232a6f5504c69b18d7353e27a0aca93dbd3a59d549

IO/Import/vtk.module
@@ -20,6 +20,8 @@ PRIVATE_DEPENDS
   VTK::IOImage
   VTK::jsoncpp
 TEST_DEPENDS
+  VTK::CommonColor
+  VTK::IOExport
   VTK::IOImage
   VTK::IOXML
   VTK::ImagingCore

commit a703a78683ab333da68512b9588bf162576d7282

Fix vtkOBJExporter file corruption when writing triangle strips
Triangle strips were incorrectly exported to OBJ format (generating unreadable OBJ file)
if point normals were present but texture coordinates were not present.
It was a regression (copy-paste error) introduced in revision 7db5c1954195cb4c999cbc09bcd6f14be022d336 (2019-08-03).

Face information starts with `f ` string and contain vertex_index/texture_index/normal_index values
(see https://en.wikipedia.org/wiki/Wavefront_.obj_file#Face_elements).
However, in the output the `f ` prefix and the `i1` point's vertex and normal indices were missing.

IO/Export/vtkOBJExporter.cxx
@@ -422,6 +422,8 @@ void vtkOBJExporter::WriteAnActor(
           else
           {
             // treating vtkIdType as int
+            fpObj << "f " << static_cast<int>(indx[i1]) + idStart << "//"
+                  << static_cast<int>(indx[i1]) + idStart << " ";
             fpObj << static_cast<int>(indx[i2]) + idStart << "//"
                   << static_cast<int>(indx[i2]) + idStart << " ";
             fpObj << static_cast<int>(indx[i]) + idStart << "//"