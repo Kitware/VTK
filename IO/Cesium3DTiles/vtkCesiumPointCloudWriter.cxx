445f81fdcc7a7bf65f12daaca00b154a3b9ffb6f
Dan Lipsa <dan.lipsa@kitware.com>
2022-03-28 13:59:33 -0400

    Fix comment

--- IO/Cesium3DTiles/vtkCesiumPointCloudWriter.cxx
+++ IO/Cesium3DTiles/vtkCesiumPointCloudWriter.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    vtkGLTFWriter.cxx
+  Module:    vtkCesiumPointCloudWriter.cxx
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
--- IO/Cesium3DTiles/vtkCesiumPointCloudWriter.h
+++ IO/Cesium3DTiles/vtkCesiumPointCloudWriter.h
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Visualization Toolkit
-  Module:    vtkGLTFWriter.h
+  Module:    vtkCesiumPointCloudWriter.h
 
   Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
   All rights reserved.
