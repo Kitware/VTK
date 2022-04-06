e6ce16d09273f467129baec806a5018cf324f0b6
Ben Boeckel <ben.boeckel@kitware.com>
2022-04-06 09:49:35 -0400

    zlib: update to 1.2.12

--- ThirdParty/zlib/update.sh
+++ ThirdParty/zlib/update.sh
@@ -8,7 +8,7 @@ readonly name="zlib"
 readonly ownership="Zlib Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/zlib.git"
-readonly tag="for/vtk-20181015-1.2.11"
+readonly tag="for/vtk-20220406-1.2.12"
 readonly paths="
 CMakeLists.txt
 
