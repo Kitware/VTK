cfc7995a792dd6074530d2f166d40681f6d812b2
Patrick Avery <patrick.avery@kitware.com>
2022-04-19 12:43:53 -0500

    Bump looking glass remote module commit
    The new commits include some refactoring and support for
    device-specific quilt settings.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- Remote/RenderingLookingGlass.remote.cmake
+++ Remote/RenderingLookingGlass.remote.cmake
@@ -5,5 +5,5 @@
 vtk_fetch_module(RenderingLookingGlass
         "Looking Glass Support for VTK"
         GIT_REPOSITORY https://github.com/Kitware/LookingGlassVTKModule
-        GIT_TAG 9fc15a989adad2f6eeda2328286f78b647f080d2
+        GIT_TAG 2954dd021466462ac685792faa6ecdf1844ca977
         )
