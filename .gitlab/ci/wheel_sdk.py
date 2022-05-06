97c97d44578bc8a10f50f9b04adb2a40f8d51923
Chris Harris <chris.harris@kitware.com>
2022-05-06 11:12:54 -0400

    Update wheel SDK tarball name
    Add 'wheel-sdk' to front rather than the end.

--- .gitlab/ci/wheel_sdk.py
+++ .gitlab/ci/wheel_sdk.py
@@ -5,7 +5,7 @@ dist_path = Path('dist')
 install_path = Path('install')
 
 wheel_file = next(dist_path.glob('*.whl'))
-sdk_name = f'{wheel_file.stem}-sdk'
+sdk_name = wheel_file.stem.replace('vtk-', 'vtk-wheel-sdk-')
 tarball_name = f'{sdk_name}.tar.xz'
 
 install_path.rename(sdk_name)
