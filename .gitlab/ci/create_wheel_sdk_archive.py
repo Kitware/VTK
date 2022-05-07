6b26668d2f8f678f32a3adeb3fede9b1b096c779
Patrick Avery <patrick.avery@kitware.com>
2022-05-07 06:07:34 -0500

    Use xz format
    Although the extension was "xz", it was actually performing "gz"
    compression.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- .gitlab/ci/create_wheel_sdk_archive.py
+++ .gitlab/ci/create_wheel_sdk_archive.py
@@ -10,6 +10,6 @@ tarball_name = f'{sdk_name}.tar.xz'
 
 install_path.rename(sdk_name)
 try:
-    subprocess.check_call(['tar', 'czf', tarball_name, sdk_name])
+    subprocess.check_call(['tar', 'cJf', tarball_name, sdk_name])
 finally:
     Path(sdk_name).rename(install_path)
