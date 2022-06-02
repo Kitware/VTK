Kitware/VTK

commit b576a06c601d40b0e2cec821606085511e32fa1a

Fix vtkOpenXRManager singleton destructor not being called
Instantiate vtkOpenXRManager on the stack to have its destructor called
automatically when the program ends.

Rendering/OpenXR/vtkOpenXRManager.cxx
@@ -39,82 +39,65 @@ struct vtkXVisualInfo : public XVisualInfo
 #define VTK_CHECK_NULL_XRHANDLE(handle, msg)                                                       \
   if (handle == XR_NULL_HANDLE)                                                                    \
   {                                                                                                \
-    vtkErrorMacro(<< msg << " is a null handle.");                                                 \
+    vtkErrorWithObjectMacro(nullptr, << msg << " is a null handle.");                              \
     return false;                                                                                  \
   }
-
-vtkStandardNewMacro(vtkOpenXRManager);
-
-vtkOpenXRManager* vtkOpenXRManager::UniqueInstance = nullptr;
-
-//----------------------------------------------------------------------------
-vtkOpenXRManager* vtkOpenXRManager::GetInstance()
-{
-  if (!vtkOpenXRManager::UniqueInstance)
-  {
-    vtkOpenXRManager::UniqueInstance = new vtkOpenXRManager();
-  }
-
-  return vtkOpenXRManager::UniqueInstance;
-}
 
 //------------------------------------------------------------------------------
 bool vtkOpenXRManager::Initialize(vtkOpenGLRenderWindow* helperWindow)
 {
-  this->DebugOn();
-
   if (!this->CreateInstance())
   {
-    vtkWarningMacro("Initialize failed to CreateInstance");
+    vtkWarningWithObjectMacro(nullptr, "Initialize failed to CreateInstance");
     return false;
   }
 
   // Create the SubactionPaths (left / right hand and head)
   if (!this->CreateSubactionPaths())
   {
-    vtkWarningMacro("Initialize failed to CreateSubactionPaths");
+    vtkWarningWithObjectMacro(nullptr, "Initialize failed to CreateSubactionPaths");
     return false;
   }
 
   if (!this->CreateSystem())
   {
-    vtkWarningMacro("Initialize failed to CreateSystem");
+    vtkWarningWithObjectMacro(nullptr, "Initialize failed to CreateSystem");
     return false;
   }
 
   if (!this->CheckGraphicsRequirements())
   {
-    vtkWarningMacro("Initialize failed in CheckGraphicsRequirements");
+    vtkWarningWithObjectMacro(nullptr, "Initialize failed in CheckGraphicsRequirements");
     return false;
   }
 
   if (!this->CreateGraphicsBinding(helperWindow))
   {
-    vtkWarningMacro("Initialize failed to CreateGraphicsBinding");
+    vtkWarningWithObjectMacro(nullptr, "Initialize failed to CreateGraphicsBinding");
     return false;
   }
 
   if (!this->CreateSession())
   {
-    vtkWarningMacro("Initialize failed to CreateSession");
+    vtkWarningWithObjectMacro(nullptr, "Initialize failed to CreateSession");
     return false;
   }
 
   if (!this->CreateReferenceSpace())
   {
-    vtkWarningMacro("Initialize failed to CreateReferenceSpace");
+    vtkWarningWithObjectMacro(nullptr, "Initialize failed to CreateReferenceSpace");
     return false;
   }
 
   if (!this->CreateSwapchains())
   {
-    vtkWarningMacro("Initialize failed to CreateSwapChains");
+    vtkWarningWithObjectMacro(nullptr, "Initialize failed to CreateSwapChains");
     return false;
   }
 
   if (!this->LoadControllerModels())
   {
-    vtkWarningMacro("Initialize failed to LoadController Models");
+    vtkWarningWithObjectMacro(nullptr, "Initialize failed to LoadController Models");
     return false;
   }
 
@@ -232,8 +215,9 @@ bool vtkOpenXRManager::WaitAndBeginFrame()
 
     if (viewCountOutput != viewCount)
     {
-      vtkWarningMacro(<< "ViewCountOutput (" << viewCountOutput << ") is different than ViewCount ("
-                      << viewCount << ") !");
+      vtkWarningWithObjectMacro(nullptr, << "ViewCountOutput (" << viewCountOutput
+                                         << ") is different than ViewCount (" << viewCount
+                                         << ") !");
     }
   }
 
@@ -293,14 +277,16 @@ bool vtkOpenXRManager::PrepareRendering(
   {
     if (colorSwapchain.Width != depthSwapchain.Width)
     {
-      vtkErrorMacro(<< "Color swapchain width (" << colorSwapchain.Width
-                    << ") differs from depth swapchain width (" << depthSwapchain.Width << ").");
+      vtkErrorWithObjectMacro(nullptr, << "Color swapchain width (" << colorSwapchain.Width
+                                       << ") differs from depth swapchain width ("
+                                       << depthSwapchain.Width << ").");
       return false;
     }
     if (colorSwapchain.Height != depthSwapchain.Height)
     {
-      vtkErrorMacro(<< "Color swapchain height (" << colorSwapchain.Height
-                    << ") differs from depth swapchain height (" << depthSwapchain.Height << ").");
+      vtkErrorWithObjectMacro(nullptr, << "Color swapchain height (" << colorSwapchain.Height
+                                       << ") differs from depth swapchain height ("
+                                       << depthSwapchain.Height << ").");
       return false;
     }
   }
@@ -429,7 +415,7 @@ bool vtkOpenXRManager::XrCheckError(const XrResult& result, const std::string& m
   {
     char xRResultString[XR_MAX_RESULT_STRING_SIZE];
     xrResultToString(this->Instance, result, xRResultString);
-    vtkErrorMacro(<< message << " [" << xRResultString << "].");
+    vtkErrorWithObjectMacro(nullptr, << message << " [" << xRResultString << "].");
     return false;
   }
   return true;
@@ -442,7 +428,7 @@ bool vtkOpenXRManager::XrCheckWarn(const XrResult& result, const std::string& me
   {
     char xRResultString[XR_MAX_RESULT_STRING_SIZE];
     xrResultToString(this->Instance, result, xRResultString);
-    vtkWarningMacro(<< message << " [" << xRResultString << "].");
+    vtkWarningWithObjectMacro(nullptr, << message << " [" << xRResultString << "].");
     return false;
   }
   return true;
@@ -492,10 +478,6 @@ void vtkOpenXRManager::PrintSystemProperties(XrSystemProperties* systemPropertie
     {
       XrSystemHandTrackingPropertiesEXT* ht =
         static_cast<XrSystemHandTrackingPropertiesEXT*>(systemProperties->next);
-      if (!this->Debug)
-      {
-        (void)ht;
-      }
       std::cout << "\tHand Tracking       : " << ht->supportsHandTracking << std::endl;
     }
     next = next->next;
@@ -537,10 +519,6 @@ void vtkOpenXRManager::PrintViewConfigViewInfo(
   for (size_t i = 0; i < viewconfigViews.size(); ++i)
   {
     const auto& vcfgv = viewconfigViews[i];
-    if (!this->Debug)
-    {
-      (void)vcfgv;
-    }
     std::cout << "View Configuration View " << i << std::endl;
     std::cout << "\tResolution       : Recommended: " << vcfgv.recommendedImageRectWidth << "x"
               << vcfgv.recommendedImageRectHeight << ", Max: " << vcfgv.maxImageRectWidth << "x"
@@ -661,7 +639,7 @@ bool vtkOpenXRManager::CreateInstance()
   // For instance, only OpenGL extension is supported so is mandatory
   if (!this->HasOpenGLExtension)
   {
-    vtkErrorMacro(<< "OpenGL extension is not supported. Aborting.");
+    vtkErrorWithObjectMacro(nullptr, << "OpenGL extension is not supported. Aborting.");
     return false;
   }
 
@@ -778,7 +756,8 @@ bool vtkOpenXRManager::CreateSystem()
       "Failed to get environment blend modes count");
     if (count == 0)
     {
-      vtkErrorMacro("A system must support at least one environment blend mode.");
+      vtkErrorWithObjectMacro(
+        nullptr, "A system must support at least one environment blend mode.");
     }
 
     std::vector<XrEnvironmentBlendMode> environmentBlendModes(count);
@@ -870,7 +849,7 @@ bool vtkOpenXRManager::CreateGraphicsBinding(vtkOpenGLRenderWindow* helperWindow
   graphicsBindingGLWin32->hGLRC = wglGetCurrentContext();
 
 #else
-  vtkErrorMacro(<< "Only X11 and Win32 are supported at the moment.");
+  vtkErrorWithObjectMacro(nullptr, << "Only X11 and Win32 are supported at the moment.");
   return false;
 #endif
 
@@ -976,8 +955,8 @@ std::tuple<int64_t, int64_t> vtkOpenXRManager::SelectSwapchainPixelFormats()
         std::begin(applicationSupportedFormats), std::end(applicationSupportedFormats));
     if (found == std::end(runtimePreferredFormats))
     {
-      vtkErrorMacro(<< "No runtime swapchain " << formatName
-                    << " format in the list is supported.");
+      vtkErrorWithObjectMacro(
+        nullptr, << "No runtime swapchain " << formatName << " format in the list is supported.");
       return (int64_t)-1;
     }
     return *found;
@@ -1102,8 +1081,8 @@ bool vtkOpenXRManager::CreateConfigViews()
     "Failed to get view configuration view count!");
   if (viewCount != this->StereoViewCount)
   {
-    vtkWarningMacro(<< "StereoViewCount (" << this->StereoViewCount
-                    << ") is different than viewCount (" << viewCount << ")");
+    vtkWarningWithObjectMacro(nullptr, << "StereoViewCount (" << this->StereoViewCount
+                                       << ") is different than viewCount (" << viewCount << ")");
   }
 
   this->RenderResources->ConfigViews.resize(viewCount, { XR_TYPE_VIEW_CONFIGURATION_VIEW });
@@ -1148,13 +1127,13 @@ bool vtkOpenXRManager::SelectActiveActionSet(unsigned int index)
 {
   if (this->ActionSets.size() == 0)
   {
-    vtkErrorMacro(<< "An action set must be created prior to select one.");
+    vtkErrorWithObjectMacro(nullptr, << "An action set must be created prior to select one.");
     return false;
   }
   if (index >= this->ActionSets.size())
   {
-    vtkWarningMacro(<< "The selected action set at index : " << index
-                    << " does not exist. Pick the first one");
+    vtkWarningWithObjectMacro(nullptr,
+      << "The selected action set at index : " << index << " does not exist. Pick the first one");
     index = 0;
   }
 
@@ -1241,9 +1220,9 @@ bool vtkOpenXRManager::CreateOneAction(
       if (!this->CreateOneActionSpace(actionT.Action, this->SubactionPaths[hand],
             vtkOpenXRUtilities::GetIdentityPose(), actionT.PoseSpaces[hand]))
       {
-        vtkErrorMacro(<< "Failed to create pose action space for "
-                      << (hand == vtkOpenXRManager::ControllerIndex::Left ? "left" : "right")
-                      << " hand");
+        vtkErrorWithObjectMacro(nullptr,
+          << "Failed to create pose action space for "
+          << (hand == vtkOpenXRManager::ControllerIndex::Left ? "left" : "right") << " hand");
         return false;
       };
     }
@@ -1407,9 +1386,10 @@ bool vtkOpenXRManager::ApplyVibration(const Action_t& actionT, const int hand,
 
   if (actionT.ActionType != XR_ACTION_TYPE_VIBRATION_OUTPUT)
   {
-    vtkErrorMacro(<< "vtkOpenXRManager::ApplyVibration must be called for an action of type "
-                     "XR_ACTION_TYPE_VIBRATION_OUTPUT, not a "
-                  << vtkOpenXRUtilities::GetActionTypeAsString(actionT.ActionType));
+    vtkErrorWithObjectMacro(
+      nullptr, << "vtkOpenXRManager::ApplyVibration must be called for an action of type "
+                  "XR_ACTION_TYPE_VIBRATION_OUTPUT, not a "
+               << vtkOpenXRUtilities::GetActionTypeAsString(actionT.ActionType));
     return false;
   }
 

Rendering/OpenXR/vtkOpenXRCamera.cxx
@@ -52,7 +52,7 @@ void vtkOpenXRCamera::UpdateWorldToEyeMatrices(vtkRenderer* ren)
   this->WorldToPhysicalMatrix->Invert();
   // at this point it is now correctly worldToPhysical
 
-  const XrPosef* xrPose = vtkOpenXRManager::GetInstance()->GetViewPose(LEFT_EYE);
+  const XrPosef* xrPose = vtkOpenXRManager::GetInstance().GetViewPose(LEFT_EYE);
   if (xrPose == nullptr)
   {
     vtkErrorMacro(<< "No pose for left eye, cannot update view transform");
@@ -64,7 +64,7 @@ void vtkOpenXRCamera::UpdateWorldToEyeMatrices(vtkRenderer* ren)
   vtkMatrix4x4::Multiply4x4(
     this->PhysicalToLeftEyeMatrix, this->WorldToPhysicalMatrix, this->WorldToLeftEyeMatrix);
 
-  xrPose = vtkOpenXRManager::GetInstance()->GetViewPose(RIGHT_EYE);
+  xrPose = vtkOpenXRManager::GetInstance().GetViewPose(RIGHT_EYE);
   if (xrPose == nullptr)
   {
     vtkErrorMacro(<< "No pose for right eye, cannot update view transform");
@@ -86,7 +86,7 @@ void vtkOpenXRCamera::UpdateEyeToProjectionMatrices(vtkRenderer* ren)
   double znear = this->ClippingRange[0] / scale;
   double zfar = this->ClippingRange[1] / scale;
 
-  XrFovf const* xrFov = vtkOpenXRManager::GetInstance()->GetProjectionFov(LEFT_EYE);
+  XrFovf const* xrFov = vtkOpenXRManager::GetInstance().GetProjectionFov(LEFT_EYE);
   if (xrFov == nullptr)
   {
     vtkErrorMacro(<< "No fov for left eye, cannot update projection matrix");
@@ -94,7 +94,7 @@ void vtkOpenXRCamera::UpdateEyeToProjectionMatrices(vtkRenderer* ren)
   }
   vtkOpenXRUtilities::CreateProjectionFov(this->LeftEyeToProjectionMatrix, *xrFov, znear, zfar);
 
-  xrFov = vtkOpenXRManager::GetInstance()->GetProjectionFov(RIGHT_EYE);
+  xrFov = vtkOpenXRManager::GetInstance().GetProjectionFov(RIGHT_EYE);
   if (xrFov == nullptr)
   {
     vtkErrorMacro(<< "No fov for right eye, cannot update projection matrix");

Rendering/OpenXR/vtkOpenXRManager.h
@@ -17,16 +17,18 @@
  * @brief   Singleton class that holds a collection of utility functions
  *          and member variables to communicate with the OpenXR runtime
  *
- * vtkOpenXRManager
+ * vtkOpenXRManager is not a vtkObject, the singleton unique instance gets
+ * allocated on the stack the first time vtkOpenXRManager::GetInstance() is
+ * called.
  */
 
 #ifndef vtkOpenXRManager_h
 #define vtkOpenXRManager_h
 
-#include "vtkObject.h"
 #include "vtkRenderingOpenXRModule.h" // needed for exports
 
 #include "vtkOpenXR.h"
+#include "vtkSystemIncludes.h"
 
 #include <array>
 #include <memory>
@@ -35,17 +37,18 @@
 
 class vtkOpenGLRenderWindow;
 
-class VTKRENDERINGOPENXR_EXPORT vtkOpenXRManager : public vtkObject
+class VTKRENDERINGOPENXR_EXPORT vtkOpenXRManager
 {
 public:
-  static vtkOpenXRManager* New();
-  vtkTypeMacro(vtkOpenXRManager, vtkObject);
-
   //@{
   /**
-   * Return the singleton instance with no reference counting.
+   * Return the singleton instance.
    */
-  static vtkOpenXRManager* GetInstance();
+  static vtkOpenXRManager& GetInstance()
+  {
+    static vtkOpenXRManager UniqueInstance;
+    return UniqueInstance;
+  }
   //@}
 
   //@{
@@ -349,7 +352,7 @@ public:
 
 protected:
   vtkOpenXRManager() = default;
-  ~vtkOpenXRManager() override = default;
+  ~vtkOpenXRManager() = default;
 
   //@{
   /**
@@ -587,8 +590,6 @@ protected:
 private:
   vtkOpenXRManager(const vtkOpenXRManager&) = delete;
   void operator=(const vtkOpenXRManager&) = delete;
-
-  static vtkOpenXRManager* UniqueInstance;
 };
 
 #endif

Rendering/OpenXR/vtkOpenXRRenderWindow.cxx
@@ -81,12 +81,9 @@ vtkRenderWindowInteractor* vtkOpenXRRenderWindow::MakeRenderWindowInteractor()
 //------------------------------------------------------------------------------
 bool vtkOpenXRRenderWindow::GetSizeFromAPI()
 {
-  vtkOpenXRManager* xrManager = vtkOpenXRManager::GetInstance();
-  if (!xrManager)
-  {
-    return false;
-  }
-  std::tie(this->Size[0], this->Size[1]) = xrManager->GetRecommandedImageRectSize();
+  vtkOpenXRManager& xrManager = vtkOpenXRManager::GetInstance();
+
+  std::tie(this->Size[0], this->Size[1]) = xrManager.GetRecommandedImageRectSize();
 
   return true;
 }
@@ -121,8 +118,8 @@ void vtkOpenXRRenderWindow::Initialize()
   this->MakeCurrent();
   this->OpenGLInit();
 
-  vtkOpenXRManager* xrManager = vtkOpenXRManager::GetInstance();
-  if (!xrManager->Initialize(this->HelperWindow))
+  vtkOpenXRManager& xrManager = vtkOpenXRManager::GetInstance();
+  if (!xrManager.Initialize(this->HelperWindow))
   {
     // Set to false because the above init of the HelperWindow sets it to true
     this->Initialized = false;
@@ -133,11 +130,11 @@ void vtkOpenXRRenderWindow::Initialize()
   // Create one framebuffer per view
   this->CreateFramebuffers();
 
-  std::tie(this->Size[0], this->Size[1]) = xrManager->GetRecommandedImageRectSize();
+  std::tie(this->Size[0], this->Size[1]) = xrManager.GetRecommandedImageRectSize();
 
   vtkDebugMacro(<< "Size : " << this->Size[0] << ", " << this->Size[1]);
 
-  std::string strWindowTitle = "VTK - " + xrManager->GetOpenXRPropertiesAsString();
+  std::string strWindowTitle = "VTK - " + xrManager.GetOpenXRPropertiesAsString();
   this->SetWindowName(strWindowTitle.c_str());
 
   this->Initialized = true;
@@ -150,7 +147,7 @@ void vtkOpenXRRenderWindow::Finalize()
 {
   this->ReleaseGraphicsResources(this);
 
-  vtkOpenXRManager::GetInstance()->Finalize();
+  vtkOpenXRManager::GetInstance().Finalize();
 
   if (this->HelperWindow && this->HelperWindow->GetGenericContext())
   {
@@ -161,9 +158,9 @@ void vtkOpenXRRenderWindow::Finalize()
 //------------------------------------------------------------------------------
 void vtkOpenXRRenderWindow::Render()
 {
-  vtkOpenXRManager* xrManager = vtkOpenXRManager::GetInstance();
+  vtkOpenXRManager& xrManager = vtkOpenXRManager::GetInstance();
 
-  if (!xrManager->WaitAndBeginFrame())
+  if (!xrManager.WaitAndBeginFrame())
   {
     return;
   }
@@ -173,7 +170,7 @@ void vtkOpenXRRenderWindow::Render()
     this->UpdateHMDMatrixPose();
   }
 
-  if (xrManager->GetShouldRenderCurrentFrame())
+  if (xrManager.GetShouldRenderCurrentFrame())
   {
     // Start rendering
     this->Superclass::Render();
@@ -183,7 +180,7 @@ void vtkOpenXRRenderWindow::Render()
     vtkWarningMacro(<< "Not rendered");
   }
 
-  xrManager->EndFrame();
+  xrManager.EndFrame();
 }
 
 //------------------------------------------------------------------------------
@@ -196,7 +193,7 @@ void vtkOpenXRRenderWindow::UpdateHMDMatrixPose()
   // use left eye as stand in for HMD right now
   // todo add event for head pose
 
-  const XrPosef* xrPose = vtkOpenXRManager::GetInstance()->GetViewPose(LEFT_EYE);
+  const XrPosef* xrPose = vtkOpenXRManager::GetInstance().GetViewPose(LEFT_EYE);
   if (xrPose == nullptr)
   {
     vtkErrorMacro(<< "No pose for left eye");
@@ -255,10 +252,10 @@ void vtkOpenXRRenderWindow::StereoRenderComplete()
 //------------------------------------------------------------------------------
 void vtkOpenXRRenderWindow::RenderOneEye(const uint32_t eye)
 {
-  vtkOpenXRManager* xrManager = vtkOpenXRManager::GetInstance();
+  vtkOpenXRManager& xrManager = vtkOpenXRManager::GetInstance();
 
   FramebufferDesc& eyeFramebufferDesc = this->FramebufferDescs[eye];
-  if (!xrManager->PrepareRendering(
+  if (!xrManager.PrepareRendering(
         eye, eyeFramebufferDesc.ResolveColorTextureId, eyeFramebufferDesc.ResolveDepthTextureId))
   {
     return;
@@ -275,7 +272,7 @@ void vtkOpenXRRenderWindow::RenderOneEye(const uint32_t eye)
   this->RenderFramebuffer(eyeFramebufferDesc);
 
   // Release this swapchain image
-  xrManager->ReleaseSwapchainImage(eye);
+  xrManager.ReleaseSwapchainImage(eye);
 }
 
 //------------------------------------------------------------------------------
@@ -322,8 +319,8 @@ bool vtkOpenXRRenderWindow::CreateFramebuffers(uint32_t vtkNotUsed(viewCount))
   // So we call glFrameBufferTexture2D at each frame with the texture provided by
   // the runtime
   // That's why we only generate framebuffers here
-  vtkOpenXRManager* xrManager = vtkOpenXRManager::GetInstance();
-  uint32_t viewCount = xrManager->GetViewCount();
+  vtkOpenXRManager& xrManager = vtkOpenXRManager::GetInstance();
+  uint32_t viewCount = xrManager.GetViewCount();
   this->FramebufferDescs.resize(viewCount);
   for (size_t i = 0; i < viewCount; ++i)
   {
@@ -342,7 +339,7 @@ bool vtkOpenXRRenderWindow::BindTextureToFramebuffer(FramebufferDesc& framebuffe
   glFramebufferTexture2D(
     GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, framebufferDesc.ResolveColorTextureId, 0);
 
-  if (vtkOpenXRManager::GetInstance()->IsDepthExtensionSupported())
+  if (vtkOpenXRManager::GetInstance().IsDepthExtensionSupported())
   {
     glFramebufferTexture2D(
       GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, framebufferDesc.ResolveDepthTextureId, 0);
@@ -373,7 +370,7 @@ void vtkOpenXRRenderWindow::RenderFramebuffer(FramebufferDesc& framebufferDesc)
   glBlitFramebuffer(0, 0, this->Size[0], this->Size[1], 0, 0, this->Size[0], this->Size[1],
     GL_COLOR_BUFFER_BIT, GL_LINEAR);
 
-  if (vtkOpenXRManager::GetInstance()->IsDepthExtensionSupported())
+  if (vtkOpenXRManager::GetInstance().IsDepthExtensionSupported())
   {
     glBlitFramebuffer(0, 0, this->Size[0], this->Size[1], 0, 0, this->Size[0], this->Size[1],
       GL_DEPTH_BUFFER_BIT, GL_NEAREST);

commit 7e6f478eb846db65ebe807cf070b625b9023f804

Fix use of vtkDebugMacro in vtkOpenXRManager that is not a vtkObject
Fix issue introduced by b576a06c [1].

[1]: https://gitlab.kitware.com/vtk/vtk/-/commit/b576a06c601d40b0e2cec821606085511e32fa1a

Rendering/OpenXR/vtkOpenXRManager.cxx
@@ -161,7 +161,7 @@ bool vtkOpenXRManager::BeginSession()
     return false;
   }
 
-  vtkDebugMacro(<< "Session started.");
+  vtkDebugWithObjectMacro(nullptr, "Session started.");
 
   this->SessionRunning = true;
 
@@ -708,8 +708,8 @@ bool vtkOpenXRManager::CreateSystem()
   this->XrCheckError(xrGetSystem(this->Instance, &system_get_info, &this->SystemId),
     "Failed to get system for HMD form factor.");
 
-  vtkDebugMacro(<< "Successfully got XrSystem with id " << this->SystemId
-                << " for HMD form factor.");
+  vtkDebugWithObjectMacro(
+    nullptr, "Successfully got XrSystem with id " << this->SystemId << " for HMD form factor.");
 
   // checking system properties is generally  optional, but we are interested in hand tracking
   // support
@@ -878,9 +878,9 @@ bool vtkOpenXRManager::CreateSession()
   }
 
 #ifdef XR_USE_GRAPHICS_API_OPENGL
-  vtkDebugMacro(<< "Successfully created a session with OpenGL!");
+  vtkDebugWithObjectMacro(nullptr, "Successfully created a session with OpenGL!");
 #elif XR_USE_GRAPHICS_API_D3D11
-  vtkDebugMacro(<< "Successfully created a session with DirectX!");
+  vtkDebugWithObjectMacro(nullptr, "Successfully created a session with DirectX!");
 #endif
 
   return true;
@@ -939,7 +939,8 @@ std::tuple<int64_t, int64_t> vtkOpenXRManager::SelectSwapchainPixelFormats()
   this->XrCheckError(xrEnumerateSwapchainFormats(this->Session, 0, &swapchainFormatsCount, nullptr),
     "Failed to get number of supported swapchain formats");
 
-  vtkDebugMacro(<< "Runtime supports " << swapchainFormatsCount << " swapchain formats");
+  vtkDebugWithObjectMacro(
+    nullptr, "Runtime supports " << swapchainFormatsCount << " swapchain formats");
 
   std::vector<int64_t> swapchainFormats(swapchainFormatsCount);
   this->XrCheckError(xrEnumerateSwapchainFormats(this->Session, swapchainFormatsCount,
@@ -971,7 +972,8 @@ std::tuple<int64_t, int64_t> vtkOpenXRManager::SelectSwapchainPixelFormats()
       selectPixelFormat(swapchainFormats, this->GetSupportedDepthFormats(), "depth");
     if (depthSwapchainFormat == -1)
     {
-      vtkDebugMacro(<< "Disabling depth extension as no depth format are supported");
+      vtkDebugWithObjectMacro(
+        nullptr, "Disabling depth extension as no depth format are supported");
       this->OptionalExtensions.DepthExtensionSupported = false;
     }
   }
@@ -1104,7 +1106,8 @@ bool vtkOpenXRManager::CreateConfigViews()
 bool vtkOpenXRManager::CreateActionSet(
   const std::string& actionSetName, const std::string& localizedActionSetName)
 {
-  vtkDebugMacro(<< "Create action set " << actionSetName << ": " << localizedActionSetName);
+  vtkDebugWithObjectMacro(
+    nullptr, "Create action set " << actionSetName << ": " << localizedActionSetName);
 
   XrActionSetCreateInfo actionSetInfo{ XR_TYPE_ACTION_SET_CREATE_INFO };
 
@@ -1252,7 +1255,7 @@ bool vtkOpenXRManager::CreateOneActionSpace(const XrAction& action, const XrPath
 bool vtkOpenXRManager::SuggestActions(
   const std::string& profile, std::vector<XrActionSuggestedBinding>& actionSuggestedBindings)
 {
-  vtkDebugMacro(<< "SuggestActions for profile : " << profile);
+  vtkDebugWithObjectMacro(nullptr, "SuggestActions for profile : " << profile);
   VTK_CHECK_NULL_XRHANDLE(this->Instance, "vtkOpenXRManager::SuggestActions, Instance");
 
   XrPath interactionProfilePath;