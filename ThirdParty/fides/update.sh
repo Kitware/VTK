51665834d937f63039914752427feaf5010167ac
Caitlin Ross <caitlin.ross@kitware.com>
2022-06-06 16:42:15 -0500

    fides: update tag

--- ThirdParty/fides/update.sh
+++ ThirdParty/fides/update.sh
@@ -8,7 +8,7 @@ readonly name="fides"
 readonly ownership="Fides Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/fides.git"
-readonly tag="for/vtk-20220524-master-9fb7b15"
+readonly tag="for/vtk-20220607-master-ac77ac7"
 readonly paths="
 .gitattributes
 LICENSE.txt
