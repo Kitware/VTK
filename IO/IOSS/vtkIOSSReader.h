d51dffc614e8b6b52a56ca9f464897a56dad6c50
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2022-06-17 14:53:16 -0400

    Add set/get functions for IOSS database properties
    - Fixes failing test VTK::IOIOSSCxx-TestIOSSExodusWriterCrinkleClip

--- IO/IOSS/vtkIOSSReader.h
+++ IO/IOSS/vtkIOSSReader.h
@@ -224,6 +224,25 @@ public:
 
   ///@{
   /**
+   * Set whether the reader should treat numeric suffixes for a variable as vector components.
+   * If true, DENSITY_1, DENSITY_2, DENSITY_3, DENSITY_4 fields will be parsed as 4 vtkDataArrays
+   * each with 1 component. When false, those fields will be parsed as a 4-component vtkDataArray
+   * named DESNITY.
+   */
+  void SetIgnoreRealNFields(bool value);
+  bool GetIgnoreRealNFields();
+  ///@}
+
+  ///@{
+  /**
+   * Set the character used to separate suffix from the field.
+   */
+  void SetFieldSuffixSeparator(const char* value);
+  std::string GetFieldSuffixSeparator();
+  ///@}
+
+  ///@{
+  /**
    * When set to true, the reader can automatically locate and load additional
    * files that are part of the collection.
    */
