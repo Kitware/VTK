fd9371ba24ff0a0577dda961c9425b2a03947ebd
Ben Boeckel <ben.boeckel@kitware.com>
2022-06-27 16:08:56 -0400

    Filters/ParallelChemistry: use `override` where possible

--- Domains/ParallelChemistry/vtkPSimpleBondPerceiver.h
+++ Domains/ParallelChemistry/vtkPSimpleBondPerceiver.h
@@ -43,7 +43,7 @@ public:
 
 protected:
   vtkPSimpleBondPerceiver() = default;
-  ~vtkPSimpleBondPerceiver() = default;
+  ~vtkPSimpleBondPerceiver() override = default;
 
   /**
    * Create ghosts level in molecule.
