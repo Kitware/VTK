e4ace431f36623ae24ec69499de2cfa4010e3a06
Chris Harris <chris.harris@kitware.com>
2022-07-23 10:13:19 -0400

    Push RenderingLookingGlass forward

--- Remote/RenderingLookingGlass.remote.cmake
+++ Remote/RenderingLookingGlass.remote.cmake
@@ -5,5 +5,5 @@
 vtk_fetch_module(RenderingLookingGlass
         "Looking Glass Support for VTK"
         GIT_REPOSITORY https://github.com/Kitware/LookingGlassVTKModule
-        GIT_TAG 8dbf47ba22b8b03c2bd4bf4d8635272f54db8dda
+        GIT_TAG 55dad9093d03b2b6443f772d1c3aad6bfd1b6f0c
         )
