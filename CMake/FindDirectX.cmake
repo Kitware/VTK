e44a42d70dc8af07fc5342b169785407eb82d8d1
Lucas Gandel <lucas.gandel@kitware.com>
2022-08-23 11:56:50 +0200

    Add CMake Find module for DirectX libraries

---
+++ CMake/FindDirectX.cmake
@@ -0,0 +1,15 @@
+if (WIN32)
+  # Create imported target DirectX::d3d11
+  if (NOT TARGET DirectX::d3d11)
+    add_library(DirectX::d3d11 INTERFACE IMPORTED)
+    set_target_properties(DirectX::d3d11 PROPERTIES IMPORTED_LIBNAME "d3d11.lib")
+  endif()
+
+  # Create imported target DirectX::dxgi
+  if (NOT TARGET DirectX::dxgi)
+    add_library(DirectX::dxgi INTERFACE IMPORTED)
+    set_target_properties(DirectX::dxgi PROPERTIES IMPORTED_LIBNAME "dxgi.lib")
+  endif()
+
+  set(DirectX_FOUND TRUE)
+endif()
