e88efc8313407cbbff7c863b314329703066946d
Ben Boeckel <ben.boeckel@kitware.com>
2022-09-30 07:49:57 -0400

    ci: update wheel builds to 3.10.7

--- .gitlab/ci/download_wheel_python.cmake
+++ .gitlab/ci/download_wheel_python.cmake
@@ -8,22 +8,22 @@ set(python36_version "3.6.8") # Source-only releases after this.
 set(python37_version "3.7.9") # Source-only releases after this.
 set(python38_version "3.8.10") # Source-only releases after this.
 set(python39_version "3.9.13") # Source-only releases after this.
-set(python310_version "3.10.1")
+set(python310_version "3.10.7")
 
 # Hashes for various deployments.
 set(python36_windows_x86_64_hash "3f70798c4885d11842869c9fb2842c8f8bafed1ebfac04e3f4ae1af6498527ce")
 set(python37_windows_x86_64_hash "edd1b8c491635939f7b7e538650607db8307d6dfd3fef043ec5bc21ce4035700")
 set(python38_windows_x86_64_hash "d4e7e83de0db659697eea034dd2b1620ff26ac7062709b60d723307a68aa5d81")
 set(python39_windows_x86_64_hash "004683810c0e0b4ff10025392ac95e699e99d8c3566f415aa7fa35c6d4882f88")
-set(python310_windows_x86_64_hash "4c61e26989fdfac974309e77bc855c843b1f5b438a8a81b762095c0d838581de")
+set(python310_windows_x86_64_hash "51e5ebc5d3cc3572a5d138881e0582e021ef059762796c0414cdd172dd8662e2")
 
 set(python36_macos_x86_64_hash "bd3b68dfc9787c39312c8bd554853fe750abd999e100690c3ed81c29447b02d3")
 set(python37_macos_x86_64_hash "1d31a228ac921c13787f74e0b9e7a04ae5806e70c5226e23711840bf0f0c9e90")
 set(python38_macos_x86_64_hash "8c49fa50d34529e58769d3901e9e079554424d59bc1aa7dceb82c8c63f09cbc1")
 set(python39_macos_arm64_hash "e6b95bb926feff99e38bcd4986feb8897b36170a6c6c01b36da7d8e3daac5b6b")
 set(python39_macos_x86_64_hash "357fffe2efe80eef7136362db2e6616341c046dac5e26614478c7c0248c16709")
-set(python310_macos_arm64_hash "df329edb8ec81fc6eb1e5d03b53dbe318edbc94fb9b9682246b4074b24bc711e")
-set(python310_macos_x86_64_hash "514dc729b95458af7207d7fd9e8827f3182afbcff7edc6666cec73e2a2c52190")
+set(python310_macos_arm64_hash "fac43b4015166ab49dedb553e687b06f6212601902ac7cd833471393e88411b5")
+set(python310_macos_x86_64_hash "79c32332738fe09846939ae86d52d72d955d9d34637fc47b275bcb74943c150f")
 
 # Extracting information from the build configuration.
 if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "windows")
