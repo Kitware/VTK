2695958d19729a25e79ae5e9feadff0b5671a255
Philipp Weißenbacher <philipp.weissenbacher@gmx.at>
2022-11-08 10:31:43 +0100

    Fixed formatting

--- Filters/Core/vtkSmoothPolyDataFilter.cxx
+++ Filters/Core/vtkSmoothPolyDataFilter.cxx
@@ -138,6 +138,7 @@ vtkSmoothPolyDataFilter::vtkSmoothPolyDataFilter()
   this->SetNumberOfInputPorts(2);
 }
 
+//------------------------------------------------------------------------------
 vtkSmoothPolyDataFilter::~vtkSmoothPolyDataFilter() = default;
 
 //------------------------------------------------------------------------------
