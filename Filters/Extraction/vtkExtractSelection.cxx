Kitware/VTK

commit af54b16d94dfc7a9a806d3e1ac39cc7ebedaa1a1

vtkExtractSelection: ShallowCopy only when there is a need

Filters/Extraction/vtkExtractSelection.cxx
@@ -267,6 +267,22 @@ int vtkExtractSelection::RequestData(vtkInformation* vtkNotUsed(request),
     }
   }
 
+  // Check if vtkSelector::ExpandToConnectedElements will be used.
+  // This is useful because we can omit shallow copy of the input data.
+  bool expandToConnectedElements = false;
+  for (unsigned int cc = 0, max = selection->GetNumberOfNodes(); cc < max; ++cc)
+  {
+    auto node = selection->GetNode(cc);
+    int association = vtkSelectionNode::ConvertSelectionFieldToAttributeType(node->GetFieldType());
+    const int layers = node->GetProperties()->Get(vtkSelectionNode::CONNECTED_LAYERS());
+
+    if (layers >= 1 && (association == vtkDataObject::POINT || association == vtkDataObject::CELL))
+    {
+      expandToConnectedElements = true;
+      break;
+    }
+  }
+
   if (auto inputCD = vtkCompositeDataSet::SafeDownCast(input))
   {
     auto outputCD = vtkCompositeDataSet::SafeDownCast(output);
@@ -287,10 +303,24 @@ int vtkExtractSelection::RequestData(vtkInformation* vtkNotUsed(request),
       auto blockInput = inIter->GetCurrentDataObject();
       if (blockInput)
       {
-        auto clone = blockInput->NewInstance();
-        clone->ShallowCopy(blockInput);
+        vtkSmartPointer<vtkDataObject> clone;
+        if (expandToConnectedElements || this->PreserveTopology)
+        {
+          clone.TakeReference(input->NewInstance());
+          clone->ShallowCopy(input);
+        }
+        else
+        {
+          if (assoc != vtkDataObject::ROW)
+          {
+            clone.TakeReference(vtkUnstructuredGrid::New());
+          }
+          else
+          {
+            clone.TakeReference(vtkTable::New());
+          }
+        }
         outputCD->SetDataSet(inIter, clone);
-        clone->FastDelete();
       }
     }
 
@@ -366,8 +396,22 @@ int vtkExtractSelection::RequestData(vtkInformation* vtkNotUsed(request),
     assert(output != nullptr);
 
     vtkSmartPointer<vtkDataObject> clone;
-    clone.TakeReference(input->NewInstance());
-    clone->ShallowCopy(input);
+    if (expandToConnectedElements || this->PreserveTopology)
+    {
+      clone.TakeReference(input->NewInstance());
+      clone->ShallowCopy(input);
+    }
+    else
+    {
+      if (assoc != vtkDataObject::ROW)
+      {
+        clone.TakeReference(vtkUnstructuredGrid::New());
+      }
+      else
+      {
+        clone.TakeReference(vtkTable::New());
+      }
+    }
 
     // Evaluate the operators.
     vtkLogStartScope(TRACE, "execute selectors");

commit c872965ff0eda34bb6e4a941741867d8fb9c48b5

vtkExtractSelection: Fix wrong NewInstance/ShallowCopy introduced in af54b16d

Filters/Extraction/vtkExtractSelection.cxx
@@ -306,8 +306,8 @@ int vtkExtractSelection::RequestData(vtkInformation* vtkNotUsed(request),
         vtkSmartPointer<vtkDataObject> clone;
         if (expandToConnectedElements || this->PreserveTopology)
         {
-          clone.TakeReference(input->NewInstance());
-          clone->ShallowCopy(input);
+          clone.TakeReference(blockInput->NewInstance());
+          clone->ShallowCopy(blockInput);
         }
         else
         {