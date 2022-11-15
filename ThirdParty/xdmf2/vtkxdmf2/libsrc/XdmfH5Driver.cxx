8701b90c78502d429e2a58973833de6215ef43f0
Ryan Krattiger <ryan.krattiger@kitware.com>
2022-11-15 15:00:36 -0600

    Xdmf2: Update HDF5 driver for 1.13.2

--- ThirdParty/xdmf2/vtkxdmf2/libsrc/XdmfH5Driver.cxx
+++ ThirdParty/xdmf2/vtkxdmf2/libsrc/XdmfH5Driver.cxx
@@ -157,6 +157,7 @@ static herr_t H5FD_dsm_write(H5FD_t *_file, H5FD_mem_t type, hid_t fapl_id, hadd
 
 static const H5FD_class_t H5FD_dsm_g = {
 #if (H5_VERS_MAJOR>1)||((H5_VERS_MAJOR==1)&&(H5_VERS_MINOR>=13))
+    0,                          /*version       */
     H5_VFD_RESERVED + 0xd3f2,   /*value         */
 #endif
     "dsm",                      /*name          */
@@ -190,6 +191,12 @@ static const H5FD_class_t H5FD_dsm_g = {
     NULL,                       /*get_handle    */
     H5FD_dsm_read,              /*read          */
     H5FD_dsm_write,             /*write         */
+#if (H5_VERS_MAJOR>1)||((H5_VERS_MAJOR==1)&&(H5_VERS_MINOR>=13))
+    NULL,                       /*read_vector     */
+    NULL,                       /*write_vector    */
+    NULL,                       /*read_selection  */
+    NULL,                       /*write_selection */
+#endif
     NULL,                       /*flush         */
 #if (H5_VERS_MAJOR>1)||((H5_VERS_MAJOR==1)&&(H5_VERS_MINOR>=8))
     NULL,                       /* truncate     */
