e81a2fecc8f70339d2cbde7623e982c428f45eb7
Ryan Krattiger <ryan.krattiger@kitware.com>
2022-11-30 20:24:39 -0600

    Workaround using 1.13.1 in provided hdf5

--- ThirdParty/xdmf2/vtkxdmf2/libsrc/XdmfH5Driver.cxx
+++ ThirdParty/xdmf2/vtkxdmf2/libsrc/XdmfH5Driver.cxx
@@ -156,8 +156,10 @@ static herr_t H5FD_dsm_write(H5FD_t *_file, H5FD_mem_t type, hid_t fapl_id, hadd
 }
 
 static const H5FD_class_t H5FD_dsm_g = {
-#if (H5_VERS_MAJOR>1)||((H5_VERS_MAJOR==1)&&(H5_VERS_MINOR>=13))
+#if (H5_VERS_MAJOR>1)||((H5_VERS_MAJOR==1)&&((H5_VERS_MINOR>13)||((H5_VERS_MINOR==13)&&(H5_VERS_RELEASE>=2))))
     0,                          /*version       */
+#endif
+#if (H5_VERS_MAJOR>1)||((H5_VERS_MAJOR==1)&&(H5_VERS_MINOR>=13))
     H5_VFD_RESERVED + 0xd3f2,   /*value         */
 #endif
     "dsm",                      /*name          */
@@ -191,7 +193,7 @@ static const H5FD_class_t H5FD_dsm_g = {
     NULL,                       /*get_handle    */
     H5FD_dsm_read,              /*read          */
     H5FD_dsm_write,             /*write         */
-#if (H5_VERS_MAJOR>1)||((H5_VERS_MAJOR==1)&&(H5_VERS_MINOR>=13))
+#if (H5_VERS_MAJOR>1)||((H5_VERS_MAJOR==1)&&((H5_VERS_MINOR>13)||((H5_VERS_MINOR==13)&&(H5_VERS_RELEASE>=2))))
     NULL,                       /*read_vector     */
     NULL,                       /*write_vector    */
     NULL,                       /*read_selection  */
