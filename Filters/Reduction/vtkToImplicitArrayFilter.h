264997c57fbfbee5868df84a71c3285e68960a1a
Julien Fausty <julien.fausty@kitware.com>
2022-12-02 17:37:50 +0100

    add(FiltersReduction): module premise
    The new module is meant to contain filters that reduce data either in
    memory or in meaning or in precision, etc.

---
+++ Filters/Reduction/vtkToImplicitArrayFilter.h
@@ -0,0 +1,22 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkToImplicitArrayFilter.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#ifndef vtkToImplicitArrayFilter_h
+#define vtkToImplicitArrayFilter_h
+
+class vtkToImplicitArrayFilter
+{
+};
+
+#endif // vtkToImplicitArrayFilter_h
