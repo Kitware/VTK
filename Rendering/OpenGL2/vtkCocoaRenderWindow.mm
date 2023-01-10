6e0395907da610fd5665e9258fadc6dc08a29e2b
David Gobbi <david.gobbi@gmail.com>
2023-01-10 14:57:49 -0700

    Make Cocoa context-view connection before MakeCurrent
    Because vtkOpenGLRenderWindow::Start() calls MakeCurrent(), it must
    be moved until after vtkCocoaRenderWindow::Start() has done all of
    the context setup.  This brings the order of operations in line with
    vtkXOpenGLRenderWindow.

--- Rendering/OpenGL2/vtkCocoaRenderWindow.mm
+++ Rendering/OpenGL2/vtkCocoaRenderWindow.mm
@@ -1046,7 +1046,7 @@ void vtkCocoaRenderWindow::CreateGLContext()
 // Initialize the rendering process.
 void vtkCocoaRenderWindow::Start()
 {
-  this->Superclass::Start();
+  this->Initialize();
 
   // make sure the hardware is up to date otherwise
   // the backing store may not match the current window
@@ -1076,8 +1076,7 @@ void vtkCocoaRenderWindow::Start()
     [context update];
   }
 
-  // set the current window
-  this->MakeCurrent();
+  this->Superclass::Start();
 }
 
 //----------------------------------------------------------------------------
