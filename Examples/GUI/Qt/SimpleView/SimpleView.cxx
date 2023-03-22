7755eaae5b7b82732e1b0613437d13bc8aa0e463
Julien Fausty <julien.fausty@kitware.com>
2023-03-22 17:26:08 +0100

    SimpleView: add vtkAttributeDataToTableFilter

--- Examples/GUI/Qt/SimpleView/SimpleView.cxx
+++ Examples/GUI/Qt/SimpleView/SimpleView.cxx
@@ -12,7 +12,7 @@
 
 #include "vtkGenericOpenGLRenderWindow.h"
 #include "vtkSmartPointer.h"
-#include <vtkDataObjectToTable.h>
+#include <vtkAttributeDataToTableFilter.h>
 #include <vtkElevationFilter.h>
 #include <vtkNew.h>
 #include <vtkPolyDataMapper.h>
@@ -62,9 +62,9 @@ SimpleView::SimpleView()
 
   // Just a bit of Qt interest: Culling off the
   // point data and handing it to a vtkQtTableView
-  vtkNew<vtkDataObjectToTable> toTable;
+  vtkNew<vtkAttributeDataToTableFilter> toTable;
   toTable->SetInputConnection(elevation->GetOutputPort());
-  toTable->SetFieldType(vtkDataObjectToTable::POINT_DATA);
+  toTable->SetFieldAssociation(vtkDataObject::FIELD_ASSOCIATION_POINTS);
 
   // Here we take the end of the VTK pipeline and give it to a Qt View
   this->TableView->SetRepresentationFromInputConnection(toTable->GetOutputPort());
