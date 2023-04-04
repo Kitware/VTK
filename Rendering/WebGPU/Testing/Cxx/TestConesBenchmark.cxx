3db36221d5d6a785823a903c8759b26fbffbbeb4
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2023-04-04 12:11:13 -0400

    RenderingWebGPU: Adapt cone benchmark with cell colors
    - apply randomized colors for each cone face.
    - extend the emscripten example with cell colors.

--- Rendering/WebGPU/Testing/Cxx/TestConesBenchmark.cxx
+++ Rendering/WebGPU/Testing/Cxx/TestConesBenchmark.cxx
@@ -1,9 +1,11 @@
 #include "vtkActor.h"
 #include "vtkCamera.h"
+#include "vtkCellData.h"
 #include "vtkColorTransferFunction.h"
 #include "vtkConeSource.h"
 #include "vtkElevationFilter.h"
 #include "vtkInteractorStyleTrackballCamera.h"
+#include "vtkMinimalStandardRandomSequence.h"
 #include "vtkNew.h"
 #include "vtkObject.h"
 #include "vtkPolyDataMapper.h"
@@ -13,6 +15,7 @@
 #include "vtkRenderer.h"
 #include "vtkScalarsToColors.h"
 #include "vtkSphereSource.h"
+#include "vtkUnsignedCharArray.h"
 
 int TestConesBenchmark(int argc, char* argv[])
 {
@@ -24,25 +27,43 @@ int TestConesBenchmark(int argc, char* argv[])
   renWin->AddRenderer(renderer);
 
   double x = 0.0, y = 0.0, z = 0.0;
+  vtkNew<vtkMinimalStandardRandomSequence> seq;
   double spacingX = 2.0, spacingY = 2.0, spacingZ = 2.0;
-  for (int k = 0; k < 1; ++k)
+  int nx = 100, ny = 10, nz = 10;
+  for (int k = 0; k < nz; ++k)
   {
-    for (int j = 0; j < 8; ++j)
+    for (int j = 0; j < ny; ++j)
     {
-      for (int i = 0; i < 8; ++i)
+      for (int i = 0; i < nx; ++i)
       {
-        vtkNew<vtkConeSource> cone;
-        cone->SetCenter(x, y, z);
+        vtkNew<vtkConeSource> coneSrc;
+        coneSrc->SetResolution(10);
+        coneSrc->SetCenter(x, y, z);
         x += spacingX;
-        // map elevation output to graphics primitives.
+
+        coneSrc->Update();
+        vtkPolyData* cone = coneSrc->GetOutput();
+
+        seq->SetSeed(k * ny * nx + j * nx + i);
+        vtkNew<vtkUnsignedCharArray> colors;
+        colors->SetNumberOfComponents(4);
+        for (vtkIdType cellId = 0; cellId < cone->GetNumberOfPolys(); ++cellId)
+        {
+          double red = seq->GetNextRangeValue(0, 255.);
+          double green = seq->GetNextRangeValue(0, 255.);
+          double blue = seq->GetNextRangeValue(0, 255.);
+          colors->InsertNextTuple4(red, green, blue, 255);
+        }
+        cone->GetCellData()->SetScalars(colors);
+
         vtkNew<vtkPolyDataMapper> mapper;
-        // mapper->SetLookupTable(lut);
-        mapper->SetInputConnection(cone->GetOutputPort());
+        mapper->SetInputData(cone);
+        mapper->Update();
+        mapper->SetStatic(true);
         vtkNew<vtkActor> actor;
         actor->SetMapper(mapper);
-        // mapper->DebugOn();
-        // actor->GetProperty()->SetPointSize(6.0);
-        // actor->GetProperty()->SetRepresentationToPoints();
+        actor->GetProperty()->SetEdgeVisibility(1);
+        actor->GetProperty()->SetEdgeColor(1.0, 1.0, 1.0);
         mapper->Update();
         actor->SetOrigin(x, y, z);
         actor->RotateZ(i * j);
