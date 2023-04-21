5575bd38e2b47b91231507a9ea231c052bf9d05f
Thomas Galland <thomas.galland@kitware.com>
2023-04-21 09:40:32 +0200

    Add new HTG contour strategy
    HTG contour now have 2 contour strategy in the 3D case.
    The former one is called VTK_HTG_CONTOUR_FAST and corresponds to the original behavior.
    The second one is called VTK_HTG_CONTOUR_QUALITY and allow better contour results in some cases (when generated dual cells used for contouring appears to be concave).
    Note that this new strategy is much more slower than the first one.

---
+++ Filters/HyperTree/Testing/Cxx/TestHyperTreeGridBinary3DContourDecomposePolyhedra.cxx
@@ -0,0 +1,139 @@
+/*==================================================================
+
+  Program:   Visualization Toolkit
+  Module:    TestHyperTreeGridBinary3DContourDecomposePolyhedra.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+===================================================================*/
+// .SECTION Thanks
+// Funded by CEA, DAM, DIF, F-91297 Arpajon, France
+
+#include "vtkCamera.h"
+#include "vtkCellData.h"
+#include "vtkHyperTreeGrid.h"
+#include "vtkHyperTreeGridContour.h"
+#include "vtkHyperTreeGridGeometry.h"
+#include "vtkHyperTreeGridPreConfiguredSource.h"
+#include "vtkHyperTreeGridToDualGrid.h"
+#include "vtkHyperTreeGridToUnstructuredGrid.h"
+#include "vtkNew.h"
+#include "vtkPointData.h"
+#include "vtkPolyData.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkProperty.h"
+#include "vtkRegressionTestImage.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkRenderer.h"
+
+#include "vtkHyperTreeGridSource.h"
+
+int TestHyperTreeGridBinary3DContourDecomposePolyhedra(int argc, char* argv[])
+{
+  constexpr int depth = 5;
+  constexpr int nbOfContours = 4;
+
+  // Generate HTG
+  vtkNew<vtkHyperTreeGridSource> htgSource;
+  htgSource->SetMaxDepth(depth);
+  htgSource->SetDimensions(4, 4, 3); // Dimension 3 GridCell 3, 3, 2
+  htgSource->SetGridScale(1.5, 1., .7);
+  htgSource->SetBranchFactor(2);
+  htgSource->SetDescriptor(
+    "RRR .R. .RR ..R ..R .R.|R....... ........ ........ ...R.... .RRRR.R. RRRRR.RR ........ "
+    "........ ........|........ ........ ........ RR.RR.RR ........ RR...... ........ ........ "
+    "........ ........ ........ ........ ........ ..RRR...|........ ..R..... ........ ........ "
+    "........ ........ ........ ........ ........ ........ ........|........");
+  htgSource->Update();
+
+  // Set contouring scalars
+  vtkHyperTreeGrid* htg = vtkHyperTreeGrid::SafeDownCast(htgSource->GetOutput());
+  htg->GetCellData()->SetScalars(htg->GetCellData()->GetArray("Depth"));
+
+  // Contour
+  vtkNew<vtkHyperTreeGridContour> contour;
+  contour->SetInputConnection(htgSource->GetOutputPort());
+  contour->SetStrategy3D(vtkHyperTreeGridContour::USE_DECOMPOSED_POLYHEDRA);
+  contour->SetNumberOfContours(nbOfContours);
+  double resolution = (depth - 1) / (nbOfContours + 1.);
+  double isovalue = resolution;
+  for (int i = 0; i < nbOfContours; ++i, isovalue += resolution)
+  {
+    contour->SetValue(i, isovalue);
+  }
+
+  // Geometry
+  vtkNew<vtkHyperTreeGridGeometry> geometry;
+  geometry->SetInputConnection(htgSource->GetOutputPort());
+  geometry->Update();
+  vtkPolyData* pd = geometry->GetPolyDataOutput();
+
+  // Mappers
+  vtkMapper::SetResolveCoincidentTopologyToPolygonOffset();
+  vtkNew<vtkPolyDataMapper> mapper1;
+  mapper1->SetInputConnection(contour->GetOutputPort());
+  mapper1->SetScalarRange(pd->GetCellData()->GetArray("Depth")->GetRange());
+  vtkNew<vtkPolyDataMapper> mapper2;
+  mapper2->SetInputConnection(contour->GetOutputPort());
+  mapper2->ScalarVisibilityOff();
+  vtkNew<vtkPolyDataMapper> mapper3;
+  mapper3->SetInputConnection(geometry->GetOutputPort());
+  mapper3->ScalarVisibilityOff();
+
+  // Actors
+  vtkNew<vtkActor> actor1;
+  actor1->SetMapper(mapper1);
+  vtkNew<vtkActor> actor2;
+  actor2->SetMapper(mapper2);
+  actor2->GetProperty()->SetRepresentationToWireframe();
+  actor2->GetProperty()->SetColor(.3, .3, .3);
+  actor2->GetProperty()->SetLineWidth(1);
+  vtkNew<vtkActor> actor3;
+  actor3->SetMapper(mapper3);
+  actor3->GetProperty()->SetRepresentationToWireframe();
+  actor3->GetProperty()->SetColor(.7, .7, .7);
+
+  // Camera
+  double bd[6] = { 0. };
+  pd->GetBounds(bd);
+  vtkNew<vtkCamera> camera;
+  camera->SetClippingRange(1., 100.);
+  camera->SetFocalPoint(pd->GetCenter());
+  camera->SetPosition(-.8 * bd[1], 2.1 * bd[3], -4.8 * bd[5]);
+
+  // Renderer
+  vtkNew<vtkRenderer> renderer;
+  renderer->SetActiveCamera(camera);
+  renderer->SetBackground(1., 1., 1.);
+  renderer->AddActor(actor1);
+  renderer->AddActor(actor2);
+  renderer->AddActor(actor3);
+
+  // Render window
+  vtkNew<vtkRenderWindow> renWin;
+  renWin->AddRenderer(renderer);
+  renWin->SetSize(400, 400);
+  renWin->SetMultiSamples(0);
+
+  // Interactor
+  vtkNew<vtkRenderWindowInteractor> iren;
+  iren->SetRenderWindow(renWin);
+
+  // Render and test
+  renWin->Render();
+
+  int retVal = vtkRegressionTestImageThreshold(renWin, 60);
+  if (retVal == vtkRegressionTester::DO_INTERACTOR)
+  {
+    iren->Start();
+  }
+
+  return !retVal;
+}
--- Filters/HyperTree/vtkHyperTreeGridContour.cxx
+++ Filters/HyperTree/vtkHyperTreeGridContour.cxx
@@ -18,8 +18,11 @@
 #include "vtkBitArray.h"
 #include "vtkCellArray.h"
 #include "vtkCellData.h"
+#include "vtkCellIterator.h"
 #include "vtkContourHelper.h"
 #include "vtkContourValues.h"
+#include "vtkDoubleArray.h"
+#include "vtkGenericCell.h"
 #include "vtkHyperTree.h"
 #include "vtkHyperTreeGrid.h"
 #include "vtkHyperTreeGridNonOrientedCursor.h"
@@ -35,24 +38,53 @@
 #include "vtkPixel.h"
 #include "vtkPointData.h"
 #include "vtkPolyData.h"
+#include "vtkPolyhedron.h"
+#include "vtkPolyhedronUtilities.h"
 #include "vtkUnsignedCharArray.h"
+#include "vtkUnstructuredGrid.h"
 #include "vtkVoxel.h"
 
+#include <memory>
+
 VTK_ABI_NAMESPACE_BEGIN
-static const unsigned int MooreCursors1D[2] = { 0, 2 };
-static const unsigned int MooreCursors2D[8] = { 0, 1, 2, 3, 5, 6, 7, 8 };
-static const unsigned int MooreCursors3D[26] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15,
-  16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 };
-static const unsigned int* MooreCursors[3] = {
+
+namespace
+{
+const unsigned int MooreCursors1D[2] = { 0, 2 };
+const unsigned int MooreCursors2D[8] = { 0, 1, 2, 3, 5, 6, 7, 8 };
+const unsigned int MooreCursors3D[26] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17,
+  18, 19, 20, 21, 22, 23, 24, 25, 26 };
+const unsigned int* MooreCursors[3] = {
   MooreCursors1D,
   MooreCursors2D,
   MooreCursors3D,
 };
 
+// Conversion table of canonical ids from voxel to polyhedron
+constexpr vtkIdType CANONICAL_FACES[24] = { 2, 3, 1, 0, 1, 5, 4, 0, 4, 6, 2, 0, 3, 7, 5, 1, 2, 6, 7,
+  3, 5, 7, 6, 4 };
+constexpr std::size_t POLY_FACES_SIZE = 31;
+constexpr vtkIdType POLY_FACES_NB = 6;
+constexpr vtkIdType POLY_FACES_POINTS_NB = 4;
+constexpr vtkIdType POLY_POINTS_NB = 8;
+}
+
+//------------------------------------------------------------------------------
+struct vtkHyperTreeGridContour::vtkInternals
+{
+  // Temporary data structures related to USE_DECOMPOSED_POLYHEDRA strategy
+  std::vector<vtkIdType> Faces;
+  vtkNew<vtkPolyhedron> Polyhedron;
+  vtkNew<vtkGenericCell> Tetra;
+  vtkNew<vtkDoubleArray> TetraScalars;
+};
+
+//------------------------------------------------------------------------------
 vtkStandardNewMacro(vtkHyperTreeGridContour);
 
 //------------------------------------------------------------------------------
 vtkHyperTreeGridContour::vtkHyperTreeGridContour()
+  : Internals(new vtkHyperTreeGridContour::vtkInternals())
 {
   // Initialize storage for contour values
   this->ContourValues = vtkContourValues::New();
@@ -83,6 +115,11 @@ vtkHyperTreeGridContour::vtkHyperTreeGridContour()
 
   // Input scalars point to null by default
   this->InScalars = nullptr;
+
+  // Initialize temporal structures related to USE_DECOMPOSED_POLYHEDRA strategy
+  this->Internals->Polyhedron->GetPointIds()->SetNumberOfIds(::POLY_POINTS_NB);
+  this->Internals->Polyhedron->GetPoints()->SetNumberOfPoints(::POLY_POINTS_NB);
+  this->Internals->Faces.reserve(::POLY_FACES_SIZE);
 }
 
 //------------------------------------------------------------------------------
@@ -305,15 +342,15 @@ int vtkHyperTreeGridContour::ProcessTrees(vtkHyperTreeGrid* input, vtkDataObject
   newPts->Allocate(estimatedSize, estimatedSize);
 
   // Create storage for output vertices
-  vtkCellArray* newVerts = vtkCellArray::New();
+  vtkNew<vtkCellArray> newVerts;
   newVerts->AllocateExact(estimatedSize, estimatedSize);
 
   // Create storage for output lines
-  vtkCellArray* newLines = vtkCellArray::New();
+  vtkNew<vtkCellArray> newLines;
   newLines->AllocateExact(estimatedSize, estimatedSize);
 
   // Create storage for output polygons
-  vtkCellArray* newPolys = vtkCellArray::New();
+  vtkNew<vtkCellArray> newPolys;
   newPolys->AllocateExact(estimatedSize, estimatedSize);
 
   // Create storage for output scalar values
@@ -329,11 +366,13 @@ int vtkHyperTreeGridContour::ProcessTrees(vtkHyperTreeGrid* input, vtkDataObject
   }
   this->Locator->InitPointInsertion(newPts, input->GetBounds(), estimatedSize);
 
-  vtkNew<vtkPointData> inPointData;
-  inPointData->PassData(input->GetCellData());
+  // Used to store the input cell data (hyper tree grid cells)
+  // as point data (dual mesh point data), the two being equivalent.
+  vtkNew<vtkPointData> dualPointData;
+  dualPointData->PassData(input->GetCellData());
 
   // Instantiate a contour helper for convenience, with triangle generation on
-  this->Helper = new vtkContourHelper(this->Locator, newVerts, newLines, newPolys, inPointData,
+  this->Helper = new vtkContourHelper(this->Locator, newVerts, newLines, newPolys, dualPointData,
     nullptr, output->GetPointData(), nullptr, estimatedSize, true);
 
   // Create storage to keep track of selected cells
@@ -379,7 +418,7 @@ int vtkHyperTreeGridContour::ProcessTrees(vtkHyperTreeGrid* input, vtkDataObject
     // Initialize new Moore cursor at root of current tree
     input->InitializeNonOrientedMooreSuperCursor(supercursor, index);
     // Compute contours recursively
-    this->RecursivelyProcessTree(supercursor);
+    this->RecursivelyProcessTree(supercursor, newVerts, newLines, newPolys, dualPointData);
   } // it
 
   // Set output
@@ -396,7 +435,6 @@ int vtkHyperTreeGridContour::ProcessTrees(vtkHyperTreeGrid* input, vtkDataObject
   {
     output->SetPolys(newPolys);
   }
-  newPolys->Delete();
 
   // Clean up
   this->SelectedCells->Delete();
@@ -411,8 +449,6 @@ int vtkHyperTreeGridContour::ProcessTrees(vtkHyperTreeGrid* input, vtkDataObject
   delete this->Helper;
   this->CellScalars->Delete();
   newPts->Delete();
-  newVerts->Delete();
-  newLines->Delete();
   this->Locator->Initialize();
 
   // Squeeze output
@@ -514,7 +550,8 @@ bool vtkHyperTreeGridContour::RecursivelyPreProcessTree(vtkHyperTreeGridNonOrien
 
 //------------------------------------------------------------------------------
 void vtkHyperTreeGridContour::RecursivelyProcessTree(
-  vtkHyperTreeGridNonOrientedMooreSuperCursor* supercursor)
+  vtkHyperTreeGridNonOrientedMooreSuperCursor* supercursor, vtkCellArray* newVerts,
+  vtkCellArray* newLines, vtkCellArray* newPolys, vtkPointData* inPd)
 {
   // Retrieve global index of input cursor
   vtkIdType id = supercursor->GetGlobalNodeIndex();
@@ -569,7 +606,7 @@ void vtkHyperTreeGridContour::RecursivelyProcessTree(
         // Create child cursor from parent in input grid
         supercursor->ToChild(child);
         // Recurse
-        this->RecursivelyProcessTree(supercursor);
+        this->RecursivelyProcessTree(supercursor, newVerts, newLines, newPolys, inPd);
         supercursor->ToParent();
       }
     }
@@ -595,7 +632,11 @@ void vtkHyperTreeGridContour::RecursivelyProcessTree(
         vtkIdType numContours = this->ContourValues->GetNumberOfContours();
         double* values = this->ContourValues->GetValues();
 
-        // Generate contour topology depending on dimensionality
+        /* Generate contour topology depending on dimensionality
+         * XXX: please note that the generated dual vtkPixel / vtkVoxel do not meet the criteria
+         * defined in their respective classes (orthogonal quadrilaterals / parallelepipeds) and
+         * seems only used here for convenience (reasons needs to be determined explicitly).
+         */
         vtkCell* cell = nullptr;
         switch (dim)
         {
@@ -607,11 +648,16 @@ void vtkHyperTreeGridContour::RecursivelyProcessTree(
             break;
           case 3:
             cell = this->Voxel;
+            break;
+          default:
+            vtkErrorMacro("Unsuported cell dimension had been encountered (must be 1, 2 or 3).");
+            return;
         } // switch ( dim )
 
         // Iterate over cell corners
         double x[3];
         supercursor->GetPoint(x);
+
         for (unsigned int _cornerIdx = 0; _cornerIdx < numLeavesCorners; ++_cornerIdx)
         {
           // Get cursor corresponding to this corner
@@ -628,11 +674,103 @@ void vtkHyperTreeGridContour::RecursivelyProcessTree(
           // Assign scalar value attached to this contour item
           this->CellScalars->SetTuple(_cornerIdx, this->InScalars->GetTuple(idN));
         } // cornerIdx
-        // Compute cell isocontour for each isovalue
-        for (int c = 0; c < numContours; ++c)
+
+        /* If we are in 3D and the contour strategy is set to USE_DECOMPOSED_POLYHEDRA,
+         * convert each voxel to polyhedron, decompose them and apply the contour on
+         * resulting tetrahedrons to give better results in the concave case.
+         * XXX: Here we assume that voxels are valid when converting them to polyhedrons.
+         * Highly degenerated voxels (faces having duplicated points) will lead to degenerated
+         * polyhedrons. However the computation of the contour after the decomposition seems to
+         * be insensitive to this issue for now (edge cases are still possible and should be
+         * reported if encountered).
+         */
+        if (this->Strategy3D == USE_DECOMPOSED_POLYHEDRA && dim == 3)
         {
-          this->Helper->Contour(cell, values[c], this->CellScalars, this->CurrentId);
-        } // c
+          // Insert points and global point IDs
+          for (int i = 0; i < ::POLY_POINTS_NB; ++i)
+          {
+            this->Internals->Polyhedron->GetPointIds()->SetId(i, cell->GetPointId(i));
+            this->Internals->Polyhedron->GetPoints()->SetPoint(i, cell->GetPoints()->GetPoint(i));
+          }
+
+          // Construct faces from voxel point ids (global ids)
+          this->Internals->Faces.clear();
+          this->Internals->Faces.emplace_back(::POLY_FACES_NB);
+          for (int faceId = 0, canonicalId = 0; faceId < ::POLY_FACES_NB; faceId++)
+          {
+            this->Internals->Faces.emplace_back(::POLY_FACES_POINTS_NB);
+            for (int i = 0; i < ::POLY_FACES_POINTS_NB; i++, canonicalId++)
+            {
+              this->Internals->Faces.emplace_back(cell->GetPointId(::CANONICAL_FACES[canonicalId]));
+            }
+          }
+
+          this->Internals->Polyhedron->SetFaces(this->Internals->Faces.data());
+          this->Internals->Polyhedron->Initialize();
+
+          // Decompose the this->Internals->Polyhedron
+          auto resultUG = vtkPolyhedronUtilities::Decompose(
+            this->Internals->Polyhedron, inPd, this->CurrentId, nullptr);
+
+          auto outPointData = vtkPointData::SafeDownCast(this->OutData);
+          if (!outPointData)
+          {
+            vtkErrorMacro("Unable to retrieve the output point data.");
+            return;
+          }
+
+          /* Estimated size: estimated number of generated triangles (before merging them).
+           * Only used in that case. Unused here because we choose to output triangles.
+           */
+          constexpr int estimatedSize = 0;
+
+          /* Instantiate a new contour helper
+           * Needed because we have to change the input point data (now indexed on resultUG point
+           * ids)
+           */
+          vtkContourHelper helper(this->Locator, newVerts, newLines, newPolys,
+            resultUG->GetPointData(), nullptr, outPointData, nullptr, estimatedSize, true);
+
+          // Retrieve the contouring array in the resultUG
+          auto contourScalars = resultUG->GetPointData()->GetArray(this->InScalars->GetName());
+          if (!contourScalars)
+          {
+            vtkErrorMacro(
+              "Unable to find the scalars used for contouring in decomposed dual cell.");
+            return;
+          }
+
+          // Compute polyhedron isocontour for each isovalue
+          for (int c = 0; c < numContours; ++c)
+          {
+            // Iterate on each tetrahedron of resultUG
+            vtkSmartPointer<vtkCellIterator> iter;
+            iter.TakeReference(resultUG->NewCellIterator());
+            for (iter->InitTraversal(); !iter->IsDoneWithTraversal(); iter->GoToNextCell())
+            {
+              iter->GetCell(this->Internals->Tetra);
+
+              // Scalars used for contouring need to be indexed on tetrahedron local ids
+              this->Internals->TetraScalars->Reset();
+              this->Internals->TetraScalars->SetNumberOfComponents(
+                contourScalars->GetNumberOfComponents());
+              this->Internals->TetraScalars->SetNumberOfTuples(iter->GetNumberOfPoints());
+              contourScalars->GetTuples(iter->GetPointIds(), this->Internals->TetraScalars);
+
+              vtkIdType cellId = iter->GetCellId();
+              helper.Contour(
+                this->Internals->Tetra, values[c], this->Internals->TetraScalars, cellId);
+            }
+          }
+        }
+        else // USE_VOXELS || dim != 3
+        {
+          // Compute cell isocontour for each isovalue
+          for (int c = 0; c < numContours; ++c)
+          {
+            this->Helper->Contour(cell, values[c], this->CellScalars, this->CurrentId);
+          }
+        }
 
         // Increment output cell counter
         ++this->CurrentId;
