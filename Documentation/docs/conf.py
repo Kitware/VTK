a9794d2fbdf885d3208b19e40c3326d8f391687c
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-06-06 02:00:22 -0400

    docs: Add instructions for building documentation
    Since the instructions suggest to create a local environment called `.venv`,
    it is  added to `.gitignore` and excluded from the documentation
    paths in `conf.py`.

--- Documentation/docs/conf.py
+++ Documentation/docs/conf.py
@@ -60,7 +60,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md"]
+exclude_patterns = ["_build", ".venv", "Thumbs.db", ".DS_Store", "README.md"]
 
 todo_include_todos = True
 
