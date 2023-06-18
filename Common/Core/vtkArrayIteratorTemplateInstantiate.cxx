c67678f98f8486cae711a3f3ef541048d11137ca
Ben Boeckel <ben.boeckel@kitware.com>
2023-06-17 21:56:10 -0400

    vtkOStreamWrapper: add missing includes
    This actually comes from `vtkSetGet.h` macros, but these seem to be the
    only two files having issues (RogueResearch7's 10.11 builds).

--- Common/Core/vtkArrayIteratorTemplateInstantiate.cxx
+++ Common/Core/vtkArrayIteratorTemplateInstantiate.cxx
@@ -16,6 +16,8 @@
 
 #include "vtkArrayIteratorTemplate.txx"
 
+#include "vtkOStreamWrapper.h"
+
 VTK_ABI_NAMESPACE_BEGIN
 vtkInstantiateTemplateMacro(template class VTKCOMMONCORE_EXPORT vtkArrayIteratorTemplate);
 template class VTKCOMMONCORE_EXPORT vtkArrayIteratorTemplate<vtkStdString>;
