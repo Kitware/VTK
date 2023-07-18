4360a6d9585b814ae5bd48b5866cc2c04e7b3bfa
Thomas Galland <thomas.galland@kitware.com>
2023-07-18 16:23:23 +0200

    Add assembly selection and file open

--- IO/FDS/vtkFDSReader.cxx
+++ IO/FDS/vtkFDSReader.cxx
@@ -14,15 +14,50 @@
 =========================================================================*/
 
 #include "vtkFDSReader.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
 #include "vtkObjectFactory.h"
+#include "vtkPartitionedDataSetCollection.h"
+
+#include <vtksys/FStream.hxx>
+#include <vtksys/SystemTools.hxx>
+
+#include <vector>
 
 VTK_ABI_NAMESPACE_BEGIN
+
+// ----------------------------------------------------------------------------
+struct vtkFDSReader::vtkInternals
+{
+  bool OpenFile(const std::string& fileName);
+
+  vtksys::ifstream File;
+};
+
+//-----------------------------------------------------------------------------
+bool vtkFDSReader::vtkInternals::OpenFile(const std::string& fileName)
+{
+  vtksys::SystemTools::Stat_t fs;
+  if (!vtksys::SystemTools::Stat(fileName.c_str(), &fs))
+  {
+    this->File.open(fileName.c_str(), ios::in);
+  }
+
+  if (this->File.fail())
+  {
+    vtkErrorWithObjectMacro(nullptr, "Could not open file: " << fileName);
+    return false;
+  }
+
+  return true;
+}
 
 //------------------------------------------------------------------------------
 vtkStandardNewMacro(vtkFDSReader)
 
   //------------------------------------------------------------------------------
   vtkFDSReader::vtkFDSReader()
+  : Internals(new vtkFDSReader::vtkInternals())
 {
   this->SetNumberOfInputPorts(0);
 }
@@ -36,17 +71,85 @@ void vtkFDSReader::PrintSelf(ostream& os, vtkIndent indent)
   this->Superclass::PrintSelf(os, indent);
 }
 
+//----------------------------------------------------------------------------
+bool vtkFDSReader::AddSelector(const char* selector)
+{
+  if (selector && this->Selectors.insert(selector).second)
+  {
+    this->Modified();
+    return true;
+  }
+
+  return false;
+}
+
+//----------------------------------------------------------------------------
+void vtkFDSReader::ClearSelectors()
+{
+  if (!this->Selectors.empty())
+  {
+    this->Selectors.clear();
+    this->Modified();
+  }
+}
+
 //------------------------------------------------------------------------------
 int vtkFDSReader::RequestInformation(vtkInformation* vtkNotUsed(request),
   vtkInformationVector** vtkNotUsed(inputVector), vtkInformationVector* vtkNotUsed(outputVector))
 {
   if (this->FileName.empty())
   {
-    vtkErrorMacro("Requires valid input file name");
+    vtkErrorMacro("Requires valid input file name.");
     return 0;
   }
 
-  cout << "Filename : " << this->FileName << endl;
+  if (!this->Internals->OpenFile(this->FileName))
+  {
+    return 0;
+  }
+
+  std::string rootNodeName = vtksys::SystemTools::GetFilenameWithoutLastExtension(this->FileName);
+
+  // Setup dummy assembly
+  this->Assembly->SetNodeName(vtkDataAssembly::GetRootNode(), rootNodeName.c_str());
+
+  // Level 0
+  const auto base = this->Assembly->AddNodes({ "Devices", "Slices", "Boundaries" });
+  // Level 1
+  const auto devices = this->Assembly->AddNodes({ "device_1", "device_2" }, base[0]);
+  const auto slices =
+    this->Assembly->AddNodes({ "slice_3D_1", "slice_3D_2", "Slices_1D", "Slices_2D" }, base[1]);
+  const auto boundaries = this->Assembly->AddNodes({ "boundary_1", "boundary_2" }, base[2]);
+  // Level 2
+  const auto slices1D = this->Assembly->AddNodes({ "slice_1D_1", "slice_1D_2" }, slices[2]);
+  const auto slices2D = this->Assembly->AddNodes({ "slice_2D_1", "slice_2D_2" }, slices[3]);
+
+  // Update Assembly widget
+  this->AssemblyTag += 1;
+
+  return 1;
+}
+
+// ----------------------------------------------------------------------------
+int vtkFDSReader::RequestData(vtkInformation* vtkNotUsed(request),
+  vtkInformationVector** vtkNotUsed(inputVector), vtkInformationVector* outputVector)
+{
+  vtkInformation* outInfo = outputVector->GetInformationObject(0);
+  vtkPartitionedDataSetCollection* output = vtkPartitionedDataSetCollection::GetData(outInfo);
+
+  if (!output)
+  {
+    vtkErrorMacro("Unable to retrieve the output !");
+    return 0;
+  }
+
+  const std::vector<std::string> selectors(this->Selectors.begin(), this->Selectors.end());
+  const auto selectedNodes = this->Assembly->SelectNodes(selectors);
+
+  vtkNew<vtkDataAssembly> outAssembly;
+  outAssembly->SubsetCopy(this->Assembly, selectedNodes);
+  output->SetDataAssembly(outAssembly);
+
   return 1;
 }
 
--- IO/FDS/vtkFDSReader.h
+++ IO/FDS/vtkFDSReader.h
@@ -16,10 +16,14 @@
 #ifndef vtkFDSReader_h
 #define vtkFDSReader_h
 
-#include "vtkIOFDSModule.h" // For export macro
+#include "vtkDataAssembly.h" // For vtkDataAssembly
+#include "vtkIOFDSModule.h"  // For export macro
+#include "vtkNew.h"          // For vtkNew
 #include "vtkPartitionedDataSetCollectionAlgorithm.h"
 
-#include <string>
+#include <memory> // For std::unique_ptr
+#include <set>    // For std::set
+#include <string> // For std::string
 
 VTK_ABI_NAMESPACE_BEGIN
 
@@ -43,17 +47,54 @@ public:
   vtkGetMacro(FileName, std::string);
   ///@}
 
+  /**
+   * TODO
+   */
+  vtkGetNewMacro(Assembly, vtkDataAssembly);
+
+  /**
+   * Whenever the assembly is changed, this tag gets changed. Note, users should
+   * not assume that this is monotonically increasing but instead simply rely on
+   * its value to determine if the assembly may have changed since last time.
+   *
+   * It is set to 0 whenever there's no valid assembly available.
+   */
+  vtkGetMacro(AssemblyTag, int);
+
+  ///@{
+  /**
+   * API to set selectors. Multiple selectors can be added using `AddSelector`.
+   * The order in which selectors are specified is not preserved and has no
+   * impact on the result.
+   *
+   * `AddSelector` returns true if the selector was added, false if the selector
+   * was already specified and hence not added.
+   *
+   * @sa vtkDataAssembly::SelectNodes
+   */
+  bool AddSelector(const char* selector);
+  void ClearSelectors();
+  ///@}
+
 protected:
   vtkFDSReader();
   ~vtkFDSReader() override;
 
   int RequestInformation(vtkInformation*, vtkInformationVector**, vtkInformationVector*) override;
+  int RequestData(vtkInformation*, vtkInformationVector**, vtkInformationVector*) override;
 
 private:
   vtkFDSReader(const vtkFDSReader&) = delete;
   void operator=(const vtkFDSReader&) = delete;
 
+  int AssemblyTag = 0;
   std::string FileName;
+  std::set<std::string> Selectors;
+
+  vtkNew<vtkDataAssembly> Assembly;
+
+  struct vtkInternals;
+  std::unique_ptr<vtkInternals> Internals;
 };
 
 VTK_ABI_NAMESPACE_END
