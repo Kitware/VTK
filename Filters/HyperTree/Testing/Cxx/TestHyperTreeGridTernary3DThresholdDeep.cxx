07e3f94441c53e2ddaff3ffee0598dde09208bb5
Julien Fausty <julien.fausty@kitware.com>
2023-09-15 18:08:53 +0200

    HTG: add testing for new threshold modes

---
+++ Filters/HyperTree/Testing/Cxx/TestHyperTreeGridTernary3DThresholdDeep.cxx
@@ -0,0 +1,113 @@
+// SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "vtkCamera.h"
+#include "vtkCellData.h"
+#include "vtkDataSetMapper.h"
+#include "vtkHyperTreeGrid.h"
+#include "vtkHyperTreeGridGeometry.h"
+#include "vtkHyperTreeGridSource.h"
+#include "vtkHyperTreeGridThreshold.h"
+#include "vtkNew.h"
+#include "vtkPolyData.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkProperty.h"
+#include "vtkRegressionTestImage.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkRenderer.h"
+
+int TestHyperTreeGridTernary3DThresholdDeep(int argc, char* argv[])
+{
+  // Hyper tree grid
+  vtkNew<vtkHyperTreeGridSource> htGrid;
+  htGrid->SetMaxDepth(6);
+  htGrid->SetDimensions(4, 4, 3); // GridCell 3, 3, 2
+  htGrid->SetGridScale(1.5, 1., .7);
+  htGrid->SetBranchFactor(3);
+  htGrid->SetDescriptor(
+    "RRR .R. .RR ..R ..R .R.|R.......................... ........................... "
+    "........................... .............R............. ....RR.RR........R......... "
+    ".....RRRR.....R.RR......... ........................... ........................... "
+    "...........................|........................... ........................... "
+    "........................... ...RR.RR.......RR.......... ........................... "
+    "RR......................... ........................... ........................... "
+    "........................... ........................... ........................... "
+    "........................... ........................... "
+    "............RRR............|........................... ........................... "
+    ".......RR.................. ........................... ........................... "
+    "........................... ........................... ........................... "
+    "........................... ........................... "
+    "...........................|........................... ...........................");
+  htGrid->Update();
+  vtkHyperTreeGrid* htg = vtkHyperTreeGrid::SafeDownCast(htGrid->GetOutput());
+  htg->GetCellData()->SetScalars(htg->GetCellData()->GetArray("Depth"));
+
+  // Threshold
+  vtkNew<vtkHyperTreeGridThreshold> threshold;
+  threshold->SetInputConnection(htGrid->GetOutputPort());
+  threshold->SetMemoryStrategy(vtkHyperTreeGridThreshold::DeepThreshold);
+  threshold->SetLowerThreshold(1.);
+  threshold->SetUpperThreshold(3.);
+
+  // Geometries
+  vtkNew<vtkHyperTreeGridGeometry> geometry1;
+  geometry1->SetInputConnection(htGrid->GetOutputPort());
+  geometry1->Update();
+  vtkPolyData* pd = geometry1->GetPolyDataOutput();
+  vtkNew<vtkHyperTreeGridGeometry> geometry2;
+  geometry2->SetInputConnection(threshold->GetOutputPort());
+
+  // Mappers
+  vtkMapper::SetResolveCoincidentTopologyToPolygonOffset();
+  vtkNew<vtkDataSetMapper> mapper1;
+  mapper1->SetInputConnection(geometry2->GetOutputPort());
+  mapper1->SetScalarRange(pd->GetCellData()->GetArray("Depth")->GetRange());
+  vtkNew<vtkPolyDataMapper> mapper2;
+  mapper2->SetInputConnection(geometry1->GetOutputPort());
+  mapper2->ScalarVisibilityOff();
+
+  // Actors
+  vtkNew<vtkActor> actor1;
+  actor1->SetMapper(mapper1);
+  vtkNew<vtkActor> actor2;
+  actor2->SetMapper(mapper2);
+  actor2->GetProperty()->SetRepresentationToWireframe();
+  actor2->GetProperty()->SetColor(.7, .7, .7);
+
+  // Camera
+  double bd[6];
+  pd->GetBounds(bd);
+  vtkNew<vtkCamera> camera;
+  camera->SetClippingRange(1., 100.);
+  camera->SetFocalPoint(pd->GetCenter());
+  camera->SetPosition(-.8 * bd[1], 2.1 * bd[3], -4.8 * bd[5]);
+
+  // Renderer
+  vtkNew<vtkRenderer> renderer;
+  renderer->SetActiveCamera(camera);
+  renderer->SetBackground(1., 1., 1.);
+  renderer->AddActor(actor1);
+  renderer->AddActor(actor2);
+
+  // Render window
+  vtkNew<vtkRenderWindow> renWin;
+  renWin->AddRenderer(renderer);
+  renWin->SetSize(400, 400);
+  renWin->SetMultiSamples(0);
+
+  // Interactor
+  vtkNew<vtkRenderWindowInteractor> iren;
+  iren->SetRenderWindow(renWin);
+
+  // Render and test
+  renWin->Render();
+
+  int retVal = vtkRegressionTestImageThreshold(renWin, 90);
+  if (retVal == vtkRegressionTester::DO_INTERACTOR)
+  {
+    iren->Start();
+  }
+
+  return !retVal;
+}
---
+++ Filters/HyperTree/Testing/Cxx/TestHyperTreeGridTernary3DThresholdMaterialDeep.cxx
@@ -0,0 +1,128 @@
+// SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "vtkCamera.h"
+#include "vtkCellData.h"
+#include "vtkDataSetMapper.h"
+#include "vtkHyperTreeGrid.h"
+#include "vtkHyperTreeGridGeometry.h"
+#include "vtkHyperTreeGridSource.h"
+#include "vtkHyperTreeGridThreshold.h"
+#include "vtkNew.h"
+#include "vtkPolyData.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkProperty.h"
+#include "vtkRegressionTestImage.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkRenderer.h"
+
+int TestHyperTreeGridTernary3DThresholdMaterialDeep(int argc, char* argv[])
+{
+  // Hyper tree grid
+  vtkNew<vtkHyperTreeGridSource> htGrid;
+  htGrid->SetMaxDepth(6);
+  htGrid->SetDimensions(4, 4, 3); // GridCell 3, 3, 2
+  htGrid->SetGridScale(1.5, 1., .7);
+  htGrid->SetBranchFactor(3);
+  htGrid->UseMaskOn();
+  htGrid->SetDescriptor(
+    "RRR .R. .RR ..R ..R .R.|R.......................... ........................... "
+    "........................... .............R............. ....RR.RR........R......... "
+    ".....RRRR.....R.RR......... ........................... ........................... "
+    "...........................|........................... ........................... "
+    "........................... ...RR.RR.......RR.......... ........................... "
+    "RR......................... ........................... ........................... "
+    "........................... ........................... ........................... "
+    "........................... ........................... "
+    "............RRR............|........................... ........................... "
+    ".......RR.................. ........................... ........................... "
+    "........................... ........................... ........................... "
+    "........................... ........................... "
+    "...........................|........................... ...........................");
+  htGrid->SetMask(
+    "111 011 011 111 011 110|111111111111111111111111111 111111111111111111111111111 "
+    "000000000100110111111111111 111111111111111111111111111 111111111111111111111111111 "
+    "111111111111111111111111111 111111111111111111111111111 111111111111111111111111111 "
+    "000110011100000100100010100|000001011011111111111111111 111111111111111111111111111 "
+    "111111111111111111111111111 111111111111001111111101111 111111111111111111111111111 "
+    "111111111111111111111111111 111111111111111111111111111 111111111111111111111111111 "
+    "111111111111111111111111111 111111111111111111111111111 111111111111111111111111111 "
+    "111111111111111111111111111 111111111111111111111111111 "
+    "111111111111111111111111111|000000000111100100111100100 000000000111001001111001001 "
+    "000000111100100111111111111 000000111001001111111111111 111111111111111111111111111 "
+    "111111111111111111111111111 111111111111111111111111111 111111111111111111111111111 "
+    "111111111111111111111111111 111111111111111111111111111 "
+    "110110110100111110111000000|111111111111111111111111111 111111111111111111111111111");
+  htGrid->Update();
+  vtkHyperTreeGrid* htg = vtkHyperTreeGrid::SafeDownCast(htGrid->GetOutput());
+  htg->GetCellData()->SetScalars(htg->GetCellData()->GetArray("Depth"));
+
+  // ThresholdMaterial
+  vtkNew<vtkHyperTreeGridThreshold> threshold;
+  threshold->SetMemoryStrategy(vtkHyperTreeGridThreshold::DeepThreshold);
+  threshold->SetInputConnection(htGrid->GetOutputPort());
+  threshold->SetLowerThreshold(1.);
+  threshold->SetUpperThreshold(3.);
+
+  // Geometries
+  vtkNew<vtkHyperTreeGridGeometry> geometry1;
+  geometry1->SetInputConnection(htGrid->GetOutputPort());
+  geometry1->Update();
+  vtkPolyData* pd = geometry1->GetPolyDataOutput();
+  vtkNew<vtkHyperTreeGridGeometry> geometry2;
+  geometry2->SetInputConnection(threshold->GetOutputPort());
+
+  // Mappers
+  vtkMapper::SetResolveCoincidentTopologyToPolygonOffset();
+  vtkNew<vtkDataSetMapper> mapper1;
+  mapper1->SetInputConnection(geometry2->GetOutputPort());
+  mapper1->SetScalarRange(pd->GetCellData()->GetArray("Depth")->GetRange());
+  vtkNew<vtkPolyDataMapper> mapper2;
+  mapper2->SetInputConnection(geometry1->GetOutputPort());
+  mapper2->ScalarVisibilityOff();
+
+  // Actors
+  vtkNew<vtkActor> actor1;
+  actor1->SetMapper(mapper1);
+  vtkNew<vtkActor> actor2;
+  actor2->SetMapper(mapper2);
+  actor2->GetProperty()->SetRepresentationToWireframe();
+  actor2->GetProperty()->SetColor(.7, .7, .7);
+
+  // Camera
+  double bd[6];
+  pd->GetBounds(bd);
+  vtkNew<vtkCamera> camera;
+  camera->SetClippingRange(1., 100.);
+  camera->SetFocalPoint(pd->GetCenter());
+  camera->SetPosition(-.8 * bd[1], 2.1 * bd[3], -4.8 * bd[5]);
+
+  // Renderer
+  vtkNew<vtkRenderer> renderer;
+  renderer->SetActiveCamera(camera);
+  renderer->SetBackground(1., 1., 1.);
+  renderer->AddActor(actor1);
+  renderer->AddActor(actor2);
+
+  // Render window
+  vtkNew<vtkRenderWindow> renWin;
+  renWin->AddRenderer(renderer);
+  renWin->SetSize(400, 400);
+  renWin->SetMultiSamples(0);
+
+  // Interactor
+  vtkNew<vtkRenderWindowInteractor> iren;
+  iren->SetRenderWindow(renWin);
+
+  // Render and test
+  renWin->Render();
+
+  int retVal = vtkRegressionTestImageThreshold(renWin, 115);
+  if (retVal == vtkRegressionTester::DO_INTERACTOR)
+  {
+    iren->Start();
+  }
+
+  return !retVal;
+}
