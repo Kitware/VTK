b2f5d0768f222998d9839433a10cb956874c4ceb
Ryan Krattiger <ryan.krattiger@kitware.com>
2023-09-15 16:53:08 -0500

    ZSpace: Use vtkDynamicLoader

--- Rendering/ZSpace/vtkZSpaceCoreCompatibilitySDKManager.h
+++ Rendering/ZSpace/vtkZSpaceCoreCompatibilitySDKManager.h
@@ -15,6 +15,7 @@
 #ifndef vtkZSpaceCoreCompatibilitySDKManager_h
 #define vtkZSpaceCoreCompatibilitySDKManager_h
 
+#include "vtkDynamicLoader.h"
 #include "vtkZSpaceSDKManager.h"
 
 // Disable "anonymous struct/union" warning on zSpace compat headers
@@ -22,8 +23,7 @@
 #include "zSpaceCoreCompatibility.h" // zSpace header
 #pragma warning(default : 4201)
 
-#include <vector>    // for std::vector
-#include <windows.h> // for HMODULE
+#include <vector> // for std::vector
 
 VTK_ABI_NAMESPACE_BEGIN
 
@@ -116,7 +116,7 @@ protected:
   zSpaceCoreCompatEntryPoints EntryPts;
 
   // Handle to the zSpace Core Compatibility API dynamic library (.dll).
-  HMODULE zSpaceCoreCompatDllModuleHandle;
+  vtkLibHandle zSpaceCoreCompatDllModuleHandle;
 
   // Handle to the current window
   HWND WidondowHandle;
@@ -126,7 +126,7 @@ protected:
    * zSpace Core Compatibility API entry point functions (at runtime)
    */
   bool loadZspaceCoreCompatibilityEntryPoints(const char* zSpaceCoreCompatDllFilePath,
-    HMODULE& zSpaceCoreCompatDllModuleHandle, zSpaceCoreCompatEntryPoints& entryPoints);
+    vtkLibHandle& zSpaceCoreCompatDllModuleHandle, zSpaceCoreCompatEntryPoints& entryPoints);
 
   /**
    * zSpace stores matrix in column-major format (as OpenGL). The matrix
