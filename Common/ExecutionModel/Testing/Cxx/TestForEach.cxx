ad4150925b8e2d59b9892d6cf50964c45b94c974
Charles Gueunet <charles.gueunet@kitware.com>
2023-09-26 15:41:44 +0200

    Complete TestForEach with more complex pipelines

--- Common/ExecutionModel/Testing/Cxx/TestForEach.cxx
+++ Common/ExecutionModel/Testing/Cxx/TestForEach.cxx
@@ -3,8 +3,10 @@
 
 #include <vtkEndFor.h>
 #include <vtkForEach.h>
+#include <vtkImageDataToPointSet.h>
 #include <vtkNew.h>
 #include <vtkPartitionedDataSetCollection.h>
+#include <vtkRandomAttributeGenerator.h>
 #include <vtkSpatioTemporalHarmonicsSource.h>
 
 #include <cstdlib>
@@ -42,11 +44,78 @@ bool TestNoPipeline()
   return true;
 }
 
+bool TestSimplePipeline()
+{
+  vtkNew<vtkSpatioTemporalHarmonicsSource> source;
+
+  vtkNew<vtkForEach> forEach;
+  forEach->SetInputConnection(source->GetOutputPort());
+
+  vtkNew<vtkImageDataToPointSet> toPointSet;
+  toPointSet->SetInputConnection(forEach->GetOutputPort());
+
+  vtkNew<vtkEndFor> endFor;
+  endFor->SetInputConnection(toPointSet->GetOutputPort());
+
+  endFor->Update();
+
+  auto output = endFor->GetOutput();
+  auto pdsc = vtkPartitionedDataSetCollection::SafeDownCast(output);
+  if (!pdsc)
+  {
+    std::cerr << "Output was not partitioned data set collection" << std::endl;
+    return false;
+  }
+
+  if (pdsc->GetNumberOfPartitionedDataSets() != ::NB_SOURCE_TIME_STEPS)
+  {
+    std::cerr << "Output did not have correct number of blocks" << std::endl;
+    return false;
+  }
+  return true;
+}
+
+bool TestComplexPipeline()
+{
+  vtkNew<vtkSpatioTemporalHarmonicsSource> source;
+
+  vtkNew<vtkForEach> forEach;
+  forEach->SetInputConnection(source->GetOutputPort());
+
+  vtkNew<vtkImageDataToPointSet> toPointSet;
+  toPointSet->SetInputConnection(forEach->GetOutputPort());
+
+  vtkNew<vtkRandomAttributeGenerator> random;
+  random->SetInputConnection(toPointSet->GetOutputPort());
+
+  vtkNew<vtkEndFor> endFor;
+  endFor->SetInputConnection(random->GetOutputPort());
+
+  endFor->Update();
+
+  auto output = endFor->GetOutput();
+  auto pdsc = vtkPartitionedDataSetCollection::SafeDownCast(output);
+  if (!pdsc)
+  {
+    std::cerr << "Output was not partitioned data set collection" << std::endl;
+    return false;
+  }
+
+  if (pdsc->GetNumberOfPartitionedDataSets() != ::NB_SOURCE_TIME_STEPS)
+  {
+    std::cerr << "Output did not have correct number of blocks" << std::endl;
+    return false;
+  }
+  return true;
+}
+
 }
 
 int TestForEach(int, char*[])
 {
   bool res = true;
   res &= ::TestNoPipeline();
+  res &= ::TestSimplePipeline();
+  res &= ::TestComplexPipeline();
   return res ? EXIT_SUCCESS : EXIT_FAILURE;
 }
