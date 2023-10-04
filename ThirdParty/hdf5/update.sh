a97f82ad9c068a3ac6a4c6733dc01c5c677348e1
Adrien Stucky <adrien.stucky@kitware.com>
2023-10-04 17:33:26 +0200

    edited update.sh to bump hdf5

--- ThirdParty/hdf5/update.sh
+++ ThirdParty/hdf5/update.sh
@@ -8,7 +8,7 @@ readonly name="hdf5"
 readonly ownership="HDF Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/hdf5.git"
-readonly tag="for/vtk-20220414-1.13.1"
+readonly tag="for/vtk-20231004-1.13.1"
 readonly paths="
 CMakeFilters.cmake
 CMakeInstallation.cmake
