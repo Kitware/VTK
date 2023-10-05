a19e2d22e012acc3b46bd99c33a94b0ab9f40d8f
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2023-10-05 16:02:43 -0400

    doc: Added documentation for cylinder source

--- Filters/Sources/vtkCylinderSource.h
+++ Filters/Sources/vtkCylinderSource.h
@@ -4,13 +4,12 @@
  * @class   vtkCylinderSource
  * @brief   generate a polygonal cylinder centered at the origin
  *
- * vtkCylinderSource creates a polygonal cylinder centered at Center;
- * The axis of the cylinder is aligned along the global y-axis.
- * The height and radius of the cylinder can be specified, as well as the
- * number of sides. It is also possible to control whether the cylinder is
- * open-ended or capped. If you have the end points of the cylinder, you
- * should use a vtkLineSource followed by a vtkTubeFilter instead of the
- * vtkCylinderSource.
+ * vtkCylinderSource creates a polygonal cylinder centered at Center; The axis of the cylinder is
+ * aligned along the global y-axis.  The height and radius of the cylinder can be specified, as well
+ * as the number of sides. It is also possible to control whether the cylinder is open-ended or
+ * capped. The cylinder can be given a capsular shape by enabling hemispherical end caps. If you
+ * have the end points of the cylinder, you should use a vtkLineSource followed by a vtkTubeFilter
+ * instead of the vtkCylinderSource.
  *
  * @sa
  * vtkCylinder
