1296557345d51ff6978ee48d045396a112d17013
Ben Boeckel <ben.boeckel@kitware.com>
2023-10-29 14:52:19 -0400

    h5part: test inclusion of h5part

---
+++ ThirdParty/h5part/Testing/Cxx/TestIncludeH5Part.cxx
@@ -0,0 +1,11 @@
+#include "vtk_h5part.h"
+// clang-format off
+#include VTK_H5PART(H5Part.h)
+// clang-format on
+
+#include <cstdlib>
+
+int TestIncludeH5Part(int /*argc*/, char* /*argv*/[])
+{
+  return EXIT_SUCCESS;
+}
