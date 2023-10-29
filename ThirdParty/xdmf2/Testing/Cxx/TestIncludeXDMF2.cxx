7b99a7fcafe1ef028048bbbf9991683b6bfd38a4
Ben Boeckel <ben.boeckel@kitware.com>
2023-10-29 14:52:19 -0400

    xdmf2: test inclusion of xdmf2

---
+++ ThirdParty/xdmf2/Testing/Cxx/TestIncludeXDMF2.cxx
@@ -0,0 +1,11 @@
+#include "vtk_xdmf2.h"
+// clang-format off
+#include VTKXDMF2_HEADER(XdmfArray.h)
+// clang-format on
+
+#include <cstdlib>
+
+int TestIncludeXDMF2(int /*argc*/, char* /*argv*/[])
+{
+  return EXIT_SUCCESS;
+}
