79e684c5f19d6b2832fa5fb52399520fa0d1aa44
Ben Boeckel <ben.boeckel@kitware.com>
2023-10-29 14:52:19 -0400

    libxml2: test inclusion of libxml2

---
+++ ThirdParty/libxml2/Testing/Cxx/TestIncludeLibXml2.cxx
@@ -0,0 +1,11 @@
+#include "vtk_libxml2.h"
+// clang-format off
+#include VTKLIBXML2_HEADER(parser.h)
+// clang-format on
+
+#include <cstdlib>
+
+int TestIncludeLibXml2(int /*argc*/, char* /*argv*/[])
+{
+  return EXIT_SUCCESS;
+}
