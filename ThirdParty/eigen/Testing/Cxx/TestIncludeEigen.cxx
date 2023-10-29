b4503a30d35d4f5539fdb1014485332250102678
Ben Boeckel <ben.boeckel@kitware.com>
2023-10-29 14:52:19 -0400

    eigen: test inclusion of eigen

---
+++ ThirdParty/eigen/Testing/Cxx/TestIncludeEigen.cxx
@@ -0,0 +1,11 @@
+#include "vtk_eigen.h"
+// clang-format off
+#include VTK_EIGEN(Eigen)
+// clang-format on
+
+#include <cstdlib>
+
+int TestIncludeEigen(int /*argc*/, char* /*argv*/[])
+{
+  return EXIT_SUCCESS;
+}
