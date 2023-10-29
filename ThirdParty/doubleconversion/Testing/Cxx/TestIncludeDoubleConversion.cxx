885c931feb185c35cc5acc7bcb82b920f31788a2
Ben Boeckel <ben.boeckel@kitware.com>
2023-10-29 14:52:19 -0400

    doubleconversion: test inclusion of doubleconversion

---
+++ ThirdParty/doubleconversion/Testing/Cxx/TestIncludeDoubleConversion.cxx
@@ -0,0 +1,11 @@
+#include "vtk_doubleconversion.h"
+// clang-format off
+#include VTK_DOUBLECONVERSION_HEADER(double-conversion.h)
+// clang-format on
+
+#include <cstdlib>
+
+int TestIncludeDoubleConversion(int /*argc*/, char* /*argv*/[])
+{
+  return EXIT_SUCCESS;
+}
