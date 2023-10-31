48227798fc84f52f843002c0098cbe5ec51b5854
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2023-10-30 22:58:49 -0400

    anari: Switch to SPDX copyright

--- Rendering/ANARI/vtkAnariLightNode.cxx
+++ Rendering/ANARI/vtkAnariLightNode.cxx
@@ -1,17 +1,5 @@
-/*=========================================================================
-
-  Program:   Visualization Toolkit
-  Module:    vtkAnariLightNode.cxx
-
-  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-  All rights reserved.
-  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notice for more information.
-
-=========================================================================*/
+// SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+// SPDX-License-Identifier: BSD-3-Clause
 #include "vtkAnariLightNode.h"
 #include "vtkAnariCameraNode.h"
 #include "vtkAnariProfiling.h"
--- Rendering/ANARI/vtkAnariViewNodeFactory.h
+++ Rendering/ANARI/vtkAnariViewNodeFactory.h
@@ -1,17 +1,5 @@
-/*=========================================================================
-
-  Program:   Visualization Toolkit
-  Module:    vtkAnariViewNodeFactory.h
-
-  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-  All rights reserved.
-  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notice for more information.
-
-=========================================================================*/
+// SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+// SPDX-License-Identifier: BSD-3-Clause
 /**
  * @class   vtkAnariViewNodeFactory
  * @brief   matches vtk rendering classes to specific ANARI ViewNode classes
