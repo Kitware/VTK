2f385c8224fe3cb00413b35dfbd01653425fe142
Ben Boeckel <ben.boeckel@kitware.com>
2023-10-30 22:04:06 -0400

    jsoncpp: update to use `HEADER_DIRECTORIES`

--- ThirdParty/jsoncpp/update.sh
+++ ThirdParty/jsoncpp/update.sh
@@ -8,7 +8,7 @@ readonly name="jsoncpp"
 readonly ownership="JsonCpp Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/jsoncpp.git"
-readonly tag="for/vtk-20210908-1.9.4"
+readonly tag="for/vtk-20231030-1.9.4"
 readonly paths="
 .gitattributes
 CMakeLists.vtk.txt
