dd52bd84e71424da38f37175db32f34cd6457636
Ryan Krattiger <ryan.krattiger@kitware.com>
2023-12-27 11:40:26 -0600

    Update ABI mangling tests
    Infovis/Boost module does not need ABI mangling
    IO/XML tests drop exceptions to C API
    Add MANGLING label to ABI mangling tests
    Fix bug in parsing exceptions in ABI mangling test driver

--- Testing/Core/CheckSymbolMangling.py
+++ Testing/Core/CheckSymbolMangling.py
@@ -3,8 +3,10 @@
 import os
 import argparse as cli
 
+
 def isWindows():
     return os.name == 'nt'
+
 
 def LibrarySymbols(lib):
     sym_list = []
@@ -14,7 +16,7 @@ def LibrarySymbols(lib):
             sym = line.split('|')[1].strip()
             sym_list.append(sym)
     else:
-        stream = os.popen('nm %s' % lib)
+        stream = os.popen('nm -gC --defined %s' % lib)
         sym_codes = ['B', 'D', 'R', 'T']
         for line in stream:
             if len(line) < 20:
@@ -43,15 +45,36 @@ if not args.prefix:
     print('No prefix to check.')
     exit(0)
 
-exemptions = args.exemptions.split(';')
+exemptions = args.exemptions.split(';') if args.exemptions else []
 
 bad_sym = []
 
+# Special symbols can be ignored
+# exemptions.extend(["__bss_start", "_edata", "_end"])
+
+RESERVED_NAMES = []
+if not isWindows():
+    external_data_syms = ["_edata", "_etext", "_end"]
+    static_data_syms = ["__bss_start", "__bss_end"]
+    RESERVED_NAMES += external_data_syms + static_data_syms
+
+
+def is_reserved_name(sym):
+    return sym in RESERVED_NAMES
+
+
 for o in args.files.split(';'):
+    print("-- Checking object file: ", o)
     for sym in LibrarySymbols(o):
-        if not args.prefix in sym:
+        if is_reserved_name(sym):
+            continue
+
+        if args.prefix not in sym:
+            print("-- Found symbol", sym)
             if not any(exempt in sym for exempt in exemptions):
                 bad_sym.append(sym)
+            else:
+                print("Exempt")
 
 if bad_sym:
     print('Found symbols that are missing mangling.')
