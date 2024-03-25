2036d70318711eecd575b25db845317956bcbff6
Ben Boeckel <ben.boeckel@kitware.com>
2024-03-25 05:35:34 -0400

    ci: update to buildcache 0.29.0

--- .gitlab/ci/buildcache.ps1
+++ .gitlab/ci/buildcache.ps1
@@ -1,14 +1,14 @@
 $erroractionpreference = "stop"
 
-$version = "0.27.6"
-$sha256sum = "48631671fd26a6f5bb4b6755afb3cd9618194c74b93ec5abd633c22343045134"
+$version = "0.29.0"
+$sha256sum = "55AB902C0F1F0FBBBFFE083DCA4691A4874E1FDE64CEAACC13E1399BF79F5825"
 $filename = "buildcache-windows"
 $tarball = "$filename.zip"
 
 $outdir = $pwd.Path
 $outdir = "$outdir\.gitlab"
 $ProgressPreference = "SilentlyContinue"
-Invoke-WebRequest -Uri "https://github.com/mbitsnbites/buildcache/releases/download/v$version/$tarball" -OutFile "$outdir\$tarball"
+Invoke-WebRequest -Uri "https://gitlab.com/bits-n-bites/buildcache/-/releases/v$version/downloads/$tarball" -OutFile "$outdir\$tarball"
 $hash = Get-FileHash "$outdir\$tarball" -Algorithm SHA256
 if ($hash.Hash -ne $sha256sum) {
     exit 1
