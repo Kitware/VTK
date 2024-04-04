7ce8855a77eb08646d98a2e49489153bd6ee02d3
Cory Quammen <cory.quammen@kitware.com>
2024-04-03 22:16:56 -0400

    TestReflectionFilter: add test for triangle strip with even number of tris

--- Filters/General/Testing/Cxx/TestReflectionFilter.cxx
+++ Filters/General/Testing/Cxx/TestReflectionFilter.cxx
@@ -165,5 +165,77 @@ int TestReflectionFilter(int, char*[])
       cellPtIds->GetId(3) == 1,
     "quad");
 
+  // Test reflection of a triangle strip with even number of triangles
+  vtkNew<vtkUnstructuredGrid> strip;
+  vtkNew<vtkPoints> stripPoints;
+  stripPoints->InsertNextPoint(0.0, 0.0, -0.25);
+  stripPoints->InsertNextPoint(-1.0, 0.0, -0.25);
+  stripPoints->InsertNextPoint(-1.0, 0.0, -1.0);
+  stripPoints->InsertNextPoint(0.0, -0.5, -0.25);
+  stripPoints->InsertNextPoint(-1.0, -0.5, -0.25);
+  stripPoints->InsertNextPoint(-1.0, -0.5, -1.0);
+  strip->SetPoints(stripPoints);
+
+  vtkNew<vtkIdList> stripVerts;
+  stripVerts->InsertNextId(3);
+  stripVerts->InsertNextId(0);
+  stripVerts->InsertNextId(4);
+  stripVerts->InsertNextId(1);
+  stripVerts->InsertNextId(5);
+  stripVerts->InsertNextId(2);
+
+  strip->InsertNextCell(VTK_TRIANGLE_STRIP, stripVerts);
+
+  vtkNew<vtkReflectionFilter> stripReflectionFilter;
+  stripReflectionFilter->SetInputData(strip.GetPointer());
+  stripReflectionFilter->CopyInputOn();
+  stripReflectionFilter->FlipAllInputArraysOn();
+  stripReflectionFilter->SetPlaneToXMin();
+  stripReflectionFilter->Update();
+
+  vtkUnstructuredGrid* reflectedStrip =
+    vtkUnstructuredGrid::SafeDownCast(stripReflectionFilter->GetOutput());
+
+  AssertMacro(reflectedStrip->GetNumberOfPoints() == 12, "strip number of points");
+  vtkPoints* reflectedStripPts = reflectedStrip->GetPoints();
+  reflectedStripPts->GetPoint(0, pt);
+  AssertMacro(pt[0] == 0 && pt[1] == 0 && pt[2] == -0.25, "strip point mismatch");
+  reflectedStripPts->GetPoint(1, pt);
+  AssertMacro(pt[0] == -1.0 && pt[1] == 0 && pt[2] == -0.25, "strip point mismatch");
+  reflectedStripPts->GetPoint(2, pt);
+  AssertMacro(pt[0] == -1.0 && pt[1] == 0 && pt[2] == -1.0, "strip point mismatch");
+  reflectedStripPts->GetPoint(3, pt);
+  AssertMacro(pt[0] == 0 && pt[1] == -0.5 && pt[2] == -0.25, "strip point mismatch");
+  reflectedStripPts->GetPoint(4, pt);
+  AssertMacro(pt[0] == -1.0 && pt[1] == -0.5 && pt[2] == -0.25, "strip point mismatch");
+  reflectedStripPts->GetPoint(5, pt);
+  AssertMacro(pt[0] == -1.0 && pt[1] == -0.5 && pt[2] == -1.0, "strip point mismatch");
+  reflectedStripPts->GetPoint(6, pt);
+  AssertMacro(pt[0] == -2.0 && pt[1] == 0 && pt[2] == -0.25, "strip point mismatch");
+  reflectedStripPts->GetPoint(7, pt);
+  AssertMacro(pt[0] == -1.0 && pt[1] == 0 && pt[2] == -0.25, "strip point mismatch");
+  reflectedStripPts->GetPoint(8, pt);
+  AssertMacro(pt[0] == -1.0 && pt[1] == 0 && pt[2] == -1.0, "strip point mismatch");
+  reflectedStripPts->GetPoint(9, pt);
+  AssertMacro(pt[0] == -2.0 && pt[1] == -0.5 && pt[2] == -0.25, "strip point mismatch");
+  reflectedStripPts->GetPoint(10, pt);
+  AssertMacro(pt[0] == -1.0 && pt[1] == -0.5 && pt[2] == -0.25, "strip point mismatch");
+  reflectedStripPts->GetPoint(11, pt);
+  AssertMacro(pt[0] == -1.0 && pt[1] == -0.5 && pt[2] == -1.0, "strip point mismatch");
+
+  // There should be two strips
+  AssertMacro(reflectedStrip->GetNumberOfCells() == 2, "Expected 2 triangle strips");
+
+  // Check second strip
+  cellPtIds->Initialize();
+  reflectedStrip->GetCellPoints(1, cellPtIds);
+
+  AssertMacro(cellPtIds->GetNumberOfIds() == 7, "Expected 7 ids for triangle strip");
+  std::array<vtkIdType, 7> stripExpectedIds = { { 9, 10, 6, 10, 7, 11, 8 } };
+  for (vtkIdType i = 0; i < cellPtIds->GetNumberOfIds(); ++i)
+  {
+    AssertMacro(cellPtIds->GetId(i) == stripExpectedIds[i], "Cell point id mismatch");
+  }
+
   return EXIT_SUCCESS;
 }
