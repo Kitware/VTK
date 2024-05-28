d336e0264123af8bddfce36e9e969d5951ace2f1
Léon Victor <leon.victor@kitware.com>
2024-05-28 14:47:06 +0200

    DecimatePolylineStrategy: Remove empty impl. file

--- Filters/Core/vtkDecimatePolylineStrategy.cxx
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-// SPDX-License-Identifier: BSD-3-Clause
-
-#include "vtkDecimatePolylineStrategy.h"
-
-VTK_ABI_NAMESPACE_BEGIN
-
-VTK_ABI_NAMESPACE_END
