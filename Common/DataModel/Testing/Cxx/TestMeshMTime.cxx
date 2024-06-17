301b62be255ea9146ac7ae9a829111ecaecfa806
Léon Victor <leon.victor@kitware.com>
2024-06-17 10:35:34 +0200

    GetMeshMTime: Add tests for vtkPolyData / vtkUnstructuredGrid

--- Common/DataModel/Testing/Cxx/TestMeshMTime.cxx
+++ Common/DataModel/Testing/Cxx/TestMeshMTime.cxx
@@ -1,88 +1,148 @@
 // SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
 // SPDX-License-Identifier: BSD-3-Clause
 
+#include "vtkCellData.h"
 #include "vtkFloatArray.h"
 #include "vtkImageData.h"
 #include "vtkLogger.h"
 #include "vtkNew.h"
 #include "vtkPointData.h"
 #include "vtkPolyData.h"
+#include "vtkUnstructuredGrid.h"
 
 namespace
 {
-int TestDefaultImplementation()
+
+int CompareTimes(
+  vtkMTimeType before, vtkMTimeType after, bool expectUpdate, const std::string& modifiedObjectName)
+{
+  if (expectUpdate && before == after)
+  {
+    vtkLog(ERROR,
+      "Modifying " << modifiedObjectName << " should modify MeshMTime. Before: " << before
+                   << "; after: " << after);
+    return EXIT_FAILURE;
+  }
+
+  if (!expectUpdate && before != after)
+  {
+    vtkLog(ERROR,
+      "Modifying " << modifiedObjectName << " should not modify MeshMTime. Expected: " << before
+                   << "; got: " << after);
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
+
+int TestFieldDataModification(vtkDataSet* dataset, bool expectMeshMTimeUpdate)
 {
   int result = EXIT_SUCCESS;
 
+  // Create dummy point/cell data
   vtkNew<vtkFloatArray> floatArray;
-  floatArray->SetNumberOfValues(1);
-  floatArray->SetValue(0, 0);
-
-  // Image data does not implement GetMeshMTime (for now);
-  // Modifications to data arrays should update MeshMTime.
-  vtkNew<vtkImageData> imageData;
-  imageData->GetPointData()->AddArray(floatArray);
 
   // Modify point data arrays
-  const vtkMTimeType initialMeshMTime = imageData->GetMeshMTime();
-  imageData->GetPointData()->AddArray(floatArray);
-  const vtkMTimeType newMeshMTime = imageData->GetMeshMTime();
+  vtkMTimeType initialMeshMTime = dataset->GetMeshMTime();
+  dataset->GetPointData()->AddArray(floatArray);
+  vtkMTimeType newMeshMTime = dataset->GetMeshMTime();
 
-  if (initialMeshMTime == newMeshMTime)
-  {
-    vtkLog(ERROR, "Expected modified MeshMTime.");
-    result &= EXIT_FAILURE;
-  }
+  result &= CompareTimes(initialMeshMTime, newMeshMTime, expectMeshMTimeUpdate, "point data");
+
+  // Modify cell data arrays
+  initialMeshMTime = dataset->GetMeshMTime();
+  dataset->GetCellData()->AddArray(floatArray);
+  newMeshMTime = dataset->GetMeshMTime();
+
+  result &= CompareTimes(initialMeshMTime, newMeshMTime, expectMeshMTimeUpdate, "cell data");
 
   return result;
 }
 
-int TestCustomImplementation()
+int TestPolyData()
 {
+  vtkLogScopeF(INFO, "Test vtkPolyData");
+
   int result = EXIT_SUCCESS;
 
-  vtkNew<vtkFloatArray> floatArray;
-  floatArray->SetNumberOfValues(1);
-  floatArray->SetValue(0, 0);
+  // Create dummy dataset
+  float point[3] = { 0, 0, 0 };
+  vtkNew<vtkPoints> points;
+  points->SetNumberOfPoints(1);
+  points->SetPoint(0, point);
+
+  vtkNew<vtkPolyData> polydata;
+  polydata->SetPoints(points);
+
+  // Modifying points/lines/verts/polys themselves should.
+  // Modifying field data shouldn't modify MeshMTime;
+  result &= TestFieldDataModification(polydata, false);
+
+  // Modify lines
+  vtkMTimeType initialMeshMTime = polydata->GetMeshMTime();
+  polydata->SetLines(vtkNew<vtkCellArray>());
+  vtkMTimeType newMeshMTime = polydata->GetMeshMTime();
+
+  result &= CompareTimes(initialMeshMTime, newMeshMTime, true, "lines");
+
+  // Modify strips
+  initialMeshMTime = polydata->GetMeshMTime();
+  polydata->SetStrips(vtkNew<vtkCellArray>());
+  newMeshMTime = polydata->GetMeshMTime();
+
+  result &= CompareTimes(initialMeshMTime, newMeshMTime, true, "strips");
+
+  // Modify polys
+  initialMeshMTime = polydata->GetMeshMTime();
+  polydata->SetPolys(vtkNew<vtkCellArray>());
+  newMeshMTime = polydata->GetMeshMTime();
+
+  result &= CompareTimes(initialMeshMTime, newMeshMTime, true, "polys");
+
+  // Modify verts
+  initialMeshMTime = polydata->GetMeshMTime();
+  polydata->SetVerts(vtkNew<vtkCellArray>());
+  newMeshMTime = polydata->GetMeshMTime();
+
+  result &= CompareTimes(initialMeshMTime, newMeshMTime, true, "verts");
+
+  return result;
+}
+
+int TestUnstructuredGrid()
+{
+  vtkLogScopeF(INFO, "Test vtkUnstructuredGrid");
+
+  int result = EXIT_SUCCESS;
 
   float point[3] = { 0, 0, 0 };
   vtkNew<vtkPoints> points;
   points->SetNumberOfPoints(1);
   points->SetPoint(0, point);
 
-  // vtkPolyData implements GetMeshMTime:
   // Modifying field data shouldn't modify MeshMTime;
-  // Modifying points themselves should.
-  vtkNew<vtkPolyData> polydata;
-  polydata->SetPoints(points);
-  polydata->GetPointData()->AddArray(floatArray);
+  // Modifying connectivity should.
+  vtkNew<vtkUnstructuredGrid> grid;
+  grid->SetPoints(points);
 
-  // Modify point data array
-  vtkMTimeType initialMeshMTime = polydata->GetMeshMTime();
-  floatArray->SetValue(0, 1);
-  vtkMTimeType newMeshMTime = polydata->GetMeshMTime();
+  result &= TestFieldDataModification(grid, false);
 
-  if (initialMeshMTime != newMeshMTime)
-  {
-    vtkLog(ERROR,
-      "Modifying point data should not modify MeshMTime. Expected: " << initialMeshMTime
-                                                                     << ", Got: " << newMeshMTime);
-    result &= EXIT_FAILURE;
-  }
+  // Modify connectivity
+  vtkMTimeType initialMeshMTime = grid->GetMeshMTime();
+  grid->SetCells(VTK_TRIANGLE, vtkNew<vtkCellArray>());
+  vtkMTimeType newMeshMTime = grid->GetMeshMTime();
 
-  // Modify points
-  initialMeshMTime = polydata->GetMeshMTime();
-  float newPoint[3] = { 1, 1, 1 };
-  points->SetNumberOfPoints(2);
-  points->SetPoint(1, newPoint);
-  newMeshMTime = polydata->GetMeshMTime();
+  result &= CompareTimes(initialMeshMTime, newMeshMTime, true, "connectivity");
 
-  if (initialMeshMTime == newMeshMTime)
-  {
-    vtkLog(ERROR, "Modifying point data should modify MeshMTime.");
-    result &= EXIT_FAILURE;
-  }
+  return result;
+}
 
+int TestDefaultImplementation(vtkDataSet* noMeshMTimeImplementationDataset)
+{
+  vtkLogScopeF(INFO, "Test default implementation");
+
+  int result = EXIT_SUCCESS;
+  result &= TestFieldDataModification(noMeshMTimeImplementationDataset, true);
   return result;
 }
 }
@@ -92,8 +152,12 @@ int TestMeshMTime(int, char*[])
 {
   int result = EXIT_SUCCESS;
 
-  result &= TestDefaultImplementation();
-  result &= TestCustomImplementation();
+  result &= TestPolyData();
+  result &= TestUnstructuredGrid();
+
+  // Note: Image data uses the default implementation; this might change in the future
+  vtkNew<vtkImageData> imageData;
+  result &= TestDefaultImplementation(imageData);
 
   return result;
 }
