f5b2c039eb412cbbacbdce52402ba1bbef750abb
Léon Victor <leon.victor@kitware.com>
2024-07-09 09:31:25 +0200

    ensight-reader: Expand test to use all  output partitions

--- IO/EnSight/Testing/Cxx/TestEnSightReaderStaticMeshCache.cxx
+++ IO/EnSight/Testing/Cxx/TestEnSightReaderStaticMeshCache.cxx
@@ -1,18 +1,39 @@
 // SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
 // SPDX-License-Identifier: BSD-3-Clause
 
+#include "vtkCompositeDataIterator.h"
+#include "vtkDataSet.h"
 #include "vtkEnSightGoldCombinedReader.h"
 #include "vtkLogger.h"
 #include "vtkNew.h"
 #include "vtkPartitionedDataSetCollection.h"
+#include "vtkSmartPointer.h"
 #include "vtkTestUtilities.h"
 #include "vtkType.h"
-#include "vtkUnstructuredGrid.h"
+
+#include <vector>
+
+namespace
+{
+//------------------------------------------------------------------------------
+void GetPartitionnedMeshMTimes(
+  vtkPartitionedDataSetCollection* pdsc, std::vector<vtkMTimeType>& times)
+{
+  vtkSmartPointer<vtkCompositeDataIterator> iter;
+  iter.TakeReference(pdsc->NewIterator());
+  for (iter->GoToFirstItem(); !iter->IsDoneWithTraversal(); iter->GoToNextItem())
+  {
+    auto ds = vtkDataSet::SafeDownCast(iter->GetCurrentDataObject());
+    vtkMTimeType time = ds->GetMeshMTime();
+    times.push_back(time);
+  }
+}
+}
 
 //------------------------------------------------------------------------------
 int TestEnSightReaderStaticMeshCache(int argc, char* argv[])
 {
-  vtkLog(INFO, "Test default cache");
+  vtkLog(INFO, "Test EnSight Combined Reader static mesh cache");
   bool success = true;
 
   vtkNew<vtkEnSightGoldCombinedReader> enSightReader;
@@ -22,21 +43,26 @@ int TestEnSightReaderStaticMeshCache(int argc, char* argv[])
   enSightReader->UpdateTimeStep(0);
   auto outDO = enSightReader->GetOutputDataObject(0);
   auto outPDSC = vtkPartitionedDataSetCollection::SafeDownCast(outDO);
-  auto outUG = vtkUnstructuredGrid::SafeDownCast(outPDSC->GetPartition(0, 0));
 
-  vtkMTimeType beforeMeshMTime = outUG->GetMeshMTime();
+  std::vector<vtkMTimeType> beforeMeshMTimes;
+  ::GetPartitionnedMeshMTimes(outPDSC, beforeMeshMTimes);
 
   enSightReader->UpdateTimeStep(1);
   outDO = enSightReader->GetOutputDataObject(0);
   outPDSC = vtkPartitionedDataSetCollection::SafeDownCast(outDO);
-  outUG = vtkUnstructuredGrid::SafeDownCast(outPDSC->GetPartition(0, 0));
 
-  vtkMTimeType afterMeshMTime = outUG->GetMeshMTime();
+  std::vector<vtkMTimeType> afterMeshMTimes;
+  ::GetPartitionnedMeshMTimes(outPDSC, afterMeshMTimes);
 
-  if (afterMeshMTime != beforeMeshMTime)
+  for (auto beforeIt = beforeMeshMTimes.begin(), afterIt = afterMeshMTimes.begin();
+       beforeIt != beforeMeshMTimes.end() && afterIt != afterMeshMTimes.end();
+       ++beforeIt, ++afterIt)
   {
-    vtkLog(ERROR, "MeshMTime differs between time steps. Mesh was not properly cached.");
-    success = false;
+    if (*beforeIt != *afterIt)
+    {
+      vtkLog(ERROR, "MeshMTime differs between time steps. Mesh was not properly cached.");
+      success = false;
+    }
   }
 
   return success ? EXIT_SUCCESS : EXIT_FAILURE;
