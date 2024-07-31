a1c80290b4dbbdb526a061af621c38536bfffe9e
Louis Gombert <louis.gombert@kitware.com>
2024-07-31 12:35:44 +0000

    HTGSource: improve testing for 1-level descriptors

--- Filters/Sources/Testing/Cxx/TestHyperTreeGridSourceDistributed.cxx
+++ Filters/Sources/Testing/Cxx/TestHyperTreeGridSourceDistributed.cxx
@@ -8,6 +8,57 @@
 #include "vtkLogger.h"
 #include "vtkMPIController.h"
 
+namespace
+{
+constexpr int NB_PROCS = 3;
+
+template <int NbTrees>
+struct SourceConfig
+{
+  unsigned int Depth;
+  unsigned int BranchFactor;
+  std::array<unsigned int, 3> Dimensions;
+  std::array<double, 3> GridScale;
+  std::string Descriptor;
+  std::array<unsigned char, NbTrees> ExpectedProcess;
+};
+}
+
+template <int NbTrees>
+bool TestSource(const SourceConfig<NbTrees>& config, int myRank, int nbRanks)
+{
+  // Create HTG Source with process selection
+  vtkNew<vtkHyperTreeGridSource> htGrid;
+  htGrid->SetDebug(true);
+  htGrid->SetMaxDepth(config.Depth);
+  htGrid->SetBranchFactor(config.BranchFactor);
+  htGrid->SetDimensions(config.Dimensions.data());
+  htGrid->SetGridScale(config.GridScale.data());
+  htGrid->SetDescriptor(config.Descriptor.c_str());
+  htGrid->UpdatePiece(myRank, nbRanks, 0);
+  auto htg = htGrid->GetHyperTreeGridOutput();
+
+  // Test that the right trees appear on selected process
+  vtkIdType inIndex = 0;
+  vtkHyperTreeGrid::vtkHyperTreeGridIterator it;
+  htg->InitializeTreeIterator(it);
+  int treeIndex = 0;
+  bool success = true;
+  vtkNew<vtkHyperTreeGridNonOrientedCursor> cursor;
+  while (it.GetNextTree(inIndex))
+  {
+    htg->InitializeNonOrientedCursor(cursor, inIndex, true);
+    if ((config.ExpectedProcess[treeIndex++] == myRank) == cursor->IsMasked())
+    {
+      vtkErrorWithObjectMacro(
+        nullptr, "Tree #" << treeIndex - 1 << " does not appear on the right process.");
+      success = false;
+    }
+  }
+
+  return success;
+}
+
 int TestHyperTreeGridSourceDistributed(int argc, char* argv[])
 {
   // Initialize MPI Controller
@@ -18,10 +69,9 @@ int TestHyperTreeGridSourceDistributed(int argc, char* argv[])
   int myRank = controller->GetLocalProcessId();
   int nbRanks = controller->GetNumberOfProcesses();
 
-  constexpr int expectedProcs = 2;
-  if (nbRanks != 2)
+  if (nbRanks != NB_PROCS)
   {
-    vtkErrorWithObjectMacro(nullptr, "Expected " << expectedProcs << " processes, got " << nbRanks);
+    vtkErrorWithObjectMacro(nullptr, "Expected " << NB_PROCS << " processes, got " << nbRanks);
     controller->Finalize();
     return EXIT_FAILURE;
   }
@@ -29,41 +79,32 @@ int TestHyperTreeGridSourceDistributed(int argc, char* argv[])
   std::string threadName = "rank-" + std::to_string(controller->GetLocalProcessId());
   vtkLogger::SetThreadName(threadName);
 
-  // Create HTG Source with process selection
-  vtkNew<vtkHyperTreeGridSource> htGrid;
-  htGrid->SetMaxDepth(6);
-  htGrid->SetDebug(true);
-  htGrid->SetDimensions(3, 4, 1);
-  htGrid->SetGridScale(1.5, 1., 10.);
-  htGrid->SetBranchFactor(2);
-  htGrid->SetDescriptor(
+  SourceConfig<6> source1{ 6, 2, { 3, 4, 1 }, { 1.5, 1., 10. },
     "0RR1RR0R.|.... .R.. RRRR R... R...|.R.. ...R ..RR .R.. R... .... ....|.... "
-    "...R ..R. .... .R.. R...|.... .... .R.. ....|....");
-  htGrid->UpdatePiece(myRank, nbRanks, 0);
-  auto htg = htGrid->GetHyperTreeGridOutput();
+    "...R ..R. .... .R.. R...|.... .... .R.. ....|....",
+    { 0, 0, 1, 1, 0, 0 } };
 
-  // Test that the right trees appear on selected process
-  std::array<std::array<bool, 6>, 2> expectedTreeMasking{
-    std::array<bool, 6>{ false, false, true, true, false, false },
-    std::array<bool, 6>{ true, true, false, false, true, true },
+  SourceConfig<4> source2{
+    1,
+    1,
+    { 3, 3, 1 },
+    { 1.5, 1., 10. },
+    "0..2.1.",
+    { 0, 0, 2, 1 },
   };
 
-  vtkIdType inIndex = 0;
-  vtkHyperTreeGrid::vtkHyperTreeGridIterator it;
-  htg->InitializeTreeIterator(it);
-  int treeIndex = 0;
   bool success = true;
-  vtkNew<vtkHyperTreeGridNonOrientedCursor> cursor;
-  while (it.GetNextTree(inIndex))
-  {
-    htg->InitializeNonOrientedCursor(cursor, inIndex, true);
-    if (expectedTreeMasking[myRank][treeIndex++] != cursor->IsMasked())
-    {
-      vtkErrorWithObjectMacro(
-        nullptr, "Tree #" << treeIndex - 1 << " does not appear on the right process.");
-      success = false;
-    }
-  }
+  success &= ::TestSource(source1, myRank, nbRanks);
+  success &= ::TestSource(source2, myRank, nbRanks);
+
+  // Default to 0, ignore chars at the end
+  source2.Descriptor = "...2.101";
+  source2.ExpectedProcess = { 0, 0, 0, 2 };
+  success &= ::TestSource(source2, myRank, nbRanks);
+
+  source2.Descriptor = ".1.0.2.";
+  source2.ExpectedProcess = { 0, 1, 0, 2 };
+  success &= ::TestSource(source2, myRank, nbRanks);
 
   controller->Finalize();
   return success ? EXIT_SUCCESS : EXIT_FAILURE;
