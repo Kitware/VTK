f208d64cc53b391bfb41ee67f44a106bbaca8d8c
Léon Victor <leon.victor@kitware.com>
2024-08-14 16:11:30 +0200

    Revert incorrect changes to ForceStaticMeshFilter

--- Filters/Temporal/Testing/Cxx/TestForceStaticMesh.cxx
+++ Filters/Temporal/Testing/Cxx/TestForceStaticMesh.cxx
@@ -1,24 +1,36 @@
 // SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
 // SPDX-License-Identifier: BSD-3-Clause
+
 #include "vtkActor.h"
 #include "vtkConeSource.h"
 #include "vtkDataSetMapper.h"
 #include "vtkForceStaticMesh.h"
 #include "vtkGenerateTimeSteps.h"
 #include "vtkGroupDataSetsFilter.h"
-#include "vtkImageData.h"
-#include "vtkInformation.h"
-#include "vtkInformationVector.h"
+#include "vtkLogger.h"
 #include "vtkNew.h"
 #include "vtkObjectFactory.h"
 #include "vtkPartitionedDataSetCollection.h"
-#include "vtkPointData.h"
 #include "vtkRandomAttributeGenerator.h"
 #include "vtkRenderWindow.h"
 #include "vtkRenderWindowInteractor.h"
 #include "vtkRenderer.h"
-#include "vtkStreamingDemandDrivenPipeline.h"
-#include "vtkUnstructuredGrid.h"
+#include "vtkType.h"
+#include <cstdlib>
+
+//------------------------------------------------------------------------------
+void GetPartitionsMeshMTimes(
+  vtkPartitionedDataSetCollection* pdsc, std::vector<vtkMTimeType>& times)
+{
+  vtkSmartPointer<vtkCompositeDataIterator> iter;
+  iter.TakeReference(pdsc->NewIterator());
+  for (iter->GoToFirstItem(); !iter->IsDoneWithTraversal(); iter->GoToNextItem())
+  {
+    auto ds = vtkDataSet::SafeDownCast(iter->GetCurrentDataObject());
+    vtkMTimeType time = ds ? ds->GetMeshMTime() : -1;
+    times.push_back(time);
+  }
+}
 
 //------------------------------------------------------------------------------
 // Program main
@@ -57,21 +69,28 @@ int TestForceStaticMesh(int vtkNotUsed(argc), char* vtkNotUsed(argv)[])
 
   auto outputPDC =
     vtkPartitionedDataSetCollection::SafeDownCast(forceStatic->GetOutputDataObject(0));
-  auto outputPD = vtkPolyData::SafeDownCast(outputPDC->GetPartitionAsDataObject(0, 0));
-  const auto initMeshTime = outputPD->GetMeshMTime();
+
+  std::vector<vtkMTimeType> beforeMeshMTimes;
+  ::GetPartitionsMeshMTimes(outputPDC, beforeMeshMTimes);
 
   forceStatic->UpdateTimeStep(2); // update scalars, not mesh
 
   outputPDC = vtkPartitionedDataSetCollection::SafeDownCast(forceStatic->GetOutputDataObject(0));
-  outputPD = vtkPolyData::SafeDownCast(outputPDC->GetPartitionAsDataObject(0, 0));
-  if (outputPD->GetMeshMTime() == initMeshTime)
+  std::vector<vtkMTimeType> afterMeshMTimes;
+  ::GetPartitionsMeshMTimes(outputPDC, afterMeshMTimes);
+
+  for (auto beforeIt = beforeMeshMTimes.begin(), afterIt = afterMeshMTimes.begin();
+       beforeIt != beforeMeshMTimes.end() && afterIt != afterMeshMTimes.end();
+       ++beforeIt, ++afterIt)
   {
-    std::cout << "Time comparison OK." << std::endl;
+    if (*beforeIt != *afterIt)
+    {
+      vtkLog(ERROR, "GetMeshMTime has changed, mesh not static !");
+      return EXIT_FAILURE;
+    }
   }
-  else
-  {
-    std::cerr << "ERROR: GetMeshMTime has changed, mesh not static !" << std::endl;
-  }
+
+  auto outputPD = vtkPolyData::SafeDownCast(outputPDC->GetPartitionAsDataObject(0, 0));
 
   vtkNew<vtkDataSetMapper> mapper;
   mapper->SetInputData(outputPD);
