7ad26cf26413d67021179d26d7f4b741ff7f573c
Timothée Couble <timothee.couble@kitware.com>
2024-08-22 17:04:06 +0200

    Deprecate vtkVectorOperators.h use vtkVector.h instead

--- Common/DataModel/vtkVectorOperators.h
+++ Common/DataModel/vtkVectorOperators.h
@@ -4,11 +4,20 @@
 #ifndef vtkVectorOperators_h
 #define vtkVectorOperators_h
 
-// This set of operators enhance the vtkVector classes, allowing various
-// operator overloads one might expect.
+// This file used to set operators that enhance the vtkVector classes.
+// We reimplemented them directly in vtkVector.h, and this file is now only kept
+// for deprecation purpose and should be removed once we hit the deprecation milestone.
+#include "vtkDeprecation.h" // For VTK_DEPRECATED_IN_9_4_0
 #include "vtkVector.h"
 
 VTK_ABI_NAMESPACE_BEGIN
+
+namespace
+{
+VTK_DEPRECATED_IN_9_4_0("vtkVectorOperators.h is deprecated use vtkVector.h instead")
+constexpr int vtkVectorOperators_h_is_deprecated = 0;
+constexpr int please_dont_use_vtkVectorOperators_h = vtkVectorOperators_h_is_deprecated;
+};
 
 VTK_ABI_NAMESPACE_END
 #endif
