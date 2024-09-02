1bbc13f058f692aa3c0186f944a1e491186711ec
Xinhua Zhang <sean.xinhua.zhang@gmail.com>
2024-09-02 15:54:35 -0400

    Added missing {}

--- Filters/FlowPaths/vtkVectorFieldTopology.cxx
+++ Filters/FlowPaths/vtkVectorFieldTopology.cxx
@@ -2149,7 +2149,9 @@ int vtkVectorFieldTopology::RequestData(vtkInformation* vtkNotUsed(request),
         }
 
         if (!appendFlag)
+        {
           continue;
+        }
 
         criticalPoints->GetPoints()->InsertNextPoint(
           criticalPointsTemporary->GetPoints()->GetPoint(pointId));
