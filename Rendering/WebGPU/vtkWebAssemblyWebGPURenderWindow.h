8643fe81254ea25c0efc76871439c00612c47a40
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2024-09-23 11:28:35 -0400

    Do not set window title for wasm render windows
    - typically, wasm rendering applications already set the title
      in JS or the HTML document.
    - Previously, VTK used to override the user setting. This commit
      prevents that overriding behavior.

--- Rendering/WebGPU/vtkWebAssemblyWebGPURenderWindow.h
+++ Rendering/WebGPU/vtkWebAssemblyWebGPURenderWindow.h
@@ -71,12 +71,6 @@ public:
    */
   int* GetPosition() VTK_SIZEHINT(2) override;
 
-  /**
-   * Set the name of the window. This appears at the top of the window
-   * normally.
-   */
-  void SetWindowName(const char*) override;
-
   void* GetGenericWindowId() override { return (void*)this->WindowId; }
   void* GetGenericDrawable() override { return (void*)this->WindowId; }
 
