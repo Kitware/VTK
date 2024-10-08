6bedf4c2c78975368f989d560f2ea094f4d00534
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2024-10-08 16:33:34 -0400

    Validate ClassName and SuperClassName only when required in deserializer

--- Serialization/Manager/Testing/Python/TestUpdateObjectFromState.py
+++ Serialization/Manager/Testing/Python/TestUpdateObjectFromState.py
@@ -56,5 +56,30 @@ class TestUpdateObjectFromState(vtkTesting.vtkTest):
         self.assertTupleEqual(camera.GetPosition(), (0, 0.2, 1.3))
         self.assertEqual(camera.GetViewAngle(), 60)
 
+    def test3(self):
+        from vtkmodules.vtkSerializationManager import vtkObjectManager
+        from vtkmodules.vtkRenderingCore import vtkCamera
+        import json
+
+        # Scenario 3:
+        # An object is registered and serialized. Later, a new state is created with modified properties.
+        # Unlike Scenario 2, this new state does not have keys that were originally part of the state like
+        # "ClassName", "SuperClassNames" and "vtk-object-manager-kept-alive"
+        # Call UpdateObjectFromState() with modified state. Verify the real object's properties match
+        # with the state's properties.
+
+        manager = vtkObjectManager()
+        manager.Initialize()
+
+        camera = vtkCamera()
+        cid = manager.RegisterObject(camera)
+        manager.UpdateStatesFromObjects()
+
+        manager.UpdateObjectFromState(json.dumps({"Id": cid, "Position": (0, 0.2, 1.3)}))
+        self.assertTupleEqual(camera.GetPosition(), (0, 0.2, 1.3))
+
+        manager.UpdateObjectFromState(json.dumps({"Id": cid, "ViewAngle": 60}))
+        self.assertEqual(camera.GetViewAngle(), 60)
+
 if __name__ == "__main__":
     vtkTesting.main([(TestUpdateObjectFromState, 'test')])
