56bd2d7c21fda98638759d6360e1a1dc986f7cae
Ben Boeckel <ben.boeckel@kitware.com>
2024-10-09 22:48:32 -0400

    vtkVersion: avoid merge conflicts when branching releases

--- CMake/vtkVersion.cmake
+++ CMake/vtkVersion.cmake
@@ -1,6 +1,11 @@
 # VTK version number components.
 set(VTK_MAJOR_VERSION 9)
 set(VTK_MINOR_VERSION 4)
+
+# Git conflict avoidance barrier. When branching for an `X.Y.0.rc1` branch, if
+# the date component is updated while it is in progress, the bump of the minor
+# version ends up conflicting with the patch version update.
+
 set(VTK_BUILD_VERSION 20241010)
 
 if (NOT VTK_MINOR_VERSION LESS 100)
