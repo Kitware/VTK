cb17cc59b18623b7f035476ff091c6f22d7007c3
Christos Tsolakis <christos.tsolakis@kitware.com>
2024-11-15 10:47:29 -0500

    ci: Use python3.13 in windows ci jobs

--- .gitlab/ci/download_python.cmake
+++ .gitlab/ci/download_python.cmake
@@ -2,10 +2,10 @@ cmake_minimum_required(VERSION 3.12)
 
 set(python_url_root "https://www.paraview.org/files/dependencies/python-for-wheels")
 
-set(python_version "3.11.4")
+set(python_version "3.13.0")
 if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "windows")
   set(python_subdir "python-${python_version}-windows-x86_64")
-  set(sha256sum "699df2d656c7227c3ba93d640255cd875e3d92e1a475f5c59408c6125515165f")
+  set(sha256sum "b733a8c7d8d30aa5d0742c00de419294ec385797586672076c337885a440d701")
 else ()
   message(FATAL_ERROR
     "Unknown platform for Python")
