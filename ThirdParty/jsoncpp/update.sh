f24e675882ee1ea24c85f2fd8def99ffcb8a6d27
Cory Quammen <cory.quammen@kitware.com>
2024-12-03 18:16:45 -0500

    jsoncpp: update to 1.9.6

--- ThirdParty/jsoncpp/update.sh
+++ ThirdParty/jsoncpp/update.sh
@@ -8,7 +8,7 @@ readonly name="jsoncpp"
 readonly ownership="JsonCpp Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/jsoncpp.git"
-readonly tag="for/vtk-20231030-1.9.4"
+readonly tag="for/vtk-20241206-1.9.6"
 readonly paths="
 .gitattributes
 CMakeLists.vtk.txt
