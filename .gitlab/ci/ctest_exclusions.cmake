7b53f2f42ec248731a1898d4218e8f87a04fc40a
Ben Boeckel <ben.boeckel@kitware.com>
2024-12-08 08:58:19 +0100

    ci: exclude tests with macOS M4 geometry shader issues

--- .gitlab/ci/ctest_exclusions.cmake
+++ .gitlab/ci/ctest_exclusions.cmake
@@ -359,6 +359,15 @@ if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "macos_arm64")
     "^VTK::RenderingAnnotationCxx-TestCubeAxesWithYLines$")
 endif ()
 
+if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "macos")
+  list(APPEND test_exclusions
+    # geometry shader issues (observed on M4 hardware)
+    # https://gitlab.kitware.com/vtk/vtk/-/issues/19555
+    "^VTK::IOIOSSCxx-TestIOSSApplyDisplacementsCGNS$"
+    "^VTK::IOADIOS2Cxx-TestADIOS2BPReaderSingleTimeStep$"
+    "^VTK::CommonDataModelPython-TestClipPolyhedra$")
+endif ()
+
 if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "wheel_macos" AND
     "$ENV{CMAKE_CONFIGURATION}" MATCHES "arm64")
   list(APPEND test_exclusions
