9276de1b21fa9b60113f6c179605f3edd3a781e2
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2025-01-09 08:45:35 -0500

    Fix tidy bugprone-too-small-loop-variable

--- Rendering/FreeType/vtkFreeTypeTools.cxx
+++ Rendering/FreeType/vtkFreeTypeTools.cxx
@@ -2005,10 +2005,10 @@ void vtkFreeTypeTools::OutlineToPath(int x, int y, FT_Outline* outline, vtkPath*
 
   if (outline->n_points > 0)
   {
-    short point = 0;
-    for (short contour = 0; contour < outline->n_contours; ++contour)
+    unsigned short point = 0;
+    for (unsigned short contour = 0; contour < outline->n_contours; ++contour)
     {
-      short contourEnd = outline->contours[contour];
+      const auto contourEnd = outline->contours[contour];
       controlType lastTag = FIRST_POINT;
       double contourStartVec[2];
       contourStartVec[0] = contourStartVec[1] = 0.0;
