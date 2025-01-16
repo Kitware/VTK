3dc003b342bd4a8fe061f14138d99dee6f666c8a
Cory Quammen <cory.quammen@kitware.com>
2025-01-15 21:10:54 -0500

    ci: update el7-mindeps to install devtoolset-8
    Install gcc 8 as the new minimum dependency.
    
    Because mirrorlist.centos.org no longer exists, (el7 is EOL), also had
    to modify the repos to not try to access that site by commenting out
    the mirrorlist line and uncommenting the baseurl line.

--- .gitlab/ci/docker/el7-mindeps/install_deps.sh
+++ .gitlab/ci/docker/el7-mindeps/install_deps.sh
@@ -2,13 +2,39 @@
 
 set -e
 
+# mirrorlist.centos.org no longer exists because el7 is past end of life.
+# To get packages, replace mirrorlist with baseurl and change mirror.centos.org
+# to vault.centos.org.
+sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/CentOS-*.repo
+sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/CentOS-*.repo
+sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/CentOS-*.repo
+
 # Install extra dependencies for VTK
 yum install -y --setopt=install_weak_deps=False \
     bzip2 patch git-core
 
+# To install devtools-8 and Python 3.8 via scl
+yum install -y --setopt=install_weak_deps=False \
+    centos-release-scl
+
+# Clean up additional repos configured by centos-release-scl
+sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/CentOS-*.repo
+sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/CentOS-*.repo
+sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/CentOS-*.repo
+
 # Core build tools
 yum install -y --setopt=install_weak_deps=False \
-    gcc gcc-c++
+    devtoolset-8
+
+yum install -y --setopt=install_weak_deps=False \
+    rh-python38-python rh-python38-python-devel \
+    rh-python38-python-pip rh-python38-python-numpy
+
+# Install a venv which provides `matplotlib`
+scl enable rh-python38 -- python3 -m venv /opt/python38/venv
+/opt/python38/venv/bin/pip install matplotlib
+# Ignore mpi4py for now as the CI job doesn't build with MPI anyways.
+# scl enable rh-python38 -- /opt/python38/venv/bin/pip install mpi4py
 
 # MPI dependencies
 yum install -y --setopt=install_weak_deps=False \
@@ -28,20 +54,6 @@ yum install -y --setopt=install_weak_deps=False \
     libogg-devel libtheora-devel protobuf-devel libxkbcommon boost-devel \
     tbb-devel libiodbc-devel libarchive-devel freeglut-devel sqlite-devel \
     fontconfig-devel
-
-# Python 3.8 via scl
-yum install -y --setopt=install_weak_deps=False \
-    centos-release-scl
-
-yum install -y --setopt=install_weak_deps=False \
-    rh-python38-python rh-python38-python-devel \
-    rh-python38-python-pip rh-python38-python-numpy
-
-# Install a venv which provides `matplotlib`
-scl enable rh-python38 -- python3 -m venv /opt/python38/venv
-/opt/python38/venv/bin/pip install matplotlib
-# Ignore mpi4py for now as the CI job doesn't build with MPI anyways.
-# scl enable rh-python38 -- /opt/python38/venv/bin/pip install mpi4py
 
 # EPEL for more tools
 yum install -y --setopt=install_weak_deps=False \
