eaf1113311549a0d54cf1b8e4b5ad9fa3d34a102
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2025-01-28 09:15:19 -0500

    ci: bump emsdk to 4.0.3 using cmake script

---
+++ .gitlab/ci/download_emsdk.cmake
@@ -0,0 +1,45 @@
+cmake_minimum_required(VERSION 3.12)
+
+# Input variables.
+set(emsdk_version "4.0.3")
+set(emsdk_ext "zip")
+set(emsdk_hash "0a406a89b2b8cebcdca3ca9da16c27266b3761bf6e1f251140b73758f690aef6")
+set(emsdk_suffix "${emsdk_version}.${emsdk_ext}")
+
+set(emsdk_url "https://github.com/emscripten-core/emsdk/archive/refs/tags")
+set(emsdk_file "emsdk-${emsdk_suffix}")
+
+# Download the file.
+file(DOWNLOAD
+  "${emsdk_url}/${emsdk_suffix}"
+  ".gitlab/${emsdk_file}"
+  STATUS download_status
+  EXPECTED_HASH "SHA256=${emsdk_hash}")
+
+  # Check the download status.
+list(GET download_status 0 res)
+if (res)
+  list(GET download_status 1 err)
+  message(FATAL_ERROR
+    "Failed to download ${emsdk_file}: ${err}")
+endif ()
+
+# Extract the file.
+execute_process(
+  COMMAND
+    "${CMAKE_COMMAND}"
+    -E tar
+    xf "${emsdk_file}"
+  WORKING_DIRECTORY ".gitlab"
+  RESULT_VARIABLE res
+  ERROR_VARIABLE err
+  ERROR_STRIP_TRAILING_WHITESPACE)
+if (res)
+  message(FATAL_ERROR
+    "Failed to extract ${emsdk_file}: ${err}")
+endif ()
+
+# Move to a predictable prefix.
+file(RENAME
+  ".gitlab/emsdk-${emsdk_version}"
+  ".gitlab/emsdk")
