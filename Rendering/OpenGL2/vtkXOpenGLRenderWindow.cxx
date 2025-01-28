89ba57622c3b74a3a17c04913cdbb63bef1116fe
Ben Boeckel <ben.boeckel@kitware.com>
2025-01-28 18:46:51 +0100

    vtkXOpenGLRenderWindow: remove dead code

--- Rendering/OpenGL2/vtkXOpenGLRenderWindow.cxx
+++ Rendering/OpenGL2/vtkXOpenGLRenderWindow.cxx
@@ -116,8 +116,6 @@ vtkXOpenGLRenderWindowInternal::vtkXOpenGLRenderWindowInternal(vtkRenderWindow*)
 
 vtkStandardNewMacro(vtkXOpenGLRenderWindow);
 
-#define MAX_LIGHTS 8
-
 GLXFBConfig vtkXOpenGLRenderWindowTryForFBConfig(Display* DisplayId, int drawable_type,
   vtkTypeBool doublebuff, vtkTypeBool stereo, vtkTypeBool stencil, bool srgb)
 {
@@ -184,24 +182,6 @@ GLXFBConfig vtkXOpenGLRenderWindowTryForFBConfig(Display* DisplayId, int drawabl
   // cout << "            STATUS : FAILURE!!!" << endl;
   return None;
 }
-
-// dead code?
-#if 0
-XVisualInfo *vtkXOpenGLRenderWindowTryForVisual(Display *DisplayId,
-                                                vtkTypeBool doublebuff,
-                                                vtkTypeBool stereo,
-                                                int stencil, bool srgb)
-{
-  GLXFBConfig fbc = vtkXOpenGLRenderWindowTryForFBConfig(DisplayId,
-       GLX_WINDOW_BIT,
-       doublebuff, stereo,
-       stencil, srgb);
-
-  XVisualInfo *v = glXGetVisualFromFBConfig( DisplayId, fbc);
-
-  return v;
-}
-#endif
 
 GLXFBConfig vtkXOpenGLRenderWindowGetDesiredFBConfig(Display* DisplayId, vtkTypeBool& win_stereo,
   vtkTypeBool& win_doublebuffer, int drawable_type, vtkTypeBool& stencil, bool srgb)
