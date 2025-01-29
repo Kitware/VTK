bca44ed66397d0f057c4fbcfe7c41fae2f711a57
Cory Quammen <cory.quammen@kitware.com>
2025-01-29 14:37:11 -0500

    vtkTableBasedClipCases.cxx: remove unneeded implementation file
    This file only instantiated several constexpr static data
    members. Since that was removed, this implementation file does nothing
    and has been removed.

--- Filters/General/vtkTableBasedClipCases.cxx
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-// SPDX-FileCopyrightText: Copyright (c) 2000 - 2009, Lawrence Livermore National Security, LLC
-// SPDX-License-Identifier: BSD-3-Clause
-
-#include "vtkTableBasedClipCases.h"
