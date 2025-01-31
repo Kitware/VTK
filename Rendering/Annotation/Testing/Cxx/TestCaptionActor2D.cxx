bb3be5b050c58b8f0bdcc34698c661044676a3c1
Bernhard Froehler <bernhard@bfroehler.info>
2025-01-31 23:30:20 +0100

    Update CaptionActor2D test case

--- Rendering/Annotation/Testing/Cxx/TestCaptionActor2D.cxx
+++ Rendering/Annotation/Testing/Cxx/TestCaptionActor2D.cxx
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
 // SPDX-License-Identifier: BSD-3-Clause
 
+#include <vtkArrowSource.h>
 #include <vtkCaptionActor2D.h>
 #include <vtkNew.h>
 #include <vtkRenderWindow.h>
@@ -16,7 +17,17 @@ int TestCaptionActor2D(int, char*[])
   captionActor->SetAttachmentPoint(0, 0, 0);
   captionActor->SetCaption("(2) 2.27");
   captionActor->BorderOff();
-  captionActor->LeaderOff();
+
+  vtkNew<vtkArrowSource> leaderGlyphSource;
+  leaderGlyphSource->SetShaftRadius(0.2);
+  leaderGlyphSource->SetTipRadius(0.5);
+  leaderGlyphSource->SetTipLength(0.6);
+  leaderGlyphSource->Update();
+
+  captionActor->SetLeaderGlyphConnection(leaderGlyphSource->GetOutputPort());
+  captionActor->SetLeaderGlyphSize(0.05);
+  captionActor->SetMaximumLeaderGlyphSize(30.0);
+
   captionActor->SetPadding(0);
   captionActor->GetCaptionTextProperty()->SetJustificationToLeft();
   captionActor->GetCaptionTextProperty()->ShadowOff();
@@ -24,6 +35,7 @@ int TestCaptionActor2D(int, char*[])
   captionActor->GetCaptionTextProperty()->SetFontFamilyToCourier();
   captionActor->GetCaptionTextProperty()->SetFontSize(24);
   captionActor->GetTextActor()->SetTextScaleModeToNone();
+  captionActor->SetPosition(0.0, 50.0);
 
   vtkNew<vtkRenderer> renderer;
   renderer->SetBackground(0, 0, 0);
