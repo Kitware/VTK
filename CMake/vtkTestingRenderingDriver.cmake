f72780b127ff93ac7eeaabbb36d41d4aceb906a6
Ben Boeckel <ben.boeckel@kitware.com>
2025-02-02 08:54:21 +0100

    vtkTestingRenderingDriver: fix indentation of template code

--- CMake/vtkTestingRenderingDriver.cmake
+++ CMake/vtkTestingRenderingDriver.cmake
@@ -18,38 +18,38 @@ SET(CMAKE_TESTDRIVER_BEFORE_TESTMAIN
 
     int interactive = 0;
     for (int ii = 0; ii < ac; ++ii)
-      {
+    {
       if (strcmp(av[ii], \"-I\") == 0)
-        {
+      {
         interactive = 1;
         continue;
-        }
+      }
       if (ii < ac-1 && strcmp(av[ii], \"-V\") == 0)
-        {
+      {
         vtkTestingInteractor::ValidBaseline = std::string(av[++ii]);
         continue;
-        }
+      }
       if (ii < ac-1 && strcmp(av[ii], \"-T\") == 0)
-        {
+      {
         vtkTestingInteractor::TempDirectory = std::string(av[++ii]);
         continue;
-        }
+      }
       if (ii < ac-1 && strcmp(av[ii], \"-D\") == 0)
-        {
+      {
         vtkTestingInteractor::DataDirectory = std::string(av[++ii]);
         continue;
-        }
+      }
       if (ii < ac-1 && strcmp(av[ii], \"-E\") == 0)
-        {
+      {
         vtkTestingInteractor::ErrorThreshold = atof(av[++ii]);
         continue;
-        }
+      }
       if (ii < ac-1 && strcmp(av[ii], \"-v\") == 0)
-        {
+      {
         vtkLogger::SetStderrVerbosity(static_cast<vtkLogger::Verbosity>(atoi(av[++ii])));
         continue;
-        }
       }
+    }
 
     // init logging
     vtkLogger::Init(ac, av, nullptr);
@@ -59,44 +59,44 @@ SET(CMAKE_TESTDRIVER_BEFORE_TESTMAIN
 
     vtkSmartPointer<vtkTestingObjectFactory> factory = vtkSmartPointer<vtkTestingObjectFactory>::New();
     if (!interactive)
-      {
+    {
       // Disable any other overrides before registering our factory.
       vtkObjectFactoryCollection *collection = vtkObjectFactory::GetRegisteredFactories();
       collection->InitTraversal();
       vtkObjectFactory *f = collection->GetNextItem();
       while (f)
-        {
+      {
         f->Disable(\"vtkRenderWindowInteractor\");
         f = collection->GetNextItem();
-        }
-      vtkObjectFactory::RegisterFactory(factory);
       }
+      vtkObjectFactory::RegisterFactory(factory);
+    }
 "
 )
 
 SET(CMAKE_TESTDRIVER_AFTER_TESTMAIN
 "
-   if (result == VTK_SKIP_RETURN_CODE)
-     {
-     printf(\"Unsupported runtime configuration: Test returned \"
-            \"VTK_SKIP_RETURN_CODE. Skipping test.\\n\");
-     return result;
-     }
+  if (result == VTK_SKIP_RETURN_CODE)
+  {
+    printf(\"Unsupported runtime configuration: Test returned \"
+           \"VTK_SKIP_RETURN_CODE. Skipping test.\\n\");
+    return result;
+  }
 
-   if (!interactive)
-     {
-     if (vtkTestingInteractor::TestReturnStatus != -1)
-        {
-        if (vtkTestingInteractor::TestReturnStatus != vtkTesting::PASSED)
-          {
-          result = EXIT_FAILURE;
-          }
-        else
-          {
-          result = EXIT_SUCCESS;
-          }
-        }
-      vtkObjectFactory::UnRegisterFactory(factory);
+  if (!interactive)
+  {
+    if (vtkTestingInteractor::TestReturnStatus != -1)
+    {
+      if (vtkTestingInteractor::TestReturnStatus != vtkTesting::PASSED)
+      {
+        result = EXIT_FAILURE;
       }
+      else
+      {
+        result = EXIT_SUCCESS;
+      }
+    }
+    vtkObjectFactory::UnRegisterFactory(factory);
+  }
 "
 )
