daf33bd19cb9c953fd27630284999af8ef743b54
Ben Boeckel <ben.boeckel@kitware.com>
2025-02-20 00:30:26 +0100

    ci: download a newer `ninja` for Mesa builds
    The wheel containers have shifted and they no longer provide a newer
    `ninja` package.

--- .gitlab/ci/docker/python-osmesa/install_deps.sh
+++ .gitlab/ci/docker/python-osmesa/install_deps.sh
@@ -4,6 +4,38 @@ set -e
 
 # Install extra dependencies for OSMesa builds
 yum install -y --setopt=install_weak_deps=False \
-    meson ninja-build flex
+    meson flex
 
 yum clean all
+
+readonly version="1.12.1"
+
+case "$( uname -m )" in
+    x86_64)
+        sha256sum="6f98805688d19672bd699fbbfa2c2cf0fc054ac3df1f0e6a47664d963d530255"
+        platform="linux"
+        ;;
+    aarch64)
+        sha256sum="5c25c6570b0155e95fce5918cb95f1ad9870df5768653afe128db822301a05a1"
+        platform="linux-aarch64"
+        ;;
+    *)
+        echo "Unrecognized architecture $( uname -m )"
+        exit 1
+        ;;
+esac
+readonly sha256sum
+readonly platform
+
+readonly filename="ninja-$platform"
+readonly tarball="$filename.zip"
+
+echo "$sha256sum  $tarball" > ninja.sha256sum
+curl -OL "https://github.com/ninja-build/ninja/releases/download/v$version/$tarball"
+sha256sum --check ninja.sha256sum
+python3 -c "
+import zipfile
+with zipfile.ZipFile('$tarball', 'r') as zip_ref:
+    zip_ref.extractall('/usr/local/bin')
+"
+chmod +x /usr/local/bin/ninja
