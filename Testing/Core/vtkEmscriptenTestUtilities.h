5ee9930be3679e4caf4befb6a76bfcf26e81d895
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2025-02-26 13:45:40 -0500

    Post exit code from unit tests when asyncify=on
    - with asyncify, the python server always got an exit code of `1`
      because `main` implicitly returned 1.
    - as a result, exit code is specially handled so that `ctest` can decide whether the test failed or passed.

--- Testing/Core/vtkEmscriptenTestUtilities.h
+++ Testing/Core/vtkEmscriptenTestUtilities.h
@@ -10,6 +10,8 @@
 #include <cstdint>                // for uint8_t
 #include <string>                 // for string
 
+#include <emscripten/emscripten.h> // for emscripten_ API
+
 VTK_ABI_NAMESPACE_BEGIN
 
 class VTKTESTINGCORE_EXPORT vtkEmscriptenTestUtilities
@@ -24,12 +26,16 @@ public:
     const std::string& hostFilePath, const std::string& sandboxedFilePath);
   static std::string PreloadDataFile(const std::string& hostFilePath);
   static void DumpFile(const std::string& hostFilePath, const void* data, std::size_t n);
+  static void PostExitCode(int code);
 };
 
+// These functions are implemented externally in javascript.
+// The static methods of vtkEmscriptenTestUtilities call into these methods.
 extern "C"
 {
   void* vtkPreloadDataFileIntoMemory(const char* hostFilePath);
   void vtkDumpFile(const char* hostFilePath, const uint8_t* data, size_t nbytes);
+  void vtkPostExitCode(int code);
 }
 
 VTK_ABI_NAMESPACE_END
