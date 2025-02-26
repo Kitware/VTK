aaeffce2d2655b437a87964cf9c3f3b1cdd8c4ee
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2025-02-26 13:33:18 -0500

    Support std::string_view in vtkOStreamWrapper

--- Common/Core/vtkOStreamWrapper.cxx
+++ Common/Core/vtkOStreamWrapper.cxx
@@ -82,6 +82,13 @@ vtkOStreamWrapper& vtkOStreamWrapper::operator<<(std_string const& s)
 }
 
 //------------------------------------------------------------------------------
+vtkOStreamWrapper& vtkOStreamWrapper::operator<<(std_string_view const& s)
+{
+  this->ostr << reinterpret_cast<std::string_view const&>(s);
+  return *this;
+}
+
+//------------------------------------------------------------------------------
 #if defined(__IBMCPP__)
 vtkOStreamWrapper& vtkOStreamWrapper::WriteInternal(const char* a)
 {
