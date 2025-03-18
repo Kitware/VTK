dfe4bb0bc356ce6fb5cddd1af7679af9b5c04fa2
Cory Quammen <cory.quammen@kitware.com>
2025-03-17 22:05:46 -0400

    libharu: update from v2.4.4 to v2.4.5

--- ThirdParty/libharu/update.sh
+++ ThirdParty/libharu/update.sh
@@ -8,7 +8,7 @@ readonly name="libharu"
 readonly ownership="Libharu Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/libharu.git"
-readonly tag="for/vtk-20241208-2.4.4"
+readonly tag="for/vtk-20250317-2.4.5"
 readonly paths="
 .gitattributes
 CMakeLists.txt
