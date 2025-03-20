3d80303349ed16754883146d67c1eed349a89d0f
Quinn Powell <quinn.powell@kitware.com>
2025-03-20 10:08:18 -0400

    WrapSerDes: code generation for std::map<string, vtkObject *> properties

--- Wrapping/Tools/vtkWrap.h
+++ Wrapping/Tools/vtkWrap.h
@@ -42,6 +42,7 @@ extern "C"
   VTKWRAPPINGTOOLS_EXPORT int vtkWrap_IsZeroCopyPointer(const ValueInfo* val);
   VTKWRAPPINGTOOLS_EXPORT int vtkWrap_IsArrayRef(const ValueInfo* val);
   VTKWRAPPINGTOOLS_EXPORT int vtkWrap_IsStdVector(const ValueInfo* val);
+  VTKWRAPPINGTOOLS_EXPORT int vtkWrap_IsStdMap(const ValueInfo* val);
   VTKWRAPPINGTOOLS_EXPORT int vtkWrap_IsVTKObject(const ValueInfo* val);
   VTKWRAPPINGTOOLS_EXPORT int vtkWrap_IsVTKSmartPointer(const ValueInfo* val);
   VTKWRAPPINGTOOLS_EXPORT int vtkWrap_IsSpecialObject(const ValueInfo* val);
