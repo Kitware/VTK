7cc48099d489bbf17eb481bb7bc5e76d13558c74
Spiros Tsalikis <spiros.tsalikis@kitware.com>
2025-04-01 13:18:22 -0400

    scn: Add third-party

--- .gitlab/ci/configure_fedora39_x86_64_mpi_python_qt_tbb_mangling.cmake
+++ .gitlab/ci/configure_fedora39_x86_64_mpi_python_qt_tbb_mangling.cmake
@@ -3,14 +3,14 @@
 # too.  In which case we would want to turn this OFF.
 set(VTK_USE_EXTERNAL ON CACHE STRING "")
 
-# This tells VTK to use its internal vendored ThirdParty/ libraries in the build
-# rather than what may or may not be available on the system.
-set(VTK_MODULE_USE_EXTERNAL_VTK_libharu OFF CACHE BOOL "")
+# These libraries are not supported right now.
 set(VTK_MODULE_USE_EXTERNAL_VTK_exprtk OFF CACHE BOOL "")
 set(VTK_MODULE_USE_EXTERNAL_VTK_ioss OFF CACHE BOOL "")
-set(VTK_MODULE_USE_EXTERNAL_VTK_fmt OFF CACHE BOOL "")
 set(VTK_MODULE_USE_EXTERNAL_VTK_fast_float OFF CACHE BOOL "")
-set(VTK_MODULE_USE_EXTERNAL_VTK_verdict OFF CACHE BOOL "")
+set(VTK_MODULE_USE_EXTERNAL_VTK_fmt OFF CACHE BOOL "")
+set(VTK_MODULE_USE_EXTERNAL_VTK_libharu OFF CACHE BOOL "")
+set(VTK_MODULE_USE_EXTERNAL_VTK_scn OFF CACHE BOOL "")
 set(VTK_MODULE_USE_EXTERNAL_VTK_token OFF CACHE BOOL "")
+set(VTK_MODULE_USE_EXTERNAL_VTK_verdict OFF CACHE BOOL "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora39.cmake")
---
+++ ThirdParty/scn/update.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+shopt -s dotglob
+
+readonly name="scn"
+readonly ownership="scnlib Upstream <kwrobot@kitware.com>"
+readonly subtree="ThirdParty/$name/vtk$name"
+readonly repo="https://gitlab.kitware.com/third-party/scnlib.git"
+readonly tag="for/vtk-20250403-4.0.1"
+readonly paths="
+.gitattributes
+include/scn/*.h
+src/scn/*.h
+src/scn/*.cpp
+CMakeLists.vtk.txt
+LICENSE
+LICENSE.nanorange
+README.kitware.md
+README.md
+"
+
+extract_source () {
+    git_archive
+    pushd "$extractdir/$name-reduced"
+    mv -v include/scn include/vtkscn
+    mv -v src/scn src/vtkscn
+    mv CMakeLists.vtk.txt CMakeLists.txt
+    popd
+}
+
+. "${BASH_SOURCE%/*}/../update-common.sh"
