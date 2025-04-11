f15003731cbe7ee6ec677eef99580ac23693b92f
Spiros Tsalikis <spiros.tsalikis@kitware.com>
2025-04-11 12:51:11 -0400

    Expand TestSCN for std::FILE

--- Common/Core/Testing/Cxx/TestSCN.cxx
+++ Common/Core/Testing/Cxx/TestSCN.cxx
@@ -9,6 +9,8 @@
 // clang-format on
 
 #include <cassert>
+#include <cstdio>
+#include <fstream>
 #include <iostream>
 
 int TestSCN(int, char*[])
@@ -23,7 +25,35 @@ int TestSCN(int, char*[])
   }
   else
   {
-    std::cout << "Couldn't parse a word: " << result.error().msg() << std::endl;
+    std::cerr << "Couldn't parse a word: " << result.error().msg() << std::endl;
   }
-  return 0;
+
+  // dump hello world to a file
+  std::ofstream fout;
+  fout.open("test.txt");
+  fout << "Hello world";
+  fout.close();
+
+  // create std::FILE* from a file
+  std::FILE* file = std::fopen("test.txt", "r");
+  if (!file)
+  {
+    std::cerr << "Failed to open file" << std::endl;
+    return EXIT_FAILURE;
+  }
+  if (auto resultIO = scn::scan<std::string, std::string>(file, "{:s} {:s}"))
+  {
+    auto [hello, world] = resultIO->values();
+    std::cout << hello << " " << world << std::endl;
+  }
+  else
+  {
+    std::cerr << "Couldn't parse a word: " << resultIO.error().msg() << std::endl;
+  }
+  // close the file
+  std::fclose(file);
+  // delete the file
+  std::remove("test.txt");
+
+  return EXIT_SUCCESS;
 }
