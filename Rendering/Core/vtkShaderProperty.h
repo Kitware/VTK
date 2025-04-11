ee8eacf30e76b441f53c54687831e5114c104ba3
Quinn Powell <quinn.powell@kitware.com>
2025-04-11 14:31:13 -0400

    Marshalling: manual serialization for vtkShaderProperty

--- Rendering/Core/vtkShaderProperty.h
+++ Rendering/Core/vtkShaderProperty.h
@@ -21,12 +21,12 @@
 #include "vtkNew.h" // For iVars
 #include "vtkObject.h"
 #include "vtkRenderingCoreModule.h" // For export macro
-#include "vtkWrappingHints.h"       // For VTK_MARSHALAUTO
+#include "vtkWrappingHints.h"       // For VTK_MARSHALMANUAL
 
 VTK_ABI_NAMESPACE_BEGIN
 class vtkUniforms;
 
-class VTKRENDERINGCORE_EXPORT VTK_MARSHALAUTO vtkShaderProperty : public vtkObject
+class VTKRENDERINGCORE_EXPORT VTK_MARSHALMANUAL vtkShaderProperty : public vtkObject
 {
 public:
   vtkTypeMacro(vtkShaderProperty, vtkObject);
