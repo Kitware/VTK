91e2ce96c509a9590198c63e731c9ea8d8b93027
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2025-04-30 14:10:47 -0400

    ci: enable wasm64 serialization package and upload job
    - closes vtk/vtk#19617
    - instead of node, use sed to read version from CMake/vtkVersion.cmake

--- .gitlab/ci/upload_webassembly_archive.sh
+++ .gitlab/ci/upload_webassembly_archive.sh
@@ -5,20 +5,18 @@ set -x
 
 # Select the wasm architecture for the package.
 readonly architecture="${1}"
-case "$architecture" in
-    wasm64-emscripten)
-        node_args="--experimental-wasm-memory64"
-        ;;
-    *)
-        node_args=""
-        ;;
-esac
-readonly node_args
-version=$( node $node_args --eval "import('./build/install/bin/vtkWasmSceneManager.mjs').then(m => m.default().then(i => console.log(i.getVTKVersion())))" )
+# Compute the version number for the upload.
+major="$( sed -ne '/set.VTK_MAJOR_VERSION/s/.* \([0-9]*\))/\1/p' CMake/vtkVersion.cmake )"
+readonly major
+minor="$( sed -ne '/set.VTK_MINOR_VERSION/s/.* \([0-9]*\))/\1/p' CMake/vtkVersion.cmake )"
+readonly minor
+build="$( sed -ne '/set.VTK_BUILD_VERSION/s/.* \([0-9]*\))/\1/p' CMake/vtkVersion.cmake )"
+readonly build
+version="$major.$minor.$build"
 readonly version
 readonly prefix="vtk"
 readonly package_name="$prefix-$architecture"
 
 cd build/install/bin
-curl --header "JOB-TOKEN: $CI_JOB_TOKEN" --upload-file "$prefix-$version-$architecture.tar.gz" "$CI_API_V4_URL/projects/$CI_PROJECT_ID/packages/generic/$package_name/$version/$prefix-$version-$architecture.tar.gz"
+curl --header "JOB-TOKEN: $CI_JOB_TOKEN" --upload-file "$prefix-$package_name.tar.gz" "$CI_API_V4_URL/projects/$CI_PROJECT_ID/packages/generic/$package_name/$version/$prefix-$package_name.tar.gz"
 cd ../../
