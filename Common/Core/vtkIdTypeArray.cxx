c201350da69d9625fdafadfb6c6c1f675813903b
Alexy Pellegrini <alexy.pellegrini@kitware.com>
2025-05-26 11:42:12 +0200

    Add vtk<Type>Array to bulk instantiation

--- Common/Core/vtkIdTypeArray.cxx
+++ Common/Core/vtkIdTypeArray.cxx
@@ -1,12 +1,5 @@
 // SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
 // SPDX-License-Identifier: BSD-3-Clause
-// We never need to instantiate vtkAOSDataArrayTemplate<vtkIdType> or
-// vtkArrayIteratorTemplate<vtkIdType> because they are instantiated
-// by the corresponding array for its native type.  Therefore this
-// code should not be uncommented and is here for reference:
-//   #define VTK_AOS_DATA_ARRAY_TEMPLATE_INSTANTIATING
-//   #include "vtkAOSDataArrayTemplate.txx"
-//   VTK_AOS_DATA_ARRAY_TEMPLATE_INSTANTIATE(vtkIdType);
 
 #include "vtkIdTypeArray.h"
 
