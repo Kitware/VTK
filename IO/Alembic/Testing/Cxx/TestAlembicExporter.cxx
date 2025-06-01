92461d3ac14f608d84e5c0d91efc780f446f006c
Cory Quammen <cory.quammen@kitware.com>
2025-06-01 18:23:04 -0400

    TestAlembicExporter: check that textures are exported correctly
    Add a texture that is applied to one of the actors and check that it
    and the textures for color maps are saved to the same directory as the
    .abc file.

--- IO/Alembic/Testing/Cxx/TestAlembicExporter.cxx
+++ IO/Alembic/Testing/Cxx/TestAlembicExporter.cxx
@@ -4,8 +4,10 @@
 #include "vtkActor.h"
 #include "vtkAlembicExporter.h"
 #include "vtkElevationFilter.h"
+#include "vtkImageData.h"
 #include "vtkLogger.h"
 #include "vtkNew.h"
+#include "vtkPointData.h"
 #include "vtkPolyDataMapper.h"
 #include "vtkRenderWindow.h"
 #include "vtkRenderWindowInteractor.h"
@@ -13,6 +15,8 @@
 #include "vtkSphereSource.h"
 #include "vtkSuperquadricSource.h"
 #include "vtkTestUtilities.h"
+#include "vtkTexture.h"
+#include "vtkUnsignedCharArray.h"
 #include <vtksys/SystemTools.hxx>
 
 #include <cstdlib>
@@ -47,6 +51,27 @@ int TestAlembicExporter(int argc, char* argv[])
   vtkNew<vtkActor> actor2;
   actor2->SetMapper(mapper2);
 
+  // Create a simple image data with a texture
+  // This will be used to test that the exporter correctly handles textures.
+  // The image will be a red square.
+  vtkNew<vtkImageData> image;
+  image->SetDimensions(8, 8, 1);
+  image->AllocateScalars(VTK_UNSIGNED_CHAR, 3);
+  vtkUnsignedCharArray* scalars =
+    vtkArrayDownCast<vtkUnsignedCharArray>(image->GetPointData()->GetScalars());
+  if (!scalars)
+  {
+    vtkLog(ERROR, "Failed to allocate scalars for image data.");
+    return EXIT_FAILURE;
+  }
+  scalars->FillComponent(0, 255); // Set red channel to 255
+  scalars->FillComponent(1, 0);   // Set green channel to 0
+  scalars->FillComponent(2, 0);   // Set blue channel to 0
+
+  vtkNew<vtkTexture> texture;
+  texture->SetInputData(image);
+  actor2->SetTexture(texture);
+
   vtkNew<vtkRenderer> renderer;
   renderer->AddActor(actor);
   renderer->AddActor(actor2);
@@ -65,6 +90,22 @@ int TestAlembicExporter(int argc, char* argv[])
   auto correctSize = vtksys::SystemTools::FileLength(filename);
   if (correctSize == 0)
   {
+    return EXIT_FAILURE;
+  }
+
+  // Check if the texture file for the color map was created
+  std::string textureFilename = rootname + "_full_tex0.png";
+  if (!vtksys::SystemTools::FileExists(textureFilename.c_str(), true /* file */))
+  {
+    vtkLog(ERROR, "File " << textureFilename << " for color map texture was not created.");
+    return EXIT_FAILURE;
+  }
+
+  // Check if the texture file for the actor texture was created
+  textureFilename = rootname + "_full_tex1.png";
+  if (!vtksys::SystemTools::FileExists(textureFilename.c_str(), true /* file */))
+  {
+    vtkLog(ERROR, "File " << textureFilename << " for actor texture was not created.");
     return EXIT_FAILURE;
   }
 
@@ -87,6 +128,15 @@ int TestAlembicExporter(int argc, char* argv[])
     return EXIT_FAILURE;
   }
 
+  textureFilename = rootname + "_empty_tex0.png";
+  if (vtksys::SystemTools::FileExists(textureFilename.c_str(), true /* file */))
+  {
+    vtkLog(ERROR,
+      "File " << textureFilename
+              << " for color map texture should not have been created for an empty export.");
+    return EXIT_FAILURE;
+  }
+
   actor->VisibilityOn();
   actor->SetMapper(nullptr);
   exporter->Write();
@@ -100,6 +150,14 @@ int TestAlembicExporter(int argc, char* argv[])
     vtkLog(ERROR,
       "File should not contain geometry"
       " (actor has no mapper)");
+    return EXIT_FAILURE;
+  }
+
+  textureFilename = rootname + "_empty_tex0.png";
+  if (vtksys::SystemTools::FileExists(textureFilename.c_str(), true /* file */))
+  {
+    vtkLog(ERROR,
+      "File " << textureFilename << " for color map texture was created but should not have been.");
     return EXIT_FAILURE;
   }
 
