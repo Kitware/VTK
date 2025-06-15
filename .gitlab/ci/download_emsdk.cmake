a6347637707354a34e4e08f40e39f17d46493483
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2025-06-15 18:16:03 -0400

    ci: bump emsdk to 4.0.10
    - closes vtk/vtk#19695

--- .gitlab/ci/download_emsdk.cmake
+++ .gitlab/ci/download_emsdk.cmake
@@ -1,9 +1,9 @@
 cmake_minimum_required(VERSION 3.12)
 
 # Input variables.
-set(emsdk_version "4.0.9")
+set(emsdk_version "4.0.10")
 set(emsdk_ext "zip")
-set(emsdk_hash "30263c749b98503118e090a0019db0a6180f680bb5d717ae3db40d3ed1614e0e")
+set(emsdk_hash "ec058146a1aa7c166e04fc7af560adfed05a9bbaed42821ad67fb2acbe666fd0")
 set(emsdk_suffix "${emsdk_version}.${emsdk_ext}")
 
 set(emsdk_url "https://github.com/emscripten-core/emsdk/archive/refs/tags")
