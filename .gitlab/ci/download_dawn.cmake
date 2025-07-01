8e9e682e771d251f237655e478b0b44dc9bf89c1
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2025-06-30 20:01:27 -0400

    ci: update dawn version
    - remove wasm32/wasm64 package downloads since they are not used anymore after https://gitlab.kitware.com/vtk/vtk/-/merge_requests/12215

--- .gitlab/ci/download_dawn.cmake
+++ .gitlab/ci/download_dawn.cmake
@@ -1,8 +1,8 @@
 cmake_minimum_required(VERSION 3.12)
 
 # Input variables.
-set(dawn_version "7153")
-set(dawn_build_date "20250502.0")
+set(dawn_version "20250621.085109")
+set(dawn_build_date "20250630.1")
 
 # Avoids matching wasm(32|64)_emscripten_windows_chrome_ext_vtk
 # This is equivalent to `if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "windows(?!.*wasm)")`
@@ -10,33 +10,25 @@ set(dawn_build_date "20250502.0")
 if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "windows" AND NOT "$ENV{CMAKE_CONFIGURATION}" MATCHES "wasm")
   set(dawn_platform "windows-x86_64")
   set(dawn_ext "zip")
-  set(dawn_hash "fe52157fd1234010d25d5d6bb0922fc718df95bff1be47a303e176d2c4295cad")
+  set(dawn_hash "c626c476823261e77460d494c8b187ac58573974482f6993aed36e310c6d910e")
 elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "macos_arm64")
   set(dawn_platform "macos-arm64")
   set(dawn_ext "tar.gz")
-  set(dawn_hash "01810d72ce560958c26132f5e7da62aac30069f3b5484f4c57b340d23d81af23")
+  set(dawn_hash "2d988707f53fca93667b263de7f7155ce8dc7cdcb0dfab6346f01df2c6af7597")
 elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "macos_x86_64")
   set(dawn_platform "macos-x86_64")
   set(dawn_ext "tar.gz")
-  set(dawn_hash "72aaac22a3e9172060fea92a5b2efdd65c0fc82b585642ab5dd9dd35ad1449ad")
+  set(dawn_hash "8801308bb8a0635012ac1b0379070201d5954116cad83389613d33203844dd9d")
 elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "fedora")
   set(dawn_platform "linux-x86_64")
   set(dawn_ext "tar.gz")
-  set(dawn_hash "95b253cd52c5c0ab8ceff9a5bc37627353dde6430006b9460a4b0d5467d90f3a")
-elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "wasm32")
-  set(dawn_platform "wasm32-emscripten")
-  set(dawn_ext "tar.gz")
-  set(dawn_hash "dfa56d448ae16df92f45c429582856ac1e575bd335397e6d95e589aeb98e5660")
-elseif ("$ENV{CMAKE_CONFIGURATION}" MATCHES "wasm64")
-  set(dawn_platform "wasm64-emscripten")
-  set(dawn_ext "tar.gz")
-  set(dawn_hash "5c20ab08d2ef5b1c10a9c27b83202bc837fea9eb1c5fb5ae63f41aaebc98bc61")
+  set(dawn_hash "f1ca00e9eb356ef14f31affd312071953958d56aef57cc2b05001ebab338dc7d")
 else ()
   message(FATAL_ERROR "Unknown platform for dawn")
 endif ()
 
 set(dawn_url "https://gitlab.kitware.com/api/v4/projects/6955/packages/generic/dawn/v${dawn_version}-${dawn_build_date}")
-set(dawn_file "dawn-${dawn_version}-${dawn_platform}.${dawn_ext}")
+set(dawn_file "dawn-v${dawn_version}-${dawn_platform}.${dawn_ext}")
 
 # Download the file.
 file(DOWNLOAD
@@ -70,5 +62,5 @@ endif ()
 
 # Move to a predictable prefix.
 file(RENAME
-  ".gitlab/dawn-${dawn_version}-${dawn_platform}"
+  ".gitlab/dawn-v${dawn_version}-${dawn_platform}"
   ".gitlab/dawn")
