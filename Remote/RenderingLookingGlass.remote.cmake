e2eb761e2a34f459a3fa6e12ab2c54d021f07ae8
Mathieu Westphal <mathieu.westphal@kitware.com>
2025-07-08 10:18:54 +0200

    Update LookingGlassVTKModule for vtkVectorOperators include removal

--- Remote/RenderingLookingGlass.remote.cmake
+++ Remote/RenderingLookingGlass.remote.cmake
@@ -5,5 +5,5 @@
 vtk_fetch_module(RenderingLookingGlass
         "Looking Glass Support for VTK"
         GIT_REPOSITORY https://github.com/Kitware/LookingGlassVTKModule
-        GIT_TAG 89b4ded13b653a90c8c8f6c77ecae8af751650ac
+        GIT_TAG 145e306d2f46808713d5b2b930ee5acbd6096a3e
         )
