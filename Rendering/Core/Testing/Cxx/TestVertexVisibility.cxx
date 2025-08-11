71a5565b3652fe868016e7185f6e09b57dda0af3
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2025-08-11 10:32:03 -0400

    Fix toggling vertex visibility
    - closes vtk/vtk#19758

--- Rendering/Core/Testing/Cxx/TestVertexVisibility.cxx
+++ Rendering/Core/Testing/Cxx/TestVertexVisibility.cxx
@@ -16,12 +16,10 @@
 //------------------------------------------------------------------------------
 int TestVertexVisibility(int argc, char* argv[])
 {
-  vtkNew<vtkRenderer> renderer;
-  renderer->SetBackground(0.0, 0.0, 0.0);
 
   vtkNew<vtkRenderWindow> renderWindow;
   renderWindow->SetSize(300, 300);
-  renderWindow->AddRenderer(renderer);
+  renderWindow->SetMultiSamples(0);
 
   vtkNew<vtkRenderWindowInteractor> iren;
   iren->SetRenderWindow(renderWindow);
@@ -34,27 +32,40 @@ int TestVertexVisibility(int argc, char* argv[])
   vtkNew<vtkPolyDataMapper> mapper;
   mapper->SetInputConnection(sphere->GetOutputPort());
 
-  vtkNew<vtkActor> actor;
-  renderer->AddActor(actor);
-  actor->SetMapper(mapper);
-  actor->GetProperty()->SetDiffuseColor(0.4, 1.0, 1.0);
+  for (int i = 0; i < 2; ++i)
+  {
+    vtkNew<vtkRenderer> renderer;
+    renderer->SetBackground(0.0, 0.0, 0.0);
+    renderWindow->AddRenderer(renderer);
+    vtkNew<vtkActor> actor;
+    renderer->AddActor(actor);
+    actor->SetMapper(mapper);
+    actor->GetProperty()->SetDiffuseColor(0.4, 1.0, 1.0);
 
-  vtkNew<vtkProperty> backProp;
-  backProp->SetDiffuseColor(0.4, 0.65, 0.8);
-  actor->SetBackfaceProperty(backProp);
+    vtkNew<vtkProperty> backProp;
+    backProp->SetDiffuseColor(0.4, 0.65, 0.8);
+    actor->SetBackfaceProperty(backProp);
 
-  actor->GetProperty()->VertexVisibilityOn();
-  actor->GetProperty()->SetVertexColor(1.0, 0.5, 1.0);
-  actor->GetProperty()->SetPointSize(14.0);
-  actor->GetProperty()->RenderPointsAsSpheresOn();
+    actor->GetProperty()->VertexVisibilityOn();
+    actor->GetProperty()->SetVertexColor(1.0, 0.5, 1.0);
+    actor->GetProperty()->SetPointSize(12.0);
+    actor->GetProperty()->RenderPointsAsSpheresOn();
 
-  renderWindow->SetMultiSamples(0);
-  renderer->ResetCamera();
-  renderer->GetActiveCamera()->Elevation(-45);
-  renderer->GetActiveCamera()->OrthogonalizeViewUp();
-  renderer->GetActiveCamera()->Zoom(1.5);
-  renderer->ResetCameraClippingRange();
-  renderWindow->Render();
+    renderer->SetViewport(i == 0 ? 0.0 : 0.5, 0.0, i == 0 ? 0.5 : 1.0, 1.0);
+    renderer->ResetCamera();
+    renderer->GetActiveCamera()->Elevation(-45);
+    renderer->GetActiveCamera()->OrthogonalizeViewUp();
+    renderer->GetActiveCamera()->Zoom(1.5);
+    renderer->ResetCameraClippingRange();
+    renderWindow->Render();
+
+    if (i == 1)
+    {
+      renderer->SetBackground(0.2, 0.3, 0.1);
+      actor->GetProperty()->VertexVisibilityOff();
+      renderWindow->Render();
+    }
+  }
 
   int retVal = vtkRegressionTestImageThreshold(renderWindow, 0.05);
   if (retVal == vtkRegressionTester::DO_INTERACTOR)
