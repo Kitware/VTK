90512421be70358767e09f17d9cd169f0e3c6a85
Kitware Robot <kwrobot@kitware.com>
2025-08-28 00:01:04 -0400

    VTK Nightly Date Stamp

--- CMake/vtkVersion.cmake
+++ CMake/vtkVersion.cmake
@@ -6,7 +6,7 @@ set(VTK_MINOR_VERSION 5)
 # the date component is updated while it is in progress, the bump of the minor
 # version ends up conflicting with the patch version update.
 
-set(VTK_BUILD_VERSION 20250827)
+set(VTK_BUILD_VERSION 20250828)
 
 if (NOT VTK_MINOR_VERSION LESS 100)
   message(FATAL_ERROR
