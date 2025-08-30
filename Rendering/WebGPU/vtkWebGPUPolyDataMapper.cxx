a9abf6085547338ad7c377a3b0cbd94719b722aa
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2025-08-30 12:01:39 -0400

    Add logging to vtkWebGPUPolyDataMapper for better traceability and debugging

--- Rendering/WebGPU/vtkWebGPUPolyDataMapper.cxx
+++ Rendering/WebGPU/vtkWebGPUPolyDataMapper.cxx
@@ -9,6 +9,7 @@
 #include "vtkDataArray.h"
 #include "vtkDataArrayRange.h"
 #include "vtkFloatArray.h"
+#include "vtkLogger.h"
 #include "vtkLookupTable.h"
 #include "vtkMath.h"
 #include "vtkMatrix4x4.h"
@@ -190,6 +191,7 @@ vtkPolyDataMapper::MapperHashType vtkWebGPUPolyDataMapper::GenerateHash(vtkPolyD
 //------------------------------------------------------------------------------
 void vtkWebGPUPolyDataMapper::RenderPiece(vtkRenderer* renderer, vtkActor* actor)
 {
+  vtkLogScopeFunction(TRACE);
   auto wgpuRenderWindow = vtkWebGPURenderWindow::SafeDownCast(renderer->GetRenderWindow());
   // Note for emscripten: the indirection to js getTimeNow is a bit costly. it can quickly add up
   // for really large number of actors. However, vtkRenderWindow caps it to 5 times per second. the
@@ -204,7 +206,9 @@ void vtkWebGPUPolyDataMapper::RenderPiece(vtkRenderer* renderer, vtkActor* actor
   auto* wgpuConfiguration = wgpuRenderWindow->GetWGPUConfiguration();
   auto* wgpuRenderer = vtkWebGPURenderer::SafeDownCast(renderer);
   auto* displayProperty = actor->GetProperty();
-
+  vtkLog(TRACE,
+    "RenderPiece for actor: " << actor << " in renderer: " << renderer
+                              << " in stage: " << static_cast<int>(wgpuRenderer->GetRenderStage()));
   switch (wgpuRenderer->GetRenderStage())
   {
     case vtkWebGPURenderer::RenderStageEnum::UpdatingBuffers:
@@ -295,6 +299,7 @@ void vtkWebGPUPolyDataMapper::RenderPiece(vtkRenderer* renderer, vtkActor* actor
       // setup graphics pipeline
       if (this->GetNeedToRebuildGraphicsPipelines(actor, renderer))
       {
+        vtkLog(TRACE, "rebuild graphics pipelines");
         // Create bind group for the point/cell attribute buffers.
         this->MeshAttributeBindGroup = this->CreateMeshAttributeBindGroup(
           wgpuConfiguration->GetDevice(), this->GetObjectDescription() + "-MeshAttributeBindGroup");
@@ -471,6 +476,7 @@ vtkWebGPUPolyDataMapper::DrawCallArgs vtkWebGPUPolyDataMapper::GetDrawCallArgsFo
 void vtkWebGPUPolyDataMapper::RecordDrawCommands(
   vtkRenderer* renderer, vtkActor* actor, const wgpu::RenderPassEncoder& passEncoder)
 {
+  vtkLogScopeFunction(TRACE);
   passEncoder.SetBindGroup(2, this->MeshAttributeBindGroup);
 
   auto* wgpuRenderWindow = vtkWebGPURenderWindow::SafeDownCast(renderer->GetRenderWindow());
@@ -701,6 +707,7 @@ void vtkWebGPUPolyDataMapper::RecordDrawCommands(
 void vtkWebGPUPolyDataMapper::RecordDrawCommands(
   vtkRenderer* renderer, vtkActor* actor, const wgpu::RenderBundleEncoder& bundleEncoder)
 {
+  vtkLog(TRACE, "record draw commands to bundle");
   bundleEncoder.SetBindGroup(2, this->MeshAttributeBindGroup);
 
   auto* wgpuRenderWindow = vtkWebGPURenderWindow::SafeDownCast(renderer->GetRenderWindow());
@@ -1380,6 +1387,7 @@ void vtkWebGPUPolyDataMapper::ResetPointCellAttributeState()
 //------------------------------------------------------------------------------
 void vtkWebGPUPolyDataMapper::UpdateMeshGeometryBuffers(vtkWebGPURenderWindow* wgpuRenderWindow)
 {
+  vtkLogScopeFunction(TRACE);
   if ((this->CachedInput != nullptr) && (this->CurrentInput != nullptr) &&
     (this->CurrentInput != this->CachedInput))
   {
@@ -1747,6 +1755,7 @@ const char* vtkWebGPUPolyDataMapper::GetGraphicsPipelineTypeAsString(
 void vtkWebGPUPolyDataMapper::SetupGraphicsPipelines(
   const wgpu::Device& device, vtkRenderer* renderer, vtkActor* actor)
 {
+  vtkLogScopeFunction(TRACE);
   auto* wgpuActor = vtkWebGPUActor::SafeDownCast(actor);
   auto* wgpuRenderWindow = vtkWebGPURenderWindow::SafeDownCast(renderer->GetRenderWindow());
   auto* wgpuRenderer = vtkWebGPURenderer::SafeDownCast(renderer);
@@ -3449,7 +3458,7 @@ bool vtkWebGPUPolyDataMapper::GetNeedToRebuildGraphicsPipelines(
     return true;
   }
   // have the clipping planes changed?
-  if (this->LastNumClipPlanes != this->GetNumberOfClippingPlanes())
+  if (this->LastNumClipPlanes != std::min(this->GetNumberOfClippingPlanes(), 6))
   {
     return true;
   }
