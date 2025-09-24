Kitware/VTK

commit 5f52b85b63093bd2348ac301e857d11298e9b82d

Importer: Generalize FileName and Stream API

IO/Import/vtk3DSImporter.cxx
@@ -113,7 +113,6 @@ vtk3DSImporter::vtk3DSImporter()
   this->MeshList = nullptr;
   this->MaterialList = nullptr;
   this->MatPropList = nullptr;
-  this->FileName = nullptr;
   this->FileFD = nullptr;
   this->ComputeNormals = 0;
 }
@@ -121,10 +120,11 @@ vtk3DSImporter::vtk3DSImporter()
 int vtk3DSImporter::ImportBegin()
 {
   vtkDebugMacro(<< "Opening import file as binary");
-  this->FileFD = vtksys::SystemTools::Fopen(this->FileName, "rb");
+  char* filename = this->GetFileName();
+  this->FileFD = vtksys::SystemTools::Fopen(filename, "rb");
   if (this->FileFD == nullptr)
   {
-    vtkErrorMacro(<< "Unable to open file: " << this->FileName);
+    vtkErrorMacro(<< "Unable to open file: " << filename);
     return 0;
   }
   return this->Read3DS();
@@ -162,7 +162,7 @@ int vtk3DSImporter::Read3DS()
 
   if (parse_3ds_file(this) == 0)
   {
-    vtkErrorMacro(<< "Error readings .3ds file: " << this->FileName << "\n");
+    vtkErrorMacro(<< "Error readings .3ds file: " << this->GetFileName() << "\n");
     return 0;
   }
 
@@ -447,15 +447,11 @@ vtk3DSImporter::~vtk3DSImporter()
 
   // then delete the list structure
   VTK_LIST_KILL(this->MatPropList);
-
-  delete[] this->FileName;
 }
 
 void vtk3DSImporter::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
-  os << indent << "File Name: " << (this->FileName ? this->FileName : "(none)") << "\n";
-
   os << indent << "Compute Normals: " << (this->ComputeNormals ? "On\n" : "Off\n");
 }
 VTK_ABI_NAMESPACE_END

IO/Import/vtkGLTFImporter.cxx
@@ -492,12 +492,6 @@ void ApplyTransformToCamera(vtkSmartPointer<vtkCamera> cam, vtkSmartPointer<vtkM
 }
 
 //------------------------------------------------------------------------------
-vtkGLTFImporter::~vtkGLTFImporter()
-{
-  this->SetFileName(nullptr);
-}
-
-//------------------------------------------------------------------------------
 void vtkGLTFImporter::InitializeLoader()
 {
   this->Loader = vtkSmartPointer<vtkGLTFDocumentLoader>::New();
@@ -507,7 +501,9 @@ void vtkGLTFImporter::InitializeLoader()
 int vtkGLTFImporter::ImportBegin()
 {
   // Make sure we have a file to read.
-  if (!this->Stream && !this->FileName)
+  vtkResourceStream* stream = this->GetStream();
+  char* filename = this->GetFileName();
+  if (!stream && !filename)
   {
     vtkErrorMacro("Neither FileName nor Stream has been specified.");
     return 0;
@@ -526,19 +522,19 @@ int vtkGLTFImporter::ImportBegin()
 
   // Check extension
   std::vector<char> glbBuffer;
-  if (this->Stream != nullptr)
+  if (stream != nullptr)
   {
-    // this->Stream is defined.
+    // stream is defined.
     if (this->StreamIsBinary)
     {
-      if (!this->Loader->LoadStreamBuffer(this->Stream, glbBuffer))
+      if (!this->Loader->LoadStreamBuffer(stream, glbBuffer))
       {
         vtkErrorMacro("Error loading binary data");
         return 0;
       }
     }
 
-    if (!this->Loader->LoadModelMetaDataFromStream(this->Stream, this->StreamURILoader))
+    if (!this->Loader->LoadModelMetaDataFromStream(stream, this->StreamURILoader))
     {
       vtkErrorMacro("Error loading model metadata");
       return 0;
@@ -546,18 +542,18 @@ int vtkGLTFImporter::ImportBegin()
   }
   else
   {
-    // this->FileName is defined.
-    std::string extension = vtksys::SystemTools::GetFilenameLastExtension(this->FileName);
+    // filename is defined.
+    std::string extension = vtksys::SystemTools::GetFilenameLastExtension(filename);
     if (extension == ".glb")
     {
-      if (!this->Loader->LoadFileBuffer(this->FileName, glbBuffer))
+      if (!this->Loader->LoadFileBuffer(filename, glbBuffer))
       {
         vtkErrorMacro("Error loading binary data");
         return 0;
       }
     }
 
-    if (!this->Loader->LoadModelMetaDataFromFile(this->FileName))
+    if (!this->Loader->LoadModelMetaDataFromFile(filename))
     {
       vtkErrorMacro("Error loading model metadata");
       return 0;
@@ -1164,16 +1160,7 @@ bool vtkGLTFImporter::GetTemporalInformation(vtkIdType animationIndex, double fr
 void vtkGLTFImporter::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
-  os << indent;
-  if (this->Stream != nullptr)
-  {
-    os << "Stream (" << (this->StreamIsBinary ? "binary" : "ascii") << ")";
-  }
-  else
-  {
-    os << "File Name: " << (this->FileName ? this->FileName : "(none)");
-  }
-  os << "\n";
+  os << indent << "StreamIsBinary: " << this->StreamIsBinary << "\n";
 }
 
 //------------------------------------------------------------------------------

IO/Import/vtkVRMLImporter.cxx
@@ -86,7 +86,6 @@ vtkVRMLImporter::vtkVRMLImporter()
   this->CurrentMapper = nullptr;
   this->CurrentLut = nullptr;
   this->CurrentTransform = nullptr;
-  this->FileName = nullptr;
   this->FileFD = nullptr;
   this->Parser = new vtkVRMLYaccData;
   this->ShapeResolution = 12;
@@ -100,9 +99,6 @@ vtkVRMLImporter::~vtkVRMLImporter()
     this->CurrentTransform->Delete();
     this->CurrentTransform = nullptr;
   }
-
-  delete[] this->FileName;
-  this->FileName = nullptr;
 
   while (this->Internal->Heap.Count() > 0)
   {
@@ -134,7 +130,6 @@ vtkVRMLImporter::~vtkVRMLImporter()
 void vtkVRMLImporter::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
-  os << indent << "File Name: " << (this->FileName ? this->FileName : "(none)") << "\n";
 
   os << "Defined names in File:" << endl;
   if (this->Parser->useList)
@@ -153,15 +148,16 @@ int vtkVRMLImporter::OpenImportFile()
 {
   vtkDebugMacro(<< "Opening import file");
 
-  if (!this->FileName)
+  char* filename = this->GetFileName();
+  if (!filename)
   {
     vtkErrorMacro(<< "No file specified!");
     return 0;
   }
-  this->FileFD = vtksys::SystemTools::Fopen(this->FileName, "r");
+  this->FileFD = vtksys::SystemTools::Fopen(filename, "r");
   if (this->FileFD == nullptr)
   {
-    vtkErrorMacro(<< "Unable to open file: " << this->FileName);
+    vtkErrorMacro(<< "Unable to open file: " << filename);
     return 0;
   }
   return 1;
@@ -255,7 +251,7 @@ int vtkVRMLImporter::ImportBegin()
     // Not sure why I have to do this but its not working when
     // When I use the FileFD file pointer...
     // File existence already checked.
-    this->Parser->yyin = vtksys::SystemTools::Fopen(this->FileName, "r");
+    this->Parser->yyin = vtksys::SystemTools::Fopen(this->GetFileName(), "r");
     if (!this->Parser->yyin)
     {
       throw std::exception();

IO/Import/vtk3DSImporter.h
@@ -7,6 +7,7 @@
  * vtk3DSImporter imports 3D Studio files into vtk.
  *
  * This importer doesn't support scene hierarchy API
+ * This importer doesn't support reading streams
  *
  * This importer supports the collection API
  *
@@ -31,14 +32,6 @@ public:
 
   vtkTypeMacro(vtk3DSImporter, vtkImporter);
   void PrintSelf(ostream& os, vtkIndent indent) override;
-
-  ///@{
-  /**
-   * Specify the name of the file to read.
-   */
-  vtkSetFilePathMacro(FileName);
-  vtkGetFilePathMacro(FileName);
-  ///@}
 
   ///@{
   /**
@@ -80,7 +73,6 @@ protected:
   vtkPolyData* GeneratePolyData(vtk3DSMesh* meshPtr);
   int Read3DS();
 
-  char* FileName;
   FILE* FileFD;
   vtkTypeBool ComputeNormals;
 

IO/Import/vtkGLTFImporter.h
@@ -45,6 +45,11 @@
  *   The importer supports the KHR_texture_transform extension, but assumes all texture transforms
  * are equal. The base color texture transform is used for all textures.
  *
+ * When selecting the input method, `Stream` has a
+ * higher priority than `FileName` i.e. if a stream is provided, the filename is ignored.
+ * \note When readering from stream, if the stream contains non-data URIs, specifying a custom uri
+ * loader is crucial. \sa SetStreamURILoader() \sa SetStreamIsBinary()
+ *
  * @sa
  * vtkImporter
  * vtkGLTFReader
@@ -55,9 +60,8 @@
 
 #include "vtkIOImportModule.h" // For export macro
 #include "vtkImporter.h"
-#include "vtkResourceStream.h" // For Stream
-#include "vtkSmartPointer.h"   // For SmartPointer
-#include "vtkURILoader.h"      // For URILoader
+#include "vtkSmartPointer.h" // For SmartPointer
+#include "vtkURILoader.h"    // For URILoader
 
 #include <map>    // For map
 #include <vector> // For vector
@@ -78,28 +82,6 @@ public:
 
   vtkTypeMacro(vtkGLTFImporter, vtkImporter);
   void PrintSelf(ostream& os, vtkIndent indent) override;
-
-  ///@{
-  /**
-   * Specify the name of the file to read.
-   */
-  vtkSetFilePathMacro(FileName);
-  vtkGetFilePathMacro(FileName);
-  ///@}
-
-  ///@{
-  /**
-   * Specify the glTF source stream to read from. When selecting the input method, `Stream` has a
-   * higher priority than `FileName` i.e. if a stream is provided, the filename is ignored.
-   *
-   * \note If the stream contains non-data URIs, specifying a custom uri loader is crucial.
-   * \sa SetStreamURILoader()
-   *
-   * \sa SetStreamIsBinary()
-   */
-  vtkSetSmartPointerMacro(Stream, vtkResourceStream);
-  vtkGetSmartPointerMacro(Stream, vtkResourceStream);
-  ///@}
 
   ///@{
   /**
@@ -192,7 +174,7 @@ public:
 
 protected:
   vtkGLTFImporter() = default;
-  ~vtkGLTFImporter() override;
+  ~vtkGLTFImporter() override = default;
 
   /**
    * Initialize the document loader.
@@ -215,8 +197,6 @@ protected:
    */
   virtual void ApplyArmatureProperties(vtkActor* actor);
 
-  char* FileName = nullptr;
-  vtkSmartPointer<vtkResourceStream> Stream;
   vtkSmartPointer<vtkURILoader> StreamURILoader;
   bool StreamIsBinary = false;
 

IO/Import/vtkImporter.cxx
@@ -24,6 +24,7 @@ vtkImporter::vtkImporter() = default;
 //------------------------------------------------------------------------------
 vtkImporter::~vtkImporter()
 {
+  this->SetFileName(nullptr);
   this->SetRenderWindow(nullptr);
 
   if (this->Renderer)
@@ -112,6 +113,9 @@ void vtkImporter::PrintSelf(ostream& os, vtkIndent indent)
   {
     os << "(none)\n";
   }
+
+  this->Stream->PrintSelf(os, indent.GetNextIndent());
+  os << indent << "File Name: " << (this->FileName ? this->FileName : "(none)");
 }
 
 //------------------------------------------------------------------------------

IO/Import/vtkImporter.h
@@ -35,6 +35,7 @@
 
 #include "vtkDataAssembly.h"   // for vtkDataAssembly
 #include "vtkIOImportModule.h" // For export macro
+#include "vtkResourceStream.h" // For Stream
 #include "vtkSmartPointer.h"   // for vtkSmartPointer
 
 #include "vtkObject.h"
@@ -57,6 +58,24 @@ class VTKIOIMPORT_EXPORT vtkImporter : public vtkObject
 public:
   vtkTypeMacro(vtkImporter, vtkObject);
   void PrintSelf(ostream& os, vtkIndent indent) override;
+
+  ///@{
+  /**
+   * Specify file name of the file to read
+   */
+  vtkSetFilePathMacro(FileName);
+  vtkGetFilePathMacro(FileName);
+  ///@}
+
+  ///@{
+  /**
+   * Specify stream to read from
+   * When both `Stream` and `Filename` are set, it's left to the implementation to determine which
+   * one is used. If both are not set, importer outputs nothing.
+   */
+  vtkSetSmartPointerMacro(Stream, vtkResourceStream);
+  vtkGetSmartPointerMacro(Stream, vtkResourceStream);
+  ///@}
 
   ///@{
   /**
@@ -255,6 +274,9 @@ private:
   void operator=(const vtkImporter&) = delete;
 
   bool SetAndCheckUpdateStatus();
+
+  char* FileName = nullptr;
+  vtkSmartPointer<vtkResourceStream> Stream;
 
   UpdateStatusEnum UpdateStatus = UpdateStatusEnum::SUCCESS;
   bool ImportArmature = false;

IO/Import/vtkOBJImporter.cxx
@@ -32,15 +32,8 @@ VTK_ABI_NAMESPACE_BEGIN
 vtkStandardNewMacro(vtkOBJImporter);
 vtkStandardNewMacro(vtkOBJPolyDataProcessor);
 
-//------------------------------------------------------------------------------
-vtkOBJImporter::vtkOBJImporter()
+namespace
 {
-  this->Impl = vtkSmartPointer<vtkOBJPolyDataProcessor>::New();
-}
-
-//------------------------------------------------------------------------------
-vtkOBJImporter::~vtkOBJImporter() = default;
-
 int CanReadFile(vtkObject* that, const std::string& fname)
 {
   FILE* fileFD = vtksys::SystemTools::Fopen(fname, "rb");
@@ -52,14 +45,24 @@ int CanReadFile(vtkObject* that, const std::string& fname)
   fclose(fileFD);
   return 1;
 }
+}
+
+//------------------------------------------------------------------------------
+vtkOBJImporter::vtkOBJImporter()
+{
+  this->Impl = vtkSmartPointer<vtkOBJPolyDataProcessor>::New();
+}
+
+//------------------------------------------------------------------------------
+vtkOBJImporter::~vtkOBJImporter() = default;
 
 int vtkOBJImporter::ImportBegin()
 {
-  if (!CanReadFile(this, this->GetFileName()))
+  if (!::CanReadFile(this, this->GetFileName()))
   {
     return 0;
   }
-  if (!std::string(GetFileNameMTL()).empty() && !CanReadFile(this, this->GetFileNameMTL()))
+  if (!std::string(GetFileNameMTL()).empty() && !::CanReadFile(this, this->GetFileNameMTL()))
   {
     return 0;
   }
@@ -75,6 +78,7 @@ void vtkOBJImporter::ImportEnd()
 //------------------------------------------------------------------------------
 void vtkOBJImporter::ReadData()
 {
+  this->Impl->SetFileName(this->GetFileName());
   this->Impl->Update();
   if (Impl->GetSuccessParsingFiles())
   {
@@ -96,11 +100,6 @@ void vtkOBJImporter::PrintSelf(std::ostream& os, vtkIndent indent)
   vtkImporter::PrintSelf(os, indent);
 }
 
-void vtkOBJImporter::SetFileName(const char* arg)
-{
-  this->Impl->SetFileName(arg);
-}
-
 void vtkOBJImporter::SetFileNameMTL(const char* arg)
 {
   this->Impl->SetMTLfileName(arg);
@@ -109,11 +108,6 @@ void vtkOBJImporter::SetFileNameMTL(const char* arg)
 void vtkOBJImporter::SetTexturePath(const char* path)
 {
   this->Impl->SetTexturePath(path);
-}
-
-const char* vtkOBJImporter::GetFileName() const
-{
-  return this->Impl->GetFileName().data();
 }
 
 const char* vtkOBJImporter::GetFileNameMTL() const
@@ -209,7 +203,6 @@ struct vtkOBJImportedPolyDataWithMaterial
 vtkOBJPolyDataProcessor::vtkOBJPolyDataProcessor()
 {
   // Instantiate object with nullptr filename, and no materials yet loaded.
-  this->FileName = "";
   this->MTLFileName = "";
   this->DefaultMTLFileName = true;
   this->TexturePath = "";
@@ -1266,7 +1259,6 @@ void vtkOBJPolyDataProcessor::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
 
-  os << indent << "FileName: " << (this->FileName.empty() ? this->FileName : "(none)") << "\n";
   os << indent << "MTLFileName: " << (this->MTLFileName.empty() ? this->MTLFileName : "(none)")
      << "\n";
   os << indent << "TexturePath: " << (this->TexturePath.empty() ? this->TexturePath : "(none)")

commit 13b754db562f8ab560d42fcf3ddb8136b2e36ccf

Fix compiler errors with VTK_USE_FUTURE_CONST=YES by adding consts
This was caused by 5f52b85b63093bd2348ac301e857d11298e9b82d.

IO/Import/vtk3DSImporter.cxx
@@ -149,9 +149,10 @@ int vtk3DSImporter::Read3DS()
   vtkNew<vtkFileResourceStream> fileStream;
   if (!this->TempStream)
   {
-    if (!fileStream->Open(this->GetFileName()))
+    const char* filename = this->GetFileName();
+    if (!fileStream->Open(filename))
     {
-      vtkErrorMacro("Unable to open " << this->GetFileName() << " , aborting.");
+      vtkErrorMacro("Unable to open " << filename << " , aborting.");
       return 0;
     }
 

IO/Import/vtkGLTFImporter.cxx
@@ -502,7 +502,7 @@ int vtkGLTFImporter::ImportBegin()
 {
   // Make sure we have a file to read.
   vtkResourceStream* stream = this->GetStream();
-  char* filename = this->GetFileName();
+  const char* filename = this->GetFileName();
   if (!stream && !filename)
   {
     vtkErrorMacro("Neither FileName nor Stream has been specified.");

IO/Import/vtkVRMLImporter.cxx
@@ -148,7 +148,7 @@ int vtkVRMLImporter::OpenImportFile()
 {
   vtkDebugMacro(<< "Opening import file");
 
-  char* filename = this->GetFileName();
+  const char* filename = this->GetFileName();
   if (!filename)
   {
     vtkErrorMacro(<< "No file specified!");

IO/Parallel/vtkNek5000Reader.cxx
@@ -1001,7 +1001,7 @@ int vtkNek5000Reader::RequestInformation(vtkInformation* vtkNotUsed(request),
     // Might consider having just the master node read the .nek5000 file, and broadcast each line to
     // the other processes ??
 
-    char* filename = this->GetFileName();
+    const char* filename = this->GetFileName();
     std::ifstream inPtr(this->GetFileName());
 
     // print the name of the file we're supposed to open