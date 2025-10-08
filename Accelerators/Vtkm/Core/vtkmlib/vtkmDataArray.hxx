e5e500d741cd0b1c73ed4c28a40f7a208f64e402
Spiros Tsalikis <spiros.tsalikis@kitware.com>
2025-10-08 15:51:22 -0400

    vtkmDataArray: Fix ReallocateTuples with nullptr helper
    (cherry picked from commit a36c122f312de27b821839a3cfd48f6e673ac136)

--- Accelerators/Vtkm/Core/vtkmlib/vtkmDataArray.hxx
+++ Accelerators/Vtkm/Core/vtkmlib/vtkmDataArray.hxx
@@ -665,7 +665,7 @@ bool vtkmDataArray<T>::ReallocateTuples(vtkIdType numberOfTuples)
       return false;
     }
   }
-  return false;
+  return this->AllocateTuples(numberOfTuples);
 }
 
 VTK_ABI_NAMESPACE_END
