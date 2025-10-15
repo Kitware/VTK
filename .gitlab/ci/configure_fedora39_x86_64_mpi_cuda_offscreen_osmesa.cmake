1ddc762e6fc245a52f1901fc57aa89ca7e5f6ef7
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2025-10-15 18:55:59 -0400

    ci: increment computing capabilities ver in cuda job

--- .gitlab/ci/configure_fedora39_x86_64_mpi_cuda_offscreen_osmesa.cmake
+++
@@ -1,8 +0,0 @@
-# Currently Fides does not support Viskores+CUDA
-# issue: https://gitlab.kitware.com/vtk/fides/-/issues/13
-set(VTK_MODULE_ENABLE_VTK_fides NO CACHE STRING "")
-
-# Lowest-common denominator.
-set(Viskores_CUDA_Architecture "pascal" CACHE STRING "")
-
-include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora39_x86_64_cuda.cmake")
