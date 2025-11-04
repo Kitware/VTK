25c0129dce164dd0e8d9f1fe16a52d1cf8d9a8c0
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2025-11-03 22:27:47 -0500

    Improve ANARI renderer parameter test

--- Rendering/ANARI/Testing/Cxx/TestAnariRendererParameters.cxx
+++ Rendering/ANARI/Testing/Cxx/TestAnariRendererParameters.cxx
@@ -1,9 +1,5 @@
 // SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
 // SPDX-License-Identifier: BSD-3-Clause
-// This test verifies that lighting works as expected with ANARI.
-
-// SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-// SPDX-License-Identifier: BSD-3-Clause
 
 /**Description
  * This test checks the anari renderer parameter introspection code.
@@ -13,12 +9,12 @@
 #include "vtkAnariRenderer.h"
 #include "vtkLogger.h"
 #include "vtkTesting.h"
-#include <cstdlib>
+#include <anari/frontend/anari_enums.h>
 
 int TestAnariRendererParameters(int argc, char* argv[])
 {
   cout << "CTEST_FULL_OUTPUT (Avoid ctest truncation of output)" << endl;
-  vtkLogger::SetStderrVerbosity(vtkLogger::Verbosity::VERBOSITY_WARNING);
+  vtkLogger::SetStderrVerbosity(vtkLogger::Verbosity::VERBOSITY_INFO);
   bool useDebugDevice = false;
 
   for (int i = 0; i < argc; i++)
@@ -41,26 +37,70 @@ int TestAnariRendererParameters(int argc, char* argv[])
   }
   auto* aren = anariPass->GetAnariRenderer();
 
-  adev->SetupAnariDeviceFromLibrary("environment", "default", useDebugDevice);
+  // Ensure that we use the helide implementation for this test.
+  adev->SetupAnariDeviceFromLibrary("helide", "default", useDebugDevice);
+
+  // Uncomment the following line to test a different implementation provided by ANARI_LIBRARY
+  // environment variable.
+  // adev->SetupAnariDeviceFromLibrary("environment", "default", useDebugDevice);
 
   auto renParams = aren->GetRendererParameters();
 
-  std::cout << "Number of parameters " << renParams.size() << std::endl;
+  int retVal = EXIT_SUCCESS;
+  if (renParams.empty())
+  {
+    vtkLogF(ERROR, "No renderer parameters found. Is the Anari device set up correctly?");
+    return EXIT_FAILURE;
+  }
+  vtkLogF(INFO, "Found %zu renderer parameters.", renParams.size());
   for (auto iter = renParams.cbegin(); iter != renParams.cend(); ++iter)
   {
-    std::cout << "Parameter: " << iter->first << "\n\tType: " << iter->second
-              << "\n\tDescription: " << aren->GetRendererParameterDescription((*iter))
-              << "\n\tRequired: " << aren->IsRendererParameterRequired((*iter))
-              << "\n\tDefault: " << aren->GetRendererParameterDefault((*iter))
-              << "\n\tMinimum: " << aren->GetRendererParameterMinimum((*iter))
-              << "\n\tMaximum: " << aren->GetRendererParameterMaximum((*iter))
-              << "\n\tValue: " << aren->GetRendererParameterValue((*iter)) << std::endl;
+    vtkLogF(INFO, "----------------------------------------");
+    vtkLogF(INFO, "Parameter: %s", iter->first.c_str());
+    vtkLogF(INFO, "\tType: %d", iter->second);
+    vtkLogF(INFO, "\tDescription: %s", aren->GetRendererParameterDescription((*iter)).c_str());
+    vtkLogF(INFO, "\tRequired: %s", aren->IsRendererParameterRequired((*iter)) ? "true" : "false");
+    switch (iter->second)
+    {
+      case ANARI_BOOL:
+        vtkLogF(INFO, "\tDefault: %s",
+          (*(static_cast<const int*>(aren->GetRendererParameterDefault((*iter))))) ? "true"
+                                                                                   : "false");
+        vtkLogF(INFO, "\tValue: %s",
+          (*(static_cast<const int*>(aren->GetRendererParameterValue((*iter))))) ? "true"
+                                                                                 : "false");
+        break;
+      case ANARI_INT16:
+        vtkLogF(INFO, "\tDefault: %d",
+          *(static_cast<const int*>(aren->GetRendererParameterDefault((*iter)))));
+        vtkLogF(INFO, "\tValue: %d",
+          *(static_cast<const int*>(aren->GetRendererParameterValue((*iter)))));
+        vtkLogF(INFO, "\tMinimum: %d",
+          *(static_cast<const int*>(aren->GetRendererParameterMinimum((*iter)))));
+        vtkLogF(INFO, "\tMaximum: %d",
+          *(static_cast<const int*>(aren->GetRendererParameterMaximum((*iter)))));
+        break;
+      case ANARI_FLOAT16:
+        vtkLogF(INFO, "\tDefault: %f",
+          *(static_cast<const float*>(aren->GetRendererParameterDefault((*iter)))));
+        vtkLogF(INFO, "\tValue: %f",
+          *(static_cast<const float*>(aren->GetRendererParameterValue((*iter)))));
+        vtkLogF(INFO, "\tMinimum: %f",
+          *(static_cast<const float*>(aren->GetRendererParameterMinimum((*iter)))));
+        vtkLogF(INFO, "\tMaximum: %f",
+          *(static_cast<const float*>(aren->GetRendererParameterMaximum((*iter)))));
+        break;
+      case ANARI_STRING:
+        vtkLogF(INFO, "\tDefault: %s",
+          static_cast<const char*>(aren->GetRendererParameterDefault((*iter))));
+        vtkLogF(
+          INFO, "\tValue: %s", static_cast<const char*>(aren->GetRendererParameterValue((*iter))));
+        break;
+      default:
+        vtkLogF(INFO, "\tNot printing default/value/minimum/maximum for this type.");
+        break;
+    }
   }
 
-  int retVal = EXIT_SUCCESS;
-  if (renParams.size() < 1)
-  {
-    retVal = EXIT_FAILURE;
-  }
   return retVal;
 }
