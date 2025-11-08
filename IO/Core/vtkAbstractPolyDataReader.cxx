d3b5d490ec31abdab0d0c0ea4aeb9252e0e3ed0d
Mathieu Westphal <mathieu.westphal@kitware.com>
2025-11-08 12:03:35 +0100

    vtkAbstractPolyDataReader: Use Stream mtime in GetMTime

--- IO/Core/vtkAbstractPolyDataReader.cxx
+++ IO/Core/vtkAbstractPolyDataReader.cxx
@@ -5,20 +5,36 @@
 #include "vtkAbstractPolyDataReader.h"
 
 VTK_ABI_NAMESPACE_BEGIN
+
+//----------------------------------------------------------------------------
 vtkAbstractPolyDataReader::vtkAbstractPolyDataReader()
 {
   this->FileName = nullptr;
   this->SetNumberOfInputPorts(0);
 }
 
+//----------------------------------------------------------------------------
 vtkAbstractPolyDataReader::~vtkAbstractPolyDataReader()
 {
   this->SetFileName(nullptr);
 }
 
+//----------------------------------------------------------------------------
 void vtkAbstractPolyDataReader::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os, indent);
   os << indent << "FileName: " << (this->FileName ? this->FileName : "NONE") << endl;
 }
+
+//----------------------------------------------------------------------------
+vtkMTimeType vtkAbstractPolyDataReader::GetMTime()
+{
+  auto mtime = this->Superclass::GetMTime();
+  if (this->Stream)
+  {
+    mtime = std::max(mtime, this->Stream->GetMTime());
+  }
+  return mtime;
+}
+
 VTK_ABI_NAMESPACE_END
