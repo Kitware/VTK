0d72c47c2cc5c2d2e2bca796dd84e54acc7272c1
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2025-11-11 07:01:20 -0500

    ci: Set job pool link size to 1 on linux
    - on windows, emscripten does not show file lock issues, so nothing needed for that

--- .gitlab/ci/configure_wasm32_emscripten_linux.cmake
+++ .gitlab/ci/configure_wasm32_emscripten_linux.cmake
@@ -1,4 +1,1 @@
-# Ensure threads are not enabled.
-set(VTK_WEBASSEMBLY_THREADS OFF CACHE BOOL "")
-
-include("${CMAKE_CURRENT_LIST_DIR}/configure_wasm_common.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/configure_wasm_linux.cmake")
