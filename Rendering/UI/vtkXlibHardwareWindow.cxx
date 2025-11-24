8484cdf6e096634faf7428d9823ec1e5cd9853ee
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2025-11-24 17:24:28 -0500

    Use spdx copyright with the hardware window/view classes

--- Rendering/UI/vtkXlibHardwareWindow.cxx
+++ Rendering/UI/vtkXlibHardwareWindow.cxx
@@ -1,17 +1,6 @@
-/*=========================================================================
+// SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+// SPDX-License-Identifier: BSD-3-Clause
 
-  Program:   Visualization Toolkit
-  Module:    vtkXlibHardwareWindow.cxx
-
-  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
-  All rights reserved.
-  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
-
-  This software is distributed WITHOUT ANY WARRANTY; without even
-  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-  PURPOSE.  See the above copyright notice for more information.
-
-=========================================================================*/
 // vtk includes
 #include "vtkXlibHardwareWindow.h"
 #include "vtkImageData.h"
