7ba6d0aefb76b5bf7f9ab1d878a18c653e269054
David Gobbi <david.gobbi@gmail.com>
2025-12-13 08:08:48 -0700

    Update the ref to vtkDICOM remote module
    This adds the <iostream> header to a file that requires it, since
    VTK no longer includes <iostream> automatically.

--- Remote/vtkDICOM.remote.cmake
+++ Remote/vtkDICOM.remote.cmake
@@ -5,5 +5,5 @@
 vtk_fetch_module(vtkDICOM
   "Dicom classes and utilities"
   GIT_REPOSITORY https://github.com/dgobbi/vtk-dicom
-  GIT_TAG fd3760157aabdd9531291cff635bbde6531c2c52
+  GIT_TAG d1e25090f354542d7bd85c08ee36d8dc22bbbcd8
   )
