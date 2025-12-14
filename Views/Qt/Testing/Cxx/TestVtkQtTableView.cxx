Kitware/VTK

commit a6fea41873b6c8afbbd84a7d529f31f8a147d87e

vtkQtTableView: add vtkAttributeDataToTableFilter

Views/Qt/Testing/Cxx/TestVtkQtTableView.cxx
@@ -20,7 +20,7 @@
 
 #include "vtkQtTableView.h"
 
-#include "vtkDataObjectToTable.h"
+#include "vtkAttributeDataToTableFilter.h"
 #include "vtkSmartPointer.h"
 #include "vtkSphereSource.h"
 #include "vtkTable.h"
@@ -37,9 +37,9 @@ int TestVtkQtTableView(int argc, char* argv[])
 
   // Create a sphere and create a vtkTable from its point data (normal vectors)
   VTK_CREATE(vtkSphereSource, sphereSource);
-  VTK_CREATE(vtkDataObjectToTable, tableConverter);
+  VTK_CREATE(vtkAttributeDataToTableFilter, tableConverter);
   tableConverter->SetInputConnection(sphereSource->GetOutputPort());
-  tableConverter->SetFieldType(vtkDataObjectToTable::POINT_DATA);
+  tableConverter->SetFieldAssociation(vtkDataObject::FIELD_ASSOCIATION_POINTS);
   tableConverter->Update();
   vtkTable* pointTable = tableConverter->GetOutput();
 

Views/Qt/vtkQtTableView.cxx
@@ -33,8 +33,8 @@
 #include "vtkAnnotationLayers.h"
 #include "vtkAnnotationLink.h"
 #include "vtkApplyColors.h"
+#include "vtkAttributeDataToTableFilter.h"
 #include "vtkConvertSelection.h"
-#include "vtkDataObjectToTable.h"
 #include "vtkDataRepresentation.h"
 #include "vtkDataSetAttributes.h"
 #include "vtkGraph.h"
@@ -50,6 +50,17 @@
 #include "vtkTable.h"
 #include "vtkViewTheme.h"
 
+namespace
+{
+const std::map<int, int> FIELD_ASSOCIATION_MAP = { { vtkQtTableView::FIELD_DATA,
+                                                     vtkDataObject::FIELD_ASSOCIATION_NONE },
+  { vtkQtTableView::POINT_DATA, vtkDataObject::FIELD_ASSOCIATION_POINTS },
+  { vtkQtTableView::CELL_DATA, vtkDataObject::FIELD_ASSOCIATION_CELLS },
+  { vtkQtTableView::VERTEX_DATA, vtkDataObject::FIELD_ASSOCIATION_VERTICES },
+  { vtkQtTableView::EDGE_DATA, vtkDataObject::FIELD_ASSOCIATION_EDGES },
+  { vtkQtTableView::ROW_DATA, vtkDataObject::FIELD_ASSOCIATION_ROWS } };
+}
+
 VTK_ABI_NAMESPACE_BEGIN
 vtkStandardNewMacro(vtkQtTableView);
 
@@ -57,11 +68,11 @@ vtkStandardNewMacro(vtkQtTableView);
 vtkQtTableView::vtkQtTableView()
 {
   this->ApplyColors = vtkSmartPointer<vtkApplyColors>::New();
-  this->DataObjectToTable = vtkSmartPointer<vtkDataObjectToTable>::New();
+  this->DataObjectToTable = vtkSmartPointer<vtkAttributeDataToTableFilter>::New();
   this->AddSelectedColumn = vtkSmartPointer<vtkAddMembershipArray>::New();
   this->AddSelectedColumn->SetInputConnection(0, this->DataObjectToTable->GetOutputPort());
 
-  this->DataObjectToTable->SetFieldType(vtkDataObjectToTable::VERTEX_DATA);
+  this->DataObjectToTable->SetFieldAssociation(vtkDataObject::FIELD_ASSOCIATION_VERTICES);
   this->AddSelectedColumn->SetFieldType(vtkAddMembershipArray::VERTEX_DATA);
   this->FieldType = vtkQtTableView::VERTEX_DATA;
   this->AddSelectedColumn->SetOutputArrayName("vtkAddMembershipArray membership");
@@ -154,7 +165,7 @@ void vtkQtTableView::SetSortingEnabled(bool state)
 //------------------------------------------------------------------------------
 void vtkQtTableView::SetFieldType(int type)
 {
-  this->DataObjectToTable->SetFieldType(type);
+  this->DataObjectToTable->SetFieldAssociation(::FIELD_ASSOCIATION_MAP.at(type));
   this->AddSelectedColumn->SetFieldType(type);
   if (this->FieldType != type)
   {

Views/Qt/vtkQtTableView.h
@@ -45,7 +45,7 @@ class QTableView;
 VTK_ABI_NAMESPACE_BEGIN
 class vtkAddMembershipArray;
 class vtkApplyColors;
-class vtkDataObjectToTable;
+class vtkAttributeDataToTableFilter;
 class vtkIdTypeArray;
 class vtkQtTableModelAdapter;
 
@@ -245,7 +245,7 @@ private:
   char* ColorArrayNameInternal;
 
   vtkSmartPointer<vtkAddMembershipArray> AddSelectedColumn;
-  vtkSmartPointer<vtkDataObjectToTable> DataObjectToTable;
+  vtkSmartPointer<vtkAttributeDataToTableFilter> DataObjectToTable;
   vtkSmartPointer<vtkApplyColors> ApplyColors;
 
   vtkQtTableView(const vtkQtTableView&) = delete;

commit 4125bd2a26650e9094bf2f7364a0654e3360c50e

Fix TestVtkQtTableView so it shows data.
This appears to have been a regression caused by commit a6fea41873b6c8afbbd84a7d529f31f8a147d87e.

Views/Qt/Testing/Cxx/TestVtkQtTableView.cxx
@@ -9,7 +9,9 @@
 #include "vtkSphereSource.h"
 #include "vtkTable.h"
 
+#include <QAbstractItemModel>
 #include <QApplication>
+#include <QTableView>
 #include <QTimer>
 #include <QWidget>
 
@@ -31,8 +33,18 @@ int TestVtkQtTableView(int argc, char* argv[])
   VTK_CREATE(vtkQtTableView, tableView);
   tableView->SetSplitMultiComponentColumns(true);
   tableView->AddRepresentationFromInput(pointTable);
+  tableView->SetFieldType(vtkQtTableView::ROW_DATA);
   tableView->Update();
   tableView->GetWidget()->show();
+
+  // Verify that data is shown in the table:
+  auto tw = dynamic_cast<QTableView*>(tableView->GetWidget());
+  auto rr = tw->visualRect(tw->model()->index(1, 0));
+  std::cout << "Rect " << rr.width() << " x " << rr.height() << "\n";
+  if (rr.width() <= 0 || rr.height() <= 0)
+  {
+    std::cerr << "ERROR: bounding rectangle of row 1 should not be empty.\n";
+  }
 
   // Start the Qt event loop to run the application
   QTimer::singleShot(500, &app, SLOT(quit()));