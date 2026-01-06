c2f1d695ac1ee2ae866e5c505e4f20f7081a68ef
David Gobbi <david.gobbi@gmail.com>
2026-01-06 14:24:45 -0700

    Deprecate vtkImageInterpolatorInternals.h
    Print a deprecation message when this header is included, so that
    people know to include vtkInterpolationMath.h instead.

--- Imaging/Core/vtkImageInterpolatorInternals.h
+++ Imaging/Core/vtkImageInterpolatorInternals.h
@@ -8,6 +8,16 @@
 #ifndef vtkImageInterpolatorInternals_h
 #define vtkImageInterpolatorInternals_h
 
+// This file is obsolete and will be removed in a future version of VTK.
+// C++ can't [[deprecate]] a file, so we can't use VTK_DEPRECATED_IN_9_6_0
+#if VTK_DEPRECATION_LEVEL >= VTK_VERSION_CHECK(9, 5, 20250513)
+#if defined(__GNUC__) || __cplusplus >= 202302L
+#warning "Please include vtkInterpolationMath.h instead."
+#else // this is a rarely used internal header, it wasn't even exported
+#error "Please include vtkInterpolationMath.h instead."
+#endif
+#endif
+
 // The contents of this header were moved to the following public header:
 #include "vtkInterpolationMath.h"
 
