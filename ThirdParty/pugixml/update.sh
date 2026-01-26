51cff3edada16670573ac86eb0c8e0ebc84dbf24
Ben Boeckel <ben.boeckel@kitware.com>
2026-01-26 00:03:16 -0500

    pugixml: update for target names

--- ThirdParty/pugixml/update.sh
+++ ThirdParty/pugixml/update.sh
@@ -8,7 +8,7 @@ readonly name="pugixml"
 readonly ownership="Proj Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/pugixml.git"
-readonly tag="for/vtk/20250918-master-1.15-ge86ffc5"
+readonly tag="for/vtk-20260125-master-1.15-ge86ffc5"
 readonly paths="
 .gitattributes
 LICENSE.md
