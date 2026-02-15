e2ca65942c0504038495e5d3d4d9f264cd1b697f
Ben Boeckel <ben.boeckel@kitware.com>
2026-02-14 21:51:39 -0500

    expat: update for target name fix
    Fixes: #19946

--- ThirdParty/expat/update.sh
+++ ThirdParty/expat/update.sh
@@ -8,7 +8,7 @@ readonly name="expat"
 readonly ownership="Expat Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/expat.git"
-readonly tag="for/vtk-20250317-2.7.0"
+readonly tag="for/vtk-20260214-2.7.0"
 readonly paths="
 expat/.gitattributes
 expat/CMakeLists.txt
