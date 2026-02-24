Kitware/VTK

commit c7bdf447adad1134c2398bc2f38f1ec317f27023

Edit vtkSMPToolsImpl.cxx to fix openmp warning

Common/Core/SMP/OpenMP/vtkSMPToolsImpl.cxx
@@ -112,7 +112,7 @@ void vtkSMPToolsImplForOpenMP(vtkIdType first, vtkIdType last, vtkIdType grain,
     grain = (estimateGrain > 0) ? estimateGrain : 1;
   }
 
-  omp_set_nested(nestedActivated);
+  omp_set_max_active_levels(nestedActivated);
 
 #pragma omp single
   threadIdStack->emplace(omp_get_thread_num());

commit 1c3a37cdf37623beea5885996e9c0898244ffc8f

OpenMP/vtkSMPToolsImpl: restore support for OpenMP pre-5.0
`omp_set_nested` was deprecated in 5.1, but its replacement was only
introduced in 5.0. Restore support for compilers not yet
implementing 5.0 by using a preprocessor check.

Fixes: c7bdf447ad (Edit vtkSMPToolsImpl.cxx to fix openmp warning, 2025-09-16) (via !12466)
Closes: #18544

Common/Core/SMP/OpenMP/vtkSMPToolsImpl.cxx
@@ -114,7 +114,11 @@ void vtkSMPToolsImplForOpenMP(vtkIdType first, vtkIdType last, vtkIdType grain,
     grain = (estimateGrain > 0) ? estimateGrain : 1;
   }
 
+#if _OPENMP >= 201811 // 5.0
   omp_set_max_active_levels(nestedActivated);
+#else
+  omp_set_nested(nestedActivated);
+#endif
 
 #pragma omp single
   threadIdStack->emplace(omp_get_thread_num());