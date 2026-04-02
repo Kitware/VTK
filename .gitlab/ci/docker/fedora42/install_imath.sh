08f4309fd9c08da72bdf0e8b36e37ba5daa27b55
Spiros Tsalikis <spiros.tsalikis@kitware.com>
2026-04-02 13:05:16 -0400

    ci: update to imath 3.2.2

--- .gitlab/ci/docker/fedora42/install_imath.sh
+++ .gitlab/ci/docker/fedora42/install_imath.sh
@@ -2,25 +2,16 @@
 
 set -e
 
-readonly imath_version="3.2.1"
-readonly imath_tarball="v$imath_version.tar.gz"
-readonly imath_sha256sum="b2c8a44c3e4695b74e9644c76f5f5480767355c6f98cde58ba0e82b4ad8c63ce"
+readonly imath_repo="https://github.com/AcademySoftwareFoundation/Imath.git"
+readonly imath_commit="v3.2.2"
 
 readonly imath_root="$HOME/imath"
-
 readonly imath_src="$imath_root/src"
 readonly imath_build="$imath_root/build"
 
-mkdir -p "$imath_root" \
-    "$imath_src" "$imath_build"
-cd "$imath_root"
+git clone -b "$imath_commit" "$imath_repo" "$imath_src"
 
-echo "$imath_sha256sum  $imath_tarball" > imath.sha256sum
-curl -OL "https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/$imath_tarball"
-sha256sum --check imath.sha256sum
-
-tar -C "$imath_src" --strip-components=1 -xf "$imath_tarball"
-
+mkdir -p "$imath_build"
 cd "$imath_build"
 
 cmake -GNinja "$imath_src" \
