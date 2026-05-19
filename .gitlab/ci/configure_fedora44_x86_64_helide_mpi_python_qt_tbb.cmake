f4934da5853ecc48b2c900690f73c6975ac76221
Scott Wittenburg <scott.wittenburg@kitware.com>
2026-05-19 15:20:59 -0600

    ci: Update CI images to support testing Catalyst w/ ext Conduit
    - Install conduit flavors in /opt to keep them out of the way of
        existing build configurations.
    - Install catalyst w/ external conduit, also in /opt, to stay
        independent of existing catalyst installs.
    - Test catalyst with external conduit on linux:
      - x84_64: use extdeps build
      - aarch64: use the only build available

--- .gitlab/ci/configure_fedora44_x86_64_helide_mpi_python_qt_tbb.cmake
+++ .gitlab/ci/configure_fedora44_x86_64_helide_mpi_python_qt_tbb.cmake
@@ -9,6 +9,11 @@ set(VTK_MODULE_USE_EXTERNAL_VTK_token OFF CACHE BOOL "")
 set(VTK_MODULE_USE_EXTERNAL_VTK_verdict OFF CACHE BOOL "")
 set(VTK_MODULE_USE_EXTERNAL_VTK_vtkviskores OFF CACHE BOOL "")
 
-set(CMAKE_PREFIX_PATH "/opt/anari" CACHE STRING "")
+# Enable Catalyst and Conduit to test their external integration
+set(VTK_ENABLE_CATALYST ON CACHE BOOL "")
+set(VTK_MODULE_ENABLE_VTK_conduit YES CACHE STRING "")
+set(VTK_MODULE_ENABLE_VTK_fides YES CACHE STRING "")
+
+set(CMAKE_PREFIX_PATH "/opt/catalyst-ext/mpich;/opt/conduit/mpich;/opt/anari" CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora44.cmake")
