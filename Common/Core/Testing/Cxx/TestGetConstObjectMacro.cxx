ef02c5da159efe4207034d5fa9c0cd60667ea90a
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2026-05-22 18:23:28 -0400

    Fix compiler warning about overriding final

--- Common/Core/Testing/Cxx/TestGetConstObjectMacro.cxx
+++ Common/Core/Testing/Cxx/TestGetConstObjectMacro.cxx
@@ -10,7 +10,7 @@
 #include <iostream>
 
 // Test class using vtkGetConstObjectMacro
-class vtkTestConstObjectGetter final : public vtkObject
+class vtkTestConstObjectGetter : public vtkObject
 {
 public:
   static vtkTestConstObjectGetter* New();
@@ -21,7 +21,7 @@ public:
 
 protected:
   vtkTestConstObjectGetter() = default;
-  ~vtkTestConstObjectGetter() final { this->SetData(nullptr); }
+  ~vtkTestConstObjectGetter() override { this->SetData(nullptr); }
 
 private:
   vtkTestConstObjectGetter(const vtkTestConstObjectGetter&) = delete;
