d4720a697896087eb258d211a4eb6a54d8c5e960
Sean McBride <sean@rogue-research.com>
2026-06-03 13:40:03 -0400

    Removed dead code from obsolete VTK_MAJOR_VERSION check

--- IO/Image/vtkNIFTIImageReader.cxx
+++ IO/Image/vtkNIFTIImageReader.cxx
@@ -1051,11 +1051,7 @@ int vtkNIFTIImageReader::RequestData(vtkInformation* request,
 
   // get the data object, allocate memory
   vtkImageData* data = static_cast<vtkImageData*>(outInfo->Get(vtkDataObject::DATA_OBJECT()));
-#if VTK_MAJOR_VERSION >= 6
   this->AllocateOutputData(data, outInfo, extent);
-#else
-  this->AllocateOutputData(data, extent);
-#endif
 
   data->GetPointData()->GetScalars()->SetName("NIFTI");
 
