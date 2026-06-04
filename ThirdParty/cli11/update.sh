ec82cc7982215cbd9dbe5ff5d39fb6f0186b00a1
Cory Quammen <cory.quammen@kitware.com>
2026-06-04 13:27:05 -0400

    cli11: update vendored version to 2.8.1

--- ThirdParty/cli11/update.sh
+++ ThirdParty/cli11/update.sh
@@ -8,7 +8,7 @@ readonly name="cli11"
 readonly ownership="CLI11 Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/cli11.git"
-readonly tag="for/vtk-20250317-2.5.0"
+readonly tag="for/vtk-20260604-2.6.2"
 readonly paths="
 include/CLI/*.hpp
 include/CLI/impl/*.hpp
