5273836335c17fe131731b827b72920401890d51
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2026-06-10 15:42:44 -0400

    thirdparty: update viskores to v1.2.0-rc1

--- ThirdParty/viskores/update.sh
+++ ThirdParty/viskores/update.sh
@@ -8,7 +8,7 @@ readonly name="viskores"
 readonly ownership="Viskores upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name/$name"
 readonly repo="https://github.com/Viskores/viskores.git"
-readonly tag="v1.1.1"
+readonly tag="v1.2.0-rc1"
 readonly paths="
 CMake/testing/ViskoresTestWrappers.cmake
 CMake/FindPyexpander.cmake
@@ -40,10 +40,19 @@ viskores/
 viskoresstd/
 "
 
-
 extract_source () {
     git_archive
     pushd "$extractdir/$name-reduced"
+
+    # Apply formatting patch: hhttps://github.com/Viskores/viskores/pull/356
+    curl -OL https://github.com/Viskores/viskores/commit/970fcddbc328dc75496cfd384d6f1d917db01fc5.patch
+    patch -p1 < 970fcddbc328dc75496cfd384d6f1d917db01fc5.patch
+    rm 970fcddbc328dc75496cfd384d6f1d917db01fc5.patch
+
+    # VTK CI sets CMAKE_OSX_DEPLOYMENT_TARGET=10.10 which hides std::filesystem::absolute
+    # This patch use getcwd as opposed to std::filesystem::absolute in macos version < 10.15
+    patch -p5 < "${toplevel_dir}/ThirdParty/viskores/apple-std-absolute-alternative.patch"
+
     # This module which is not used by neither VTK or ParaView has very long
     # paths that can over the limit of characters for Windows NTFS paths.
     rm -rf viskores/filter/scalar_topology
