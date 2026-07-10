Kitware/VTK

commit 88a5da8c7ae554f955281fe7969026200c72b22a

vtkWindow::GetOffScreenRendering: Deprecate

Rendering/Parallel/vtkParallelRenderManager.cxx
@@ -1045,7 +1045,7 @@ void vtkParallelRenderManager::SetImageReductionFactorForUpdateRate(double desir
 //------------------------------------------------------------------------------
 void vtkParallelRenderManager::SetRenderWindowSize()
 {
-  if (!this->RenderWindow->GetOffScreenRendering())
+  if (!this->RenderWindow->GetShowWindow())
   {
     // Make sure we can support the requested image size.
     const int* screensize = this->RenderWindow->GetScreenSize();

Rendering/Core/Testing/Cxx/TestOffscreenIsOffscreen.cxx
@@ -9,5 +9,5 @@ int TestOffscreenIsOffscreen(int, char*[])
   vtkNew<vtkRenderWindow> renWin;
   // This test is only run if VTK_DEFAULT_RENDER_WINDOW_OFFSCREEN is on. So the default should
   // be to use offscreen rendering
-  return !renWin->GetOffScreenRendering();
+  return !renWin->GetShowWindow();
 }

Common/Core/vtkWindow.h
@@ -15,6 +15,7 @@
 #define vtkWindow_h
 
 #include "vtkCommonCoreModule.h" // For export macro
+#include "vtkDeprecation.h"      // For VTK_DEPRECATED_IN_9_8_0
 #include "vtkObject.h"
 #include "vtkWrappingHints.h" // For VTK_MARSHALAUTO
 
@@ -243,6 +244,7 @@ public:
    * instead.
    */
   VTK_MARSHALEXCLUDE(VTK_MARSHAL_EXCLUDE_REASON_IS_INTERNAL)
+  VTK_DEPRECATED_IN_9_8_0("Use GetShowWindow instead")
   vtkTypeBool GetOffScreenRendering() { return this->GetShowWindow() ? 0 : 1; }
 
   /**

Rendering/SceneGraph/vtkWindowNode.cxx
@@ -65,38 +65,9 @@ void vtkWindowNode::Synchronize(bool prepass)
     {
       return;
     }
-    /*
-      GetAAFrames()   vtkRenderWindow virtual
-      GetActualSize() vtkWindow
-      GetAlphaBitPlanes()     vtkRenderWindow virtual
-      GetDoubleBuffer()       vtkWindow       virtual
-      GetDPI()        vtkWindow       virtual
-      GetFDFrames()   vtkRenderWindow virtual
-      GetFullScreen() vtkRenderWindow virtual
-      GetLineSmoothing()      vtkRenderWindow virtual
-      GetMapped()     vtkWindow       virtual
-      GetMTime()      vtkObject       virtual
-      GetMultiSamples()       vtkRenderWindow virtual
-      GetNeverRendered()      vtkRenderWindow virtual
-      GetNumberOfLayers()     vtkRenderWindow virtual
-      GetOffScreenRendering() vtkWindow       virtual
-      GetPointSmoothing()     vtkRenderWindow virtual
-      GetPolygonSmoothing()   vtkRenderWindow virtual
-      GetPosition()   vtkWindow       virtual
-      GetScreenSize()=0       vtkWindow       pure virtual
-    */
     const int* sz = mine->GetSize();
     this->Size[0] = sz[0];
     this->Size[1] = sz[1];
-    /*
-      GetStereoType() vtkRenderWindow virtual
-      GetSubFrames()  vtkRenderWindow virtual
-      GetSwapBuffers()        vtkRenderWindow virtual
-      GetTileScale()  vtkWindow       virtual
-      GetTileViewport()       vtkWindow       virtual
-      GetUseConstantFDOffsets()       vtkRenderWindow virtual
-    */
-
     auto const& renderers = this->GetChildren();
     for (auto ren : renderers)
     {

commit 61f2faf8290f8944863b04d1b7fa265acfc45d2a

vtkWindow: Fix incorrect usage of GetShowWindow
Fix incorrect usage of GetShowWindow introduced in 88a5da8c7ae554f955281fe7969026200c72b22a

Rendering/Parallel/vtkParallelRenderManager.cxx
@@ -1045,7 +1045,7 @@ void vtkParallelRenderManager::SetImageReductionFactorForUpdateRate(double desir
 //------------------------------------------------------------------------------
 void vtkParallelRenderManager::SetRenderWindowSize()
 {
-  if (!this->RenderWindow->GetShowWindow())
+  if (this->RenderWindow->GetShowWindow())
   {
     // Make sure we can support the requested image size.
     const int* screensize = this->RenderWindow->GetScreenSize();

Rendering/Core/Testing/Cxx/TestOffscreenIsOffscreen.cxx
@@ -9,5 +9,6 @@ int TestOffscreenIsOffscreen(int, char*[])
   vtkNew<vtkRenderWindow> renWin;
   // This test is only run if VTK_DEFAULT_RENDER_WINDOW_OFFSCREEN is on. So the default should
   // be to use offscreen rendering
-  return !renWin->GetShowWindow();
+  // now tested using `GetShowWindow` because of API deprecation
+  return renWin->GetShowWindow();
 }