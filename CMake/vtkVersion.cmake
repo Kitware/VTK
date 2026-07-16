45dfcbc8bf97c755e8475d5d65dcfacf70d09910
Ben Boeckel <ben.boeckel@kitware.com>
2026-07-16 15:47:17 -0400

    vtkVersion: fix the version
    The robot's check for what is allowed into `release` was incorrect and
    allowed extra commits to come into `release` that should not have been.
    This merge basically reverted the version script to the `master` copy.
    The release checklist will be updated with the proper command to use.

--- CMake/vtkVersion.cmake
+++ CMake/vtkVersion.cmake
@@ -1,12 +1,12 @@
 # VTK version number components.
 set(VTK_MAJOR_VERSION 9)
-set(VTK_MINOR_VERSION 6)
+set(VTK_MINOR_VERSION 7)
 
 # Git conflict avoidance barrier. When branching for an `X.Y.0.rc1` branch, if
 # the date component is updated while it is in progress, the bump of the minor
 # version ends up conflicting with the patch version update.
 
-set(VTK_BUILD_VERSION 20260627)
+set(VTK_BUILD_VERSION 0)
 
 if (NOT VTK_MINOR_VERSION LESS 100)
   message(FATAL_ERROR
