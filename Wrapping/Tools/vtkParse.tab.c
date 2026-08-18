abfab215c11b2ad2330994ea660dcd3bdecf3136
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2026-08-18 14:38:05 -0400

    change env var for debug mode in vtkParse to vtkParse_DEBUG

--- Wrapping/Tools/vtkParse.tab.c
+++ Wrapping/Tools/vtkParse.tab.c
@@ -9898,7 +9898,7 @@ FileInfo* vtkParse_ParseFile(const char* filename, FILE* ifile, FILE* errfile)
   startSig();
 
   parseDebug = 0;
-  if (getenv("DEBUG") != NULL)
+  if (getenv("vtkParse_DEBUG") != NULL)
   {
     parseDebug = 1;
   }
