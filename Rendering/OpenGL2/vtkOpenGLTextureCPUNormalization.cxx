37cc1ac56a18902bf0a73a63dc9e8ab493571f2f
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2026-09-01 14:49:59 -0400

    Look up normalized float texture formats instead of computing them

--- Rendering/OpenGL2/vtkOpenGLTextureCPUNormalization.cxx
+++ Rendering/OpenGL2/vtkOpenGLTextureCPUNormalization.cxx
@@ -7,6 +7,40 @@
 
 VTK_ABI_NAMESPACE_BEGIN
 
+namespace
+{
+// The 32 bit float internal formats are not consecutive, so look them up.
+GLenum NormalizedFloatInternalFormat(int numComps)
+{
+  switch (numComps)
+  {
+    case 1:
+      return GL_R32F;
+    case 2:
+      return GL_RG32F;
+    case 3:
+      return GL_RGB32F;
+    default:
+      return GL_RGBA32F;
+  }
+}
+
+GLenum NormalizedFloatFormat(int numComps)
+{
+  switch (numComps)
+  {
+    case 1:
+      return GL_RED;
+    case 2:
+      return GL_RG;
+    case 3:
+      return GL_RGB;
+    default:
+      return GL_RGBA;
+  }
+}
+}
+
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkOpenGLTextureCPUNormalization);
 
@@ -48,21 +82,9 @@ bool vtkOpenGLTextureCPUNormalization::ConvertUShortToFloat(const void* sourceDa
 
   // Upload converted data
   glBindTexture(GL_TEXTURE_2D, targetTexture);
-  GLenum format = GL_RED;
-  if (numComps == 2)
-  {
-    format = GL_RG;
-  }
-  else if (numComps == 3)
-  {
-    format = GL_RGB;
-  }
-  else
-  {
-    format = GL_RGBA;
-  }
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F + (numComps - 1), static_cast<GLsizei>(width),
-    static_cast<GLsizei>(height), 0, format, GL_FLOAT, dst);
+  glTexImage2D(GL_TEXTURE_2D, 0, NormalizedFloatInternalFormat(numComps),
+    static_cast<GLsizei>(width), static_cast<GLsizei>(height), 0, NormalizedFloatFormat(numComps),
+    GL_FLOAT, dst);
 
   vtkOpenGLStaticCheckErrorMacro("Failed to upload converted texture data");
   return true;
@@ -93,21 +115,9 @@ bool vtkOpenGLTextureCPUNormalization::ConvertShortToFloat(const void* sourceDat
 
   // Upload converted data
   glBindTexture(GL_TEXTURE_2D, targetTexture);
-  GLenum format2 = GL_RED;
-  if (numComps == 2)
-  {
-    format2 = GL_RG;
-  }
-  else if (numComps == 3)
-  {
-    format2 = GL_RGB;
-  }
-  else
-  {
-    format2 = GL_RGBA;
-  }
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F + (numComps - 1), static_cast<GLsizei>(width),
-    static_cast<GLsizei>(height), 0, format2, GL_FLOAT, dst);
+  glTexImage2D(GL_TEXTURE_2D, 0, NormalizedFloatInternalFormat(numComps),
+    static_cast<GLsizei>(width), static_cast<GLsizei>(height), 0, NormalizedFloatFormat(numComps),
+    GL_FLOAT, dst);
 
   vtkOpenGLStaticCheckErrorMacro("Failed to upload converted texture data");
   return true;
