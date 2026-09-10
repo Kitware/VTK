52e1d9d993a99f94f0ffac5498cefa578cf6fe3e
Ben Boeckel <ben.boeckel@kitware.com>
2026-09-10 07:41:24 -0400

    ci: suppress warning about Conduit minimum versions

--- .gitlab/ci/configure_fedora44_x86_64_helide_mpi_python_qt_tbb.cmake
+++ .gitlab/ci/configure_fedora44_x86_64_helide_mpi_python_qt_tbb.cmake
@@ -15,5 +15,9 @@ set(VTK_MODULE_ENABLE_VTK_conduit YES CACHE STRING "")
 set(VTK_MODULE_ENABLE_VTK_fides YES CACHE STRING "")
 
 set(CMAKE_PREFIX_PATH "/opt/catalyst-ext/mpich;/opt/conduit/mpich;/opt/anari" CACHE STRING "")
+# Conduit has a `cmake_minimum_required(3.8)` call which warns with new CMake.
+# Add a phantom `...3.13` to suppress the warning.
+# https://github.com/llnl/conduit/issues/1697
+set(CMAKE_POLICY_VERSION_MINIMUM "3.13" CACHE STRING "")
 
 include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora44.cmake")
