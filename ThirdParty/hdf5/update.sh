6efd118f3b49c7ac1fa28443cc6b3ac71584591c
Louis Gombert <louis.gombert@kitware.com>
2026-09-16 16:49:21 +0200

    Update update.sh tag version for hdf5

--- ThirdParty/hdf5/update.sh
+++ ThirdParty/hdf5/update.sh
@@ -8,7 +8,7 @@ readonly name="hdf5"
 readonly ownership="HDF Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/hdf5.git"
-readonly tag="for/vtk-20260701-2.1.1"
+readonly tag="for/vtk-20260915-2.1.1"
 readonly paths="
 CMakeBuildOptions.cmake
 CMakeFilters.cmake
